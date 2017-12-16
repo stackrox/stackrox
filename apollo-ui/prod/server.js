ec5ead19c920ff18edcc97179c74d44b79d36716
Ivan Kornienko <i@stackrox.com>
2017-12-15 16:47:35 -0800

    AP-82: support dynamic URLs in express.js server

--- apollo-ui/prod/server.js
+++ apollo-ui/prod/server.js
@@ -4,10 +4,17 @@ var app = express();
 
 var endpoint = process.env.ROX_APOLLO_ENDPOINT || "apollo.apollo_net:8080";
 
-app.use(express.static('./build'));
+app.use(express.static(__dirname + '/build'));
+
 app.use('/v1', proxy({
     target: "https://" + endpoint,
     changeOrigin: true,
     secure: false
 }));
+
+// redirect all dynamically added URLs to index.html (let react app to handle 404.)
+app.get('/*', (req, res) => {
+    res.sendFile(__dirname + '/build/index.html');
+});
+
 app.listen(3000);
