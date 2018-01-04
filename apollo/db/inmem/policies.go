fdc7f66cd868f8481e467627bb7baaaa41478ff2
Connor Gorman <cgorman@stackrox.com>
2018-01-04 10:42:58 -0800

    Remove the inmem policies

--- apollo/db/inmem/policies.go
+++ apollo/db/inmem/policies.go
@@ -1,64 +1,32 @@
 package inmem
 
 import (
-	"fmt"
-	"sort"
-	"sync"
-
 	"bitbucket.org/stack-rox/apollo/apollo/db"
 	"bitbucket.org/stack-rox/apollo/pkg/api/generated/api/v1"
-	"github.com/golang/protobuf/proto"
 )
 
 type policyStore struct {
-	policies      map[string]*v1.Policy
-	policiesMutex sync.Mutex
-
-	persistent db.PolicyStorage
+	db.PolicyStorage
 }
 
 func newPolicyStore(persistent db.PolicyStorage) *policyStore {
 	return &policyStore{
-		policies:   make(map[string]*v1.Policy),
-		persistent: persistent,
+		PolicyStorage: persistent,
 	}
-}
-
-func (s *policyStore) clone(policy *v1.Policy) *v1.Policy {
-	return proto.Clone(policy).(*v1.Policy)
-}
-
-func (s *policyStore) loadFromPersistent() error {
-	s.policiesMutex.Lock()
-	defer s.policiesMutex.Unlock()
-	policies, err := s.persistent.GetPolicies(&v1.GetPoliciesRequest{})
-	if err != nil {
-		return err
-	}
-	for _, p := range policies {
-		s.policies[p.Name] = p
-	}
-	return nil
-}
-
-// GetPolicy returns a policy by name.
-func (s *policyStore) GetPolicy(name string) (p *v1.Policy, exist bool, err error) {
-	s.policiesMutex.Lock()
-	defer s.policiesMutex.Unlock()
-	p, exist = s.policies[name]
-	return
 }
 
 // GetPolicies returns policies according to request.
-func (s *policyStore) GetPolicies(request *v1.GetPoliciesRequest) (policies []*v1.Policy, err error) {
-	s.policiesMutex.Lock()
-	defer s.policiesMutex.Unlock()
-
+func (s *policyStore) GetPolicies(request *v1.GetPoliciesRequest) ([]*v1.Policy, error) {
+	policies, err := s.PolicyStorage.GetPolicies(request)
+	if err != nil {
+		return nil, err
+	}
 	namesSet := stringWrap(request.GetName()).asSet()
 	categoriesSet := categoriesWrap(request.GetCategory()).asSet()
 	severitiesSet := severitiesWrap(request.GetSeverity()).asSet()
 
-	for _, p := range s.policies {
+	filteredPolicies := policies[:0]
+	for _, p := range policies {
 		if len(request.GetDisabled()) == 1 && p.GetDisabled() != request.GetDisabled()[0] {
 			continue
 		}
@@ -75,11 +43,9 @@ func (s *policyStore) GetPolicies(request *v1.GetPoliciesRequest) (policies []*v
 			continue
 		}
 
-		policies = append(policies, s.clone(p))
+		filteredPolicies = append(filteredPolicies, p)
 	}
-
-	sort.SliceStable(policies, func(i, j int) bool { return policies[i].Name < policies[j].Name })
-	return
+	return filteredPolicies, nil
 }
 
 func (s *policyStore) matchCategories(alertCategories []v1.Policy_Category, categorySet map[v1.Policy_Category]struct{}) bool {
@@ -90,44 +56,4 @@ func (s *policyStore) matchCategories(alertCategories []v1.Policy_Category, cate
 	}
 
 	return false
-}
-
-// AddPolicy adds the policy to the database.
-func (s *policyStore) AddPolicy(policy *v1.Policy) error {
-	s.policiesMutex.Lock()
-	defer s.policiesMutex.Unlock()
-	if _, ok := s.policies[policy.Name]; ok {
-		return fmt.Errorf("policy with name %v already exists and cannot be added again", policy.Name)
-	}
-	if err := s.persistent.AddPolicy(policy); err != nil {
-		return err
-	}
-	s.upsertPolicy(policy)
-	return nil
-}
-
-// UpdatePolicy updates the policy.
-func (s *policyStore) UpdatePolicy(policy *v1.Policy) error {
-	s.policiesMutex.Lock()
-	defer s.policiesMutex.Unlock()
-	if err := s.persistent.UpdatePolicy(policy); err != nil {
-		return err
-	}
-	s.upsertPolicy(policy)
-	return nil
-}
-
-func (s *policyStore) upsertPolicy(policy *v1.Policy) {
-	s.policies[policy.Name] = s.clone(policy)
-}
-
-// RemovePolicy removes the policy.
-func (s *policyStore) RemovePolicy(name string) error {
-	s.policiesMutex.Lock()
-	defer s.policiesMutex.Unlock()
-	if err := s.persistent.RemovePolicy(name); err != nil {
-		return err
-	}
-	delete(s.policies, name)
-	return nil
 }
