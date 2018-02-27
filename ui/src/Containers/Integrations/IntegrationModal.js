stackrox/stackrox

commit 37aa21954393e5b977b75c233aded6aa33061051

Introduce Redux for storing alerts / violations The commit is focused on adding Redux support incrementally, main changes are   * introduce Redux to handle /main/violations route   * use redux-saga for managing side-effects   * use normalizr to keep entities separated   * use reselect to memoize Redux selectors   * utilize ducks structure (with reducer, action types, actions and selectors co-existing in one file)
Merged in ivan/rfc-introduce-redux-violations (pull request #246)

ui/src/Containers/Integrations/IntegrationModal.js
@@ -11,7 +11,7 @@ import Modal from 'Components/Modal';
 import Table from 'Components/Table';
 import Panel from 'Components/Panel';
 import tableColumnDescriptor from 'Containers/Integrations/tableColumnDescriptor';
-import AuthService from 'Providers/AuthService';
+import AuthService from 'services/AuthService';
 
 const sourceMap = {
     authProviders: {
@@ -405,9 +405,9 @@ const api = {
     authProviders: {
         save: data =>
             data.id !== undefined && data.id !== ''
-                ? axios.put(`/v1/authProviders/${data.id}`, data)
+                ? axios.put(`/v1/authservices/${data.id}`, data)
                 : axios.post('/v1/authProviders', data),
-        delete: data => axios.delete(`/v1/authProviders/${data.id}`)
+        delete: data => axios.delete(`/v1/authservices/${data.id}`)
     },
     registries: {
         save: data =>

ui/src/Components/AuthRedirectRoute.js
@@ -4,7 +4,7 @@ import { withRouter } from 'react-router-dom';
 import queryString from 'query-string';
 import { ClipLoader } from 'react-spinners';
 
-import AuthService from 'Providers/AuthService';
+import AuthService from 'services/AuthService';
 
 class AuthRedirectRoute extends Component {
     static propTypes = {

ui/src/Components/ProtectedRoute.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
-import AuthService from 'Providers/AuthService';
+import AuthService from 'services/AuthService';
 
 const ProtectedRoute = ({ ...rest }) => {
     const to = {

ui/src/Containers/AppPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import ProtectedRoute from 'Components/ProtectedRoute';
 import AuthRedirectRoute from 'Components/AuthRedirectRoute';
@@ -8,16 +8,14 @@ import MainPage from 'Containers/MainPage';
 import LoginPage from 'Containers/Login/LoginPage';
 
 const AppPage = () => (
-    <Router>
-        <Auth>
-            <Switch>
-                <ProtectedRoute path="/main" component={MainPage} />
-                <Route path="/login" component={LoginPage} />
-                <AuthRedirectRoute path="/auth/response/oidc" />
-                <Redirect from="/" to="/main" />
-            </Switch>
-        </Auth>
-    </Router>
+    <Auth>
+        <Switch>
+            <ProtectedRoute path="/main" component={MainPage} />
+            <Route path="/login" component={LoginPage} />
+            <AuthRedirectRoute path="/auth/response/oidc" />
+            <Redirect from="/" to="/main" />
+        </Switch>
+    </Auth>
 );
 
 export default AppPage;

ui/src/Containers/Auth.js
@@ -5,7 +5,7 @@ import { withRouter } from 'react-router-dom';
 import { ClipLoader } from 'react-spinners';
 import axios from 'axios';
 
-import AuthService from 'Providers/AuthService';
+import AuthService from 'services/AuthService';
 
 const excludedUrls = ['/login', '/auth/response/oidc'];
 

ui/src/Containers/Compliance/BenchmarksPage.js
@@ -8,7 +8,7 @@ import { sortNumber } from 'sorters/sorters';
 import Table from 'Components/Table';
 import Select from 'Components/Select';
 import BenchmarksSidePanel from 'Containers/Compliance/BenchmarksSidePanel';
-import * as service from 'Providers/BenchmarksService';
+import * as service from 'services/BenchmarksService';
 
 const reducer = (action, prevState, nextState) => {
     switch (action) {

ui/src/Containers/Compliance/CompliancePage.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import Tabs from 'Components/Tabs';
 import TabContent from 'Components/TabContent';
 import BenchmarksPage from 'Containers/Compliance/BenchmarksPage';
-import { fetchBenchmarks } from 'Providers/BenchmarksService';
+import { fetchBenchmarks } from 'services/BenchmarksService';
 
 class CompliancePage extends Component {
     constructor(props) {

ui/src/Containers/Dashboard/DashboardPage.js
@@ -20,8 +20,8 @@ import TwoLevelPieChart from 'Components/visuals/TwoLevelPieChart';
 import CustomLineChart from 'Components/visuals/CustomLineChart';
 import DashboardBenchmarks from 'Containers/Dashboard/DashboardBenchmarks';
 import SeverityTile from 'Containers/Dashboard/SeverityTile';
-import { fetchBenchmarks, fetchLastScan } from 'Providers/BenchmarksService';
-import fetchClusters from 'Providers/ClustersService';
+import { fetchBenchmarks, fetchLastScan } from 'services/BenchmarksService';
+import fetchClusters from 'services/ClustersService';
 
 import { severityLabels } from 'messages/common';
 

commit 9c2ace5d34341a832cde19e9c50027f561a0e81c

Fix URL for getting auth providers The bug is introduced by commit 37aa21954393e5b977b75c233aded6aa33061051 (find/replace).
Merged in ivan/fix-auth-providers-bug (pull request #266)

ui/src/Containers/Integrations/IntegrationModal.js
@@ -405,9 +405,9 @@ const api = {
     authProviders: {
         save: data =>
             data.id !== undefined && data.id !== ''
-                ? axios.put(`/v1/authservices/${data.id}`, data)
+                ? axios.put(`/v1/authProviders/${data.id}`, data)
                 : axios.post('/v1/authProviders', data),
-        delete: data => axios.delete(`/v1/authservices/${data.id}`)
+        delete: data => axios.delete(`/v1/authProviders/${data.id}`)
     },
     registries: {
         save: data =>