5b448415e606db6ccf7a09b2428bbbb1f3dfc83c
Connor Gorman <cgorman@stackrox.com>
2018-04-13 18:41:27 +0000

    [Testing] Upload logs from central and sensor to circleci

---
+++ scripts/ci/collect-service-logs.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -eu
+
+# Collect Service Logs script
+#
+# Extracts service logs from the given Kubernetes cluster and saves them off for
+# future examination.
+#
+# Usage:
+#   collect-service-logs.sh SERVICE
+#
+# Example:
+# $ ./scripts/ci/collect-service-logs.sh central
+#
+# Assumptions:
+# - Must be called from the root of the Apollo git repository.
+# - Logs are saved under /tmp/k8s-service-logs/
+
+main() {
+    service_name="$1"
+    if [ -z "$service_name" ]; then
+        echo no service specified
+        return 1
+    fi
+
+    log_dir="/tmp/k8s-service-logs/${service_name}"
+    mkdir -p "$log_dir"
+
+    kubectl describe "deploy/${service_name}" -n stackrox > "${log_dir}/describe.log"
+    kubectl     logs "deploy/${service_name}" -n stackrox > "${log_dir}/logs.log"
+}
+
+main "$@"
