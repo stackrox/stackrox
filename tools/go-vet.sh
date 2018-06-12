f4d29beeebc137380757c68b48dc4145b08bb119
Viswajith Venugopal <vv@stackrox.com>
2018-06-12 21:44:02 +0000

    Make tools/go-vet.sh work with newer versions of go vet.
    * The script was broken by the newer version of `go vet`, since `go vet` stopped echoing "Exit status $?" lines. Update it to work both with the new and old versions of `go vet`.
    * This is a direct port of a commit made in the `stackrox` repo.
    
    Merged in viswa/fix-go-vet (pull request #532)
    Approved-by: Connor Gorman <cgorman@stackrox.com>

--- tools/go-vet.sh
+++ tools/go-vet.sh
@@ -4,24 +4,28 @@
 ignored=0
 total=0
 status=0
+
 while read -r line; do
     if [[ "$line" =~ ^exit\ status\ ([[:digit:]]+)$ ]]; then
         status="${BASH_REMATCH[1]}"
         continue
     fi
-    total=$((total + 1))
-    if [[ "$line" =~ ^([^:]+):([[:digit:]]+): ]]; then
-        filename="${BASH_REMATCH[1]}"
-        lineno="${BASH_REMATCH[2]}"
-        line_in_file=$(tail -n+"$lineno" "$filename" | head -n1)
-        if [[ "$line_in_file" =~ //\ NOVET$ ]]; then
-            echo >&2 "(${line} -- IGNORED due to NOVET annotation)"
-            ignored=$((ignored + 1))
-            continue
+
+    line_to_echo="${line}"
+    if [[ "$line" != \#* ]]; then # Ignore comment lines
+        total=$((total + 1))
+        if [[ "$line" =~ ^([^:]+):([[:digit:]]+): ]]; then
+            filename="${BASH_REMATCH[1]}"
+            lineno="${BASH_REMATCH[2]}"
+            line_in_file=$(tail -n+"$lineno" "$filename" | head -n1)
+            if [[ "$line_in_file" =~ //\ NOVET$ ]]; then
+                line_to_echo="(${line} -- IGNORED due to NOVET annotation)"
+                ignored=$((ignored + 1))
+            fi
         fi
     fi
-    echo >&2 "$line"
-done < <(go vet "$@" 2>&1)
+    echo >&2 "${line_to_echo}"
+done < <(go vet "$@" 2>&1; echo "exit status $?")
 
 echo "Found ${total} errors, ignored ${ignored}"
 if (( total == ignored )); then
