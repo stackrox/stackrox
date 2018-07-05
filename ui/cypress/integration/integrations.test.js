stackrox/stackrox

commit 2b5108c325cf6005f2e22fdd26f2df8d27678598

Stamp VERSION file into the Dockerfile. Code stamping is preferred, but more difficult
Approved-by: Ivan Kornienko <i@stackrox.com>

ui/src/Containers/Integrations/Clusters/ClusterEditForm.js
@@ -12,18 +12,18 @@ const CommonFields = () => (
         <FormField label="Name" required>
             <ReduxTextField name="name" placeholder="Cluster name" />
         </FormField>
-        <FormField label="Image name (Prevent location)" required>
-            <ReduxTextField
-                name="preventImage"
-                placeholder="stackrox.io/prevent:[current-version]"
-            />
-        </FormField>
     </React.Fragment>
 );
 
-const K8sFields = () => (
+const K8sFields = ({ metadata }) => (
     <React.Fragment>
         <CommonFields />
+        <FormField label="Prevent Image" required>
+            <ReduxTextField
+                name="preventImage"
+                placeholder={`stackrox.io/prevent:${metadata.version}`}
+            />
+        </FormField>
         <FormField label="Central API Endpoint" required>
             <ReduxTextField name="centralApiEndpoint" placeholder="central.stackrox:443" />
         </FormField>
@@ -36,9 +36,21 @@ const K8sFields = () => (
     </React.Fragment>
 );
 
-const OpenShiftFields = () => (
+K8sFields.propTypes = {
+    metadata: PropTypes.shape({ version: PropTypes.string }).isRequired
+};
+
+const OpenShiftFields = ({ metadata }) => (
     <React.Fragment>
         <CommonFields />
+        <FormField label="Prevent Image" required>
+            <ReduxTextField
+                name="preventImage"
+                placeholder={`docker-registry.default.svc.local:5000/stackrox/prevent:${
+                    metadata.version
+                }`}
+            />
+        </FormField>
         <FormField label="Central API Endpoint" required>
             <ReduxTextField name="centralApiEndpoint" placeholder="central.stackrox:443" />
         </FormField>
@@ -48,9 +60,19 @@ const OpenShiftFields = () => (
     </React.Fragment>
 );
 
-const DockerFields = () => (
+OpenShiftFields.propTypes = {
+    metadata: PropTypes.shape({ version: PropTypes.string }).isRequired
+};
+
+const DockerFields = ({ metadata }) => (
     <React.Fragment>
         <CommonFields />
+        <FormField label="Prevent Image" required>
+            <ReduxTextField
+                name="preventImage"
+                placeholder={`stackrox.io/prevent:${metadata.version}`}
+            />
+        </FormField>
         <FormField label="Central API Endpoint" required>
             <ReduxTextField name="centralApiEndpoint" placeholder="central.prevent_net:443" />
         </FormField>
@@ -60,23 +82,71 @@ const DockerFields = () => (
     </React.Fragment>
 );
 
+DockerFields.propTypes = {
+    metadata: PropTypes.shape({ version: PropTypes.string }).isRequired
+};
+
 const clusterFields = {
     SWARM_CLUSTER: DockerFields,
     OPENSHIFT_CLUSTER: OpenShiftFields,
     KUBERNETES_CLUSTER: K8sFields
 };
 
-const ClusterEditForm = ({ clusterType }) => {
+const ClusterEditForm = ({ clusterType, metadata }) => {
     const ClusterFields = clusterFields[clusterType];
     if (!ClusterFields) throw new Error(`Unknown cluster type "${clusterType}"`);
     return (
         <form className="p-4 w-full mb-8" data-test-id="cluster-form">
-            <ClusterFields />
+            <ClusterFields metadata={metadata} />
         </form>
     );
 };
 ClusterEditForm.propTypes = {
-    clusterType: PropTypes.oneOf(clusterTypes).isRequired
+    clusterType: PropTypes.oneOf(clusterTypes).isRequired,
+    metadata: PropTypes.shape({ version: PropTypes.string }).isRequired
 };
 
-export default reduxForm({ form: clusterFormId })(ClusterEditForm);
+const ConnectedForm = reduxForm({ form: clusterFormId })(ClusterEditForm);
+
+const initialValuesFactories = {
+    SWARM_CLUSTER: metadata => ({
+        preventImage: `stackrox.io/prevent:${metadata.version}`,
+        centralApiEndpoint: 'central.prevent_net:443'
+    }),
+    OPENSHIFT_CLUSTER: metadata => ({
+        preventImage: `docker-registry.default.svc.local:5000/stackrox/prevent:${metadata.version}`,
+        centralApiEndpoint: 'central.stackrox:443',
+        openshift: {
+            params: {
+                namespace: 'stackrox'
+            }
+        }
+    }),
+    KUBERNETES_CLUSTER: metadata => ({
+        preventImage: `stackrox.io/prevent:${metadata.version}`,
+        centralApiEndpoint: 'central.stackrox:443',
+        openshift: {
+            params: {
+                namespace: 'stackrox',
+                imagePullSecret: 'stackrox'
+            }
+        }
+    })
+};
+
+const FormWrapper = ({ metadata, clusterType }) => {
+    const initialValues = initialValuesFactories[clusterType](metadata);
+    return (
+        <ConnectedForm
+            clusterType={clusterType}
+            metadata={metadata}
+            initialValues={initialValues}
+        />
+    );
+};
+FormWrapper.propTypes = {
+    clusterType: PropTypes.oneOf(clusterTypes).isRequired,
+    metadata: PropTypes.shape({ version: PropTypes.string }).isRequired
+};
+
+export default FormWrapper;

ui/src/Containers/Integrations/Clusters/ClusterWizardPanel.js
@@ -12,7 +12,7 @@ import { downloadClusterYaml } from 'services/ClustersService';
 
 import Panel from 'Components/Panel';
 import PanelButton from 'Components/PanelButton';
-import ClusterEditForm from './ClusterEditForm';
+import FormWrapper from './ClusterEditForm';
 import ClusterDeploymentPage from './ClusterDeploymentPage';
 
 class ClusterWizardPanel extends Component {
@@ -25,7 +25,8 @@ class ClusterWizardPanel extends Component {
         }),
         currentPage: PropTypes.oneOf(Object.values(wizardPages)).isRequired,
         onFinish: PropTypes.func.isRequired,
-        onNext: PropTypes.func.isRequired
+        onNext: PropTypes.func.isRequired,
+        metadata: PropTypes.shape({ version: PropTypes.string }).isRequired
     };
 
     static defaultProps = {
@@ -77,9 +78,10 @@ class ClusterWizardPanel extends Component {
         switch (this.props.currentPage) {
             case wizardPages.FORM:
                 return (
-                    <ClusterEditForm
+                    <FormWrapper
                         clusterType={this.props.clusterType}
                         initialValues={formInitialValues}
+                        metadata={this.props.metadata}
                     />
                 );
             case wizardPages.DEPLOYMENT:
@@ -123,7 +125,8 @@ const getCluster = createSelector(
 
 const mapStateToProps = createStructuredSelector({
     cluster: getCluster,
-    currentPage: selectors.getWizardCurrentPage
+    currentPage: selectors.getWizardCurrentPage,
+    metadata: selectors.getMetadata
 });
 
 const mapDispatchToProps = {

ui/cypress/integration/integrations.test.js
@@ -106,7 +106,7 @@ describe('Cluster Creation Flow', () => {
         cy
             .get(selectors.readOnlyView)
             .eq(2)
-            .should('have.text', 'Image name (Prevent location):stackrox/prevent:latest');
+            .should('have.text', 'Prevent Image:stackrox/prevent:latest');
 
         cy
             .get(selectors.readOnlyView)

.gitignore
@@ -23,6 +23,7 @@ deps
 report.xml
 test.log
 protoc-*
+image/VERSION
 
 # Bazel
 bazel-*

Makefile
@@ -75,10 +75,11 @@ API_SERVICES += benchmark_schedule_service
 API_SERVICES += benchmark_service
 API_SERVICES += benchmark_trigger_service
 API_SERVICES += cluster_service
+API_SERVICES += deployment_service
 API_SERVICES += dnr_integration_service
 API_SERVICES += image_integration_service
-API_SERVICES += deployment_service
 API_SERVICES += image_service
+API_SERVICES += metadata_service
 API_SERVICES += notifier_service
 API_SERVICES += ping_service
 API_SERVICES += policy_service
@@ -186,11 +187,13 @@ image: gazelle clean-image
 	cp bazel-bin/benchmark-bootstrap/linux_amd64_pure_stripped/benchmark-bootstrap image/bin/benchmark-bootstrap
 	cp bazel-bin/sensor/swarm/linux_amd64_pure_stripped/swarm image/bin/swarm-sensor
 	cp bazel-bin/sensor/kubernetes/linux_amd64_pure_stripped/kubernetes image/bin/kubernetes-sensor
+	echo "$(TAG)" > image/VERSION
 	chmod +w image/bin/*
 	docker build -t stackrox/prevent:$(TAG) image/
 	docker build -t stackrox/prevent-health:$(TAG) prometheus/container
 	@echo "Built images with tag: $(TAG)"
 	@echo "You may wish to:       export PREVENT_IMAGE_TAG=$(TAG)"
+
 
 ###########
 ## Clean ##

api/v1/metadata_service.proto
@@ -1,0 +1,22 @@
+syntax = "proto3";
+
+option go_package = "v1";
+
+import "google/api/annotations.proto";
+import "google/protobuf/empty.proto";
+
+package v1;
+
+message Metadata {
+    string version = 1;
+}
+
+service MetadataService {
+
+    rpc GetMetadata(google.protobuf.Empty) returns (Metadata) {
+        option (google.api.http) = {
+            get: "/v1/metadata"
+        };
+    }
+
+}

central/main.go
@@ -33,6 +33,7 @@ import (
 	iiService "bitbucket.org/stack-rox/apollo/central/imageintegration/service"
 	interceptorSingletons "bitbucket.org/stack-rox/apollo/central/interceptor/singletons"
 	logimbueHandler "bitbucket.org/stack-rox/apollo/central/logimbue/handler"
+	metadataService "bitbucket.org/stack-rox/apollo/central/metadata/service"
 	"bitbucket.org/stack-rox/apollo/central/metrics"
 	notifierService "bitbucket.org/stack-rox/apollo/central/notifier/service"
 	pingService "bitbucket.org/stack-rox/apollo/central/ping/service"
@@ -101,6 +102,7 @@ func (c *central) startGRPCServer() {
 	c.server.Register(dnrIntegrationService.Singleton())
 	c.server.Register(imageService.Singleton())
 	c.server.Register(iiService.Singleton())
+	c.server.Register(metadataService.New())
 	c.server.Register(notifierService.Singleton())
 	c.server.Register(pingService.Singleton())
 	c.server.Register(policyService.Singleton())

central/metadata/service/service.go
@@ -1,0 +1,29 @@
+package service
+
+import (
+	"bitbucket.org/stack-rox/apollo/generated/api/v1"
+	"bitbucket.org/stack-rox/apollo/pkg/logging"
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+var (
+	log = logging.LoggerForModule()
+)
+
+// Service is the GRPC service interface that provides the entry point for processing deployment events.
+type Service interface {
+	RegisterServiceServer(grpcServer *grpc.Server)
+	RegisterServiceHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) error
+
+	AuthFuncOverride(ctx context.Context, fullMethodName string) (context.Context, error)
+
+	GetMetadata(context.Context, *empty.Empty) (*v1.Metadata, error)
+}
+
+// New returns a new instance of service.
+func New() Service {
+	return &serviceImpl{}
+}

commit 356f9d2ccf97587712bae9827e82ff635eb0a602

AP-784: Fix cluster form initial values The regression was introduced by commit 2b5108c325cf6005f2e22fdd26f2df8d27678598.
Merged in ivan/AP-784-fix-cluser-form (pull request #597)

ui/src/Containers/Integrations/Clusters/ClusterEditForm.js
@@ -134,19 +134,28 @@ const initialValuesFactories = {
     })
 };
 
-const FormWrapper = ({ metadata, clusterType }) => {
-    const initialValues = initialValuesFactories[clusterType](metadata);
+const FormWrapper = ({ metadata, clusterType, initialValues }) => {
+    const combinedInitialValues = {
+        ...initialValuesFactories[clusterType](metadata),
+        type: clusterType,
+        ...initialValues // passed initial values can override anything
+    };
+
     return (
         <ConnectedForm
             clusterType={clusterType}
             metadata={metadata}
-            initialValues={initialValues}
+            initialValues={combinedInitialValues}
         />
     );
 };
 FormWrapper.propTypes = {
     clusterType: PropTypes.oneOf(clusterTypes).isRequired,
-    metadata: PropTypes.shape({ version: PropTypes.string }).isRequired
+    metadata: PropTypes.shape({ version: PropTypes.string }).isRequired,
+    initialValues: PropTypes.shape({})
+};
+FormWrapper.defaultProps = {
+    initialValues: {}
 };
 
 export default FormWrapper;

ui/src/Containers/Integrations/Clusters/ClusterWizardPanel.js
@@ -12,7 +12,7 @@ import { downloadClusterYaml } from 'services/ClustersService';
 
 import Panel from 'Components/Panel';
 import PanelButton from 'Components/PanelButton';
-import FormWrapper from './ClusterEditForm';
+import ClusterEditForm from './ClusterEditForm';
 import ClusterDeploymentPage from './ClusterDeploymentPage';
 
 class ClusterWizardPanel extends Component {
@@ -26,11 +26,14 @@ class ClusterWizardPanel extends Component {
         currentPage: PropTypes.oneOf(Object.values(wizardPages)).isRequired,
         onFinish: PropTypes.func.isRequired,
         onNext: PropTypes.func.isRequired,
-        metadata: PropTypes.shape({ version: PropTypes.string }).isRequired
+        metadata: PropTypes.shape({ version: PropTypes.string })
     };
 
     static defaultProps = {
-        cluster: null
+        cluster: null,
+        metadata: {
+            version: 'latest'
+        }
     };
 
     componentWillUnmount() {
@@ -70,17 +73,12 @@ class ClusterWizardPanel extends Component {
     }
 
     renderPage() {
-        const formInitialValues = {
-            type: this.props.clusterType,
-            ...this.props.cluster
-        };
-
         switch (this.props.currentPage) {
             case wizardPages.FORM:
                 return (
-                    <FormWrapper
+                    <ClusterEditForm
                         clusterType={this.props.clusterType}
-                        initialValues={formInitialValues}
+                        initialValues={this.props.cluster}
                         metadata={this.props.metadata}
                     />
                 );

ui/cypress/integration/integrations.test.js
@@ -67,7 +67,7 @@ describe('Cluster Creation Flow', () => {
         cy.fixture('clusters/single.json').as('singleCluster');
         cy.route('GET', api.clusters.list, '@singleCluster').as('clusters');
         cy.route('POST', api.clusters.zip, {}).as('download');
-        cy.route('POST', api.clusters.list, { cluster: { id: 'newCluster1' } }).as('addCluster');
+        cy.route('POST', api.clusters.list).as('addCluster');
         cy.visit('/');
         cy.get(selectors.configure).click();
         cy.get(selectors.navLink).click();
@@ -114,35 +114,47 @@ describe('Cluster Creation Flow', () => {
             .should('have.text', 'Central API Endpoint:central.stackrox:443');
     });
 
-    it('Should be able to fill out the Swarm form, download config files and see cluster checked-in', () => {
+    it.only('Should be able to fill out the Swarm form, download config files and see cluster checked-in', () => {
         cy.get(selectors.dockerSwarmTile).click();
 
         cy.get(selectors.buttons.add).click();
 
-        cy.get(selectors.clusterForm.nameInput).type('Swarm Cluster 1');
+        const clusterName = 'Swarm Cluster TestInstance';
+        cy.get(selectors.clusterForm.nameInput).type(clusterName);
         cy.get(selectors.clusterForm.imageInput).type('stackrox/prevent:latest');
         cy.get(selectors.clusterForm.endpointInput).type('central.prevent_net:443');
 
         cy.get(selectors.buttons.next).click();
-        cy.wait('@addCluster');
-
-        cy.get(selectors.buttons.download).click();
-        cy.wait('@download');
-
-        cy.get('div:contains("Waiting for the cluster to check-in successfully...")');
-
-        // make cluster to "check-in" by adding "lastContact"
         cy
-            .route('GET', `${api.clusters.list}/newCluster1`, {
-                cluster: {
-                    id: 'newCluster1',
-                    lastContact: '2018-06-25T19:12:44.955289Z'
-                }
-            })
-            .as('getCluster');
-        cy.wait('@getCluster');
-        cy.get(
-            'div:contains("Success! The cluster has been recognized properly by Prevent. You may now save the configuration.")'
-        );
+            .wait('@addCluster')
+            .its('responseBody')
+            .then(response => {
+                const clusterId = response.cluster.id;
+
+                cy.get(selectors.buttons.download).click();
+                cy.wait('@download');
+
+                cy.get('div:contains("Waiting for the cluster to check-in successfully...")');
+
+                // make cluster to "check-in" by adding "lastContact"
+                cy
+                    .route('GET', `${api.clusters.list}/${clusterId}`, {
+                        cluster: {
+                            id: clusterId,
+                            lastContact: '2018-06-25T19:12:44.955289Z'
+                        }
+                    })
+                    .as('getCluster');
+                cy.wait('@getCluster');
+                cy.get(
+                    'div:contains("Success! The cluster has been recognized properly by Prevent. You may now save the configuration.")'
+                );
+
+                // clean up after the test by deleting the cluster
+                cy.get(`table tr:contains("${clusterName}") td input[type="checkbox"]`).check();
+                cy.get(selectors.buttons.delete).click();
+                cy.get(selectors.buttons.confirm).click();
+                cy.get(`table tr:contains("${clusterName}")`).should('not.exist');
+            });
     });
 });