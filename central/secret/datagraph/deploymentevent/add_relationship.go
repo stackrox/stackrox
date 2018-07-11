1e180280d661780a65a4d89d6b9e6da7274a5954
Robert Stark <rs@stackrox.com>
2018-07-11 21:45:22 +0000

    Merged in rs-SearchFix (pull request #626)
    Update secret relationship modification to use sort.Search correctly.
    
    * Update secret relationship maintanance to use sort.Search correctly.
    
    * add early exit
    
    * handle no match condition.
    
    Approved-by: Viswajith Venugopal <vv@stackrox.com>
    Approved-by: Connor Gorman <cgorman@stackrox.com>

--- central/secret/datagraph/deploymentevent/add_relationship.go
+++ central/secret/datagraph/deploymentevent/add_relationship.go
@@ -32,70 +32,66 @@ func stapleNewRelationships(stapleTo, stapleFrom *v1.SecretRelationship) {
 
 func stapleContainerRelationships(stapleTo, stapleFrom *v1.SecretRelationship) {
 	for _, relationship := range stapleFrom.ContainerRelationships {
-		// Check if the relationship already exists, and replace it if so.
-		currentLength := len(stapleTo.ContainerRelationships)
-		pos := sort.Search(currentLength, func(n int) bool {
-			return relationship.GetId() == stapleTo.ContainerRelationships[n].GetId()
-		})
-		if pos != currentLength {
-			stapleTo.ContainerRelationships[pos] = stapleFrom.ContainerRelationships[pos]
-			continue
-		}
-
-		// Search for where the relationships should be inserted since it is not present.
-		pos = sort.Search(currentLength, func(n int) bool {
-			if n == 0 && relationship.GetId() < stapleTo.ContainerRelationships[n].GetId() {
-				return true
-			} else if n == currentLength-1 && relationship.GetId() > stapleTo.ContainerRelationships[n].GetId() {
-				return true
-			}
-			return relationship.GetId() < stapleTo.ContainerRelationships[n].GetId() && relationship.GetId() > stapleTo.ContainerRelationships[n-1].GetId()
-		})
-
-		// Replace slice with a new slice that has the new relationship inserted.
-		var newSlice []*v1.SecretContainerRelationship
-		if pos != 0 {
-			newSlice = stapleTo.ContainerRelationships[0:pos]
-		}
-		newSlice = append(newSlice, relationship)
-		if pos != currentLength {
-			newSlice = append(newSlice, stapleTo.ContainerRelationships[pos:currentLength]...)
-		}
-		stapleTo.ContainerRelationships = newSlice
+		stapleContainerRelationship(stapleTo, relationship)
 	}
+}
+
+func stapleContainerRelationship(stapleTo *v1.SecretRelationship, relationship *v1.SecretContainerRelationship) {
+	// Check if the relationship already exists, and replace it if so.
+	currentLength := len(stapleTo.ContainerRelationships)
+
+	// Search for where the relationships should be inserted since it is not present.
+	pos := sort.Search(currentLength, func(n int) bool {
+		return relationship.GetId() <= stapleTo.ContainerRelationships[n].GetId()
+	})
+
+	// Container id already present, so just overwrite.
+	if pos != currentLength && relationship.GetId() == stapleTo.ContainerRelationships[pos].GetId() {
+		stapleTo.ContainerRelationships[pos] = relationship
+		return
+	}
+
+	// Replace slice with a new slice that has the new relationship inserted.
+	var newSlice []*v1.SecretContainerRelationship
+	if pos != 0 {
+		newSlice = stapleTo.ContainerRelationships[0:pos]
+	}
+	newSlice = append(newSlice, relationship)
+	if pos != currentLength {
+		newSlice = append(newSlice, stapleTo.ContainerRelationships[pos:currentLength]...)
+	}
+	stapleTo.ContainerRelationships = newSlice
 }
 
 func stapleDeploymentRelationships(stapleTo, stapleFrom *v1.SecretRelationship) {
 	for _, relationship := range stapleFrom.DeploymentRelationships {
-		// Check if the relationship already exists, and replace it if so.
-		currentLength := len(stapleTo.DeploymentRelationships)
-		pos := sort.Search(currentLength, func(n int) bool {
-			return relationship.GetId() == stapleTo.DeploymentRelationships[n].GetId()
-		})
-		if pos != currentLength {
-			stapleTo.DeploymentRelationships[pos] = stapleFrom.DeploymentRelationships[pos]
-			continue
-		}
-
-		// Search for where the relationships should be inserted since it is not present.
-		pos = sort.Search(currentLength, func(n int) bool {
-			if n == 0 && relationship.GetId() < stapleTo.DeploymentRelationships[n].GetId() {
-				return true
-			} else if n == currentLength-1 && relationship.GetId() > stapleTo.DeploymentRelationships[n].GetId() {
-				return true
-			}
-			return relationship.GetId() < stapleTo.DeploymentRelationships[n].GetId() && relationship.GetId() > stapleTo.DeploymentRelationships[n-1].GetId()
-		})
-
-		// Replace slice with a new slice that has the new relationship inserted.
-		var newSlice []*v1.SecretDeploymentRelationship
-		if pos != 0 {
-			newSlice = stapleTo.DeploymentRelationships[0:pos]
-		}
-		newSlice = append(newSlice, relationship)
-		if pos != currentLength {
-			newSlice = append(newSlice, stapleTo.DeploymentRelationships[pos:currentLength]...)
-		}
-		stapleTo.DeploymentRelationships = newSlice
+		stapleDeploymentRelationship(stapleTo, relationship)
 	}
+}
+
+func stapleDeploymentRelationship(stapleTo *v1.SecretRelationship, relationship *v1.SecretDeploymentRelationship) {
+	// Check if the relationship already exists, and replace it if so.
+	currentLength := len(stapleTo.DeploymentRelationships)
+
+	// Search for where the relationships should be inserted since it is not present.
+	pos := sort.Search(currentLength, func(n int) bool {
+		return relationship.GetId() <= stapleTo.DeploymentRelationships[n].GetId()
+	})
+
+	// Deployment id already present, so just overwrite.
+	if pos != currentLength && relationship.GetId() == stapleTo.DeploymentRelationships[pos].GetId() {
+		stapleTo.DeploymentRelationships[pos] = relationship
+		return
+	}
+
+	// Replace slice with a new slice that has the new relationship inserted.
+	var newSlice []*v1.SecretDeploymentRelationship
+	if pos != 0 {
+		newSlice = stapleTo.DeploymentRelationships[0:pos]
+	}
+	newSlice = append(newSlice, relationship)
+	if pos != currentLength {
+		newSlice = append(newSlice, stapleTo.DeploymentRelationships[pos:currentLength]...)
+	}
+	stapleTo.DeploymentRelationships = newSlice
 }
--- central/secret/datagraph/deploymentevent/remove_relationship.go
+++ central/secret/datagraph/deploymentevent/remove_relationship.go
@@ -30,48 +30,60 @@ func removeRelationships(removeFrom, toRemove *v1.SecretRelationship) {
 
 func removeContainerRelationships(removeFrom, toRemove *v1.SecretRelationship) {
 	for _, relationship := range toRemove.ContainerRelationships {
-		// Search for the container relationship we want to remove.
-		currentLength := len(removeFrom.ContainerRelationships)
-		pos := sort.Search(currentLength, func(n int) bool {
-			return relationship.GetId() == removeFrom.ContainerRelationships[n].GetId()
-		})
-		if pos == currentLength {
-			// Relationship isn't present, so nothing to do.
-			return
-		}
-
-		// Build a new slice without the container relationship we want to remove.
-		var newSlice []*v1.SecretContainerRelationship
-		if pos > 0 {
-			newSlice = removeFrom.ContainerRelationships[0:pos]
-		}
-		if pos < currentLength-1 {
-			newSlice = append(newSlice, removeFrom.ContainerRelationships[pos+1:currentLength]...)
-		}
-		removeFrom.ContainerRelationships = newSlice
+		removeContainerRelationship(removeFrom, relationship)
 	}
+}
+
+func removeContainerRelationship(removeFrom *v1.SecretRelationship, relationship *v1.SecretContainerRelationship) {
+	// Search for the container relationship we want to remove.
+	currentLength := len(removeFrom.ContainerRelationships)
+	// Search for where the relationships should be inserted since it is not present.
+	pos := sort.Search(currentLength, func(n int) bool {
+		return relationship.GetId() <= removeFrom.ContainerRelationships[n].GetId()
+	})
+
+	// Container id already present, so just overwrite.
+	if pos == currentLength || relationship.GetId() != removeFrom.ContainerRelationships[pos].GetId() {
+		return
+	}
+
+	// Build a new slice without the container relationship we want to remove.
+	var newSlice []*v1.SecretContainerRelationship
+	if pos > 0 {
+		newSlice = removeFrom.ContainerRelationships[0:pos]
+	}
+	if pos < currentLength-1 {
+		newSlice = append(newSlice, removeFrom.ContainerRelationships[pos+1:currentLength]...)
+	}
+	removeFrom.ContainerRelationships = newSlice
 }
 
 func removeDeploymentRelationships(removeFrom, toRemove *v1.SecretRelationship) {
 	for _, relationship := range toRemove.DeploymentRelationships {
-		// Search for the deployment relationship we want to remove.
-		currentLength := len(removeFrom.DeploymentRelationships)
-		pos := sort.Search(currentLength, func(n int) bool {
-			return relationship.GetId() == removeFrom.DeploymentRelationships[n].GetId()
-		})
-		if pos == currentLength {
-			// Relationship isn't present, so nothing to do.
-			return
-		}
-
-		// Build a new slice without the deployment relationship we want to remove.
-		var newSlice []*v1.SecretDeploymentRelationship
-		if pos > 0 {
-			newSlice = removeFrom.DeploymentRelationships[0:pos]
-		}
-		if pos < currentLength-1 {
-			newSlice = append(newSlice, removeFrom.DeploymentRelationships[pos+1:currentLength]...)
-		}
-		removeFrom.DeploymentRelationships = newSlice
+		removeDeploymentRelationship(removeFrom, relationship)
 	}
+}
+
+func removeDeploymentRelationship(removeFrom *v1.SecretRelationship, relationship *v1.SecretDeploymentRelationship) {
+	// Search for the container relationship we want to remove.
+	currentLength := len(removeFrom.DeploymentRelationships)
+	// Search for where the relationships should be inserted since it is not present.
+	pos := sort.Search(currentLength, func(n int) bool {
+		return relationship.GetId() <= removeFrom.DeploymentRelationships[n].GetId()
+	})
+
+	// Deployment id already present, so just overwrite.
+	if pos == currentLength || relationship.GetId() != removeFrom.DeploymentRelationships[pos].GetId() {
+		return
+	}
+
+	// Build a new slice without the container relationship we want to remove.
+	var newSlice []*v1.SecretDeploymentRelationship
+	if pos > 0 {
+		newSlice = removeFrom.DeploymentRelationships[0:pos]
+	}
+	if pos < currentLength-1 {
+		newSlice = append(newSlice, removeFrom.DeploymentRelationships[pos+1:currentLength]...)
+	}
+	removeFrom.DeploymentRelationships = newSlice
 }
