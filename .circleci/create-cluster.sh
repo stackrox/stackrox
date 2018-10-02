48bc6995d50f8746e04406a9aa187d146f47121b
Connor Gilbert <connorgilbert@gmail.com>
2018-10-01 23:18:59 -0700

    CI: Fix GKE subnet size to avoid claiming /14s (stackrox/rox#340)
    GKE clusters can sometimes fail to provision because of contention
    for 10.0.0.0/8 in the stackrox-ci project. Reduce the size of the
    subnet requests and explicitly specify subnet sizes for nodes,
    pods, and services to stave off such issues.
    
    Also, delete clusters before retrying their creation, since
    the name will be the same as the previous attempt.

--- .circleci/create-cluster.sh
+++ .circleci/create-cluster.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
+### Network Sizing ###
+# The overall subnetwork ("--create-subnetwork") is used for nodes.
+# The "cluster" secondary range is for pods ("--cluster-ipv4-cidr").
+# The "services" secondary range is for ClusterIP services ("--services-ipv4-cidr").
+# See https://cloud.google.com/kubernetes-engine/docs/how-to/alias-ips#cluster_sizing.
+
 gcloud container clusters create \
     --machine-type n1-standard-2 \
     --num-nodes 3 \
-    --create-subnetwork range=/19 \
+    --create-subnetwork range=/28 \
+    --cluster-ipv4-cidr=/20 \
+    --services-ipv4-cidr=/24 \
     --enable-ip-alias \
     --enable-network-policy \
     --image-type UBUNTU \
