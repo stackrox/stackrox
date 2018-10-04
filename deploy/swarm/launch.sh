dd10d4290cee8f1e4392cc845af363917e5c5a9e
Yathi Naik <yathi@stackrox.com>
2018-10-04 09:34:48 -0700

    ROX-500: Read env for runtime policies (stackrox/rox#366)

--- deploy/swarm/launch.sh
+++ deploy/swarm/launch.sh
@@ -12,7 +12,14 @@ function launch_central {
     OLD_DOCKER_TLS_VERIFY="$DOCKER_TLS_VERIFY"
     unset DOCKER_HOST DOCKER_CERT_PATH DOCKER_TLS_VERIFY
 
-    docker run --rm "$PREVENT_IMAGE" deploy swarm -i "$PREVENT_IMAGE" -p 8080 none > "$SWARM_DIR/central.zip"
+    flags=()
+    if [[ "${ROX_RUNTIME_POLICIES}" = "true" ]]; then
+      flags=(--flags ROX_RUNTIME_POLICIES)
+    fi
+
+    set -u
+
+    docker run --rm "$PREVENT_IMAGE" ${flags[@]+"${flags[@]}"} deploy swarm -i "$PREVENT_IMAGE" -p 8080 none > "$SWARM_DIR/central.zip"
 
     export DOCKER_HOST="$OLD_DOCKER_HOST"
     export DOCKER_CERT_PATH="$OLD_DOCKER_CERT_PATH"
