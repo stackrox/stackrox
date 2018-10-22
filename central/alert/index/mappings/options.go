9a190741bb1734ae31bb61773da9f83b01af87df
Connor Gorman <connorgorman@users.noreply.github.com>
2018-10-22 13:57:35 -0700

    ROX-54: Slim down violations search fields (stackrox/rox#528)

--- central/alert/index/mappings/options.go
+++ central/alert/index/mappings/options.go
@@ -13,20 +13,14 @@ var OptionsMap = map[search.FieldLabel]*v1.SearchField{
 	search.LifecycleStage: search.NewLifecycleField(v1.SearchCategory_ALERTS, "alert.lifecycle_stage"),
 	search.Enforcement:    search.NewEnforcementField(v1.SearchCategory_ALERTS, "alert.enforcement.action"),
 
-	search.PolicyID:   search.NewStringField(v1.SearchCategory_ALERTS, "alert.policy.id"),
+	search.PolicyID:   search.NewField(v1.SearchCategory_ALERTS, "alert.policy.id", v1.SearchDataType_SEARCH_STRING, search.OptionHidden),
 	search.PolicyName: search.NewStringField(v1.SearchCategory_ALERTS, "alert.policy.name"),
 	search.Category:   search.NewStringField(v1.SearchCategory_ALERTS, "alert.policy.categories"),
 	search.Severity:   search.NewSeverityField(v1.SearchCategory_ALERTS, "alert.policy.severity"),
 
-	search.DeploymentID:   search.NewStringField(v1.SearchCategory_ALERTS, "alert.deployment.id"),
+	search.DeploymentID:   search.NewField(v1.SearchCategory_ALERTS, "alert.deployment.id", v1.SearchDataType_SEARCH_STRING, search.OptionHidden),
 	search.Cluster:        search.NewStringField(v1.SearchCategory_ALERTS, "alert.deployment.cluster_name"),
 	search.Namespace:      search.NewStringField(v1.SearchCategory_ALERTS, "alert.deployment.namespace"),
 	search.Label:          search.NewMapField(v1.SearchCategory_ALERTS, "alert.deployment.labels"),
 	search.DeploymentName: search.NewStringField(v1.SearchCategory_ALERTS, "alert.deployment.name"),
-	search.Privileged:     search.NewBoolField(v1.SearchCategory_ALERTS, "alert.deployment.containers.security_context.privileged"),
-
-	search.ImageName:     search.NewStringField(v1.SearchCategory_ALERTS, "alert.deployment.containers.image.name.full_name"),
-	search.ImageRegistry: search.NewStringField(v1.SearchCategory_ALERTS, "alert.deployment.containers.image.name.registry"),
-	search.ImageRemote:   search.NewStringField(v1.SearchCategory_ALERTS, "alert.deployment.containers.image.name.remote"),
-	search.ImageTag:      search.NewStringField(v1.SearchCategory_ALERTS, "alert.deployment.containers.image.name.tag"),
 }
