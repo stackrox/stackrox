81569c2461356929bc78e48665c3a10fa4518372
Viswajith Venugopal <viswajithiii@gmail.com>
2018-11-03 19:17:23 -0700

    ROX-724 Specify namespace when giving Clairify image pull secrets (stackrox/rox#635)

--- image/templates/openshift/clairify.sh
+++ image/templates/openshift/clairify.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-oc secrets add serviceaccount/clairify secrets/stackrox --for=pull
+oc -n "{{.K8sConfig.Namespace}}" secrets add serviceaccount/clairify secrets/stackrox --for=pull
 
 oc create -f "$DIR/clairify.yaml"
