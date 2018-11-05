5f4accae7d5963871ee83b8ff55cef869441ae11
Josh Komoroske <jdkomo@gmail.com>
2018-11-05 13:57:16 -0800

    Improvements to Central port-forward scripts (stackrox/rox#625)

--- image/templates/kubernetes/port-forward.sh
+++ image/templates/kubernetes/port-forward.sh
@@ -1,17 +1,19 @@
-#!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+#!/bin/sh
 
-if [[ -z "$1" ]]; then
-	echo "usage: bash port-forward.sh 8443"
+if [ -z "$1" ]; then
+	echo "usage: $0 8443"
 	echo "The above would forward localhost:8443 to central:443."
 	exit 1
 fi
 
-until [ "$(kubectl get pod -n {{.K8sConfig.Namespace}} --selector 'app=central' | grep Running | wc -l)" -eq 1 ]; do
-    echo -n .
+printf 'Connecting...'
+while true; do
+    central_pod="$(kubectl get pod -n '{{.K8sConfig.Namespace}}' --selector 'app=central' --field-selector 'status.phase=Running' --output 'jsonpath={.items..metadata.name}' 2>/dev/null)"
+    [ -z  "$central_pod" ] || break
+    printf '.'
     sleep 1
 done
+echo
 
-export CENTRAL_POD="$(kubectl get pod -n {{.K8sConfig.Namespace}} --selector 'app=central' --output=jsonpath='{.items..metadata.name} {.items..status.phase}' | grep Running | cut -f 1 -d ' ')"
-kubectl port-forward -n "{{.K8sConfig.Namespace}}" "${CENTRAL_POD}" "$1:443" > /dev/null &
-echo "Access central on localhost:$1"
+nohup kubectl port-forward -n '{{.K8sConfig.Namespace}}' "$central_pod" "$1:443" 1>/dev/null 2>&1 &
+echo "Access central on https://localhost:$1"
