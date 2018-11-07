b09ac74ff2b5ad5dbb7bfc44d9bfa537c4887f65
Connor Gorman <connorgorman@users.noreply.github.com>
2018-11-07 11:33:29 -0800

    Add downsampling to InfluxDB at 12h, 2w, forever intervals (stackrox/rox#647)

--- monitoring/grafana-entrypoint.sh
+++ monitoring/grafana-entrypoint.sh
@@ -6,15 +6,41 @@ cat <<EOF > /etc/grafana/provisioning/datasources/influxdb.yaml
 apiVersion: 1
 
 datasources:
-- name: InfluxDB
+- name: InfluxDB 12h
   type: influxdb
   access: proxy
   orgId: 1
   url: https://localhost:8086
   password: ${PASSWORD}
   user: telegraf
-  database: telegraf
+  database: telegraf_12h
   isDefault: true
+  jsonData:
+    tlsSkipVerify: true
+  version: 1
+  editable: false
+- name: InfluxDB 2w
+  type: influxdb
+  access: proxy
+  orgId: 1
+  url: https://localhost:8086
+  password: ${PASSWORD}
+  user: telegraf
+  database: telegraf_2w
+  isDefault: false
+  jsonData:
+    tlsSkipVerify: true
+  version: 1
+  editable: false
+- name: InfluxDB forever
+  type: influxdb
+  access: proxy
+  orgId: 1
+  url: https://localhost:8086
+  password: ${PASSWORD}
+  user: telegraf
+  database: telegraf_forever
+  isDefault: false
   jsonData:
     tlsSkipVerify: true
   version: 1
--- monitoring/influx-post-start.sh
+++ monitoring/influx-post-start.sh
@@ -1,9 +1,26 @@
 #! /bin/sh
 
+# let influx get to a boot up point. Should be a query to see if its alive with a max timeout
+sleep 10
+
 INFLUXDB_ADMIN_USER=telegraf
 INFLUXDB_ADMIN_PASSWORD=$(cat /run/secrets/stackrox.io/monitoring/client/password)
 
+# create the default user
 QUERY="CREATE USER \"$INFLUXDB_ADMIN_USER\" WITH PASSWORD '$INFLUXDB_ADMIN_PASSWORD' WITH ALL PRIVILEGES"
-sleep 10
-
 /influx -ssl -unsafeSsl -execute "$QUERY"
+
+runQuery()
+{
+    /influx -ssl -unsafeSsl -username "$INFLUXDB_ADMIN_USER" -password "$INFLUXDB_ADMIN_PASSWORD" -execute "$1"
+}
+
+# Create the databases
+runQuery "CREATE DATABASE \"telegraf_12h\" WITH DURATION 12h NAME \"12_hours\""
+runQuery "CREATE DATABASE \"telegraf_2w\" WITH DURATION 2w NAME \"2_weeks\""
+runQuery "CREATE DATABASE \"telegraf_forever\""
+
+# Create the continuous queries for down sampling
+runQuery "CREATE CONTINUOUS QUERY \"telegraf_downsample_1m\" ON \"telegraf_12h\" BEGIN SELECT min(*), max(*), mean(*) INTO \"telegraf_2w\".\"2_weeks\".:MEASUREMENT FROM /.*/ GROUP BY time(1m),* END"
+runQuery "CREATE CONTINUOUS QUERY \"telegraf_downsample_10m\" ON \"telegraf_12h\" BEGIN SELECT min(*), max(*), mean(*) INTO \"telegraf_forever\".\"autogen\".:MEASUREMENT FROM /.*/ GROUP BY time(10m),* END"
+
