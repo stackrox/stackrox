4db846960b77365ceb3716f537a8c6199f0a1daa
Ivan Kornienko <ivan.s.kornienko@gmail.com>
2018-12-15 22:19:48 -0800

    Fix execution of Cypress tests locally (stackrox/rox#871)
    Extract basic auth creds / auth token retrieval logic and use it for cypress launch locally and via CI.

---
+++ scripts/k8s/export-basic-auth-creds.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Finds basic auth credentials in the deploy dir and exports ROX_USERNAME and 
+# ROX_PASSWORD. The script is intented to be sourced (for the parent shell to
+# get access to the exported vars). Deploy dir is passed as a parameter.
+
+usage() {
+  echo "Usage: $0 <deploy_dir>"
+  exit 2
+}
+
+DEPLOY_DIR=$1
+[ -n "${DEPLOY_DIR}" ] || usage
+
+if [ -f ${DEPLOY_DIR}/central-deploy/password ]; then
+    password="$(< ${DEPLOY_DIR}/central-deploy/password)"
+    export ROX_USERNAME=admin
+    export ROX_PASSWORD="$password"
+else
+    echo "Expected to find file ${DEPLOY_DIR}/central-deploy/password"
+    exit 1
+fi
---
+++ ui/scripts/get-auth-token.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Makes a request to create a new auth token that is printed to stdout.
+# Env vars ROX_USERNAME and ROX_PASSWORD are expected to be set for basic auth,
+# otherwise k8s deployment assumed from where basic auth creds are retrieved.
+
+api_endpoint="${UI_BASE_URL:-https://localhost:8000}"
+
+if [[ -z "$ROX_USERNAME" || -z "$ROX_PASSWORD" ]]; then
+  # basic auth creds weren't set (e.g. by CI), assume local k8s deployment
+  source ../scripts/k8s/export-basic-auth-creds.sh ../deploy/k8s
+fi
+
+if [[ -n "$ROX_USERNAME" && -n "$ROX_PASSWORD" ]]; then
+  rox_auth_token="$(
+  curl -sk -u "${ROX_USERNAME}:${ROX_PASSWORD}" "${api_endpoint}/v1/apitokens/generate" \
+    -X POST \
+    -d '{"name": "ui_tests", "role": "Admin"}' \
+    | jq -r '.token // ""')"
+else
+  echo >&2 "Expected ROX_USERNAME and ROX_PASSWORD env vars for basic auth creds"
+  exit 1
+fi
+
+if [[ -z "$rox_auth_token" ]]; then
+  echo >&2 "Could not issue an auth token"
+  exit 1
+fi
+
+echo $rox_auth_token
