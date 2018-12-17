50556faf820ad070099ed647a2b04e14c206a698
Robby Cochran <kudzoo@gmail.com>
2018-12-17 13:16:13 -0800

    Fix gcp unit test that fails when building on gcp dev vm (stackrox/rox#882)

--- pkg/gcp/metadata_test.go
+++ pkg/gcp/metadata_test.go
@@ -3,10 +3,13 @@ package gcp
 import (
 	"testing"
 
+	"cloud.google.com/go/compute/metadata"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestNotOnGCP(t *testing.T) {
-	_, err := GetMetadata()
-	assert.NoError(t, err)
+	if !metadata.OnGCE() {
+		_, err := GetMetadata()
+		assert.NoError(t, err)
+	}
 }
