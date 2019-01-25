e7420b849929945c297b11c34d3bcd7b5706fefc
Yathi Naik <yathi@stackrox.com>
2019-01-25 13:31:32 -0800

    Added better checks for SSH (stackrox/rox#1071)

--- central/compliance/checks/nist800-190/check412/check.go
+++ central/compliance/checks/nist800-190/check412/check.go
@@ -1,37 +1,101 @@
 package check412
 
 import (
+	"strings"
+
 	"github.com/stackrox/rox/central/compliance/checks/common"
 	"github.com/stackrox/rox/central/compliance/framework"
+	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/logging"
 )
 
 const (
 	standardID = "NIST_800_190:4_1_2"
 )
 
+var (
+	log = logging.New("NIST_800_190:4_1_2")
+)
+
 func init() {
 	framework.MustRegisterNewCheck(
 		standardID,
 		framework.ClusterKind,
-		[]string{"Policies", "ImageIntegrations"},
+		[]string{"Policies", "ImageIntegrations", "ProcessIndicators", "Deployments"},
 		func(ctx framework.ComplianceContext) {
 			checkNIST412(ctx)
 		})
 }
 
 func checkNIST412(ctx framework.ComplianceContext) {
-	checkSSHPolicies(ctx)
+	checkSSHPortAndProcesses(ctx)
 	checkPrivilegedCategoryPolicies(ctx)
 	common.IsImageScannerInUse(ctx)
 	common.CheckBuildTimePolicyEnforced(ctx)
 }
 
-func checkSSHPolicies(ctx framework.ComplianceContext) {
-	common.CheckPolicyInUse(ctx, "Secure Shell (ssh) Port Exposed")
-	common.CheckPolicyInUse(ctx, "Secure Shell Server (sshd) Execution")
+func checkSSHPortAndProcesses(ctx framework.ComplianceContext) {
+	// Map process indicators to deployments.
+	deploymentIDToIndicators := make(map[string][]*storage.ProcessIndicator)
+	for _, indicator := range ctx.Data().ProcessIndicators() {
+		indicators := deploymentIDToIndicators[indicator.GetDeploymentId()]
+		indicators = append(indicators, indicator)
+		deploymentIDToIndicators[indicator.GetDeploymentId()] = indicators
+	}
+
+	framework.ForEachDeployment(ctx, func(ctx framework.ComplianceContext, deployment *storage.Deployment) {
+		if deploymentHasSSHProcess(deploymentIDToIndicators, deployment) && !sshPolicyEnforced(ctx) {
+			framework.Fail(ctx, "Deployment has ssh process running and no policy to enforce against them.")
+		} else if deploymentHasSSHProcess(deploymentIDToIndicators, deployment) && sshPolicyEnforced(ctx) {
+			log.Errorf("SSH Policy is being enforced but found ssh process running in deployment %s", deployment.GetId())
+			framework.Fail(ctx, "Deployment has ssh process running.")
+		} else {
+			framework.Pass(ctx, "Deployment has no ssh process running.")
+		}
+	})
 }
 
 func checkPrivilegedCategoryPolicies(ctx framework.ComplianceContext) {
 	common.CheckAnyPolicyInCategoryEnforced(ctx, "Privileges")
 	common.CheckAnyPolicyInCategoryEnforced(ctx, "Vulnerability Management")
+}
+
+// deploymentHasSSHProcess returns true if the deployment has ssh process running.
+func deploymentHasSSHProcess(deploymentToIndicators map[string][]*storage.ProcessIndicator, deployment *storage.Deployment) bool {
+	for deploymentID, indicators := range deploymentToIndicators {
+		if deploymentID != deployment.GetId() {
+			continue
+		}
+		for _, indicator := range indicators {
+			if strings.Contains(indicator.GetSignal().GetExecFilePath(), "ssh") ||
+				strings.Contains(indicator.GetSignal().GetExecFilePath(), "sshd") {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// sshPolicyEnforced checks if there is a policy to detect and enforce
+// ssh daemons running in deployments.
+func sshPolicyEnforced(ctx framework.ComplianceContext) bool {
+	policies := ctx.Data().Policies()
+	for _, p := range policies {
+		a := common.NewAnder(doesPolicyHaveSSH(p), common.IsPolicyEnabled(p), common.IsPolicyEnforced(p))
+		if a.Execute() {
+			return true
+		}
+	}
+
+	return false
+}
+
+func doesPolicyHaveSSH(policy *storage.Policy) common.Andable {
+	return func() bool {
+		if policy.GetFields() != nil && policy.GetFields().GetProcessPolicy() != nil &&
+			policy.GetFields().GetProcessPolicy().GetName() == "sshd" {
+			return true
+		}
+		return false
+	}
 }
--- central/compliance/checks/nist800-190/check412/check_test.go
+++ central/compliance/checks/nist800-190/check412/check_test.go
@@ -22,9 +22,19 @@ var (
 	testDeployments = []*storage.Deployment{
 		{
 			Id: uuid.NewV4().String(),
+			Containers: []*storage.Container{
+				{
+					Name: "container1",
+				},
+			},
 		},
 		{
 			Id: uuid.NewV4().String(),
+			Containers: []*storage.Container{
+				{
+					Name: "container2",
+				},
+			},
 		},
 	}
 
@@ -47,12 +57,57 @@ var (
 		EnforcementActions: []storage.EnforcementAction{storage.EnforcementAction_SCALE_TO_ZERO_ENFORCEMENT},
 	}
 
-	sshPolicy = storage.Policy{
+	buildPolicy = storage.Policy{
 		Id:                 uuid.NewV4().String(),
-		Name:               "Secure Shell (ssh) Port Exposed",
-		Categories:         []string{"Security Best Practices"},
-		Disabled:           false,
-		EnforcementActions: []storage.EnforcementAction{storage.EnforcementAction_FAIL_BUILD_ENFORCEMENT},
+		Name:               "Sample build time policy",
+		LifecycleStages:    []storage.LifecycleStage{storage.LifecycleStage_BUILD},
+		EnforcementActions: []storage.EnforcementAction{storage.EnforcementAction_SCALE_TO_ZERO_ENFORCEMENT},
+	}
+
+	indicatorsWithSSH = []*storage.ProcessIndicator{
+		{
+			Id:            uuid.NewV4().String(),
+			DeploymentId:  testDeployments[0].GetId(),
+			ContainerName: testDeployments[0].GetContainers()[0].GetName(),
+			Signal: &storage.ProcessSignal{
+				Pid:          15,
+				Name:         "ssh",
+				ExecFilePath: "/usr/bin/ssh",
+			},
+		},
+		{
+			Id:            uuid.NewV4().String(),
+			DeploymentId:  testDeployments[1].GetId(),
+			ContainerName: testDeployments[1].GetContainers()[0].GetName(),
+			Signal: &storage.ProcessSignal{
+				Pid:          32,
+				Name:         "sshd",
+				ExecFilePath: "/bin/sshd",
+			},
+		},
+	}
+
+	indicatorsWithoutSSH = []*storage.ProcessIndicator{
+		{
+			Id:            uuid.NewV4().String(),
+			DeploymentId:  testDeployments[0].GetId(),
+			ContainerName: testDeployments[0].GetContainers()[0].GetName(),
+			Signal: &storage.ProcessSignal{
+				Pid:          15,
+				Name:         "ssh",
+				ExecFilePath: "/bin/bash",
+			},
+		},
+		{
+			Id:            uuid.NewV4().String(),
+			DeploymentId:  testDeployments[1].GetId(),
+			ContainerName: testDeployments[1].GetContainers()[0].GetName(),
+			Signal: &storage.ProcessSignal{
+				Pid:          32,
+				Name:         "sshd",
+				ExecFilePath: "/bin/zsh",
+			},
+		},
 	}
 
 	privPolicyDisabled = storage.Policy{
@@ -62,8 +117,40 @@ var (
 		EnforcementActions: []storage.EnforcementAction{storage.EnforcementAction_SCALE_TO_ZERO_ENFORCEMENT},
 	}
 
-	imageIntegration = storage.ImageIntegration{
-		Name: "Clairify",
+	imageIntegrations = []*storage.ImageIntegration{
+		{
+			Name: "Clairify",
+			Categories: []storage.ImageIntegrationCategory{
+				storage.ImageIntegrationCategory_SCANNER,
+			},
+		},
+		{
+			Name: "DTR",
+			Categories: []storage.ImageIntegrationCategory{
+				storage.ImageIntegrationCategory_REGISTRY,
+			},
+		},
+	}
+
+	sshPolicy = storage.Policy{
+		Id:   uuid.NewV4().String(),
+		Name: "SSH Policy",
+		Fields: &storage.PolicyFields{
+			ProcessPolicy: &storage.ProcessPolicy{
+				Name: "sshd",
+			},
+		},
+		EnforcementActions: []storage.EnforcementAction{storage.EnforcementAction_SCALE_TO_ZERO_ENFORCEMENT},
+	}
+
+	sshPolicyWithNoEnforcement = storage.Policy{
+		Id:   uuid.NewV4().String(),
+		Name: "SSH Policy",
+		Fields: &storage.PolicyFields{
+			ProcessPolicy: &storage.ProcessPolicy{
+				Name: "sshd",
+			},
+		},
 	}
 )
 
@@ -77,6 +164,7 @@ func TestNIST412_Success(t *testing.T) {
 	policies := make(map[string]*storage.Policy)
 	policies[cvssPolicyEnabledAndEnforced.GetName()] = &cvssPolicyEnabledAndEnforced
 	policies[sshPolicy.GetName()] = &sshPolicy
+	policies[buildPolicy.GetName()] = &buildPolicy
 
 	categoryPolicies := make(map[string]set.StringSet)
 	policySet := set.NewStringSet()
@@ -85,16 +173,18 @@ func TestNIST412_Success(t *testing.T) {
 
 	privSet := set.NewStringSet()
 	privSet.Add(cvssPolicyEnabledAndEnforced.Name)
-	categoryPolicies["Vulnerability Management"] = privSet
+	categoryPolicies["Privileges"] = privSet
 
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 
 	data := complianceMocks.NewMockComplianceDataRepository(mockCtrl)
 	data.EXPECT().Cluster().AnyTimes().Return(testCluster)
+	data.EXPECT().Deployments().AnyTimes().Return(toMap(testDeployments))
 	data.EXPECT().Policies().AnyTimes().Return(policies)
 	data.EXPECT().PolicyCategories().AnyTimes().Return(categoryPolicies)
-	data.EXPECT().ImageIntegrations().AnyTimes().Return([]*storage.ImageIntegration{&imageIntegration})
+	data.EXPECT().ImageIntegrations().AnyTimes().Return(imageIntegrations)
+	data.EXPECT().ProcessIndicators().AnyTimes().Return(indicatorsWithoutSSH)
 
 	run, err := framework.NewComplianceRun(check)
 	require.NoError(t, err)
@@ -104,13 +194,21 @@ func TestNIST412_Success(t *testing.T) {
 	results := run.GetAllResults()
 	checkResults := results[standardID]
 	require.NotNil(t, checkResults)
-
-	require.Len(t, checkResults.Evidence(), 2)
+	require.Len(t, checkResults.Evidence(), 4)
 	assert.Equal(t, framework.PassStatus, checkResults.Evidence()[0].Status)
 	assert.Equal(t, framework.PassStatus, checkResults.Evidence()[1].Status)
+	assert.Equal(t, framework.PassStatus, checkResults.Evidence()[2].Status)
+	assert.Equal(t, framework.PassStatus, checkResults.Evidence()[3].Status)
+
+	for _, deployment := range domain.Deployments() {
+		deploymentResults := checkResults.ForChild(deployment)
+		assert.NoError(t, deploymentResults.Error())
+		assert.Len(t, deploymentResults.Evidence(), 1)
+		assert.Equal(t, framework.PassStatus, deploymentResults.Evidence()[0].Status)
+	}
 }
 
-func TestNIST412_Fail(t *testing.T) {
+func TestNIST412_FAIL(t *testing.T) {
 	t.Parallel()
 
 	registry := framework.RegistrySingleton()
@@ -119,20 +217,27 @@ func TestNIST412_Fail(t *testing.T) {
 
 	policies := make(map[string]*storage.Policy)
 	policies[privPolicyDisabled.GetName()] = &privPolicyDisabled
+	policies[sshPolicy.GetName()] = &sshPolicyWithNoEnforcement
+
+	categoryPolicies := make(map[string]set.StringSet)
+	policySet := set.NewStringSet()
+	policySet.Add(cvssPolicyEnabledAndEnforced.Name)
+	categoryPolicies["Vulnerability Management"] = policySet
+
+	privSet := set.NewStringSet()
+	privSet.Add(cvssPolicyEnabledAndEnforced.Name)
+	categoryPolicies["Privileges"] = privSet
 
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 
-	categoryPolicies := make(map[string]set.StringSet)
-	privSet := set.NewStringSet()
-	privSet.Add(privPolicyDisabled.Name)
-	categoryPolicies["Privileges"] = privSet
-
 	data := complianceMocks.NewMockComplianceDataRepository(mockCtrl)
 	data.EXPECT().Cluster().AnyTimes().Return(testCluster)
+	data.EXPECT().Deployments().AnyTimes().Return(toMap(testDeployments))
 	data.EXPECT().Policies().AnyTimes().Return(policies)
 	data.EXPECT().PolicyCategories().AnyTimes().Return(categoryPolicies)
-	data.EXPECT().ImageIntegrations().AnyTimes().Return([]*storage.ImageIntegration{})
+	data.EXPECT().ImageIntegrations().AnyTimes().Return(nil)
+	data.EXPECT().ProcessIndicators().AnyTimes().Return(indicatorsWithSSH)
 
 	run, err := framework.NewComplianceRun(check)
 	require.NoError(t, err)
@@ -142,9 +247,24 @@ func TestNIST412_Fail(t *testing.T) {
 	results := run.GetAllResults()
 	checkResults := results[standardID]
 	require.NotNil(t, checkResults)
-
-	require.Len(t, checkResults.Evidence(), 3)
-	assert.Equal(t, framework.PassStatus, checkResults.Evidence()[0].Status)
-	assert.Equal(t, framework.PassStatus, checkResults.Evidence()[1].Status)
+	require.Len(t, checkResults.Evidence(), 4)
+	assert.Equal(t, framework.FailStatus, checkResults.Evidence()[0].Status)
+	assert.Equal(t, framework.FailStatus, checkResults.Evidence()[1].Status)
 	assert.Equal(t, framework.FailStatus, checkResults.Evidence()[2].Status)
+	assert.Equal(t, framework.FailStatus, checkResults.Evidence()[3].Status)
+
+	for _, deployment := range domain.Deployments() {
+		deploymentResults := checkResults.ForChild(deployment)
+		assert.NoError(t, deploymentResults.Error())
+		assert.Len(t, deploymentResults.Evidence(), 1)
+		assert.Equal(t, framework.FailStatus, deploymentResults.Evidence()[0].Status)
+	}
+}
+
+func toMap(in []*storage.Deployment) map[string]*storage.Deployment {
+	merp := make(map[string]*storage.Deployment, len(in))
+	for _, np := range in {
+		merp[np.GetId()] = np
+	}
+	return merp
 }
