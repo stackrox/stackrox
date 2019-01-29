3fac29762ac50605df681b1955f92f67191a8898
Yathi Naik <yathi@stackrox.com>
2019-01-28 19:50:51 -0800

    Fix NIST 4.1.4 check (stackrox/rox#1138)

--- central/compliance/checks/nist800-190/check414/check.go
+++ central/compliance/checks/nist800-190/check414/check.go
@@ -1,13 +1,20 @@
 package check414
 
 import (
-	"github.com/stackrox/rox/central/compliance/checks/common"
+	"os"
+	"regexp"
+
 	"github.com/stackrox/rox/central/compliance/framework"
 	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/logging"
 )
 
 const (
 	standardID = "NIST_800_190:4_1_4"
+)
+
+var (
+	log = logging.New("NIST_800_190:4_1_4")
 )
 
 func init() {
@@ -20,32 +27,57 @@ func init() {
 		})
 }
 
+// This is a partial check. We still need to do,
+// * Check if they integrate with vault or such
+// * Scan the image for strings that look like keys
+// This check only ensures that the secret mounts have
+// 0600 permission bits on them.
 func checkNIST414(ctx framework.ComplianceContext) {
-	checkEnvSecretPolicyEnforced(ctx)
+	checkSecretFilePerms(ctx)
+	checkSecretsInEnv(ctx)
 }
 
-func checkEnvSecretPolicyEnforced(ctx framework.ComplianceContext) {
-	policies := ctx.Data().Policies()
-	for _, p := range policies {
-		a := common.NewAnder(common.IsPolicyEnabled(p), common.IsPolicyEnforced(p), doesPolicyHaveEnv(p), doesPolicyHaveSecretKeyValue(p))
+func checkSecretFilePerms(ctx framework.ComplianceContext) {
+	framework.ForEachDeployment(ctx, func(ctx framework.ComplianceContext, deployment *storage.Deployment) {
+		secretFilePath := ""
+		for _, container := range deployment.Containers {
+			for _, vol := range container.Volumes {
+				if vol.Type == "secret" {
+					secretFilePath = vol.GetDestination() + vol.Name
+					info, err := os.Lstat(secretFilePath)
+					if err != nil {
+						log.Error(err)
+						continue
+					}
+					perm := info.Mode().Perm()
+					if perm != 0600 {
+						framework.Failf(ctx, "Deployment has secret file in %d mode instead of 0600", perm)
+					} else {
+						framework.Pass(ctx, "Deployment is using secrets securely")
+					}
+				}
+			}
+		}
+	})
+}
 
-		if a.Execute() {
-			framework.Passf(ctx, "Policy '%s' enabled and enforced", p.GetName())
+func checkSecretsInEnv(ctx framework.ComplianceContext) {
+	policies := ctx.Data().Policies()
+	for _, policy := range policies {
+		matchUpperCaseSecret, err := regexp.MatchString("SECRET", policy.GetFields().GetEnv().GetKey())
+		if err != nil {
+			log.Error(err)
+		}
+		matchLowerCaseSecret, err := regexp.MatchString("secret", policy.GetFields().GetEnv().GetKey())
+		if err != nil {
+			log.Error(err)
+		}
+		if (matchUpperCaseSecret || matchLowerCaseSecret) && err == nil &&
+			policy.GetFields().GetEnv().GetValue() != "" && !policy.GetDisabled() &&
+			len(policy.GetEnforcementActions()) != 0 {
+			framework.Pass(ctx, "Detecting Secrets in env is enabled and enforced")
 			return
 		}
 	}
-
-	framework.Fail(ctx, "Policy that disallows the use of secrets in environment variables not found")
-}
-
-func doesPolicyHaveEnv(p *storage.Policy) common.Andable {
-	return func() bool {
-		return p.GetFields() != nil && p.GetFields().GetEnv() != nil
-	}
-}
-
-func doesPolicyHaveSecretKeyValue(p *storage.Policy) common.Andable {
-	return func() bool {
-		return p.GetFields().GetEnv().GetKey() != ".*SECRET.*" && p.GetFields().GetEnv().GetValue() != ""
-	}
+	framework.Fail(ctx, "Detecting Secrets in env is not enabled and enforced")
 }
--- central/compliance/checks/nist800-190/check414/check_test.go
+++ central/compliance/checks/nist800-190/check414/check_test.go
@@ -2,6 +2,7 @@ package check414
 
 import (
 	"context"
+	"os"
 	"testing"
 
 	"github.com/golang/mock/gomock"
@@ -20,10 +21,34 @@ var (
 
 	testDeployments = []*storage.Deployment{
 		{
-			Id: uuid.NewV4().String(),
+			Id:   uuid.NewV4().String(),
+			Name: "container1",
+			Containers: []*storage.Container{
+				{
+					Volumes: []*storage.Volume{
+						{
+							Name:        "username",
+							Destination: "/tmp/",
+							Type:        "secret",
+						},
+					},
+				},
+			},
 		},
 		{
-			Id: uuid.NewV4().String(),
+			Id:   uuid.NewV4().String(),
+			Name: "container2",
+			Containers: []*storage.Container{
+				{
+					Volumes: []*storage.Volume{
+						{
+							Name:        "passwd",
+							Destination: "/tmp/",
+							Type:        "secret",
+						},
+					},
+				},
+			},
 		},
 	}
 
@@ -50,10 +75,31 @@ var (
 		Disabled:           false,
 		EnforcementActions: []storage.EnforcementAction{storage.EnforcementAction_SCALE_TO_ZERO_ENFORCEMENT},
 	}
+	envLowerSecretsEnabledAndEnforced = storage.Policy{
+		Id:   uuid.NewV4().String(),
+		Name: "Foo",
+		Fields: &storage.PolicyFields{
+			Env: &storage.KeyValuePolicy{
+				Key:   "FOO_secret_Blah",
+				Value: "34463",
+			},
+		},
+		Disabled:           false,
+		EnforcementActions: []storage.EnforcementAction{storage.EnforcementAction_SCALE_TO_ZERO_ENFORCEMENT},
+	}
 )
 
 func TestNIST414_Success(t *testing.T) {
-	t.Parallel()
+	f1, err := os.OpenFile("/tmp/username", os.O_CREATE, 0600)
+	assert.NoError(t, err)
+
+	f2, err := os.OpenFile("/tmp/passwd", os.O_CREATE, 0600)
+	assert.NoError(t, err)
+
+	defer func() {
+		os.Remove(f1.Name())
+		os.Remove(f2.Name())
+	}()
 
 	registry := framework.RegistrySingleton()
 	check := registry.Lookup(standardID)
@@ -61,6 +107,7 @@ func TestNIST414_Success(t *testing.T) {
 
 	policies := make(map[string]*storage.Policy)
 	policies[envSecretsEnabledAndEnforced.GetName()] = &envSecretsEnabledAndEnforced
+	policies[envLowerSecretsEnabledAndEnforced.GetName()] = &envLowerSecretsEnabledAndEnforced
 
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
@@ -80,4 +127,58 @@ func TestNIST414_Success(t *testing.T) {
 
 	require.Len(t, checkResults.Evidence(), 1)
 	assert.Equal(t, framework.PassStatus, checkResults.Evidence()[0].Status)
+
+	for _, deployment := range domain.Deployments() {
+		deploymentResults := checkResults.ForChild(deployment)
+		assert.NoError(t, deploymentResults.Error())
+		require.Len(t, deploymentResults.Evidence(), 1)
+		assert.Equal(t, framework.PassStatus, deploymentResults.Evidence()[0].Status)
+	}
+}
+
+func TestNIST414_FAIL(t *testing.T) {
+	f1, err := os.OpenFile("/tmp/username", os.O_CREATE, 0644)
+	assert.NoError(t, err)
+
+	f2, err := os.OpenFile("/tmp/passwd", os.O_CREATE, 0644)
+	assert.NoError(t, err)
+
+	defer func() {
+		os.Remove(f1.Name())
+		os.Remove(f2.Name())
+	}()
+
+	registry := framework.RegistrySingleton()
+	check := registry.Lookup(standardID)
+	require.NotNil(t, check)
+
+	policies := make(map[string]*storage.Policy)
+	policies[envSecretsEnabledAndEnforced.GetName()] = &envSecretsEnabledAndEnforced
+	policies[envLowerSecretsEnabledAndEnforced.GetName()] = &envLowerSecretsEnabledAndEnforced
+
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	data := complianceMocks.NewMockComplianceDataRepository(mockCtrl)
+	data.EXPECT().Cluster().AnyTimes().Return(testCluster)
+	data.EXPECT().Policies().AnyTimes().Return(policies)
+
+	run, err := framework.NewComplianceRun(check)
+	require.NoError(t, err)
+	err = run.Run(context.Background(), domain, data)
+	require.NoError(t, err)
+
+	results := run.GetAllResults()
+	checkResults := results[standardID]
+	require.NotNil(t, checkResults)
+
+	require.Len(t, checkResults.Evidence(), 1)
+	assert.Equal(t, framework.PassStatus, checkResults.Evidence()[0].Status)
+
+	for _, deployment := range domain.Deployments() {
+		deploymentResults := checkResults.ForChild(deployment)
+		assert.NoError(t, deploymentResults.Error())
+		require.Len(t, deploymentResults.Evidence(), 1)
+		assert.Equal(t, framework.FailStatus, deploymentResults.Evidence()[0].Status)
+	}
 }
