851c33ecc4e265f2b16f70e8ba20f2e808eff53a
Kevin Wajdowicz <k@stackrox.com>
2019-01-30 15:35:45 -0800

    Update compliance test apis (stackrox/rox#1163)

--- qa-tests-backend/src/main/groovy/services/ComplianceManagementService.groovy
+++ qa-tests-backend/src/main/groovy/services/ComplianceManagementService.groovy
@@ -2,14 +2,16 @@ package services
 
 import com.google.protobuf.Timestamp
 import io.stackrox.proto.api.v1.ComplianceManagementServiceGrpc
-import io.stackrox.proto.api.v1.ComplianceManagementServiceOuterClass
 import io.stackrox.proto.api.v1.ComplianceManagementServiceOuterClass.AddComplianceRunScheduleRequest
+import io.stackrox.proto.api.v1.ComplianceManagementServiceOuterClass.ComplianceRun
+import io.stackrox.proto.api.v1.ComplianceManagementServiceOuterClass.ComplianceRunSelection
 import io.stackrox.proto.api.v1.ComplianceManagementServiceOuterClass.DeleteComplianceRunScheduleRequest
 import io.stackrox.proto.api.v1.ComplianceManagementServiceOuterClass.GetComplianceRunSchedulesRequest
+import io.stackrox.proto.api.v1.ComplianceManagementServiceOuterClass.GetComplianceRunStatusesRequest
 import io.stackrox.proto.api.v1.ComplianceManagementServiceOuterClass.GetRecentComplianceRunsRequest
+import io.stackrox.proto.api.v1.ComplianceManagementServiceOuterClass.TriggerComplianceRunRequest
 import io.stackrox.proto.api.v1.ComplianceManagementServiceOuterClass.TriggerComplianceRunsRequest
 import io.stackrox.proto.api.v1.ComplianceManagementServiceOuterClass.UpdateComplianceRunScheduleRequest
-import io.stackrox.proto.api.v1.ComplianceManagementServiceOuterClass.ComplianceRunSelection
 import io.stackrox.proto.storage.ComplianceManagement.ComplianceRunSchedule
 
 class ComplianceManagementService extends BaseService {
@@ -19,29 +21,65 @@ class ComplianceManagementService extends BaseService {
 
     static triggerComplianceRun(String standardId, String clusterId) {
         try {
-            return getComplianceManagementClient().triggerRuns(
-                    TriggerComplianceRunsRequest.newBuilder()
-                            .setSelection(
-                                ComplianceRunSelection.newBuilder()
-                                    .setStandardId(standardId)
-                                    .setClusterId(clusterId))
+            return getComplianceManagementClient().triggerRun(
+                    TriggerComplianceRunRequest.newBuilder()
+                            .setStandardId(standardId)
+                            .setClusterId(clusterId)
                             .build()
-            ).getStartedRuns(0)
+            ).startedRun
         } catch (Exception e) {
             println "Error triggering compliance run: ${e.toString()}"
         }
     }
 
     static triggerComplianceRunAndWait(String standardId, String clusterId) {
-        ComplianceManagementServiceOuterClass.ComplianceRun complianceRun = triggerComplianceRun(standardId, clusterId)
+        ComplianceRun complianceRun = triggerComplianceRun(standardId, clusterId)
         println "triggered ${standardId} compliance run"
         println "waiting for the run to finish..."
-        while (complianceRun.state != ComplianceManagementServiceOuterClass.ComplianceRun.State.FINISHED) {
+        while (complianceRun.state != ComplianceRun.State.FINISHED) {
             complianceRun = getRecentRuns(standardId).find { it.id == complianceRun.id }
             sleep 2000
         }
         println "${standardId} run completed!"
         return complianceRun.id
+    }
+
+    static triggerComplianceRuns(String standardId = null, String clusterId = null) {
+        ComplianceRunSelection.Builder selection = ComplianceRunSelection.newBuilder()
+        standardId ? selection.setStandardId(standardId) : selection.setStandardId("*")
+        clusterId ? selection.setClusterId(clusterId) : selection.setClusterId("*")
+        try {
+            return getComplianceManagementClient().triggerRuns(
+                    TriggerComplianceRunsRequest.newBuilder().setSelection(selection.build()).build()
+            ).startedRunsList
+        } catch (Exception e) {
+            println "Error triggering compliance runs: ${e.toString()}"
+        }
+    }
+
+    static triggerComplianceRunsAndWait(String standardId = null, String clusterId = null) {
+        def complianceRunIds = [:]
+        List<ComplianceRun> complianceRuns = triggerComplianceRuns(standardId, clusterId)
+        println complianceRuns
+        println "triggered ${standardId ?: "all"} compliance run${standardId ? "" : "s"}"
+        println "waiting for the run${standardId ? "" : "s"} to finish..."
+        for (ComplianceRun run : complianceRuns) {
+            while (run.state != ComplianceRun.State.FINISHED) {
+                run = getRecentRuns(standardId).find { it.id == run.id }
+                sleep 2000
+            }
+            println "${run.standardId} run completed!"
+            complianceRunIds.put(run.standardId, run.id)
+        }
+        return complianceRunIds
+    }
+
+    static getRunStatuses(List<String> runIds) {
+        return getComplianceManagementClient().getRunStatuses(
+                GetComplianceRunStatusesRequest.newBuilder()
+                        .addAllRunIds(runIds)
+                        .build()
+        )
     }
 
     static getRecentRuns(String standardId = null, String clusterId = null, Timestamp since = null) {
