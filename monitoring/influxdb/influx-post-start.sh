5eef4fd65f01da6e368be69157b1417334849a7b
Connor Gorman <connorgorman@users.noreply.github.com>
2019-02-04 18:54:20 -0800

    Fix monitoring restarts by having post-exec-start wait (stackrox/rox#1129)

--- monitoring/influxdb/influx-post-start.sh
+++ monitoring/influxdb/influx-post-start.sh
@@ -1,12 +1,20 @@
 #! /bin/sh
 
-# let influx get to a boot up point. Should be a query to see if its alive with a max timeout
-sleep 10
-
 runQuery()
 {
     /influx -execute "$1"
 }
+
+statusCheck()
+{
+    runQuery "SHOW DATABASES" > /dev/null 2>&1
+}
+
+echo "Waiting for InfluxDB server to start"
+while ! statusCheck; do
+    echo -n "."
+    sleep 1
+done
 
 # Create the databases
 runQuery "CREATE DATABASE \"telegraf_12h\" WITH DURATION 12h NAME \"12_hours\""
@@ -16,3 +24,5 @@ runQuery "CREATE DATABASE \"telegraf_forever\""
 # Create the continuous queries for down sampling
 runQuery "CREATE CONTINUOUS QUERY \"telegraf_downsample_1m\" ON \"telegraf_12h\" BEGIN SELECT min(*), max(*), mean(*) INTO \"telegraf_2w\".\"2_weeks\".:MEASUREMENT FROM /.*/ GROUP BY time(1m),* END"
 runQuery "CREATE CONTINUOUS QUERY \"telegraf_downsample_10m\" ON \"telegraf_12h\" BEGIN SELECT min(*), max(*), mean(*) INTO \"telegraf_forever\".\"autogen\".:MEASUREMENT FROM /.*/ GROUP BY time(10m),* END"
+
+echo "Successfully started InfluxDB"
