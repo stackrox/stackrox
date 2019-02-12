1c29d0187f9a9d1933a966cb5827cd0574966634
Viswajith Venugopal <viswajithiii@gmail.com>
2019-02-11 18:29:01 -0800

    Allow overriding yarn start's target using an env variable (stackrox/rox#1332)

--- ui/src/setupProxy.js
+++ ui/src/setupProxy.js
@@ -1,7 +1,11 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function main(app) {
-    const defaultOptions = { target: 'https://localhost:8000', changeOrigin: true, secure: false };
+    const defaultOptions = {
+        target: process.env.YARN_START_TARGET || 'https://localhost:8000',
+        changeOrigin: true,
+        secure: false
+    };
 
     app.use(proxy('/v1', defaultOptions));
     app.use(proxy('/api', defaultOptions));
