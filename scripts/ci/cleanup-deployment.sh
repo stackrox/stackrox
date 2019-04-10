6c03c1c18e3dc4bd35379bc622455d77b83c94f4
Connor Gorman <connorgorman@users.noreply.github.com>
2019-04-10 14:22:58 -0700

    Wait for resource deletion in cleanup script (stackrox/rox#1869)

--- scripts/ci/cleanup-deployment.sh
+++ scripts/ci/cleanup-deployment.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-kubectl -n stackrox get cm,deploy,ds,networkpolicy,pv,pvc,secret,svc,serviceaccount -o name | xargs kubectl -n stackrox delete
+kubectl -n stackrox get cm,deploy,ds,networkpolicy,pv,pvc,secret,svc,serviceaccount -o name | xargs kubectl -n stackrox delete --wait
