a0191cf339cde0c14acfd99de1796f2e4eb621a6
Assaf Weinberg <assaf@stackrox.com>
2019-04-29 14:09:29 -0400

    ROX-1891 - handle missing cluster version data (stackrox/rox#1950)

--- ui/src/Containers/Compliance/widgets/ClusterVersion.js
+++ ui/src/Containers/Compliance/widgets/ClusterVersion.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { CLUSTER_VERSION_QUERY as QUERY } from 'queries/cluster';
 import { clusterVersionLabels } from 'messages/common';
 import { format as dateFormat } from 'date-fns';
+import NoResultsMessage from 'Components/NoResultsMessage';
 
 import Widget from 'Components/Widget';
 import Query from 'Components/ThrowingQuery';
@@ -18,18 +19,24 @@ const ClusterVersion = ({ clusterId }) => {
                 if (!loading && data && data.cluster) {
                     const { type } = data.cluster;
                     const { orchestratorMetadata } = data.cluster.status;
-                    headerText = clusterVersionLabels[type];
-                    contents = (
-                        <div className="py-8 w-full flex flex-col items-center justify-between">
-                            <div className="text-4xl text-primary-700 font-500">
-                                {orchestratorMetadata.version}
+                    if (!orchestratorMetadata || !type) {
+                        contents = (
+                            <NoResultsMessage message="An error occurred retrieving cluster version data." />
+                        );
+                    } else {
+                        headerText = clusterVersionLabels[type];
+                        contents = (
+                            <div className="py-8 w-full flex flex-col items-center justify-between">
+                                <div className="text-4xl text-primary-700 font-500">
+                                    {orchestratorMetadata.version}
+                                </div>
+                                <div className="text-base-500">
+                                    Build Date:&nbsp;
+                                    {dateFormat(orchestratorMetadata.buildDate, 'MMMM DD, YYYY')}
+                                </div>
                             </div>
-                            <div className="text-base-500">
-                                Build Date:&nbsp;
-                                {dateFormat(orchestratorMetadata.buildDate, 'MMMM DD, YYYY')}
-                            </div>
-                        </div>
-                    );
+                        );
+                    }
                 }
                 return (
                     <Widget header={headerText} bodyClassName="p-2">
