f86e9421992695eb9842f2444e79ec061fc45c2f
Connor Gorman <connorgorman@users.noreply.github.com>
2019-05-15 01:34:32 -0400

    Remove continuous queries from InfluxDB (stackrox/rox#2202)
    Removing continuous queries provides more stability as continuous queries utilize quite a bit of RAM and CPU

--- monitoring/influxdb/influx-post-start.sh
+++ monitoring/influxdb/influx-post-start.sh
@@ -18,11 +18,5 @@ done
 
 # Create the databases
 runQuery "CREATE DATABASE \"telegraf_12h\" WITH DURATION 12h NAME \"12_hours\""
-runQuery "CREATE DATABASE \"telegraf_2w\" WITH DURATION 2w NAME \"2_weeks\""
-runQuery "CREATE DATABASE \"telegraf_forever\""
-
-# Create the continuous queries for down sampling
-runQuery "CREATE CONTINUOUS QUERY \"telegraf_downsample_1m\" ON \"telegraf_12h\" BEGIN SELECT min(*), max(*), mean(*) INTO \"telegraf_2w\".\"2_weeks\".:MEASUREMENT FROM /.*/ GROUP BY time(1m),* END"
-runQuery "CREATE CONTINUOUS QUERY \"telegraf_downsample_10m\" ON \"telegraf_12h\" BEGIN SELECT min(*), max(*), mean(*) INTO \"telegraf_forever\".\"autogen\".:MEASUREMENT FROM /.*/ GROUP BY time(10m),* END"
 
 echo "Successfully started InfluxDB"
