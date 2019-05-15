63cc0288cb0ffa191eeb749f4dfbba95efdf5839
Robert Stark <42253135+derbergkert@users.noreply.github.com>
2019-05-15 10:56:03 -0700

    ROX-2219: Add enforcement to Group DS (stackrox/rox#2192)

--- central/group/datastore/datastore_impl.go
+++ central/group/datastore/datastore_impl.go
@@ -2,9 +2,16 @@ package datastore
 
 import (
 	"context"
+	"errors"
 
 	"github.com/stackrox/rox/central/group/datastore/internal/store"
+	"github.com/stackrox/rox/central/role/resources"
 	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/sac"
+)
+
+var (
+	groupSAC = sac.ForResource(resources.Group)
 )
 
 type dataStoreImpl struct {
@@ -12,33 +19,81 @@ type dataStoreImpl struct {
 }
 
 func (ds *dataStoreImpl) Get(ctx context.Context, props *storage.GroupProperties) (*storage.Group, error) {
+	if ok, err := groupSAC.ReadAllowed(ctx); err != nil {
+		return nil, err
+	} else if !ok {
+		return nil, nil
+	}
+
 	return ds.storage.Get(props)
 }
 
 func (ds *dataStoreImpl) GetAll(ctx context.Context) ([]*storage.Group, error) {
+	if ok, err := groupSAC.ReadAllowed(ctx); err != nil {
+		return nil, err
+	} else if !ok {
+		return nil, nil
+	}
+
 	return ds.storage.GetAll()
 }
 
 func (ds *dataStoreImpl) Walk(ctx context.Context, authProviderID string, attributes map[string][]string) ([]*storage.Group, error) {
+	if ok, err := groupSAC.ReadAllowed(ctx); err != nil {
+		return nil, err
+	} else if !ok {
+		return nil, nil
+	}
+
 	return ds.storage.Walk(authProviderID, attributes)
 }
 
 func (ds *dataStoreImpl) Add(ctx context.Context, group *storage.Group) error {
+	if ok, err := groupSAC.WriteAllowed(ctx); err != nil {
+		return err
+	} else if !ok {
+		return errors.New("permission denied")
+	}
+
 	return ds.storage.Add(group)
 }
 
 func (ds *dataStoreImpl) Update(ctx context.Context, group *storage.Group) error {
+	if ok, err := groupSAC.WriteAllowed(ctx); err != nil {
+		return err
+	} else if !ok {
+		return errors.New("permission denied")
+	}
+
 	return ds.storage.Update(group)
 }
 
 func (ds *dataStoreImpl) Upsert(ctx context.Context, group *storage.Group) error {
+	if ok, err := groupSAC.WriteAllowed(ctx); err != nil {
+		return err
+	} else if !ok {
+		return errors.New("permission denied")
+	}
+
 	return ds.storage.Upsert(group)
 }
 
 func (ds *dataStoreImpl) Mutate(ctx context.Context, remove, update, add []*storage.Group) error {
+	if ok, err := groupSAC.WriteAllowed(ctx); err != nil {
+		return err
+	} else if !ok {
+		return errors.New("permission denied")
+	}
+
 	return ds.storage.Mutate(remove, update, add)
 }
 
 func (ds *dataStoreImpl) Remove(ctx context.Context, props *storage.GroupProperties) error {
+	if ok, err := groupSAC.WriteAllowed(ctx); err != nil {
+		return err
+	} else if !ok {
+		return errors.New("permission denied")
+	}
+
 	return ds.storage.Remove(props)
 }
