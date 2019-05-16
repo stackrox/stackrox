327c45a2ea517c9748cb7d7bd659fec99078fe84
Shweta Hemdev <42253398+sh-sr@users.noreply.github.com>
2019-05-16 14:41:59 -0700

    ROX-2248 - Logout stackrox if a user login without any license permission (stackrox/rox#2232)

--- ui/src/Containers/License/LicenseReminder.js
+++ ui/src/Containers/License/LicenseReminder.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import ReactRouterPropTypes from 'react-router-prop-types';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectors } from 'reducers';
@@ -9,35 +8,10 @@ import {
     createExpirationMessageWithLink,
     createExpirationMessageWithoutLink
 } from 'Containers/License/helpers';
-import {
-    differenceInDays,
-    differenceInHours,
-    differenceInMinutes,
-    differenceInSeconds
-} from 'date-fns';
-import { licenseStartUpPath } from 'routePaths';
 
 import MessageBanner from 'Components/MessageBanner';
 
-const getDelay = expirationDate => {
-    const dateNow = new Date();
-
-    const daysLeft = differenceInDays(expirationDate, dateNow);
-    if (daysLeft >= 2) return 1000 * 60 * 60 * 24;
-
-    const hoursLeft = differenceInHours(expirationDate, dateNow);
-    if (hoursLeft >= 2) return 1000 * 60 * 60;
-
-    const minutesLeft = differenceInMinutes(expirationDate, dateNow);
-    if (minutesLeft >= 2) return 1000 * 60;
-
-    const secondsLeft = differenceInSeconds(expirationDate, dateNow);
-    if (secondsLeft >= 1) return 1000;
-
-    return null;
-};
-
-const LicenseReminder = ({ expirationDate, history, shouldHaveReadPermission }) => {
+const LicenseReminder = ({ expirationDate, shouldHaveReadPermission }) => {
     const createExpirationMessage = shouldHaveReadPermission('Licenses')
         ? createExpirationMessageWithLink
         : createExpirationMessageWithoutLink;
@@ -54,21 +28,9 @@ const LicenseReminder = ({ expirationDate, history, shouldHaveReadPermission }) 
 
     useEffect(
         () => {
-            const delay = getDelay(expirationDate);
-            let timerID;
-            if (delay) {
-                timerID = setInterval(
-                    () => setExpirationMessage(createExpirationMessage(expirationDate)),
-                    delay
-                );
-            } else {
-                history.push(licenseStartUpPath);
-            }
-            return function cleanup() {
-                clearInterval(timerID);
-            };
+            setExpirationMessage(createExpirationMessage(expirationDate));
         },
-        [createExpirationMessage, expirationDate, history]
+        [createExpirationMessage, expirationDate]
     );
 
     if (!shouldHaveReadPermission('Licenses')) return null;
@@ -90,13 +52,11 @@ const LicenseReminder = ({ expirationDate, history, shouldHaveReadPermission }) 
 
 LicenseReminder.propTypes = {
     expirationDate: PropTypes.string,
-    history: ReactRouterPropTypes.history,
     shouldHaveReadPermission: PropTypes.func.isRequired
 };
 
 LicenseReminder.defaultProps = {
-    expirationDate: null,
-    history: null
+    expirationDate: null
 };
 
 const mapStateToProps = createStructuredSelector({
