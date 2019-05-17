850d05577c2fc856db98e49d23a3e6b3eb932ec1
Joseph Roger Altmaier <josephaltmaier@users.noreply.github.com>
2019-05-17 13:06:26 -0700

    ROX-2250 SAC for imageintegration datastore (stackrox/rox#2243)
    * Update and test imageintegration datastore
    
    * Skip individual tests as per PR feedback

--- central/imageintegration/datastore/datastore_impl.go
+++ central/imageintegration/datastore/datastore_impl.go
@@ -2,11 +2,18 @@ package datastore
 
 import (
 	"context"
+	"errors"
 
 	"github.com/stackrox/rox/central/imageintegration/store"
+	"github.com/stackrox/rox/central/role/resources"
 	v1 "github.com/stackrox/rox/generated/api/v1"
 	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/sac"
 	"github.com/stackrox/rox/pkg/set"
+)
+
+var (
+	groupSAC = sac.ForResource(resources.ImageIntegration)
 )
 
 type datastoreImpl struct {
@@ -15,11 +22,23 @@ type datastoreImpl struct {
 
 // GetImageIntegration is pass-through to the underlying store.
 func (ds *datastoreImpl) GetImageIntegration(ctx context.Context, id string) (*storage.ImageIntegration, bool, error) {
+	if ok, err := groupSAC.ReadAllowed(ctx); err != nil {
+		return nil, false, err
+	} else if !ok {
+		return nil, false, nil
+	}
+
 	return ds.storage.GetImageIntegration(id)
 }
 
 // GetImageIntegrations provides an in memory layer on top of the underlying DB based storage.
 func (ds *datastoreImpl) GetImageIntegrations(ctx context.Context, request *v1.GetImageIntegrationsRequest) ([]*storage.ImageIntegration, error) {
+	if ok, err := groupSAC.ReadAllowed(ctx); err != nil {
+		return nil, err
+	} else if !ok {
+		return nil, nil
+	}
+
 	integrations, err := ds.storage.GetImageIntegrations()
 	if err != nil {
 		return nil, err
@@ -41,15 +60,33 @@ func (ds *datastoreImpl) GetImageIntegrations(ctx context.Context, request *v1.G
 
 // AddImageIntegration is pass-through to the underlying store.
 func (ds *datastoreImpl) AddImageIntegration(ctx context.Context, integration *storage.ImageIntegration) (string, error) {
+	if ok, err := groupSAC.WriteAllowed(ctx); err != nil {
+		return "", err
+	} else if !ok {
+		return "", errors.New("permission denied")
+	}
+
 	return ds.storage.AddImageIntegration(integration)
 }
 
 // UpdateImageIntegration is pass-through to the underlying store.
 func (ds *datastoreImpl) UpdateImageIntegration(ctx context.Context, integration *storage.ImageIntegration) error {
+	if ok, err := groupSAC.WriteAllowed(ctx); err != nil {
+		return err
+	} else if !ok {
+		return errors.New("permission denied")
+	}
+
 	return ds.storage.UpdateImageIntegration(integration)
 }
 
 // RemoveImageIntegration is pass-through to the underlying store.
 func (ds *datastoreImpl) RemoveImageIntegration(ctx context.Context, id string) error {
+	if ok, err := groupSAC.WriteAllowed(ctx); err != nil {
+		return err
+	} else if !ok {
+		return errors.New("permission denied")
+	}
+
 	return ds.storage.RemoveImageIntegration(id)
 }
--- central/imageintegration/datastore/datastore_impl_test.go
+++ central/imageintegration/datastore/datastore_impl_test.go
@@ -6,9 +6,12 @@ import (
 
 	bolt "github.com/etcd-io/bbolt"
 	"github.com/stackrox/rox/central/imageintegration/store"
+	"github.com/stackrox/rox/central/role/resources"
 	v1 "github.com/stackrox/rox/generated/api/v1"
 	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/pkg/bolthelper"
+	"github.com/stackrox/rox/pkg/features"
+	"github.com/stackrox/rox/pkg/sac"
 	"github.com/stackrox/rox/pkg/testutils"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
@@ -21,6 +24,10 @@ func TestImageIntegrationDataStore(t *testing.T) {
 type ImageIntegrationDataStoreTestSuite struct {
 	suite.Suite
 
+	hasNoneCtx  context.Context
+	hasReadCtx  context.Context
+	hasWriteCtx context.Context
+
 	db *bolt.DB
 
 	store     store.Store
@@ -28,6 +35,16 @@ type ImageIntegrationDataStoreTestSuite struct {
 }
 
 func (suite *ImageIntegrationDataStoreTestSuite) SetupTest() {
+	suite.hasNoneCtx = sac.WithGlobalAccessScopeChecker(context.Background(), sac.DenyAllAccessScopeChecker())
+	suite.hasReadCtx = sac.WithGlobalAccessScopeChecker(context.Background(),
+		sac.AllowFixedScopes(
+			sac.AccessModeScopeKeys(storage.Access_READ_ACCESS),
+			sac.ResourceScopeKeys(resources.ImageIntegration)))
+	suite.hasWriteCtx = sac.WithGlobalAccessScopeChecker(context.Background(),
+		sac.AllowFixedScopes(
+			sac.AccessModeScopeKeys(storage.Access_READ_ACCESS, storage.Access_READ_WRITE_ACCESS),
+			sac.ResourceScopeKeys(resources.ImageIntegration)))
+
 	db, err := bolthelper.NewTemp(testutils.DBFileName(suite.Suite))
 	if err != nil {
 		suite.FailNow("Failed to make BoltDB", err.Error())
@@ -53,7 +70,7 @@ func (suite *ImageIntegrationDataStoreTestSuite) TestIntegrations() {
 func (suite *ImageIntegrationDataStoreTestSuite) TestIntegrationsFiltering() {
 	// Remove the default integrations
 	for _, i := range store.DefaultImageIntegrations {
-		suite.NoError(suite.datastore.RemoveImageIntegration(context.TODO(), i.GetId()))
+		suite.NoError(suite.datastore.RemoveImageIntegration(suite.hasWriteCtx, i.GetId()))
 	}
 
 	integrations := []*storage.ImageIntegration{
@@ -77,17 +94,20 @@ func (suite *ImageIntegrationDataStoreTestSuite) TestIntegrationsFiltering() {
 
 	// Test Add
 	for _, r := range integrations {
-		id, err := suite.datastore.AddImageIntegration(context.TODO(), r)
+		id, err := suite.datastore.AddImageIntegration(suite.hasWriteCtx, r)
 		suite.NoError(err)
 		suite.NotEmpty(id)
 	}
 
-	actualIntegrations, err := suite.datastore.GetImageIntegrations(context.TODO(), &v1.GetImageIntegrationsRequest{})
+	actualIntegrations, err := suite.datastore.GetImageIntegrations(suite.hasWriteCtx, &v1.GetImageIntegrationsRequest{})
 	suite.NoError(err)
 	suite.ElementsMatch(integrations, actualIntegrations)
 }
 
 func testIntegrations(t *testing.T, insertStorage store.Store, retrievalStorage DataStore) {
+	ctx := sac.WithGlobalAccessScopeChecker(context.Background(), sac.AllowFixedScopes(
+		sac.AccessModeScopeKeys(storage.Access_READ_ACCESS, storage.Access_READ_WRITE_ACCESS),
+		sac.ResourceScopeKeys(resources.ImageIntegration)))
 	integrations := []*storage.ImageIntegration{
 		{
 			Name: "registry1",
@@ -114,7 +134,7 @@ func testIntegrations(t *testing.T, insertStorage store.Store, retrievalStorage 
 		assert.NotEmpty(t, id)
 	}
 	for _, r := range integrations {
-		got, exists, err := retrievalStorage.GetImageIntegration(context.TODO(), r.GetId())
+		got, exists, err := retrievalStorage.GetImageIntegration(ctx, r.GetId())
 		assert.NoError(t, err)
 		assert.True(t, exists)
 		assert.Equal(t, got, r)
@@ -130,7 +150,7 @@ func testIntegrations(t *testing.T, insertStorage store.Store, retrievalStorage 
 	}
 
 	for _, r := range integrations {
-		got, exists, err := retrievalStorage.GetImageIntegration(context.TODO(), r.GetId())
+		got, exists, err := retrievalStorage.GetImageIntegration(ctx, r.GetId())
 		assert.NoError(t, err)
 		assert.True(t, exists)
 		assert.Equal(t, got, r)
@@ -142,8 +162,149 @@ func testIntegrations(t *testing.T, insertStorage store.Store, retrievalStorage 
 	}
 
 	for _, r := range integrations {
-		_, exists, err := retrievalStorage.GetImageIntegration(context.TODO(), r.GetId())
+		_, exists, err := retrievalStorage.GetImageIntegration(ctx, r.GetId())
 		assert.NoError(t, err)
 		assert.False(t, exists)
 	}
+}
+
+func getIntegration(name string) *storage.ImageIntegration {
+	return &storage.ImageIntegration{
+		Name: name,
+		IntegrationConfig: &storage.ImageIntegration_Docker{
+			Docker: &storage.DockerConfig{
+				Endpoint: "https://endpoint1",
+			},
+		},
+	}
+}
+
+func (suite *ImageIntegrationDataStoreTestSuite) storeIntegration(name string) *storage.ImageIntegration {
+	integration := getIntegration(name)
+	id, err := suite.store.AddImageIntegration(integration)
+	suite.NoError(err)
+	suite.NotEmpty(id)
+	return integration
+}
+
+func (suite *ImageIntegrationDataStoreTestSuite) TestEnforcesGet() {
+	if !features.ScopedAccessControl.Enabled() {
+		suite.T().Skip()
+	}
+	group, exists, err := suite.datastore.GetImageIntegration(suite.hasNoneCtx, "Some ID")
+	suite.NoError(err, "expected no error, should return nil without access")
+	suite.False(exists, "expected exists to be false as access was denied and bools can't be nil")
+	suite.Nil(group, "expected return value to be nil")
+}
+
+func (suite *ImageIntegrationDataStoreTestSuite) TestAllowsGet() {
+	if !features.ScopedAccessControl.Enabled() {
+		suite.T().Skip()
+	}
+	integration := suite.storeIntegration("Joseph Rules")
+
+	gotInt, exists, err := suite.datastore.GetImageIntegration(suite.hasReadCtx, integration.GetId())
+	suite.NoError(err, "expected no error trying to read with permissions")
+	suite.Equal(integration, gotInt)
+	suite.True(exists)
+
+	gotInt, exists, err = suite.datastore.GetImageIntegration(suite.hasWriteCtx, integration.GetId())
+	suite.NoError(err, "expected no error trying to read with permissions")
+	suite.Equal(integration, gotInt)
+	suite.True(exists)
+}
+
+func (suite *ImageIntegrationDataStoreTestSuite) TestEnforcesGetBatch() {
+	if !features.ScopedAccessControl.Enabled() {
+		suite.T().Skip()
+	}
+	integrations, err := suite.datastore.GetImageIntegrations(suite.hasNoneCtx, &v1.GetImageIntegrationsRequest{})
+	suite.NoError(err, "expected no error, should return nil without access")
+	suite.Nil(integrations, "expected return value to be nil")
+}
+
+func (suite *ImageIntegrationDataStoreTestSuite) TestAllowsGetBatch() {
+	if !features.ScopedAccessControl.Enabled() {
+		suite.T().Skip()
+	}
+	integration := suite.storeIntegration("Some Integration")
+	integrationList := []*storage.ImageIntegration{integration}
+
+	getRequest := &v1.GetImageIntegrationsRequest{Name: integration.GetName(), Cluster: integration.GetClusterId()}
+
+	gotImages, err := suite.datastore.GetImageIntegrations(suite.hasReadCtx, getRequest)
+	suite.NoError(err, "expected no error trying to read with permissions")
+	suite.ElementsMatch(integrationList, gotImages)
+
+	gotImages, err = suite.datastore.GetImageIntegrations(suite.hasWriteCtx, getRequest)
+	suite.NoError(err, "expected no error trying to read with permissions")
+	suite.ElementsMatch(integrationList, gotImages)
+}
+
+func (suite *ImageIntegrationDataStoreTestSuite) TestEnforcesAdd() {
+	if !features.ScopedAccessControl.Enabled() {
+		suite.T().Skip()
+	}
+	integrationOne := getIntegration("some kinda name")
+	id, err := suite.datastore.AddImageIntegration(suite.hasNoneCtx, integrationOne)
+	suite.Error(err, "expected an error trying to write without permissions")
+	suite.Empty(id)
+
+	integrationTwo := getIntegration("Get named, you")
+	id, err = suite.datastore.AddImageIntegration(suite.hasReadCtx, integrationTwo)
+	suite.Error(err, "expected an error trying to write without permissions")
+	suite.Empty(id)
+}
+
+func (suite *ImageIntegrationDataStoreTestSuite) TestAllowsAdd() {
+	if !features.ScopedAccessControl.Enabled() {
+		suite.T().Skip()
+	}
+	id, err := suite.datastore.AddImageIntegration(suite.hasWriteCtx, getIntegration("namenamenamename"))
+	suite.NoError(err, "expected no error trying to write with permissions")
+	suite.NotEmpty(id)
+}
+
+func (suite *ImageIntegrationDataStoreTestSuite) TestEnforcesUpdate() {
+	if !features.ScopedAccessControl.Enabled() {
+		suite.T().Skip()
+	}
+	integration := suite.storeIntegration("name")
+
+	err := suite.datastore.UpdateImageIntegration(suite.hasNoneCtx, integration)
+	suite.Error(err, "expected an error trying to write without permissions")
+
+	err = suite.datastore.UpdateImageIntegration(suite.hasReadCtx, integration)
+	suite.Error(err, "expected an error trying to write without permissions")
+}
+
+func (suite *ImageIntegrationDataStoreTestSuite) TestAllowsUpdate() {
+	if !features.ScopedAccessControl.Enabled() {
+		suite.T().Skip()
+	}
+	integration := suite.storeIntegration("joseph is the best")
+
+	err := suite.datastore.UpdateImageIntegration(suite.hasWriteCtx, integration)
+	suite.NoError(err, "expected no error trying to write with permissions")
+}
+
+func (suite *ImageIntegrationDataStoreTestSuite) TestEnforcesRemove() {
+	if !features.ScopedAccessControl.Enabled() {
+		suite.T().Skip()
+	}
+	err := suite.datastore.RemoveImageIntegration(suite.hasNoneCtx, "blerk")
+	suite.Error(err, "expected an error trying to write without permissions")
+
+	err = suite.datastore.RemoveImageIntegration(suite.hasReadCtx, "hkddsfk")
+	suite.Error(err, "expected an error trying to write without permissions")
+}
+
+func (suite *ImageIntegrationDataStoreTestSuite) TestAllowsRemove() {
+	if !features.ScopedAccessControl.Enabled() {
+		suite.T().Skip()
+	}
+	integration := suite.storeIntegration("jdgbfdkjh")
+
+	err := suite.datastore.RemoveImageIntegration(suite.hasWriteCtx, integration.GetId())
+	suite.NoError(err, "expected no error trying to write with permissions")
 }
