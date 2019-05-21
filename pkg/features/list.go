949ff08dec24fa4b10144b7f0ab8701fc900d8a4
David Terrell <13537+dbt@users.noreply.github.com>
2019-05-21 07:57:08 -0700

    feature flag not used (stackrox/rox#2284)

--- pkg/features/list.go
+++ pkg/features/list.go
@@ -13,9 +13,6 @@ var (
 	// NB: When removing this feature flag, please also remove references to it in .circleci/config.yml
 	K8sRBAC = registerFeature("Enable k8s RBAC objects collection and processing", "ROX_K8S_RBAC", false)
 
-	// CentralTLSSecretLoader will read secrets from the orchestrator to configure TLS.
-	CentralTLSSecretLoader = registerFeature("Enable Central TLS secret loading from orchestrator", "ROX_CENTRAL_TLS", false)
-
 	// ProcessWhitelist will enable the process whitelist API
 	// NB: When removing this feature flag, please also remove references to it in .circleci/config.yml
 	ProcessWhitelist = registerFeature("Enable Process Whitelist API", "ROX_PROCESS_WHITELIST", true)
