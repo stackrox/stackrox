c1428baeb7b548284b6e93156003a51261e4857b
Khushboo Sancheti <42253461+clickboo@users.noreply.github.com>
2019-05-24 14:22:59 -0700

    Turning the k8s RBAC feature flag on by default. (stackrox/rox#2347)

--- pkg/features/list.go
+++ pkg/features/list.go
@@ -11,7 +11,7 @@ var (
 
 	// K8sRBAC is used to enable k8s rbac collection and processing
 	// NB: When removing this feature flag, please also remove references to it in .circleci/config.yml
-	K8sRBAC = registerFeature("Enable k8s RBAC objects collection and processing", "ROX_K8S_RBAC", false)
+	K8sRBAC = registerFeature("Enable k8s RBAC objects collection and processing", "ROX_K8S_RBAC", true)
 
 	// ProcessWhitelist will enable the process whitelist API
 	// NB: When removing this feature flag, please also remove references to it in .circleci/config.yml
