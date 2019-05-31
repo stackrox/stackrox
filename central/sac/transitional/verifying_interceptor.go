07571746e5f4e1214e038f2417dfcb8600e4c543
Malte Isberner <misberner@users.noreply.github.com>
2019-05-31 20:33:24 +0200

    Reduce Scoped Access Control Logspam (stackrox/rox#2412)

--- central/sac/transitional/verifying_interceptor.go
+++ central/sac/transitional/verifying_interceptor.go
@@ -2,15 +2,22 @@ package transitional
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/stackrox/rox/pkg/concurrency"
 	"github.com/stackrox/rox/pkg/grpc/authz"
 	"github.com/stackrox/rox/pkg/logging"
 	"github.com/stackrox/rox/pkg/sac"
+	"github.com/stackrox/rox/pkg/set"
+	"github.com/stackrox/rox/pkg/sync"
 	"google.golang.org/grpc"
 )
 
 var (
 	log = logging.LoggerForModule()
+
+	logMessages      = set.NewStringSet()
+	logMessagesMutex sync.Mutex
 )
 
 // VerifySACScopeChecksInterceptor is a GRPC unary interceptor that verifies that the permissions
@@ -31,7 +38,15 @@ func VerifySACScopeChecksInterceptor(ctx context.Context, req interface{}, serve
 	serviceMethodPerms := authz.GetPermissionMapForServiceMethod(serverInfo.Server, serverInfo.FullMethod)
 	usedPerms := recordingSCC.UsedPermissions()
 	if !serviceMethodPerms.IsLessOrEqual(usedPerms) {
-		log.Errorf("Method %s required permissions %v, but scoped access control only checked for permissions %v", serverInfo.FullMethod, serviceMethodPerms, usedPerms)
+		logMsg := fmt.Sprintf("Method %s required permissions %v, but scoped access control only checked for permissions %v", serverInfo.FullMethod, serviceMethodPerms, usedPerms)
+
+		added := false
+		concurrency.WithLock(&logMessagesMutex, func() {
+			added = logMessages.Add(logMsg)
+		})
+		if added {
+			log.Error(logMsg)
+		}
 	}
 
 	return resp, nil
