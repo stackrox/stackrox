e0aa0d536f716b5bc07652f6602fb44bbc62c0bf
Connor Gorman <connorgorman@users.noreply.github.com>
2019-06-14 11:46:07 -0700

    Retry go-get-version multiple times to avoid flakiness (stackrox/rox#2547)
    * Retry go-get-version multiple times to avoid flakiness

--- scripts/go-get-version.sh
+++ scripts/go-get-version.sh
@@ -8,14 +8,26 @@ function usage() {
 
 [[ "$#" -ge 2 ]] || usage
 
-package="$1"
-hash_or_tag="$2"
-skip_install="$3"
+function install() {
+  package="$1"
+  hash_or_tag="$2"
+  skip_install="$3"
 
-package_without_trailing_dots="${package%/...}"
-go get -d -v "${package_without_trailing_dots}"
-cd "${GOPATH}/src/${package_without_trailing_dots}" || { echo "Couldn't cd to the directory!"; exit 1; }
+  package_without_trailing_dots="${package%/...}"
+  go get -d -v "${package_without_trailing_dots}"
+  cd "${GOPATH}/src/${package_without_trailing_dots}" || { echo "Couldn't cd to the directory!"; return 1; }
 
-git rev-parse --git-dir > /dev/null 2>&1 || { echo "This script only supports git-based packages!"; exit 1; }
-git checkout -q "${hash_or_tag}" || { echo "git checkout failed!"; exit 1; }
-[[ "$skip_install" = "--skip-install" ]] || go install "${package}" || { echo "go install failed!"; exit 1; }
+  git rev-parse --git-dir > /dev/null 2>&1 || { echo "This script only supports git-based packages!"; return 1; }
+  git checkout -q "${hash_or_tag}" || { echo "git checkout failed!"; exit 1; }
+  [[ "$skip_install" = "--skip-install" ]] || go install "${package}" || { echo "go install failed!"; return 1; }
+}
+
+for i in {1..10}; do
+  if install $@; then
+    exit 0
+  fi
+  sleep 1
+done
+
+echo "failed all retries"
+exit 1
