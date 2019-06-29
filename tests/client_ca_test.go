66104116b8040287f25feb72dbf24aa31c65bab2
Malte Isberner <misberner@users.noreply.github.com>
2019-06-29 02:02:17 +0200

    ROX-2189: Setup a client CA using roxctl in k8s-tests and verify its presence (stackrox/rox#2688)

---
+++ tests/client_ca_test.go
@@ -0,0 +1,56 @@
+package tests
+
+import (
+	"bytes"
+	"crypto/tls"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/cloudflare/cfssl/helpers"
+	"github.com/stackrox/rox/pkg/features"
+	"github.com/stackrox/rox/pkg/testutils"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestClientCARequested(t *testing.T) {
+	t.Parallel()
+
+	if !features.ClientCAAuth.Enabled() {
+		t.SkipNow()
+	}
+
+	clientCAFile := os.Getenv("CLIENT_CA_PATH")
+	require.NotEmpty(t, clientCAFile, "no client CA file path set")
+	pemBytes, err := ioutil.ReadFile(clientCAFile)
+	require.NoErrorf(t, err, "Could not read client CA file %s", clientCAFile)
+
+	caCert, err := helpers.ParseCertificatePEM(pemBytes)
+	require.NoError(t, err, "Could not parse client CA PEM data")
+
+	var acceptableCAs [][]byte
+	tlsConf := &tls.Config{
+		InsecureSkipVerify: true,
+		ServerName:         "central.stackrox",
+		GetClientCertificate: func(cri *tls.CertificateRequestInfo) (*tls.Certificate, error) {
+			acceptableCAs = cri.AcceptableCAs
+			return &tls.Certificate{}, nil
+		},
+	}
+
+	conn, err := tls.Dial("tcp", testutils.RoxAPIEndpoint(t), tlsConf)
+	require.NoError(t, err, "could not connect to central")
+	_ = conn.Handshake()
+	_ = conn.Close()
+
+	found := false
+	for _, acceptableCA := range acceptableCAs {
+		if bytes.Equal(acceptableCA, caCert.RawSubject) {
+			found = true
+			break
+		}
+	}
+
+	assert.True(t, found, "server did not request appropriate client certs")
+}
