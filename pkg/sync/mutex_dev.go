17d34f673649fead89fb434a06de9c95d0a9edcc
Malte Isberner <misberner@users.noreply.github.com>
2019-06-30 18:51:48 +0200

    Make mutex watchdog timeout configurable (stackrox/rox#2696)

--- pkg/sync/mutex_dev.go
+++ pkg/sync/mutex_dev.go
@@ -4,16 +4,42 @@ package sync
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 	"sync"
 	"time"
 
+	"github.com/pkg/errors"
 	"github.com/stackrox/rox/pkg/debug"
+	"github.com/stackrox/rox/pkg/utils"
 )
 
 const (
 	// TODO(ROX-2163) - tracks the move to 5 seconds from 1 second
-	lockTimeout = 5 * time.Second
+	defaultLockTimeout = 5 * time.Second
+
+	// We omit the `ROX_` prefix as this is fairly general (potential open-sourcing). Also, this is not a setting in the
+	// classical sense that is read by roxctl and propagated to deploying. It is a debug setting that normally should
+	// not be changed (and if it does, it will usually happen via editing the deployment directly, in any case with a
+	// small impact radius).
+	lockTimeoutSettingEnvVar = "MUTEX_WATCHDOG_TIMEOUT_SECS"
 )
+
+var (
+	lockTimeout = defaultLockTimeout
+)
+
+func init() {
+	// Deliberately initialize lockTimeout in an init function instead of via a sync.Once to keep the overhead
+	// when creating mutexes as low as possible.
+	timeoutSettingStr := os.Getenv(lockTimeoutSettingEnvVar)
+	if timeoutSettingStr == "" {
+		return
+	}
+	timeoutSecs, err := strconv.Atoi(timeoutSettingStr)
+	utils.Must(errors.Wrap(err, "could not parse watchdog timeout setting"))
+	lockTimeout = time.Duration(timeoutSecs) * time.Second
+}
 
 func watchdog(action string, ch <-chan struct{}, timeout time.Duration, stacktrace debug.LazyStacktrace) {
 	t := time.NewTimer(timeout)
