6bddcae0425c22d4bffeac4d2518ae51d9f8b281
Connor Gorman <connorgorman@users.noreply.github.com>
2019-07-10 11:17:43 -0700

    Upgrade BadgerDB to 1.6.0 (stackrox/rox#2768)

--- central/globaldb/export/restore.go
+++ central/globaldb/export/restore.go
@@ -63,7 +63,9 @@ func tryRestoreBadger(r io.Reader, outDir string) error {
 		return errors.Wrap(err, "could not create new badger DB in empty dir")
 	}
 
-	if err := db.Load(r); err != nil {
+	// The second argument is the number of maxPendingWrites that are allowed
+	// where 10 seems to be a reasonable number
+	if err := db.Load(r, 10); err != nil {
 		return errors.Wrap(err, "could not load badger DB backup")
 	}
 	if err := db.Close(); err != nil {
--- pkg/badgerhelper/null_logger.go
+++ pkg/badgerhelper/null_logger.go
@@ -2,6 +2,7 @@ package badgerhelper
 
 type nullLogger struct{}
 
+func (nullLogger) Debugf(string, ...interface{})   {}
 func (nullLogger) Infof(string, ...interface{})    {}
 func (nullLogger) Warningf(string, ...interface{}) {}
 func (nullLogger) Errorf(string, ...interface{})   {}
