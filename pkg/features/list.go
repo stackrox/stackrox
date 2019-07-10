e629684c7c20131d37638e7bad69eed1c50c8885
Malte Isberner <misberner@users.noreply.github.com>
2019-07-10 21:30:17 +0200

    ROX-2702: Enable plaintext exposure feature flag by default (stackrox/rox#2765)

--- pkg/features/list.go
+++ pkg/features/list.go
@@ -13,5 +13,5 @@ var (
 	ClientCAAuth = registerFeature("Client Certificate Authentication", "ROX_CLIENT_CA_AUTH", true)
 
 	// PlaintextExposure enables specifying a port under which to expose the UI and API without TLS.
-	PlaintextExposure = registerFeature("Plaintext UI & API exposure", "ROX_PLAINTEXT_EXPOSURE", false)
+	PlaintextExposure = registerFeature("Plaintext UI & API exposure", "ROX_PLAINTEXT_EXPOSURE", true)
 )
