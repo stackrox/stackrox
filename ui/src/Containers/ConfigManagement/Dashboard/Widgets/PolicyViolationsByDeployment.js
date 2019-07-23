7b93f0ce86123359358eff6d1ee4fd9578e140cc
Saif Chaudhry <sachaudh@users.noreply.github.com>
2019-07-23 08:51:00 -0700

    ROX-2759: Deployments with most severy policy violations should have an empty state (stackrox/rox#2862)

--- ui/src/Containers/ConfigManagement/Dashboard/Widgets/PolicyViolationsByDeployment.js
+++ ui/src/Containers/ConfigManagement/Dashboard/Widgets/PolicyViolationsByDeployment.js
@@ -11,6 +11,7 @@ import ReactRouterPropTypes from 'react-router-prop-types';
 import severityColorMap from 'constants/severityColors';
 import { Tooltip } from 'react-tippy';
 import pluralize from 'pluralize';
+import NoResultsMessage from 'Components/NoResultsMessage';
 
 const severityFontMap = {
     CRITICAL_SEVERITY: 'var(--alert-800)',
@@ -87,8 +88,6 @@ const PolicyViolationsByDeployment = ({ match, location }) => {
                 let viewAllLink;
 
                 if (!loading && data && networkStatus === networkStatuses.READY) {
-                    const results = processData(data);
-                    const slicedData = results.slice(0, 10);
                     const linkTo = URLService.getURL(match, location)
                         .base(entityTypes.DEPLOYMENT)
                         .url();
@@ -101,74 +100,85 @@ const PolicyViolationsByDeployment = ({ match, location }) => {
                         </Link>
                     );
 
-                    contents = (
-                        <ul className="list-reset w-full columns-2 columns-gap-0 text-sm">
-                            {slicedData.map((item, index) => (
-                                <Link
-                                    key={`${item.id}-${index}`}
-                                    to={`${linkTo}/${item.id}`}
-                                    className="no-underline text-base-600"
-                                >
-                                    <li className="hover:bg-base-200">
-                                        <div
-                                            className={`flex flex-row border-base-300 ${
-                                                index !== 4 && index !== 9 ? 'border-b' : ''
-                                            } ${index < 5 ? 'border-r' : ''}`}
-                                        >
-                                            <div className="flex flex-col flex-1 truncate py-2 px-4">
-                                                <span className="pb-2">
-                                                    {index + 1}. {item.name}
-                                                </span>
-                                                <ul className="list-reset flex">
-                                                    {Object.keys(item.counts).map(type => {
-                                                        const style = {
-                                                            backgroundColor: severityColorMap[type],
-                                                            color: severityFontMap[type],
-                                                            borderColor: severityColorMap[type]
-                                                        };
-                                                        const count = item.counts[type];
-
-                                                        if (count === 0) return null;
-
-                                                        const tipText = `${item.counts[type]} ${
-                                                            severityTexts[type]
-                                                        } ${pluralize('Violation', count)}`;
-                                                        return (
-                                                            <Tooltip
-                                                                position="top"
-                                                                trigger="mouseenter"
-                                                                animation="none"
-                                                                duration={0}
-                                                                arrow
-                                                                distance={20}
-                                                                html={
-                                                                    <span className="text-sm">
-                                                                        {tipText}
-                                                                    </span>
-                                                                }
-                                                                key={`${type}`}
-                                                                unmountHTMLWhenHide
-                                                            >
-                                                                <li
-                                                                    className="p-1 border rounded mr-2"
-                                                                    style={style}
-                                                                >
-                                                                    <span>{count} </span>
-                                                                    <span className="uppercase">
-                                                                        {type.charAt(0)}
-                                                                    </span>
-                                                                </li>
-                                                            </Tooltip>
-                                                        );
-                                                    })}
-                                                </ul>
-                                            </div>
-                                        </div>
-                                    </li>
-                                </Link>
-                            ))}
-                        </ul>
+                    const deploymentsWithAlerts = data.deployments.filter(
+                        datum => datum.deployAlerts.length
                     );
+                    if (!deploymentsWithAlerts.length) {
+                        contents = <NoResultsMessage message="No deployments with violations" />;
+                    } else {
+                        const results = processData(deploymentsWithAlerts);
+                        const slicedData = results.slice(0, 10);
+
+                        contents = (
+                            <ul className="list-reset w-full columns-2 columns-gap-0 text-sm">
+                                {slicedData.map((item, index) => (
+                                    <Link
+                                        key={`${item.id}-${index}`}
+                                        to={`${linkTo}/${item.id}`}
+                                        className="no-underline text-base-600"
+                                    >
+                                        <li className="hover:bg-base-200">
+                                            <div
+                                                className={`flex flex-row border-base-300 ${
+                                                    index !== 4 && index !== 9 ? 'border-b' : ''
+                                                } ${index < 5 ? 'border-r' : ''}`}
+                                            >
+                                                <div className="flex flex-col flex-1 truncate py-2 px-4">
+                                                    <span className="pb-2">
+                                                        {index + 1}. {item.name}
+                                                    </span>
+                                                    <ul className="list-reset flex">
+                                                        {Object.keys(item.counts).map(type => {
+                                                            const style = {
+                                                                backgroundColor:
+                                                                    severityColorMap[type],
+                                                                color: severityFontMap[type],
+                                                                borderColor: severityColorMap[type]
+                                                            };
+                                                            const count = item.counts[type];
+
+                                                            if (count === 0) return null;
+
+                                                            const tipText = `${item.counts[type]} ${
+                                                                severityTexts[type]
+                                                            } ${pluralize('Violation', count)}`;
+                                                            return (
+                                                                <Tooltip
+                                                                    position="top"
+                                                                    trigger="mouseenter"
+                                                                    animation="none"
+                                                                    duration={0}
+                                                                    arrow
+                                                                    distance={20}
+                                                                    html={
+                                                                        <span className="text-sm">
+                                                                            {tipText}
+                                                                        </span>
+                                                                    }
+                                                                    key={`${type}`}
+                                                                    unmountHTMLWhenHide
+                                                                >
+                                                                    <li
+                                                                        className="p-1 border rounded mr-2"
+                                                                        style={style}
+                                                                    >
+                                                                        <span>{count} </span>
+                                                                        <span className="uppercase">
+                                                                            {type.charAt(0)}
+                                                                        </span>
+                                                                    </li>
+                                                                </Tooltip>
+                                                            );
+                                                        })}
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    </Link>
+                                ))}
+                            </ul>
+                        );
+                    }
                 }
                 return (
                     <Widget
