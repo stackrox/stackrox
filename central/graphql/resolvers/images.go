0cd08490b5c23aab4a39e51a699326a1b6f79b1c
Khushboo Sancheti <42253461+clickboo@users.noreply.github.com>
2019-07-23 13:06:41 -0700

    Number of deployments for each image, can bbe scoped using a query. (stackrox/rox#2879)

--- central/graphql/resolvers/images.go
+++ central/graphql/resolvers/images.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/graph-gophers/graphql-go"
 	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/search"
 	"github.com/stackrox/rox/pkg/utils"
 )
 
@@ -13,6 +14,7 @@ func init() {
 	utils.Must(
 		schema.AddQuery("images(query:String): [Image!]!"),
 		schema.AddQuery("image(sha:ID!): Image"),
+		schema.AddExtraResolver("Image", "deploymentIDs(query:String): [String!]!"),
 		schema.AddExtraResolver("ImageScanComponent", "layerIndex: Int"),
 	)
 }
@@ -37,6 +39,26 @@ func (resolver *Resolver) Image(ctx context.Context, args struct{ Sha graphql.ID
 	}
 	return resolver.wrapImage(
 		resolver.ImageDataStore.GetImage(ctx, string(args.Sha)))
+}
+
+// DeploymentIDs returns the deployments which use this image for the identified image, if it exists
+func (resolver *imageResolver) DeploymentIDs(ctx context.Context, args rawQuery) ([]string, error) {
+	if err := readDeployments(ctx); err != nil {
+		return nil, err
+	}
+	q, err := args.AsV1QueryOrEmpty()
+	if err != nil {
+		return nil, err
+	}
+
+	imageIDQuery := search.NewQueryBuilder().AddExactMatches(search.ImageSHA, resolver.data.GetId()).ProtoQuery()
+
+	results, err := resolver.root.DeploymentDataStore.Search(ctx, search.NewConjunctionQuery(imageIDQuery, q))
+	if err != nil {
+		return nil, err
+	}
+
+	return search.ResultsToIDSet(results).AsSlice(), nil
 }
 
 func (resolver *Resolver) getImage(ctx context.Context, id string) *storage.Image {
