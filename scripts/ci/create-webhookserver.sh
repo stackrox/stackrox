f28333804826beca8db228e45aa0fcf375cd0980
David Terrell <13537+dbt@users.noreply.github.com>
2019-07-31 14:07:22 -0700

    [webhookserver] Add debug information on failure (stackrox/rox#2963)

--- scripts/ci/create-webhookserver.sh
+++ scripts/ci/create-webhookserver.sh
@@ -2,9 +2,17 @@
 
 set -e
 
+printstatus() {
+    echo current resource status ...
+    echo
+    kubectl -n stackrox get all -l app=webhookserver
+}
+
+trap printstatus ERR
+
 kubectl create -f webhookserver/server.yaml
 sleep 5
-POD=$(kubectl -n stackrox get pod -o jsonpath='{.items[?(@.metadata.labels.app=="webhookserver")].metadata.name}')
-kubectl -n stackrox wait --for=condition=ready "pod/$POD" --timeout=2m
+POD=$(kubectl -n stackrox get pod -l app=webhookserver -o name)
+kubectl -n stackrox wait --for=condition=ready "$POD" --timeout=2m
 nohup kubectl -n stackrox port-forward "${POD}" 8080:8080 </dev/null > /dev/null 2>&1 &
 sleep 1
