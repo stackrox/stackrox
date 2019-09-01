97f1384abfe8905da2cb92ab390d7546838a1350
Malte Isberner <misberner@users.noreply.github.com>
2019-09-01 18:43:49 +0200

    Update dev license and signing key (stackrox/rox#3323)

--- scripts/ci/create-dev-license-update-pr.sh
+++ scripts/ci/create-dev-license-update-pr.sh
@@ -15,6 +15,13 @@ branch_name="$1"
 
 pr_response_file="$(mktemp)"
 
+message="Hello,
+it is time to update our development license.
+Once all CI checks pass on this PR, please approve and merge it. Afterwards, please delete the branch.
+
+**NOTE**: If CI checks fail, the most likely reason is that the signing key needs to be rotated. Follow
+the instructions on https://stack-rox.atlassian.net/wiki/spaces/ENGKB/pages/874053651 for this."
+
 curl -sS --fail \
 	-o "$pr_response_file" \
 	-X POST \
@@ -22,7 +29,7 @@ curl -sS --fail \
 	'https://api.github.com/repos/stackrox/rox/pulls' \
 	-d"{
 	\"title\": \"Automatic update of dev license ($(date '+%Y-%m-%d'))\",
-	\"body\": \"Hello,\nit is time to update our development license.\nOnce all CI checks pass on this PR, please approve and merge it. Afterwards, please delete the branch.\",
+	\"body\": $(jq -sR <<<"$message"),
 	\"head\": \"${branch_name}\",
 	\"base\": \"master\"
 }"
