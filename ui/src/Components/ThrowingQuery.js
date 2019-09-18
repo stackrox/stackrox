0408ee19fce3a70bbd05b5d148277d85fb19c5f2
David Terrell <13537+dbt@users.noreply.github.com>
2019-09-18 13:29:17 -0700

    add error placeholder for ThrowingQuery (stackrox/rox#3418)

--- ui/src/Components/ThrowingQuery.js
+++ ui/src/Components/ThrowingQuery.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import { Query } from 'react-apollo';
-
+import PropTypes from 'prop-types';
 import Raven from 'raven-js';
+import * as Icons from 'react-feather';
+
+const GraphQLError = ({ error }) => (
+    <div className="flex h-full w-1/2 m-auto items-center justify-center bg-base-100 text-base-600">
+        <div className="flex items-center justify-center">
+            <Icons.XSquare size="48" />
+        </div>
+        <div className="pl-2">
+            <div className="text-2xl">An Error has occurred</div>
+            <div className="pt-2">{error.message}</div>
+        </div>
+    </div>
+);
+
+GraphQLError.propTypes = {
+    error: PropTypes.shape({ message: PropTypes.string.isRequired }).isRequired
+};
 
 const ThrowingQuery = ({ children, ...rest }) => (
     <Query {...rest}>
-        {queryResult => {
-            if (queryResult.error) {
-                Raven.captureException(queryResult.error);
+        {({ error, ...queryResult }) => {
+            if (error) {
+                Raven.captureException(error);
+                if (process.env.NODE_ENV === 'development') {
+                    return <GraphQLError error={error} />;
+                }
             }
-            return children(queryResult);
+            return children({ error, ...queryResult });
         }}
     </Query>
 );
