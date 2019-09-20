ae8b987d6005e8ac94dad807da620e6165d07fb1
Assaf Weinberg <assaf@stackrox.com>
2019-09-20 13:08:04 -0700

    added url read and write (stackrox/rox#3505)

---
+++ ui/src/modules/URLReadWrite.js
@@ -0,0 +1,139 @@
+import pageTypes from 'constants/pageTypes';
+import appContexts from 'constants/appContextTypes';
+import { generatePath } from 'react-router-dom';
+import qs from 'qs';
+import {
+    nestedPaths as workflowPaths,
+    riskPath,
+    secretsPath,
+    urlEntityListTypes,
+    urlEntityTypes,
+    policiesPath
+} from '../routePaths';
+
+function getTypeKeyFromParamValue(value, listOnly) {
+    const listMatch = Object.entries(urlEntityListTypes).find(entry => entry[1] === value);
+    const entityMatch = Object.entries(urlEntityTypes).find(entry => entry[1] === value);
+    const match = listOnly ? listMatch : listMatch || entityMatch;
+    return match ? match[0] : null;
+}
+
+function isListType(value) {
+    return Object.values(urlEntityListTypes).includes(value);
+}
+
+function getPageType(workflowState) {
+    if (workflowState.pageEntityListType) {
+        return pageTypes.LIST;
+    }
+    if (workflowState.pageEntityType) {
+        return pageTypes.ENTITY;
+    }
+    return pageTypes.DASHBOARD;
+}
+
+export function generateURL(workflowState, queryParams) {
+    const pageType = getPageType(workflowState);
+    const { context } = workflowState;
+
+    const defaultPathMap = {
+        [pageTypes.DASHBOARD]: workflowPaths.DASHBOARD,
+        [pageTypes.ENTITY]: workflowPaths.ENTITY,
+        [pageTypes.LIST]: workflowPaths.LIST
+    };
+
+    const legacyPathMap = {
+        [appContexts.RISK]: {
+            [pageTypes.ENTITY]: riskPath,
+            [pageTypes.LIST]: '/main/risk',
+            [pageTypes.DASHBOARD]: '/main/risk'
+        },
+        [appContexts.SECRET]: {
+            [pageTypes.ENTITY]: secretsPath,
+            [pageTypes.LIST]: '/main/configmanagement/secrets',
+            [pageTypes.DASHBOARD]: '/main/configmanagement/secrets'
+        },
+        [appContexts.POLICY]: {
+            [pageTypes.ENTITY]: policiesPath,
+            [pageTypes.LIST]: '/main/policies',
+            [pageTypes.DASHBOARD]: '/main/policies'
+        }
+    };
+
+    const contextPaths = legacyPathMap[context] || defaultPathMap;
+    if (!contextPaths)
+        throw new Error(`Can't generate a URL. No paths found for context ${context}`);
+
+    const path = contextPaths[pageType];
+    if (!path)
+        throw new Error(
+            `Can't generate a URL. No path found for context ${context} and page type ${pageType}`
+        );
+
+    const params = { ...workflowState };
+
+    // Patching url params for legacy contexts
+    if (context === appContexts.SECRET) {
+        params.secretId = params.pageEntityId;
+    } else if (context === appContexts.RISK) {
+        params.deploymentId = params.pageEntityId;
+    }
+
+    // Mapping from entity types to url entityTypes
+    params.pageEntityListType = urlEntityListTypes[params.pageEntityListType];
+    params.entityType2 =
+        urlEntityTypes[params.entityType2] || urlEntityListTypes[params.entityListType2];
+    params.entityType1 =
+        urlEntityTypes[params.entityType1] || urlEntityListTypes[params.entityListType1];
+    params.pageEntityType = urlEntityTypes[params.pageEntityType];
+
+    const queryString = queryParams
+        ? qs.stringify(queryParams, {
+              addQueryPrefix: true,
+              arrayFormat: 'repeat',
+              encodeValuesOnly: true
+          })
+        : '';
+
+    return generatePath(path, params) + queryString;
+}
+
+export function parseURL(match, location) {
+    if (!match) return {};
+    const params = { ...match.params };
+
+    // Mapping from url to entity types
+    if (params.pageEntityListType)
+        params.pageEntityListType = getTypeKeyFromParamValue(params.pageEntityListType);
+    if (params.entityListType2) {
+        params.entityListType2 = getTypeKeyFromParamValue(params.entityListType2, true);
+    } else if (params.entityType2) {
+        if (isListType(params.entityType2)) {
+            params.entityListType2 = getTypeKeyFromParamValue(params.entityType2, true);
+            delete params.entityType2;
+        } else {
+            params.entityType2 = getTypeKeyFromParamValue(params.entityType2);
+        }
+    }
+
+    if (params.entityListType1) {
+        params.entityListType1 = getTypeKeyFromParamValue(params.entityListType1);
+    } else if (params.entityType1) {
+        if (isListType(params.entityType1)) {
+            params.entityListType1 = getTypeKeyFromParamValue(params.entityType1, true);
+            delete params.entityType1;
+        } else {
+            params.entityType1 = getTypeKeyFromParamValue(params.entityType1);
+        }
+    }
+    if (params.pageEntityType)
+        params.pageEntityType = getTypeKeyFromParamValue(params.pageEntityType);
+
+    const query =
+        location && location.search ? qs.parse(location.search, { ignoreQueryPrefix: true }) : {};
+
+    return {
+        params,
+        query
+    };
+}
---
+++ ui/src/modules/URLReadWrite.test.js
@@ -0,0 +1,168 @@
+import entityTypes from 'constants/entityTypes';
+import appContexts from 'constants/appContextTypes';
+import { urlEntityListTypes, urlEntityTypes } from '../routePaths';
+import { parseURL, generateURL } from './URLReadWrite';
+
+function getMatch(params) {
+    return {
+        params
+    };
+}
+
+function getLocation(search) {
+    return {
+        search
+    };
+}
+
+const searchParams = {
+    s1: {
+        sk1: 'v1',
+        sk2: 'v2'
+    },
+    sort1: 's1',
+    s2: {
+        sk3: 'v3',
+        sk4: 'v4'
+    },
+    sort2: 's2'
+};
+
+describe('ParseURL', () => {
+    it('reads workflow state params from url', () => {
+        const URLParams = {
+            context: appContexts.CONFIG_MANAGEMENT,
+            pageEntityType: urlEntityTypes.CLUSTER,
+            pageEntityId: 'pageEntityId',
+            entityListType1: urlEntityListTypes.DEPLOYMENT,
+            entityId1: 'entityId1',
+            entityType2: urlEntityTypes.NAMESPACE,
+            entityId2: 'entityId2'
+        };
+
+        const match = getMatch(URLParams);
+        const location = getLocation(searchParams);
+
+        const workflowStateParams = parseURL(match, location).params;
+        expect(workflowStateParams.pageEntityType).toEqual(entityTypes.CLUSTER);
+        expect(workflowStateParams.entityListType1).toEqual(entityTypes.DEPLOYMENT);
+        expect(workflowStateParams.entityType2).toEqual(entityTypes.NAMESPACE);
+    });
+
+    // TODO: test translation of urlEntityListTypes to urlEntityTypes
+});
+
+describe('GenerateURL', () => {
+    // TODO: use workflowStateManager to get a real workflow state objects
+    it('generates an entity<>entity<>entity page url from workflowState', () => {
+        const workflowState = {
+            context: appContexts.CONFIG_MANAGEMENT,
+            pageEntityType: entityTypes.CLUSTER,
+            pageEntityId: 'pageEntityId',
+            entityType1: entityTypes.DEPLOYMENT,
+            entityId1: 'entityId1',
+            entityType2: entityTypes.NAMESPACE,
+            entityId2: 'entityId2'
+        };
+        const url = generateURL(workflowState, searchParams);
+        const queryString = `?s1[sk1]=${searchParams.s1.sk1}&s1[sk2]=${searchParams.s1.sk2}&sort1=${
+            searchParams.sort1
+        }&s2[sk3]=${searchParams.s2.sk3}&s2[sk4]=${searchParams.s2.sk4}&sort2=${
+            searchParams.sort2
+        }`;
+        expect(url).toBe(
+            `/main/configmanagement/cluster/pageEntityId/deployment/entityId1/namespace/entityId2${queryString}`
+        );
+    });
+
+    it('generates an entity<>list<>entity page url from workflowState', () => {
+        const workflowState = {
+            context: appContexts.CONFIG_MANAGEMENT,
+            pageEntityType: entityTypes.CLUSTER,
+            pageEntityId: 'pageEntityId',
+            entityListType1: entityTypes.DEPLOYMENT,
+            entityId1: 'entityId1',
+            entityType2: entityTypes.NAMESPACE,
+            entityId2: 'entityId2'
+        };
+        const url = generateURL(workflowState);
+        expect(url).toBe(
+            `/main/configmanagement/cluster/pageEntityId/deployments/entityId1/namespace/entityId2`
+        );
+    });
+
+    it('generates an entity<>entity<>list page url from workflowState', () => {
+        const workflowState = {
+            context: appContexts.CONFIG_MANAGEMENT,
+            pageEntityType: entityTypes.CLUSTER,
+            pageEntityId: 'pageEntityId',
+            entityType1: entityTypes.DEPLOYMENT,
+            entityId1: 'entityId1',
+            entityListType2: entityTypes.NAMESPACE,
+            entityId2: 'entityId2'
+        };
+        const url = generateURL(workflowState);
+        expect(url).toBe(
+            `/main/configmanagement/cluster/pageEntityId/deployment/entityId1/namespaces/entityId2`
+        );
+    });
+
+    it('generates an entity<>list<>list page url from workflowState', () => {
+        const workflowState = {
+            context: appContexts.CONFIG_MANAGEMENT,
+            pageEntityType: entityTypes.CLUSTER,
+            pageEntityId: 'pageEntityId',
+            entityListType1: entityTypes.DEPLOYMENT,
+            entityId1: 'entityId1',
+            entityListType2: entityTypes.NAMESPACE,
+            entityId2: 'entityId2'
+        };
+        const url = generateURL(workflowState);
+        expect(url).toBe(
+            `/main/configmanagement/cluster/pageEntityId/deployments/entityId1/namespaces/entityId2`
+        );
+    });
+
+    it('generates an list<>entity page url from workflowState', () => {
+        const workflowState = {
+            context: appContexts.CONFIG_MANAGEMENT,
+            pageEntityListType: entityTypes.CLUSTER,
+            entityId1: 'entityId1',
+            entityType2: entityTypes.NAMESPACE,
+            entityId2: 'entityId2'
+        };
+        const url = generateURL(workflowState);
+        expect(url).toBe(`/main/configmanagement/clusters/entityId1/namespace/entityId2`);
+    });
+
+    it('generates an list<>list page url from workflowState', () => {
+        const workflowState = {
+            context: appContexts.CONFIG_MANAGEMENT,
+            pageEntityListType: entityTypes.CLUSTER,
+            entityId1: 'entityId1',
+            entityListType2: entityTypes.NAMESPACE,
+            entityId2: 'entityId2'
+        };
+        const url = generateURL(workflowState);
+        expect(url).toBe(`/main/configmanagement/clusters/entityId1/namespaces/entityId2`);
+    });
+
+    it('generates a query string from searchParams', () => {
+        const workflowState = {
+            context: appContexts.CONFIG_MANAGEMENT,
+            pageEntityType: entityTypes.CLUSTER,
+            pageEntityId: 'pageEntityId',
+            entityType1: entityTypes.DEPLOYMENT,
+            entityId1: 'entityId1',
+            entityType2: entityTypes.NAMESPACE,
+            entityId2: 'entityId2'
+        };
+        const url = generateURL(workflowState, searchParams);
+        const queryString = `?s1[sk1]=${searchParams.s1.sk1}&s1[sk2]=${searchParams.s1.sk2}&sort1=${
+            searchParams.sort1
+        }&s2[sk3]=${searchParams.s2.sk3}&s2[sk4]=${searchParams.s2.sk4}&sort2=${
+            searchParams.sort2
+        }`;
+        expect(url).toEqual(expect.stringContaining(queryString));
+    });
+});
