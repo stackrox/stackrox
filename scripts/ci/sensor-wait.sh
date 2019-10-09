bfd23e6c886e5fbcb3115e7164111fdb65a827ac
Malte Isberner <misberner@users.noreply.github.com>
2019-10-08 18:21:49 -0700

    ROX-3464: Remove :ro suffix in compliance DS mount paths, use readOnly: true instead (stackrox/rox#3643)

--- scripts/ci/sensor-wait.sh
+++ scripts/ci/sensor-wait.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -eu
 
 # Wait for sensor to be up
@@ -14,7 +14,12 @@ set -eu
 
 main() {
     echo "Waiting for sensor to start"
+    start_time="$(date '+%s')"
     until [ "$(kubectl get pod -n stackrox --selector 'app=sensor' | grep Running | wc -l)" -eq 1 ]; do
+    	if (( $(date '+%s') - start_time > 300 )); then
+    		echo >&2 "Timed out after 5m"
+    		exit 1
+    	fi
         echo -n .
         sleep 1
     done
@@ -25,7 +30,12 @@ main() {
     fi
 
     echo "Waiting for collectors to start"
+    start_time="$(date '+%s')"
     until [ "$(kubectl -n stackrox get ds/collector | tail -n +2 | awk '{print $2}')" -eq "$(kubectl -n stackrox get ds/collector | tail -n +2 | awk '{print $4}')" ]; do
+    	if (( $(date '+%s') - start_time > 300 )); then
+    		echo >&2 "Timed out after 5m"
+    		exit 1
+    	fi
         echo -n .
         sleep 1
     done
