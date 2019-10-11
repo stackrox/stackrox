752b0393bd5ef8dec8d9abe839a2ff4f707e32bc
David Terrell <13537+dbt@users.noreply.github.com>
2019-10-11 15:00:42 -0700

    ROX-3472 API page doesn't load (stackrox/rox#3666)
    Looks like some dependency versions got out of sync and styled-components
    wasn't working right. I bumped redoc and styled-components and it still
    wasn't rendering, so I rewrote the page with hooks and the problem fixed
    itself, so... yay?
    
    It is, for what it's worth, way easier to read the code than it was before.

--- ui/src/Containers/Docs/ApiPage.js
+++ ui/src/Containers/Docs/ApiPage.js
@@ -1,8 +1,43 @@
-import React from 'react';
-import SwaggerBrowserComponent from './SwaggerBrowserComponent';
+import React, { useEffect, useState } from 'react';
+import { RedocStandalone } from 'redoc';
+import Raven from 'raven-js';
+
+import LoadingSection from 'Components/LoadingSection';
+import axios from 'services/instance';
+
+function SwaggerBrowser({ uri }) {
+    const [result, setResult] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
+    useEffect(
+        () => {
+            const fetchData = async () => {
+                try {
+                    const fetchResult = await axios(uri);
+                    setResult(fetchResult);
+                    setLoading(false);
+                } catch (e) {
+                    Raven.captureException(e);
+                    setIsError(true);
+                }
+            };
+            fetchData();
+        },
+        [uri]
+    );
+    if (isError) {
+        return <div>Unable to load API data.</div>;
+    }
+    if (loading) {
+        return <LoadingSection />;
+    }
+    if (result) {
+        return <RedocStandalone spec={result.data} />;
+    }
+}
 
 function ApiPage() {
-    return <SwaggerBrowserComponent uri="/api/docs/swagger" />;
+    return <SwaggerBrowser uri="/api/docs/swagger" />;
 }
 
 export default ApiPage;
