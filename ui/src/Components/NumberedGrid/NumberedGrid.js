bbc32da475aa4a881196d6ecc5beb132f30c4e19
Saif Chaudhry <sachaudh@users.noreply.github.com>
2019-10-16 09:20:28 -0700

    ROX-3442: Created NumberedGrid Component (stackrox/rox#3683)

---
+++ ui/src/Components/NumberedGrid/NumberedGrid.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const NumberedGrid = ({ data }) => {
+    const list = data.map(({ text, subText, url, component }, index) => {
+        const className = `inline-block w-full py-2 px-2 border-b border-base-300 ${
+            url ? 'hover:bg-base-200' : ''
+        }`;
+        let content = (
+            <div className="flex flex-1 items-center">
+                <span className="text-base-600 self-center text-2xl tracking-widest pl-4 pr-4 font-600">
+                    {index + 1}
+                </span>
+                <div className="flex flex-col">
+                    {subText && (
+                        <div className="text-base-500 italic font-600 text-sm mb-1">{subText}</div>
+                    )}
+                    <div className="text-base-600 font-600 text-base">{text}</div>
+                    {component && <div className="mt-2">{component}</div>}
+                </div>
+            </div>
+        );
+        if (url) {
+            content = (
+                <Link
+                    key={text}
+                    to={url}
+                    className="flex items-center no-underline text-base-600 hover:bg-base-200 inline-block w-full"
+                >
+                    {content}
+                </Link>
+            );
+        }
+        return <li className={className}>{content}</li>;
+    });
+    return (
+        <ul
+            className="list-reset w-full columns-2 columns-gap-0"
+            style={{ columnRule: '1px solid var(--base-300)' }}
+        >
+            {list}
+        </ul>
+    );
+};
+
+NumberedGrid.propTypes = {
+    data: PropTypes.arrayOf(
+        PropTypes.shape({
+            text: PropTypes.string.isRequired,
+            subText: PropTypes.string,
+            components: PropTypes.element,
+            url: PropTypes.string
+        })
+    )
+};
+
+NumberedGrid.defaultProps = {
+    data: []
+};
+
+export default NumberedGrid;
---
+++ ui/src/Components/NumberedGrid/NumberedGrid.stories.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+
+import SeverityStackedPill from 'Components/visuals/SeverityStackedPill';
+import NumberedGrid from './NumberedGrid';
+
+export default {
+    title: 'NumberedGrid',
+    component: NumberedGrid
+};
+
+export const withText = () => {
+    const data = [
+        { text: 'docker.io/library/nginx:1.7.9' },
+        { text: 'k8s.gcr.io/prometheus-to-sd:v0.3.1' },
+        { text: 'docker.io/stackrox/scanner:0.5.4' }
+    ];
+    return <NumberedGrid data={data} />;
+};
+
+export const withSubText = () => {
+    const data = [
+        { text: 'docker.io/library/nginx:1.7.9', subText: 'remote/stackrox' },
+        { text: 'k8s.gcr.io/prometheus-to-sd:v0.3.1', subText: 'remote/stackrox' },
+        { text: 'docker.io/stackrox/scanner:0.5.4', subText: 'remote/stackrox' },
+        { text: 'docker.io/stackrox/scanner:2.5.4', subText: 'remote/stackrox' }
+    ];
+    return <NumberedGrid data={data} />;
+};
+
+export const withURL = () => {
+    const data = [
+        { text: 'docker.io/library/nginx:1.7.9', url: '/main/images/123' },
+        { text: 'k8s.gcr.io/prometheus-to-sd:v0.3.1', url: '/main/images/456' },
+        { text: 'docker.io/stackrox/scanner:0.5.4', url: '/main/images/789' },
+        { text: 'docker.io/stackrox/scanner:2.5.4', url: '/main/images/123' }
+    ];
+    return (
+        <MemoryRouter>
+            <NumberedGrid data={data} />
+        </MemoryRouter>
+    );
+};
+
+export const withComponent = () => {
+    const data = [
+        {
+            text: 'docker.io/library/nginx:1.7.9',
+            component: <SeverityStackedPill low={10} medium={20} />
+        },
+        {
+            text: 'k8s.gcr.io/prometheus-to-sd:v0.3.1',
+            component: <SeverityStackedPill high={30} critical={5} />
+        },
+        {
+            text: 'docker.io/stackrox/scanner:0.5.4',
+            component: <SeverityStackedPill low={10} medium={20} high={30} critical={5} />
+        },
+        {
+            text: 'docker.io/stackrox/scanner:0.5.4',
+            component: <SeverityStackedPill low={10} medium={20} high={30} critical={5} />
+        }
+    ];
+    return (
+        <MemoryRouter>
+            <NumberedGrid data={data} />
+        </MemoryRouter>
+    );
+};
