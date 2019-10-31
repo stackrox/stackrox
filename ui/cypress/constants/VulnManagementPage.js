c8176c7462a89ed7a9b2b34eb71009def51a0358
Rhama Sundara <49660438+rhamasv@users.noreply.github.com>
2019-10-31 11:59:49 -0700

    UI tests to validate the behavior of view all button of dashboard widgets of vuln mgmt page (stackrox/rox#3861)
    * added tests to validate behavior of view all button for all the widgets in dashboard of vukln mgmt page
    
    * added todo in the comments to check for sort category in the link for each of the widget

--- ui/cypress/constants/VulnManagementPage.js
+++ ui/cypress/constants/VulnManagementPage.js
@@ -8,9 +8,11 @@ export const url = {
         namespaces: `${baseURL}/namespaces`,
         deployments: `${baseURL}/deployments`,
         images: `${baseURL}/images`,
-        components: `${baseURL}/components`
+        components: `${baseURL}/components`,
+        cves: `${baseURL}/cves`
     }
 };
+
 export const dashboardSelectors = {
     widgets: "[data-test-id='widget']",
     tileLinks: "[data-test-id='tile-link']",
@@ -18,7 +20,11 @@ export const dashboardSelectors = {
     applicationAndInfrastructureDropdown: 'button:contains("Application & Infrastructure")',
     getMenuListItem: name => {
         return `[data-test-id="menu-list"] [data-test-id="${name}"]`;
-    }
+    },
+    getWidget: title => {
+        return `[data-test-id="widget"]:contains('${title}')`;
+    },
+    viewAllButton: 'button:contains("View All")'
 };
 export const selectors = {
     ...dashboardSelectors
--- ui/cypress/integration/vulnmanagement/dashboard.test.js
+++ ui/cypress/integration/vulnmanagement/dashboard.test.js
@@ -77,4 +77,52 @@ describe('Vuln Management Dashboard Page', () => {
         cy.get(selectors.getMenuListItem('images')).click();
         cy.url().should('contain', url.list.images);
     });
+    // TODO  add a check that there is a sort param on the link URL for sorting by the widget's appropriate sort
+    it('clicking the "Top Riskiest Images" widget\'s "View All" button should take you to the images list', () => {
+        cy.visit(url.dashboard);
+        cy.get(selectors.getWidget('Top Riskiest Images'))
+            .find(selectors.viewAllButton)
+            .click();
+        cy.url().should('contain', url.list.images);
+    });
+    // TODO  add a check that there is a sort param on the link URL for sorting by the widget's appropriate sort
+    it('clicking the "Frequently Violated Policies" widget\'s "View All" button should take you to the policies list', () => {
+        cy.visit(url.dashboard);
+        cy.get(selectors.getWidget('Frequently Violated Policies'))
+            .find(selectors.viewAllButton)
+            .click();
+        cy.url().should('contain', url.list.policies);
+    });
+    // TODO  add a check that there is a sort param on the link URL for sorting by the widget's appropriate sort
+    it('clicking the "Most Recent Vulnerabilities" widget\'s "View All" button should take you to the CVEs list', () => {
+        cy.visit(url.dashboard);
+        cy.get(selectors.getWidget('Most Recent Vulnerabilities'))
+            .find(selectors.viewAllButton)
+            .click();
+        cy.url().should('contain', url.list.cves);
+    });
+    // TODO  add a check that there is a sort param on the link URL for sorting by the widget's appropriate sort
+    it('clicking the "Most Common Vulnerabilities" widget\'s "View All" button should take you to the CVEs list', () => {
+        cy.visit(url.dashboard);
+        cy.get(selectors.getWidget('Most Common Vulnerabilities'))
+            .find(selectors.viewAllButton)
+            .click();
+        cy.url().should('contain', url.list.cves);
+    });
+    // TODO  add a check that there is a sort param on the link URL for sorting by the widget's appropriate sort
+    it('clicking the "Deployments With Most Severe Policy Violations" widget\'s "View All" button should take you to the policies list', () => {
+        cy.visit(url.dashboard);
+        cy.get(selectors.getWidget('Deployments With Most Severe Policy Violations'))
+            .find(selectors.viewAllButton)
+            .click();
+        cy.url().should('contain', url.list.deployments);
+    });
+    // TODO  add a check that there is a sort param on the link URL for sorting by the widget's appropriate sort
+    it('clicking the "Clusters With Most K8s Vulnerabilities" widget\'s "View All" button should take you to the clusters list', () => {
+        cy.visit(url.dashboard);
+        cy.get(selectors.getWidget('Clusters With Most K8s Vulnerabilities'))
+            .find(selectors.viewAllButton)
+            .click();
+        cy.url().should('contain', url.list.clusters);
+    });
 });
