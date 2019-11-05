828065f12316db05ff3b5755ec06122702da6587
nilanshu-sr <51386380+nilanshu-sr@users.noreply.github.com>
2019-11-05 15:44:40 -0800

    ROX-3590: Unit test coverage for K8s vulns APIs (stackrox/rox#3912)

---
+++ central/cve/converter/convert_utils_test.go
@@ -0,0 +1,243 @@
+package converter
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stackrox/k8s-istio-cve-pusher/nvd"
+	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/protoconv"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	publishedDateTime    = "2006-01-02T15:04Z"
+	lastModifiedDateTime = "2006-05-02T15:04Z"
+)
+
+func TestNvdCVEsToEmbeddedVulnerabilities(t *testing.T) {
+
+	cves := []nvd.CVEEntry{
+		{
+			CVE: nvd.CVE{
+				Metadata: nvd.CVEMetadata{
+					CVEID: "cve-2019-1",
+				},
+				References: nvd.ReferenceData{
+					Reference: []nvd.CVEReference{
+						{
+							URL: "Reference1",
+						},
+						{
+							URL: "Reference2",
+						},
+					},
+				},
+				Descriptions: nvd.CVEDescription{
+					Description: []nvd.DescriptionData{
+						{
+							Lang:  "en",
+							Value: "Description1",
+						},
+						{
+							Lang:  "en",
+							Value: "Description2",
+						},
+					},
+				},
+			},
+			LastModifiedDateTime: lastModifiedDateTime,
+			PublishedDateTime:    publishedDateTime,
+			Impact: nvd.Impact{
+				BaseMetricV2: nvd.BaseMetricV2{
+					CVSSv2: nvd.CVSSv2{
+						VectorString:     "AV:N/AC:L/Au:N/C:C/I:C/A:C",
+						Score:            10,
+						AccessVector:     "NETWORK",
+						AccessComplexity: "LOW",
+						Authentication:   "NONE",
+						ConfImpact:       "COMPLETE",
+						IntegImpact:      "COMPLETE",
+						AvailImpact:      "COMPLETE",
+					},
+					Severity:            "HIGH",
+					ExploitabilityScore: 10,
+					ImpactScore:         10,
+				},
+				BaseMetricV3: nvd.BaseMetricV3{
+					CVSSv3: nvd.CVSSv3{
+						VectorString:       "CVSS:3.0/AV:N/AC:L/PR:N/UI:N/S:U/C:H/I:H/A:H",
+						Score:              9.8,
+						AttackVector:       "NETWORK",
+						AttackComplexity:   "LOW",
+						PrivilegesRequired: "NONE",
+						UserInteraction:    "NONE",
+						Scope:              "UNCHANGED",
+						ConfImpact:         "HIGH",
+						IntegImpact:        "HIGH",
+						AvailImpact:        "HIGH",
+						Severity:           "CRITICAL",
+					},
+					ExploitabilityScore: 3.9,
+					ImpactScore:         5.9,
+				},
+			},
+		},
+		{
+			CVE: nvd.CVE{
+				Metadata: nvd.CVEMetadata{
+					CVEID: "cve-2019-2",
+				},
+				References: nvd.ReferenceData{
+					Reference: []nvd.CVEReference{
+						{
+							URL: "Reference3",
+						},
+						{
+							URL: "Reference4",
+						},
+					},
+				},
+				Descriptions: nvd.CVEDescription{
+					Description: []nvd.DescriptionData{
+						{
+							Lang:  "en",
+							Value: "Description3",
+						},
+						{
+							Lang:  "en",
+							Value: "Description4",
+						},
+					},
+				},
+			},
+			LastModifiedDateTime: lastModifiedDateTime,
+			PublishedDateTime:    publishedDateTime,
+			Impact: nvd.Impact{
+				BaseMetricV2: nvd.BaseMetricV2{
+					CVSSv2: nvd.CVSSv2{
+						VectorString:     "AV:N/AC:L/Au:S/C:N/I:P/A:N",
+						Score:            4,
+						AccessVector:     "NETWORK",
+						AccessComplexity: "LOW",
+						Authentication:   "SINGLE",
+						ConfImpact:       "NONE",
+						IntegImpact:      "PARTIAL",
+						AvailImpact:      "NONE",
+					},
+					Severity:            "MEDIUM",
+					ExploitabilityScore: 8,
+					ImpactScore:         2.9,
+				},
+				BaseMetricV3: nvd.BaseMetricV3{
+					CVSSv3: nvd.CVSSv3{
+						VectorString:       "CVSS:3.0/AV:N/AC:L/PR:L/UI:N/S:C/C:N/I:H/A:N",
+						Score:              7.7,
+						AttackVector:       "NETWORK",
+						AttackComplexity:   "LOW",
+						PrivilegesRequired: "LOW",
+						UserInteraction:    "NONE",
+						Scope:              "CHANGED",
+						ConfImpact:         "NONE",
+						IntegImpact:        "HIGH",
+						AvailImpact:        "NONE",
+						Severity:           "HIGH",
+					},
+					ExploitabilityScore: 3.1,
+					ImpactScore:         4,
+				},
+			},
+		},
+	}
+
+	expectedVuls := []storage.EmbeddedVulnerability{
+		{
+			Cve:          "cve-2019-1",
+			Cvss:         float32(cves[0].Impact.BaseMetricV3.CVSSv3.Score),
+			Summary:      "Description1",
+			Link:         "https://nvd.nist.gov/vuln/detail/cve-2019-1",
+			ScoreVersion: storage.EmbeddedVulnerability_V3,
+			CvssV2: &storage.CVSSV2{
+				Vector:              "AV:N/AC:L/Au:N/C:C/I:C/A:C",
+				AttackVector:        storage.CVSSV2_ATTACK_NETWORK,
+				AccessComplexity:    storage.CVSSV2_ACCESS_LOW,
+				Authentication:      storage.CVSSV2_AUTH_NONE,
+				Confidentiality:     storage.CVSSV2_IMPACT_COMPLETE,
+				Integrity:           storage.CVSSV2_IMPACT_COMPLETE,
+				Availability:        storage.CVSSV2_IMPACT_COMPLETE,
+				ExploitabilityScore: float32(cves[0].Impact.BaseMetricV2.ExploitabilityScore),
+				ImpactScore:         float32(cves[0].Impact.BaseMetricV2.ImpactScore),
+				Score:               float32(cves[0].Impact.BaseMetricV2.CVSSv2.Score),
+				Severity:            storage.CVSSV2_HIGH,
+			},
+			CvssV3: &storage.CVSSV3{
+				Vector:              "CVSS:3.0/AV:N/AC:L/PR:N/UI:N/S:U/C:H/I:H/A:H",
+				ExploitabilityScore: float32(cves[0].Impact.BaseMetricV3.ExploitabilityScore),
+				ImpactScore:         float32(cves[0].Impact.BaseMetricV3.ImpactScore),
+				AttackVector:        storage.CVSSV3_ATTACK_NETWORK,
+				AttackComplexity:    storage.CVSSV3_COMPLEXITY_LOW,
+				PrivilegesRequired:  storage.CVSSV3_PRIVILEGE_NONE,
+				UserInteraction:     storage.CVSSV3_UI_NONE,
+				Scope:               storage.CVSSV3_UNCHANGED,
+				Confidentiality:     storage.CVSSV3_IMPACT_HIGH,
+				Integrity:           storage.CVSSV3_IMPACT_HIGH,
+				Availability:        storage.CVSSV3_IMPACT_HIGH,
+				Score:               float32(cves[0].Impact.BaseMetricV3.CVSSv3.Score),
+				Severity:            storage.CVSSV3_CRITICAL,
+			},
+			VulnerabilityType: storage.EmbeddedVulnerability_K8S_VULNERABILITY,
+		},
+		{
+			Cve:          "cve-2019-2",
+			Cvss:         float32(cves[1].Impact.BaseMetricV3.CVSSv3.Score),
+			Summary:      "Description3",
+			Link:         "https://nvd.nist.gov/vuln/detail/cve-2019-2",
+			ScoreVersion: storage.EmbeddedVulnerability_V3,
+			CvssV2: &storage.CVSSV2{
+				Vector:              "AV:N/AC:L/Au:S/C:N/I:P/A:N",
+				AttackVector:        storage.CVSSV2_ATTACK_NETWORK,
+				AccessComplexity:    storage.CVSSV2_ACCESS_LOW,
+				Authentication:      storage.CVSSV2_AUTH_SINGLE,
+				Confidentiality:     storage.CVSSV2_IMPACT_NONE,
+				Integrity:           storage.CVSSV2_IMPACT_PARTIAL,
+				Availability:        storage.CVSSV2_IMPACT_NONE,
+				ExploitabilityScore: float32(cves[1].Impact.BaseMetricV2.ExploitabilityScore),
+				ImpactScore:         float32(cves[1].Impact.BaseMetricV2.ImpactScore),
+				Score:               float32(cves[1].Impact.BaseMetricV2.CVSSv2.Score),
+				Severity:            storage.CVSSV2_MEDIUM,
+			},
+			CvssV3: &storage.CVSSV3{
+				Vector:              "CVSS:3.0/AV:N/AC:L/PR:L/UI:N/S:C/C:N/I:H/A:N",
+				ExploitabilityScore: float32(cves[1].Impact.BaseMetricV3.ExploitabilityScore),
+				ImpactScore:         float32(cves[1].Impact.BaseMetricV3.ImpactScore),
+				AttackVector:        storage.CVSSV3_ATTACK_NETWORK,
+				AttackComplexity:    storage.CVSSV3_COMPLEXITY_LOW,
+				PrivilegesRequired:  storage.CVSSV3_PRIVILEGE_LOW,
+				UserInteraction:     storage.CVSSV3_UI_NONE,
+				Scope:               storage.CVSSV3_CHANGED,
+				Confidentiality:     storage.CVSSV3_IMPACT_NONE,
+				Integrity:           storage.CVSSV3_IMPACT_HIGH,
+				Availability:        storage.CVSSV3_IMPACT_NONE,
+				Score:               float32(cves[1].Impact.BaseMetricV3.CVSSv3.Score),
+				Severity:            storage.CVSSV3_HIGH,
+			},
+			VulnerabilityType: storage.EmbeddedVulnerability_K8S_VULNERABILITY,
+		},
+	}
+
+	for i := 0; i < len(cves); i++ {
+		expectedVul := &expectedVuls[i]
+
+		ts, err := time.Parse(timeFormat, publishedDateTime)
+		assert.Nil(t, err)
+		expectedVul.PublishedOn = protoconv.ConvertTimeToTimestamp(ts)
+
+		ts, err = time.Parse(timeFormat, lastModifiedDateTime)
+		assert.Nil(t, err)
+		expectedVul.LastModified = protoconv.ConvertTimeToTimestamp(ts)
+
+		actualVul, err := NvdCveToEmbeddedVulnerability(&cves[i])
+		assert.Nil(t, err)
+		assert.Equal(t, actualVul, expectedVul)
+	}
+}
--- central/graphql/resolvers/vulnerabilities_test.go
+++ central/graphql/resolvers/vulnerabilities_test.go
@@ -1,9 +1,12 @@
 package resolvers
 
 import (
+	"context"
 	"testing"
 
+	"github.com/golang/mock/gomock"
 	"github.com/stackrox/k8s-istio-cve-pusher/nvd"
+	clusterMocks "github.com/stackrox/rox/central/cluster/datastore/mocks"
 	v1 "github.com/stackrox/rox/generated/api/v1"
 	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/pkg/search"
@@ -208,7 +211,7 @@ func TestMultipleVersionCVEAffectsCluster(t *testing.T) {
 	cve3 := nvd.CVEEntry{
 		CVE: nvd.CVE{
 			Metadata: nvd.CVEMetadata{
-				CVEID: "CVE-2",
+				CVEID: "CVE-3",
 			},
 		},
 		Configurations: nvd.Configuration{
@@ -245,7 +248,7 @@ func TestMultipleVersionCVEAffectsCluster(t *testing.T) {
 	assert.Equal(t, false, ret)
 
 	ret = isClusterAffectedByCVE(cluster, &cve2)
-	assert.Equal(t, false, ret)
+	assert.Equal(t, true, ret)
 
 	cluster.Status.OrchestratorMetadata.Version = "v1.15.9"
 	ret = isClusterAffectedByCVE(cluster, &cve1)
@@ -330,7 +333,7 @@ func TestSingleAndMultipleVersionCVEAffectsCluster(t *testing.T) {
 
 	cluster.Status.OrchestratorMetadata.Version = "v1.10.3-rc1"
 	ret = isClusterAffectedByCVE(cluster, &cve)
-	assert.Equal(t, false, ret)
+	assert.Equal(t, true, ret)
 }
 
 func TestCountCVEsAffectsCluster(t *testing.T) {
@@ -443,4 +446,342 @@ func TestCountCVEsAffectsCluster(t *testing.T) {
 	}
 	assert.Equal(t, countAffectedClusters, 3)
 	assert.Equal(t, countFixableCVEs, 1)
+}
+
+func TestNonK8sCPEMatch(t *testing.T) {
+	cluster := &storage.Cluster{
+		Id:   "test_cluster_id1",
+		Name: "cluster1",
+		Status: &storage.ClusterStatus{
+			OrchestratorMetadata: &storage.OrchestratorMetadata{
+				Version: "v1.10.6",
+			},
+		},
+	}
+
+	cves := []nvd.CVEEntry{
+		{
+			CVE: nvd.CVE{
+				Metadata: nvd.CVEMetadata{
+					CVEID: "CVE-2019-1",
+				},
+			},
+			Configurations: nvd.Configuration{
+				Nodes: []nvd.Node{
+					{
+						Operator: "OR",
+						CPEMatch: []nvd.CPEMatch{
+							{
+								Vulnerable:            true,
+								CPE23Uri:              "cpe:2.3:a:vendorfoo:projectbar:*:*:*:*:*:*:*:*",
+								VersionStartIncluding: "1.10.1",
+								VersionEndExcluding:   "1.10.9",
+							},
+							{
+								Vulnerable:            true,
+								CPE23Uri:              "cpe:2.3:a:vendorfoo:projectbar:*:*:*:*:*:*:*:*",
+								VersionStartIncluding: "1.11.1",
+								VersionEndExcluding:   "1.11.9",
+							},
+							{
+								Vulnerable: true,
+								CPE23Uri:   "cpe:2.3:a:vendorfoo:projectbar:1.13.1:*:*:*:*:*:*:*",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			CVE: nvd.CVE{
+				Metadata: nvd.CVEMetadata{
+					CVEID: "CVE-2019-2",
+				},
+			},
+			Configurations: nvd.Configuration{
+				Nodes: []nvd.Node{
+					{
+						Operator: "OR",
+						CPEMatch: []nvd.CPEMatch{
+							{
+								Vulnerable:            true,
+								CPE23Uri:              "cpe:2.3:a:vendorfoo:projectbar:*:*:*:*:*:*:*:*",
+								VersionStartIncluding: "1.10.1",
+								VersionEndExcluding:   "1.10.9",
+							},
+							{
+								Vulnerable:            true,
+								CPE23Uri:              "cpe:2.3:a:vendorfoo:projectbar:*:*:*:*:*:*:*:*",
+								VersionStartIncluding: "1.11.1",
+								VersionEndExcluding:   "1.11.9",
+							},
+							{
+								Vulnerable: true,
+								CPE23Uri:   "cpe:2.3:a:vendorfoo:projectbar:1.13.1:*:*:*:*:*:*:*",
+							},
+						},
+					},
+					{
+						Operator: "OR",
+						CPEMatch: []nvd.CPEMatch{
+							{
+								Vulnerable:            true,
+								CPE23Uri:              "cpe:2.3:a:kubernetes:kubernetes:*:*:*:*:*:*:*:*",
+								VersionStartIncluding: "1.10.1",
+								VersionEndExcluding:   "1.10.9",
+							},
+							{
+								Vulnerable: true,
+								CPE23Uri:   "cpe:2.3:a:kubernetes:kubernetes:1.13.1:*:*:*:*:*:*:*",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	ret := isClusterAffectedByCVE(cluster, &cves[0])
+	assert.Equal(t, false, ret)
+	ret = isClusterAffectedByCVE(cluster, &cves[1])
+	assert.Equal(t, true, ret)
+}
+
+func TestFixableCVEs(t *testing.T) {
+	cves := []nvd.CVEEntry{
+		{
+			CVE: nvd.CVE{
+				Metadata: nvd.CVEMetadata{
+					CVEID: "CVE-2019-1",
+				},
+			},
+			Configurations: nvd.Configuration{
+				Nodes: []nvd.Node{
+					{
+						Operator: "OR",
+						CPEMatch: []nvd.CPEMatch{
+							{
+								Vulnerable:            true,
+								CPE23Uri:              "cpe:2.3:a:kubernetes:kubernetes:*:*:*:*:*:*:*:*",
+								VersionStartIncluding: "1.10.1",
+								VersionEndExcluding:   "1.10.9",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			CVE: nvd.CVE{
+				Metadata: nvd.CVEMetadata{
+					CVEID: "CVE-2",
+				},
+			},
+			Configurations: nvd.Configuration{
+				Nodes: []nvd.Node{
+					{
+						Operator: "OR",
+						CPEMatch: []nvd.CPEMatch{
+							{
+								Vulnerable: true,
+								CPE23Uri:   "cpe:2.3:a:kubernetes:kubernetes:1.10.6:*:*:*:*:*:*:*",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			CVE: nvd.CVE{
+				Metadata: nvd.CVEMetadata{
+					CVEID: "CVE-3",
+				},
+			},
+			Configurations: nvd.Configuration{
+				Nodes: []nvd.Node{
+					{
+						Operator: "OR",
+						CPEMatch: []nvd.CPEMatch{
+							{
+								Vulnerable:            true,
+								CPE23Uri:              "cpe:2.3:a:kubernetes:kubernetes:*:*:*:*:*:*:*:*",
+								VersionStartIncluding: "1.10.3",
+								VersionEndIncluding:   "1.10.7",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	actual := isK8sCVEFixable(&cves[0])
+	assert.Equal(t, actual, true)
+	actual = isK8sCVEFixable(&cves[1])
+	assert.Equal(t, actual, false)
+	actual = isK8sCVEFixable(&cves[2])
+	assert.Equal(t, actual, false)
+}
+
+func TestK8sCVEEnvImpact(t *testing.T) {
+	expected := []float64{0.6, 0.4, 0.4}
+
+	clusters := []*storage.Cluster{
+		{
+			Id:   "test_cluster_id1",
+			Name: "cluster1",
+			Status: &storage.ClusterStatus{
+				OrchestratorMetadata: &storage.OrchestratorMetadata{
+					Version: "v1.14.2",
+				},
+			},
+		},
+		{
+			Id:   "test_cluster_id2",
+			Name: "cluster2",
+			Status: &storage.ClusterStatus{
+				OrchestratorMetadata: &storage.OrchestratorMetadata{
+					Version: "v1.14.5+build1",
+				},
+			},
+		},
+		{
+			Id:   "test_cluster_id3",
+			Name: "cluster3",
+			Status: &storage.ClusterStatus{
+				OrchestratorMetadata: &storage.OrchestratorMetadata{
+					Version: "v1.15.4-beta1",
+				},
+			},
+		},
+		{
+			Id:   "test_cluster_id4",
+			Name: "cluster4",
+			Status: &storage.ClusterStatus{
+				OrchestratorMetadata: &storage.OrchestratorMetadata{
+					Version: "v1.16.3-alpha1+build2",
+				},
+			},
+		},
+		{
+			Id:   "test_cluster_id5",
+			Name: "cluster4",
+			Status: &storage.ClusterStatus{
+				OrchestratorMetadata: &storage.OrchestratorMetadata{
+					Version: "v1.17.5",
+				},
+			},
+		},
+	}
+
+	ctrl := gomock.NewController(t)
+	clusterDataStore := clusterMocks.NewMockDataStore(ctrl)
+	clusterDataStore.EXPECT().GetClusters(gomock.Any()).Return(clusters, nil).AnyTimes()
+
+	resolver := &Resolver{
+		ClusterDataStore: clusterDataStore,
+	}
+
+	cves := []nvd.CVEEntry{
+		{
+			CVE: nvd.CVE{
+				Metadata: nvd.CVEMetadata{
+					CVEID: "CVE-2019-1",
+				},
+			},
+			Configurations: nvd.Configuration{
+				Nodes: []nvd.Node{
+					{
+						Operator: "OR",
+						CPEMatch: []nvd.CPEMatch{
+							{
+								Vulnerable: true,
+								CPE23Uri:   "cpe:2.3:a:kubernetes:kubernetes:1.15.4:*:*:*:*:*:*:*",
+							},
+							{
+								Vulnerable:            true,
+								CPE23Uri:              "cpe:2.3:a:kubernetes:kubernetes:*:*:*:*:*:*:*:*",
+								VersionStartIncluding: "1.16.1",
+								VersionEndIncluding:   "1.16.9",
+							},
+							{
+								Vulnerable:            true,
+								CPE23Uri:              "cpe:2.3:a:kubernetes:kubernetes:*:*:*:*:*:*:*:*",
+								VersionStartIncluding: "1.17.1",
+								VersionEndExcluding:   "1.17.7",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			CVE: nvd.CVE{
+				Metadata: nvd.CVEMetadata{
+					CVEID: "CVE-2019-2",
+				},
+			},
+			Configurations: nvd.Configuration{
+				Nodes: []nvd.Node{
+					{
+						Operator: "OR",
+						CPEMatch: []nvd.CPEMatch{
+							{
+								Vulnerable:            true,
+								CPE23Uri:              "cpe:2.3:a:kubernetes:kubernetes:*:*:*:*:*:*:*:*",
+								VersionStartIncluding: "1.14.1",
+								VersionEndExcluding:   "1.14.9",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			CVE: nvd.CVE{
+				Metadata: nvd.CVEMetadata{
+					CVEID: "CVE-2019-3",
+				},
+			},
+
+			Configurations: nvd.Configuration{
+				Nodes: []nvd.Node{
+					{
+						Operator: "OR",
+						CPEMatch: []nvd.CPEMatch{
+							{
+								Vulnerable:            true,
+								CPE23Uri:              "cpe:2.3:a:kubernetes:kubernetes:*:*:*:*:*:*:*:*",
+								VersionStartIncluding: "1.10.1",
+								VersionEndIncluding:   "1.10.9",
+							},
+							{
+								Vulnerable:            true,
+								CPE23Uri:              "cpe:2.3:a:kubernetes:kubernetes:*:*:*:*:*:*:*:*",
+								VersionStartIncluding: "1.11.1",
+								VersionEndExcluding:   "1.11.7",
+							},
+							{
+								Vulnerable: true,
+								CPE23Uri:   "cpe:2.3:a:kubernetes:kubernetes:1.14.5:*:*:*:*:*:*:*",
+							},
+							{
+								Vulnerable: true,
+								CPE23Uri:   "cpe:2.3:a:kubernetes:kubernetes:1.15.4:alpha1:*:*:*:*:*:*",
+							},
+							{
+								Vulnerable: true,
+								CPE23Uri:   "cpe:2.3:a:kubernetes:kubernetes:1.15.4:beta1:*:*:*:*:*:*",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for i, cve := range cves {
+		actual, err := resolver.getAffectedClusterPercentage(context.Background(), &cve)
+		assert.Nil(t, err)
+		assert.Equal(t, actual, expected[i])
+	}
 }
