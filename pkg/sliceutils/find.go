211be85181d3ce96e1727ee125ae19aeb5e5eaee
Viswajith Venugopal <viswajithiii@gmail.com>
2019-11-05 12:08:28 -0800

    Add a FindMatching utility to pkg/sliceutils (stackrox/rox#3926)
    Add a `sliceutils.FindMatching` which is like sliceutils.Find, but takes a `func(elemType) bool` as the second argument. Had a use-case for this when writing tests in the scanner repo.

--- pkg/sliceutils/find.go
+++ pkg/sliceutils/find.go
@@ -19,3 +19,51 @@ func Find(slice interface{}, elem interface{}) int {
 	}
 	return -1
 }
+
+// FindMatching returns the first index of slice where the passed predicate -- which must be a
+// func(elemType) bool OR a func(*elemType) bool -- returns true, or -1 if it doesn't return true for any element.
+// Example usage:
+// FindMatching([]string{"a", "b", "cd"}, func(s string) bool {
+//   return len(s) > 1
+// })
+// will return 2.
+// Note that the predicate could also be a func(s *string) bool if you want to avoid copying.
+// This function will automatically pass pointers to each slice element if you pass such a predicate.
+// It uses reflect, and will be slow.
+// It panics at runtime if the arguments are of invalid types. There is no compile-time
+// safety of any kind.
+// Use ONLY in program initialization blocks, and in tests.
+func FindMatching(slice interface{}, predicate interface{}) int {
+	sliceVal := reflect.ValueOf(slice)
+	if sliceVal.Kind() != reflect.Slice {
+		panic(fmt.Errorf("FindMatching: value is not of slice kind but %v", sliceVal.Kind()))
+	}
+
+	predVal := reflect.ValueOf(predicate)
+	if predVal.Kind() != reflect.Func {
+		panic(fmt.Errorf("FindMatching: value is not of predicate kind but %v", predVal.Kind()))
+	}
+
+	predType := predVal.Type()
+	if predType.NumIn() != 1 {
+		panic(fmt.Errorf("FindMatching: expected func to be unary but it was %d-ary", predType.NumIn()))
+	}
+	if predType.NumOut() != 1 {
+		panic(fmt.Errorf("FindMatching: expected func to have exactly one return value, but it had %d", predType.NumOut()))
+	}
+
+	funcNeedsPtr := reflect.PtrTo(sliceVal.Type().Elem()).AssignableTo(predType.In(0))
+
+	l := sliceVal.Len()
+	for i := 0; i < l; i++ {
+		valToUse := sliceVal.Index(i)
+		if funcNeedsPtr {
+			valToUse = valToUse.Addr()
+		}
+		out := predVal.Call([]reflect.Value{valToUse})
+		if out[0].Bool() {
+			return i
+		}
+	}
+	return -1
+}
--- pkg/sliceutils/find_test.go
+++ pkg/sliceutils/find_test.go
@@ -15,3 +15,40 @@ func TestFind(t *testing.T) {
 	assert.Equal(t, -1, Find(slice, 3))
 	assert.Equal(t, 1, Find(slice, myType(3)))
 }
+
+type myWeirdStruct struct {
+	a string
+	b int
+}
+
+func TestFindMatching(t *testing.T) {
+	t.Parallel()
+	a := assert.New(t)
+
+	slice := []myWeirdStruct{
+		{"twinkle twinkle", 1},
+		{"little star", 2},
+	}
+	a.Equal(-1, FindMatching(slice, func(weirdStruct myWeirdStruct) bool {
+		return weirdStruct.b > 2
+	}))
+	a.Equal(0, FindMatching(slice, func(weirdStruct myWeirdStruct) bool {
+		return weirdStruct.b > 0
+	}))
+	a.Equal(1, FindMatching(slice, func(weirdStruct myWeirdStruct) bool {
+		return weirdStruct.b > 1
+	}))
+	a.Equal(1, FindMatching(slice, func(weirdStruct *myWeirdStruct) bool {
+		return weirdStruct.b > 1
+	}))
+	a.Panics(func() {
+		FindMatching([]string{"1", "2"}, func(int) bool {
+			return false
+		})
+	})
+	a.Panics(func() {
+		FindMatching("1", func(string) bool {
+			return false
+		})
+	})
+}
