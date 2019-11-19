58a075180e1f84cf7e875e7e28cb311096287c0f
Viswajith Venugopal <viswajithiii@gmail.com>
2019-11-19 15:46:25 -0800

    Add a util to figure out if a directory is empty (stackrox/rox#4076)

---
+++ pkg/fileutils/dir_empty.go
@@ -0,0 +1,27 @@
+package fileutils
+
+import (
+	"io"
+	"os"
+
+	"github.com/stackrox/rox/pkg/utils"
+)
+
+// DirExistsAndIsEmpty returns whether the given path is an existing directory, and exists.
+// It is more efficient than calling ioutil.ReadDir for large directories.
+// It returns false unless it determines for certain that the directory exists and is empty --
+// in particular, this means that any underlying filesystem-related errors are swallowed.
+// Callers needing more fine-grained information will not be able to use this function.
+func DirExistsAndIsEmpty(path string) bool {
+	f, err := os.Open(path)
+	if err != nil {
+		return false
+	}
+	defer utils.IgnoreError(f.Close)
+	_, err = f.Readdirnames(1)
+	// If no error, it means the directory was not empty.
+	// If another error other than io.EOF, it means we
+	// couldn't determine if the directory was empty.
+	// (This happens if, for example, the path is not a directory.)
+	return err == io.EOF
+}
---
+++ pkg/fileutils/dir_empty_test.go
@@ -0,0 +1,33 @@
+package fileutils
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func assertPathReturns(t *testing.T, path string, retVal bool) {
+	assert.Equal(t, retVal, DirExistsAndIsEmpty(path))
+}
+
+func TestDirEmpty(t *testing.T) {
+	tempDir, err := ioutil.TempDir("", "")
+	require.NoError(t, err)
+	defer func() {
+		require.NoError(t, os.RemoveAll(tempDir))
+	}()
+
+	assertPathReturns(t, tempDir, true)
+	randomFile := filepath.Join(tempDir, "RANDOM_FILE")
+	assertPathReturns(t, randomFile, false)
+
+	f, err := os.Create(randomFile)
+	require.NoError(t, err)
+	require.NoError(t, f.Close())
+	assertPathReturns(t, tempDir, false)
+	assertPathReturns(t, randomFile, false)
+}
