818402d581b472805feb79f60e29ff75ab8ae56a
Robert Stark <42253135+derbergkert@users.noreply.github.com>
2019-12-02 09:59:34 -0800

    Add flag for managed DB mode (stackrox/rox#4130)

--- pkg/features/list.go
+++ pkg/features/list.go
@@ -12,6 +12,9 @@ var (
 	// ProbeUpload enables the possibility to upload collector probes to central.
 	ProbeUpload = registerFeature("Enable support for uploading collector probes", "ROX_PROBE_UPLOAD", true)
 
+	// ManagedDB enabled the newly StackRox managed DB transaction sequencing.
+	ManagedDB = registerFeature("Use managed sequencing for the embedded Badger DB", "ROX_MANAGED_DB", false)
+
 	// LanguageScanner enables the deployment of the language scanner
 	LanguageScanner = registerFeature("Enable support for the version of the image scanner that detects on languages", "ROX_LANGUAGE_SCANNER", false)
 )
