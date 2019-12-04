3dd4f5722a90d5bed7f061fab0ab7b8e184a38fb
Saif Chaudhry <sachaudh@users.noreply.github.com>
2019-12-03 16:00:55 -0800

    ROX-2081: Network graph filter bar shows auto-complete options that aren't in the selected cluster (stackrox/rox#4144)

--- ui/src/sagas/searchAutocompleteSagas.js
+++ ui/src/sagas/searchAutocompleteSagas.js
@@ -1,21 +1,34 @@
-import { take, call, fork, put } from 'redux-saga/effects';
+import { take, call, fork, put, select } from 'redux-saga/effects';
 
 import * as service from 'services/SearchService';
 import { actions, types } from 'reducers/searchAutocomplete';
+import { selectors } from 'reducers';
 
-function* getAutoCompleteResults(query) {
+function* getAutoCompleteResults(request) {
     try {
-        const result = yield call(service.fetchAutoCompleteResults, query);
+        const result = yield call(service.fetchAutoCompleteResults, request);
         yield put(actions.recordAutoCompleteResponse(result));
     } catch (error) {
         yield put(actions.recordAutoCompleteResponse([]));
     }
 }
 
+function* getNetworkAutoCompleteResults(request) {
+    const selectedClusterId = yield select(selectors.getSelectedNetworkClusterId);
+    const queryWithClusterId = `Cluster Id:${selectedClusterId}+${request.query}`;
+    const newRequest = { ...request, query: queryWithClusterId };
+    yield getAutoCompleteResults(newRequest);
+}
+
 function* watchFetchRequest() {
     while (true) {
         const request = yield take(types.SEND_AUTOCOMPLETE_REQUEST);
-        yield fork(getAutoCompleteResults, request);
+        const location = yield select(selectors.getLocation);
+        if (location.pathname === '/main/network') {
+            yield fork(getNetworkAutoCompleteResults, request);
+        } else {
+            yield fork(getAutoCompleteResults, request);
+        }
     }
 }
 
