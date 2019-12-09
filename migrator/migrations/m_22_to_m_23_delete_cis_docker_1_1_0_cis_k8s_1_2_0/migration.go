ee8428f8ae881fd9f1a71043825c4dc6f28ddb4b
Mandar Darwatkar <mandar@stackrox.com>
2019-12-09 15:36:39 -0800

    [ROX-3767] Wipe out unsupported compliance standards run results (stackrox/rox#4190)
    * Wipe out unsupported compliance standards run results

---
+++ migrator/migrations/m_22_to_m_23_delete_cis_docker_1_1_0_cis_k8s_1_2_0/migration.go
@@ -0,0 +1,63 @@
+package m22to23
+
+import (
+	"github.com/dgraph-io/badger"
+	bolt "github.com/etcd-io/bbolt"
+	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/migrator/migrations"
+	"github.com/stackrox/rox/migrator/types"
+)
+
+var complianceResultsBucketName = []byte("compliance-run-results")
+
+var migration = types.Migration{
+	StartingSeqNum: 22,
+	VersionAfter:   storage.Version{SeqNum: 23},
+	Run:            deleteUnsupportedComplianceStandards,
+}
+
+func deleteUnsupportedComplianceStandards(db *bolt.DB, _ *badger.DB) error {
+	err := db.Update(func(tx *bolt.Tx) error {
+		complianceResultsBucket := tx.Bucket(complianceResultsBucketName)
+		if complianceResultsBucket == nil {
+			return nil
+		}
+
+		return deleteUnsupportedStandardRunResults(complianceResultsBucket)
+	})
+	return err
+}
+
+func deleteUnsupportedStandardRunResults(resultsBucket *bolt.Bucket) error {
+	return resultsBucket.ForEach(func(clusterKey, _ []byte) error {
+		clusterBucket := resultsBucket.Bucket(clusterKey)
+		if clusterBucket == nil {
+			return nil
+		}
+
+		return deleteUnsupportedStandardRunResultsForCluster(clusterBucket)
+	})
+}
+
+func deleteUnsupportedStandardRunResultsForCluster(clusterBucket *bolt.Bucket) error {
+	return clusterBucket.ForEach(func(standardKey, _ []byte) error {
+		if !standardToDelete(string(standardKey)) {
+			return nil
+		}
+
+		standardBucket := clusterBucket.Bucket(standardKey)
+		if standardBucket == nil {
+			return nil
+		}
+
+		return clusterBucket.DeleteBucket(standardKey)
+	})
+}
+
+func standardToDelete(id string) bool {
+	return id == "CIS_Docker_v1_1_0" || id == "CIS_Kubernetes_v1_2_0"
+}
+
+func init() {
+	migrations.MustRegisterMigration(migration)
+}
---
+++ migrator/migrations/m_22_to_m_23_delete_cis_docker_1_1_0_cis_k8s_1_2_0/migration_test.go
@@ -0,0 +1,123 @@
+package m22to23
+
+import (
+	"testing"
+
+	"github.com/etcd-io/bbolt"
+	"github.com/pkg/errors"
+	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/migrator/bolthelpers"
+	"github.com/stackrox/rox/pkg/testutils"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMigration(t *testing.T) {
+	cases := []struct {
+		runResults *storage.ComplianceRunResults
+		deleted    bool
+	}{
+		{
+			runResults: &storage.ComplianceRunResults{
+				RunMetadata: &storage.ComplianceRunMetadata{
+					ClusterId:  "1",
+					StandardId: "CIS_Docker_v1_1_0",
+				},
+			},
+			deleted: true,
+		},
+		{
+			runResults: &storage.ComplianceRunResults{
+				RunMetadata: &storage.ComplianceRunMetadata{
+					ClusterId:  "1",
+					StandardId: "CIS_Kubernetes_v1_4_1",
+				},
+			},
+			deleted: false,
+		},
+		{
+			runResults: &storage.ComplianceRunResults{
+				RunMetadata: &storage.ComplianceRunMetadata{
+					ClusterId:  "2",
+					StandardId: "CIS_Kubernetes_v1_2_0",
+				},
+			},
+			deleted: true,
+		},
+		{
+			runResults: &storage.ComplianceRunResults{
+				RunMetadata: &storage.ComplianceRunMetadata{
+					ClusterId:  "2",
+					StandardId: "CIS_Kubernetes_v1_5",
+				},
+			},
+			deleted: false,
+		},
+		{
+			runResults: &storage.ComplianceRunResults{
+				RunMetadata: &storage.ComplianceRunMetadata{
+					ClusterId:  "2",
+					StandardId: "CIS_Docker_v1_2_0",
+				},
+			},
+			deleted: false,
+		},
+	}
+
+	db, err := bolthelpers.NewTemp(testutils.DBFileNameForT(t))
+	require.NoError(t, err)
+
+	createComplianceRunResultsBucket(t, db)
+
+	for _, c := range cases {
+		fillResultsData(t, db, c.runResults)
+	}
+
+	require.NoError(t, deleteUnsupportedComplianceStandards(db, nil))
+
+	for _, c := range cases {
+		validateMigration(t, db, c.runResults, c.deleted)
+	}
+}
+
+func createComplianceRunResultsBucket(t *testing.T, db *bbolt.DB) {
+	assert.NoError(t, db.Update(func(tx *bbolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists(complianceResultsBucketName)
+		return err
+	}))
+}
+
+func fillResultsData(t *testing.T, db *bbolt.DB, runResults *storage.ComplianceRunResults) {
+	assert.NoError(t, db.Update(func(tx *bbolt.Tx) error {
+		resultsBucket := tx.Bucket(complianceResultsBucketName)
+		clusterID := runResults.GetRunMetadata().GetClusterId()
+		clusterBucket, err := resultsBucket.CreateBucketIfNotExists([]byte(clusterID))
+		if err != nil {
+			return errors.Wrapf(err, "creating bucket for cluster %q", clusterID)
+		}
+
+		standardID := runResults.GetRunMetadata().GetStandardId()
+		_, err = clusterBucket.CreateBucketIfNotExists([]byte(standardID))
+		if err != nil {
+			return errors.Wrapf(err, "creating bucket for standard %q", clusterID)
+		}
+
+		return nil
+	}))
+}
+
+func validateMigration(t *testing.T, db *bbolt.DB, runResults *storage.ComplianceRunResults, deleted bool) {
+	require.NoError(t, db.View(func(tx *bbolt.Tx) error {
+		resultsBucket := tx.Bucket(complianceResultsBucketName)
+		clusterBucket := resultsBucket.Bucket([]byte(runResults.GetRunMetadata().GetClusterId()))
+		standardBucket := clusterBucket.Bucket([]byte(runResults.GetRunMetadata().GetStandardId()))
+
+		if deleted {
+			assert.Nil(t, standardBucket)
+		} else {
+			assert.NotNil(t, standardBucket)
+		}
+
+		return nil
+	}))
+}
