1d9ef3e661bee525616517eb3b028b6143644c27
Connor Gorman <connorgorman@users.noreply.github.com>
2019-12-16 16:17:55 -0800

    ROX-3784: Fix delete-sensor.sh to delete all resources with sensor auto-upgrade label (stackrox/rox#4236)

--- image/templates/kubernetes/kubectl/delete-sensor.sh
+++ image/templates/kubernetes/kubectl/delete-sensor.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-kubectl -n stackrox delete secret -l auto-upgrade.stackrox.io/component=sensor
-
-{{if .AdmissionController}}
-kubectl -n stackrox delete validatingwebhookconfiguration stackrox
-{{end}}
+kubectl -n stackrox delete cm,secret,sa,svc,validatingwebhookconfigurations,ds,deploy,netpol,psp,clusterrole,clusterrolebinding,role,rolebinding -l auto-upgrade.stackrox.io/component=sensor --wait
 
 if ! kubectl get -n stackrox deploy/central; then
     kubectl delete -n stackrox secret stackrox
--- image/templates/openshift/kubectl/delete-sensor.sh
+++ image/templates/openshift/kubectl/delete-sensor.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-oc -n stackrox delete secret -l auto-upgrade.stackrox.io/component=sensor
+oc -n stackrox delete cm,secret,sa,svc,validatingwebhookconfigurations,ds,deploy,netpol,psp,clusterrole,clusterrolebinding,role,rolebinding -l auto-upgrade.stackrox.io/component=sensor --wait
 
 if ! oc get -n stackrox deploy/central > /dev/null; then
     oc delete -n stackrox secret stackrox
