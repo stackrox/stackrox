d654c8158157f2a999bf853bc6423e834e61c05f
Viswajith Venugopal <viswajithiii@gmail.com>
2020-03-05 11:25:17 -0800

    Clean up UI test per comments (stackrox/rox#4916)

--- ui/src/constants/tableColumns.test.js
+++ ui/src/constants/tableColumns.test.js
@@ -1,35 +1,38 @@
 import { getColumnsByEntity, getColumnsByStandard } from './tableColumns';
 import { resourceTypes, standardTypes } from './entityTypes';
 
-function expectColumnsToContainAndNotContain(columns, shouldContain, shouldNotContain) {
+function expectColumnsToContain(columns, shouldContain) {
     const accessors = columns.map(c => c.accessor);
     expect(accessors).toEqual(expect.arrayContaining(shouldContain));
-    if (shouldNotContain) {
-        expect(accessors).toEqual(expect.not.arrayContaining(shouldNotContain));
-    }
+}
+
+function expectColumnsNotToContain(columns, shouldNotContain) {
+    const accessors = columns.map(c => c.accessor);
+    expect(accessors).toEqual(expect.not.arrayContaining(shouldNotContain));
 }
 
 describe('Get columns', () => {
-    it('can get columns by entity without exclusion', async () => {
-        expectColumnsToContainAndNotContain(getColumnsByEntity(resourceTypes.CLUSTER), [
+    it('can get columns by entity without exclusion', () => {
+        expectColumnsToContain(getColumnsByEntity(resourceTypes.CLUSTER), [
             'id',
             standardTypes.NIST_800_190,
             standardTypes.NIST_SP_800_53_Rev_4
         ]);
     });
 
-    it('can get columns by entity with exclusion', async () => {
-        expectColumnsToContainAndNotContain(
-            getColumnsByEntity(resourceTypes.CLUSTER, [standardTypes.NIST_SP_800_53_Rev_4]),
-            ['id', standardTypes.NIST_800_190],
-            [standardTypes.NIST_SP_800_53_Rev_4]
-        );
+    it('can get columns by entity with exclusion', () => {
+        const columns = getColumnsByEntity(resourceTypes.CLUSTER, [
+            standardTypes.NIST_SP_800_53_Rev_4
+        ]);
+        expectColumnsToContain(columns, ['id', standardTypes.NIST_800_190]);
+        expectColumnsNotToContain(columns, [standardTypes.NIST_SP_800_53_Rev_4]);
     });
 
-    it('can get columns by standard', async () => {
-        expectColumnsToContainAndNotContain(
-            getColumnsByStandard(standardTypes.NIST_SP_800_53_Rev_4),
-            ['id', 'compliance', 'control']
-        );
+    it('can get columns by standard', () => {
+        expectColumnsToContain(getColumnsByStandard(standardTypes.NIST_SP_800_53_Rev_4), [
+            'id',
+            'compliance',
+            'control'
+        ]);
     });
 });
