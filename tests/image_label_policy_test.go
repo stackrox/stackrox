3ccfbbc456b64a6bff02bb1685dd1c9a2b30bad1
Joseph Roger Altmaier <josephaltmaier@users.noreply.github.com>
2020-03-05 15:31:17 -0800

    Add required image label test retry loop (stackrox/rox#4919)

--- tests/image_label_policy_test.go
+++ tests/image_label_policy_test.go
@@ -2,47 +2,67 @@ package tests
 
 import (
 	"context"
+	"errors"
 	"testing"
 	"time"
 
 	"github.com/gogo/protobuf/proto"
 	v1 "github.com/stackrox/rox/generated/api/v1"
 	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/retry"
 	"github.com/stackrox/rox/pkg/testutils"
 	"github.com/stretchr/testify/require"
 )
 
+const (
+	reqLabelPolicyID = "d3e480c1-c6de-4cd2-9006-9a3eb3ad36b6"
+)
+
 func TestRequiredImageLabelPolicy(t *testing.T) {
-	id := "d3e480c1-c6de-4cd2-9006-9a3eb3ad36b6"
 	conn := testutils.GRPCConnectionToCentral(t)
+
+	// Make sure image label policy is enabled
+	policyService := v1.NewPolicyServiceClient(conn)
+	originalState := getPolicy(t, policyService, reqLabelPolicyID)
+	enabledPolicy := proto.Clone(originalState).(*storage.Policy)
+	enabledPolicy.Disabled = false
+	setPolicy(t, policyService, enabledPolicy)
+	defer setPolicy(t, policyService, originalState)
+	time.Sleep(5 * time.Second) // Allow time to sync the policy with sensor
 
 	// Create a deployment which should violate the policy
 	setupNginxLatestTagDeployment(t)
 	defer teardownNginxLatestTagDeployment(t)
 
-	// Make sure image label policy is enabled
-	policyService := v1.NewPolicyServiceClient(conn)
-	originalState := getPolicy(t, policyService, id)
-	enabledPolicy := proto.Clone(originalState).(*storage.Policy)
-	enabledPolicy.Disabled = false
-	setPolicy(t, policyService, enabledPolicy)
-	defer setPolicy(t, policyService, originalState)
+	// Retry to get the new alert
+	err := retry.WithRetry(
+		func() error {
+			// Get all alerts
+			alertService := v1.NewAlertServiceClient(conn)
+			ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+			alertResp, err := alertService.ListAlerts(ctx, &v1.ListAlertsRequest{})
+			cancel()
+			require.NoError(t, err)
 
-	// Get all alerts
-	alertService := v1.NewAlertServiceClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-	alertResp, err := alertService.ListAlerts(ctx, &v1.ListAlertsRequest{})
-	cancel()
+			// Make sure there are violations for the test deployment
+			var imageLabelAlerts []*storage.ListAlert
+			for _, alert := range alertResp.GetAlerts() {
+				if alert.GetPolicy().GetId() == reqLabelPolicyID && alert.GetDeployment().GetName() == nginxDeploymentName {
+					imageLabelAlerts = append(imageLabelAlerts, alert)
+				}
+			}
+			if len(imageLabelAlerts) == 0 {
+				return errors.New("No image label alerts found")
+			}
+			return nil
+		},
+		// Upper bound should be 60 seconds, first try happens immediately so this will wait up to 65 seconds
+		retry.Tries(66),
+		retry.BetweenAttempts(func(_ int) {
+			time.Sleep(time.Second)
+		}),
+	)
 	require.NoError(t, err)
-
-	// Make sure there are violations for the test deployment
-	var imageLabelAlerts []*storage.ListAlert
-	for _, alert := range alertResp.GetAlerts() {
-		if alert.GetPolicy().GetId() == id && alert.GetDeployment().GetName() == nginxDeploymentName {
-			imageLabelAlerts = append(imageLabelAlerts, alert)
-		}
-	}
-	require.NotEmpty(t, imageLabelAlerts)
 
 	// TODO: Test an image which should not violate the policy
 }
@@ -58,7 +78,6 @@ func getPolicy(t *testing.T, client v1.PolicyServiceClient, policyID string) *st
 
 func setPolicy(t *testing.T, client v1.PolicyServiceClient, policy *storage.Policy) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-	// This API call does not return until new alerts have been generated, no need for additional waiting.
 	_, err := client.PutPolicy(ctx, policy)
 	cancel()
 	require.NoError(t, err)
