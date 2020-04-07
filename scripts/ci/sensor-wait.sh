c5ad9b03858067bb864d0400fbfb2f49d3cf2fa5
Viswajith Venugopal <viswajithiii@gmail.com>
2020-04-07 15:01:37 -0700

    Make `upgrade-test` ensure that the sensor for the migrated cluster can run (stackrox/rox#5270)
    * Patch svc/central in upgrade-test to ensure that sensors can connect.
    * Run the sensor from the cluster object that's in the original saved DB in the upgrade-test.
    * Fix waitForSensorK8s to wait for sensors to become ready.

--- scripts/ci/sensor-wait.sh
+++ scripts/ci/sensor-wait.sh
@@ -15,13 +15,20 @@ set -eu
 main() {
     echo "Waiting for sensor to start"
     start_time="$(date '+%s')"
-    until [ "$(kubectl get pod -n stackrox --selector 'app=sensor' | grep Running | wc -l)" -eq 1 ]; do
-    	if (( $(date '+%s') - start_time > 300 )); then
-    		echo >&2 "Timed out after 5m"
-    		exit 1
-    	fi
-        echo -n .
-        sleep 1
+    while true; do
+      sensor_json="$(kubectl -n stackrox get deploy/sensor -o json)"
+      if [[ "$(jq '.status.replicas' <<<"${sensor_json}")" == 1 && "$(jq '.status.readyReplicas' <<<"${sensor_json}")" == 1 ]]; then
+        break
+      fi
+      echo $sensor_json
+      if (( $(date '+%s') - start_time > 300 )); then
+        kubectl -n stackrox get pod -o wide
+        kubectl -n stackrox get deploy -o wide
+        echo >&2 "Timed out after 5m"
+        exit 1
+      fi
+      echo -n .
+      sleep 1
     done
     echo "Sensor is running"
 
@@ -32,12 +39,12 @@ main() {
     echo "Waiting for collectors to start"
     start_time="$(date '+%s')"
     until [ "$(kubectl -n stackrox get ds/collector | tail -n +2 | awk '{print $2}')" -eq "$(kubectl -n stackrox get ds/collector | tail -n +2 | awk '{print $4}')" ]; do
-    	if (( $(date '+%s') - start_time > 300 )); then
-    		echo >&2 "Timed out after 5m"
-    		exit 1
-    	fi
-        echo -n .
-        sleep 1
+      if (( $(date '+%s') - start_time > 300 )); then
+        echo >&2 "Timed out after 5m"
+        exit 1
+      fi
+      echo -n .
+      sleep 1
     done
     echo "Collectors are running"
 }
