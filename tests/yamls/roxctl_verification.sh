d7176675eb9d096b632c5c39e6d229e0f44af86e
Viswajith Venugopal <viswajithiii@gmail.com>
2020-04-10 14:39:00 -0700

    Pass CA in roxctl invocations in tests/yamls/roxctl_verification.sh to avoid long warnings being printed (stackrox/rox#5258)

--- tests/yamls/roxctl_verification.sh
+++ tests/yamls/roxctl_verification.sh
@@ -2,12 +2,15 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-ENDPOINT="$1"
+extra_args=()
+if [[ -n "$CA" ]]; then
+  extra_args+=(--ca "$CA")
+fi
 
 FAILED="false"
 for yaml in $(ls "$DIR"/*.yaml); do
-	NUM_ALERTS=$(roxctl -e "$API_ENDPOINT" -p "$ROX_PASSWORD" deployment check --file $yaml --json | \
-	    jq '.alerts[].policy.name | select(.=="Latest tag" or .=="No resource requests or limits specified")' | jq -s '. | length')
+	NUM_ALERTS="$(roxctl "${extra_args[@]}" -e "$API_ENDPOINT" -p "$ROX_PASSWORD" deployment check --file $yaml --json | \
+	    jq '.alerts[].policy.name | select(.=="Latest tag" or .=="No resource requests or limits specified")' | jq -s '. | length')"
 	if [[ $NUM_ALERTS != "2" ]]; then
 		>&2 echo "Did not find 2 alerts for $yaml"
 		FAILED="true"
