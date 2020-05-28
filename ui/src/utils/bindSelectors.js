65defb5aaaa064f2965f30611df45e75b2800d88
Ivan Kornienko <i@stackrox.com>
2020-05-28 07:46:50 -0700

    Enable IDE to resolve bound Redux selectors (stackrox/rox#5789)

--- ui/src/utils/bindSelectors.js
+++ ui/src/utils/bindSelectors.js
@@ -2,9 +2,10 @@
  * Given the map (object) of selectors that work on a particular slice of a global state, and a slicer that extracts this slice form a global state,
  * returns map of selectors with the same keys but with selectors that work on a global state.
  *
+ * @template {Object.<string, Function>} T
  * @param {Function} slicer
- * @param {Object.<string, Function>} selectors
- * @returns {Object.<string, Function>} map of selectors that can accept as a parameter the same state as slicer accepts
+ * @param {T} selectors
+ * @returns {T} map of selectors that can accept as a parameter the same state as slicer accepts
  */
 const bindSelectors = (slicer, selectors) =>
     Object.keys(selectors).reduce(
