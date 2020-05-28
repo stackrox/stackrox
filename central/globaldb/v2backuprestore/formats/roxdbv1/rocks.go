264f79fc797934b013e5373e5606742a94ff46b4
Robert Stark <42253135+derbergkert@users.noreply.github.com>
2020-05-28 13:29:30 -0700

    Throw an error on restore from a RocksDB backup if RocksDB is disabled. (stackrox/rox#5794)

--- central/globaldb/v2backuprestore/formats/roxdbv1/rocks.go
+++ central/globaldb/v2backuprestore/formats/roxdbv1/rocks.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/stackrox/rox/central/globaldb/v2backuprestore/common"
+	"github.com/stackrox/rox/pkg/env"
 	"github.com/stackrox/rox/pkg/rocksdb"
 	pkgTar "github.com/stackrox/rox/pkg/tar"
 	"github.com/stackrox/rox/pkg/utils"
@@ -19,6 +20,9 @@ const (
 )
 
 func restoreRocksDB(ctx common.RestoreFileContext, fileReader io.Reader, size int64) error {
+	if !env.RocksDB.BooleanSetting() {
+		return errors.New("cannot restore from a rocksdb backup, rocksdb is disabled")
+	}
 	absDirPath, err := ctx.Mkdir(rocksDBPath, 0700)
 	if err != nil {
 		return errors.Wrap(err, "could not create badger database directory")
