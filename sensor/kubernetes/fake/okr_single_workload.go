6335a948d388ac9dc03e39d3c22992d422b5ea10
Connor Gorman <connorgorman@users.noreply.github.com>
2020-06-18 16:11:12 -0700

    Add OKR single workload that launches the resources, but does not clean them up (stackrox/rox#5857)

---
+++ sensor/kubernetes/fake/okr_single_workload.go
@@ -0,0 +1,41 @@
+package fake
+
+import (
+	"time"
+
+	"github.com/stackrox/rox/pkg/kubernetes"
+)
+
+func init() {
+	workloadRegistry["okr-single-load"] = okrSingleLoad
+}
+
+var (
+	okrSingleLoad = &workload{
+		DeploymentWorkload: []deploymentWorkload{
+			{
+				DeploymentType: kubernetes.Deployment,
+				NumDeployments: 10000,
+				PodWorkload: podWorkload{
+					NumPods:           3,
+					NumContainers:     3,
+					LifecycleDuration: 24 * time.Hour,
+
+					ProcessWorkload: processWorkload{
+						ProcessInterval: 24 * time.Hour, // deployments * pods / rate = process / second
+						AlertRate:       0.001,          // 0.1% of all processes will trigger a runtime alert
+					},
+				},
+				UpdateInterval:    24 * time.Hour,
+				LifecycleDuration: 24 * time.Hour,
+			},
+		},
+		NodeWorkload: nodeWorkload{
+			NumNodes: 1000,
+		},
+		NetworkWorkload: networkWorkload{
+			FlowInterval: 24 * time.Hour,
+			BatchSize:    500,
+		},
+	}
+)
