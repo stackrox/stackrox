8d4debcfd92720509187f5bc7cf9bce5cdecd332
Connor Gorman <connorgorman@users.noreply.github.com>
2020-07-09 07:44:53 -0700

    Extend wait timeout on webhook server to allow OpenShift more time to pull (stackrox/rox#6068)

--- scripts/ci/create-webhookserver.sh
+++ scripts/ci/create-webhookserver.sh
@@ -13,6 +13,6 @@ trap printstatus ERR
 kubectl create -f webhookserver/server.yaml
 sleep 5
 POD=$(kubectl -n stackrox get pod -l app=webhookserver -o name)
-kubectl -n stackrox wait --for=condition=ready "$POD" --timeout=2m
+kubectl -n stackrox wait --for=condition=ready "$POD" --timeout=5m
 nohup kubectl -n stackrox port-forward "${POD}" 8080:8080 </dev/null > /dev/null 2>&1 &
 sleep 1
