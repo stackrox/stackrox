526c70d388e2038887e88e09c6eaa6f22a657236
Gavin Jefferies <gavin@stackrox.com>
2020-07-17 18:09:56 -0700

    CI: add deployment objects to the API grabber (stackrox/rox#6123)

--- scripts/grab-data-from-central.sh
+++ scripts/grab-data-from-central.sh
@@ -29,15 +29,19 @@ main() {
 
     mkdir -p ${dest}
 
-    curl -s --insecure -u ${ROX_USERNAME}:${ROX_PASSWORD} https://${api_endpoint}/v1/images | jq > ${dest}/images.json
-    curl -s --insecure -u ${ROX_USERNAME}:${ROX_PASSWORD} https://${api_endpoint}/v1/imageintegrations | jq > ${dest}/imageintegrations.json
+    curl -s --insecure -u ${ROX_USERNAME}:${ROX_PASSWORD} https://localhost:8000/v1/images | jq > ${dest}/images.json
+    curl -s --insecure -u ${ROX_USERNAME}:${ROX_PASSWORD} https://localhost:8000/v1/imageintegrations | jq > ${dest}/imageintegrations.json
+    curl -s --insecure -u ${ROX_USERNAME}:${ROX_PASSWORD} https://localhost:8000/v1/deployments | jq > ${dest}/deployments.json
 
-    image_list=$(cat ${dest}/images.json | jq '.images[].id')
+    for objects in "images" "deployments"; do
+        jq_tweezer=".${objects}[].id"
+        object_list=$(cat ${dest}/${objects}.json | jq "${jq_tweezer}")
 
-    mkdir -p ${dest}/images
-    for image_id in ${image_list}; do
-        image_id=$(echo ${image_id} | sed s/\"//g)
-        curl -s --insecure -u ${ROX_USERNAME}:${ROX_PASSWORD} https://${api_endpoint}/v1/images/${image_id} | jq > ${dest}/images/${image_id}.json
+        mkdir -p ${dest}/${objects}
+        for id in ${object_list}; do
+            id=$(echo ${id} | sed s/\"//g)
+            curl -s --insecure -u ${ROX_USERNAME}:${ROX_PASSWORD} https://localhost:8000/v1/${objects}/${id} | jq > ${dest}/${objects}/${id}.json
+        done
     done
 }
 
