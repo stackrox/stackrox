c1173988706c289d545288eedebb48e9517dd6f5
Thomas Voß <thomas@stackrox.com>
2020-07-29 22:12:37 +0200

    [scripts] Pipe output through log encoder (stackrox/rox#6199)

--- image/static-bin/start-central.sh
+++ image/static-bin/start-central.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 rm -rf /tmp/scorch.bleve || /bin/true # wipeout the temporary index on start
-restore-central-db
-rollback-rocksdb
+restore-central-db 2>&1 | /stackrox/roxctl log-convert --module=restore-central-db
+rollback-rocksdb 2>&1 | /stackrox/roxctl log-convert --module=rollback-rocksdb
 /stackrox/bin/migrator
-rocksdb-migration
+rocksdb-migration 2>&1 | /stackrox/roxctl log-convert --module=rocksdb-migration
 
 RESTART_EXE="$(readlink -f "$0")" exec /stackrox/central "$@"
