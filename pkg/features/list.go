140196a704c23b3a483b0873ffc698fcde6f4d5e
Malte Isberner <2822367+misberner@users.noreply.github.com>
2020-08-19 18:46:59 +0200

    Flip network graph ports flag (stackrox/rox#6326)

--- pkg/features/list.go
+++ pkg/features/list.go
@@ -32,5 +32,5 @@ var (
 	AwsSecurityHubIntegration = registerFeature("Show AWS Security Hub Integration in UI", "ROX_AWS_SECURITY_HUB_INTEGRATION", false)
 
 	// NetworkGraphPorts enables port-related features in the network graph.
-	NetworkGraphPorts = registerFeature("Enable port-related features in network graph", "ROX_NETWORK_GRAPH_PORTS", false)
+	NetworkGraphPorts = registerFeature("Enable port-related features in network graph", "ROX_NETWORK_GRAPH_PORTS", true)
 )
