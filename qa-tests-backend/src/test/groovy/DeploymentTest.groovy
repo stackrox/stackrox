e9c5152d982d70b1d20f7198740015c0fea560f5
Connor Gorman <connorgorman@users.noreply.github.com>
2020-08-25 08:31:11 -0700

    Fix invalid deployment <-> image linking test and add some more (stackrox/rox#6355)

--- qa-tests-backend/src/test/groovy/DeploymentTest.groovy
+++ qa-tests-backend/src/test/groovy/DeploymentTest.groovy
@@ -1,7 +1,10 @@
+import groups.BAT
 import io.stackrox.proto.api.v1.SearchServiceOuterClass.RawQuery
+import org.junit.experimental.categories.Category
 import services.DeploymentService
 import services.ImageService
 import objects.Deployment
+import spock.lang.Unroll
 import util.Timer
 
 class DeploymentTest extends BaseSpecification {
@@ -20,6 +23,8 @@ class DeploymentTest extends BaseSpecification {
         orchestrator.deleteDeployment(DEPLOYMENT)
     }
 
+    @Unroll
+    @Category([BAT])
     def "Verify deployment -> image links #query"() {
         when:
         Timer t = new Timer(3, 10)
@@ -32,16 +37,23 @@ class DeploymentTest extends BaseSpecification {
 
         then:
         def results = DeploymentService.listDeploymentsSearch(RawQuery.newBuilder().setQuery(query).build())
-        assert results.deploymentsList.size() >= 1
+        assert results.deploymentsList.find { x -> x.getName() == DEPLOYMENT_NAME } != null
 
         where:
         "Data inputs are: "
         query                                                                                                   | _
         "Image:docker.io/library/nginx@sha256:204a9a8e65061b10b92ad361dd6f406248404fe60efd5d6a8f2595f18bb37aad" | _
         "Image Sha:sha256:204a9a8e65061b10b92ad361dd6f406248404fe60efd5d6a8f2595f18bb37aad"                     | _
-        "Image Tag:latest"                                                                                      | _
+        "CVE:CVE-2018-18314+Fixable:true"                                                                       | _
+        "Deployment:${DEPLOYMENT_NAME}+Image:r/docker.*"                                                        | _
+        "Image:r/docker.*"                                                                                      | _
+        "Image:!stackrox.io"                                                                                    | _
+        "Deployment:${DEPLOYMENT_NAME}+Image:!stackrox.io"                                                      | _
+        "Image Remote:library/nginx+Image Registry:docker.io"                                                   | _
     }
 
+    @Unroll
+    @Category([BAT])
     def "Verify image -> deployment links #query"() {
         when:
         Timer t = new Timer(3, 10)
@@ -54,13 +66,16 @@ class DeploymentTest extends BaseSpecification {
 
         then:
         def images = ImageService.getImages(RawQuery.newBuilder().setQuery(query).build())
-        assert images.size() >= 1
+        assert images.find {
+            x -> x.getId() == "sha256:204a9a8e65061b10b92ad361dd6f406248404fe60efd5d6a8f2595f18bb37aad" } != null
 
         where:
         "Data inputs are: "
-        query                           | _
-        "Deployment:${DEPLOYMENT_NAME}" | _
-        "Label:app=test"                | _
+        query                                                                                                   | _
+        "Deployment:${DEPLOYMENT_NAME}"                                                                         | _
+        "Label:app=test"                                                                                        | _
+        "Image:docker.io/library/nginx@sha256:204a9a8e65061b10b92ad361dd6f406248404fe60efd5d6a8f2595f18bb37aad" | _
+        "Label:app=test+Image:docker.io/library/nginx"                                                          | _
     }
 
 }
