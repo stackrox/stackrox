100418785c5bf11546cb70f8d57191d61f50c1b3
Viswajith Venugopal <viswajithiii@gmail.com>
2020-09-21 10:24:26 -0700

    Extend CI signing key validity for dev builds (stackrox/rox#6569)

--- central/license/singleton/ci_not_valid_after_dev.go
+++ central/license/singleton/ci_not_valid_after_dev.go
@@ -7,5 +7,7 @@ import (
 )
 
 const (
-	ciSigningKeyLatestNotValidAfterOffset = 365 * 24 * time.Hour
+	// This time is set really high because we may need to run a release in CI for up to 2 years after it was first
+	// released, to just upgrades from that release.
+	ciSigningKeyLatestNotValidAfterOffset = 2 * 365 * 24 * time.Hour
 )
