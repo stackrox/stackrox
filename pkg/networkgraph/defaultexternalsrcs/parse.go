931ee2d9d1c970444ab34470d0cf20e1d5d4f363
Mandar Darwatkar <mandar@stackrox.com>
2020-11-13 15:49:02 -0800

    [ROX-5958] Be resilient to multi-region, multi-service networks (stackrox/rox#6880)
    * Be resilient to multi-region, multi-service network

--- pkg/networkgraph/defaultexternalsrcs/parse.go
+++ pkg/networkgraph/defaultexternalsrcs/parse.go
@@ -2,13 +2,19 @@ package defaultexternalsrcs
 
 import (
 	"encoding/json"
+	"strings"
 
 	"github.com/pkg/errors"
 	"github.com/stackrox/external-network-pusher/pkg/common"
 	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/pkg/networkgraph/externalsrcs"
+	"github.com/stackrox/rox/pkg/set"
 	"github.com/stackrox/rox/pkg/stringutils"
 	"github.com/stackrox/rox/pkg/utils"
+)
+
+const (
+	multiRegion = "multi-region"
 )
 
 // ParseProviderNetworkData parses the provider networks bytes (default network graph external source), into *storage.NetworkEntity.
@@ -18,72 +24,109 @@ func ParseProviderNetworkData(data []byte) ([]*storage.NetworkEntity, error) {
 		return nil, errors.Wrap(err, "unmarshaling provider networks")
 	}
 
-	networkEntities, err := ParseProviderNetworksToProto(networkData.ProviderNetworks...)
-	if err != nil {
-		return nil, errors.Wrap(err, "parsing provider networks into proto")
-	}
-	return networkEntities, nil
+	return newNetworkDataParser().parse(networkData), nil
 }
 
-// ParseProviderNetworksToProto parses ProviderNetworkRanges object(s) into *storage.NetworkEntity object(s).
-func ParseProviderNetworksToProto(providers ...*common.ProviderNetworkRanges) ([]*storage.NetworkEntity, error) {
-	var ret []*storage.NetworkEntity
+type networkEntity struct {
+	id           string
+	provider     string
+	globalRegion string
+	regions      set.StringSet
+	cidr         string
+}
 
-	for _, provider := range providers {
+func (e *networkEntity) ToProto() *storage.NetworkEntityInfo {
+	return &storage.NetworkEntityInfo{
+		Id:   e.id,
+		Type: storage.NetworkEntityInfo_EXTERNAL_SOURCE,
+		Desc: &storage.NetworkEntityInfo_ExternalSource_{
+			ExternalSource: &storage.NetworkEntityInfo_ExternalSource{
+				Name: getNameNetworkEntity(e),
+				Source: &storage.NetworkEntityInfo_ExternalSource_Cidr{
+					Cidr: e.cidr,
+				},
+				Default: true,
+			},
+		},
+	}
+}
+
+func getNameNetworkEntity(entity *networkEntity) string {
+	if entity == nil {
+		return ""
+	}
+
+	if entity.globalRegion == "" || entity.globalRegion == "unknown" {
+		return entity.provider
+	}
+
+	return entity.provider + "/" + entity.globalRegion
+}
+
+type networkDataParser struct {
+	entities map[string]*networkEntity
+}
+
+func newNetworkDataParser() *networkDataParser {
+	return &networkDataParser{
+		entities: make(map[string]*networkEntity),
+	}
+}
+
+func (p *networkDataParser) parse(sources *common.ExternalNetworkSources) []*storage.NetworkEntity {
+	for _, provider := range sources.ProviderNetworks {
 		for _, region := range provider.RegionNetworks {
 			for _, service := range region.ServiceNetworks {
 				for _, cidr := range service.IPv4Prefixes {
-					if entity := generateEntity(provider.ProviderName, region.RegionName, service.ServiceName, cidr); entity != nil {
-						ret = append(ret, entity)
-					}
+					p.generateEntity(provider.ProviderName, region.RegionName, service.ServiceName, cidr)
 				}
 
 				for _, cidr := range service.IPv6Prefixes {
-					if entity := generateEntity(provider.ProviderName, region.RegionName, service.ServiceName, cidr); entity != nil {
-						ret = append(ret, entity)
-					}
+					p.generateEntity(provider.ProviderName, region.RegionName, service.ServiceName, cidr)
 				}
 			}
 		}
 	}
-	return ret, nil
+
+	ret := make([]*storage.NetworkEntity, 0, len(p.entities))
+	for _, entity := range p.entities {
+		ret = append(ret, &storage.NetworkEntity{
+			Info: entity.ToProto(),
+		})
+	}
+
+	return ret
 }
 
-func generateEntity(provider, region, service, cidr string) *storage.NetworkEntity {
+func (p *networkDataParser) generateEntity(provider, region, service, cidr string) *networkEntity {
 	// Validation failures logging is skipped to avoid log spam. Further validation is done at datastore upsert.
 	if stringutils.AtLeastOneEmpty(provider, cidr) {
 		return nil
-	}
-
-	var name string
-	if region == "" || region == "unknown" {
-		name = provider
-	} else {
-		name = provider + "/" + region
 	}
 
 	// Error is unexpected.
 	id, err := externalsrcs.NewGlobalScopedScopedID(cidr)
 	utils.Should(errors.Wrapf(err, "generating id for network %s/%s/%s", provider, region, cidr))
 
-	return &storage.NetworkEntity{
-		Info: &storage.NetworkEntityInfo{
-			Id:   id.String(),
-			Type: storage.NetworkEntityInfo_EXTERNAL_SOURCE,
-			Desc: &storage.NetworkEntityInfo_ExternalSource_{
-				ExternalSource: &storage.NetworkEntityInfo_ExternalSource{
-					Name: name,
-					Source: &storage.NetworkEntityInfo_ExternalSource_Cidr{
-						Cidr: cidr,
-					},
-					Default: true,
-					Metadata: &storage.NetworkEntityInfo_ExternalSource_Metadata{
-						Provider: provider,
-						Region:   region,
-						Service:  service,
-					},
-				},
-			},
-		},
+	entity := p.entities[id.String()]
+	if entity == nil {
+		entity = &networkEntity{
+			id:           id.String(),
+			provider:     provider,
+			globalRegion: region,
+			regions:      set.NewStringSet(),
+			cidr:         cidr,
+		}
 	}
+
+	if region != "" {
+		entity.regions.Add(strings.ToLower(region))
+	}
+
+	if len(entity.regions) > 1 {
+		entity.globalRegion = multiRegion
+	}
+
+	p.entities[id.String()] = entity
+	return entity
 }
