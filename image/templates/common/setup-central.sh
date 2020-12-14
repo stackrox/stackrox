e73b0ac5f2f7d5b954db2ce26126a31b7eec4848
Moritz Clasmeier (mtesseract) <mtesseract@users.noreply.github.com>
2020-12-14 13:22:00 +0100

    ROX-6104: Make namespace parameterizable in setup scripts (stackrox/rox#7073)
    Make namespace parameterizable in setup scripts using the environment variable ROX_NAMESPACE.

--- image/templates/common/setup-central.sh
+++ image/templates/common/setup-central.sh
@@ -18,21 +18,22 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
 KUBE_COMMAND=${KUBE_COMMAND:-{{.K8sConfig.Command}}}
+NAMESPACE="${ROX_NAMESPACE:-stackrox}"
 
-${KUBE_COMMAND} get namespace stackrox &>/dev/null || ${KUBE_COMMAND} create namespace stackrox
-${KUBE_COMMAND} -n stackrox annotate namespace/stackrox --overwrite openshift.io/node-selector=""
+${KUBE_COMMAND} get namespace "$NAMESPACE" &>/dev/null || ${KUBE_COMMAND} create namespace "$NAMESPACE"
+${KUBE_COMMAND} annotate "namespace/${NAMESPACE}" --overwrite openshift.io/node-selector=""
 
-if ! ${KUBE_COMMAND} get secret/stackrox -n stackrox > /dev/null; then
+if ! ${KUBE_COMMAND} get secret/stackrox -n "$NAMESPACE" > /dev/null; then
   registry_auth="$("${DIR}/docker-auth.sh" -m k8s "{{.K8sConfig.Registry}}")"
   [[ -n "$registry_auth" ]] || { echo >&2 "Unable to get registry auth info." ; exit 1 ; }
-  ${KUBE_COMMAND} create --namespace "stackrox" -f - <<EOF
+  ${KUBE_COMMAND} create --namespace "$NAMESPACE" -f - <<EOF
 apiVersion: v1
 data:
   .dockerconfigjson: ${registry_auth}
 kind: Secret
 metadata:
   name: stackrox
-  namespace: stackrox
+  namespace: "$NAMESPACE"
   labels:
     app.kubernetes.io/name: stackrox
 type: kubernetes.io/dockerconfigjson
