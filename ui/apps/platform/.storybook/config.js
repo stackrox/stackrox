6e8d60dc3fe0765a708fc748ffa86a0fedf6fd61
Mark Pedrotti <pedrottimark@gmail.com>
2020-12-15 08:50:33 -0500

    Remove Storybook addon-info from config.js in apps/platform (stackrox/rox#7085)
    * Remove Storybook addon-info from devDependencies in apps/platform
    
    * Fix errors

--- ui/apps/platform/.storybook/config.js
+++ ui/apps/platform/.storybook/config.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { addDecorator, addParameters, configure } from '@storybook/react';
-import { withInfo } from '@storybook/addon-info';
+// import { withInfo } from '@storybook/addon-info';
 
 import { withThemes } from 'storybook-addon-themes';
 
+/*
 addDecorator(
     withInfo({
         inline: true, // show headings, sourcecode, and props along with the examples,
@@ -16,6 +17,7 @@ addDecorator(
         })
     })
 );
+*/
 
 // whitespace around the story, which would otherwise start in the top-left
 addDecorator(storyFn => <div style={{ padding: "10px 20px 20px" }}>{storyFn()}</div>);
