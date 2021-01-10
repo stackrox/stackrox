83578f0dbcfb6ddbc21a51df09d28e5f22d697d5
Saif Chaudhry <sachaudh@users.noreply.github.com>
2021-01-10 09:09:26 -0800

    ROX-6276: Show correct entity name in baseline settings (stackrox/rox#7246)
    * ROX-6276: Show correct entity name in baseline settings
    
    * added test
    
    (cherry picked from commit 93a45c73e70ad2f7a81dc077118768f22aa0e3b6)

--- ui/apps/platform/src/Containers/Network/Wizard/NetworkDeploymentOverlay/BaselineSettings/useFetchNetworkBaselines.ts
+++ ui/apps/platform/src/Containers/Network/Wizard/NetworkDeploymentOverlay/BaselineSettings/useFetchNetworkBaselines.ts
@@ -3,9 +3,20 @@ import { useEffect, useState } from 'react';
 import { fetchNetworkBaselines } from 'services/NetworkService';
 import { filterLabels } from 'constants/networkFilterModes';
 import { FlattenedNetworkBaseline } from 'Containers/Network/networkTypes';
-import { networkFlowStatus } from 'constants/networkGraph';
+import { networkFlowStatus, nodeTypes } from 'constants/networkGraph';
 
 type Result = { isLoading: boolean; data: FlattenedNetworkBaseline[]; error: string | null };
+
+export function getPeerEntityName(peer): string {
+    switch (peer.entity.info.type) {
+        case nodeTypes.EXTERNAL_ENTITIES:
+            return 'External Entities';
+        case nodeTypes.CIDR_BLOCK:
+            return peer.entity.info.externalSource.name;
+        default:
+            return peer.entity.info.deployment.name;
+    }
+}
 
 /*
  * This hook does an API call to the baseline status API to get the baseline status
@@ -19,14 +30,15 @@ function useFetchNetworkBaselines({ deploymentId, filterState }): Result {
 
         networkBaselinesPromise
             .then((response) => {
-                const { deploymentName, namespace, peers } = response;
-                const data = peers.reduce((acc, curr) => {
-                    curr.properties.forEach((property) => {
+                const { namespace, peers } = response;
+                const data = peers.reduce((acc, currPeer) => {
+                    currPeer.properties.forEach((property) => {
+                        const name = getPeerEntityName(currPeer);
                         const peer = {
                             entity: {
-                                id: curr.entity.info.id,
-                                type: curr.entity.info.type,
-                                name: deploymentName,
+                                id: currPeer.entity.info.id,
+                                type: currPeer.entity.info.type,
+                                name,
                                 namespace,
                             },
                             ingress: property.ingress,
