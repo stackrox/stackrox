9d0c449605ab0fea81dc2f836a72954358090a7d
Matthias <Maddosaurus@users.noreply.github.com>
2021-01-18 14:32:11 +0100

    ROX-6295: add roxctl image scan retry (stackrox/rox#7277)

--- roxctl/image/scan/scan.go
+++ roxctl/image/scan/scan.go
@@ -3,12 +3,14 @@ package scan
 import (
 	"context"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/golang/protobuf/jsonpb"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	v1 "github.com/stackrox/rox/generated/api/v1"
+	"github.com/stackrox/rox/pkg/retry"
 	"github.com/stackrox/rox/roxctl/common"
 	"github.com/stackrox/rox/roxctl/common/flags"
 	"github.com/stackrox/rox/roxctl/common/util"
@@ -21,6 +23,8 @@ func Command() *cobra.Command {
 		force          bool
 		includeSnoozed bool
 		format         string
+		retryDelay     int
+		retryCount     int
 	)
 	c := &cobra.Command{
 		Use: "scan",
@@ -32,15 +36,32 @@ func Command() *cobra.Command {
 				return errors.New("Unexpected format: " + format)
 
 			}
-			return scanImage(image, force, includeSnoozed, format, flags.Timeout(c))
+			return scanImageWithRetry(image, force, includeSnoozed, format, flags.Timeout(c), retryDelay, retryCount)
 		}),
 	}
 
 	c.Flags().StringVarP(&image, "image", "i", "", "image name and reference. (e.g. nginx:latest or nginx@sha256:...)")
 	c.Flags().BoolVarP(&force, "force", "f", false, "the --force flag ignores Central's cache for the scan and forces a fresh re-pull from Scanner")
 	c.Flags().BoolVarP(&includeSnoozed, "include-snoozed", "a", true, "the --include-snoozed flag returns both snoozed and unsnoozed CVEs if set to false")
-	c.Flags().StringVarP(&format, "format", "", "json", "format of the output. Choose output format from json, csv, and pretty. Default to be json.")
+	c.Flags().StringVarP(&format, "format", "", "json", "format of the output. Choose output format from json, csv, and pretty.")
+	c.Flags().IntVarP(&retryDelay, "retry-delay", "d", 3, "set time to wait between retries in seconds")
+	c.Flags().IntVarP(&retryCount, "retries", "r", 0, "Number of retries before exiting as error")
 	return c
+}
+
+func scanImageWithRetry(image string, force, includeSnoozed bool, format string, timeout time.Duration, retryDelay int, retryCount int) error {
+	err := retry.WithRetry(func() error {
+		return scanImage(image, force, includeSnoozed, format, timeout)
+	},
+		retry.Tries(retryCount+1),
+		retry.OnFailedAttempts(func(err error) {
+			fmt.Fprintf(os.Stderr, "Scanning image failed: %v. Retrying after %v seconds\n", err, retryDelay)
+			time.Sleep(time.Duration(retryDelay) * time.Second)
+		}))
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func scanImage(image string, force, includeSnoozed bool, format string, timeout time.Duration) error {
