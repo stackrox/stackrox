392871d3afc2b4b55ccd4346caa716891a70561f
Ivan Kornienko <i@stackrox.com>
2021-01-29 13:48:33 -0800

    Add types to fetchingReduxRoutines and minor refactoring (stackrox/rox#7437)

--- ui/apps/platform/src/reducers/loading.js
+++ ui/apps/platform/src/reducers/loading.js
@@ -1,22 +1,22 @@
 import { combineReducers } from 'redux';
-import { filterRequestActionTypes, getFetchingActionName } from 'utils/fetchingReduxRoutines';
+import { getFetchingActionInfo, FetchingActionState } from 'utils/fetchingReduxRoutines';
 
 const loading = (state = {}, action) => {
     const { type } = action;
-    const matches = filterRequestActionTypes(type);
+    const info = getFetchingActionInfo(type);
 
     // not a *_REQUEST / *_SUCCESS /  *_FAILURE actions, so we ignore them
-    if (!matches) {
+    if (!info) {
         return state;
     }
 
-    const { requestName, requestState } = matches;
+    const { prefix, fetchingState } = info;
     return {
         ...state,
         // Store whether a request is happening at the moment or not
         // e.g. will be true when receiving GET_TODOS_REQUEST
         //      and false when receiving GET_TODOS_SUCCESS / GET_TODOS_FAILURE
-        [requestName]: requestState,
+        [prefix]: fetchingState === FetchingActionState.REQUEST,
     };
 };
 
@@ -26,9 +26,12 @@ const reducer = combineReducers({
 
 export default reducer;
 
-const getLoadingStatus = (state, type) => {
-    const action = getFetchingActionName(type);
-    return state.loading[action] || false;
+const getLoadingStatus = (state, fetchingActionTypes) => {
+    const info = getFetchingActionInfo(fetchingActionTypes.REQUEST);
+    if (!info) {
+        return false;
+    }
+    return state.loading[info.prefix] || false;
 };
 
 export const selectors = {
