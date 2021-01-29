fe7615ec695eebbe28cb5d965efe74aad1c24207
Saif Chaudhry <sachaudh@users.noreply.github.com>
2021-01-29 10:42:31 -0800

    Hide AlertBaselineViolations based on feature flag (stackrox/rox#7493)

--- ui/apps/platform/src/Containers/Network/Wizard/NetworkDeploymentOverlay/BaselineSettings/BaselineSettings.tsx
+++ ui/apps/platform/src/Containers/Network/Wizard/NetworkDeploymentOverlay/BaselineSettings/BaselineSettings.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from 'react';
 
 import { filterModes } from 'constants/networkFilterModes';
+import useFeatureFlagEnabled from 'hooks/useFeatureFlagEnabled';
 import useFetchNetworkBaselines from './useFetchNetworkBaselines';
 
 import NetworkBaselines from '../NetworkBaselines';
@@ -19,6 +20,9 @@ function BaselineSettings({
     filterState,
     onNavigateToEntity,
 }: BaselineSettingsProps): ReactElement {
+    const isBaselineViolationEnabled = useFeatureFlagEnabled(
+        'ROX_NETWORK_DETECTION_BASELINE_VIOLATION'
+    );
     const {
         data: { networkBaselines, isAlertingEnabled },
         isLoading,
@@ -28,15 +32,14 @@ function BaselineSettings({
         filterState,
     });
 
+    const headerComponents = isBaselineViolationEnabled && (
+        <AlertBaselineViolations deploymentId={deploymentId} isEnabled={isAlertingEnabled} />
+    );
+
     return (
         <NetworkBaselines
             header="Baseline Settings"
-            headerComponents={
-                <AlertBaselineViolations
-                    deploymentId={deploymentId}
-                    isEnabled={isAlertingEnabled}
-                />
-            }
+            headerComponents={headerComponents}
             isLoading={isLoading}
             networkBaselines={networkBaselines}
             deploymentId={deploymentId}
