74d60ad7e11a3ddb98281ea68a0efb783f52be45
Khushboo Sancheti <42253461+clickboo@users.noreply.github.com>
2021-02-11 22:33:57 -0800

    Delete secured cluster sccs on openshift in delete sensor script (stackrox/rox#7602)

--- image/templates/sensor/openshift/delete-sensor.sh
+++ image/templates/sensor/openshift/delete-sensor.sh
@@ -3,6 +3,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
 oc -n stackrox delete cm,secret,sa,svc,validatingwebhookconfigurations,ds,deploy,netpol,psp,clusterrole,clusterrolebinding,role,rolebinding -l auto-upgrade.stackrox.io/component=sensor --wait
 
+oc delete scc -l auto-upgrade.stackrox.io/component=sensor --wait
+
 if ! oc get -n stackrox deploy/central > /dev/null; then
     oc delete -n stackrox secret stackrox
 fi
