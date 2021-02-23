d3ae83fcdf502c757e009ffd4c18f02f5c9b62ab
Van Wilson <wilson@stackrox.com>
2021-02-23 17:34:27 -0500

    Add support of colon (:) in RH CVE database URLs (stackrox/rox#7681)

--- ui/apps/platform/src/utils/urlUtils.js
+++ ui/apps/platform/src/utils/urlUtils.js
@@ -4,7 +4,7 @@ const urlPattern = new RegExp(
     '^(https?:\\/\\/)?' + // protocol
         '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
         '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+        '(\\:\\d+)?(\\/[-a-z\\d%:_.~+]*)*' + // port and path
         '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
         '(\\#[-a-z\\d_]*)?$',
     'i'
