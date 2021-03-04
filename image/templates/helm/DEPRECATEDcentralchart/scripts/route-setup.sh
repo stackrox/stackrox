34ae21bdf6a5b788b843619a16a76b8dbe6aaac9
Simon Bäumer <baeumer@pm.me>
2021-03-04 11:40:37 +0100

    Remove deprecated helm charts (stackrox/rox#7735)

--- image/templates/helm/DEPRECATEDcentralchart/scripts/route-setup.sh
+++
@@ -1,8 +0,0 @@
-{{if eq .ClusterType.String "OPENSHIFT_CLUSTER"}}
-#!/usr/bin/env bash
-# This route will be accessible through central-stackrox.your-cluster-subdomain.
-oc create route passthrough central -n stackrox --service central
-# This route allows other clusters to connect via mutual TLS with the
-# Server Name Indication "central.stackrox".
-oc create route passthrough central-mtls -n stackrox --hostname=central.stackrox --service central
-{{- end}}
