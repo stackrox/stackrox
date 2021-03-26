c0c4619c018ceaf99c641ddaf18dd1d5cfae78be
Gavin Jefferies <gavin@stackrox.com>
2021-03-26 15:52:35 -0700

    RS-135: OpenShift 4.x automation (stackrox/rox#7366)

--- scripts/ci/openshift-gcr-secrets.sh
+++ scripts/ci/openshift-gcr-secrets.sh
@@ -3,4 +3,4 @@
 oc create namespace qa
 oc project qa
 oc create secret docker-registry --docker-username=_json_key --docker-password="$GOOGLE_CREDENTIALS_GCR_SCANNER" --docker-server https://us.gcr.io --docker-email stackrox@stackrox.com gcr
-oc secrets add serviceaccount/default secrets/gcr --for=pull
+oc secrets link serviceaccount/default secrets/gcr --for=pull
