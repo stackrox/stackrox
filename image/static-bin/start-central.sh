a31aa81e6721a0a0e7cf4e43e72cfe65bdd60709
Cong Du <c-du@users.noreply.github.com>
2021-04-01 15:10:14 -0700

    ROX-6712: Add persistent log to migrator and main loop of central (stackrox/rox#7963)
    Co-authored-by: Viswajith Venugopal <viswajithiii@gmail.com>

--- image/static-bin/start-central.sh
+++ image/static-bin/start-central.sh
@@ -2,8 +2,11 @@
 
 set -e
 
+. "$(dirname "$0")/db-functions"
+
 rm -rf /tmp/scorch.bleve || /bin/true # wipeout the temporary index on start
-move-to-current 2>&1 | /stackrox/roxctl log-convert --module=move-to-current
-/stackrox/bin/migrator
+move-to-current 2>&1 | PERSISTENT_LOG=true /stackrox/roxctl log-convert --module=move-to-current
+trunc_log 3000 | PERSISTENT_LOG=true /stackrox/roxctl log-convert --module=start-central
+PERSISTENT_LOG=true /stackrox/bin/migrator
 
 RESTART_EXE="$(readlink -f "$0")" exec /stackrox/central "$@"
