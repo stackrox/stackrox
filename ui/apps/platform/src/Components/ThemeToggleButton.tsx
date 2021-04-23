644ec8e6a6b408b2ae0bcf149b09a35700148df7
Mark Pedrotti <pedrottimark@gmail.com>
2021-04-23 12:33:28 -0400

    ROX-6945: Update ThemeToggleButton to PatternFly (stackrox/rox#8088)
    (cherry picked from commit 97ee9ecffc5cc5ca8760d2a1d622785d3b8d3507)

--- ui/apps/platform/src/Components/ThemeToggleButton.tsx
+++ ui/apps/platform/src/Components/ThemeToggleButton.tsx
@@ -1,22 +1,24 @@
-import React, { ReactElement } from 'react';
+import React, { CSSProperties, ReactElement } from 'react';
 import { Moon, Sun } from 'react-feather';
-import { Tooltip, TooltipOverlay } from '@stackrox/ui-components';
+import { Button, Tooltip } from '@patternfly/react-core';
 
 import { useTheme } from 'Containers/ThemeProvider';
+
+// On masthead, black text on white background like a dropdown menu.
+const styleTooltip = {
+    '--pf-c-tooltip__content--Color': 'var(--pf-global--Color--100)',
+    '--pf-c-tooltip__content--BackgroundColor': 'var(--pf-global--BackgroundColor--100)',
+} as CSSProperties;
 
 const ThemeToggleButton = (): ReactElement => {
     const themeState = useTheme();
     const tooltipText = themeState.isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode';
+
     return (
-        <Tooltip content={<TooltipOverlay>{tooltipText}</TooltipOverlay>}>
-            <button
-                aria-label="Invert theme"
-                onClick={themeState.toggle}
-                type="button"
-                className="flex items-center pt-3 pb-2 px-4 no-underline rounded-l-sm"
-            >
-                <span>{themeState.isDarkMode ? <Sun size="16" /> : <Moon size="16" />}</span>
-            </button>
+        <Tooltip content={<div>{tooltipText}</div>} position="bottom" style={styleTooltip}>
+            <Button aria-label="Invert theme" onClick={themeState.toggle} variant="plain">
+                {themeState.isDarkMode ? <Sun size="16" /> : <Moon size="16" />}
+            </Button>
         </Tooltip>
     );
 };
