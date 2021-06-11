1a38816f5a56820a3068bf1d2bb009f5aee484a7
Marcin Owsiany <marcin@stackrox.com>
2021-06-11 14:36:24 +0200

    ROX-7353 fix secret watch when running on cluster (stackrox/rox#8437)

--- operator/pkg/central/rbac.go
+++ operator/pkg/central/rbac.go
@@ -3,3 +3,4 @@ package central
 //+kubebuilder:rbac:groups=platform.stackrox.io,resources=centrals,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=platform.stackrox.io,resources=centrals/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=platform.stackrox.io,resources=centrals/finalizers,verbs=update
+//+kubebuilder:rbac:groups=apps,resources=secrets,verbs=get;list;watch
--- operator/pkg/securedcluster/rbac.go
+++ operator/pkg/securedcluster/rbac.go
@@ -3,3 +3,4 @@ package securedcluster
 //+kubebuilder:rbac:groups=platform.stackrox.io,resources=securedclusters,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=platform.stackrox.io,resources=securedclusters/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=platform.stackrox.io,resources=securedclusters/finalizers,verbs=update
+//+kubebuilder:rbac:groups=apps,resources=secrets,verbs=get;list;watch
