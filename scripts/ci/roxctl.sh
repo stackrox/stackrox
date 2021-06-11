31ea10ec7051ee3d81199605c1e033c42ad25250
Gavin Jefferies <gavin@stackrox.com>
2021-06-10 17:34:41 -0700

    CI: add missing file (stackrox/rox#8431)

---
+++ scripts/ci/roxctl.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+# A wrapper for roxctl command execution in CI.
+
+usage() {
+    echo "$0 <other roxctl args>"
+}
+
+main() {
+    if [ "$#" -eq 0 ]; then
+        usage
+        exit 1
+    fi
+    if [ -z "${ROX_PASSWORD}" ]; then
+        echo "ROX_PASSWORD must be set"
+        exit 1
+    fi
+
+    api_hostname=localhost
+    api_port=8000
+    lb_ip=$(kubectl -n stackrox get svc/central-loadbalancer -o jsonpath='{.status.loadBalancer.ingress[0].ip}' || true)
+    if [ -n "${lb_ip}" ]; then
+        api_hostname="${lb_ip}"
+        api_port=443
+    fi
+    api_endpoint="${api_hostname}:${api_port}"
+
+    roxctl -e "${api_endpoint}" -p "${ROX_PASSWORD}" $*
+}
+
+main "$@"
