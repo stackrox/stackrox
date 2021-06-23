41f91692d67218d5df49694bd3b5f89aeb68cef1
Malte Isberner <2822367+misberner@users.noreply.github.com>
2021-06-23 09:00:54 +0200

    ROX-7390: Limit Helm release history (stackrox/rox#8518)

--- operator/pkg/reconciler/reconciler_factory.go
+++ operator/pkg/reconciler/reconciler_factory.go
@@ -1,13 +1,28 @@
 package reconciler
 
 import (
+	"strconv"
+	"strings"
+
 	"github.com/joelanford/helm-operator/pkg/reconciler"
 	"github.com/joelanford/helm-operator/pkg/values"
 	"github.com/pkg/errors"
 	"github.com/stackrox/rox/image"
 	"github.com/stackrox/rox/pkg/charts"
+	"github.com/stackrox/rox/pkg/env"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+const (
+	// defaultMaxReleaseHistorySize is the default maximum size of the Helm release history. The value of 10 is chosen
+	// as a value that should be large enough in practice to allow meaningful manual investigations/recoveries, but
+	// small enough such that overall space consumption will not be a concern.
+	defaultMaxReleaseHistorySize = 10
+)
+
+var (
+	maxReleaseHistorySizeSetting = env.RegisterSetting("ROX_MAX_HELM_RELEASE_HISTORY")
 )
 
 // SetupReconcilerWithManager creates and registers a new helm reconciler to the given controller manager.
@@ -17,12 +32,21 @@ func SetupReconcilerWithManager(mgr ctrl.Manager, gvk schema.GroupVersionKind, c
 		return err
 	}
 
+	maxReleaseHistorySize := defaultMaxReleaseHistorySize
+	if maxHistoryStr := strings.TrimSpace(maxReleaseHistorySizeSetting.Setting()); maxHistoryStr != "" {
+		maxReleaseHistorySize, err = strconv.Atoi(maxHistoryStr)
+		if err != nil {
+			return errors.Wrapf(err, "invalid %s setting", maxReleaseHistorySizeSetting.EnvVar())
+		}
+	}
+
 	reconcilerOpts := []reconciler.Option{
 		reconciler.WithChart(*chart),
 		reconciler.WithGroupVersionKind(gvk),
 		reconciler.WithValueTranslator(translator),
 		//TODO(ROX-7362): re-evaluate enabling depended watches
 		reconciler.SkipDependentWatches(true),
+		reconciler.WithMaxReleaseHistory(maxReleaseHistorySize),
 	}
 	reconcilerOpts = append(reconcilerOpts, extraOpts...)
 
