70c60cfb47a92fec251cd3ce5a50fd0a7ed66645
Viswajith Venugopal <viswajithiii@gmail.com>
2021-06-30 15:17:41 -0700

    Remove deprecated UpgradesTests (stackrox/rox#8626)

--- qa-tests-backend/src/test/groovy/UpgradesTest.groovy
+++ qa-tests-backend/src/test/groovy/UpgradesTest.groovy
@@ -1,21 +1,13 @@
-import com.google.protobuf.Timestamp
-import com.google.protobuf.UnknownFieldSet
 import com.google.protobuf.util.JsonFormat
 import groovy.io.FileType
 import groups.Upgrade
 import io.stackrox.proto.api.v1.PolicyServiceOuterClass
 import io.stackrox.proto.api.v1.SummaryServiceOuterClass
-import io.stackrox.proto.storage.ClusterOuterClass
 import io.stackrox.proto.storage.PolicyOuterClass
-import io.stackrox.proto.storage.ProcessIndicatorOuterClass
 import io.stackrox.proto.storage.ScopeOuterClass
-import org.junit.Assume
 import org.junit.experimental.categories.Category
-import services.ClusterService
-import services.ConfigService
 import services.GraphQLService
 import services.PolicyService
-import services.ProcessService
 import services.SummaryService
 import spock.lang.Unroll
 import util.Env
@@ -38,104 +30,6 @@ class UpgradesTest extends BaseSpecification {
                 }
             }
         }"""
-
-    @Category(Upgrade)
-    def "Verify cluster exists and that field values are retained"() {
-        given:
-        "Only run on specific upgrade from 2.4.16"
-        Assume.assumeTrue(CLUSTERID=="260e11a3-cbea-464c-95f0-588fa7695b49")
-
-        expect:
-        def clusters = ClusterService.getClusters()
-        clusters.size() == 1
-        def expectedCluster = ClusterOuterClass.Cluster.newBuilder()
-                .setId(CLUSTERID)
-                .setName("remote")
-                .setType(ClusterOuterClass.ClusterType.KUBERNETES_CLUSTER)
-                .setPriority(1)
-                .setMainImage("stackrox/main:2.4.16.4")
-                .setCentralApiEndpoint("central.stackrox:443")
-                .setCollectionMethod(ClusterOuterClass.CollectionMethod.KERNEL_MODULE)
-                .setRuntimeSupport(true)
-                .setTolerationsConfig(ClusterOuterClass.TolerationsConfig.newBuilder()
-                        .setDisabled(true)
-                        .build())
-                .setHealthStatus(ClusterOuterClass.ClusterHealthStatus.newBuilder()
-                        .setSensorHealthStatus(ClusterOuterClass.ClusterHealthStatus.HealthStatusLabel.UNHEALTHY)
-                        .setCollectorHealthStatus(ClusterOuterClass.ClusterHealthStatus.HealthStatusLabel.UNAVAILABLE)
-                        .setOverallHealthStatus(ClusterOuterClass.ClusterHealthStatus.HealthStatusLabel.UNHEALTHY)
-                        .setLastContact(Timestamp.newBuilder().setSeconds(1551412107).setNanos(857477786).build())
-                        .build()
-                )
-                .setStatus(ClusterOuterClass.ClusterStatus.newBuilder()
-                        .setProviderMetadata(ClusterOuterClass.ProviderMetadata.newBuilder()
-                                .setGoogle(ClusterOuterClass.GoogleProviderMetadata.newBuilder()
-                                        .setProject("ultra-current-825")
-                                        .setClusterName("setup-devde6c6")
-                                        .build())
-                                .setRegion("us-west1")
-                                .setZone("us-west1-c")
-                                .build())
-                        .setOrchestratorMetadata(ClusterOuterClass.OrchestratorMetadata.newBuilder()
-                                .setVersion("v1.11.7-gke.4")
-                                .setBuildDate(Timestamp.newBuilder().setSeconds(1549394549).build())
-                                .build())
-                        .build())
-                .setDynamicConfig(ClusterOuterClass.DynamicClusterConfig.newBuilder()
-                        .setAdmissionControllerConfig(ClusterOuterClass.AdmissionControllerConfig.newBuilder()
-                                .setTimeoutSeconds(3)))
-                .build()
-
-        def cluster = ClusterOuterClass.Cluster.newBuilder(clusters.get(0))
-                .setUnknownFields(UnknownFieldSet.defaultInstance)
-                .build()
-        cluster == expectedCluster
-    }
-
-    @Category(Upgrade)
-    def "Verify process indicators have cluster IDs and namespaces added"() {
-        given:
-        "Only run on specific upgrade from 2.4.16"
-        Assume.assumeTrue(CLUSTERID=="260e11a3-cbea-464c-95f0-588fa7695b49")
-
-        expect:
-        "Migrated ProcessIndicators to have a cluster ID and a namespace"
-        def processIndicators = ProcessService.getProcessIndicatorsByDeployment("33b3eb66-3bd4-11e9-b563-42010a8a0101")
-        processIndicators.size() > 0
-        for (ProcessIndicatorOuterClass.ProcessIndicator indicator : processIndicators) {
-            assert(indicator.getClusterId() == CLUSTERID)
-            assert(indicator.getNamespace() != "")
-        }
-    }
-
-    @Category(Upgrade)
-    def "Verify cluster has listen on exec/pf webhook turned on"() {
-        given:
-        Assume.assumeTrue(CLUSTERID=="260e11a3-cbea-464c-95f0-588fa7695b49")
-
-        expect:
-        "Migrated clusters to have admissionControllerEvents set to true"
-        def cluster = ClusterService.getCluster()
-        cluster != null
-        assert(cluster.ClusterOuterClass.getAdmissionControllerEvents() == true)
-    }
-
-    @Category(Upgrade)
-    def "Verify private config contains the correct retention duration for alerts and images"() {
-        given:
-        "Only run on specific upgrade from 2.4.16"
-        Assume.assumeTrue(CLUSTERID=="260e11a3-cbea-464c-95f0-588fa7695b49")
-
-        expect:
-        "Alert retention duration is nil, image retention duration is 7 days"
-        def config = ConfigService.getConfig()
-        config != null
-        config.getPrivateConfig().getAlertConfig() != null
-        config.getPrivateConfig().getAlertConfig().getAllRuntimeRetentionDurationDays() == 0
-        config.getPrivateConfig().getAlertConfig().getResolvedDeployRetentionDurationDays() == 0
-        config.getPrivateConfig().getAlertConfig().getDeletedRuntimeRetentionDurationDays() == 0
-        config.getPrivateConfig().getImageRetentionDurationDays() == 7
-    }
 
     @Category(Upgrade)
     def "Verify that summary API returns non-zero values on upgrade"() {
