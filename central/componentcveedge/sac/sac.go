ac57f608417530f042fd177d271ea0b0aa67bf8d
Cong Du <c-du@users.noreply.github.com>
2021-07-30 10:54:39 -0700

    ROX-7655: Perform node SAC check for ComponentCVEEdge (stackrox/rox#8939)

--- central/componentcveedge/sac/sac.go
+++ central/componentcveedge/sac/sac.go
@@ -3,17 +3,22 @@ package sac
 import (
 	"github.com/stackrox/rox/central/dackbox"
 	"github.com/stackrox/rox/central/role/resources"
+	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/pkg/sac"
 	"github.com/stackrox/rox/pkg/search/filtered"
 )
 
 var (
 	imageSAC = sac.ForResource(resources.Image)
+	nodeSAC  = sac.ForResource(resources.Node)
 
-	componentCVEEdgeSACFilter = filtered.MustCreateNewSACFilter(
-		filtered.WithResourceHelper(imageSAC),
-		filtered.WithScopeTransform(dackbox.ComponentVulnEdgeSACTransform),
-		filtered.WithReadAccess())
+	componentCVEEdgeSACFilter = filtered.NewEdgeSourceFilter(
+		dackbox.MustCreateNewSharedObjectSACFilter(
+			dackbox.WithNode(nodeSAC, dackbox.ComponentToNodeBucketPath),
+			dackbox.WithImage(imageSAC, dackbox.ComponentToImageBucketPath),
+			dackbox.WithSharedObjectAccess(storage.Access_READ_ACCESS),
+		),
+	)
 )
 
 // GetSACFilter returns the sac filter for componentCVEEdge ids.
