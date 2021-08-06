b8051b60d45c056e84db91e62ae0413e49e7099f
Mark Pedrotti <pedrottimark@gmail.com>
2021-08-06 09:25:46 -0400

    ROX-7745: Improve confirmation modal to delete cluster init bundle (stackrox/rox#8979)
    (cherry picked from commit ccc01a361e69c93924cd73f1b84fafc17de455c1)

--- ui/apps/platform/src/Containers/Integrations/IntegrationsListPage/ConfirmationTexts.tsx
+++ ui/apps/platform/src/Containers/Integrations/IntegrationsListPage/ConfirmationTexts.tsx
@@ -16,18 +16,6 @@ export function DeleteAPITokensConfirmationText({
     );
 }
 
-export function DeleteClusterInitBundlesConfirmationText({
-    numIntegrations,
-}: ConfirmationTextProps): ReactElement {
-    return (
-        <div>
-            Are you sure you want to revoke {numIntegrations} cluster init&nbsp;
-            {pluralize('bundle', numIntegrations)}? Revoking a cluster init bundle will cause the
-            StackRox services installed with it in clusters to lose connectivity.
-        </div>
-    );
-}
-
 export function DeleteIntegrationsConfirmationText({
     numIntegrations,
 }: ConfirmationTextProps): ReactElement {
