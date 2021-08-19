343c17557d1b3b5d57f4b6d6fc503fe6ee59eb14
Matthias <Maddosaurus@users.noreply.github.com>
2021-08-19 12:19:00 +0200

    ROX-7831: Update GitHub Action for Operator Release to work on tags instead of branches (stackrox/rox#9147)
    (cherry picked from commit d40652532257a4540a365ff9ed7bd9ba1a3f4b51)

--- scripts/redhat-infra-release/prepare-rh-release.sh
+++ scripts/redhat-infra-release/prepare-rh-release.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-# prepare-rh-release <path to rox repo> <source branch> <target branch>
+# prepare-rh-release <path to rox repo> <source tag> <operator target tag> <target branch>
 #
 # Prepares the rox repo at given path for an Operator release on RH infrastructure.
-# As we're experiencing slowness/issues with Cachito, the given source branch will be used as a base for:
+# As we're experiencing slowness/issues with Cachito, the given source tag will be used as a base for:
 # - Removing all Go dependencies except the ones Operator relies on
 # - Generating autogen sources
 # These changes will be comitted to the given target branch, OVERWRITING anything in that branch
+# The resulting commit will be tagged with the given operator target tag.
 #
 # Important: this script runs in dry-run mode by default. You MUST set DRY_RUN=false in the environment
 # if you want it to actually push the target branch.
@@ -24,17 +25,19 @@ commit_msg="Add proto gen sources and remove non-operator deps"
 
 repo_path="${1:-}"
 [[ -n "$repo_path" ]] || die "No path to rox repo specified"
-source_branch="${2:-}"
-[[ -n "$source_branch" ]] || die "No source branch specified"
-target_branch="${3:-}"
+source_tag="${2:-}"
+[[ -n "$source_tag" ]] || die "No source tag specified"
+target_operator_tag="${3:-}"
+[[ -n "$target_operator_tag" ]] || die "No target operator tag specified"
+target_branch="${4:-}"
 [[ -n "$target_branch" ]] || die "No target branch specified"
 
 
 info "Entering rox repo at $repo_path"
 cd "$repo_path"
 
-info "Switching to $source_branch"
-git checkout "$source_branch"
+info "Switching to $source_tag"
+git checkout "$source_tag"
 
 info "Generating sources and introducing changes"
 "${run_path}/generate-proto-sources.sh" "${repo_path}"
@@ -49,6 +52,14 @@ git switch -C "$target_branch"
 git add -A
 git -c "user.name=$git_user" -c "user.email=$git_user_email" commit -am "$commit_msg"
 
+info "Tagging HEAD commit of $target_branch with $target_operator_tag"
+git -c "user.name=$git_user" -c "user.email=$git_user_email" tag "${target_operator_tag}"
+
+push_command="git push --dry-run"
+push_tag_message="Pushing tag $target_operator_tag to remote"
+push_branch_message="Pushing branch to origin/$target_branch"
+
+
 if [[ "${DRY_RUN:-}" != "false" ]]; then
   echo
   echo "==================================================================================="
@@ -57,9 +68,16 @@ if [[ "${DRY_RUN:-}" != "false" ]]; then
   echo " If you DO need to push locally, invoke this script with DRY_RUN=false set.        "
   echo "==================================================================================="
   echo
-  echo "Used $source_branch as source and would have pushed to origin/$target_branch"
-  exit 0
+  echo "Used $source_tag as source and would have pushed branch origin/$target_branch and tag $target_operator_tag"
+  echo
+  push_tag_message="DRY RUN: ${push_tag_message}"
+  push_branch_message="DRY RUN: ${push_branch_message}"
+else
+  push_command="git push"  # not a dry run
 fi
 
-info "Pushing branch to origin/$target_branch"
-git push --force origin "$target_branch"
+info "${push_tag_message}"
+$push_command origin "${target_operator_tag}"
+
+info "${push_branch_message}"
+$push_command --force origin "$target_branch"
