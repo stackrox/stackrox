e7848f056d6d7c633691e75dde13c8a19a019dae
Linda Song <ls@stackrox.com>
2021-10-05 18:16:09 -0700

    ROX-7828: Resetting Initial Values on Save for Access Scope (stackrox/rox#9510)

--- ui/apps/platform/src/Containers/AccessControl/AccessScopes/AccessScopeForm.tsx
+++ ui/apps/platform/src/Containers/AccessControl/AccessScopes/AccessScopeForm.tsx
@@ -215,6 +215,7 @@ function AccessScopeForm({
             })
             .finally(() => {
                 setIsSubmitting(false);
+                resetForm({ values });
             });
     }
 
