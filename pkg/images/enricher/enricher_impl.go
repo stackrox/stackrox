08e8306134640ca420eccd0f6bbbc2db703c3d12
Cong Du <c-du@users.noreply.github.com>
2021-10-11 20:01:27 -0700

    ROX-8180: Remove executables from image scan in image cache (stackrox/rox#9554)

--- pkg/images/enricher/enricher_impl.go
+++ pkg/images/enricher/enricher_impl.go
@@ -342,9 +342,9 @@ func (e *enricherImpl) enrichImageWithScanner(ctx EnrichmentContext, image *stor
 	image.Scan = scan
 	FillScanStats(image)
 
-	// Clone the cachedScan because the scan is used within the image leading to race conditions
-	cachedScan := scan.Clone()
+	cachedScan := prepareImageScanForCache(scan)
 	e.scanCache.Add(getRef(image), cachedScan)
+
 	if image.GetId() == "" {
 		if digest := image.GetMetadata().GetV2().GetDigest(); digest != "" {
 			e.scanCache.Add(digest, cachedScan)
@@ -354,6 +354,16 @@ func (e *enricherImpl) enrichImageWithScanner(ctx EnrichmentContext, image *stor
 		}
 	}
 	return ScanSucceeded, nil
+}
+
+func prepareImageScanForCache(scan *storage.ImageScan) *storage.ImageScan {
+	// Clone the cachedScan because the scan is used within the image leading to race conditions
+	clonedScan := scan.Clone()
+	// Executables are processed and stored in active component updater executable cache and hence we remove them here.
+	for _, component := range clonedScan.GetComponents() {
+		component.Executables = nil
+	}
+	return clonedScan
 }
 
 // FillScanStats fills in the higher level stats from the scan data.
