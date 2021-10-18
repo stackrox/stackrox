5e7f8ef0243869805e89b4a76b5fc36a83e0115b
Connor Gorman <cgorman@stackrox.com>
2021-10-18 15:27:25 -0700

    remove json iter

--- tools/generate-helpers/pg-bindings/main.go
+++ tools/generate-helpers/pg-bindings/main.go
@@ -105,7 +105,7 @@ func compileStmtOrPanic(db *sql.DB, query string) *sql.Stmt {
 
 const (
 	createTableQuery = "create table if not exists {{.Table}} (id varchar primary key, value jsonb)"
-	createIDIndexQuery = "create index {{.Table}}_id on {{.Table}} using hash ((id))"
+	createIDIndexQuery = "create index if not exists {{.Table}}_id on {{.Table}} using hash ((id))"
 )
 
 // New returns a new Store instance using the provided sql instance.
