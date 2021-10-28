37387efa5cc3ad45ed8421e2eb89f3ebf162dbde
Connor Gorman <cgorman@stackrox.com>
2021-10-27 23:08:22 -0700

    needs

---
+++ pkg/postgres/query.go
@@ -0,0 +1,19 @@
+package postgres
+
+import (
+	"strconv"
+	"strings"
+)
+
+func GetValues(start, end int) string {
+	var sb strings.Builder
+	sb.WriteString("(")
+	for i := start; i < end; i++ {
+		sb.WriteString("$"+strconv.Itoa(i))
+		if i != end-1 {
+			sb.WriteString(", ")
+		}
+	}
+	sb.WriteString(")")
+	return sb.String()
+}
