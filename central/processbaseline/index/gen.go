948804ec90bfd9d9c907944174a0ae212fc177f3
Connor Gorman <cgorman@stackrox.com>
2021-11-23 13:59:38 -0800

    Yeah, don't index them as well

--- central/processbaseline/index/gen.go
+++ central/processbaseline/index/gen.go
@@ -1,4 +1,4 @@
 package index
 
-//go:generate blevebindings-wrapper --object ProcessBaseline --singular Baseline --search-category PROCESS_BASELINES
+//go:generate blevebindings-wrapper --object ProcessBaseline --singular ProcessBaseline --search-category PROCESS_BASELINES
 //go:generate mockgen-wrapper Indexer
