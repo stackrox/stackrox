2b7382eaeca66e7c8e1b3154690445a63caaaa7a
Nakul Chander <1407686+theencee@users.noreply.github.com>
2021-12-07 15:37:03 -0800

    review comments

--- pkg/env/reprocessing_interval.go
+++ pkg/env/reprocessing_interval.go
@@ -7,4 +7,8 @@ var (
 	ReprocessInterval = registerDurationSetting("ROX_REPROCESSING_INTERVAL", 4*time.Hour)
 	// ActiveVulnRefreshInterval will set the duration for which to refresh active components and vulnerabilities.
 	ActiveVulnRefreshInterval = registerDurationSetting("ROX_ACTIVE_VULN_REFRESH_INTERVAL", 15*time.Minute)
+	// VulnDeferralTimedReObserveInterval will set the duration for when to check to see if timed vuln deferrals need to be checked for expiry.
+	VulnDeferralTimedReObserveInterval = registerDurationSetting("ROX_VULN_TIMED_DEFERRAL_REOBSERVE_INTERVAL", 1*time.Hour)
+	// VulnDeferralFixableReObserveInterval will set the duration for when to check to see if "when fixable" vuln deferrals need to be checked for expiry.
+	VulnDeferralFixableReObserveInterval = registerDurationSetting("ROX_VULN_FIXABLE_DEFERRAL_REOBSERVE_INTERVAL", 4*time.Hour)
 )
