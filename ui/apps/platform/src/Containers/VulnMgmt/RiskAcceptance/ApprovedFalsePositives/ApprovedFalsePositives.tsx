919c26047e2089a632e4818a6608626499b36d57
Saif Chaudhry <sachaudh@users.noreply.github.com>
2021-12-07 11:15:19 -0800

    ROX-8261+ROX-8263+ROX-8264+ROX-8265+ROX-8564: Added the approved false positives table (#20)
    ROX-8261: Added GraphQL API to get data
    ROX-8263: Added table with columns and values
    ROX-8264: Should allow you to reobserve a CVE
    ROX-8265: Should show a modal when clicking the comments link
    ROX-8564: Add GraphQL API to reobserve CVE

---
+++ ui/apps/platform/src/Containers/VulnMgmt/RiskAcceptance/ApprovedFalsePositives/ApprovedFalsePositives.tsx
@@ -0,0 +1,53 @@
+import React, { ReactElement } from 'react';
+import { Bullseye, Spinner } from '@patternfly/react-core';
+import { useQuery, useApolloClient } from '@apollo/client';
+
+import {
+    GetVulnerabilityRequestsData,
+    GetVulnerabilityRequestsVars,
+    GET_VULNERABILITY_REQUESTS,
+} from '../vulnerabilityRequests.graphql';
+
+import ApprovedFalsePositivesTable from './ApprovedFalsePositivesTable';
+
+function ApprovedFalsePositives(): ReactElement {
+    const client = useApolloClient();
+    const { loading: isLoading, data } = useQuery<
+        GetVulnerabilityRequestsData,
+        GetVulnerabilityRequestsVars
+    >(GET_VULNERABILITY_REQUESTS, {
+        variables: {
+            query: 'Request Status:APPROVED+Requested Vulnerability State:FALSE_POSITIVE+Expired Request:false',
+            pagination: {
+                limit: 20,
+                offset: 0,
+                sortOption: {
+                    field: 'id',
+                    reversed: false,
+                },
+            },
+        },
+    });
+
+    async function updateTable() {
+        await client.refetchQueries({
+            include: [GET_VULNERABILITY_REQUESTS],
+        });
+    }
+
+    if (isLoading) {
+        return (
+            <Bullseye>
+                <Spinner size="sm" />
+            </Bullseye>
+        );
+    }
+
+    const rows = data?.results || [];
+
+    return (
+        <ApprovedFalsePositivesTable rows={rows} updateTable={updateTable} isLoading={isLoading} />
+    );
+}
+
+export default ApprovedFalsePositives;
