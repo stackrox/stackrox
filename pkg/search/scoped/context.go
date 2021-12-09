1d95dc91a2c1b7eae66d82c4eef2b345cbf6f2aa
Khushboo Sancheti <42253461+clickboo@users.noreply.github.com>
2021-12-09 14:27:17 -0800

    Adding support to save the scope hierarchy in the Context object, and to be able to query context for the scope at a particular level. (#72)

--- pkg/search/scoped/context.go
+++ pkg/search/scoped/context.go
@@ -8,8 +8,9 @@ import (
 
 // Scope hold an id and scope level for scoping searches.
 type Scope struct {
-	ID    string
-	Level v1.SearchCategory
+	ID     string
+	Level  v1.SearchCategory
+	Parent *Scope
 }
 
 // scopedContextKey is the key for the scope value in the context.
@@ -22,6 +23,10 @@ type scopedContextValue struct {
 
 // Context returns a new context with the scope attached.
 func Context(ctx context.Context, scope Scope) context.Context {
+	inner, ok := ctx.Value(scopedContextKey{}).(*scopedContextValue)
+	if ok {
+		scope.Parent = &inner.scope
+	}
 	return context.WithValue(ctx, scopedContextKey{}, &scopedContextValue{
 		scope: scope,
 	})
@@ -38,4 +43,27 @@ func GetScope(hasGraphContext context.Context) (Scope, bool) {
 	}
 	s := inter.(*scopedContextValue)
 	return s.scope, true
+}
+
+// GetScopeAtLevel returns the Scope from the input context with the given level, nil if that level doesn't exist in the scope hierarchy
+func GetScopeAtLevel(hasGraphContext context.Context, level v1.SearchCategory) (Scope, bool) {
+	if hasGraphContext == nil {
+		return Scope{}, false
+	}
+	inter := hasGraphContext.Value(scopedContextKey{})
+	if inter == nil {
+		return Scope{}, false
+	}
+	scope := inter.(*scopedContextValue).scope
+	return getScopeAtLevel(&scope, level)
+}
+
+func getScopeAtLevel(scope *Scope, level v1.SearchCategory) (Scope, bool) {
+	if scope == nil {
+		return Scope{}, false
+	}
+	if scope.Level == level {
+		return *scope, true
+	}
+	return getScopeAtLevel(scope.Parent, level)
 }
