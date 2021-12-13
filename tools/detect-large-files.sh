1d8fbf1aeabd9343ff0b729194572684c197a1b9
Michaël <michael@redhat.com>
2021-12-13 11:37:05 +0100

    ROX-8663: Generalize scripts (#71)
    Large git files detection and git hooks installation scripts.

---
+++ tools/detect-large-files.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# Finds large files that have been checked in to Git.
+set -euo pipefail
+
+GIT_REPO_TOP="$(git rev-parse --show-toplevel)"
+
+allowlist_path=""
+if [[ $# -eq 1 ]]
+then
+  allowlist_path="$1"
+fi
+
+allowed_files=""
+if [[ -n "$allowlist_path" ]]
+then
+  allowed_files=$(egrep -v '^\s*(#.*)$' "${allowlist_path}" | xargs git -C "$GIT_REPO_TOP" ls-files --)
+fi
+
+large_files=$(git ls-tree --full-tree -l -r HEAD "$GIT_REPO_TOP" | awk '$4 > 50*1024 {print$5}')
+
+
+IFS=$'\n' read -d '' -r -a non_allowed_files < <(
+  {
+    echo "${large_files}"
+    echo "${allowed_files}"
+    echo "${allowed_files}"
+  } | sort | uniq -u
+) || true
+
+
+[[ "${#non_allowed_files[@]}" == 0 ]] || {
+  echo "Found large files in the working tree. Please remove them!"
+  echo "If you must add them, you need to explicitly add them to the allow list file ${allowlist_path:-and provide it as an argument to this script.}"
+  echo "Files were: "
+  printf '  %s\n' "${non_allowed_files[@]}"
+  exit 1
+} >&2
