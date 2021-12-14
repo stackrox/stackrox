25a15c167c092c1049a7953f41ef7afa2e6d3091
Mark Pedrotti <mark@stackrox.com>
2021-12-13 19:33:41 -0500

    ROX-8361: Add MitreAttackVectorsFormSection to policies wizard step 1

---
+++ ui/apps/platform/src/Containers/Policies/PatternFly/Wizard/Step1/MitreTacticSelect.tsx
@@ -0,0 +1,66 @@
+import React, { ReactElement, useState } from 'react';
+import { Select, SelectOption } from '@patternfly/react-core';
+
+import { MitreAttackVector } from 'types/mitre.proto';
+
+type MitreTacticSelectProps = {
+    className: string;
+    getIsDisabledOption: (optionId: string) => boolean;
+    handleSelectOption: (optionId: string) => void;
+    isDisabled: boolean;
+    label: string;
+    mitreAttackVectors: MitreAttackVector[];
+    tacticId: string;
+};
+
+/*
+ * Select to add or replace a MITRE ATT&CK tactic.
+ */
+function MitreTacticSelect({
+    className,
+    getIsDisabledOption, // is tactic already selected
+    handleSelectOption,
+    isDisabled, // Select element is disabled if tactic has techniques
+    label,
+    mitreAttackVectors,
+    tacticId,
+}: MitreTacticSelectProps): ReactElement {
+    const [isOpen, setIsOpen] = useState(false);
+
+    function onSelect(_event, optionId) {
+        setIsOpen(false);
+        handleSelectOption(optionId);
+    }
+
+    return (
+        <Select
+            aria-label={label}
+            className={className}
+            isDisabled={isDisabled}
+            isOpen={isOpen}
+            onSelect={onSelect}
+            onToggle={setIsOpen}
+            placeholderText={label}
+            selections={tacticId}
+        >
+            {mitreAttackVectors.map(({ tactic: { id, name } }) => {
+                // See MitreAttackVectorsFormSection.css for name and id style rules.
+                return (
+                    <SelectOption
+                        key={id}
+                        value={id}
+                        isDisabled={getIsDisabledOption(id)}
+                        className="mitre-tactic-option"
+                    >
+                        <div className="pf-u-display-flex pf-u-flex-nowrap pf-u-justify-content-space-between">
+                            <span className="name">{name}</span>
+                            <span className="id">{id}</span>
+                        </div>
+                    </SelectOption>
+                );
+            })}
+        </Select>
+    );
+}
+
+export default MitreTacticSelect;
