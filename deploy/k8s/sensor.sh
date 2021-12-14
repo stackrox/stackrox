6469305a1f20ca53b75bfab3934265f71f27c3ad
Gavin Jefferies <gavin@stackrox.com>
2021-12-14 17:41:40 +0000

    Revert "RS-374: Remove echoed secret"
    This reverts commit db91b905af19156d7debb11b8b632078c7c5b59c.

--- deploy/k8s/sensor.sh
+++ deploy/k8s/sensor.sh
@@ -4,13 +4,13 @@ set -e
 K8S_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 COMMON_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../common && pwd)"
 
-source "$COMMON_DIR"/deploy.sh
-source "$COMMON_DIR"/k8sbased.sh
-source "$COMMON_DIR"/env.sh
-source "$K8S_DIR"/env.sh
+source $COMMON_DIR/deploy.sh
+source $COMMON_DIR/k8sbased.sh
+source $COMMON_DIR/env.sh
+source $K8S_DIR/env.sh
 
 if [[ -z $CLUSTER ]]; then
-    read -r -p "Enter cluster name to create: " CLUSTER
+    read -p "Enter cluster name to create: " CLUSTER
 fi
 echo "CLUSTER set to $CLUSTER"
 
@@ -18,8 +18,8 @@ if [[ -z "${ROX_ADMIN_PASSWORD}" ]]; then
     export ROX_ADMIN_PASSWORD="${ROX_PASSWORD:-}"
 fi
 if [[ -z "$ROX_ADMIN_PASSWORD" && -f "${K8S_DIR}/central-deploy/password" ]]; then
-	ROX_ADMIN_PASSWORD="$(cat "${K8S_DIR}"/central-deploy/password)"
-    export ROX_ADMIN_PASSWORD
+	export ROX_ADMIN_PASSWORD="$(cat ${K8S_DIR}/central-deploy/password)"
 fi
+echo "ROX_ADMIN_PASSWORD set to ${ROX_ADMIN_PASSWORD}"
 
 launch_sensor "$K8S_DIR"
