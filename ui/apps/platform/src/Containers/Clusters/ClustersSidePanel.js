56af1454d35a35bc0196326d54a8024e989db295
Juan Rodriguez Hortala <juanrh@redhat.com>
2021-12-21 10:58:35 +0100

    Small refactor to have smaller diff, and replace if with switch

--- ui/apps/platform/src/Containers/Clusters/ClustersSidePanel.js
+++ ui/apps/platform/src/Containers/Clusters/ClustersSidePanel.js
@@ -131,43 +131,46 @@ function ClustersSidePanel({ selectedClusterId, setSelectedClusterId }) {
                             setPollingDelay(null);
                         }
 
-                        if (managerType(cluster) === 'MANAGER_TYPE_KUBERNETES_OPERATOR') {
-                            if (wizardStep === wizardSteps.FORM) {
-                                setMessageState({
-                                    type: 'warn',
-                                    message: (
-                                        <>
-                                            <h3 className="font-700 mb-2">
-                                                Operator-managed cluster
-                                            </h3>
-                                            <p>
-                                                This is an operator-managed cluster. The settings of
-                                                operator-managed clusters cannot be changed here and
-                                                must instead be changed by updating its custom
-                                                resource definition (CRD).
-                                            </p>
-                                        </>
-                                    ),
-                                });
-                            }
-                        }
-
-                        if (managerType(cluster) === 'MANAGER_TYPE_HELM_CHART') {
-                            if (wizardStep === wizardSteps.FORM) {
-                                setMessageState({
-                                    type: 'warn',
-                                    message: (
-                                        <>
-                                            <h3 className="font-700 mb-2">Helm-managed cluster</h3>
-                                            <p>
-                                                This is an Helm-managed cluster. The settings of
-                                                Helm-managed clusters cannot be changed here, please
-                                                ask your DevOps team to change the settings by
-                                                updating the Helm values.
-                                            </p>
-                                        </>
-                                    ),
-                                });
+                        if (wizardStep === wizardSteps.FORM) {
+                            switch (managerType(cluster)) {
+                                case 'MANAGER_TYPE_HELM_CHART':
+                                    setMessageState({
+                                        type: 'warn',
+                                        message: (
+                                            <>
+                                                <h3 className="font-700 mb-2">
+                                                    Helm-managed cluster
+                                                </h3>
+                                                <p>
+                                                    This is an Helm-managed cluster. The settings of
+                                                    Helm-managed clusters cannot be changed here,
+                                                    please ask your DevOps team to change the
+                                                    settings by updating the Helm values.
+                                                </p>
+                                            </>
+                                        ),
+                                    });
+                                    break;
+                                case 'MANAGER_TYPE_KUBERNETES_OPERATOR':
+                                    setMessageState({
+                                        type: 'warn',
+                                        message: (
+                                            <>
+                                                <h3 className="font-700 mb-2">
+                                                    Operator-managed cluster
+                                                </h3>
+                                                <p>
+                                                    This is an operator-managed cluster. The
+                                                    settings of operator-managed clusters cannot be
+                                                    changed here and must instead be changed by
+                                                    updating its custom resource definition (CRD).
+                                                </p>
+                                            </>
+                                        ),
+                                    });
+                                    break;
+                                default:
+                                    break;
                             }
                         }
                     })
