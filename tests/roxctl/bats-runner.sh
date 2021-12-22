d00b9b5464f9eab383166de033ec9d11b13e992b
Piotr Rygielski <prygiels@redhat.com>
2021-12-22 12:13:51 +0100

    Make Bats generate junit reports when run on CI

--- tests/roxctl/bats-runner.sh
+++ tests/roxctl/bats-runner.sh
@@ -3,11 +3,16 @@
 set -uo pipefail
 
 BATS_TESTS="${1:-tests/roxctl/bats-tests}"
+CIRCLECI="${CIRCLECI:-}"
 echo "Using Bats version: $(bats --version)"
 echo "Testing roxctl version: '$(roxctl version)'"
 
 # All flags but --tap require at least Bats v1.5.0
-BATS_FLAGS=( "--tap" "--print-output-on-failure" "--verbose-run" "--show-output-of-passing-tests" )
+BATS_FLAGS=( "--print-output-on-failure" "--verbose-run" "--show-output-of-passing-tests" )
+if [[ $CIRCLECI == "true" ]]; then
+  mkdir -p roxctl-test-output
+  BATS_FLAGS+=( "--report-formatter" "junit" "--output" "roxctl-test-output" )
+fi
 
 # Running all bats test suites found in the directory
 echo "Running Bats with parameters: " "${BATS_FLAGS[@]}"
