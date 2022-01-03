0314f6e59d30c1480631e3ff3b3cc9c5d12fecf5
Frederico Bittencourt <fbittenc@redhat.com>
2022-01-03 17:27:19 +0100

    use GetImageFlavorFromEnv where env var can be fetched

---
+++ pkg/images/defaults/flavor_release_test.go
@@ -0,0 +1,31 @@
+//go:build release
+// +build release test
+
+package defaults
+
+import (
+	"testing"
+
+	"github.com/stackrox/rox/pkg/buildinfo/testbuildinfo"
+	"github.com/stackrox/rox/pkg/testutils/envisolator"
+	"github.com/stackrox/rox/pkg/version/testutils"
+	"github.com/stretchr/testify/suite"
+)
+
+type devPanicTestSuite struct {
+	suite.Suite
+	envIsolator *envisolator.EnvIsolator
+}
+
+func TestShouldPanic(t *testing.T) {
+	suite.Run(t, new(devPanicTestSuite))
+}
+
+func (s *devPanicTestSuite) TestShouldPanic() {
+	testbuildinfo.SetForTest(s.T())
+	testutils.SetExampleVersion(s.T())
+	s.envIsolator = envisolator.NewEnvIsolator(s.T())
+	s.envIsolator.Setenv(imageFlavorEnvName, "development_development")
+	s.PanicsWithError("Cannot use development_development flavor in build release", func(){ GetImageFlavorFromEnv() })
+}
+
