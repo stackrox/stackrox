7a35476011a2a2fd96b8e327c9ad6600e27ecb7c
Juan Rodriguez Hortala <juanrh@redhat.com>
2022-01-05 12:56:56 +0100

    Reorder func to have entry point on top, and aux funcs dowmn

--- central/localscanner/service.go
+++ central/localscanner/service.go
@@ -37,17 +37,26 @@ func (s *serviceImpl) RegisterServiceHandler(context.Context, *runtime.ServeMux,
 	return nil
 }
 
-func localScannerCertificatesFor(serviceType storage.ServiceType, namespace string, clusterID string) (*central.LocalScannerCertificates, error) {
-	certificates, err := generateServiceCertMap(serviceType, namespace, clusterID)
+func (s *serviceImpl) IssueLocalScannerCerts(ctx context.Context, request *central.IssueLocalScannerCertsRequest) (*central.IssueLocalScannerCertsResponse, error) {
+	clusterID, err := getClusterID(ctx)
 	if err != nil {
-		return nil, errors.Wrapf(err, "error generating certificate for service %s", serviceType)
+		return nil, errors.Wrap(err, "could not determine cluster ID")
 	}
 
-	return &central.LocalScannerCertificates{
-		Ca:          certificates[mtls.CACertFileName],
-		Cert:        certificates[mtls.ServiceCertFileName],
-		Key:         certificates[mtls.ServiceKeyFileName],
-	}, nil
+	return issueLocalScannerCerts(request.GetNamespace(), clusterID)
+}
+
+func getClusterID(ctx context.Context) (string, error) {
+	var requestingServiceIdentity *storage.ServiceIdentity
+	if id := authn.IdentityFromContextOrNil(ctx); id != nil {
+		requestingServiceIdentity = id.Service()
+	}
+	clusterID, err := centralsensor.GetClusterID(clusterid.Get(), requestingServiceIdentity.GetId())
+	if err != nil {
+		return "", err
+	}
+
+	return clusterID, nil
 }
 
 func issueLocalScannerCerts(namespace string, clusterID string) (*central.IssueLocalScannerCertsResponse, error) {
@@ -74,24 +83,15 @@ func issueLocalScannerCerts(namespace string, clusterID string) (*central.IssueL
 	}, nil
 }
 
-func getClusterID(ctx context.Context) (string, error) {
-	var requestingServiceIdentity *storage.ServiceIdentity
-	if id := authn.IdentityFromContextOrNil(ctx); id != nil {
-		requestingServiceIdentity = id.Service()
-	}
-	clusterID, err := centralsensor.GetClusterID(clusterid.Get(), requestingServiceIdentity.GetId())
+func localScannerCertificatesFor(serviceType storage.ServiceType, namespace string, clusterID string) (*central.LocalScannerCertificates, error) {
+	certificates, err := generateServiceCertMap(serviceType, namespace, clusterID)
 	if err != nil {
-		return "", err
+		return nil, errors.Wrapf(err, "error generating certificate for service %s", serviceType)
 	}
 
-	return clusterID, nil
-}
-
-func (s *serviceImpl) IssueLocalScannerCerts(ctx context.Context, request *central.IssueLocalScannerCertsRequest) (*central.IssueLocalScannerCertsResponse, error) {
-	clusterID, err := getClusterID(ctx)
-	if err != nil {
-		return nil, errors.Wrap(err, "could not determine cluster ID")
-	}
-
-	return issueLocalScannerCerts(request.GetNamespace(), clusterID)
+	return &central.LocalScannerCertificates{
+		Ca:   certificates[mtls.CACertFileName],
+		Cert: certificates[mtls.ServiceCertFileName],
+		Key:  certificates[mtls.ServiceKeyFileName],
+	}, nil
 }
