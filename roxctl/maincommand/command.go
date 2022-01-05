9f1b965fc310c7e7d779f82e3e4ea707eab0a35e
Piotr Rygielski <prygiels@redhat.com>
2022-01-05 12:36:32 +0100

    Use proper version set for 'roxctl version --json'

--- roxctl/maincommand/command.go
+++ roxctl/maincommand/command.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/stackrox/rox/pkg/buildinfo"
 	"github.com/stackrox/rox/pkg/version"
 	"github.com/stackrox/rox/roxctl/central"
 	"github.com/stackrox/rox/roxctl/cluster"
@@ -30,7 +31,10 @@ func versionCommand() *cobra.Command {
 			if useJSON, _ := c.Flags().GetBool("json"); useJSON {
 				enc := json.NewEncoder(os.Stdout)
 				enc.SetIndent("", "  ")
-				return enc.Encode(version.GetAllVersionsUnified()) // Warning! Development version of roxctl will display unified versions of scanner and collector
+				if buildinfo.ReleaseBuild {
+					return enc.Encode(version.GetAllVersionsUnified())
+				}
+				return enc.Encode(version.GetAllVersions())
 			}
 			fmt.Println(version.GetMainVersion())
 			return nil
