55eba5f1efe778794b4b88fc3a505021825e0bb2
Connor Gorman <cgorman@stackrox.com>
2022-01-06 15:21:53 -0800

    try this

--- pkg/dackbox/crud/cache.go
+++ pkg/dackbox/crud/cache.go
@@ -1,8 +1,15 @@
 package crud
 
 import (
+	"time"
+
 	"github.com/golang/protobuf/proto"
+	"github.com/stackrox/rox/pkg/logging"
 	"github.com/stackrox/rox/pkg/sync"
+)
+
+var (
+	log = logging.LoggerForModule()
 )
 
 // NewCache creates a new cache
@@ -29,10 +36,15 @@ func (c *Cache) Exists(key []byte) bool {
 
 // Get returns the cloned message for the passed key if it exists
 func (c *Cache) Get(key []byte) (proto.Message, bool) {
+	start := time.Now()
 	c.lock.RLock()
+	log.Infof("Get runlock took: %d ms", time.Since(start))
 	defer c.lock.RUnlock()
 
 	if msg, ok := c.cache[string(key)]; ok {
+		defer func(start time.Time) {
+			log.Infof("Get clone took: %d ms", time.Since(start))
+		}(time.Now())
 		return proto.Clone(msg), true
 	}
 	return nil, false
@@ -40,16 +52,26 @@ func (c *Cache) Get(key []byte) (proto.Message, bool) {
 
 // Set populates the cache
 func (c *Cache) Set(key []byte, msg proto.Message) {
+	start := time.Now()
 	c.lock.Lock()
+	log.Infof("Set lock took: %d ms", time.Since(start))
 	defer c.lock.Unlock()
 
+	defer func(start time.Time) {
+		log.Infof("set and clone took: %d ms", time.Since(start))
+	}(time.Now())
 	c.cache[string(key)] = proto.Clone(msg)
 }
 
 // Delete removes an item from the cache
 func (c *Cache) Delete(key []byte) {
+	start := time.Now()
 	c.lock.Lock()
+	log.Infof("delete lock took: %d ms", time.Since(start))
 	defer c.lock.Unlock()
 
+	defer func(start time.Time) {
+		log.Infof("delete key took: %d ms", time.Since(start))
+	}(time.Now())
 	delete(c.cache, string(key))
 }
