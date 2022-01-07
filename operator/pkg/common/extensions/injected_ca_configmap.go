5ff993bc25f58895525b5c9811b42cd2b4e16f7f
Michaël Petrov <michael@redhat.com>
2022-01-07 12:25:31 +0100

    Helm pre-install ConfigMap

--- operator/pkg/common/extensions/injected_ca_configmap.go
+++ operator/pkg/common/extensions/injected_ca_configmap.go
@@ -15,37 +15,38 @@ import (
 )
 
 const (
-	configMapName = "injected-trusted-ca"
-	label         = "config.openshift.io/inject-trusted-cabundle"
+	configMapNamePrefix = "injected-cabundle-"
+	label               = "config.openshift.io/inject-trusted-cabundle"
 )
 
 // InjectTrustedCAConfigMapExtension returns an extension that takes care of reconciling the injected trusted CA ConfigMap.
 func InjectTrustedCAConfigMapExtension(k8s kubernetes.Interface) extensions.ReconcileExtension {
 	return func(ctx context.Context, obj *unstructured.Unstructured, statusUpdater func(statusFunc extensions.UpdateStatusFunc), _ logr.Logger) error {
 		client := k8s.CoreV1().ConfigMaps(obj.GetNamespace())
-		switch configmap, err := getConfigMap(ctx, client, obj); {
+		name := configMapNamePrefix + obj.GetName()
+		switch configmap, err := getConfigMap(ctx, name, client, obj); {
 		case err != nil:
 			return err
 		case configmap == nil:
 			if _, err := client.Create(ctx, makeConfigMap(obj), metav1.CreateOptions{}); err != nil {
-				return errors.Wrapf(err, "cannot create configMap %s", configMapName)
+				return errors.Wrapf(err, "cannot create configMap %s", name)
 			}
 		case takeControl(configmap, obj):
 			if _, err := client.Update(ctx, configmap, metav1.UpdateOptions{}); err != nil {
-				return errors.Wrapf(err, "cannot control configMap %s", configMapName)
+				return errors.Wrapf(err, "cannot control configMap %s", name)
 			}
 		}
 		return nil
 	}
 }
 
-func getConfigMap(ctx context.Context, client v1.ConfigMapInterface, obj *unstructured.Unstructured) (*corev1.ConfigMap, error) {
-	configmap, err := client.Get(ctx, configMapName, metav1.GetOptions{})
+func getConfigMap(ctx context.Context, name string, client v1.ConfigMapInterface, obj *unstructured.Unstructured) (*corev1.ConfigMap, error) {
+	configmap, err := client.Get(ctx, name, metav1.GetOptions{})
 	if err != nil {
 		if apiErrors.IsNotFound(err) {
 			return nil, nil
 		}
-		return nil, errors.Wrapf(err, "cannot retrieve configMap %s", configMapName)
+		return nil, errors.Wrapf(err, "cannot retrieve configMap %s", name)
 	}
 	labels := configmap.GetLabels()
 	if labels != nil {
@@ -54,68 +55,58 @@ func getConfigMap(ctx context.Context, client v1.ConfigMapInterface, obj *unstru
 		}
 	}
 	if labels == nil {
-		return nil, errors.Errorf("configMap %s exists, but is not properly labeled", configMapName)
+		return nil, errors.Errorf("configMap %s exists, but is not properly labeled", name)
 	}
 
 	return configmap, nil
 }
 
 func makeConfigMap(controller *unstructured.Unstructured) *corev1.ConfigMap {
-	return &corev1.ConfigMap{
+	cm := &corev1.ConfigMap{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "v1",
 			Kind:       "ConfigMap",
 		},
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      configMapName,
+			Name:      configMapNamePrefix,
 			Namespace: controller.GetNamespace(),
-			OwnerReferences: []metav1.OwnerReference{
-				*metav1.NewControllerRef(controller, controller.GroupVersionKind()),
-			},
 			Labels: map[string]string{
 				label: "true",
 			},
 		}}
+	takeControl(cm, controller)
+	return cm
 }
 
-// takeControl returns true if the provided configmap structure has been altered and therefore
+// takeControl returns true if the provided object has been altered and therefore
 // needs to be updated in the cluster.
-func takeControl(configmap *corev1.ConfigMap, obj *unstructured.Unstructured) bool {
-	controller := getController(configmap)
-
-	// Don't alter configmap if it's already properly controlled.
-	if controller != nil &&
-		(controller.Kind == "Central" && obj.GetKind() == "SecuredCluster" ||
-			controller.UID == obj.GetUID()) {
-		return false
-	}
-
+func takeControl(obj metav1.Object, newController *unstructured.Unstructured) bool {
 	not := false
 
-	// Remove control from the existing controller, but keep it as an owner.
-	controller.Controller = &not
-	controller.BlockOwnerDeletion = &not
+	if currentController := metav1.GetControllerOfNoCopy(obj); currentController != nil {
+
+		// Don't alter the object if it's already properly controlled.
+		if currentController.UID == newController.GetUID() ||
+			currentController.Kind == "Central" && newController.GetKind() == "SecuredCluster" ||
+			newController.GetDeletionTimestamp() != nil {
+			return false
+		}
+
+		// Remove control from the existing controller, but keep it as an owner.
+		currentController.Controller = &not
+		currentController.BlockOwnerDeletion = &not
+	}
 
 	// Add new controller.
-	newref := metav1.NewControllerRef(obj, obj.GroupVersionKind())
+	newref := metav1.NewControllerRef(newController, newController.GroupVersionKind())
 
 	// Allow k8s garbage collector to delete the owner if needed.
 	newref.BlockOwnerDeletion = &not
 
-	refs := configmap.GetOwnerReferences()
+	refs := obj.GetOwnerReferences()
 	if refs == nil {
 		refs = []metav1.OwnerReference{}
 	}
-	configmap.SetOwnerReferences(append(refs, *newref))
+	obj.SetOwnerReferences(append(refs, *newref))
 	return true
-}
-
-func getController(configmap *corev1.ConfigMap) *metav1.OwnerReference {
-	refs := configmap.GetOwnerReferences()
-	for i := range refs {
-		if refs[i].Controller != nil && *refs[i].Controller {
-			return &refs[i]
-		}
-	}
-	return nil
 }
--- operator/pkg/common/extensions/injected_ca_configmap_test.go
+++ operator/pkg/common/extensions/injected_ca_configmap_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/types"
 )
@@ -18,7 +19,7 @@ func makeTestController(kind string, uid types.UID) *unstructured.Unstructured {
 	return controller
 }
 
-func TestConfigMapController(t *testing.T) {
+func TestTakeControl(t *testing.T) {
 	scanner := makeTestController("Scanner", "0000")
 	sensor := makeTestController("Sensor", "0001")
 	central1 := makeTestController("Central", "0002")
@@ -27,18 +28,18 @@ func TestConfigMapController(t *testing.T) {
 	cm := makeConfigMap(scanner)
 	assert.False(t, takeControl(cm, scanner))
 
-	assert.Equal(t, getController(cm).UID, scanner.GetUID())
-	assert.NotEqual(t, getController(cm).UID, sensor.GetUID())
+	assert.Equal(t, metav1.GetControllerOfNoCopy(cm).UID, scanner.GetUID())
+	assert.NotEqual(t, metav1.GetControllerOfNoCopy(cm).UID, sensor.GetUID())
 
 	assert.True(t, takeControl(cm, sensor))
-	assert.NotEqual(t, getController(cm).UID, scanner.GetUID())
-	assert.Equal(t, getController(cm).UID, sensor.GetUID())
+	assert.NotEqual(t, metav1.GetControllerOfNoCopy(cm).UID, scanner.GetUID())
+	assert.Equal(t, metav1.GetControllerOfNoCopy(cm).UID, sensor.GetUID())
 
 	assert.True(t, takeControl(cm, central1))
-	assert.NotEqual(t, getController(cm).UID, sensor.GetUID())
-	assert.Equal(t, getController(cm).UID, central1.GetUID())
+	assert.NotEqual(t, metav1.GetControllerOfNoCopy(cm).UID, sensor.GetUID())
+	assert.Equal(t, metav1.GetControllerOfNoCopy(cm).UID, central1.GetUID())
 
 	assert.True(t, takeControl(cm, central2))
-	assert.NotEqual(t, getController(cm).UID, central1.GetUID())
-	assert.Equal(t, getController(cm).UID, central2.GetUID())
+	assert.NotEqual(t, metav1.GetControllerOfNoCopy(cm).UID, central1.GetUID())
+	assert.Equal(t, metav1.GetControllerOfNoCopy(cm).UID, central2.GetUID())
 }
