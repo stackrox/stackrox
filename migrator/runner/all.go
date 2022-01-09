a5fd26937b3e11460581b0cca3ead514da80d9ed
Mandar Darwatkar <mandar@stackrox.com>
2022-01-09 12:42:54 -0800

    [ROX-8898] Migrate CVE Suppressed to Vulnerability State (#249)
    * Migrate CVE Suppressed to Vulnerability State

--- migrator/runner/all.go
+++ migrator/runner/all.go
@@ -47,4 +47,5 @@ import (
 	_ "github.com/stackrox/rox/migrator/migrations/m_86_to_m_87_microdnf_policy"
 	_ "github.com/stackrox/rox/migrator/migrations/m_87_to_m_88_central_secret_policy"
 	_ "github.com/stackrox/rox/migrator/migrations/m_88_to_m_89_update_log4shell_policy"
+	_ "github.com/stackrox/rox/migrator/migrations/m_89_to_m_90_vuln_state"
 )
