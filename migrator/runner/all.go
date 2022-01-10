841cea980f87684de89f06472d5123ecfab240d7
Mandar Darwatkar <mandar@stackrox.com>
2022-01-10 15:10:17 -0800

    [ROX-8694] Add vuln req perms to those with write image perms (#251)
    * [ROX-8694] Add vuln req perms to those with write image perms

--- migrator/runner/all.go
+++ migrator/runner/all.go
@@ -48,4 +48,5 @@ import (
 	_ "github.com/stackrox/rox/migrator/migrations/m_87_to_m_88_central_secret_policy"
 	_ "github.com/stackrox/rox/migrator/migrations/m_88_to_m_89_update_log4shell_policy"
 	_ "github.com/stackrox/rox/migrator/migrations/m_89_to_m_90_vuln_state"
+	_ "github.com/stackrox/rox/migrator/migrations/m_90_to_m_91_snooze_permissions"
 )
