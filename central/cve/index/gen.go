8b4cfd9a2e91ee6ae3ec75b442519173d317e51b
Viswajith Venugopal <viswajithiii@gmail.com>
2022-01-11 12:04:32 -0800

    Speed up go generate (#137)

--- central/cve/index/gen.go
+++ central/cve/index/gen.go
@@ -1,4 +1,3 @@
 package index
 
-//go:generate blevebindings-wrapper --write-options=false --options-path mappings --object CVE --singular CVE --search-category VULNERABILITIES
-//go:generate mockgen-wrapper Indexer
+//go:generate blevebindings-wrapper --write-options=false --options-path mappings --object CVE --singular CVE --search-category VULNERABILITIES --generate-mock-indexer
