3fc19de3081b7aa4dc5414cf21adb4f34639ca81
Michaël Petrov <michael@redhat.com>
2022-01-12 09:22:28 +0100

    WIP: refactoring and other fixes after review

--- operator/pkg/common/extensions/injected_ca_configmap.go
+++ operator/pkg/common/extensions/injected_ca_configmap.go
@@ -8,54 +8,38 @@ import (
 	"github.com/pkg/errors"
 	corev1 "k8s.io/api/core/v1"
 	apiErrors "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/client-go/kubernetes"
-)
-
-const (
-	configMapNamePrefix = "injected-cabundle-"
-	label               = "config.openshift.io/inject-trusted-cabundle"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
 // InjectTrustedCAConfigMapExtension returns an extension that takes care of reconciling the injected trusted CA ConfigMap.
-func InjectTrustedCAConfigMapExtension(k8s kubernetes.Interface) extensions.ReconcileExtension {
+func InjectTrustedCAConfigMapExtension(k8s client.Client) extensions.ReconcileExtension {
 	return func(ctx context.Context, obj *unstructured.Unstructured, statusUpdater func(statusFunc extensions.UpdateStatusFunc), _ logr.Logger) error {
-		client := k8s.CoreV1().ConfigMaps(obj.GetNamespace())
-		name := configMapNamePrefix + obj.GetName()
-		configmap, err := client.Get(ctx, name, metav1.GetOptions{})
-		if err != nil {
+		configMap := &corev1.ConfigMap{}
+		key := client.ObjectKey{Namespace: obj.GetNamespace(), Name: "injected-cabundle-" + obj.GetName()}
+		if err := k8s.Get(ctx, key, configMap); err != nil {
 			if apiErrors.IsNotFound(err) {
+				// Ignore missing ConfigMap.
 				return nil
 			}
-			return errors.Wrapf(err, "cannot retrieve configMap %s", name)
+			return errors.Wrapf(err, "cannot retrieve configMap %s", key.Name)
 		}
-		if err = validate(configmap); err != nil {
+		if err := validate(configMap); err != nil {
 			return err
 		}
-		if !metav1.IsControlledBy(configmap, obj) {
-			takeControl(configmap, obj)
-			if _, err := client.Update(ctx, configmap, metav1.UpdateOptions{}); err != nil {
-				return errors.Wrapf(err, "cannot control configMap %s", name)
+		if controllerutil.SetControllerReference(obj, configMap, nil) == nil {
+			if err := k8s.Update(ctx, configMap); err != nil {
+				return errors.Wrapf(err, "cannot control configMap %s", key.Name)
 			}
 		}
 		return nil
 	}
 }
 
-func validate(configmap *corev1.ConfigMap) error {
-	labels := configmap.GetLabels()
-	if labels[label] != "true" {
-		return errors.Errorf("configMap %s exists, but is not properly labeled", configmap.GetName())
+func validate(configMap *corev1.ConfigMap) error {
+	if configMap.GetLabels()["config.openshift.io/inject-trusted-cabundle"] != "true" {
+		return errors.Errorf("configMap %s exists, but is not properly labeled", configMap.GetName())
 	}
 	return nil
-}
-
-func takeControl(configmap metav1.Object, controller *unstructured.Unstructured) {
-	refs := configmap.GetOwnerReferences()
-	if refs == nil {
-		refs = []metav1.OwnerReference{}
-	}
-	newref := metav1.NewControllerRef(controller, controller.GroupVersionKind())
-	configmap.SetOwnerReferences(append(refs, *newref))
 }
