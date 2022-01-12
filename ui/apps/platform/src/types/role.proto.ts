0f06ae68c3e2443e21b481cdcf89955331d13922
Mark Pedrotti <pedrottimark@gmail.com>
2022-01-11 21:06:19 -0500

    Replace specific code with generic usePermissions hook in Policies (#280)

--- ui/apps/platform/src/types/role.proto.ts
+++ ui/apps/platform/src/types/role.proto.ts
@@ -1,7 +1,1 @@
 export type Access = 'NO_ACCESS' | 'READ_ACCESS' | 'READ_WRITE_ACCESS';
-
-// @TODO: Add more options as needed
-export type ResourceName =
-    | 'VulnerabilityManagementApprovals'
-    | 'VulnerabilityManagementRequests'
-    | 'VulnerabilityReports';
