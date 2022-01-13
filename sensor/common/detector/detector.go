d9400f5866152d405a1d7f8731bf667d223bcfa1
md2119 <mandar@stackrox.com>
2022-01-12 21:44:12 -0800

    Change deployment reprocessing in sensor

--- sensor/common/detector/detector.go
+++ sensor/common/detector/detector.go
@@ -365,13 +365,8 @@ func (d *detectorImpl) ReprocessDeployments(deploymentIDs ...string) {
 	defer d.deploymentDetectionLock.Unlock()
 
 	for _, deploymentID := range deploymentIDs {
-		deployment := d.deploymentStore.Get(deploymentID)
-		if deployment == nil {
-			continue
-		}
-
+		d.deduper.removeDeployment(deploymentID)
 		d.markDeploymentForProcessing(deploymentID)
-		go d.enricher.blockingScan(deployment, central.ResourceAction_UPDATE_RESOURCE)
 	}
 }
 
