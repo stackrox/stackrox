1e4de870b707d06038927d2e8a973c0ffbc22bb3
Mikhail Sugakov <misha@stackrox.com>
2022-01-14 19:50:50 +0100

    Try assert `-slim` in any part of the collector image
    to address failure in roxctl tests that expect `-slim` only in the
    image tag. We have release images that go with `-slim` in the name
    hence trying to make the check simply verify presence (or absence)
    of substring.
    
    The failed test is here
    https://app.circleci.com/pipelines/github/stackrox/stackrox/2282/workflows/4570d727-8f80-4ff3-9392-1d6df598b7bd/jobs/100194

--- tests/roxctl/slim-collector.sh
+++ tests/roxctl/slim-collector.sh
@@ -27,6 +27,17 @@ curl_central() {
   curl -Sskf -u "admin:${ROX_PASSWORD}" "https://${API_ENDPOINT}/${url}" "$@"
 }
 
+check_image() {
+  image="$1"
+  condition="$2"
+  if [[ "$condition" == "has -slim" ]]; then
+    [[ "$image" == *"-slim"* ]]
+    return $?
+  fi
+  [[ "$image" != *"-slim"* ]]
+  return $?
+}
+
 # Retrieve API token
 API_TOKEN_JSON="$(curl_central v1/apitokens/generate \
   -d '{"name": "test", "role": "Admin"}')" \
@@ -37,7 +48,7 @@ export ROX_API_TOKEN
 
 test_collector_image_references_in_deployment_bundles() {
     SLIM_COLLECTOR_FLAG="$1"
-    EXPECTED_IMAGE_TAG="$2"
+    EXPECTED_IMAGE_CONDITION="$2"
 
     CLUSTER_NAME="test-cluster-$RANDOM"
     echo "Testing correctness of collector image references for clusters generated with $SLIM_COLLECTOR_FLAG (cluster name is $CLUSTER_NAME)"
@@ -65,13 +76,12 @@ test_collector_image_references_in_deployment_bundles() {
     fi
 
     # Verify that generated bundle references the expected collector image.
-    COLLECTOR_IMAGE_TAG="$(egrep 'image: \S+/collector' "sensor-${CLUSTER_NAME}/collector.yaml" | sed -e 's/[^:]*: "[^:]*:\(.*\)"$/\1/;')"
-    COLLECTOR_IMAGE_TAG_SUFFIX="$(echo "$COLLECTOR_IMAGE_TAG" | sed -e 's/.*-\([^-]*\)$/\1/;')"
+    COLLECTOR_IMAGE="$(egrep 'image: \S+/collector' "sensor-${CLUSTER_NAME}/collector.yaml" | sed -e 's/[^:]*: "\(.*\)"$/\1/;')"
 
-    if [ "$COLLECTOR_IMAGE_TAG_SUFFIX" == "$EXPECTED_IMAGE_TAG" ]; then
-        echo "[OK] Newly generated bundle references $EXPECTED_IMAGE_TAG collector image"
+    if check_image "$COLLECTOR_IMAGE" "$EXPECTED_IMAGE_CONDITION"; then
+        echo "[OK] Newly generated bundle collector image satisfies condition: $EXPECTED_IMAGE_CONDITION"
     else
-        eecho "[FAIL] Newly generated bundle does not reference $EXPECTED_IMAGE_TAG collector image (referenced collector image tag is $COLLECTOR_IMAGE_TAG)"
+        eecho "[FAIL] Newly generated bundle collector image does not satisfy condition: $EXPECTED_IMAGE_CONDITION (collector image is $COLLECTOR_IMAGE)"
         FAILURES=$((FAILURES + 1))
         return
     fi
@@ -81,13 +91,12 @@ test_collector_image_references_in_deployment_bundles() {
     # Verify that refetching deployment bundle for newly created cluster works as expected (i.e. that the bundle references the expected collector image).
     OUTPUT="$(roxctl --insecure-skip-tls-verify --insecure -e "$API_ENDPOINT" \
     sensor get-bundle --output-dir="sensor-${CLUSTER_NAME}-refetched" "$CLUSTER_NAME" 2>&1)"
-    COLLECTOR_IMAGE_TAG="$(egrep 'image: \S+/collector' "sensor-${CLUSTER_NAME}-refetched/collector.yaml" | sed -e 's/[^:]*: "[^:]*:\(.*\)"$/\1/;')"
-    COLLECTOR_IMAGE_TAG_SUFFIX="$(echo "$COLLECTOR_IMAGE_TAG" | sed -e 's/.*-\([^-]*\)$/\1/;')"
+    COLLECTOR_IMAGE="$(egrep 'image: \S+/collector' "sensor-${CLUSTER_NAME}-refetched/collector.yaml" | sed -e 's/[^:]*: "\(.*\)"$/\1/;')"
 
-    if [ "$COLLECTOR_IMAGE_TAG_SUFFIX" == "$EXPECTED_IMAGE_TAG" ]; then
-        echo "[OK] Refetched deployment bundle still references $EXPECTED_IMAGE_TAG collector image"
+    if check_image "$COLLECTOR_IMAGE" "$EXPECTED_IMAGE_CONDITION"; then
+        echo "[OK] Refetched deployment collector image still satisfies condition: $EXPECTED_IMAGE_CONDITION"
     else
-        eecho "[FAIL] Refetched deployment bundle does not reference $EXPECTED_IMAGE_TAG collector image (referenced collector image tag is $COLLECTOR_IMAGE_TAG)"
+        eecho "[FAIL] Refetched deployment bundle collector image does not satisfy condition: $EXPECTED_IMAGE_CONDITION (collector image is $COLLECTOR_IMAGE)"
         eecho "Captured output was:"
         eecho "$OUTPUT"
         FAILURES=$((FAILURES + 1))
@@ -106,9 +115,9 @@ test_collector_image_references_in_deployment_bundles() {
     fi
 }
 
-test_collector_image_references_in_deployment_bundles "--slim-collector" "slim"
-test_collector_image_references_in_deployment_bundles "--slim-collector=auto" "slim" # Central is deployed in online mode in CI
-test_collector_image_references_in_deployment_bundles "--slim-collector=false" "latest"
+test_collector_image_references_in_deployment_bundles "--slim-collector" "has -slim"
+test_collector_image_references_in_deployment_bundles "--slim-collector=auto" "has -slim" # Central is deployed in online mode in CI
+test_collector_image_references_in_deployment_bundles "--slim-collector=false" "does not have -slim"
 
 if [ $FAILURES -eq 0 ]; then
   echo "Passed"
