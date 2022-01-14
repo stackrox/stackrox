a81cff25f240f619023fc130a227ed34ed51ecf8
Saif Chaudhry <schaudhr@redhat.com>
2022-01-14 11:55:52 -0800

    ROX-8258: Search filter for Approved False Positives

---
+++ ui/apps/platform/src/Containers/VulnMgmt/RiskAcceptance/ApprovedFalsePositives/ApprovedFalsePositivesSearchFilter.tsx
@@ -0,0 +1,56 @@
+import React, { ReactElement, useState } from 'react';
+import { Button, ButtonVariant, InputGroup, TextInput } from '@patternfly/react-core';
+import { SearchIcon } from '@patternfly/react-icons';
+
+import { SearchFilter } from 'types/search';
+
+export type ApprovedFalsePositivesSearchFilterProps = {
+    searchFilter: SearchFilter;
+    setSearchFilter: React.Dispatch<React.SetStateAction<SearchFilter>>;
+};
+
+function ApprovedFalsePositivesSearchFilter({
+    searchFilter,
+    setSearchFilter,
+}: ApprovedFalsePositivesSearchFilterProps): ReactElement {
+    const [inputValue, setInputValue] = useState<string>('');
+
+    const selectedAttribute = 'Request ID';
+
+    function handleSearchChange(value) {
+        const modifiedSearchObject = { ...searchFilter };
+        if (value === '' || (Array.isArray(value) && value.length === 0)) {
+            delete modifiedSearchObject[selectedAttribute];
+        } else {
+            modifiedSearchObject[selectedAttribute] = value;
+        }
+        setSearchFilter(modifiedSearchObject);
+    }
+
+    function handleInputChange(value) {
+        setInputValue(value);
+    }
+
+    return (
+        <InputGroup>
+            <TextInput
+                name="requestIDSearchInput"
+                id="requestIDSearchInput"
+                type="search"
+                aria-label="request id search input"
+                placeholder="Filter by Request ID"
+                onChange={handleInputChange}
+                value={inputValue}
+            />
+            <Button
+                variant={ButtonVariant.control}
+                aria-label="search button for search input"
+                onClick={() => handleSearchChange(inputValue)}
+            >
+                <SearchIcon />
+            </Button>
+        </InputGroup>
+    );
+}
+
+export default ApprovedFalsePositivesSearchFilter;
