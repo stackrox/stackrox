89baabd967098ca0b48736533f164d78ec164e0c
Simon Baeumer <sbaumer@redhat.com>
2022-01-16 11:21:50 +0100

    Remove compile step from script

--- dev-tools/debug-helm-chart.sh
+++ dev-tools/debug-helm-chart.sh
@@ -1,19 +1,13 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-# Wrapper around helm but builds roxctl and generates new rox charts.
+# Wrapper around helm to sweeten the development experience by rendering both Helm charts before executing Helm
 # Example testing central-services-chart:
 # ./debug-helm-chart.sh upgrade --install --dry-run stackrox-central-services ./stackrox-central-services-chart -n stackrox --set imagePullSecrets.allowNone=true
 #
 # Usage: ./debug-helm-chart.sh [NAME] [CHART] [flags]
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-if [[ "$(uname)" == "Darwin"* ]]; then
-  make -C "$DIR/../" cli-darwin
-else
-  make -C "$DIR/../" cli-linux
-fi
 
 "$DIR/roxctl.sh" helm output central-services --remove --debug
 "$DIR/roxctl.sh" helm output secured-cluster-services --remove --debug
