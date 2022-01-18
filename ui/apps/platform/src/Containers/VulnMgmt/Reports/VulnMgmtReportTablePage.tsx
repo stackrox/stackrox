d3e435217fde8ab4a1cb5a2e6509b7871621f2ea
Van J. Wilson <vwilson@redhat.com>
2022-01-18 16:21:07 -0500

    Refactor fetching reports into hook

--- ui/apps/platform/src/Containers/VulnMgmt/Reports/VulnMgmtReportTablePage.tsx
+++ ui/apps/platform/src/Containers/VulnMgmt/Reports/VulnMgmtReportTablePage.tsx
@@ -3,10 +3,14 @@ import React, { useEffect, useState, ReactElement } from 'react';
 import { Link } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import {
+    Alert,
+    AlertVariant,
+    Bullseye,
     Button,
     ButtonVariant,
     PageSection,
     PageSectionVariants,
+    Spinner,
     Text,
     TextContent,
     Title,
@@ -14,23 +18,17 @@ import {
     ToolbarContent,
     ToolbarItem,
 } from '@patternfly/react-core';
-import useDeepCompareEffect from 'use-deep-compare-effect';
 
 import ACSEmptyState from 'Components/ACSEmptyState';
 import PageTitle from 'Components/PageTitle';
 import { searchCategories } from 'constants/entityTypes';
 import usePermissions from 'hooks/usePermissions';
 import useSearchOptions from 'hooks/useSearchOptions';
+import useFetchReports from 'hooks/useFetchReports';
 import useTableSort from 'hooks/useTableSort';
 import { vulnManagementReportsPath } from 'routePaths';
-import { fetchReports, fetchReportsCount, deleteReport, runReport } from 'services/ReportsService';
-import { ReportConfiguration } from 'types/report.proto';
-import {
-    filterAllowedSearch,
-    convertToRestSearch,
-    convertSortToGraphQLFormat,
-    convertSortToRestFormat,
-} from 'utils/searchUtils';
+import { deleteReport, runReport } from 'services/ReportsService';
+import { filterAllowedSearch } from 'utils/searchUtils';
 import VulnMgmtReportTablePanel from './VulnMgmtReportTablePanel';
 import VulnMgmtReportTableColumnDescriptor from './VulnMgmtReportTableColumnDescriptor';
 import { VulnMgmtReportQueryObject } from './VulnMgmtReport.utils';
@@ -47,12 +45,10 @@ function ReportTablePage({ query }: ReportTablePageProps): ReactElement {
 
     const pageSearch = query.s;
     const filteredSearch = filterAllowedSearch(searchOptions, pageSearch || {});
-    const restSearch = convertToRestSearch(filteredSearch || {});
 
     // Handle changes in the current table page.
     const [currentPage, setCurrentPage] = useState(1);
     const [perPage, setPerPage] = useState(20);
-    const [reportCount, setReportCount] = useState(0);
 
     // To handle sort options.
     const columns = VulnMgmtReportTableColumnDescriptor;
@@ -68,31 +64,12 @@ function ReportTablePage({ query }: ReportTablePageProps): ReactElement {
         sortOption,
     } = useTableSort(columns, defaultSort);
 
-    const [reports, setReports] = useState<ReportConfiguration[]>([]);
-
-    useDeepCompareEffect(() => {
-        fetchReportsCount()
-            .then((count) => {
-                setReportCount(count);
-            })
-            .catch((error) => {
-                // TODO
-            });
-    }, [restSearch]);
-
-    useDeepCompareEffect(() => {
-        refreshReportList();
-    }, [restSearch, currentPage, perPage, sortOption]);
-
-    function refreshReportList() {
-        fetchReports(restSearch || [], sortOption, currentPage - 1, perPage)
-            .then((reportsResponse) => {
-                setReports(reportsResponse);
-            })
-            .catch(() => {
-                // TODO: show error message on failure
-            });
-    }
+    const { reports, reportCount, error, isLoading, triggerRefresh } = useFetchReports(
+        filteredSearch,
+        sortOption,
+        currentPage,
+        perPage
+    );
 
     function onRunReports(reportIds) {
         const runPromises = reportIds.map((id) => runReport(id));
@@ -100,7 +77,7 @@ function ReportTablePage({ query }: ReportTablePageProps): ReactElement {
         // Note: errors are handled and displayed down at the call site,
         //       ui/apps/platform/src/Containers/VulnMgmt/Reports/VulnMgmtReportTablePage.tsx
         return Promise.all(runPromises).then(() => {
-            refreshReportList();
+            triggerRefresh();
         });
     }
 
@@ -110,7 +87,7 @@ function ReportTablePage({ query }: ReportTablePageProps): ReactElement {
         // Note: errors are handled and displayed down at the call site,
         //       ui/apps/platform/src/Containers/VulnMgmt/Reports/VulnMgmtReportTablePage.tsx
         return Promise.all(deletePromises).then(() => {
-            refreshReportList();
+            triggerRefresh();
         });
     }
 
@@ -149,9 +126,24 @@ function ReportTablePage({ query }: ReportTablePageProps): ReactElement {
                     </ToolbarContent>
                 </Toolbar>
             </PageSection>
-            {reports.length > 0 ? (
+            {!!error && (
+                <Alert
+                    isInline
+                    variant={AlertVariant.danger}
+                    title={error}
+                    className="pf-u-mb-lg"
+                />
+            )}
+            {isLoading && (
+                <PageSection variant={PageSectionVariants.light} isFilled>
+                    <Bullseye>
+                        <Spinner isSVG size="lg" />
+                    </Bullseye>
+                </PageSection>
+            )}
+            {!isLoading && reports && reports?.length > 0 && (
                 <VulnMgmtReportTablePanel
-                    reports={reports}
+                    reports={reports || []}
                     reportCount={reportCount}
                     currentPage={currentPage}
                     setCurrentPage={setCurrentPage}
@@ -165,7 +157,8 @@ function ReportTablePage({ query }: ReportTablePageProps): ReactElement {
                     onRunReports={onRunReports}
                     onDeleteReports={onDeleteReports}
                 />
-            ) : (
+            )}
+            {!isLoading && !reports?.length && (
                 <PageSection variant={PageSectionVariants.light} isFilled>
                     <ACSEmptyState title="No reports are currently configured." />
                 </PageSection>
