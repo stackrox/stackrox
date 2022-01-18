35633225a2916a7016c78562a5f1d44a020a2841
RTann <rtann@redhat.com>
2022-01-18 15:15:41 -0800

    updates

--- sensor/kubernetes/listener/resources/registry_store_test.go
+++ sensor/kubernetes/listener/resources/registry_store_test.go
@@ -3,22 +3,23 @@ package resources
 import (
 	"testing"
 
+	"github.com/stackrox/rox/pkg/docker/types"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestRegistryStore(t *testing.T) {
 	rs := newRegistryStore()
-	rs.addOrUpdateRegistry("a", "reg1", dockerConfigEntry{
+	rs.addOrUpdateRegistry("a", "reg1", types.DockerConfigEntry{
 		Username: "test1",
 		Password: "test1pass",
 		Email:    "test1@test.com",
 	})
-	rs.addOrUpdateRegistry("a", "reg2", dockerConfigEntry{
+	rs.addOrUpdateRegistry("a", "reg2", types.DockerConfigEntry{
 		Username: "test2",
 		Password: "test2pass",
 		Email:    "test2@test.com",
 	})
-	rs.addOrUpdateRegistry("b", "reg3", dockerConfigEntry{
+	rs.addOrUpdateRegistry("b", "reg3", types.DockerConfigEntry{
 		Username: "test3",
 		Password: "test2pass",
 		Email:    "test3@test.com",
--- sensor/kubernetes/listener/resources/secrets.go
+++ sensor/kubernetes/listener/resources/secrets.go
@@ -11,6 +11,7 @@ import (
 	"github.com/cloudflare/cfssl/certinfo"
 	"github.com/stackrox/rox/generated/internalapi/central"
 	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/docker/types"
 	"github.com/stackrox/rox/pkg/features"
 	"github.com/stackrox/rox/pkg/protoconv"
 	"github.com/stackrox/rox/pkg/registries/docker"
@@ -27,79 +28,6 @@ const (
 	saAnnotation = "kubernetes.io/service-account.name"
 	defaultSA    = "default"
 )
-
-// The following types are copied from the Kubernetes codebase,
-// since it is not placed in any of the officially supported client
-// libraries.
-// dockerConfigJSON represents ~/.docker/config.json file info
-// see https://github.com/docker/docker/pull/12009
-type dockerConfigJSON struct {
-	Auths dockerConfig `json:"auths"`
-}
-
-// dockerConfig represents the config file used by the docker CLI.
-// This config that represents the credentials that should be used
-// when pulling images from specific image repositories.
-type dockerConfig map[string]dockerConfigEntry
-
-// dockerConfigEntry is an entry in the dockerConfig.
-type dockerConfigEntry struct {
-	Username string
-	Password string
-	Email    string
-}
-
-// dockerConfigEntryWithAuth is used solely for deserializing the Auth field
-// into a dockerConfigEntry during JSON deserialization.
-type dockerConfigEntryWithAuth struct {
-	// +optional
-	Username string `json:"username,omitempty"`
-	// +optional
-	Password string `json:"password,omitempty"`
-	// +optional
-	Email string `json:"email,omitempty"`
-	// +optional
-	Auth string `json:"auth,omitempty"`
-}
-
-// decodeDockerConfigFieldAuth deserializes the "auth" field from dockercfg into a
-// username and a password. The format of the auth field is base64(<username>:<password>).
-func decodeDockerConfigFieldAuth(field string) (username, password string, err error) {
-	decoded, err := base64.StdEncoding.DecodeString(field)
-	if err != nil {
-		return
-	}
-
-	parts := strings.SplitN(string(decoded), ":", 2)
-	if len(parts) != 2 {
-		err = errors.New("unable to parse auth field")
-		return
-	}
-
-	username = parts[0]
-	password = parts[1]
-
-	return
-}
-
-func (d *dockerConfigEntry) UnmarshalJSON(data []byte) error {
-	var tmp dockerConfigEntryWithAuth
-	err := json.Unmarshal(data, &tmp)
-	if err != nil {
-		return err
-	}
-
-	d.Username = tmp.Username
-	d.Password = tmp.Password
-	d.Email = tmp.Email
-
-	if len(tmp.Auth) == 0 {
-		return nil
-	}
-
-	d.Username, d.Password, err = decodeDockerConfigFieldAuth(tmp.Auth)
-	return err
-}
 
 var dataTypeMap = map[string]storage.SecretType{
 	"-----BEGIN CERTIFICATE-----":              storage.SecretType_PUBLIC_CERTIFICATE,
@@ -204,7 +132,7 @@ func newSecretDispatcher(regStore *RegistryStore) *secretDispatcher {
 	}
 }
 
-func dockerConfigToImageIntegration(registry string, dce dockerConfigEntry) *storage.ImageIntegration {
+func dockerConfigToImageIntegration(registry string, dce types.DockerConfigEntry) *storage.ImageIntegration {
 	registryType := docker.GenericDockerRegistryType
 	if urlfmt.TrimHTTPPrefixes(registry) == redhatRegistryEndpoint {
 		registryType = rhel.RedHatRegistryType
@@ -226,7 +154,7 @@ func dockerConfigToImageIntegration(registry string, dce dockerConfigEntry) *sto
 }
 
 func (s *secretDispatcher) processDockerConfigEvent(secret *v1.Secret, action central.ResourceAction) []*central.SensorEvent {
-	var dockerConfig dockerConfig
+	var dockerConfig types.DockerConfig
 	switch secret.Type {
 	case v1.SecretTypeDockercfg:
 		data, ok := secret.Data[v1.DockerConfigKey]
@@ -242,7 +170,7 @@ func (s *secretDispatcher) processDockerConfigEvent(secret *v1.Secret, action ce
 		if !ok {
 			return nil
 		}
-		var dockerConfigJSON dockerConfigJSON
+		var dockerConfigJSON types.DockerConfigJSON
 		if err := json.Unmarshal(data, &dockerConfigJSON); err != nil {
 			log.Error(err)
 			return nil
