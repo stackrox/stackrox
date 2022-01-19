16adcbc2c2bcec20de65841e76d63f0301653efd
Saif Chaudhry <sachaudh@users.noreply.github.com>
2022-01-19 12:05:39 -0800

    fix: risk acceptance bug fixes (#355)

--- ui/apps/platform/src/hooks/usePermissions.ts
+++ ui/apps/platform/src/hooks/usePermissions.ts
@@ -9,19 +9,15 @@ type UsePermissionsResponse = {
     hasNoAccess: (resourceName: ResourceName) => boolean;
     hasReadAccess: (resourceName: ResourceName) => boolean;
     hasReadWriteAccess: (resourceName: ResourceName) => boolean;
-    currentUserName: string;
 };
 type UserRolePermissions = (state) => { resourceToAccess: Record<ResourceName, Access> };
-type CurrentUserName = (state) => string;
 
 const stateSelector = createStructuredSelector({
     userRolePermissions: selectors.getUserRolePermissions as UserRolePermissions,
-    // @TODO: currentUserName can be moved into it's own hook to separate concerns
-    currentUserName: selectors.getCurrentUserName as CurrentUserName,
 });
 
 const usePermissions = (): UsePermissionsResponse => {
-    const { userRolePermissions, currentUserName } = useSelector(stateSelector);
+    const { userRolePermissions } = useSelector(stateSelector);
 
     function hasNoAccess(resourceName: ResourceName) {
         const access = userRolePermissions?.resourceToAccess[resourceName];
@@ -38,7 +34,7 @@ const usePermissions = (): UsePermissionsResponse => {
         return access === 'READ_WRITE_ACCESS';
     }
 
-    return { hasNoAccess, hasReadAccess, hasReadWriteAccess, currentUserName };
+    return { hasNoAccess, hasReadAccess, hasReadWriteAccess };
 };
 
 export default usePermissions;
