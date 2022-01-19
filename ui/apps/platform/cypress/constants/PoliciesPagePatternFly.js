078fa80043f97a0474165154ebe1846a11e5ab95
Mark Pedrotti <pedrottimark@gmail.com>
2022-01-19 09:37:51 -0500

    Write exportPolicy integration test for PatternFly (#353)

--- ui/apps/platform/cypress/constants/PoliciesPagePatternFly.js
+++ ui/apps/platform/cypress/constants/PoliciesPagePatternFly.js
@@ -1,3 +1,20 @@
 export const url = '/main/policies-pf';
 
-export const selectors = {};
+export const selectors = {
+    table: {
+        trFirst: 'tbody tr:nth-child(1)',
+        linkToPolicy: 'td[data-label="Policy"] a',
+        buttonForActionToggle: 'td.pf-c-table__action button.pf-c-dropdown__toggle',
+        buttonForActionItem: 'td.pf-c-table__action ul li[role="menuitem"] button',
+    },
+    page: {
+        buttonForActionToggle: 'button.pf-c-dropdown__toggle:contains("Actions")',
+        buttonForActionItem:
+            'button.pf-c-dropdown__toggle:contains("Actions") + ul li[role="menuitem"] button',
+    },
+    toast: {
+        title: 'ul.pf-c-alert-group .pf-c-alert__title',
+        description: 'ul.pf-c-alert-group .pf-c-alert__description',
+    },
+    wizard: {},
+};
