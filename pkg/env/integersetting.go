ab984a0727911bec5dfb09e65c65ee10497c509e
Connor Gorman <connorgorman@users.noreply.github.com>
2022-01-19 23:11:49 -0800

    Add a rate limit on ScanImageInternal in central (#267)

---
+++ pkg/env/integersetting.go
@@ -0,0 +1,47 @@
+package env
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// IntegerSetting represents an environment variable which should be parsed into an integer
+type IntegerSetting struct {
+	envVar       string
+	defaultValue int
+}
+
+// EnvVar returns the string name of the environment variable
+func (s *IntegerSetting) EnvVar() string {
+	return s.envVar
+}
+
+// Setting returns the string form of the integer environment variable
+func (s *IntegerSetting) Setting() string {
+	return fmt.Sprintf("%d", s.IntegerSetting())
+}
+
+// IntegerSetting returns the integer object represented by the environment variable
+func (s *IntegerSetting) IntegerSetting() int {
+	val := os.Getenv(s.envVar)
+	if val == "" {
+		return s.defaultValue
+	}
+	v, err := strconv.Atoi(val)
+	if err != nil {
+		return s.defaultValue
+	}
+	return v
+}
+
+// RegisterIntegerSetting globally registers and returns a new integer setting.
+func RegisterIntegerSetting(envVar string, defaultValue int) *IntegerSetting {
+	s := &IntegerSetting{
+		envVar:       envVar,
+		defaultValue: defaultValue,
+	}
+
+	Settings[s.EnvVar()] = s
+	return s
+}
