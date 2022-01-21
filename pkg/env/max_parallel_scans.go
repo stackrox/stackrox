5230175726e852bd59887a9a10ba1a973a7b7369
Viswajith Venugopal <viswajithiii@gmail.com>
2022-01-20 16:07:41 -0800

    Update MaxParallelImageScanInternal default value to 30 (#390)

--- pkg/env/max_parallel_scans.go
+++ pkg/env/max_parallel_scans.go
@@ -2,5 +2,5 @@ package env
 
 var (
 	// MaxParallelImageScanInternal sets the max number of parallel scans on the ImageScanInternal endpoint.
-	MaxParallelImageScanInternal = RegisterIntegerSetting("ROX_MAX_PARALLEL_IMAGE_SCAN_INTERNAL", 20)
+	MaxParallelImageScanInternal = RegisterIntegerSetting("ROX_MAX_PARALLEL_IMAGE_SCAN_INTERNAL", 30)
 )
