cf03df579d472e82e3f1edbf378d42188f054020
Juan Rodriguez Hortala <juanrh@redhat.com>
2022-01-21 12:47:30 +0100

    add error handler and validator

--- pkg/retry/retry_source.go
+++ pkg/retry/retry_source.go
@@ -8,14 +8,16 @@ import (
 	"k8s.io/apimachinery/pkg/util/wait"
 )
 
-// RetryableSource is a value that allows asking for a result, and returns the
-// corresponding result asynchronously.
-// Clients only care about the first value returned in ResultC().
-// AskForResult() can be called several times to retry the result computation, the
+// RetryableSource is a proxy with an object that is able to compute a result, but
+// that might forget our request, or return an error result, and that returns the
+// result asynchronously.
+// AskForResult() can be called to request a result, that should be make available in the
+// returned channel. Each time AskForResult() is called the previously returned channel is abandoned.
+// Retry() can be called several times to retry the result computation, the
 // RetryableSource is in charge of handling the cancellation of the computation if needed.
 type RetryableSource interface {
-	AskForResult()
-	ResultC() chan *Result
+	AskForResult() chan *Result
+	Retry()
 }
 
 // Result wraps a pair (result, err) produced by a source. By convention
@@ -29,7 +31,11 @@ type Result struct {
 type RetryableSourceRetriever struct {
 	// time to consider failed a call to AskForResult() that didn't return a result yet.
 	RequestTimeout time.Duration
-	ErrReporter    func(err error)
+	// optionally specify a function to invoke on each error. waitDuration is the time until
+	// the next retry.
+	OnError func(err error, timeToNextRetry time.Duration)
+	// optionally specify a validation function for each result.
+	ValidateResult func(interface{}) bool
 	// should be reset between calls to Run.
 	Backoff      wait.Backoff
 	timeoutC     chan struct{}
@@ -49,7 +55,7 @@ func NewRetryableSourceRetriever(backoff wait.Backoff, requestTimeout time.Durat
 func (r *RetryableSourceRetriever) Run(ctx context.Context, source RetryableSource) (interface{}, error) {
 	r.timeoutC = make(chan struct{})
 
-	source.AskForResult()
+	resultC := source.AskForResult()
 	r.setTimeoutTimer(r.RequestTimeout)
 	defer r.setTimeoutTimer(-1)
 	for {
@@ -59,24 +65,30 @@ func (r *RetryableSourceRetriever) Run(ctx context.Context, source RetryableSour
 		case <-r.timeoutC:
 			// assume result will never come.
 			r.handleError(errors.New("timeout"), source)
-		case result := <-source.ResultC():
+		case result := <-resultC:
 			err := result.err
 			if err != nil {
 				r.handleError(err, source)
 			} else {
-				return result.v, nil
+				if r.ValidateResult != nil && !r.ValidateResult(result.v) {
+					err := errors.Errorf("validation failed for value %v", result.v)
+					r.handleError(err, source)
+				} else {
+					return result.v, nil
+				}
 			}
 		}
 	}
 }
 
 func (r *RetryableSourceRetriever) handleError(err error, source RetryableSource) {
-	if r.ErrReporter != nil {
-		r.ErrReporter(err)
+	waitDuration := r.Backoff.Step()
+	if r.OnError != nil {
+		r.OnError(err, waitDuration)
 	}
 	r.setTimeoutTimer(-1)
-	time.AfterFunc(r.Backoff.Step(), func() {
-		source.AskForResult()
+	time.AfterFunc(waitDuration, func() {
+		source.Retry()
 		r.setTimeoutTimer(r.RequestTimeout)
 	})
 }
