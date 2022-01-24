7eb24046196a2c10e00f366af14adb0b947e9660
Connor Gorman <cgorman@stackrox.com>
2022-01-24 13:14:15 -0800

    m

--- central/image/service/service_impl.go
+++ central/image/service/service_impl.go
@@ -204,7 +204,6 @@ func (s *serviceImpl) ScanImageInternal(ctx context.Context, request *v1.ScanIma
 		// even if we weren't able to enrich it
 	}
 
-
 	if img.GetId() != "" {
 		// Save the image if we received an ID from sensor
 		// Otherwise, our inferred ID may not match
