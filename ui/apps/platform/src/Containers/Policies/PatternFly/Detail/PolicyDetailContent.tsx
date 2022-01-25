70f91d7af6fff20aa3220b8a44ff6025a88750e8
Linda Song <ls@stackrox.com>
2022-01-25 14:50:34 -0500

    ROX-8372: Updated Step 5 Review Policy section (#426)

--- ui/apps/platform/src/Containers/Policies/PatternFly/Detail/PolicyDetailContent.tsx
+++ ui/apps/platform/src/Containers/Policies/PatternFly/Detail/PolicyDetailContent.tsx
@@ -5,39 +5,58 @@ import { Title, Divider } from '@patternfly/react-core';
 import { Cluster } from 'types/cluster.proto';
 import { NotifierIntegration } from 'types/notifier.proto';
 import { Policy } from 'types/policy.proto';
-import MitreAttackVectorsView from 'Containers/MitreAttackVectors/MitreAttackVectorsView';
 import PolicyOverview from './PolicyOverview';
 import BooleanPolicyLogicSection from '../Wizard/Step3/BooleanPolicyLogicSection';
+import PolicyScopeSection from './PolicyScopeSection';
+import PolicyBehaviorSection from './PolicyBehaviorSection';
 
 type PolicyDetailContentProps = {
     clusters: Cluster[];
     policy: Policy;
     notifiers: NotifierIntegration[];
+    isReview?: boolean;
 };
 
 function PolicyDetailContent({
     clusters,
     policy,
     notifiers,
+    isReview = false,
 }: PolicyDetailContentProps): React.ReactElement {
+    const { enforcementActions, eventSource, exclusions, scope, lifecycleStages } = policy;
     return (
-        <Formik initialValues={policy} onSubmit={() => {}}>
-            {() => (
+        <>
+            <PolicyOverview policy={policy} notifiers={notifiers} isReview={isReview} />
+            <Title headingLevel="h2" className="pf-u-mb-md pf-u-pt-lg">
+                Policy behavior
+            </Title>
+            <Divider component="div" className="pf-u-mb-md" />
+            <PolicyBehaviorSection
+                lifecycleStages={lifecycleStages}
+                eventSource={eventSource}
+                enforcementActions={enforcementActions}
+            />
+            <Formik initialValues={policy} onSubmit={() => {}}>
+                {() => (
+                    <>
+                        <Title headingLevel="h2" className="pf-u-mb-md pf-u-pt-lg">
+                            Policy criteria
+                        </Title>
+                        <Divider component="div" className="pf-u-pb-md" />
+                        <BooleanPolicyLogicSection readOnly />
+                    </>
+                )}
+            </Formik>
+            {(scope.length > 0 || exclusions.length > 0) && (
                 <>
-                    <PolicyOverview clusters={clusters} policy={policy} notifiers={notifiers} />
                     <Title headingLevel="h2" className="pf-u-mb-md pf-u-pt-lg">
-                        MITRE ATT&CK
+                        Policy scope
                     </Title>
                     <Divider component="div" />
-                    <MitreAttackVectorsView policyId={policy.id} />
-                    <Title headingLevel="h2" className="pf-u-mb-md">
-                        Policy criteria
-                    </Title>
-                    <Divider component="div" className="pf-u-pb-md" />
-                    <BooleanPolicyLogicSection readOnly />
+                    <PolicyScopeSection scope={scope} exclusions={exclusions} clusters={clusters} />
                 </>
             )}
-        </Formik>
+        </>
     );
 }
 
