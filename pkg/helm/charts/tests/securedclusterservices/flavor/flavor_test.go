3c1eec0224915a73bfcf9c1c114c8a1fe90281fe
Simon Baeumer <sbaumer@redhat.com>
2022-01-25 12:34:53 +0100

    add release build protection

--- pkg/helm/charts/tests/securedclusterservices/flavor/flavor_test.go
+++ pkg/helm/charts/tests/securedclusterservices/flavor/flavor_test.go
@@ -6,6 +6,7 @@ import (
 
 	helmTest "github.com/stackrox/helmtest/pkg/framework"
 	"github.com/stackrox/rox/image"
+	"github.com/stackrox/rox/pkg/buildinfo"
 	"github.com/stackrox/rox/pkg/buildinfo/testbuildinfo"
 	"github.com/stackrox/rox/pkg/features"
 	"github.com/stackrox/rox/pkg/helm/charts"
@@ -39,7 +40,11 @@ func TestWithDifferentImageFlavors(t *testing.T) {
 			require.NoError(t, err, "error retrieving chart template")
 			metaVals := charts.GetMetaValuesForFlavor(imageFlavor)
 			metaVals.ClusterName = "Test"
-			metaVals.FeatureFlags[features.LocalImageScanning.EnvVar()] = false
+
+			if !buildinfo.ReleaseBuild {
+				metaVals.FeatureFlags[features.LocalImageScanning.EnvVar()] = false
+			}
+
 			ch, err := tpl.InstantiateAndLoad(metaVals)
 			require.NoError(t, err, "error instantiating chart")
 
--- pkg/helm/charts/tests/securedclusterservices/helmtest_test.go
+++ pkg/helm/charts/tests/securedclusterservices/helmtest_test.go
@@ -5,6 +5,7 @@ import (
 
 	helmTest "github.com/stackrox/helmtest/pkg/framework"
 	"github.com/stackrox/rox/image"
+	"github.com/stackrox/rox/pkg/buildinfo"
 	"github.com/stackrox/rox/pkg/features"
 	metaUtil "github.com/stackrox/rox/pkg/helm/charts/testutils"
 	"github.com/stretchr/testify/require"
@@ -16,7 +17,10 @@ func TestWithHelmtest(t *testing.T) {
 	tpl, err := helmImage.GetSecuredClusterServicesChartTemplate()
 	require.NoError(t, err, "error retrieving chart template")
 	metaVals := metaUtil.MakeMetaValuesForTest(t)
-	metaVals.FeatureFlags[features.LocalImageScanning.EnvVar()] = false
+
+	if !buildinfo.ReleaseBuild {
+		metaVals.FeatureFlags[features.LocalImageScanning.EnvVar()] = false
+	}
 
 	ch, err := tpl.InstantiateAndLoad(metaVals)
 	require.NoError(t, err, "error instantiating chart")
