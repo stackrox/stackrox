3cc04b38f7483b26e09d8bd39c3cddc5cb0d7f3c
Juan Rodriguez Hortala <juanrh@redhat.com>
2022-01-26 12:18:01 +0100

    Create CertificateRequester to request certificates from central

---
+++ sensor/kubernetes/localscanner/certificate_requester.go
@@ -0,0 +1,68 @@
+package localscanner
+
+import (
+	"context"
+
+	"github.com/stackrox/rox/generated/internalapi/central"
+	"github.com/stackrox/rox/pkg/logging"
+	"github.com/stackrox/rox/pkg/uuid"
+)
+
+var (
+	_ CertificateRequester = (*certRequesterSyncImpl)(nil)
+	log                    = logging.LoggerForModule()
+)
+
+// CertificateRequester request a new set of local scanner certificates to central.
+type CertificateRequester interface {
+	RequestCertificates(ctx context.Context) (*central.IssueLocalScannerCertsResponse, error)
+}
+
+// NewCertificateRequester creates a new CertificateRequester that communicates through
+// the specified channels, and that uses a fresh request ID.
+func NewCertificateRequester(msgFromSensorC chan *central.MsgFromSensor,
+	msgToSensorC chan *central.IssueLocalScannerCertsResponse) CertificateRequester {
+	return &certRequesterSyncImpl{
+		requestID:      uuid.NewV4().String(),
+		msgFromSensorC: msgFromSensorC,
+		msgToSensorC:   msgToSensorC,
+	}
+}
+
+type certRequesterSyncImpl struct {
+	requestID      string
+	msgFromSensorC chan *central.MsgFromSensor
+	msgToSensorC   chan *central.IssueLocalScannerCertsResponse
+}
+
+func (i *certRequesterSyncImpl) RequestCertificates(ctx context.Context) (*central.IssueLocalScannerCertsResponse, error) {
+	msg := &central.MsgFromSensor{
+		Msg: &central.MsgFromSensor_IssueLocalScannerCertsRequest{
+			IssueLocalScannerCertsRequest: &central.IssueLocalScannerCertsRequest{
+				RequestId: i.requestID,
+			},
+		},
+	}
+	select {
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	case i.msgFromSensorC <- msg:
+		log.Debugf("request to issue local Scanner certificates sent to Central succesfully: %v", msg)
+	}
+
+	var response *central.IssueLocalScannerCertsResponse
+	for response == nil {
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		case newResponse := <-i.msgToSensorC:
+			if newResponse.GetRequestId() != i.requestID {
+				log.Debugf("ignoring response with unknown request id %q", response.GetRequestId())
+			} else {
+				response = newResponse
+			}
+		}
+	}
+
+	return response, nil
+}
