346abf9393c456363309658cfbf176f7e2b67217
RTann <rtann@redhat.com>
2022-01-25 16:13:31 -0800

    remove logs

--- sensor/kubernetes/listener/resources/secrets.go
+++ sensor/kubernetes/listener/resources/secrets.go
@@ -195,10 +195,6 @@ func (s *secretDispatcher) processDockerConfigEvent(secret *v1.Secret, action ce
 				err := s.regStore.upsertRegistry(secret.GetNamespace(), registry, dce)
 				if err != nil {
 					log.Errorf("Unable to upsert registry %q into store: %v", registry, err)
-				} else {
-					// TODO: delete me
-					log.Infof("Successfully added registry")
-					log.Infof("Registries: %v", s.regStore.store)
 				}
 			}
 		}
