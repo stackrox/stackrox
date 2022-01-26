df762d14e1af0e5affe23195c9a49ecb5220f0a3
Simon Bäumer <baeumer@pm.me>
2022-01-26 15:15:01 +0100

    Add common helmtest loader and run func (#430)

--- pkg/helm/charts/tests/centralservices/flavor/flavor_test.go
+++ pkg/helm/charts/tests/centralservices/flavor/flavor_test.go
@@ -7,11 +7,9 @@ import (
 	helmTest "github.com/stackrox/helmtest/pkg/framework"
 	"github.com/stackrox/rox/image"
 	"github.com/stackrox/rox/pkg/buildinfo/testbuildinfo"
-	"github.com/stackrox/rox/pkg/helm/charts"
+	helmChartTestUtils "github.com/stackrox/rox/pkg/helm/charts/testutils"
 	"github.com/stackrox/rox/pkg/images/defaults"
 	"github.com/stackrox/rox/pkg/version/testutils"
-	"github.com/stretchr/testify/require"
-	"helm.sh/helm/v3/pkg/chartutil"
 )
 
 const testDir = "testdata/helmtest"
@@ -62,26 +60,10 @@ func TestWithDifferentImageFlavors(t *testing.T) {
 	for name, f := range imageFlavorCases {
 		imageFlavor := f()
 		t.Run(name, func(t *testing.T) {
-			helmImage := image.GetDefaultImage()
-			tpl, err := helmImage.GetCentralServicesChartTemplate()
-			require.NoError(t, err, "error retrieving chart template")
-			metaVals := charts.GetMetaValuesForFlavor(imageFlavor)
-			ch, err := tpl.InstantiateAndLoad(metaVals)
-			require.NoError(t, err, "error instantiating chart")
-
-			suite, err := helmTest.NewLoader(testDir, helmTest.WithAdditionalTestDirs(path.Join(testDir, name))).LoadSuite()
-			require.NoError(t, err, "failed to load helmtest suite")
-
-			target := &helmTest.Target{
-				Chart: ch,
-				ReleaseOptions: chartutil.ReleaseOptions{
-					Name:      "stackrox-central-services",
-					Namespace: "stackrox",
-					IsInstall: true,
-				},
-			}
-
-			suite.Run(t, target)
+			helmChartTestUtils.RunHelmTestSuite(t, testDir, image.CentralServicesChartPrefix, helmChartTestUtils.RunHelmTestSuiteOpts{
+				Flavor:       &imageFlavor,
+				HelmTestOpts: []helmTest.LoaderOpt{helmTest.WithAdditionalTestDirs(path.Join(testDir, name))},
+			})
 		})
 	}
 }
