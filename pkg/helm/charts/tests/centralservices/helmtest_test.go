87d19c91336ac59e29b8525334b6779e5abdd002
Simon Baeumer <sbaumer@redhat.com>
2022-01-26 13:52:43 +0100

    X-Smart-Squash: Squashed 20 commits:
    7cfc62024 Add slim mode to scanner
    4bb6fdb52 docs
    edef7b604 Add Scanner slim image
    9b591dae5 Add scanner image version
    fd1d96030 fix style
    4b72e52eb Add slim image
    70bccc659 Add feature flag protections
    2e934c5f0 fix tests
    4a6477a38 Use slim in image name
    9a6bafbe8 Fix image name
    cf5a49d4d Add feature flags
    67fe668c6 CR fixes
    d354263f5 fix
    5b01e2215 fix rebase
    076229429 Add slim and full tests
    cdc761d99 remove scanner test from Central chart
    d3ec82f79 update helmtest
    6d1ee025f god mod tidy
    63d977b5b CR fixes
    4c6cf3ea2 Remove slim tests from full image

--- pkg/helm/charts/tests/centralservices/helmtest_test.go
+++ pkg/helm/charts/tests/centralservices/helmtest_test.go
@@ -3,10 +3,19 @@ package centralservices
 import (
 	"testing"
 
+	helmTest "github.com/stackrox/helmtest/pkg/framework"
 	"github.com/stackrox/rox/image"
+	"github.com/stackrox/rox/pkg/buildinfo"
 	helmChartTestUtils "github.com/stackrox/rox/pkg/helm/charts/testutils"
+
 )
 
 func TestWithHelmtest(t *testing.T) {
-	helmChartTestUtils.RunHelmTestSuite(t, "testdata/helmtest", image.CentralServicesChartPrefix, helmChartTestUtils.RunHelmTestSuiteOpts{})
+	additionalTestDirs := []string{"../shared/scanner-full"}
+	if !buildinfo.ReleaseBuild {
+		additionalTestDirs = append(additionalTestDirs, "../shared/scanner-slim")
+	}
+	helmChartTestUtils.RunHelmTestSuite(t, "testdata/helmtest", image.CentralServicesChartPrefix, helmChartTestUtils.RunHelmTestSuiteOpts{
+		HelmTestOpts: []helmTest.LoaderOpt{helmTest.WithAdditionalTestDirs(additionalTestDirs...)},
+	})
 }
