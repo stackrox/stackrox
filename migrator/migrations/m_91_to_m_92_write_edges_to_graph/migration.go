396fc075c475ae39862c2764a31fd71a587b4362
md2119 <mandar@stackrox.com>
2022-01-26 19:50:19 -0800

    Write image cve edges to the graph

---
+++ migrator/migrations/m_91_to_m_92_write_edges_to_graph/migration.go
@@ -0,0 +1,70 @@
+package m91tom92
+
+import (
+	"github.com/pkg/errors"
+	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/migrator/migrations"
+	"github.com/stackrox/rox/migrator/migrations/dackboxhelpers"
+	"github.com/stackrox/rox/migrator/migrations/m_91_to_m_92_write_edges_to_graph/sortedkeys"
+	"github.com/stackrox/rox/migrator/migrations/rocksdbmigration"
+	"github.com/stackrox/rox/migrator/types"
+	"github.com/tecbot/gorocksdb"
+)
+
+var (
+	migration = types.Migration{
+		StartingSeqNum: 91,
+		VersionAfter:   storage.Version{SeqNum: 92},
+		Run:            writeImageCVEEdgesToGraph,
+	}
+
+	readOpts  = gorocksdb.NewDefaultReadOptions()
+	writeOpts = gorocksdb.NewDefaultWriteOptions()
+)
+
+func init() {
+	migrations.MustRegisterMigration(migration)
+}
+
+func writeImageCVEEdgesToGraph(databases *types.Databases) error {
+	it := databases.RocksDB.NewIterator(readOpts)
+	defer it.Close()
+
+	connections := make(map[string]sortedkeys.SortedKeys)
+	wb := gorocksdb.NewWriteBatch()
+	defer wb.Destroy()
+
+	for it.Seek(imageCVEEdgePrefix); it.ValidForPrefix(imageCVEEdgePrefix); it.Next() {
+		imageCVEEdgeKey := it.Key().Copy()
+		id := rocksdbmigration.GetIDFromPrefixedKey(imageCVEEdgePrefix, imageCVEEdgeKey)
+		edgeID, err := dackboxhelpers.FromString(string(id))
+		if err != nil {
+			return err
+		}
+
+		imageKey := getImageKey([]byte(edgeID.ParentID))
+		fromKey := getGraphKey(imageKey)
+		cveKey := getCVEKey([]byte(edgeID.ChildID))
+		edgesInGraph, err := databases.RocksDB.Get(readOpts, fromKey)
+		if err != nil {
+			return err
+		}
+
+		if edgesInGraph.Exists() {
+			ss, err := sortedkeys.Unmarshal(edgesInGraph.Data())
+			if err != nil {
+				return err
+			}
+			connections[string(imageKey)] = ss
+		}
+		connections[string(imageKey)], _ = connections[string(imageKey)].Insert(cveKey)
+
+		for _, tos := range connections {
+			wb.Put(fromKey, tos.Marshal())
+		}
+		if err := databases.RocksDB.Write(writeOpts, wb); err != nil {
+			return errors.Wrap(err, "writing to RocksDB")
+		}
+	}
+	return nil
+}
--- migrator/runner/all.go
+++ migrator/runner/all.go
@@ -49,4 +49,5 @@ import (
 	_ "github.com/stackrox/rox/migrator/migrations/m_88_to_m_89_update_log4shell_policy"
 	_ "github.com/stackrox/rox/migrator/migrations/m_89_to_m_90_vuln_state"
 	_ "github.com/stackrox/rox/migrator/migrations/m_90_to_m_91_snooze_permissions"
+	_ "github.com/stackrox/rox/migrator/migrations/m_91_to_m_92_write_edges_to_graph"
 )
