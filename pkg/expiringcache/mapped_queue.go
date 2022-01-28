ddceda4349f061ad5f3505db3fc6ac88b1d21976
Connor Gorman <cgorman@stackrox.com>
2022-01-27 17:55:52 -0800

    Properly flush images from sensor caches on image deletions

--- pkg/expiringcache/mapped_queue.go
+++ pkg/expiringcache/mapped_queue.go
@@ -22,6 +22,7 @@ type mappedQueue interface {
 
 	get(key interface{}) interface{}
 	getAllValues() []interface{}
+	getAllKeys() []interface{}
 }
 
 func newMappedQueue() mappedQueue {
@@ -91,6 +92,17 @@ func (mq *mappedQueueImpl) getAllValues() []interface{} {
 	ret := make([]interface{}, 0, mq.queue.Len())
 	for next := mq.queue.Front(); next != nil; next = next.Next() {
 		ret = append(ret, next.Value.(*mappedQueueElement).value)
+	}
+	return ret
+}
+
+func (mq *mappedQueueImpl) getAllKeys() []interface{} {
+	if mq.queue.Len() == 0 {
+		return nil
+	}
+	ret := make([]interface{}, 0, mq.queue.Len())
+	for next := mq.queue.Front(); next != nil; next = next.Next() {
+		ret = append(ret, next.Value.(*mappedQueueElement).key)
 	}
 	return ret
 }
