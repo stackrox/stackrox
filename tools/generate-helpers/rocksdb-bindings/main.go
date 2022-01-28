925d8d688ade215c712d14ea4ad2ec03067271fb
ivan-degtiarenko <degtiarenko@stackrox.com>
2022-01-28 18:58:24 +0100

    allocCluster is a typo

--- tools/generate-helpers/rocksdb-bindings/main.go
+++ tools/generate-helpers/rocksdb-bindings/main.go
@@ -105,7 +105,7 @@ func New(db *rocksdb.RocksDB) Store {
 	globaldb.RegisterBucket(bucket, "{{.Type}}")
 	{{- if .UniqKeyFunc}}
 	return &storeImpl{
-		crud: generic.NewUniqueKeyCRUD(db, bucket, {{if .NoKeyField}}nil{{else}}keyFunc{{end}}, allocCluster, uniqKeyFunc, {{.TrackIndex}}),
+		crud: generic.NewUniqueKeyCRUD(db, bucket, {{if .NoKeyField}}nil{{else}}keyFunc{{end}}, alloc, uniqKeyFunc, {{.TrackIndex}}),
 	}
 	{{- else}}
 	return &storeImpl{
