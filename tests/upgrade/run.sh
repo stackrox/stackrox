96086f6416dad544d3b4b06f2427f7931953182c
Gavin Jefferies <gavin@stackrox.com>
2022-01-29 04:48:39 +0000

    provide earlier roxctl

--- tests/upgrade/run.sh
+++ tests/upgrade/run.sh
@@ -371,9 +371,7 @@ test_upgrade_paths() {
 deploy_earlier_central() {
     info "Deploying: $EARLIER_TAG..."
 
-    info "Making roxctl to match earlier version for deploy scripts"
-    make cli
-
+    gsutil cp "gs://stackrox-ci/roxctl-$EARLIER_TAG" "bin/$TEST_HOST_OS/roxctl"
     PATH="bin/$TEST_HOST_OS:$PATH" MAIN_IMAGE_TAG="$EARLIER_TAG" ./deploy/k8s/central.sh
 
     get_central_basic_auth_creds
