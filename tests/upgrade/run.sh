f6a6b0e6315a00e72efea100009718449dd7839d
Gavin Jefferies <gavin@stackrox.com>
2022-01-31 18:54:37 +0000

    RS-454: remove docker requirement from upgrade test

--- tests/upgrade/run.sh
+++ tests/upgrade/run.sh
@@ -371,7 +371,12 @@ test_upgrade_paths() {
 deploy_earlier_central() {
     info "Deploying: $EARLIER_TAG..."
 
-    MAIN_IMAGE_TAG="$EARLIER_TAG" ./deploy/k8s/central.sh
+    mkdir -p "bin/$TEST_HOST_OS"
+    gsutil cp "gs://stackrox-ci/roxctl-$EARLIER_TAG" "bin/$TEST_HOST_OS/roxctl"
+    chmod +x "bin/$TEST_HOST_OS/roxctl"
+    PATH="bin/$TEST_HOST_OS:$PATH" command -v roxctl
+    PATH="bin/$TEST_HOST_OS:$PATH" roxctl version
+    PATH="bin/$TEST_HOST_OS:$PATH" MAIN_IMAGE_TAG="$EARLIER_TAG" ./deploy/k8s/central.sh
 
     get_central_basic_auth_creds
 }
