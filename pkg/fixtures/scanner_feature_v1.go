4e31f30697d3bc97f45a92fc01afdf599eebf1f0
RTann <rtann@redhat.com>
2022-02-04 12:51:51 -0800

    style

--- pkg/fixtures/scanner_feature_v1.go
+++ pkg/fixtures/scanner_feature_v1.go
@@ -5,6 +5,7 @@ import (
 	"github.com/stackrox/scanner/pkg/component"
 )
 
+// ScannerFeaturesV1 returns a slice of *v1.Feature.
 func ScannerFeaturesV1() []*v1.Feature {
 	return []*v1.Feature{
 		{
@@ -16,11 +17,11 @@ func ScannerFeaturesV1() []*v1.Feature {
 					Path: "/bin/rpm",
 					RequiredFeatures: []*v1.FeatureNameVersion{
 						{
-							Name: "glibc",
+							Name:    "glibc",
 							Version: "1",
 						},
 						{
-							Name: "lib.so",
+							Name:    "lib.so",
 							Version: "2",
 						},
 					},
@@ -28,37 +29,37 @@ func ScannerFeaturesV1() []*v1.Feature {
 			},
 			Vulnerabilities: []*v1.Vulnerability{
 				{
-					Name: "CVE-2022-1234",
+					Name:        "CVE-2022-1234",
 					Description: "This is the worst vulnerability I have ever seen",
-					Link: "https://access.redhat.com/security/cve/CVE-2022-1234",
-					Severity: "Important",
-					FixedBy: "4.16.1",
+					Link:        "https://access.redhat.com/security/cve/CVE-2022-1234",
+					Severity:    "Important",
+					FixedBy:     "4.16.1",
 					MetadataV2: &v1.Metadata{
 						CvssV2: &v1.CVSSMetadata{
-							Vector:               "AV:A/AC:M/Au:M/C:N/I:P/A:C",
-							Score:                5.4,
-							ExploitabilityScore:  3.5,
-							ImpactScore:          7.8,
+							Vector:              "AV:A/AC:M/Au:M/C:N/I:P/A:C",
+							Score:               5.4,
+							ExploitabilityScore: 3.5,
+							ImpactScore:         7.8,
 						},
 						CvssV3: &v1.CVSSMetadata{
-							Vector:               "CVSS:3.1/AV:A/AC:L/PR:L/UI:N/S:U/C:L/I:N/A:H",
-							Score:                6.3,
-							ExploitabilityScore:  2.1,
-							ImpactScore:          4.2,
+							Vector:              "CVSS:3.1/AV:A/AC:L/PR:L/UI:N/S:U/C:L/I:N/A:H",
+							Score:               6.3,
+							ExploitabilityScore: 2.1,
+							ImpactScore:         4.2,
 						},
 					},
 				},
 				{
-					Name: "CVE-2022-1235",
+					Name:        "CVE-2022-1235",
 					Description: "This is the second worst vulnerability I have ever seen",
-					Link: "https://access.redhat.com/security/cve/CVE-2022-1235",
-					Severity: "Moderate",
+					Link:        "https://access.redhat.com/security/cve/CVE-2022-1235",
+					Severity:    "Moderate",
 					MetadataV2: &v1.Metadata{
 						CvssV2: &v1.CVSSMetadata{
-							Vector:               "AV:A/AC:M/Au:M/C:N/I:P/A:C",
-							Score:                5.4,
-							ExploitabilityScore:  3.5,
-							ImpactScore:          7.8,
+							Vector:              "AV:A/AC:M/Au:M/C:N/I:P/A:C",
+							Score:               5.4,
+							ExploitabilityScore: 3.5,
+							ImpactScore:         7.8,
 						},
 					},
 				},
@@ -71,10 +72,10 @@ func ScannerFeaturesV1() []*v1.Feature {
 			AddedByLayer: "sha256:idk0",
 		},
 		{
-			Name: "java.jar",
-			Version: "1",
-			FeatureType: component.JavaSourceType.String(),
-			Location: "/java/jar/path/java.jar",
+			Name:         "java.jar",
+			Version:      "1",
+			FeatureType:  component.JavaSourceType.String(),
+			Location:     "/java/jar/path/java.jar",
 			AddedByLayer: "sha256:idk1",
 		},
 	}
