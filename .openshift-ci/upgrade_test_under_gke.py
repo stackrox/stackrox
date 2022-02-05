a440c8be7724d7137a39ef84390d679577e5a4bf
Gavin Jefferies <gavin@stackrox.com>
2022-02-05 00:16:43 +0000

    kill command after timeout

--- .openshift-ci/upgrade_test_under_gke.py
+++ .openshift-ci/upgrade_test_under_gke.py
@@ -7,14 +7,14 @@ A hook for OpenShift CI to execute an upgrade test
 import subprocess
 import sys
 
-class Constants:
-    PROVISION_TIMEOUT = 20*60
-    WAIT_TIMEOUT = 20*60
-    TEST_TIMEOUT = 60*60
-    TEARDOWN_TIMEOUT = 5*60
-    CLUSTER_ID = "upgrade-test"
 
 class UpgradeTest:
+    PROVISION_TIMEOUT = 20 * 60
+    WAIT_TIMEOUT = 20 * 60
+    TEST_TIMEOUT = 60 * 60
+    TEARDOWN_TIMEOUT = 5 * 60
+    CLUSTER_ID = "upgrade-test"
+
     def __init__(self):
         self.needs_teardown = False
         self.needs_post_analysis = False
@@ -51,36 +51,49 @@ class UpgradeTest:
         sys.exit(exitstatus)
 
     def provision(self):
-        cmd = subprocess.Popen(["scripts/ci/gke.sh", "provision_gke_cluster", Constants.CLUSTER_ID])
+        with subprocess.Popen(
+            ["scripts/ci/gke.sh", "provision_gke_cluster", UpgradeTest.CLUSTER_ID]
+        ) as cmd:
 
-        self.needs_teardown = True
-
-        if cmd.wait(Constants.PROVISION_TIMEOUT) != 0:
-            raise RuntimeError("Cluster provision failed")
+            self.needs_teardown = True
+            try:
+                exitstatus = cmd.wait(UpgradeTest.PROVISION_TIMEOUT)
+                if exitstatus != 0:
+                    raise RuntimeError(f"Cluster provision failed: exit {exitstatus}")
+            except subprocess.TimeoutExpired as err:
+                cmd.kill()
+                raise err
 
     def wait(self):
-        cmd = subprocess.Popen(["scripts/ci/gke.sh", "wait_for_cluster"])
-
-        if cmd.wait(Constants.WAIT_TIMEOUT) != 0:
-            raise RuntimeError("Wait for cluster failed")
+        subprocess.run(
+            ["scripts/ci/gke.sh", "wait_for_cluster"],
+            check=True,
+            timeout=UpgradeTest.WAIT_TIMEOUT,
+        )
 
     def run_test(self):
-        cmd = subprocess.Popen(["tests/upgrade/run.sh"])
+        with subprocess.Popen(["tests/upgrade/run.sh"]) as cmd:
 
-        self.needs_post_analysis = True
-
-        if cmd.wait(Constants.TEST_TIMEOUT) != 0:
-            raise RuntimeError("Test failed")
+            self.needs_post_analysis = True
+            try:
+                exitstatus = cmd.wait(UpgradeTest.TEST_TIMEOUT)
+                if exitstatus != 0:
+                    raise RuntimeError(f"Test failed: exit {exitstatus}")
+            except subprocess.TimeoutExpired as err:
+                cmd.kill()
+                raise err
 
     def post_test_analysis(self):
         print("The future home for debug gathering and analysis")
         return 0
 
     def teardown(self):
-        cmd = subprocess.Popen(["scripts/ci/gke.sh", "teardown_gke_cluster"])
+        subprocess.run(
+            ["scripts/ci/gke.sh", "teardown_gke_cluster"],
+            check=True,
+            timeout=UpgradeTest.TEARDOWN_TIMEOUT,
+        )
 
-        if cmd.wait(Constants.TEARDOWN_TIMEOUT) != 0:
-            raise RuntimeError("Teardown failed")
 
 if __name__ == "__main__":
     UpgradeTest().run()
