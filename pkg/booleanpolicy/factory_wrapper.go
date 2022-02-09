00ce9b3dc6c85918f45f2750b29ab70abdbca31b
Viswajith Venugopal <viswajithiii@gmail.com>
2022-02-09 14:45:10 -0800

    Add some logging

--- pkg/booleanpolicy/factory_wrapper.go
+++ pkg/booleanpolicy/factory_wrapper.go
@@ -2,6 +2,9 @@ package booleanpolicy
 
 import (
 	"errors"
+	"math/rand"
+	"reflect"
+	"time"
 
 	"github.com/stackrox/rox/pkg/booleanpolicy/evaluator"
 	"github.com/stackrox/rox/pkg/booleanpolicy/evaluator/pathutil"
@@ -15,17 +18,52 @@ type factoryWrapper struct {
 	opaBasedFactory regocompile.RegoCompiler
 }
 
+type evaluatorWrapper struct {
+	regoEvaluator   evaluator.Evaluator
+	legacyEvaluator evaluator.Evaluator
+
+	q *query.Query
+}
+
+func (e *evaluatorWrapper) Evaluate(obj *pathutil.AugmentedObj) (*evaluator.Result, bool) {
+	if e.regoEvaluator == nil {
+		return e.legacyEvaluator.Evaluate(obj)
+	}
+	start := time.Now()
+	regoResult, regoMatched := e.regoEvaluator.Evaluate(obj)
+	regoDone := time.Now()
+	legacyResult, legacyMatched := e.legacyEvaluator.Evaluate(obj)
+	legacyDone := time.Now()
+	if rand.Intn(100) < 1 {
+		log.Infof("Rego took %s; legacy took %s", regoDone.Sub(start), legacyDone.Sub(regoDone))
+	}
+	if regoMatched != legacyMatched || !reflect.DeepEqual(regoResult, legacyResult) {
+		objValue, _ := obj.GetFullValue()
+		log.Infof("Rego took %s; legacy took %s", regoDone.Sub(start), legacyDone.Sub(regoDone))
+		log.Errorf("Got different values for OPA and legacy. OPA matched: %v; legacy matched: %v;"+
+			" opa result %+v; legacy result: %+v; query was %+v, obj is %+v", regoMatched, legacyMatched, regoResult, legacyResult, e.q, objValue)
+	}
+	return legacyResult, legacyMatched
+}
+
 func (f *factoryWrapper) GenerateEvaluator(q *query.Query) (evaluator.Evaluator, error) {
+	e := &evaluatorWrapper{q: q}
 	if features.OPABasedEvaluator.Enabled() {
-		eval, err := f.opaBasedFactory.CompileRegoBasedEvaluator(q)
-		if err == nil {
-			return eval, nil
-		}
-		if !errors.Is(err, regocompile.ErrRegoNotYetSupported) {
-			return nil, err
+		regoEvaluator, err := f.opaBasedFactory.CompileRegoBasedEvaluator(q)
+		if err != nil {
+			if !errors.Is(err, regocompile.ErrRegoNotYetSupported) {
+				return nil, err
+			}
+		} else {
+			e.regoEvaluator = regoEvaluator
 		}
 	}
-	return f.legacyFactory.GenerateEvaluator(q)
+	legacyEvaluator, err := f.legacyFactory.GenerateEvaluator(q)
+	if err != nil {
+		return nil, err
+	}
+	e.legacyEvaluator = legacyEvaluator
+	return e, nil
 }
 
 // MustCreateFactoryWrapper returns a factory wrapper.
