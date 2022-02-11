70d0a8d5715b8ec44005fce7ddfb541789971ca2
Alex Rukletsov <alexr@stackrox.com>
2022-02-11 13:42:52 +0100

    Provide comments to common sentinel errors.

--- pkg/errox/errors.go
+++ pkg/errox/errors.go
@@ -10,16 +10,46 @@ import (
 // in some cases, e.g., when embedded into a GraphQL response, thus changing
 // them might break error matching for some clients, e.g., the UI.
 var (
-	AlreadyExists             = makeSentinel("already exists")
-	InvalidArgs               = makeSentinel("invalid arguments")
-	NotFound                  = makeSentinel("not found")
+	NotFound = makeSentinel("not found")
+
+	// AlreadyExists indicates that a request is syntactically correct but can't
+	// be fulfilled due to a conflict with existing data. Such request could
+	// have succeeded given a different system state.
+	AlreadyExists = makeSentinel("already exists")
+
+	// InvalidArgs indicates bad user request. Such request will always fail
+	// regardless of the system state.
+	InvalidArgs = makeSentinel("invalid arguments")
+
+	// ReferencedByAnotherObject indicates that the requested object cannot be
+	// removed or updated because it is referred to / in use by another object.
+	// Future system state might allow the request.
 	ReferencedByAnotherObject = makeSentinel("referenced by another object")
-	InvariantViolation        = makeSentinel("invariant violation")
-	NoCredentials             = makeSentinel("credentials not found")
-	NoValidRole               = makeSentinel("no valid role")
-	NotAuthorized             = makeSentinel("not authorized")
-	NoAuthzConfigured         = makeSentinel("service authorization is misconfigured")
-	// When adding a new error please update the translators (gRPC, HTTP etc.).
+
+	// InvariantViolation indicates that some internal invariant has been
+	// violated and the underlying system component is in an inconsistent state.
+	// *Must not* be used for invalid user input.
+	InvariantViolation = makeSentinel("invariant violation")
+
+	// NoCredentials indicates that valid credentials are required but have not
+	// been provided. The request can not be processed.
+	NoCredentials = makeSentinel("credentials not found")
+
+	// NoValidRole indicates that though user credentials have been provided,
+	// they do not specify a valid role. This usually happens because of
+	// misconfigured access control. The effect is similar to NoCredentials.
+	NoValidRole = makeSentinel("no valid role")
+
+	// NotAuthorized indicates that valid credentials have been provided but are
+	// insufficient for processing the request.
+	NotAuthorized = makeSentinel("not authorized")
+
+	// NoAuthzConfigured indicates that authorization is not implemented for a
+	// service. This is a programming error.
+	NoAuthzConfigured = makeSentinel("service authorization is misconfigured")
+
+
+	// When adding a new error please update the translators in this package (gRPC, etc.).
 )
 
 // GenericNoValidRole wraps ErrNoValidRole with a generic error message.
