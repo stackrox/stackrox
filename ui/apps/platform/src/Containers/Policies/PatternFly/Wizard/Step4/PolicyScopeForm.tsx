d9a90b58671aad6c4ccc976486ae0543dbf14bcb
Mark Pedrotti <mark@stackrox.com>
2022-02-11 14:42:36 -0500

    Make requested change to sentence for Exclude by scope

--- ui/apps/platform/src/Containers/Policies/PatternFly/Wizard/Step4/PolicyScopeForm.tsx
+++ ui/apps/platform/src/Containers/Policies/PatternFly/Wizard/Step4/PolicyScopeForm.tsx
@@ -150,12 +150,11 @@ function PolicyScopeForm() {
                     <FlexItem flex={{ default: 'flex_1' }}>
                         <Title headingLevel="h3">Exclude by scope</Title>
                         <div className="pf-u-mt-sm">
-                            Use Exclude by scope to exclude entities from your policy. It only
-                            applies to check running deployments (the Deploy lifecycle stage) or
-                            runtime activities (the Run lifecycle stage). You can add multiple
-                            scopes and also use regular expressions (RE2 syntax) for namespaces and
-                            labels. However, you can&apos;t use regular expressions for selecting
-                            deployments.
+                            Use Exclude by scope to exclude entities from your policy. This function
+                            is only available for Deploy and Runtime lifecycle stages. You can add
+                            multiple scopes and also use regular expressions (RE2 syntax) fo
+                            namespaces and labels. However, you can&apos;t use regular expressions
+                            for selecting deployments.
                         </div>
                     </FlexItem>
                     <FlexItem className="pf-u-pr-md" alignSelf={{ default: 'alignSelfCenter' }}>
