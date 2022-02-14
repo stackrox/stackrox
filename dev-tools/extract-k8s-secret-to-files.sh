43da7b33dbd923215acbc611b4d0d24e86d877ae
Simon Baeumer <sbaumer@redhat.com>
2022-02-14 18:07:12 +0100

    Add extract secret to files script

---
+++ dev-tools/extract-k8s-secret-to-files.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+# Extract secret data entries into files on your local filesystem.
+# Usage: ./extract-k8s-secrets-to-files.sh [namespace] [secret name]
+namespace="$1"
+secret_name="$2"
+
+if [[ -z "$namespace" ]]; then
+  echo "namespace required"
+  exit 1
+fi
+if [[ -z "$secret_name" ]]; then
+  echo "secret name required"
+  exit 1
+fi
+
+certs=$(kubectl -n "$namespace" get secrets "$secret_name" -o json | jq -r '.data | keys[]')
+
+for item in $(echo "$certs" | tr "\n" "\t");
+do
+  echo "Written data to $item"
+  content=$(kubectl -n "$namespace" get secrets "$secret_name" -o json | jq -r --arg key "$item" '.data[$key]')
+  echo "$content" | base64 --decode > "$item"
+done
