6069e31e8a3ee2a5c4d26c8a452747d928524bd3
Simon Baeumer <sbaumer@redhat.com>
2022-02-17 12:18:34 +0100

    Namespace optional

--- dev-tools/extract-k8s-secret-to-files.sh
+++ dev-tools/extract-k8s-secret-to-files.sh
@@ -1,22 +1,29 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-# Extract secret data entries into files on your local filesystem.
-# Usage: ./extract-k8s-secrets-to-files.sh [namespace] [secret name]
-namespace="$1"
-secret_name="$2"
+# Extract secret data entries into files on your local filesystem. Uses the default kubectl configuration.
+# Usage: ./extract-k8s-secrets-to-files.sh [secret name] <namespace>
+secret_name="$1"
+namespace="$2"
 
-if [[ "$#" -ne 2 ]]; then
-    echo "Expected 2 args, but found $#"
-    echo "Usage: $0 [namespace] [secret name]"
+if [[ "$#" -lt 1 ]]; then
+    echo "Expected at least 1 arg, but found $#"
+    echo "Usage: $0 [secret name] <namespace>"
     exit 1
 fi
 
-certs=$(kubectl -n "$namespace" get secrets "$secret_name" -o json | jq -r '.data | keys[]')
+kubectl_args=()
+if [[ -n "$namespace" ]]; then
+  kubectl_args+=(
+    -n "$namespace"
+  )
+fi
+
+certs=$(kubectl "${kubectl_args[@]}" get secrets "$secret_name" -o json | jq -r '.data | keys[]')
 
 for item in $(echo "$certs" | tr "\n" "\t");
 do
-  content=$(kubectl -n "$namespace" get secrets "$secret_name" -o json | jq -r --arg key "$item" '.data[$key]')
+  content=$(kubectl "${kubectl_args[@]}" get secrets "$secret_name" -o json | jq -r --arg key "$item" '.data[$key]')
   echo "$content" | base64 --decode > "$item"
   echo "Written data to $item"
 done
