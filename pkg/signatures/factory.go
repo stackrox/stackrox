c8ecea6b9a751ee9a3586c3a650cf08eb1ad3e5f
ivan-degtiarenko <degtiarenko@stackrox.com>
2022-02-21 16:53:21 +0100

    Fix usages of newPublicKeyVerifier

--- pkg/signatures/factory.go
+++ pkg/signatures/factory.go
@@ -19,7 +19,7 @@ type SignatureVerifier interface {
 func NewSignatureVerifier(config *storage.SignatureVerificationConfig) (SignatureVerifier, error) {
 	switch cfg := config.GetConfig().(type) {
 	case *storage.SignatureVerificationConfig_CosignVerification:
-		return newCosignPublicKeyVerifier(cfg.CosignVerification)
+		return newPublicKeyVerifier(cfg.CosignVerification)
 	default:
 		// Should theoretically never happen.
 		return nil, errox.Newf(errox.InvariantViolation,
