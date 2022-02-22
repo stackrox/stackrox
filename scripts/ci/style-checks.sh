8afedd927e931e1c95a2766c9a3c8cafbbbcb9e6
Gavin Jefferies <gavin@stackrox.com>
2022-02-22 22:55:27 +0000

    access

---
+++ scripts/ci/style-checks.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Run style checks
+
+SCRIPTS_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../.. && pwd)"
+
+source "$SCRIPTS_ROOT/scripts/lib.sh"
+
+style_checks() {
+    if is_GITHUB_ACTIONS; then
+        require_environment "GITHUB_TOKEN"
+        git config --global "url.https://${GITHUB_TOKEN}@github.com/.insteadOf" https://github.com/
+    fi
+
+    make style
+}
+
+if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
+    style_checks "$*"
+fi
