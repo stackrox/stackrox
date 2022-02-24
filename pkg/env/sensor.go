490582a7e9c3df5ec28eae4fe52a3e4cb091891c
RTann <rtann@redhat.com>
2022-02-23 19:00:27 -0800

    Revert "ROX-9280: Scan image with local Scanner (#517)"
    This reverts commit 24d73248e139781f390e77088219cc00a8cd11dc.

--- pkg/env/sensor.go
+++ pkg/env/sensor.go
@@ -11,11 +11,4 @@ var (
 
 	// SensorEndpoint is used to communicate the sensor endpoint to other services in the same cluster.
 	SensorEndpoint = RegisterSetting("ROX_SENSOR_ENDPOINT", WithDefault("sensor.stackrox.svc:443"))
-
-	// ScannerGRPCEndpoint is used to communicate the scanner endpoint to other services in the same cluster.
-	// This is typically used for Sensor to communicate with a local Scanner-slim's gRPC server.
-	ScannerGRPCEndpoint = RegisterSetting("ROX_SCANNER_GRPC_ENDPOINT", WithDefault("scanner-slim.stackrox.svc:8443"))
-
-	// UseLocalScanner is used to specify if Sensor should attempt to scan images via a local Scanner.
-	UseLocalScanner = RegisterBooleanSetting("ROX_USE_LOCAL_SCANNER", false)
 )
