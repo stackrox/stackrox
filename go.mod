64f1f53ccf2a7424c7850e9435eb5208eb2f100d
Ross <rtann@redhat.com>
2022-02-24 08:58:08 -0800

    Revert "Revert "ROX-9280: Scan image with local Scanner (#517)" (#732)" (#733)

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.16
 
 // CAVEAT: This introduces a circular dependency. If you change this line, you MUST change the "exclude"
 // directive at the bottom of the file as well.
-require github.com/stackrox/scanner v0.0.0-20220106020903-2744339f7e9d
+require github.com/stackrox/scanner v0.0.0-20220214215744-13c0e1db0298
 
 require (
 	cloud.google.com/go/compute v1.3.0
---
+++ sensor/common/scannerclient/grpc_client.go
@@ -0,0 +1,88 @@
+package scannerclient
+
+import (
+	"context"
+	"strings"
+
+	"github.com/pkg/errors"
+	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/clientconn"
+	"github.com/stackrox/rox/pkg/images/utils"
+	"github.com/stackrox/rox/pkg/logging"
+	"github.com/stackrox/rox/pkg/mtls"
+	"github.com/stackrox/rox/pkg/registries/types"
+	scannerV1 "github.com/stackrox/scanner/generated/scanner/api/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+)
+
+var (
+	log = logging.LoggerForModule()
+)
+
+// client is a Scanner gRPC client.
+type client struct {
+	client scannerV1.ImageScanServiceClient
+	conn   *grpc.ClientConn
+}
+
+// dial Scanner and return a new client.
+// dial is non-blocking and returns a non-nil error upon configuration error.
+func dial(endpoint string) (*client, error) {
+	if endpoint == "" {
+		return nil, errors.New("Invalid Scanner endpoint (empty)")
+	}
+
+	endpoint = strings.TrimPrefix(endpoint, "https://")
+	if strings.Contains(endpoint, "://") {
+		return nil, errors.Errorf("Scanner endpoint has unsupported scheme: %s", endpoint)
+	}
+
+	tlsConfig, err := clientconn.TLSConfig(mtls.ScannerSubject, clientconn.TLSConfigOptions{
+		UseClientCert: clientconn.MustUseClientCert,
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to initialize Scanner TLS config")
+	}
+
+	// This is non-blocking. If we ever want this to block,
+	// then add the grpc.WithBlock() DialOption.
+	conn, err := grpc.Dial(endpoint, grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig)))
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to dial Scanner")
+	}
+
+	log.Infof("Dialing Scanner at %s", endpoint)
+
+	return &client{
+		client: scannerV1.NewImageScanServiceClient(conn),
+		conn:   conn,
+	}, nil
+}
+
+// GetImageAnalysis retrieves the image analysis results for the given image.
+func (c *client) GetImageAnalysis(ctx context.Context, image *storage.Image, cfg *types.Config) (*scannerV1.GetImageComponentsResponse, error) {
+	name := image.GetName().GetFullName()
+
+	resp, err := c.client.GetImageComponents(ctx, &scannerV1.GetImageComponentsRequest{
+		Image: utils.GetFullyQualifiedFullName(image),
+		Registry: &scannerV1.RegistryData{
+			Url:      cfg.URL,
+			Username: cfg.Username,
+			Password: cfg.Password,
+			Insecure: cfg.Insecure,
+		},
+	})
+	if err != nil {
+		log.Debugf("Unable to get image components from local Scanner for image %s: %v", name, err)
+		return nil, errors.Wrap(err, "getting image components from scanner")
+	}
+
+	log.Debugf("Received image components from local Scanner for image %s", name)
+
+	return resp, nil
+}
+
+func (c *client) Close() error {
+	return c.conn.Close()
+}
