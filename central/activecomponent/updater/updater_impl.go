c9f14191c828dc337b186c4e2d53016545e8c5a6
Tomasz Janiszewski <janiszt@gmail.com>
2022-02-25 11:34:12 +0100

    Use context todo instad of all access

--- central/activecomponent/updater/updater_impl.go
+++ central/activecomponent/updater/updater_impl.go
@@ -101,8 +101,7 @@ func (u *updaterImpl) Update() {
 		return
 	}
 	if len(u.deploymentToUpdates) == 0 {
-		ctx := sac.WithAllAccess(context.Background())
-		ids, err := u.deploymentStore.GetDeploymentIDs(ctx)
+		ids, err := u.deploymentStore.GetDeploymentIDs(context.TODO())
 		if err != nil {
 			log.Errorf("failed to fetch deployment ids: %v", err)
 			return
