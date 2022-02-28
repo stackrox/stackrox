664a0dab5aa23ad66e0c6d95d0c4f773a02bb752
Connor Gorman <connorgorman@users.noreply.github.com>
2022-02-28 07:59:11 -0800

    Initial pass at autogenerating the index interface for Postgres (#701)

---
+++ tools/generate-helpers/pg-table-bindings/index.go.tpl
@@ -0,0 +1,71 @@
+
+package postgres
+
+import (
+    "reflect"
+	"time"
+
+	mappings "{{.OptionsPath}}"
+	metrics "github.com/stackrox/rox/central/metrics"
+	v1 "github.com/stackrox/rox/generated/api/v1"
+	storage "github.com/stackrox/rox/generated/storage"
+	ops "github.com/stackrox/rox/pkg/metrics"
+	search "github.com/stackrox/rox/pkg/search"
+	"github.com/stackrox/rox/pkg/search/postgres/mapping"
+	"github.com/stackrox/rox/pkg/search/postgres"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/stackrox/rox/pkg/search/blevesearch"
+	"github.com/stackrox/rox/pkg/postgres/walker"
+)
+
+func init() {
+	mapping.RegisterCategoryToTable(v1.{{.SearchCategory}}, walker.Walk(reflect.TypeOf((*{{.Type}})(nil)), "{{.Table}}"))
+}
+
+func NewIndexer(db *pgxpool.Pool) *indexerImpl {
+	return &indexerImpl {
+		db: db,
+	}
+}
+
+type indexerImpl struct {
+	db *pgxpool.Pool
+}
+
+func (b *indexerImpl) Count(q *v1.Query, opts ...blevesearch.SearchOption) (int, error) {
+	defer metrics.SetIndexOperationDurationTime(time.Now(), ops.Count, "{{.TrimmedType}}")
+
+	return postgres.RunCountRequest(v1.{{.SearchCategory}}, q, b.db, mappings.OptionsMap)
+}
+
+func (b *indexerImpl) Search(q *v1.Query, opts ...blevesearch.SearchOption) ([]search.Result, error) {
+	defer metrics.SetIndexOperationDurationTime(time.Now(), ops.Search, "{{.TrimmedType}}")
+
+	return postgres.RunSearchRequest(v1.{{.SearchCategory}}, q, b.db, mappings.OptionsMap)
+}
+
+//// Stubs for satisfying interfaces
+
+func (b *indexerImpl) Add{{.TrimmedType}}(deployment *{{.Type}}) error {
+	return nil
+}
+
+func (b *indexerImpl) Add{{.TrimmedType}}s(_ []*{{.Type}}) error {
+	return nil
+}
+
+func (b *indexerImpl) Delete{{.TrimmedType}}(id string) error {
+	return nil
+}
+
+func (b *indexerImpl) Delete{{.TrimmedType}}s(_ []string) error {
+	return nil
+}
+
+func (b *indexerImpl) MarkInitialIndexingComplete() error {
+	return nil
+}
+
+func (b *indexerImpl) NeedsInitialIndexing() (bool, error) {
+	return false, nil
+}
--- tools/generate-helpers/pg-table-bindings/test/postgres/codegen_test.go
+++ tools/generate-helpers/pg-table-bindings/test/postgres/codegen_test.go
@@ -6,10 +6,39 @@ import (
 	"context"
 	"testing"
 
+	"github.com/gogo/protobuf/types"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/pkg/postgres/pgtest"
+	"github.com/stackrox/rox/pkg/search"
 	"github.com/stretchr/testify/assert"
+)
+
+var (
+	singleKey = &storage.TestSingleKeyStruct{
+		Key: "key1",
+		Name: "name",
+		StringSlice: []string {
+			"slice1", "slice2",
+		},
+		Bool: true,
+		Uint64: 16,
+		Int64: 32,
+		Float: 4.56,
+		Labels: map[string]string {
+			"key1": "value1",
+			"key2": "value2",
+		},
+		Timestamp: &types.Timestamp{
+			Seconds:              1645640515,
+			Nanos:                0,
+		},
+		Enum: storage.TestSingleKeyStruct_ENUM1,
+		Enums: []storage.TestSingleKeyStruct_Enum {
+			storage.TestSingleKeyStruct_ENUM1,
+			storage.TestSingleKeyStruct_ENUM2,
+		},
+	}
 )
 
 func TestStore(t *testing.T) {
@@ -27,17 +56,122 @@ func TestStore(t *testing.T) {
 	Destroy(pool)
 	store := New(pool)
 
-	singleKey := &storage.TestSingleKeyStruct{
-		Key: "key1",
-	}
-	dep, exists, err := store.Get(singleKey.GetKey())
+	testStruct := singleKey.Clone()
+	dep, exists, err := store.Get(testStruct.GetKey())
 	assert.NoError(t, err)
 	assert.False(t, exists)
 	assert.Nil(t, dep)
 
-	assert.NoError(t, store.Upsert(singleKey))
-	dep, exists, err = store.Get(singleKey.GetKey())
+	assert.NoError(t, store.Upsert(testStruct))
+	dep, exists, err = store.Get(testStruct.GetKey())
 	assert.NoError(t, err)
 	assert.True(t, exists)
-	assert.Equal(t, singleKey, dep)
+	assert.Equal(t, testStruct, dep)
+}
+
+func TestIndex(t *testing.T) {
+	source := pgtest.GetConnectionString(t)
+	config, err := pgxpool.ParseConfig(source)
+	if err != nil {
+		panic(err)
+	}
+	pool, err := pgxpool.ConnectConfig(context.Background(), config)
+	if err != nil {
+		panic(err)
+	}
+	defer pool.Close()
+
+	Destroy(pool)
+	store := New(pool)
+
+	testStruct := singleKey.Clone()
+	assert.NoError(t, store.Upsert(testStruct))
+
+	cases := []struct {
+		name string
+		queryBuilder *search.QueryBuilder
+		numResults int
+	} {
+		{
+			name: "basic string match",
+			queryBuilder: search.NewQueryBuilder().AddExactMatches(search.TestKey, testStruct.GetKey()),
+			numResults: 1,
+		},
+		{
+			name: "basic string no match",
+			queryBuilder: search.NewQueryBuilder().AddExactMatches(search.TestKey, "nomatch"),
+			numResults: 0,
+		},
+
+		// TODO
+		//{
+		//	name: "basic string slice match",
+		//	queryBuilder: search.NewQueryBuilder().AddExactMatches(search.TestStringSlice, testStruct.StringSlice[1]),
+		//	numResults: 1,
+		//},
+		//{
+		//	name: "basic string slice no match",
+		//	queryBuilder: search.NewQueryBuilder().AddExactMatches(search.TestStringSlice, "nomatch"),
+		//	numResults: 0,
+		//},
+
+		// TODO
+		//{
+		//	name: "basic bool match",
+		//	queryBuilder: search.NewQueryBuilder().AddExactMatches(search.TestStringSlice, testStruct.StringSlice[1]),
+		//	numResults: 1,
+		//},
+		//{
+		//	name: "basic bool no match",
+		//	queryBuilder: search.NewQueryBuilder().AddExactMatches(search.TestStringSlice, "nomatch"),
+		//	numResults: 0,
+		//},
+
+		// TODO
+		//{
+		//	name: "basic bool match",
+		//	queryBuilder: search.NewQueryBuilder().AddExactMatches(search.TestStringSlice, testStruct.StringSlice[1]),
+		//	numResults: 1,
+		//},
+		//{
+		//	name: "basic bool no match",
+		//	queryBuilder: search.NewQueryBuilder().AddExactMatches(search.TestStringSlice, "nomatch"),
+		//	numResults: 0,
+		//},
+
+		/*
+		singleKey = &storage.TestSingleKeyStruct{
+				Key: "key1",
+				Name: "name",
+				StringSlice: []string {
+					"slice1", "slice2",
+				},
+				Bool: true,
+				Uint64: 16,
+				Int64: 32,
+				Float: 4.56,
+				Labels: map[string]string {
+					"key1": "value1",
+					"key2": "value2",
+				},
+				Timestamp: &types.Timestamp{
+					Seconds:              1645640515,
+					Nanos:                0,
+				},
+				Enum: storage.TestSingleKeyStruct_ENUM1,
+				Enums: []storage.TestSingleKeyStruct_Enum {
+					storage.TestSingleKeyStruct_ENUM1,
+					storage.TestSingleKeyStruct_ENUM2,
+				},
+			}
+		 */
+	}
+	index := NewIndexer(pool)
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			results, err := index.Search(c.queryBuilder.ProtoQuery())
+			assert.NoError(t, err)
+			assert.Len(t, results, c.numResults)
+		})
+	}
 }
