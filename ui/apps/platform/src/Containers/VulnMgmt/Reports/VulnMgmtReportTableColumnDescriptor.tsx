828362935dd3ed666e4e2b2d91a1d648a4058d22
Linda Song <ls@stackrox.com>
2022-02-28 15:19:51 -0800

    fixing typo

--- ui/apps/platform/src/Containers/VulnMgmt/Reports/VulnMgmtReportTableColumnDescriptor.tsx
+++ ui/apps/platform/src/Containers/VulnMgmt/Reports/VulnMgmtReportTableColumnDescriptor.tsx
@@ -58,6 +58,7 @@ const VulnMgmtReportTableColumnDescriptor = [
                                     ) : (
                                         ''
                                     )}
+                                    )
                                 </div>
                             </div>
                         }
