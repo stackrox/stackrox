4c6f42eef2f4865a1368283a7852578ec5550da5
Frederico Bittencourt <fbittenc@redhat.com>
2022-03-02 11:17:05 +0100

    exercise comments: move legacy determination to central_env

--- roxctl/sensor/util/central_env.go
+++ roxctl/sensor/util/central_env.go
@@ -2,8 +2,11 @@ package util
 
 import (
 	"context"
+	"fmt"
 
 	v1 "github.com/stackrox/rox/generated/api/v1"
+	"github.com/stackrox/rox/pkg/buildinfo"
+	"github.com/stackrox/rox/pkg/images/defaults"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -12,14 +15,21 @@ const (
 	warningKernelSupportAvailableUnimplemented = `Central does not support API for checking if kernel support is available. Not using a slim collector image.
 Please upgrade Central if slim collector images shall be used.`
 	warningFailedAToGetKernelSupportAvailable = "Failed to retrieve KernelSupportAvailable property from Central"
-	warningCantDetermineMainImageFromCentral  = "Can't rely on central configuration to determine default values. Using %s as main registry."
+	warningLegacyCentralDefaultMain           = "Central is running on a legacy version, main will be defaulted to %s if no override is provided"
 )
 
 // CentralEnv contains information about Central's runtime environment.
 type CentralEnv struct {
+	// KernelSupportAvailable will be fetched from ClusterDefaults API. If talking to a legacy central, the data will
+	// be fetched from legacy API GetKernelSupportAvailable. If both calls fail, false is returned and the error is
+	// stored in Error property.
 	KernelSupportAvailable bool
-	Warnings               []string
-	Error                  error
+	// MainImage will only be set if central most likely won't accept an empty main image. In this case roxctl
+	// will try to derive the default MainImage locally by checking the release flag.
+	// If connecting to a newer (>= 3.69) version of central MainImage will be empty.
+	MainImage string
+	Warnings  []string
+	Error     error
 }
 
 func (e *CentralEnv) fetchClusterDefaults(ctx context.Context, service v1.ClustersServiceClient) error {
@@ -29,6 +39,16 @@ func (e *CentralEnv) fetchClusterDefaults(ctx context.Context, service v1.Cluste
 	}
 	e.KernelSupportAvailable = clusterDefault.GetKernelSupportAvailable()
 	return nil
+}
+
+func (e *CentralEnv) getMainImageFromFlavor() string {
+	var flavor defaults.ImageFlavor
+	if buildinfo.ReleaseBuild {
+		flavor = defaults.RHACSReleaseImageFlavor()
+	} else {
+		flavor = defaults.DevelopmentBuildImageFlavor()
+	}
+	return flavor.MainImageNoTag()
 }
 
 func (e *CentralEnv) fetchClusterDefaultsLegacy(ctx context.Context, service v1.ClustersServiceClient) {
@@ -45,6 +65,12 @@ func (e *CentralEnv) fetchClusterDefaultsLegacy(ctx context.Context, service v1.
 	} else {
 		e.KernelSupportAvailable = resp.KernelSupportAvailable
 	}
+
+	// MainImage is only set if roxctl is communicating with a legacy version of central that most likely won't
+	// be able to accept empty main image in PostCluster request.
+	mainImage := e.getMainImageFromFlavor()
+	e.Warnings = append(e.Warnings, fmt.Sprintf(warningLegacyCentralDefaultMain, mainImage))
+	e.MainImage = mainImage
 }
 
 func (e *CentralEnv) populateCentralEnv(ctx context.Context, service v1.ClustersServiceClient) {
@@ -55,7 +81,9 @@ func (e *CentralEnv) populateCentralEnv(ctx context.Context, service v1.Clusters
 }
 
 // RetrieveCentralEnvOrDefault is a convenience function wrapping `PopulateCentralEnv`. It populates a fresh `CentralEnv`
-// struct with defaults and warnings. Warnings are to be used by the caller to properly display them.
+// struct with defaults and warnings. This function fallbacks to legacy APIs if the defaults are not available.
+// Ultimately, it will default values locally when it can't determine configuration from central.
+// Warnings are to be used by the caller to properly display them.
 // If there is an error fetching defaults from the API the error will be returned in `CentralEnv`.
 func RetrieveCentralEnvOrDefault(ctx context.Context, service v1.ClustersServiceClient) CentralEnv {
 	env := CentralEnv{}
