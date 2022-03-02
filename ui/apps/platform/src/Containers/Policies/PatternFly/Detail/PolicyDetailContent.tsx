5d30032ee3af3145fa4ea69bda1521dd6822b663
David Vail <dcvail17@gmail.com>
2022-03-02 08:22:37 -0500

    Update layout of Violations table and detail pages. (#757)

--- ui/apps/platform/src/Containers/Policies/PatternFly/Detail/PolicyDetailContent.tsx
+++ ui/apps/platform/src/Containers/Policies/PatternFly/Detail/PolicyDetailContent.tsx
@@ -49,7 +49,7 @@ function PolicyDetailContent({
         <div data-testid="policy-details">
             <Flex direction={{ default: 'column' }}>
                 <PolicyOverview policy={policy} notifiers={notifiers} isReview={isReview} />
-                <Title headingLevel="h2" className="pf-u-mb-md pf-u-pt-lg">
+                <Title headingLevel="h3" className="pf-u-mb-md pf-u-pt-lg">
                     Policy behavior
                 </Title>
                 <Divider component="div" className="pf-u-mb-md" />
@@ -61,7 +61,7 @@ function PolicyDetailContent({
                 <Formik initialValues={policy} onSubmit={() => {}}>
                     {() => (
                         <>
-                            <Title headingLevel="h2" className="pf-u-mb-md pf-u-pt-lg">
+                            <Title headingLevel="h3" className="pf-u-mb-md pf-u-pt-lg">
                                 Policy criteria
                             </Title>
                             <Divider component="div" />
@@ -74,7 +74,7 @@ function PolicyDetailContent({
                 </Formik>
                 {(scope?.length > 0 || exclusions?.length > 0) && (
                     <>
-                        <Title headingLevel="h2" className="pf-u-mb-md pf-u-pt-lg">
+                        <Title headingLevel="h3" className="pf-u-mb-md pf-u-pt-lg">
                             Policy scope
                         </Title>
                         <Divider component="div" />
