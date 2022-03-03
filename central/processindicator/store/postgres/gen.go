84fc6b131d1044ec492563a4498418df4bb6d532
Dave Shrewsberry <dashrews@redhat.com>
2022-03-03 09:01:39 -0500

    removing todo now that I found options

--- central/processindicator/store/postgres/gen.go
+++ central/processindicator/store/postgres/gen.go
@@ -1,4 +1,3 @@
 package postgres
 
-//Todo, do we need something like --options-path central/alert/mappings
 //go:generate pg-table-bindings-wrapper --type=storage.ProcessIndicator --table=process_indicators --search-category PROCESS_INDICATORS --options-path pkg/search/options/processindicators
