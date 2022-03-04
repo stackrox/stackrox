8222dd672f7de67ba06b50c593ee7b3b8a14ed7c
David Vail <dcvail17@gmail.com>
2022-03-04 08:52:22 -0500

    Remove default branding on Login page.

--- ui/apps/platform/src/Components/LoadingSection.js
+++ ui/apps/platform/src/Components/LoadingSection.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { ClipLoader } from 'react-spinners';
+import { Flex } from '@patternfly/react-core';
 
 const LoadingSection = ({ message }) => (
-    <div className="flex flex-col items-center justify-center h-full">
+    <Flex className="pf-u-flex-direction-column pf-u-h-100 pf-u-justify-content-center pf-u-align-items-center">
         <ClipLoader color="white" loading size={20} />
-        <div className="mt-4">{message}</div>
-    </div>
+        <div className="pf-u-mt-sm pf-u-color-light-100">{message}</div>
+    </Flex>
 );
 
 LoadingSection.propTypes = {
