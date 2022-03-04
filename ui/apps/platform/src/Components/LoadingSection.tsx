e05621c262edd63b05b421e7124e7d62f957355f
David Vail <dcvail17@gmail.com>
2022-03-04 08:53:30 -0500

    Convert LoadingSection to TS

--- ui/apps/platform/src/Components/LoadingSection.tsx
+++ ui/apps/platform/src/Components/LoadingSection.tsx
@@ -1,21 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { ClipLoader } from 'react-spinners';
 import { Flex } from '@patternfly/react-core';
 
-const LoadingSection = ({ message }) => (
+interface LoadingSectionProps {
+    message: string;
+}
+
+const LoadingSection = ({ message = 'Loading...' }: LoadingSectionProps) => (
     <Flex className="pf-u-flex-direction-column pf-u-h-100 pf-u-justify-content-center pf-u-align-items-center">
         <ClipLoader color="white" loading size={20} />
         <div className="pf-u-mt-sm pf-u-color-light-100">{message}</div>
     </Flex>
 );
-
-LoadingSection.propTypes = {
-    message: PropTypes.string,
-};
-
-LoadingSection.defaultProps = {
-    message: 'Loading...',
-};
 
 export default LoadingSection;
