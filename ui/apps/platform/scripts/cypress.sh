479f3a2ec31f979b90a6807183eab55e1c5826db
Gavin Jefferies <gavin@stackrox.com>
2022-03-04 15:53:22 +0000

    avoid cypress timeout and save debug

--- ui/apps/platform/scripts/cypress.sh
+++ ui/apps/platform/scripts/cypress.sh
@@ -33,5 +33,5 @@ if [ $2 == "--spec" ]; then
     fi
     cypress run --spec "cypress/integration/$3"
 else 
-    DEBUG="cypress:*" cypress "$@" 2> /dev/null
+    DEBUG="*" cypress "$@" 2> "${artifacts_dir}/cypress.err"
 fi
