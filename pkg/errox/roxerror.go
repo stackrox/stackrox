025235c1591de965ef870cdb52023201f9ef0368
Michaël Petrov <michael@redhat.com>
2022-03-04 15:20:42 +0100

    nits

--- pkg/errox/roxerror.go
+++ pkg/errox/roxerror.go
@@ -1,5 +1,7 @@
 package errox
 
+import "fmt"
+
 // RoxError is the interface of rox errors.
 type RoxError interface {
 	error
@@ -30,4 +32,27 @@ func (e *roxError) Error() string {
 // Unwrap returns the base of the error.
 func (e *roxError) Unwrap() error {
 	return e.base
+}
+
+// New creates an error based on the existing roxError, but with the
+// personalized error message. Essentially, it allows for preserving the error
+// base error in the chain but hide its message.
+//
+// Example:
+//     ErrRecordNotFound := errox.NotFound.New("record not found")
+//     ErrRecordNotFound.Error() == "record not found" // true
+//     errors.Is(ErrRecordNotFound, errox.NotFound)    // true
+func (e *roxError) New(message string) RoxError {
+	return &roxError{message, e}
+}
+
+// CausedBy adds a cause to the roxError. The resulting message is a combination
+// of the rox error and the cause following a colon.
+//
+// Example:
+//     return errox.InvalidArgument.CausedBy(err)
+// or
+//     return errox.InvalidArgument.CausedBy("unknown parameter")
+func (e *roxError) CausedBy(cause interface{}) error {
+	return fmt.Errorf("%w: %v", e, cause)
 }
