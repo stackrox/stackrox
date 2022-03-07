79e85020404105bb6d8cc576da58d54b373f2bbe
Michaël <michael@redhat.com>
2022-03-07 12:00:54 +0100

    ROX-9206: errox.CausedBy for error reclassification (#647)

--- pkg/errox/roxerror.go
+++ pkg/errox/roxerror.go
@@ -1,17 +1,51 @@
+// Package errox implements tooling and an interface for project errors
+// handling, and a list of base sentinel errors.
+//
+// Usage
+//
+// Base new errors on one of the existing sentinel errors:
+//     ObjectNotFound := errox.NotFound.New("object not found")
+//
+// Classify encountered errors by making them a cause:
+//     err := parse(args)
+//     return errox.InvalidArgs.CausedBy(err)
+//
+// Check error class:
+//     if errors.Is(err, errox.InvalidArgs) ...
+//
+// Format error messages:
+//     return errox.NotFound.New(fmt.Sprintf("file '%s' not found", filename))
+//
+// Create error factories for generic errors:
+//     ErrInvalidAlgorithmF := func(alg string) RoxError {
+//         return errox.InvalidArgs.New(
+//             fmt.Sprintf("invalid algorithm %q used", alg))
+//     }
+//     ...
+//     return ErrInvalidAlgorithmF("256")
 package errox
 
-import (
-	"fmt"
-)
+import "fmt"
+
+// RoxError is the interface of rox errors.
+type RoxError interface {
+	error
+	Unwrap() error
+	New(message string) RoxError
+	CausedBy(cause interface{}) error
+}
 
 type roxError struct {
 	message string
 	base    error
 }
 
+// Ensure roxError implements RoxError.
+var _ RoxError = (*roxError)(nil)
+
 // makeSentinel returns a new sentinel error. Semantically this is very close to
 // `errors.New(message)` from the standard library.
-func makeSentinel(message string) error {
+func makeSentinel(message string) RoxError {
 	return &roxError{message, nil}
 }
 
@@ -25,26 +59,25 @@ func (e *roxError) Unwrap() error {
 	return e.base
 }
 
-// New creates an error based on another error, e.g., an existing sentinel
-// error, but with the personalized error message. Essentially, it allows to
-// preserve the error base error in the chain but hide its message.
+// New creates an error based on the existing roxError, but with the
+// personalized error message. Essentially, it allows for preserving the error
+// base error in the chain but hide its message.
 //
 // Example:
-//     myPackageError := errox.New(errox.NotFound, "file not found")
-//     myPackageError.Error() == "file not found" // true
-//     errors.Is(myPackageError, errox.NotFound)  // true
-func New(base error, message string) error {
-	return &roxError{message, base}
+//     ErrRecordNotFound := errox.NotFound.New("record not found")
+//     ErrRecordNotFound.Error() == "record not found" // true
+//     errors.Is(ErrRecordNotFound, errox.NotFound)    // true
+func (e *roxError) New(message string) RoxError {
+	return &roxError{message, e}
 }
 
-// Newf creates an error based on another error, e.g., an existing sentinel
-// error, but with the personalized formatted error message. Essentially, it
-// allows to preserve the error base error in the chain but hide its message.
+// CausedBy adds a cause to the roxError. The resulting message is a combination
+// of the rox error and the cause following a colon.
 //
 // Example:
-//     myPackageError := errox.Newf(errox.NotFound, "file '%s' not found", "sh")
-//     myPackageError.Error() == "file 'sh' not found" // true
-//     errors.Is(myPackageError, errox.NotFound)       // true
-func Newf(base error, format string, args ...interface{}) error {
-	return New(base, fmt.Sprintf(format, args...))
+//     return errox.InvalidArgument.CausedBy(err)
+// or
+//     return errox.InvalidArgument.CausedBy("unknown parameter")
+func (e *roxError) CausedBy(cause interface{}) error {
+	return fmt.Errorf("%w: %v", e, cause)
 }
