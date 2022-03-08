d6628b06215a3302fbebfd2aa237953352b40442
Tomasz Janiszewski <janiszt@gmail.com>
2022-03-08 19:52:34 +0100

    Run postgres unit tests in CI (#852)

--- pkg/postgres/pgtest/conn.go
+++ pkg/postgres/pgtest/conn.go
@@ -2,7 +2,6 @@ package pgtest
 
 import (
 	"fmt"
-	"os"
 	"testing"
 
 	"k8s.io/utils/env"
@@ -10,8 +9,9 @@ import (
 
 // GetConnectionString returns a connection string for integration testing with Postgres
 func GetConnectionString(_ *testing.T) string {
-	user := os.Getenv("USER")
+	user := env.GetString("POSTGRES_USER", "postgres")
+	pass := env.GetString("POSTGRES_PASSWORD", "")
 	database := env.GetString("POSTGRES_DB", "postgres")
 	host := env.GetString("POSTGRES_HOST", "localhost")
-	return fmt.Sprintf("host=%s port=5432 database=%s user=%s sslmode=disable statement_timeout=600000 pool_min_conns=90 pool_max_conns=90", host, database, user)
+	return fmt.Sprintf("host=%s port=5432 database=%s user=%s password=%s sslmode=disable statement_timeout=600000 pool_min_conns=1 pool_max_conns=90", host, database, user, pass)
 }
