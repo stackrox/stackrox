6883568a6e602ac385d81b9c6cb9af6348e8e109
Linda Song <ls@stackrox.com>
2022-03-10 15:57:07 -0800

    moving deleting field key from request to separate branch

--- ui/apps/platform/src/Containers/Policies/PatternFly/policies.utils.ts
+++ ui/apps/platform/src/Containers/Policies/PatternFly/policies.utils.ts
@@ -429,9 +429,6 @@ function postFormatNestedPolicyFields(policy: Policy): Policy {
                     value: formatValueStr(value as ValueObj, policyGroup.fieldName),
                 };
             });
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            delete serverPolicy.policySections[sectionIdx].policyGroups[groupIdx].fieldKey;
         });
     });
     return serverPolicy;
