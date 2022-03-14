b9eb0fc7da043dc03bb3d93a706f07395ae3f9d8
Juan Rodriguez Hortala <juanrh@redhat.com>
2022-03-14 11:43:47 +0100

    Set ROX_USE_LOCAL_SCANNER in Sensor to true when central scanner is available

--- sensor/kubernetes/sensor/sensor.go
+++ sensor/kubernetes/sensor/sensor.go
@@ -163,7 +163,7 @@ func CreateSensor(client client.Interface, workloadHandler *fake.WorkloadManager
 		return nil, errors.Wrap(err, "creating central client")
 	}
 
-	if features.LocalImageScanning.Enabled() && securedClusterIsNotManagedManually(helmManagedConfig) && env.UseLocalScanner.BooleanSetting() {
+	if features.LocalImageScanning.Enabled() && securedClusterIsNotManagedManually(helmManagedConfig) {
 		podName := os.Getenv("POD_NAME")
 		components = append(components,
 			localscanner.NewLocalScannerTLSIssuer(client.Kubernetes(), sensorNamespace, podName))
