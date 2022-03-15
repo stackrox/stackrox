bca96113da9e4cdae3177622e278c864ebcc8f37
David Vail <dcvail17@gmail.com>
2022-03-15 09:51:54 -0400

    Await a single network request on product branding tests to allow title time to render.

--- ui/apps/platform/cypress/integration/productBranding.test.js
+++ ui/apps/platform/cypress/integration/productBranding.test.js
@@ -2,6 +2,7 @@ import withAuth from '../helpers/basicAuth';
 import selectors from '../constants/GeneralPage';
 import { url as dashboardUrl } from '../constants/DashboardPage';
 import { url as loginUrl } from '../constants/LoginPage';
+import * as api from '../constants/apiEndpoints';
 
 describe('Logo and title product branding checks', () => {
     withAuth();
@@ -10,7 +11,9 @@ describe('Logo and title product branding checks', () => {
 
     it('Should render the login page with matching logo and page title', () => {
         // Ensure that the page title matches the logo branding
+        cy.intercept(api.metadata).as('metadataReq');
         cy.visit(loginUrl);
+        cy.wait('@metadataReq');
         cy.title().then((title) => {
             if (title.includes(redHatTitleText)) {
                 cy.get(selectors.rhacsLogoImage);
@@ -25,7 +28,9 @@ describe('Logo and title product branding checks', () => {
 
     it('Should render the main dashboard with matching logo and page title', () => {
         // Ensure that the page title matches the logo branding
+        cy.intercept('POST', api.dashboard.summaryCounts).as('summaryCounts');
         cy.visit(dashboardUrl);
+        cy.wait('@summaryCounts');
         cy.title().then((title) => {
             if (title.includes(redHatTitleText)) {
                 cy.get(selectors.rhacsLogoImage);
