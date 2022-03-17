4be19a2cf41df277a3ad64b7182ef2e317d991b8
Linda Song <ls@stackrox.com>
2022-03-17 16:15:02 -0700

    PF Policies Step 3 Policy Criteria (BPL) e2e tests (#952)

--- ui/apps/platform/src/Containers/Policies/PatternFly/Wizard/Step3/PolicyCriteriaCategory.tsx
+++ ui/apps/platform/src/Containers/Policies/PatternFly/Wizard/Step3/PolicyCriteriaCategory.tsx
@@ -7,17 +7,27 @@ import PolicyCriteriaKey from './PolicyCriteriaKey';
 type PolicyCriteriaCategoryProps = {
     category: string;
     keys: Descriptor[];
+    isOpenDefault?: boolean;
 };
 
-function PolicyCriteriaCategory({ category, keys }: PolicyCriteriaCategoryProps) {
-    const [isExpanded, setIsExpanded] = React.useState(false);
+function PolicyCriteriaCategory({
+    category,
+    keys,
+    isOpenDefault = false,
+}: PolicyCriteriaCategoryProps) {
+    const [isExpanded, setIsExpanded] = React.useState(isOpenDefault);
 
     function onToggle(expanded: boolean) {
         setIsExpanded(expanded);
     }
 
     return (
-        <ExpandableSection isExpanded={isExpanded} onToggle={onToggle} toggleText={category}>
+        <ExpandableSection
+            isExpanded={isExpanded}
+            onToggle={onToggle}
+            toggleText={category}
+            data-testid="policy-criteria-key-group"
+        >
             {keys.map((key) => (
                 <PolicyCriteriaKey fieldKey={key} key={key.name} />
             ))}
