c5b2e4deb1fedc47448f83e1e9a966f350f9404e
Viswajith Venugopal <viswajithiii@gmail.com>
2022-03-18 17:57:01 +0530

    Make highlights work in Postgres (#966)
    * Make highlights work for all column types
    * Refactor select field generation so that the select fields are generated along with the where clauses, instead of doing that in a separate, parallel pass. This makes it cleaner and was also required to make certain kinds of highlights work.
    * Add highlights for array columns and maps (for this, I ended up doing post-filtering in Go space rather than trying to do it all in SQL, because it required SQL that was too complicated to programmatically generate, with inner joins and with clauses).

--- pkg/search/postgres/query/enum_query.go
+++ pkg/search/postgres/query/enum_query.go
@@ -10,27 +10,50 @@ import (
 	"github.com/stackrox/rox/pkg/search/enumregistry"
 )
 
-func enumEquality(columnName string, field *pkgSearch.Field, enumValues []int32) (*QueryEntry, error) {
+func enumEquality(columnName string, enumValues []int32) (WhereClause, error) {
 	var queries []string
 	var values []interface{}
 	for _, s := range enumValues {
-		entry, err := newStringQuery(columnName, field, strconv.Itoa(int(s)), pkgSearch.Equality)
+		entry, err := newStringQueryWhereClause(columnName, strconv.Itoa(int(s)), pkgSearch.Equality)
 		if err != nil {
-			return nil, err
+			return WhereClause{}, err
 		}
 		queries = append(queries, entry.Query)
 		values = append(values, entry.Values...)
 	}
-	return &QueryEntry{
+	return WhereClause{
 		Query:  fmt.Sprintf("(%s)", strings.Join(queries, " or ")),
 		Values: values,
+		equivalentGoFunc: func(foundValue interface{}) bool {
+			asInt := int32(foundValue.(int))
+			for _, enumValue := range enumValues {
+				if enumValue == asInt {
+					return true
+				}
+			}
+			return false
+		},
 	}, nil
 }
 
-func newEnumQuery(columnName string, field *pkgSearch.Field, value string, queryModifiers ...pkgSearch.QueryModifier) (*QueryEntry, error) {
+func newEnumQuery(ctx *queryAndFieldContext) (*QueryEntry, error) {
+	whereClause, err := newEnumQueryWhereClause(ctx.qualifiedColumnName, ctx.field, ctx.value, ctx.queryModifiers...)
+	if err != nil {
+		return nil, err
+	}
+	qe := qeWithSelectFieldIfNeeded(ctx, &whereClause, func(i interface{}) interface{} {
+		return enumregistry.Lookup(ctx.field.FieldPath, int32(*(i.(*int))))
+	})
+	qe.enumStringifyFunc = func(i int32) string {
+		return enumregistry.Lookup(ctx.field.FieldPath, i)
+	}
+	return qe, nil
+}
+
+func newEnumQueryWhereClause(columnName string, field *pkgSearch.Field, value string, queryModifiers ...pkgSearch.QueryModifier) (WhereClause, error) {
 	var enumValues []int32
 	if len(queryModifiers) > 2 {
-		return nil, fmt.Errorf("unsupported: more than 2 query modifiers for enum query: %+v", queryModifiers)
+		return WhereClause{}, fmt.Errorf("unsupported: more than 2 query modifiers for enum query: %+v", queryModifiers)
 	}
 	var equality bool
 	switch len(queryModifiers) {
@@ -38,7 +61,7 @@ func newEnumQuery(columnName string, field *pkgSearch.Field, value string, query
 		if queryModifiers[0] == pkgSearch.Negation && queryModifiers[1] == pkgSearch.Regex {
 			re, err := regexp.Compile(value)
 			if err != nil {
-				return nil, fmt.Errorf("invalid regex: %w", err)
+				return WhereClause{}, fmt.Errorf("invalid regex: %w", err)
 			}
 
 			enumValues = enumregistry.GetComplementOfValuesMatchingRegex(field.FieldPath, re)
@@ -48,7 +71,7 @@ func newEnumQuery(columnName string, field *pkgSearch.Field, value string, query
 			enumValues = enumregistry.GetComplementByExactMatches(field.FieldPath, value)
 			break
 		}
-		return nil, fmt.Errorf("unsupported: invalid combination of query modifiers for enum query: %+v", queryModifiers)
+		return WhereClause{}, fmt.Errorf("unsupported: invalid combination of query modifiers for enum query: %+v", queryModifiers)
 	case 1:
 		switch queryModifiers[0] {
 		case pkgSearch.Negation:
@@ -56,13 +79,13 @@ func newEnumQuery(columnName string, field *pkgSearch.Field, value string, query
 		case pkgSearch.Regex:
 			re, err := regexp.Compile(value)
 			if err != nil {
-				return nil, fmt.Errorf("invalid regex: %w", err)
+				return WhereClause{}, fmt.Errorf("invalid regex: %w", err)
 			}
 			enumValues = enumregistry.GetValuesMatchingRegex(field.FieldPath, re)
 		case pkgSearch.Equality:
 			enumValues = enumregistry.GetExactMatches(field.FieldPath, value)
 		default:
-			return nil, fmt.Errorf("unsupported query modifier for enum query: %v", queryModifiers[0])
+			return WhereClause{}, fmt.Errorf("unsupported query modifier for enum query: %v", queryModifiers[0])
 		}
 	case 0:
 		prefix, value := parseNumericPrefix(value)
@@ -71,29 +94,39 @@ func newEnumQuery(columnName string, field *pkgSearch.Field, value string, query
 		}
 		enumValues = enumregistry.Get(field.FieldPath, value)
 		if len(enumValues) == 0 {
-			return NewFalseQuery(), nil
+			return NewFalseQuery().Where, nil
 		}
 
 		// Equality means no numeric cast required, and could benefit from hash indexes
 		if equality {
-			return enumEquality(columnName, field, enumValues)
+			return enumEquality(columnName, enumValues)
 		}
 
 		var queries []string
 		var values []interface{}
+		var equivalentGoFuncs []func(interface{}) bool
 		for _, s := range enumValues {
-			entry := createNumericQuery(columnName, field, prefix, float64(s))
+			entry := createNumericQuery(columnName, prefix, float64(s))
 			queries = append(queries, entry.Query)
 			values = append(values, entry.Values...)
+			equivalentGoFuncs = append(equivalentGoFuncs, entry.equivalentGoFunc)
 		}
-		return &QueryEntry{
+		return WhereClause{
 			Query:  fmt.Sprintf("(%s)", strings.Join(queries, " or ")),
 			Values: values,
+			equivalentGoFunc: func(foundValue interface{}) bool {
+				for _, f := range equivalentGoFuncs {
+					if f(foundValue) {
+						return true
+					}
+				}
+				return false
+			},
 		}, nil
 	}
 
 	if len(enumValues) == 0 {
-		return nil, fmt.Errorf("could not find corresponding enum at field %q with value %q and modifiers %+v", field.FieldPath, value, queryModifiers)
+		return WhereClause{}, fmt.Errorf("could not find corresponding enum at field %q with value %q and modifiers %+v", field.FieldPath, value, queryModifiers)
 	}
-	return enumEquality(columnName, field, enumValues)
+	return enumEquality(columnName, enumValues)
 }
--- pkg/search/postgres/query/time_query.go
+++ pkg/search/postgres/query/time_query.go
@@ -6,8 +6,6 @@ import (
 	"strconv"
 	"strings"
 	"time"
-
-	"github.com/stackrox/rox/pkg/search"
 )
 
 const (
@@ -16,19 +14,19 @@ const (
 	sqlTimeStampFormat = "2006-01-02 15:04:05 -07:00"
 )
 
-func newTimeQuery(column string, _ *search.Field, value string, modifiers ...search.QueryModifier) (*QueryEntry, error) {
-	if len(modifiers) > 0 {
+func newTimeQuery(ctx *queryAndFieldContext) (*QueryEntry, error) {
+	if len(ctx.queryModifiers) > 0 {
 		return nil, errors.New("modifiers not supported for time query")
 	}
-	prefix, trimmedValue := parseNumericPrefix(value)
+	prefix, trimmedValue := parseNumericPrefix(ctx.value)
 	var formattedTime string
 	if t, ok := parseTimeString(trimmedValue); ok {
 		// If the date query is a singular datetime with no prefix, then need to create a numeric query with the min = date. max = date + 1
 		if prefix == "" {
-			return &QueryEntry{
-				Query:  fmt.Sprintf("%s >= $$ and %s < $$", column, column),
+			return qeWithSelectFieldIfNeeded(ctx, &WhereClause{
+				Query:  fmt.Sprintf("%s >= $$ and %s < $$", ctx.qualifiedColumnName, ctx.qualifiedColumnName),
 				Values: []interface{}{t.Format(sqlTimeStampFormat), t.Add(dayDuration).Format(sqlTimeStampFormat)},
-			}, nil
+			}, nil), nil
 		}
 		formattedTime = t.Format(sqlTimeStampFormat)
 	} else if d, ok := parseDuration(trimmedValue); ok {
@@ -41,10 +39,10 @@ func newTimeQuery(column string, _ *search.Field, value string, modifiers ...sea
 		return nil, fmt.Errorf("invalid time query (prefix: %s, value: %s). Must be of the format (01/02/2006 or 1d)", prefix, trimmedValue)
 	}
 
-	return &QueryEntry{
-		Query:  fmt.Sprintf("%s %s $$", column, prefix),
+	return qeWithSelectFieldIfNeeded(ctx, &WhereClause{
+		Query:  fmt.Sprintf("%s %s $$", ctx.qualifiedColumnName, prefix),
 		Values: []interface{}{formattedTime},
-	}, nil
+	}, nil), nil
 }
 
 func parseDuration(d string) (time.Duration, bool) {
