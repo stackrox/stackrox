d1bf4060e7d8684879e40c31aa8fd7cbba06955c
Gavin Jefferies <gavin@stackrox.com>
2022-03-18 16:04:46 +0000

    cleanup

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -68,10 +68,6 @@ is_OPENSHIFT_CI() {
     [[ "${OPENSHIFT_CI:-}" == "true" ]]
 }
 
-is_GITHUB_ACTIONS() {
-    [[ "${GITHUB_ACTIONS:-}" == "true" ]]
-}
-
 is_darwin() {
     uname -a | grep -i darwin >/dev/null 2>&1
 }
