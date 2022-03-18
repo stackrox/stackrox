81115cd881e1f276583ee8a3f604881887b33028
Viswajith Venugopal <viswajithiii@gmail.com>
2022-03-18 17:57:01 +0530

    Make highlights work in Postgres (#966)
    * Make highlights work for all column types
    * Refactor select field generation so that the select fields are generated along with the where clauses, instead of doing that in a separate, parallel pass. This makes it cleaner and was also required to make certain kinds of highlights work.
    * Add highlights for array columns and maps (for this, I ended up doing post-filtering in Go space rather than trying to do it all in SQL, because it required SQL that was too complicated to programmatically generate, with inner joins and with clauses).

--- pkg/search/postgres/query/query_functions.go
+++ pkg/search/postgres/query/query_functions.go
@@ -2,127 +2,88 @@ package pgsearch
 
 import (
 	"fmt"
-	"strconv"
 
-	"github.com/pkg/errors"
-	"github.com/stackrox/rox/pkg/parse"
 	"github.com/stackrox/rox/pkg/postgres/walker"
 	pkgSearch "github.com/stackrox/rox/pkg/search"
 	"github.com/stackrox/rox/pkg/stringutils"
-	"github.com/stackrox/rox/pkg/utils"
 )
 
-type queryFunction func(column string, field *pkgSearch.Field, value string, queryModifiers ...pkgSearch.QueryModifier) (*QueryEntry, error)
+type queryAndFieldContext struct {
+	qualifiedColumnName string
+	field               *pkgSearch.Field
+	dbField             *walker.Field
+
+	value          string
+	highlight      bool
+	queryModifiers []pkgSearch.QueryModifier
+}
+
+func qeWithSelectFieldIfNeeded(ctx *queryAndFieldContext, whereClause *WhereClause, postTransformFunc func(interface{}) interface{}) *QueryEntry {
+	qe := &QueryEntry{Where: *whereClause}
+	if ctx.highlight {
+		qe.SelectedFields = []SelectQueryField{{
+			SelectPath:    ctx.qualifiedColumnName,
+			FieldType:     ctx.dbField.DataType,
+			FieldPath:     ctx.field.FieldPath,
+			PostTransform: postTransformFunc,
+		}}
+	}
+	return qe
+}
+
+type queryFunction func(ctx *queryAndFieldContext) (*QueryEntry, error)
 
 var datatypeToQueryFunc = map[walker.DataType]queryFunction{
 	walker.String:      newStringQuery,
 	walker.Bool:        newBoolQuery,
-	walker.StringArray: queryOnArray(newStringQuery),
+	walker.StringArray: queryOnArray(newStringQuery, getStringArrayPostTransformFunc),
 	walker.DateTime:    newTimeQuery,
 	walker.Enum:        newEnumQuery,
 	walker.Integer:     newNumericQuery,
 	walker.Numeric:     newNumericQuery,
-	walker.EnumArray:   queryOnArray(newEnumQuery),
-	walker.IntArray:    queryOnArray(newNumericQuery),
+	walker.EnumArray:   queryOnArray(newEnumQuery, getEnumArrayPostTransformFunc),
+	walker.IntArray:    queryOnArray(newNumericQuery, getIntArrayPostTransformFunc),
 	// Map is handled separately.
-
-	// TODOs
 }
 
-func matchFieldQuery(dbField *walker.Field, field *pkgSearch.Field, value string) (*QueryEntry, error) {
+func matchFieldQuery(dbField *walker.Field, field *pkgSearch.Field, value string, highlight bool) (*QueryEntry, error) {
 	qualifiedColName := dbField.Schema.Table + "." + dbField.ColumnName
+
+	ctx := &queryAndFieldContext{
+		qualifiedColumnName: qualifiedColName,
+		field:               field,
+		dbField:             dbField,
+		highlight:           highlight,
+		value:               value,
+	}
+
 	// Special case: wildcard
 	if stringutils.MatchesAny(value, pkgSearch.WildcardString, pkgSearch.NullString) {
-		return handleExistenceQueries(qualifiedColName, value), nil
+		return handleExistenceQueries(ctx), nil
 	}
 
 	if dbField.DataType == walker.Map {
-		return newMapQuery(qualifiedColName, field, value)
+		return newMapQuery(ctx)
 	}
 
 	trimmedValue, modifiers := pkgSearch.GetValueAndModifiersFromString(value)
-	return datatypeToQueryFunc[dbField.DataType](qualifiedColName, field, trimmedValue, modifiers...)
+	ctx.value = trimmedValue
+	ctx.queryModifiers = modifiers
+	return datatypeToQueryFunc[dbField.DataType](ctx)
 }
 
-func handleExistenceQueries(root string, value string) *QueryEntry {
-	switch value {
+func handleExistenceQueries(ctx *queryAndFieldContext) *QueryEntry {
+	switch ctx.value {
 	case pkgSearch.WildcardString:
-		return &QueryEntry{
-			Query: fmt.Sprintf("%s is not null", root),
-		}
+		return qeWithSelectFieldIfNeeded(ctx, &WhereClause{
+			Query: fmt.Sprintf("%s is not null", ctx.qualifiedColumnName),
+		}, nil)
 	case pkgSearch.NullString:
-		return &QueryEntry{
-			Query: fmt.Sprintf("%s is null", root),
-		}
+		return qeWithSelectFieldIfNeeded(ctx, &WhereClause{
+			Query: fmt.Sprintf("%s is null", ctx.qualifiedColumnName),
+		}, nil)
 	default:
-		log.Fatalf("existence query for value %s is not currently handled", value)
+		log.Fatalf("existence query for value %s is not currently handled", ctx.value)
 	}
 	return nil
-}
-
-func newStringQuery(columnName string, _ *pkgSearch.Field, value string, queryModifiers ...pkgSearch.QueryModifier) (*QueryEntry, error) {
-	if len(value) == 0 {
-		return nil, errors.New("value in search query cannot be empty")
-	}
-
-	if len(queryModifiers) == 0 {
-		return &QueryEntry{
-			Query:  fmt.Sprintf("%s ilike $$", columnName),
-			Values: []interface{}{value + "%"},
-		}, nil
-	}
-	if queryModifiers[0] == pkgSearch.AtLeastOne {
-		panic("I dont think this is used")
-	}
-	var negationString string
-	if negated := queryModifiers[0] == pkgSearch.Negation; negated {
-		negationString = "!"
-		queryModifiers = queryModifiers[1:]
-	}
-
-	if len(queryModifiers) == 0 {
-		return &QueryEntry{
-			Query:  fmt.Sprintf("NOT (%s ilike $$)", columnName),
-			Values: []interface{}{value + "%"},
-		}, nil
-	}
-
-	switch queryModifiers[0] {
-	case pkgSearch.Regex:
-		return &QueryEntry{
-			Query:  fmt.Sprintf("%s %s~* $$", columnName, negationString),
-			Values: []interface{}{value},
-		}, nil
-	case pkgSearch.Equality:
-		return &QueryEntry{
-			Query:  fmt.Sprintf("%s %s= $$", columnName, negationString),
-			Values: []interface{}{value},
-		}, nil
-	}
-	err := errors.Errorf("unknown query modifier: %s", queryModifiers[0])
-	utils.Should(err)
-	return nil, err
-}
-
-func newBoolQuery(column string, field *pkgSearch.Field, value string, modifiers ...pkgSearch.QueryModifier) (*QueryEntry, error) {
-	if len(modifiers) > 0 {
-		return nil, errors.Errorf("modifiers for bool query not allowed: %+v", modifiers)
-	}
-	res, err := parse.FriendlyParseBool(value)
-	if err != nil {
-		return nil, err
-	}
-	// explicitly apply equality check
-	return newStringQuery(column, field, strconv.FormatBool(res), pkgSearch.Equality)
-}
-
-func queryOnArray(baseQuery queryFunction) queryFunction {
-	return func(column string, field *pkgSearch.Field, value string, queryModifiers ...pkgSearch.QueryModifier) (*QueryEntry, error) {
-		baseQ, err := baseQuery("elem", field, value, queryModifiers...)
-		if err != nil {
-			return nil, err
-		}
-		baseQ.Query = fmt.Sprintf("exists (select * from unnest(%s) as elem where %s)", column, baseQ.Query)
-		return baseQ, nil
-	}
 }
--- tools/generate-helpers/pg-table-bindings/multitest/postgres/store.go
+++ tools/generate-helpers/pg-table-bindings/multitest/postgres/store.go
@@ -63,6 +63,7 @@ create table if not exists multikey (
     Enum integer,
     Enums int[],
     String_ varchar,
+    IntSlice int[],
     Embedded_Embedded varchar,
     Oneofstring varchar,
     Oneofnested_Nested varchar,
@@ -141,13 +142,14 @@ func insertIntoMultikey(ctx context.Context, tx pgx.Tx, obj *storage.TestMultiKe
 		obj.GetEnum(),
 		obj.GetEnums(),
 		obj.GetString_(),
+		obj.GetIntSlice(),
 		obj.GetEmbedded().GetEmbedded(),
 		obj.GetOneofstring(),
 		obj.GetOneofnested().GetNested(),
 		serialized,
 	}
 
-	finalStr := "INSERT INTO multikey (Key1, Key2, StringSlice, Bool, Uint64, Int64, Float, Labels, Timestamp, Enum, Enums, String_, Embedded_Embedded, Oneofstring, Oneofnested_Nested, serialized) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16) ON CONFLICT(Key1, Key2) DO UPDATE SET Key1 = EXCLUDED.Key1, Key2 = EXCLUDED.Key2, StringSlice = EXCLUDED.StringSlice, Bool = EXCLUDED.Bool, Uint64 = EXCLUDED.Uint64, Int64 = EXCLUDED.Int64, Float = EXCLUDED.Float, Labels = EXCLUDED.Labels, Timestamp = EXCLUDED.Timestamp, Enum = EXCLUDED.Enum, Enums = EXCLUDED.Enums, String_ = EXCLUDED.String_, Embedded_Embedded = EXCLUDED.Embedded_Embedded, Oneofstring = EXCLUDED.Oneofstring, Oneofnested_Nested = EXCLUDED.Oneofnested_Nested, serialized = EXCLUDED.serialized"
+	finalStr := "INSERT INTO multikey (Key1, Key2, StringSlice, Bool, Uint64, Int64, Float, Labels, Timestamp, Enum, Enums, String_, IntSlice, Embedded_Embedded, Oneofstring, Oneofnested_Nested, serialized) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17) ON CONFLICT(Key1, Key2) DO UPDATE SET Key1 = EXCLUDED.Key1, Key2 = EXCLUDED.Key2, StringSlice = EXCLUDED.StringSlice, Bool = EXCLUDED.Bool, Uint64 = EXCLUDED.Uint64, Int64 = EXCLUDED.Int64, Float = EXCLUDED.Float, Labels = EXCLUDED.Labels, Timestamp = EXCLUDED.Timestamp, Enum = EXCLUDED.Enum, Enums = EXCLUDED.Enums, String_ = EXCLUDED.String_, IntSlice = EXCLUDED.IntSlice, Embedded_Embedded = EXCLUDED.Embedded_Embedded, Oneofstring = EXCLUDED.Oneofstring, Oneofnested_Nested = EXCLUDED.Oneofnested_Nested, serialized = EXCLUDED.serialized"
 	_, err := tx.Exec(ctx, finalStr, values...)
 	if err != nil {
 		return err
