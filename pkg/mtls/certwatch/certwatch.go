6effd0d26d2a4ff0622b353aad450e390fa5a2bc
Yury Kovalev <ykovalev@redhat.com>
2022-03-23 18:50:38 +0100

    ROX-9287: Fix review comments

--- pkg/mtls/certwatch/certwatch.go
+++ pkg/mtls/certwatch/certwatch.go
@@ -1,10 +1,10 @@
+// Package certwatch provides functions to monitor certificate directory updates at runtime.
 package certwatch
 
 import (
 	"context"
 	"crypto/tls"
 	"crypto/x509"
-	"strings"
 	"time"
 
 	"github.com/stackrox/rox/pkg/k8scfgwatch"
@@ -17,16 +17,17 @@ const (
 )
 
 var (
-	log = logging.LoggerForModule()
+	log                     = logging.LoggerForModule()
+	_   k8scfgwatch.Handler = (*handler)(nil) // compile time check that the handler implements the interface
 )
 
 type loadCertificateFunc func(dir string) (*tls.Certificate, error)
 type updateCertificateFunc func(cert *tls.Certificate)
 
 // WatchCertDir starts watching the directory containing certificates
-func WatchCertDir(certChain string, dir string, loadCert loadCertificateFunc, updateCert updateCertificateFunc) {
+func WatchCertDir(dir string, loadCert loadCertificateFunc, updateCert updateCertificateFunc) {
 	wh := &handler{
-		certChain:  certChain,
+		dir:        dir,
 		loadCert:   loadCert,
 		updateCert: updateCert,
 	}
@@ -39,7 +40,7 @@ func WatchCertDir(certChain string, dir string, loadCert loadCertificateFunc, up
 }
 
 type handler struct {
-	certChain  string
+	dir        string
 	loadCert   loadCertificateFunc
 	updateCert updateCertificateFunc
 }
@@ -51,21 +52,23 @@ func (h *handler) OnChange(dir string) (interface{}, error) {
 func (h *handler) OnStableUpdate(val interface{}, err error) {
 	var cert *tls.Certificate
 	if err != nil {
-		log.Errorf("Error reading %s TLS certificates: %v. Disabling %s TLS certificate.", h.certChain, err, h.certChain)
+		log.Errorf("Error reading TLS certificates: %v. Disabling TLS certificate. Watch dir: %q", err, h.dir)
 	} else {
 		cert, _ = val.(*tls.Certificate)
 		if cert == nil {
-			log.Infof("No %s TLS certificate found. Using internal certificates for HTTPS", h.certChain)
+			log.Infof("No TLS certificate found. Using internal certificates for HTTPS. Watch dir: %q", h.dir)
 		} else {
-			log.Infof("%s TLS certificate loaded, using cert with DN %s for HTTPS", strings.Title(h.certChain), cert.Leaf.Subject)
+			log.Infof("TLS certificate loaded, using the following cert for HTTPS: (SerialNumber: %s, Subject: %s, DNSNames, %s), watch dir: %q",
+				cert.Leaf.SerialNumber, cert.Leaf.Subject, cert.Leaf.DNSNames, h.dir)
 
 			parsedChain, err := x509utils.ParseCertificateChain(cert.Certificate)
 			if err != nil {
-				log.Errorf("Error parsing certificate #%d in the %s certificate chain: %v", len(parsedChain), h.certChain, err)
+				log.Errorf("Error parsing certificate #%d in the certificate chain (dir: %q): %v", len(parsedChain), h.dir, err)
 			} else if err := x509utils.VerifyCertificateChain(parsedChain, x509.VerifyOptions{}); err != nil {
-				log.Warnf("This server does not trust its own %s certificate! "+
+				log.Warnf("This server does not trust its own certificate! "+
 					"If you see certificate trust issues in your clients (e.g. sensors), "+
-					"please ensure that your certificate PEM file includes the entire certificate chain.", h.certChain)
+					"please ensure that your certificate PEM file includes the entire certificate chain."+
+					"Watch dir: %q", h.dir)
 			}
 		}
 	}
@@ -74,5 +77,5 @@ func (h *handler) OnStableUpdate(val interface{}, err error) {
 }
 
 func (h *handler) OnWatchError(err error) {
-	log.Errorf("Error watching %s TLS certificate directory: %v. Not updating TLS certificates!", h.certChain, err)
+	log.Errorf("Error watching TLS certificate directory %q: %v. Not updating TLS certificates!", h.dir, err)
 }
