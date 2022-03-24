2553ab4304e22f348b2976957ba126780a011b83
RTann <rtann@redhat.com>
2022-03-24 12:20:01 -0700

    update

--- sensor/kubernetes/listener/resources/secrets.go
+++ sensor/kubernetes/listener/resources/secrets.go
@@ -200,7 +200,7 @@ func (s *secretDispatcher) processDockerConfigEvent(secret *v1.Secret, action ce
 				if err != nil {
 					log.Errorf("Unable to upsert registry %q into store: %v", registry, err)
 				}
-				log.Debugf("Upserted registry %q for namespace %q into store", registry, secret.GetNamespace())
+				log.Infof("Upserted registry %q for namespace %q into store", registry, secret.GetNamespace())
 			}
 		}
 
