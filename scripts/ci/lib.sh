800471a5ff75825f6d6eebaa82d3f80434080e61
Gavin Jefferies <gavin@stackrox.com>
2022-03-28 22:12:06 +0000

    The upgrade test running under OSCI.

--- scripts/ci/lib.sh
+++ scripts/ci/lib.sh
@@ -383,6 +383,60 @@ mark_collector_release() {
     /scripts/create_update_pr.sh "${branch_name}" collector "Update RELEASED_VERSIONS" "Add entry into the RELEASED_VERSIONS file"
 }
 
+pr_has_label() {
+    if [[ -z "${1:-}" ]]; then
+        die "usage: pr_has_label <expected label>"
+    fi
+
+    require_environment "GITHUB_TOKEN"
+
+    local expected_label="$1"
+    get_pr_details | jq '([.labels | .[].name]  // []) | .[]' -r | grep -qx "${expected_label}"
+}
+
+get_pr_details() {
+    require_environment "GITHUB_TOKEN"
+
+    local pull_request
+    local org
+    local repo
+
+    if is_CIRCLECI; then
+        [ -n "${CIRCLE_PULL_REQUEST}" ] || { echo "Not on a PR, ignoring label overrides"; exit 3; }
+        [ -n "${CIRCLE_PROJECT_USERNAME}" ] || { echo "CIRCLE_PROJECT_USERNAME not found" ; exit 2; }
+        [ -n "${CIRCLE_PROJECT_REPONAME}" ] || { echo "CIRCLE_PROJECT_REPONAME not found" ; exit 2; }
+        pull_request="${CIRCLE_PULL_REQUEST}"
+        org="${CIRCLE_PROJECT_USERNAME}"
+        repo="${CIRCLE_PROJECT_REPONAME}"
+    elif is_OPENSHIFT_CI; then
+        pull_request=$(jq -r <<<"$JOB_SPEC" '.refs.pulls[0].number')
+        org=$(jq -r <<<"$JOB_SPEC" '.refs.org')
+        repo=$(jq -r <<<"$JOB_SPEC" '.refs.repo')
+    else
+        die "not supported"
+    fi
+
+    url="https://api.github.com/repos/${org}/${repo}/pulls/${pull_request}"
+    curl -sS -H "Authorization: token ${GITHUB_TOKEN}" "${url}"
+}
+
+openshift_ci_mods() {
+    # For ci_export(), override BASH_ENV from stackrox-test with something that is writable.
+    BASH_ENV=$(mktemp)
+    export BASH_ENV
+
+    # For gradle
+    info "HOME ${HOME:-}"
+    export GRADLE_USER_HOME="${HOME}"
+    info "GRADLE_USER_HOME ${GRADLE_USER_HOME:-}"
+    # For OpenJDK
+    set -x
+    mkdir -p "/tmp/.java/.userPrefs"
+    chmod -R 0777 "/tmp/.java"
+    ls -laR "/tmp/.java"
+    set +x
+}
+
 if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
     if [[ "$#" -lt 1 ]]; then
         die "When invoked at the command line a method is required."
