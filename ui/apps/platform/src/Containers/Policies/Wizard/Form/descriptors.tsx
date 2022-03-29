158c558acc20ee96aec89d7a74f69c28795922eb
Frederico Bittencourt <fbittenc@redhat.com>
2022-03-29 10:17:36 +0200

    remove UI field definition for now

--- ui/apps/platform/src/Containers/Policies/Wizard/Form/descriptors.tsx
+++ ui/apps/platform/src/Containers/Policies/Wizard/Form/descriptors.tsx
@@ -645,35 +645,6 @@ export const policyConfigurationDescriptor: Descriptor[] = [
         })),
         canBooleanLogic: true,
     },
-    // TODO: Check how can we create fields without user input
-    // {
-    //     label: 'Missing Ingress Network Policy',
-    //     name: 'Missing Ingress Network Policy',
-    //     shortName: 'Missing Ingress Network Policy',
-    //     category: policyCriteriaCategories.NETWORKING,
-    //     type: 'select',
-    //     options: [
-    //         {
-    //             label: 'default',
-    //             value: 'true',
-    //         },
-    //     ],
-    //     canBooleanLogic: true,
-    // },
-    // {
-    //     label: 'Missing Egress Network Policy',
-    //     name: 'Missing Egress Network Policy',
-    //     shortName: 'Missing Egress Network Policy',
-    //     category: policyCriteriaCategories.NETWORKING,
-    //     type: 'select',
-    //     options: [
-    //         {
-    //             label: 'default',
-    //             value: 'true',
-    //         },
-    //     ],
-    //     canBooleanLogic: true,
-    // },
     {
         label: 'Protocol',
         name: 'Exposed Port Protocol',
