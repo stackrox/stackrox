985805513e171b1bc05aca1163150fae22583736
Mark Pedrotti <pedrottimark@gmail.com>
2022-03-29 16:46:23 -0400

    Replace cypress server and route with intercept in networkGraph part 1 (#1091)

--- ui/apps/platform/cypress/integration/networkGraph/tooltip.test.js
+++ ui/apps/platform/cypress/integration/networkGraph/tooltip.test.js
@@ -1,43 +1,20 @@
 import * as api from '../../constants/apiEndpoints';
-import { url as networkUrl, selectors as networkPageSelectors } from '../../constants/NetworkPage';
+import { selectors as networkPageSelectors } from '../../constants/NetworkPage';
 import withAuth from '../../helpers/basicAuth';
-import { clickOnNodeByName, mouseOverNodeByName } from '../../helpers/networkGraph';
+import {
+    clickOnNodeByName,
+    mouseOverNodeByName,
+    visitNetworkGraphWithMockedData,
+} from '../../helpers/networkGraph';
 import selectors from '../../selectors/index';
 
 const { cytoscapeContainer } = networkPageSelectors;
-
-function navigateToNetworkGraphWithMockedData() {
-    cy.server();
-
-    cy.fixture('network/networkGraph.json').as('networkGraphJson');
-    cy.route('GET', api.network.networkGraph, '@networkGraphJson').as('networkGraph');
-
-    cy.fixture('network/networkPolicies.json').as('networkPoliciesJson');
-    cy.route('GET', api.network.networkPoliciesGraph, '@networkPoliciesJson').as('networkPolicies');
-
-    cy.visit(networkUrl);
-    cy.wait('@networkGraph');
-    cy.wait('@networkPolicies');
-}
 
 // TODO: update mock data to reflect the new Anomalous/Baseline Flows of Network Detection
 describe.skip('Network Graph tooltip', () => {
     withAuth();
 
     describe('deployment node', () => {
-        beforeEach(() => {
-            cy.server();
-            cy.route('GET', api.risks.riskyDeployments).as('deployments');
-
-            cy.fixture('network/networkGraph.json').as('networkGraphJson');
-            cy.route('GET', api.network.networkGraph, '@networkGraphJson').as('networkGraph');
-
-            cy.fixture('network/networkPolicies.json').as('networkPoliciesJson');
-            cy.route('GET', api.network.networkPoliciesGraph, '@networkPoliciesJson').as(
-                'networkPolicies'
-            );
-        });
-
         const {
             table: { cells: cellsSelector },
         } = selectors;
@@ -49,10 +26,11 @@ describe.skip('Network Graph tooltip', () => {
         const getEgressFlowsText = (count) => `${count} egress flows`;
 
         // TODO: re-enable when https://stack-rox.atlassian.net/browse/ROX-5904 is fixed
-        xit('has no bidirectional', () => {
-            cy.fixture('network/sensorDeployment.json').as('sensorDeploymentJson');
-            cy.route('GET', api.network.deployment, '@sensorDeploymentJson').as('sensorDeployment');
-            navigateToNetworkGraphWithMockedData();
+        it.skip('has no bidirectional', () => {
+            cy.intercept('GET', api.network.deployment, {
+                fixture: 'network/sensorDeployment.json',
+            }).as('sensorDeployment');
+            visitNetworkGraphWithMockedData();
 
             const name = 'sensor';
 
@@ -87,11 +65,10 @@ describe.skip('Network Graph tooltip', () => {
         });
 
         it('has bidirectional', () => {
-            cy.fixture('network/centralDeployment.json').as('centralDeploymentJson');
-            cy.route('GET', api.network.deployment, '@centralDeploymentJson').as(
-                'centralDeployment'
-            );
-            navigateToNetworkGraphWithMockedData();
+            cy.intercept('GET', api.network.deployment, {
+                fixture: 'network/centralDeployment.json',
+            }).as('centralDeployment');
+            visitNetworkGraphWithMockedData();
 
             const name = 'central';
 
@@ -126,11 +103,10 @@ describe.skip('Network Graph tooltip', () => {
         });
 
         it('has ingress only', () => {
-            cy.fixture('network/scannerDbDeployment.json').as('scannerDbDeploymentJson');
-            cy.route('GET', api.network.deployment, '@scannerDbDeploymentJson').as(
-                'scannerDbDeployment'
-            );
-            navigateToNetworkGraphWithMockedData();
+            cy.intercept('GET', api.network.deployment, {
+                fixture: 'network/scannerDbDeployment.json',
+            }).as('scannerDbDeployment');
+            visitNetworkGraphWithMockedData();
 
             const name = 'scanner-db';
 
@@ -167,11 +143,10 @@ describe.skip('Network Graph tooltip', () => {
         });
 
         it('has egress only', () => {
-            cy.fixture('network/collectorDeployment.json').as('collectorDeploymentJson');
-            cy.route('GET', api.network.deployment, '@collectorDeploymentJson').as(
-                'collectorDeployment'
-            );
-            navigateToNetworkGraphWithMockedData();
+            cy.intercept('GET', api.network.deployment, {
+                fixture: 'network/collectorDeployment.json',
+            }).as('collectorDeployment');
+            visitNetworkGraphWithMockedData();
 
             const name = 'collector';
 
