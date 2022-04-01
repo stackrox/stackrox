1ba7a8522b79bb96328dcfe515f7ac67f25967ae
Yury Kovalev <ykovalev@redhat.com>
2022-04-01 13:49:07 +0200

    ROX-9967: Temporary switch off init-bundle rotation in the operator

--- operator/pkg/central/extensions/reconcile_tls_test.go
+++ operator/pkg/central/extensions/reconcile_tls_test.go
@@ -145,7 +145,8 @@ func TestCreateCentralTLS(t *testing.T) {
 	for name, c := range cases {
 		c := c
 		if strings.Contains(name, "init bundle secrets should be created") {
-			// See ROX-9023.
+			// See ROX-9967.
+			// TODO(ROX-9969): Remove this exclusion after the init-bundle cert rotation stabilization.
 			continue
 		}
 		t.Run(name, func(t *testing.T) {
