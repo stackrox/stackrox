82108122a8abbae99b255e579fb0752f4b360636
Mark Pedrotti <mark@stackrox.com>
2022-04-01 17:09:25 -0400

    Add new roles reducers to fix failing unit test

--- ui/apps/platform/src/reducers/roles.test.js
+++ ui/apps/platform/src/reducers/roles.test.js
@@ -7,6 +7,8 @@ describe('roles reducer', () => {
             resources: [],
             selectedRole: null,
             userRolePermissions: null,
+            userRolePermissionsError: null,
+            isLoadingUserRolePermissions: true,
         };
         const state = reducer(undefined, {});
 
