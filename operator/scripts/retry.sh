cee940d50d019ccc26448a181ca83ebfb385beb8
Moritz Clasmeier <moritz@stackrox.com>
2022-04-05 10:58:01 +0200

    Review

--- operator/scripts/retry.sh
+++ operator/scripts/retry.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-DELAY_BETWEEN_ATTEMPTS=2 # Seconds
+set -eu -o pipefail
+
+# shellcheck source=./hack/common.sh
+source "$(dirname "$0")/../hack/common.sh"
 
 eecho() {
   echo "$@" >&2
@@ -11,32 +14,24 @@ die() {
   exit 1
 }
 
-if [ $# -lt 1 ]; then
-    die "Usage: $0 <number of retries> <command> [ <command arg> ... ]"
-fi
-
-N_ATTEMPTS="$1"
-shift
-
-if ! [ "$N_ATTEMPTS" -gt 0 ] 2>/dev/null; then
-    die "Error: '$N_ATTEMPTS' is not a valid number of attempts, please provide a positive natural number."
-fi
-
-CMD=$*
-
-for i in $(seq 1 "$N_ATTEMPTS"); do
-    eecho "** Executing '$CMD' (attempt $i of $N_ATTEMPTS) **"
-    eecho
-    $CMD
-    ret=$?
-    eecho
-    if [ $ret -eq 0 ]; then
-        exit 0
+function main() {
+    if [ $# -lt 2 ]; then
+        die "Usage: $0 <number of attempts> <command> [ <command arg> ... ]"
     fi
-    if [ "$i" -lt "$N_ATTEMPTS" ]; then
-        sleep $DELAY_BETWEEN_ATTEMPTS
-    fi
-done
 
-eecho
-die "** Command failed $N_ATTEMPTS times in a row, giving up"
+    local -r n_attempts="${1:-}"; shift
+    if ! [ "$n_attempts" -gt 0 ] 2>/dev/null; then
+        die "Error: '$n_attempts' is not a valid number of attempts, please provide a positive natural number."
+    fi
+
+    local -r delay="${1:-}"; shift
+    if ! { [ "$delay" -gt 0 ] 2>/dev/null || [ "$delay" -eq 0 ] 2>/dev/null; }; then
+        die "Error: '$delay' is not a number of seconds."
+    fi
+
+    eecho "** Executing '$*' with $n_attempts attempts **"
+    eecho
+    retry "$n_attempts" "$delay" "$@"
+}
+
+main "$@"
