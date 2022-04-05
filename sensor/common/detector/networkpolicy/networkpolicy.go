61a44661b2c613d06362d3e6aaed960450fe9dc2
Frederico Bittencourt <fbittenc@redhat.com>
2022-04-05 10:23:24 +0200

    refactor check for ingress|egress policies

--- sensor/common/detector/networkpolicy/networkpolicy.go
+++ sensor/common/detector/networkpolicy/networkpolicy.go
@@ -31,12 +31,14 @@ func (np *Finder) GetNetworkPoliciesApplied(deployment *storage.Deployment) *aug
 	}
 
 	var hasIngress, hasEgress bool
+
+lookingForPolicies:
 	for _, policy := range np.store.Find(deployment.Namespace, deployment.Labels) {
 		for _, policyType := range policy.GetSpec().GetPolicyTypes() {
-			if policyType == storage.NetworkPolicyType_INGRESS_NETWORK_POLICY_TYPE {
-				hasIngress = true
-			} else if policyType == storage.NetworkPolicyType_EGRESS_NETWORK_POLICY_TYPE {
-				hasEgress = true
+			hasIngress = hasIngress || policyType == storage.NetworkPolicyType_INGRESS_NETWORK_POLICY_TYPE
+			hasEgress = hasEgress || policyType == storage.NetworkPolicyType_EGRESS_NETWORK_POLICY_TYPE
+			if hasIngress && hasEgress {
+				break lookingForPolicies
 			}
 		}
 	}
