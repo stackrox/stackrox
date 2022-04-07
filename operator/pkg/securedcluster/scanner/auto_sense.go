4b49ec9dbd7534ce478206a818c4c43cff15b33b
Aleksandr Kurlov <sasha.kurlov@gmail.com>
2022-04-07 14:46:53 +0200

    Update operator/pkg/securedcluster/scanner/auto_sense.go
    Co-authored-by: Marcin Owsiany <porridge@redhat.com>

--- operator/pkg/securedcluster/scanner/auto_sense.go
+++ operator/pkg/securedcluster/scanner/auto_sense.go
@@ -14,7 +14,7 @@ import (
 )
 
 const (
-	// ClusterVersionDefaultName is a default name for the auto created ClusterVersion k8s custom resource on OpenShift
+	// ClusterVersionDefaultName is a default name for the auto created ClusterVersion k8s custom resource on OpenShift.
 	clusterVersionDefaultName = "version"
 )
 
