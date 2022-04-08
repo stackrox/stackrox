ff4f35cf12210c38d06bfb6a09ea2e9017a625fc
Tomasz Janiszewski <janiszt@gmail.com>
2022-04-08 16:23:05 +0200

    fixes for review

--- tools/generate-helpers/pg-table-bindings/list_test.go
+++ tools/generate-helpers/pg-table-bindings/list_test.go
@@ -16,6 +16,9 @@ func TestStorageToResource(t *testing.T) {
 	assert.Equal(t, "Namespace", storageToResource("*storage.NamespaceMetadata"))
 	assert.Equal(t, "Cluster", storageToResource("*storage.Cluster"))
 	assert.Equal(t, "Cluster", storageToResource("storage.Cluster"))
+	assert.Equal(t, "*fake", storageToResource("fake"))
+	assert.Equal(t, "fake", storageToResource("storage.fake"))
+	assert.Equal(t, "fake", storageToResource("*storage.fake"))
 }
 
 func TestIsGloballyScoped(t *testing.T) {
@@ -23,15 +26,19 @@ func TestIsGloballyScoped(t *testing.T) {
 	assert.False(t, isGloballyScoped("*storage.NamespaceMetadata"))
 	assert.True(t, isGloballyScoped("*storage.Policy"))
 	assert.True(t, isGloballyScoped("storage.Policy"))
+	assert.True(t, isGloballyScoped("fake"))
 }
 
 func TestIsDirectlyScoped(t *testing.T) {
 	for typ, directlyScoped := range map[proto.Message]bool{
-		&storage.NamespaceMetadata{}: true,
-		&storage.Cluster{}:           true,
-		&storage.Deployment{}:        true,
-		&storage.Image{}:             false,
-		&storage.CVE{}:               false,
+		&storage.NamespaceMetadata{}:    true,
+		&storage.Cluster{}:              true,
+		&storage.Deployment{}:           true,
+		&storage.Image{}:                false,
+		&storage.CVE{}:                  false,
+		&storage.Policy{}:               true,
+		&storage.VulnerabilityRequest{}: true, // no resource metadata
+		&storage.Email{}:                true, // no resource metadata
 	} {
 		t.Run(fmt.Sprintf("%T directly scoped: %t", typ, directlyScoped), func(t *testing.T) {
 			assert.Equal(t, directlyScoped, isDirectlyScoped(walker.Walk(reflect.TypeOf(typ), "")))
