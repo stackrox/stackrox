322f12dda58cd6b8dfae7de9886a26662228a7d5
David Vail <dcvail17@gmail.com>
2022-04-11 15:01:25 -0400

    Update Violations page to track search parameters in URL (#1114)

---
+++ ui/apps/platform/src/hooks/useEffectAfterFirstRender.test.ts
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import { renderHook, act } from '@testing-library/react-hooks';
+import useEffectAfterFirstRender from './useEffectAfterFirstRender';
+
+test('useEffectAfterFirstRender should mutate variable on all renders after the initial render', () => {
+    let runCount = 0;
+    const { result } = renderHook(() => {
+        useEffectAfterFirstRender(() => {
+            runCount += 1;
+        });
+        return useState({});
+    });
+
+    expect(runCount).toEqual(0);
+
+    act(() => {
+        const [, setObj] = result.current;
+        setObj({});
+    });
+    expect(runCount).toEqual(1);
+
+    act(() => {
+        const [, setObj] = result.current;
+        setObj({});
+    });
+    expect(runCount).toEqual(2);
+});
