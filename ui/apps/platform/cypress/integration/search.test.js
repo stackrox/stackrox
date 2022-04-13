548e507b0fe0891c2a02dc234d3d74b6d490de55
Mark Pedrotti <mark@stackrox.com>
2022-04-13 11:59:15 -0400

    Replace cypress server and route with intercept in search

--- ui/apps/platform/cypress/integration/search.test.js
+++ ui/apps/platform/cypress/integration/search.test.js
@@ -1,63 +1,67 @@
 import { selectors } from '../constants/SearchPage';
 import * as api from '../constants/apiEndpoints';
 import withAuth from '../helpers/basicAuth';
+import { visitMainDashboard } from '../helpers/main';
 
-// TODO: Fix for ROX-6826
-describe.skip('Global Search Modal', () => {
-    withAuth();
+function visitSearch() {
+    visitMainDashboard();
 
-    beforeEach(() => {
-        cy.server();
-        cy.fixture('search/globalSearchResults.json').as('globalSearchResultsJson');
-        cy.route('GET', api.search.globalSearchWithResults, '@globalSearchResultsJson').as(
-            'globalSearchResults'
-        );
-        cy.fixture('search/metadataOptions.json').as('metadataOptionsJson');
-        cy.route('GET', api.search.options, '@metadataOptionsJson').as('globalSearchOptions');
-        cy.visit('/main/dashboard');
-        cy.get(selectors.globalSearchButton).click();
+    cy.get(selectors.globalSearchButton).click();
+}
+
+function searchWithFixture(searchTuples, fixture) {
+    cy.intercept('GET', api.search.results, { fixture }).as('getSearchResults');
+
+    cy.get(selectors.globalSearch.input).clear();
+    searchTuples.forEach(([category, value]) => {
+        cy.get(selectors.globalSearch.input).type(`${category}{enter}`);
+        cy.get(selectors.globalSearch.input).type(`${value}{enter}`);
     });
 
-    // TODO: Fix for ROX-6826
-    xit('Should have 6 tabs with the "All" tab selected by default', () => {
-        cy.wait('@globalSearchOptions');
-        cy.get(selectors.globalSearch.input).type('Cluster:{enter}', {
-            force: true,
-        });
-        cy.get(selectors.globalSearch.input).type('remote{enter}', {
-            force: true,
-        });
-        cy.wait('@globalSearchResults');
-        cy.get(selectors.allTab).should('have.class', 'border-primary-400');
-        cy.get(selectors.violationsTab).should('not.have.class', 'border-primary-400');
-        cy.get(selectors.policiesTab).should('not.have.class', 'border-primary-400');
-        cy.get(selectors.deploymentsTab).should('not.have.class', 'border-primary-400');
-        cy.get(selectors.imagesTab).should('not.have.class', 'border-primary-400');
-        cy.get(selectors.secretsTab).should('not.have.class', 'border-primary-400');
+    cy.wait('@getSearchResults');
+}
+
+describe('Global Search Modal', () => {
+    withAuth();
+
+    it('should have empty state instead of count and tabs if search filter is empty', () => {
+        visitSearch();
+
+        cy.get(`${selectors.empty.head}:contains("Search all data")`).should('exist');
+        cy.get(
+            `${selectors.empty.body}:contains("Choose one or more filter values to search")`
+        ).should('exist');
+
+        cy.get(selectors.globalSearchResults.header).should('not.exist');
+        cy.get(selectors.tab).should('not.exist');
+    });
+
+    it('Should have 6 tabs with the "All" tab selected by default', () => {
+        visitSearch();
+        searchWithFixture([['Cluster:', 'remote']], 'search/globalSearchResults.json');
+
+        cy.get(`${selectors.tab}:contains("All")`).should('have.class', 'pf-m-current');
+        cy.get(`${selectors.tab}:contains("Violations")`).should('not.have.class', 'pf-m-current');
+        cy.get(`${selectors.tab}:contains("Policies")`).should('not.have.class', 'pf-m-current');
+        cy.get(`${selectors.tab}:contains("Deployments")`).should('not.have.class', 'pf-m-current');
+        cy.get(`${selectors.tab}:contains("Images")`).should('not.have.class', 'pf-m-current');
+        cy.get(`${selectors.tab}:contains("Secrets")`).should('not.have.class', 'pf-m-current');
     });
 
     it('Should filter search results', () => {
-        cy.wait('@globalSearchOptions');
-        cy.get(selectors.globalSearch.input).type('Cluster:{enter}', {
-            force: true,
-        });
-        cy.get(selectors.globalSearch.input).type('remote{enter}', {
-            force: true,
-        });
-        cy.wait('@globalSearchResults');
-        cy.get(selectors.globalSearchResults.header).should('not.have.text', '0 search results');
+        visitSearch();
+        searchWithFixture([['Cluster:', 'remote']], 'search/globalSearchResults.json');
+
+        cy.get(selectors.globalSearchResults.header).should('have.text', '4 search results');
     });
 
     it('Should send you to the Violations page', () => {
-        cy.wait('@globalSearchOptions');
-        cy.get(selectors.globalSearch.input).type('Cluster:{enter}', {
-            force: true,
-        });
-        cy.get(selectors.globalSearch.input).type('remote{enter}', {
-            force: true,
-        });
-        cy.wait('@globalSearchResults');
-        cy.get(selectors.viewOnViolationsLabelChip).click();
+        visitSearch();
+        searchWithFixture([['Cluster:', 'remote']], 'search/globalSearchResults.json');
+
+        cy.get(`section#All ${selectors.viewOnChip}:contains("VIOLATIONS")`).click();
+        // TODO because 404 for /v1/alerts/6f68ef75-a96d-4121-ad89-92cf8cde0062
+        // replace button with anchor and assert on href attribute?
         cy.location('pathname').should(
             'eq',
             '/main/violations/6f68ef75-a96d-4121-ad89-92cf8cde0062'
@@ -65,41 +69,32 @@ describe.skip('Global Search Modal', () => {
     });
 
     it('Should send you to the Risk page', () => {
-        cy.wait('@globalSearchOptions');
-        cy.get(selectors.globalSearch.input).type('Cluster:{enter}', {
-            force: true,
-        });
-        cy.get(selectors.globalSearch.input).type('remote{enter}', {
-            force: true,
-        });
-        cy.wait('@globalSearchResults');
-        cy.get(selectors.viewOnRiskLabelChip).click();
+        visitSearch();
+        searchWithFixture([['Cluster:', 'remote']], 'search/globalSearchResults.json');
+
+        cy.get(`section#All ${selectors.viewOnChip}:contains("RISK")`).click();
+        // TODO because 404 for /v1/deploymentswithrisk/ppqqu24i8x16j7annv2bjphyy
+        // replace button with anchor and assert on href attribute?
         cy.location('pathname').should('eq', '/main/risk/ppqqu24i8x16j7annv2bjphyy');
     });
 
     it('Should send you to the Policies page', () => {
-        cy.wait('@globalSearchOptions');
-        cy.get(selectors.globalSearch.input).type('Cluster:{enter}', {
-            force: true,
-        });
-        cy.get(selectors.globalSearch.input).type('remote{enter}', {
-            force: true,
-        });
-        cy.wait('@globalSearchResults');
-        cy.get(selectors.viewOnPoliciesLabelChip).click();
+        visitSearch();
+        searchWithFixture([['Cluster:', 'remote']], 'search/globalSearchResults.json');
+
+        cy.get(`section#All ${selectors.viewOnChip}:contains("POLICIES")`).click();
+        // TODO because 404 for /v1/policies/0ea8d235-b02a-41ee-a61d-edcb2c1b0eac
+        // replace button with anchor and assert on href attribute?
         cy.location('pathname').should('eq', '/main/policies/0ea8d235-b02a-41ee-a61d-edcb2c1b0eac');
     });
 
     it('Should send you to the Images page', () => {
-        cy.wait('@globalSearchOptions');
-        cy.get(selectors.globalSearch.input).type('Cluster:{enter}', {
-            force: true,
-        });
-        cy.get(selectors.globalSearch.input).type('remote{enter}', {
-            force: true,
-        });
-        cy.wait('@globalSearchResults');
-        cy.get(selectors.viewOnImagesLabelChip).click();
+        visitSearch();
+        searchWithFixture([['Cluster:', 'remote']], 'search/globalSearchResults.json');
+
+        cy.get(`section#All ${selectors.viewOnChip}:contains("IMAGES")`).click();
+        // TODO because could not find image for id
+        // replace button with anchor and assert on href attribute?
         cy.location('pathname').should(
             'eq',
             '/main/vulnerability-management/images/sha256:9342f82b178a4325aec19f997400e866bf7c6bf9d59dd74e1358f971159dd7b8'
