4ff06bcb1e750b26a97febc5ddfba2c452634236
Gavin Jefferies <gavin@stackrox.com>
2022-04-13 18:01:39 +0000

    env

--- .openshift-ci-migration/binary_build_commands.sh
+++ .openshift-ci-migration/binary_build_commands.sh
@@ -2,6 +2,10 @@
 
 set -euo pipefail
 
+# These are not set in the binary_build_commands env.
+export CI=true
+export OPENSHIFT_CI=true
+
 # For migration the build source needs to reside in /go/src/github.com/stackrox/rox-openshift-ci-mirror.
 # The initial migrate.sh has cloned stackrox/stackrox and cloned the target branch. 
 cp -ur /go/src/github.com/stackrox/stackrox/* /go/src/github.com/stackrox/rox-openshift-ci-mirror
