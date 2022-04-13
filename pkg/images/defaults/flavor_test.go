caf984f27a69c9cbcea4c810d4a9eb9fbf7b42b1
Jan Schnathmeier <jschnath@redhat.com>
2022-04-13 09:11:16 +0200

    Fix instance of ImageFlavorEnvName not being capitalized

--- pkg/images/defaults/flavor_test.go
+++ pkg/images/defaults/flavor_test.go
@@ -64,7 +64,7 @@ func (s *imageFlavorTestSuite) TestGetImageFlavorFromEnv() {
 
 	for envValue, testCase := range testCases {
 		s.Run(envValue, func() {
-			s.envIsolator.Setenv(imageFlavorEnvName, envValue)
+			s.envIsolator.Setenv(ImageFlavorEnvName, envValue)
 			if testCase.shouldPanicAlways {
 				s.getEnvShouldPanic()
 				return
