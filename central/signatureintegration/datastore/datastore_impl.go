552e13835d85eb20b9245a98ed59d992df0eb794
dhaus67 <89904305+dhaus67@users.noreply.github.com>
2022-04-14 19:00:11 +0200

    Do not delete signature integrations that are referenced by policies. (#1259)

--- central/signatureintegration/datastore/datastore_impl.go
+++ central/signatureintegration/datastore/datastore_impl.go
@@ -2,13 +2,16 @@ package datastore
 
 import (
 	"context"
+	"strings"
 
 	"github.com/pkg/errors"
+	policyDatastore "github.com/stackrox/rox/central/policy/datastore"
 	"github.com/stackrox/rox/central/role/resources"
 	"github.com/stackrox/rox/central/signatureintegration/store"
 	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/pkg/errox"
 	"github.com/stackrox/rox/pkg/sac"
+	"github.com/stackrox/rox/pkg/search"
 	"github.com/stackrox/rox/pkg/set"
 	"github.com/stackrox/rox/pkg/sync"
 )
@@ -17,8 +20,16 @@ var (
 	signatureSAC = sac.ForResource(resources.SignatureIntegration)
 )
 
+const (
+	// invisiblePolicyPlaceHolder will be used as a placeholder when returning policy names that have references to
+	// signature integration for policies that are invisible to the user due to missing access scopes.
+	invisiblePolicyPlaceHolder = "<hidden>"
+)
+
 type datastoreImpl struct {
 	storage store.SignatureIntegrationStore
+
+	policyStore policyDatastore.DataStore
 
 	lock sync.RWMutex
 }
@@ -109,6 +120,12 @@ func (d *datastoreImpl) RemoveSignatureIntegration(ctx context.Context, id strin
 		return err
 	}
 
+	// We want to avoid deleting a signature integration which is referenced by any policy. If that is the case,
+	// stop the deletion of the policy and return an appropriate error message to the user.
+	if err := d.verifyIntegrationIDIsNotInPolicy(ctx, id); err != nil {
+		return err
+	}
+
 	return d.storage.Delete(ctx, id)
 }
 
@@ -147,6 +164,89 @@ func (d *datastoreImpl) verifyIntegrationIDDoesNotExist(ctx context.Context, id 
 		return errox.AlreadyExists.Newf("signature integration id=%s already exists", id)
 	}
 	return nil
+}
+
+func (d *datastoreImpl) verifyIntegrationIDIsNotInPolicy(ctx context.Context, id string) error {
+	policyCtx := sac.WithGlobalAccessScopeChecker(context.Background(),
+		sac.AllowFixedScopes(sac.AccessModeScopeKeys(storage.Access_READ_ACCESS),
+			sac.ResourceScopeKeys(resources.Policy)))
+
+	policies, err := d.policyStore.GetAllPolicies(policyCtx)
+	if err != nil {
+		return errors.Wrap(err, "retrieving all policies")
+	}
+
+	var policiesContainingID []string
+	for _, p := range policies {
+		if checkIfPolicyContainsID(id, p) {
+			policiesContainingID = append(policiesContainingID, p.GetName())
+		}
+	}
+
+	if len(policiesContainingID) != 0 {
+		integration, err := d.getSignatureIntegrationByID(ctx, id)
+		if err != nil {
+			return err
+		}
+
+		// Fetch policies with the user context. Return any error that's not sac.ErrResourceAccessDenied.
+		policiesVisibleToUser, err := d.policyStore.GetAllPolicies(ctx)
+		if err != nil && !errors.Is(err, sac.ErrResourceAccessDenied) {
+			return errors.Wrap(err, "retrieving all policies visible to user")
+		}
+
+		listOfPolicies := strings.Join(removePoliciesInvisibleToUser(policiesVisibleToUser, policiesContainingID), ",")
+
+		return errox.ReferencedByAnotherObject.Newf("cannot delete signature integration %q since there are "+
+			"existing policies that reference it: [%s]", integration.GetName(), listOfPolicies)
+	}
+
+	return nil
+}
+
+func checkIfPolicyContainsID(id string, policy *storage.Policy) bool {
+	for _, section := range policy.GetPolicySections() {
+		for _, group := range section.GetPolicyGroups() {
+			// Only check values of the "Image Signature Verified By" field.
+			if group.GetFieldName() == search.ImageSignatureVerifiedBy.String() {
+				for _, v := range group.GetValues() {
+					if v.GetValue() == id {
+						return true
+					}
+				}
+			}
+		}
+	}
+	return false
+}
+
+// removePoliciesInvisibleToUser will intersect the policy names of policies which are visible to a user
+// and policies which have references of signature integrations.
+// It will return a list of policy names that are visible to the user, replacing policies
+// that have a reference but are not visibile to the user with "<hidden>".
+func removePoliciesInvisibleToUser(policiesVisibleToUser []*storage.Policy,
+	policiesWithReferences []string) []string {
+	// Get names of all policies that are accessible to the user.
+	policyNamesVisibleToUser := set.NewStringSet()
+	for _, p := range policiesVisibleToUser {
+		policyNamesVisibleToUser.Add(p.GetName())
+	}
+
+	// Ensure we only return policy names that are visible to the user.
+	var policyNames []string
+	for _, p := range policiesWithReferences {
+		if policyNamesVisibleToUser.Contains(p) {
+			policyNames = append(policyNames, p)
+		}
+	}
+
+	// If we had to skip any amount of policies, add "<hidden>" as a placeholder for non-visible policies referencing
+	// integration to the user.
+	if len(policiesWithReferences) != len(policyNames) {
+		policyNames = append(policyNames, invisiblePolicyPlaceHolder)
+	}
+
+	return policyNames
 }
 
 func getPublicKeyPEMSet(integration *storage.SignatureIntegration) set.StringSet {
