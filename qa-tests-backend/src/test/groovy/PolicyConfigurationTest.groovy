9074123133b5dac845a72370e44155d3b40176ec
gavin-stackrox <gavin@stackrox.com>
2022-04-14 14:05:40 -0700

    really fix Image remote

--- qa-tests-backend/src/test/groovy/PolicyConfigurationTest.groovy
+++ qa-tests-backend/src/test/groovy/PolicyConfigurationTest.groovy
@@ -271,7 +271,7 @@ class PolicyConfigurationTest extends BaseSpecification {
                         .setFields(PolicyFields.newBuilder()
                                 .setImageName(
                                         ImageNamePolicy.newBuilder()
-                                                .setRemote("quay.io/rhacs-eng/qa")
+                                                .setRemote("rhacs-eng/qa")
                                                 .build())
                                 .build())
                         .build()                       | DEPLOYMENTNGINX | null
