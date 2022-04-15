ec7a06290a78d5d85af42f4de865f7dea2568987
Mark Pedrotti <mark@stackrox.com>
2022-04-15 13:04:50 -0400

    Edit proto files according to example response

--- ui/apps/platform/src/types/cve.proto.ts
+++ ui/apps/platform/src/types/cve.proto.ts
@@ -1,73 +1,11 @@
 export type VulnerabilitySeverity =
+    // | 'UNKNOWN_VULNERABILITY_SEVERITY'
     | 'LOW_VULNERABILITY_SEVERITY'
     | 'MODERATE_VULNERABILITY_SEVERITY'
     | 'IMPORTANT_VULNERABILITY_SEVERITY'
     | 'CRITICAL_VULNERABILITY_SEVERITY';
 
 export type VulnerabilityState = 'OBSERVED' | 'DEFERRED' | 'FALSE_POSITIVE';
-
-export type CVE = {
-    id: string;
-    cve: string;
-    operatingSystem: string;
-    cvss: number; // float
-    impactScore: number; // float
-
-    // For internal purposes only. This will only be populated prior to upsert into datastore.
-    // Cluster cves are split between k8s and istio. The type info is a relationship attribute and not a property of cve itself.
-    // TODO: Move type to relationship objects.
-
-    type: CVEType;
-    types: CVEType[];
-
-    summary: string;
-    link: string;
-    // This indicates the timestamp when the cve was first published in the cve feeds.
-    publishedOn: string; // ISO 8601 date string
-    // Time when the CVE was first seen in the system.
-    createdAt: string; // ISO 8601 date string
-    lastModified: string; // ISO 8601 date string
-    references: CVEReference[];
-
-    scoreVersion: CVEScoreVersion;
-    cvssV2: CVSSV2;
-    cvssV3: CVSSV3;
-
-    // TODO: Move suppression field out of CVE object. Maybe create equivalent dummy vulnerability requests.
-    // Unfortunately, although there exists image SAC check on legacy suppress APIs,
-    // one can snooze node and cluster type vulns, so we will have to carry over the support for node and cluster cves.
-
-    suppressed: boolean;
-    suppressActivation: string; // ISO 8601 date string
-    suppressExpiry: string; // ISO 8601 date string
-
-    distroSpecifics: Record<string, CVEDistroSpecific>;
-    severity: VulnerabilitySeverity;
-};
-
-export type CVEDistroSpecific = {
-    severity: VulnerabilitySeverity;
-    cvss: number; // float
-    scoreVersion: CVEScoreVersion;
-    cvssV2: CVSSV2;
-    cvssV3: CVSSV3;
-};
-
-export type CVEReference = {
-    URI: string;
-    tags: string[];
-};
-
-// No unset for automatic backwards compatibility
-export type CVEScoreVersion = 'V2' | 'V3' | 'UNKNOWN';
-
-export type CVEType =
-    | 'UNKNOWN_CVE'
-    | 'IMAGE_CVE'
-    | 'K8S_CVE'
-    | 'ISTIO_CVE'
-    | 'NODE_CVE'
-    | 'OPENSHIFT_CVE';
 
 export type CVSSV2 = {
     vector: string;
@@ -98,7 +36,7 @@ export type CVSSV3 = {
     exploitabilityScore: number; // float
     impactScore: number; // float
     attackVector: AttackVectorV3;
-    attackComplexity: ComplexityV3;
+    attackComplexity: AttackComplexityV3;
     privilegesRequired: PrivilegesV3;
     userInteraction: UserInteractionV3;
     scope: ScopeV3;
@@ -115,7 +53,7 @@ export type AttackVectorV3 =
     | 'ATTACK_NETWORK'
     | 'ATTACK_PHYSICAL';
 
-export type ComplexityV3 = 'COMPLEXITY_LOW' | 'COMPLEXITY_HIGH';
+export type AttackComplexityV3 = 'COMPLEXITY_LOW' | 'COMPLEXITY_HIGH';
 
 export type ImpactV3 = 'IMPACT_NONE' | 'IMPACT_LOW' | 'IMPACT_HIGH';
 
--- ui/apps/platform/src/types/node.proto.ts
+++ ui/apps/platform/src/types/node.proto.ts
@@ -12,15 +12,12 @@ export type Node = {
     taints: Taint[];
     clusterId: string;
     clusterName: string;
-    // TODO(ROX-6895): "Label" search term is ambiguous.
     labels: Record<string, string>;
     annotations: Record<string, string>;
     // When the cluster reported the node was added
     joinedAt: string; // ISO 8601 date string
-    // node internal IP addresses
     internalIpAddresses: string[];
-    // node external IP addresses
-    external_ip_addresses: string[];
+    externalIpAddresses: string[];
     // From NodeInfo
     containerRuntimeVersion: string; // deprecated, use containerRuntime.version
     containerRuntime: ContainerRuntimeInfo;
@@ -31,26 +28,15 @@ export type Node = {
     osImage: string;
     kubeletVersion: string;
     kubeProxyVersion: string;
-
     lastUpdated: string; // ISO 8601 date string
-    // Time we received an update from Kubernetes.
     k8sUpdated: string; // ISO 8601 date string
-
     scan: NodeScan;
-    // oneof set_components {
-    components: number; // int32
-    // }
-    // oneof set_cves {
-    cves: number; // int32
-    // }
-    // oneof set_fixable {
-    fixableCves: number; // int32
-    // }
-    priority: number; // int64
+    components?: number; // int32
+    cves?: number; // int32
+    fixableCves?: number; // int32
+    priority: string; // int64
     riskScore: number; // float
-    // oneof set_top_cvss {
-    topCvss: number; // float
-    // }
+    topCvss?: number; // float
 };
 
 export type NodeScan = {
@@ -63,20 +49,7 @@ export type EmbeddedNodeScanComponent = {
     name: string;
     version: string;
     vulns: EmbeddedVulnerability[];
-    priority: number; // int64
-    // oneof set_top_cvss {
-    topCvss: number; // float
-    // }
+    priority: string; // int64
+    topCvss?: number; // float
     riskScore: number; // float
-};
-
-export type NodeComponentEdge = {
-    // base 64 encoded Node:Component ids.
-    id: string;
-};
-
-export type NodeCVEEdge = {
-    // base 64 encoded Node:CVE ids.
-    id: string;
-    firstNodeOccurrence: string; // ISO 8601 date string
 };
