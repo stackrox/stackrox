dc83624d41ef63d8eb5a1560fc9be9e14c87e451
Mandar Darwatkar <mandar@stackrox.com>
2022-04-19 16:00:01 -0700

    [ROX-10235] Link namespaces and clusters in postgres (#1347)
    * Link namespaces and clusters in postgres

--- central/namespace/store/postgres/gen.go
+++ central/namespace/store/postgres/gen.go
@@ -1,3 +1,3 @@
 package postgres
 
-//go:generate pg-table-bindings-wrapper --type=storage.NamespaceMetadata --table=namespaces --search-category NAMESPACES --options-path central/namespace/index/mappings
+//go:generate pg-table-bindings-wrapper --type=storage.NamespaceMetadata --table=namespaces --search-category NAMESPACES --options-path central/namespace/index/mappings --references=clusters:storage.Cluster
