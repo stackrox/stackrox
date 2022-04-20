19b340d7dc3c6dcea538bea70de8d35c46b3501a
Aleksandr Kurlov <akurlov@redhat.com>
2022-04-20 10:03:03 +0200

    Try hard coded value

--- operator/pkg/central/extensions/reconcile_admin_password.go
+++ operator/pkg/central/extensions/reconcile_admin_password.go
@@ -118,7 +118,7 @@ func (r *reconcileAdminPasswordExtensionRun) updateStatus(status *platform.Centr
 	}
 	fmt.Printf("Password secret name: %s", r.passwordSecretName)
 	status.Central.AdminPassword.Info = r.infoUpdate
-	status.Central.AdminPassword.Reference = r.passwordSecretName
+	status.Central.AdminPassword.Reference = "central-htpasswd"
 	if status.Central.AdminPassword.SecretName != nil {
 		status.Central.AdminPassword.SecretName.Name = r.passwordSecretName
 	} else {
