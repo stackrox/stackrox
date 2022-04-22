710b865611744618fb3a900e8b3b596e6e624bf8
David Vail <dcvail17@gmail.com>
2022-04-22 16:06:48 -0400

    Reduce flake and improve assertions in Dashboard tests (#1395)

--- ui/apps/platform/cypress/helpers/main.js
+++ ui/apps/platform/cypress/helpers/main.js
@@ -3,7 +3,7 @@ import { url } from '../constants/DashboardPage';
 
 // eslint-disable-next-line import/prefer-default-export
 export function visitMainDashboard() {
-    cy.intercept('POST', api.dashboard.summaryCounts).as('summaryCounts');
+    cy.intercept('GET', api.risks.riskyDeployments).as('riskyDeployments');
     cy.visit(url);
-    cy.wait('@summaryCounts');
+    cy.wait('@riskyDeployments');
 }
