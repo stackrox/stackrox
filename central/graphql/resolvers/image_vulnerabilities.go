78394d23576bf1724f21b65b3f4644c2a68333c9
Oscar Ward <oscarward95@gmail.com>
2022-04-25 12:04:05 -0700

    refactor

--- central/graphql/resolvers/image_vulnerabilities.go
+++ central/graphql/resolvers/image_vulnerabilities.go
@@ -4,8 +4,9 @@ import (
 	"context"
 	"time"
 
+	"github.com/pkg/errors"
 	"github.com/stackrox/rox/central/metrics"
-	v1 "github.com/stackrox/rox/generated/api/v1"
+	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/pkg/features"
 	pkgMetrics "github.com/stackrox/rox/pkg/metrics"
 	"github.com/stackrox/rox/pkg/search"
@@ -18,9 +19,9 @@ func init() {
 		schema.AddType("EmbeddedImageVulnerability", []string{
 			"id: ID!",
 			"cve: String!",
-			//	"cvss: Float!",
-			//	"scoreVersion: String!",
-			//	"vectors: EmbeddedVulnerabilityVectors",
+			"cvss: Float!",
+			"scoreVersion: String!",
+			//"vectors: EmbeddedVulnerabilityVectors",
 			//	"link: String!",
 			//	"summary: String!",
 			//	"fixedByVersion: String!",
@@ -51,102 +52,56 @@ func init() {
 		schema.AddQuery("imageVulnerability(id: ID): EmbeddedImageVulnerability"),
 		schema.AddQuery("imageVulnerabilities(query: String, scopeQuery: String, pagination: Pagination): [EmbeddedImageVulnerability!]!"),
 		schema.AddQuery("imageVulnerabilityCount(query: String): Int!"),
-		//schema.AddExtraResolver("EmbeddedImageVulnerability", `unusedVarSink(query: String): Int`),
+		schema.AddExtraResolver("EmbeddedImageVulnerability", `unusedVarSink(query: String): Int`),
 	)
 }
 
-//func (resolver *Resolver) ImageVulnerability(ctx context.Context, args IDQuery) (VulnerabilityResolver, error) {
-//	//defer metrics.SetGraphQLOperationDurationTime(time.Now(), pkgMetrics.Root, "ImageVulnerability")
-//	if err := readCVEs(ctx); err != nil {
-//		return nil, err
-//	}
-//
-//	log.Errorf("osward -- ImageVulnerability")
-//	vuln, exists, err := resolver.CVEDataStore.Get(ctx, string(*args.ID))
-//	if err != nil {
-//		return nil, err
-//	} else if !exists {
-//		return nil, errors.Errorf("image cve not found: %s", string(*args.ID))
-//	}
-//	log.Errorf("osward -- vuln.GetType %s", vuln.GetType())
-//	vulnResolver, err := resolver.wrapCVE(vuln, true, nil)
-//	if err != nil {
-//		return nil, err
-//	}
-//	vulnResolver.ctx = ctx
-//	return vulnResolver, nil
-//}
+// ImageVulnerability returns a vulnerability of the given id
+func (resolver *Resolver) ImageVulnerability(ctx context.Context, args IDQuery) (VulnerabilityResolver, error) {
+	defer metrics.SetGraphQLOperationDurationTime(time.Now(), pkgMetrics.Root, "ImageVulnerability")
+	if features.PostgresDatastore.Enabled() {
+		// TODO add postgres support
+		return nil, errors.New("Resolver ImageVulnerability does not support postgres yet")
+	} else {
+		return resolver.vulnerabilityV2(ctx, args)
+	}
+}
 
 // ImageVulnerabilities resolves a set of image vulnerabilities based on a query.
 func (resolver *Resolver) ImageVulnerabilities(ctx context.Context, q PaginatedQuery) ([]VulnerabilityResolver, error) {
 	defer metrics.SetGraphQLOperationDurationTime(time.Now(), pkgMetrics.Root, "ImageVulnerabilities")
 	if features.PostgresDatastore.Enabled() {
 		// TODO add postgres support
-		return nil, nil
+		return nil, errors.New("Resolver ImageVulnerabilities does not support postgres yet")
 	} else {
-		baseQuery, err := q.AsV1QueryOrEmpty()
-		if err != nil {
-			return nil, err
-		}
-
-		scopeQuery, err := q.AsV1ScopeQueryOrEmpty()
-		if err != nil {
-			return nil, err
-		}
-
-		ctx, err = resolver.AddDistroContext(ctx, baseQuery, scopeQuery)
-		if err != nil {
-			return nil, err
-		}
-
-		newQuery, err := filterToImageVulns(baseQuery)
-		if err != nil {
-			return nil, err
-		}
-		return resolver.vulnerabilitiesV2Query(ctx, newQuery)
+		query := search.AddRawQueriesAsConjunction(q.String(),
+			search.NewQueryBuilder().AddExactMatches(search.CVEType, storage.CVE_IMAGE_CVE.String()).Query())
+		return resolver.vulnerabilitiesV2(ctx, PaginatedQuery{Query: &query, Pagination: q.Pagination})
 	}
 }
 
 // ImageVulnerabilityCount returns count of all image vulnerabilities across infrastructure
 func (resolver *Resolver) ImageVulnerabilityCount(ctx context.Context, args RawQuery) (int32, error) {
 	defer metrics.SetGraphQLOperationDurationTime(time.Now(), pkgMetrics.Root, "ImageVulnerabilityCount")
-	baseQuery, err := args.AsV1QueryOrEmpty()
-	if err != nil {
-		return 0, err
+	if features.PostgresDatastore.Enabled() {
+		// TODO add postgres support
+		return 0, errors.New("Resolver ImageVulnerabilityCount does not support postgres yet")
+	} else {
+		query := search.AddRawQueriesAsConjunction(args.String(),
+			search.NewQueryBuilder().AddExactMatches(search.CVEType, storage.CVE_IMAGE_CVE.String()).Query())
+		return resolver.vulnerabilityCountV2(ctx, RawQuery{Query: &query})
 	}
-	newQuery, err := filterToImageVulns(baseQuery)
-	if err != nil {
-		return 0, err
-	}
-	return resolver.vulnerabilityCountV2Query(ctx, newQuery)
 }
 
 // ImageVulnCounter returns a VulnerabilityCounterResolver for the input query.s
-//func (resolver *Resolver) ImageVulnCounter(ctx context.Context, args RawQuery) (*VulnerabilityCounterResolver, error) {
-//	log.Errorf("osward -- ImageVulnCounter")
-//	//defer metrics.SetGraphQLOperationDurationTime(time.Now(), pkgMetrics.Root, "VulnCounter")
-//	return resolver.vulnCounterV2(ctx, args)
-//}
-
-// filterToImageVulns adds filtering to vulnerability type `image` as a conjunction to the query for use with legacy api
-func filterToImageVulns(baseQuery *v1.Query) (*v1.Query, error) {
-	filterImageQuery := search.NewQueryBuilder().AddExactMatches("CVE Type", "image").ProtoQuery()
-	//	 &v1.Query{
-	//	Query: &v1.Query_BaseQuery{
-	//		BaseQuery: &v1.BaseQuery{
-	//			Query: &v1.BaseQuery_MatchFieldQuery{
-	//				MatchFieldQuery: &v1.MatchFieldQuery{
-	//					Field: "CVE Type",
-	//					Value: "image",
-	//				},
-	//			},
-	//		},
-	//	},
-	//}
-
-	newQuery, err := search.AddAsConjunction(filterImageQuery, baseQuery)
-	if err != nil {
-		return nil, err
+func (resolver *Resolver) ImageVulnCounter(ctx context.Context, args RawQuery) (*VulnerabilityCounterResolver, error) {
+	defer metrics.SetGraphQLOperationDurationTime(time.Now(), pkgMetrics.Root, "VulnCounter")
+	if features.PostgresDatastore.Enabled() {
+		// TODO add postgres support
+		return nil, errors.New("Resolver ImageVulnCounter does not support postgres yet")
+	} else {
+		query := search.AddRawQueriesAsConjunction(args.String(),
+			search.NewQueryBuilder().AddExactMatches(search.CVEType, storage.CVE_IMAGE_CVE.String()).Query())
+		return resolver.vulnCounterV2(ctx, RawQuery{Query: &query})
 	}
-	return newQuery, nil
 }
