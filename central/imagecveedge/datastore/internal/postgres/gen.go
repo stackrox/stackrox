ac450118c990df6fe43fc8bc27b4dd88aef81252
Viswajith Venugopal <viswajithiii@gmail.com>
2022-04-27 09:46:43 -0700

    Remove options map from generated indexers (#1439)

--- central/imagecveedge/datastore/internal/postgres/gen.go
+++ central/imagecveedge/datastore/internal/postgres/gen.go
@@ -1,3 +1,3 @@
 package postgres
 
-//go:generate pg-table-bindings-wrapper --type=storage.ImageCVEEdge --table=image_cve_relations --search-category IMAGE_VULN_EDGE --options-path central/imagecveedge/mappings  --references=images:storage.Image,image_cves:storage.CVE --join-table true
+//go:generate pg-table-bindings-wrapper --type=storage.ImageCVEEdge --table=image_cve_relations --search-category IMAGE_VULN_EDGE --references=images:storage.Image,image_cves:storage.CVE --join-table true
