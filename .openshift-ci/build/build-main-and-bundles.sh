96fc8230b5ebda0ce81e2b3cd94a0d1602ae3e5d
Gavin Jefferies <gavin@stackrox.com>
2022-04-28 22:53:06 +0000

    debug

--- .openshift-ci/build/build-main-and-bundles.sh
+++ .openshift-ci/build/build-main-and-bundles.sh
@@ -122,7 +122,11 @@ build_main_and_bundles() {
     # be in the 'src' delivered by OSCI in the final env so this can then be removed.
     git submodule update --init
 
+    "$ROOT/status.sh" || true
+
     openshift_ci_mods
+
+    "$ROOT/status.sh" || true
 
     background_build_ui
     build_cli
