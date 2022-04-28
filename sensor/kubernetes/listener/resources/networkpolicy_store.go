bdd6fcc90a2fcdd8a16964fc4d92fff8cb067cc6
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2022-04-28 14:33:17 +0200

    Add 2 basic network policy store metrics

--- sensor/kubernetes/listener/resources/networkpolicy_store.go
+++ sensor/kubernetes/listener/resources/networkpolicy_store.go
@@ -3,6 +3,7 @@ package resources
 import (
 	"github.com/stackrox/rox/pkg/labels"
 	"github.com/stackrox/rox/pkg/sync"
+	"github.com/stackrox/rox/sensor/common/detector/metrics"
 	"github.com/stackrox/rox/sensor/common/store"
 
 	"github.com/stackrox/rox/generated/storage"
@@ -112,11 +113,21 @@ func (n *networkPolicyStoreImpl) All() map[string]*storage.NetworkPolicy {
 	return result
 }
 
+func (n *networkPolicyStoreImpl) updateStateMetric() {
+	for ns, m := range n.data {
+		metrics.ObserveNetworkPolicyStoreState(ns, len(m))
+	}
+}
+
 // Delete removes network policy from the store
 func (n *networkPolicyStoreImpl) Delete(ID, ns string) {
 	n.lock.Lock()
+	defer n.updateStateMetric()
 	defer n.lock.Unlock()
-	if _, nsFound := n.data[ns]; nsFound {
+	if policies, nsFound := n.data[ns]; nsFound {
+		if policy, policyFound := policies[ID]; policyFound {
+			metrics.ObserveNetworkPolicyStoreEvent("delete", ns, len(policy.GetSpec().GetPodSelector().GetMatchLabels()))
+		}
 		delete(n.data[ns], ID)
 		if len(n.data[ns]) == 0 {
 			delete(n.data, ns)
@@ -127,11 +138,17 @@ func (n *networkPolicyStoreImpl) Delete(ID, ns string) {
 // Upsert adds or updates network policy based on the namespace and ID
 func (n *networkPolicyStoreImpl) Upsert(np *storage.NetworkPolicy) {
 	n.lock.Lock()
+	defer n.updateStateMetric()
 	defer n.lock.Unlock()
 
 	if _, nsFound := n.data[np.GetNamespace()]; !nsFound {
 		n.data[np.GetNamespace()] = make(map[string]*storage.NetworkPolicy)
 	}
+	event := "add"
+	if _, exists := n.data[np.GetNamespace()][np.GetId()]; exists {
+		event = "update"
+	}
+	metrics.ObserveNetworkPolicyStoreEvent(event, np.GetNamespace(), len(np.GetSpec().GetPodSelector().GetMatchLabels()))
 	n.data[np.GetNamespace()][np.GetId()] = np
 }
 
