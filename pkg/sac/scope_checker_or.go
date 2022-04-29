02b20653d17721ed34d39b5188adc0f14f084c29
Daniel Haus <dhaus@redhat.com>
2022-04-29 02:24:57 +0200

    Add OrScopeChecker, allowing to combine multiple ScopeCheckers in a logical disjunction.

---
+++ pkg/sac/scope_checker_or.go
@@ -0,0 +1,201 @@
+package sac
+
+import (
+	"context"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/auth/permissions"
+	"github.com/stackrox/rox/pkg/sac/effectiveaccessscope"
+)
+
+type orScopeChecker struct {
+	scopeCheckers []ScopeChecker
+}
+
+// NewOrScopeChecker returns a new scope checker wrapping the given scope checkers,
+// each result will be combined in a disjunction (OR).
+func NewOrScopeChecker(scopeCheckers ...ScopeChecker) ScopeChecker {
+	return orScopeChecker{
+		scopeCheckers: scopeCheckers,
+	}
+}
+
+func (s orScopeChecker) SubScopeChecker(keys ...ScopeKey) ScopeChecker {
+	for i := range s.scopeCheckers {
+		s.scopeCheckers[i] = s.scopeCheckers[i].SubScopeChecker(keys...)
+	}
+	return s
+}
+
+func (s orScopeChecker) PerformChecks(ctx context.Context) error {
+	var performChecksErrs *multierror.Error
+	for _, checker := range s.scopeCheckers {
+		err := checker.PerformChecks(ctx)
+		// Short-circuit on the first non-error perform check.
+		if err == nil {
+			return nil
+		}
+		performChecksErrs = multierror.Append(performChecksErrs, err)
+	}
+	return performChecksErrs
+}
+
+func (s orScopeChecker) TryAllowed(subScopeKeys ...ScopeKey) TryAllowedResult {
+	for _, checker := range s.scopeCheckers {
+		if res := checker.TryAllowed(subScopeKeys...); res == Allow {
+			return res
+		}
+	}
+	return Deny
+}
+
+func (s orScopeChecker) Allowed(ctx context.Context, subScopeKeys ...ScopeKey) (bool, error) {
+	var allowedErrs *multierror.Error
+	for _, checker := range s.scopeCheckers {
+		allowed, err := checker.Allowed(ctx, subScopeKeys...)
+		// Short-circuit on the first allowed check result.
+		if allowed {
+			return allowed, nil
+		}
+		if err != nil {
+			allowedErrs = multierror.Append(allowedErrs, err)
+		}
+	}
+
+	return false, allowedErrs
+}
+
+func (s orScopeChecker) TryAnyAllowed(subScopeKeyss [][]ScopeKey) TryAllowedResult {
+	result := Deny
+	for _, checker := range s.scopeCheckers {
+		if res := checker.TryAnyAllowed(subScopeKeyss); res == Allow {
+			return res
+		} else if res == Unknown {
+			result = Unknown
+		}
+	}
+	return result
+}
+
+func (s orScopeChecker) AnyAllowed(ctx context.Context, subScopeKeyss [][]ScopeKey) (bool, error) {
+	var anyAllowedErrs *multierror.Error
+	for _, checker := range s.scopeCheckers {
+		allowed, err := checker.AnyAllowed(ctx, subScopeKeyss)
+		// Short-circuit on the first allowed check result.
+		if allowed {
+			return allowed, nil
+		}
+		if err != nil {
+			anyAllowedErrs = multierror.Append(anyAllowedErrs, err)
+		}
+	}
+
+	return false, anyAllowedErrs
+}
+
+func (s orScopeChecker) TryAllAllowed(subScopeKeyss [][]ScopeKey) TryAllowedResult {
+	result := Deny
+	for _, checker := range s.scopeCheckers {
+		if res := checker.TryAllAllowed(subScopeKeyss); res == Allow {
+			return res
+		} else if res == Unknown {
+			result = Unknown
+		}
+	}
+	return result
+}
+
+func (s orScopeChecker) AllAllowed(ctx context.Context, subScopeKeyss [][]ScopeKey) (bool, error) {
+	var anyAllowedErrs *multierror.Error
+	for _, checker := range s.scopeCheckers {
+		allowed, err := checker.AllAllowed(ctx, subScopeKeyss)
+		// Short-circuit on the first allowed check result.
+		if allowed {
+			return allowed, nil
+		}
+		if err != nil {
+			anyAllowedErrs = multierror.Append(anyAllowedErrs, err)
+		}
+	}
+
+	return false, anyAllowedErrs
+}
+
+func (s orScopeChecker) ForClusterScopedObject(obj ClusterScopedObject) ScopeChecker {
+	for i := range s.scopeCheckers {
+		s.scopeCheckers[i] = s.scopeCheckers[i].SubScopeChecker(ClusterScopeKey(obj.GetClusterId()))
+	}
+	return s
+}
+
+func (s orScopeChecker) ForNamespaceScopedObject(obj NamespaceScopedObject) ScopeChecker {
+	for i := range s.scopeCheckers {
+		s.scopeCheckers[i] = s.scopeCheckers[i].SubScopeChecker(
+			ClusterScopeKey(obj.GetClusterId()), NamespaceScopeKey(obj.GetNamespace()))
+	}
+	return s
+}
+
+func (s orScopeChecker) AccessMode(am storage.Access) ScopeChecker {
+	for i := range s.scopeCheckers {
+		s.scopeCheckers[i] = s.scopeCheckers[i].SubScopeChecker(AccessModeScopeKey(am))
+	}
+	return s
+}
+
+func (s orScopeChecker) Resource(resource permissions.ResourceHandle) ScopeChecker {
+	for i := range s.scopeCheckers {
+		s.scopeCheckers[i] = s.scopeCheckers[i].SubScopeChecker(ResourceScopeKey(resource.GetResource()))
+	}
+	return s
+}
+
+func (s orScopeChecker) ClusterID(clusterID string) ScopeChecker {
+	for i := range s.scopeCheckers {
+		s.scopeCheckers[i] = s.scopeCheckers[i].SubScopeChecker(ClusterScopeKey(clusterID))
+	}
+	return s
+}
+
+func (s orScopeChecker) Namespace(namespace string) ScopeChecker {
+	for i := range s.scopeCheckers {
+		s.scopeCheckers[i] = s.scopeCheckers[i].SubScopeChecker(NamespaceScopeKey(namespace))
+	}
+	return s
+}
+
+func (s orScopeChecker) Check(ctx context.Context, pred ScopePredicate) (bool, error) {
+	var checkErrs *multierror.Error
+	for _, checker := range s.scopeCheckers {
+		allowed, err := checker.Check(ctx, pred)
+		if allowed {
+			return allowed, nil
+		}
+		if err != nil {
+			checkErrs = multierror.Append(checkErrs, err)
+		}
+	}
+
+	return false, checkErrs
+}
+
+func (s orScopeChecker) EffectiveAccessScope(
+	resource permissions.ResourceWithAccess) (*effectiveaccessscope.ScopeTree, error) {
+	var effectiveAccessScopeErrs *multierror.Error
+	var root = effectiveaccessscope.DenyAllEffectiveAccessScope()
+	for _, checker := range s.scopeCheckers {
+		eas, err := checker.EffectiveAccessScope(resource)
+		if eas != nil {
+			root.Merge(eas)
+			continue
+		}
+		if err != nil {
+			effectiveAccessScopeErrs = multierror.Append(effectiveAccessScopeErrs, err)
+		}
+	}
+	if effectiveAccessScopeErrs != nil {
+		return nil, effectiveAccessScopeErrs
+	}
+	return root, nil
+}
