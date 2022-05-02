81af4b933e987422a4a3838746f11d9aebdc1387
Mandar Darwatkar <mandar@stackrox.com>
2022-05-02 16:25:20 -0700

    [ROX-10236] Link deployments and namespace table (#1524)

--- central/deployment/store/postgres/gen.go
+++ central/deployment/store/postgres/gen.go
@@ -1,3 +1,3 @@
 package postgres
 
-//go:generate pg-table-bindings-wrapper --type=storage.Deployment --table=deployments --search-category DEPLOYMENTS --references=images:storage.Image
+//go:generate pg-table-bindings-wrapper --type=storage.Deployment --table=deployments --search-category DEPLOYMENTS --references=images:storage.Image,namespaces:storage.NamespaceMetadata
