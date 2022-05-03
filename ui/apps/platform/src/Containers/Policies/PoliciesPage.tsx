cbfe5f3bf76701f4bec4de157f79716818ffd2e0
David Vail <dcvail17@gmail.com>
2022-05-03 08:26:45 -0400

    Update Policies to use URL search hook (#1462)

--- ui/apps/platform/src/Containers/Policies/PoliciesPage.tsx
+++ ui/apps/platform/src/Containers/Policies/PoliciesPage.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { useHistory, useLocation, useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 
 import usePermissions from 'hooks/usePermissions';
-import { policiesBasePath } from 'routePaths';
-import { SearchFilter } from 'types/search';
+import useURLSearch from 'hooks/useURLSearch';
 
 import NotFoundMessage from 'Components/NotFoundMessage';
-import { getUrlQueryStringForSearchFilter } from 'utils/searchUtils';
 import { parsePoliciesSearchString } from './policies.utils';
 import PoliciesTablePage from './Table/PoliciesTablePage';
 import PolicyPage from './PolicyPage';
@@ -24,23 +22,15 @@ function PoliciesPage() {
      * /main/policies?s[Lifecycle Stage]=BUILD&s[Lifecycle State]=DEPLOY
      * /main/policies?s[Lifecycle State]=RUNTIME&s[Severity]=CRITICAL_SEVERITY
      */
-    const history = useHistory();
     const location = useLocation();
     const { search } = location;
-    const { pageAction, searchFilter } = parsePoliciesSearchString(search);
+    const { searchFilter, setSearchFilter } = useURLSearch();
+    const { pageAction } = parsePoliciesSearchString(search);
     const { policyId } = useParams();
 
     const { hasReadAccess, hasReadWriteAccess } = usePermissions();
     const hasReadAccessForPolicy = hasReadAccess('Policy');
     const hasWriteAccessForPolicy = hasReadWriteAccess('Policy');
-
-    function handleChangeSearchFilter(changedSearchFilter: SearchFilter) {
-        // Browser history has only the most recent search filter.
-        history.replace({
-            pathname: policiesBasePath,
-            search: getUrlQueryStringForSearchFilter(changedSearchFilter, 's'),
-        });
-    }
 
     if (!hasReadAccessForPolicy) {
         return <NotFoundMessage title="404: We couldn't find that page" />;
@@ -59,7 +49,7 @@ function PoliciesPage() {
     return (
         <PoliciesTablePage
             hasWriteAccessForPolicy={hasWriteAccessForPolicy}
-            handleChangeSearchFilter={handleChangeSearchFilter}
+            handleChangeSearchFilter={setSearchFilter}
             searchFilter={searchFilter}
         />
     );
