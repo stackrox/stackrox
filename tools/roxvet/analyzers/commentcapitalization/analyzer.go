a59006a6b0b80dff6dbe50a467d9a7054262f5c3
Jan Schnathmeier <jschnath@redhat.com>
2022-05-05 11:52:19 +0200

    Simplify

--- tools/roxvet/analyzers/commentcapitalization/analyzer.go
+++ tools/roxvet/analyzers/commentcapitalization/analyzer.go
@@ -104,13 +104,13 @@ func checkCommentCaseMatches(pass *analysis.Pass, doc *ast.CommentGroup, objectN
 		if unicode.IsLower(firstObjectLetter) && unicode.ToUpper(firstObjectLetter) == firstCommentLetter {
 			pass.Report(analysis.Diagnostic{
 				Pos:     position,
-				Message: fmt.Sprintf("Mismatching capitalization for %s %s and comment starting with %s, please change the comment to \"// %c%s\"", objectType, objectName, firstCommentWord, unicode.ToLower(firstCommentLetter), firstCommentWord[:1]),
+				Message: fmt.Sprintf("Mismatching capitalization for %s %s and comment starting with %s, please change the comment to \"// %s\"", objectType, objectName, firstCommentWord, objectName),
 			})
 		}
 		if unicode.IsLower(firstCommentLetter) && unicode.ToUpper(firstCommentLetter) == firstObjectLetter {
 			pass.Report(analysis.Diagnostic{
 				Pos:     position,
-				Message: fmt.Sprintf("Mismatching capitalization for %s %s and comment starting with %s, please change the comment to \"// %c%s\"", objectType, objectName, firstCommentWord, unicode.ToUpper(firstCommentLetter), firstCommentWord[:1]),
+				Message: fmt.Sprintf("Mismatching capitalization for %s %s and comment starting with %s, please change the comment to \"// %s\"", objectType, objectName, firstCommentWord, objectName),
 			})
 		}
 	}
