d35a2694ee422dd852c50564f02312ce32204aa8
md2119 <mandar@stackrox.com>
2022-05-05 10:08:13 -0700

    rm node-cve edge since it is unused

--- central/nodecveedge/store/postgres/gen.go
+++
@@ -1,3 +0,0 @@
-package postgres
-
-//go:generate pg-table-bindings-wrapper --type=storage.NodeCVEEdge --table=nodes_to_cves --search-category NODE_VULN_EDGE --references=nodes:storage.Node,node_cves:storage.CVE --join-table true
