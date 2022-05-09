26c6a8d1051a1e45a638014f0cb0910961f16c7c
Dave Shrewsberry <99685630+dashrews78@users.noreply.github.com>
2022-05-09 16:22:51 -0400

    Dashrews/disable flaky process baseline test (#1624)
     ignore test

--- qa-tests-backend/src/test/groovy/ProcessBaselinesTest.groovy
+++ qa-tests-backend/src/test/groovy/ProcessBaselinesTest.groovy
@@ -17,6 +17,7 @@ import org.apache.commons.lang3.StringUtils
 import org.junit.experimental.categories.Category
 
 import services.ProcessBaselineService
+import spock.lang.Ignore
 import spock.lang.Shared
 import spock.lang.Unroll
 
@@ -403,6 +404,8 @@ class ProcessBaselinesTest extends BaseSpecification {
     }
 
     @Category(BAT)
+    // TODO:  ROX-10903
+    @Ignore
     def "Processes come in after baseline deleted by API"() {
         when:
         def deployment = DEPLOYMENTS.find { it.name == deploymentName }
