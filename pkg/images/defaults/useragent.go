e052fd4627c0dc0af327b4ea5e934f1e2b69cd02
Johannes Malsam <jmalsam@redhat.com>
2022-05-10 16:03:05 +0200

    fixed typo in UserAgent comment

--- pkg/images/defaults/useragent.go
+++ pkg/images/defaults/useragent.go
@@ -8,8 +8,8 @@ import (
 	"github.com/stackrox/rox/pkg/version"
 )
 
-// UserAgent return a default value to set the UserAgent header when sending HTTP requests to K8s API Server
-// use UserAgent only in central or operator applications otherwise it may panic, for more info see defaults.GetImageFlavorFromEnv
+// UserAgent returns a default value to set the UserAgent header when sending HTTP requests to K8s API Server.
+// Use UserAgent only in central or operator applications otherwise it may panic, for more context see defaults.GetImageFlavorFromEnv
 func UserAgent() string {
 	return fmt.Sprintf("%s/v%s %s (%s/%s)", os.Args[0], version.GetMainVersion(), GetImageFlavorFromEnv().Name, runtime.GOOS, runtime.GOARCH)
 }
