bbc24c133098a5289ec93cf1b6e8c4414c3b6b16
Charmik Sheth <101146970+charmik-redhat@users.noreply.github.com>
2022-05-10 10:15:49 -0700

    Add NodeScanComponent schema type

---
+++ central/graphql/resolvers/node_components.go
@@ -0,0 +1,33 @@
+package resolvers
+
+import "github.com/stackrox/rox/pkg/utils"
+
+func init() {
+	schema := getBuilder()
+	utils.Must(
+		schema.AddType("NodeScanComponent", []string{
+			"license: License",
+			"id: ID!",
+			"name: String!",
+			"version: String!",
+			"topVuln: EmbeddedVulnerability",
+			"vulns(query: String, scopeQuery: String, pagination: Pagination): [EmbeddedVulnerability]!",
+			"vulnCount(query: String, scopeQuery: String): Int!",
+			"vulnCounter(query: String): VulnerabilityCounter!",
+			"plottedVulns(query: String): PlottedVulnerabilities!",
+			"lastScanned: Time",
+			"activeState(query: String): ActiveState",
+			"nodes(query: String, scopeQuery: String, pagination: Pagination): [Node!]!",
+			"nodeCount(query: String, scopeQuery: String): Int!",
+			"priority: Int!",
+			"source: String!",
+			"location(query: String): String!",
+			"riskScore: Float!",
+			"fixedIn: String!",
+			"unusedVarSink(query: String): Int",
+		}),
+		schema.AddQuery("component(id: ID): NodeScanComponent"),
+		schema.AddQuery("components(query: String, scopeQuery: String, pagination: Pagination): [NodeScanComponent!]!"),
+		schema.AddQuery("componentCount(query: String): Int!"),
+	)
+}
