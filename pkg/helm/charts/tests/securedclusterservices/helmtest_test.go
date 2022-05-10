9f55e2323034fb6938a539556d0901cf1e966052
Simon Bäumer <sbaumer@redhat.com>
2022-05-10 11:11:38 +0200

    Enable scanner-slim tests by default (#1062)

--- pkg/helm/charts/tests/securedclusterservices/helmtest_test.go
+++ pkg/helm/charts/tests/securedclusterservices/helmtest_test.go
@@ -3,7 +3,6 @@ package securedclusterservices
 import (
 	"testing"
 
-	helmTest "github.com/stackrox/helmtest/pkg/framework"
 	"github.com/stackrox/rox/image"
 	"github.com/stackrox/rox/pkg/buildinfo"
 	"github.com/stackrox/rox/pkg/features"
@@ -12,13 +11,7 @@ import (
 )
 
 func TestWithHelmtest(t *testing.T) {
-	additionalTestDirs := []string{}
-	if !buildinfo.ReleaseBuild {
-		additionalTestDirs = append(additionalTestDirs, "testdata/scanner-slim")
-	}
-
 	helmChartTestUtils.RunHelmTestSuite(t, "testdata/helmtest", image.SecuredClusterServicesChartPrefix, helmChartTestUtils.RunHelmTestSuiteOpts{
-		HelmTestOpts: []helmTest.LoaderOpt{helmTest.WithAdditionalTestDirs(additionalTestDirs...)},
 		MetaValuesOverridesFunc: func(values *charts.MetaValues) {
 			// TODO(ROX-8793): The tests will be enabled in a follow-up ticket because the current implementation breaks helm chart rendering.
 			if !buildinfo.ReleaseBuild {
