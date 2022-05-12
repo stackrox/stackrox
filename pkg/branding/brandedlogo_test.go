dedf9d3c39d7ea1f95940bac4480b343a38ded08
Jan Schnathmeier <jschnath@redhat.com>
2022-05-12 07:49:33 +0200

    Make hash strings hexadecimal and add commands to generate file hashes

--- pkg/branding/brandedlogo_test.go
+++ pkg/branding/brandedlogo_test.go
@@ -1,21 +1,25 @@
 package branding
 
 import (
+	"encoding/hex"
 	"testing"
 
 	"github.com/cloudflare/cfssl/scan/crypto/sha256"
 	"github.com/stretchr/testify/assert"
 )
 
-var (
-	logoRHACSBase64hash    = [32]uint8{0x7f, 0x3e, 0x82, 0x96, 0x3a, 0x70, 0x5c, 0x41, 0xca, 0xc0, 0x96, 0xb5, 0x16, 0xde, 0xe0, 0x68, 0xec, 0x1e, 0x86, 0x93, 0xd5, 0x5e, 0xc6, 0xd8, 0x36, 0x54, 0x6b, 0x12, 0xc6, 0x17, 0xe1, 0x95}
-	logoStackRoxBase64hash = [32]uint8{0x54, 0x5f, 0xa0, 0x92, 0xc7, 0x24, 0x1e, 0xc8, 0x7f, 0x1c, 0x2b, 0x7f, 0x7a, 0x79, 0x8e, 0x35, 0x7, 0x27, 0xf0, 0x3f, 0xff, 0xe5, 0x23, 0xcc, 0x7b, 0x68, 0x98, 0xf7, 0x2c, 0xf5, 0xbc, 0xe8}
+const (
+	// run "find pkg/branding/files -type f -print -exec bash -c 'base64 -w0 {} | sha256sum' \;" to generate hashes
+	// or where base64 doesn't support a -w0 option (eg. MacOS)
+	// run "find pkg/branding/files -type f -print -exec bash -c 'base64 {} | tr -d \\n | sha256sum' \;" instead
+	logoRHACSBase64hash    = "7f3e82963a705c41cac096b516dee068ec1e8693d55ec6d836546b12c617e195"
+	logoStackRoxBase64hash = "545fa092c7241ec87f1c2b7f7a798e350727f03fffe523cc7b6898f72cf5bce8"
 )
 
 func TestGetBrandedLogo(t *testing.T) {
 	tests := map[string]struct {
 		productBrandingEnv string
-		brandedLogoHash    [32]byte
+		brandedLogoHash    string
 	}{
 		"RHACS branding": {
 			productBrandingEnv: "RHACS_BRANDING",
@@ -33,7 +37,8 @@ func TestGetBrandedLogo(t *testing.T) {
 	for name, tt := range tests {
 		t.Run(name, func(t *testing.T) {
 			t.Setenv("ROX_PRODUCT_BRANDING", tt.productBrandingEnv)
-			receivedLogoHash := sha256.Sum256([]byte(GetLogoBase64()))
+			logoHashBytes := sha256.Sum256([]byte(GetLogoBase64()))
+			receivedLogoHash := hex.EncodeToString(logoHashBytes[:])
 			assert.Equal(t, tt.brandedLogoHash, receivedLogoHash)
 		})
 	}
