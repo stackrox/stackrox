61c68cee38d660d6132568d0b24a6cc9eb7689f1
Gavin Jefferies <gavin@stackrox.com>
2022-05-12 19:14:49 +0000

    proper test

--- .openshift-ci/pre_tests.py
+++ .openshift-ci/pre_tests.py
@@ -22,7 +22,7 @@ class PreSystemTests:
             [
                 "scripts/ci/lib.sh",
                 "poll_for_opensource_images",
-                str(PreSystemTests.POLL_TIMEOUT),
+                120,
             ],
             check=True,
             timeout=PreSystemTests.POLL_TIMEOUT * 1.2,
