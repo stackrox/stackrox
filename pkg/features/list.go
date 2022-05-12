abf32f1f6c7ac61262ea6d89bd37d7529f364ad5
David Vail <dcvail17@gmail.com>
2022-05-11 20:59:52 -0400

    Update feature flag name.

--- pkg/features/list.go
+++ pkg/features/list.go
@@ -46,6 +46,6 @@ var (
 	// NewPolicyCategories enables new policy categories as first-class entities.
 	NewPolicyCategories = registerFeature("Enable new policy categories as first-class entities", "ROX_NEW_POLICY_CATEGORIES", false)
 
-	// NewMetricsDashboard enables the PatternFly version of the main dashboard with Action Widgets. (used in the front-end app only)
-	NewMetricsDashboard = registerFeature("Enable PatternFly version of New Metrics Dashboard", "ROX_NEW_METRICS_DASHBOARD", false)
+	// SecurityMetricsPhaseOne enables the PatternFly version of the main dashboard with Action Widgets. (used in the front-end app only)
+	SecurityMetricsPhaseOne = registerFeature("Enable PatternFly version of Security Metrics Dashboard", "ROX_SECURITY_METRICS_PHASE_ONE", false)
 )
