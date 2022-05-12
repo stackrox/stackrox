cb82e52f43838ecde61560df4e736917cec2901a
Johannes Malsam <jmalsam@redhat.com>
2022-05-12 14:19:40 +0200

    changed SetUserAgent util to general Config util function

--- operator/pkg/utils/rest_config.go
+++ operator/pkg/utils/rest_config.go
@@ -7,10 +7,12 @@ import (
 	"github.com/stackrox/rox/pkg/images/defaults"
 	"github.com/stackrox/rox/pkg/version"
 	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
 )
 
-// SetUserAgent returns a rest.Config configured to set the UserAgent header when sending HTTP requests to K8s API Server.
-func SetUserAgent(config *rest.Config) *rest.Config {
+// GetConfigOrDie returns the default *rest.Config for the operator's kubernetes client
+func GetConfigOrDie() *rest.Config {
+	config := ctrl.GetConfigOrDie()
 	config.UserAgent = fmt.Sprintf("%s/v%s %s (%s/%s)", "rhacs-operator", version.GetMainVersion(), defaults.GetImageFlavorNameFromEnv(), runtime.GOOS, runtime.GOARCH)
 	return config
 }
