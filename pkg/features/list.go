f9da396df55a6cc855eb6704ab85c099f332ba87
David Vail <dcvail17@gmail.com>
2022-05-12 10:17:49 -0400

    Add feature flag for Security Metrics dashboard phase one. (#1619)

--- pkg/features/list.go
+++ pkg/features/list.go
@@ -45,4 +45,7 @@ var (
 
 	// NewPolicyCategories enables new policy categories as first-class entities.
 	NewPolicyCategories = registerFeature("Enable new policy categories as first-class entities", "ROX_NEW_POLICY_CATEGORIES", false)
+
+	// SecurityMetricsPhaseOne enables the PatternFly version of the main dashboard with Action Widgets. (used in the front-end app only)
+	SecurityMetricsPhaseOne = registerFeature("Enable PatternFly version of Security Metrics Dashboard", "ROX_SECURITY_METRICS_PHASE_ONE", false)
 )
