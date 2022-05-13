aed080e6fab31cbbbad01c7cfb741549664dc7c6
Khushboo Sancheti <42253461+clickboo@users.noreply.github.com>
2022-05-13 14:59:18 -0700

    ROX-9871: Adding a foreign key relationship between cluster health id… (#1694)

--- central/cluster/store/clusterhealth/postgres/gen.go
+++ central/cluster/store/clusterhealth/postgres/gen.go
@@ -1,3 +1,3 @@
 package postgres
 
-//go:generate pg-table-bindings-wrapper --type=storage.ClusterHealthStatus --table=cluster_health_status
+//go:generate pg-table-bindings-wrapper --type=storage.ClusterHealthStatus --table=cluster_health_status --references=clusters:storage.Cluster
