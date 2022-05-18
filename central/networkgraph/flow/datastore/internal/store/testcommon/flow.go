148bf7eea7dea57c3317519c665f37c5fd27f280
Yann Brillouet <ybrillou@redhat.com>
2022-05-18 09:51:59 +0200

    Restore the timestamp rounding method

--- central/networkgraph/flow/datastore/internal/store/testcommon/flow.go
+++ central/networkgraph/flow/datastore/internal/store/testcommon/flow.go
@@ -174,8 +174,8 @@ func (suite *FlowStoreTestSuite) TestRemoveAllMatching() {
 	t2 := time.Now()
 	if features.PostgresDatastore.Enabled() {
 		// Round the timestamps to the microsecond
-		t1.Truncate(1000)
-		t2.Truncate(1000)
+		t1 = time.Date(t1.Year(), t1.Month(), t1.Day(), t1.Hour(), t1.Minute(), t1.Second(), (t1.Nanosecond()/1000)*1000, t1.Location())
+		t2 = time.Date(t2.Year(), t2.Month(), t2.Day(), t2.Hour(), t2.Minute(), t2.Second(), (t2.Nanosecond()/1000)*1000, t2.Location())
 	}
 	flows := []*storage.NetworkFlow{
 		{
