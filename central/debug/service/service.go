b8bc1550bece5e373c923ab306aa07e42e75e047
Mladen Todorovic <mtodor@redhat.com>
2022-05-18 12:16:23 +0200

    Review comment - discussion_r874797933

--- central/debug/service/service.go
+++ central/debug/service/service.go
@@ -416,8 +416,7 @@ func (s *serviceImpl) getRoles(_ context.Context) (interface{}, error) {
 			Role: role,
 		}
 
-		resolvedRole, errGetResolved := s.roleDataStore.GetAndResolveRole(accessRolesCtx, role.Name)
-		if errGetResolved == nil {
+		if resolvedRole, err := s.roleDataStore.GetAndResolveRole(accessRolesCtx, role.Name); err == nil {
 			// Get better formatting of permission sets
 			diagRole.PermissionSet = map[string]string{}
 			for permName, accessRight := range resolvedRole.GetPermissions() {
