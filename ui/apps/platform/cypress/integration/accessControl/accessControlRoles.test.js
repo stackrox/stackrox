91317fbb2806992d4f37613490e404e07d4706de
David Vail <dcvail17@gmail.com>
2022-05-23 12:02:13 -0400

    Update action text in Access Control to match UX mocks. (#1781)

--- ui/apps/platform/cypress/integration/accessControl/accessControlRoles.test.js
+++ ui/apps/platform/cypress/integration/accessControl/accessControlRoles.test.js
@@ -55,7 +55,7 @@ describe('Access Control Roles', () => {
         cy.get(selectors.navLinkCurrent).should('have.text', h2);
 
         cy.contains(selectors.h2, /^\d+ results? found$/).should('exist');
-        cy.get(selectors.list.addButton).should('have.text', 'Add role');
+        cy.get(selectors.list.createButton).should('have.text', 'Create role');
 
         cy.get(`${selectors.list.th}:contains("Name")`);
         cy.get(`${selectors.list.th}:contains("Description")`);
@@ -127,9 +127,9 @@ describe('Access Control Roles', () => {
     it('adds a new role and form disables name input when editing an existing role', () => {
         visitRoles();
 
-        cy.get(selectors.list.addButton).click();
+        cy.get(selectors.list.createButton).click();
 
-        cy.get(selectors.h2).should('have.text', 'Add role');
+        cy.get(selectors.h2).should('have.text', 'Create role');
         cy.get(selectors.form.notEditableLabel).should('not.exist');
         cy.get(selectors.form.editButton).should('not.exist');
         cy.get(selectors.form.saveButton).should('be.disabled');
