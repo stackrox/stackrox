a07a618ee3d50f271086b1b6d9f29ccb9ba4221d
Tomasz Janiszewski <janiszt@gmail.com>
2022-05-23 18:15:00 +0200

    Add test for no ids

--- central/processbaseline/store/postgres/store_test.go
+++ central/processbaseline/store/postgres/store_test.go
@@ -357,11 +357,18 @@ func (s *ProcessBaselinesStoreSuite) TestSACGetMany() {
 	} {
 		s.T().Run(fmt.Sprintf("with %s", name), func(t *testing.T) {
 			actual, missingIndices, err := s.store.GetMany(ctxs[name], []string{objA.GetId(), objB.GetId()})
-			s.NoError(err)
+			assert.NoError(t, err)
 			assert.Equal(t, expected.elems, actual)
 			assert.Equal(t, expected.missingIndices, missingIndices)
 		})
 	}
+
+	s.T().Run("with no ids", func(t *testing.T) {
+		actual, missingIndices, err := s.store.GetMany(withAllAccessCtx, []string{})
+		assert.Nil(t, err)
+		assert.Nil(t, actual)
+		assert.Nil(t, missingIndices)
+	})
 }
 
 const (
