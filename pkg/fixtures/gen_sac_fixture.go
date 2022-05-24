65f6d195c035579ae41da5c25032a6a94b9ac7b5
Daniel Haus <dhaus@redhat.com>
2022-05-24 04:00:07 +0200

    Add integration tests + fixture for process baseline.

--- pkg/fixtures/gen_sac_fixture.go
+++ pkg/fixtures/gen_sac_fixture.go
@@ -14,6 +14,7 @@ import (
 //go:generate genny -in=$GOFILE -imp=github.com/stackrox/rox/generated/storage -out=service_account_$GOFILE gen "Resource=*storage.ServiceAccount"
 //go:generate genny -in=$GOFILE -imp=github.com/stackrox/rox/generated/storage -out=risk_$GOFILE gen "Resource=*storage.Risk"
 //go:generate genny -in=$GOFILE -imp=github.com/stackrox/rox/generated/storage -out=process_baseline_result_$GOFILE gen "Resource=*storage.ProcessBaselineResults"
+//go:generate genny -in=$GOFILE -imp=github.com/stackrox/rox/generated/storage -out=process_baseline_$GOFILE gen "Resource=*storage.ProcessBaseline"
 type Resource generic.Type
 
 // GetSACTestResourceSet returns a set of mock Resource that can be used
