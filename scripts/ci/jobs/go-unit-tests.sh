ab85b1625fbac64b0ea256adcec3f88518a31d70
Gavin Jefferies <gavin@stackrox.com>
2022-05-25 16:26:00 -0700

    RS-519: Onboard unit tests to OpenShift CI (#1843)

---
+++ scripts/ci/jobs/go-unit-tests.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../../.. && pwd)"
+source "$ROOT/scripts/ci/lib.sh"
+
+set -euo pipefail
+
+go_unit_tests() {
+    info "Starting go unit tests (GOTAGS=${GOTAGS})"
+    if [[ "$GOTAGS" == "release" ]]; then
+        export ROX_IMAGE_FLAVOR=stackrox.io
+    else
+        export ROX_IMAGE_FLAVOR=development_build
+    fi
+    make go-unit-tests GOTAGS="${GOTAGS}"
+    
+    info "Saving junit XML report"
+    make generate-junit-reports
+    store_test_results junit-reports reports
+}
+
+go_unit_tests "$*"
