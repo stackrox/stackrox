845230ec3f46c25d8908038aeb3105cccdc1e2e9
Gavin Jefferies <gavin@stackrox.com>
2022-05-28 00:03:34 +0000

    OSCI: handle 524 from definitions.io

--- image/fetch-stackrox-data.sh
+++ image/fetch-stackrox-data.sh
@@ -4,16 +4,24 @@
 
 set -euo pipefail
 
+wget_with_retries() {
+    wget --tries=5 --retry-on-http-error=524 -O "$1" "$2"
+}
+
+quiet_wget_with_retries() {
+    wget -q --tries=5 --retry-on-http-error=524 -O "$1" "$2"
+}
+
 fetch_stackrox_data() {
     mkdir -p /stackrox-data/cve/istio
-    wget -O /stackrox-data/cve/istio/checksum "https://definitions.stackrox.io/cve/istio/checksum"
-    wget -O /stackrox-data/cve/istio/cve-list.json "https://definitions.stackrox.io/cve/istio/cve-list.json"
+    wget_with_retries /stackrox-data/cve/istio/checksum "https://definitions.stackrox.io/cve/istio/checksum"
+    wget_with_retries /stackrox-data/cve/istio/cve-list.json "https://definitions.stackrox.io/cve/istio/cve-list.json"
 
     mkdir -p /tmp/external-networks
     local latest_prefix
-    latest_prefix="$(wget -q https://definitions.stackrox.io/external-networks/latest_prefix -O -)"
-    wget -O /tmp/external-networks/checksum "https://definitions.stackrox.io/${latest_prefix}/checksum"
-    wget -O /tmp/external-networks/networks "https://definitions.stackrox.io/${latest_prefix}/networks"
+    latest_prefix="$(quiet_wget_with_retries - https://definitions.stackrox.io/external-networks/latest_prefix)"
+    wget_with_retries /tmp/external-networks/checksum "https://definitions.stackrox.io/${latest_prefix}/checksum"
+    wget_with_retries /tmp/external-networks/networks "https://definitions.stackrox.io/${latest_prefix}/networks"
     test -s /tmp/external-networks/checksum
     test -s /tmp/external-networks/networks
     mkdir /stackrox-data/external-networks
