02f080d52409cb3946e3abd1e0ffbd2cfea9c7ef
Gavin Jefferies <gavin@stackrox.com>
2022-05-28 01:33:17 +0000

    one dot too far

--- image/fetch-stackrox-data.sh
+++ image/fetch-stackrox-data.sh
@@ -2,7 +2,7 @@
 
 # Fetches data used by the stackrox:main image
 
-ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../.. && pwd)"
+ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. && pwd)"
 source "$ROOT/scripts/ci/lib.sh"
 
 set -euo pipefail
