d89688e0b86c4f40fa6e310031056fb3b66d7834
Connor Gorman <cgorman@redhat.com>
2022-05-31 10:59:14 -0700

    ROX-9655: Hook up integrationhealth to Postgres

---
+++ central/integrationhealth/store/store.go
@@ -0,0 +1,14 @@
+package store
+
+import (
+	"context"
+
+	"github.com/stackrox/rox/generated/storage"
+)
+
+type Store interface {
+	Get(ctx context.Context, id string) (*storage.IntegrationHealth, bool, error)
+	Upsert(ctx context.Context, obj *storage.IntegrationHealth) error
+	Delete(ctx context.Context, id string) error
+	Walk(ctx context.Context, fn func(obj *storage.IntegrationHealth) error) error
+}
