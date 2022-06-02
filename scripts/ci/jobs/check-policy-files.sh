81acbd0b9ea6bca130a3c196ab1808c6da3ab86a
house-d <105243888+house-d@users.noreply.github.com>
2022-06-02 11:43:03 -0600

    turn off shell debug logging

--- scripts/ci/jobs/check-policy-files.sh
+++ scripts/ci/jobs/check-policy-files.sh
@@ -3,7 +3,7 @@
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../../.. && pwd)"
 source "$ROOT/scripts/ci/lib.sh"
 
-set -evxuo pipefail
+set -euo pipefail
 
 check_policy_files() {
     info 'Ensure all JSON policies in "./pkg/defaults/policies/" are of latest version.'
@@ -14,7 +14,6 @@ check_policy_files() {
     make policyutil
     policyutil upgrade -d pkg/defaults/policies/files -o /tmp/policies-in-standard-form --ensure-read-only mitre --ensure-read-only criteria
     diff pkg/defaults/policies/files /tmp/policies-in-standard-form > /tmp/policies-diff || true
-    stat /tmp/policies-diff
     if [[ -s /tmp/policies-diff ]]; then
         echo 'error: Found policies that are not in standard form.' \
             'Check "policies-diff" for affected policies; use "policyutil" to fix them.'
