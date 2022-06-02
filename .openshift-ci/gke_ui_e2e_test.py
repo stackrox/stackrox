0c3f4a2439a91efbde3a7ff639740c1e294e0d5a
Gavin Jefferies <gavin@stackrox.com>
2022-06-02 21:31:46 +0000

    back to lb

--- .openshift-ci/gke_ui_e2e_test.py
+++ .openshift-ci/gke_ui_e2e_test.py
@@ -15,8 +15,7 @@ os.environ["ORCHESTRATOR_FLAVOR"] = "k8s"
 
 # Override test env defaults here:
 # (for defaults see: tests/e2e/lib.sh export_test_environment())
-os.environ["LOAD_BALANCER"] = "none"
-os.environ["LOCAL_PORT"] = "8000"
+os.environ["LOAD_BALANCER"] = "lb"
 os.environ["OUTPUT_FORMAT"] = "helm"
 
 ClusterTestRunner(
