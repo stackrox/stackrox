f2b0c4c75a4e29a3cbe65dd4b3b251a3b0e88bc1
house-d <105243888+house-d@users.noreply.github.com>
2022-06-03 15:24:04 -0600

    store test results

--- scripts/ci/jobs/check-mitre-bundles.sh
+++ scripts/ci/jobs/check-mitre-bundles.sh
@@ -1,47 +1,36 @@
 #!/bin/bash
 
+ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../../.. && pwd)"
+source "$ROOT/scripts/ci/lib.sh"
+
 set -xveou pipefail
 
-#  check-mitre-attack-bundle-up-to-date:
-#    executor: custom
-#    resource_class: small
-#    steps:
-#      - run:
-#          name: Skipping until ROX-8486 is resolved
-#          command: |
-#            echo "Skipping until ROX-8486 is resolved"
-#            circleci step halt
-#
-#      - run:
 #          name: Determine whether to skip MITRE ATT&CK bundle check
 #          command: |
 #            if [[ -z "${CIRCLE_TAG}" ]]; then
 #              echo "Not a tagged build, skipping MITRE ATT&CK bundle check"
 #              circleci step halt
 #            fi
-#
-#      - checkout
-#      - restore-go-mod-cache
-#      - setup-go-build-env
-#
-#      - run:
+set
 
 # shellcheck disable=SC2016
 echo 'Ensure MITRE ATT&CK bundle at "./pkg/mitre/files/mitre.json" is up-to-date. (If this fails, run `mitreutil fetch` and commit the result.)'
+
+# echo 'Skipping until ROX-8486 is resolved' && exit 0
 
 function check_mitre_attach_bundle_up_to_date() {
     make deps
     make mitre
     mitre fetch --domain enterprise --out /tmp/enterprise-mitre.json
     diff pkg/mitre/files/mitre.json /tmp/enterprise-mitre.json > /tmp/mitre-diff || true
+
+    store_test_results /tmp/mitre-diff mitre-diff
+
     if [[ -s /tmp/mitre-diff ]]; then
         echo 'error: MITRE ATT&CK bundle at 'pkg/mitre/files/mitre.json' is not up-to-date. Check "mitre-diff" for more informtaion.'
         cat /tmp/mitre-diff
         exit 1
     fi
 }
-#      - ci-artifacts/store:
-#          path: /tmp/mitre-diff
-#          destination: mitre-diff
 
 check_mitre_attach_bundle_up_to_date
