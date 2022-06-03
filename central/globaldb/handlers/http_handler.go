85c0724e11ea891f752c84255da6ec8ddb10264c
Dave Shrewsberry <dashrews@redhat.com>
2022-06-03 13:29:01 -0400

    cleanup

--- central/globaldb/handlers/http_handler.go
+++ central/globaldb/handlers/http_handler.go
@@ -56,6 +56,8 @@ func deferredRestart(ctx context.Context) {
 // RestoreDB is a handler that takes in a DB and restores Central to it
 func RestoreDB(boltDB *bolt.DB, rocksDB *rocksdb.RocksDB) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		log.Info("Starting DB restore ...")
+		
 		// This is the old v1 API.  No need to support that for Postgres
 		if features.PostgresDatastore.Enabled() {
 			logAndWriteErrorMsg(w, http.StatusInternalServerError, "api is deprecated and does not support Postgres.")
