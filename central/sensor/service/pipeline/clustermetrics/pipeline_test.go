acb4c5727ad979649bb3ca5086c31e4bdef8eb81
Stephan Heßelmann <shesselm@redhat.com>
2022-06-04 01:06:31 +0200

    Reset ClusterMetrics on disconnect
    The current implementation does not explicitly reset the cluster metrics
    upon a disconnect of the Sensor <-> Central connection. This can be an
    issue because the reported cluster metrics Central could be outdated
    without being marked as stale by Prometheus.
    
    The latter occurs only when Central itself is offline. However, if
    Sensor is offline, Central keeps the last known gauge value in memory
    and reports it to Prometheus. The solution is an explicit reset of the
    cluster metrics. With the proposed implementation, the cluster metrics
    will be reset to their zero value if the connection drops. They will
    pick up again once a new connection is established.

---
+++ central/sensor/service/pipeline/clustermetrics/pipeline_test.go
@@ -0,0 +1,59 @@
+package clustermetrics
+
+import (
+	"context"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	metricsMocks "github.com/stackrox/rox/central/sensor/service/pipeline/clustermetrics/mocks"
+	"github.com/stackrox/rox/generated/internalapi/central"
+	"github.com/stackrox/rox/pkg/fixtures"
+	"github.com/stretchr/testify/suite"
+)
+
+func TestPipeline(t *testing.T) {
+	suite.Run(t, new(PipelineTestSuite))
+}
+
+type PipelineTestSuite struct {
+	suite.Suite
+	pipeline     *pipelineImpl
+	metricsStore *metricsMocks.MockMetricsStore
+	mockCtrl     *gomock.Controller
+}
+
+func (suite *PipelineTestSuite) SetupTest() {
+	suite.mockCtrl = gomock.NewController(suite.T())
+
+	suite.metricsStore = metricsMocks.NewMockMetricsStore(suite.mockCtrl)
+	suite.pipeline = NewPipeline(suite.metricsStore).(*pipelineImpl)
+}
+
+func (suite *PipelineTestSuite) TearDownTest() {
+	suite.mockCtrl.Finish()
+}
+
+func (suite *PipelineTestSuite) TestClusterMetricsMessageFromSensor() {
+	deployment := fixtures.GetDeployment()
+	clusterID := deployment.GetClusterId()
+	expectedMetrics := &central.ClusterMetrics{NodeCount: 1, CpuCapacity: 10}
+
+	suite.metricsStore.EXPECT().Set(clusterID, expectedMetrics)
+
+	err := suite.pipeline.Run(context.Background(), clusterID, &central.MsgFromSensor{
+		Msg: &central.MsgFromSensor_ClusterMetrics{
+			ClusterMetrics: expectedMetrics,
+		},
+	}, nil)
+	suite.NoError(err)
+}
+
+func (suite *PipelineTestSuite) TestClusterMetricsResetOnPipelineFinish() {
+	deployment := fixtures.GetDeployment()
+	clusterID := deployment.GetClusterId()
+	expectedMetrics := &central.ClusterMetrics{}
+
+	suite.metricsStore.EXPECT().Set(clusterID, expectedMetrics)
+
+	suite.pipeline.OnFinish(clusterID)
+}
