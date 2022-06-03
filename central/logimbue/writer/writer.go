87c1338ae61d45ee0e870e40e74a51bd5c9812fe
Connor Gorman <cgorman@redhat.com>
2022-06-02 20:36:09 -0700

    Fix

--- central/logimbue/writer/writer.go
+++ central/logimbue/writer/writer.go
@@ -1,11 +1,26 @@
 package writer
 
 import (
+	"encoding/json"
 	"io"
 
 	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/pkg/jsonutil"
 )
+
+type safeRawMessage []byte
+
+// MarshalJSON returns m as the JSON encoding of m.
+func (m safeRawMessage) MarshalJSON() ([]byte, error) {
+	if m == nil {
+		return []byte("null"), nil
+	}
+	msg, err := json.Marshal(json.RawMessage(m))
+	if err != nil {
+		msg, err = json.Marshal(map[string]interface{}{"encodingError": err.Error(), "raw": string(m)})
+	}
+	return msg, err
+}
 
 // WriteLogs takes the LogImbue logs from the Store and writes them to Writer.
 func WriteLogs(w io.Writer, logs []*storage.LogImbue) error {
@@ -17,7 +32,7 @@ func WriteLogs(w io.Writer, logs []*storage.LogImbue) error {
 		return err
 	}
 	for _, alog := range logs {
-		err = jsonWriter.WriteObject(string(alog.Log))
+		err = jsonWriter.WriteObject(safeRawMessage(alog.Log))
 		if err != nil {
 			return err
 		}
