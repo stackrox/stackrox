b6f19299d3977f1ce837addf561c82145d871cbf
Cong Du <c-du@users.noreply.github.com>
2022-06-03 17:14:12 -0700

    temp

--- migrator/migrations/n_1_to_n_2_postgres_alerts/store.go
+++ migrator/migrations/n_1_to_n_2_postgres_alerts/store.go
@@ -671,8 +671,8 @@ func Destroy(ctx context.Context, db *pgxpool.Pool) {
 	dropTableAlerts(ctx, db)
 }
 
-// NewTestStore returns a new Store instance for testing
-func NewTestStore(ctx context.Context, db *pgxpool.Pool, gormDB *gorm.DB) Store {
+// CreateTableAndNewStore returns a new Store instance for testing
+func CreateTableAndNewStore(ctx context.Context, db *pgxpool.Pool, gormDB *gorm.DB) Store {
 	pkgSchema.ApplySchemaForTable(ctx, gormDB, baseTable)
 	return New(db)
 }
