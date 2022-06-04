d201f308e960905e740702a3912cee7e8fbb274e
Gavin Jefferies <gavin@stackrox.com>
2022-06-03 17:28:47 -0700

    OSCI: Onboard UI e2e (#1913)

--- tests/e2e/lib.sh
+++ tests/e2e/lib.sh
@@ -10,6 +10,95 @@ TEST_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../.. && pwd)"
 source "$TEST_ROOT/scripts/lib.sh"
 source "$TEST_ROOT/scripts/ci/lib.sh"
 
+deploy_stackrox() {
+    deploy_central
+
+    get_central_basic_auth_creds
+    wait_for_api
+    setup_client_TLS_certs
+
+    deploy_sensor
+    sensor_wait
+
+    # Bounce collectors to avoid restarts on initial module pull
+    kubectl -n stackrox delete pod -l app=collector --grace-period=0
+
+    sensor_wait
+}
+
+# export_test_environment() - Persist environment variables for the remainder of
+# this context (context == whatever pod or VM this test is running in)
+# Existing settings are maintained to allow override for different test flavors.
+export_test_environment() {
+    ci_export ADMISSION_CONTROLLER_UPDATES "${ADMISSION_CONTROLLER_UPDATES:-true}"
+    ci_export ADMISSION_CONTROLLER "${ADMISSION_CONTROLLER:-true}"
+    ci_export COLLECTION_METHOD "${COLLECTION_METHOD:-ebpf}"
+    ci_export GCP_IMAGE_TYPE "${GCP_IMAGE_TYPE:-COS}"
+    ci_export LOAD_BALANCER "${LOAD_BALANCER:-none}"
+    ci_export LOCAL_PORT "${LOCAL_PORT:-443}"
+    ci_export MONITORING_SUPPORT "${MONITORING_SUPPORT:-false}"
+    ci_export SCANNER_SUPPORT "${SCANNER_SUPPORT:-true}"
+
+    ci_export ROX_BASELINE_GENERATION_DURATION "${ROX_BASELINE_GENERATION_DURATION:-1m}"
+    ci_export ROX_NETWORK_BASELINE_OBSERVATION_PERIOD "${ROX_NETWORK_BASELINE_OBSERVATION_PERIOD:-2m}"
+    ci_export ROX_NEW_POLICY_CATEGORIES "${ROX_NEW_POLICY_CATEGORIES:-true}"
+    ci_export ROX_POLICIES_PATTERNFLY "${ROX_POLICIES_PATTERNFLY:-true}"
+    ci_export ROX_SECURITY_METRICS_PHASE_ONE "${ROX_SECURITY_METRICS_PHASE_ONE:-true}"
+    ci_export ROX_SYSTEM_HEALTH_PF "${ROX_SYSTEM_HEALTH_PF:-true}"
+}
+
+deploy_central() {
+    info "Deploying central"
+
+    # If we're running a nightly build or race condition check, then set CGO_CHECKS=true so that central is
+    # deployed with strict checks
+    if is_nightly_tag || pr_has_label ci-race-tests; then
+        ci_export CGO_CHECKS "true"
+    fi
+
+    if pr_has_label ci-race-tests; then
+        ci_export IS_RACE_BUILD "true"
+    fi
+
+    if [[ -z "${OUTPUT_FORMAT:-}" ]]; then
+        if pr_has_label ci-helm-deploy; then
+            ci_export OUTPUT_FORMAT helm
+        fi
+    fi
+
+    DEPLOY_DIR="deploy/${ORCHESTRATOR_FLAVOR}"
+    "$ROOT/${DEPLOY_DIR}/central.sh"
+}
+
+deploy_sensor() {
+    info "Deploying sensor"
+
+    ci_export ROX_AFTERGLOW_PERIOD "15"
+    if [[ "${OUTPUT_FORMAT:-}" == "helm" ]]; then
+        echo "Deploying Sensor using Helm ..."
+        ci_export SENSOR_HELM_DEPLOY "true"
+        ci_export ADMISSION_CONTROLLER "true"
+    else
+        echo "Deploying sensor using kubectl ... "
+        if [[ -n "${IS_RACE_BUILD:-}" ]]; then
+            # builds with -race are slow at generating the sensor bundle
+            # https://stack-rox.atlassian.net/browse/ROX-6987
+            ci_export ROXCTL_TIMEOUT "60s"
+        fi
+    fi
+
+    DEPLOY_DIR="deploy/${ORCHESTRATOR_FLAVOR}"
+    "$ROOT/${DEPLOY_DIR}/sensor.sh"
+
+    if [[ "${ORCHESTRATOR_FLAVOR}" == "openshift" ]]; then
+        # Sensor is CPU starved under OpenShift causing all manner of test failures:
+        # https://stack-rox.atlassian.net/browse/ROX-5334
+        # https://stack-rox.atlassian.net/browse/ROX-6891
+        # et al.
+        kubectl -n stackrox set resources deploy/sensor -c sensor --requests 'cpu=2' --limits 'cpu=4'
+    fi
+}
+
 get_central_basic_auth_creds() {
     info "Getting central basic auth creds"
 
