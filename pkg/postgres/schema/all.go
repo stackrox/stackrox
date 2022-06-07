efc619f119d02eb98bdd043134cc7ef6da814b9a
Cong Du <c-du@users.noreply.github.com>
2022-06-06 18:23:12 -0700

    Apply schemas after upgrade

--- pkg/postgres/schema/all.go
+++ pkg/postgres/schema/all.go
@@ -71,6 +71,7 @@ func ApplyAllSchemas(ctx context.Context, gormDB *gorm.DB) {
 		if strings.HasPrefix(rt.Schema.Table, "test_") {
 			continue
 		}
+		log.Debug("Applying schema for table %s", rt.Schema.Table)
 		pgutils.CreateTableFromModel(ctx, gormDB, rt.CreateStmt)
 	}
 }
