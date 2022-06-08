27d4735ccc50bc91832b581a5cd137bb80dbe814
Charmik Sheth <101146970+charmik-redhat@users.noreply.github.com>
2022-06-08 12:34:46 -0700

    [ROX-10811] : Add PlottedNodeVulnerabilities resolver and tie it to sub-resolvers in cluster, node and node components (#1701)

---
+++ central/graphql/resolvers/plotted_vulnerabilities_common.go
@@ -0,0 +1,63 @@
+package resolvers
+
+import (
+	"context"
+
+	v1 "github.com/stackrox/rox/generated/api/v1"
+	"github.com/stackrox/rox/pkg/search"
+)
+
+func getPlottedVulnsIdsAndFixableCount(ctx context.Context, root *Resolver, args RawQuery) ([]string, int, error) {
+	q, err := args.AsV1QueryOrEmpty()
+	if err != nil {
+		return nil, 0, err
+	}
+
+	q = tryUnsuppressedQuery(q)
+	q.Pagination = &v1.QueryPagination{
+		SortOptions: []*v1.QuerySortOption{
+			{
+				Field:    search.CVSS.String(),
+				Reversed: true,
+			},
+		},
+	}
+	all, err := root.CVEDataStore.Search(ctx, q)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	fixable, err := root.CVEDataStore.Count(ctx,
+		search.ConjunctionQuery(q, search.NewQueryBuilder().AddBools(search.Fixable, true).ProtoQuery()))
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return search.ResultsToIDs(all), fixable, nil
+}
+
+func unwrappedPlottedVulnerabilities(ctx context.Context, resolver *Resolver, cveIds []string, args PaginatedQuery) ([]*cVEResolver, error) {
+	q, err := args.AsV1QueryOrEmpty()
+	if err != nil {
+		return nil, err
+	}
+
+	if len(cveIds) == 0 {
+		return nil, nil
+	}
+
+	cvesInterface, err := paginationWrapper{
+		pv: q.GetPagination(),
+	}.paginate(cveIds, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	vulns, err := resolver.CVEDataStore.GetBatch(ctx, cvesInterface.([]string))
+	if err != nil {
+		return nil, err
+	}
+
+	vulnResolvers, err := resolver.wrapCVEs(vulns, err)
+	return vulnResolvers, err
+}
