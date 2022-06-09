37e5137117fc91c2ef9a016cd6dbd2bf5ea59c74
md2119 <mandar@stackrox.com>
2022-06-09 08:47:54 -0700

    Datastores for node graph edges

--- central/nodecomponentedge/datastore/singleton.go
+++ central/nodecomponentedge/datastore/singleton.go
@@ -1,11 +1,15 @@
 package datastore
 
 import (
-	globaldb "github.com/stackrox/rox/central/globaldb/dackbox"
+	globaldb "github.com/stackrox/rox/central/globaldb"
+	globalDackbox "github.com/stackrox/rox/central/globaldb/dackbox"
 	"github.com/stackrox/rox/central/globalindex"
 	"github.com/stackrox/rox/central/nodecomponentedge/index"
 	"github.com/stackrox/rox/central/nodecomponentedge/search"
+	"github.com/stackrox/rox/central/nodecomponentedge/store"
 	"github.com/stackrox/rox/central/nodecomponentedge/store/dackbox"
+	"github.com/stackrox/rox/central/nodecomponentedge/store/postgres"
+	"github.com/stackrox/rox/pkg/features"
 	"github.com/stackrox/rox/pkg/sync"
 )
 
@@ -16,10 +20,18 @@ var (
 )
 
 func initialize() {
-	storage := dackbox.New(globaldb.GetGlobalDackBox())
-	searcher := search.New(storage, index.New(globalindex.GetGlobalIndex()))
+	var storage store.Store
+	var searcher search.Searcher
 
-	ad = New(globaldb.GetGlobalDackBox(), storage, index.New(globalindex.GetGlobalIndex()), searcher)
+	if features.PostgresDatastore.Enabled() {
+		storage = postgres.New(globaldb.GetPostgres())
+		searcher = search.New(storage, index.New(globalindex.GetGlobalIndex()))
+	} else {
+		storage = dackbox.New(globalDackbox.GetGlobalDackBox())
+		searcher = search.New(storage, index.New(globalindex.GetGlobalIndex()))
+	}
+
+	ad = New(globalDackbox.GetGlobalDackBox(), storage, index.New(globalindex.GetGlobalIndex()), searcher)
 }
 
 // Singleton provides the interface for non-service external interaction.
