bee07b50fe221c607747c0253c3ad3c4ba5f332e
Mandar Darwatkar <mandar@stackrox.com>
2022-06-09 15:58:22 -0700

    Datastores for node graph edges (#2009)
    * Datastores for node graph edges

---
+++ central/nodecomponentcveedge/datastore/index/index.go
@@ -0,0 +1,18 @@
+package index
+
+import (
+	v1 "github.com/stackrox/rox/generated/api/v1"
+	storage "github.com/stackrox/rox/generated/storage"
+	search "github.com/stackrox/rox/pkg/search"
+	blevesearch "github.com/stackrox/rox/pkg/search/blevesearch"
+)
+
+// Indexer provides indexing functionality for storage.NodeComponentCVEEdge objects.
+type Indexer interface {
+	AddNodeComponentCVEEdge(componentcveedge *storage.NodeComponentCVEEdge) error
+	AddNodeComponentCVEEdges(componentcveedges []*storage.NodeComponentCVEEdge) error
+	Count(q *v1.Query, opts ...blevesearch.SearchOption) (int, error)
+	DeleteNodeComponentCVEEdge(id string) error
+	DeleteNodeComponentCVEEdges(ids []string) error
+	Search(q *v1.Query, opts ...blevesearch.SearchOption) ([]search.Result, error)
+}
---
+++ central/nodecomponentcveedge/datastore/singleton.go
@@ -0,0 +1,30 @@
+package datastore
+
+import (
+	"github.com/stackrox/rox/central/globaldb"
+	"github.com/stackrox/rox/central/nodecomponentcveedge/datastore/search"
+	"github.com/stackrox/rox/central/nodecomponentcveedge/datastore/store/postgres"
+	"github.com/stackrox/rox/pkg/sync"
+	"github.com/stackrox/rox/pkg/utils"
+)
+
+var (
+	once sync.Once
+
+	ds DataStore
+)
+
+func initialize() {
+	storage := postgres.New(globaldb.GetPostgres())
+	indexer := postgres.NewIndexer(globaldb.GetPostgres())
+
+	var err error
+	ds, err = New(storage, indexer, search.New(storage, indexer))
+	utils.CrashOnError(err)
+}
+
+// Singleton returns a singleton instance of cve datastore
+func Singleton() DataStore {
+	once.Do(initialize)
+	return ds
+}
