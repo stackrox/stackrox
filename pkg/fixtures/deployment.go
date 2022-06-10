c67fd1095f7e991945a76e9f5ab0d560167bb778
Yann Brillouet <ybrillou@redhat.com>
2022-06-10 10:04:06 +0200

    Prepare nginx scoped deployment for dackbox tests

--- pkg/fixtures/deployment.go
+++ pkg/fixtures/deployment.go
@@ -115,3 +115,174 @@ func GetDeployment() *storage.Deployment {
 	dep.Containers = append(dep.Containers, &storage.Container{Name: "supervulnerable", Image: types2.ToContainerImage(GetImage())})
 	return dep
 }
+
+func GetDeploymentCoreDNS_1_8_0(id string) *storage.Deployment {
+	return &storage.Deployment{
+		Id:                    id,
+		Name:                  "coredns",
+		Hash:                  7991865522150829945,
+		Type:                  "Deployment",
+		Namespace:             "kube-system",
+		NamespaceId:           "317ac0e5-2ecd-4c4d-b3a1-58f9f93909a2",
+		OrchestratorComponent: true,
+		Replicas:              2,
+		Labels:                map[string]string{"k8s-app": "kube-dns"},
+		PodLabels:             map[string]string{"k8s-app": "kube-dns"},
+		LabelSelector:         &storage.LabelSelector{MatchLabels: map[string]string{"k8s-app": "kube-dns"}},
+		Created:               &types.Timestamp{Seconds: 1643589436},
+		ClusterId:             "50d16311-13cd-4690-9fd8-9c5c88f47ad2",
+		ClusterName:           "remote",
+		Containers: []*storage.Container{
+			{
+				Id: "c40f9039-15ee-47f0-a5d4-a52f28eb0318:coredns",
+				Config: &storage.ContainerConfig{
+					Args: []string{"-conf", "/etc/coredns/Corefile"},
+				},
+				Image: &storage.ContainerImage{
+					Id:   GetCoreDNS_1_8_0().GetId(),
+					Name: GetCoreDNS_1_8_0().GetName(),
+				},
+				SecurityContext: &storage.SecurityContext{
+					DropCapabilities:       []string{"all"},
+					AddCapabilities:        []string{"NET_BIND_SERVICE"},
+					ReadOnlyRootFilesystem: true,
+				},
+				Volumes: []*storage.Volume{
+					{
+						Name:        "config-volume",
+						Source:      "coredns",
+						Destination: "/etc/coredns",
+						ReadOnly:    true,
+						Type:        "ConfigMap",
+					},
+				},
+				Ports: []*storage.PortConfig{
+					{
+						Name:          "dns",
+						ContainerPort: 53,
+						Protocol:      "UDP",
+						Exposure:      storage.PortConfig_INTERNAL,
+						ExposureInfos: []*storage.PortConfig_ExposureInfo{
+							{
+								Level:            storage.PortConfig_INTERNAL,
+								ServiceName:      "kube-dns",
+								ServiceId:        "6ec876d4-a877-4b10-97a1-8ebfa718eec6",
+								ServiceClusterIp: "10.10.0.10",
+								ServicePort:      53,
+							},
+						},
+					},
+					{
+						Name:          "dns-tcp",
+						ContainerPort: 53,
+						Protocol:      "TCP",
+						Exposure:      storage.PortConfig_INTERNAL,
+						ExposureInfos: []*storage.PortConfig_ExposureInfo{
+							{
+								Level:            storage.PortConfig_INTERNAL,
+								ServiceName:      "kube-dns",
+								ServiceId:        "6ec876d4-a877-4b10-97a1-8ebfa718eec6",
+								ServiceClusterIp: "10.10.0.10",
+								ServicePort:      53,
+							},
+						},
+					},
+					{
+						Name:          "metrics",
+						ContainerPort: 9153,
+						Protocol:      "TCP",
+						Exposure:      storage.PortConfig_INTERNAL,
+						ExposureInfos: []*storage.PortConfig_ExposureInfo{
+							{
+								Level:            storage.PortConfig_INTERNAL,
+								ServiceName:      "kube-dns",
+								ServiceId:        "6ec876d4-a877-4b10-97a1-8ebfa718eec6",
+								ServiceClusterIp: "10.10.0.10",
+								ServicePort:      9153,
+							},
+						},
+					},
+				},
+				Resources: &storage.Resources{
+					CpuCoresRequest: 0.1,
+					MemoryMbRequest: 70.0,
+					MemoryMbLimit:   170.0,
+				},
+				Name:           "coredns",
+				LivenessProbe:  &storage.LivenessProbe{Defined: true},
+				ReadinessProbe: &storage.ReadinessProbe{Defined: true},
+			},
+		},
+		Priority:                      4,
+		ServiceAccount:                "",
+		ServiceAccountPermissionLevel: storage.PermissionLevel_ELEVATED_CLUSTER_WIDE,
+		AutomountServiceAccountToken:  true,
+		Tolerations: []*storage.Toleration{
+			{
+				Key:      "CriticalAddonsOnly",
+				Operator: storage.Toleration_TOLERATION_OPERATOR_EXISTS,
+			},
+			{
+				Key:         "node-role.kubernetes.io/master",
+				TaintEffect: storage.TaintEffect_NO_SCHEDULE_TAINT_EFFECT,
+			},
+			{
+				Key:         "node-role.kubernetes.io/control-plane",
+				TaintEffect: storage.TaintEffect_NO_SCHEDULE_TAINT_EFFECT,
+			},
+		},
+		Ports: []*storage.PortConfig{
+			{
+				Name:          "dns",
+				ContainerPort: 53,
+				Protocol:      "UDP",
+				Exposure:      storage.PortConfig_INTERNAL,
+				ExposureInfos: []*storage.PortConfig_ExposureInfo{
+					{
+						Level:            storage.PortConfig_INTERNAL,
+						ServiceName:      "kube-dns",
+						ServiceId:        "6ec876d4-a877-4b10-97a1-8ebfa718eec6",
+						ServiceClusterIp: "10.10.0.10",
+						ServicePort:      53,
+					},
+				},
+			},
+			{
+				Name:          "dns-tcp",
+				ContainerPort: 53,
+				Protocol:      "TCP",
+				Exposure:      storage.PortConfig_INTERNAL,
+				ExposureInfos: []*storage.PortConfig_ExposureInfo{
+					{
+						Level:            storage.PortConfig_INTERNAL,
+						ServiceName:      "kube-dns",
+						ServiceId:        "6ec876d4-a877-4b10-97a1-8ebfa718eec6",
+						ServiceClusterIp: "10.10.0.10",
+						ServicePort:      53,
+					},
+				},
+			},
+			{
+				Name:          "metrics",
+				ContainerPort: 9153,
+				Protocol:      "TCP",
+				Exposure:      storage.PortConfig_INTERNAL,
+				ExposureInfos: []*storage.PortConfig_ExposureInfo{
+					{
+						Level:            storage.PortConfig_INTERNAL,
+						ServiceName:      "kube-dns",
+						ServiceId:        "6ec876d4-a877-4b10-97a1-8ebfa718eec6",
+						ServiceClusterIp: "10.10.0.10",
+						ServicePort:      9153,
+					},
+				},
+			},
+		},
+		StateTimestamp: 1654762976894737,
+		RiskScore:      1.9846836,
+	}
+}
+
+func GetScopedDeploymentNginX_xxx() *storage.Deployment {
+	return nil
+}
