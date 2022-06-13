4ae713a914c7a090c12dace21d6ee5b878b47d76
Connor Gorman <cgorman@redhat.com>
2022-06-13 14:42:13 -0700

    clean up

--- central/compliance/datastore/datastore.go
+++ central/compliance/datastore/datastore.go
@@ -6,15 +6,12 @@ import (
 	"github.com/stackrox/rox/central/compliance"
 	"github.com/stackrox/rox/central/compliance/datastore/internal/store"
 	"github.com/stackrox/rox/central/compliance/datastore/types"
-	v1 "github.com/stackrox/rox/generated/api/v1"
 	"github.com/stackrox/rox/generated/storage"
 )
 
 // DataStore is the interface for accessing stored compliance data
 //go:generate mockgen-wrapper
 type DataStore interface {
-	QueryControlResults(ctx context.Context, query *v1.Query) ([]*storage.ComplianceControlResult, error)
-
 	GetSpecificRunResults(ctx context.Context, clusterID, standardID, runID string, flags types.GetFlags) (types.ResultsWithStatus, error)
 	GetLatestRunResults(ctx context.Context, clusterID, standardID string, flags types.GetFlags) (types.ResultsWithStatus, error)
 	GetLatestRunResultsBatch(ctx context.Context, clusterIDs, standardIDs []string, flags types.GetFlags) (map[compliance.ClusterStandardPair]types.ResultsWithStatus, error)
