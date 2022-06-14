49a5375ab8496e1f40f979bfa5aaf39c8b821dac
David Vail <dcvail17@gmail.com>
2022-06-14 18:53:01 -0400

    Delete mystery code change...

--- ui/apps/platform/src/Containers/Clusters/ClustersPage.js
+++ ui/apps/platform/src/Containers/Clusters/ClustersPage.js
@@ -61,7 +61,6 @@ const ClustersPage = ({
                     searchCategory="CLUSTERS"
                     placeholder="Add one or more filters"
                     handleChangeSearchFilter={setSearchFilter}
-                    autoFocus={!selectedClusterId}
                 />
                 <div className="flex items-center ml-4 mr-3">
                     <HashLink
