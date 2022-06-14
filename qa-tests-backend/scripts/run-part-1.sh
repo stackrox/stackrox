8c6458cced557efbff117bfd805ca9ca63f385eb
Gavin Jefferies <gavin@stackrox.com>
2022-06-14 02:03:39 +0000

    need GCP

--- qa-tests-backend/scripts/run-part-1.sh
+++ qa-tests-backend/scripts/run-part-1.sh
@@ -3,6 +3,7 @@
 # Tests part I of qa-tests-backend. Formerly CircleCI gke-api-e2e-tests.
 
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../.. && pwd)"
+source "$ROOT/scripts/ci/gcp.sh"
 source "$ROOT/scripts/ci/lib.sh"
 source "$ROOT/scripts/ci/sensor-wait.sh"
 source "$ROOT/tests/e2e/lib.sh"
@@ -24,6 +25,7 @@ test_part_1() {
         install_built_roxctl_in_gopath
     fi
 
+    setup_gcp
     setup_deployment_env false false
     remove_existing_stackrox_resources
     setup_default_TLS_certs
