aa4fe07144097aa92234aa25d5b57778d7d696fc
Gavin Jefferies <gavin@stackrox.com>
2022-06-14 21:29:11 +0000

    workaround

--- qa-tests-backend/src/test/groovy/SummaryTest.groovy
+++ qa-tests-backend/src/test/groovy/SummaryTest.groovy
@@ -19,7 +19,7 @@ import util.Env
 class SummaryTest extends BaseSpecification {
 
     @Category([BAT])
-    @IgnoreIf({ Env.CI_JOBNAME.contains("postgres") })
+    @IgnoreIf({ Env.CI_JOBNAME.contains("postgres") || System.getenv("OPENSHIFT_CI_CLUSTER_CLAIM") == "openshift-4" })
     def "Verify TopNav counts for Nodes, Deployments, and Secrets"() {
         // https://issues.redhat.com/browse/ROX-6844
         Assume.assumeFalse(ClusterService.isOpenShift4())
@@ -102,6 +102,7 @@ class SummaryTest extends BaseSpecification {
     }
 
     @Category([BAT])
+    @IgnoreIf({ System.getenv("OPENSHIFT_CI_CLUSTER_CLAIM") == "openshift-4" })
     def "Verify namespace details"() {
         // https://issues.redhat.com/browse/ROX-6844
         Assume.assumeFalse(ClusterService.isOpenShift4())
