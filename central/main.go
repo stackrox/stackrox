f919e5225c2e76b6b8f9ac8f53042d4cdf486405
md2119 <mandar@stackrox.com>
2022-06-15 10:52:45 -0700

    register service behind feature flag

--- central/main.go
+++ central/main.go
@@ -293,7 +293,6 @@ func servicesToRegister(registry authproviders.Registry, authzTraceSink observe.
 		complianceService.Singleton(),
 		configService.Singleton(),
 		credentialExpiryService.Singleton(),
-		cveService.Singleton(),
 		debugService.New(
 			clusterDataStore.Singleton(),
 			connection.ManagerSingleton(),
@@ -349,6 +348,8 @@ func servicesToRegister(registry authproviders.Registry, authzTraceSink observe.
 	if features.PostgresDatastore.Enabled() {
 		servicesToRegister = append(servicesToRegister, imageCVEService.Singleton())
 		servicesToRegister = append(servicesToRegister, nodeCVEService.Singleton())
+	} else {
+		servicesToRegister = append(servicesToRegister, cveService.Singleton())
 	}
 
 	if features.ImageSignatureVerification.Enabled() {
