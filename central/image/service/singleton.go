c084965f069f292fac47644fd20334f7c1b7eeca
Mandar Darwatkar <mandar@stackrox.com>
2022-06-16 15:44:34 -0700

    Remove unused instances of cve datastore (#2110)
    * Remove unused usages of cve datastore

--- central/image/service/singleton.go
+++ central/image/service/singleton.go
@@ -1,14 +1,11 @@
 package service
 
 import (
-	legacyImageCVEDataStore "github.com/stackrox/rox/central/cve/datastore"
-	cveDataStore "github.com/stackrox/rox/central/cve/image/datastore"
 	"github.com/stackrox/rox/central/enrichment"
 	"github.com/stackrox/rox/central/image/datastore"
 	"github.com/stackrox/rox/central/risk/manager"
 	"github.com/stackrox/rox/central/sensor/service/connection"
 	watchedImageDataStore "github.com/stackrox/rox/central/watchedimage/datastore"
-	"github.com/stackrox/rox/pkg/features"
 	"github.com/stackrox/rox/pkg/sync"
 )
 
@@ -19,13 +16,7 @@ var (
 )
 
 func initialize() {
-	var imageCVEDataStore cveDataStore.DataStore
-	if features.PostgresDatastore.Enabled() {
-		imageCVEDataStore = cveDataStore.Singleton()
-	} else {
-		imageCVEDataStore = legacyImageCVEDataStore.Singleton()
-	}
-	as = New(datastore.Singleton(), imageCVEDataStore, watchedImageDataStore.Singleton(), manager.Singleton(), connection.ManagerSingleton(), enrichment.ImageEnricherSingleton(), enrichment.ImageMetadataCacheSingleton())
+	as = New(datastore.Singleton(), watchedImageDataStore.Singleton(), manager.Singleton(), connection.ManagerSingleton(), enrichment.ImageEnricherSingleton(), enrichment.ImageMetadataCacheSingleton())
 }
 
 // Singleton provides the instance of the Service interface to register.
