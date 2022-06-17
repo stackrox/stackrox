dd67c1d3ea8c02c78951f3171496dc3199dc0823
Gavin Jefferies <gavin@stackrox.com>
2022-06-17 17:29:20 +0000

    Port check-docs and check-scanner-collector to OpenShift CI

--- scripts/ci/gke.sh
+++ scripts/ci/gke.sh
@@ -93,6 +93,9 @@ create_cluster() {
     else
         die "Support is missing for this CI environment"
     fi
+    # . from branch names
+    tags="${tags//./-}"
+    labels="${labels//./-}"
     # lowercase
     tags="${tags,,}"
     labels="${labels,,}"
