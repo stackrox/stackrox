e0d68bb9699360dccf7eaa48f3a58a9ad0c108f6
Cong Du <c-du@users.noreply.github.com>
2022-06-17 23:28:03 -0700

    rest

--- central/authprovider/datastore/internal/store/postgres/gen.go
+++ central/authprovider/datastore/internal/store/postgres/gen.go
@@ -1,3 +1,3 @@
 package postgres
 
-//go:generate pg-table-bindings-wrapper --type=storage.AuthProvider --get-all-func
+//go:generate pg-table-bindings-wrapper --type=storage.AuthProvider --get-all-func --postgres-migration-seq 3 --migrate-from "boltdb:authProviders"
