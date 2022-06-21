d8d03ea359fa42150066b6bc8cf34c7a0c8a9efa
Tomasz Janiszewski <janiszt@gmail.com>
2022-06-21 10:12:08 +0200

    Make class abstract

--- qa-tests-backend/src/test/groovy/SACTest.groovy
+++ qa-tests-backend/src/test/groovy/SACTest.groovy
@@ -30,7 +30,7 @@ import spock.lang.Unroll
 
 @Category(BAT)
 @IgnoreIf({ Env.CI_JOBNAME.contains("postgres") })
-class SACTest extends BaseSpecification {
+abstract class SACTest extends BaseSpecification {
     static final private String DEPLOYMENTNGINX_NAMESPACE_QA1 = "sac-deploymentnginx-qa1"
     static final private String NAMESPACE_QA1 = "qa-test1"
     static final private String DEPLOYMENTNGINX_NAMESPACE_QA2 = "sac-deploymentnginx-qa2"
