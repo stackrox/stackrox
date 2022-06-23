61222180898f4ccaa7e915ea8d719016b940aa44
Mandar Darwatkar <mandar@stackrox.com>
2022-06-23 08:12:14 -0700

    ImageCVE proto (#2156)
    * ImageCVE proto

--- central/cve/image/datastore/store/postgres/gen.go
+++ central/cve/image/datastore/store/postgres/gen.go
@@ -1,3 +1,3 @@
 package postgres
 
-//go:generate pg-table-bindings-wrapper --type=storage.CVE --table=image_cves --search-category VULNERABILITIES
+//go:generate pg-table-bindings-wrapper --type=storage.ImageCVE --table=image_cves --search-category IMAGE_VULNERABILITIES
