50bde5f2932f2800bdcc16103a0da0930bbb13ab
Cong Du <c-du@users.noreply.github.com>
2022-06-23 01:39:32 -0700

    more bolt

--- central/sensorupgradeconfig/datastore/internal/store/postgres/gen.go
+++ central/sensorupgradeconfig/datastore/internal/store/postgres/gen.go
@@ -1,3 +1,3 @@
 package postgres
 
-//go:generate pg-table-bindings-wrapper --type=storage.SensorUpgradeConfig --singleton
+//go:generate pg-table-bindings-wrapper --type=storage.SensorUpgradeConfig --singleton --postgres-migration-seq 51 --migrate-from "boltdb:sensor-upgrade-config"
