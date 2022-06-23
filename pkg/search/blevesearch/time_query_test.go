587f24eb0e76bbd0ead0327f7c285eccdb8cef67
Viswajith Venugopal <viswajithiii@gmail.com>
2022-06-23 15:10:08 -0700

    Remove redundant test

--- pkg/search/blevesearch/time_query_test.go
+++ pkg/search/blevesearch/time_query_test.go
@@ -116,37 +116,6 @@ func floatToTime(val float64) time.Time {
 	return time.Unix(int64(seconds), nanos)
 }
 
-func TestParseDuration(t *testing.T) {
-	var cases = []struct {
-		value    string
-		duration time.Duration
-		valid    bool
-	}{
-		{
-			value:    "1",
-			duration: 24 * 60 * 60 * time.Second,
-			valid:    true,
-		},
-		{
-			value:    "1d",
-			duration: 24 * 60 * 60 * time.Second,
-			valid:    true,
-		},
-		{
-			value:    "lol",
-			duration: time.Second,
-			valid:    false,
-		},
-	}
-	for _, c := range cases {
-		t.Run(c.value, func(t *testing.T) {
-			duration, valid := parseDuration(c.value)
-			require.Equal(t, c.valid, valid)
-			assert.Equal(t, c.duration, duration)
-		})
-	}
-}
-
 func TestTimeQuery(t *testing.T) {
 	fakeNow := timeutil.MustParse(time.RFC3339, "2022-06-24T12:00:00Z")
 	float1dayLater := float64(fakeNow.Add(24 * time.Hour).Unix())
