98ad71d47c798753b3c6bdf6c0d5a850da5c2bf8
house-d <105243888+house-d@users.noreply.github.com>
2022-06-27 15:45:37 -0600

    remove symlink

--- scripts/ci/jobs/check-generated.sh
+++ scripts/ci/jobs/check-generated.sh
@@ -10,8 +10,6 @@ go mod tidy
 # shellcheck disable=SC2016
 echo 'Ensure that generated files are up to date. (If this fails, run `make proto-generated-srcs && make go-generated-srcs` and commit the result.)'
 function generated_files-are-up-to-date() {
-    ln -s /go/src/github.com/stackrox /go/src/github.com/rox
-
     git ls-files --others --exclude-standard >/tmp/untracked
     make proto-generated-srcs
     # Print the timestamp along with each new line of output, so we can track how long each command takes
