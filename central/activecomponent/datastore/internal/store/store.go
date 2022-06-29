6918e8acd55e202398d94e5a7ae6eff0116aab37
Connor Gorman <connorgorman@users.noreply.github.com>
2022-06-29 12:19:52 -0700

    Add contexts to ActiveComponent stores (#2251)

--- central/activecomponent/datastore/internal/store/store.go
+++ central/activecomponent/datastore/internal/store/store.go
@@ -1,16 +1,17 @@
 package store
 
 import (
+	"context"
+
 	"github.com/stackrox/rox/generated/storage"
 )
 
 // Store provides storage functionality for active component.
 //go:generate mockgen-wrapper
 type Store interface {
-	Exists(id string) (bool, error)
-	Get(id string) (*storage.ActiveComponent, bool, error)
-	GetBatch(ids []string) ([]*storage.ActiveComponent, []int, error)
-
-	UpsertBatch(activeComponents []*storage.ActiveComponent) error
-	DeleteBatch(ids ...string) error
+	Exists(ctx context.Context, id string) (bool, error)
+	Get(ctx context.Context, id string) (*storage.ActiveComponent, bool, error)
+	GetMany(ctx context.Context, ids []string) ([]*storage.ActiveComponent, []int, error)
+	UpsertMany(ctx context.Context, activeComponents []*storage.ActiveComponent) error
+	DeleteMany(ctx context.Context, ids []string) error
 }
