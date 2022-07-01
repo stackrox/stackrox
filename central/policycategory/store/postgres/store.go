ef3af7a999d1817e01e90ee99d268145279592ff
Khushboo Sancheti <42253461+clickboo@users.noreply.github.com>
2022-07-01 15:19:54 -0700

    Minor fixes to policy category sort fields and sac (#2289)

--- central/policycategory/store/postgres/store.go
+++ central/policycategory/store/postgres/store.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/gogo/protobuf/proto"
+	"github.com/hashicorp/go-multierror"
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/pkg/errors"
@@ -73,7 +74,7 @@ func New(db *pgxpool.Pool) Store {
 	}
 }
 
-func insertIntoPolicyCategories(ctx context.Context, tx pgx.Tx, obj *storage.PolicyCategory) error {
+func insertIntoPolicyCategories(ctx context.Context, batch *pgx.Batch, obj *storage.PolicyCategory) error {
 
 	serialized, marshalErr := obj.Marshal()
 	if marshalErr != nil {
@@ -88,10 +89,7 @@ func insertIntoPolicyCategories(ctx context.Context, tx pgx.Tx, obj *storage.Pol
 	}
 
 	finalStr := "INSERT INTO policy_categories (Id, Name, serialized) VALUES($1, $2, $3) ON CONFLICT(Id) DO UPDATE SET Id = EXCLUDED.Id, Name = EXCLUDED.Name, serialized = EXCLUDED.serialized"
-	_, err := tx.Exec(ctx, finalStr, values...)
-	if err != nil {
-		return err
-	}
+	batch.Queue(finalStr, values...)
 
 	return nil
 }
@@ -193,18 +191,18 @@ func (s *storeImpl) upsert(ctx context.Context, objs ...*storage.PolicyCategory)
 	defer release()
 
 	for _, obj := range objs {
-		tx, err := conn.Begin(ctx)
-		if err != nil {
+		batch := &pgx.Batch{}
+		if err := insertIntoPolicyCategories(ctx, batch, obj); err != nil {
 			return err
 		}
-
-		if err := insertIntoPolicyCategories(ctx, tx, obj); err != nil {
-			if err := tx.Rollback(ctx); err != nil {
-				return err
-			}
-			return err
+		batchResults := conn.SendBatch(ctx, batch)
+		var result *multierror.Error
+		for i := 0; i < batch.Len(); i++ {
+			_, err := batchResults.Exec()
+			result = multierror.Append(result, err)
 		}
-		if err := tx.Commit(ctx); err != nil {
+		batchResults.Close()
+		if err := result.ErrorOrNil(); err != nil {
 			return err
 		}
 	}
