4a0303a05250f500d996d763110c25fb1f460712
Linda Song <ls@stackrox.com>
2022-07-01 16:05:34 -0700

    PR comments

--- ui/apps/platform/src/Containers/PolicyManagement/PolicyManagementPage.tsx
+++ ui/apps/platform/src/Containers/PolicyManagement/PolicyManagementPage.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
-import {
-    policiesPath,
-    policyManagementBasePath,
-    deprecatedPoliciesBasePath,
-    policyCategoriesPath,
-} from 'routePaths';
+import { policiesPath, policyManagementBasePath, policyCategoriesPath } from 'routePaths';
+import useFeatureFlags from 'hooks/useFeatureFlags';
 import PoliciesPage from 'Containers/Policies/PoliciesPage';
 import PolicyCategoriesPage from 'Containers/PolicyCategories/PolicyCategoriesPage';
 
 function PolicyManagementPage() {
+    const { isFeatureFlagEnabled } = useFeatureFlags();
+    const isPolicyCategoriesEnabled = isFeatureFlagEnabled('ROX_NEW_POLICY_CATEGORIES');
     return (
         <Switch>
             <Redirect exact from={policyManagementBasePath} to={policiesPath} />
-            <Redirect exact from={deprecatedPoliciesBasePath} to={policiesPath} />
             <Route path={policiesPath}>
                 <PoliciesPage />
             </Route>
-            <Route path={policyCategoriesPath}>
-                <PolicyCategoriesPage />
-            </Route>
+            {isPolicyCategoriesEnabled && (
+                <Route path={policyCategoriesPath}>
+                    <PolicyCategoriesPage />
+                </Route>
+            )}
         </Switch>
     );
 }
