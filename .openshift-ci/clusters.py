9beb7a0c53e153ae9cd3418d9196db73f7da6584
Gavin Jefferies <gavin@stackrox.com>
2022-07-03 17:37:38 +0000

    CI: handle ci-operator teardown

--- .openshift-ci/clusters.py
+++ .openshift-ci/clusters.py
@@ -5,6 +5,7 @@ Clusters used in test
 """
 
 import os
+import signal
 import subprocess
 import time
 
@@ -23,10 +24,7 @@ class GKECluster:
     PROVISION_TIMEOUT = 20 * 60
     WAIT_TIMEOUT = 20 * 60
     TEARDOWN_TIMEOUT = 5 * 60
-    PROVISION_PATH = "scripts/ci/gke.sh"
-    WAIT_PATH = "scripts/ci/gke.sh"
-    REFRESH_PATH = "scripts/ci/gke.sh"
-    TEARDOWN_PATH = "scripts/ci/gke.sh"
+    GKE_SCRIPT = "scripts/ci/gke.sh"
 
     def __init__(self, cluster_id):
         self.cluster_id = cluster_id
@@ -34,26 +32,32 @@ class GKECluster:
 
     def provision(self):
         with subprocess.Popen(
-            [GKECluster.PROVISION_PATH, "provision_gke_cluster", self.cluster_id]
+            [GKECluster.GKE_SCRIPT, "provision_gke_cluster", self.cluster_id]
         ) as cmd:
 
             try:
                 exitstatus = cmd.wait(GKECluster.PROVISION_TIMEOUT)
                 if exitstatus != 0:
                     raise RuntimeError(f"Cluster provision failed: exit {exitstatus}")
+                # OpenShift CI sends a SIGINT when tests are canceled
+                def handler():
+                    print("Tearing down the cluster due to SIGINT")
+                    self.teardown()
+
+                signal.signal(signal.SIGINT, handler)
             except subprocess.TimeoutExpired as err:
                 popen_graceful_kill(cmd)
                 raise err
 
         subprocess.run(
-            [GKECluster.WAIT_PATH, "wait_for_cluster"],
+            [GKECluster.GKE_SCRIPT, "wait_for_cluster"],
             check=True,
             timeout=GKECluster.WAIT_TIMEOUT,
         )
 
         # pylint: disable=consider-using-with
         self.refresh_token_cmd = subprocess.Popen(
-            [GKECluster.REFRESH_PATH, "refresh_gke_token"]
+            [GKECluster.GKE_SCRIPT, "refresh_gke_token"]
         )
 
         return self
@@ -63,13 +67,14 @@ class GKECluster:
             print("Pausing teardown because /tmp/hold-cluster exists")
             time.sleep(60)
 
-        try:
-            popen_graceful_kill(self.refresh_token_cmd)
-        except Exception as err:
-            print(f"Could not terminate the token refresh: {err}")
+        if self.refresh_token_cmd is not None:
+            try:
+                popen_graceful_kill(self.refresh_token_cmd)
+            except Exception as err:
+                print(f"Could not terminate the token refresh: {err}")
 
         subprocess.run(
-            [GKECluster.TEARDOWN_PATH, "teardown_gke_cluster"],
+            [GKECluster.GKE_SCRIPT, "teardown_gke_cluster"],
             check=True,
             timeout=GKECluster.TEARDOWN_TIMEOUT,
         )
