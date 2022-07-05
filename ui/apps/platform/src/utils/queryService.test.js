8c9d42a436ca703635fc705977cca5f94f5d8aec
David Vail <dcvail17@gmail.com>
2022-07-05 07:55:10 -0400

    Add unit tests for new search term quote functionality.

--- ui/apps/platform/src/utils/queryService.test.js
+++ ui/apps/platform/src/utils/queryService.test.js
@@ -20,6 +20,32 @@ describe('queryService.', () => {
 
             expect(queryStr).toEqual('cve:CVE-2005-2541,CVE-2017-12424,CVE-2018-16402');
         });
+
+        it('correctly quote wraps exact search term values', () => {
+            const singleValueQueryObject = {
+                'Cluster ID': '8afe70aa-0092-4c3f-a744-d259e863e262',
+            };
+
+            const singleValueQueryString = queryService.objectToWhereClause(singleValueQueryObject);
+
+            expect(singleValueQueryString).toEqual(
+                'Cluster ID:"8afe70aa-0092-4c3f-a744-d259e863e262"'
+            );
+
+            const multiValueQueryObject = {
+                'Cluster ID': [
+                    '8afe70aa-0092-4c3f-a744-d259e863e262',
+                    '7c9786b3-d436-40f2-aff5-6ea758d15830',
+                    '395b243e-bd99-404b-96d1-493768f54ca3',
+                ],
+            };
+
+            const multiValueQueryString = queryService.objectToWhereClause(multiValueQueryObject);
+
+            expect(multiValueQueryString).toEqual(
+                'Cluster ID:"8afe70aa-0092-4c3f-a744-d259e863e262","7c9786b3-d436-40f2-aff5-6ea758d15830","395b243e-bd99-404b-96d1-493768f54ca3"'
+            );
+        });
     });
 
     describe('entityContextToQueryObject', () => {
