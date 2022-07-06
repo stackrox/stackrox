b9c5e07ffd06cea6f2de5fdac32c7c500b4ded4a
md2119 <mandar@stackrox.com>
2022-07-06 16:58:31 -0700

    Remove unused fields from storage.CVE

--- central/cve/fetcher/manager_impl_test.go
+++ central/cve/fetcher/manager_impl_test.go
@@ -148,7 +148,6 @@ func TestReconcileCVEsInDB(t *testing.T) {
 		{
 			CVE: &storage.CVE{
 				Id:   "CVE-1",
-				Cve:  "CVE-1",
 				Link: "https://nvd.nist.gov/vuln/detail/CVE-1",
 				Type: storage.CVE_K8S_CVE,
 			},
@@ -168,7 +167,6 @@ func TestReconcileCVEsInDB(t *testing.T) {
 		{
 			CVE: &storage.CVE{
 				Id:   "CVE-2",
-				Cve:  "CVE-2",
 				Link: "https://nvd.nist.gov/vuln/detail/CVE-2",
 				Type: storage.CVE_K8S_CVE,
 			},
@@ -185,7 +183,6 @@ func TestReconcileCVEsInDB(t *testing.T) {
 		{
 			CVE: &storage.CVE{
 				Id:   "CVE-3",
-				Cve:  "CVE-3",
 				Link: "https://nvd.nist.gov/vuln/detail/CVE-3",
 				Type: storage.CVE_K8S_CVE,
 			},
