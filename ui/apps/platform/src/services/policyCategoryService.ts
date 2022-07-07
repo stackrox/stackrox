b992b661a6323e15b33331d70abb8e72e2c06531
Mark Pedrotti <mark@stackrox.com>
2022-07-07 13:48:23 -0400

    Write frontend policyCategory service for UI

---
+++ ui/apps/platform/src/services/policyCategoryService.ts
@@ -0,0 +1,50 @@
+import axios from './instance';
+
+const policyCategoriesUrl = '/v1/policycategories';
+
+export type PolicyCategory = {
+    id: string;
+    // central/policycategory/service/service_impl.go
+    // policy category must have a name between 5 and 128 characters long with no new lines or dollar signs
+    name: string;
+    isDefault: boolean;
+};
+
+export function getPolicyCategory(id: string): Promise<PolicyCategory> {
+    return axios
+        .get<PolicyCategory>(`${policyCategoriesUrl}/${id}`)
+        .then((response) => response.data);
+}
+
+export function getPolicyCategories(): Promise<PolicyCategory[]> {
+    return axios
+        .get<{ categories: PolicyCategory[] }>(policyCategoriesUrl)
+        .then((response) => response.data.categories);
+}
+
+/*
+ * The id property of the argument has empty string value.
+ * The id property of the response has unique value assigned by backend.
+ */
+export function postPolicyCategory(policyCategory: PolicyCategory): Promise<PolicyCategory> {
+    return axios
+        .post<PolicyCategory>(policyCategoriesUrl, policyCategory)
+        .then((response) => response.data);
+}
+
+/*
+ * Because the response is empty:
+ * For success: UI is responsible to update the name property of the renamed policyCategory object.
+ * For failure: UI is responsible not to update the name property of the policyCategory object.
+ */
+export function renamePolicyCategory(id: string, newCategoryName: string): Promise<Empty> {
+    return axios
+        .put<Empty>(`${policyCategoriesUrl}/${id}`, { newCategoryName })
+        .then((response) => response.data);
+}
+
+export function deletePolicyCategory(id: string): Promise<Empty> {
+    return axios.delete<Empty>(`${policyCategoriesUrl}/${id}`).then((response) => response.data);
+}
+
+type Empty = Record<string, never>;
