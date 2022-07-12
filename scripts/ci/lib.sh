ec22aa03773a4e00049f52822d3e921601cf7704
Gavin Jefferies <gavin@stackrox.com>
2022-07-11 20:44:14 -0700

    CI: fix notifier for merge test failures (#2371)

--- scripts/ci/lib.sh
+++ scripts/ci/lib.sh
@@ -993,18 +993,15 @@ store_test_results() {
 send_slack_notice_for_failures_on_merge() {
     local exitstatus="${1:-}"
 
-    # TODO(RS-509) - remove this once it is clear why messages are not reaching #test-failures
-    set -x
-
     if ! is_OPENSHIFT_CI || [[ "$exitstatus" == "0" ]] || is_in_PR_context || is_nightly_run; then
         return 0
     fi
 
     local tag
     tag="$(make --quiet tag)"
-    [[ "$tag" =~ $RELEASE_RC_TAG_BASH_REGEX ]] || {
+    if [[ "$tag" =~ $RELEASE_RC_TAG_BASH_REGEX ]]; then
         return 0
-    }
+    fi
 
     local webhook_url="${TEST_FAILURES_NOTIFY_WEBHOOK}"
 
