5c464a147f5fdedf181a32439b5c09effa7a1e51
Frederico Bittencourt <fbittenc@redhat.com>
2022-07-12 10:59:41 +0200

    (3) Proposed interface for Graph functions + a failing test to exemplify
    This interface allows us to process create/updates/deletes and
    generate snapshots at the same time. Whenever we receive an event
    we always want to find all the segment of the cluster that needs
    to be updated.

--- sensor/common/ingestion/dependency/graph.go
+++ sensor/common/ingestion/dependency/graph.go
@@ -11,3 +11,39 @@ func NewGraph(stores *ingestion.ResourceStore) *Graph {
 		stores: stores,
 	}
 }
+
+type SnapshotNode struct {
+	Kind string
+	Object interface{}
+	Children []*SnapshotNode
+}
+
+type ClusterSnapshot struct {
+	TopLevelNodes []*SnapshotNode
+}
+
+// GenerateSnapshotFromUpsert processes a create or update to the graph and returns a snapshot of
+// every cluster segment affected by this update.
+// Empty namespace means that this is a global level resource.
+// It returns a tree with a top level resource that needs to be updated and all its dependencies.
+// For example, if a snapshot for a Role is being created, the dependency graph will look for
+// the top-most parent of this role and return all the dependencies under it. If the top-level
+// parent is a deployment, it might return a snapshot looking like:
+//   [Deployment] --> [Binding] -> [Role*]
+//               \
+//                `--> [Network Policy]
+//  *Role is the deployment that triggered the snapshot
+//
+// If the edges changed on an update, this snapshot will contain both the old and the new
+// top-level linked to the updated resource.
+func (g *Graph) GenerateSnapshotFromUpsert(kind, namespace, id string) *ClusterSnapshot {
+	return &ClusterSnapshot{}
+}
+
+// GenerateSnapshotFromDelete processes a deletion event to the graph and returns a snapshot of
+// every cluster segment affected by this update.
+// This work similarly to GenerateSnapshotFromUpsert. But it removes the edges to and from the
+// node after the snapshot is processed.
+func (g *Graph) GenerateSnapshotFromDelete(kind, namespace, id string) *ClusterSnapshot {
+	return &ClusterSnapshot{}
+}
--- sensor/common/ingestion/dependency/graph_test.go
+++ sensor/common/ingestion/dependency/graph_test.go
@@ -4,9 +4,18 @@ import (
 	"testing"
 
 	"github.com/golang/mock/gomock"
+	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/sensor/common/ingestion"
 	mocksStore "github.com/stackrox/rox/sensor/common/store/mocks"
+	"github.com/stretchr/testify/require"
 )
+
+func givenDeployment(ns, id string) *storage.Deployment {
+	return &storage.Deployment{
+		Namespace: ns,
+		Id: id,
+	}
+}
 
 func Test_AddDeploymentToGraph(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
@@ -20,5 +29,15 @@ func Test_AddDeploymentToGraph(t *testing.T) {
 		PodStore:      podStore,
 	}
 
-	NewGraph(mockResources)
+	d1 := givenDeployment("example", "d1")
+	deploymentStore.EXPECT().Get(gomock.Eq("d1")).Return(d1)
+
+	g := NewGraph(mockResources)
+
+
+	snapshot := g.GenerateSnapshotFromUpsert("Deployment", "example", "d1")
+
+	require.Len(t, snapshot.TopLevelNodes, 1)
+	require.Equal(t, "Deployment", snapshot.TopLevelNodes[0].Kind)
+	require.Len(t, snapshot.TopLevelNodes[0].Children, 0)
 }
