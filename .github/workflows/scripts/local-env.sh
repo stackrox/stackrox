ada6f39f55e8fed121d1b8719b4a75dd2b2d5501
Michaël Petrov <smartptr@gmail.com>
2022-07-12 16:03:46 +0200

    Local run improvements

--- .github/workflows/scripts/local-env.sh
+++ .github/workflows/scripts/local-env.sh
@@ -3,6 +3,9 @@
 # Sets some necessary variables up for local testing.
 # JIRA_TOKEN still has to be set manually.
 #
+# Usage example:
+#     bash local-env.sh create-cluster flavor name ...
+#
 export GITHUB_STEP_SUMMARY=/dev/stdout
 GITHUB_ACTOR="$(git config --get user.email)"
 export GITHUB_ACTOR
@@ -13,4 +16,12 @@ export main_branch=master
 
 export DRY_RUN=true
 
-source "$(git rev-parse --show-toplevel)/.github/workflows/scripts/common.sh"
+SCRIPTS_ROOT="$(git rev-parse --show-toplevel)/.github/workflows/scripts"
+
+# Supress shellcheck false warning:
+# shellcheck source=/dev/null
+source "$SCRIPTS_ROOT/common.sh"
+
+SCRIPT="$SCRIPTS_ROOT/$1.sh"
+shift
+bash "$SCRIPT" "$@"
