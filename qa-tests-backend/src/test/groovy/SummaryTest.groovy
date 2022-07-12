7ce58d9339df71974ccf74a4896a86bc3ed0789e
Mandar Darwatkar <mandar@stackrox.com>
2022-07-12 13:43:04 -0700

    Enable SummaryTest on Postgres (#2201)

--- qa-tests-backend/src/test/groovy/SummaryTest.groovy
+++ qa-tests-backend/src/test/groovy/SummaryTest.groovy
@@ -14,12 +14,11 @@ import services.NamespaceService
 import services.NodeService
 import services.SummaryService
 import spock.lang.IgnoreIf
-import util.Env
 
 class SummaryTest extends BaseSpecification {
 
     @Category([BAT])
-    @IgnoreIf({ Env.CI_JOBNAME.contains("postgres") || System.getenv("OPENSHIFT_CI_CLUSTER_CLAIM") == "openshift-4" })
+    @IgnoreIf({System.getenv("OPENSHIFT_CI_CLUSTER_CLAIM") == "openshift-4" })
     def "Verify TopNav counts for Nodes, Deployments, and Secrets"() {
         // https://issues.redhat.com/browse/ROX-6844
         Assume.assumeFalse(ClusterService.isOpenShift4())
@@ -54,7 +53,6 @@ class SummaryTest extends BaseSpecification {
     }
 
     @Category([BAT])
-    @IgnoreIf({ Env.CI_JOBNAME.contains("postgres") })
     def "Verify node details"() {
         given:
         "fetch the list of nodes"
