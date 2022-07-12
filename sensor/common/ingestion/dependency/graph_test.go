74a96cae78ecddf46f2f4b0f15d77d89a82f4a3b
Frederico Bittencourt <fbittenc@redhat.com>
2022-07-12 15:41:30 +0200

    (4) Additional test for Deployment -> Network Policy association
    This tests made me realize something: there is a coupling between
    adding things to the store and to the graph.
    
    I'm assuming that adding things to the resources store is done
    outside of the dependency graph package. And since we need to
    use the stores `Find()` functions to find the matching resources
    whenever a new resource is added, this creates an odd case:
    
    What happens if we generate a snapshot for a new resource (d1) and
    when we run `Find()` on NetworkPolicy store we find a netpol (n1)
    that isn't in the graph? This could happen if the caller didn't call
    `GenerateSnapshotFromUpsert` for n1. In this case, should we then
    add n1 to the graph and return the snapshot of d1 *with* n1? Or
    should it be ignored since the dependency graph doesn't have it?

--- sensor/common/ingestion/dependency/graph_test.go
+++ sensor/common/ingestion/dependency/graph_test.go
@@ -7,37 +7,101 @@ import (
 	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/sensor/common/ingestion"
 	mocksStore "github.com/stackrox/rox/sensor/common/store/mocks"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 )
 
-func givenDeployment(ns, id string) *storage.Deployment {
-	return &storage.Deployment{
-		Namespace: ns,
-		Id: id,
+func Test_DependencyGraphSuite(t *testing.T) {
+	suite.Run(t, new(DependencyGraphSuite))
+}
+
+type DependencyGraphSuite struct {
+	suite.Suite
+
+	mockCtrl      *gomock.Controller
+	mockResources *ingestion.ResourceStore
+
+	deploymentStore *mocksStore.MockDeploymentStore
+	netpolStore     *mocksStore.MockNetworkPolicyStore
+	podStore        *mocksStore.MockPodStore
+
+	graph *Graph
+}
+
+var _ suite.SetupTestSuite = &DependencyGraphSuite{}
+
+func (s *DependencyGraphSuite) SetupTest() {
+	s.mockCtrl = gomock.NewController(s.T())
+
+	s.deploymentStore = mocksStore.NewMockDeploymentStore(s.mockCtrl)
+	s.netpolStore = mocksStore.NewMockNetworkPolicyStore(s.mockCtrl)
+	s.podStore = mocksStore.NewMockPodStore(s.mockCtrl)
+
+	s.mockResources = &ingestion.ResourceStore{
+		Deployments:   s.deploymentStore,
+		NetworkPolicy: s.netpolStore,
+		PodStore:      s.podStore,
+	}
+
+	s.graph = NewGraph(s.mockResources)
+}
+
+func (s *DependencyGraphSuite) Test_SingleDeployment() {
+	d1 := givenDeployment("example", "d1", nil)
+	s.deploymentStore.EXPECT().Get(gomock.Eq("d1")).Return(d1)
+
+	snapshot := s.graph.GenerateSnapshotFromUpsert("Deployment", "example", "d1")
+
+	s.Require().Len(snapshot.TopLevelNodes, 1)
+	s.Require().Equal("Deployment", snapshot.TopLevelNodes[0].Kind)
+	s.Require().Len(snapshot.TopLevelNodes[0].Children, 0)
+}
+
+func (s *DependencyGraphSuite) Test_DeploymentWithOneNetPolicy() {
+	d1 := givenDeployment("example", "d1", appLabel("test"))
+	s.deploymentStore.EXPECT().Get(gomock.Eq("d1")).Return(d1)
+
+	n1 := givenNetworkPolicy("example", "n1", appLabel("test"))
+	s.netpolStore.EXPECT().Get(gomock.Eq("n1")).Return(n1)
+	s.netpolStore.EXPECT().Find(gomock.Eq("example"), gomock.Eq(appLabel("test"))).
+		Return(map[string]*storage.NetworkPolicy{"n1": n1})
+
+	results := []*ClusterSnapshot{
+		s.graph.GenerateSnapshotFromUpsert("NetworkPolicy", "example", "n1"),
+		s.graph.GenerateSnapshotFromUpsert("Deployment",  "example", "d1"),
+	}
+
+	// In this case, both snapshots should look exactly the same. Given both resources are in
+	// stores before we request `GenerateSnapshotFromUpsert`.
+	for _, resultCase := range results {
+		s.Require().Len(resultCase.TopLevelNodes, 1)
+		s.Require().Equal("Deployment", resultCase.TopLevelNodes[0].Kind)
+		s.Require().Len(resultCase.TopLevelNodes[0].Children, 1)
+		s.Require().Equal("NetworkPolicy", resultCase.TopLevelNodes[0].Children[0].Kind)
 	}
 }
 
-func Test_AddDeploymentToGraph(t *testing.T) {
-	mockCtrl := gomock.NewController(t)
-	deploymentStore := mocksStore.NewMockDeploymentStore(mockCtrl)
-	netpolStore := mocksStore.NewMockNetworkPolicyStore(mockCtrl)
-	podStore := mocksStore.NewMockPodStore(mockCtrl)
-
-	mockResources := &ingestion.ResourceStore{
-		Deployments:   deploymentStore,
-		NetworkPolicy: netpolStore,
-		PodStore:      podStore,
+func appLabel(value string) map[string]string {
+	return map[string]string{
+		"app": value,
 	}
+}
 
-	d1 := givenDeployment("example", "d1")
-	deploymentStore.EXPECT().Get(gomock.Eq("d1")).Return(d1)
+func givenDeployment(ns, id string, labels map[string]string) *storage.Deployment {
+	return &storage.Deployment{
+		Labels:    labels,
+		Namespace: ns,
+		Id:        id,
+	}
+}
 
-	g := NewGraph(mockResources)
-
-
-	snapshot := g.GenerateSnapshotFromUpsert("Deployment", "example", "d1")
-
-	require.Len(t, snapshot.TopLevelNodes, 1)
-	require.Equal(t, "Deployment", snapshot.TopLevelNodes[0].Kind)
-	require.Len(t, snapshot.TopLevelNodes[0].Children, 0)
+func givenNetworkPolicy(ns, id string, podSelector map[string]string) *storage.NetworkPolicy {
+	return &storage.NetworkPolicy{
+		Namespace: ns,
+		Id:        id,
+		Spec: &storage.NetworkPolicySpec{
+			PodSelector: &storage.LabelSelector{
+				MatchLabels: podSelector,
+			},
+		},
+	}
 }
