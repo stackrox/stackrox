395216a321d5a6d83c93a14a86f1bbaf2f566d29
Yann Brillouet <ybrillou@redhat.com>
2022-07-13 10:31:08 +0200

    Prefactor - Ensure exists return true in case of query with multiple table joins too

--- tools/generate-helpers/pg-table-bindings/test/postgres/store.go
+++ tools/generate-helpers/pg-table-bindings/test/postgres/store.go
@@ -248,7 +248,9 @@ func (s *storeImpl) upsert(ctx context.Context, objs ...*storage.TestSingleKeySt
 			_, err := batchResults.Exec()
 			result = multierror.Append(result, err)
 		}
-		batchResults.Close()
+		if err := batchResults.Close(); err != nil {
+			return err
+		}
 		if err := result.ErrorOrNil(); err != nil {
 			return err
 		}
@@ -333,7 +335,9 @@ func (s *storeImpl) Exists(ctx context.Context, key string) (bool, error) {
 	)
 
 	count, err := postgres.RunCountRequestForSchema(schema, q, s.db)
-	return count == 1, err
+	// With joins and multiple paths to the scoping resources, it can happen that the Count query for an object identifier
+	// returns more than 1, despite the fact that the identifier is unique in the table.
+	return count > 0, err
 }
 
 // Get returns the object, if it exists from the store
