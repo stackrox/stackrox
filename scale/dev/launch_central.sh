8bfdd14d4292c2e44bab39f8db6f22e7c6b1eb32
Connor Gorman <cgorman@redhat.com>
2022-07-15 10:59:22 -0700

    more resources

--- scale/dev/launch_central.sh
+++ scale/dev/launch_central.sh
@@ -10,17 +10,14 @@ if ! kubectl -n stackrox get pvc/stackrox-db > /dev/null; then
   exit 1
 fi
 
-# Create signature integrations to verify image signatures.
-"${DIR}"/signatures/create-signature-integrations.sh
-
 kubectl -n stackrox set env deploy/central MUTEX_WATCHDOG_TIMEOUT_SECS=0  ROX_SCALE_TEST=true
 if [[ $(kubectl get nodes -o json | jq '.items | length') == 1 ]]; then
   exit 0
 fi
 
-kubectl -n stackrox patch deploy/central -p '{"spec":{"template":{"spec":{"containers":[{"name":"central","resources":{"requests":{"memory":"8Gi","cpu":"4"},"limits":{"memory":"20Gi","cpu":"8"}}}]}}}}'
+kubectl -n stackrox patch deploy/central -p '{"spec":{"template":{"spec":{"containers":[{"name":"central","resources":{"requests":{"memory":"16Gi","cpu":"8"},"limits":{"memory":"16Gi","cpu":"8"}}}]}}}}'
 if [[ "$ROX_POSTGRES_DATASTORE" == "true" ]]; then
-  kubectl -n stackrox patch deploy/central-db -p '{"spec":{"template":{"spec":{"containers":[{"name":"central-db","resources":{"requests":{"memory":"8Gi","cpu":"4"},"limits":{"memory":"20Gi","cpu":"8"}}}]}}}}'
+  kubectl -n stackrox patch deploy/central-db -p '{"spec":{"template":{"spec":{"containers":[{"name":"central-db","resources":{"requests":{"memory":"32Gi","cpu":"16"},"limits":{"memory":"32Gi","cpu":"16"}}}]}}}}'
 fi
 
 ./port-forward.sh 8000
