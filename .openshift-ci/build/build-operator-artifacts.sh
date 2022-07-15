3e3e7886f41f5f026510764e43fe0b499ead4c25
Marcin Owsiany <porridge@redhat.com>
2022-07-15 11:35:35 +0200

    fix dir ref

--- .openshift-ci/build/build-operator-artifacts.sh
+++ .openshift-ci/build/build-operator-artifacts.sh
@@ -23,8 +23,8 @@ build_operator_bundle_and_binary() {
     make -C operator bundle bundle-post-process build SILENT=
     # TODO(porridge): a hack to get opm to build an index based on bundle directory before bundle image is pushed
     # The hacked opm tool will first see if a directory named as the reference exists, and if so, use its content as if it's an unpacked image of that name.
-    mkdir -p "$(make --quiet default-image-registry)"
-    cp -a build/bundle "$(make --quiet default-image-registry)/stackrox-operator-bundle:v$(make --quiet --no-print-directory -C operator tag)"
+    mkdir -p operator/"$(make --quiet default-image-registry)"
+    cp -a operator/build/bundle "operator/$(make --quiet default-image-registry)/stackrox-operator-bundle:v$(make --quiet --no-print-directory -C operator tag)"
     make -C operator index-build SKIP_INDEX_DOCKER_BUILD=--skip-build SILENT=
 }
 
