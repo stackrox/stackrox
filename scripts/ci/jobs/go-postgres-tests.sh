264cea8cec689a2c530a498dfb085cb8b335251e
Gavin Jefferies <gavin@stackrox.com>
2022-07-15 11:18:45 -0700

    CI: Onboard postgres unit tests (#2420)

--- scripts/ci/jobs/go-postgres-tests.sh
+++ scripts/ci/jobs/go-postgres-tests.sh
@@ -7,6 +7,19 @@ set -euo pipefail
 
 go_postgres_unit_tests() {
     info "Starting go postgres unit tests"
+
+    initdb "${HOME}/data"
+    pg_ctl -D "${HOME}/data" -l logfile -o "-k /tmp" start
+    export PGHOST=/tmp
+    createuser -s postgres
+
+    make go-postgres-unit-tests || touch FAIL
+
+    info "Saving junit XML report"
+    make generate-junit-reports || touch FAIL
+    store_test_results junit-reports reports
+
+    [[ ! -f FAIL ]] || die "Unit tests failed"
 }
 
 go_postgres_unit_tests "$*"
