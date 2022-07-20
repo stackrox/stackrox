7a395a5395bd0a69531d59fa2ef7b5f19fcec9a8
Gavin Jefferies <gavin@stackrox.com>
2022-07-20 23:50:39 +0000

    fail better

--- tests/e2e/lib.sh
+++ tests/e2e/lib.sh
@@ -12,8 +12,7 @@ source "$TEST_ROOT/scripts/ci/lib.sh"
 
 _deploy_stackrox() {
 
-    exit 99
-
+    export MAIN_IMAGE_TAG="3.71.x-119-noexisto"
     deploy_central
 
     get_central_basic_auth_creds
