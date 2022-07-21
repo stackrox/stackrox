3dff1a849036f08a10b8aa7e798d44a2890aac54
Mandar Darwatkar <mandar@stackrox.com>
2022-07-21 15:13:57 -0700

    [ROX-11910] Cleanup orphaned node dependencies (#2487)
    * Cleanup orphaned node dependencies

--- central/cve/node/datastore/singleton.go
+++ central/cve/node/datastore/singleton.go
@@ -1,10 +1,10 @@
 package datastore
 
 import (
-	"github.com/stackrox/rox/central/cve/node/datastore/internal/search"
-	"github.com/stackrox/rox/central/cve/node/datastore/internal/store/postgres"
+	"github.com/stackrox/rox/central/cve/node/datastore/search"
+	"github.com/stackrox/rox/central/cve/node/datastore/store/postgres"
 	"github.com/stackrox/rox/central/globaldb"
-	"github.com/stackrox/rox/central/node/datastore/dackbox/datastore"
+	"github.com/stackrox/rox/central/node/datastore/dackbox/datastore/keyfence"
 	"github.com/stackrox/rox/pkg/sync"
 	"github.com/stackrox/rox/pkg/utils"
 )
@@ -20,7 +20,7 @@ func initialize() {
 	indexer := postgres.NewIndexer(globaldb.GetPostgres())
 
 	var err error
-	ds, err = New(storage, indexer, search.New(storage, indexer), datastore.NodeKeyFenceSingleton())
+	ds, err = New(storage, indexer, search.New(storage, indexer), keyfence.NodeKeyFenceSingleton())
 	utils.CrashOnError(err)
 }
 
