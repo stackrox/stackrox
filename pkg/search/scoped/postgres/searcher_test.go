18a75871d72422035a4efeccffcdbaa93d4c9f2d
md2119 <mandar@stackrox.com>
2022-07-22 14:15:04 -0700

    remove suite

--- pkg/search/scoped/postgres/searcher_test.go
+++ pkg/search/scoped/postgres/searcher_test.go
@@ -3,40 +3,15 @@ package postgres
 import (
 	"testing"
 
-	"github.com/golang/mock/gomock"
 	v1 "github.com/stackrox/rox/generated/api/v1"
-	"github.com/stackrox/rox/pkg/dackbox/keys/transformation"
 	"github.com/stackrox/rox/pkg/postgres/schema"
 	"github.com/stackrox/rox/pkg/search"
-	"github.com/stackrox/rox/pkg/search/mocks"
 	"github.com/stackrox/rox/pkg/search/postgres/mapping"
 	"github.com/stackrox/rox/pkg/search/scoped"
-	"github.com/stretchr/testify/suite"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestWithScoping(t *testing.T) {
-	t.Parallel()
-	suite.Run(t, new(scopedSearcherTestSuite))
-}
-
-type scopedSearcherTestSuite struct {
-	suite.Suite
-
-	mockSearcher *mocks.MockSearcher
-
-	mockCtrl *gomock.Controller
-}
-
-func (s *scopedSearcherTestSuite) SetupTest() {
-	s.mockCtrl = gomock.NewController(s.T())
-	s.mockSearcher = mocks.NewMockSearcher(s.mockCtrl)
-}
-
-func (s *scopedSearcherTestSuite) TearDownTest() {
-	s.mockCtrl.Finish()
-}
-
-func (s *scopedSearcherTestSuite) TestScoping() {
+func TestScoping(t *testing.T) {
 	mapping.RegisterCategoryToTable(v1.SearchCategory_CLUSTERS, schema.ClustersSchema)
 	mapping.RegisterCategoryToTable(v1.SearchCategory_NAMESPACES, schema.NamespacesSchema)
 
@@ -52,8 +27,8 @@ func (s *scopedSearcherTestSuite) TestScoping() {
 		search.NewQueryBuilder().AddExactMatches(search.ClusterID, "c1").ProtoQuery(),
 	)
 	actual, err := scopeQuery(query, scopes)
-	s.NoError(err)
-	s.Equal(expected, actual)
+	assert.NoError(t, err)
+	assert.Equal(t, expected, actual)
 
 	scopes = []scoped.Scope{
 		{
@@ -71,12 +46,6 @@ func (s *scopedSearcherTestSuite) TestScoping() {
 		search.NewQueryBuilder().AddExactMatches(search.NamespaceID, "n1").ProtoQuery(),
 	)
 	actual, err = scopeQuery(query, scopes)
-	s.NoError(err)
-	s.Equal(expected, actual)
-}
-
-type testProvider map[v1.SearchCategory]transformation.OneToMany
-
-func (tp testProvider) Get(sc v1.SearchCategory) transformation.OneToMany {
-	return tp[sc]
+	assert.NoError(t, err)
+	assert.Equal(t, expected, actual)
 }
