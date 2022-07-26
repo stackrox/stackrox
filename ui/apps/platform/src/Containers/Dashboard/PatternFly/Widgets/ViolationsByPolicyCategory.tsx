844990d5fdb2efb9163c4d3b56556787880b68c6
David Vail <dcvail17@gmail.com>
2022-07-26 15:08:55 -0400

    Un-reverse order of legend in Violations by policy category widget. (#2522)

--- ui/apps/platform/src/Containers/Dashboard/PatternFly/Widgets/ViolationsByPolicyCategory.tsx
+++ ui/apps/platform/src/Containers/Dashboard/PatternFly/Widgets/ViolationsByPolicyCategory.tsx
@@ -23,7 +23,6 @@ import {
     getInteractiveLegendItemStyles,
 } from '@patternfly/react-charts';
 import sortBy from 'lodash/sortBy';
-import cloneDeep from 'lodash/cloneDeep';
 
 import { LinkableChartLabel } from 'Components/PatternFly/Charts/LinkableChartLabel';
 import { AlertGroup } from 'services/AlertsService';
@@ -52,10 +51,6 @@ import useWidgetConfig from 'hooks/useWidgetConfig';
 import useAlertGroups from '../hooks/useAlertGroups';
 import WidgetCard from './WidgetCard';
 import NoDataEmptyState from './NoDataEmptyState';
-
-// The ordering of the legend and the hidden severities runs from Critical->Low
-// so we reverse the order of the default Low->Critical in most cases.
-const severitiesCriticalToLow = [...severitiesLowToCritical].reverse();
 
 /**
  * This function iterates an array of AlertGroups and zeros out severities that
@@ -144,16 +139,13 @@ function tooltipForCategory(
     countsBySeverity: CountsBySeverity,
     hiddenSeverities: Set<PolicySeverity>
 ): string {
-    return severitiesCriticalToLow
+    return severitiesLowToCritical
         .filter((severity) => !hiddenSeverities.has(severity))
         .map((severity) => `${severityLabels[severity]}: ${countsBySeverity[severity][category]}`)
         .join('\n');
 }
 
-// This widget uses a theme with the legend order in the opposite direction
-// of the PatternFly defaults
-const chartTheme = cloneDeep(patternflySeverityTheme);
-chartTheme.legend.colorScale.reverse();
+const chartTheme = patternflySeverityTheme;
 
 function ViolationsByPolicyCategoryChart({
     alertGroups,
@@ -180,8 +172,6 @@ function ViolationsByPolicyCategoryChart({
     const topOrderedGroups = sortedAlertGroups.slice(0, 5).reverse();
     const countsBySeverity = getCountsBySeverity(topOrderedGroups);
 
-    // The bars run opposite to the severity display in the rest of the widget, so we iterate the original
-    // order of Low->Critical
     const bars = severitiesLowToCritical.map((severity) => {
         const counts = countsBySeverity[severity];
         const data = Object.entries(counts).map(([group, count]) => ({
@@ -208,7 +198,7 @@ function ViolationsByPolicyCategoryChart({
     });
 
     function getLegendData() {
-        const legendData = severitiesCriticalToLow.map((severity) => {
+        const legendData = severitiesLowToCritical.map((severity) => {
             return {
                 name: severityLabels[severity],
                 ...getInteractiveLegendItemStyles(hiddenSeverities.has(severity)),
@@ -219,7 +209,7 @@ function ViolationsByPolicyCategoryChart({
 
     function onLegendClick({ index }: { index: number }) {
         const newHidden = new Set(hiddenSeverities);
-        const targetSeverity = severitiesCriticalToLow[index];
+        const targetSeverity = severitiesLowToCritical[index];
         if (newHidden.has(targetSeverity)) {
             newHidden.delete(targetSeverity);
             // Do not allow the user to disable all severities
@@ -237,7 +227,7 @@ function ViolationsByPolicyCategoryChart({
                 domainPadding={{ x: [20, 20] }}
                 events={getInteractiveLegendEvents({
                     chartNames: [Object.values(severityLabels)],
-                    isHidden: (index) => hiddenSeverities.has(severitiesCriticalToLow[index]),
+                    isHidden: (index) => hiddenSeverities.has(severitiesLowToCritical[index]),
                     legendName: 'legend',
                     onLegendClick,
                 })}
