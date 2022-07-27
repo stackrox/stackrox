f27c40f911528947b6fb44d8004cec3d464ebd1d
Viswajith Venugopal <viswajithiii@gmail.com>
2022-07-27 16:45:50 -0700

    Fix gogen

--- pkg/search/derivationtype_string.go
+++ pkg/search/derivationtype_string.go
@@ -9,11 +9,12 @@ func _() {
 	// Re-run the stringer command to generate them again.
 	var x [1]struct{}
 	_ = x[CountDerivationType-0]
+	_ = x[SimpleReverseSortDerivationType-1]
 }
 
-const _DerivationType_name = "CountDerivationType"
+const _DerivationType_name = "CountDerivationTypeSimpleReverseSortDerivationType"
 
-var _DerivationType_index = [...]uint8{0, 19}
+var _DerivationType_index = [...]uint8{0, 19, 50}
 
 func (i DerivationType) String() string {
 	if i < 0 || i >= DerivationType(len(_DerivationType_index)-1) {
