ddbd321b52c0b87a3d7d4112342539dfb4e10ebc
Gavin Jefferies <gavin@stackrox.com>
2022-07-28 17:19:46 -0700

    ROX-11701: Add EKS cluster e2e test support (#2547)

---
+++ scripts/ci/jobs/eks_qa_e2e_tests.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env -S python3 -u
+
+"""
+Run qa-tests-backend in an EKS cluster provided via automation-flavors/eks.
+"""
+import os
+from base_qa_e2e_test import make_qa_e2e_test_runner
+from clusters import AutomationFlavorsCluster
+
+# set required test parameters
+os.environ["ORCHESTRATOR_FLAVOR"] = "k8s"
+os.environ["COLLECTION_METHOD"] = "kernel-module"
+
+make_qa_e2e_test_runner(cluster=AutomationFlavorsCluster()).run()
