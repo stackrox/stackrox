71df86350107b5cbe516c6fda558688336a20593
David Vail <dcvail17@gmail.com>
2022-08-03 14:11:59 -0400

    Update prop types of `Tab` to satisfy type error.

--- ui/apps/platform/src/Components/Tab.tsx
+++ ui/apps/platform/src/Components/Tab.tsx
@@ -1,14 +1,14 @@
-import { ReactElement } from 'react';
+import React, { ReactElement, ReactNode } from 'react';
 
 type TabProps = {
     title?: string;
-    children: ReactElement;
+    children: ReactNode;
 };
 
 // The "title" prop is necessary when used with the "useTabs" hook
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function Tab({ title, children }: TabProps): ReactElement {
-    return children;
+    return <>children</>;
 }
 
 export default Tab;
