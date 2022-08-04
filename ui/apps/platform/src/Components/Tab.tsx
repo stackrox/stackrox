be89a101eb430660bf42b4e72447f1bdbd009b68
David Vail <dcvail17@gmail.com>
2022-08-04 11:45:42 -0400

    Change test code to conform with app code, instead of the reverse.

--- ui/apps/platform/src/Components/Tab.tsx
+++ ui/apps/platform/src/Components/Tab.tsx
@@ -1,14 +1,14 @@
-import React, { ReactElement, ReactNode } from 'react';
+import { ReactElement } from 'react';
 
 type TabProps = {
     title?: string;
-    children: ReactNode;
+    children: ReactElement;
 };
 
 // The "title" prop is necessary when used with the "useTabs" hook
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function Tab({ title, children }: TabProps): ReactElement {
-    return <>children</>;
+    return children;
 }
 
 export default Tab;
