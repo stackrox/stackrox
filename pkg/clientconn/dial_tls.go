c82f99a6a3f033f9a3b9300af8bc8c90fc733017
Ross Tannenbaum <rtann@redhat.com>
2022-08-04 19:47:52 -0400

    Replace grpc.WithInsecure() (#2631)

--- pkg/clientconn/dial_tls.go
+++ pkg/clientconn/dial_tls.go
@@ -14,7 +14,8 @@ type DialTLSFunc func(ctx context.Context, endpoint string, tlsClientConf *tls.C
 
 // DialTLS establishes a gRPC connection to the given endpoint, optionally using TLS for securing the transport layer
 // and the given dial options.
-// Note: if tlsClientConf is nil, the options *must* contain `WithInsecure()`, otherwise the connection will fail.
+// Note: if tlsClientConf is nil, the options *must* contain `WithInsecure()` or `WithTransportCredentials(insecure.NewCredentials())`,
+// otherwise the connection will fail.
 func DialTLS(ctx context.Context, endpoint string, tlsClientConf *tls.Config, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
 	allOpts := make([]grpc.DialOption, 0, len(opts)+1)
 	if tlsClientConf != nil {
@@ -27,7 +28,8 @@ func DialTLS(ctx context.Context, endpoint string, tlsClientConf *tls.Config, op
 
 // DialTLSWebSocket establishes a gRPC connection via a WebSocket proxy to the given endpoint,
 // optionally using TLS for securing the transport layer and the given dial options.
-// Note: if tlsClientConf is nil, the options *must* contain `WithInsecure()`, otherwise the connection will fail.
+// Note: if tlsClientConf is nil, the options *must* contain `WithInsecure()` or `WithTransportCredentials(insecure.NewCredentials())`,
+// otherwise the connection will fail.
 func DialTLSWebSocket(ctx context.Context, endpoint string, tlsClientConf *tls.Config, opts ...grpc.DialOption) (*grpc.ClientConn, error) {
 	return client.ConnectViaProxy(ctx, endpoint, tlsClientConf, client.DialOpts(opts...), client.UseWebSocket(true))
 }
