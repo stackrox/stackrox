6e46d1a6da5aad2aa65aa80848f8c45f2574e29c
Gavin Jefferies <gavin@stackrox.com>
2022-08-04 23:16:07 +0000

    the Real OSD GCP

---
+++ scripts/ci/jobs/osd_gcp_qa_e2e_tests.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env -S python3 -u
+
+"""
+Run qa-tests-backend in an OSD GCP cluster provided via automation-flavors/osd.
+"""
+import os
+from base_qa_e2e_test import make_qa_e2e_test_runner
+from clusters import AutomationFlavorsCluster
+
+# set required test parameters
+os.environ["ORCHESTRATOR_FLAVOR"] = "openshift"
+
+make_qa_e2e_test_runner(cluster=AutomationFlavorsCluster()).run()
