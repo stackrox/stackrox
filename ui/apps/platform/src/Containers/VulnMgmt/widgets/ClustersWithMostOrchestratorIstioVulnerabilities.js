a82ed433c7d0be0c6b47d721510aadca7e808809
Van J. Wilson <vwilson@redhat.com>
2022-08-08 19:08:08 -0400

    Fix CVE counts in Clusters dashboard widget

--- ui/apps/platform/src/Containers/VulnMgmt/widgets/ClustersWithMostOrchestratorIstioVulnerabilities.js
+++ ui/apps/platform/src/Containers/VulnMgmt/widgets/ClustersWithMostOrchestratorIstioVulnerabilities.js
@@ -20,7 +20,6 @@ import openShiftSVG from 'images/openShift.svg';
 import { Tooltip, TooltipOverlay } from '@stackrox/ui-components';
 import useFeatureFlags from 'hooks/useFeatureFlags';
 
-// need to add query for fixable cves for dashboard once it's supported
 const CLUSTER_WITH_MOST_ORCHESTRATOR_ISTIO_VULNERABILTIES = gql`
     query clustersWithMostOrchestratorIstioVulnerabilities {
         results: clusters {
@@ -42,6 +41,23 @@ const CLUSTER_WITH_MOST_ORCHESTRATOR_ISTIO_VULNERABILTIES = gql`
             openShiftVulns {
                 cve
                 isFixable
+            }
+        }
+    }
+`;
+const CLUSTER_WITH_MOST_CLUSTER_VULNERABILTIES = gql`
+    query clustersWithMostClusterVulnerabilities {
+        results: clusters {
+            id
+            name
+            isGKECluster
+            isOpenShiftCluster
+            clusterVulnerabilityCount
+            clusterVulnerabilities {
+                cve
+                isFixable
+                vulnerabilityType
+                vulnerabilityTypes
             }
         }
     }
@@ -72,6 +88,130 @@ const getVulnDataByType = (workflowState, clusterId, vulnType, vulns, showVmUpda
 };
 
 const processData = (data, workflowState, limit, showVmUpdates) => {
+    if (!data.results) {
+        return [];
+    }
+    const results = sortBy(data.results, ['clusterVulnerabilityCount'])
+        .slice(-limit)
+        .map(({ id, name, isGKECluster, isOpenShiftCluster, clusterVulnerabilities }) => {
+            const clusterUrl = workflowState.resetPage(entityTypes.CLUSTER, id).toUrl();
+
+            const { k8sVulns, istioVulns, openShiftVulns } = clusterVulnerabilities.reduce(
+                (acc, vuln) => {
+                    if (vuln.vulnerabilityType === 'K8S_CVE') {
+                        return { ...acc, k8sVulns: [...acc.k8sVulns, vuln] };
+                    }
+                    if (vuln.vulnerabilityType === 'ISTIO_CVE') {
+                        return { ...acc, istioVulns: [...acc.istioVulns, vuln] };
+                    }
+                    if (vuln.vulnerabilityType === 'OPENSHIFT_CVE') {
+                        return { ...acc, openShiftVulns: [...acc.openShiftVulns, vuln] };
+                    }
+                    return acc;
+                },
+                { k8sVulns: [], istioVulns: [], openShiftVulns: [] }
+            );
+            const {
+                cveCount: k8sCveCount,
+                fixableCount: k8sFixableCount,
+                url: k8sUrl,
+                fixableUrl: k8sFixableUrl,
+            } = getVulnDataByType(workflowState, id, 'K8S_CVE', k8sVulns, showVmUpdates);
+            const {
+                cveCount: istioCveCount,
+                fixableCount: istioFixableCount,
+                url: istioUrl,
+                fixableUrl: istioFixableUrl,
+            } = getVulnDataByType(workflowState, id, 'ISTIO_CVE', istioVulns, showVmUpdates);
+            const {
+                cveCount: openShiftCveCount,
+                fixableCount: openShiftFixableCount,
+                url: openShiftUrl,
+                fixableUrl: openShiftFixableUrl,
+            } = getVulnDataByType(
+                workflowState,
+                id,
+                'OPENSHIFT_CVE',
+                openShiftVulns,
+                showVmUpdates
+            );
+
+            const indicationTooltipText = isGKECluster
+                ? 'These CVEs might have been patched by GKE. Please check the GKE release notes or security bulletin to find out more.'
+                : 'These CVEs were not patched in the current Kubernetes version of this cluster.';
+
+            const indicatorIcon = isGKECluster ? (
+                <HelpCircle className="w-4 h-4 text-warning-700 ml-2" />
+            ) : (
+                <AlertCircle className="w-4 h-4 text-alert-700 ml-2" />
+            );
+
+            const orchestratorContent = isOpenShiftCluster ? (
+                <div className="flex items-center justify-left mr-8">
+                    <Tooltip content={<TooltipOverlay>OpenShift Vulnerabilities</TooltipOverlay>}>
+                        <div className="flex">
+                            <img src={openShiftSVG} alt="openshift" className="pr-2" />
+                            <FixableCVECount
+                                cves={openShiftCveCount}
+                                url={openShiftUrl}
+                                fixableUrl={openShiftFixableUrl}
+                                fixable={openShiftFixableCount}
+                                orientation="vertical"
+                                showZero
+                            />
+                        </div>
+                    </Tooltip>
+                </div>
+            ) : (
+                <div className="flex flex-1 items-center justify-left mr-8">
+                    <Tooltip content={<TooltipOverlay>Kubernetes Vulnerabilities</TooltipOverlay>}>
+                        <div className="flex">
+                            <img src={kubeSVG} alt="kube" className="pr-2" />
+                            <FixableCVECount
+                                cves={k8sCveCount}
+                                url={k8sUrl}
+                                fixableUrl={k8sFixableUrl}
+                                fixable={k8sFixableCount}
+                                orientation="vertical"
+                                showZero
+                            />
+                        </div>
+                    </Tooltip>
+                    <Tooltip content={<TooltipOverlay>{indicationTooltipText}</TooltipOverlay>}>
+                        {indicatorIcon}
+                    </Tooltip>
+                </div>
+            );
+
+            const orchestratorIstioContent = (
+                <div className="flex">
+                    {orchestratorContent}
+                    <Tooltip content={<TooltipOverlay>Istio Vulnerabilities</TooltipOverlay>}>
+                        <div className="flex items-center justify-left pr-2">
+                            <img src={istioSVG} alt="istio" className="pr-2" />
+                            <FixableCVECount
+                                cves={istioCveCount}
+                                url={istioUrl}
+                                fixableUrl={istioFixableUrl}
+                                fixable={istioFixableCount}
+                                orientation="vertical"
+                                showZero
+                            />
+                        </div>
+                    </Tooltip>
+                </div>
+            );
+            return {
+                text: name,
+                url: clusterUrl,
+                component: orchestratorIstioContent,
+            };
+        });
+    return results.slice(0, 8); // @TODO: Remove and add pagination when available
+};
+
+// TODO: remove this old version of the function after transition
+const processDataLegacy = (data, workflowState, limit, showVmUpdates) => {
     if (!data.results) {
         return [];
     }
@@ -193,17 +333,21 @@ const processData = (data, workflowState, limit, showVmUpdates) => {
 };
 
 const ClustersWithMostOrchestratorVulnerabilities = ({ entityContext, limit }) => {
+    const { isFeatureFlagEnabled } = useFeatureFlags();
+    const showVmUpdates = isFeatureFlagEnabled('ROX_FRONTEND_VM_UPDATES');
+    const queryToUse = showVmUpdates
+        ? CLUSTER_WITH_MOST_CLUSTER_VULNERABILTIES
+        : CLUSTER_WITH_MOST_ORCHESTRATOR_ISTIO_VULNERABILTIES;
+
     const {
         loading,
         data = {},
         error,
-    } = useQuery(CLUSTER_WITH_MOST_ORCHESTRATOR_ISTIO_VULNERABILTIES, {
+    } = useQuery(queryToUse, {
         variables: {
             query: queryService.entityContextToQueryString(entityContext),
         },
     });
-    const { isFeatureFlagEnabled } = useFeatureFlags();
-    const showVmUpdates = isFeatureFlagEnabled('ROX_FRONTEND_VM_UPDATES');
 
     let content = <Loader />;
 
@@ -216,7 +360,9 @@ const ClustersWithMostOrchestratorVulnerabilities = ({ entityContext, limit }) =
 
             content = <NoResultsMessage message={parsedMessage} className="p-3" icon="warn" />;
         } else {
-            const processedData = processData(data, workflowState, limit, showVmUpdates);
+            const processedData = showVmUpdates
+                ? processData(data, workflowState, limit, showVmUpdates)
+                : processDataLegacy(data, workflowState, limit, showVmUpdates);
 
             if (!processedData || processedData.length === 0) {
                 content = (
