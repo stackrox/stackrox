386688a58922098274d5dce1ee9fd5f684c84f27
David Vail <dcvail17@gmail.com>
2022-08-09 17:04:01 -0400

    Add auth interceptors.

--- ui/apps/platform/src/console-plugins/modules/Violations.tsx
+++ ui/apps/platform/src/console-plugins/modules/Violations.tsx
@@ -3,6 +3,7 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 import ViolationsPage from 'Containers/Violations/ViolationsPage';
 import axios from 'services/instance';
+import { addAuthInterceptors } from 'services/AuthService';
 
 const baseURL = 'https://central-stackrox.apps.ui-08-08-hack-a-thon-3.openshift.infra.rox.systems';
 
@@ -14,6 +15,9 @@ const apolloClient = new ApolloClient({
 axios.interceptors.request.use((config) => {
     return { ...config, baseURL };
 });
+
+// TODO We need a way to get the JWT into localStorage `access_token` for authenticated requests
+addAuthInterceptors(console.error);
 
 export default function Violations() {
     return (
