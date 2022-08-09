e5d6dc68a85efda7801ce710f9e3648f3ddc8ab4
Yann Brillouet <ybrillou@redhat.com>
2022-08-09 16:40:19 +0200

    Fix corner cases of rejected access

--- central/networkgraph/entity/datastore/internal/store/postgres/permission_checker_impl.go
+++ central/networkgraph/entity/datastore/internal/store/postgres/permission_checker_impl.go
@@ -3,7 +3,11 @@ package postgres
 import (
 	"context"
 
+	"github.com/stackrox/rox/central/role/resources"
+	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/auth/permissions"
 	"github.com/stackrox/rox/pkg/sac"
+	"github.com/stackrox/rox/pkg/sac/effectiveaccessscope"
 	"github.com/stackrox/rox/pkg/sync"
 )
 
@@ -12,6 +16,8 @@ type permissionCheckerImpl struct{}
 var (
 	once     sync.Once
 	instance PermissionChecker
+
+	networkGraphSAC = sac.ForResource(resources.NetworkGraph)
 )
 
 func permissionCheckerSingleton() PermissionChecker {
@@ -21,42 +27,60 @@ func permissionCheckerSingleton() PermissionChecker {
 	return instance
 }
 
-func (permissionCheckerImpl) CountAllowed(ctx context.Context) (bool, error) {
+func genericPassThrough(ctx context.Context, access storage.Access) (bool, error) {
+	scopeChecker := networkGraphSAC.ScopeChecker(ctx, access)
+	eas, err := scopeChecker.EffectiveAccessScope(permissions.ResourceWithAccess{
+		Resource: resources.NetworkGraph,
+		Access:   access,
+	})
+	if err != nil {
+		return false, err
+	}
+	if eas == nil {
+		return false, nil
+	}
+	if eas.State == effectiveaccessscope.Excluded {
+		return false, nil
+	}
 	return true, nil
+}
+
+func (permissionCheckerImpl) CountAllowed(ctx context.Context) (bool, error) {
+	return genericPassThrough(ctx, storage.Access_READ_ACCESS)
 }
 
 func (permissionCheckerImpl) ExistsAllowed(ctx context.Context) (bool, error) {
-	return true, nil
+	return genericPassThrough(ctx, storage.Access_READ_ACCESS)
 }
 
 func (permissionCheckerImpl) GetAllowed(ctx context.Context) (bool, error) {
-	return true, nil
+	return genericPassThrough(ctx, storage.Access_READ_ACCESS)
 }
 
 func (permissionCheckerImpl) WalkAllowed(ctx context.Context) (bool, error) {
-	return true, nil
+	return genericPassThrough(ctx, storage.Access_READ_ACCESS)
 }
 
 func (permissionCheckerImpl) UpsertAllowed(ctx context.Context, keys ...sac.ScopeKey) (bool, error) {
-	return true, nil
+	return genericPassThrough(ctx, storage.Access_READ_WRITE_ACCESS)
 }
 
 func (permissionCheckerImpl) UpsertManyAllowed(ctx context.Context, keys ...sac.ScopeKey) (bool, error) {
-	return true, nil
+	return genericPassThrough(ctx, storage.Access_READ_WRITE_ACCESS)
 }
 
 func (permissionCheckerImpl) DeleteAllowed(ctx context.Context, keys ...sac.ScopeKey) (bool, error) {
-	return true, nil
+	return genericPassThrough(ctx, storage.Access_READ_WRITE_ACCESS)
 }
 
 func (permissionCheckerImpl) GetIDsAllowed(ctx context.Context) (bool, error) {
-	return true, nil
+	return genericPassThrough(ctx, storage.Access_READ_ACCESS)
 }
 
 func (permissionCheckerImpl) GetManyAllowed(ctx context.Context) (bool, error) {
-	return true, nil
+	return genericPassThrough(ctx, storage.Access_READ_ACCESS)
 }
 
 func (permissionCheckerImpl) DeleteManyAllowed(ctx context.Context, keys ...sac.ScopeKey) (bool, error) {
-	return true, nil
+	return genericPassThrough(ctx, storage.Access_READ_WRITE_ACCESS)
 }
