ea24a6f182a600f77612ef4eb314a6c21044fa42
Matthias <Maddosaurus@users.noreply.github.com>
2022-08-11 17:47:25 +0200

    ROX-11775: Introduce different file outputs for roxctl generate netpol (#2602)

--- roxctl/generate/netpol/cta.go
+++ roxctl/generate/netpol/cta.go
@@ -1,23 +1,104 @@
 package netpol
 
 import (
+	"os"
+	"path/filepath"
+	"strings"
+
 	"github.com/np-guard/cluster-topology-analyzer/pkg/controller"
 	"github.com/pkg/errors"
 	"github.com/stackrox/rox/pkg/protoconv/networkpolicy"
+	v1 "k8s.io/api/networking/v1"
 )
 
 func (cmd *generateNetpolCommand) generateNetpol() error {
 	recommendedNetpols, err := controller.PoliciesFromFolderPath(cmd.folderPath)
 	if err != nil {
-		return errors.Wrap(err, "Error synthesizing policies from folder")
+		return errors.Wrap(err, "error synthesizing policies from folder")
 	}
+	if _, err := os.Stat(cmd.outputFolderPath); err == nil {
+		if err := os.RemoveAll(cmd.outputFolderPath); err != nil {
+			return errors.Wrapf(err, "failed to remove output path %s", cmd.outputFolderPath)
+		}
+		cmd.env.Logger().WarnfLn("Removed output path %s", cmd.outputFolderPath)
+	}
+	cmd.env.Logger().InfofLn("Writing generated Network Policies to %s", cmd.outputFolderPath)
 
+	var mergedPolicy string
+	yamlPolicies := make([]string, 0, len(recommendedNetpols))
 	for _, netpol := range recommendedNetpols {
 		yamlPolicy, err := networkpolicy.KubernetesNetworkPolicyWrap{NetworkPolicy: netpol}.ToYaml()
 		if err != nil {
-			return errors.Wrap(err, "Error converting YAML into Network Policy")
+			return errors.Wrap(err, "error converting Network Policy object to YAML")
 		}
-		cmd.env.Logger().PrintfLn("---\n\n%s", yamlPolicy)
+		yamlPolicies = append(yamlPolicies, yamlPolicy)
+	}
+	mergedPolicy = strings.Join(yamlPolicies, "\n---\n")
+
+	if cmd.mergeMode {
+		if err := cmd.saveNetpolsToMergedFile(mergedPolicy); err != nil {
+			return errors.Wrap(err, "error saving merged Network Policies")
+		}
+		return nil
+	}
+
+	if cmd.splitMode {
+		if err := cmd.saveNetpolsToFolder(recommendedNetpols); err != nil {
+			return errors.Wrap(err, "error saving split Network Policies")
+		}
+		return nil
+	}
+
+	if cmd.stdoutMode {
+		cmd.printNetpols(mergedPolicy)
+		return nil
+	}
+
+	cmd.env.Logger().WarnfLn("No mode selected, not providing any output.")
+	return nil
+}
+
+func (cmd *generateNetpolCommand) printNetpols(combinedNetpols string) {
+	cmd.env.Logger().PrintfLn(combinedNetpols)
+}
+
+func (cmd *generateNetpolCommand) saveNetpolsToMergedFile(combinedNetpols string) error {
+	dirpath, filename := filepath.Split(cmd.outputFilePath)
+	if filename == "" {
+		filename = "policies.yaml"
+	}
+
+	if err := writeFile(filename, dirpath, combinedNetpols); err != nil {
+		return errors.Wrap(err, "error writing merged Network Policies")
 	}
 	return nil
+}
+
+func (cmd *generateNetpolCommand) saveNetpolsToFolder(recommendedNetpols []*v1.NetworkPolicy) error {
+	for _, netpol := range recommendedNetpols {
+		policyName := netpol.GetName()
+		if policyName == "" {
+			policyName = string(netpol.GetUID())
+		}
+		filename := policyName + ".yaml"
+
+		yamlPolicy, err := networkpolicy.KubernetesNetworkPolicyWrap{NetworkPolicy: netpol}.ToYaml()
+		if err != nil {
+			return errors.Wrap(err, "error converting Network Policy object to yaml")
+		}
+
+		if err := writeFile(filename, cmd.outputFolderPath, yamlPolicy); err != nil {
+			return errors.Wrap(err, "error writing policy to file")
+		}
+	}
+	return nil
+}
+
+func writeFile(filename string, destDir string, content string) error {
+	outputPath := filepath.Join(destDir, filename)
+	if err := os.MkdirAll(filepath.Dir(outputPath), 0755); err != nil {
+		return errors.Wrapf(err, "error creating directory for file %q", filename)
+	}
+
+	return errors.Wrap(os.WriteFile(outputPath, []byte(content), os.FileMode(0644)), "error writing file")
 }
