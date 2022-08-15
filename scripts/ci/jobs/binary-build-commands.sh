2f437616c565bd1845ce20e80228823787f148da
Gavin Jefferies <gavin@stackrox.com>
2022-08-15 08:36:22 +0000

    add binary-build-commands.sh stub

---
+++ scripts/ci/jobs/binary-build-commands.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../../.. && pwd)"
+source "$ROOT/scripts/ci/lib.sh"
+
+set -euo pipefail
+
+make_test_bin() {
+    info "Making test-bin"
+
+    make cli-build upgrader
+    install_built_roxctl_in_gopath
+}
+
+make_test_bin "$*"
