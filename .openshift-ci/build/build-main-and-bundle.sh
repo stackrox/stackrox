ffc64cbbc195d50249341111cb3e23fa8f10c02b
Gavin Jefferies <gavin@stackrox.com>
2022-08-15 11:32:54 +0000

    no longer -dirty & needs a JOB_NAME

--- .openshift-ci/build/build-main-and-bundle.sh
+++ .openshift-ci/build/build-main-and-bundle.sh
@@ -114,10 +114,7 @@ cleanup_image() {
 }
 
 build_main_and_bundles() {
-    # avoid a -dirty tag
-    info "Reset to remove Dockerfile modification by OpenShift CI"
-    git restore .
-    git status
+    export JOB_NAME="build-main-and-bundle"
 
     openshift_ci_mods
 
