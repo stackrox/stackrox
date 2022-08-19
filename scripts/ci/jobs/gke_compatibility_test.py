b46bafb026fbad14b4548a8f8ed9019ea65defcc
Frederico Bittencourt <fbittenc@redhat.com>
2022-08-19 16:24:10 +0200

    add prow job

---
+++ scripts/ci/jobs/gke_compatibility_test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env -S python3 -u
+
+"""
+Run tests/e2e in a GKE cluster using older sensors
+"""
+import os
+from runners import ClusterTestRunner
+from clusters import GKECluster
+from pre_tests import PreSystemTests
+from ci_tests import NonGroovyE2e
+from post_tests import PostClusterTest, FinalPost
+
+# set required test parameters
+os.environ["ORCHESTRATOR_FLAVOR"] = "k8s"
+os.environ["SENSOR_IMAGE_TAG"] = "3.70.0"
+
+ClusterTestRunner(
+    cluster=GKECluster("nongroovy-test"),
+    pre_test=PreSystemTests(),
+    test=NonGroovyE2e(),
+    post_test=PostClusterTest(
+        check_stackrox_logs=False,
+    ),
+    final_post=FinalPost(
+        store_qa_test_debug_logs=False,
+        store_qa_spock_results=False,
+    ),
+).run()
