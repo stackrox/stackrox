4f2be689e907a2b36823a19d16c948c7499f860a
Mark Pedrotti <pedrottimark@gmail.com>
2022-08-22 06:15:56 -0400

    ROX-12190: [Hackathon] Replace search modal with search page in UI (#2689)

--- ui/apps/platform/src/Containers/MainPage/Header/GlobalSearchButton.tsx
+++ ui/apps/platform/src/Containers/MainPage/Header/GlobalSearchButton.tsx
@@ -1,37 +1,42 @@
 import React, { ReactElement } from 'react';
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { Flex, FlexItem } from '@patternfly/react-core';
 import { SearchIcon } from '@patternfly/react-icons';
 
+import useFeatureFlagEnabled from 'hooks/useFeatureFlags';
 import { actions as globalSearchActions } from 'reducers/globalSearch';
-
-type GlobalSearchButtonProps = {
-    toggleGlobalSearchView: () => void;
-};
+import { searchPath } from 'routePaths';
 
 /*
  * Use HTML button with PatternFly Button class to inherit masthead color.
  */
-const GlobalSearchButton = ({ toggleGlobalSearchView }: GlobalSearchButtonProps): ReactElement => (
-    <button
-        type="button"
-        onClick={toggleGlobalSearchView}
-        className="pf-c-button ignore-react-onclickoutside"
-    >
-        <Flex alignItems={{ default: 'alignItemsCenter' }} spaceItems={{ default: 'spaceItemsSm' }}>
-            <FlexItem>
-                <SearchIcon alt="" />
-            </FlexItem>
-            <FlexItem>Search</FlexItem>
-        </Flex>
-    </button>
-);
+function GlobalSearchButton(): ReactElement {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const { isFeatureFlagEnabled } = useFeatureFlagEnabled();
 
-const mapDispatchToProps = (dispatch) => ({
-    // TODO: type redux props
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    toggleGlobalSearchView: () => dispatch(globalSearchActions.toggleGlobalSearchView()),
-});
+    function onClick() {
+        if (isFeatureFlagEnabled('ROX_SEARCH_PAGE_UI')) {
+            history.push(searchPath);
+        } else {
+            dispatch(globalSearchActions.toggleGlobalSearchView());
+        }
+    }
 
-export default withRouter(connect(null, mapDispatchToProps)(GlobalSearchButton));
+    return (
+        <button type="button" onClick={onClick} className="pf-c-button ignore-react-onclickoutside">
+            <Flex
+                alignItems={{ default: 'alignItemsCenter' }}
+                spaceItems={{ default: 'spaceItemsSm' }}
+            >
+                <FlexItem>
+                    <SearchIcon alt="" />
+                </FlexItem>
+                <FlexItem>Search</FlexItem>
+            </Flex>
+        </button>
+    );
+}
+
+export default GlobalSearchButton;
