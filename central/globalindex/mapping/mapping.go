a86b69314600a485d6d3d895a4e482be441dd47a
Yann Brillouet <ybrillou@redhat.com>
2022-08-23 13:11:32 +0200

    Implement postgres EntityOptionsMap

--- central/globalindex/mapping/mapping.go
+++ central/globalindex/mapping/mapping.go
@@ -37,6 +37,8 @@ import (
 	serviceAccountOptions "github.com/stackrox/rox/central/serviceaccount/mappings"
 	vulnReqMapping "github.com/stackrox/rox/central/vulnerabilityrequest/mappings"
 	v1 "github.com/stackrox/rox/generated/api/v1"
+	"github.com/stackrox/rox/pkg/features"
+	"github.com/stackrox/rox/pkg/postgres/schema"
 	"github.com/stackrox/rox/pkg/search"
 	"github.com/stackrox/rox/pkg/search/blevesearch"
 	"github.com/stackrox/rox/pkg/search/options/deployments"
@@ -94,8 +96,144 @@ func singleTermAnalyzer() map[string]interface{} {
 	}
 }
 
+func getPostgresEntityOptionsMap() map[v1.SearchCategory]search.OptionsMap {
+	// Note: with the dackbox graph split brought with the postgres migration, the concept
+	// of Component (ImageComponent) was split into ImageComponent and NodeComponent.
+	// The old content seems to focus mostly on image Components.
+	// componentSearchOptions := search.CombineOptionsMaps(
+	// 	imageComponentMapping.OptionsMap,
+	// 	imageComponentEdgeMapping.OptionsMap,
+	// 	componentVulnEdgeMapping.OptionsMap,
+	// 	imageMapping.OptionsMap,
+	// 	cveMapping.OptionsMap,
+	// 	imageMapping.ImageDeploymentOptions,
+	// )
+	componentSearchOptions := search.CombineOptionsMaps(
+		schema.ImageComponentsSchema.OptionsMap,
+		schema.ImageComponentEdgesSchema.OptionsMap,
+		schema.ImageComponentCveEdgesSchema.OptionsMap,
+		schema.ImagesSchema.OptionsMap,
+		schema.ImageCvesSchema.OptionsMap,
+	)
+
+	// Note: with the dackbox graph split brought with the postgres migration, the concept
+	// of CVE was split into ClusterCVE, ImageCVE and NodeCVE. The old content seems to focus
+	// mostly on image CVEs.
+	// cveSearchOptions := search.CombineOptionsMaps(
+	// 	cveMapping.OptionsMap,
+	// 	componentVulnEdgeMapping.OptionsMap,
+	// 	imageComponentMapping.OptionsMap,
+	// 	imageComponentEdgeMapping.OptionsMap,
+	// 	imageMapping.OptionsMap,
+	// 	deployments.OptionsMap,
+	// )
+	cveSearchOptions := search.CombineOptionsMaps(
+		schema.ImageCvesSchema.OptionsMap,
+		schema.ImageComponentCveEdgesSchema.OptionsMap,
+		schema.ImageComponentsSchema.OptionsMap,
+		schema.ImageComponentEdgesSchema.OptionsMap,
+		schema.ImagesSchema.OptionsMap,
+		schema.DeploymentsSchema.OptionsMap,
+	)
+
+	// Images in dackbox support an expanded set of search options
+	// imageSearchOptions := search.CombineOptionsMaps(
+	// 	imageMapping.OptionsMap,
+	// 	imageMapping.ImageDeploymentOptions,
+	// 	imageComponentEdgeMapping.OptionsMap,
+	// 	imageComponentMapping.OptionsMap,
+	// 	componentVulnEdgeMapping.OptionsMap,
+	// 	cveMapping.OptionsMap,
+	// )
+	imageSearchOptions := search.CombineOptionsMaps(
+		schema.ImagesSchema.OptionsMap,
+		schema.ImageComponentEdgesSchema.OptionsMap,
+		schema.ImageComponentsSchema.OptionsMap,
+		schema.ImageComponentCveEdgesSchema.OptionsMap,
+		schema.ImageCvesSchema.OptionsMap,
+	)
+
+	// nodeSearchOptions := search.CombineOptionsMaps(
+	// 	nodeMapping.OptionsMap,
+	// 	nodeComponentEdgeMapping.OptionsMap,
+	// 	imageComponentMapping.OptionsMap,
+	// 	componentVulnEdgeMapping.OptionsMap,
+	// 	cveMapping.OptionsMap,
+	// )
+	nodeSearchOptions := search.CombineOptionsMaps(
+		schema.NodesSchema.OptionsMap,
+		schema.NodeComponentEdgesSchema.OptionsMap,
+		schema.NodeComponentsSchema.OptionsMap,
+		schema.NodeComponentsCvesEdgesSchema.OptionsMap,
+		schema.NodeCvesSchema.OptionsMap,
+	)
+
+	// EntityOptionsMap is a mapping from search categories to the options map for that category.
+	// search document maps are also built off this map
+	entityOptionsMap := map[v1.SearchCategory]search.OptionsMap{
+		// v1.SearchCategory_ACTIVE_COMPONENT:      activeComponentMappings.OptionsMap,
+		v1.SearchCategory_ACTIVE_COMPONENT: schema.ActiveComponentsSchema.OptionsMap,
+		// v1.SearchCategory_ALERTS:                alertMapping.OptionsMap,
+		v1.SearchCategory_ALERTS: schema.AlertsSchema.OptionsMap,
+		// v1.SearchCategory_CLUSTER_VULN_EDGE:     clusterVulnEdgeMapping.OptionsMap,
+		v1.SearchCategory_CLUSTER_VULN_EDGE: schema.ClusterCveEdgesSchema.OptionsMap,
+		// v1.SearchCategory_CLUSTERS:              clusterMapping.OptionsMap,
+		v1.SearchCategory_CLUSTERS:            schema.ClustersSchema.OptionsMap,
+		v1.SearchCategory_COMPLIANCE_STANDARD: index.StandardOptions,
+		v1.SearchCategory_COMPLIANCE_CONTROL:  index.ControlOptions,
+		// v1.SearchCategory_COMPONENT_VULN_EDGE:   componentVulnEdgeMapping.OptionsMap,
+		v1.SearchCategory_COMPONENT_VULN_EDGE: schema.ImageComponentCveEdgesSchema.OptionsMap,
+		// v1.SearchCategory_DEPLOYMENTS:           deployments.OptionsMap,
+		v1.SearchCategory_DEPLOYMENTS: schema.DeploymentsSchema.OptionsMap,
+		// v1.SearchCategory_IMAGE_COMPONENT_EDGE:  imageComponentEdgeMapping.OptionsMap,
+		v1.SearchCategory_IMAGE_COMPONENT_EDGE: schema.ImageComponentEdgesSchema.OptionsMap,
+		v1.SearchCategory_IMAGE_COMPONENTS:     componentSearchOptions,
+		// v1.SearchCategory_IMAGE_VULN_EDGE:       imageCVEEdgeMapping.OptionsMap,
+		v1.SearchCategory_IMAGE_VULN_EDGE: schema.ImageCveEdgesSchema.OptionsMap,
+		v1.SearchCategory_IMAGES:          imageSearchOptions,
+		// v1.SearchCategory_NAMESPACES:            namespaceMapping.OptionsMap,
+		v1.SearchCategory_NAMESPACES: schema.NamespacesSchema.OptionsMap,
+		// v1.SearchCategory_NODE_COMPONENT_EDGE:   nodeComponentEdgeMappings.OptionsMap,
+		v1.SearchCategory_NODE_COMPONENT_EDGE: schema.NodeComponentEdgesSchema.OptionsMap,
+		v1.SearchCategory_NODE_COMPONENTS:     schema.NodeComponentsSchema.OptionsMap,
+		v1.SearchCategory_NODES:               nodeSearchOptions,
+		// v1.SearchCategory_PODS:                  podMapping.OptionsMap,
+		v1.SearchCategory_PODS: schema.PodsSchema.OptionsMap,
+		// v1.SearchCategory_POLICIES:              policyMapping.OptionsMap,
+		v1.SearchCategory_POLICIES: schema.PoliciesSchema.OptionsMap,
+		// v1.SearchCategory_POLICY_CATEGORIES:     policyCategoryMapping.OptionsMap,
+		v1.SearchCategory_POLICY_CATEGORIES: schema.PolicyCategoriesSchema.OptionsMap,
+		// v1.SearchCategory_PROCESS_BASELINES:     processBaselineMapping.OptionsMap,
+		v1.SearchCategory_PROCESS_BASELINES: schema.ProcessBaselinesSchema.OptionsMap,
+		// v1.SearchCategory_PROCESS_INDICATORS:    processindicators.OptionsMap,
+		v1.SearchCategory_PROCESS_INDICATORS: schema.ProcessIndicatorsSchema.OptionsMap,
+		// v1.SearchCategory_REPORT_CONFIGURATIONS: reportConfigurationsMapping.OptionsMap,
+		v1.SearchCategory_REPORT_CONFIGURATIONS: schema.ReportConfigurationsSchema.OptionsMap,
+		// v1.SearchCategory_RISKS:                 riskMappings.OptionsMap,
+		v1.SearchCategory_RISKS: schema.RisksSchema.OptionsMap,
+		// v1.SearchCategory_ROLES:                 roleOptions.OptionsMap,
+		v1.SearchCategory_ROLES: schema.K8sRolesSchema.OptionsMap,
+		// v1.SearchCategory_ROLEBINDINGS:          roleBindingOptions.OptionsMap,
+		v1.SearchCategory_ROLEBINDINGS: schema.RoleBindingsSchema.OptionsMap,
+		// v1.SearchCategory_SECRETS:               secretOptions.OptionsMap,
+		v1.SearchCategory_SECRETS: schema.SecretsSchema.OptionsMap,
+		// v1.SearchCategory_SERVICE_ACCOUNTS:      serviceAccountOptions.OptionsMap,
+		v1.SearchCategory_SERVICE_ACCOUNTS: schema.ServiceAccountsSchema.OptionsMap,
+		// v1.SearchCategory_SUBJECTS:              subjectMapping.OptionsMap,
+		v1.SearchCategory_SUBJECTS: schema.RoleBindingsSchema.OptionsMap,
+		//v1.SearchCategory_VULN_REQUEST:          vulnReqMapping.OptionsMap,
+		v1.SearchCategory_VULN_REQUEST:    schema.VulnerabilityRequestsSchema.OptionsMap,
+		v1.SearchCategory_VULNERABILITIES: cveSearchOptions,
+	}
+
+	return entityOptionsMap
+}
+
 // GetEntityOptionsMap is a mapping from search categories to the options
 func GetEntityOptionsMap() map[v1.SearchCategory]search.OptionsMap {
+	if features.PostgresDatastore.Enabled() {
+		return getPostgresEntityOptionsMap()
+	}
 	nodeSearchOptions := search.CombineOptionsMaps(
 		nodeMapping.OptionsMap,
 		nodeComponentEdgeMapping.OptionsMap,
