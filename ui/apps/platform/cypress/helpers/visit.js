4cfae29fa7e0445d9dbebd9808a2245dda584b64
Mark Pedrotti <pedrottimark@gmail.com>
2022-08-25 13:49:24 -0400

    Prevent timing failures in auth providers integration tests (#2805)

--- ui/apps/platform/cypress/helpers/visit.js
+++ ui/apps/platform/cypress/helpers/visit.js
@@ -18,10 +18,42 @@ import { interceptRequests, waitForResponses } from './request';
  * graphqlMultiAliasMap: { opname: { aliases, routeHandler }, … }
  *
  * Optionally wait for responses with waitOptions: { requestTimeout, responseTimeout }
+ *
+ * @param {string} pageUrl
+ * @param {{ routeMatcherMap?: Record<string, { method: string, url: string }>, opnameAliasesMap?: Record<string, (request: Object) => boolean>, waitOptions?: { requestTimeout?: number, responseTimeout?: number } }} [requestConfig]
+ * @param {Record<string, { body: unknown } | { fixture: string }>} [staticResponseMap]
  */
 export function visit(pageUrl, requestConfig, staticResponseMap) {
     cy.intercept('GET', api.featureFlags).as('featureflags');
     cy.intercept('GET', api.roles.mypermissions).as('mypermissions');
+    cy.intercept('GET', api.system.configPublic).as('config/public');
+    interceptRequests(requestConfig, staticResponseMap);
+
+    cy.visit(pageUrl);
+
+    cy.wait(['@featureflags', '@mypermissions', '@config/public']);
+    waitForResponses(requestConfig);
+}
+
+/*
+ * Visit page to test conditional rendering for user role permissions specified as response or fixture.
+ *
+ * { body: { resourceToAccess: { … } } }
+ * { fixture: 'fixtures/wherever/whatever.json' }
+ *
+ * @param {string} pageUrl
+ * @param {{ body: { resourceToAccess: Record<string, string> } } | { fixture: string }} permissionsStaticResponseMap
+ * @param {{ routeMatcherMap?: Record<string, { method: string, url: string }>, opnameAliasesMap?: Record<string, (request: Object) => boolean>, waitOptions?: { requestTimeout?: number, responseTimeout?: number } }} [requestConfig]
+ * @param {Record<string, { body: unknown } | { fixture: string }>} [staticResponseMap]
+ */
+export function visitWithPermissions(
+    pageUrl,
+    permissionsStaticResponse,
+    requestConfig,
+    staticResponseMap
+) {
+    cy.intercept('GET', api.featureFlags).as('featureflags');
+    cy.intercept('GET', api.roles.mypermissions, permissionsStaticResponse).as('mypermissions');
     cy.intercept('GET', api.system.configPublic).as('config/public');
     interceptRequests(requestConfig, staticResponseMap);
 
