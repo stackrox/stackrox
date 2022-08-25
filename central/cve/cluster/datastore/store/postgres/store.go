60a56a8892bfcc1ce8f359b248b804f003a30bf0
Yann Brillouet <ybrillou@redhat.com>
2022-08-25 13:45:34 +0200

    ROX-11284: Map ClusterCVE to Cluster resource

--- central/cve/cluster/datastore/store/postgres/store.go
+++ central/cve/cluster/datastore/store/postgres/store.go
@@ -43,7 +43,7 @@ const (
 var (
 	log            = logging.LoggerForModule()
 	schema         = pkgSchema.ClusterCvesSchema
-	targetResource = resources.ClusterCVE
+	targetResource = resources.Cluster
 )
 
 type Store interface {
