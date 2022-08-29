bcc31bbf74ebd06665e5572f1b6accf8a24518be
Marcin Owsiany <porridge@redhat.com>
2022-08-29 10:37:33 +0200

    Do not call handle_e2e_progress_failures in operator e2e tests.

--- scripts/ci/jobs/openshift_4_operator_e2e_tests.py
+++ scripts/ci/jobs/openshift_4_operator_e2e_tests.py
@@ -13,4 +13,4 @@ ClusterTestRunner(
     pre_test=PreSystemTests(),
     test=OperatorE2eTest(),
     post_test=PostClusterTest(collect_central_artifacts=False),
-    final_post=FinalPost()).run()
+    final_post=FinalPost(handle_e2e_progress_failures=False)).run()
