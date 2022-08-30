653a181699f91463b09771022dd442b4fbb2c726
md2119 <mandar@stackrox.com>
2022-08-30 11:02:53 -0700

    Make operating system searchable

--- central/cve/node/datastore/store/postgres/store.go
+++ central/cve/node/datastore/store/postgres/store.go
@@ -90,6 +90,7 @@ func insertIntoNodeCves(ctx context.Context, batch *pgx.Batch, obj *storage.Node
 		obj.GetCveBaseInfo().GetCve(),
 		pgutils.NilOrTime(obj.GetCveBaseInfo().GetPublishedOn()),
 		pgutils.NilOrTime(obj.GetCveBaseInfo().GetCreatedAt()),
+		obj.GetOperatingSystem(),
 		obj.GetCvss(),
 		obj.GetSeverity(),
 		obj.GetImpactScore(),
@@ -98,7 +99,7 @@ func insertIntoNodeCves(ctx context.Context, batch *pgx.Batch, obj *storage.Node
 		serialized,
 	}
 
-	finalStr := "INSERT INTO node_cves (Id, CveBaseInfo_Cve, CveBaseInfo_PublishedOn, CveBaseInfo_CreatedAt, Cvss, Severity, ImpactScore, Snoozed, SnoozeExpiry, serialized) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) ON CONFLICT(Id) DO UPDATE SET Id = EXCLUDED.Id, CveBaseInfo_Cve = EXCLUDED.CveBaseInfo_Cve, CveBaseInfo_PublishedOn = EXCLUDED.CveBaseInfo_PublishedOn, CveBaseInfo_CreatedAt = EXCLUDED.CveBaseInfo_CreatedAt, Cvss = EXCLUDED.Cvss, Severity = EXCLUDED.Severity, ImpactScore = EXCLUDED.ImpactScore, Snoozed = EXCLUDED.Snoozed, SnoozeExpiry = EXCLUDED.SnoozeExpiry, serialized = EXCLUDED.serialized"
+	finalStr := "INSERT INTO node_cves (Id, CveBaseInfo_Cve, CveBaseInfo_PublishedOn, CveBaseInfo_CreatedAt, OperatingSystem, Cvss, Severity, ImpactScore, Snoozed, SnoozeExpiry, serialized) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) ON CONFLICT(Id) DO UPDATE SET Id = EXCLUDED.Id, CveBaseInfo_Cve = EXCLUDED.CveBaseInfo_Cve, CveBaseInfo_PublishedOn = EXCLUDED.CveBaseInfo_PublishedOn, CveBaseInfo_CreatedAt = EXCLUDED.CveBaseInfo_CreatedAt, OperatingSystem = EXCLUDED.OperatingSystem, Cvss = EXCLUDED.Cvss, Severity = EXCLUDED.Severity, ImpactScore = EXCLUDED.ImpactScore, Snoozed = EXCLUDED.Snoozed, SnoozeExpiry = EXCLUDED.SnoozeExpiry, serialized = EXCLUDED.serialized"
 	batch.Queue(finalStr, values...)
 
 	return nil
@@ -123,6 +124,8 @@ func (s *storeImpl) copyFromNodeCves(ctx context.Context, tx pgx.Tx, objs ...*st
 		"cvebaseinfo_publishedon",
 
 		"cvebaseinfo_createdat",
+
+		"operatingsystem",
 
 		"cvss",
 
@@ -155,6 +158,8 @@ func (s *storeImpl) copyFromNodeCves(ctx context.Context, tx pgx.Tx, objs ...*st
 			pgutils.NilOrTime(obj.GetCveBaseInfo().GetPublishedOn()),
 
 			pgutils.NilOrTime(obj.GetCveBaseInfo().GetCreatedAt()),
+
+			obj.GetOperatingSystem(),
 
 			obj.GetCvss(),
 
