4b2a4c133899087c9ba403399c2af561b82904e7
Yann Brillouet <ybrillou@redhat.com>
2022-08-31 11:55:47 +0200

    Alter postgres Global Index mapping

--- central/globalindex/mapping/mapping.go
+++ central/globalindex/mapping/mapping.go
@@ -26,7 +26,6 @@ import (
 	podMapping "github.com/stackrox/rox/central/pod/mappings"
 	policyMapping "github.com/stackrox/rox/central/policy/index/mappings"
 	policyCategoryMapping "github.com/stackrox/rox/central/policycategory/index/mappings"
-
 	processBaselineMapping "github.com/stackrox/rox/central/processbaseline/index/mappings"
 	roleOptions "github.com/stackrox/rox/central/rbac/k8srole/mappings"
 	roleBindingOptions "github.com/stackrox/rox/central/rbac/k8srolebinding/mappings"
@@ -100,77 +99,104 @@ func getPostgresEntityOptionsMap() map[v1.SearchCategory]search.OptionsMap {
 	// Note: with the dackbox graph split brought with the postgres migration, the concept
 	// of CVE was split into ClusterCVE, ImageCVE and NodeCVE. The old content seems to focus
 	// mostly on image CVEs.
-	cveSearchOptions := search.CombineOptionsMaps(
-		schema.ImageCvesSchema.OptionsMap,
-		schema.ImageComponentCveEdgesSchema.OptionsMap,
-		schema.ImageComponentsSchema.OptionsMap,
-		schema.ImageComponentEdgesSchema.OptionsMap,
-		schema.ImagesSchema.OptionsMap,
-		schema.DeploymentsSchema.OptionsMap,
+	// cveSearchOptions := search.CombineOptionsMaps(
+	// 	schema.ImageCvesSchema.OptionsMap,
+	// 	schema.ImageComponentCveEdgesSchema.OptionsMap,
+	// 	schema.ImageComponentsSchema.OptionsMap,
+	// 	schema.ImageComponentEdgesSchema.OptionsMap,
+	// 	schema.ImagesSchema.OptionsMap,
+	// 	schema.DeploymentsSchema.OptionsMap,
+	// )
+	clusterToVulnerabilitySearchOptions := search.CombineOptionsMaps(
+		schema.ClusterCvesSchema.OptionsMaps,
+		schema.ClusterCveEdgesSchema.OptionsMaps,
+		schema.ClustersSchema.OptionsMaps,
 	)
 
 	// Note: with the dackbox graph split brought with the postgres migration, the concept
 	// of Component (ImageComponent) was split into ImageComponent and NodeComponent.
 	// The old content seems to focus mostly on image Components.
-	imageComponentSearchOptions := search.CombineOptionsMaps(
-		schema.ImageComponentsSchema.OptionsMap,
-		schema.ImageComponentEdgesSchema.OptionsMap,
-		schema.ImageComponentCveEdgesSchema.OptionsMap,
-		schema.ImagesSchema.OptionsMap,
-		schema.ImageCvesSchema.OptionsMap,
-	)
+	// imageComponentSearchOptions := search.CombineOptionsMaps(
+	// 	schema.ImageComponentsSchema.OptionsMap,
+	// 	schema.ImageComponentEdgesSchema.OptionsMap,
+	// 	schema.ImageComponentCveEdgesSchema.OptionsMap,
+	// 	schema.ImagesSchema.OptionsMap,
+	// 	schema.ImageCvesSchema.OptionsMap,
+	// )
 
 	// Images in dackbox support an expanded set of search options
-	imageSearchOptions := search.CombineOptionsMaps(
-		schema.ImagesSchema.OptionsMap,
-		schema.ImageComponentEdgesSchema.OptionsMap,
-		schema.ImageComponentsSchema.OptionsMap,
-		schema.ImageComponentCveEdgesSchema.OptionsMap,
+	// imageSearchOptions := search.CombineOptionsMaps(
+	// 	schema.ImagesSchema.OptionsMap,
+	// 	schema.ImageComponentEdgesSchema.OptionsMap,
+	// 	schema.ImageComponentsSchema.OptionsMap,
+	// 	schema.ImageComponentCveEdgesSchema.OptionsMap,
+	// 	schema.ImageCvesSchema.OptionsMap,
+	// )
+	imageToVulnerabilitySearchOptions := search.CombineOptionsMaps(
 		schema.ImageCvesSchema.OptionsMap,
+		schema.ImageCveEdgesSchema.OptionsMap,
+		schema.ImageComponentCveEdgesSchema.OptionsMap,
+		schema.ImageComponentsSchema.OptionsMap,
+		schema.ImageComponentEdgesSchema.OptionsMap,
+		schema.ImagesSchema.OptionsMap,
+		schema.DeploymentsSchema.OptionsMap,
+		schema.NamespacesSchema.OptionsMap,
+		schema.ClustersSchema.OptionsMap,
 	)
 
-	nodeSearchOptions := search.CombineOptionsMaps(
-		schema.NodesSchema.OptionsMap,
-		schema.NodeComponentEdgesSchema.OptionsMap,
-		schema.NodeComponentsSchema.OptionsMap,
-		schema.NodeComponentsCvesEdgesSchema.OptionsMap,
+	// nodeSearchOptions := search.CombineOptionsMaps(
+	// 	schema.NodesSchema.OptionsMap,
+	// 	schema.NodeComponentEdgesSchema.OptionsMap,
+	// 	schema.NodeComponentsSchema.OptionsMap,
+	// 	schema.NodeComponentsCvesEdgesSchema.OptionsMap,
+	// 	schema.NodeCvesSchema.OptionsMap,
+	// )
+	nodeToVulnerabilitySearchOptions := search.CombineOptionsMaps(
 		schema.NodeCvesSchema.OptionsMap,
+		schema.NodeComponentsCvesEdgesSchema.OptionsMap,
+		schema.NodeComponentsSchema.OptionsMap,
+		schema.NodeComponentEdgesSchema.OptionsMap,
+		schema.NodesSchema.OptionsMap,
+		schema.ClustersSchema.OptionsMap,
 	)
 
 	// EntityOptionsMap is a mapping from search categories to the options map for that category.
 	// search document maps are also built off this map
 	entityOptionsMap := map[v1.SearchCategory]search.OptionsMap{
-		v1.SearchCategory_ACTIVE_COMPONENT:      schema.ActiveComponentsSchema.OptionsMap,
-		v1.SearchCategory_ALERTS:                schema.AlertsSchema.OptionsMap,
-		v1.SearchCategory_CLUSTER_VULN_EDGE:     schema.ClusterCveEdgesSchema.OptionsMap,
-		v1.SearchCategory_CLUSTERS:              schema.ClustersSchema.OptionsMap,
-		v1.SearchCategory_COMPLIANCE_STANDARD:   index.StandardOptions,
-		v1.SearchCategory_COMPLIANCE_CONTROL:    index.ControlOptions,
-		v1.SearchCategory_COMPONENT_VULN_EDGE:   schema.ImageComponentCveEdgesSchema.OptionsMap,
-		v1.SearchCategory_DEPLOYMENTS:           schema.DeploymentsSchema.OptionsMap,
-		v1.SearchCategory_IMAGE_COMPONENT_EDGE:  schema.ImageComponentEdgesSchema.OptionsMap,
-		v1.SearchCategory_IMAGE_COMPONENTS:      imageComponentSearchOptions,
-		v1.SearchCategory_IMAGE_INTEGRATIONS:    schema.ImageIntegrationsSchema.OptionsMap,
-		v1.SearchCategory_IMAGE_VULN_EDGE:       schema.ImageCveEdgesSchema.OptionsMap,
-		v1.SearchCategory_IMAGES:                imageSearchOptions,
-		v1.SearchCategory_NAMESPACES:            schema.NamespacesSchema.OptionsMap,
-		v1.SearchCategory_NODE_COMPONENT_EDGE:   schema.NodeComponentEdgesSchema.OptionsMap,
-		v1.SearchCategory_NODE_COMPONENTS:       schema.NodeComponentsSchema.OptionsMap,
-		v1.SearchCategory_NODES:                 nodeSearchOptions,
-		v1.SearchCategory_PODS:                  schema.PodsSchema.OptionsMap,
-		v1.SearchCategory_POLICIES:              schema.PoliciesSchema.OptionsMap,
-		v1.SearchCategory_POLICY_CATEGORIES:     schema.PolicyCategoriesSchema.OptionsMap,
-		v1.SearchCategory_PROCESS_BASELINES:     schema.ProcessBaselinesSchema.OptionsMap,
-		v1.SearchCategory_PROCESS_INDICATORS:    schema.ProcessIndicatorsSchema.OptionsMap,
-		v1.SearchCategory_REPORT_CONFIGURATIONS: schema.ReportConfigurationsSchema.OptionsMap,
-		v1.SearchCategory_RISKS:                 schema.RisksSchema.OptionsMap,
-		v1.SearchCategory_ROLES:                 schema.K8sRolesSchema.OptionsMap,
-		v1.SearchCategory_ROLEBINDINGS:          schema.RoleBindingsSchema.OptionsMap,
-		v1.SearchCategory_SECRETS:               schema.SecretsSchema.OptionsMap,
-		v1.SearchCategory_SERVICE_ACCOUNTS:      schema.ServiceAccountsSchema.OptionsMap,
-		v1.SearchCategory_SUBJECTS:              schema.RoleBindingsSchema.OptionsMap,
-		v1.SearchCategory_VULN_REQUEST:          schema.VulnerabilityRequestsSchema.OptionsMap,
-		v1.SearchCategory_VULNERABILITIES:       cveSearchOptions,
+		v1.SearchCategory_ACTIVE_COMPONENT:        schema.ActiveComponentsSchema.OptionsMap,
+		v1.SearchCategory_ALERTS:                  schema.AlertsSchema.OptionsMap,
+		v1.SearchCategory_CLUSTER_VULN_EDGE:       clusterToVulnerabilitySearchOptions,
+		v1.SearchCategory_CLUSTER_VULNERABILITIES: clusterToVulnerabilitySearchOptions,
+		v1.SearchCategory_CLUSTERS:                schema.ClustersSchema.OptionsMap,
+		v1.SearchCategory_COMPLIANCE_STANDARD:     index.StandardOptions,
+		v1.SearchCategory_COMPLIANCE_CONTROL:      index.ControlOptions,
+		v1.SearchCategory_COMPONENT_VULN_EDGE:     schema.ImageComponentCveEdgesSchema.OptionsMap,
+		v1.SearchCategory_DEPLOYMENTS:             schema.DeploymentsSchema.OptionsMap,
+		v1.SearchCategory_IMAGE_COMPONENT_EDGE:    imageToVulnerabilitySearchOptions,
+		v1.SearchCategory_IMAGE_COMPONENTS:        imageToVulnerabilitySearchOptions,
+		v1.SearchCategory_IMAGE_INTEGRATIONS:      schema.ImageIntegrationsSchema.OptionsMap,
+		v1.SearchCategory_IMAGE_VULN_EDGE:         imageToVulnerabilitySearchOptions,
+		v1.SearchCategory_IMAGE_VULNERABILITIES:   imageToVulnerabilitySearchOptions,
+		v1.SearchCategory_IMAGES:                  imageToVulnerabilitySearchOptions,
+		v1.SearchCategory_NAMESPACES:              schema.NamespacesSchema.OptionsMap,
+		v1.SearchCategory_NODE_COMPONENT_EDGE:     nodeToVulnerabilitySearchOptions,
+		v1.SearchCategory_NODE_COMPONENTS:         nodeToVulnerabilitySearchOptions,
+		v1.SearchCategory_NODE_VULNERABILITIES:    nodeToVulnerabilitySearchOptions,
+		v1.SearchCategory_NODES:                   nodeToVulnerabilitySearchOptions,
+		v1.SearchCategory_PODS:                    schema.PodsSchema.OptionsMap,
+		v1.SearchCategory_POLICIES:                schema.PoliciesSchema.OptionsMap,
+		v1.SearchCategory_POLICY_CATEGORIES:       schema.PolicyCategoriesSchema.OptionsMap,
+		v1.SearchCategory_PROCESS_BASELINES:       schema.ProcessBaselinesSchema.OptionsMap,
+		v1.SearchCategory_PROCESS_INDICATORS:      schema.ProcessIndicatorsSchema.OptionsMap,
+		v1.SearchCategory_REPORT_CONFIGURATIONS:   schema.ReportConfigurationsSchema.OptionsMap,
+		v1.SearchCategory_RISKS:                   schema.RisksSchema.OptionsMap,
+		v1.SearchCategory_ROLES:                   schema.K8sRolesSchema.OptionsMap,
+		v1.SearchCategory_ROLEBINDINGS:            schema.RoleBindingsSchema.OptionsMap,
+		v1.SearchCategory_SECRETS:                 schema.SecretsSchema.OptionsMap,
+		v1.SearchCategory_SERVICE_ACCOUNTS:        schema.ServiceAccountsSchema.OptionsMap,
+		v1.SearchCategory_SUBJECTS:                schema.RoleBindingsSchema.OptionsMap,
+		v1.SearchCategory_VULN_REQUEST:            schema.VulnerabilityRequestsSchema.OptionsMap,
+		// v1.SearchCategory_VULNERABILITIES:       cveSearchOptions,
 	}
 
 	return entityOptionsMap
