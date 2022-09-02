33cc4a8975b131cc39b73d639e9d3e4c966333e1
Malte Isberner <2822367+misberner@users.noreply.github.com>
2022-09-02 12:46:17 +0200

    Fix local operator build/start (#2950)

--- operator/hack/ensure-rox-image-exist.sh
+++ operator/hack/ensure-rox-image-exist.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 git fetch origin
 
 root_dir="$(git rev-parse --show-toplevel)"
-image_registry="$(make --quiet -C "$root_dir" default-image-registry)"
-main_image_tag="${MAIN_IMAGE_TAG:-"$(make --quiet -C "$root_dir" tag)"}"
+image_registry="$(make --quiet --no-print-directory -C "$root_dir" default-image-registry)"
+main_image_tag="${MAIN_IMAGE_TAG:-"$(make --quiet --no-print-directory -C "$root_dir" tag)"}"
 main_image="${image_registry}/main:${main_image_tag}"
 
 echo "Ensuring $main_image is available locally"
@@ -19,6 +19,12 @@ fi
 echo "Trying to pull $main_image"
 if ! docker pull "$main_image"; then
   echo "Could not pull $main_image, trying to build it."
+  # Check if building from the checked out tree would actually give us the image we want.
+  working_copy_tag="$(make --quiet --no-print-directory -C "$root_dir" tag)"
+  if [[ "${main_image_tag}" != "${working_copy_tag}" ]]; then
+    echo "I don't know how to build ${main_image} (currently checked out tree would result in tag ${working_copy_tag})"
+    exit 1
+  fi
   make -C "$root_dir" image
 fi
 
