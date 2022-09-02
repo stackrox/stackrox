3998750dd525caf610dbe5cddb086606aa971804
Gavin Jefferies <gavin@stackrox.com>
2022-09-02 09:08:39 -0700

    ROX-11699, ROX-11700: Onboard ARO and ROSA (#2922)

--- scripts/ci/jobs/aro_qa_e2e_tests.py
+++ scripts/ci/jobs/aro_qa_e2e_tests.py
@@ -3,5 +3,12 @@
 """
 Run qa-tests-backend in an ARO cluster provided via automation-flavors/aro.
 """
+import os
+from base_qa_e2e_test import make_qa_e2e_test_runner
+from clusters import AutomationFlavorsCluster
 
-print("stub for ARO tests")
+# set required test parameters
+os.environ["ORCHESTRATOR_FLAVOR"] = "openshift"
+os.environ["SENSOR_HELM_DEPLOY"] = "true"
+
+make_qa_e2e_test_runner(cluster=AutomationFlavorsCluster()).run()
--- scripts/ci/jobs/rosa_qa_e2e_tests.py
+++ scripts/ci/jobs/rosa_qa_e2e_tests.py
@@ -3,5 +3,12 @@
 """
 Run qa-tests-backend in a ROSA cluster provided via automation-flavors/rosa.
 """
+import os
+from base_qa_e2e_test import make_qa_e2e_test_runner
+from clusters import AutomationFlavorsCluster
 
-print("stub for ROSA tests")
+# set required test parameters
+os.environ["ORCHESTRATOR_FLAVOR"] = "openshift"
+os.environ["SENSOR_HELM_DEPLOY"] = "true"
+
+make_qa_e2e_test_runner(cluster=AutomationFlavorsCluster()).run()
