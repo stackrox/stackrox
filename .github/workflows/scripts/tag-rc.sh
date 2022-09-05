d49dbf0a625ff30326c467156a6a3a712aaf202c
Michaël <michael@redhat.com>
2022-09-05 13:11:02 +0200

    ROX-11891: Delete tag from the release HEAD and add an empty commit (#2888)

---
+++ .github/workflows/scripts/tag-rc.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#
+# Add an empty commit and delete the remote tag if the tag exists.
+# Tag and push the branch.
+#
+# Local wet run:
+#
+#   DRY_RUN=false bash local-env.sh tag-rc <tag>
+#
+set -euo pipefail
+
+TAG="$1"
+
+check_not_empty \
+    TAG \
+    \
+    DRY_RUN
+
+if [ "$(git tag "$TAG" --points-at HEAD)" = "$TAG" ]; then
+    git push --delete origin "$TAG"
+    gh_log warning "Existing tag '$TAG' has been deleted."
+fi
+
+git commit --allow-empty --message "Empty commit to trigger CI"
+git tag --force --annotate "$TAG" --message "Upstream release automation"
+
+if [ "$DRY_RUN" = "false" ]; then
+    git push --follow-tags
+fi
+
+gh_summary "Release branch has been tagged with $TAG."
