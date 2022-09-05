58c5384d94a73380639248b190335b21fdc51a85
Frederico Bittencourt <fred.rbittencourt@gmail.com>
2022-09-05 09:41:53 +0200

    ROX-12318: QA compatibility test parameterized sensor version (#2856)

--- qa-tests-backend/scripts/run-part-1.sh
+++ qa-tests-backend/scripts/run-part-1.sh
@@ -8,6 +8,7 @@ source "$ROOT/scripts/ci/lib.sh"
 source "$ROOT/scripts/ci/sensor-wait.sh"
 source "$ROOT/tests/e2e/lib.sh"
 source "$ROOT/tests/scripts/setup-certs.sh"
+source "$ROOT/qa-tests-backend/scripts/lib.sh"
 
 set -euo pipefail
 
@@ -31,29 +32,6 @@ test_part_1() {
     get_ECR_docker_pull_password
 
     run_tests_part_1
-}
-
-deploy_default_psp() {
-    info "Deploy Default PSP for stackrox namespace"
-    "${ROOT}/scripts/ci/create-default-psp.sh"
-}
-
-deploy_webhook_server() {
-    info "Deploy Webhook server"
-
-    local certs_dir
-    certs_dir="$(mktemp -d)"
-    "${ROOT}/scripts/ci/create-webhookserver.sh" "${certs_dir}"
-    ci_export GENERIC_WEBHOOK_SERVER_CA_CONTENTS "$(cat "${certs_dir}/ca.crt")"
-}
-
-get_ECR_docker_pull_password() {
-    info "Get AWS ECR Docker Pull Password"
-
-    aws --version
-    local pass
-    pass="$(aws --region="${AWS_ECR_REGISTRY_REGION}" ecr get-login-password)"
-    ci_export AWS_ECR_DOCKER_PULL_PASSWORD "${pass}"
 }
 
 run_tests_part_1() {
