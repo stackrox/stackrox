cf72a26a5a59d5d4db65f17ec097984d6e59c446
Van Wilson <vwilson@redhat.com>
2022-09-12 12:25:49 -0400

    ROX-12375: Disable sort on Images count until after Postgres launch (#3039)

--- ui/apps/platform/src/Containers/VulnMgmt/List/Components/VulnMgmtListComponents.js
+++ ui/apps/platform/src/Containers/VulnMgmt/List/Components/VulnMgmtListComponents.js
@@ -157,7 +157,7 @@ export function getComponentTableColumns(workflowState) {
         {
             Header: `Images`,
             entityType: entityTypes.IMAGE,
-            headerClassName: `w-1/8 ${defaultHeaderClassName}`,
+            headerClassName: `w-1/8 ${nonSortableHeaderClassName}`,
             className: `w-1/8 ${defaultColumnClassName}`,
             id: componentSortFields.IMAGE_COUNT,
             accessor: 'imageCount',
@@ -169,7 +169,9 @@ export function getComponentTableColumns(workflowState) {
                     selectedRowId={original.id}
                 />
             ),
-            sortField: componentSortFields.IMAGE_COUNT,
+            // TODO: restore sorting on this field, see https://issues.redhat.com/browse/ROX-12548 for context
+            // sortField: componentSortFields.IMAGES,
+            sortable: false,
         },
         {
             Header: `Deployments`,
