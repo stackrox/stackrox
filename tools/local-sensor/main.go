172b876c41bf83f0a539a71cf32374f6d03db038
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2022-09-16 10:03:48 +0200

    Remove play-code

--- tools/local-sensor/main.go
+++ tools/local-sensor/main.go
@@ -382,19 +382,3 @@ func dumpMessages(messages []*central.MsgFromSensor, start, end time.Time, outfi
 	}
 	f(messages, start, end, outfile)
 }
-
-// PrintMemUsage outputs the current, total and OS memory being used. As well as the number
-// of garage collection cycles completed.
-func PrintMemUsage() {
-	var m runtime.MemStats
-	runtime.ReadMemStats(&m)
-	// For info on each, see: https://golang.org/pkg/runtime/#MemStats
-	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
-	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
-	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
-	fmt.Printf("\tNumGC = %v\n", m.NumGC)
-}
-
-func bToMb(b uint64) uint64 {
-	return b / 1024 / 1024
-}
