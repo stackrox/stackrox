3bf98936b93050bc2ad72bb7446af6d1320b3f04
Tom Martensen <tmartens@redhat.com>
2022-09-20 11:55:49 +0200

    Add parameter to draft-release-notes to specify the output file
    Co-authored-by: Piotr Rygielski <114479+vikin91@users.noreply.github.com>

--- .github/workflows/scripts/draft-release-notes.sh
+++ .github/workflows/scripts/draft-release-notes.sh
@@ -7,10 +7,12 @@ set -euo pipefail
 
 VERSION="$1"
 RELEASE_BRANCH="$2"
+OUTPUT="${3:-RELEASE_NOTES_GENERATED.md}"
 
 check_not_empty \
     VERSION \
-    RELEASE_BRANCH
+    RELEASE_BRANCH \
+    OUTPUT
 
 get_current_release_changelog() {
     gh api \
@@ -20,10 +22,10 @@ get_current_release_changelog() {
 }
 
 create_release_notes() {
-    sed -n "/^## \[$ESCAPED_VERSION]$/,/^## \[/p" CHANGELOG.md | sed '1d;$d' > RELEASE_NOTES_GENERATED.md
-    if [ $(wc -m RELEASE_NOTES_GENERATED.md | awk '{ print $1 }') -gt 150000 ]; then
+    sed -n "/^## \[$ESCAPED_VERSION]$/,/^## \[/p" CHANGELOG.md | sed '1d;$d' > "$OUTPUT"
+    if [ $(wc -m "$OUTPUT" | awk '{ print $1 }') -gt 150000 ]; then
         PREVIOUS_VERSION=$(sed -n "/^## \[$ESCAPED_VERSION]$/,/^## \[/p" CHANGELOG.md | tail -n 1 | tr -d '#[] ')
-        echo "**Full Changelog**: https://github.com/${REPO_NAME}/compare/${PREVIOUS_VERSION}...${VERSION}" > RELEASE_NOTES_GENERATED.md
+        echo "**Full Changelog**: https://github.com/${REPO_NAME}/compare/${PREVIOUS_VERSION}...${VERSION}" > "$OUTPUT"
     fi
 }
 
