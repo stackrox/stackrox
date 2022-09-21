72e4a8cf3bb71690499561a1bdfefda158a1fe55
Mark Pedrotti <pedrottimark@gmail.com>
2022-09-21 11:50:28 -0400

    Call assertSortedItems in vulnmanagement integration tests (#3144)

--- ui/apps/platform/cypress/integration/vulnmanagement/nodeCvesListPages.test.js
+++ ui/apps/platform/cypress/integration/vulnmanagement/nodeCvesListPages.test.js
@@ -1,8 +1,16 @@
 import { selectors } from '../../constants/VulnManagementPage';
 import withAuth from '../../helpers/basicAuth';
 import { hasFeatureFlag } from '../../helpers/features';
+import {
+    assertSortedItems,
+    callbackForPairOfAscendingNumberValuesFromElements,
+    callbackForPairOfAscendingVulnerabilitySeverityValuesFromElements,
+    callbackForPairOfDescendingNumberValuesFromElements,
+    callbackForPairOfDescendingVulnerabilitySeverityValuesFromElements,
+} from '../../helpers/sort';
 import { hasExpectedHeaderColumns } from '../../helpers/vmWorkflowUtils';
 import {
+    interactAndWaitForVulnerabilityManagementEntities,
     verifySecondaryEntities,
     visitVulnerabilityManagementEntities,
 } from '../../helpers/vulnmanagement/entities';
@@ -38,8 +46,77 @@ describe('Vulnerability Management Node CVEs', () => {
         );
     });
 
-    // TODO to be fixed after back end sorting is fixed
-    // validateSortForCVE(selectors.cvesCvssScoreCol);
+    it('should sort the CVSS Score column', () => {
+        visitVulnerabilityManagementEntities(entitiesKey);
+
+        const thSelector = '.rt-th:contains("CVSS Score")';
+        const tdSelector = '.rt-td:nth-child(7) [data-testid="label-chip"]';
+
+        // 0. Initial table state indicates that the column is sorted descending.
+        cy.get(thSelector).should('have.class', '-sort-desc');
+        cy.get(tdSelector).then((items) => {
+            assertSortedItems(items, callbackForPairOfDescendingNumberValuesFromElements);
+        });
+
+        // 1. Sort ascending by the column.
+        interactAndWaitForVulnerabilityManagementEntities(() => {
+            cy.get(thSelector).click();
+        }, entitiesKey);
+        cy.location('search').should('eq', '?sort[0][id]=CVSS&sort[0][desc]=false');
+
+        cy.get(thSelector).should('have.class', '-sort-asc');
+        cy.get(tdSelector).then((items) => {
+            assertSortedItems(items, callbackForPairOfAscendingNumberValuesFromElements);
+        });
+
+        // 2. Sort descending by the column.
+        cy.get(thSelector).click(); // no request because initial response has been cached
+        cy.location('search').should('eq', '?sort[0][id]=CVSS&sort[0][desc]=true');
+
+        cy.get(thSelector).should('have.class', '-sort-desc');
+        // Do not assert because of potential timing problem: get td elements before table re-renders.
+    });
+
+    // TODO Investigate whether not yet supported or incorrect field in payload.
+    it.skip('should sort the Severity column', () => {
+        visitVulnerabilityManagementEntities(entitiesKey);
+
+        const thSelector = '.rt-th:contains("Severity")';
+        const tdSelector = '.rt-td:nth-child(6)';
+
+        // 0. Initial table state indicates that the column is not sorted.
+        cy.get(thSelector)
+            .should('not.have.class', '-sort-asc')
+            .should('not.have.class', '-sort-desc');
+
+        // 1. Sort ascending by the column.
+        interactAndWaitForVulnerabilityManagementEntities(() => {
+            cy.get(thSelector).click();
+        }, entitiesKey);
+        cy.location('search').should('eq', '?sort[0][id]=Severity&sort[0][desc]=false');
+
+        cy.get(thSelector).should('have.class', '-sort-asc');
+        cy.get(tdSelector).then((items) => {
+            assertSortedItems(
+                items,
+                callbackForPairOfAscendingVulnerabilitySeverityValuesFromElements
+            );
+        });
+
+        // 2. Sort descending by the column.
+        interactAndWaitForVulnerabilityManagementEntities(() => {
+            cy.get(thSelector).click();
+        }, entitiesKey);
+        cy.location('search').should('eq', '?sort[0][id]=Severity&sort[0][desc]=true');
+
+        cy.get(thSelector).should('have.class', '-sort-desc');
+        cy.get(tdSelector).then((items) => {
+            assertSortedItems(
+                items,
+                callbackForPairOfDescendingVulnerabilitySeverityValuesFromElements
+            );
+        });
+    });
 
     it('should display vulnerability descriptions', () => {
         visitVulnerabilityManagementEntities(entitiesKey);
