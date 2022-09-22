c570987816e450cceca97fb6224623f0c53f1ee6
David Vail <dcvail17@gmail.com>
2022-09-22 11:47:19 -0400

    Implement deletion for collections table

--- ui/apps/platform/src/Containers/Collections/CollectionsTablePage.tsx
+++ ui/apps/platform/src/Containers/Collections/CollectionsTablePage.tsx
@@ -11,16 +11,22 @@ import {
     Alert,
     Bullseye,
     Spinner,
+    AlertActionCloseButton,
+    AlertGroup,
+    AlertVariant,
 } from '@patternfly/react-core';
+import pluralize from 'pluralize';
 
 import PageTitle from 'Components/PageTitle';
 import LinkShim from 'Components/PatternFly/LinkShim';
 import { collectionsPath } from 'routePaths';
 import useRestQuery from 'Containers/Dashboard/hooks/useRestQuery';
-import { getCollectionCount, listCollections } from 'services/CollectionsService';
+import { deleteCollection, getCollectionCount, listCollections } from 'services/CollectionsService';
 import useURLSearch from 'hooks/useURLSearch';
 import useURLPagination from 'hooks/useURLPagination';
 import useURLSort from 'hooks/useURLSort';
+import { Empty } from 'services/types';
+import useToasts, { Toast } from 'hooks/patternfly/useToasts';
 import CollectionsTable from './CollectionsTable';
 
 type CollectionsTablePageProps = {
@@ -37,18 +43,66 @@ function CollectionsTablePage({ hasWriteAccessForCollections }: CollectionsTable
     const pagination = useURLPagination(20);
     const { page, perPage, setPage } = pagination;
     const { sortOption, getSortParams } = useURLSort(sortOptions);
+    const { toasts, addToast, removeToast } = useToasts();
 
     const listQuery = useCallback(
         () => listCollections(searchFilter, sortOption, page - 1, perPage),
         [searchFilter, sortOption, page, perPage]
     );
-    const { data: listData, loading: listLoading, error: listError } = useRestQuery(listQuery);
+    const {
+        data: listData,
+        loading: listLoading,
+        error: listError,
+        refetch: listRefetch,
+    } = useRestQuery(listQuery);
 
     const countQuery = useCallback(() => getCollectionCount(searchFilter), [searchFilter]);
-    const { data: countData, loading: countLoading, error: countError } = useRestQuery(countQuery);
+    const {
+        data: countData,
+        loading: countLoading,
+        error: countError,
+        refetch: countRefetch,
+    } = useRestQuery(countQuery);
     const isDataAvailable = typeof listData !== 'undefined' && typeof countData !== 'undefined';
     const isLoading = !isDataAvailable && (listLoading || countLoading);
     const loadError = listError || countError;
+
+    /**
+     * Deletes an array of collections by ids. Will alert individually for any deletion
+     * requests that fail.
+     */
+    function onCollectionDelete(ids: string[]) {
+        const promises: Promise<Empty>[] = [];
+        ids.forEach((id) => {
+            const deletionPromise = deleteCollection(id).request.catch((err) => {
+                addToast(`Could not delete collection ${id}`, 'danger', err.message);
+                return Promise.reject(err);
+            });
+            promises.push(deletionPromise);
+        });
+
+        return Promise.allSettled(promises).then((promiseResults) => {
+            const totalDeleted = promiseResults.filter((res) => res.status === 'fulfilled').length;
+            const collectionText = pluralize('collection', ids.length);
+
+            if (totalDeleted > 0 && totalDeleted === ids.length) {
+                // All collections deleted successfully
+                addToast(
+                    `Successfully deleted ${totalDeleted} selected ${collectionText}`,
+                    'success'
+                );
+                // Some, but not all, deletion requests failed
+            } else if (totalDeleted > 0) {
+                addToast(
+                    `Deleted ${totalDeleted} of ${ids.length} selected ${collectionText}`,
+                    'warning'
+                );
+            }
+
+            listRefetch();
+            countRefetch();
+        });
+    }
 
     let pageContent = (
         <PageSection variant="light" isFilled>
@@ -81,6 +135,7 @@ function CollectionsTablePage({ hasWriteAccessForCollections }: CollectionsTable
                         setSearchFilter(value);
                     }}
                     getSortParams={getSortParams}
+                    onCollectionDelete={onCollectionDelete}
                     hasWriteAccess={hasWriteAccessForCollections}
                 />
             </PageSection>
@@ -113,6 +168,26 @@ function CollectionsTablePage({ hasWriteAccessForCollections }: CollectionsTable
             </PageSection>
             <Divider component="div" />
             {pageContent}
+            <AlertGroup isToast isLiveRegion>
+                {toasts.map(({ key, variant, title, children }: Toast) => (
+                    <Alert
+                        key={key}
+                        variant={variant}
+                        title={title}
+                        timeout
+                        onTimeout={() => removeToast(key)}
+                        actionClose={
+                            <AlertActionCloseButton
+                                title={title}
+                                variantLabel={variant}
+                                onClose={() => removeToast(key)}
+                            />
+                        }
+                    >
+                        {children}
+                    </Alert>
+                ))}
+            </AlertGroup>
         </>
     );
 }
