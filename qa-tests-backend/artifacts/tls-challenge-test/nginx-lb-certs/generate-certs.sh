06239fe69aafeb6debb845f74e4a70f8be9bc102
Malte Isberner <malte@redhat.com>
2022-09-22 19:36:15 +0200

    use SHA256 signature for test certificate

--- qa-tests-backend/artifacts/tls-challenge-test/nginx-lb-certs/generate-certs.sh
+++ qa-tests-backend/artifacts/tls-challenge-test/nginx-lb-certs/generate-certs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -eo pipefail
+set -exo pipefail
 
 root_ca_exts="
   [req]
@@ -10,13 +10,12 @@ root_ca_exts="
   basicConstraints=CA:TRUE,pathlen:0
 "
 
-openssl req -config <(echo "${root_ca_exts}") -nodes -new -x509 -newkey 2048 -keyout ca-key.pem -out ca.pem -days 1825 -subj "/CN=LoadBalancer Certificate Authority"
+openssl req -config <(echo "${root_ca_exts}") -nodes -new -x509 -newkey 4096 -keyout ca-key.pem -out ca.pem -days 1825 -subj "/CN=LoadBalancer Certificate Authority"
 
 leaf_ca_exts="subjectAltName=DNS:nginx-loadbalancer.qa-tls-challenge"
-openssl genrsa -out leaf-key.pem 2048
+openssl genrsa -out leaf-key.pem 4096
 openssl req -new -key leaf-key.pem -subj "/CN=nginx LoadBalancer" | \
-     openssl x509 -extfile <(echo "${leaf_ca_exts}") -req -CAcreateserial -CA ca.pem -CAkey ca-key.pem -out leaf-cert.pem -days 1825
-
-openssl x509 -outform der -in leaf-key.pem
+     openssl x509 -sha256 -extfile <(echo "${leaf_ca_exts}") -req -CAcreateserial -CA ca.pem -CAkey ca-key.pem -out leaf-cert.pem -days 1825
 
 rm ca.srl
+
