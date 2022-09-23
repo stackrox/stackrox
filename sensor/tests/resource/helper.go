2de65f09db1a67bd73025a584c340e0f9df92b91
Luis Valero Martín <lvalerom@redhat.com>
2022-09-23 16:11:02 +0200

    ROX-12569 Change local-sensor cluster id (#3127)
    * fix Error: uuid: incorrect UUID length 4 in string "1234" in sensor integration tests
    
    * add README.md with instruction for the replay test

--- sensor/tests/resource/helper.go
+++ sensor/tests/resource/helper.go
@@ -234,7 +234,7 @@ func startSensorAndFakeCentral(env *envconf.Config, config CentralConfig) (*cent
 	utils.CrashOnError(os.Setenv("ROX_MTLS_CA_KEY_FILE", "../../../../tools/local-sensor/certs/caKey.pem"))
 
 	fakeCentral := centralDebug.MakeFakeCentralWithInitialMessages(
-		message.SensorHello("1234"),
+		message.SensorHello("00000000-0000-4000-A000-000000000000"),
 		message.ClusterConfig(),
 		message.PolicySync(config.InitialSystemPolicies),
 		message.BaselineSync([]*storage.ProcessBaseline{}))
