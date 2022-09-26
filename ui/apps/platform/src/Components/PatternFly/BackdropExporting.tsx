1a32b4cb5067f71593e94d308b3d89b647443a77
Mark Pedrotti <mark@stackrox.com>
2022-09-26 15:53:39 -0400

    Add isSVG prop to Spinner element

--- ui/apps/platform/src/Components/PatternFly/BackdropExporting.tsx
+++ ui/apps/platform/src/Components/PatternFly/BackdropExporting.tsx
@@ -5,7 +5,7 @@ function BackdropExporting(): ReactElement {
     return (
         <Backdrop>
             <Bullseye>
-                <Spinner />
+                <Spinner isSVG />
             </Bullseye>
         </Backdrop>
     );
