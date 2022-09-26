34561ef814ff55619998ab483aea394a12861116
Oscar Ward <oscarward95@gmail.com>
2022-09-26 09:29:50 -0700

    quick pr fix

--- central/resourcecollection/store/postgres/gen.go
+++ central/resourcecollection/store/postgres/gen.go
@@ -1,3 +1,3 @@
 package postgres
 
-//go:generate pg-table-bindings-wrapper --type=storage.ResourceCollection --table=collections --search-category COLLECTIONS COLLECTION_NAME
+//go:generate pg-table-bindings-wrapper --type=storage.ResourceCollection --table=collections --search-category COLLECTIONS
