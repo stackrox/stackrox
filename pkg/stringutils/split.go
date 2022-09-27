31839cc1666a821e78afe7b7490f9859ce8af9d8
Connor Gorman <connorgorman@users.noreply.github.com>
2022-09-27 12:34:46 -0700

    ROX-12633: Prefer alert lifecycle stage over policy lifecycle stage (#3203)

--- pkg/stringutils/split.go
+++ pkg/stringutils/split.go
@@ -58,3 +58,17 @@ func GetAfterLast(str, sep string) string {
 	}
 	return p2
 }
+
+// GetBetween gets the string between the two passed strings, otherwise it returns the empty string
+func GetBetween(str, start, end string) string {
+	startIdx := strings.Index(str, start)
+	if startIdx == -1 || startIdx == len(str)-len(start) {
+		return ""
+	}
+	offset := startIdx + len(start)
+	endIdx := strings.Index(str[offset:], end)
+	if endIdx == -1 {
+		return ""
+	}
+	return str[offset : offset+endIdx]
+}
