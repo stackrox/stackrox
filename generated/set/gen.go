10f66cf9d2b26ecd3a8884b19bf0480d4f984f04
Michaël Petrov <smartptr@gmail.com>
2022-09-27 10:59:34 +0200

    generic set

--- generated/set/gen.go
+++
@@ -1,4 +0,0 @@
-package set
-
-//go:generate genny -in=../../pkg/set/generic.go -out=v1-search-cats-set.go -imp=github.com/stackrox/rox/generated/api/v1 gen "KeyType=v1.SearchCategory"
-//go:generate genny -in=../../pkg/set/generic.go -out=upgrade-progress-state-set.go -imp=github.com/stackrox/rox/generated/storage gen "KeyType=storage.UpgradeProgress_UpgradeState"
