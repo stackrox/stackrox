5490d03b1997c4a8cad602446024b767019c0d98
Connor Gorman <connorgorman@users.noreply.github.com>
2022-09-28 14:20:35 -0700

    Properly propagate context to Postgres delete request (#3226)

--- migrator/migrations/n_22_to_n_23_postgres_external_backups/postgres/postgres_plugin.go
+++ migrator/migrations/n_22_to_n_23_postgres_external_backups/postgres/postgres_plugin.go
@@ -296,7 +296,7 @@ func (s *storeImpl) Delete(ctx context.Context, id string) error {
 		search.NewQueryBuilder().AddDocIDs(id).ProtoQuery(),
 	)
 
-	return postgres.RunDeleteRequestForSchema(schema, q, s.db)
+	return postgres.RunDeleteRequestForSchema(ctx, schema, q, s.db)
 }
 
 // DeleteByQuery removes the objects based on the passed query
@@ -309,7 +309,7 @@ func (s *storeImpl) DeleteByQuery(ctx context.Context, query *v1.Query) error {
 		query,
 	)
 
-	return postgres.RunDeleteRequestForSchema(schema, q, s.db)
+	return postgres.RunDeleteRequestForSchema(ctx, schema, q, s.db)
 }
 
 // GetIDs returns all the IDs for the store
@@ -384,7 +384,7 @@ func (s *storeImpl) DeleteMany(ctx context.Context, ids []string) error {
 		search.NewQueryBuilder().AddDocIDs(ids...).ProtoQuery(),
 	)
 
-	return postgres.RunDeleteRequestForSchema(schema, q, s.db)
+	return postgres.RunDeleteRequestForSchema(ctx, schema, q, s.db)
 }
 
 // Walk iterates over all of the objects in the store and applies the closure
