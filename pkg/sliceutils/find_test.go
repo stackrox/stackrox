0b00919534d1168a2f86076a0a43bc8fce0c532e
Malte Isberner <malte@redhat.com>
2022-09-29 22:44:31 +0200

    fix test case

--- pkg/sliceutils/find_test.go
+++ pkg/sliceutils/find_test.go
@@ -12,8 +12,8 @@ func TestFind(t *testing.T) {
 	t.Parallel()
 
 	slice := []myType{1, 3, 7}
-	assert.Equal(t, -1, Find(slice, 3))
-	assert.Equal(t, 1, Find(slice, myType(3)))
+	assert.Equal(t, 1, Find(slice, 3))
+	assert.Equal(t, -1, Find(slice, 6))
 }
 
 type myWeirdStruct struct {
