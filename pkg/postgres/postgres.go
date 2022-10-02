149631fdb4940bb52150ac3c093c5c397040d0eb
Connor Gorman <cgorman@redhat.com>
2022-10-02 12:18:50 -0700

    temp

--- pkg/postgres/postgres.go
+++ pkg/postgres/postgres.go
@@ -3,13 +3,10 @@ package postgres
 import (
 	"context"
 
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
 )
-
-// Postgres is a wrapper around access to the database
-type Postgres struct {
-	*pgxpool.Pool
-}
 
 // ConnectConfig wraps pgxpool.Pool with the Postgres struct
 func ConnectConfig(ctx context.Context, config *pgxpool.Config) (*Postgres, error) {
@@ -31,4 +28,29 @@ func Connect(ctx context.Context, connString string) (*Postgres, error) {
 	return &Postgres{
 		Pool: pool,
 	}, nil
+}
+
+// Postgres is a wrapper around access to the database
+type Postgres struct {
+	*pgxpool.Pool
+}
+
+func (p *Postgres) Begin(ctx context.Context) (pgx.Tx, error) {
+
+	return p.Pool.Begin(ctx)
+}
+
+func (p *Postgres) Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error) {
+
+	return p.Pool.Query(ctx, sql, args...)
+}
+
+func (p *Postgres) QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row {
+
+	return p.Pool.QueryRow(ctx, sql, args...)
+}
+
+func (p *Postgres) Exec(ctx context.Context, sql string, args ...interface{}) (pgconn.CommandTag, error) {
+
+	return p.Pool.Exec(ctx, sql, args...)
 }
