be9596a5a74606c270e0f14c18aee717480c0102
David Shrewsberry <99685630+dashrews78@users.noreply.github.com>
2022-10-04 05:09:40 -0400

    Dashrews/ROX-10199 delete stale network flows (#3229)
    remove stale network flows.

--- central/postgres/pruning.go
+++ central/postgres/pruning.go
@@ -14,6 +14,22 @@ const (
 	pruneClusterHealthStatusesStmt = `DELETE FROM cluster_health_statuses child WHERE NOT EXISTS
 		(SELECT 1 FROM clusters parent WHERE
 		child.Id = parent.Id)`
+
+	pruneStaleNetworkFlowsStmt = `DELETE FROM network_flows a USING (
+      SELECT MAX(flow_id) as max_flow, props_srcentity_type, props_srcentity_id, props_dstentity_type, props_dstentity_id, props_dstport, props_l4protocol, clusterid
+        FROM network_flows
+        GROUP BY props_srcentity_type, props_srcentity_id, props_dstentity_type, props_dstentity_id, props_dstport, props_l4protocol, clusterid
+		HAVING COUNT(*) > 1
+      ) b
+      WHERE a.props_srcentity_type = b.props_srcentity_type
+ 	AND a.props_srcentity_id = b.props_srcentity_id
+ 	AND a.props_dstentity_type = b.props_dstentity_type
+ 	AND a.props_dstentity_id = b.props_dstentity_id
+	AND a.props_dstport = b.props_dstport
+	AND a.props_l4protocol = b.props_l4protocol
+	AND a.clusterid = b.clusterid
+      AND a.flow_id <> b.max_flow;
+	`
 )
 
 var (
@@ -33,5 +49,12 @@ func PruneActiveComponents(ctx context.Context, pool *pgxpool.Pool) {
 func PruneClusterHealthStatuses(ctx context.Context, pool *pgxpool.Pool) {
 	if _, err := pool.Exec(ctx, pruneClusterHealthStatusesStmt); err != nil {
 		log.Errorf("failed to prune cluster health statuses: %v", err)
+	}
+}
+
+// PruneStaleNetworkFlows - prunes duplicate network flows
+func PruneStaleNetworkFlows(ctx context.Context, pool *pgxpool.Pool) {
+	if _, err := pool.Exec(ctx, pruneStaleNetworkFlowsStmt); err != nil {
+		log.Errorf("failed to prune stale network flows: %v", err)
 	}
 }
