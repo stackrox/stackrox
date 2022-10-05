f88a1b3350c8cc152957a680891468df1b1cef0f
Misha Sugakov <537715+msugakov@users.noreply.github.com>
2022-10-05 22:46:32 +0200

    Pull k8s events as yaml file also in case of test failure (#3315)

--- scripts/ci/collect-service-logs.sh
+++ scripts/ci/collect-service-logs.sh
@@ -81,6 +81,8 @@ main() {
     done
 
     kubectl -n "${namespace}" get events -o wide >"${log_dir}/events.txt"
+    kubectl -n "${namespace}" get events -o yaml >"${log_dir}/events.yaml"
+
     find "${log_dir}" -type f -size 0 -delete
 }
 
