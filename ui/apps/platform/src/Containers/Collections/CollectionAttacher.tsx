5f86fe9ec00b2574ce6b335cfce68fe235dc9a4b
David Vail <dcvail17@gmail.com>
2022-10-11 12:16:36 -0400

    Implement markup structure of collection form (#3348)

---
+++ ui/apps/platform/src/Containers/Collections/CollectionAttacher.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CollectionAttacher() {
+    return <></>;
+}
+
+export default CollectionAttacher;
---
+++ ui/apps/platform/src/Containers/Collections/CollectionResults.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CollectionResults() {
+    return <></>;
+}
+
+export default CollectionResults;
