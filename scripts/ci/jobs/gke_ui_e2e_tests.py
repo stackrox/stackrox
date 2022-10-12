f75784fceb1639a12c6ce8aaffa1f4772a8ce166
Tomasz Janiszewski <tomek@redhat.com>
2022-10-12 15:24:26 +0200

    Update gke_ui_e2e_tests.py

--- scripts/ci/jobs/gke_ui_e2e_tests.py
+++ scripts/ci/jobs/gke_ui_e2e_tests.py
@@ -13,10 +13,6 @@ from post_tests import PostClusterTest, FinalPost
 # set required test parameters
 os.environ["ORCHESTRATOR_FLAVOR"] = "k8s"
 
-# Override test env defaults here:
-# (for defaults see: tests/e2e/lib.sh export_test_environment())
-os.environ["OUTPUT_FORMAT"] = "helm"
-
 # use postgres
 os.environ["ROX_POSTGRES_DATASTORE"] = "true"
 
