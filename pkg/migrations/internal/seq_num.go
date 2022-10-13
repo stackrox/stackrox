41ddf96788c41b037a5c39e697374360dcfa810a
Connor Gorman <connorgorman@users.noreply.github.com>
2022-10-13 04:05:49 -0500

    Remove ComplianceRunSchedule completely (#3285)

--- pkg/migrations/internal/seq_num.go
+++ pkg/migrations/internal/seq_num.go
@@ -4,7 +4,7 @@ var (
 	// CurrentDBVersionSeqNum is the current DB version number.
 	// This must be incremented every time we write a migration.
 	// It is a shared constant between central and the migrator binary.
-	CurrentDBVersionSeqNum = 108
+	CurrentDBVersionSeqNum = 109
 	// PostgresDBVersionPlus is the current DB version number with Postgres DB data migration.
 	PostgresDBVersionPlus = 56
 )
