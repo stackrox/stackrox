88136db67aeea61a2ddbf143be21ac1a3f6c743a
David Vail <dcvail17@gmail.com>
2022-10-17 09:01:12 -0400

    Delete unused code

--- ui/apps/platform/src/utils/type.utils.ts
+++
@@ -1,8 +0,0 @@
-// TODO Comment
-export function ensureExhaustive(value: never) {
-    // eslint-disable-next-line no-console
-    console.error(
-        'This should never occur, and if it does there is a mismatch between compile time and runtime values'
-    );
-    return value;
-}
