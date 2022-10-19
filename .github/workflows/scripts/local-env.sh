470d12f68d827dad615f9608ce895a62c4eab461
Tom Martensen <tmartens@redhat.com>
2022-10-19 15:56:45 +0200

    ROX-12585, ROX-10890, ROX-12557, ROX-13153: Improve Github Actions usage (#3470)

--- .github/workflows/scripts/local-env.sh
+++ .github/workflows/scripts/local-env.sh
@@ -4,22 +4,23 @@
 # JIRA_TOKEN still has to be set manually.
 #
 # Usage example:
-#     bash local-env.sh create-cluster flavor name ...
+#     bash local-env.sh check-jira-release 3.73.0 ROX
 #
 export GITHUB_STEP_SUMMARY=/dev/stdout
-GITHUB_ACTOR="$(git config --get user.email)"
+export GITHUB_OUTPUT=/dev/stdout
+GITHUB_ACTOR=$(git config --get user.email)
 export GITHUB_ACTOR
 export GITHUB_REPOSITORY=stackrox/stackrox
 export GITHUB_SERVER_URL=https://github.com
 
-main_branch="$(gh repo view --json defaultBranchRef --jq .defaultBranchRef.name)"
+main_branch=$(gh repo view --json defaultBranchRef --jq .defaultBranchRef.name)
 export main_branch
 
 if [ -z "$DRY_RUN" ]; then
     export DRY_RUN=true
 fi
 
-SCRIPTS_ROOT="$(git rev-parse --show-toplevel)/.github/workflows/scripts"
+SCRIPTS_ROOT=$(git rev-parse --show-toplevel)/.github/workflows/scripts
 
 CI="false" # true if running in GitHub context
 export CI
