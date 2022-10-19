c368827db4c019a2e7ab3358a8674933fa7cf592
Cong Du <c-du@users.noreply.github.com>
2022-10-19 10:50:55 -0700

    ROX-13181: Fix more production config to central db (#3478)

--- image/postgres/scripts/init-entrypoint.sh
+++ image/postgres/scripts/init-entrypoint.sh
@@ -4,5 +4,5 @@ set -Eeo pipefail
 
 # Initialize DB if it does not exist
 if [ ! -s "$PGDATA/PG_VERSION" ]; then
-  initdb --auth-host=scram-sha-256 --auth-local=scram-sha-256 --pwfile /run/secrets/stackrox.io/secrets/password
+  initdb --auth-host=scram-sha-256 --auth-local=scram-sha-256 --pwfile /run/secrets/stackrox.io/secrets/password --data-checksums
 fi
