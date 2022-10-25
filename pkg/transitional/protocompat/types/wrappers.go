6139595d0dea2ee17764a67528183f13b43b7c3f
Malte Isberner <malte.isberner@gmail.com>
2022-10-25 17:36:28 +0200

    uint32value wrapper

---
+++ pkg/transitional/protocompat/types/wrappers.go
@@ -0,0 +1,8 @@
+package types
+
+import "google.golang.org/protobuf/types/known/wrapperspb"
+
+type (
+	// UInt32Value is an alias for wrapperspb.UInt32Value
+	UInt32Value = wrapperspb.UInt32Value
+)
