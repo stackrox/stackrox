79225006884b937a71ec5fb1e5c5f436d987e449
Mark Pedrotti <pedrottimark@gmail.com>
2022-10-26 10:00:22 -0400

    Move skipped test from auth to separate logout integration test file (#3557)

---
+++ ui/apps/platform/cypress/integration/logout.test.js
@@ -0,0 +1,42 @@
+import * as api from '../constants/apiEndpoints';
+import { url as loginUrl } from '../constants/LoginPage';
+import { selectors as navSelectors } from '../constants/TopNavigation';
+import withAuth from '../helpers/basicAuth';
+import { visitMainDashboard } from '../helpers/main';
+import { interactAndWaitForResponses } from '../helpers/request';
+
+const logoutAlias = 'logout';
+
+const requestConfigForLogout = {
+    routeMatcherMap: {
+        [logoutAlias]: {
+            method: 'POST',
+            url: api.auth.logout,
+        },
+    },
+};
+
+const staticResponseMapForLogout = {
+    [logoutAlias]: {
+        body: {},
+    },
+};
+
+describe('Logout', () => {
+    withAuth();
+
+    it('go to login page after logout on user menu', () => {
+        visitMainDashboard();
+
+        interactAndWaitForResponses(
+            () => {
+                cy.get(navSelectors.menuButton).click();
+                cy.get(navSelectors.menuList.logoutButton).click();
+            },
+            requestConfigForLogout,
+            staticResponseMapForLogout
+        );
+
+        cy.location('pathname').should('eq', loginUrl);
+    });
+});
