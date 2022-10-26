ee5792526c003e16d49dcb26e48a513a84bbcbd5
Hardik Ahuja <hardikahuja9999@gmail.com>
2022-10-27 02:39:08 +0530

    Update deploy.sh

--- deploy/k8s/deploy.sh
+++ deploy/k8s/deploy.sh
@@ -3,5 +3,5 @@ set -e
 
 K8S_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-source "${K8S_DIR}/central.sh"
-source "${K8S_DIR}/sensor.sh"
+source "${K8S_DIR}"/central.sh
+source "${K8S_DIR}"/sensor.sh
