0a0095b17f30c6b2523e43f18f55d8e2ea6b395f
Matthias Meidinger <mmeiding@redhat.com>
2022-10-26 14:32:43 +0200

    Set NodeScanInterval default value visibility to private

--- pkg/env/node_scan.go
+++ pkg/env/node_scan.go
@@ -3,16 +3,16 @@ package env
 import "time"
 
 var (
-	NodeRescanIntervalDefault = 4 * time.Hour
+	nodeRescanIntervalDefault = 4 * time.Hour
 	// NodeRescanInterval will set the duration for when to scan nodes for vulnerabilities (NodeScanV2)
-	NodeRescanInterval = registerDurationSetting("ROX_NODE_RESCAN_INTERVAL", NodeRescanIntervalDefault)
+	NodeRescanInterval = registerDurationSetting("ROX_NODE_RESCAN_INTERVAL", nodeRescanIntervalDefault)
 )
 
 // GetNodeRescanInterval returns NodeRescanInterval if positive, otherwise returns the default
 func GetNodeRescanInterval() time.Duration {
 	if NodeRescanInterval.DurationSetting() <= 0 {
-		log.Warnf("Negative or zero duration found. Setting to %s.", NodeRescanIntervalDefault.String())
-		return NodeRescanIntervalDefault
+		log.Warnf("Negative or zero duration found. Setting to %s.", nodeRescanIntervalDefault.String())
+		return nodeRescanIntervalDefault
 	}
 	return NodeRescanInterval.DurationSetting()
 }
