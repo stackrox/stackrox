dc04d150726a5fb87d97175a6f18183817e9a31a
Ross Tannenbaum <rtannenb@redhat.com>
2022-10-27 11:08:14 -0700

    validate duration settings (#3558)

--- pkg/env/durationsetting.go
+++ pkg/env/durationsetting.go
@@ -1,10 +1,12 @@
 package env
 
 import (
+	"fmt"
 	"os"
 	"time"
 
 	"github.com/stackrox/rox/pkg/logging"
+	"github.com/stackrox/rox/pkg/utils"
 )
 
 var (
@@ -15,6 +17,7 @@ var (
 type DurationSetting struct {
 	envVar          string
 	defaultDuration time.Duration
+	opts            durationSettingOpts
 }
 
 // EnvVar returns the string name of the environment variable
@@ -32,20 +35,39 @@ func (d *DurationSetting) DurationSetting() time.Duration {
 	val := os.Getenv(d.envVar)
 	if val != "" {
 		dur, err := time.ParseDuration(val)
-		if err == nil {
+		if err == nil && validateDuration(dur, d.opts) == nil {
 			return dur
 		}
-		log.Warnf("%s is not a valid environment variable for %s, using default value: %s", val, d.envVar, d.defaultDuration.String())
+		log.Warnf("%s is not a valid environment variable for %s, using default value: %v", val, d.envVar, d.defaultDuration)
 	}
 	return d.defaultDuration
 }
 
-func registerDurationSetting(envVar string, defaultDuration time.Duration) *DurationSetting {
+func registerDurationSetting(envVar string, defaultDuration time.Duration, options ...DurationSettingOption) *DurationSetting {
+	var opts durationSettingOpts
+	for _, o := range options {
+		o.apply(&opts)
+	}
+
+	utils.CrashOnError(validateDuration(defaultDuration, opts))
+
 	s := &DurationSetting{
 		envVar:          envVar,
 		defaultDuration: defaultDuration,
+		opts:            opts,
 	}
 
 	Settings[s.EnvVar()] = s
 	return s
+}
+
+func validateDuration(d time.Duration, opts durationSettingOpts) error {
+	if d < 0 {
+		return fmt.Errorf("invalid duration: %v < 0", d)
+	}
+	if !opts.zeroAllowed && d == 0 {
+		return fmt.Errorf("invalid duration: %v == 0", d)
+	}
+
+	return nil
 }
