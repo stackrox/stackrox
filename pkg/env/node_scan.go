624fb1a09a00e55824e0909b91e274940143ca36
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2022-10-27 11:14:19 +0200

    Unexport nodeRescanInterval

--- pkg/env/node_scan.go
+++ pkg/env/node_scan.go
@@ -4,15 +4,15 @@ import "time"
 
 var (
 	nodeRescanIntervalDefault = 4 * time.Hour
-	// NodeRescanInterval will set the duration for when to scan nodes for vulnerabilities (NodeScanV2)
-	NodeRescanInterval = registerDurationSetting("ROX_NODE_RESCAN_INTERVAL", nodeRescanIntervalDefault)
+	// nodeRescanInterval will set the duration for when to scan nodes for vulnerabilities (NodeScanV2)
+	nodeRescanInterval = registerDurationSetting("ROX_NODE_RESCAN_INTERVAL", nodeRescanIntervalDefault)
 )
 
 // GetNodeRescanInterval returns NodeRescanInterval if positive, otherwise returns the default
 func GetNodeRescanInterval() time.Duration {
-	if NodeRescanInterval.DurationSetting() <= 0 {
+	if nodeRescanInterval.DurationSetting() <= 0 {
 		log.Warnf("Negative or zero duration found. Setting to %s.", nodeRescanIntervalDefault.String())
 		return nodeRescanIntervalDefault
 	}
-	return NodeRescanInterval.DurationSetting()
+	return nodeRescanInterval.DurationSetting()
 }
