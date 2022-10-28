b870acf39387d890051c17932a8a5ff2c51a5a96
Jan Schnathmeier <jschnath@redhat.com>
2022-10-28 11:06:13 +0200

    Finish quay tag querying

--- .openshift-ci/sensorversions.py
+++ .openshift-ci/sensorversions.py
@@ -6,30 +6,74 @@ Returns the latest patches of the last 4 major versions
 
 import sys
 import re
+import requests
+import json
+
+def isReleaseVersion(version):
+    return bool(re.search(r"\d+\.\d+\.\d+$", version))
+
+def filterTags(tags):
+    filteredtags = []
+    for t in tags:
+        name = t['name']
+        if isReleaseVersion(name):
+            filteredtags.append(name)
+    return filteredtags
+
+def queryQuayForTags():
+    tags = []
+    page = 1
+    pageNotEmpty = True
+    while pageNotEmpty:
+        print("Going through page " + str(page))
+        apiresponse = requests.get("https://quay.io/api/v1/repository/stackrox-io/main/tag/?page=" + str(page) + "&limit=100")
+        rawtags = apiresponse.json()['tags']
+        tags.extend(filterTags(rawtags))
+        if not bool(rawtags):
+            pageNotEmpty = False
+        page+=1
+    return sorted(set(tags))
+
+def transformTagsToNumbers(tags):
+    numTags = []
+    for t in tags:
+        numTags.append(splitVersion(t))
+    return numTags
 
 def splitVersion(version):
     digits = re.search(r"(\d+)\.(\d+)\.\D*(\d+)", version)
     return int(digits.group(1)), int(digits.group(2)), int(digits.group(3))
 
-# TODO: figure out how to check if an image exists and check in this function
-def imageExists(version):
-    x, y, z = splitVersion(version)
-    return (y > z)
-
 # TODO: grab current_image from os.environ["MAIN_IMAGE_TAG"] after manual testing is done
-def getLast4SensorVersions(current_version):
+def getLastSensorVersionsFromQuay(current_version, num_versions):
+    tags = queryQuayForTags()
+    numericaltags = transformTagsToNumbers(tags)
+    print(numericaltags)
+    print(numericaltags[::-1])
+
     x,y,z = splitVersion(current_version)
+
     latestversions = []
-    for y_test in range(y-3,y+1):
-        z_test = 0
-        while imageExists("quay.io/stackrox-io/main:" + str(x) + "." + str(y_test) + "." + str(z_test+1)):
-            z_test += 1
-        latestversions.append(str(x) + "." + str(y_test) + "." + str(z_test))
+
+    ycurr = y
+    for tags in numericaltags[::-1]:
+        if tags[1] < y-num_versions+1:
+            break
+        if tags[1] < ycurr:
+            ycurr = tags[1]
+        if tags[1] == ycurr:
+            latestversions.append(str(tags[0]) + "." + str(tags[1]) + "." + str(tags[2]))
+            ycurr-=1
+
     return latestversions
 
 def main(argv):
-    latestversions = getLast4SensorVersions(argv[1])
-    print(str(latestversions[0]) + " " + str(latestversions[1]) + " " + str(latestversions[2]) + " " + str(latestversions[3]))
+    latestversions = getLastSensorVersionsFromQuay(argv[1], 4)
+    printversions = ""
+    for version in latestversions:
+        printversions += str(version) + " "
+    print(printversions)
+
 
 if (__name__ == "__main__"):
     main(sys.argv)
