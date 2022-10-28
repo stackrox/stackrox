44c2d4d6fffcc7210f3cc9ea7aee7f3a8131eb06
rdani <rachana_sudhindradani@intuit.com>
2022-10-28 09:16:55 +0530

    Update shellcheck comment, add double quotes

--- deploy/k8s/sensor.sh
+++ deploy/k8s/sensor.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
-# shellcheck disable=SC2086
 # shellcheck disable=SC1091
 set -e
 
 K8S_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 COMMON_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../common && pwd)"
 
-source $COMMON_DIR/deploy.sh
-source $COMMON_DIR/k8sbased.sh
-source $COMMON_DIR/env.sh
-source $K8S_DIR/env.sh
+source "$COMMON_DIR"/deploy.sh
+source "$COMMON_DIR"/k8sbased.sh
+source "$COMMON_DIR"/env.sh
+source "$K8S_DIR"/env.sh
 
 if [[ -z $CLUSTER ]]; then
     # shellcheck disable=SC2162
