751f5969df374c1cca0de3768fc58fe3a56c1f10
Mark Pedrotti <mark@stackrox.com>
2022-10-31 11:38:05 -0400

    Add test file and delete encapsulated paths

---
+++ ui/apps/platform/cypress/integration/apiReference.test.js
@@ -0,0 +1,35 @@
+import { visitMainDashboard } from '../helpers/main';
+import { interactAndWaitForResponses } from '../helpers/request';
+import { visit } from '../helpers/visit';
+
+const apiReferencePath = '/main/apidocs';
+
+const apiReferenceAlias = 'docs/swagger';
+
+const requestConfig = {
+    routeMatcherMap: {
+        [apiReferenceAlias]: '/api/docs/swagger',
+    },
+};
+
+const title = 'API Reference';
+
+describe('API Reference', () => {
+    it('should visit via menu on top nav', () => {
+        visitMainDashboard();
+
+        interactAndWaitForResponses(() => {
+            cy.get('button[aria-label="Help menu"').click();
+            cy.get(`a:contains("${title}")`).click();
+        }, requestConfig);
+
+        cy.location('pathname').should('eq', apiReferencePath);
+        cy.get(`h1:contains("${title}")`);
+    });
+
+    it('should visit via path', () => {
+        visit(apiReferencePath, requestConfig);
+
+        cy.get(`h1:contains("${title}")`);
+    });
+});
