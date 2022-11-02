8ca4ea1eeb522c7f8d62562e06cbda76be9334ac
Cong Du <c-du@users.noreply.github.com>
2022-11-01 21:20:31 -0700

    ROX-13204: Create endpoint to generate central db certificates (#3587)

--- central/certgen/service.go
+++ central/certgen/service.go
@@ -53,6 +53,12 @@ func (s *serviceImpl) CustomRoutes() []routes.CustomRoute {
 			ServerHandler: http.HandlerFunc(s.securedClusterHandler),
 			Compression:   false,
 		},
+		{
+			Route:         "/api/extensions/certgen/centraldb",
+			Authorizer:    user.With(permissions.Modify(resources.ServiceIdentity)),
+			ServerHandler: http.HandlerFunc(s.centralDBHandler),
+			Compression:   false,
+		},
 	}
 }
 
