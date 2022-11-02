5f554180e9c11d1c80d923875c783e59787a1de3
Luis Valero Martín <lvalerom@redhat.com>
2022-11-02 10:36:59 +0100

    ROX-12502 Add re-sync feature flag (#3555)

--- pkg/features/list.go
+++ pkg/features/list.go
@@ -48,4 +48,7 @@ var (
 
 	// ProcessesListeningOnPort enables the NetworkFlow code to also update the processes that are listening on ports
 	ProcessesListeningOnPort = registerFeature("Enable Processes Listening on Port", "ROX_PROCESSES_LISTENING_ON_PORT", false)
+
+	// ResyncDisabled disables the resync behavior of the kubernetes listeners in sensor
+	ResyncDisabled = registerFeature("Disable the re-sync", "ROX_RESYNC_DISABLED", false)
 )
