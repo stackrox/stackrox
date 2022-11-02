79b94a9ad2f6cb0ad7ee6b611f71da49f8aee656
Malte Isberner <malte.isberner@gmail.com>
2022-11-02 22:12:19 +0100

    also apply to test files

--- tools/roxvet/common/file_filters.go
+++ tools/roxvet/common/file_filters.go
@@ -3,6 +3,7 @@ package common
 import (
 	"go/ast"
 	"regexp"
+	"strconv"
 )
 
 var (
@@ -22,4 +23,19 @@ func IsGeneratedFile(f *ast.File) bool {
 	}
 
 	return generatedCodeRegex.MatchString(firstComment.Text)
+}
+
+// IsTestFile checks if the given file is a test file.
+// Note: rather than going off of a _test.go suffix, we check if the file imports the "testing" package.
+func IsTestFile(f *ast.File) bool {
+	for _, imp := range f.Imports {
+		pkgPath, err := strconv.Unquote(imp.Path.Value)
+		if err != nil {
+			continue
+		}
+		if pkgPath == "testing" {
+			return true
+		}
+	}
+	return false
 }
