b2d1d4c4a7357a6a3e6ef45061ff119755777c61
Mark Pedrotti <mark@stackrox.com>
2022-11-03 09:54:40 -0400

    Fix type in RegExp

--- ui/apps/platform/cypress/helpers/title.js
+++ ui/apps/platform/cypress/helpers/title.js
@@ -2,5 +2,5 @@
  * Return regular expression to assert page title independent of product branding in testing environment.
  */
 export function getRegExpForTitleWithBranding(title) {
-    return new RegExp(`^${title} | (Red Hat Advanced Cluster Security|StackRox)}$`);
+    return new RegExp(`^${title} | (Red Hat Advanced Cluster Security|StackRox)$`);
 }
