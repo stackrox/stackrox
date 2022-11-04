d41843e8b6b9774f0cdd124e6b53dd749ac6efc9
Frederico Bittencourt <fbittenc@redhat.com>
2022-11-04 10:55:11 +0100

    use deployment reference abstraction when processing deployment objects

---
+++ sensor/common/store/resolver/resolver.go
@@ -0,0 +1,12 @@
+package resolver
+
+import "github.com/stackrox/rox/sensor/common/store"
+
+type DeploymentReference func(store store.DeploymentStore) []string
+
+// ResolveDeploymentIds is an identify function that simply returns a list of deployment ids passed
+func ResolveDeploymentIds(ids ...string) DeploymentReference {
+	return func(_ store.DeploymentStore) []string {
+		return ids
+	}
+}
