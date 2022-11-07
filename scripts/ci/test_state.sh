cca07ff13d5019f3e037864d7a65c8430540919e
Gavin Jefferies <gavin@stackrox.com>
2022-11-07 08:45:51 -0800

    Update scripts/ci/test_state.sh
    Co-authored-by: Marcin Owsiany <porridge@redhat.com>

--- scripts/ci/test_state.sh
+++ scripts/ci/test_state.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# State files used to track test progress across script invocations
+# State files used to track test progress across script invocations.
 
 export STATE_CLUSTER_PROVISIONED="/tmp/state_cluster_provisioned"
 export STATE_IMAGES_AVAILABLE="/tmp/state_images_available"
