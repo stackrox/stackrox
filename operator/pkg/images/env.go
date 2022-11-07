ff7568febbeefa7dd5b7545769efb08a0c27d599
Misha Sugakov <537715+msugakov@users.noreply.github.com>
2022-11-07 13:06:25 +0100

    ROX-9858: Override central-db img with RELATED_IMAGE_CENTRAL_DB (#3718)

--- operator/pkg/images/env.go
+++ operator/pkg/images/env.go
@@ -5,6 +5,7 @@ import "github.com/stackrox/rox/pkg/env"
 // Environment variable settings for related image overrides.
 var (
 	Main          = env.RegisterSetting("RELATED_IMAGE_MAIN")
+	CentralDB     = env.RegisterSetting("RELATED_IMAGE_CENTRAL_DB")
 	Scanner       = env.RegisterSetting("RELATED_IMAGE_SCANNER")
 	ScannerSlim   = env.RegisterSetting("RELATED_IMAGE_SCANNER_SLIM")
 	ScannerDB     = env.RegisterSetting("RELATED_IMAGE_SCANNER_DB")
