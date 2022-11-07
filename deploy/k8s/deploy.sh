35c499415496154ee7b58c4aadad41d89ab3390b
Hardik Ahuja <hardikahuja9999@gmail.com>
2022-11-07 20:36:08 +0530

    Update deploy.sh

--- deploy/k8s/deploy.sh
+++ deploy/k8s/deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-# shellcheck disable=SC1091
 set -e
 
 K8S_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
-
-source "${K8S_DIR}"/central.sh
-source "${K8S_DIR}"/sensor.sh
+# shellcheck source=/dev/null
+source "${K8S_DIR}/central.sh"
+# shellcheck source=/dev/null
+source "${K8S_DIR}/sensor.sh"
