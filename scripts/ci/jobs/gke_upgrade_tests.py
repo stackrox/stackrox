63776adafccc14f5164ee0422d02f074fc2b0f9f
Shane Bostick <tbostick@gmail.com>
2022-11-08 05:17:02 -0800

    git-add-commit [auto]

--- scripts/ci/jobs/gke_upgrade_tests.py
+++ scripts/ci/jobs/gke_upgrade_tests.py
@@ -10,22 +10,8 @@ from pre_tests import PreSystemTests
 from ci_tests import UpgradeTest
 from post_tests import PostClusterTest, FinalPost
 
-# TODO(sbostick): just dev notes — ok to remove
-# This entrypoint runs groovy tests via
-#   * tests/upgrade/run.sh
-#   * tests/upgrade/lib.sh
-# Where it uses:
-#   * make -C qa-tests-backend smoke-test
-#   * make -C qa-tests-backend upgrade-test
-
-# TODO(sbostick): check DEPLOY_DIR
-# (hard-coded in tests/upgrade/run.sh to "deploy/k8s")
-# might need to override for Openshift secured cluster
-
 os.environ["ORCHESTRATOR_FLAVOR"] = "k8s"
 os.environ["SENSOR_HELM_DEPLOY"] = "true"
-
-# don't use postgres
 os.environ["ROX_POSTGRES_DATASTORE"] = "false"
 
 ClusterTestRunner(
