1bce3c8329a8a5c7106e71a3bcfda4c4c4421b1c
Mandar Darwatkar <mandar@stackrox.com>
2022-11-08 08:35:49 -0800

    [ROX-12923] Walk retries - remainder work (#3729)

--- central/sensor/service/pipeline/complianceoperatorrules/pipeline.go
+++ central/sensor/service/pipeline/complianceoperatorrules/pipeline.go
@@ -14,6 +14,7 @@ import (
 	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/pkg/complianceoperator/api/v1alpha1"
 	"github.com/stackrox/rox/pkg/metrics"
+	"github.com/stackrox/rox/pkg/postgres/pgutils"
 	"github.com/stackrox/rox/pkg/set"
 )
 
@@ -37,14 +38,16 @@ type pipelineImpl struct {
 
 func (s *pipelineImpl) Reconcile(ctx context.Context, clusterID string, storeMap *reconciliation.StoreMap) error {
 	existingIDs := set.NewStringSet()
-
-	err := s.datastore.Walk(ctx, func(rule *storage.ComplianceOperatorRule) error {
-		if rule.GetClusterId() == clusterID {
-			existingIDs.Add(rule.GetId())
-		}
-		return nil
-	})
-	if err != nil {
+	walkFn := func() error {
+		existingIDs.Clear()
+		return s.datastore.Walk(ctx, func(rule *storage.ComplianceOperatorRule) error {
+			if rule.GetClusterId() == clusterID {
+				existingIDs.Add(rule.GetId())
+			}
+			return nil
+		})
+	}
+	if err := pgutils.RetryIfPostgres(walkFn); err != nil {
 		return err
 	}
 	store := storeMap.Get((*central.SensorEvent_ComplianceOperatorRule)(nil))
