906cf4b34cba33410a43e80c516491477221b46b
gavin-stackrox <gavin@stackrox.com>
2022-11-11 09:46:48 +0100

    todo

--- tests/e2e/run-e2e-tests-locally.sh
+++ tests/e2e/run-e2e-tests-locally.sh
@@ -6,13 +6,27 @@ set -euo pipefail
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../.. && pwd)"
 source "$ROOT/scripts/lib.sh"
 
-# TODO opts
+# TODO opts - -flavor to distinguish between qa-tests-backend, non-groovy, ui
+#             -suite? -case?
+#             -pre-deployed?
 
-# TODO teardown
+# TODO safety check that this is running against an infra gke-default cluster -
+# ignore if 'pre-deployed'
+
+# TODO teardown - probably optional but defaults to true for 'full' test runs
+
+# TODO got image? - for a 'full' test run we might want to poll for 'canonical'
+# images - poll_for_system_test_images - warn about `make tag` ~ /-dirty/?
 
 # TODO has vault?
 
-# TODO import all the e2e secrets from vualt
+# TODO log in to vault? - can we CLI access to the collections? helpme.
+
+# TODO can access e2e creds? more helpme.
+
+# TODO import all the e2e secrets from vault. be secure.
+
+# TODO space for required env settings e.g. CI=true
 
 # --flavor qa
 "$ROOT/qa-tests-backend/scripts/run-part-1.sh"
