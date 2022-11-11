46a3d8fb16053b637a89d34e6aa2c2ba9b4745c5
gavin-stackrox <gavin@stackrox.com>
2022-11-11 07:34:05 +0100

    todo

--- tests/e2e/run-e2e-tests-locally.sh
+++ tests/e2e/run-e2e-tests-locally.sh
@@ -6,7 +6,13 @@ set -euo pipefail
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../.. && pwd)"
 source "$ROOT/scripts/lib.sh"
 
-# TODO all the bits
+# TODO opts
+
+# TODO teardown
+
+# TODO has vault?
+
+# TODO import all the e2e secrets from vualt
 
 # --flavor qa
 "$ROOT/qa-tests-backend/scripts/run-part-1.sh"
