8f28b02c25e5357a65983635bbf2793379b4fdd4
Jan Schnathmeier <jschnath@redhat.com>
2022-11-14 15:20:43 +0100

    Refactor and simplify using dict

--- .openshift-ci/sensor_versions.py
+++ .openshift-ci/sensor_versions.py
@@ -7,6 +7,7 @@ Returns the latest patches of the last 4 major versions
 import sys
 import re
 import subprocess
+from collections import defaultdict
 
 def is_release_tag_git_cli(version):
     return bool(re.search(r"^\d+\.\d+\.\d+$", version))
@@ -22,33 +23,14 @@ def cli_output_to_tags(stdoutput):
     separated = stdoutput.decode(encoding="utf-8").splitlines()
     return separated
 
-def transform_tags_to_numbers(tags):
-    numTags = []
-    for t in tags:
-        numTags.append(split_version(t))
-    return sorted(numTags)
-
-def split_version(version):
-    digits = re.search(r"(\d+)\.(\d+)\.(\d+)", version)
-    return int(digits.group(1)), int(digits.group(2)), int(digits.group(3))
-
-def extract_y_from_main_image_tag(mainimagetag):
-    return int(re.search(r"\d+\.(\d+)", mainimagetag).group(1))
-
 def get_latest_tags(tags, num_versions):
-    numericaltags = transform_tags_to_numbers(tags)
-    y = extract_y_from_main_image_tag(make_image_tag())
-    latestversions = []
-    ycurr = y
-    for tags in numericaltags[::-1]:
-        if tags[1] < y-num_versions+1:
-            break
-        if tags[1] < ycurr:
-            ycurr = tags[1]
-        if tags[1] == ycurr:
-            latestversions.append(str(tags[0]) + "." + str(tags[1]) + "." + str(tags[2]))
-            ycurr-=1
-    return latestversions
+    top_patch_version = defaultdict(int)
+    for t in tags:
+        [major, minor, patch] = t.split('.')
+        k = '.'.join([major, minor])
+        top_patch_version[k] = max(top_patch_version[k], int(patch))
+    top_major_versions = sorted(list(top_patch_version.keys()), reverse=True)[:num_versions]
+    return [t + '.' + str(top_patch_version[t]) for t in top_major_versions]
 
 def make_image_tag():
     return subprocess.check_output(["make", "tag"]).decode(encoding="utf-8")
