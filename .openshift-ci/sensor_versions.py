793544e33a950f89adf24e10301a4f5c3b253470
Jan Schnathmeier <jschnath@redhat.com>
2022-11-14 15:36:21 +0100

    Ensure that no sensor versions newer than central get tested

--- .openshift-ci/sensor_versions.py
+++ .openshift-ci/sensor_versions.py
@@ -23,17 +23,22 @@ def cli_output_to_tags(stdoutput):
     separated = stdoutput.decode(encoding="utf-8").splitlines()
     return separated
 
+def make_image_tag():
+    return subprocess.check_output(["make", "tag"]).decode(encoding="utf-8")
+
+def extract_y_from_main_image_tag(mainimagetag):
+    return int(re.search(r"\d+\.(\d+)", mainimagetag).group(1))
+
 def get_latest_tags(tags, num_versions):
+    main_image_y = extract_y_from_main_image_tag(make_image_tag())
     top_patch_version = defaultdict(int)
     for t in tags:
         [major, minor, patch] = t.split('.')
         k = '.'.join([major, minor])
-        top_patch_version[k] = max(top_patch_version[k], int(patch))
+        if (int(minor) <= main_image_y):
+            top_patch_version[k] = max(top_patch_version[k], int(patch))
     top_major_versions = sorted(list(top_patch_version.keys()), reverse=True)[:num_versions]
     return [t + '.' + str(top_patch_version[t]) for t in top_major_versions]
-
-def make_image_tag():
-    return subprocess.check_output(["make", "tag"]).decode(encoding="utf-8")
 
 # get_last_sensor_versions_from_git_tags_cli gets the latest patches of the last num_versions major versions via Git CLI
 def get_last_sensor_versions_from_git_tags_cli(num_versions):
