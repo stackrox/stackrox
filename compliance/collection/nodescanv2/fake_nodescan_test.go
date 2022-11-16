b42b7d49fd2c3f31631310a782505a1adaa9eefd
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2022-11-16 17:02:31 +0100

    X-Smart-Branch-Parent: master
    Experiment with calls to Analyze

---
+++ compliance/collection/nodescanv2/fake_nodescan_test.go
@@ -0,0 +1,23 @@
+package nodescanv2
+
+import (
+	"testing"
+
+	"github.com/stackrox/rox/generated/storage"
+	"github.com/stretchr/testify/suite"
+)
+
+func TestFakeNodeScan(t *testing.T) {
+	suite.Run(t, &NodeScanSuite{})
+}
+
+type FakeNodeScanSuite struct {
+	suite.Suite
+}
+
+func (n *FakeNodeScanSuite) TestMessageFormat() {
+	fns, err := (&FakeNodeScanner{}).Scan("someNode")
+	n.Nil(err)
+	n.NotNil(fns)
+	n.IsType(&storage.NodeScanV2{}, fns)
+}
