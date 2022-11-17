dc2502a85dfba1909a373efbf55ff085215598ab
JoukoVirtanen <jouko@stackrox.com>
2022-11-16 20:50:12 -0800

    X-Smart-Squash: Squashed 50 commits:
    4edd34f98f First attempt at testing processes listening on port api
    aea9c8a75f Fixed typo
    7ab4f2862d Corrected import path
    25bfda92b0 make style passes locally
    4dbf4eef12 Trying to add mock api which just returns a blank response
    d83d9bc076 Added missing import
    871c05fb4c Trying to fix mock api
    a99d5f5602 Still trying
    e07d52726c Mocking both GetProcessesListeningOnPortsByNamespace and GetProcessesListeningOnPortsByNamespaceAndDeployment
    219d0a8813 Trying to fix return
    402be4e8fe Fixed return type
    9732c712bf Trying to fix ProcessListeningOnPortWithDeploymentId return
    2510cdb4c3 Changed []v1.ProcessListeningOnPortWithDeploymentId to []*v1.ProcessListeningOnPortWithDeploymentId
    842c269cf4 Added attribute names
    41ad86bf8c Using context
    6b546a25bf Importing allow
    524db1078b Removed commented out code
    6834fccb2c Changed service ProcessListeningOnPortsService to service ProcessesListeningOnPortsService. Other minor changes
    e360603715 make proto-generated-srcs
    a6bfbb5cc3 Changed Process to Processes in service_impl.go where needed
    af4819af3b Changed one more Process to Processes
    d744fdf6b3 Fixed imports
    34fd4982f2 Removed unused imports
    e35d289c8e Added processesListening on ports to main.go
    5aead48201 Brought in changes from mtodor/ROX-13222-fix-nil-issue
    ac310b6165 Removed unneeded space
    901e95720f Fixed import
    f8b80a07a8 Empty commit API return 200
    62ef8dac60 Used gofmt on central/main.go
    16ec9d9a01 Changed RUNTIME to BAT and Integration
    afe02df5ff Empty commit to trigger ci
    a29ecd2b13 Added imports for BAT and Integration
    29538bee2f Test passes locally
    517eb9419a Trying to return something from the API
    c13e5e3a8a Removed duplicate definition
    2bf29c4d99 Fixed incorrect nested definition
    232b6c34f4 service_impl.go passes make style locally
    27953e6f5b Includes process information in mock API
    16f048b63f Non-blank mock responses from both api endpoints
    2f827ff587 Test passes locally
    7c5c605860 Tests for both endpoints
    355a49f978 Fixed style check issues
    4289e00e4d Fixed style issues
    1e5e84aa53 Changed name of processeslisteningonports directory to processlisteningonport
    cbb0b51b7f Removed commented out code
    360a91d469 Changed name of processeslisteningonports directory to processlisteningonport
    70212dc6da Fixed style error in central/main.go
    92d4c5f804 Renamed processlisteningonport back to processeslisteningonports
    8e5aeed10e Renamed processes_listening_on_ports_service.proto to process_listening_on_port_service.proto and processeslisteningonports to processlisteningonport
    60b91b4fc6 make proto-generated-srcs

---
+++ central/processlisteningonport/service/service_impl.go
@@ -0,0 +1,92 @@
+package service
+
+import (
+	"context"
+
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	v1 "github.com/stackrox/rox/generated/api/v1"
+	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/grpc/authz/allow"
+	"google.golang.org/grpc"
+)
+
+type serviceImpl struct{}
+
+// RegisterServiceServer registers this service with the given gRPC Server.
+func (s *serviceImpl) RegisterServiceServer(grpcServer *grpc.Server) {
+	v1.RegisterProcessesListeningOnPortsServiceServer(grpcServer, s)
+}
+
+// RegisterServiceHandler registers this service with the given gRPC Gateway endpoint.
+func (s *serviceImpl) RegisterServiceHandler(ctx context.Context, mux *runtime.ServeMux, conn *grpc.ClientConn) error {
+	return v1.RegisterProcessesListeningOnPortsServiceHandler(ctx, mux, conn)
+}
+
+// AuthFuncOverride specifies the auth criteria for this API.
+func (s *serviceImpl) AuthFuncOverride(ctx context.Context, fullMethodName string) (context.Context, error) {
+	return ctx, allow.Anonymous().Authorized(ctx, fullMethodName)
+}
+
+func (s *serviceImpl) GetProcessesListeningOnPortsByNamespace(context.Context, *v1.GetProcessesListeningOnPortsByNamespaceRequest) (*v1.GetProcessesListeningOnPortsWithDeploymentResponse, error) {
+	processIndicatorUniqueKey := &storage.ProcessIndicatorUniqueKey{
+		PodId:               "nginx-5da7f5-fdsaf",
+		ContainerName:       "nginx",
+		ProcessName:         "nginx",
+		ProcessExecFilePath: "/usr/bin/nginx",
+		ProcessArgs:         "fake args",
+	}
+
+	processListeningOnPort := &storage.ProcessListeningOnPort{
+		Port:           80,
+		Protocol:       storage.L4Protocol_L4_PROTOCOL_UDP,
+		Process:        processIndicatorUniqueKey,
+		CloseTimestamp: nil,
+	}
+
+	processListeningOnPortWithDeploymentID := &v1.ProcessListeningOnPortWithDeploymentId{
+		DeploymentId:              "nginx",
+		ProcessesListeningOnPorts: []*storage.ProcessListeningOnPort{processListeningOnPort},
+	}
+
+	result := &v1.GetProcessesListeningOnPortsWithDeploymentResponse{
+		ProcessesListeningOnPortsWithDeployment: []*v1.ProcessListeningOnPortWithDeploymentId{processListeningOnPortWithDeploymentID},
+	}
+	return result, nil
+}
+
+func (s *serviceImpl) GetProcessesListeningOnPortsByNamespaceAndDeployment(context.Context, *v1.GetProcessesListeningOnPortsByNamespaceAndDeploymentRequest) (*v1.GetProcessesListeningOnPortsResponse, error) {
+	processIndicatorUniqueKey1 := &storage.ProcessIndicatorUniqueKey{
+		PodId:               "nginx-5da7f5-fdsaf",
+		ContainerName:       "nginx",
+		ProcessName:         "nginx",
+		ProcessExecFilePath: "/usr/bin/nginx",
+		ProcessArgs:         "fake args",
+	}
+
+	processListeningOnPort1 := &storage.ProcessListeningOnPort{
+		Port:           80,
+		Protocol:       storage.L4Protocol_L4_PROTOCOL_UDP,
+		Process:        processIndicatorUniqueKey1,
+		CloseTimestamp: nil,
+	}
+
+	processIndicatorUniqueKey2 := &storage.ProcessIndicatorUniqueKey{
+		PodId:               "visa-5da7f5-fdsaf",
+		ContainerName:       "visa",
+		ProcessName:         "visa",
+		ProcessExecFilePath: "/usr/bin/visa",
+		ProcessArgs:         "fake args for visa",
+	}
+
+	processListeningOnPort2 := &storage.ProcessListeningOnPort{
+		Port:           8080,
+		Protocol:       storage.L4Protocol_L4_PROTOCOL_TCP,
+		Process:        processIndicatorUniqueKey2,
+		CloseTimestamp: nil,
+	}
+
+	result := &v1.GetProcessesListeningOnPortsResponse{
+		ProcessesListeningOnPorts: []*storage.ProcessListeningOnPort{processListeningOnPort1, processListeningOnPort2},
+	}
+	return result, nil
+}
