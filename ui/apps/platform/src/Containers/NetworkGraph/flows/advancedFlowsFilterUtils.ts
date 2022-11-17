d9b8c5b3f992523eecf0983f0929ef9b7fdf50d1
Saif Chaudhry <sachaudh@users.noreply.github.com>
2022-11-17 10:48:49 -0800

    ROX-13168: Add traffic filters (#3780)

---
+++ ui/apps/platform/src/Containers/NetworkGraph/flows/advancedFlowsFilterUtils.ts
@@ -0,0 +1,42 @@
+import { AdvancedFlowsFilterType, FilterValue } from './types';
+
+function isValidPort(port: string): boolean {
+    const portNum = parseInt(port, 10);
+    if (Number.isNaN(portNum)) {
+        return false;
+    }
+    return portNum >= 1 && portNum <= 65535;
+}
+
+// This function is used to convert our filters data structure to an array of strings
+// which the Select component expects
+export function filtersToSelections(filters: AdvancedFlowsFilterType): FilterValue[] {
+    return Object.values(filters).reduce((acc, curr) => {
+        return [...acc, ...Object.values(curr)];
+    }, []);
+}
+
+// This function is used to convert the selections array of strings, which the Select component
+// expects, into our filters data structure
+export function selectionsToFilters(selections: string[]): AdvancedFlowsFilterType {
+    const filters: AdvancedFlowsFilterType = {
+        flows: [],
+        directionality: [],
+        protocols: [],
+        ports: [],
+    };
+    selections.forEach((selection) => {
+        if (selection === 'anomalous' || selection === 'baseline') {
+            filters.flows.push(selection);
+        } else if (selection === 'ingress' || selection === 'egress') {
+            filters.directionality.push(selection);
+        } else if (selection === 'TCP' || selection === 'UDP') {
+            filters.protocols.push(selection);
+        } else if (isValidPort(selection)) {
+            filters.ports.push(selection);
+        } else {
+            // do nothing
+        }
+    });
+    return filters;
+}
