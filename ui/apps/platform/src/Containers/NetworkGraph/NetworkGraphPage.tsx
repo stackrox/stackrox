57730d452ee8fb6f73be33e4ac5fdd9bd0de23fd
Van J. Wilson <vwilson@redhat.com>
2022-11-21 18:37:41 -0500

    Add query string parsing to PF Network Graph

--- ui/apps/platform/src/Containers/NetworkGraph/NetworkGraphPage.tsx
+++ ui/apps/platform/src/Containers/NetworkGraph/NetworkGraphPage.tsx
@@ -15,8 +15,12 @@ import {
 } from '@patternfly/react-core';
 import { Model } from '@patternfly/react-topology';
 
+import useFetchClusters from 'hooks/useFetchClusters';
+import useURLSearch from 'hooks/useURLSearch';
+import { SearchFilter } from 'types/search';
 import { fetchNetworkFlowGraph } from 'services/NetworkService';
-import { fetchClustersAsArray, Cluster } from 'services/ClustersService';
+import { getQueryString } from 'utils/queryStringUtils';
+import timeWindowToDate from 'utils/timeWindows';
 
 import PageTitle from 'Components/PageTitle';
 import EdgeStateSelect, { EdgeState } from './EdgeStateSelect';
@@ -29,36 +33,82 @@ const emptyModel = {
     graph: graphModel,
 };
 
+// TODO: get real time window from user input
+const timeWindow = 'Past hours';
+
+// TODO: refactor to another file and import
+function getScopeHierarchyFromSearch(searchFilter: SearchFilter) {
+    const hierarchy: { cluster: string | undefined; namespaces: string[]; deployments: string[] } =
+        {
+            cluster: undefined,
+            namespaces: [],
+            deployments: [],
+        };
+
+    if (!searchFilter.Cluster || Array.isArray(searchFilter.Cluster)) {
+        return hierarchy;
+    }
+    hierarchy.cluster = searchFilter.Cluster;
+
+    if (searchFilter.Namespace) {
+        hierarchy.namespaces = Array.isArray(searchFilter.Namespace)
+            ? searchFilter.Namespace
+            : [searchFilter.Namespace];
+
+        if (searchFilter.Deployment) {
+            hierarchy.deployments = Array.isArray(searchFilter.Deployment)
+                ? searchFilter.Deployment
+                : [searchFilter.Deployment];
+        }
+    }
+    return hierarchy;
+}
+
 function NetworkGraphPage() {
     const [edgeState, setEdgeState] = useState<EdgeState>('active');
     const [model, setModel] = useState<Model>(emptyModel);
     const [isLoading, setIsLoading] = useState(false);
-    const [clusters, setClusters] = useState<Cluster[]>([]);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { searchFilter, setSearchFilter } = useURLSearch();
+
+    const {
+        cluster: clusterFromUrl,
+        namespaces: namespacesFromUrl,
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        deployments: deploymentsFromUrl,
+    } = getScopeHierarchyFromSearch(searchFilter);
+
+    const { clusters } = useFetchClusters();
 
     useEffect(() => {
-        fetchClustersAsArray()
-            .then((response) => {
-                setClusters(response);
-            })
-            .catch(() => {
-                // TODO
-            });
-    }, []);
+        if (clusters.length > 0 && clusterFromUrl) {
+            const selectedClusterId = clusters.find((cl) => cl.name === clusterFromUrl)?.id;
+            if (selectedClusterId) {
+                setIsLoading(true);
 
-    useEffect(() => {
-        if (clusters.length > 0) {
-            setIsLoading(true);
-            fetchNetworkFlowGraph(clusters[0].id, [])
-                .then(({ response }) => {
-                    const dataModel = transformData(response.nodes);
-                    setModel(dataModel);
-                })
-                .catch(() => {
-                    // TODO
-                })
-                .finally(() => setIsLoading(false));
+                const remainingQuery = { ...searchFilter };
+                delete remainingQuery.Cluster;
+                delete remainingQuery.Namespace;
+
+                const queryToUse = getQueryString(remainingQuery);
+
+                fetchNetworkFlowGraph(
+                    selectedClusterId,
+                    namespacesFromUrl,
+                    queryToUse,
+                    timeWindowToDate(timeWindow) || undefined
+                )
+                    .then(({ response }) => {
+                        const dataModel = transformData(response.nodes);
+                        setModel(dataModel);
+                    })
+                    .catch(() => {
+                        // TODO
+                    })
+                    .finally(() => setIsLoading(false));
+            }
         }
-    }, [clusters]);
+    }, [clusters, clusterFromUrl]);
 
     return (
         <>
