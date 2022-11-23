217216f3b1655b2be5a166882897edd58d7d01b9
Gavin Jefferies <gavin@stackrox.com>
2022-11-23 00:25:34 +0000

    revert

--- scripts/ci/create-webhookserver.sh
+++ scripts/ci/create-webhookserver.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../.. && pwd)"
-source "$ROOT/scripts/lib.sh"
-
 set -e
+
+die() {
+  echo >&2 "$@"
+  exit 1
+}
 
 certs_tmp_dir="$1"
 [[ -n "${certs_tmp_dir}" ]] || die "Usage: $0 <certs_dir>"
+
 
 printstatus() {
     echo current resource status ...
@@ -16,8 +19,11 @@ printstatus() {
 
 trap printstatus ERR
 
-"${ROOT}/tests/scripts/setup-certs.sh" "${certs_tmp_dir}" webhookserver.stackrox "Webhook Server CA"
-cd "${ROOT}/webhookserver"
+gitroot="$(git rev-parse --show-toplevel)"
+[[ -n "${gitroot}" ]] || die "Could not determine git root"
+
+"${gitroot}/tests/scripts/setup-certs.sh" "${certs_tmp_dir}" webhookserver.stackrox "Webhook Server CA"
+cd "${gitroot}/webhookserver"
 mkdir -p chart/certs
 cp "${certs_tmp_dir}/tls.crt" "${certs_tmp_dir}/tls.key" chart/certs
 helm -n stackrox upgrade --install webhookserver chart/
@@ -27,14 +33,5 @@ pod="$(kubectl -n stackrox get pod -l app=webhookserver -o name)"
 echo "Got pod ${pod}"
 [[ -n "${pod}" ]]
 kubectl -n stackrox wait --for=condition=ready "${pod}" --timeout=5m
-
-echo "Testing that 8080 is available for port-forward"
-exitstatus=0
-timeout 5s kubectl -n stackrox port-forward "${pod}" 8080:8080 || exitstatus="$?"
-if [[ "${exitstatus}" != "124" ]]; then
-    die "ERROR: local port 8080 is not available for webhookserver port-forward"
-fi
-
-echo "Starting background 8080 port-forward for webhookserver"
 nohup kubectl -n stackrox port-forward "${pod}" 8080:8080 </dev/null > /dev/null 2>&1 &
 sleep 1
