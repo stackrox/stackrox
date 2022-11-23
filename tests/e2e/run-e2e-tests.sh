d9922ab6c3cf9bd2e4a31c530d7425c7f76dcd92
Gavin Jefferies <gavin@stackrox.com>
2022-11-23 00:02:42 +0000

    vault help

--- tests/e2e/run-e2e-tests.sh
+++ tests/e2e/run-e2e-tests.sh
@@ -13,7 +13,7 @@ source "$ROOT/scripts/ci/gcp.sh"
 
 usage() { 
     cat <<_EOH_
-Usage: $0 [-m <tag>] [-o k8s|openshift] [-y] [<flavor one of qa|e2e|ui|upgrade, defaults to qa>]
+Usage: $0 [-m <tag>] [-o k8s|openshift] [-y] [<e2e flavor: one of qa|e2e|ui|upgrade, defaults to qa>]
   -m - override 'make tag' for the version to install.
   -o - choose the cluster variety. defaults to k8s.
   -y - run without prompts.
@@ -28,6 +28,7 @@ if [[ ! -f "/i-am-rox-ci-image" ]]; then
       -w "$ROOT" \
       -e "KUBECONFIG=${kubeconfig}" \
       -v "${kubeconfig}:${kubeconfig}:z" \
+      -e VAULT_TOKEN \
       --platform linux/amd64 \
       --rm -it \
       --entrypoint="$0" \
@@ -38,7 +39,7 @@ fi
 orchestrator="k8s"
 prompt="true"
 
-while getopts ":dyo:m:" option; do
+while getopts ":yo:m:" option; do
     case "$option" in
         o)
             orchestrator="${OPTARG}"
@@ -97,28 +98,27 @@ _EOVERSION_
     exit 1
 fi
 
+# Do we need to login to vault?
 export VAULT_ADDR=https://vault.ci.openshift.org/
-vault login
+if ! vault kv list kv/selfservice/stackrox-stackrox-e2e-tests > /dev/null 2>&1; then
+    echo "Login to OpenShift CI Vault."
+    vault login || true
 
-if ! vault kv list kv/selfservice/stackrox-stackrox-e2e-tests 2>&1 | sed -e 's/^/vault output: /'; then
-    cat <<_EOL_
+    if ! vault kv list kv/selfservice/stackrox-stackrox-e2e-tests 2>&1 | sed -e 's/^/vault output: /'; then
+        cat <<_EOVAULTHELP_
 ERROR: Cannot list vault secrets.
-There are a number of required steps:
+There are a number of required steps to get access to vault:
 1. Log in to the secrets collection manager at https://selfservice.vault.ci.openshift.org/secretcollection?ui=true
-   (This is a RedHat-ism and will require SSO)
+(This is a Red Hat-ism and will require SSO)
 2. Ask a team member to add you to the collections required for this test:
-   stackrox-stackrox-initial and stackrox-stackrox-e2e-tests.
+stackrox-stackrox-initial and stackrox-stackrox-e2e-tests.
 3. Login to the vault at: https://vault.ci.openshift.org/ui/vault/secrets (Use OIDC)
-   You should see these secrets under kv/
-4. Copy a 'token' from that UI and use it to log in to vault with:
-   export VAULT_ADDR=https://vault.ci.openshift.org/
-   vault login
-5. Test with:
-   vault kv list kv/selfservice/stackrox-stackrox-e2e-tests
-6. Rerun this script.
-The 'token' will expire periodically and you will need to re-login.
-_EOL_
-    exit 1
+You should see these secrets under kv/
+4. Copy a 'token' from that UI and rerun this script.
+The 'token' will expire periodically and you will need to renew it through the vault UI.
+_EOVAULTHELP_
+        exit 1
+    fi
 fi
 
 context="$(kubectl config current-context)"
@@ -142,13 +142,12 @@ vault kv get -format=json kv/selfservice/stackrox-stackrox-e2e-tests/credentials
 eval "$(vault kv get -format=json kv/selfservice/stackrox-stackrox-e2e-tests/credentials \
   | jq -r '.data.data | to_entries[] | select( .key|test("^[A-Z]") ) | "export \(.key|@sh)=\(.value|@sh)"')"
 
+# GCP login using the CI service account is required to access infra GKE clusters.
 setup_gcp
 
 if ! kubectl get nodes > /dev/null; then
     die "ERROR: Cannot access a cluster in your environment. Check KUBECONFIG, etc"
 fi
-
-### Run the test! Hold on to your hats.
 
 info "Running the test."
 
@@ -157,7 +156,4 @@ export ORCHESTRATOR_FLAVOR="$orchestrator"
 # required to get a running central
 export ROX_POSTGRES_DATASTORE="${ROX_POSTGRES_DATASTORE:-false}"
 
-# --flavor qa
 "$ROOT/qa-tests-backend/scripts/run-part-1.sh" 2>&1 | sed -e 's/^/test output: /'
-# --flavor e2e
-#"$ROOT/tests/e2e/run.sh"
