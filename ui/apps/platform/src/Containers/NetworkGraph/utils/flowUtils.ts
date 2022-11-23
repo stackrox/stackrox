778af0ba27edee075734ba3e6b805a70c7b054ac
Saif Chaudhry <sachaudh@users.noreply.github.com>
2022-11-23 09:58:04 -0800

    ROX-13175: add table to show baseline flows (#3874)

--- ui/apps/platform/src/Containers/NetworkGraph/utils/flowUtils.ts
+++ ui/apps/platform/src/Containers/NetworkGraph/utils/flowUtils.ts
@@ -3,11 +3,19 @@ import { Flow } from '../types';
 
 export function getAllUniquePorts(flows: Flow[]) {
     const allPorts = flows.reduce((acc, curr) => {
-        if (curr.children.length) {
+        if (curr.children && curr.children.length) {
             return [...acc, ...curr.children.map((child) => child.port)];
         }
         return [...acc, curr.port];
     }, [] as string[]);
     const allUniquePorts = uniq(allPorts);
     return allUniquePorts;
+}
+
+export function getNumFlows(flows: Flow[]) {
+    const numFlows = flows.reduce((acc, curr) => {
+        // if there are no children then it counts as 1 flow
+        return acc + (curr.children && curr.children.length ? curr.children.length : 1);
+    }, 0);
+    return numFlows;
 }
