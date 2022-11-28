876cc5065e4835a0940551ad75f6f3d22644d3a7
David Shrewsberry <99685630+dashrews78@users.noreply.github.com>
2022-11-28 12:28:01 -0500

    Dashrews/rox 12398 backup test eof (WIP) (#3527)
    Add a wait_for_api before running the restore to make sure central from any previous steps is in a good place.

--- tests/e2e/lib.sh
+++ tests/e2e/lib.sh
@@ -440,6 +440,12 @@ db_backup_and_restore_test() {
         die "missing args. usage: db_backup_and_restore_test <output dir>"
     fi
 
+    require_environment "API_ENDPOINT"
+    require_environment "ROX_PASSWORD"
+
+    # Ensure central is ready for requests after any previous tests
+    wait_for_api
+
     local output_dir="$1"
     info "Backing up to ${output_dir}"
     mkdir -p "$output_dir"
