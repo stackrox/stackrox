59a72b8084628910a008c0288accdc45964bf48b
Dave Shrewsberry <dashrews@redhat.com>
2022-11-28 14:02:13 -0500

    use sequence instead of version

--- migrator/clone/postgres/db_clone_manager_impl.go
+++ migrator/clone/postgres/db_clone_manager_impl.go
@@ -183,7 +183,7 @@ func (d *dbCloneManagerImpl) GetCloneToMigrate(rocksVersion *migrations.Migratio
 	}
 
 	prevClone, prevExists := d.cloneMap[PreviousClone]
-	if d.rollbackEnabled() && currClone.GetVersion() != version.GetMainVersion() {
+	if d.rollbackEnabled() && currClone.GetSeqNum() != migrations.CurrentDBVersionSeqNum() {
 		// If previous clone has the same version as current version, the previous upgrade was not completed.
 		// Central could be in a loop of booting up the service. So we should continue to run with current.
 		if prevExists && currClone.GetVersion() == prevClone.GetVersion() {
