a3f16c9c93a131b45faabc435a9f7bd8da7781ae
Misha Sugakov <misha@stackrox.com>
2022-11-29 21:04:26 +0100

    Migrate `CentralReciever` to use `Stopper`

--- sensor/common/sensor/central_receiver_impl.go
+++ sensor/common/sensor/central_receiver_impl.go
@@ -10,47 +10,45 @@ import (
 
 type centralReceiverImpl struct {
 	receivers []common.SensorComponent
-
-	stopC    concurrency.ErrorSignal
-	stoppedC concurrency.ErrorSignal
+	stopper   concurrency.Stopper
 }
 
 func (s *centralReceiverImpl) Start(stream central.SensorService_CommunicateClient, onStops ...func(error)) {
 	go s.receive(stream, onStops...)
 }
 
-func (s *centralReceiverImpl) Stop(err error) {
-	s.stopC.SignalWithError(err)
+func (s *centralReceiverImpl) Stop(_ error) {
+	s.stopper.Client().Stop()
 }
 
 func (s *centralReceiverImpl) Stopped() concurrency.ReadOnlyErrorSignal {
-	return &s.stoppedC
+	return s.stopper.Client().Stopped()
 }
 
-// Take in data processed by central, run post processing, then send it to the output channel.
+// Take in data processed by central, run post-processing, then send it to the output channel.
 func (s *centralReceiverImpl) receive(stream central.SensorService_CommunicateClient, onStops ...func(error)) {
 	defer func() {
-		s.stoppedC.SignalWithError(s.stopC.Err())
-		runAll(s.stopC.Err(), onStops...)
+		s.stopper.Flow().ReportStopped()
+		runAll(s.stopper.Client().Stopped().Err(), onStops...)
 	}()
 
 	for {
 		select {
-		case <-s.stopC.Done():
+		case <-s.stopper.Flow().StopRequested():
 			return
 
 		case <-stream.Context().Done():
-			s.stopC.SignalWithError(stream.Context().Err())
+			s.stopper.Flow().StopWithError(stream.Context().Err())
 			return
 
 		default:
 			msg, err := stream.Recv()
 			if err == io.EOF {
-				s.stopC.Signal()
+				s.stopper.Flow().StopWithError(nil)
 				return
 			}
 			if err != nil {
-				s.stopC.SignalWithError(err)
+				s.stopper.Flow().StopWithError(err)
 				return
 			}
 			for _, r := range s.receivers {
