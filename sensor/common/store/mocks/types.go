4784fc6c8327a100943a5980f66834c5cb1d4a4c
Frederico Bittencourt <fbittenc@redhat.com>
2022-11-29 13:52:15 +0100

    commit generated code

--- sensor/common/store/mocks/types.go
+++ sensor/common/store/mocks/types.go
@@ -368,3 +368,54 @@ func (mr *MockRBACStoreMockRecorder) GetPermissionLevelForDeployment(deployment 
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetPermissionLevelForDeployment", reflect.TypeOf((*MockRBACStore)(nil).GetPermissionLevelForDeployment), deployment)
 }
+
+// MockProvider is a mock of Provider interface.
+type MockProvider struct {
+	ctrl     *gomock.Controller
+	recorder *MockProviderMockRecorder
+}
+
+// MockProviderMockRecorder is the mock recorder for MockProvider.
+type MockProviderMockRecorder struct {
+	mock *MockProvider
+}
+
+// NewMockProvider creates a new mock instance.
+func NewMockProvider(ctrl *gomock.Controller) *MockProvider {
+	mock := &MockProvider{ctrl: ctrl}
+	mock.recorder = &MockProviderMockRecorder{mock}
+	return mock
+}
+
+// EXPECT returns an object that allows the caller to indicate expected use.
+func (m *MockProvider) EXPECT() *MockProviderMockRecorder {
+	return m.recorder
+}
+
+// RBAC mocks base method.
+func (m *MockProvider) RBAC() store.RBACStore {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "RBAC")
+	ret0, _ := ret[0].(store.RBACStore)
+	return ret0
+}
+
+// RBAC indicates an expected call of RBAC.
+func (mr *MockProviderMockRecorder) RBAC() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RBAC", reflect.TypeOf((*MockProvider)(nil).RBAC))
+}
+
+// Services mocks base method.
+func (m *MockProvider) Services() store.ServiceStore {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "Services")
+	ret0, _ := ret[0].(store.ServiceStore)
+	return ret0
+}
+
+// Services indicates an expected call of Services.
+func (mr *MockProviderMockRecorder) Services() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Services", reflect.TypeOf((*MockProvider)(nil).Services))
+}
