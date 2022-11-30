da569d4b66c4513b21635fb8c4074a23988ad3ba
Gavin Jefferies <gavin@stackrox.com>
2022-11-30 09:40:13 -0800

    ROX-9115: Add suite/case/config/debug options to e2e test runner (#3933)
    Co-authored-by: Tomasz Janiszewski <tomek@redhat.com>
    Co-authored-by: Marcin Owsiany <porridge@redhat.com>

--- qa-tests-backend/src/main/groovy/util/Env.groovy
+++ qa-tests-backend/src/main/groovy/util/Env.groovy
@@ -23,6 +23,8 @@ class Env {
     static final IN_CI = (System.getenv("CI") != null)
     static final CI_JOBNAME = System.getenv("CI_JOB_NAME") ?: System.getenv("CIRCLE_JOB") ?: ""
     static final CI_TAG = System.getenv("CIRCLE_TAG")
+    static final GATHER_QA_TEST_DEBUG_LOGS = (System.getenv("GATHER_QA_TEST_DEBUG_LOGS") == "true")
+    static final QA_TEST_DEBUG_LOGS = System.getenv("QA_TEST_DEBUG_LOGS") ?: ""
 
     private static final Env INSTANCE = new Env()
 
