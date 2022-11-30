01372a0e5c5998cfb600f2b0c0d62d672f16b79a
Frederico Bittencourt <fbittenc@redhat.com>
2022-11-30 09:15:05 +0100

    ROX-13216: Resolver component: implement deployment resolution given deployment ID reference (#3895)

--- sensor/kubernetes/eventpipeline/resolver/resolver.go
+++ sensor/kubernetes/eventpipeline/resolver/resolver.go
@@ -1,13 +1,16 @@
 package resolver
 
 import (
+	"github.com/stackrox/rox/sensor/common/store"
 	"github.com/stackrox/rox/sensor/kubernetes/eventpipeline/component"
 )
 
 // New instantiates a Resolver component
-func New(outputQueue component.OutputQueue) component.Resolver {
+func New(outputQueue component.OutputQueue, deploymentStore store.DeploymentStore, provider store.Provider) component.Resolver {
 	return &resolverImpl{
-		outputQueue: outputQueue,
-		innerQueue:  make(chan *component.ResourceEvent),
+		outputQueue:     outputQueue,
+		innerQueue:      make(chan *component.ResourceEvent),
+		deploymentStore: deploymentStore,
+		storeProvider:   provider,
 	}
 }
--- sensor/kubernetes/eventpipeline/resolver/resolver_impl.go
+++ sensor/kubernetes/eventpipeline/resolver/resolver_impl.go
@@ -1,12 +1,23 @@
 package resolver
 
 import (
+	"github.com/stackrox/rox/generated/internalapi/central"
+	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/logging"
+	"github.com/stackrox/rox/sensor/common/store"
 	"github.com/stackrox/rox/sensor/kubernetes/eventpipeline/component"
+)
+
+var (
+	log = logging.LoggerForModule()
 )
 
 type resolverImpl struct {
 	outputQueue component.OutputQueue
 	innerQueue  chan *component.ResourceEvent
+
+	deploymentStore store.DeploymentStore
+	storeProvider   store.Provider
 }
 
 // Start the resolverImpl component
@@ -38,8 +49,48 @@ func (r *resolverImpl) runResolver() {
 
 // processMessage resolves the dependencies and forwards the message to the outputQueue
 func (r *resolverImpl) processMessage(msg *component.ResourceEvent) {
-	// TODO: resolve dependencies
+	if msg.DeploymentReference != nil {
+		referenceIds := msg.DeploymentReference(r.deploymentStore)
+
+		for _, id := range referenceIds {
+			preBuiltDeployment := r.deploymentStore.Get(id)
+			if preBuiltDeployment == nil {
+				log.Warnf("Deployment with id %s not found", id)
+				continue
+			}
+
+			permissionLevel := r.storeProvider.RBAC().GetPermissionLevelForDeployment(preBuiltDeployment)
+			exposureInfo := r.storeProvider.Services().
+				GetExposureInfos(preBuiltDeployment.GetNamespace(), preBuiltDeployment.GetLabels())
+
+			d, err := r.deploymentStore.BuildDeploymentWithDependencies(id, store.Dependencies{
+				PermissionLevel: permissionLevel,
+				Exposures:       exposureInfo,
+			})
+
+			if err != nil {
+				log.Warnf("Failed to build deployment dependency: %s", err)
+				continue
+			}
+
+			event := component.NewResourceEvent([]*central.SensorEvent{toEvent(msg.ParentResourceAction, d)},
+				[]component.CompatibilityDetectionMessage{{Object: d, Action: msg.ParentResourceAction}}, nil)
+
+			component.MergeResourceEvents(msg, event)
+		}
+	}
+
 	r.outputQueue.Send(msg)
+}
+
+func toEvent(action central.ResourceAction, deployment *storage.Deployment) *central.SensorEvent {
+	return &central.SensorEvent{
+		Id:     deployment.GetId(),
+		Action: action,
+		Resource: &central.SensorEvent_Deployment{
+			Deployment: deployment.Clone(),
+		},
+	}
 }
 
 var _ component.Resolver = (*resolverImpl)(nil)
