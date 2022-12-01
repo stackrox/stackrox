6089b2236df2b9c547a8edfdac58c70c1e0311a0
Dmitrii Dolgov <9erthalion6@gmail.com>
2022-12-01 13:45:41 +0100

    Process Listening On Ports
    Implement data store needed to persist and query PLOP objects, and
    corresponding internal API pipeline to actually process it.

---
+++ central/sensor/service/pipeline/processlisteningonport/pipeline.go
@@ -0,0 +1,61 @@
+package processlisteningonport
+
+import (
+	"context"
+
+	"github.com/stackrox/rox/central/processlisteningonport/datastore"
+	"github.com/stackrox/rox/central/sensor/service/common"
+	"github.com/stackrox/rox/central/sensor/service/pipeline"
+	"github.com/stackrox/rox/central/sensor/service/pipeline/reconciliation"
+	"github.com/stackrox/rox/generated/internalapi/central"
+	"github.com/stackrox/rox/pkg/logging"
+)
+
+var (
+	log = logging.LoggerForModule()
+)
+
+// GetPipeline returns an instantiation of this particular pipeline
+func GetPipeline() pipeline.Fragment {
+	return &pipelineImpl{
+		dataStore: datastore.Singleton(),
+	}
+}
+
+type pipelineImpl struct {
+	dataStore datastore.DataStore
+}
+
+func (s *pipelineImpl) Reconcile(
+	ctx context.Context,
+	clusterID string,
+	_ *reconciliation.StoreMap,
+) error {
+
+	// Nothing to reconcile
+	return nil
+}
+
+func (s *pipelineImpl) Match(msg *central.MsgFromSensor) bool {
+	return msg.GetProcessListeningOnPortUpdate() != nil
+}
+
+// Run runs the pipeline template on the input and returns the output.
+func (s *pipelineImpl) Run(
+	ctx context.Context,
+	clusterID string,
+	msg *central.MsgFromSensor,
+	injector common.MessageInjector,
+) error {
+
+	portProcesses := msg.GetProcessListeningOnPortUpdate().GetProcessesListeningOnPorts()
+
+	log.Debugf("Store PLOP object: %+v", portProcesses)
+	if err := s.dataStore.AddProcessListeningOnPort(ctx, portProcesses...); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *pipelineImpl) OnFinish(clusterID string) {}
