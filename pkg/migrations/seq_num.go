fecf3e7cb89e52cbd94067852f8129bb2f7f2ae0
Cong Du <c-du@users.noreply.github.com>
2022-12-07 10:12:41 -0800

    Remove test codes

--- pkg/migrations/seq_num.go
+++ pkg/migrations/seq_num.go
@@ -1,10 +1,8 @@
 package migrations
 
 import (
-	"github.com/pkg/errors"
 	"github.com/stackrox/rox/pkg/env"
 	"github.com/stackrox/rox/pkg/migrations/internal"
-	"github.com/stackrox/rox/pkg/utils"
 )
 
 // CurrentDBVersionSeqNum is the current DB version number.
@@ -14,8 +12,6 @@ func CurrentDBVersionSeqNum() int {
 	if env.PostgresDatastoreEnabled.BooleanSetting() {
 		return internal.CurrentDBVersionSeqNum
 	}
-	// XXX: to remove. The following is just to test how many test would fail
-	utils.Should(errors.Errorf("ROX_POSTGRES_DATASTORE should be true. We do not support RocksDB anymore."))
 	return internal.LastRocksDBVersionSeqNum
 }
 
