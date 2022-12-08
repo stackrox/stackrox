9cf5bbe9c6e56da48ad77092677482e3aaf044fe
Yann Brillouet <91869377+rhybrillou@users.noreply.github.com>
2022-12-08 15:39:43 +0100

    ROX-11756: Add SAC tests for cluster CVE object (#4012)

---
+++ central/cve/cluster/datastore/datastore_test_constructors.go
@@ -0,0 +1,17 @@
+package datastore
+
+import (
+	"testing"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/stackrox/rox/central/cve/cluster/datastore/search"
+	"github.com/stackrox/rox/central/cve/cluster/datastore/store/postgres"
+)
+
+// GetTestPostgresDataStore provides a datastore connected to postgres for testing purposes.
+func GetTestPostgresDataStore(_ *testing.T, pool *pgxpool.Pool) (DataStore, error) {
+	dbstore := postgres.NewFullStore(pool)
+	indexer := postgres.NewIndexer(pool)
+	searcher := search.New(dbstore, indexer)
+	return New(dbstore, indexer, searcher)
+}
