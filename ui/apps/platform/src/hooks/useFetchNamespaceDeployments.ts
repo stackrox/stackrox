d0bf98f2a37bf3f53deec0bc7b1bcbf569d7867d
Van Wilson <vwilson@redhat.com>
2022-12-12 15:00:31 -0500

    ROX-13746:  Add Deployments to Network Graph filter (#4093)

---
+++ ui/apps/platform/src/hooks/useFetchNamespaceDeployments.ts
@@ -0,0 +1,66 @@
+import { useState, useEffect } from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+import queryService from 'utils/queryService';
+
+type Deployment = {
+    id: string;
+    name: string;
+};
+export type NamespaceWithDeployments = {
+    metadata: {
+        id: string;
+        name: string;
+    };
+    deployments: Deployment[];
+};
+type DeploymentResponse = {
+    results: NamespaceWithDeployments[];
+};
+
+const DEPLOYMENTS_FOR_NAMESPACE_QUERY = gql`
+    query getNamespaceDeployments($query: String!) {
+        results: namespaces(query: $query) {
+            metadata {
+                name
+                id
+            }
+            deployments {
+                name
+                id
+            }
+        }
+    }
+`;
+
+function useFetchNamespaceDeployments(selectedNamespaceIds: string[]) {
+    const [availableDeployments, setAvailableNamespaces] = useState<NamespaceWithDeployments[]>([]);
+
+    const searchClause = { 'Namespace ID': selectedNamespaceIds };
+    // If the selectedNamespaceId has not been set yet, do not run the gql query
+    const queryOptions =
+        selectedNamespaceIds.length > 0
+            ? { variables: { query: queryService.objectToWhereClause(searchClause) } }
+            : { skip: true };
+
+    const { loading, error, data } = useQuery<DeploymentResponse, { query: string }>(
+        DEPLOYMENTS_FOR_NAMESPACE_QUERY,
+        queryOptions
+    );
+
+    useEffect(() => {
+        if (!data || !data.results) {
+            return;
+        }
+
+        setAvailableNamespaces(data.results);
+    }, [data]);
+
+    return {
+        loading,
+        error,
+        deploymentsByNamespace: availableDeployments,
+    };
+}
+
+export default useFetchNamespaceDeployments;
