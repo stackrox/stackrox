5b9ab8823224b6390ec88232cc4e9ad4181df4cc
Tomasz Janiszewski <tomek@redhat.com>
2022-12-13 21:26:01 +0100

    Update lib.sh

--- scripts/ci/lib.sh
+++ scripts/ci/lib.sh
@@ -1186,12 +1186,12 @@ store_test_results() {
     local to="$2"
 
     if ! is_in_PR_context; then
-{
+    {
         info "Creating JIRA task for failures found in $from"
         wget -q https://github.com/stackrox/junit2jira/releases/download/v0.0.1/junit2jira && \
         chmod +x junit2jira && \
         ./junit2jira -junit-reports-dir "$from"
-} || true
+    } || true
     fi
 
     info "Copying test results from $from to $to"
