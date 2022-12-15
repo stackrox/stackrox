6e6444167df3d70f70a19c875a0cd4f24b883be9
Ross Tannenbaum <rtannenb@redhat.com>
2022-12-15 12:30:16 -0800

    ROX-13943: Hide "sourced" autogenerated image integrations behind a feature flag (#4160)

--- pkg/registries/types/types.go
+++ pkg/registries/types/types.go
@@ -27,6 +27,7 @@ type Registry interface {
 // integration formed the interface
 type ImageRegistry interface {
 	Registry
+	DataSource() *storage.DataSource
 	Source() *storage.ImageIntegration
 }
 
