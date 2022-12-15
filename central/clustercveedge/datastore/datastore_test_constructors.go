0b34ee45da6a8af433f1010fe6cfdf28cdb36b4a
Yann Brillouet <91869377+rhybrillou@users.noreply.github.com>
2022-12-15 13:34:09 +0100

    ROX-11755: SAC tests for ClusterCVEEdge object (#4034)

--- central/clustercveedge/datastore/datastore_test_constructors.go
+++ central/clustercveedge/datastore/datastore_test_constructors.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 
 	"github.com/blevesearch/bleve"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/stackrox/rox/central/clustercveedge/datastore/store/postgres"
 	"github.com/stackrox/rox/central/clustercveedge/index"
 	"github.com/stackrox/rox/central/clustercveedge/search"
 	dackboxStore "github.com/stackrox/rox/central/clustercveedge/store/dackbox"
@@ -12,6 +14,14 @@ import (
 	dackboxConcurrency "github.com/stackrox/rox/pkg/dackbox/concurrency"
 	rocksdbBase "github.com/stackrox/rox/pkg/rocksdb"
 )
+
+// GetTestPostgresDataStore provides a datastore connected to postgres for testing purposes.
+func GetTestPostgresDataStore(t *testing.T, pool *pgxpool.Pool) (DataStore, error) {
+	storage := postgres.NewFullStore(pool)
+	indexer := postgres.NewIndexer(pool)
+	searcher := search.NewV2(storage, indexer)
+	return New(nil, storage, indexer, searcher)
+}
 
 // GetTestRocksBleveDataStore provides a datastore connected to rocksdb and bleve for testing purposes.
 func GetTestRocksBleveDataStore(t *testing.T, _ *rocksdbBase.RocksDB, bleveIndex bleve.Index, dacky *dackbox.DackBox, keyFence dackboxConcurrency.KeyFence) (DataStore, error) {
