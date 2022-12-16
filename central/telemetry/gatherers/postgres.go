9a2d294d711db05d1e6686e5433b71578beb5a94
David Shrewsberry <99685630+dashrews78@users.noreply.github.com>
2022-12-16 12:53:59 -0500

    Dashrews/ROX-13102 Telemetry gatherer was using rocks/bolt on startup and no gatherer for Postgres (#4163)
    add postgres metrics to telemetry-json in diagnostic bundle

---
+++ central/telemetry/gatherers/postgres.go
@@ -0,0 +1,52 @@
+package gatherers
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/stackrox/rox/central/globaldb"
+	"github.com/stackrox/rox/pkg/errorhelpers"
+	"github.com/stackrox/rox/pkg/migrations"
+	"github.com/stackrox/rox/pkg/postgres/pgadmin"
+	"github.com/stackrox/rox/pkg/telemetry/data"
+)
+
+type postgresGatherer struct {
+	db          *pgxpool.Pool
+	adminConfig *pgxpool.Config
+}
+
+func newPostgresGatherer(db *pgxpool.Pool, adminConfig *pgxpool.Config) *postgresGatherer {
+	return &postgresGatherer{
+		db:          db,
+		adminConfig: adminConfig,
+	}
+}
+
+// Gather returns telemetry information about the Postgres database used by this central
+func (d *postgresGatherer) Gather(ctx context.Context) *data.DatabaseStats {
+	errorList := errorhelpers.NewErrorList("postgres telemetry gather")
+
+	currentDBBytes, err := pgadmin.GetDatabaseSize(d.adminConfig, migrations.GetCurrentClone())
+	errorList.AddError(err)
+
+	tableStats := globaldb.CollectPostgresStats(ctx, d.db)
+
+	dbStats := &data.DatabaseStats{
+		Type:      "postgres",
+		UsedBytes: currentDBBytes,
+		Tables:    tableStats,
+		Errors:    errorList.ErrorStrings(),
+	}
+
+	// Check Postgres remaining capacity
+	availableDBBytes, err := pgadmin.GetRemainingCapacity(d.adminConfig)
+	errorList.AddError(err)
+
+	// In RDS or BYOBD configurations we may not be able to calculate this.
+	if availableDBBytes > 0 {
+		dbStats.AvailableBytes = availableDBBytes
+	}
+
+	return dbStats
+}
