332446352e414bce4fa777ccd330bc9d2aad9ce7
Tomasz Janiszewski <tomek@redhat.com>
2022-12-19 13:59:58 +0100

    add -threshold 5

--- scripts/ci/lib.sh
+++ scripts/ci/lib.sh
@@ -1188,9 +1188,9 @@ store_test_results() {
     if ! is_in_PR_context; then
     {
         info "Creating JIRA task for failures found in $from"
-        curl --retry 5 -SsfL https://github.com/stackrox/junit2jira/releases/download/v0.0.1/junit2jira -o junit2jira && \
+        curl --retry 5 -SsfL https://github.com/stackrox/junit2jira/releases/download/v0.0.2/junit2jira -o junit2jira && \
         chmod +x junit2jira && \
-        ./junit2jira -junit-reports-dir "$from"
+        ./junit2jira -junit-reports-dir "$from" -threshold 5
     } || true
     fi
 
