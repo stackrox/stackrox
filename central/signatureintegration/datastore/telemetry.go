882c57b949be49adb61c5c9fd39b31538062b02d
Michaël <smartptr@gmail.com>
2022-12-19 18:02:39 +0100

    ROX-13582: Gathering central datastore properties (#4071)
    Co-authored-by: dhaus67 <dhaus@redhat.com>

---
+++ central/signatureintegration/datastore/telemetry.go
@@ -0,0 +1,22 @@
+package datastore
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	"github.com/stackrox/rox/pkg/sac"
+	"github.com/stackrox/rox/pkg/telemetry/phonehome"
+)
+
+// Gather number of signature integrations.
+var Gather phonehome.GatherFunc = func(ctx context.Context) (map[string]any, error) {
+	// WithAllAccess is required only to fetch and calculate the number of
+	// signature integrations. It is not propagated anywhere else.
+	ctx = sac.WithAllAccess(ctx)
+	totals := make(map[string]any)
+	si := Singleton()
+	if err := phonehome.AddTotal(ctx, totals, "Signature Integrations", si.GetAllSignatureIntegrations); err != nil {
+		return nil, errors.Wrap(err, "failed to get signature integrations")
+	}
+	return totals, nil
+}
