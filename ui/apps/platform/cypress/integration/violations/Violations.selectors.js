036a872d6f1127a9b1f4ab94f54430ad98f81e39
Mark Pedrotti <mark@stackrox.com>
2022-12-19 10:53:54 -0500

    Delete unused selectors

--- ui/apps/platform/cypress/integration/violations/Violations.selectors.js
+++ ui/apps/platform/cypress/integration/violations/Violations.selectors.js
@@ -1,17 +1,9 @@
 export const selectors = {
-    tableRow: 'tbody tr',
-    firstTableRow: 'tbody tr:nth(0)',
-    tableRowContains: (text) => `tbody tr:contains("${text}")`,
-    firstTableRowLink: 'tbody tr:nth(0) a',
-    lastTableRow: 'tbody tr:last',
-    lastTableRowLink: 'tbody tr:last a',
     actions: {
         btn: 'td.pf-c-table__action button[aria-label="Actions"]',
         excludeDeploymentBtn: 'button:contains("Exclude deployment")',
         resolveBtn: 'button:contains("Mark as resolved")',
         resolveAndAddToBaselineBtn: 'button:contains("Resolve and add to process baseline")',
-        dropdown: '[data-testid="violations-bulk-actions-dropdown"]',
-        addTagsBtn: '[data-testid="bulk-add-tags-btn"]',
     },
     details: {
         page: '[data-testid="violation-details-page"]',
@@ -33,15 +25,5 @@ export const selectors = {
             '[data-testid="deployment-details"] [data-testid="container-configuration"]',
         securityContext: '[data-testid="deployment-details"] [data-testid="security-context"]',
         portConfiguration: '[data-testid="deployment-details"] [data-testid="port-configuration"]',
-        snapshotWarning:
-            '[data-testid="deployment-details"] [data-testid="deployment-snapshot-warning"]',
     },
-    table: {
-        rows: 'tbody tr',
-    },
-    viewDeploymentsButton: 'button:contains("View Deployments")',
-    clusterFieldInModal: '.ReactModalPortal > .ReactModal__Overlay span:contains("Cluster")',
-    securityBestPractices: '[data-testid="deployment-security-practices"]',
-    runtimeProcessCards: '[data-testid="runtime-processes"]',
-    excludedDeploymentRow: '.rt-tr:contains("metadata-proxy-v0.1")',
 };
