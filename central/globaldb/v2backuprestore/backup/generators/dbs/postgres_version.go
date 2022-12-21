810d2046a6efeeef647f09483ce72a69ccd80515
Dave Shrewsberry <dashrews@redhat.com>
2022-12-21 09:22:02 -0500

    write version to bundle for debugging

---
+++ central/globaldb/v2backuprestore/backup/generators/dbs/postgres_version.go
@@ -0,0 +1,50 @@
+package dbs
+
+import (
+	"context"
+	"fmt"
+	"io"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+	versionUtils "github.com/stackrox/rox/central/version/utils"
+	"gopkg.in/yaml.v3"
+)
+
+// NewPostgresVersion returns a generator for Postgres backups.
+// We take in the connection to connect to the DB
+func NewPostgresVersion(db *pgxpool.Pool) *PostgresVersion {
+	return &PostgresVersion{
+		db: db,
+	}
+}
+
+// PostgresVersion is an implementation of a StreamGenerator which writes information from the version table to the input io.Writer.
+type PostgresVersion struct {
+	db *pgxpool.Pool
+}
+
+// WriteTo writes a backup of Postgres to the writer
+func (ps *PostgresVersion) WriteTo(ctx context.Context, out io.Writer) error {
+	version, err := versionUtils.ReadVersionPostgres(ps.db)
+	if err != nil {
+		log.Fatalf("Could not retrieve version data: %v", err)
+		return err
+	}
+
+	_, err = out.Write([]byte(fmt.Sprint("VERSION FILE FOR INFORMATION PURPOSES ONLY.  NO LONGER USED TO CALCULATE VERSION.\n")))
+	if err != nil {
+		return err
+	}
+
+	bytes, err := yaml.Marshal(version)
+	if err != nil {
+		return err
+	}
+
+	_, err = out.Write(bytes)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
