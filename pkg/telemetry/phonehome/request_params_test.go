0187eb7a789268df15e22b8a1e577115f41a3201
Michaël Petrov <smartptr@gmail.com>
2022-12-21 16:26:20 +0100

    service method comparator

--- pkg/telemetry/phonehome/request_params_test.go
+++ pkg/telemetry/phonehome/request_params_test.go
@@ -7,14 +7,33 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestRequestParams_GetMethod(t *testing.T) {
-	rp := &RequestParams{Path: "/v1.Service/Method"}
-	assert.Equal(t, rp.Path, rp.GetMethod(), "must be equal to Path, as there's no request details")
+func TestRequestParams_Is(t *testing.T) {
+	assert.True(t, (&RequestParams{}).
+		Is(&ServiceMethod{}),
+	)
+	assert.True(t, (&RequestParams{Method: "/v1.service/method"}).
+		Is(&ServiceMethod{GRPCMethod: "/v1.service/method"}),
+	)
+	assert.True(t, (&RequestParams{Method: "CONNECT", Path: "/v1/method"}).
+		Is(&ServiceMethod{HTTPMethod: http.MethodConnect, HTTPPath: "/v1/method"}),
+	)
+	assert.True(t, (&RequestParams{Method: "CONNECT", Path: "/v1/method"}).
+		Is(&ServiceMethod{GRPCMethod: "different", HTTPMethod: http.MethodConnect, HTTPPath: "/v1/method"}),
+	)
 
-	rp = &RequestParams{Path: "/v1/method"}
-	rp.HTTPReq, _ = http.NewRequest(http.MethodPost, "/path", nil)
-	assert.Equal(t, http.MethodPost, rp.GetMethod(), "must be POST, as the HTTP request is provided")
-
-	rp.HTTPReq, _ = http.NewRequest("", "/path", nil)
-	assert.Equal(t, http.MethodGet, rp.GetMethod(), "must be GET, as this is the default HTTP method")
+	assert.False(t, (&RequestParams{Method: "some path"}).
+		Is(&ServiceMethod{}),
+	)
+	assert.False(t, (&RequestParams{Path: "/v2.service/method"}).
+		Is(&ServiceMethod{GRPCMethod: "/v1.service/method"}),
+	)
+	assert.False(t, (&RequestParams{Method: "CONNECT", Path: "/v2/method"}).
+		Is(&ServiceMethod{HTTPMethod: http.MethodConnect, HTTPPath: "/v1/method"}),
+	)
+	assert.False(t, (&RequestParams{Method: "GET", Path: "/v1/method"}).
+		Is(&ServiceMethod{HTTPMethod: http.MethodConnect, HTTPPath: "/v1/method"}),
+	)
+	assert.False(t, (&RequestParams{Method: "GET", Path: "/v1/method"}).
+		Is(&ServiceMethod{GRPCMethod: "/v1/method"}),
+	)
 }
