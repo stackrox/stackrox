d4c7f0320238b8ebe9d863ab8248d67dd7064847
David Vail <dcvail17@gmail.com>
2022-12-22 12:44:23 -0500

    Re-add deployment count for related entities banner

--- ui/apps/platform/src/Containers/VulnMgmt/Entity/Policy/VulnMgmtEntityPolicy.js
+++ ui/apps/platform/src/Containers/VulnMgmt/Entity/Policy/VulnMgmtEntityPolicy.js
@@ -69,6 +69,7 @@ const VulnMgmtEntityPolicy = ({
                         name
                     }
                 }
+                deploymentCount${queryVarParam}
                 unusedVarSink(query: $scopeQuery)
             }
         }
