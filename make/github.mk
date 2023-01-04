a292d9d83164afaf58469de30a541dd528c52dc1
Marcin Owsiany <porridge@redhat.com>
2023-01-04 08:33:58 +0100

    Move GET_GITHUB_RELEASE_FN to a new helper file.
    Also decrease verbosity while at it.

---
+++ make/github.mk
@@ -0,0 +1,18 @@
+# github.mk
+# Helpers for fetching released binaries from github projects.
+
+# For usage instructions, see uses of this macro elsewhere, since there is no
+# single standard for architecture naming.
+GET_GITHUB_RELEASE_FN = get_github_release() { \
+	[ -x $${1} ] || { \
+		set -euo pipefail ;\
+		mkdir -p bin ;\
+		attempts=5 ;\
+		for i in $$(seq $$attempts); do \
+			curl --silent --show-error --fail --location --output $${1} $${2} && break ;\
+			[ $$i -eq $$attempts ] && exit 1; \
+		done ;\
+		[[ "$$(uname -s)" != "Darwin" ]] || xattr -c $${1} ;\
+		chmod +x $${1} ;\
+	} \
+}
