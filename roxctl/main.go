1f093480ff3577327bded521cfac49dba0662b20
Gavin Jefferies <gavin@stackrox.com>
2023-01-04 00:26:05 +0000

    break

--- roxctl/main.go
+++ roxctl/main.go
@@ -15,7 +15,7 @@ import (
 
 func main() {
 	c := maincommand.Command()
-	addHelp(c.Commands())
+	addHlp(c.Commands())
 
 	// This is a workaround. Cobra/pflag takes care of presenting flag usage information
 	// to the user including the respective flag default values.
