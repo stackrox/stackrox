5d3d73fa27238c537c0f7d4eb43d63de2b7b088c
daynewlee <yli3@redhat.com>
2023-01-04 11:44:51 -0600

    Quick fix

--- compliance/collection/nodeinventorizer/nodeinventory.go
+++ compliance/collection/nodeinventorizer/nodeinventory.go
@@ -32,7 +32,6 @@ func (n *NodeInventoryCollector) Scan(nodeName string) (*storage.NodeInventory, 
 
 	protoComponents := protoComponentsFromScanComponents(componentsHost)
 
-	log.Info("Generating node inventory ...")
 	m := &storage.NodeInventory{
 		NodeName:   nodeName,
 		ScanTime:   timestamp.TimestampNow(),
@@ -40,7 +39,7 @@ func (n *NodeInventoryCollector) Scan(nodeName string) (*storage.NodeInventory, 
 	}
 	// uncertifiedRHEL is false since scanning is only supported on RHCOS for now,
 	// which only exists in certified versions. Therefore, no specific notes needed
-	//if uncertifiedRHEL can be true in the future, we can add Note_CERTIFIED_RHEL_SCAN_UNAVAILABLE
+	// if uncertifiedRHEL can be true in the future, we can add Note_CERTIFIED_RHEL_SCAN_UNAVAILABLE
 	m.Notes = []scannerV1.Note{scannerV1.Note_LANGUAGE_CVES_UNAVAILABLE}
 
 	return m, nil
