ffb906983db13d00aa87b889d6fce28a4cdc091f
Michaël Petrov <smartptr@gmail.com>
2023-01-05 15:49:50 +0100

    doc.go

---
+++ pkg/telemetry/phonehome/doc.go
@@ -0,0 +1,19 @@
+/*
+Package phonehome provides interfaces and implementation for telemetry data
+collection.
+
+The package provides the following entities:
+
+ - client configuration;
+ - Telemeter interface, and its [segment] implementation;
+ - API interceptor;
+ - periodic data gatherer.
+
+# Client configuration
+
+# API Interceptor
+
+# Periodic data gatherer
+
+*/
+package phonehome
