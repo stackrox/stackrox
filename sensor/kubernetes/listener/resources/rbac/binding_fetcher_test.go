cb620aa09ae8ca7f53e2c9b8fa2bc930e2656f2c
Frederico Bittencourt <fbittenc@redhat.com>
2023-01-10 13:42:38 -0300

    add extra test for RoleBinding on binding_fetcher

--- sensor/kubernetes/listener/resources/rbac/binding_fetcher_test.go
+++ sensor/kubernetes/listener/resources/rbac/binding_fetcher_test.go
@@ -13,6 +13,8 @@ import (
 
 func Test_FetchBindingRemovesLastAppliedConfig(t *testing.T) {
 	fakeClient := fake.NewSimpleClientset()
+	fetcher := newBindingFetcher(fakeClient)
+
 	_, err := fakeClient.RbacV1().ClusterRoleBindings().Create(context.Background(), &v1.ClusterRoleBinding{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: "test-binding",
@@ -32,7 +34,6 @@ func Test_FetchBindingRemovesLastAppliedConfig(t *testing.T) {
 
 	require.NoError(t, err)
 
-	fetcher := newBindingFetcher(fakeClient)
 	event, err := fetcher.generateDependentEvent(namespacedBindingID{
 		name:      "test-binding",
 		namespace: "",
@@ -43,6 +44,39 @@ func Test_FetchBindingRemovesLastAppliedConfig(t *testing.T) {
 
 	assert.Equal(t, event.GetBinding().GetRoleId(), "role-uid")
 	annotations := event.GetBinding().GetAnnotations()
+	assert.Len(t, annotations, 1)
+	assert.Equal(t, annotations["SomeKey"], "SomeValue")
+
+	_, err = fakeClient.RbacV1().RoleBindings("test-ns").Create(context.Background(), &v1.RoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-ns-binding",
+			UID:       "test--ns-uid",
+			Namespace: "test-ns",
+			Annotations: map[string]string{
+				"SomeKey": "SomeValue",
+				"kubectl.kubernetes.io/last-applied-configuration": "{\"some_prop\": \"value\"}",
+			},
+		},
+		Subjects: nil,
+		RoleRef: v1.RoleRef{
+			APIGroup: "rbac.authorization.k8s.io",
+			Kind:     "Role",
+			Name:     "test-role",
+		},
+	}, metav1.CreateOptions{})
+
+	require.NoError(t, err)
+
+	event, err = fetcher.generateDependentEvent(namespacedBindingID{
+		name:      "test-ns-binding",
+		namespace: "test-ns",
+		uid:       "test-ns-uid",
+	}, "role-uid", true)
+
+	require.NoError(t, err)
+
+	assert.Equal(t, event.GetBinding().GetRoleId(), "role-uid")
+	annotations = event.GetBinding().GetAnnotations()
 	assert.Len(t, annotations, 1)
 	assert.Equal(t, annotations["SomeKey"], "SomeValue")
 }
