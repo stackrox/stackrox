0b6c338e4e8f1312379e39b5cd41a6fffb733017
Michaël Petrov <smartptr@gmail.com>
2023-01-11 16:35:50 +0100

    Redirect requests to internal docs to online docs.

--- central/ui/mux.go
+++ central/ui/mux.go
@@ -1,14 +1,20 @@
 package ui
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/stackrox/rox/pkg/version"
 )
 
 // Mux returns a HTTP Handler that knows how to serve the UI assets,
 // including Javascript, HTML, and other items.
 func Mux() http.Handler {
 	mux := http.NewServeMux()
-	mux.Handle("/docs/product/", http.StripPrefix("/docs/product/", http.FileServer(http.Dir("/stackrox/static-data/product-docs"))))
+	mux.Handle("/docs/product/", http.StripPrefix("/docs/product/",
+		http.RedirectHandler(fmt.Sprintf("https://docs.openshift.com/acs/%s/welcome/index.html",
+			version.GetMajorMinor(version.GetMainVersion())),
+			http.StatusMovedPermanently)))
 	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("/ui/static"))))
 	mux.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "/ui/favicon.ico")
