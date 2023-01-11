1906f5e26dcdae6caf4a95dbbb3893b3b047db00
Linda Song <ls@stackrox.com>
2023-01-10 22:26:23 -0800

    PR comments

--- ui/apps/platform/src/Containers/NetworkGraph/components/LegendContent.tsx
+++ ui/apps/platform/src/Containers/NetworkGraph/components/LegendContent.tsx
@@ -12,47 +12,52 @@ import { ReactComponent as NoPolicyRules } from 'images/network-graph/no-policy-
 
 function LegendContent() {
     return (
-        <Flex direction={{ default: 'column' }}>
-            <FlexItem>
-                <Title headingLevel="h4" className="pf-u-pb-sm">
-                    Node types
-                </Title>
-                <DescriptionListCompact isHorizontal termWidth="20px" className="pf-u-pl-md">
-                    <DescriptionListItem term={<BuilderImageIcon />} desc="Deployment" />
-                    <DescriptionListItem
-                        term={<PficonNetworkRangeIcon />}
-                        desc="External CIDR block"
-                    />
-                </DescriptionListCompact>
-            </FlexItem>
-            <FlexItem>
-                <Title headingLevel="h4" className="pf-u-pb-sm">
-                    Deployment badges
-                </Title>
-                <DescriptionListCompact isHorizontal termWidth="20px" className="pf-u-pl-md">
-                    <DescriptionListItem
-                        term={<BothPolicyRules width="22px" height="22px" />}
-                        desc="Isolated by network poilcy rules"
-                        groupClassName="pf-u-align-items-center"
-                    />
-                    <DescriptionListItem
-                        term={<NoPolicyRules width="22px" height="22px" />}
-                        desc="All traffic allowed (No network policies)"
-                        groupClassName="pf-u-align-items-center"
-                    />
-                    <DescriptionListItem
-                        term={<EgressOnly width="22px" height="22px" />}
-                        desc="Only has an egress network policy"
-                        groupClassName="pf-u-align-items-center"
-                    />
-                    <DescriptionListItem
-                        term={<IngressOnly width="22px" height="22px" />}
-                        desc="Only has an ingress network policy"
-                        groupClassName="pf-u-align-items-center"
-                    />
-                </DescriptionListCompact>
-            </FlexItem>
-        </Flex>
+        <>
+            <Title headingLevel="h3" className="pf-u-screen-reader">
+                Legend
+            </Title>
+            <Flex direction={{ default: 'column' }}>
+                <FlexItem>
+                    <Title headingLevel="h5" className="pf-u-pb-sm">
+                        Node types
+                    </Title>
+                    <DescriptionListCompact isHorizontal termWidth="20px" className="pf-u-pl-md">
+                        <DescriptionListItem term={<BuilderImageIcon />} desc="Deployment" />
+                        <DescriptionListItem
+                            term={<PficonNetworkRangeIcon />}
+                            desc="External CIDR block"
+                        />
+                    </DescriptionListCompact>
+                </FlexItem>
+                <FlexItem>
+                    <Title headingLevel="h5" className="pf-u-pb-sm">
+                        Deployment badges
+                    </Title>
+                    <DescriptionListCompact isHorizontal termWidth="20px" className="pf-u-pl-md">
+                        <DescriptionListItem
+                            term={<BothPolicyRules width="22px" height="22px" />}
+                            desc="Isolated by network poilcy rules"
+                            groupClassName="pf-u-align-items-center"
+                        />
+                        <DescriptionListItem
+                            term={<NoPolicyRules width="22px" height="22px" />}
+                            desc="All traffic allowed (No network policies)"
+                            groupClassName="pf-u-align-items-center"
+                        />
+                        <DescriptionListItem
+                            term={<EgressOnly width="22px" height="22px" />}
+                            desc="Only has an egress network policy"
+                            groupClassName="pf-u-align-items-center"
+                        />
+                        <DescriptionListItem
+                            term={<IngressOnly width="22px" height="22px" />}
+                            desc="Only has an ingress network policy"
+                            groupClassName="pf-u-align-items-center"
+                        />
+                    </DescriptionListCompact>
+                </FlexItem>
+            </Flex>
+        </>
     );
 }
 
