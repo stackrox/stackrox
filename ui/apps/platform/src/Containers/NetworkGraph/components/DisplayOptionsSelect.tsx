b7bb62b5ca1570873869b0c93665dea043824d31
Linda Song <ls@stackrox.com>
2023-01-12 12:15:42 -0800

    styling

--- ui/apps/platform/src/Containers/NetworkGraph/components/DisplayOptionsSelect.tsx
+++ ui/apps/platform/src/Containers/NetworkGraph/components/DisplayOptionsSelect.tsx
@@ -1,15 +1,10 @@
 import React, { useState } from 'react';
-import {
-    Select,
-    SelectVariant,
-    SelectGroup,
-    SelectOption,
-    Flex,
-    FlexItem,
-} from '@patternfly/react-core';
+import { Select, SelectVariant, SelectGroup, SelectOption } from '@patternfly/react-core';
 import { PficonNetworkRangeIcon } from '@patternfly/react-icons';
 
 import { ReactComponent as NoPolicyRules } from 'images/network-graph/no-policy-rules.svg';
+
+import './DisplayOptionsSelect.css';
 
 export type DisplayOption = 'policyStatusBadge' | 'externalBadge' | 'edgeLabel';
 
@@ -42,20 +37,16 @@ function DisplayOptionsSelect({ selectedOptions, setSelectedOptions }: DisplayOp
             selections={selectedOptions}
             placeholderText="Display options"
             isGrouped
+            id="display-options-dropdown"
         >
             <SelectGroup label="Deployment visuals" key="deployment">
                 <SelectOption key={0} value="policyStatusBadge">
-                    <Flex>
-                        <FlexItem>
-                            <NoPolicyRules width="22px" height="22px" />
-                        </FlexItem>
-                        <FlexItem>
-                            <span>Network policy status badge</span>
-                        </FlexItem>
-                    </Flex>
+                    <NoPolicyRules width="22px" height="22px" className="pf-u-mr-xs" />
+                    Network policy status badge
                 </SelectOption>
                 <SelectOption key={1} value="externalBadge">
-                    <PficonNetworkRangeIcon /> Active external traffic badge
+                    <PficonNetworkRangeIcon className="pf-u-ml-xs pf-u-mr-sm" /> Active external
+                    traffic badge
                 </SelectOption>
             </SelectGroup>
             <SelectGroup label="Edge visuals" key="edge">
