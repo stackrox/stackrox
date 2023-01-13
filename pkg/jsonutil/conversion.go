fc21514bd8f5f2f3043fc2cc18d79ac09bed2328
David Shrewsberry <99685630+dashrews78@users.noreply.github.com>
2023-01-13 13:01:37 -0500

    Dashrews/ROX-14209 update sensor to ensure jsonpb unmarshal has AllowUnknownFields set to true (#4391)
    update use of jsonpb for sensor to ensure unmarshal is done with AllowUknownFields as true

--- pkg/jsonutil/conversion.go
+++ pkg/jsonutil/conversion.go
@@ -2,6 +2,7 @@ package jsonutil
 
 import (
 	"bytes"
+	"io"
 	"regexp"
 	"strings"
 
@@ -34,12 +35,17 @@ func JSONUnmarshaler() *jsonpb.Unmarshaler {
 
 // JSONToProto converts a string containing JSON into a proto message.
 func JSONToProto(json string, m proto.Message) error {
-	return JSONUnmarshaler().Unmarshal(strings.NewReader(json), m)
+	return JSONReaderToProto(strings.NewReader(json), m)
 }
 
 // JSONBytesToProto converts bytes containing JSON into a proto message.
 func JSONBytesToProto(contents []byte, m proto.Message) error {
-	return JSONUnmarshaler().Unmarshal(bytes.NewReader(contents), m)
+	return JSONReaderToProto(bytes.NewReader(contents), m)
+}
+
+// JSONReaderToProto converts bytes from a reader containing JSON into a proto message.
+func JSONReaderToProto(reader io.Reader, m proto.Message) error {
+	return JSONUnmarshaler().Unmarshal(reader, m)
 }
 
 // ProtoToJSON converts a proto message into a string containing JSON.
