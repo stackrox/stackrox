4768357acbb0f0441a2309d612cd7f9c0ee78432
Michaël Petrov <smartptr@gmail.com>
2023-01-16 15:35:50 +0100

    Add nodes and cpu capacity to sec cluster identity

--- central/cluster/datastore/telemetry.go
+++ central/cluster/datastore/telemetry.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/stackrox/rox/central/role/resources"
 	"github.com/stackrox/rox/central/telemetry/centralclient"
+	"github.com/stackrox/rox/generated/internalapi/central"
 	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/pkg/sac"
 	"github.com/stackrox/rox/pkg/telemetry/phonehome"
@@ -21,19 +22,23 @@ func trackClusterRegistered(cluster *storage.Cluster) {
 	}
 }
 
+func makeClusterProperties(cluster *storage.Cluster) map[string]any {
+	return map[string]any{
+		"Main Image":           cluster.GetMainImage(),
+		"Admission Controller": cluster.GetAdmissionController(),
+		"Collection Method":    cluster.GetCollectionMethod().String(),
+		"Collector Image":      cluster.GetCollectorImage(),
+		"Managed By":           cluster.GetManagedBy().String(),
+		"Priority":             cluster.GetPriority(),
+		"Cluster Type":         cluster.GetType().String(),
+		"Slim Collector":       cluster.GetSlimCollector(),
+	}
+}
+
 func trackClusterInitialized(cluster *storage.Cluster) {
 	if cfg := centralclient.InstanceConfig(); cfg.Enabled() {
 		cfg.Telemeter().Group(cfg.GroupID, cluster.GetId(), nil)
-		cfg.Telemeter().Identify(cluster.GetId(), map[string]any{
-			"Main Image":           cluster.GetMainImage(),
-			"Admission Controller": cluster.GetAdmissionController(),
-			"Collection Method":    cluster.GetCollectionMethod().String(),
-			"Collector Image":      cluster.GetCollectorImage(),
-			"Managed By":           cluster.GetManagedBy().String(),
-			"Priority":             cluster.GetPriority(),
-			"Cluster Type":         cluster.GetType().String(),
-			"Slim Collector":       cluster.GetSlimCollector(),
-		})
+		cfg.Telemeter().Identify(cluster.GetId(), makeClusterProperties(cluster))
 		cfg.Telemeter().Track("Secured Cluster Initialized", cluster.GetId(), map[string]any{
 			"Health": cluster.GetHealthStatus().OverallHealthStatus.String(),
 		})
@@ -52,4 +57,24 @@ var Gather phonehome.GatherFunc = func(ctx context.Context) (map[string]any, err
 		return nil, err
 	}
 	return props, nil
+}
+
+// UpdateSecuredClusterIdentity is called by the clustermetrics pipeline on
+// the reception of the cluster metrics from a sensor.
+func UpdateSecuredClusterIdentity(ctx context.Context, clusterID string, metrics *central.ClusterMetrics) {
+	if cfg := centralclient.InstanceConfig(); cfg.Enabled() {
+		ctx = sac.WithGlobalAccessScopeChecker(ctx,
+			sac.AllowFixedScopes(
+				sac.AccessModeScopeKeys(storage.Access_READ_ACCESS),
+				sac.ResourceScopeKeys(resources.Cluster)))
+
+		cluster, ok, err := Singleton().GetCluster(ctx, clusterID)
+		if err != nil || !ok {
+			return
+		}
+		props := makeClusterProperties(cluster)
+		props["Total Nodes"] = metrics.NodeCount
+		props["CPU Capacity"] = metrics.CpuCapacity
+		cfg.Telemeter().Identify(cluster.GetId(), makeClusterProperties(cluster))
+	}
 }
