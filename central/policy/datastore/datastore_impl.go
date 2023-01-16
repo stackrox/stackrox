3574cb2c8b9e6b57647aa2ace73470825a509fdd
dhaus67 <dhaus@redhat.com>
2023-01-16 14:05:06 +0100

    ROX-12620: Deprecate Policy and VulnerabilityReports resources (#4108)

--- central/policy/datastore/datastore_impl.go
+++ central/policy/datastore/datastore_impl.go
@@ -27,12 +27,14 @@ import (
 )
 
 var (
-	log       = logging.LoggerForModule()
+	log = logging.LoggerForModule()
+	// TODO: ROX-13888 Replace Policy with WorkflowAdministration.
 	policySAC = sac.ForResource(resources.Policy)
 
 	policyCtx = sac.WithGlobalAccessScopeChecker(context.Background(),
 		sac.AllowFixedScopes(
 			sac.AccessModeScopeKeys(storage.Access_READ_ACCESS, storage.Access_READ_WRITE_ACCESS),
+			// TODO: ROX-13888 Replace Policy with WorkflowAdministration.
 			sac.ResourceScopeKeys(resources.Policy)))
 )
 
