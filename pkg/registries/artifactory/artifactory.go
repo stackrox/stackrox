0c73ee401d333670ae54e64d7a6ed29638448735
David Caravello <119438707+dcaravel@users.noreply.github.com>
2023-01-17 15:44:05 -0600

    remove now unnecessary things

--- pkg/registries/artifactory/artifactory.go
+++ pkg/registries/artifactory/artifactory.go
@@ -2,41 +2,14 @@ package artifactory
 
 import (
 	"github.com/stackrox/rox/generated/storage"
-	"github.com/stackrox/rox/pkg/logging"
 	"github.com/stackrox/rox/pkg/registries/docker"
 	"github.com/stackrox/rox/pkg/registries/types"
 )
 
-var (
-	logger = logging.LoggerForModule()
-)
-
 // Creator provides the type and registries.Creator to add to the registry of image registries.
 func Creator() (string, func(integration *storage.ImageIntegration) (types.Registry, error)) {
-	return "artifactory", newRegistry
-}
-
-var _ types.Registry = (*registry)(nil)
-
-type registry struct {
-	*docker.Registry
-}
-
-func newRegistry(integration *storage.ImageIntegration) (types.Registry, error) {
-	dockerRegistry, err := docker.NewDockerRegistry(integration)
-	if err != nil {
-		return nil, err
+	return "artifactory", func(integration *storage.ImageIntegration) (types.Registry, error) {
+		reg, err := docker.NewDockerRegistry(integration)
+		return reg, err
 	}
-	return &registry{
-		Registry: dockerRegistry,
-	}, nil
-}
-
-// Test implements a valid Test function for Artifactory
-func (r *registry) Test() error {
-	err := r.Client.Ping()
-	if err != nil {
-		logger.Errorf("error testing Artifactory integration: %v", err)
-	}
-	return err
 }
