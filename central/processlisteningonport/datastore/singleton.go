9d561b931c928fbfc7114ac0612bf93c8647254b
Dmitrii Dolgov <9erthalion6@gmail.com>
2023-01-18 11:20:37 +0100

    Sanity check for PLOP DataStore singletone

--- central/processlisteningonport/datastore/singleton.go
+++ central/processlisteningonport/datastore/singleton.go
@@ -4,6 +4,7 @@ import (
 	"github.com/stackrox/rox/central/globaldb"
 	processIndicatorDataStore "github.com/stackrox/rox/central/processindicator/datastore"
 	"github.com/stackrox/rox/central/processlisteningonport/store/postgres"
+	"github.com/stackrox/rox/pkg/env"
 	"github.com/stackrox/rox/pkg/sync"
 )
 
@@ -21,6 +22,11 @@ func initialize() {
 
 // Singleton provides the interface for non-service external interaction.
 func Singleton() DataStore {
+	if !env.PostgresDatastoreEnabled.BooleanSetting() {
+		log.Warnf("Tried to get datastore singletone for PLOP without Postgres")
+		return nil
+	}
+
 	once.Do(initialize)
 	return store
 }
