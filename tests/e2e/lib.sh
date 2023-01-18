ba75c9f16007e650581ff5f6c43aca24d0cfcf03
Brad Lugo <blugo@redhat.com>
2023-01-17 20:32:24 -0800

    PR feedback

--- tests/e2e/lib.sh
+++ tests/e2e/lib.sh
@@ -280,18 +280,20 @@ check_for_stackrox_OOMs() {
     local objects
     objects=$(ls "$dir"/stackrox/pods/*_object.json || true)
     if [[ -n "$objects" ]]; then
-        for describe in $objects; do
+        for object in $objects; do
             local pod_name
             # This wack jq slurp flag with the if statement is due to https://github.com/stedolan/jq/issues/1142
-            if pod_name=$(jq -ser 'if . == [] then null else .[] | select(.kind=="Pod") | .metadata.name end' "$describe"); then
+            if pod_name=$(jq -ser 'if . == [] then null else .[] | select(.kind=="Pod") | .metadata.name end' "$object"); then
                 info "Checking $pod_name for OOMKilled"
-                if jq -e '. | select(.status.containerStatuses[].lastState.terminated.reason=="OOMKilled")' "$describe" >/dev/null 2>&1; then
-                    echo "OOM $describe"
+                if jq -e '. | select(.status.containerStatuses[].lastState.terminated.reason=="OOMKilled")' "$object" >/dev/null 2>&1; then
+                    echo "OOM $object"
                     save_junit_failure "OOMCheck-$pod_name" "OOMCheck" "$pod_name was OOMKilled"
                 else
-                    echo "NOT OOM $describe"
+                    echo "NOT OOM $object"
                     save_junit_success "OOMCheck-$pod_name" "$pod_name was not OOMKilled"
                 fi
+            else
+                echo "found $object that isn't a pod object"
             fi
         done
     fi
