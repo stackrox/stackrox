696e3935be2a74dcb429312dc0bef32fe60d19be
Dave Shrewsberry <dashrews@redhat.com>
2023-01-18 05:46:54 -0500

    clean up

--- pkg/env/central_diagnostics.go
+++ pkg/env/central_diagnostics.go
@@ -3,7 +3,4 @@ package env
 var (
 	// EnableCentralDiagnostics is set to true to signal that diagnostic bundles or dumps should contain information about central or the environment that central runs in.
 	EnableCentralDiagnostics = RegisterBooleanSetting("ROX_ENABLE_CENTRAL_DIAGNOSTICS", true)
-
-	// EnableCentralDatabaseDiagnostics is set to true to signal that diagnostic bundles or dumps should contain information about central database or the environment that central runs in.
-	EnableCentralDatabaseDiagnostics = RegisterBooleanSetting("ROX_ENABLE_CENTRAL_DATABASE_DIAGNOSTICS", true)
 )
