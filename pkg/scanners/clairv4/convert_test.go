140668fe972b3396893f41cb87bcef908855f082
RTann <rtannenb@redhat.com>
2023-01-18 16:58:16 -0800

    for now

---
+++ pkg/scanners/clairv4/convert_test.go
@@ -0,0 +1,45 @@
+package clairv4
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestName(t *testing.T) {
+	testcases := []struct{
+		original string
+		expected string
+	}{
+		{
+			// Alpine
+			original: "CVE-2018-16840",
+			expected: "CVE-2018-16840",
+		},
+		{
+			// RHEL
+			original: "RHSA-2023:0173: libxml2 security update (Moderate)",
+			expected: "RHSA-2023:0173",
+		},
+		{
+			// pyup.io
+			original: "pyup.io-38834 (CVE-2020-26137)",
+			expected: "CVE-2020-26137",
+		},
+		{
+			// Debian
+			original: "DSA-4591-1 cyrus-sasl2",
+			expected: "DSA-4591-1",
+		},
+		{
+			// Ubuntu
+			original: "CVE-2022-45061 on Ubuntu 22.04 LTS (jammy) - medium.",
+			expected: "CVE-2022-45061",
+		},
+	}
+	for _, testcase := range testcases {
+		t.Run(testcase.original, func(t *testing.T) {
+			assert.Equal(t, testcase.expected, name(testcase.original))
+		})
+	}
+}
