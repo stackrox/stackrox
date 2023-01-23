90ff02e0128fa0e7b7f66592b44eb25d3ea7b849
RTann <rtannenb@redhat.com>
2023-01-23 10:47:59 -0800

    updates

--- qa-tests-backend/src/main/groovy/objects/ImageIntegration.groovy
+++ qa-tests-backend/src/main/groovy/objects/ImageIntegration.groovy
@@ -98,7 +98,7 @@ class ClairV4ScannerIntegration implements ImageIntegration {
     static ImageIntegrationOuterClass.ImageIntegration.Builder getCustomBuilder(Map customArgs = [:]) {
         Map defaultArgs = [
                 name: "clairv4",
-                endpoint: Env.get("CLAIR_V4_ENDPOINT", ""),
+                endpoint: Env.get("CLAIR_V4_ENDPOINT", "http://clairv4.qa-clairv4:8000"),
                 insecure: true,
         ]
         Map args = defaultArgs + customArgs
