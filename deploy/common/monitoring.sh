7a7caa21504519fa74c382d92c591a2a88ac2c83
Matthias Meidinger <mmeiding@redhat.com>
2023-01-24 10:23:43 +0100

    WIP: Clean patch for MONITORING_SUPPORT

--- deploy/common/monitoring.sh
+++ deploy/common/monitoring.sh
@@ -2,6 +2,8 @@
 
 kubectl -n stackrox patch svc/sensor -p '{"spec":{"ports":[{"name":"monitoring","port":9090,"protocol":"TCP","targetPort":9090}]}}'
 kubectl -n stackrox patch svc/central -p '{"spec":{"ports":[{"name":"monitoring","port":9090,"protocol":"TCP","targetPort":9090}]}}'
+kubectl -n stackrox patch daemonset/collector --type='json' -p='[{"op": "add", "path": "/spec/template/spec/containers/1/ports", "value":[{"containerPort":9091,"name":"cmonitor","protocol":"TCP"}]}]'
+kubectl -n stackrox set env daemonsets/collector --containers="compliance" ROX_METRICS_PORT=":9091"
 
 # Modify network policies to allow ingress
 kubectl apply -f - <<EOF
@@ -20,6 +22,24 @@ spec:
   podSelector:
     matchExpressions:
     - {key: app, operator: In, values: [central, sensor]}
+  policyTypes:
+  - Ingress
+---
+apiVersion: networking.k8s.io/v1
+kind: NetworkPolicy
+metadata:
+  labels:
+    app.kubernetes.io/name: stackrox
+  name: allow-cmonitoring
+  namespace: stackrox
+spec:
+  ingress:
+  - ports:
+    - port: 9091
+      protocol: TCP
+  podSelector:
+    matchLabels:
+      app: collector
   policyTypes:
   - Ingress
 EOF
