3f4932d58b2f11b05c90074fcf35544c91fee570
Tomasz Janiszewski <janiszt@gmail.com>
2023-01-25 11:41:57 +0100

    Create JIRA tasks on CI exit trap

--- scripts/ci/lib.sh
+++ scripts/ci/lib.sh
@@ -35,6 +35,7 @@ ci_exit_trap() {
     echo "Exit code is: ${exit_code}"
 
     (send_slack_notice_for_failures_on_merge "${exit_code}") || { echo "ERROR: Could not slack a test failure message"; }
+    (create_jira_issues_for_failures "${exit_code}") || { echo "ERROR: Could not create JIRA tasks for a test failures"; }
 
     while [[ -e /tmp/hold ]]; do
         info "Holding this job for debug"
@@ -1156,20 +1157,29 @@ store_test_results() {
     local from="$1"
     local to="$2"
 
-    if ! is_in_PR_context; then
-    {
-        info "Creating JIRA task for failures found in $from"
-        curl --retry 5 -SsfL https://github.com/stackrox/junit2jira/releases/download/v0.0.3/junit2jira -o junit2jira && \
-        chmod +x junit2jira && \
-        ./junit2jira -junit-reports-dir "$from" -threshold 5
-    } || true
-    fi
-
     info "Copying test results from $from to $to"
 
     local dest="${ARTIFACT_DIR}/junit-$to"
 
     cp -a "$from" "$dest" || true # (best effort)
+}
+
+create_jira_issues_for_failures() {
+    local exitstatus="${1:-}"
+
+    if ! is_OPENSHIFT_CI || [[ "$exitstatus" == "0" ]] || is_in_PR_context; then
+        return 0
+    fi
+
+    if [[ -z "${ARTIFACT_DIR}" ]]; then
+        info "Warning: create_jira_issues_for_failures() requires the \$ARTIFACT_DIR variable to be set"
+        return 1
+    fi
+
+    info "Creating JIRA task for failures found in ${ARTIFACT_DIR}"
+    curl --retry 5 -SsfL https://github.com/stackrox/junit2jira/releases/download/v0.0.3/junit2jira -o junit2jira && \
+    chmod +x junit2jira && \
+    ./junit2jira -junit-reports-dir "${ARTIFACT_DIR}" -threshold 5
 }
 
 send_slack_notice_for_failures_on_merge() {
