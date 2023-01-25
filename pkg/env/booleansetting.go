52c75b590ee90d50926601b6770936f992bbac98
Connor Gorman <cgorman@redhat.com>
2023-01-25 15:41:37 -0800

    Create forward-looking build with Postgres permanently one

--- pkg/env/booleansetting.go
+++ pkg/env/booleansetting.go
@@ -7,6 +7,7 @@ import (
 
 // BooleanSetting represents an environment variable which should be parsed into a boolean
 type BooleanSetting struct {
+	changeable     bool
 	envVar         string
 	defaultBoolean bool
 }
@@ -23,6 +24,9 @@ func (d *BooleanSetting) Setting() string {
 
 // BooleanSetting returns the bool object represented by the environment variable
 func (d *BooleanSetting) BooleanSetting() bool {
+	if !d.changeable {
+		return d.defaultBoolean
+	}
 	val := os.Getenv(d.envVar)
 	if val == "" {
 		return d.defaultBoolean
@@ -34,6 +38,19 @@ func (d *BooleanSetting) BooleanSetting() bool {
 // RegisterBooleanSetting globally registers and returns a new boolean setting.
 func RegisterBooleanSetting(envVar string, defaultBoolean bool) *BooleanSetting {
 	s := &BooleanSetting{
+		changeable:     true,
+		envVar:         envVar,
+		defaultBoolean: defaultBoolean,
+	}
+
+	Settings[s.EnvVar()] = s
+	return s
+}
+
+// RegisterUnchangeableBooleanSetting allows for very little code to change, but for the env var to not listen to the env var
+func RegisterUnchangeableBooleanSetting(envVar string, defaultBoolean bool) *BooleanSetting {
+	s := &BooleanSetting{
+		changeable:     false,
 		envVar:         envVar,
 		defaultBoolean: defaultBoolean,
 	}
--- pkg/env/postgres_datastore.go
+++ pkg/env/postgres_datastore.go
@@ -2,5 +2,5 @@ package env
 
 var (
 	// PostgresDatastoreEnabled toggles whether to use Postgres for the datastore or not.
-	PostgresDatastoreEnabled = RegisterBooleanSetting("ROX_POSTGRES_DATASTORE", false)
+	PostgresDatastoreEnabled = RegisterUnchangeableBooleanSetting("ROX_POSTGRES_DATASTORE", true)
 )
