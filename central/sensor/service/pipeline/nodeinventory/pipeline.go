baa2b6e81fe94b94ba7aab98f933e1d693563876
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2023-01-26 11:04:52 +0100

    Unexport nodeinventory.newPipeline

--- central/sensor/service/pipeline/nodeinventory/pipeline.go
+++ central/sensor/service/pipeline/nodeinventory/pipeline.go
@@ -24,11 +24,11 @@ var (
 
 // GetPipeline returns an instantiation of this particular pipeline
 func GetPipeline() pipeline.Fragment {
-	return NewPipeline(clusterDataStore.Singleton(), nodeDatastore.Singleton(), enrichment.NodeEnricherSingleton(), riskManager.Singleton())
+	return newPipeline(clusterDataStore.Singleton(), nodeDatastore.Singleton(), enrichment.NodeEnricherSingleton(), riskManager.Singleton())
 }
 
-// NewPipeline returns a new instance of Pipeline.
-func NewPipeline(clusters clusterDataStore.DataStore, nodes nodeDatastore.DataStore, enricher enricher.NodeEnricher, riskManager riskManager.Manager) pipeline.Fragment {
+// newPipeline returns a new instance of Pipeline.
+func newPipeline(clusters clusterDataStore.DataStore, nodes nodeDatastore.DataStore, enricher enricher.NodeEnricher, riskManager riskManager.Manager) pipeline.Fragment {
 	return &pipelineImpl{
 		clusterStore:  clusters,
 		nodeDatastore: nodes,
