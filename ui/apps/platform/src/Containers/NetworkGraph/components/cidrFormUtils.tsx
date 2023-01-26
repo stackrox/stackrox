9948c62d07897dab307dcf68fac56f130957aec1
Linda Song <ls@stackrox.com>
2023-01-26 12:26:20 -0800

    oops there was a style problem

--- ui/apps/platform/src/Containers/NetworkGraph/components/cidrFormUtils.tsx
+++ ui/apps/platform/src/Containers/NetworkGraph/components/cidrFormUtils.tsx
@@ -1,4 +1,5 @@
 function getHasDuplicateFields(values, field) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     const fieldValues = values.entities.map(({ entity }) => entity[field]);
     return new Set(fieldValues).size !== fieldValues.length;
 }
