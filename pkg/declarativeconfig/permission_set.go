341f12484f39d8ce409e875e92271716867238ff
ivan-degtiarenko <degtiarenko@stackrox.com>
2023-01-26 15:08:27 +0100

    Add comments.

--- pkg/declarativeconfig/permission_set.go
+++ pkg/declarativeconfig/permission_set.go
@@ -6,24 +6,29 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+// PermissionSet is representation of storage.PermissionSet that supports transformation from YAML.
 type PermissionSet struct {
 	Name             string               `yaml:"name,omitempty"`
 	Description      string               `yaml:"description,omitempty"`
 	ResourceToAccess []ResourceWithAccess `yaml:"resources,omitempty"`
 }
 
+// Access is representation of storage.Access that supports transformation from YAML.
 type Access storage.Access
 
+// ResourceWithAccess unites resource name and corresponding access level.
 type ResourceWithAccess struct {
 	Resource string `yaml:"resource,omitempty"`
 	Access   Access `yaml:"access,omitempty"`
 }
 
+// MarshalYAML transforms Access to YAML format.
 func (a Access) MarshalYAML() ([]byte, error) {
 	protoAccess := storage.Access(a)
 	return []byte(protoAccess.String()), nil
 }
 
+// UnmarshalYAML makes transformation from YAML to Access.
 func (a *Access) UnmarshalYAML(value *yaml.Node) error {
 	var v string
 	if err := value.Decode(&v); err != nil {
