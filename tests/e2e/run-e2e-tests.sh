ec52769ec2a21377a1a1f72ddda4b0a6ab1061b0
Marcin Owsiany <porridge@redhat.com>
2023-01-26 12:59:49 +0100

    ROX-14352: Bump CI images to latest version. (#4485)

--- tests/e2e/run-e2e-tests.sh
+++ tests/e2e/run-e2e-tests.sh
@@ -137,7 +137,7 @@ if [[ ! -f "/i-am-rox-ci-image" ]]; then
       --platform linux/amd64 \
       --rm -it \
       --entrypoint="$0" \
-      quay.io/stackrox-io/apollo-ci:stackrox-test-0.3.53 "$@"
+      quay.io/stackrox-io/apollo-ci:stackrox-test-0.3.55 "$@"
     exit 0
 fi
 
