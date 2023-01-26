07d7b342f297263d6034f65fdd81ce0e49eb9051
ivan-degtiarenko <degtiarenko@stackrox.com>
2023-01-26 19:14:34 +0100

    YAML -> Go struct transformation

--- pkg/declarativeconfig/access_scope.go
+++ pkg/declarativeconfig/access_scope.go
@@ -1,10 +1,57 @@
 package declarativeconfig
 
-import "github.com/stackrox/rox/generated/storage"
+import (
+	"github.com/pkg/errors"
+	"github.com/stackrox/rox/generated/storage"
+	"gopkg.in/yaml.v3"
+)
 
-// TODO: currently it does not correspond to YAML format described in access_scope_test
 type AccessScope struct {
-	Name        string                           `yaml:"name,omitempty"`
-	Description string                           `yaml:"description,omitempty"`
-	Rules       *storage.SimpleAccessScope_Rules `yaml:"rules,omitempty"`
+	Name        string `yaml:"name,omitempty"`
+	Description string `yaml:"description,omitempty"`
+	Rules       Rules  `yaml:"rules,omitempty"`
+}
+
+type Namespace struct {
+	Cluster   string `yaml:"cluster,omitempty"`
+	Namespace string `yaml:"namespace,omitempty"`
+}
+
+type Operator storage.SetBasedLabelSelector_Operator
+
+// MarshalYAML transforms Access to YAML format.
+func (a Operator) MarshalYAML() ([]byte, error) {
+	protoAccess := storage.SetBasedLabelSelector_Operator(a)
+	return []byte(protoAccess.String()), nil
+}
+
+// UnmarshalYAML makes transformation from YAML to Access.
+func (a *Operator) UnmarshalYAML(value *yaml.Node) error {
+	var v string
+	if err := value.Decode(&v); err != nil {
+		return err
+	}
+	i, ok := storage.SetBasedLabelSelector_Operator_value[v]
+	if !ok {
+		return errors.Errorf("Operator value %s not found", v)
+	}
+	*a = Operator(i)
+	return nil
+}
+
+type Requirement struct {
+	Key      string   `yaml:"key,omitempty"`
+	Operator Operator `yaml:"operator,omitempty"`
+	Values   []string `yaml:"values,omitempty"`
+}
+
+type LabelSelector struct {
+	Requirements []Requirement `yaml:"requirements,omitempty"`
+}
+
+type Rules struct {
+	IncludedClusters        []string        `yaml:"includedClusters,omitempty"`
+	IncludedNamespaces      []Namespace     `yaml:"includedNamespaces,omitempty"`
+	ClusterLabelSelectors   []LabelSelector `yaml:"clusterLabelSelectors,omitempty"`
+	NamespaceLabelSelectors []LabelSelector `yaml:"namespaceLabelSelectors,omitempty"`
 }
--- pkg/declarativeconfig/access_scope_test.go
+++ pkg/declarativeconfig/access_scope_test.go
@@ -19,28 +19,28 @@ rules:
     - cluster: clusterC
       namespace: namespaceC
     - cluster: clusterD
-    - namespace: namespaceD
+      namespace: namespaceD
   clusterLabelSelectors:
-  	- requirements:
+    - requirements:
       - key: a
-        operator: In
+        operator: IN
         values: [a, b, c]
       - key: b
-        operator: NotIn
+        operator: NOT_IN
         values: [b, d]
     - requirements:
-       - key: c
-        operator: In
+      - key: c
+        operator: IN
         values: [d, e, f]
       - key: d
-        operator: NotIn
+        operator: NOT_IN
         values: [x, y, z]
 `)
 	as := AccessScope{}
 
 	err := yaml.Unmarshal(data, &as)
 	assert.NoError(t, err)
+	// TODO: more asserts
 	assert.Equal(t, "test-name", as.Name)
 	assert.Equal(t, "test-description", as.Description)
-
 }
