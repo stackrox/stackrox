80d647e62d6dfac49743802dff8176ef1c4b8876
David Shrewsberry <99685630+dashrews78@users.noreply.github.com>
2023-01-26 07:21:11 -0500

    Dashrews/ROX-14524 delete decommission rocks code for safety (#4545)
     remove decommission of rocks code

--- migrator/clone/metadata/db_clone_impl.go
+++ migrator/clone/metadata/db_clone_impl.go
@@ -29,9 +29,6 @@ kubectl -n stackrox set env deploy/central ROX_DONT_COMPARE_DEV_BUILDS=true
 
 	// ErrUnableToRestore -- cannot restore upgraded backup to a downgraded central
 	ErrUnableToRestore = "The backup bundle being restored is from an upgraded version of central and thus cannot applied.  The restored version %s, current version %s"
-
-	// ErrUnsupportedDatabase -- cannot use this database as it is stale after previous upgrades to Postgres
-	ErrUnsupportedDatabase = "Central has previously underwent multiple upgrades to Postgres.  This version of RocksDB is stale and not supported."
 )
 
 // DBClone -- holds information related to DB clones
