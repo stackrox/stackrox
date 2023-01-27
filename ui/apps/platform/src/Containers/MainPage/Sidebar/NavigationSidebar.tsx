25bcfcafb0f7a908ce2c3834efd2806d83053789
Van Wilson <vwilson@redhat.com>
2023-01-27 18:19:43 -0500

    ROX-14468: Make new Network Graph a Tech Preview in nav (#4575)

--- ui/apps/platform/src/Containers/MainPage/Sidebar/NavigationSidebar.tsx
+++ ui/apps/platform/src/Containers/MainPage/Sidebar/NavigationSidebar.tsx
@@ -68,11 +68,6 @@ function NavigationSidebar({
         );
     }
 
-    // TODO remove this temporary extra config menu item when the PF network graph goes live in the main menu
-    if (isFeatureFlagEnabled('ROX_NETWORK_GRAPH_PATTERNFLY')) {
-        platformConfigurationPaths.push(networkBasePathPF);
-    }
-
     const Navigation = (
         <Nav id="nav-primary-simple">
             <NavList id="nav-list-simple">
@@ -81,6 +76,13 @@ function NavigationSidebar({
                     path={dashboardPath}
                     title={basePathToLabelMap[dashboardPath]}
                 />
+                {isFeatureFlagEnabled('ROX_NETWORK_GRAPH_PATTERNFLY') && (
+                    <LeftNavItem
+                        isActive={location.pathname.includes(networkBasePathPF)}
+                        path={networkBasePathPF}
+                        title={basePathToLabelMap[networkBasePathPF]}
+                    />
+                )}
                 <LeftNavItem
                     isActive={
                         location.pathname.includes(networkBasePath) &&
