44215a3d2f4009d3c3a97c0c852f3560f9a628be
Gavin Jefferies <gavin@stackrox.com>
2023-01-29 15:43:09 +0000

    less assumption

--- scripts/ci/openshift.sh
+++ scripts/ci/openshift.sh
@@ -22,25 +22,31 @@ scale_worker_nodes() {
     oc -n openshift-machine-api get machineset
     oc -n openshift-machine-api get machines
 
-    original_machine_count="$(get_running_machine_count)"
-    # (TODO(fixme): hack: rely on the machine sets all being workers)
-    first_machine_set="$(oc -n openshift-machine-api get machineset -o json | jq -r '.items[0].metadata.name')"
-    # (TODO(fixme): hack: relying on the machine set only having 1 running node)
-    new_machineset_count=$((1 + increment))
-    oc -n openshift-machine-api scale machineset "$first_machine_set" --replicas="$new_machineset_count"
+    original_count="$(get_running_worker_count)"
+    first_worker_machine_set="$(get_first_worker_machine_set)"
+    current_replicas="$(oc -n openshift-machine-api get machineset "$first_worker_machine_set" -o json | jq -r '.spec.replicas')"
+    new_machineset_count=$((current_replicas + increment))
+    oc -n openshift-machine-api scale machineset "$first_worker_machine_set" --replicas="$new_machineset_count"
 
-    expected_machine_count=$((original_machine_count + increment))
+    expected_count=$((original_count + increment))
     # note: timeout of this loop is handled by the calling context
-    while [[ "$(get_running_machine_count)" != "$expected_machine_count" ]]; do
-        info "Current machine state does not have the running count we desire ($expected_machine_count)"
+    while [[ "$(get_running_worker_count)" != "$expected_count" ]]; do
+        info "Current machine state does not have the running count we desire ($expected_count)"
         oc  -n openshift-machine-api get machines
         sleep 60
     done
 }
 
-get_running_machine_count() {
-    oc -n openshift-machine-api get machines -o json | \
+get_running_worker_count() {
+    oc -n openshift-machine-api get machines -l "machine.openshift.io/cluster-api-machine-role=worker" -o json | \
     jq -r '.items | map(select(.status.providerStatus.instanceState | ascii_upcase=="RUNNING")) | length'
+}
+
+get_first_worker_machine_set() {
+    oc -n openshift-machine-api get machineset -o json | \
+    jq -r '[ .items[] |
+             select(.spec.template.metadata.labels["machine.openshift.io/cluster-api-machine-role"]=="worker")
+           ][0] | .metadata.name'
 }
 
 if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
