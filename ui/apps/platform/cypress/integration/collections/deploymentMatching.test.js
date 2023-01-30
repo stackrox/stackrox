41c7035cf0a0721d14d1871d8db8eb289beece2e
David Vail <dcvail17@gmail.com>
2023-01-30 09:10:58 -0500

    .

--- ui/apps/platform/cypress/integration/collections/deploymentMatching.test.js
+++ ui/apps/platform/cypress/integration/collections/deploymentMatching.test.js
@@ -4,7 +4,6 @@ import {
     assertDeploymentsAreMatched,
     assertDeploymentsAreMatchedExactly,
     assertDeploymentsAreNotMatched,
-    tryCreateCollection,
     tryDeleteCollection,
     visitCollections,
 } from './Collections.helpers';
