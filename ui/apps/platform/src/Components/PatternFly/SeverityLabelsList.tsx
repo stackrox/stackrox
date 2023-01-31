5bb6792ec99d46e73d3ff4ef249479f71a877ee5
David Vail <dcvail17@gmail.com>
2023-01-31 13:57:39 -0500

    Fix VM report severity colors

--- ui/apps/platform/src/Components/PatternFly/SeverityLabelsList.tsx
+++ ui/apps/platform/src/Components/PatternFly/SeverityLabelsList.tsx
@@ -8,10 +8,21 @@ export type SeverityLabelsListProps = {
     severities: VulnerabilitySeverity[];
 };
 
+const vulnerabilitySeverityLabelColors = {
+    CRITICAL_VULNERABILITY_SEVERITY: 'red',
+    IMPORTANT_VULNERABILITY_SEVERITY: 'orange',
+    MODERATE_VULNERABILITY_SEVERITY: 'gold',
+    LOW_VULNERABILITY_SEVERITY: 'grey',
+} as const;
+
 function SeverityLabelsList({ severities }: SeverityLabelsListProps): ReactElement {
     if (severities?.length > 0) {
         const severityLabels = severities.map((fixValue) => (
-            <Label className="pf-u-mr-sm" color="red" isCompact>
+            <Label
+                className="pf-u-mr-sm"
+                color={vulnerabilitySeverityLabelColors[fixValue]}
+                isCompact
+            >
                 {vulnerabilitySeverityLabels[fixValue]}
             </Label>
         ));
