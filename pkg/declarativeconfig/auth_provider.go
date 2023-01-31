0af333a9fdfbf459f0780b756ffff2ce675d0f63
ivan-degtiarenko <degtiarenko@stackrox.com>
2023-01-31 21:44:54 +0100

    Add remaining auth provider configs

--- pkg/declarativeconfig/auth_provider.go
+++ pkg/declarativeconfig/auth_provider.go
@@ -1,15 +1,18 @@
 package declarativeconfig
 
+// RequiredAttribute is representation of storage.AuthProvider_RequiredAttribute that supports transformation from YAML.
 type RequiredAttribute struct {
 	AttributeKey   string `yaml:"key,omitempty"`
 	AttributeValue string `yaml:"value,omitempty"`
 }
 
+// ClaimMapping represents a single entry in "claim_mappings" field in auth provider proto.
 type ClaimMapping struct {
-	PathToClaim    string `yaml:"pathToClaim,omitempty"`
-	MapToClaimName string `yaml:"mapTo,omitempty"`
+	Path string `yaml:"path,omitempty"`
+	Name string `yaml:"name,omitempty"`
 }
 
+// OIDCConfig contains config values for OIDC auth provider.
 type OIDCConfig struct {
 	Issuer                    string `yaml:"issuer,omitempty"`
 	CallbackMode              string `yaml:"mode,omitempty"`
@@ -19,17 +22,28 @@ type OIDCConfig struct {
 	DontUseClientSecretConfig bool   `yaml:"dontUseClientSecretConfig,omitempty"`
 }
 
+// SAMLConfig contains config values for SAML auth provider.
 type SAMLConfig struct {
-	// TODO: add more fields
-	// "sp_issuer"
-	// "idp_metadata_url"
-	// "idp_cert_pem"
-	// "idp_sso_url"
-	// "idp_nameid_format"
-	SPIssuer string `yaml:"spIssuer,omitempty"`
+	SpIssuer     string `yaml:"spIssuer,omitempty"`
+	MetadataURL  string `yaml:"metadataURL,omitempty"`
+	CertPEM      string `yaml:"certPEM,omitempty"`
+	SsoURL       string `yaml:"ssoURL,omitempty"`
+	NameidFormat string `yaml:"nameIdFormat,omitempty"`
 }
 
-// AuthProvider is representation of storage.PermissionSet that supports transformation from YAML.
+// IAPConfig contains config values for IAP auth provider.
+type IAPConfig struct {
+	Audience string `yaml:"audience,omitempty"`
+}
+
+// UserpkiConfig contains config values for User Certificates auth provider.
+type UserpkiConfig struct {
+	Keys string `yaml:"keys,omitempty"`
+}
+
+// AuthProvider is representation of storage.AuthProvider that supports transformation from YAML.
+// To specify auth provider type, you need to either specify oidc, saml, iap, userpki config or
+// set enableOpenshift variable to true.
 type AuthProvider struct {
 	Name               string              `yaml:"name,omitempty"`
 	MinimumRoleName    string              `yaml:"minimumRole,omitempty"`
@@ -40,8 +54,12 @@ type AuthProvider struct {
 	ClaimMappings      []ClaimMapping      `yaml:"claimMappings,omitempty"`
 	OIDCConfig         *OIDCConfig         `yaml:"oidc,omitempty"`
 	SAMLConfig         *SAMLConfig         `yaml:"saml,omitempty"`
+	IAPConfig          *IAPConfig          `yaml:"iap,omitempty"`
+	UserpkiConfig      *UserpkiConfig      `yaml:"userpki,omitempty"`
+	EnableOpenshift    bool                `yaml:"enableOpenshift,omitempty"`
 }
 
+// Group is representation of storage.Group that supports transformation from YAML.
 type Group struct {
 	AttributeKey   string `yaml:"key,omitempty"`
 	AttributeValue string `yaml:"value,omitempty"`
