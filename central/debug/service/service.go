3836af18e34c3217f9c60fdf6b74b091dccf5c5e
Yann Brillouet <91869377+rhybrillou@users.noreply.github.com>
2023-01-31 16:58:03 +0100

    Prepare Role resource grouping with Access (#4447)

--- central/debug/service/service.go
+++ central/debug/service/service.go
@@ -434,6 +434,7 @@ func (s *serviceImpl) getRoles(_ context.Context) (interface{}, error) {
 	accessRolesCtx := sac.WithGlobalAccessScopeChecker(context.Background(),
 		sac.AllowFixedScopes(
 			sac.AccessModeScopeKeys(storage.Access_READ_ACCESS),
+			// TODO: ROX-14398 Replace Role with Access
 			sac.ResourceScopeKeys(resources.Role)))
 
 	roles, errGetRoles := s.roleDataStore.GetAllRoles(accessRolesCtx)
