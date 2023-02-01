57f0b6fbeba58692e29fcdd08010aea49e8c7b6e
Saif Chaudhry <schaudhr@redhat.com>
2023-02-01 12:06:34 -0800

    fix: layout fixes to optimize spacing

--- ui/apps/platform/src/Containers/NetworkGraph/externalEntities/ExternalEntitiesSideBar.tsx
+++ ui/apps/platform/src/Containers/NetworkGraph/externalEntities/ExternalEntitiesSideBar.tsx
@@ -91,8 +91,9 @@ function ExternalEntitiesSideBar({
                     </FlexItem>
                 </Flex>
             </StackItem>
-            <StackItem isFilled style={{ overflow: 'auto' }} className="pf-u-p-md">
-                <Stack hasGutter>
+            <Divider component="hr" />
+            <StackItem isFilled style={{ overflow: 'auto' }}>
+                <Stack className="pf-u-p-md">
                     <StackItem>
                         <Flex>
                             <FlexItem flex={{ default: 'flex_1' }}>
@@ -110,10 +111,10 @@ function ExternalEntitiesSideBar({
                             </FlexItem>
                         </Flex>
                     </StackItem>
-                    <Divider component="hr" />
-                    <StackItem>
-                        <Toolbar>
-                            <ToolbarContent>
+                    <Divider component="hr" className="pf-u-py-md" />
+                    <StackItem className="pf-u-pb-md">
+                        <Toolbar className="pf-u-p-0">
+                            <ToolbarContent className="pf-u-px-0">
                                 <ToolbarItem>
                                     <FlowsTableHeaderText type="active" numFlows={numFlows} />
                                 </ToolbarItem>
