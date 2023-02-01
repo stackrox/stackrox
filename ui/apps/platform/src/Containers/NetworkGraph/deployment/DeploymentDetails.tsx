c0819b829c619229ea3786b3c21e0d23645701e7
Saif Chaudhry <sachaudh@users.noreply.github.com>
2023-02-01 12:47:26 -0800

    Network Graph Bug Fixes (#4670)

--- ui/apps/platform/src/Containers/NetworkGraph/deployment/DeploymentDetails.tsx
+++ ui/apps/platform/src/Containers/NetworkGraph/deployment/DeploymentDetails.tsx
@@ -20,7 +20,7 @@ import {
     TextVariants,
     Title,
 } from '@patternfly/react-core';
-import { ExclamationCircleIcon } from '@patternfly/react-icons';
+import { ExclamationCircleIcon, ExclamationTriangleIcon } from '@patternfly/react-icons';
 import pluralize from 'pluralize';
 
 import { Deployment } from 'types/deployment.proto';
@@ -97,26 +97,30 @@ function DeploymentDetails({
                                         direction={{ default: 'row' }}
                                         alignItems={{ default: 'alignItemsCenter' }}
                                     >
-                                        <FlexItem>
-                                            <Label
-                                                variant="outline"
-                                                color="red"
-                                                icon={<ExclamationCircleIcon />}
-                                            >
-                                                {numExternalFlows} external{' '}
-                                                {pluralize('flow', numExternalFlows)}
-                                            </Label>
-                                        </FlexItem>
-                                        <FlexItem>
-                                            <Label
-                                                variant="outline"
-                                                color="gold"
-                                                icon={<ExclamationCircleIcon />}
-                                            >
-                                                {numInternalFlows} internal{' '}
-                                                {pluralize('flow', numInternalFlows)}
-                                            </Label>
-                                        </FlexItem>
+                                        {numExternalFlows !== 0 && (
+                                            <FlexItem>
+                                                <Label
+                                                    variant="outline"
+                                                    color="red"
+                                                    icon={<ExclamationCircleIcon />}
+                                                >
+                                                    {numExternalFlows} external{' '}
+                                                    {pluralize('flow', numExternalFlows)}
+                                                </Label>
+                                            </FlexItem>
+                                        )}
+                                        {numInternalFlows !== 0 && (
+                                            <FlexItem>
+                                                <Label
+                                                    variant="outline"
+                                                    color="gold"
+                                                    icon={<ExclamationTriangleIcon />}
+                                                >
+                                                    {numInternalFlows} internal{' '}
+                                                    {pluralize('flow', numInternalFlows)}
+                                                </Label>
+                                            </FlexItem>
+                                        )}
                                     </Flex>
                                 </DescriptionListDescription>
                             </DescriptionListGroup>
