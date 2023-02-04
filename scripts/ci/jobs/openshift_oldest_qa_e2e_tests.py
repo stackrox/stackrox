1d177fc049074fb9e32723ebbcccfd00b996602e
Gavin Jefferies <gavin@stackrox.com>
2023-02-04 01:02:05 +0000

    PG=true

--- scripts/ci/jobs/openshift_oldest_qa_e2e_tests.py
+++ scripts/ci/jobs/openshift_oldest_qa_e2e_tests.py
@@ -5,13 +5,16 @@ Run qa-tests-backend in a openshift 4 cluster provided via a hive cluster_claim.
 """
 import os
 from base_qa_e2e_test import make_qa_e2e_test_runner
-from clusters import NullCluster
+from clusters import OpenShiftScaleWorkersCluster
 
 # set required test parameters
 os.environ["DEPLOY_STACKROX_VIA_OPERATOR"] = "false"
 os.environ["OPENSHIFT_CI_CLUSTER_CLAIM"] = "openshift-4"
 os.environ["ORCHESTRATOR_FLAVOR"] = "openshift"
 os.environ["OUTPUT_FORMAT"] = "helm"
-os.environ["ROX_POSTGRES_DATASTORE"] = "false"
+os.environ["ROX_POSTGRES_DATASTORE"] = "true"
 
-make_qa_e2e_test_runner(cluster=NullCluster()).run()
+# Scale up the cluster to support postgres
+cluster = OpenShiftScaleWorkersCluster(increment=1)
+
+make_qa_e2e_test_runner(cluster=cluster).run()
