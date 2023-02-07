c29b981ce95ee719f549633cedb93a761c22857e
dhaus67 <dhaus@redhat.com>
2023-02-07 21:53:57 +0100

    ROX-14148: Prefactor Transformer abstraqction for declarative configuration. (#4697)

---
+++ pkg/declarativeconfig/transform/transformer.go
@@ -0,0 +1,37 @@
+package transform
+
+import (
+	"reflect"
+
+	"github.com/gogo/protobuf/proto"
+	"github.com/stackrox/rox/pkg/declarativeconfig"
+	"github.com/stackrox/rox/pkg/errox"
+)
+
+// Transformer transforms a declarativeconfig.Configuration to proto.Message(s).
+type Transformer interface {
+	Transform(config declarativeconfig.Configuration) (map[reflect.Type][]proto.Message, error)
+}
+
+// New creates a Transformer that can handle transforming all currently supported declarativeconfig.Configuration.
+func New() Transformer {
+	return &universalTransformer{configurationTransformers: map[string]Transformer{
+		declarativeconfig.AuthProviderConfiguration:  nil,
+		declarativeconfig.AccessScopeConfiguration:   nil,
+		declarativeconfig.RoleConfiguration:          nil,
+		declarativeconfig.PermissionSetConfiguration: nil,
+	}}
+}
+
+type universalTransformer struct {
+	configurationTransformers map[string]Transformer
+}
+
+func (t *universalTransformer) Transform(config declarativeconfig.Configuration) (map[reflect.Type][]proto.Message, error) {
+	ct, exists := t.configurationTransformers[config.Type()]
+	if !exists {
+		return nil, errox.InvariantViolation.Newf("no transformation logic for declarative config type %s found",
+			config.Type())
+	}
+	return ct.Transform(config)
+}
