4cffe024a69a397d9d59d058e21a7beaf307226a
Gavin Jefferies <gavin@stackrox.com>
2023-02-06 17:58:28 -0800

    CI: Fix openshift e2e (#4730)

--- scripts/ci/jobs/openshift_penultimate_qa_e2e_tests.py
+++ scripts/ci/jobs/openshift_penultimate_qa_e2e_tests.py
@@ -5,7 +5,7 @@ Run qa-tests-backend in a openshift 4 cluster provided via a hive cluster_claim.
 """
 import os
 from base_qa_e2e_test import make_qa_e2e_test_runner
-from clusters import NullCluster
+from clusters import OpenShiftScaleWorkersCluster
 
 # set required test parameters
 os.environ["DEPLOY_STACKROX_VIA_OPERATOR"] = "true"
@@ -13,4 +13,7 @@ os.environ["OPENSHIFT_CI_CLUSTER_CLAIM"] = "openshift-4"
 os.environ["ORCHESTRATOR_FLAVOR"] = "openshift"
 os.environ["ROX_POSTGRES_DATASTORE"] = "false"
 
-make_qa_e2e_test_runner(cluster=NullCluster()).run()
+# Scale up the cluster to support postgres
+cluster = OpenShiftScaleWorkersCluster(increment=1)
+
+make_qa_e2e_test_runner(cluster=cluster).run()
