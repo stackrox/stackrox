b42091cbd7a6b272acdc08b0d249ca155fbad8fb
Chris Porter <chris@stackrox.com>
2023-02-09 09:11:28 -0500

    help output wording for force option

--- roxctl/image/check/check.go
+++ roxctl/image/check/check.go
@@ -85,7 +85,7 @@ func Command(cliEnvironment environment.Environment) *cobra.Command {
 	pkgUtils.Must(c.MarkFlagRequired("image"))
 	c.Flags().IntVarP(&imageCheckCmd.retryDelay, "retry-delay", "d", 3, "set time to wait between retries in seconds.")
 	c.Flags().IntVarP(&imageCheckCmd.retryCount, "retries", "r", 3, "number of retries before exiting as error.")
-	c.Flags().BoolVarP(&imageCheckCmd.force, "force", "f", false, "the --force flag ignores Central's cache for the image and forces a fresh re-pull from Scanner")
+	c.Flags().BoolVarP(&imageCheckCmd.force, "force", "f", false, "the --force flag bypasses Central's cache for the image and forces a fresh re-pull from Scanner")
 	c.Flags().BoolVar(&imageCheckCmd.sendNotifications, "send-notifications", false,
 		"whether to send notifications for violations (notifications will be sent to the notifiers "+
 			"configured in each violated policy).")
