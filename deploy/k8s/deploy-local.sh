1f650c83cb9f5e6ed6a6e4db693d97ce892472da
Matthias Meidinger <mmeiding@redhat.com>
2023-02-10 10:38:58 +0100

    Set PSP generation disabled by default

--- deploy/k8s/deploy-local.sh
+++ deploy/k8s/deploy-local.sh
@@ -7,7 +7,6 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # assuming deployment on Kubernetes in Docker for Mac or Minikube
 export COLLECTION_METHOD="${COLLECTION_METHOD:-ebpf}"
 export MONITORING_SUPPORT="${MONITORING_SUPPORT:-false}"
-export POD_SECURITY_POLICIES="${POD_SECURITY_POLICIES:-false}"
 
 # shellcheck source=/dev/null
 "$DIR"/deploy.sh
