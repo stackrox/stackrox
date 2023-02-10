c842c9df91589a6b62002c62e7180dfc41fc2203
ivan-degtiarenko <degtiarenko@stackrox.com>
2023-02-10 14:24:25 +0100

    Update user store documentation.

--- central/user/datastore/internal/store/store.go
+++ central/user/datastore/internal/store/store.go
@@ -18,7 +18,7 @@ type Store interface {
 }
 
 // New returns a new instance of a Store.
-// For now we will store information for up to 1000 users for 1 day.
+// The information is stored for 1 day.
 func New() Store {
 	return &storeImpl{
 		ec: expiringcache.NewExpiringCache(24 * time.Hour),
