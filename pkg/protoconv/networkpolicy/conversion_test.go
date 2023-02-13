303d15663d472e34b0080aa8efc1610bd977745e
Misha Sugakov <misha@stackrox.com>
2023-02-13 19:05:20 +0100

    Change values to map and move to global level
    Changed to map to randomize tests order.
    Moved to global level in order to be able to split tests into smaller
    ones.

--- pkg/protoconv/networkpolicy/conversion_test.go
+++ pkg/protoconv/networkpolicy/conversion_test.go
@@ -11,246 +11,229 @@ import (
 	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
-func TestNetworkPolicyConversion(t *testing.T) {
-	port := intstr.FromInt(5978)
-	protocol := coreV1.ProtocolTCP
-	cases := []struct {
-		name string
-		np   *v1.NetworkPolicy
-	}{
-		{
-			// Test empty to empty - this is actually very important as it ensures an empty list is different from a list not specified
-			// Kubernetes is very picky about a nil slice vs a non nil slice in terms of the implication in a network policy
-			name: "Empty to Empty",
-			np: &v1.NetworkPolicy{
-				TypeMeta: metav1.TypeMeta{
-					Kind: "NetworkPolicy",
-				},
-				Spec: v1.NetworkPolicySpec{
-					PolicyTypes: []v1.PolicyType{
-						v1.PolicyTypeIngress,
-						v1.PolicyTypeEgress,
-					},
+var (
+	port     = intstr.FromInt(5978)
+	protocol = coreV1.ProtocolTCP
+
+	cases = map[string]*v1.NetworkPolicy{
+		// Test empty to empty - this is actually very important as it ensures an empty list is different from a list not specified
+		// Kubernetes is very picky about a nil slice vs a non nil slice in terms of the implication in a network policy
+		"Empty to Empty": {
+			TypeMeta: metav1.TypeMeta{
+				Kind: "NetworkPolicy",
+			},
+			Spec: v1.NetworkPolicySpec{
+				PolicyTypes: []v1.PolicyType{
+					v1.PolicyTypeIngress,
+					v1.PolicyTypeEgress,
 				},
 			},
 		},
-		{
-			name: "Empty",
-			np: &v1.NetworkPolicy{
-				TypeMeta: metav1.TypeMeta{
-					APIVersion: "networking.k8s.io/v1",
-					Kind:       "NetworkPolicy",
-				},
-				ObjectMeta: metav1.ObjectMeta{
-					Name: "test-np",
-				},
-				Spec: v1.NetworkPolicySpec{
-					PolicyTypes: []v1.PolicyType{
-						v1.PolicyTypeIngress,
-						v1.PolicyTypeEgress,
-					},
+		"Empty": {
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "networking.k8s.io/v1",
+				Kind:       "NetworkPolicy",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test-np",
+			},
+			Spec: v1.NetworkPolicySpec{
+				PolicyTypes: []v1.PolicyType{
+					v1.PolicyTypeIngress,
+					v1.PolicyTypeEgress,
 				},
 			},
 		},
-		{
-			name: "Ingress",
-			np: &v1.NetworkPolicy{
-				TypeMeta: metav1.TypeMeta{
-					APIVersion: "networking.k8s.io/v1",
-					Kind:       "NetworkPolicy",
-				},
-				ObjectMeta: metav1.ObjectMeta{
-					Name: "test-np",
-				},
-				Spec: v1.NetworkPolicySpec{
-					PolicyTypes: []v1.PolicyType{
-						v1.PolicyTypeIngress,
-					},
-					Ingress: []v1.NetworkPolicyIngressRule{
-						{
-							From: []v1.NetworkPolicyPeer{
-								{
-									IPBlock: &v1.IPBlock{
-										CIDR:   "172.17.0.0/16",
-										Except: []string{"172.17.1.0/24"},
-									},
-									NamespaceSelector: &metav1.LabelSelector{
-										MatchLabels: map[string]string{
-											"project": "myproject",
-										},
-										MatchExpressions: []metav1.LabelSelectorRequirement{
-											{
-												Key:      "environment",
-												Operator: metav1.LabelSelectorOpNotIn,
-												Values:   []string{"testing", "staging"},
-											},
-										},
-									},
-									PodSelector: &metav1.LabelSelector{
-										MatchLabels: map[string]string{
-											"role": "frontend",
-										},
-										MatchExpressions: []metav1.LabelSelectorRequirement{
-											{
-												Key:      "status",
-												Operator: metav1.LabelSelectorOpIn,
-												Values:   []string{"active"},
-											},
-										},
-									},
-								},
-							},
-							Ports: []v1.NetworkPolicyPort{
-								{
-									Protocol: &protocol,
-									Port:     &port,
-								},
-							},
-						},
-					},
-				},
+		"Ingress": {
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "networking.k8s.io/v1",
+				Kind:       "NetworkPolicy",
 			},
-		},
-		{
-			name: "Egress",
-			np: &v1.NetworkPolicy{
-				TypeMeta: metav1.TypeMeta{
-					APIVersion: "networking.k8s.io/v1",
-					Kind:       "NetworkPolicy",
-				},
-				ObjectMeta: metav1.ObjectMeta{
-					Name: "test-np",
-				},
-				Spec: v1.NetworkPolicySpec{
-					PolicyTypes: []v1.PolicyType{
-						v1.PolicyTypeEgress,
-					},
-					Egress: []v1.NetworkPolicyEgressRule{
-						{
-							To: []v1.NetworkPolicyPeer{
-								{
-									NamespaceSelector: &metav1.LabelSelector{
-										MatchLabels: map[string]string{
-											"project": "myproject",
-										},
-									},
-									IPBlock: &v1.IPBlock{
-										CIDR:   "172.17.0.0/16",
-										Except: []string{"172.17.1.0/24"},
-									},
-									PodSelector: &metav1.LabelSelector{
-										MatchLabels: map[string]string{
-											"role": "frontend",
-										},
-									},
-								},
-							},
-							Ports: []v1.NetworkPolicyPort{
-								{
-									Protocol: &protocol,
-									Port:     &port,
-								},
-							},
-						},
-					},
-				},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test-np",
 			},
-		},
-		{
-			name: "Ingress and Egress",
-			np: &v1.NetworkPolicy{
-				TypeMeta: metav1.TypeMeta{
-					APIVersion: "network.k8s.io/v1",
-					Kind:       "NetworkPolicy",
+			Spec: v1.NetworkPolicySpec{
+				PolicyTypes: []v1.PolicyType{
+					v1.PolicyTypeIngress,
 				},
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "test-network-policy",
-					Namespace: "default",
-				},
-				Spec: v1.NetworkPolicySpec{
-					PodSelector: metav1.LabelSelector{
-						MatchLabels: map[string]string{
-							"role": "db",
-						},
-						MatchExpressions: []metav1.LabelSelectorRequirement{
+				Ingress: []v1.NetworkPolicyIngressRule{
+					{
+						From: []v1.NetworkPolicyPeer{
 							{
-								Key:      "status",
-								Operator: metav1.LabelSelectorOpNotIn,
-								Values:   []string{"disabled", "suspended"},
+								IPBlock: &v1.IPBlock{
+									CIDR:   "172.17.0.0/16",
+									Except: []string{"172.17.1.0/24"},
+								},
+								NamespaceSelector: &metav1.LabelSelector{
+									MatchLabels: map[string]string{
+										"project": "myproject",
+									},
+									MatchExpressions: []metav1.LabelSelectorRequirement{
+										{
+											Key:      "environment",
+											Operator: metav1.LabelSelectorOpNotIn,
+											Values:   []string{"testing", "staging"},
+										},
+									},
+								},
+								PodSelector: &metav1.LabelSelector{
+									MatchLabels: map[string]string{
+										"role": "frontend",
+									},
+									MatchExpressions: []metav1.LabelSelectorRequirement{
+										{
+											Key:      "status",
+											Operator: metav1.LabelSelectorOpIn,
+											Values:   []string{"active"},
+										},
+									},
+								},
+							},
+						},
+						Ports: []v1.NetworkPolicyPort{
+							{
+								Protocol: &protocol,
+								Port:     &port,
 							},
 						},
 					},
-					PolicyTypes: []v1.PolicyType{
-						v1.PolicyTypeIngress,
-						v1.PolicyTypeEgress,
-					},
-					Ingress: []v1.NetworkPolicyIngressRule{
-						{
-							From: []v1.NetworkPolicyPeer{
-								{
-									IPBlock: &v1.IPBlock{
-										CIDR:   "172.17.0.0/16",
-										Except: []string{"172.17.1.0/24"},
-									},
-									NamespaceSelector: &metav1.LabelSelector{
-										MatchLabels: map[string]string{
-											"project": "myproject",
-										},
-										MatchExpressions: []metav1.LabelSelectorRequirement{
-											{
-												Key:      "environment",
-												Operator: metav1.LabelSelectorOpNotIn,
-												Values:   []string{"testing", "staging"},
-											},
-										},
-									},
-									PodSelector: &metav1.LabelSelector{
-										MatchLabels: map[string]string{
-											"role": "frontend",
-										},
-										MatchExpressions: []metav1.LabelSelectorRequirement{
-											{
-												Key:      "status",
-												Operator: metav1.LabelSelectorOpIn,
-												Values:   []string{"active"},
-											},
-										},
+				},
+			},
+		},
+		"Egress": {
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "networking.k8s.io/v1",
+				Kind:       "NetworkPolicy",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test-np",
+			},
+			Spec: v1.NetworkPolicySpec{
+				PolicyTypes: []v1.PolicyType{
+					v1.PolicyTypeEgress,
+				},
+				Egress: []v1.NetworkPolicyEgressRule{
+					{
+						To: []v1.NetworkPolicyPeer{
+							{
+								NamespaceSelector: &metav1.LabelSelector{
+									MatchLabels: map[string]string{
+										"project": "myproject",
 									},
 								},
-							},
-							Ports: []v1.NetworkPolicyPort{
-								{
-									Protocol: &protocol,
-									Port:     &port,
+								IPBlock: &v1.IPBlock{
+									CIDR:   "172.17.0.0/16",
+									Except: []string{"172.17.1.0/24"},
+								},
+								PodSelector: &metav1.LabelSelector{
+									MatchLabels: map[string]string{
+										"role": "frontend",
+									},
 								},
 							},
 						},
+						Ports: []v1.NetworkPolicyPort{
+							{
+								Protocol: &protocol,
+								Port:     &port,
+							},
+						},
 					},
-					Egress: []v1.NetworkPolicyEgressRule{
+				},
+			},
+		},
+		"Ingress and Egress": {
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: "network.k8s.io/v1",
+				Kind:       "NetworkPolicy",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test-network-policy",
+				Namespace: "default",
+			},
+			Spec: v1.NetworkPolicySpec{
+				PodSelector: metav1.LabelSelector{
+					MatchLabels: map[string]string{
+						"role": "db",
+					},
+					MatchExpressions: []metav1.LabelSelectorRequirement{
 						{
-							To: []v1.NetworkPolicyPeer{
-								{
-									IPBlock: &v1.IPBlock{
-										CIDR:   "172.17.0.0/16",
-										Except: []string{"172.17.1.0/24"},
+							Key:      "status",
+							Operator: metav1.LabelSelectorOpNotIn,
+							Values:   []string{"disabled", "suspended"},
+						},
+					},
+				},
+				PolicyTypes: []v1.PolicyType{
+					v1.PolicyTypeIngress,
+					v1.PolicyTypeEgress,
+				},
+				Ingress: []v1.NetworkPolicyIngressRule{
+					{
+						From: []v1.NetworkPolicyPeer{
+							{
+								IPBlock: &v1.IPBlock{
+									CIDR:   "172.17.0.0/16",
+									Except: []string{"172.17.1.0/24"},
+								},
+								NamespaceSelector: &metav1.LabelSelector{
+									MatchLabels: map[string]string{
+										"project": "myproject",
 									},
-									NamespaceSelector: &metav1.LabelSelector{
-										MatchLabels: map[string]string{
-											"project": "myproject",
+									MatchExpressions: []metav1.LabelSelectorRequirement{
+										{
+											Key:      "environment",
+											Operator: metav1.LabelSelectorOpNotIn,
+											Values:   []string{"testing", "staging"},
 										},
 									},
-									PodSelector: &metav1.LabelSelector{
-										MatchLabels: map[string]string{
-											"role": "frontend",
+								},
+								PodSelector: &metav1.LabelSelector{
+									MatchLabels: map[string]string{
+										"role": "frontend",
+									},
+									MatchExpressions: []metav1.LabelSelectorRequirement{
+										{
+											Key:      "status",
+											Operator: metav1.LabelSelectorOpIn,
+											Values:   []string{"active"},
 										},
 									},
 								},
 							},
-							Ports: []v1.NetworkPolicyPort{
-								{
-									Protocol: &protocol,
-									Port:     &port,
+						},
+						Ports: []v1.NetworkPolicyPort{
+							{
+								Protocol: &protocol,
+								Port:     &port,
+							},
+						},
+					},
+				},
+				Egress: []v1.NetworkPolicyEgressRule{
+					{
+						To: []v1.NetworkPolicyPeer{
+							{
+								IPBlock: &v1.IPBlock{
+									CIDR:   "172.17.0.0/16",
+									Except: []string{"172.17.1.0/24"},
 								},
+								NamespaceSelector: &metav1.LabelSelector{
+									MatchLabels: map[string]string{
+										"project": "myproject",
+									},
+								},
+								PodSelector: &metav1.LabelSelector{
+									MatchLabels: map[string]string{
+										"role": "frontend",
+									},
+								},
+							},
+						},
+						Ports: []v1.NetworkPolicyPort{
+							{
+								Protocol: &protocol,
+								Port:     &port,
 							},
 						},
 					},
@@ -258,15 +241,18 @@ func TestNetworkPolicyConversion(t *testing.T) {
 			},
 		},
 	}
-	for _, c := range cases {
-		t.Run(c.name, func(t *testing.T) {
+)
+
+func TestNetworkPolicyConversion(t *testing.T) {
+	for name, np := range cases {
+		t.Run(name, func(t *testing.T) {
 			// Test k8s conversion is correct
-			protoNetworkPolicy := KubernetesNetworkPolicyWrap{NetworkPolicy: c.np}.ToRoxNetworkPolicy()
+			protoNetworkPolicy := KubernetesNetworkPolicyWrap{NetworkPolicy: np}.ToRoxNetworkPolicy()
 			k8sPolicy := RoxNetworkPolicyWrap{NetworkPolicy: protoNetworkPolicy}.ToKubernetesNetworkPolicy()
-			assert.Equal(t, c.np, k8sPolicy)
+			assert.Equal(t, np, k8sPolicy)
 
 			// Test `status` field is not present in the generated yaml
-			yaml, err := KubernetesNetworkPolicyWrap{c.np}.ToYaml()
+			yaml, err := KubernetesNetworkPolicyWrap{np}.ToYaml()
 			assert.NoError(t, err)
 			uObj, err := k8sutil.UnstructuredFromYAML(yaml)
 			assert.NoError(t, err, "yaml generation should succeed")
@@ -283,7 +269,7 @@ func TestNetworkPolicyConversion(t *testing.T) {
 			k8sPolicies, err := YamlWrap{Yaml: yaml}.ToKubernetesNetworkPolicies()
 			assert.NoError(t, err, "k8s policy generation should succeed")
 			assert.Equal(t, 1, len(k8sPolicies), "expected one policy from the yaml")
-			assert.Equal(t, c.np, k8sPolicies[0])
+			assert.Equal(t, np, k8sPolicies[0])
 
 			// Generation from yaml to roxNetworkPolicy
 			protoNetworkPolicies, err := YamlWrap{Yaml: yaml}.ToRoxNetworkPolicies()
@@ -291,7 +277,7 @@ func TestNetworkPolicyConversion(t *testing.T) {
 			assert.Equal(t, 1, len(protoNetworkPolicies), "expected one policy from the yaml")
 
 			// Check that yaml->rox, and k8s->rox creates the same network policy
-			protoNetworkPolicyFromK8s := KubernetesNetworkPolicyWrap{c.np}.ToRoxNetworkPolicy()
+			protoNetworkPolicyFromK8s := KubernetesNetworkPolicyWrap{np}.ToRoxNetworkPolicy()
 			assert.Equal(t, protoNetworkPolicyFromK8s, protoNetworkPolicies[0])
 		})
 	}
