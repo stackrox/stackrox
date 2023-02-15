0f79b6ecce21bc070da72071e8d08281a0be73c9
Luis Valero Martín <lvalerom@redhat.com>
2023-02-15 17:45:46 +0100

    Disable Sensor resync only in vanilla k8s (#4882)

--- deploy/k8s/deploy.sh
+++ deploy/k8s/deploy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+# Enable new event pipeline without re-sync for k8s deployments
+export ROX_RESYNC_DISABLED="true"
+
 K8S_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 # shellcheck source=/dev/null
 source "${K8S_DIR}/central.sh"
