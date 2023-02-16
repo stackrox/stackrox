603b2fdbd44571828e68e80bfb3696b02c63e241
Yury Kovalev <ykovalev@redhat.com>
2023-02-16 17:06:48 +0100

    Do not show Manage Tokens button when certs are up to date

--- ui/apps/platform/src/Containers/Clusters/Components/CredentialExpirationWidget.tsx
+++ ui/apps/platform/src/Containers/Clusters/Components/CredentialExpirationWidget.tsx
@@ -5,6 +5,8 @@ import CredentialExpiration from './CredentialExpiration';
 import CredentialInteraction from './CredentialInteraction';
 import ManageTokensButton from './ManageTokensButton';
 
+import { isCertificateExpiringSoon } from '../cluster.helpers';
+
 type CredentialExpirationWidgetProps = {
     clusterId: string;
     status: ClusterStatus;
@@ -16,11 +18,20 @@ const CredentialExpirationWidget = ({
     status,
     isManagerTypeNonConfigurable,
 }: CredentialExpirationWidgetProps) => {
+    const certExpiryStatus = status?.certExpiryStatus;
+    if (!certExpiryStatus?.sensorCertExpiry) {
+        return <CredentialExpiration certExpiryStatus={certExpiryStatus} />;
+    }
+
     if (isManagerTypeNonConfigurable) {
+        const currentDatetime = new Date();
+        if (!isCertificateExpiringSoon(certExpiryStatus, currentDatetime)) {
+            return <CredentialExpiration certExpiryStatus={certExpiryStatus} />;
+        }
         return (
             <Flex direction={{ default: 'column' }}>
                 <FlexItem>
-                    <CredentialExpiration certExpiryStatus={status?.certExpiryStatus} />
+                    <CredentialExpiration certExpiryStatus={certExpiryStatus} />
                 </FlexItem>
                 <FlexItem>
                     <ManageTokensButton />
@@ -28,13 +39,10 @@ const CredentialExpirationWidget = ({
             </Flex>
         );
     }
-    if (!status?.certExpiryStatus?.sensorCertExpiry) {
-        return <CredentialExpiration certExpiryStatus={status?.certExpiryStatus} />;
-    }
 
     return (
         <CredentialInteraction
-            certExpiryStatus={status?.certExpiryStatus}
+            certExpiryStatus={certExpiryStatus}
             upgradeStatus={status?.upgradeStatus}
             clusterId={clusterId}
         />
