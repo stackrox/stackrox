b991b6f43764839728832508c91a929fb8086d97
Dave Shrewsberry <dashrews@redhat.com>
2023-02-17 05:17:44 -0500

    trying to clean up a sensor

--- tests/upgrade/postgres_run.sh
+++ tests/upgrade/postgres_run.sh
@@ -203,7 +203,7 @@ test_upgrade_paths() {
     wait_for_api
 
     # Cleanup the scaled sensor before smoke tests
-    "${REPO_FOR_TIME_TRAVEL}"/deploy/k8s/delete-sensor.sh
+    "${REPO_FOR_TIME_TRAVEL}"/deploy/k8s/sensor-deploy/delete-sensor.sh
 
     info "Fetching a sensor bundle for cluster 'remote'"
     "$TEST_ROOT/bin/$TEST_HOST_PLATFORM/roxctl" version
