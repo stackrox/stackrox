63dfb43536d97a7c710717c888c3122f7bb5fd77
Mark Pedrotti <mark@stackrox.com>
2023-02-21 15:26:19 -0500

    Edit DatabaseStatusBanner

--- ui/apps/platform/src/Containers/MainPage/Banners/DatabaseStatusBanner.tsx
+++ ui/apps/platform/src/Containers/MainPage/Banners/DatabaseStatusBanner.tsx
@@ -29,11 +29,9 @@ function DatabaseStatusBanner(): ReactElement | null {
             });
     }, [pollEpoch]);
 
-    const showDatabaseWarning = isServerReachable && !isDatabaseAvailable;
-
-    if (showDatabaseWarning) {
+    if (isServerReachable && !isDatabaseAvailable) {
         return (
-            <Banner className="pf-u-text-align-center" isSticky variant={AlertVariant.danger}>
+            <Banner className="pf-u-text-align-center" variant={AlertVariant.danger}>
                 <span className="pf-u-text-align-center">
                     The database is currently not available. If this problem persists, please
                     contact support.
