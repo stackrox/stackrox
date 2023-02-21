5e3413ace9b83695611f6d66c1272ca54bd7b21c
Mark Pedrotti <pedrottimark@gmail.com>
2023-02-21 13:50:08 -0500

    Encapsulate OutdatedVersionBanner element and rewrite metadata reducer in TypeScript (#4887)

--- ui/apps/platform/src/hooks/useMetadata.ts
+++ ui/apps/platform/src/hooks/useMetadata.ts
@@ -1,13 +1,10 @@
 import { useSelector } from 'react-redux';
-import { createSelector } from 'reselect';
 
 import { selectors } from 'reducers';
 import { Metadata } from 'types/metadataService.proto';
 
-const selectMetadata = createSelector([selectors.getMetadata], (metadata: Metadata) => metadata);
-
 function useMetadata(): Metadata {
-    const metadata: Metadata = useSelector(selectMetadata);
+    const metadata = useSelector(selectors.metadataSelector);
 
     return metadata;
 }
