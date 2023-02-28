504b6802b218165ffe299298b5d91c365342d336
Marcin Owsiany <porridge@redhat.com>
2023-02-28 10:18:29 +0100

    Import ObjectReference.

--- qa-tests-backend/src/main/groovy/objects/K8sServiceAccount.groovy
+++ qa-tests-backend/src/main/groovy/objects/K8sServiceAccount.groovy
@@ -1,11 +1,13 @@
 package objects
 
+import io.fabric8.kubernetes.api.model.ObjectReference
+
 class K8sServiceAccount {
     String name
     String namespace
     Map<String, String> labels = [:]
     Map<String, String> annotations = [:]
     def automountToken
-    List<io.fabric8.kubernetes.api.model.ObjectReference> secrets = []
+    List<ObjectReference> secrets = []
     String[] imagePullSecrets = []
 }
