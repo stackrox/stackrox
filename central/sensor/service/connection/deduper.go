yocum137/stackrox-1

Add Central side SensorEvent hash

central/sensor/service/connection/deduper.go
@@ -1,0 +1,77 @@
+package connection
+
+import (
+	"hash"
+	"hash/fnv"
+	"reflect"
+
+	"github.com/mitchellh/hashstructure/v2"
+	"github.com/stackrox/rox/generated/internalapi/central"
+	"github.com/stackrox/rox/pkg/alert"
+	"github.com/stackrox/rox/pkg/utils"
+)
+
+type key struct {
+	id           string
+	resourceType reflect.Type
+}
+
+func newDeduper() *deduper {
+	return &deduper{
+		lastReceived: make(map[key]uint64),
+		hasher:       fnv.New64a(),
+	}
+}
+
+type deduper struct {
+	lastReceived map[key]uint64
+
+	hasher hash.Hash64
+}
+
+func skipCheck(msg *central.MsgFromSensor) bool {
+	eventMsg, ok := msg.Msg.(*central.MsgFromSensor_Event)
+	if !ok {
+		return true
+	}
+	if eventMsg.Event.GetProcessIndicator() != nil {
+		return true
+	}
+	if alert.IsRuntimeAlertResult(msg.GetEvent().GetAlertResults()) {
+		return true
+	}
+	if eventMsg.Event.GetReprocessDeployment() != nil {
+		return true
+	}
+	return false
+}
+
+func (d *deduper) check(msg *central.MsgFromSensor) bool {
+	if skipCheck(msg) {
+		return false
+	}
+	event := msg.GetEvent()
+	key := key{
+		id:           event.GetId(),
+		resourceType: reflect.TypeOf(event.GetResource()),
+	}
+	receivedHash := event.GetSensorHash()
+	// Backwards compatibility with a previous Sensor
+	if event.GetSensorHashOneof() == nil {
+		// Compute the sensor hash
+		d.hasher.Reset()
+		hashValue, err := hashstructure.Hash(event.GetResource(), hashstructure.FormatV2, &hashstructure.HashOptions{
+			TagName: "sensorhash",
+			Hasher:  d.hasher,
+		})
+		utils.Should(err)
+
+		receivedHash = hashValue
+	}
+	prevValue, ok := d.lastReceived[key]
+	if ok && prevValue == receivedHash {
+		return true
+	}
+	d.lastReceived[key] = receivedHash
+	return false
+}

central/sensor/service/connection/sensorevents.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"strings"
 
+	"github.com/stackrox/rox/central/metrics"
 	"github.com/stackrox/rox/central/sensor/service/common"
 	"github.com/stackrox/rox/central/sensor/service/pipeline"
 	"github.com/stackrox/rox/central/sensor/service/pipeline/reconciliation"
@@ -21,6 +22,7 @@ var deploymentQueueKey = reflectutils.Type((*central.SensorEvent_Deployment)(nil
 type sensorEventHandler struct {
 	typeToQueue map[string]*workerQueue
 
+	deduper  *deduper
 	pipeline pipeline.ClusterPipeline
 	injector common.MessageInjector
 	stopSig  *concurrency.ErrorSignal
@@ -28,11 +30,12 @@ type sensorEventHandler struct {
 	reconciliationMap *reconciliation.StoreMap
 }
 
-func newSensorEventHandler(pipeline pipeline.ClusterPipeline, injector common.MessageInjector, stopSig *concurrency.ErrorSignal) *sensorEventHandler {
+func newSensorEventHandler(pipeline pipeline.ClusterPipeline, injector common.MessageInjector, stopSig *concurrency.ErrorSignal, deduper *deduper) *sensorEventHandler {
 	return &sensorEventHandler{
 		typeToQueue:       make(map[string]*workerQueue),
 		reconciliationMap: reconciliation.NewStoreMap(),
 
+		deduper:  deduper,
 		pipeline: pipeline,
 		injector: injector,
 		stopSig:  stopSig,
@@ -73,6 +76,13 @@ func (s *sensorEventHandler) addMultiplexed(ctx context.Context, msg *central.Ms
 	default:
 		utils.Should(errors.New("handler only supports events"))
 	}
+
+	if s.deduper.check(msg) {
+		metrics.IncSensorEventsDeduper(true)
+		return
+	}
+	metrics.IncSensorEventsDeduper(false)
+
 	queue := s.typeToQueue[typ]
 	// Lazily create the queue for a type if necessary
 	if queue == nil {

stackrox/stackrox

central/sensor/service/connection/deduper.go
@@ -1,0 +1,81 @@
+package connection
+
+import (
+	"hash"
+	"hash/fnv"
+	"reflect"
+
+	"github.com/mitchellh/hashstructure/v2"
+	"github.com/stackrox/rox/generated/internalapi/central"
+	"github.com/stackrox/rox/pkg/alert"
+	"github.com/stackrox/rox/pkg/utils"
+)
+
+type key struct {
+	id           string
+	resourceType reflect.Type
+}
+
+func newDeduper() *deduper {
+	return &deduper{
+		lastReceived: make(map[key]uint64),
+		hasher:       fnv.New64a(),
+	}
+}
+
+type deduper struct {
+	lastReceived map[key]uint64
+
+	hasher hash.Hash64
+}
+
+func skipDedupe(msg *central.MsgFromSensor) bool {
+	eventMsg, ok := msg.Msg.(*central.MsgFromSensor_Event)
+	if !ok {
+		return true
+	}
+	if eventMsg.Event.GetProcessIndicator() != nil {
+		return true
+	}
+	if alert.IsRuntimeAlertResult(msg.GetEvent().GetAlertResults()) {
+		return true
+	}
+	if eventMsg.Event.GetReprocessDeployment() != nil {
+		return true
+	}
+	return false
+}
+
+func (d *deduper) dedupe(msg *central.MsgFromSensor) bool {
+	if skipDedupe(msg) {
+		return false
+	}
+	event := msg.GetEvent()
+	key := key{
+		id:           event.GetId(),
+		resourceType: reflect.TypeOf(event.GetResource()),
+	}
+	if event.GetAction() == central.ResourceAction_REMOVE_RESOURCE {
+		delete(d.lastReceived, key)
+		return false
+	}
+	receivedHash := event.GetSensorHash()
+	// Backwards compatibility with a previous Sensor
+	if event.GetSensorHashOneof() == nil {
+		// Compute the sensor hash
+		d.hasher.Reset()
+		hashValue, err := hashstructure.Hash(event.GetResource(), hashstructure.FormatV2, &hashstructure.HashOptions{
+			TagName: "sensorhash",
+			Hasher:  d.hasher,
+		})
+		utils.Should(err)
+
+		receivedHash = hashValue
+	}
+	prevValue, ok := d.lastReceived[key]
+	if ok && prevValue == receivedHash {
+		return true
+	}
+	d.lastReceived[key] = receivedHash
+	return false
+}

central/sensor/service/connection/sensorevents.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"strings"
 
+	"github.com/stackrox/rox/central/metrics"
 	"github.com/stackrox/rox/central/sensor/service/common"
 	"github.com/stackrox/rox/central/sensor/service/pipeline"
 	"github.com/stackrox/rox/central/sensor/service/pipeline/reconciliation"
@@ -21,6 +22,7 @@ var deploymentQueueKey = reflectutils.Type((*central.SensorEvent_Deployment)(nil
 type sensorEventHandler struct {
 	typeToQueue map[string]*workerQueue
 
+	deduper  *deduper
 	pipeline pipeline.ClusterPipeline
 	injector common.MessageInjector
 	stopSig  *concurrency.ErrorSignal
@@ -28,11 +30,12 @@ type sensorEventHandler struct {
 	reconciliationMap *reconciliation.StoreMap
 }
 
-func newSensorEventHandler(pipeline pipeline.ClusterPipeline, injector common.MessageInjector, stopSig *concurrency.ErrorSignal) *sensorEventHandler {
+func newSensorEventHandler(pipeline pipeline.ClusterPipeline, injector common.MessageInjector, stopSig *concurrency.ErrorSignal, deduper *deduper) *sensorEventHandler {
 	return &sensorEventHandler{
 		typeToQueue:       make(map[string]*workerQueue),
 		reconciliationMap: reconciliation.NewStoreMap(),
 
+		deduper:  deduper,
 		pipeline: pipeline,
 		injector: injector,
 		stopSig:  stopSig,
@@ -73,6 +76,13 @@ func (s *sensorEventHandler) addMultiplexed(ctx context.Context, msg *central.Ms
 	default:
 		utils.Should(errors.New("handler only supports events"))
 	}
+
+	if s.deduper.dedupe(msg) {
+		metrics.IncSensorEventsDeduper(true)
+		return
+	}
+	metrics.IncSensorEventsDeduper(false)
+
 	queue := s.typeToQueue[typ]
 	// Lazily create the queue for a type if necessary
 	if queue == nil {