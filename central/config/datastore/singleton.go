ea814e030b4dd87c19b3e71017bf93bc7db7d255
Yann Brillouet <ybrillou@redhat.com>
2023-03-03 18:14:15 +0100

    ROX-12750: Resource grouping - central part for Config and DebugLogs

--- central/config/datastore/singleton.go
+++ central/config/datastore/singleton.go
@@ -68,8 +68,7 @@ func initialize() {
 		context.Background(),
 		sac.AllowFixedScopes(
 			sac.AccessModeScopeKeys(storage.Access_READ_ACCESS, storage.Access_READ_WRITE_ACCESS),
-			// TODO: ROX-12750 Replace Config with Administration.
-			sac.ResourceScopeKeys(resources.Config)))
+			sac.ResourceScopeKeys(resources.Administration)))
 	config, err := d.GetConfig(ctx)
 	if err != nil {
 		panic(err)
