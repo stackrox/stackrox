167355468a8232270b006e445597413dfc81d8af
Tomasz Janiszewski <tomek@redhat.com>
2023-03-06 16:25:28 +0100

    Fix PolicyRepresenter

--- qa-tests-backend/src/main/groovy/common/YamlGenerator.groovy
+++ qa-tests-backend/src/main/groovy/common/YamlGenerator.groovy
@@ -1,6 +1,7 @@
 package common
 
-import io.fabric8.kubernetes.api.model.IntOrString
+import groovy.transform.CompileStatic
+import io.kubernetes.client.custom.IntOrString
 import org.yaml.snakeyaml.DumperOptions
 import org.yaml.snakeyaml.Yaml
 import org.yaml.snakeyaml.introspector.Property
@@ -8,16 +9,21 @@ import org.yaml.snakeyaml.nodes.NodeTuple
 import org.yaml.snakeyaml.nodes.Tag
 import org.yaml.snakeyaml.representer.Representer
 
+@CompileStatic
 class YamlGenerator {
     static String toYaml(Object object) {
         def options = new DumperOptions()
         options.setExplicitStart(true)
         options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK)
-        def yaml = new Yaml(new PolicyRepresenter(), options)
+        def yaml = new Yaml(new PolicyRepresenter(options))
         return yaml.dumpAs(object, Tag.MAP, null)
     }
 
     static class PolicyRepresenter extends Representer {
+
+        PolicyRepresenter(DumperOptions options) {
+            super(options)
+        }
 
         @Override
         protected NodeTuple representJavaBeanProperty(Object javaBean, Property property,
@@ -25,12 +31,13 @@ class YamlGenerator {
             if (propertyValue instanceof MetaClassImpl || propertyValue == null) {
                 return null
             } else if (propertyValue instanceof IntOrString) {
+                def pv = propertyValue as IntOrString
                 return super.representJavaBeanProperty(
                         javaBean,
                         property,
-                        propertyValue.integer ?
-                                propertyValue.intValue :
-                                propertyValue.strValue,
+                        pv.integer ?
+                                pv.intValue :
+                                pv.strValue,
                         customTag
                 )
             }
