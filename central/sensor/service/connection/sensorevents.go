5da844ac7bb20f311ef5c20256b301b6c5ceb459
J. Victor Martins <jvdm@sdf.org>
2023-03-05 23:19:27 -0800

    Multiple NodeInventory in the same Node worker Q

--- central/sensor/service/connection/sensorevents.go
+++ central/sensor/service/connection/sensorevents.go
@@ -17,10 +17,14 @@ import (
 
 const workerQueueSize = 16
 
-var deploymentQueueKey = reflectutils.Type((*central.SensorEvent_Deployment)(nil))
+var (
+	deploymentEventType = reflectutils.Type((*central.SensorEvent_Deployment)(nil))
+	nodeEventType       = reflectutils.Type((*central.SensorEvent_Node)(nil))
+)
 
 type sensorEventHandler struct {
-	typeToQueue map[string]*workerQueue
+	// workerQueues are keyed by central.SensorEvent type names.
+	workerQueues map[string]*workerQueue
 
 	deduper  *deduper
 	pipeline pipeline.ClusterPipeline
@@ -32,7 +36,7 @@ type sensorEventHandler struct {
 
 func newSensorEventHandler(pipeline pipeline.ClusterPipeline, injector common.MessageInjector, stopSig *concurrency.ErrorSignal, deduper *deduper) *sensorEventHandler {
 	return &sensorEventHandler{
-		typeToQueue:       make(map[string]*workerQueue),
+		workerQueues:      make(map[string]*workerQueue),
 		reconciliationMap: reconciliation.NewStoreMap(),
 
 		deduper:  deduper,
@@ -54,7 +58,7 @@ func stripTypePrefix(s string) string {
 }
 
 func (s *sensorEventHandler) addMultiplexed(ctx context.Context, msg *central.MsgFromSensor) {
-	var typ string
+	var eventType string
 	switch evt := msg.Msg.(type) {
 	case *central.MsgFromSensor_Event:
 		switch evt.Event.Resource.(type) {
@@ -66,9 +70,14 @@ func (s *sensorEventHandler) addMultiplexed(ctx context.Context, msg *central.Ms
 			s.reconciliationMap.Close()
 			return
 		case *central.SensorEvent_ReprocessDeployment:
-			typ = deploymentQueueKey
+			eventType = deploymentEventType
+		case *central.SensorEvent_NodeInventory:
+			// This will put both NodeInventory and Node events in the same worker queue,
+			// preventing events for the same Node ID to run concurrently, since both use
+			// Node ID as the event ID.
+			eventType = nodeEventType
 		default:
-			typ = reflectutils.Type(evt.Event.Resource)
+			eventType = reflectutils.Type(evt.Event.Resource)
 			if !s.reconciliationMap.IsClosed() {
 				s.reconciliationMap.Add(evt.Event.Resource, evt.Event.Id)
 			}
@@ -83,11 +92,11 @@ func (s *sensorEventHandler) addMultiplexed(ctx context.Context, msg *central.Ms
 	}
 	metrics.IncSensorEventsDeduper(false)
 
-	queue := s.typeToQueue[typ]
+	queue := s.workerQueues[eventType]
 	// Lazily create the queue for a type if necessary
 	if queue == nil {
-		queue = newWorkerQueue(workerQueueSize, stripTypePrefix(typ))
-		s.typeToQueue[typ] = queue
+		queue = newWorkerQueue(workerQueueSize, stripTypePrefix(eventType))
+		s.workerQueues[eventType] = queue
 		go queue.run(ctx, s.stopSig, s.handleMessages)
 	}
 	queue.push(msg)
