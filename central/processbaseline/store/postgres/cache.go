2ed20f67bced28de424ddf8499feab426d153d8c
Cong Du <c-du@users.noreply.github.com>
2023-03-07 13:00:56 -0800

    for test

--- central/processbaseline/store/postgres/cache.go
+++ central/processbaseline/store/postgres/cache.go
@@ -3,20 +3,20 @@ package postgres
 import (
 	"context"
 
+	"github.com/pkg/errors"
 	v1 "github.com/stackrox/rox/generated/api/v1"
 	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/sac"
 	"github.com/stackrox/rox/pkg/sync"
+	"github.com/stackrox/rox/pkg/utils"
 )
 
-// NewMapCache takes a db crud and key func and generates a fully in memory cache that wraps the crud interface
-// NOTE: This cache expects AT MOST one writer per key. This assumption allows us to avoid taking a lock around the
-// cache and the database. Instead, we simply need to lock map operations
-func NewMapCache(dbStore Store, keyFunc func(msg storage.ProcessBaseline) []byte) (Store, error) {
+// NewWithCache takes a postgres db and generates a store with in memory cache
+// Note: This cache bypasses the SAC check and relies on the SAC check on
+func NewWithCache(dbStore Store) (Store, error) {
 	impl := &cacheImpl{
 		dbStore: dbStore,
-		keyFunc: keyFunc,
-
-		cache: make(map[string]*storage.ProcessBaseline),
+		cache:   make(map[string]*storage.ProcessBaseline),
 	}
 
 	if err := impl.populate(); err != nil {
@@ -28,9 +28,8 @@ func NewMapCache(dbStore Store, keyFunc func(msg storage.ProcessBaseline) []byte
 type cacheImpl struct {
 	dbStore Store
 
-	keyFunc func(obj storage.ProcessBaseline) []byte
-	cache   map[string]*storage.ProcessBaseline
-	lock    sync.RWMutex
+	cache map[string]*storage.ProcessBaseline
+	lock  sync.RWMutex
 }
 
 func (c *cacheImpl) populate() error {
@@ -38,7 +37,7 @@ func (c *cacheImpl) populate() error {
 	c.lock.Lock()
 	defer c.lock.Unlock()
 
-	return c.dbStore.Walk(context.Background(), func(obj *storage.ProcessBaseline) error {
+	return c.dbStore.Walk(sac.WithAllAccess(context.Background()), func(obj *storage.ProcessBaseline) error {
 		// No need to clone objects pulled directly from the DB
 		c.cache[obj.GetId()] = obj
 		return nil
@@ -120,18 +119,37 @@ func (c cacheImpl) Exists(ctx context.Context, id string) (bool, error) {
 }
 
 func (c cacheImpl) Get(ctx context.Context, id string) (*storage.ProcessBaseline, bool, error) {
-	//TODO implement me
-	panic("implement me")
+	c.lock.RLock()
+	defer c.lock.RUnlock()
+
+	obj, ok := c.cache[id]
+	if !ok {
+		return nil, false, nil
+	}
+	return obj.Clone(), true, nil
 }
 
 func (c cacheImpl) GetByQuery(ctx context.Context, query *v1.Query) ([]*storage.ProcessBaseline, error) {
-	//TODO implement me
-	panic("implement me")
+	// Call dbStore function directly. It is not in use as of current time so add a reminder here.
+	utils.Should(errors.New("Engineering alert: this function is not optimized."))
+	return c.dbStore.GetByQuery(ctx, query)
 }
 
-func (c cacheImpl) GetMany(ctx context.Context, identifiers []string) ([]*storage.ProcessBaseline, []int, error) {
-	//TODO implement me
-	panic("implement me")
+func (c cacheImpl) GetMany(ctx context.Context, ids []string) ([]*storage.ProcessBaseline, []int, error) {
+	c.lock.RLock()
+	defer c.lock.RUnlock()
+
+	objs := make([]*storage.ProcessBaseline, 0, len(ids))
+	var missingIndices []int
+	for i, id := range ids {
+		obj, ok := c.cache[id]
+		if !ok {
+			missingIndices = append(missingIndices, i)
+			continue
+		}
+		objs = append(objs, obj)
+	}
+	return objs, missingIndices, nil
 }
 
 func (c cacheImpl) GetIDs(_ context.Context) ([]string, error) {
@@ -145,9 +163,16 @@ func (c cacheImpl) GetIDs(_ context.Context) ([]string, error) {
 	return ids, nil
 }
 
-func (c cacheImpl) Walk(ctx context.Context, fn func(obj *storage.ProcessBaseline) error) error {
-	//TODO implement me
-	panic("implement me")
+func (c cacheImpl) Walk(_ context.Context, fn func(obj *storage.ProcessBaseline) error) error {
+	c.lock.RLock()
+	defer c.lock.RUnlock()
+
+	for _, obj := range c.cache {
+		if err := fn(obj); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func (c cacheImpl) AckKeysIndexed(ctx context.Context, keys ...string) error {
@@ -157,102 +182,3 @@ func (c cacheImpl) AckKeysIndexed(ctx context.Context, keys ...string) error {
 func (c cacheImpl) GetKeysToIndex(ctx context.Context) ([]string, error) {
 	return c.dbStore.GetKeysToIndex(ctx)
 }
-
-/*
-
-
-func (c *cacheImpl) GetKeys(ctx context.Context) ([]string, error) {
-	c.lock.RLock()
-	defer c.lock.RUnlock()
-
-	keys := make([]string, 0, len(c.cache))
-	for key := range c.cache {
-		keys = append(keys, key)
-	}
-	return keys, nil
-}
-
-func (c *cacheImpl) Get(ctx context.Context, id string) (*storage.ProcessBaseline, bool, error) {
-	c.lock.RLock()
-	defer c.lock.RUnlock()
-
-	msg, ok := c.cache[id]
-	if !ok {
-		return nil, false, nil
-	}
-	return proto.Clone(msg), true, nil
-}
-
-func (c *cacheImpl) GetMany(ctx context.Context, ids []string) ([]storage.ProcessBaseline, []int, error) {
-	c.lock.RLock()
-	defer c.lock.RUnlock()
-
-	msgs := make([]storage.ProcessBaseline, 0, len(ids))
-	var missingIndices []int
-	for i, id := range ids {
-		msg, ok := c.cache[id]
-		if !ok {
-			missingIndices = append(missingIndices, i)
-			continue
-		}
-		msgs = append(msgs, proto.Clone(msg))
-	}
-	return msgs, missingIndices, nil
-}
-
-func (c *cacheImpl) Walk(ctx context.Context, fn func(msg storage.ProcessBaseline) error) error {
-	c.lock.RLock()
-	defer c.lock.RUnlock()
-
-	for _, msg := range c.cache {
-		if err := fn(proto.Clone(msg)); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func (c *cacheImpl) WalkAllWithID(ctx context.Context, fn func(id []byte, msg storage.ProcessBaseline) error) error {
-	c.lock.RLock()
-	defer c.lock.RUnlock()
-
-	for id, msg := range c.cache {
-		if err := fn([]byte(id), proto.Clone(msg)); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func (c *cacheImpl) UpsertMany(ctx context.Context, objs []*storage.ProcessBaseline) error {
-}
-
-func (c *cacheImpl) UpsertWithID(ctx context.Context, id string, msg storage.ProcessBaseline) error {
-	if err := c.dbStore.UpsertWithID(id, msg); err != nil {
-		return err
-	}
-	c.lock.Lock()
-	defer c.lock.Unlock()
-
-	c.cache[id] = proto.Clone(msg)
-	return nil
-}
-
-func (c *cacheImpl) UpsertManyWithIDs(ctx context.Context, ids []string, msgs []storage.ProcessBaseline) error {
-	if len(ids) != len(msgs) {
-		return errors.Errorf("length(ids) %d does not match len(msgs) %d", len(ids), len(msgs))
-	}
-
-	if err := c.dbStore.UpsertManyWithIDs(ids, msgs); err != nil {
-		return err
-	}
-	c.lock.Lock()
-	defer c.lock.Unlock()
-
-	for i, id := range ids {
-		c.cache[id] = proto.Clone(msgs[i])
-	}
-	return nil
-}
-
-*/
