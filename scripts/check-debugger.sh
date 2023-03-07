3490ef369cf857f7abdc96ac5c7bb9179569557b
Gavin Jefferies <gavin@stackrox.com>
2023-03-07 15:14:56 -0800

    missed another

--- scripts/check-debugger.sh
+++ scripts/check-debugger.sh
@@ -17,7 +17,7 @@ if [[ -n "${BUILD_TAG}" && "${DEBUG_BUILD}" == "yes" ]]; then
 fi
 
 # This searches for a file in the image without running the container.
-container=$(docker create stackrox/main:${BUILD_TAG})
+container=$(docker create stackrox/main:"${TAG}")
 docker export "${container}" | tar t | grep 'bin/dlv$' && found_dlv="yes" || found_dlv="no"
 docker rm "${container}" &>/dev/null
 
