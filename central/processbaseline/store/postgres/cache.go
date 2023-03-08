0a87e4ccdcc6ce5618989488ff2129098c2cb42d
Cong Du <c-du@users.noreply.github.com>
2023-03-08 09:25:49 -0800

    LRU

--- central/processbaseline/store/postgres/cache.go
+++ central/processbaseline/store/postgres/cache.go
@@ -3,12 +3,19 @@ package postgres
 import (
 	"context"
 
+	lru "github.com/hashicorp/golang-lru/v2"
 	"github.com/pkg/errors"
 	v1 "github.com/stackrox/rox/generated/api/v1"
 	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/pkg/sac"
+	"github.com/stackrox/rox/pkg/set"
 	"github.com/stackrox/rox/pkg/sync"
 	"github.com/stackrox/rox/pkg/utils"
+)
+
+var (
+	// maxCacheSize = 1024 * 1024 // Target value
+	maxCacheSize = 20 // low value testing correctness
 )
 
 // NewWithCache takes a postgres db and generates a store with in memory cache
@@ -16,10 +23,18 @@ import (
 func NewWithCache(dbStore Store) (Store, error) {
 	impl := &cacheImpl{
 		dbStore: dbStore,
-		cache:   make(map[string]*storage.ProcessBaseline),
+	}
+	var err error
+
+	impl.cache, err = lru.NewWithEvict(maxCacheSize, func(key string, value *storage.ProcessBaseline) {
+		impl.overflowIDs.Add(key)
+	})
+
+	if err != nil {
+		return nil, err
 	}
 
-	if err := impl.populate(); err != nil {
+	if err = impl.populate(); err != nil {
 		return nil, err
 	}
 	return impl, nil
@@ -28,8 +43,9 @@ func NewWithCache(dbStore Store) (Store, error) {
 type cacheImpl struct {
 	dbStore Store
 
-	cache map[string]*storage.ProcessBaseline
-	lock  sync.RWMutex
+	cache       *lru.Cache[string, *storage.ProcessBaseline]
+	overflowIDs set.StringSet
+	lock        sync.RWMutex
 }
 
 func (c *cacheImpl) populate() error {
@@ -38,17 +54,22 @@ func (c *cacheImpl) populate() error {
 	defer c.lock.Unlock()
 
 	return c.dbStore.Walk(sac.WithAllAccess(context.Background()), func(obj *storage.ProcessBaseline) error {
-		// No need to clone objects pulled directly from the DB
-		c.cache[obj.GetId()] = obj
+		c.cache.Add(obj.GetId(), obj)
 		return nil
 	})
 }
 
 func (c *cacheImpl) addNoLock(obj *storage.ProcessBaseline) {
-	c.cache[obj.GetId()] = obj.Clone()
+	c.cache.Add(obj.GetId(), obj.Clone())
+	c.overflowIDs.Remove(obj.GetId())
 }
 
-func (c cacheImpl) Upsert(ctx context.Context, obj *storage.ProcessBaseline) error {
+func (c *cacheImpl) deleteNoLock(id string) {
+	c.cache.Remove(id)
+	c.overflowIDs.Remove(id)
+}
+
+func (c *cacheImpl) Upsert(ctx context.Context, obj *storage.ProcessBaseline) error {
 	if err := c.dbStore.Upsert(ctx, obj); err != nil {
 		return err
 	}
@@ -56,10 +77,11 @@ func (c cacheImpl) Upsert(ctx context.Context, obj *storage.ProcessBaseline) err
 	defer c.lock.Unlock()
 
 	c.addNoLock(obj)
+	c.sanityCheckNoLock(ctx)
 	return nil
 }
 
-func (c cacheImpl) UpsertMany(ctx context.Context, objs []*storage.ProcessBaseline) error {
+func (c *cacheImpl) UpsertMany(ctx context.Context, objs []*storage.ProcessBaseline) error {
 	if err := c.dbStore.UpsertMany(ctx, objs); err != nil {
 		return err
 	}
@@ -69,10 +91,11 @@ func (c cacheImpl) UpsertMany(ctx context.Context, objs []*storage.ProcessBaseli
 	for _, obj := range objs {
 		c.addNoLock(obj)
 	}
+	c.sanityCheckNoLock(ctx)
 	return nil
 }
 
-func (c cacheImpl) Delete(ctx context.Context, id string) error {
+func (c *cacheImpl) Delete(ctx context.Context, id string) error {
 	if err := c.dbStore.Delete(ctx, id); err != nil {
 		return err
 	}
@@ -80,17 +103,18 @@ func (c cacheImpl) Delete(ctx context.Context, id string) error {
 	c.lock.Lock()
 	defer c.lock.Unlock()
 
-	delete(c.cache, id)
+	c.deleteNoLock(id)
 
+	c.sanityCheckNoLock(ctx)
 	return nil
 }
 
-func (c cacheImpl) DeleteByQuery(ctx context.Context, q *v1.Query) error {
-	//TODO implement me
+func (c *cacheImpl) DeleteByQuery(ctx context.Context, q *v1.Query) error {
+	// TODO implement me
 	panic("implement me")
 }
 
-func (c cacheImpl) DeleteMany(ctx context.Context, ids []string) error {
+func (c *cacheImpl) DeleteMany(ctx context.Context, ids []string) error {
 	if err := c.dbStore.DeleteMany(ctx, ids); err != nil {
 		return err
 	}
@@ -98,87 +122,156 @@ func (c cacheImpl) DeleteMany(ctx context.Context, ids []string) error {
 	defer c.lock.Unlock()
 
 	for _, id := range ids {
-		delete(c.cache, id)
+		c.deleteNoLock(id)
 	}
+	c.sanityCheckNoLock(ctx)
 	return nil
 }
 
-func (c cacheImpl) Count(ctx context.Context) (int, error) {
+func (c *cacheImpl) Count(ctx context.Context) (int, error) {
 	c.lock.RLock()
 	defer c.lock.RUnlock()
 
-	return len(c.cache), nil
+	c.sanityCheckNoLock(ctx)
+	return c.cache.Len() + c.overflowIDs.Cardinality(), nil
 }
 
-func (c cacheImpl) Exists(ctx context.Context, id string) (bool, error) {
+func (c *cacheImpl) Exists(ctx context.Context, id string) (bool, error) {
 	c.lock.RLock()
 	defer c.lock.RUnlock()
 
-	_, ok := c.cache[id]
-	return ok, nil
+	c.sanityCheckNoLock(ctx)
+	return c.cache.Contains(id) || c.overflowIDs.Contains(id), nil
 }
 
-func (c cacheImpl) Get(ctx context.Context, id string) (*storage.ProcessBaseline, bool, error) {
+func (c *cacheImpl) Get(ctx context.Context, id string) (*storage.ProcessBaseline, bool, error) {
 	c.lock.RLock()
 	defer c.lock.RUnlock()
 
-	obj, ok := c.cache[id]
+	c.sanityCheckNoLock(ctx)
+	if c.overflowIDs.Contains(id) {
+		if c.cache.Contains(id) {
+			utils.Should(errors.Errorf("cache and overflow contains both %s", id))
+		}
+		obj, exists, err := c.dbStore.Get(ctx, id)
+		if err == nil {
+			return nil, false, err
+		}
+		if !exists {
+			utils.Should(errors.Errorf("cache inconsistency for missing entry in database with id %s", id))
+		}
+		c.cache.Add(id, obj)
+		c.sanityCheckNoLock(ctx)
+		return obj.Clone(), exists, err
+	}
+
+	obj, ok := c.cache.Get(id)
 	if !ok {
 		return nil, false, nil
 	}
 	return obj.Clone(), true, nil
 }
 
-func (c cacheImpl) GetByQuery(ctx context.Context, query *v1.Query) ([]*storage.ProcessBaseline, error) {
+func (c *cacheImpl) GetByQuery(ctx context.Context, query *v1.Query) ([]*storage.ProcessBaseline, error) {
 	// Call dbStore function directly. It is not in use as of current time so add a reminder here.
 	utils.Should(errors.New("Engineering alert: this function is not optimized."))
 	return c.dbStore.GetByQuery(ctx, query)
 }
 
-func (c cacheImpl) GetMany(ctx context.Context, ids []string) ([]*storage.ProcessBaseline, []int, error) {
+func (c *cacheImpl) GetMany(ctx context.Context, ids []string) ([]*storage.ProcessBaseline, []int, error) {
 	c.lock.RLock()
 	defer c.lock.RUnlock()
 
+	c.sanityCheckNoLock(ctx)
 	objs := make([]*storage.ProcessBaseline, 0, len(ids))
 	var missingIndices []int
+	var idsToFetch []string
 	for i, id := range ids {
-		obj, ok := c.cache[id]
+		if c.overflowIDs.Contains(id) {
+			idsToFetch = append(idsToFetch, id)
+			continue
+		}
+		obj, ok := c.cache.Get(id)
 		if !ok {
 			missingIndices = append(missingIndices, i)
 			continue
 		}
-		objs = append(objs, obj)
+		objs = append(objs, obj.Clone())
 	}
+
+	fetched, missing, err := c.dbStore.GetMany(ctx, idsToFetch)
+	if err != nil {
+		return nil, nil, err
+	}
+	if len(missing) != 0 {
+		utils.Should(errors.Errorf("unexpected missing elements in fetch %v, missing %v", idsToFetch, missing))
+	}
+
+	for _, fetchedObj := range fetched {
+		c.addNoLock(fetchedObj)
+		objs = append(objs, fetchedObj)
+	}
+	c.sanityCheckNoLock(ctx)
+
 	return objs, missingIndices, nil
 }
 
-func (c cacheImpl) GetIDs(_ context.Context) ([]string, error) {
+func (c *cacheImpl) GetIDs(ctx context.Context) ([]string, error) {
 	c.lock.RLock()
 	defer c.lock.RUnlock()
 
-	ids := make([]string, 0, len(c.cache))
-	for id := range c.cache {
-		ids = append(ids, id)
-	}
+	c.sanityCheckNoLock(ctx)
+	ids := c.cache.Keys()
+	ids = append(ids, c.overflowIDs.AsSlice()...)
+
+	c.sanityCheckNoLock(ctx)
 	return ids, nil
 }
 
-func (c cacheImpl) Walk(_ context.Context, fn func(obj *storage.ProcessBaseline) error) error {
+func (c *cacheImpl) Walk(ctx context.Context, fn func(obj *storage.ProcessBaseline) error) error {
 	c.lock.RLock()
 	defer c.lock.RUnlock()
-
-	for _, obj := range c.cache {
+	c.sanityCheckNoLock(ctx)
+	for _, id := range c.cache.Keys() {
+		// Since Walk access every entry, no bother to update access of cache
+		obj, _ := c.cache.Peek(id)
 		if err := fn(obj); err != nil {
 			return err
+		}
+	}
+
+	if c.overflowIDs.Cardinality() > 0 {
+		objs, missing, err := c.dbStore.GetMany(ctx, c.overflowIDs.AsSlice())
+		if err != nil {
+			return nil
+		}
+		if len(missing) != 0 {
+			utils.Should(errors.Errorf("unexpected missing entries: %+v", missing))
+		}
+		for _, obj := range objs {
+			if err = fn(obj); err != nil {
+				return err
+			}
 		}
 	}
 	return nil
 }
 
-func (c cacheImpl) AckKeysIndexed(ctx context.Context, keys ...string) error {
+func (c *cacheImpl) AckKeysIndexed(ctx context.Context, keys ...string) error {
 	return c.dbStore.AckKeysIndexed(ctx, keys...)
 }
 
-func (c cacheImpl) GetKeysToIndex(ctx context.Context) ([]string, error) {
+func (c *cacheImpl) GetKeysToIndex(ctx context.Context) ([]string, error) {
 	return c.dbStore.GetKeysToIndex(ctx)
+}
+
+func (c *cacheImpl) sanityCheckNoLock(ctx context.Context) {
+	count, err := c.dbStore.Count(sac.WithAllAccess(context.Background()))
+	utils.Should(err)
+	if overlaps := c.overflowIDs.Intersect(set.NewStringSet(c.cache.Keys()...)); overlaps.Cardinality() != 0 {
+		utils.Should(errors.Errorf("unexpected overlap %v", overlaps))
+	}
+	if count != c.cache.Len()+c.overflowIDs.Cardinality() {
+		utils.Should(errors.Errorf("inconsistent cache count db %d cache %d", count, c.cache.Len()+c.overflowIDs.Cardinality()))
+	}
 }
