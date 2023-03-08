f8cf73144d3918d67b5470720d6856e763797135
J. Victor Martins <jvdm@sdf.org>
2023-03-07 18:50:53 -0800

    Update env

--- pkg/env/node_scan.go
+++ pkg/env/node_scan.go
@@ -6,10 +6,9 @@ var (
 	// NodeScanningInterval is the base value of the interval duration between node scans.
 	NodeScanningInterval = registerDurationSetting("ROX_NODE_SCANNING_INTERVAL", 4*time.Hour)
 
-	// NodeScanningIntervalDeviation is the percentage by which each interval
-	// duration between node scans will deviate from the base interval time. A value
-	// equal or lower than zero disables deviation.
-	NodeScanningIntervalDeviation = RegisterIntegerSetting("ROX_NODE_SCANNING_INTERVAL_DEVIATION", 10)
+	// NodeScanningIntervalDeviation is the duration node scans will deviate from the
+	// base interval time. The value is capped by the base interval.
+	NodeScanningIntervalDeviation = registerDurationSetting("ROX_NODE_SCANNING_INTERVAL_DEVIATION", 24*time.Minute)
 
 	// NodeScanningMaxInitialWait is the maximum wait time before the first node
 	// scan, which is randomly generated. Set zero to disable the initial node
