518a6c41c14fe5983de2b8722f60e7952a93c724
Connor Gorman <cgorman@redhat.com>
2023-03-08 12:54:41 -0800

    tests

--- pkg/scanners/clairify/clairify_test.go
+++ pkg/scanners/clairify/clairify_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/pkg/clair/mock"
+	"github.com/stackrox/rox/pkg/env"
 	clairV1 "github.com/stackrox/scanner/api/v1"
 	"github.com/stretchr/testify/assert"
 )
@@ -41,6 +42,8 @@ func getTestScan() (*clairV1.LayerEnvelope, *storage.ImageScan, *storage.Image) 
 }
 
 func TestConvertLayerToImageScan(t *testing.T) {
+	t.Setenv(env.ActiveVulnMgmt.EnvVar(), "true")
+
 	layer, protoScan, image := getTestScan()
 	actualScan := convertLayerToImageScan(image, layer)
 	// Ignore Scan time in the test, as it is defined as the time we retrieve the scan.
