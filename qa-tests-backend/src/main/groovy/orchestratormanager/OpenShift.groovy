f146c07f0cea373b391ac6158036c844040fb85c
Marcin Owsiany <porridge@redhat.com>
2023-03-09 11:06:43 +0100

    a few more missing static imports

--- qa-tests-backend/src/main/groovy/orchestratormanager/OpenShift.groovy
+++ qa-tests-backend/src/main/groovy/orchestratormanager/OpenShift.groovy
@@ -1,5 +1,7 @@
 package orchestratormanager
 
+import static util.Helpers.withRetry
+
 import groovy.util.logging.Slf4j
 import io.fabric8.kubernetes.client.KubernetesClientException
 import io.fabric8.openshift.api.model.ProjectRequest
