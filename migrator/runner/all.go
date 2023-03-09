20201b38f779d741933a75600ddc8160fde7f90d
dhaus67 <dhaus@redhat.com>
2023-03-09 16:13:34 +0100

    ROX-12578: Migration for groups table (#5142)

--- migrator/runner/all.go
+++ migrator/runner/all.go
@@ -129,4 +129,5 @@ import (
 	_ "github.com/stackrox/rox/migrator/migrations/m_170_to_m_171_create_policy_categories_and_edges"
 	_ "github.com/stackrox/rox/migrator/migrations/m_171_to_m_172_move_scope_to_collection_in_report_configurations"
 	_ "github.com/stackrox/rox/migrator/migrations/m_172_to_m_173_network_flows_partition"
+	_ "github.com/stackrox/rox/migrator/migrations/m_173_to_m_174_group_unique_constraint"
 )
