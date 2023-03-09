a060215df079cb0fcb65fe40f0489865d4dea218
Jan Schnathmeier <jschnath@redhat.com>
2023-03-09 11:05:06 +0100

    Fix style

--- sensor/common/compliance/compliance_communicator.go
+++ sensor/common/compliance/compliance_communicator.go
@@ -11,48 +11,57 @@ import (
 	"github.com/stackrox/rox/sensor/common"
 )
 
+// MessageToComplianceWithAddress adds the hostname to sensor.MsgToCompliance so we know where to send it to.
 type MessageToComplianceWithAddress struct {
 	msg       *sensor.MsgToCompliance
 	hostname  string
 	broadcast bool
 }
 
+// Multiplexer combines n input channels of type T into one output channel of type T
 type Multiplexer[T any] struct {
 	inputChannels  []<-chan *T
 	outputCommands chan *T
-	//connectionMap  map[string]sensor.ComplianceService_CommunicateServer
-	//manager connectionManager
+	// connectionMap  map[string]sensor.ComplianceService_CommunicateServer
+	// manager connectionManager
 
 	wg      sync.WaitGroup
 	started concurrency.Signal
 }
 
+// Notify is unimplemented, part of the component interface
 func (c *Multiplexer[T]) Notify(e common.SensorComponentEvent) {
-	return
+	// unimplemented
 }
 
+// Start starts the Multiplexer. It is important not to call this before all AddChannel calls
 func (c *Multiplexer[T]) Start() error {
 	// TODO maybe error if this fails(?)
 	c.run()
 	return nil
 }
 
+// Stop is unimplemented, part of the component interface
 func (c *Multiplexer[T]) Stop(err error) {
 }
 
+// Capabilities is unimplemented, part of the component interface
 func (c *Multiplexer[T]) Capabilities() []centralsensor.SensorCapability {
 	return nil
 }
 
+// ProcessMessage is unimplemented, part of the component interface
 func (c *Multiplexer[T]) ProcessMessage(msg *central.MsgToSensor) error {
 	return nil
 }
 
+// ResponsesC is unimplemented, part of the component interface
 func (c *Multiplexer[T]) ResponsesC() <-chan *central.MsgFromSensor {
 	return nil
 }
 
-func NewComplianceCommunicator[T any]() *Multiplexer[T] {
+// NewMultiplexer creates a Multiplexer of type T
+func NewMultiplexer[T any]() *Multiplexer[T] {
 	communicator := Multiplexer[T]{
 		inputChannels:  make([]<-chan *T, 0),
 		outputCommands: make(chan *T),
@@ -62,7 +71,7 @@ func NewComplianceCommunicator[T any]() *Multiplexer[T] {
 }
 
 // AddChannel Adds a channel to ComplianceCommunicator, AddChannel must be called
-// for ALL channels before calling Run()
+// for ALL channels before calling Start()
 func (c *Multiplexer[T]) AddChannel(channel <-chan *T) {
 	if c.started.IsDone() {
 		panic("Cannot AddChannel after component is started")
@@ -112,6 +121,7 @@ func FanIn[T any](ctx context.Context, channels ...<-chan *T) <-chan *T {
 	return multiplexedStream
 }
 
+// GetCommandsC returns the multiplexed output channel combining all input channels added with AddChannel
 func (c *Multiplexer[T]) GetCommandsC() <-chan *T {
 	return c.outputCommands
 }
