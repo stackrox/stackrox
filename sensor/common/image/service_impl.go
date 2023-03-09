308e77e622ae455125ac2f2e81e7f125feb1a260
David Caravello <119438707+dcaravel@users.noreply.github.com>
2023-03-09 15:25:28 -0600

    enable forced local scanning in adm controller flow

--- sensor/common/image/service_impl.go
+++ sensor/common/image/service_impl.go
@@ -8,9 +8,13 @@ import (
 	v1 "github.com/stackrox/rox/generated/api/v1"
 	"github.com/stackrox/rox/generated/internalapi/sensor"
 	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/env"
 	"github.com/stackrox/rox/pkg/expiringcache"
 	grpcPkg "github.com/stackrox/rox/pkg/grpc"
 	"github.com/stackrox/rox/pkg/grpc/authz/idcheck"
+	"github.com/stackrox/rox/pkg/registries/docker"
+	registryTypes "github.com/stackrox/rox/pkg/registries/types"
+	"github.com/stackrox/rox/pkg/tlscheck"
 	"github.com/stackrox/rox/sensor/common/imagecacheutils"
 	"github.com/stackrox/rox/sensor/common/registry"
 	"github.com/stackrox/rox/sensor/common/scan"
@@ -62,8 +66,8 @@ func (s *serviceImpl) GetImage(ctx context.Context, req *sensor.GetImageRequest)
 	// it is considered cluster-local.
 	req.Image.IsClusterLocal = s.registryStore.HasRegistryForImage(req.GetImage().GetName())
 
-	// Ask Central to scan the image if the image is not internal.
-	if !req.GetImage().GetIsClusterLocal() {
+	// Ask Central to scan the image if the image is not internal and local scanning is not forced
+	if !req.GetImage().GetIsClusterLocal() && !env.ForceLocalImageScanning.BooleanSetting() {
 		scanResp, err := s.centralClient.ScanImageInternal(ctx, &v1.ScanImageInternalRequest{
 			Image:      req.GetImage(),
 			CachedOnly: !req.GetScanInline(),
@@ -76,13 +80,59 @@ func (s *serviceImpl) GetImage(ctx context.Context, req *sensor.GetImageRequest)
 		}, nil
 	}
 
-	img, err := scan.EnrichLocalImage(ctx, s.centralClient, req.GetImage())
+	var err error
+	var img *storage.Image
+	if req.GetImage().GetIsClusterLocal() {
+		img, err = scan.EnrichLocalImage(ctx, s.centralClient, req.GetImage())
+
+	} else {
+		// ForceLocalImageScanning must be true
+		var reg registryTypes.Registry
+		reg, err = s.registryStore.GetFirstRegistryForImage(req.GetImage().GetName())
+
+		if err != nil {
+			// no registry found, create a new one temporarily that requires no auth
+			reg, err = createNoAuthDockerRegistry(ctx, req.GetImage().GetName().Registry)
+			if err != nil {
+				return nil, errors.Wrap(err, "creating no-auth image integration")
+			}
+		}
+
+		img, err = scan.EnrichLocalImageFromRegistry(ctx, s.centralClient, req.GetImage(), reg)
+	}
+
 	if err != nil {
 		return nil, errors.Wrap(err, "scanning image via local scanner")
 	}
 	return &sensor.GetImageResponse{
 		Image: img,
 	}, nil
+}
+
+// createNoAuthDockerRegistry creates a new registry image integration of type docker that
+// has no credentials
+//
+// TODO: temporary until image integrations from central are used instead
+func createNoAuthDockerRegistry(ctx context.Context, registry string) (registryTypes.Registry, error) {
+	secure, err := tlscheck.CheckTLS(ctx, registry)
+	if err != nil {
+		return nil, err
+	}
+
+	reg, err := docker.NewDockerRegistry(&storage.ImageIntegration{
+		Id:         registry,
+		Name:       registry,
+		Type:       "docker",
+		Categories: []storage.ImageIntegrationCategory{storage.ImageIntegrationCategory_REGISTRY},
+		IntegrationConfig: &storage.ImageIntegration_Docker{
+			Docker: &storage.DockerConfig{
+				Endpoint: registry,
+				Insecure: !secure,
+			},
+		},
+	})
+
+	return reg, err
 }
 
 // RegisterServiceServer registers this service with the given gRPC Server.
