74ff7d105c9c5943dda6d34d2d0c5a52e8326ea8
ivan-degtiarenko <degtiarenko@stackrox.com>
2023-03-09 12:50:15 +0100

    Address feedback from Mark.

--- ui/apps/platform/cypress/integration/accessControl/accessControlOrigin.test.js
+++ ui/apps/platform/cypress/integration/accessControl/accessControlOrigin.test.js
@@ -5,19 +5,20 @@ import {
     accessScopesKey,
     authProvidersAlias,
     authProvidersKey,
-    permissionSetsKey,
+    clickEntityNameInTable,
     groupsAlias,
+    permissionSetsAlias,
+    permissionSetsKey,
     rolesAlias,
     rolesKey,
-    clickEntityNameInTable,
-    permissionSetsAlias,
     visitAccessControlEntities,
 } from './accessControl.helpers';
 import { selectors } from './accessControl.selectors';
 
-describe('Access Control declarative Access scopes', () => {
+describe('Access Control declarative resources', () => {
     withAuth();
 
+    // Access scopes.
     it('declarative access scopes in list view have correct origin and are disabled', () => {
         const staticResponseMap = {
             [accessScopesAlias]: {
@@ -53,76 +54,7 @@ describe('Access Control declarative Access scopes', () => {
         cy.get(selectors.form.inputDescription).should('be.disabled');
     });
 
-    it('declarative permission sets in list view have correct origin and are disabled', () => {
-        const staticResponseMap = {
-            [permissionSetsAlias]: {
-                fixture: 'auth/declarativePermissionSet.json',
-            },
-        };
-        visitAccessControlEntities(permissionSetsKey, staticResponseMap);
-
-        cy.get('td[data-label="Origin"]').should('have.text', 'Declarative');
-        cy.get('td.pf-c-table__action button[aria-label="Actions"]').should('be.disabled');
-    });
-
-    it('list link for declarative permission set goes to form which has label instead of button and disabled input values', () => {
-        const staticResponseMap = {
-            [permissionSetsAlias]: {
-                fixture: 'auth/declarativePermissionSet.json',
-            },
-        };
-        visitAccessControlEntities(permissionSetsKey, staticResponseMap);
-
-        const entityName = 'permission-set-test-name';
-        clickEntityNameInTable(permissionSetsKey, entityName);
-
-        cy.get(`h2:contains("${entityName}")`);
-        cy.get(`li.pf-c-breadcrumb__item:nth-child(2):contains("${entityName}")`);
-
-        cy.get('.pf-c-label:contains("Declarative")').should('exist');
-
-        cy.get(selectors.form.notEditableLabel).should('exist');
-        cy.get(selectors.form.editButton).should('not.exist');
-
-        cy.get(selectors.form.inputName).should('be.disabled');
-        cy.get(selectors.form.inputDescription).should('be.disabled');
-    });
-
-    it('declarative roles in list view have correct origin and are disabled', () => {
-        const staticResponseMap = {
-            [rolesAlias]: {
-                fixture: 'auth/declarativeRole.json',
-            },
-        };
-        visitAccessControlEntities(rolesKey, staticResponseMap);
-
-        cy.get('td[data-label="Origin"]').should('have.text', 'Declarative');
-        cy.get('td.pf-c-table__action button[aria-label="Actions"]').should('be.disabled');
-    });
-
-    it('list link for declarative role goes to form which has label instead of button and disabled input values', () => {
-        const staticResponseMap = {
-            [rolesAlias]: {
-                fixture: 'auth/declarativeRole.json',
-            },
-        };
-        visitAccessControlEntities(rolesKey, staticResponseMap);
-
-        const entityName = 'test-role-name';
-        clickEntityNameInTable(rolesKey, entityName);
-
-        cy.get(`h2:contains("${entityName}")`);
-        cy.get(`li.pf-c-breadcrumb__item:nth-child(2):contains("${entityName}")`);
-
-        cy.get('.pf-c-label:contains("Declarative")').should('exist');
-
-        cy.get(selectors.form.notEditableLabel).should('exist');
-        cy.get(selectors.form.editButton).should('not.exist');
-
-        cy.get(selectors.form.inputName).should('be.disabled');
-        cy.get(selectors.form.inputDescription).should('be.disabled');
-    });
-
+    // Auth providers.
     it('declarative auth providers in list view have correct origin and are disabled', () => {
         const staticResponseMap = {
             [authProvidersAlias]: {
@@ -170,5 +102,77 @@ describe('Access Control declarative Access scopes', () => {
         cy.get('input[id="groups[0].props.key-select-typeahead"]').should('be.disabled');
         cy.get('button[id="groups[0].roleName"]').should('be.disabled');
         cy.get('button[aria-label="Delete rule"]').should('not.exist');
+    });
+
+    // Permission sets.
+    it('declarative permission sets in list view have correct origin and are disabled', () => {
+        const staticResponseMap = {
+            [permissionSetsAlias]: {
+                fixture: 'auth/declarativePermissionSet.json',
+            },
+        };
+        visitAccessControlEntities(permissionSetsKey, staticResponseMap);
+
+        cy.get('td[data-label="Origin"]').should('have.text', 'Declarative');
+        cy.get('td.pf-c-table__action button[aria-label="Actions"]').should('be.disabled');
+    });
+
+    it('list link for declarative permission set goes to form which has label instead of button and disabled input values', () => {
+        const staticResponseMap = {
+            [permissionSetsAlias]: {
+                fixture: 'auth/declarativePermissionSet.json',
+            },
+        };
+        visitAccessControlEntities(permissionSetsKey, staticResponseMap);
+
+        const entityName = 'permission-set-test-name';
+        clickEntityNameInTable(permissionSetsKey, entityName);
+
+        cy.get(`h2:contains("${entityName}")`);
+        cy.get(`li.pf-c-breadcrumb__item:nth-child(2):contains("${entityName}")`);
+
+        cy.get('.pf-c-label:contains("Declarative")').should('exist');
+
+        cy.get(selectors.form.notEditableLabel).should('exist');
+        cy.get(selectors.form.editButton).should('not.exist');
+
+        cy.get(selectors.form.inputName).should('be.disabled');
+        cy.get(selectors.form.inputDescription).should('be.disabled');
+    });
+
+    // Roles.
+    it('declarative roles in list view have correct origin and are disabled', () => {
+        const staticResponseMap = {
+            [rolesAlias]: {
+                fixture: 'auth/declarativeRole.json',
+            },
+        };
+        visitAccessControlEntities(rolesKey, staticResponseMap);
+
+        cy.get('td[data-label="Origin"]').should('have.text', 'Declarative');
+        cy.get('td.pf-c-table__action button[aria-label="Actions"]').should('be.disabled');
+    });
+
+    it('list link for declarative role goes to form which has label instead of button and disabled input values', () => {
+        const staticResponseMap = {
+            [rolesAlias]: {
+                fixture: 'auth/declarativeRole.json',
+            },
+        };
+        visitAccessControlEntities(rolesKey, staticResponseMap);
+
+        const entityName = 'test-role-name';
+        clickEntityNameInTable(rolesKey, entityName);
+
+        cy.get(`h2:contains("${entityName}")`);
+        cy.get(`li.pf-c-breadcrumb__item:nth-child(2):contains("${entityName}")`);
+
+        cy.get('.pf-c-label:contains("Declarative")').should('exist');
+
+        cy.get(selectors.form.notEditableLabel).should('exist');
+        cy.get(selectors.form.editButton).should('not.exist');
+
+        cy.get(selectors.form.inputName).should('be.disabled');
+        cy.get(selectors.form.inputDescription).should('be.disabled');
     });
 });
