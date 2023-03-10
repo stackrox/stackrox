66b1d3adec2d73cb7f935492a9e6560f660e7483
Yann Brillouet <91869377+rhybrillou@users.noreply.github.com>
2023-03-10 10:31:18 +0100

    ROX-14947: Define API to list (ID, name) pairs for cluster and namespace in user scope (#4801)

---
+++ central/role/datastore/datastore_test_constructors.go
@@ -0,0 +1,41 @@
+package datastore
+
+import (
+	"testing"
+
+	permissionSetPostgresStore "github.com/stackrox/rox/central/role/store/permissionset/postgres"
+	permissionSetRocksDBStore "github.com/stackrox/rox/central/role/store/permissionset/rocksdb"
+	rolePostgresStore "github.com/stackrox/rox/central/role/store/role/postgres"
+	roleRocksDBStore "github.com/stackrox/rox/central/role/store/role/rocksdb"
+	accessScopePostgresStore "github.com/stackrox/rox/central/role/store/simpleaccessscope/postgres"
+	accessScopeRocksDBStore "github.com/stackrox/rox/central/role/store/simpleaccessscope/rocksdb"
+	"github.com/stackrox/rox/pkg/postgres"
+	rocksdbBase "github.com/stackrox/rox/pkg/rocksdb"
+)
+
+// GetTestPostgresDataStore provides a datastore connected to postgres for testing purposes.
+func GetTestPostgresDataStore(_ *testing.T, pool *postgres.DB) (DataStore, error) {
+	permissionStore := permissionSetPostgresStore.New(pool)
+	roleStore := rolePostgresStore.New(pool)
+	scopeStore := accessScopePostgresStore.New(pool)
+
+	return New(roleStore, permissionStore, scopeStore), nil
+}
+
+// GetTestRocksBleveDataStore provides a datastore connected to rocksdb and bleve for testing purposes.
+func GetTestRocksBleveDataStore(_ *testing.T, rocksengine *rocksdbBase.RocksDB) (DataStore, error) {
+	permissionStore, err := permissionSetRocksDBStore.New(rocksengine)
+	if err != nil {
+		return nil, err
+	}
+	roleStore, err := roleRocksDBStore.New(rocksengine)
+	if err != nil {
+		return nil, err
+	}
+	scopeStore, err := accessScopeRocksDBStore.New(rocksengine)
+	if err != nil {
+		return nil, err
+	}
+
+	return New(roleStore, permissionStore, scopeStore), nil
+}
