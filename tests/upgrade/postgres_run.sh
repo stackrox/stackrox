96b00b56500db18e974abc71660c3d0089ac5346
Dave Shrewsberry <dashrews@redhat.com>
2023-03-13 11:11:27 -0400

    X-Smart-Squash: Squashed 2 commits:
    3340cee962 collector previous
    064cd54c3d exclude some stuff because comm problems with all the bounces is normal

--- tests/upgrade/postgres_run.sh
+++ tests/upgrade/postgres_run.sh
@@ -105,6 +105,13 @@ test_upgrade_paths() {
 
     cd "$TEST_ROOT"
 
+    # This test does a lot of upgrades and bounces.  If things take a little longer to bounce we can get entries in
+    # logs indicating communication problems.  Those need to be allowed in the case of this test ONLY.
+    echo "# postgres was bounced, may see some connection errors" >> scripts/ci/logcheck/allowlist-patterns
+    echo "FATAL: terminating connection due to administrator command \(SQLSTATE 57P01\)" >> scripts/ci/logcheck/allowlist-patterns
+    echo "Unable to connect to Sensor at"
+    echo >> scripts/ci/logcheck/allowlist-patterns
+
     ########################################################################################
     # Use helm to upgrade to a Postgres release.                                           #
     ########################################################################################
@@ -112,8 +119,6 @@ test_upgrade_paths() {
     helm_upgrade_to_postgres
     wait_for_api
     wait_for_scanner_to_be_ready
-    # Bounce collectors to avoid restarts on initial module pull
-    kubectl -n stackrox delete pod -l app=collector --grace-period=0
 
     # Upgraded to Postgres via helm.  Validate the upgrade.
     validate_upgrade "00_upgrade" "central upgrade to postgres" "268c98c6-e983-4f4e-95d2-9793cebddfd7"
@@ -133,12 +138,14 @@ test_upgrade_paths() {
     info "Bouncing central"
     kubectl -n stackrox delete po "$(kubectl -n stackrox get po -l app=central -o=jsonpath='{.items[0].metadata.name}')" --grace-period=0
     wait_for_api
-    # Bounce collectors to avoid restarts on if central is down long enough so sensor restarts
-    kubectl -n stackrox delete pod -l app=collector --grace-period=0
+    sensor_wait
 
     # Verify data is still there
     checkForRocksAccessScopes
     checkForPostgresAccessScopes
+
+    # Bounce collectors to avoid restarts on if central is down long enough so sensor restarts
+    kubectl -n stackrox delete pod -l app=collector --grace-period=0
 
     validate_upgrade "01-bounce-after-upgrade" "bounce after postgres upgrade" "268c98c6-e983-4f4e-95d2-9793cebddfd7"
     collect_and_check_stackrox_logs "$log_output_dir" "01_post_bounce"
@@ -159,11 +166,6 @@ test_upgrade_paths() {
     checkForPostgresAccessScopes
 
     validate_upgrade "02-bounce-db-after-upgrade" "bounce central db after postgres upgrade" "268c98c6-e983-4f4e-95d2-9793cebddfd7"
-
-    # Since we bounced the DB we may see some errors.  Those need to be allowed in the case of this test ONLY.
-    echo "# postgres was bounced, may see some connection errors" >> scripts/ci/logcheck/allowlist-patterns
-    echo "FATAL: terminating connection due to administrator command \(SQLSTATE 57P01\)" >> scripts/ci/logcheck/allowlist-patterns
-    echo >> scripts/ci/logcheck/allowlist-patterns
 
     collect_and_check_stackrox_logs "$log_output_dir" "02_post_bounce-db"
 
@@ -224,9 +226,6 @@ test_upgrade_paths() {
         "compliance=$REGISTRY/main:$CURRENT_TAG"
 
     sensor_wait
-
-    # Bounce collectors to avoid restarts on initial module pull
-    kubectl -n stackrox delete pod -l app=collector --grace-period=0
 
     wait_for_central_reconciliation
 
@@ -318,7 +317,7 @@ deploy_scaled_workload() {
 
     info "Sleep for a bit to let the scale build"
     # shellcheck disable=SC2034
-    for i in $(seq 1 200); do
+    for i in $(seq 1 150); do
         echo -n .
         sleep 5
     done
