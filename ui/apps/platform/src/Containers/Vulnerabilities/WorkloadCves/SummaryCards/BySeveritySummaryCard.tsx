b8a5bc94df9bd54b00d5e40f8ab14579ca7a52be
David Vail <dcvail17@gmail.com>
2023-03-14 16:56:41 -0400

    Add CVE severity and status icons to summary cards. (#5116)

--- ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/SummaryCards/BySeveritySummaryCard.tsx
+++ ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/SummaryCards/BySeveritySummaryCard.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Card, CardBody, CardTitle, Grid, GridItem } from '@patternfly/react-core';
+import { Card, CardBody, CardTitle, Flex, Grid, GridItem, Text } from '@patternfly/react-core';
+
+import SeverityIcons from 'Components/PatternFly/SeverityIcons';
 
 import { VulnerabilitySeverity } from 'types/cve.proto';
 import { vulnerabilitySeverityLabels } from 'messages/common';
@@ -17,23 +19,50 @@ const severitiesCriticalToLow = [
     'LOW_VULNERABILITY_SEVERITY',
 ] as const;
 
+const disabledColor100 = 'var(--pf-global--disabled-color--100)';
+const disabledColor200 = 'var(--pf-global--disabled-color--200)';
+
 function BySeveritySummaryCard({
     title,
     severityCounts,
     hiddenSeverities,
 }: BySeveritySummaryCardProps) {
     return (
-        <Card>
+        <Card isCompact>
             <CardTitle>{title}</CardTitle>
             <CardBody>
-                <Grid hasGutter>
-                    {severitiesCriticalToLow.map((severity) => (
-                        <GridItem key={severity} span={6}>
-                            {hiddenSeverities.has(severity)
-                                ? 'Results hidden'
-                                : `${severityCounts[severity]} ${vulnerabilitySeverityLabels[severity]}`}
-                        </GridItem>
-                    ))}
+                <Grid className="pf-u-pl-sm">
+                    {severitiesCriticalToLow.map((severity) => {
+                        const count = severityCounts[severity];
+                        const hasNoResults = count === 0;
+                        const isHidden = hiddenSeverities.has(severity);
+
+                        let textColor = '';
+                        let text = `${count} ${vulnerabilitySeverityLabels[severity]}`;
+
+                        if (isHidden) {
+                            textColor = disabledColor100;
+                            text = 'Results hidden';
+                        } else if (hasNoResults) {
+                            textColor = disabledColor200;
+                            text = 'No results';
+                        }
+
+                        const Icon = SeverityIcons[severity];
+
+                        return (
+                            <GridItem key={severity} span={6}>
+                                <Flex
+                                    className="pf-u-pt-sm"
+                                    spaceItems={{ default: 'spaceItemsSm' }}
+                                    alignItems={{ default: 'alignItemsCenter' }}
+                                >
+                                    <Icon color={hasNoResults ? textColor : undefined} />
+                                    <Text style={{ color: textColor }}>{text}</Text>
+                                </Flex>
+                            </GridItem>
+                        );
+                    })}
                 </Grid>
             </CardBody>
         </Card>
