70abc84d9c13ec0e76671af00574a0c35c2d5248
lvalerom <lvalerom@redhat.com>
2023-03-15 12:07:01 +0100

    enhance e2e route tests

--- qa-tests-backend/src/test/groovy/RoutesTest.groovy
+++ qa-tests-backend/src/test/groovy/RoutesTest.groovy
@@ -78,4 +78,118 @@ class RoutesTest extends BaseSpecification {
             orchestrator.deleteRoute(SERVER_DEP.name, SERVER_DEP.namespace)
         }
     }
+
+    @Tag("BAT")
+    def "Verify that routes linked to a ClusterIP service are detected correctly"() {
+        boolean resourcesCreated
+
+        given:
+        Assume.assumeTrue(Env.mustGetOrchestratorType() == OrchestratorTypes.OPENSHIFT)
+
+        when:
+        "Create the deployment"
+        SERVER_DEP.setExposeAsService(false)
+        orchestrator.createDeployment(SERVER_DEP)
+        assert SERVER_DEP.deploymentUid
+
+        then:
+        "Fetch deployment, it shouldn't have a route"
+        withRetry(10, 5) {
+            def routes = getRoutes()
+            assert routes.size() == 0
+        }
+
+        when:
+        "Create a service and a route"
+        orchestrator.createService(SERVER_DEP)
+        orchestrator.createRoute(SERVER_DEP.name, SERVER_DEP.namespace)
+        resourcesCreated = true
+
+        then:
+        "Fetch deployment, it should have the route"
+        withRetry(10, 5) {
+            def routes = getRoutes()
+            assert routes.size() == 1
+            assert routes[0].getExternalHostnamesList().size() > 0
+        }
+
+        when:
+        "Delete the service and the route"
+        orchestrator.deleteRoute(SERVER_DEP.name, SERVER_DEP.namespace)
+        orchestrator.deleteService(SERVER_DEP.name, SERVER_DEP.namespace)
+        resourcesCreated = false
+
+        then:
+        "Fetch deployment, it should no longer have the route"
+        withRetry(10, 5) {
+            def routes = getRoutes()
+            assert routes.size() == 0
+        }
+
+        cleanup:
+        SERVER_DEP.setExposeAsService(true)
+        orchestrator.deleteDeployment(SERVER_DEP)
+        if (resourcesCreated) {
+            orchestrator.deleteRoute(SERVER_DEP.name, SERVER_DEP.namespace)
+            orchestrator.deleteService(SERVER_DEP.name, SERVER_DEP.namespace)
+        }
+    }
+
+    @Tag("BAT")
+    def "Verify that routes linked to a LoadBalancer service are detected correctly"() {
+        boolean resourcesCreated
+
+        given:
+        Assume.assumeTrue(Env.mustGetOrchestratorType() == OrchestratorTypes.OPENSHIFT)
+
+        when:
+        "Create the deployment"
+        SERVER_DEP.setExposeAsService(false)
+        orchestrator.createDeployment(SERVER_DEP)
+        assert SERVER_DEP.deploymentUid
+
+        then:
+        "Fetch deployment, it shouldn't have a route"
+        withRetry(10, 5) {
+            def routes = getRoutes()
+            assert routes.size() == 0
+        }
+
+        when:
+        "Create a service and a route"
+        SERVER_DEP.setCreateLoadBalancer(false)
+        orchestrator.createService(SERVER_DEP)
+        orchestrator.createRoute(SERVER_DEP.name, SERVER_DEP.namespace)
+        resourcesCreated = true
+
+        then:
+        "Fetch deployment, it should have the route"
+        withRetry(10, 5) {
+            def routes = getRoutes()
+            assert routes.size() == 1
+            assert routes[0].getExternalHostnamesList().size() > 0
+        }
+
+        when:
+        "Delete the service and the route"
+        orchestrator.deleteRoute(SERVER_DEP.name, SERVER_DEP.namespace)
+        orchestrator.deleteService(SERVER_DEP.name, SERVER_DEP.namespace)
+        resourcesCreated = false
+
+        then:
+        "Fetch deployment, it should no longer have the route"
+        withRetry(10, 5) {
+            def routes = getRoutes()
+            assert routes.size() == 0
+        }
+
+        cleanup:
+        SERVER_DEP.setExposeAsService(true)
+        SERVER_DEP.setCreateLoadBalancer(false)
+        orchestrator.deleteDeployment(SERVER_DEP)
+        if (resourcesCreated) {
+            orchestrator.deleteRoute(SERVER_DEP.name, SERVER_DEP.namespace)
+            orchestrator.deleteService(SERVER_DEP.name, SERVER_DEP.namespace)
+        }
+    }
 }
