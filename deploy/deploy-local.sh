9a9ad383a6ab7797aca0e72eac41e46012c5b541
Connor Gorman <connorgorman@users.noreply.github.com>
2023-03-16 12:52:32 -0700

    Detect in deploy scripts if openshift or k8s is used (#5270)

---
+++ deploy/deploy-local.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+source "${DIR}/detect.sh"
+
+if is_openshift; then
+    "${DIR}/openshift/deploy-local.sh"
+else
+    "${DIR}/k8s/deploy-local.sh"
+fi
