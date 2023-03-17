28149873217b975f1c2c62e204ec1923347ba043
David Vail <dcvail17@gmail.com>
2023-03-17 09:03:35 -0400

    Integrate Image single page table with search filter.

--- ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/searchUtils.tsx
+++ ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/searchUtils.tsx
@@ -3,9 +3,9 @@ import qs from 'qs';
 import { vulnerabilitiesWorkloadCvesPath } from 'routePaths';
 import { SearchFilter } from 'types/search';
 import { getQueryString } from 'utils/queryStringUtils';
-import { ensureExhaustive } from 'utils/type.utils';
+import { ensureExhaustive, ValueOf } from 'utils/type.utils';
 
-import { CveStatusTab, isValidCveStatusTab } from './types';
+import { CveStatusTab, isValidCveStatusTab, QuerySearchFilter } from './types';
 
 export type EntityTab = 'CVE' | 'Image' | 'Deployment';
 
@@ -38,4 +38,71 @@ export function getEntityPagePath(workloadCveEntity: EntityTab, id: string): str
         default:
             return ensureExhaustive(workloadCveEntity);
     }
+}
+
+/**
+ * Coerces a search filter value obtained from the URL into an array of strings.
+ *
+ * Array values will be returned unchanged.
+ * String values will return an array of length one.
+ * undefined values will return an empty array.
+ */
+export function searchValueAsArray(searchValue: ValueOf<SearchFilter>): string[] {
+    if (!searchValue) {
+        return [];
+    }
+    if (Array.isArray(searchValue)) {
+        return searchValue;
+    }
+    return [searchValue];
+}
+
+/**
+ * Parses an open `SearchFilter` obtained from the URL into a restricted `SearchFilter` that
+ * matches the fields and values expected by the backend.
+ */
+export function parseQuerySearchFilter(rawSearchFilter: SearchFilter): QuerySearchFilter {
+    const cleanSearchFilter: QuerySearchFilter = {};
+
+    // SearchFilter values that can be directly translated over to the backend equivalent
+    const unprocessedSearchKeys = ['IMAGE', 'DEPLOYMENT', 'NAMESPACE', 'CLUSTER'] as const;
+    unprocessedSearchKeys.forEach((key) => {
+        if (rawSearchFilter[key]) {
+            cleanSearchFilter[key] = searchValueAsArray(rawSearchFilter[key]);
+        }
+    });
+
+    if (rawSearchFilter.Fixable) {
+        const rawFixable = searchValueAsArray(rawSearchFilter.Fixable);
+        const cleanFixable: ('true' | 'false')[] = [];
+
+        rawFixable.forEach((status) => {
+            if (status === 'Fixable') {
+                cleanFixable.push('true');
+            } else if (status === 'Not fixable') {
+                cleanFixable.push('false');
+            }
+        });
+
+        cleanSearchFilter.Fixable = cleanFixable;
+    }
+
+    if (rawSearchFilter.Severity) {
+        const rawSeverities = searchValueAsArray(rawSearchFilter.Severity);
+        cleanSearchFilter.Severity = [];
+
+        rawSeverities.forEach((rs) => {
+            if (rs === 'Critical') {
+                cleanSearchFilter.Severity?.push('CRITICAL_VULNERABILITY_SEVERITY');
+            } else if (rs === 'Important') {
+                cleanSearchFilter.Severity?.push('IMPORTANT_VULNERABILITY_SEVERITY');
+            } else if (rs === 'Moderate') {
+                cleanSearchFilter.Severity?.push('MODERATE_VULNERABILITY_SEVERITY');
+            } else if (rs === 'Low') {
+                cleanSearchFilter.Severity?.push('LOW_VULNERABILITY_SEVERITY');
+            }
+        });
+    }
+
+    return cleanSearchFilter;
 }
