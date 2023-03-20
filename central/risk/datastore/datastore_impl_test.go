490a45a999bce8356b6d78f50681b1330694c964
Yann Brillouet <ybrillou@redhat.com>
2023-03-20 21:16:39 +0100

    fix build

--- central/risk/datastore/datastore_impl_test.go
+++ central/risk/datastore/datastore_impl_test.go
@@ -16,7 +16,7 @@ import (
 	"github.com/stackrox/rox/pkg/postgres/pgtest"
 	"github.com/stackrox/rox/pkg/postgres/schema"
 	"github.com/stackrox/rox/pkg/sac"
-	"github.com/stackrox/rox/pkg/sca/resources"
+	"github.com/stackrox/rox/pkg/sac/resources"
 	searchPkg "github.com/stackrox/rox/pkg/search"
 	"github.com/stretchr/testify/suite"
 )
