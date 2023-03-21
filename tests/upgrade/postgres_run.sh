ff32e12f36351d775a9cc14d52d0069bd3bef284
Dave Shrewsberry <dashrews@redhat.com>
2023-03-21 06:11:15 -0400

    minor update

--- tests/upgrade/postgres_run.sh
+++ tests/upgrade/postgres_run.sh
@@ -134,7 +134,8 @@ test_upgrade_paths() {
     echo "FATAL: terminating connection due to administrator command \(SQLSTATE 57P01\)" >> /tmp/allowlist-patterns
     echo "Unable to connect to Sensor at" >> /tmp/allowlist-patterns
     echo "No suitable kernel object downloaded for kernel" >> /tmp/allowlist-patterns
-    export ALLOWLIST_FILE="/tmp/allowlist-patterns"
+    # Using ci_export so the post tests have this as well
+    ci_export ALLOWLIST_FILE "/tmp/allowlist-patterns"
 
     collect_and_check_stackrox_logs "$log_output_dir" "00_initial_check"
 
