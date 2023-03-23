de993aacfd3d16cc560f10062b46b22e066eb98d
dhaus67 <dhaus@redhat.com>
2023-03-23 16:56:25 +0100

    ROX-14694: Add deletion to the reconciliation of declarative configuration (#5186)

--- central/declarativeconfig/updater/auth_provider_updater.go
+++ central/declarativeconfig/updater/auth_provider_updater.go
@@ -4,23 +4,44 @@ import (
 	"context"
 
 	"github.com/gogo/protobuf/proto"
+	"github.com/hashicorp/go-multierror"
+	"github.com/pkg/errors"
 	authProviderDatastore "github.com/stackrox/rox/central/authprovider/datastore"
+	"github.com/stackrox/rox/central/declarativeconfig/types"
+	"github.com/stackrox/rox/central/declarativeconfig/utils"
+	groupDataStore "github.com/stackrox/rox/central/group/datastore"
 	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/pkg/auth/authproviders"
 	"github.com/stackrox/rox/pkg/errox"
+	"github.com/stackrox/rox/pkg/integrationhealth"
+	"github.com/stackrox/rox/pkg/logging"
+	"github.com/stackrox/rox/pkg/set"
 )
 
 type authProviderUpdater struct {
 	authProviderDS       authproviders.Store
 	authProviderRegistry authproviders.Registry
+	groupDS              groupDataStore.DataStore
+	reporter             integrationhealth.Reporter
+	idExtractor          types.IDExtractor
+	nameExtractor        types.NameExtractor
 }
 
 var _ ResourceUpdater = (*authProviderUpdater)(nil)
 
-func newAuthProviderUpdater(datastore authproviders.Store, registry authproviders.Registry) ResourceUpdater {
+var (
+	log = logging.LoggerForModule()
+)
+
+func newAuthProviderUpdater(authProvidersDS authproviders.Store, registry authproviders.Registry,
+	groupDS groupDataStore.DataStore, reporter integrationhealth.Reporter) ResourceUpdater {
 	return &authProviderUpdater{
-		authProviderDS:       datastore,
+		authProviderDS:       authProvidersDS,
 		authProviderRegistry: registry,
+		groupDS:              groupDS,
+		reporter:             reporter,
+		idExtractor:          types.UniversalIDExtractor(),
+		nameExtractor:        types.UniversalNameExtractor(),
 	}
 }
 
@@ -38,4 +59,34 @@ func (u *authProviderUpdater) Upsert(ctx context.Context, m proto.Message) error
 		return err
 	}
 	return nil
+}
+
+func (u *authProviderUpdater) DeleteResources(ctx context.Context, resourceIDsToSkip ...string) ([]string, error) {
+	authProviderIDsToSkip := set.NewFrozenStringSet(resourceIDsToSkip...)
+
+	authProviders, err := u.authProviderDS.GetAuthProvidersFiltered(ctx, func(authProvider *storage.AuthProvider) bool {
+		return authProvider.GetTraits().GetOrigin() == storage.Traits_DECLARATIVE &&
+			!authProviderIDsToSkip.Contains(authProvider.GetId())
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "retrieving declarative auth providers")
+	}
+
+	var authProviderDeletionErr *multierror.Error
+	var authProviderIDs []string
+	for _, authProvider := range authProviders {
+		if err := u.authProviderRegistry.DeleteProvider(ctx, authProvider.GetId(), true, true); err != nil {
+			authProviderDeletionErr = multierror.Append(authProviderDeletionErr, err)
+			authProviderIDs = append(authProviderIDs, authProvider.GetId())
+
+			u.reporter.UpdateIntegrationHealthAsync(utils.IntegrationHealthForProtoMessage(authProvider, "", err,
+				u.idExtractor, u.nameExtractor))
+			continue
+		}
+		// TODO(ROX-14700): This currently also deletes imperative groups and should resolve these references instead.
+		if err := u.groupDS.RemoveAllWithAuthProviderID(ctx, authProvider.GetId(), true); err != nil {
+			log.Errorf("Error deleting groups for auth provider id %s: %v", authProvider.GetId(), err)
+		}
+	}
+	return authProviderIDs, authProviderDeletionErr.ErrorOrNil()
 }
