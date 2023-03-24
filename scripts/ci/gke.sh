43f86819bff405e87ad2222ecd0cfcabaa1f0c11
Tomasz Janiszewski <tomek@redhat.com>
2023-03-24 16:23:49 +0100

    ROX-1137: Add support for GHA in GKE deployment (#5363)

--- scripts/ci/gke.sh
+++ scripts/ci/gke.sh
@@ -33,6 +33,9 @@ assign_env_variables() {
     if is_OPENSHIFT_CI; then
         require_environment "BUILD_ID"
         build_num="${BUILD_ID}"
+    elif is_GITHUB_ACTIONS; then
+        require_environment "GITHUB_RUN_ID"
+        build_num="${GITHUB_RUN_ID}"
     else
         die "Support is missing for this CI environment"
     fi
@@ -95,15 +98,23 @@ create_cluster() {
     if is_OPENSHIFT_CI; then
         require_environment "JOB_NAME"
         require_environment "BUILD_ID"
-        tags="${tags},stackrox-ci-${JOB_NAME:0:50}"
-        tags="${tags/%-/x}"
-        labels="${labels},stackrox-ci-job=${JOB_NAME:0:63}"
-        labels="${labels/%-/x}"
-        labels="${labels},stackrox-ci-build-id=${BUILD_ID:0:63}"
-        labels="${labels/%-/x}"
+        build_num="${BUILD_ID}"
+        job_name="${JOB_NAME}"
+    elif is_GITHUB_ACTIONS; then
+        require_environment "GITHUB_JOB"
+        require_environment "GITHUB_RUN_ID"
+        build_num="${GITHUB_RUN_ID}"
+        job_name="${GITHUB_JOB}"
     else
         die "Support is missing for this CI environment"
     fi
+
+    tags="${tags},stackrox-ci-${job_name:0:50}"
+    tags="${tags/%-/x}"
+    labels="${labels},stackrox-ci-job=${job_name:0:63}"
+    labels="${labels/%-/x}"
+    labels="${labels},stackrox-ci-build-id=${build_num:0:63}"
+    labels="${labels/%-/x}"
 
     if is_in_PR_context; then
         labels="${labels},pr=$(get_PR_number)"
