5a19b1d274a0b29d39676b4028ea9602741f6089
Saif Chaudhry <schaudhr@redhat.com>
2023-03-27 13:31:42 -0700

    ROX-15402: Change to a sticky search bar in the breadcrumb

--- ui/apps/platform/cypress/helpers/networkGraphPF.js
+++ ui/apps/platform/cypress/helpers/networkGraphPF.js
@@ -47,10 +47,12 @@ export function selectNamespace(namespace) {
         cy.get(networkGraphSelectors.selector.namespaceSelect).click();
         // Exact match to distinguish stackrox from stackrox-operator namespaces.
         cy.get(
-            `${selectSelectors.patternFlySelect.openMenu} .pf-c-select__menu-item [data-testid="namespace-name"]`
-        )
-            .contains(new RegExp(`^${namespace}$`))
-            .click();
+            `${
+                selectSelectors.patternFlySelect.openMenu
+            } .pf-c-menu__list-item [data-testid="namespace-name"]:contains(${new RegExp(
+                `^${namespace}$`
+            )})`
+        ).click();
         cy.get(networkGraphSelectors.selector.namespaceSelect).click();
     }, routeMatcherMapForClusterInNetworkGraph);
 }
@@ -59,10 +61,12 @@ export function selectDeployment(deployment) {
     interactAndWaitForResponses(() => {
         cy.get(networkGraphSelectors.selector.deploymentSelect).click();
         cy.get(
-            `${selectSelectors.patternFlySelect.openMenu} .pf-c-select__menu-item [data-testid="deployment-name"]`
-        )
-            .contains(new RegExp(`^${deployment}$`))
-            .click();
+            `${
+                selectSelectors.patternFlySelect.openMenu
+            } .pf-c-menu__list-item [data-testid="deployment-name"]:contains(${new RegExp(
+                `^${deployment}$`
+            )})`
+        ).click();
         cy.get(networkGraphSelectors.selector.deploymentSelect).click();
     }, routeMatcherMapForClusterInNetworkGraph);
 }
