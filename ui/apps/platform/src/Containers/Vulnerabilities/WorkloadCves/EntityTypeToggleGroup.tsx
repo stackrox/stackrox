cdd8507c37dd4b6cc9f8ffed782974e430df8f51
David Vail <dcvail17@gmail.com>
2023-03-29 10:39:25 -0400

    Integrate ImageCVE page with EntityTypeToggleGroup

--- ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/EntityTypeToggleGroup.tsx
+++ ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/EntityTypeToggleGroup.tsx
@@ -2,51 +2,57 @@ import React from 'react';
 import { ToggleGroup, ToggleGroupItem } from '@patternfly/react-core';
 
 import useURLStringUnion from 'hooks/useURLStringUnion';
+import { NonEmptyArray } from 'utils/type.utils';
 import { entityTabValues, EntityTab } from './types';
 
 type EntityTabToggleGroupProps = {
-    entityContext?: EntityTab;
+    className?: string;
+    entityTabs?: Readonly<NonEmptyArray<EntityTab>>;
     cveCount?: number;
     imageCount?: number;
     deploymentCount?: number;
 };
 
 function EntityTabToggleGroup({
-    entityContext,
+    className = '',
+    entityTabs = entityTabValues,
     cveCount = 0,
     imageCount = 0,
     deploymentCount = 0,
 }: EntityTabToggleGroupProps) {
-    const [activeEntityTabKey, setActiveEntityTabKey] = useURLStringUnion(
-        'entityTab',
-        entityTabValues
-    );
+    const [activeEntityTabKey, setActiveEntityTabKey] = useURLStringUnion('entityTab', entityTabs);
 
     return (
-        <ToggleGroup className="pf-u-pl-md pf-u-pt-md">
-            {entityContext !== 'CVE' && (
+        <ToggleGroup className={className}>
+            {entityTabs.includes('CVE') ? (
                 <ToggleGroupItem
                     text={`${cveCount} CVEs`}
                     buttonId="cves"
                     isSelected={activeEntityTabKey === 'CVE'}
                     onChange={() => setActiveEntityTabKey('CVE')}
                 />
+            ) : (
+                <></>
             )}
-            {entityContext !== 'Image' && (
+            {entityTabs.includes('Image') ? (
                 <ToggleGroupItem
                     text={`${imageCount} Images`}
                     buttonId="images"
                     isSelected={activeEntityTabKey === 'Image'}
                     onChange={() => setActiveEntityTabKey('Image')}
                 />
+            ) : (
+                <></>
             )}
-            {entityContext !== 'Deployment' && (
+            {entityTabs.includes('Deployment') ? (
                 <ToggleGroupItem
                     text={`${deploymentCount} Deployments`}
                     buttonId="deployments"
                     isSelected={activeEntityTabKey === 'Deployment'}
                     onChange={() => setActiveEntityTabKey('Deployment')}
                 />
+            ) : (
+                <></>
             )}
         </ToggleGroup>
     );
