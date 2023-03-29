b88536b9a5d92766a2e3eec53c6f9357c97f67db
David Shrewsberry <99685630+dashrews78@users.noreply.github.com>
2023-03-29 14:15:57 -0400

    Dashrews/upgrade test use non-expiring image for initial Postgres upgrade (#5398)
    use un-expiring image for initial postgres upgrade.

--- tests/upgrade/postgres_run.sh
+++ tests/upgrade/postgres_run.sh
@@ -7,13 +7,10 @@ set -euo pipefail
 
 TEST_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../.. && pwd)"
 
-# Build 3.73.x-608-g4ffbc83042 was chosen as it contains the fixes for issues that
-# caused corruption of RocksDB as well as the removal of destructive acts that would
-# delete RocksDB data and Postgres data based on conditions.  Additionally it contains
-# policy category Postgres changes that are required for the Upgrade tests to succeed
-# in Postgres mode.
-INITIAL_POSTGRES_TAG="3.73.x-608-g4ffbc83042"
-INITIAL_POSTGRES_SHA="4ffbc83042614f9fe4524cbf140323f3372ee6a7"
+# Build 3.74.0-1-gfe924fce30 was chosen as it is the first 3.74 build and
+# also not set to expire
+INITIAL_POSTGRES_TAG="3.74.0-1-gfe924fce30"
+INITIAL_POSTGRES_SHA="fe924fce30bbec4dbd37d731ccd505837a2c2575"
 CURRENT_TAG="$(make --quiet tag)"
 
 source "$TEST_ROOT/scripts/lib.sh"
@@ -289,6 +286,8 @@ helm_upgrade_to_postgres() {
       --set central.db.password.generate=true \
       --set central.db.serviceTLS.generate=true \
       --set central.db.persistence.persistentVolumeClaim.createClaim=true \
+      --set central.image.tag="${INITIAL_POSTGRES_TAG}" \
+      --set central.db.image.tag="${INITIAL_POSTGRES_TAG}" \
       -f "$TEST_ROOT/tests/upgrade/scale-values-public.yaml" \
       -f "$root_certificate_path" \
       --force
