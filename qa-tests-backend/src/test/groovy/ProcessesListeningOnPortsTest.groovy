8a31f228b21924feefbb6db0cb737674b1b8fe81
JoukoVirtanen <jouko@stackrox.com>
2023-03-29 20:00:28 -0700

    X-Smart-Squash: Squashed 27 commits:
    055b544854 Added test where process is killed and deployment stays up
    3e325e81f0 CloseTimestamp is sent to central
    ee5159c548 Fixed style error
    0abf705a51 Changed timing of test and added new test to confirm that active endpoints don't disappear
    adc77f638c Checking if endpoint has been used for networkflow and plop before deleting. Setting timestamp to current if previous exists, current doesn't, and timestamp is InfiniteFuture
    a881d75d6a Fixed typo
    7f06d174ba Treating timestamp.InfiniteFuture as 0
    79d117440f Fixed error
    fc52099748 Fixed error in e2e test
    4437b2e844 Fixed another typo
    e6b17e4802 Fixed typo
    caa781bfd3 Added unit tests for manager_impl.go. Removed changes to manager_impl.go
    c16c3314bc Unit tests compile
    6bbaf5b86b Fixed style error
    2c5289a11f gofmt -s manager_impl_test.go
    b05e991ede Fixed port number
    7839a3d529 Added comments
    19290be5f6 Added test that adds two different endpoints. Added test that adds endpoint with no process originator
    a0458490e5 Added test that adds two endpoints at once
    08c9e5efb3 Fixed style error
    d545dcf068 Creating and destroying the deployments for each test
    141286cade waitForResponseToHaveNumElements returns the response itself, not just a boolean. Remove rebuildForReties
    f58ee8f602 Jv rox 16135 process listening on ports close timestamp should be sent correctly to central (#5358)
    f74a2426db Removed unused import
    cc7ffab9fa Destroying deployments in setup
    3a91da3a86 Passing ts to toProto again
    955792d601 Not using indexes to get deployment ids

--- qa-tests-backend/src/test/groovy/ProcessesListeningOnPortsTest.groovy
+++ qa-tests-backend/src/test/groovy/ProcessesListeningOnPortsTest.groovy
@@ -4,7 +4,6 @@ import objects.Deployment
 import objects.K8sServiceAccount
 import objects.Service
 import util.Env
-import util.Helpers
 
 import spock.lang.IgnoreIf
 import spock.lang.Shared
@@ -21,6 +20,7 @@ class ProcessesListeningOnPortsTest extends BaseSpecification {
     // Deployment names
     static final private String TCPCONNECTIONTARGET1 = "tcp-connection-target-1"
     static final private String TCPCONNECTIONTARGET2 = "tcp-connection-target-2"
+    static final private String TCPCONNECTIONTARGET3 = "tcp-connection-target-3"
 
     // Other namespace
     static final private String OTHER_NAMESPACE = "qa2"
@@ -51,6 +51,16 @@ class ProcessesListeningOnPortsTest extends BaseSpecification {
                     .setExposeAsService(true)
                     .setCommand(["/bin/sh", "-c",])
                     .setArgs(["(socat "+SOCAT_DEBUG+" TCP-LISTEN:8081,fork STDOUT)" as String,]),
+            new Deployment()
+                    .setName(TCPCONNECTIONTARGET3)
+                    .setImage("quay.io/rhacs-eng/qa:socat")
+                    .addPort(8082, "TCP")
+                    .addLabel("app", TCPCONNECTIONTARGET3)
+                    .setExposeAsService(true)
+                    .setCommand(["/bin/sh", "-c",])
+                    .setArgs(["(socat "+SOCAT_DEBUG+" TCP-LISTEN:8082,fork STDOUT & " +
+                            "sleep 90 && pkill socat && sleep 3600)" as String,]),
+                    // The 8082 port is opened. 90 seconds later the process is killed. After that we sleep forever
         ]
     }
 
@@ -79,6 +89,7 @@ class ProcessesListeningOnPortsTest extends BaseSpecification {
                 )
         )
 
+        destroyDeployments()
         createDeployments()
     }
 
@@ -99,34 +110,17 @@ class ProcessesListeningOnPortsTest extends BaseSpecification {
         destroyDeployments()
     }
 
-    def rebuildForRetries() {
-        if (Helpers.getAttemptCount() > 1) {
-            log.info ">>>> Recreating test deployments prior to retest <<<<<"
-            destroyDeployments()
-            createDeployments()
-            log.info ">>>> Done <<<<<"
-        }
-    }
-
     @Tag("BAT")
     def "Verify networking endpoints with processes appear in API at the deployment level"() {
         given:
         "Two deployments that listen on ports are started up"
 
-        rebuildForRetries()
+        setupSpec()
 
-        String deploymentId1 = targetDeployments[0].getDeploymentUid()
-        String deploymentId2 = targetDeployments[1].getDeploymentUid()
+        String deploymentId1 = targetDeployments.find { it.name == TCPCONNECTIONTARGET1 }?.deploymentUid
+        String deploymentId2 = targetDeployments.find { it.name == TCPCONNECTIONTARGET2 }?.deploymentUid
 
-        def gotCorrectNumElements = waitForResponseToHaveNumElements(2, deploymentId1, 240)
-
-        assert gotCorrectNumElements
-
-        def processesListeningOnPorts = evaluateWithRetry(10, 10) {
-                def temp = ProcessesListeningOnPortsService
-                        .getProcessesListeningOnPortsResponse(deploymentId1)
-                return temp
-        }
+        def processesListeningOnPorts = waitForResponseToHaveNumElements(2, deploymentId1, 240)
 
         assert processesListeningOnPorts
 
@@ -153,15 +147,7 @@ class ProcessesListeningOnPortsTest extends BaseSpecification {
         assert endpoint2.signal.execFilePath == "/usr/bin/socat"
         assert endpoint2.signal.args == "-d -d -v TCP-LISTEN:8080,fork STDOUT"
 
-        gotCorrectNumElements = waitForResponseToHaveNumElements(1, deploymentId2, 240)
-
-        assert gotCorrectNumElements
-
-        processesListeningOnPorts = evaluateWithRetry(10, 10) {
-                def temp = ProcessesListeningOnPortsService
-                        .getProcessesListeningOnPortsResponse(deploymentId2)
-                return temp
-        }
+        processesListeningOnPorts = waitForResponseToHaveNumElements(1, deploymentId2, 240)
 
         assert processesListeningOnPorts
 
@@ -180,35 +166,98 @@ class ProcessesListeningOnPortsTest extends BaseSpecification {
 
         destroyDeployments()
 
-        gotCorrectNumElements = waitForResponseToHaveNumElements(0, deploymentId1, 240)
-
-        assert gotCorrectNumElements
-
-        processesListeningOnPorts = evaluateWithRetry(10, 10) {
-                def temp = ProcessesListeningOnPortsService
-                        .getProcessesListeningOnPortsResponse(deploymentId1)
-                return temp
-        }
+        processesListeningOnPorts = waitForResponseToHaveNumElements(0, deploymentId1, 240)
 
         assert processesListeningOnPorts
 
         def list2 = processesListeningOnPorts.listeningEndpointsList
         assert list2.size() == 0
 
-        gotCorrectNumElements = waitForResponseToHaveNumElements(0, deploymentId2, 240)
-
-        assert gotCorrectNumElements
-
-        processesListeningOnPorts = evaluateWithRetry(10, 10) {
-                def temp = ProcessesListeningOnPortsService
-                        .getProcessesListeningOnPortsResponse(deploymentId2)
-                return temp
-        }
+        processesListeningOnPorts = waitForResponseToHaveNumElements(0, deploymentId2, 240)
 
         assert processesListeningOnPorts
 
         def list3 = processesListeningOnPorts.listeningEndpointsList
         assert list3.size() == 0
+
+        destroyDeployments()
+    }
+
+    @Tag("BAT")
+    def "Verify networking endpoints disappear when process is terminated"() {
+        given:
+        "When a deployment listening on a port is created and then the process is terminated"
+
+        setupSpec()
+
+        String deploymentId3 = targetDeployments.find { it.name == TCPCONNECTIONTARGET3 }?.deploymentUid
+
+        def processesListeningOnPorts = waitForResponseToHaveNumElements(1, deploymentId3, 240)
+
+        // First check that the listening endpoint appears in the API
+        assert processesListeningOnPorts
+
+        def list = processesListeningOnPorts.listeningEndpointsList
+        assert list.size() == 1
+
+        def endpoint = list.find { it.endpoint.port == 8082 }
+
+        assert endpoint
+        assert endpoint.deploymentId
+        assert endpoint.podId
+        assert endpoint.containerName == TCPCONNECTIONTARGET3
+        assert endpoint.signal.name == "socat"
+        assert endpoint.signal.execFilePath == "/usr/bin/socat"
+        assert endpoint.signal.args == "-d -d -v TCP-LISTEN:8082,fork STDOUT"
+
+        processesListeningOnPorts = waitForResponseToHaveNumElements(0, deploymentId3, 180)
+
+        // Allow enough time for the process and port to close and check that it is not in the API response
+        assert processesListeningOnPorts
+
+        destroyDeployments()
+    }
+
+    @Tag("BAT")
+    def "Verify networking endpoint doesn't disappear when port stays open"() {
+        given:
+        "A deployment listening on a port is brought up and it is checked twice that the port is found"
+
+        setupSpec()
+        def clusterId = ClusterService.getClusterId()
+
+        String deploymentId2 = targetDeployments.find { it.name == TCPCONNECTIONTARGET2 }?.deploymentUid
+
+        def processesListeningOnPorts = waitForResponseToHaveNumElements(1, deploymentId2, 240)
+
+        // First check that the listening endpoint appears in the API
+        assert processesListeningOnPorts
+
+        def list = processesListeningOnPorts.listeningEndpointsList
+        assert list.size() == 1
+
+        def endpoint = list.find { it.endpoint.port == 8081 }
+
+        assert endpoint
+        assert endpoint.deploymentId
+        assert endpoint.podId
+        assert endpoint.containerName == TCPCONNECTIONTARGET2
+        assert endpoint.signal.name == "socat"
+        assert endpoint.signal.execFilePath == "/usr/bin/socat"
+        assert endpoint.signal.args == "-d -d -v TCP-LISTEN:8081,fork STDOUT"
+
+        sleep 65000 // Sleep for 65 seconds
+        processesListeningOnPorts = evaluateWithRetry(10, 10) {
+               def temp = ProcessesListeningOnPortsService
+                       .getProcessesListeningOnPortsResponse(deploymentId2)
+               return temp
+        }
+
+        // Confirm that the listening endpoint still appears in the API 65 seconds later
+        list = processesListeningOnPorts.listeningEndpointsList
+        assert list.size() == 1
+
+        destroyDeployments()
     }
 
     private waitForResponseToHaveNumElements(int numElements,
@@ -216,6 +265,7 @@ class ProcessesListeningOnPortsTest extends BaseSpecification {
 
         int intervalSeconds = 1
         int waitTime
+
         for (waitTime = 0; waitTime <= timeoutSeconds / intervalSeconds; waitTime++) {
             def processesListeningOnPorts = evaluateWithRetry(10, 10) {
                     def temp = ProcessesListeningOnPortsService
@@ -226,11 +276,11 @@ class ProcessesListeningOnPortsTest extends BaseSpecification {
             def list = processesListeningOnPorts.listeningEndpointsList
 
             if (list.size() == numElements) {
-                return true
+                return processesListeningOnPorts
             }
             sleep intervalSeconds * 1000
         }
         log.info "Timedout waiting for response to have {$numElements} elements"
-        return false
+        return null
     }
 }
