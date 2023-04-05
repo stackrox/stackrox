0c52950fd655cf0e3ec81aecd2c6dc480bd8b555
Dmitrii Dolgov <9erthalion6@gmail.com>
2023-04-05 19:07:39 +0200

    Disable PLOP E2E tests if the feature is off (#5577)
    Disable PLOP E2E tests if the feature is off.
    Remove obsolete ignore from postgres feature flag.
    Put PLOP env variable  behind BUILD_TAG.
    
    Co-authored-by: Gavin Jefferies <gavin@stackrox.com>

--- tests/e2e/lib.sh
+++ tests/e2e/lib.sh
@@ -96,9 +96,10 @@ export_test_environment() {
     ci_export ROX_SYSTEM_HEALTH_PF "${ROX_SYSTEM_HEALTH_PF:-true}"
     ci_export ROX_SYSLOG_EXTRA_FIELDS "${ROX_SYSLOG_EXTRA_FIELDS:-true}"
     ci_export ROX_VULN_MGMT_WORKLOAD_CVES "${ROX_VULN_MGMT_WORKLOAD_CVES:-true}"
-    ci_export ROX_PROCESSES_LISTENING_ON_PORT "${ROX_PROCESSES_LISTENING_ON_PORT:-true}"
 
     if [[ -z "${BUILD_TAG:-}" ]]; then
+        ci_export ROX_PROCESSES_LISTENING_ON_PORT "${ROX_PROCESSES_LISTENING_ON_PORT:-true}"
+
         # TODO(ROX-16008): Remove this once the declarative config feature flag is enabled by default.
         ci_export ROX_DECLARATIVE_CONFIGURATION "${ROX_DECLARATIVE_CONFIGURATION:-true}"
     fi
