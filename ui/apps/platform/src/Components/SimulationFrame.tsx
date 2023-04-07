38fbdcdbb5fe0c1ff87db7bf49c8687d49e1e846
Mark Pedrotti <mark@stackrox.com>
2023-04-07 15:25:31 -0400

    Simplify theme for SimulationFrame in Network Graph 1.0

--- ui/apps/platform/src/Components/SimulationFrame.tsx
+++ ui/apps/platform/src/Components/SimulationFrame.tsx
@@ -1,55 +1,40 @@
 import React, { ReactNode, ReactElement } from 'react';
 import { StopCircle } from 'react-feather';
 
-type StopSimulationButtonProps = {
-    isError?: boolean;
-    onStop: () => void;
-};
-
-function StopSimulationButton({ isError, onStop }: StopSimulationButtonProps): ReactElement {
-    const colorType = isError ? 'alert' : 'success';
-    return (
-        <button
-            type="button"
-            className={`flex items-center bg-${colorType}-700 text-base-100 font-600 uppercase p-2 mt-1 hover:bg-${colorType}-800`}
-            onClick={onStop}
-        >
-            <StopCircle className={`mr-2 w-4 h-4 text-${colorType}-100`} />
-            Stop
-        </button>
-    );
-}
-
 type SimulationLabelProps = {
-    isError?: boolean;
+    isError: boolean;
     onStop: () => void;
 };
 
 function SimulationLabel({ isError, onStop }: SimulationLabelProps): ReactElement | null {
-    const colorType = isError ? 'alert' : 'success';
+    // Note: border-4 corresponds to box shadow width in app.tw.css file.
+    const borderClass = isError ? 'border-alert-700' : 'border-success-700';
+    const bgClass = isError ? 'bg-alert-700' : 'bg-success-700';
     return (
-        <div className="absolute top-0 left-0 z-1 flex">
-            <div className={`bg-${colorType}-600 text-base-100 font-600 uppercase p-2 ml-1 mt-1`}>
-                Simulated View
-            </div>
-            <StopSimulationButton isError={isError} onStop={onStop} />
+        <div className={`absolute top-0 left-0 z-1 flex border-4 ${borderClass}`}>
+            <div className={`${bgClass} text-base-100 font-600 uppercase p-2`}>Simulated View</div>
+            <button
+                type="button"
+                className="flex items-center bg-base-100 hover:bg-base-200 text-base-600 font-600 uppercase p-2"
+                onClick={onStop}
+            >
+                <StopCircle className="mr-2 w-4 h-4" />
+                Stop
+            </button>
         </div>
     );
 }
 
 export type SimulationFrameProps = {
     children: ReactNode;
-    isError?: boolean;
+    isError: boolean;
     onStop: () => void;
 };
 
 function SimulationFrame({ children, isError, onStop }: SimulationFrameProps): ReactElement {
+    const boxShadowClass = isError ? 'before:text-alert-700' : 'before:text-success-700';
     return (
-        <div
-            className={`flex flex-1 flex-col relative simulator-mode ${
-                isError ? 'error' : 'success'
-            }`}
-        >
+        <div className={`flex flex-1 flex-col relative simulator-mode ${boxShadowClass}`}>
             <SimulationLabel isError={isError} onStop={onStop} />
             {children}
         </div>
