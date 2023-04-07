d9a75e833a233e9e8c023d105896fa6eaf024385
Connor Gorman <cgorman@redhat.com>
2023-04-07 09:17:07 -0700

    just remove

--- pkg/postgres/metrics.go
+++ pkg/postgres/metrics.go
@@ -1,8 +1,6 @@
 package postgres
 
 import (
-	"time"
-
 	"github.com/jackc/pgx/v4"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/stackrox/rox/pkg/metrics"
@@ -10,20 +8,11 @@ import (
 
 func init() {
 	prometheus.MustRegister(
-		//queryDuration,
 		queryErrors,
 	)
 }
 
 var (
-	//queryDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-	//	Namespace: metrics.PrometheusNamespace,
-	//	Subsystem: metrics.CentralSubsystem.String(),
-	//	Name:      "postgres_query_duration",
-	//	Help:      "Time in ms for a query to execute",
-	//	Buckets:   prometheus.ExponentialBuckets(4, 2, 16),
-	//}, []string{"scope", "query"})
-
 	queryErrors = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Namespace: metrics.PrometheusNamespace,
 		Subsystem: metrics.CentralSubsystem.String(),
@@ -31,20 +20,6 @@ var (
 		Help:      "Counter of errors occurring Postgres",
 	}, []string{"query", "error"})
 )
-
-func setQueryDuration(t time.Time, scope, query string) {
-	// Currently disable this metric as it is too noisy for dynamic queries
-	/*
-		if strings.HasPrefix(query, "FETCH") {
-			query = stringutils.GetUpTo(query, "_")
-		}
-		query = strings.ReplaceAll(query, "\n", " ")
-
-		logSlowQuery(t, query)
-
-		queryDuration.With(prometheus.Labels{"scope": scope, "query": query}).Observe(float64(time.Since(t).Milliseconds()))
-	*/
-}
 
 func incQueryErrors(query string, err error) {
 	if err == pgx.ErrNoRows {
