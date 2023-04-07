7d340ba5a43b23b137371f2000309abe4d710f6f
Connor Gorman <cgorman@redhat.com>
2023-04-07 08:00:07 -0700

    Disable query duration metric due to high cardinality

--- pkg/postgres/metrics.go
+++ pkg/postgres/metrics.go
@@ -1,30 +1,28 @@
 package postgres
 
 import (
-	"strings"
 	"time"
 
 	"github.com/jackc/pgx/v4"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/stackrox/rox/pkg/metrics"
-	"github.com/stackrox/rox/pkg/stringutils"
 )
 
 func init() {
 	prometheus.MustRegister(
-		queryDuration,
+		//queryDuration,
 		queryErrors,
 	)
 }
 
 var (
-	queryDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Namespace: metrics.PrometheusNamespace,
-		Subsystem: metrics.CentralSubsystem.String(),
-		Name:      "postgres_query_duration",
-		Help:      "Time in ms for a query to execute",
-		Buckets:   prometheus.ExponentialBuckets(4, 2, 16),
-	}, []string{"scope", "query"})
+	//queryDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+	//	Namespace: metrics.PrometheusNamespace,
+	//	Subsystem: metrics.CentralSubsystem.String(),
+	//	Name:      "postgres_query_duration",
+	//	Help:      "Time in ms for a query to execute",
+	//	Buckets:   prometheus.ExponentialBuckets(4, 2, 16),
+	//}, []string{"scope", "query"})
 
 	queryErrors = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Namespace: metrics.PrometheusNamespace,
@@ -35,14 +33,17 @@ var (
 )
 
 func setQueryDuration(t time.Time, scope, query string) {
-	if strings.HasPrefix(query, "FETCH") {
-		query = stringutils.GetUpTo(query, "_")
-	}
-	query = strings.ReplaceAll(query, "\n", " ")
+	// Currently disable this metric as it is too noisy for dynamic queries
+	/*
+		if strings.HasPrefix(query, "FETCH") {
+			query = stringutils.GetUpTo(query, "_")
+		}
+		query = strings.ReplaceAll(query, "\n", " ")
 
-	logSlowQuery(t, query)
+		logSlowQuery(t, query)
 
-	queryDuration.With(prometheus.Labels{"scope": scope, "query": query}).Observe(float64(time.Since(t).Milliseconds()))
+		queryDuration.With(prometheus.Labels{"scope": scope, "query": query}).Observe(float64(time.Since(t).Milliseconds()))
+	*/
 }
 
 func incQueryErrors(query string, err error) {
