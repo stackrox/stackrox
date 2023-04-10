3755cd885ea78a5cd1919bc95bfa2f97e1640457
Connor Gorman <connorgorman@users.noreply.github.com>
2023-04-10 08:01:12 -0700

    Disable query duration metric due to high cardinality (#5610)

--- pkg/postgres/metrics.go
+++ pkg/postgres/metrics.go
@@ -1,31 +1,18 @@
 package postgres
 
 import (
-	"strings"
-	"time"
-
 	"github.com/jackc/pgx/v4"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/stackrox/rox/pkg/metrics"
-	"github.com/stackrox/rox/pkg/stringutils"
 )
 
 func init() {
 	prometheus.MustRegister(
-		queryDuration,
 		queryErrors,
 	)
 }
 
 var (
-	queryDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Namespace: metrics.PrometheusNamespace,
-		Subsystem: metrics.CentralSubsystem.String(),
-		Name:      "postgres_query_duration",
-		Help:      "Time in ms for a query to execute",
-		Buckets:   prometheus.ExponentialBuckets(4, 2, 16),
-	}, []string{"scope", "query"})
-
 	queryErrors = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Namespace: metrics.PrometheusNamespace,
 		Subsystem: metrics.CentralSubsystem.String(),
@@ -33,17 +20,6 @@ var (
 		Help:      "Counter of errors occurring Postgres",
 	}, []string{"query", "error"})
 )
-
-func setQueryDuration(t time.Time, scope, query string) {
-	if strings.HasPrefix(query, "FETCH") {
-		query = stringutils.GetUpTo(query, "_")
-	}
-	query = strings.ReplaceAll(query, "\n", " ")
-
-	logSlowQuery(t, query)
-
-	queryDuration.With(prometheus.Labels{"scope": scope, "query": query}).Observe(float64(time.Since(t).Milliseconds()))
-}
 
 func incQueryErrors(query string, err error) {
 	if err == pgx.ErrNoRows {
