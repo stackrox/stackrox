44b2a5a5a2ce3b824c0304f52a056b79ed4cbfe0
Gavin Jefferies <gavin@stackrox.com>
2023-04-12 10:04:54 +0100

    reviewer feedback

--- operator/tests/run.sh
+++ operator/tests/run.sh
@@ -11,7 +11,7 @@ source "$ROOT/scripts/lib.sh"
 source "$ROOT/scripts/ci/lib.sh"
 
 test_operator_e2e() {
-    info "Starting oerator e2e tests"
+    info "Starting operator e2e tests"
 
     require_environment "KUBECONFIG"
 
@@ -20,7 +20,7 @@ See log and/or kuttl JUnit output for error details.
 Reading operator/tests/TROUBLESHOOTING_E2E_TESTS.md may also be helpful.
 _EO_KUTTL_HELP_
 
-    rm -f FAIL
+    local FAILED=0
 
     info "Fetching kuttl binary"
     junit_wrap fetch-kuttl \
@@ -31,24 +31,24 @@ _EO_KUTTL_HELP_
     info "Deploying operator"
     junit_wrap deploy-previous-operator \
                "Deploy previously released version of the operator." \
-               "See log for error details. Reading operator/tests/TROUBLESHOOTING_E2E_TESTS.md may also be helpful." \
+               "${kuttl_help}" \
                "make" "-C" "operator" "deploy-previous-via-olm"
 
     info "Executing operator upgrade test"
     junit_wrap test-upgrade \
                "Test operator upgrade from previously released version to the current one." \
                "${kuttl_help}" \
-               "make" "-C" "operator" "test-upgrade" || touch FAIL
+               "make" "-C" "operator" "test-upgrade" || FAILED=1
     store_test_results "operator/build/kuttl-test-artifacts-upgrade" "kuttl-test-artifacts-upgrade"
-    [[ ! -f FAIL ]] || die "operator upgrade tests failed"
+    [[ $FAILED = 0 ]] || die "operator upgrade tests failed"
 
     info "Executing operator e2e tests"
     junit_wrap test-e2e \
                "Run operator E2E tests." \
                "${kuttl_help}" \
-               "make" "-C" "operator" "test-e2e-deployed" || touch FAIL
+               "make" "-C" "operator" "test-e2e-deployed" || FAILED=1
     store_test_results "operator/build/kuttl-test-artifacts" "kuttl-test-artifacts"
-    [[ ! -f FAIL ]] || die "operator e2e tests failed"
+    [[ $FAILED = 0 ]] || die "operator e2e tests failed"
 
     info "Executing Operator Bundle Scorecard tests"
     junit_wrap bundle-test-image \
