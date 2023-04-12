4aa5a75be9c4d83e977240bcab3e4c6ba4188703
Linda Song <ls@stackrox.com>
2023-04-12 16:07:50 -0700

    improving the cve severity counts label component

--- ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/components/SeverityCountLabels.tsx
+++ ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/components/SeverityCountLabels.tsx
@@ -3,6 +3,8 @@ import { Flex, Label } from '@patternfly/react-core';
 
 import SeverityIcons from 'Components/PatternFly/SeverityIcons';
 
+const disabledIconColor = '#d2d2d2';
+
 type SeverityCountLabelsProps = {
     critical: number;
     important: number;
@@ -18,17 +20,33 @@ function SeverityCountLabels({ critical, important, moderate, low }: SeverityCou
 
     return (
         <Flex spaceItems={{ default: 'spaceItemsSm' }}>
-            <Label variant="outline" icon={<CriticalIcon />}>
-                {critical}
+            <Label
+                variant="outline"
+                className="pf-u-font-weight-bold"
+                icon={<CriticalIcon color={critical ? undefined : disabledIconColor} />}
+            >
+                <span className={critical ? '' : 'pf-u-color-400'}>{critical}</span>
             </Label>
-            <Label variant="outline" icon={<ImportantIcon />}>
-                {important}
+            <Label
+                variant="outline"
+                className="pf-u-font-weight-bold"
+                icon={<ImportantIcon color={important ? undefined : disabledIconColor} />}
+            >
+                <span className={important ? '' : 'pf-u-color-400'}>{important}</span>
             </Label>
-            <Label variant="outline" icon={<ModerateIcon />}>
-                {moderate}
+            <Label
+                variant="outline"
+                className="pf-u-font-weight-bold"
+                icon={<ModerateIcon color={moderate ? undefined : disabledIconColor} />}
+            >
+                <span className={moderate ? '' : 'pf-u-color-400'}>{moderate}</span>
             </Label>
-            <Label variant="outline" icon={<LowIcon />}>
-                {low}
+            <Label
+                variant="outline"
+                className="pf-u-font-weight-bold"
+                icon={<LowIcon color={low ? undefined : disabledIconColor} />}
+            >
+                <span className={low ? '' : 'pf-u-color-400'}>{low}</span>
             </Label>
         </Flex>
     );
