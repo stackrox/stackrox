92b2a4f706668632edbee520b7106575c877c936
Gavin Jefferies <gavin@stackrox.com>
2023-04-12 18:07:04 +0100

    ROX-12348, CI: Refactor operator e2e tests (#5611)

--- .openshift-ci/ci_tests.py
+++ .openshift-ci/ci_tests.py
@@ -12,7 +12,6 @@ from common import popen_graceful_kill
 class BaseTest:
     def __init__(self):
         self.test_outputs = []
-        self.test_results = {}
 
     def run_with_graceful_kill(self, args, timeout, post_start_hook=None):
         with subprocess.Popen(args) as cmd:
@@ -86,75 +85,14 @@ class PostgresUpgradeTest(BaseTest):
 
 
 class OperatorE2eTest(BaseTest):
-    # TODO(ROX-12348): adjust these timeouts once we know average run times
-    FETCH_KUTTL_TIMEOUT_SEC = 5 * 60
-    DEPLOY_TIMEOUT_SEC = 40 * 60
-    UPGRADE_TEST_TIMEOUT_SEC = 50 * 60
-    E2E_TEST_TIMEOUT_SEC = 50 * 60
-    SCORECARD_TEST_TIMEOUT_SEC = 20 * 60
-
-    def __init__(self):
-        self.test_outputs = [
-            "operator/build/kuttl-test-artifacts",
-            "operator/build/kuttl-test-artifacts-upgrade",
-        ]
-        self.test_results = {
-            "kuttl-test-artifacts": "operator/build/kuttl-test-artifacts",
-            "kuttl-test-artifacts-upgrade": "operator/build/kuttl-test-artifacts-upgrade",
-        }
+    TEST_TIMEOUT = 60 * 60 * 2
 
     def run(self):
-        print("Fetching kuttl binary")
-        self.run_with_graceful_kill(
-            ["operator/hack/junit_wrap.sh", "fetch-kuttl",
-             "Download kuttl binary.", "See log for error details.",
-             "make", "-C", "operator", "kuttl"],
-            OperatorE2eTest.FETCH_KUTTL_TIMEOUT_SEC,
-        )
-        print("Deploying operator")
-        self.run_with_graceful_kill(
-            ["operator/hack/junit_wrap.sh", "deploy-previous-operator",
-             "Deploy previously released version of the operator.",
-             "See log for error details. Reading operator/tests/TROUBLESHOOTING_E2E_TESTS.md may also be helpful.",
-             "make", "-C", "operator", "deploy-previous-via-olm"],
-            OperatorE2eTest.DEPLOY_TIMEOUT_SEC,
-        )
-
-        print("Executing operator upgrade test")
-        self.run_with_graceful_kill(
-            ["operator/hack/junit_wrap.sh", "test-upgrade",
-             "Test operator upgrade from previously released version to the current one.",
-             "See log and/or kuttl JUnit output for error details. "
-             "Reading operator/tests/TROUBLESHOOTING_E2E_TESTS.md may also be helpful.",
-             "make", "-C", "operator", "test-upgrade"],
-            OperatorE2eTest.UPGRADE_TEST_TIMEOUT_SEC,
-        )
-
         print("Executing operator e2e tests")
-        self.run_with_graceful_kill(
-            ["operator/hack/junit_wrap.sh", "test-e2e",
-             "Run operator E2E tests.",
-             "See log and/or kuttl JUnit output for error details. "
-             "Reading operator/tests/TROUBLESHOOTING_E2E_TESTS.md may also be helpful.",
-             "make", "-C", "operator", "test-e2e-deployed"],
-            OperatorE2eTest.E2E_TEST_TIMEOUT_SEC,
-        )
 
-        print("Executing Operator Bundle Scorecard tests")
         self.run_with_graceful_kill(
-            [
-                "operator/hack/junit_wrap.sh", "bundle-test-image",
-                "Run scorecard tests.",
-                "See log for error details.",
-                "./operator/scripts/retry.sh",
-                "4",
-                "2",
-                "make",
-                "-C",
-                "operator",
-                "bundle-test-image",
-            ],
-            OperatorE2eTest.SCORECARD_TEST_TIMEOUT_SEC,
+            ["operator/tests/run.sh"],
+            OperatorE2eTest.TEST_TIMEOUT,
         )
 
 
