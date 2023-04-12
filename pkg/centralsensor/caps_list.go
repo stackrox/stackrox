c90dbfa5eb8b5802694cf9e6aaebd411a8bf9b5f
JoukoVirtanen <jouko@stackrox.com>
2023-04-12 14:04:49 -0700

    Added ListeningEndpointsCap to the list of capabilities

--- pkg/centralsensor/caps_list.go
+++ pkg/centralsensor/caps_list.go
@@ -33,4 +33,7 @@ const (
 
 	// NodeScanningCap identifies the capability to scan nodes and provide node components for vulnerability analysis.
 	NodeScanningCap SensorCapability = "NodeScanning"
+
+	// ListeningEndpointsCap identifies the capability for sensor to process and send information about listening endpoints and their processes, AKA processes listening on ports 
+	ListeningEndpointsCap SensorCapability = "ListeningEndpoints"
 )
