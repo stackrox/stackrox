8bdd3a9afa94e3b4e99216fb934833b9e465fa68
Mark Pedrotti <mark@stackrox.com>
2023-04-13 09:25:32 -0400

    Refactor TopCvssLabel and adjust spacing for PDF Export

--- ui/apps/platform/src/Components/TopCvssLabel/TopCvssLabel.tsx
+++ ui/apps/platform/src/Components/TopCvssLabel/TopCvssLabel.tsx
@@ -7,32 +7,38 @@ export type TopCvssLabelProps = {
 };
 
 function TopCvssLabel({ cvss, version, expanded }: TopCvssLabelProps): ReactNode {
-    if (!cvss && cvss !== 0) {
+    // Early return here might be redundant with similar if statement preceding function call.
+    if (typeof cvss !== 'number') {
         return 'N/A';
     }
 
     const cvss1 = cvss.toFixed(1);
 
-    if (!expanded) {
+    if (expanded) {
+        // Vertical key value pairs for entity overview page.
         return (
-            <span>
-                <span data-testid="label-chip">{cvss1}</span>{' '}
-                <span className="text-xs">({version})</span>
-            </span>
+            <div className="flex flex-col">
+                <div>
+                    <span className="font-700 mr-2">Top CVSS:</span>
+                    <span>{cvss1}</span>
+                </div>
+                <div>
+                    <span className="font-700 mr-2">CVSS Version:</span>
+                    <span>{version}</span>
+                </div>
+            </div>
         );
     }
 
+    // Horizontal score and version for entities table cell.
+    // Replace mr-2 with mr-1 and space for PDF Export.
     return (
-        <div className="flex flex-col">
-            <div>
-                <span className="font-700 mr-2">Top CVSS:</span>
-                <span>{cvss1}</span>
-            </div>
-            <div>
-                <span className="font-700 mr-2">CVSS Version:</span>
-                <span>{version}</span>
-            </div>
-        </div>
+        <span>
+            <span data-testid="label-chip" className="mr-1">
+                {cvss1}
+            </span>
+            <span className="text-xs"> {version}</span>
+        </span>
     );
 }
 
