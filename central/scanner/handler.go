77d0f9aa9e2d77c7e8ed0f27bdc32fd345e6c491
Yury Kovalev <ykovalev@redhat.com>
2023-04-14 15:46:40 +0200

    ROX-16189: Add custom namespace SANs for the scanner and scanner-db certificates when loading a bundle from a banner

--- central/scanner/handler.go
+++ central/scanner/handler.go
@@ -10,6 +10,7 @@ import (
 	"github.com/pkg/errors"
 	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/pkg/apiparams"
+	"github.com/stackrox/rox/pkg/env"
 	"github.com/stackrox/rox/pkg/errorhelpers"
 	"github.com/stackrox/rox/pkg/httputil"
 	"github.com/stackrox/rox/pkg/images/defaults"
@@ -62,13 +63,13 @@ func generateFilesForScannerV1(params *apiparams.Scanner, clusterType storage.Cl
 	if err != nil {
 		return nil, errors.Wrap(err, "could not load central CA")
 	}
-
-	cert, err := mtls.IssueNewCert(mtls.ScannerSubject)
+	namespace := env.Namespace.Setting()
+	cert, err := mtls.IssueNewCert(mtls.ScannerSubject, mtls.WithNamespace(namespace))
 	if err != nil {
 		return nil, errors.Wrap(err, "could not issue scanner cert")
 	}
 
-	scannerDBCert, err := mtls.IssueNewCert(mtls.ScannerDBSubject)
+	scannerDBCert, err := mtls.IssueNewCert(mtls.ScannerDBSubject, mtls.WithNamespace(namespace))
 	if err != nil {
 		return nil, errors.Wrap(err, "could not issue scanner db cert")
 	}
--- central/scanner/handler_test.go
+++ central/scanner/handler_test.go
@@ -3,6 +3,7 @@ package scanner
 import (
 	"archive/zip"
 	"bytes"
+	"crypto/tls"
 	"encoding/json"
 	"io"
 	"net/http"
@@ -11,10 +12,17 @@ import (
 
 	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/pkg/apiparams"
+	"github.com/stackrox/rox/pkg/env"
 	"github.com/stackrox/rox/pkg/images/defaults"
 	testutilsMTLS "github.com/stackrox/rox/pkg/mtls/testutils"
 	"github.com/stackrox/rox/pkg/version/testutils"
+	"github.com/stackrox/rox/pkg/x509utils"
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
+	"golang.org/x/exp/slices"
+	"gopkg.in/yaml.v3"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
 func TestHandler(t *testing.T) {
@@ -33,10 +41,13 @@ func (s *handlerTestSuite) SetupTest() {
 
 func (s *handlerTestSuite) TestGenerateScannerHTTPHandler() {
 	s.T().Setenv(defaults.ImageFlavorEnvName, defaults.ImageFlavorNameDevelopmentBuild)
+	params := apiparams.Scanner{ClusterType: storage.ClusterType_KUBERNETES_CLUSTER.String(), ScannerImage: "docker.io/stackrox/scanner:latest"}
+	_ = s.callServer(params)
+}
+
+func (s *handlerTestSuite) callServer(params apiparams.Scanner) *zip.Reader {
 	server := httptest.NewServer(Handler())
 	defer server.Close()
-
-	params := apiparams.Scanner{ClusterType: storage.ClusterType_KUBERNETES_CLUSTER.String(), ScannerImage: "docker.io/stackrox/scanner:latest"}
 	marshaledJSON, err := json.Marshal(params)
 	s.Require().NoError(err)
 
@@ -47,6 +58,84 @@ func (s *handlerTestSuite) TestGenerateScannerHTTPHandler() {
 	s.Require().NoError(err)
 	s.Assert().NotEmpty(body)
 
-	_, err = zip.NewReader(bytes.NewReader(body), int64(len(body)))
+	zipReader, err := zip.NewReader(bytes.NewReader(body), int64(len(body)))
 	s.Assert().NoError(err)
+	return zipReader
+}
+
+func (s *handlerTestSuite) TestCertificateSAN() {
+	type testCase struct {
+		name          string
+		namespace     string
+		scannerSANs   []string
+		scannerDBSANs []string
+	}
+
+	cases := []testCase{
+		{
+			name:          "no namespace",
+			scannerSANs:   []string{"scanner.stackrox", "scanner.stackrox.svc"},
+			scannerDBSANs: []string{"scanner-db.stackrox", "scanner-db.stackrox.svc"},
+		},
+		{
+			name:          "stackrox namespace",
+			namespace:     "stackrox",
+			scannerSANs:   []string{"scanner.stackrox", "scanner.stackrox.svc"},
+			scannerDBSANs: []string{"scanner-db.stackrox", "scanner-db.stackrox.svc"},
+		},
+		{
+			name:          "custom namespace",
+			namespace:     "custom",
+			scannerSANs:   []string{"scanner.stackrox", "scanner.stackrox.svc", "scanner.custom", "scanner.custom.svc"},
+			scannerDBSANs: []string{"scanner-db.stackrox", "scanner-db.stackrox.svc", "scanner-db.custom", "scanner-db.custom.svc"},
+		},
+	}
+
+	for _, tc := range cases {
+		s.T().Run(tc.name, func(t *testing.T) {
+			if tc.namespace != "" {
+				t.Setenv(env.Namespace.EnvVar(), tc.namespace)
+			}
+			params := apiparams.Scanner{ClusterType: storage.ClusterType_KUBERNETES_CLUSTER.String(), ScannerImage: "docker.io/stackrox/scanner:latest"}
+			reader := s.callServer(params)
+			filename := "scanner/02-scanner-03-tls-secret.yaml"
+			//#nosec G101 -- This is a false positive
+			files := reader.File
+			idx := slices.IndexFunc(files, func(f *zip.File) bool {
+				return f.Name == filename
+			})
+			require.GreaterOrEqual(t, idx, 0, "%s file not found", filename)
+			tlsSecretFile, err := files[idx].Open()
+			require.NoError(t, err)
+			defer func() {
+				_ = tlsSecretFile.Close()
+			}()
+			content2, err := io.ReadAll(tlsSecretFile)
+			require.NoError(t, err)
+			content := content2
+			decoder := yaml.NewDecoder(bytes.NewReader(content))
+			var doc map[string]interface{}
+			for decoder.Decode(&doc) == nil {
+				tlsSecret := &corev1.Secret{}
+				err := runtime.DefaultUnstructuredConverter.FromUnstructured(doc, tlsSecret)
+				require.NoError(t, err)
+				switch tlsSecret.Name {
+				case "scanner-tls":
+					assertSANs(t, tlsSecret, tc.scannerSANs)
+				case "scanner-db-tls":
+					assertSANs(t, tlsSecret, tc.scannerDBSANs)
+				}
+			}
+		})
+	}
+}
+
+func assertSANs(t *testing.T, tlsSecret *corev1.Secret, sans []string) {
+	cert, err := tls.X509KeyPair([]byte(tlsSecret.StringData["cert.pem"]), []byte(tlsSecret.StringData["key.pem"]))
+	require.NoError(t, err)
+	chain, err := x509utils.ParseCertificateChain(cert.Certificate)
+	require.NoError(t, err)
+	for _, san := range sans {
+		require.Contains(t, chain[0].DNSNames, san)
+	}
 }
