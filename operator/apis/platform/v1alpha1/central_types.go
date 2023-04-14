6885996c360bdddfc96c0746fc5f02b25de0ac64
Connor Gorman <connorgorman@users.noreply.github.com>
2023-04-13 21:19:43 -0700

    Add Tech Preview designation to Operator for connection string (#5674)

--- operator/apis/platform/v1alpha1/central_types.go
+++ operator/apis/platform/v1alpha1/central_types.go
@@ -185,16 +185,18 @@ type CentralDBSpec struct {
 	//+operator-sdk:csv:customresourcedefinitions:type=spec,xDescriptors={"urn:alm:descriptor:com.tectonic.ui:hidden"}
 	IsEnabled *CentralDBEnabled `json:"isEnabled,omitempty"`
 
-	// Specify a secret that contains the password in the "password" data item.
-	// If omitted, the operator will auto-generate a DB password and store it in the "password" item
+	// Specify a secret that contains the password in the "password" data item. This can only be used when
+	// specifying a connection string manually.
+	// When omitted, the operator will auto-generate a DB password and store it in the "password" item
 	// in the "central-db-password" secret.
 	//+operator-sdk:csv:customresourcedefinitions:type=spec,displayName="Administrator Password",order=1
 	PasswordSecret *LocalSecretReference `json:"passwordSecret,omitempty"`
 
-	// Specify a connection string that corresponds to an existing database. If set, the operator will not manage Central DB.
+	// NOTE: Connecting to an external database is in Technology Preview.
+	// Specify a connection string that corresponds to an external database. If set, the operator will not manage Central DB.
 	// When using this option, you must explicitly set a password secret; automatically generating a password will not
 	// be supported.
-	//+operator-sdk:csv:customresourcedefinitions:type=spec,order=2
+	//+operator-sdk:csv:customresourcedefinitions:type=spec,order=2,displayName="Connection String (Technology Preview)"
 	ConnectionStringOverride *string `json:"connectionString,omitempty"`
 
 	// Configures how Central DB should store its persistent data. You can choose between using a persistent
