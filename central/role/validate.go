4cb2095ca46ff9ad333487d0d56a1339f7da3761
dhaus67 <dhaus@redhat.com>
2023-04-18 04:48:46 +0200

    Reintroduce validation to access scopes within roxctl (#5604)

--- central/role/validate.go
+++ central/role/validate.go
@@ -7,11 +7,10 @@ import (
 	"github.com/pkg/errors"
 	"github.com/stackrox/rox/central/role/resources"
 	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/accessscope"
 	"github.com/stackrox/rox/pkg/auth/permissions"
 	"github.com/stackrox/rox/pkg/env"
-	"github.com/stackrox/rox/pkg/sac/effectiveaccessscope"
 	"github.com/stackrox/rox/pkg/uuid"
-	"k8s.io/apimachinery/pkg/labels"
 )
 
 const (
@@ -148,54 +147,11 @@ func ValidateSimpleAccessScope(scope *storage.SimpleAccessScope) error {
 	if err := ValidateAccessScopeID(scope); err != nil {
 		multiErr = multierror.Append(multiErr, err)
 	}
-	if scope.GetName() == "" {
-		multiErr = multierror.Append(multiErr, errors.New("name field must be set"))
-	}
 
-	err := ValidateSimpleAccessScopeRules(scope.GetRules())
+	err := accessscope.ValidateSimpleAccessScopeProto(scope)
 	if err != nil {
 		multiErr = multierror.Append(multiErr, err)
 	}
 
-	return multiErr
-}
-
-// ValidateSimpleAccessScopeRules checks whether the supplied protobuf message
-// represents valid simple access scope rules.
-func ValidateSimpleAccessScopeRules(scopeRules *storage.SimpleAccessScope_Rules) error {
-	var multiErr error
-
-	for _, ns := range scopeRules.GetIncludedNamespaces() {
-		if ns.GetClusterName() == "" || ns.GetNamespaceName() == "" {
-			multiErr = multierror.Append(multiErr, errors.Errorf(
-				"both cluster_name and namespace_name fields must be set in namespace rule <%s, %s>",
-				ns.GetClusterName(), ns.GetNamespaceName()))
-		}
-	}
-	for _, labelSelector := range scopeRules.GetClusterLabelSelectors() {
-		err := validateSelectorRequirement(labelSelector)
-		if err != nil {
-			multiErr = multierror.Append(multiErr, err)
-		}
-	}
-	for _, labelSelector := range scopeRules.GetNamespaceLabelSelectors() {
-		err := validateSelectorRequirement(labelSelector)
-		if err != nil {
-			multiErr = multierror.Append(multiErr, err)
-		}
-	}
-
-	return multiErr
-}
-
-func validateSelectorRequirement(labelSelector *storage.SetBasedLabelSelector) error {
-	var multiErr error
-	for _, requirement := range labelSelector.GetRequirements() {
-		op := effectiveaccessscope.ConvertLabelSelectorOperatorToSelectionOperator(requirement.GetOp())
-		_, err := labels.NewRequirement(requirement.GetKey(), op, requirement.Values)
-		if err != nil {
-			multiErr = multierror.Append(multiErr, err)
-		}
-	}
 	return multiErr
 }
