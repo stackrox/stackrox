108a8413e36b83705fbbb8890adf696048686a5c
Gavin Jefferies <gavin@stackrox.com>
2023-04-19 05:15:06 -0700

    int init

--- qa-tests-backend/src/test/groovy/AAA.groovy
+++ qa-tests-backend/src/test/groovy/AAA.groovy
@@ -3,18 +3,21 @@ import spock.lang.Tag
 
 class AAA extends BaseSpecification {
     @Shared
-    private String runCount
+    private Integer runCount
 
     def setupSpec() {
         runCount = 0
+        log.info("setupSpec runCount: ${runCount}")
     }
 
     @Tag("BAT")
     def "Fail once to trigger secret access for defpol"() {
         when:
         runCount++
+        log.info("when runCount: ${runCount}")
 
         then:
+        log.info("test runCount: ${runCount > 1}")
         runCount > 1
     }
 }
