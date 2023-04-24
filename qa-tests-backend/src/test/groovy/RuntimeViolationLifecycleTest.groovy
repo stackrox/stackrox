0cdcdb3676e3d9a5b19258df37bf365db71bf58b
davdhacs <105243888+davdhacs@users.noreply.github.com>
2023-04-24 15:04:00 -0600

    allow failed apt for RuntimeViolationLifecycleTest,DeploymentTest

--- qa-tests-backend/src/test/groovy/RuntimeViolationLifecycleTest.groovy
+++ qa-tests-backend/src/test/groovy/RuntimeViolationLifecycleTest.groovy
@@ -25,7 +25,7 @@ class RuntimeViolationLifecycleTest extends BaseSpecification  {
         .setImage ("quay.io/rhacs-eng/qa-multi-arch:nginx-" +
                 "204a9a8e65061b10b92ad361dd6f406248404fe60efd5d6a8f2595f18bb37aad")
         .addLabel ("app", DEPLOYMENTNAME)
-        .setCommand(["sh" , "-c" , "apt-get -y update && sleep 600"])
+        .setCommand(["sh" , "-c" , "apt-get -y update || true && sleep 600"])
 
     def checkPolicyExists(String policyName) {
         assert getPolicies().stream()
