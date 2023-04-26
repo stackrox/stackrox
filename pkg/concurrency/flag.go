05868d92167ca88333160b7df2932ecc2fc6eb36
Tomasz Janiszewski <tomek@redhat.com>
2023-04-26 16:26:12 +0200

    Use alias instead of struct in concurrency.Flag

--- pkg/concurrency/flag.go
+++ pkg/concurrency/flag.go
@@ -3,28 +3,26 @@ package concurrency
 import "sync/atomic"
 
 // Flag is an atomic boolean flag.
-type Flag struct {
-	val uint32
-}
+type Flag uint32
 
 // Get gets the current value of the flag.
 func (f *Flag) Get() bool {
-	return atomic.LoadUint32(&f.val)&0x1 != 0
+	return atomic.LoadUint32((*uint32)(f))&0x1 != 0
 }
 
 // Set sets the value of the flag, discarding the previous value.
 func (f *Flag) Set(v bool) {
-	atomic.StoreUint32(&f.val, b2i(v))
+	atomic.StoreUint32((*uint32)(f), b2i(v))
 }
 
 // TestAndSet sets the value of the flag, and returns the *previous* value of the flag.
 func (f *Flag) TestAndSet(v bool) bool {
-	return atomic.SwapUint32(&f.val, b2i(v))&0x1 != 0
+	return atomic.SwapUint32((*uint32)(f), b2i(v))&0x1 != 0
 }
 
 // Toggle flips the value of the flag, and returns the *new* value of the flag.
 func (f *Flag) Toggle() bool {
-	return atomic.AddUint32(&f.val, 1)&0x1 != 0
+	return atomic.AddUint32((*uint32)(f), 1)&0x1 != 0
 }
 
 func b2i(v bool) uint32 {
