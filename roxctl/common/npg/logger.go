b297f53f4a0bc7c89d4d17c92f02d091205bd27c
shireenf-ibm <shireenf@il.ibm.com>
2023-04-27 11:43:37 +0300

    exporting the npg Logger struct

--- roxctl/common/npg/logger.go
+++ roxctl/common/npg/logger.go
@@ -4,22 +4,29 @@ import (
 	"github.com/stackrox/rox/roxctl/common/logger"
 )
 
-// log wraps the common logging interface in a NP-Guard compatible logger.
+// Logger wraps the common logging interface in a NP-Guard compatible logger.
 // It mutes all warnings and errors as they are being returned explicitly by the library.
-type log struct {
+type Logger struct {
 	l logger.Logger
 }
 
 // NewLogger returns new instance of log
-func NewLogger(l logger.Logger) *log {
-	return &log{
+func NewLogger(l logger.Logger) *Logger {
+	return &Logger{
 		l: l,
 	}
 }
 
-func (nl *log) Debugf(_ string, _ ...interface{}) {}
-func (nl *log) Infof(format string, o ...interface{}) {
+// Debugf empty func, mutes debug messages as they are being returned explicitly by the library.
+func (nl *Logger) Debugf(_ string, _ ...interface{}) {}
+
+// Infof prints a formatted string with a newline, prefixed with INFO and colorized
+func (nl *Logger) Infof(format string, o ...interface{}) {
 	nl.l.InfofLn(format, o...)
 }
-func (nl *log) Warnf(_ string, _ ...interface{})           {}
-func (nl *log) Errorf(_ error, _ string, _ ...interface{}) {}
+
+// Warnf empty func, mutes the warnings as they are being returned explicitly by NP-Guard library
+func (nl *Logger) Warnf(_ string, _ ...interface{}) {}
+
+// Errorf empty func, mutes the errors as they are being returned explicitly by NP-Guard library
+func (nl *Logger) Errorf(_ error, _ string, _ ...interface{}) {}
