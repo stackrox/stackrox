7755a3f7633d72b4502d5574efc8551b13aeffe0
Gavin Jefferies <gavin@stackrox.com>
2023-04-28 15:57:37 -0700

    variable

--- .github/workflows/scripts/build-matrix.sh
+++ .github/workflows/scripts/build-matrix.sh
@@ -3,13 +3,11 @@
 
 set -euo pipefail
 
-# Test utility functions
-
-# TEST_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../../.. && pwd)"
-# # shellcheck source=../../scripts/lib.sh
-# source "$TEST_ROOT/scripts/lib.sh"
+ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../../.. && pwd)"
+# shellcheck source=../../scripts/lib.sh
+source "$ROOT/scripts/lib.sh"
 # # shellcheck source=../../scripts/ci/lib.sh
-# source "$TEST_ROOT/scripts/ci/lib.sh"
+# source "$ROOT/scripts/ci/lib.sh"
 
 define_build_matrix() {
 
@@ -17,31 +15,37 @@ define_build_matrix() {
     {
         "pre_build_cli": {
             "include": [
-                {"name": "development", "release": false, "artifact": "cli-build-development"},
-                {"name": "prerelease", "release": true, "artifact": "cli-build-prerelease"}
+                {"name": "development", "artifact": "cli-build-development"}
             ]
         },
         "pre_build_go_binaries": {
             "include": [
-                {"name": "development", "release": false, "artifact": "go-binaries-build-development"},
-                {"name": "race condition debug", "release": false, "artifact": "go-binaries-build-rcd"},
-                {"name": "prerelease", "release": true, "artifact": "go-binaries-build-prerelease"}
+                {"name": "development", "artifact": "go-binaries-build-development"},
+                {"name": "race condition debug", "artifact": "go-binaries-build-rcd"}
             ]
         },
         "build_and_push_main": {
             "include": [
                 {"name": "stackrox branding", "branding": "STACKROX_BRANDING", "cli-artifact": "cli-build-development", "go-binaries-artifact": "go-binaries-build-development"},
                 {"name": "rhacs branding", "branding": "RHACS_BRANDING", "cli-artifact": "cli-build-development", "go-binaries-artifact": "go-binaries-build-development"},
-                {"name": "race condition debug", "branding": "STACKROX_BRANDING", "cli-artifact": "cli-build-development", "go-binaries-artifact": "go-binaries-build-rcd"},
-                {"name": "prerelease", "branding": "RHACS_BRANDING", "cli-artifact": "cli-build-prerelease", "go-binaries-artifact": "go-binaries-build-prerelease"}
+                {"name": "race condition debug", "branding": "STACKROX_BRANDING", "cli-artifact": "cli-build-development", "go-binaries-artifact": "go-binaries-build-rcd"}
             ]
         }
     }
 _EO_MATRIX_
 
+    info "Base build matrix is:"
     jq <<< "$matrix"
 
-    condensed="$(jq -c <<< "$matrix")"
+    if true; then
+        matrix="$(jq '.pre_build_cli.include += [{"name": "prerelease", "artifact": "cli-build-prerelease"}]' <<< "$matrix")"
+        matrix="$(jq '.pre_build_go_binaries.include += [{"name": "prerelease", "artifact": "go-binaries-build-prerelease"}]' <<< "$matrix")"
+        matrix="$(jq '.build_and_push_main.include += [{"name": "prerelease", "branding": "RHACS_BRANDING", "cli-artifact": "cli-build-prerelease", "go-binaries-artifact": "go-binaries-build-prerelease"}]' <<< "$matrix")"
 
+        info "Build matrix after prerelease addition:"
+        jq <<< "$matrix"
+    fi
+
+    condensed="$(jq -c <<< "$matrix")"
     echo "matrix=$condensed" >> "$GITHUB_OUTPUT"
 }
