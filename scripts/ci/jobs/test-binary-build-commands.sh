4ef0d8d8284c484651da1ba544488457b89ad65f
Gavin Jefferies <gavin@stackrox.com>
2023-04-29 10:18:09 -0700

    skip prow jobs

--- scripts/ci/jobs/test-binary-build-commands.sh
+++ scripts/ci/jobs/test-binary-build-commands.sh
@@ -9,6 +9,11 @@ set -euo pipefail
 make_test_bin() {
     info "Making test-bin"
 
+    if is_in_PR_context && pr_has_label "ci-skip-prow-jobs"; then
+        # Can skip jobs by failing this initial build step (for jobs that run from: test-bin)
+        die "ERROR: Skipping all prow (openshift/release) defined CI jobs"
+    fi
+
     make cli-build upgrader
     install_built_roxctl_in_gopath
 }
