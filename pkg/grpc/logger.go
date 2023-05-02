97f1239c423b8fbe54a9298a5a936ea8e97a1b3a
Connor Gorman <connorgorman@users.noreply.github.com>
2023-05-01 19:41:47 -0700

    ROX-15236: Silence http logs containing preface error due to tcp probe (#5857)

--- pkg/grpc/logger.go
+++ pkg/grpc/logger.go
@@ -3,7 +3,11 @@ package grpc
 import (
 	"bytes"
 	"os"
+
+	"github.com/stackrox/rox/pkg/env"
 )
+
+var managedCentral = env.ManagedCentral.BooleanSetting()
 
 // httpErrorLogger implements io.Writer interface. It is used to control
 // error messages coming from http server which can be logged.
@@ -12,8 +16,11 @@ type httpErrorLogger struct {
 
 // Write suppresses EOF error messages
 func (l httpErrorLogger) Write(p []byte) (n int, err error) {
-	if !bytes.Contains(p, []byte("EOF")) {
-		return os.Stderr.Write(p)
+	if bytes.Contains(p, []byte("EOF")) {
+		return len(p), nil
 	}
-	return len(p), nil
+	if managedCentral && bytes.Contains(p, []byte("error reading preface from client")) {
+		return len(p), nil
+	}
+	return os.Stderr.Write(p)
 }
