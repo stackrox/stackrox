8e119e0b1dfd39d9888b7cbc25b621175f745402
Yann Brillouet <ybrillou@redhat.com>
2023-05-02 11:20:42 +0200

    Change from type inheritance to attribute wrapping

--- pkg/logging/rate_limited_logger.go
+++ pkg/logging/rate_limited_logger.go
@@ -11,7 +11,7 @@ import (
 
 // RateLimitedLogger wraps a zap.SugaredLogger that supports rate limiting.
 type RateLimitedLogger struct {
-	Logger    Logger
+	logger    Logger
 	frequency float64
 	burst     int
 	// rateLimiters *lru.Cache[string, *rate.Limiter]
@@ -52,17 +52,17 @@ func (rl *RateLimitedLogger) ErrorL(limiter string, template string, args ...int
 
 // Error logs the consecutive interfaces
 func (rl *RateLimitedLogger) Error(args ...interface{}) {
-	rl.Logger.Error(args...)
+	rl.logger.Error(args...)
 }
 
 // Errorf logs the input template filled with argument data
 func (rl *RateLimitedLogger) Errorf(template string, args ...interface{}) {
-	rl.Logger.Errorf(template, args...)
+	rl.logger.Errorf(template, args...)
 }
 
 // Errorw logs the input message and keyValues
 func (rl *RateLimitedLogger) Errorw(msg string, keysAndValues ...interface{}) {
-	rl.Logger.Errorw(msg, keysAndValues...)
+	rl.logger.Errorw(msg, keysAndValues...)
 }
 
 // WarnL logs a templated warn message if allowed by the rate limiter corresponding to the identifier
@@ -77,17 +77,17 @@ func (rl *RateLimitedLogger) WarnL(limiter string, template string, args ...inte
 
 // Warn logs the consecutive interfaces
 func (rl *RateLimitedLogger) Warn(args ...interface{}) {
-	rl.Logger.Warn(args...)
+	rl.logger.Warn(args...)
 }
 
 // Warnf logs the input template filled with argument data
 func (rl *RateLimitedLogger) Warnf(template string, args ...interface{}) {
-	rl.Logger.Warnf(template, args...)
+	rl.logger.Warnf(template, args...)
 }
 
 // Warnw logs the input message and keyValues
 func (rl *RateLimitedLogger) Warnw(msg string, keysAndValues ...interface{}) {
-	rl.Logger.Warnw(msg, keysAndValues...)
+	rl.logger.Warnw(msg, keysAndValues...)
 }
 
 // InfoL logs a templated info message if allowed by the rate limiter corresponding to the identifier
@@ -102,17 +102,17 @@ func (rl *RateLimitedLogger) InfoL(limiter string, template string, args ...inte
 
 // Info logs the consecutive interfaces
 func (rl *RateLimitedLogger) Info(args ...interface{}) {
-	rl.Logger.Info(args...)
+	rl.logger.Info(args...)
 }
 
 // Infof logs the input template filled with argument data
 func (rl *RateLimitedLogger) Infof(template string, args ...interface{}) {
-	rl.Logger.Infof(template, args...)
+	rl.logger.Infof(template, args...)
 }
 
 // Infow logs the input message and keyValues
 func (rl *RateLimitedLogger) Infow(msg string, keysAndValues ...interface{}) {
-	rl.Logger.Infow(msg, keysAndValues...)
+	rl.logger.Infow(msg, keysAndValues...)
 }
 
 // DebugL logs a templated debug message if allowed by the rate limiter corresponding to the identifier
@@ -127,17 +127,17 @@ func (rl *RateLimitedLogger) DebugL(limiter string, template string, args ...int
 
 // Debug logs the consecutive interfaces
 func (rl *RateLimitedLogger) Debug(args ...interface{}) {
-	rl.Logger.Debug(args...)
+	rl.logger.Debug(args...)
 }
 
 // Debugf logs the input template filled with argument data
 func (rl *RateLimitedLogger) Debugf(template string, args ...interface{}) {
-	rl.Logger.Debugf(template, args...)
+	rl.logger.Debugf(template, args...)
 }
 
 // Debugw logs the input message and keyValues
 func (rl *RateLimitedLogger) Debugw(msg string, keysAndValues ...interface{}) {
-	rl.Logger.Debugw(msg, keysAndValues...)
+	rl.logger.Debugw(msg, keysAndValues...)
 }
 
 /*
@@ -157,7 +157,7 @@ func (rl *RateLimitedLogger) logf(level zapcore.Level, limiter string, template 
 	_, _ = rl.rateLimitedLogs.ContainsOrAdd(
 		key,
 		newRateLimitedLog(
-			rl.Logger,
+			rl.logger,
 			level,
 			rate.NewLimiter(rate.Limit(rl.frequency), rl.burst),
 			limiter,
