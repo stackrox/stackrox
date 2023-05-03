5fdb7a30a5cda686923c6a667064afb13238a5c7
Ludovic Cleroux <ludydoo@gmail.com>
2023-05-03 16:58:01 +0200

    PR Comments

--- central/metadata/service/service_impl_test.go
+++ central/metadata/service/service_impl_test.go
@@ -19,12 +19,23 @@ func TestSuite(t *testing.T) {
 
 func (s *testSuite) TestMetadataManagedCentral() {
 	srv := &serviceImpl{}
+
+	// ROX_MANAGED_CENTRAL not set
 	actual, err := srv.GetMetadata(context.Background(), &v1.Empty{})
 	s.NoError(err)
 	env.ManagedCentral.BooleanSetting()
 	s.EqualValues(false, actual.IsManagedCentral)
+
+	// ROX_MANAGED_CENTRAL set to false
+	s.T().Setenv("ROX_MANAGED_CENTRAL", "false")
+	actual, err = srv.GetMetadata(context.Background(), &v1.Empty{})
+	s.NoError(err)
+	s.EqualValues(false, actual.IsManagedCentral)
+
+	// ROX_MANAGED_CENTRAL set to true
 	s.T().Setenv("ROX_MANAGED_CENTRAL", "true")
 	actual, err = srv.GetMetadata(context.Background(), &v1.Empty{})
 	s.NoError(err)
 	s.EqualValues(true, actual.IsManagedCentral)
+
 }
