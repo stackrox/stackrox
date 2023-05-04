a3ebc5c45c4cdafda0c4ede0a7349c66cc7bf799
Alex Rukletsov <alexr@stackrox.com>
2023-05-04 20:47:13 +0200

    Update ui/apps/platform/src/Containers/SystemConfig/Details/PublicConfigTelemetryDetails.tsx
    Co-authored-by: Kerry Carmichael <kcarmich@redhat.com>

--- ui/apps/platform/src/Containers/SystemConfig/Details/PublicConfigTelemetryDetails.tsx
+++ ui/apps/platform/src/Containers/SystemConfig/Details/PublicConfigTelemetryDetails.tsx
@@ -37,7 +37,7 @@ const PublicConfigTelemetryDetails = ({
                 <p className="pf-u-mb-sm">
                     Online telemetry data collection allows Red Hat to use anonymized information to
                     enhance your user experience. Consult the documentation to see what is collected,
-                    and for how to opt out.
+                    and for information about how to opt out.
                 </p>
             </CardBody>
         </Card>
