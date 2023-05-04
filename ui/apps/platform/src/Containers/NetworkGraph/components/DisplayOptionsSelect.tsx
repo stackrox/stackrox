tigr0w/stackrox_stackrox

ROX-16605: Network graph additional display options

ui/apps/platform/src/Containers/NetworkGraph/components/DisplayOptionsSelect.tsx
@@ -1,13 +1,26 @@
 import React, { useState } from 'react';
-import { Select, SelectVariant, SelectGroup, SelectOption } from '@patternfly/react-core';
-import { PficonNetworkRangeIcon } from '@patternfly/react-icons';
+import {
+    Select,
+    SelectVariant,
+    SelectGroup,
+    SelectOption,
+    Split,
+    SplitItem,
+} from '@patternfly/react-core';
+import { PficonNetworkRangeIcon, CodeBranchIcon, FilterIcon } from '@patternfly/react-icons';
 
 import { ReactComponent as NoPolicyRules } from 'images/network-graph/no-policy-rules.svg';
 import { ReactComponent as PortLabel } from 'images/network-graph/tcp-icon.svg';
 
 import './DisplayOptionsSelect.css';
+import { CidrBlockIcon, DeploymentIcon, NamespaceIcon } from '../common/NetworkGraphIcons';
 
-export type DisplayOption = 'policyStatusBadge' | 'externalBadge' | 'edgeLabel';
+export type DisplayOption =
+    | 'policyStatusBadge'
+    | 'externalBadge'
+    | 'edgeLabel'
+    | 'namespaceTypeLabel'
+    | 'objectTypeLabel';
 
 type DisplayOptionsSelectProps = {
     selectedOptions: DisplayOption[];
@@ -61,6 +74,37 @@ function DisplayOptionsSelect({
                 <SelectOption key={2} value="edgeLabel">
                     <PortLabel width="22px" height="22px" className="pf-u-mr-xs" />
                     Port and protocol label
+                </SelectOption>
+            </SelectGroup>
+            <SelectGroup label="Selection indicators" key="namespace-type-selection">
+                <SelectOption key={2} value="namespaceTypeLabel">
+                    <Split>
+                        <SplitItem className="pf-u-mr-xs">
+                            <FilterIcon />
+                        </SplitItem>
+                        <SplitItem>Filtered namespace</SplitItem>
+                        <SplitItem className="pf-u-mx-sm">&</SplitItem>
+                        <SplitItem className="pf-u-mr-xs">
+                            <CodeBranchIcon />
+                        </SplitItem>
+                        <SplitItem>Related namespace</SplitItem>
+                    </Split>
+                </SelectOption>
+            </SelectGroup>
+            <SelectGroup label="Object type labels" key="object-type-labels">
+                <SelectOption key={2} value="objectTypeLabel">
+                    <Split>
+                        <SplitItem className="pf-u-mr-xs">
+                            <NamespaceIcon />
+                        </SplitItem>
+                        <SplitItem className="pf-u-mr-xs">
+                            <DeploymentIcon />
+                        </SplitItem>
+                        <SplitItem className="pf-u-mr-xs">
+                            <CidrBlockIcon />
+                        </SplitItem>
+                        <SplitItem>Labels</SplitItem>
+                    </Split>
                 </SelectOption>
             </SelectGroup>
         </Select>

stackrox/stackrox

ui/apps/platform/src/Containers/NetworkGraph/components/DisplayOptionsSelect.tsx
@@ -1,13 +1,26 @@
 import React, { useState } from 'react';
-import { Select, SelectVariant, SelectGroup, SelectOption } from '@patternfly/react-core';
-import { PficonNetworkRangeIcon } from '@patternfly/react-icons';
+import {
+    Select,
+    SelectVariant,
+    SelectGroup,
+    SelectOption,
+    Split,
+    SplitItem,
+} from '@patternfly/react-core';
+import { PficonNetworkRangeIcon, CodeBranchIcon, FilterIcon } from '@patternfly/react-icons';
 
 import { ReactComponent as NoPolicyRules } from 'images/network-graph/no-policy-rules.svg';
 import { ReactComponent as PortLabel } from 'images/network-graph/tcp-icon.svg';
 
 import './DisplayOptionsSelect.css';
+import { CidrBlockIcon, DeploymentIcon, NamespaceIcon } from '../common/NetworkGraphIcons';
 
-export type DisplayOption = 'policyStatusBadge' | 'externalBadge' | 'edgeLabel';
+export type DisplayOption =
+    | 'policyStatusBadge'
+    | 'externalBadge'
+    | 'edgeLabel'
+    | 'selectionIndicator'
+    | 'objectTypeLabel';
 
 type DisplayOptionsSelectProps = {
     selectedOptions: DisplayOption[];
@@ -61,6 +74,37 @@ function DisplayOptionsSelect({
                 <SelectOption key={2} value="edgeLabel">
                     <PortLabel width="22px" height="22px" className="pf-u-mr-xs" />
                     Port and protocol label
+                </SelectOption>
+            </SelectGroup>
+            <SelectGroup label="Selection indicators" key="selection-indicator">
+                <SelectOption key={2} value="selectionIndicator">
+                    <Split>
+                        <SplitItem className="pf-u-mr-xs">
+                            <FilterIcon />
+                        </SplitItem>
+                        <SplitItem>Filtered namespace</SplitItem>
+                        <SplitItem className="pf-u-mx-sm">&</SplitItem>
+                        <SplitItem className="pf-u-mr-xs">
+                            <CodeBranchIcon />
+                        </SplitItem>
+                        <SplitItem>Related namespace</SplitItem>
+                    </Split>
+                </SelectOption>
+            </SelectGroup>
+            <SelectGroup label="Object type labels" key="object-type-labels">
+                <SelectOption key={2} value="objectTypeLabel">
+                    <Split>
+                        <SplitItem className="pf-u-mr-xs">
+                            <NamespaceIcon />
+                        </SplitItem>
+                        <SplitItem className="pf-u-mr-xs">
+                            <DeploymentIcon />
+                        </SplitItem>
+                        <SplitItem className="pf-u-mr-xs">
+                            <CidrBlockIcon />
+                        </SplitItem>
+                        <SplitItem>Labels</SplitItem>
+                    </Split>
                 </SelectOption>
             </SelectGroup>
         </Select>