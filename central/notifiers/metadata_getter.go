784db0fe0adf465fb2deb21c468c04cbb902ff9f
Oscar Ward <oscarward95@gmail.com>
2023-05-04 15:00:31 -0700

    datastore backed implementation rename for clarity ; update interface comment

--- central/notifiers/metadata_getter.go
+++ central/notifiers/metadata_getter.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stackrox/rox/generated/storage"
 )
 
-// MetadataGetter provides functionality for getting information about annotation values.
+// MetadataGetter provides functionality for getting metadata information for alerts
 type MetadataGetter interface {
 	GetAnnotationValue(ctx context.Context, alert *storage.Alert, annotationKey, defaultValue string) string
 }
