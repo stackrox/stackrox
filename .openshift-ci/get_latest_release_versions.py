370a5c48ef82e8c0d8581d11bbc1eb17416f27b8
Misha Sugakov <misha@stackrox.com>
2023-05-05 21:08:05 +0200

    Fix identification of latest Helm releases
    for version compatibility tests.

--- .openshift-ci/get_latest_release_versions.py
+++ .openshift-ci/get_latest_release_versions.py
@@ -1,70 +1,143 @@
 #!/usr/bin/env python3
 
 """
-Returns the latest patches of the last n major versions
+Looks up N previous releases and outputs a Helm chart version for the most recent patch for each found release.
 """
 
-import sys
+import json
+import logging
+import pathlib
 import re
 import subprocess
-from collections import defaultdict
+import sys
 
-cmd = "helm search repo stackrox-oss -l -o json | jq -r '.[] | select( .name==\"stackrox-oss/stackrox-secured-cluster-services\" ) | .version'"
+from collections import namedtuple
 
-add_repo_cmd = "helm repo add stackrox-oss https://raw.githubusercontent.com/stackrox/helm-charts/main/opensource"
+this_script_dir = pathlib.Path(__file__).parent
+repo_root = this_script_dir.parent
+
+helm_repo_name = "temp-stackrox-oss-repo-should-not-see-me"
+
+add_repo_cmd = f"helm repo add {helm_repo_name} https://raw.githubusercontent.com/stackrox/helm-charts/main/opensource"
 update_repo_cmd = "helm repo update"
+search_cmd = f"helm search repo {helm_repo_name} -l -o json"
+remove_repo_cmd = f"helm repo remove {helm_repo_name}"
 
-def is_release_tag(version):
-    return re.search(r"^\d+\.\d+\.\d+$", version) is not None
+Version = namedtuple("Version", ["major", "minor", "patch"])
 
-def filter_tags(rawtags):
-    return set([t for t in rawtags if is_release_tag(t)])
+# Here we call "release" (or Y-Stream) the first appearance of X.Y.0 version.
+Release = namedtuple("Release", ["major", "minor"])
 
-def reduce_tags_to_latest_patch(tags):
-    top_patch_version = defaultdict(int)
-    major = "3"
-    for t in tags:
-        [minor, patch, _] = t.split('.')
-        k = '.'.join([major, minor])
-        top_patch_version[k] = max(top_patch_version[k], int(patch))
-    top_major_versions = sorted(list(top_patch_version.keys()), reverse=True)
-    return [t + "." + str(top_patch_version[t]) for t in top_major_versions]
-
-def make_image_tag():
-    return subprocess.check_output(["make", "--quiet", "--no-print-directory", "tag"]).decode(encoding="utf-8")
-
-def extract_x_y_from_main_image_tag(tag):
-    x_y = re.search(r"^(\d+)\.(\d+)", tag)
-    return int(x_y.group(1)), int(x_y.group(2))
-
-def get_latest_n_tags(tags, num_versions):
-    central_major, central_minor = extract_x_y_from_main_image_tag(make_image_tag())
-    tags_older_than_central = [t for t in tags if (int(t.split('.')[0]) < central_major or (int(t.split('.')[0]) == central_major and int(t.split('.')[1]) <= central_minor))]
-    return tags_older_than_central[:num_versions]
-
-# get_latest_release_versions gets the latest patches of the last num_versions major versions via Git CLI
-def get_latest_release_versions(num_versions):
-    rawtags = subprocess.check_output(cmd, shell=True).decode(encoding="utf-8").splitlines()
-    tags = filter_tags(rawtags)
-    latest_patch_tags = reduce_tags_to_latest_patch(tags)
-    lastN = get_latest_n_tags(latest_patch_tags, num_versions)
-    return map_to_helm_versions(lastN)
-
-def map_to_helm_versions(versions):
-    return [".".join(v.split(".")[1:]) + ".0" for v in versions]
-
-def update_helm_repo():
-    subprocess.run(add_repo_cmd, shell=True)
-    print("stackrox-oss helm repository added")
-    subprocess.run(update_repo_cmd, shell=True)
-    print("stackrox-oss helm repository update")
 
 def main(argv):
-    n = int(argv[1]) if len(argv)>1 else 4
-    update_helm_repo()
-    latestversions = get_latest_release_versions(n)
-    print(f"Last {n} versions:")
-    print("\n".join(latestversions))
+    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+    n = int(argv[1]) if len(argv) > 1 else 4
+    helm_versions = get_latest_helm_chart_versions("stackrox-secured-cluster-services", n)
+    logging.info(f"Helm chart versions for the latest {n} releases:")
+    print("\n".join(helm_versions))
 
-if (__name__ == "__main__"):
+
+def get_latest_helm_chart_versions(chart_name, num_releases):
+    add_helm_repo()
+    try:
+        update_helm_repo()
+        return __get_latest_helm_chart_versions(chart_name, num_releases)
+    finally:
+        remove_helm_repo()
+
+
+def __get_latest_helm_chart_versions(chart_name, num_releases):
+    charts = read_charts()
+    logging.info(f"Discovered total {len(charts)} charts")
+
+    filtered_charts = filter_charts_by_name(charts, chart_name)
+    logging.info(f"Found {len(filtered_charts)} charts with the given name {chart_name}")
+
+    latest_charts = get_latest_chart_for_each_release(filtered_charts)[:num_releases]
+    logging.debug(f"Identified these charts as {num_releases} latest: {latest_charts}")
+
+    return [c["version"] for c in latest_charts]
+
+
+def read_charts():
+    json_str = run_command(search_cmd, log_stdout=False)
+    charts_from_json = json.loads(json_str)
+
+    release_charts = [c for c in charts_from_json if is_release_version(c["app_version"])]
+
+    for entry in release_charts:
+        entry["parsed_app_version"] = parse_version(entry["app_version"])
+
+    return release_charts
+
+
+def is_release_version(version):
+    return re.search(r"^\d+\.\d+\.\d+$", version) is not None
+
+
+def parse_version(version_str):
+    nums = [int(s) for s in version_str.split(".")]
+    return Version(major=nums[0], minor=nums[1], patch=nums[2])
+
+
+def filter_charts_by_name(charts, chart_name):
+    return [c for c in charts if c["name"] == f"{helm_repo_name}/{chart_name}"]
+
+
+def get_latest_chart_for_each_release(charts):
+    sorted_charts = sorted(charts, key=lambda x: x["parsed_app_version"], reverse=True)
+
+    result = []
+    release = None
+
+    for chart in sorted_charts:
+        chart_release = version_to_release(chart["parsed_app_version"])
+        if chart_release != release:
+            result.append(chart)
+            release = chart_release
+
+    return result
+
+
+def version_to_release(version):
+    return Release(major=version.major, minor=version.minor)
+
+
+def add_helm_repo():
+    logging.info("Adding temp helm repository...")
+    run_command(add_repo_cmd)
+
+
+def update_helm_repo():
+    logging.info("Updating temp helm repository...")
+    run_command(update_repo_cmd)
+
+
+def remove_helm_repo():
+    logging.info("Removing temp helm repository...")
+    run_command(remove_repo_cmd)
+
+
+def run_command(command, log_stdout=True):
+    result = subprocess.run(command, shell=True, encoding='utf-8',
+                            stdin=subprocess.DEVNULL, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    stdout = format_command_output("Stdout", result.stdout) if log_stdout else ""
+    stderr = format_command_output("Stderr", result.stderr)
+    logging.debug(f"Got exit code {result.returncode} for command: {command}{stdout}{stderr}")
+
+    result.check_returncode()
+
+    return result.stdout
+
+
+def format_command_output(name, output):
+    out_no_trailing_newline = output.rstrip()
+    if not out_no_trailing_newline:
+        return ""
+    prefix = "\n" if len(out_no_trailing_newline.splitlines()) > 1 else " "
+    return f"\n{name}:{prefix}{out_no_trailing_newline}"
+
+
+if __name__ == "__main__":
     main(sys.argv)
