98f557ca4264f1cc8ffa23e0f3e458e35beb9f7d
Mandar Darwatkar <mandar@stackrox.com>
2023-05-05 10:28:01 -0700

    [ROX-16719][ROX-16802][ROX-13858] Get all distro tuples for CVE (#5892)
    * Get all distro tuples for CVE

--- central/views/imagecve/types.go
+++ central/views/imagecve/types.go
@@ -14,6 +14,7 @@ import (
 //go:generate mockgen-wrapper
 type CveCore interface {
 	GetCVE() string
+	GetCVEIDs() []string
 	GetImagesBySeverity() common.ResourceCountByCVESeverity
 	GetTopCVSS() float32
 	GetAffectedImages() int
