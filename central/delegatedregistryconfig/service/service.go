ff2833b8d95903126934227005a1499eb1e37de2
David Caravello <119438707+dcaravel@users.noreply.github.com>
2023-05-05 19:11:58 -0500

    tests, validation

--- central/delegatedregistryconfig/service/service.go
+++ central/delegatedregistryconfig/service/service.go
@@ -2,11 +2,12 @@ package service
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/pkg/errors"
 	cluster "github.com/stackrox/rox/central/cluster/datastore"
 
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
-	"github.com/pkg/errors"
 	"github.com/stackrox/rox/central/delegatedregistryconfig/datastore"
 	"github.com/stackrox/rox/central/role/resources"
 	v1 "github.com/stackrox/rox/generated/api/v1"
@@ -122,7 +123,7 @@ func (s *serviceImpl) PutConfig(ctx context.Context, config *storage.DelegatedRe
 
 	log.Debugf("PutConfig %T [%+v]", config, *config)
 
-	if err := s.validate(config); err != nil {
+	if err := s.validate(ctx, config); err != nil {
 		return nil, errors.Wrap(errox.InvalidArgs, err.Error())
 	}
 
@@ -133,22 +134,48 @@ func (s *serviceImpl) PutConfig(ctx context.Context, config *storage.DelegatedRe
 	return config, nil
 }
 
-func (s *serviceImpl) validate(config *storage.DelegatedRegistryConfig) error {
+func (s *serviceImpl) validate(ctx context.Context, config *storage.DelegatedRegistryConfig) error {
 	if config == nil {
-		// this block not reachable via GRPC-gateway invocations FYI
+		// this block not reachable via GRPC-gateway invocations
 		return errors.New("config missing")
 	}
 
-	errorList := errorhelpers.NewErrorList("Validation")
-	if config.EnabledFor != storage.DelegatedRegistryConfig_NONE {
-		// a default cluster is required if EnabledFor is set to something other than NONE
-		if config.DefaultClusterId == "" {
-			errorList.AddStrings("defaultClusterId required if enabledFor != NONE")
-		}
+	if config.EnabledFor == storage.DelegatedRegistryConfig_NONE {
+		// ignore rest of config
+		return nil
 	}
 
-	// TODO: also check if cluster IDs are valid
-	// TODO: also check if only clusters that have scanner and delegated scanning capability are used
+	errorList := errorhelpers.NewErrorList("Validation")
+	// validate the default cluster id
+	if config.DefaultClusterId == "" {
+		errorList.AddStrings("defaultClusterId required if enabledFor != NONE")
+		return errorList.ToError()
+	}
+
+	// get the valid clusters
+	validClusters, err := s.getValidClustersIds(ctx)
+	if err != nil {
+		return errors.Wrap(err, "unable to validate config")
+	}
+
+	_, isValid := validClusters[config.DefaultClusterId]
+	if !isValid {
+		errorList.AddStrings(fmt.Sprintf("default cluster %q is not a valid cluster", config.DefaultClusterId))
+	}
+
+	// validate the registries / clusters
+	for i, r := range config.Registries {
+		if r.ClusterId != "" {
+			_, isValid := validClusters[r.ClusterId]
+			if !isValid {
+				errorList.AddStrings(fmt.Sprintf("cluster %q is not valid at index %v", r.ClusterId, i))
+			}
+		}
+
+		if r.RegistryPath == "" {
+			errorList.AddStrings(fmt.Sprintf("missing registry path at index %v", i))
+		}
+	}
 
 	return errorList.ToError()
 }
@@ -165,12 +192,31 @@ func (s *serviceImpl) getClusters(ctx context.Context) ([]*v1.DelegatedRegistryC
 
 	var res []*v1.DelegatedRegistryCluster
 	for _, c := range clusters {
+
+		valid := c.GetHealthStatus().GetScannerHealthStatus() == storage.ClusterHealthStatus_HEALTHY
+
 		res = append(res, &v1.DelegatedRegistryCluster{
 			Id:      c.Id,
 			Name:    c.Name,
-			IsValid: true,
+			IsValid: valid,
 		})
 	}
 
 	return res, nil
+}
+
+func (s *serviceImpl) getValidClustersIds(ctx context.Context) (map[string]string, error) {
+	clusters, err := s.getClusters(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	r := map[string]string{}
+	for _, c := range clusters {
+		if c.IsValid {
+			r[c.Id] = c.Name
+		}
+	}
+
+	return r, nil
 }
--- central/delegatedregistryconfig/service/service_test.go
+++ central/delegatedregistryconfig/service/service_test.go
@@ -1,27 +1,98 @@
 package service
 
 import (
+	"context"
 	"testing"
 
+	"github.com/golang/mock/gomock"
+	clusterDSMocks "github.com/stackrox/rox/central/cluster/datastore/mocks"
+	v1 "github.com/stackrox/rox/generated/api/v1"
 	"github.com/stackrox/rox/generated/storage"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestValidate(t *testing.T) {
 	var cfg *storage.DelegatedRegistryConfig
 	var err error
 
-	s := serviceImpl{}
+	clustersDS := clusterDSMocks.NewMockDataStore(gomock.NewController(t))
+	clustersDS.EXPECT().GetClusters(gomock.Any()).Return(nil, nil).AnyTimes()
 
-	err = s.validate(cfg)
+	s := serviceImpl{clusterDataStore: clustersDS}
+
+	err = s.validate(context.Background(), cfg)
 	assert.ErrorContains(t, err, "config missing")
 
 	cfg = &storage.DelegatedRegistryConfig{}
 	cfg.EnabledFor = storage.DelegatedRegistryConfig_ALL
-	err = s.validate(cfg)
+	err = s.validate(context.Background(), cfg)
 	assert.ErrorContains(t, err, "defaultClusterId required")
 
 	cfg.EnabledFor = storage.DelegatedRegistryConfig_SPECIFIC
-	err = s.validate(cfg)
+	err = s.validate(context.Background(), cfg)
 	assert.ErrorContains(t, err, "defaultClusterId required")
+}
+
+func TestGetClusters(t *testing.T) {
+	var err error
+	var clusters []*v1.DelegatedRegistryCluster
+
+	clustersDS := clusterDSMocks.NewMockDataStore(gomock.NewController(t))
+
+	s := serviceImpl{clusterDataStore: clustersDS}
+
+	healthStatusScannerHealthy := &storage.ClusterHealthStatus{ScannerHealthStatus: storage.ClusterHealthStatus_HEALTHY}
+	healthStatusScannerDegraded := &storage.ClusterHealthStatus{ScannerHealthStatus: storage.ClusterHealthStatus_DEGRADED}
+	healthStatusEmpty := &storage.ClusterHealthStatus{}
+
+	clustersDS.EXPECT().GetClusters(gomock.Any()).Return(nil, nil)
+	clusters, err = s.getClusters(context.Background())
+	assert.NoError(t, err)
+	assert.Empty(t, clusters)
+
+	clustersDS.EXPECT().GetClusters(gomock.Any()).Return([]*storage.Cluster{}, nil)
+	clusters, err = s.getClusters(context.Background())
+	assert.NoError(t, err)
+	assert.Empty(t, clusters)
+
+	cluster := &storage.Cluster{Id: "id", Name: "fake", HealthStatus: nil}
+	clustersDS.EXPECT().GetClusters(gomock.Any()).Return([]*storage.Cluster{cluster}, nil)
+	clusters, err = s.getClusters(context.Background())
+	assert.NoError(t, err)
+	require.Len(t, clusters, 1)
+	assert.False(t, clusters[0].IsValid)
+
+	cluster.HealthStatus = healthStatusEmpty
+	clustersDS.EXPECT().GetClusters(gomock.Any()).Return([]*storage.Cluster{cluster}, nil)
+	clusters, err = s.getClusters(context.Background())
+	assert.NoError(t, err)
+	require.Len(t, clusters, 1)
+	assert.False(t, clusters[0].IsValid)
+
+	cluster.HealthStatus = healthStatusScannerDegraded
+	clustersDS.EXPECT().GetClusters(gomock.Any()).Return([]*storage.Cluster{cluster}, nil)
+	clusters, err = s.getClusters(context.Background())
+	assert.NoError(t, err)
+	require.Len(t, clusters, 1)
+	assert.False(t, clusters[0].IsValid)
+
+	cluster.HealthStatus = healthStatusScannerHealthy
+	clustersDS.EXPECT().GetClusters(gomock.Any()).Return([]*storage.Cluster{cluster}, nil)
+	clusters, err = s.getClusters(context.Background())
+	assert.NoError(t, err)
+	require.Len(t, clusters, 1)
+	assert.True(t, clusters[0].IsValid)
+
+	cluster1 := &storage.Cluster{Id: "id1", HealthStatus: healthStatusScannerHealthy}
+	cluster2 := &storage.Cluster{Id: "id2", HealthStatus: healthStatusScannerDegraded}
+	clustersDS.EXPECT().GetClusters(gomock.Any()).Return([]*storage.Cluster{cluster1, cluster2}, nil)
+	clusters, err = s.getClusters(context.Background())
+	assert.NoError(t, err)
+	require.Len(t, clusters, 2)
+	assert.True(t, clusters[0].IsValid)
+	assert.Equal(t, clusters[0].Id, "id1")
+	assert.False(t, clusters[1].IsValid)
+	assert.Equal(t, clusters[1].Id, "id2")
+
 }
