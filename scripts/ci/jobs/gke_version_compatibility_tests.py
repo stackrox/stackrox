e61cae7c677bba859bdb8df182e3393d0fe5ba7a
Misha Sugakov <misha@stackrox.com>
2023-05-08 11:57:31 +0200

    Switch version compatibility tests to modified helm version detection

--- scripts/ci/jobs/gke_version_compatibility_tests.py
+++ scripts/ci/jobs/gke_version_compatibility_tests.py
@@ -4,7 +4,7 @@
 Run version compatibility tests
 """
 import os
-from get_latest_release_versions import update_helm_repo, get_latest_release_versions
+from lookup_previous_helm_releases import get_previously_released_helm_chart_versions
 from compatibility_test import make_compatibility_test_runner
 from clusters import GKECluster
 
@@ -15,8 +15,7 @@ class SensorVersionsFailure(Exception):
 os.environ["ORCHESTRATOR_FLAVOR"] = "k8s"
 os.environ["ROX_POSTGRES_DATASTORE"] = "true"
 
-update_helm_repo()
-chart_versions=get_latest_release_versions(4)
+chart_versions = get_previously_released_helm_chart_versions("stackrox-secured-cluster-services", 4)
 
 gkecluster=GKECluster("compat-test")
 
