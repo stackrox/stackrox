f2748eddfe41366c1609427b74523a9f16ae6d20
Yann Brillouet <ybrillou@redhat.com>
2023-05-10 11:45:59 +0200

    Adapt to DB interface extraction

--- migrator/migrations/m_181_to_182_group_role_permission_with_access_one/permissionsetstore/store.go
+++ migrator/migrations/m_181_to_182_group_role_permission_with_access_one/permissionsetstore/store.go
@@ -49,12 +49,12 @@ type Store interface {
 }
 
 type storeImpl struct {
-	db    *postgres.DB
+	db    postgres.DB
 	mutex sync.RWMutex
 }
 
 // New returns a new Store instance using the provided sql instance.
-func New(db *postgres.DB) Store {
+func New(db postgres.DB) Store {
 	return &storeImpl{
 		db: db,
 	}
@@ -62,7 +62,7 @@ func New(db *postgres.DB) Store {
 
 //// Helper functions
 
-func insertIntoPermissionSets(ctx context.Context, batch *pgx.Batch, obj *storage.PermissionSet) error {
+func insertIntoPermissionSets(_ context.Context, batch *pgx.Batch, obj *storage.PermissionSet) error {
 
 	serialized, marshalErr := obj.Marshal()
 	if marshalErr != nil {
@@ -149,7 +149,7 @@ func (s *storeImpl) copyFromPermissionSets(ctx context.Context, tx *postgres.Tx,
 	return err
 }
 
-func (s *storeImpl) acquireConn(ctx context.Context, op ops.Op, typ string) (*postgres.Conn, func(), error) {
+func (s *storeImpl) acquireConn(ctx context.Context, _ ops.Op, _ string) (*postgres.Conn, func(), error) {
 	conn, err := s.db.Acquire(ctx)
 	if err != nil {
 		return nil, nil, err
