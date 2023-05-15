5c2ab20a36dd9db5dd4c1cf9e91209e1195d6bb9
Charmik Sheth <101146970+charmik-redhat@users.noreply.github.com>
2023-05-15 10:57:48 -0700

    [ROX-15967] : Add data model changes for report config and create report config endpoint (#5919)

--- central/reportconfigurations/service/service_impl_postgres_test.go
+++ central/reportconfigurations/service/service_impl_postgres_test.go
@@ -8,45 +8,45 @@ import (
 	notifierMocks "github.com/stackrox/rox/central/notifier/datastore/mocks"
 	"github.com/stackrox/rox/central/reportconfigurations/datastore/mocks"
 	managerMocks "github.com/stackrox/rox/central/reports/manager/mocks"
-	accessScopeMocks "github.com/stackrox/rox/central/role/datastore/mocks"
+	collectionMocks "github.com/stackrox/rox/central/resourcecollection/datastore/mocks"
 	v1 "github.com/stackrox/rox/generated/api/v1"
 	"github.com/stackrox/rox/pkg/env"
 	"github.com/stackrox/rox/pkg/fixtures"
 	"github.com/stretchr/testify/suite"
 )
 
-func TestReportConfigurationService(t *testing.T) {
-	suite.Run(t, new(TestReportConfigurationServiceTestSuite))
+func TestReportConfigurationServicePostgres(t *testing.T) {
+	suite.Run(t, new(ReportConfigurationServicePostgresTestSuite))
 }
 
-type TestReportConfigurationServiceTestSuite struct {
+type ReportConfigurationServicePostgresTestSuite struct {
 	suite.Suite
 	service               Service
 	reportConfigDatastore *mocks.MockDataStore
 	notifierDatastore     *notifierMocks.MockDataStore
-	accessScopeStore      *accessScopeMocks.MockDataStore
+	collectionDatastore   *collectionMocks.MockDataStore
 	manager               *managerMocks.MockManager
 	mockCtrl              *gomock.Controller
 }
 
-func (s *TestReportConfigurationServiceTestSuite) SetupTest() {
+func (s *ReportConfigurationServicePostgresTestSuite) SetupTest() {
 	s.mockCtrl = gomock.NewController(s.T())
-	if env.PostgresDatastoreEnabled.BooleanSetting() {
-		s.T().Skip("Skip test when postgres is enabled")
+	if !env.PostgresDatastoreEnabled.BooleanSetting() {
+		s.T().Skip("Skip postgres tests")
 		s.T().SkipNow()
 	}
 	s.reportConfigDatastore = mocks.NewMockDataStore(s.mockCtrl)
 	s.notifierDatastore = notifierMocks.NewMockDataStore(s.mockCtrl)
-	s.accessScopeStore = accessScopeMocks.NewMockDataStore(s.mockCtrl)
+	s.collectionDatastore = collectionMocks.NewMockDataStore(s.mockCtrl)
 	s.manager = managerMocks.NewMockManager(s.mockCtrl)
-	s.service = New(s.reportConfigDatastore, s.notifierDatastore, s.accessScopeStore, nil, s.manager)
+	s.service = New(s.reportConfigDatastore, s.notifierDatastore, nil, s.collectionDatastore, s.manager)
 }
 
-func (s *TestReportConfigurationServiceTestSuite) TearDownTest() {
+func (s *ReportConfigurationServicePostgresTestSuite) TearDownTest() {
 	s.mockCtrl.Finish()
 }
 
-func (s *TestReportConfigurationServiceTestSuite) TestAddValidReportConfiguration() {
+func (s *ReportConfigurationServicePostgresTestSuite) TestAddValidReportConfiguration() {
 	ctx := context.Background()
 
 	reportConfig := fixtures.GetValidReportConfiguration()
@@ -54,7 +54,7 @@ func (s *TestReportConfigurationServiceTestSuite) TestAddValidReportConfiguratio
 	s.reportConfigDatastore.EXPECT().GetReportConfiguration(ctx, reportConfig.GetId()).Return(reportConfig, true, nil)
 
 	s.notifierDatastore.EXPECT().Exists(ctx, gomock.Any()).Return(true, nil).AnyTimes()
-	s.accessScopeStore.EXPECT().AccessScopeExists(ctx, gomock.Any()).Return(true, nil).AnyTimes()
+	s.collectionDatastore.EXPECT().Exists(ctx, gomock.Any()).Return(true, nil).AnyTimes()
 
 	s.manager.EXPECT().Upsert(ctx, reportConfig).Return(nil)
 	_, err := s.service.PostReportConfiguration(ctx, &v1.PostReportConfigurationRequest{
@@ -63,11 +63,11 @@ func (s *TestReportConfigurationServiceTestSuite) TestAddValidReportConfiguratio
 	s.NoError(err)
 }
 
-func (s *TestReportConfigurationServiceTestSuite) TestAddInvalidValidReportConfigurations() {
+func (s *ReportConfigurationServicePostgresTestSuite) TestAddInvalidValidReportConfigurations() {
 	ctx := context.Background()
 
 	s.notifierDatastore.EXPECT().Exists(ctx, gomock.Any()).Return(true, nil).AnyTimes()
-	s.accessScopeStore.EXPECT().AccessScopeExists(ctx, gomock.Any()).Return(true, nil).AnyTimes()
+	s.collectionDatastore.EXPECT().Exists(ctx, gomock.Any()).Return(true, nil).AnyTimes()
 
 	noNotifierReportConfig := fixtures.GetInvalidReportConfigurationNoNotifier()
 	_, err := s.service.PostReportConfiguration(ctx, &v1.PostReportConfigurationRequest{
@@ -99,6 +99,12 @@ func (s *TestReportConfigurationServiceTestSuite) TestAddInvalidValidReportConfi
 	})
 	s.Error(err)
 
+	dailyScheduleReportConfig := fixtures.GetInvalidReportConfigurationDailySchedule()
+	_, err = s.service.PostReportConfiguration(ctx, &v1.PostReportConfigurationRequest{
+		ReportConfig: dailyScheduleReportConfig,
+	})
+	s.Error(err)
+
 	incorrectEmailReportConfig := fixtures.GetInvalidReportConfigurationIncorrectEmail()
 	_, err = s.service.PostReportConfiguration(ctx, &v1.PostReportConfigurationRequest{
 		ReportConfig: incorrectEmailReportConfig,
@@ -106,11 +112,11 @@ func (s *TestReportConfigurationServiceTestSuite) TestAddInvalidValidReportConfi
 	s.Error(err)
 }
 
-func (s *TestReportConfigurationServiceTestSuite) TestUpdateInvalidValidReportConfigurations() {
+func (s *ReportConfigurationServicePostgresTestSuite) TestUpdateInvalidValidReportConfigurations() {
 	ctx := context.Background()
 
 	s.notifierDatastore.EXPECT().Exists(ctx, gomock.Any()).Return(true, nil).AnyTimes()
-	s.accessScopeStore.EXPECT().AccessScopeExists(ctx, gomock.Any()).Return(true, nil).AnyTimes()
+	s.collectionDatastore.EXPECT().Exists(ctx, gomock.Any()).Return(true, nil).AnyTimes()
 
 	noNotifierReportConfig := fixtures.GetInvalidReportConfigurationNoNotifier()
 	_, err := s.service.UpdateReportConfiguration(ctx, &v1.UpdateReportConfigurationRequest{
@@ -137,11 +143,11 @@ func (s *TestReportConfigurationServiceTestSuite) TestUpdateInvalidValidReportCo
 	s.Error(err)
 }
 
-func (s *TestReportConfigurationServiceTestSuite) TestNotifierDoesNotExist() {
+func (s *ReportConfigurationServicePostgresTestSuite) TestNotifierDoesNotExist() {
 	ctx := context.Background()
 
 	s.notifierDatastore.EXPECT().Exists(ctx, gomock.Any()).Return(false, nil)
-	s.accessScopeStore.EXPECT().AccessScopeExists(ctx, gomock.Any()).Return(true, nil)
+	s.collectionDatastore.EXPECT().Exists(ctx, gomock.Any()).Return(true, nil).AnyTimes()
 
 	reportConfig := fixtures.GetValidReportConfiguration()
 	_, err := s.service.PostReportConfiguration(ctx, &v1.PostReportConfigurationRequest{
@@ -150,11 +156,11 @@ func (s *TestReportConfigurationServiceTestSuite) TestNotifierDoesNotExist() {
 	s.Error(err)
 }
 
-func (s *TestReportConfigurationServiceTestSuite) TestAccessScopeDoesNotExist() {
+func (s *ReportConfigurationServicePostgresTestSuite) TestAccessScopeDoesNotExist() {
 	ctx := context.Background()
 
 	s.notifierDatastore.EXPECT().Exists(ctx, gomock.Any()).Return(true, nil).AnyTimes()
-	s.accessScopeStore.EXPECT().AccessScopeExists(ctx, gomock.Any()).Return(false, nil)
+	s.collectionDatastore.EXPECT().Exists(ctx, gomock.Any()).Return(false, nil)
 
 	reportConfig := fixtures.GetValidReportConfiguration()
 	_, err := s.service.PostReportConfiguration(ctx, &v1.PostReportConfigurationRequest{
@@ -163,7 +169,7 @@ func (s *TestReportConfigurationServiceTestSuite) TestAccessScopeDoesNotExist() 
 	s.Error(err)
 }
 
-func (s *TestReportConfigurationServiceTestSuite) TestNoMailingAddresses() {
+func (s *ReportConfigurationServicePostgresTestSuite) TestNoMailingAddresses() {
 	ctx := context.Background()
 	reportConfig := fixtures.GetValidReportConfiguration()
 	reportConfig.GetEmailConfig().MailingLists = []string{}
