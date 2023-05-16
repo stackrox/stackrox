ff7f0ce97dfe4db34fc1177078bd7f34147ea24c
shireenf-ibm <shireenf@il.ibm.com>
2023-05-16 13:26:22 +0300

    fix

--- roxctl/analyze/netpol/cta.go
+++ roxctl/analyze/netpol/cta.go
@@ -63,7 +63,7 @@ func (cmd *analyzeNetpolCommand) ouputConnList(connsStr string) error {
 
 func writeFile(outputPath string, content string) error {
 	if err := os.MkdirAll(filepath.Dir(outputPath), 0755); err != nil {
-		return errors.Wrapf(err, "error creating directory for file %q", filepath.Base(outputPath))
+		return errors.Wrapf(err, "error creating directory for file %q", outputPath)
 	}
 	return errors.Wrap(os.WriteFile(outputPath, []byte(content), os.FileMode(0644)), "error writing file")
 }
