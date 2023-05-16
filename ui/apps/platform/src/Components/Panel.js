25962783e1a49ad03e295301b13ad4f0e7493bc4
Mark Pedrotti <mark@stackrox.com>
2023-05-16 16:14:44 -0400

    Delete obsolete Panel component that had tracking-wide

--- ui/apps/platform/src/Components/Panel.js
+++ ui/apps/platform/src/Components/Panel.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Tooltip } from '@patternfly/react-core';
-
-import CloseButton from './CloseButton';
 
 /*
  * PageBody is sibling of PageHeader and parent of main and side panels.
@@ -73,82 +70,3 @@ export function PanelBody({ children }) {
 }
 
 export const headerClassName = 'flex w-full min-h-14 border-b border-base-400';
-
-const Panel = (props) => (
-    <div className={`flex flex-col w-full ${props.className} h-full`} data-testid={props.id}>
-        <div className="flex-nowrap">
-            <div className={props.headerClassName}>
-                {props.leftButtons && (
-                    <div className="flex items-center pr-3 relative border-base-400 border-r hover:bg-primary-300 hover:border-primary-300">
-                        {props.leftButtons}
-                    </div>
-                )}
-                {props.headerTextComponent ? (
-                    <div className="flex" data-testid={`${props.id}-header`}>
-                        {props.headerTextComponent}
-                    </div>
-                ) : (
-                    <div
-                        className={`overflow-hidden mx-4 flex text-base-600 items-center tracking-wide leading-normal font-700 min-w-24 ${
-                            props.isUpperCase ? 'uppercase' : 'capitalize'
-                        }`}
-                        data-testid={`${props.id}-header`}
-                    >
-                        <Tooltip content={props.header}>
-                            <div className="line-clamp break-all">{props.header}</div>
-                        </Tooltip>
-                    </div>
-                )}
-
-                <div
-                    className={`flex items-center justify-end relative flex-1 ${
-                        props.onClose ? 'pl-3' : 'px-3'
-                    }`}
-                >
-                    {props.headerComponents && props.headerComponents}
-                    {props.onClose && (
-                        <CloseButton
-                            onClose={props.onClose}
-                            className={props.closeButtonClassName}
-                            iconColor={props.closeButtonIconColor}
-                        />
-                    )}
-                </div>
-            </div>
-        </div>
-        <div className={`h-full overflow-y-auto ${props.bodyClassName}`}>{props.children}</div>
-    </div>
-);
-
-Panel.propTypes = {
-    id: PropTypes.string,
-    header: PropTypes.string,
-    headerTextComponent: PropTypes.element,
-    headerClassName: PropTypes.string,
-    bodyClassName: PropTypes.string,
-    className: PropTypes.string,
-    children: PropTypes.node.isRequired,
-    onClose: PropTypes.func,
-    closeButtonClassName: PropTypes.string,
-    closeButtonIconColor: PropTypes.string,
-    headerComponents: PropTypes.element,
-    leftButtons: PropTypes.node,
-    isUpperCase: PropTypes.bool,
-};
-
-Panel.defaultProps = {
-    id: 'panel',
-    header: ' ',
-    headerTextComponent: null,
-    headerClassName,
-    bodyClassName: '',
-    className: '',
-    onClose: null,
-    closeButtonClassName: 'border-base-400 border-l',
-    closeButtonIconColor: '',
-    headerComponents: null,
-    leftButtons: null,
-    isUpperCase: true,
-};
-
-export default Panel;
