3311a0b64117263cc9776a88e2fd8efa19be25a0
Frederico Bittencourt <fbittenc@redhat.com>
2023-05-16 18:01:26 +0200

    ROX-17106: local-sensor support for connecting to Central instances (#6024)

--- sensor/common/sensor/helmconfig/env.go
+++ sensor/common/sensor/helmconfig/env.go
@@ -6,4 +6,10 @@ var (
 	// HelmConfigFingerprint is the environment variable that indicates the fingerprint of the helm cluster
 	// config to be used.
 	HelmConfigFingerprint = env.RegisterSetting("ROX_HELM_CLUSTER_CONFIG_FP")
+
+	// HelmConfigFile allows to override the helm config.yaml secret file.
+	HelmConfigFile = env.RegisterSetting("ROX_HELM_CONFIG_FILE_OVERRIDE", env.WithDefault("/run/secrets/stackrox.io/helm-cluster-config/config.yaml"))
+
+	// HelmClusterNameFile allows to override helm effective cluster name file.
+	HelmClusterNameFile = env.RegisterSetting("ROX_HELM_CLUSTER_NAME_FILE_OVERRIDE", env.WithDefault("/run/secrets/stackrox.io/helm-effective-cluster-name/cluster-name"))
 )
