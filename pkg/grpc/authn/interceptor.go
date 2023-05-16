c18d93d307177f82e2623942375663514370f972
Yann Brillouet <ybrillou@redhat.com>
2023-05-16 11:47:19 +0200

    revert to on-first use initialization of rate-limited logger

--- pkg/grpc/authn/interceptor.go
+++ pkg/grpc/authn/interceptor.go
@@ -10,7 +10,7 @@ import (
 	"github.com/stackrox/rox/pkg/errox"
 	"github.com/stackrox/rox/pkg/grpc/requestinfo"
 	"github.com/stackrox/rox/pkg/logging"
-	// "github.com/stackrox/rox/pkg/sync"
+	"github.com/stackrox/rox/pkg/sync"
 	"gopkg.in/square/go-jose.v2/jwt"
 )
 
@@ -21,7 +21,7 @@ const (
 )
 
 var (
-	// once sync.Once
+	once sync.Once
 	log  *logging.RateLimitedLogger
 )
 
@@ -29,7 +29,6 @@ type contextUpdater struct {
 	extractor IdentityExtractor
 }
 
-/*
 func getRateLimitedLogger() *logging.RateLimitedLogger {
 	once.Do(func() {
 		log = logging.NewRateLimitLogger(
@@ -42,28 +41,15 @@ func getRateLimitedLogger() *logging.RateLimitedLogger {
 	})
 	return log
 }
-*/
-
-func init() {
-	log = logging.NewRateLimitLogger(
-		logging.LoggerForModule(),
-		cacheSize,
-		1,
-		rateLimitFrequency,
-		logBurstSize,
-	)
-}
 
 func (u contextUpdater) updateContext(ctx context.Context) (context.Context, error) {
 	ri := requestinfo.FromContext(ctx)
 	id, err := u.extractor.IdentityForRequest(ctx, ri)
 	if err != nil {
 		if errors.Is(err, jwt.ErrExpired) {
-			// getRateLimitedLogger().Debugf("Cannot extract identity: token expired")
-			log.Debugf("Cannot extract identity: token expired")
+			getRateLimitedLogger().Debugf("Cannot extract identity: token expired")
 		} else {
-			// getRateLimitedLogger().WarnL(ri.Hostname, "Cannot extract identity: %v", err)
-			log.WarnL(ri.Hostname, "Cannot extract identity: %v", err)
+			getRateLimitedLogger().WarnL(ri.Hostname, "Cannot extract identity: %v", err)
 		}
 		// Ignore id value if error is not nil.
 		return context.WithValue(ctx, identityErrorContextKey{}, errox.NoCredentials.CausedBy(err)), nil
