6b52d7cc8899970e852edc1d4597e5b551363f8e
cdu <cdu@redhat.com>
2023-05-16 12:18:53 -0700

    stage

--- pkg/postgres/gorm/largeobject/large_objects.go
+++ pkg/postgres/gorm/largeobject/large_objects.go
@@ -12,7 +12,7 @@ import (
 //
 // For more details see: http://www.postgresql.org/docs/current/static/largeobjects.html
 type LargeObjects struct {
-	*gorm.DB
+	tx *gorm.DB
 }
 
 type Mode int32
@@ -24,38 +24,36 @@ const (
 
 // Create creates a new large object with an unused OID assigned
 func (o *LargeObjects) Create() (uint32, error) {
-	result := o.Raw("SELECT lo_create(?)", 0)
-	if result.Error != nil {
-		return 0, result.Error
+	o.tx = o.tx.Raw("SELECT lo_create(?)", 0)
+	if err := o.tx.Error; err != nil {
+		return 0, err
 	}
 	var oid uint32
-	result.Scan(&oid)
-	return oid, nil
+	o.tx = o.tx.Scan(&oid)
+	return oid, o.tx.Error
 }
 
 // Open opens an existing large object with the given mode. ctx will also be used for all operations on the opened large
 // object.
 func (o *LargeObjects) Open(oid uint32, mode Mode) (*LargeObject, error) {
 	var fd int32
-	result := o.Raw("select lo_open(?, ?)", oid, mode).Scan(&fd)
-	if result.Error != nil {
-		return nil, result.Error
+	o.tx = o.tx.Raw("select lo_open(?, ?)", oid, mode).Scan(&fd)
+	if err := o.tx.Error; err != nil {
+		return nil, err
 	}
-	return &LargeObject{fd: fd, tx: o.DB}, nil
+	return &LargeObject{fd: fd, tx: o.tx}, nil
 }
 
 // Unlink removes a large object from the database.
 func (o *LargeObjects) Unlink(oid uint32) error {
 	var count int32
-	result := o.Raw("select lo_unlink(?)", oid).Scan(&count)
-	if result.Error != nil {
-		return result.Error
+	o.tx = o.tx.Raw("select lo_unlink(?)", oid).Scan(&count)
+	if err := o.tx.Error; err != nil {
+		return err
 	}
-
 	if count != 1 {
 		return errors.New("failed to remove large object")
 	}
-
 	return nil
 }
 
@@ -63,10 +61,17 @@ func (o *LargeObjects) Upsert(oid uint32, r io.Reader) error {
 	obj, err := o.Open(oid, ModeWrite)
 	if err != nil {
 		return err
-	}
+	} /*
+		err = obj.Truncate(1)
+		if err != nil {
+			return err
+		}
+		if _, err = obj.Seek(0, io.SeekStart); err != nil {
+			return err
+		}*/
 	_, err = io.Copy(obj, r)
 
-	return err
+	return obj.Close()
 }
 
 func (o *LargeObjects) Get(oid uint32, w io.Writer) error {
@@ -93,9 +98,15 @@ type LargeObject struct {
 // Write writes p to the large object and returns the number of bytes written and an error if not all of p was written.
 func (o *LargeObject) Write(p []byte) (int, error) {
 	var n int
-	result := o.tx.Raw("select lowrite($1, $2)", o.fd, p).Scan(&n)
-	if result.Error != nil {
-		return n, result.Error
+	o.tx = o.tx.Raw("select lowrite($1, $2)", o.fd, p)
+	if err := o.tx.Error; err != nil {
+		return n, err
+	}
+	if err := o.tx.Scan(&n).Error; err != nil {
+		return n, err
+	}
+	if err := o.tx.Error; err != nil {
+		return n, err
 	}
 
 	if n < 0 {
@@ -107,11 +118,17 @@ func (o *LargeObject) Write(p []byte) (int, error) {
 
 // Read reads up to len(p) bytes into p returning the number of bytes read.
 func (o *LargeObject) Read(p []byte) (n int, err error) {
-	var res []byte
-	result := o.tx.Raw("select loread($1, $2)", o.fd, len(p)).Scan(&res)
+	var res []byte = make([]byte, 0, len(p))
+	o.tx = o.tx.Raw("select loread($1, $2)", o.fd, len(p))
+	if err = o.tx.Error; err != nil {
+		return 0, err
+	}
+	if err = o.tx.Row().Scan(&res); err != nil {
+		return 0, err
+	}
 	copy(p, res)
-	if result.Error != nil {
-		return len(res), result.Error
+	if err = o.tx.Error; err != nil {
+		return len(res), err
 	}
 
 	if len(res) < len(p) {
@@ -123,25 +140,25 @@ func (o *LargeObject) Read(p []byte) (n int, err error) {
 // Seek moves the current location pointer to the new location specified by offset.
 func (o *LargeObject) Seek(offset int64, whence int) (int64, error) {
 	var n int64
-	result := o.tx.Raw("select lo_lseek64($1, $2, $3)", o.fd, offset, whence).Scan(&n)
-	return n, result.Error
+	o.tx = o.tx.Raw("select lo_lseek64($1, $2, $3)", o.fd, offset, whence).Scan(&n)
+	return n, o.tx.Error
 }
 
 // Tell returns the current read or write location of the large object descriptor.
 func (o *LargeObject) Tell() (int64, error) {
 	var n int64
-	result := o.tx.Raw("select lo_tell64($1)", o.fd).Scan(&n)
-	return n, result.Error
+	o.tx = o.tx.Raw("select lo_tell64($1)", o.fd).Scan(&n)
+	return n, o.tx.Error
 }
 
 // Truncate the large object to size.
 func (o *LargeObject) Truncate(size int64) (err error) {
-	result := o.tx.Raw("select lo_truncate64(?, ?)", o.fd, size)
-	return result.Error
+	o.tx = o.tx.Raw("select lo_truncate64(?, ?)", o.fd, size)
+	return o.tx.Error
 }
 
 // Close the large object descriptor.
 func (o *LargeObject) Close() error {
-	result := o.tx.Raw("select lo_close(?)", o.fd)
-	return result.Error
+	o.tx = o.tx.Raw("select lo_close(?)", o.fd)
+	return o.tx.Error
 }
--- pkg/postgres/gorm/largeobject/utils_test.go
+++ pkg/postgres/gorm/largeobject/utils_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"crypto/rand"
+	"database/sql"
 	"testing"
 
 	pghelper "github.com/stackrox/rox/migrator/migrations/postgreshelper"
@@ -36,13 +37,325 @@ func (s *gormUtilsTestSuite) TestMigration() {
 	_, err := rand.Read(randomData)
 	s.NoError(err)
 
-	reader := bytes.NewBuffer(randomData)
 	gormDB := s.db.GetGormDB()
-	tx := gormDB.Begin()
+
+	reader := bytes.NewBuffer(randomData)
+	tx := gormDB.Begin(&sql.TxOptions{Isolation: sql.LevelRepeatableRead})
 	los := LargeObjects{tx}
 	oid, err := los.Create()
 	s.NoError(err)
 	err = los.Upsert(oid, reader)
 	s.NoError(err)
 	s.NoError(tx.Commit().Error)
+
+	tx = gormDB.Begin(&sql.TxOptions{Isolation: sql.LevelRepeatableRead})
+	los = LargeObjects{tx}
+	writer := bytes.NewBuffer([]byte{})
+	s.Require().NoError(los.Get(oid, writer))
+
+	s.Require().Equal(randomData, writer.Bytes())
+	reader = bytes.NewBuffer([]byte("hi"))
+	tx = gormDB.Begin(&sql.TxOptions{Isolation: sql.LevelRepeatableRead})
+	los = LargeObjects{tx}
+	err = los.Upsert(oid, reader)
+	s.NoError(err)
+	s.NoError(tx.Commit().Error)
+
+	tx = gormDB.Begin(&sql.TxOptions{Isolation: sql.LevelRepeatableRead})
+	los = LargeObjects{tx}
+	writer = bytes.NewBuffer([]byte{})
+	s.Require().NoError(los.Get(oid, writer))
+	s.Require().Equal([]byte("hi"), writer.Bytes())
 }
+
+/*
+func TestLargeObjects(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	conn, err := pgx.Connect(ctx, os.Getenv("PGX_TEST_DATABASE"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	skipCockroachDB(t, conn, "Server does support large objects")
+
+	tx, err := conn.Begin(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testLargeObjects(t, ctx, tx)
+}
+
+func TestLargeObjectsPreferSimpleProtocol(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	config, err := pgx.ParseConfig(os.Getenv("PGX_TEST_DATABASE"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	config.PreferSimpleProtocol = true
+
+	conn, err := pgx.ConnectConfig(ctx, config)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	skipCockroachDB(t, conn, "Server does support large objects")
+
+	tx, err := conn.Begin(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testLargeObjects(t, ctx, tx)
+}
+
+func testLargeObjects(t *testing.T, ctx context.Context, tx pgx.Tx) {
+	lo := tx.LargeObjects()
+
+	id, err := lo.Create(ctx, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	obj, err := lo.Open(ctx, id, pgx.LargeObjectModeRead|pgx.LargeObjectModeWrite)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	n, err := obj.Write([]byte("testing"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if n != 7 {
+		t.Errorf("Expected n to be 7, got %d", n)
+	}
+
+	pos, err := obj.Seek(1, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if pos != 1 {
+		t.Errorf("Expected pos to be 1, got %d", pos)
+	}
+
+	res := make([]byte, 6)
+	n, err = obj.Read(res)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(res) != "esting" {
+		t.Errorf(`Expected res to be "esting", got %q`, res)
+	}
+	if n != 6 {
+		t.Errorf("Expected n to be 6, got %d", n)
+	}
+
+	n, err = obj.Read(res)
+	if err != io.EOF {
+		t.Error("Expected io.EOF, go nil")
+	}
+	if n != 0 {
+		t.Errorf("Expected n to be 0, got %d", n)
+	}
+
+	pos, err = obj.Tell()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if pos != 7 {
+		t.Errorf("Expected pos to be 7, got %d", pos)
+	}
+
+	err = obj.Truncate(1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pos, err = obj.Seek(-1, 2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if pos != 0 {
+		t.Errorf("Expected pos to be 0, got %d", pos)
+	}
+
+	res = make([]byte, 2)
+	n, err = obj.Read(res)
+	if err != io.EOF {
+		t.Errorf("Expected err to be io.EOF, got %v", err)
+	}
+	if n != 1 {
+		t.Errorf("Expected n to be 1, got %d", n)
+	}
+	if res[0] != 't' {
+		t.Errorf("Expected res[0] to be 't', got %v", res[0])
+	}
+
+	err = obj.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = lo.Unlink(ctx, id)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = lo.Open(ctx, id, pgx.LargeObjectModeRead)
+	if e, ok := err.(*pgconn.PgError); !ok || e.Code != "42704" {
+		t.Errorf("Expected undefined_object error (42704), got %#v", err)
+	}
+}
+
+func TestLargeObjectsMultipleTransactions(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	conn, err := pgx.Connect(ctx, os.Getenv("PGX_TEST_DATABASE"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	skipCockroachDB(t, conn, "Server does support large objects")
+
+	tx, err := conn.Begin(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	lo := tx.LargeObjects()
+
+	id, err := lo.Create(ctx, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	obj, err := lo.Open(ctx, id, pgx.LargeObjectModeWrite)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	n, err := obj.Write([]byte("testing"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if n != 7 {
+		t.Errorf("Expected n to be 7, got %d", n)
+	}
+
+	// Commit the first transaction
+	err = tx.Commit(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// IMPORTANT: Use the same connection for another query
+	query := `select n from generate_series(1,10) n`
+	rows, err := conn.Query(ctx, query)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rows.Close()
+
+	// Start a new transaction
+	tx2, err := conn.Begin(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	lo2 := tx2.LargeObjects()
+
+	// Reopen the large object in the new transaction
+	obj2, err := lo2.Open(ctx, id, pgx.LargeObjectModeRead|pgx.LargeObjectModeWrite)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pos, err := obj2.Seek(1, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if pos != 1 {
+		t.Errorf("Expected pos to be 1, got %d", pos)
+	}
+
+	res := make([]byte, 6)
+	n, err = obj2.Read(res)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(res) != "esting" {
+		t.Errorf(`Expected res to be "esting", got %q`, res)
+	}
+	if n != 6 {
+		t.Errorf("Expected n to be 6, got %d", n)
+	}
+
+	n, err = obj2.Read(res)
+	if err != io.EOF {
+		t.Error("Expected io.EOF, go nil")
+	}
+	if n != 0 {
+		t.Errorf("Expected n to be 0, got %d", n)
+	}
+
+	pos, err = obj2.Tell()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if pos != 7 {
+		t.Errorf("Expected pos to be 7, got %d", pos)
+	}
+
+	err = obj2.Truncate(1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pos, err = obj2.Seek(-1, 2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if pos != 0 {
+		t.Errorf("Expected pos to be 0, got %d", pos)
+	}
+
+	res = make([]byte, 2)
+	n, err = obj2.Read(res)
+	if err != io.EOF {
+		t.Errorf("Expected err to be io.EOF, got %v", err)
+	}
+	if n != 1 {
+		t.Errorf("Expected n to be 1, got %d", n)
+	}
+	if res[0] != 't' {
+		t.Errorf("Expected res[0] to be 't', got %v", res[0])
+	}
+
+	err = obj2.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = lo2.Unlink(ctx, id)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = lo2.Open(ctx, id, pgx.LargeObjectModeRead)
+	if e, ok := err.(*pgconn.PgError); !ok || e.Code != "42704" {
+		t.Errorf("Expected undefined_object error (42704), got %#v", err)
+	}
+}
+*/
