eb4dff414a5440e939083091174d9a65d358157a
Khushboo Sancheti <42253461+clickboo@users.noreply.github.com>
2023-05-16 10:26:43 -0700

    Check for namespace existence before returning annotations (#6038)

--- sensor/admission-control/resources/namespaces/namespace.go
+++ sensor/admission-control/resources/namespaces/namespace.go
@@ -50,7 +50,10 @@ func (n *NamespaceStore) GetAnnotationsForNamespace(name string) map[string]stri
 	n.lock.Lock()
 	defer n.lock.Unlock()
 
-	return n.namespaceNamesToMetadata[name].annotations
+	if nm, ok := n.namespaceNamesToMetadata[name]; ok {
+		return nm.annotations
+	}
+	return nil
 }
 
 func (n *NamespaceStore) removeNamespace(ns *storage.NamespaceMetadata) {
