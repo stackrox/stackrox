081342920ae36fa09280a0f3024bacd6b5ae0170
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2023-05-17 11:42:49 +0200

    Use *dummySensorReplyHandlerImpl for local-compliance

--- tools/local-compliance/main.go
+++ tools/local-compliance/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	_ "net/http/pprof" // #nosec G108
 
 	"github.com/stackrox/rox/compliance/collection/compliance"
+	"github.com/stackrox/rox/generated/internalapi/sensor"
 	"github.com/stackrox/rox/pkg/logging"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 )
@@ -20,7 +22,7 @@ func main() {
 		nodeProvider: np,
 	}
 
-	srh := compliance.NewSensorReplyHandlerImpl(log, scanner)
+	srh := &dummySensorReplyHandlerImpl{log: log}
 	c := compliance.NewComplianceApp(log, np, scanner, srh)
 	c.Start()
 }
@@ -29,4 +31,18 @@ type dummyNodeNameProvider struct{}
 
 func (dnp *dummyNodeNameProvider) GetNodeName() string {
 	return "Foo"
+}
+
+type dummySensorReplyHandlerImpl struct {
+	log *logging.Logger
+}
+
+// HandleACK handles ACK message from Sensor
+func (s *dummySensorReplyHandlerImpl) HandleACK(_ context.Context, _ sensor.ComplianceService_CommunicateClient) {
+	s.log.Debugf("Received ACK from Sensor.")
+}
+
+// HandleNACK handles NACK message from Sensor
+func (s *dummySensorReplyHandlerImpl) HandleNACK(_ context.Context, _ sensor.ComplianceService_CommunicateClient) {
+	s.log.Infof("Received NACK from Sensor, resending NodeInventory in 10 seconds.")
 }
