436ecc030e1d862942b4ce6cb4d873b8773ac04a
cdu <cdu@redhat.com>
2023-05-16 17:41:12 -0700

    stage

--- pkg/postgres/gorm/largeobject/large_objects.go
+++ pkg/postgres/gorm/largeobject/large_objects.go
@@ -7,9 +7,9 @@ import (
 	"gorm.io/gorm"
 )
 
-// LargeObjects is a structure used to access the large objects API. It is only valid within the transaction where it
-// was created.
+// LargeObjects is used to access the large objects API with gorm CRM.
 //
+// This is originally created with similar API with existing github.com/jackc/pgx
 // For more details see: http://www.postgresql.org/docs/current/static/largeobjects.html
 type LargeObjects struct {
 	tx *gorm.DB
@@ -24,7 +24,7 @@ const (
 
 // Create creates a new large object with an unused OID assigned
 func (o *LargeObjects) Create() (uint32, error) {
-	o.tx = o.tx.Raw("SELECT lo_create(?)", 0)
+	o.tx = o.tx.Raw("SELECT lo_create($1)", 0)
 	if err := o.tx.Error; err != nil {
 		return 0, err
 	}
@@ -37,7 +37,7 @@ func (o *LargeObjects) Create() (uint32, error) {
 // object.
 func (o *LargeObjects) Open(oid uint32, mode Mode) (*LargeObject, error) {
 	var fd int32
-	o.tx = o.tx.Raw("select lo_open(?, ?)", oid, mode).Scan(&fd)
+	o.tx = o.tx.Raw("select lo_open($1, $2)", oid, mode).Scan(&fd)
 	if err := o.tx.Error; err != nil {
 		return nil, err
 	}
@@ -47,7 +47,7 @@ func (o *LargeObjects) Open(oid uint32, mode Mode) (*LargeObject, error) {
 // Unlink removes a large object from the database.
 func (o *LargeObjects) Unlink(oid uint32) error {
 	var count int32
-	o.tx = o.tx.Raw("select lo_unlink(?)", oid).Scan(&count)
+	o.tx = o.tx.Raw("select lo_unlink($1)", oid).Scan(&count)
 	if err := o.tx.Error; err != nil {
 		return err
 	}
@@ -103,7 +103,7 @@ type LargeObject struct {
 // Write writes p to the large object and returns the number of bytes written and an error if not all of p was written.
 func (o *LargeObject) Write(p []byte) (int, error) {
 	var n int
-	o.tx = o.tx.Raw("select lowrite(?, ?)", o.fd, p)
+	o.tx = o.tx.Raw("select lowrite($1, $2)", o.fd, p)
 	if err := o.tx.Error; err != nil {
 		return n, err
 	}
@@ -124,7 +124,7 @@ func (o *LargeObject) Write(p []byte) (int, error) {
 // Read reads up to len(p) bytes into p returning the number of bytes read.
 func (o *LargeObject) Read(p []byte) (n int, err error) {
 	var res []byte = make([]byte, 0, len(p))
-	o.tx = o.tx.Raw("select loread(?, ?)", o.fd, len(p))
+	o.tx = o.tx.Raw("select loread($1, $2)", o.fd, len(p))
 	if err = o.tx.Error; err != nil {
 		return 0, err
 	}
@@ -145,47 +145,26 @@ func (o *LargeObject) Read(p []byte) (n int, err error) {
 // Seek moves the current location pointer to the new location specified by offset.
 func (o *LargeObject) Seek(offset int64, whence int) (int64, error) {
 	var n int64
-	result := o.tx.Raw("select lo_lseek64(?, ?, ?)", o.fd, offset, whence)
-	if result.Error != nil {
-		return 0, o.tx.Error
-	}
-	row := o.tx.Row()
-	row.Scan(&n)
-	if result.Error != nil {
-		return 0, result.Error
-	}
-	return n, o.tx.Error
+	result := o.tx.Raw("select lo_lseek64($1, $2, $3)", o.fd, offset, whence).Scan(&n)
+	return n, result.Error
 }
 
 // Tell returns the current read or write location of the large object descriptor.
 func (o *LargeObject) Tell() (int64, error) {
 	var n int64
-	o.tx = o.tx.Raw("select lo_tell64($1)", o.fd).Scan(&n)
-	return n, o.tx.Error
+	result := o.tx.Raw("select lo_tell64($1)", o.fd).Scan(&n)
+	return n, result.Error
 }
 
 // Truncate the large object to size.
 func (o *LargeObject) Truncate(size int64) (n int, err error) {
-	result := o.tx.Raw("select lo_truncate64(?, ?)", o.fd, size).Scan(&n)
+	result := o.tx.Raw("select lo_truncate64($1, $2)", o.fd, size).Scan(&n)
 	return n, result.Error
 }
 
 // Close the large object descriptor.
 func (o *LargeObject) Close() error {
 	var n int
-	o.tx = o.tx.Raw("select lo_close(?)", o.fd).Scan(&n)
-	return o.tx.Error
+	result := o.tx.Raw("select lo_close($1)", o.fd).Scan(&n)
+	return result.Error
 }
-
-/*
-{	var n int64
-o.tx = o.tx.Raw("select lo_tell64(?)", o.fd)
-if o.tx.Error != nil {
-return n, o.tx.Error
-}
-row := o.tx.Row()
-if row.Err() != nil {
-return n, row.Err()
-}
-err := row.Scan(&n)
-return n, err}*/
