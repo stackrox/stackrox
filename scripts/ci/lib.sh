75921551ca37a0918db814987024494da64be364
Gavin Jefferies <gavin@stackrox.com>
2023-05-19 13:41:47 -0700

    test: do not create JIRAs!!!!

--- scripts/ci/lib.sh
+++ scripts/ci/lib.sh
@@ -1244,6 +1244,7 @@ store_test_results() {
             -junit-reports-dir "$from" \
             -orchestrator "${ORCHESTRATOR_FLAVOR:-PROW}" \
             -threshold 5 \
+            -dry-run \
             -csv-output "${csv_output}"
         bq load --skip_leading_rows=1 ci_metrics.stackrox_tests "${csv_output}"
     } || true
