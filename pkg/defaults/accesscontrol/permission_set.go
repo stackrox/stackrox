b33d6b8ab4f240f18710d09c26c6602f09f481c8
Yann Brillouet <ybrillou@redhat.com>
2023-05-23 02:34:29 +0200

    Update comment to prevent re-use of permission set ID of former default ScopeManager

--- pkg/defaults/accesscontrol/permission_set.go
+++ pkg/defaults/accesscontrol/permission_set.go
@@ -8,8 +8,8 @@ const (
 	analystPermissionSetID               = "ffffffff-ffff-fff4-f5ff-fffffffffffe"
 	continuousIntegrationPermissionSetID = "ffffffff-ffff-fff4-f5ff-fffffffffffd"
 	nonePermissionSetID                  = "ffffffff-ffff-fff4-f5ff-fffffffffffc"
-	// "ffffffff-ffff-fff4-f5ff-fffffffffffb" was used for the ScopeManager default permission set,
-	// references can remain after the removal.
+	// DO NOT RE-USE "ffffffff-ffff-fff4-f5ff-fffffffffffb"
+	// the ID was used for the ScopeManager default permission set, and may not have been removed by migration (181 to 182).
 	sensorCreatorPermissionSetID      = "ffffffff-ffff-fff4-f5ff-fffffffffffa"
 	vulnMgmtApproverPermissionSetID   = "ffffffff-ffff-fff4-f5ff-fffffffffff9"
 	vulnMgmtRequesterPermissionSetID  = "ffffffff-ffff-fff4-f5ff-fffffffffff8"
