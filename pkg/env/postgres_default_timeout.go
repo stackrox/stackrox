de8235bb001fb24c1de013d53aa579d8015b2353
Dave Shrewsberry <dashrews@redhat.com>
2023-05-23 12:34:10 -0400

    update migration timeout

--- pkg/env/postgres_default_timeout.go
+++ pkg/env/postgres_default_timeout.go
@@ -10,5 +10,5 @@ var (
 	PostgresDefaultCursorTimeout = registerDurationSetting("ROX_POSTGRES_DEFAULT_CURSOR_TIMEOUT", 10*time.Minute)
 
 	// PostgresDefaultMigrationStatementTimeout sets the default timeout for Postgres statements during migration
-	PostgresDefaultMigrationStatementTimeout = registerDurationSetting("ROX_POSTGRES_MIGRATION_STATEMENT_TIMEOUT", 20*time.Minute)
+	PostgresDefaultMigrationStatementTimeout = registerDurationSetting("ROX_POSTGRES_MIGRATION_STATEMENT_TIMEOUT", 2*time.Hour)
 )
