bc53432593dd20eede9497beea5dfcfcce069205
Yann Brillouet <91869377+rhybrillou@users.noreply.github.com>
2023-05-26 10:47:33 -0400

    ROX-14398: Remove "Scope Manager" default Role and PermissionSet (#5851)

--- central/role/datastore/permissionset_ids.go
+++ central/role/datastore/permissionset_ids.go
@@ -8,8 +8,8 @@ const (
 	analystPermissionSetID               = "ffffffff-ffff-fff4-f5ff-fffffffffffe"
 	continuousIntegrationPermissionSetID = "ffffffff-ffff-fff4-f5ff-fffffffffffd"
 	nonePermissionSetID                  = "ffffffff-ffff-fff4-f5ff-fffffffffffc"
-	// TODO: ROX-14398 Remove ScopeManager default role
-	scopeManagerPermissionSetID       = "ffffffff-ffff-fff4-f5ff-fffffffffffb"
+	// DO NOT RE-USE "ffffffff-ffff-fff4-f5ff-fffffffffffb"
+	// the ID was used for the ScopeManager default permission set, and may not have been removed by migration (182 to 183).
 	sensorCreatorPermissionSetID      = "ffffffff-ffff-fff4-f5ff-fffffffffffa"
 	vulnMgmtApproverPermissionSetID   = "ffffffff-ffff-fff4-f5ff-fffffffffff9"
 	vulnMgmtRequesterPermissionSetID  = "ffffffff-ffff-fff4-f5ff-fffffffffff8"
