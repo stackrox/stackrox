575d4ade0271b2bd9913095a9a33f601a5862fcc
Jan Schnathmeier <jschnath@redhat.com>
2023-05-31 23:35:37 +0200

    Fix namedtuple usage

--- scripts/ci/jobs/gke_version_compatibility_tests.py
+++ scripts/ci/jobs/gke_version_compatibility_tests.py
@@ -11,6 +11,7 @@ from clusters import GKECluster
 from collections import namedtuple
 from compatibility_test import make_compatibility_test_runner
 from get_latest_helm_chart_versions import get_latest_helm_chart_versions
+from pathlib import Path
 
 # start logging
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
@@ -21,7 +22,7 @@ os.environ["ROX_POSTGRES_DATASTORE"] = "true"
 
 central_chart_versions = get_latest_helm_chart_versions("stackrox-central-services", 2)
 sensor_chart_versions = get_latest_helm_chart_versions("stackrox-secured-cluster-services", 3)
-makefile_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '../../..'))
+makefile_path = Path(__file__).parent.parent.parent.parent
 latest_tag = subprocess.check_output(["make", "tag", "-C", makefile_path, "--quiet", "--no-print-director"], shell=False, encoding='utf-8').strip()
 
 if len(central_chart_versions) == 0:
@@ -31,26 +32,24 @@ if len(sensor_chart_versions) == 0:
 
 Chart_versions = namedtuple("Chart_versions", ["central_version", "sensor_version"])
 
-# Latest central vs sensor versions in sensor_chart_versions (latest 4 releases)
+# Latest central vs sensor versions in sensor_chart_versions
 test_tuples = [Chart_versions(central_version=latest_tag, sensor_version=sensor_chart_version) for sensor_chart_version in sensor_chart_versions]
-# Latest sensor vs central versions in central_chart_versions (latest 2 releases)
+# Latest sensor vs central versions in central_chart_versions
 test_tuples.extend([Chart_versions(central_version=central_chart_version, sensor_version=latest_tag) for central_chart_version in central_chart_versions])
 
 gkecluster = GKECluster("compat-test")
 
 failing_tuples = []
 for tuple in test_tuples:
-    central_version = tuple[0]
-    sensor_version = tuple[1]
-    os.environ["CENTRAL_CHART_VERSION_OVERRIDE"] = central_version
-    os.environ["SENSOR_CHART_VERSION_OVERRIDE"] = sensor_version
+    os.environ["CENTRAL_CHART_VERSION_OVERRIDE"] = tuple.central_version
+    os.environ["SENSOR_CHART_VERSION_OVERRIDE"] = tuple.sensor_version
     try:
         make_compatibility_test_runner(cluster=gkecluster).run()
     except Exception:
-        print(f"Exception \"{Exception}\" raised in compatibility test for central version {central_version} and sensor version {sensor_version}",
+        print(f"Exception \"{Exception}\" raised in compatibility test for central version {tuple.central_version} and sensor version {tuple.sensor_version}",
             file=sys.stderr)
         failing_tuples.append(tuple)
 
 if len(failing_tuples) > 0:
-    failing_string = ', '.join([("(Central v" + str(failing_tuple["central_version"]) + ", Sensor v" + str(failing_tuple["sensor_version"]) + ")") for failing_tuple in failing_tuples])
+    failing_string = ', '.join([("(Central v" + str(failing_tuple.central_version) + ", Sensor v" + str(failing_tuple.sensor_version) + ")") for failing_tuple in failing_tuples])
     raise RuntimeError("Compatibility tests failed for versions " + failing_string + ".")
