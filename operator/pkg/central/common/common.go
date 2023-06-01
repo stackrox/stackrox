be4f930c79353fef2eb6f24b38adfa0c2ac1cb47
cdu <cdu@redhat.com>
2023-06-01 14:46:11 -0700

    consolidate

---
+++ operator/pkg/central/common/common.go
@@ -0,0 +1,6 @@
+package common
+
+const (
+	// CentralPVCObsoletedAnnotation represents Central PVC has been obsoleted
+	CentralPVCObsoletedAnnotation = "platform.stackrox.io/central-pvc-obsoleted"
+)
