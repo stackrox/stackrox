2df1c8f00195a03747419a7faf983a90f96b7bc5
Daniel Haus <dhaus@redhat.com>
2023-06-05 07:15:24 +0200

    Adjust E2E tests.

--- qa-tests-backend/src/main/groovy/services/DeclarativeConfigHealthService.groovy
+++ qa-tests-backend/src/main/groovy/services/DeclarativeConfigHealthService.groovy
@@ -1,8 +1,14 @@
 package services
 
-import groovy.transform.CompileStatic
+import io.stackrox.proto.api.v1.DeclarativeConfigHealthServiceGrpc
+import io.stackrox.proto.api.v1.EmptyOuterClass.Empty
 
-@CompileStatic
 class DeclarativeConfigHealthService extends BaseService {
+    static getDeclarativeConfigHealthClient() {
+        return DeclarativeConfigHealthServiceGrpc.newBlockingStub(getChannel())
+    }
 
+    static getDeclarativeConfigHealthInfo() {
+        return getDeclarativeConfigHealthClient().getDeclarativeConfigHealths(Empty.newBuilder().build())
+    }
 }
