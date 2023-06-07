14573e0b19798c23c037657d7759858fd0e26298
Cong Du <c-du@users.noreply.github.com>
2023-06-07 08:32:50 -0700

    ROX-16557: Stop creating stackrox-db on installation - operator (#5763)
    Co-authored-by: Marcin Owsiany <porridge@redhat.com>
    Co-authored-by: Connor Gorman <connorgorman@users.noreply.github.com>

--- operator/pkg/central/reconciler/reconciler.go
+++ operator/pkg/central/reconciler/reconciler.go
@@ -53,7 +53,7 @@ func RegisterNewReconciler(mgr ctrl.Manager, selector string) error {
 
 	return reconciler.SetupReconcilerWithManager(
 		mgr, platform.CentralGVK, image.CentralServicesChartPrefix,
-		proxy.InjectProxyEnvVars(translation.Translator{}, proxyEnv),
+		proxy.InjectProxyEnvVars(translation.New(mgr.GetClient()), proxyEnv),
 		opts...,
 	)
 }
