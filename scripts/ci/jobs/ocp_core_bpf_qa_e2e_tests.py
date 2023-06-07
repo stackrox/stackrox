f76ebac5248a4f38265e907cfa98241d6c6983d2
Dmitrii Dolgov <9erthalion6@gmail.com>
2023-06-07 15:18:44 +0200

    Do not use operator based deployment
    The default value for the operator based deployment is specified via
    SecuredCluster CRD and is ebpf.

--- scripts/ci/jobs/ocp_core_bpf_qa_e2e_tests.py
+++ scripts/ci/jobs/ocp_core_bpf_qa_e2e_tests.py
@@ -9,7 +9,7 @@ from base_qa_e2e_test import make_qa_e2e_test_runner
 from clusters import OpenShiftScaleWorkersCluster
 
 # set required test parameters
-os.environ["DEPLOY_STACKROX_VIA_OPERATOR"] = "true"
+os.environ["DEPLOY_STACKROX_VIA_OPERATOR"] = "false"
 os.environ["ORCHESTRATOR_FLAVOR"] = "openshift"
 os.environ["ROX_POSTGRES_DATASTORE"] = "true"
 
