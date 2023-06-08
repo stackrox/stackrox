5fe8c9c3b349d08500be169beeb722736e576bfe
Charmik Sheth <101146970+charmik-redhat@users.noreply.github.com>
2023-06-08 11:32:51 -0700

    Add report metadata store and gorm model

---
+++ central/reports/metadata/datastore/store/postgres/gen.go
@@ -0,0 +1,3 @@
+package postgres
+
+//go:generate pg-table-bindings-wrapper --type=storage.ReportMetadata --table=report_metadatas --search-category REPORT_METADATA --references storage.ReportConfiguration
