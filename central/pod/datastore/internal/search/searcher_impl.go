35421a6dc9aaf4c18ea7c7ac679a4fe58d190eca
Dave Shrewsberry <dashrews@redhat.com>
2023-06-08 07:28:50 -0400

    test and style

--- central/pod/datastore/internal/search/searcher_impl.go
+++ central/pod/datastore/internal/search/searcher_impl.go
@@ -52,8 +52,7 @@ func (ds *searcherImpl) SearchRawPods(ctx context.Context, q *v1.Query) ([]*stor
 
 // Format the search functionality of the indexer to be filtered (for sac) and paginated.
 func formatSearcher(podIndexer blevesearch.UnsafeSearcher) search.Searcher {
-	var filteredSearcher search.Searcher
-	filteredSearcher = podsSACPostgresSearchHelper.FilteredSearcher(podIndexer) // Make the UnsafeSearcher safe.
+	filteredSearcher := podsSACPostgresSearchHelper.FilteredSearcher(podIndexer) // Make the UnsafeSearcher safe.
 	paginatedSearcher := paginated.Paginated(filteredSearcher)
 	defaultSortedSearcher := paginated.WithDefaultSortOption(paginatedSearcher, defaultSortOption)
 	return defaultSortedSearcher
