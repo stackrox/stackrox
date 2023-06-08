e1b9a7970f4810f188e7daf6af2099893d118754
Connor Gorman <cgorman@redhat.com>
2023-06-08 16:51:16 -0700

    Remove Rocksdb in central/role

--- central/role/datastore/singleton.go
+++ central/role/datastore/singleton.go
@@ -6,15 +6,10 @@ import (
 	"github.com/stackrox/rox/central/globaldb"
 	groupFilter "github.com/stackrox/rox/central/group/datastore/filter"
 	"github.com/stackrox/rox/central/role/resources"
-	"github.com/stackrox/rox/central/role/store"
 	PermissionSetPGStore "github.com/stackrox/rox/central/role/store/permissionset/postgres"
-	permissionSetPGStore "github.com/stackrox/rox/central/role/store/permissionset/rocksdb"
 	postgresRolePGStore "github.com/stackrox/rox/central/role/store/role/postgres"
-	roleStore "github.com/stackrox/rox/central/role/store/role/rocksdb"
 	postgresSimpleAccessScopeStore "github.com/stackrox/rox/central/role/store/simpleaccessscope/postgres"
-	simpleAccessScopeStore "github.com/stackrox/rox/central/role/store/simpleaccessscope/rocksdb"
 	"github.com/stackrox/rox/generated/storage"
-	"github.com/stackrox/rox/pkg/env"
 	"github.com/stackrox/rox/pkg/sac"
 	"github.com/stackrox/rox/pkg/sync"
 	"github.com/stackrox/rox/pkg/utils"
@@ -28,22 +23,9 @@ var (
 // Singleton returns the singleton providing access to the roles store.
 func Singleton() DataStore {
 	once.Do(func() {
-		var roleStorage store.RoleStore
-		var permissionSetStorage store.PermissionSetStore
-		var accessScopeStorage store.SimpleAccessScopeStore
-		if env.PostgresDatastoreEnabled.BooleanSetting() {
-			roleStorage = postgresRolePGStore.New(globaldb.GetPostgres())
-			permissionSetStorage = PermissionSetPGStore.New(globaldb.GetPostgres())
-			accessScopeStorage = postgresSimpleAccessScopeStore.New(globaldb.GetPostgres())
-		} else {
-			var err error
-			roleStorage, err = roleStore.New(globaldb.GetRocksDB())
-			utils.CrashOnError(err)
-			permissionSetStorage, err = permissionSetPGStore.New(globaldb.GetRocksDB())
-			utils.CrashOnError(err)
-			accessScopeStorage, err = simpleAccessScopeStore.New(globaldb.GetRocksDB())
-			utils.CrashOnError(err)
-		}
+		roleStorage := postgresRolePGStore.New(globaldb.GetPostgres())
+		permissionSetStorage := PermissionSetPGStore.New(globaldb.GetPostgres())
+		accessScopeStorage := postgresSimpleAccessScopeStore.New(globaldb.GetPostgres())
 		// Which role format is used is determined solely by the feature flag.
 		ds = New(roleStorage, permissionSetStorage, accessScopeStorage, groupFilter.GetFiltered)
 
