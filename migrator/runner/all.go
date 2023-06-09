492cf701739320b25929d0384a41c313e268ed1d
Nakul <1407686+theencee@users.noreply.github.com>
2023-06-08 19:44:24 -0700

    ROX-16950: Add more known namespaces to default policy exclusion list (#6036)

--- migrator/runner/all.go
+++ migrator/runner/all.go
@@ -141,4 +141,5 @@ import (
 	_ "github.com/stackrox/rox/migrator/migrations/m_182_to_m_183_remove_default_scope_manager_role"
 	_ "github.com/stackrox/rox/migrator/migrations/m_183_to_m_184_move_declarative_config_health"
 	_ "github.com/stackrox/rox/migrator/migrations/m_184_to_m_185_remove_policy_vulnerability_report_resources"
+	_ "github.com/stackrox/rox/migrator/migrations/m_185_to_m_186_more_policy_migrations"
 )
