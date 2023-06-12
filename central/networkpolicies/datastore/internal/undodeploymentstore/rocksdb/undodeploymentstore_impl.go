3eb8a7fa63cab09345c7bed8f448556cf5b00dec
Connor Gorman <connorgorman@users.noreply.github.com>
2023-06-12 08:14:24 -0700

    ROX-17615, ROX-17587: Remove RocksDB undodeploymentstore and watched images (#6444)

--- central/networkpolicies/datastore/internal/undodeploymentstore/rocksdb/undodeploymentstore_impl.go
+++
@@ -1,7 +0,0 @@
-package rocksdb
-
-import (
-	"github.com/stackrox/rox/central/networkpolicies/datastore/internal/undodeploymentstore"
-)
-
-var _ undodeploymentstore.UndoDeploymentStore = (*storeImpl)(nil)
