a356b78cbf0cf3285af588b37c5ffeaafebe4ab2
Connor Gorman <connorgorman@users.noreply.github.com>
2023-06-12 07:12:30 -0700

    ROX-17596, ROX-17597, ROX-17598: Remove RocksDB in central/role (#6413)

--- central/role/store/permissionset/rocksdb/gen.go
+++
@@ -1,3 +0,0 @@
-package rocksdb
-
-//go:generate rocksdb-bindings-wrapper --type=PermissionSet --bucket=permission_sets --cache --uniq-key-func GetName()
--- central/role/store/simpleaccessscope/rocksdb/gen.go
+++
@@ -1,3 +0,0 @@
-package rocksdb
-
-//go:generate rocksdb-bindings-wrapper --type=SimpleAccessScope --bucket=simple_access_scopes --cache --uniq-key-func GetName()
