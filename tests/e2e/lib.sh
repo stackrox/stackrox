b5cdb7cca1954d9b6dd4e0156c72256966b5221c
Dmitrii Dolgov <9erthalion6@gmail.com>
2023-06-12 16:08:08 +0200

    Use operator-based deployment
    OCP e2e tests support only operator-based deployment. Extend it with the
    possibility to set COLLECTION_METHOD.

--- tests/e2e/lib.sh
+++ tests/e2e/lib.sh
@@ -289,6 +289,10 @@ deploy_sensor_via_operator() {
        kubectl -n stackrox set env ds/collector ROX_PROCESSES_LISTENING_ON_PORT="${ROX_PROCESSES_LISTENING_ON_PORT}"
     fi
 
+    if [[ -n "${COLLECTION_METHOD:-}" ]]; then
+       kubectl -n stackrox set env ds/collector COLLECTION_METHOD="${COLLECTION_METHOD}"
+    fi
+
     # Every E2E test should have ROX_RESYNC_DISABLED="true"
     kubectl -n stackrox set env deployment/sensor ROX_RESYNC_DISABLED="true"
 }
