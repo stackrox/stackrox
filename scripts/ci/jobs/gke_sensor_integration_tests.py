046cd57d43664c8d660042e1d2ba0dfa21dc83b2
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2023-06-13 19:35:18 +0200

    Add param for running poll_for_system_test_images in PreSystemTests

--- scripts/ci/jobs/gke_sensor_integration_tests.py
+++ scripts/ci/jobs/gke_sensor_integration_tests.py
@@ -8,14 +8,12 @@ from runners import ClusterTestRunner
 from clusters import GKECluster
 from pre_tests import PreSystemTests
 from ci_tests import SensorIntegration
-from post_tests import PostClusterTest, FinalPost
 
 # set required test parameters
 os.environ["ORCHESTRATOR_FLAVOR"] = "k8s"
 
 ClusterTestRunner(
-    pre_test=PreSystemTests(),
+    pre_test=PreSystemTests(run_poll_for_system_test_images=False),
     cluster=GKECluster("sensor-integration-test"),
     test=SensorIntegration(),
 ).run()
-
