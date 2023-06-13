fb1fda0f06f09558154cbe19b438dc92f76d397d
Connor Gorman <connorgorman@users.noreply.github.com>
2023-06-12 19:10:10 -0700

    ROX-17699: Fix upgrade test by removing scale-remote cluster (#6431)

--- tests/upgrade/postgres_run.sh
+++ tests/upgrade/postgres_run.sh
@@ -230,6 +230,9 @@ test_upgrade_paths() {
     # Cleanup the scaled sensor before smoke tests
     "${REPO_FOR_TIME_TRAVEL}"/deploy/k8s/sensor-deploy/delete-sensor.sh
 
+    # Remove scaled Sensor from Central
+    "$TEST_ROOT/bin/$TEST_HOST_PLATFORM/roxctl" -e "$API_ENDPOINT" -p "$ROX_PASSWORD" cluster delete --name scale-remote
+
     info "Fetching a sensor bundle for cluster 'remote'"
     "$TEST_ROOT/bin/$TEST_HOST_PLATFORM/roxctl" version
     rm -rf sensor-remote
