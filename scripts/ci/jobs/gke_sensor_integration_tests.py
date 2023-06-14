68d6085402d91b7f7beb7300cc23ca0f71b69cd6
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2023-06-14 02:52:27 +0200

    ROX-17673: Remove pre and post tests from sensor-integration target (#6484)

--- scripts/ci/jobs/gke_sensor_integration_tests.py
+++ scripts/ci/jobs/gke_sensor_integration_tests.py
@@ -8,20 +8,12 @@ from runners import ClusterTestRunner
 from clusters import GKECluster
 from pre_tests import PreSystemTests
 from ci_tests import SensorIntegration
-from post_tests import PostClusterTest, FinalPost
 
 # set required test parameters
 os.environ["ORCHESTRATOR_FLAVOR"] = "k8s"
 
 ClusterTestRunner(
+    pre_test=PreSystemTests(run_poll_for_system_test_images=False),
     cluster=GKECluster("sensor-integration-test"),
     test=SensorIntegration(),
-    post_test=PostClusterTest(
-        check_stackrox_logs=False,
-    ),
-    final_post=FinalPost(
-        store_qa_test_debug_logs=False,
-        store_qa_spock_results=False,
-    ),
 ).run()
-
