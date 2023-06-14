75336421e12ce76ebc0045d7685ae2bb56f9bf14
Gavin Jefferies <gavin@stackrox.com>
2023-06-13 18:38:41 -0700

    ooooops

--- scripts/ci/lib.sh
+++ scripts/ci/lib.sh
@@ -1247,6 +1247,11 @@ post_process_test_results() {
             -csv-output "${csv_output}")
 
         if is_in_PR_context; then
+            info "Converting JUNIT found in ${ARTIFACT_DIR} to CSV"
+            curl --retry 5 -SsfL https://github.com/stackrox/junit2csv/releases/download/v0.0.1/junit2csv -o junit2csv && \
+            chmod +x junit2csv && \
+            ./junit2csv "${common_args[@]}"
+        else
             info "Creating JIRA issues for failures found in ${ARTIFACT_DIR}"
             curl --retry 5 -SsfL https://github.com/stackrox/junit2jira/releases/download/v0.0.8/junit2jira -o junit2jira && \
             chmod +x junit2jira && \
@@ -1257,11 +1262,6 @@ post_process_test_results() {
                 -orchestrator "${ORCHESTRATOR_FLAVOR:-PROW}" \
                 -threshold 5 \
                 "${common_args[@]}"
-        else
-            info "Converting JUNIT found in ${ARTIFACT_DIR} to CSV"
-            curl --retry 5 -SsfL https://github.com/stackrox/junit2csv/releases/download/v0.0.1/junit2csv -o junit2csv && \
-            chmod +x junit2csv && \
-            ./junit2csv "${common_args[@]}"
         fi
 
         info "Creating Big Query test records from ${csv_output}"
