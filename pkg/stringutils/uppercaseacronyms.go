2ba7f99fa238ece0f95500f727caa656b8744c52
Yann Brillouet <ybrillou@redhat.com>
2023-06-14 21:44:36 +0200

    Add function comment for new function uppercasing acronyms

--- pkg/stringutils/uppercaseacronyms.go
+++ pkg/stringutils/uppercaseacronyms.go
@@ -14,6 +14,8 @@ var (
 	}
 )
 
+// UpperCaseAcronyms returns the uppercase variant of the input if it is one of a specific set of acronyms
+// (currently API, CPU, ID, UID, UUID).
 func UpperCaseAcronyms(s string) string {
 	uc := strings.ToUpper(s)
 	if _, found := acronymsToUpperCase[uc]; found {
