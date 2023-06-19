4792014eca93a31334e342eaab625e920fddc908
Ivan Degtiarenko <78353299+ivan-degtiarenko@users.noreply.github.com>
2023-06-19 21:29:42 +0200

    Update pkg/env/private_diagnostics.go
    Co-authored-by: dhaus67 <dhaus@redhat.com>

--- pkg/env/private_diagnostics.go
+++ pkg/env/private_diagnostics.go
@@ -6,6 +6,6 @@ var (
 	// PrivateDiagnosticsEnabled toggles whether diagnostic handlers are available on private endpoint.
 	PrivateDiagnosticsEnabled = RegisterBooleanSetting("ROX_PRIVATE_DIAGNOSTICS", false)
 
-	// PrivatePortSetting has the :port or host:port string for listening for private endpoints server
+	// PrivatePortSetting has the :port or host:port string for listening for private endpoints server.
 	PrivatePortSetting = RegisterSetting("ROX_PRIVATE_PORT", WithDefault(defaultPrivatePortSetting))
 )
