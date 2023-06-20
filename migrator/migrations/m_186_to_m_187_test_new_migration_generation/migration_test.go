3ec1d5d7b275f4486b189454c79365322f5df5b6
Yann Brillouet <ybrillou@redhat.com>
2023-06-20 14:17:59 +0200

    chore(tooling): test make bootstrap_migration

---
+++ migrator/migrations/m_186_to_m_187_test_new_migration_generation/migration_test.go
@@ -0,0 +1,61 @@
+//go:build sql_integration
+
+package m186tom187
+
+import (
+    "context"
+    "testing"
+
+    pghelper "github.com/stackrox/rox/migrator/migrations/postgreshelper"
+	"github.com/stackrox/rox/migrator/types"
+	"github.com/stackrox/rox/pkg/sac"
+	"github.com/stretchr/testify/suite"
+)
+
+var (
+    ctx = sac.WithAllAccess(context.Background())
+)
+
+type migrationTestSuite struct {
+	suite.Suite
+
+	db *pghelper.TestPostgres
+}
+
+func TestMigration(t *testing.T) {
+    suite.Run(T, new(migrationTestSuite))
+}
+
+
+func (s *migrationTestSuite) SetupSuite() {
+	s.db = pghelper.ForT(s.T(), true)
+	// TODO dont-merge: Create the schemas and tables required for the pre-migration dataset push to DB
+}
+
+func (s *migrationTestSuite) TearDownSuite() {
+	s.db.Teardown(s.T())
+}
+
+
+
+func (s *migrationTestSuite) TestMigration() {
+    // TODO dont-merge: instantiate any store required for the pre-migration dataset push to DB
+
+    // TODO dont-merge: push the pre-migration dataset to DB
+
+	dbs := &types.Databases{
+	    GormDB:     s.db.GetGormDB(),
+		PostgresDB: s.db.DB,
+	}
+
+	s.Require().NoError(migration.Run(dbs))
+
+    // TODO dont-merge: instantiate any store required for the post-migration dataset pull from DB
+
+	// TODO dont-merge: pull the post-migration dataset from DB
+
+	// TODO dont-merge: validate that the post-migration dataset has the expected content
+
+}
+
+// TODO dont-merge: remove any pending TODO
