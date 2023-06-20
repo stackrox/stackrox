dd67dacd8515adc2c9b995a4e63ee4e71c3fa323
Yann Brillouet <ybrillou@redhat.com>
2023-06-20 19:31:02 +0200

    Address review comments

--- pkg/postgres/walker/stringutils.go
+++ pkg/postgres/walker/stringutils.go
@@ -12,13 +12,15 @@ func applyPointwise(ss []string, f func(string) string) {
 	}
 }
 
+// makeSingular aims at converting a title or lower-case word to its singular form.
+// assumption is made that any multi-letter word ending with s is a plural word and its singular form
+// is without trailing s.
+// Although this is not always a valid assumption (example of assumption flaw: matress),
+// this seems to be verified with the postgres table and children-table names.
 func makeSingular(s string) string {
 	l := len(s)
-	if l == 0 {
+	if l <= 1 {
 		return s
-	}
-	if l == 1 && (s[0] == 's' || s[0] == 'S') {
-		return s[:l-1]
 	}
 	if s[l-1] == 's' {
 		return s[:l-1]
