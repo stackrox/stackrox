8f12b6b59906ee60f45bfcc185573e705d8c90c5
Frederico Bittencourt <fbittenc@redhat.com>
2023-06-21 12:14:48 +0200

    add failing test: custom routes keep HTTP listener up after Stop()
    This test highlights a bug with the current implementation.
    There is a sneaky HTTPServer created for endpoints that is not stopped
    when we request stop. This is because the pointer to this server is
    only available in the `serverAndListener` struct.

--- pkg/grpc/server_test.go
+++ pkg/grpc/server_test.go
@@ -1,9 +1,15 @@
 package grpc
 
 import (
+	"crypto/tls"
 	"fmt"
+	"net/http"
 	"testing"
+	"time"
 
+	"github.com/stackrox/rox/pkg/concurrency"
+	"github.com/stackrox/rox/pkg/grpc/authz/allow"
+	"github.com/stackrox/rox/pkg/grpc/routes"
 	"github.com/stackrox/rox/pkg/mtls/verifier"
 	"github.com/stretchr/testify/suite"
 )
@@ -50,6 +56,68 @@ func (a *APIServerSuite) Test_TwoTestsStartingAPIs() {
 	}
 }
 
+func (a *APIServerSuite) Test_CustomAPI() {
+	// TODO: Use TLS mock instead of overriding this with dummy certs
+	a.T().Setenv("ROX_MTLS_CERT_FILE", "../../tools/local-sensor/certs/cert.pem")
+	a.T().Setenv("ROX_MTLS_KEY_FILE", "../../tools/local-sensor/certs/key.pem")
+	a.T().Setenv("ROX_MTLS_CA_FILE", "../../tools/local-sensor/certs/caCert.pem")
+	a.T().Setenv("ROX_MTLS_CA_KEY_FILE", "../../tools/local-sensor/certs/caKey.pem")
+
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+
+	a.Run("fetch data from /test", func() {
+		cfg, endpointReached := configWithCustomRoute()
+		api := NewAPI(cfg)
+		api.Start().Wait()
+		defer func() {
+			api.Stop().Wait()
+		}()
+
+		a.requestWithoutErr("https://localhost:8080/test")
+		a.waitForSignal(endpointReached)
+	})
+
+	a.Run("cannot fetch data from /test after server stopped", func() {
+		cfg, endpointReached := configWithCustomRoute()
+		api := NewAPI(cfg)
+		api.Start().Wait()
+		api.Stop().Wait()
+
+		_, err := http.Get("https://localhost:8080/test")
+		a.Require().Error(err)
+		a.Require().False(endpointReached.IsDone())
+	})
+
+}
+
+func (a *APIServerSuite) requestWithoutErr(url string) {
+	_, err := http.Get(url)
+	a.Require().NoError(err)
+}
+
+func (a *APIServerSuite) waitForSignal(s *concurrency.Signal) {
+	select {
+	case <-s.Done():
+		break
+	case <-time.After(2 * time.Second):
+		a.FailNow("Should have received request on endpoint")
+	}
+}
+
+func configWithCustomRoute() (Config, *concurrency.Signal) {
+	endpointReached := concurrency.NewSignal()
+	cfg := defaultConf()
+	handler := &testHandler{received: &endpointReached}
+	cfg.CustomRoutes = []routes.CustomRoute{
+		{
+			Route:         "/test",
+			Authorizer:    allow.Anonymous(),
+			ServerHandler: handler,
+		},
+	}
+	return cfg, &endpointReached
+}
+
 func defaultConf() Config {
 	return Config{
 		Endpoints: []*EndpointConfig{
@@ -61,4 +129,14 @@ func defaultConf() Config {
 			},
 		},
 	}
+}
+
+type testHandler struct {
+	name     string
+	received *concurrency.Signal
+}
+
+func (h *testHandler) ServeHTTP(w http.ResponseWriter, _ *http.Request) {
+	h.received.Signal()
+	_, _ = w.Write([]byte("Hello!"))
 }
