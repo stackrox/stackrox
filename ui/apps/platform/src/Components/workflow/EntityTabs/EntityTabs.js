cb1b305235e2678fc1a24d06d7ebe65a25a3bb1a
Mark Pedrotti <pedrottimark@gmail.com>
2023-06-21 15:02:43 -0400

    ROX-17764: Delete non-postgres code in entityRelationships UI (#6520)

--- ui/apps/platform/src/Components/workflow/EntityTabs/EntityTabs.js
+++ ui/apps/platform/src/Components/workflow/EntityTabs/EntityTabs.js
@@ -5,88 +5,27 @@ import pluralize from 'pluralize';
 import entityLabels from 'messages/entity';
 import GroupedTabs from 'Components/GroupedTabs';
 import {
-    getEntityTypesByRelationship,
+    getVulnerabilityManagementEntityTypesByRelationship,
     entityGroups,
     entityGroupMap,
 } from 'utils/entityRelationships';
-import relationshipTypes from 'constants/relationshipTypes';
 import workflowStateContext from 'Containers/workflowStateContext';
-import useFeatureFlags from 'hooks/useFeatureFlags';
-import filterEntityRelationship from 'Containers/VulnMgmt/VulnMgmt.utils/filterEntityRelationship';
 
 const EntityTabs = ({ entityType, activeTab }) => {
-    const { isFeatureFlagEnabled } = useFeatureFlags();
-    const showVMUpdates = isFeatureFlagEnabled('ROX_POSTGRES_DATASTORE');
-
     const workflowState = useContext(workflowStateContext);
     function getTab(tabType) {
         return {
-            group: entityGroupMap[tabType],
+            group: entityGroups[entityGroupMap[tabType]],
             value: tabType,
             text: pluralize(entityLabels[tabType]),
             to: workflowState.pushList(tabType).setSearch('').toUrl(),
         };
     }
 
-    let relationships = [
-        ...getEntityTypesByRelationship(
-            entityType,
-            relationshipTypes.MATCHES,
-            workflowState.useCase
-        ),
-        ...getEntityTypesByRelationship(
-            entityType,
-            relationshipTypes.CONTAINS,
-            workflowState.useCase
-        ),
-    ]
-        // @TODO: Remove the following filter step once ROX_POSTGRES_DATASTORE is ON
-        .filter((match) => {
-            return filterEntityRelationship(showVMUpdates, match);
-        });
-
-    if (!relationships) {
-        return null;
-    }
-
-    if (showVMUpdates && entityType === 'NODE') {
-        relationships = relationships.map((relatedEntityType) => {
-            if (relatedEntityType === 'COMPONENT') {
-                return 'NODE_COMPONENT';
-            }
-            if (relatedEntityType === 'CVE') {
-                return 'NODE_CVE';
-            }
-            return relatedEntityType;
-        });
-    }
-    if (showVMUpdates && entityType === 'NODE_COMPONENT') {
-        relationships = relationships.map((relatedEntityType) => {
-            if (relatedEntityType === 'CVE') {
-                return 'NODE_CVE';
-            }
-            return relatedEntityType;
-        });
-    }
-    if (showVMUpdates && entityType === 'IMAGE_COMPONENT') {
-        relationships = relationships.map((relatedEntityType) => {
-            if (relatedEntityType === 'CVE') {
-                return 'IMAGE_CVE';
-            }
-            return relatedEntityType;
-        });
-    }
-    if (showVMUpdates && entityType === 'IMAGE') {
-        relationships = relationships.map((relatedEntityType) => {
-            if (relatedEntityType === 'COMPONENT') {
-                return 'IMAGE_COMPONENT';
-            }
-            if (relatedEntityType === 'CVE') {
-                return 'IMAGE_CVE';
-            }
-            return relatedEntityType;
-        });
-    }
+    const relationships = [
+        ...getVulnerabilityManagementEntityTypesByRelationship(entityType, 'MATCHES'),
+        ...getVulnerabilityManagementEntityTypesByRelationship(entityType, 'CONTAINS'),
+    ];
 
     const entityTabs = relationships.map((relationship) => getTab(relationship, entityType));
     const groups = Object.values(entityGroups);
