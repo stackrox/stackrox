f251f3c5ea5462c5cb08f22363c809e9bdb33d1c
Saif Chaudhry <schaudhr@redhat.com>
2023-06-21 17:44:22 -0700

    fixed timezone issue

--- ui/apps/platform/src/Containers/Vulnerabilities/VulnerablityReporting/VulnReports/LastRunState.test.tsx
+++ ui/apps/platform/src/Containers/Vulnerabilities/VulnerablityReporting/VulnReports/LastRunState.test.tsx
@@ -6,6 +6,19 @@ import { ReportStatus } from 'types/report.proto';
 import LastRunState from './LastRunState';
 
 describe('LastRunState', () => {
+    let originalTimeZone;
+
+    beforeAll(() => {
+        // Save original timezone and set new one for testing
+        originalTimeZone = process.env.TZ;
+        process.env.TZ = 'UTC';
+    });
+
+    afterAll(() => {
+        // Restore original timezone
+        process.env.TZ = originalTimeZone;
+    });
+
     test('should show the time stamp of the last successful prepared download', async () => {
         const reportStatus: ReportStatus = {
             runState: 'SUCCESS',
@@ -19,7 +32,7 @@ describe('LastRunState', () => {
         render(<LastRunState reportStatus={reportStatus} />);
 
         // ASSERT
-        expect(screen.getByText('06/20/2023 | 3:59:46AM')).toBeDefined();
+        expect(screen.getByText('06/20/2023 | 10:59:46AM')).toBeDefined();
     });
 
     test('should show the time stamp of the last error when preparing a download', async () => {
@@ -35,7 +48,7 @@ describe('LastRunState', () => {
         render(<LastRunState reportStatus={reportStatus} />);
 
         // ASSERT
-        expect(screen.getByText('06/20/2023 | 3:59:46AM')).toBeDefined();
+        expect(screen.getByText('06/20/2023 | 10:59:46AM')).toBeDefined();
     });
 
     test('should show the time stamp of the last successful email', async () => {
@@ -51,7 +64,7 @@ describe('LastRunState', () => {
         render(<LastRunState reportStatus={reportStatus} />);
 
         // ASSERT
-        expect(screen.getByText('06/20/2023 | 3:59:46AM')).toBeDefined();
+        expect(screen.getByText('06/20/2023 | 10:59:46AM')).toBeDefined();
     });
 
     test('should show the time stamp of the last error attempting to email', async () => {
@@ -67,7 +80,7 @@ describe('LastRunState', () => {
         render(<LastRunState reportStatus={reportStatus} />);
 
         // ASSERT
-        expect(screen.getByText('06/20/2023 | 3:59:46AM')).toBeDefined();
+        expect(screen.getByText('06/20/2023 | 10:59:46AM')).toBeDefined();
     });
 
     test('should show a spinner and the correct text when preparing a report', async () => {
