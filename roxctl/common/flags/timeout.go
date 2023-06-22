ed89e0b7c208ddb27e9fbecc2a925dec3ba25fd7
Daniel Haus <dhaus@redhat.com>
2023-06-22 04:10:21 +0200

    rox-11796-define-timeout-for-commands

--- roxctl/common/flags/timeout.go
+++ roxctl/common/flags/timeout.go
@@ -18,12 +18,14 @@ func AddTimeoutWithDefault(c *cobra.Command, defaultDuration time.Duration) {
 
 // AddTimeout adds a timeout flag to the given command, with the global default value.
 func AddTimeout(c *cobra.Command) {
-	AddTimeoutWithDefault(c, 30*time.Second)
+	AddTimeoutWithDefault(c, 1*time.Minute)
 }
 
 // Timeout returns the set timeout.
 func Timeout(c *cobra.Command) time.Duration {
-	duration, err := c.Flags().GetDuration(timeoutFlagName)
+	// Since a command can be the one adding the timeout flag via AddTimeout, make sure we look at the combined
+	// list of persistent flag set and the commands flag set called LocalFlags.
+	duration, err := c.LocalFlags().GetDuration(timeoutFlagName)
 	if err != nil {
 		// This is a programming error. You shouldn't use the timeout flag unless you've added it to your command!
 		// This helps us fail explicitly instead of defaulting to a zero timeout and allowing people to think it worked.
