7d1d4c4de413ad69315dd35a758f1bf95215028b
Jan Schnathmeier <jschnath@redhat.com>
2023-06-22 16:53:12 +0200

    Add debug message to ClusterTestRunner

--- .openshift-ci/runners.py
+++ .openshift-ci/runners.py
@@ -82,6 +82,7 @@ class ClusterTestSetsRunner:
                 hold = err
 
         if hold is not None:
+            print(f"Exception \"{str(hold)}\" raised in ClusterTestRunner", file=sys.stderr)
             raise hold
 
     def run_test_set(self, test_set):
