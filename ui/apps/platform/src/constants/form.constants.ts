b8a7f2188a68532a32798a87d2ecdad7dad62d97
Mark Pedrotti <pedrottimark@gmail.com>
2023-06-22 10:56:37 -0400

    fix(ui): Delete unneeded Tailwind font weight (part 4) (#6611)

--- ui/apps/platform/src/constants/form.constants.ts
+++ ui/apps/platform/src/constants/form.constants.ts
@@ -1,21 +1,22 @@
 // styles for forms, until we standardize on UI components for forms
 export const labelClassName = 'block py-2 text-base-600 font-700';
-export const sublabelClassName = 'font-600';
+export const sublabelClassName = 'font-400';
 
 export const wrapperMarginClassName = 'mb-4';
 
 export const inputBaseClassName =
-    'bg-base-100 border-2 border-base-300 hover:border-base-400 font-600 leading-normal p-2 first:rounded-t last:rounded-b text-base-600 disabled:bg-base-200';
+    'bg-base-100 border-2 border-base-300 hover:border-base-400 leading-normal p-2 first:rounded-t last:rounded-b text-base-600 disabled:bg-base-200';
 export const inputTextClassName = `${inputBaseClassName} w-full`;
 export const inputNumberClassName = `${inputBaseClassName} text-right w-12`;
 
 export const divToggleOuterClassName =
-    'bg-base-100 border-2 border-base-300 hover:border-base-400 font-600 leading-normal px-2 py-2 rounded-t last:rounded-b text-base-600 w-full';
+    'bg-base-100 border-2 border-base-300 hover:border-base-400 leading-normal px-2 py-2 rounded-t last:rounded-b text-base-600 w-full';
 
 export const justifyBetweenClassName = 'flex items-center justify-between';
 
 // The select element base style includes: pr-8 w-full
+// Add font-400 to override `font-weight: 600` for select element from ui-components.css file.
 export const selectElementClassName =
-    'bg-base-100 block border-base-300 focus:border-base-500 p-2 text-base-600 z-1';
+    'bg-base-100 block border-base-300 focus:border-base-500 p-2 text-base-600 font-400 z-1';
 export const selectWrapperClassName =
-    'bg-base-100 border-2 border-base-300 hover:border-base-400 font-600 leading-normal rounded text-base-600 w-full';
+    'bg-base-100 border-2 border-base-300 hover:border-base-400 leading-normal rounded text-base-600 w-full';
