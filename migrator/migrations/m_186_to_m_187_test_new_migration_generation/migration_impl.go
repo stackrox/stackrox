3b95bf29fa958597d4026a4f4f10206887af7e73
Yann Brillouet <ybrillou@redhat.com>
2023-06-22 10:11:32 +0200

    Re-generate showcase migration

--- migrator/migrations/m_186_to_m_187_test_new_migration_generation/migration_impl.go
+++ migrator/migrations/m_186_to_m_187_test_new_migration_generation/migration_impl.go
@@ -25,8 +25,20 @@ import (
 //  - remove the scoped access control code from the copied store files
 //  - remove the metrics collection code from the copied store files
 
+// TODO(dont-merge): Determine if this change breaks a previous releases database.
+// If so increment the `MinimumSupportedDBVersionSeqNum` to the `CurrentDBVersionSeqNum` of the release immediately
+// following the release that cannot tolerate the change in pkg/migrations/internal/fallback_seq_num.go.
+//
+// For example, in 4.2 a column `column_v2` is added to replace the `column_v1` column in 4.1.
+// All the code from 4.2 onward will not reference `column_v1`. At some point in the future a rollback to 4.1
+// will not longer be supported and we want to remove `column_v1`. To do so, we will upgrade the schema to remove
+// the column and update the `MinimumSupportedDBVersionSeqNum` to be the value of `CurrentDBVersionSeqNum` in 4.2
+// as 4.1 will no longer be supported. The migration process will inform the user of an error when trying to migrate
+// to a software version that can no longer be supported by the database.
+
 func migrate(database *types.Databases) error {
 	ctx := sac.WithAllAccess(context.Background())
+	_ = database // TODO(dont-merge): remove this line, it is there to make the compiler happy while the migration code is being written.
 	_ = ctx // TODO(dont-merge): remove this line, it is there to make the compiler happy while the migration code is being written.
 
 	// TODO(dont-merge): Migration code comes here
