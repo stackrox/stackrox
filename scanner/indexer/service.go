0cea20d4acba02bd85cd39aacd500174f38d0cf7
J. Victor Martins <jvdm@sdf.org>
2023-06-23 12:08:08 -0700

    Add initial gRPC API services

--- scanner/indexer/service.go
+++ scanner/indexer/service.go
@@ -1,12 +1,43 @@
 package indexer
 
-// TODO: Make this into a gRPC service.
-type Service struct {
+import (
+	"context"
+
+	"github.com/gogo/protobuf/types"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	v4 "github.com/stackrox/rox/generated/internalapi/scanner/v4"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type indexerService struct {
+	v4.UnimplementedIndexerServer
 	indexer *Indexer
 }
 
-func NewService(indexer *Indexer) (*Service, error) {
-	return &Service{
+func NewIndexerService(indexer *Indexer) (*indexerService, error) {
+	return &indexerService{
 		indexer: indexer,
 	}, nil
+}
+
+func (s *indexerService) CreateIndexReport(ctx context.Context, req *v4.CreateIndexReportRequest) (*v4.IndexReport, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CreateIndexReport not implemented")
+}
+func (s *indexerService) GetIndexReport(ctx context.Context, req *v4.GetIndexReportRequest) (*v4.IndexReport, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetIndexReport not implemented")
+}
+func (s *indexerService) HasIndexReport(ctx context.Context, req *v4.HasIndexReportRequest) (*types.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method HasIndexReport not implemented")
+}
+
+// RegisterServiceServer registers this service with the given gRPC Server.
+func (s *indexerService) RegisterServiceServer(grpcServer *grpc.Server) {
+	v4.RegisterIndexerServer(grpcServer, s)
+}
+
+// RegisterServiceHandler registers this service with the given gRPC Gateway endpoint.
+func (s *indexerService) RegisterServiceHandler(ctx context.Context, mux *runtime.ServeMux, conn *grpc.ClientConn) error {
+	return nil
 }
--- scanner/matcher/service.go
+++ scanner/matcher/service.go
@@ -1,12 +1,40 @@
 package matcher
 
-// TODO: Make this into a gRPC service.
-type Service struct {
+import (
+	"context"
+
+	"github.com/gogo/protobuf/types"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	v4 "github.com/stackrox/rox/generated/internalapi/scanner/v4"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type matcherService struct {
+	v4.UnimplementedMatcherServer
 	matcher *Matcher
 }
 
-func NewService(matcher *Matcher) (*Service, error) {
-	return &Service{
+func NewMatcherService(matcher *Matcher) (*matcherService, error) {
+	return &matcherService{
 		matcher: matcher,
 	}, nil
+}
+
+func (s *matcherService) GetVulnerabilities(ctx context.Context, req *v4.GetVulnerabilitiesRequest) (*v4.VulnerabilityReport, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetVulnerabilities not implemented")
+}
+func (s *matcherService) GetMetadata(ctx context.Context, req *types.Empty) (*v4.Metadata, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetMetadata not implemented")
+}
+
+// RegisterServiceServer registers this service with the given gRPC Server.
+func (s *matcherService) RegisterServiceServer(grpcServer *grpc.Server) {
+	v4.RegisterMatcherServer(grpcServer, s)
+}
+
+// RegisterServiceHandler registers this service with the given gRPC Gateway endpoint.
+func (s *matcherService) RegisterServiceHandler(ctx context.Context, mux *runtime.ServeMux, conn *grpc.ClientConn) error {
+	return nil
 }
