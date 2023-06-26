223468a6ee808579172c36c89272098d356f9da4
Tomasz Janiszewski <tomek@redhat.com>
2023-06-26 18:04:47 +0200

    chroe(pg): replace multikey store with single key

--- tools/generate-helpers/pg-table-bindings/multitest/postgres/gen.go
+++ tools/generate-helpers/pg-table-bindings/multitest/postgres/gen.go
@@ -1,3 +1,3 @@
 package postgres
 
-//go:generate pg-table-bindings-wrapper --type=storage.TestMultiKeyStruct --search-category 101
+//go:generate pg-table-bindings-wrapper --type=storage.TestStruct --search-category 101
