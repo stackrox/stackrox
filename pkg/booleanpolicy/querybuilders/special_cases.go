625876bed013a1dc468e5fa846bd9a8d29d6993b
Khushboo Sancheti <42253461+clickboo@users.noreply.github.com>
2023-06-26 17:55:43 -0700

    ROX-18083: Drop ALL support in drop capabilities critiera (#6686)

--- pkg/booleanpolicy/querybuilders/special_cases.go
+++ pkg/booleanpolicy/querybuilders/special_cases.go
@@ -26,15 +26,36 @@ func ForK8sRBAC() QueryBuilder {
 
 // ForDropCaps returns a specific query builder for drop capabilities.
 // Note that here, we always negate -- the user specifies a list of capabilities that _must_ be dropped,
-// so we want to find deployments that don't drop these capabilities.
+// so we want to find deployments that don't drop these capabilities. Deployments that DROP ALL capabilities
+// implicitly drop any capabilities that are specified as values in the policy group.
 func ForDropCaps() QueryBuilder {
 	return queryBuilderFunc(func(group *storage.PolicyGroup) []*query.FieldQuery {
-		return []*query.FieldQuery{{
+		// Do the group values already contain "ALL" as a value"?
+		containsAll := false
+		for _, v := range group.Values {
+			if v.Value == "ALL" {
+				containsAll = true
+			}
+		}
+		var queries []*query.FieldQuery
+		// If values do not contain ALL already, add it, for the implicit case.
+		// If a deployment drops ALL, it drops capabilities that are specified in the values and hence
+		// that deployment must not generate a violation
+		if !containsAll {
+			queries = append(queries, &query.FieldQuery{
+				Field:  search.DropCapabilities.String(),
+				Values: []string{"ALL"},
+				Negate: true,
+			})
+		}
+		queries = append(queries, &query.FieldQuery{
 			Field:    search.DropCapabilities.String(),
 			Negate:   true,
 			Values:   mapValues(group, valueToStringExact),
 			Operator: operatorProtoMap[group.GetBooleanOperator()],
-		}}
+		})
+
+		return queries
 	})
 }
 
