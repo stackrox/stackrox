609d885fc947ca17f9e11094188d77194a4f349d
Connor Gorman <cgorman@redhat.com>
2023-06-27 15:28:45 -0700

    run checksum check:

--- image/rhel/fetch-stackrox-data.sh
+++ image/rhel/fetch-stackrox-data.sh
@@ -9,9 +9,14 @@ fetch_stackrox_data() {
     local latest_prefix
     latest_prefix="$(curl https://definitions.stackrox.io/external-networks/latest_prefix)"
     curl "https://definitions.stackrox.io/${latest_prefix}/checksum" > /tmp/external-networks/checksum
-    curl "https://definitions.stackrox.io/${latest_prefix}/networks" > /tmp/external-networks/networks
     test -s /tmp/external-networks/checksum
+
+    curl "https://definitions.stackrox.io/${latest_prefix}/networks" > /tmp/external-networks/networks
     test -s /tmp/external-networks/networks
+
+    echo -n " /tmp/external-networks/networks" >> /tmp/external-networks/checksum
+    sha256sum -c /tmp/external-networks/checksum
+
     mkdir /stackrox-data/external-networks
     zip -jr /stackrox-data/external-networks/external-networks.zip /tmp/external-networks
     rm -rf /tmp/external-networks
