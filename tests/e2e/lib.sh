a27aa8d25a870fe1daa5a4a966e97270d74b0160
Brad Rogers <61400697+bradr5@users.noreply.github.com>
2023-06-28 22:37:49 -0500

    ROX-18109: Add Compliance 2.0 feature flag (#6733)

--- tests/e2e/lib.sh
+++ tests/e2e/lib.sh
@@ -105,6 +105,7 @@ export_test_environment() {
     ci_export ROX_VULN_MGMT_WORKLOAD_CVES "${ROX_VULN_MGMT_WORKLOAD_CVES:-true}"
     ci_export ROX_SEND_NAMESPACE_LABELS_IN_SYSLOG "${ROX_SEND_NAMESPACE_LABELS_IN_SYSLOG:-true}"
     ci_export ROX_DECLARATIVE_CONFIGURATION "${ROX_DECLARATIVE_CONFIGURATION:-true}"
+    ci_export ROX_COMPLIANCE_ENHANCEMENTS "${ROX_COMPLIANCE_ENHANCEMENTS:-true}"
 
     if is_in_PR_context && pr_has_label ci-fail-fast; then
         ci_export FAIL_FAST "true"
