1f17e32343ff644df0612ed3f83c209b3ceb813c
David Vail <dcvail17@gmail.com>
2023-06-30 12:29:43 -0400

    Use live data for listening endpoints table

--- ui/apps/platform/src/Containers/Audit/ListeningEndpoints/ListeningEndpointsTable.tsx
+++ ui/apps/platform/src/Containers/Audit/ListeningEndpoints/ListeningEndpointsTable.tsx
@@ -5,12 +5,13 @@ import { Tbody, Tr, Td, TableComposable, Th, Thead } from '@patternfly/react-tab
 import { ProcessListeningOnPort } from 'services/ProcessListeningOnPortsService';
 import EmptyStateTemplate from 'Components/PatternFly/EmptyStateTemplate';
 import { l4ProtocolLabels } from 'constants/networkFlow';
+import { ListDeployment } from 'types/deployment.proto';
 
 export type ListeningEndpointsTableProps = {
-    listeningEndpoints: ProcessListeningOnPort[];
+    deployments: (ListDeployment & { listeningEndpoints: ProcessListeningOnPort[] })[];
 };
 
-function ListeningEndpointsTable({ listeningEndpoints }: ListeningEndpointsTableProps) {
+function ListeningEndpointsTable({ deployments }: ListeningEndpointsTableProps) {
     return (
         <TableComposable borders={false} variant="compact">
             <Thead noWrap>
@@ -19,14 +20,15 @@ function ListeningEndpointsTable({ listeningEndpoints }: ListeningEndpointsTable
                     <Th>PID</Th>
                     <Th>Port</Th>
                     <Th>Protocol</Th>
+                    <Th>Deployment</Th>
                     <Th>Namespace</Th>
-                    <Th>Cluster ID</Th>
+                    <Th>Cluster</Th>
                     <Th>Pod ID</Th>
                     <Th>Container name</Th>
                 </Tr>
             </Thead>
             <Tbody>
-                {listeningEndpoints.length === 0 && (
+                {deployments.length === 0 && (
                     <Tr>
                         <Td colSpan={8}>
                             <Bullseye>
@@ -35,19 +37,20 @@ function ListeningEndpointsTable({ listeningEndpoints }: ListeningEndpointsTable
                         </Td>
                     </Tr>
                 )}
-                {listeningEndpoints.map(
-                    ({ podId, endpoint, signal, containerName, namespace, clusterId }) => (
-                        <Tr key={`${podId}/${endpoint.port}`}>
+                {deployments.flatMap(({ id, name, namespace, cluster, listeningEndpoints }) =>
+                    listeningEndpoints.map(({ podId, endpoint, signal, containerName }) => (
+                        <Tr key={`${id}/${podId}/${endpoint.port}`}>
                             <Td dataLabel="Program name">{signal.name}</Td>
                             <Td dataLabel="PID">{signal.pid}</Td>
                             <Td dataLabel="Port">{endpoint.port}</Td>
                             <Td dataLabel="Protocol">{l4ProtocolLabels[endpoint.protocol]}</Td>
+                            <Td dataLabel="Deployment">{name}</Td>
                             <Td dataLabel="Namespace">{namespace}</Td>
-                            <Td dataLabel="Cluster ID">{clusterId}</Td>
+                            <Td dataLabel="Cluster">{cluster}</Td>
                             <Td dataLabel="Pod ID">{podId}</Td>
                             <Td dataLabel="Container name">{containerName}</Td>
                         </Tr>
-                    )
+                    ))
                 )}
             </Tbody>
         </TableComposable>
