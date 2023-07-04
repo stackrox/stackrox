530b70f5147fed0711cbacec458fa2c6da61a23e
Daniel Haus <dhaus@redhat.com>
2023-07-04 19:52:56 +0200

    Add log statement.

--- central/events/datastore/singleton.go
+++ central/events/datastore/singleton.go
@@ -3,18 +3,21 @@ package datastore
 import (
 	pgStore "github.com/stackrox/rox/central/events/datastore/store/postgres"
 	"github.com/stackrox/rox/central/globaldb"
+	"github.com/stackrox/rox/pkg/logging"
 	"github.com/stackrox/rox/pkg/sync"
 )
 
 var (
 	once sync.Once
 
-	ds DataStore
+	ds  DataStore
+	log = logging.LoggerForModule()
 )
 
 // Singleton returns a datastore instance to handle events.
 func Singleton() DataStore {
 	once.Do(func() {
+		log.Info("Created the singleton for the datastore")
 		ds = New(pgStore.New(globaldb.GetPostgres()))
 	})
 	return ds
