49ac9796fcc93e3d6ab252a6b6078b66ad79e32b
David Shrewsberry <99685630+dashrews78@users.noreply.github.com>
2023-07-07 10:23:01 -0400

    turn on process pruning (#6817)

--- pkg/env/process_pruning.go
+++ pkg/env/process_pruning.go
@@ -4,5 +4,5 @@ var (
 	// ProcessPruningEnabled toggles whether process pruning should be done periodically using heuristics
 	// This may be useful in certain large environments, but is fairly expensive as it requires a full
 	// sweep over the database
-	ProcessPruningEnabled = RegisterBooleanSetting("ROX_PROCESS_PRUNING", false)
+	ProcessPruningEnabled = RegisterBooleanSetting("ROX_PROCESS_PRUNING", true)
 )
