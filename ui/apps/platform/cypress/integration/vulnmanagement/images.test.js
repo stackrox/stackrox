cd14a47ed05a9d9fb50c6d3c5067e7f093134e0b
Mark Pedrotti <pedrottimark@gmail.com>
2023-07-10 08:58:21 -0400

    chore(ui): Move helpers and selectors into vulnmanagement test folder (#6808)

--- ui/apps/platform/cypress/integration/vulnmanagement/images.test.js
+++ ui/apps/platform/cypress/integration/vulnmanagement/images.test.js
@@ -1,4 +1,3 @@
-import { selectors } from '../../constants/VulnManagementPage';
 import withAuth from '../../helpers/basicAuth';
 import {
     assertSortedItems,
@@ -6,13 +5,15 @@ import {
     callbackForPairOfDescendingNumberValuesFromElements,
 } from '../../helpers/sort';
 import {
-    getCountAndNounFromImageCVEsLinkResults,
     hasTableColumnHeadings,
     interactAndWaitForVulnerabilityManagementEntities,
     verifyConditionalCVEs,
     verifySecondaryEntities,
     visitVulnerabilityManagementEntities,
-} from '../../helpers/vulnmanagement/entities';
+    visitVulnerabilityManagementEntityInSidePanel,
+    visitVulnerabilityManagementSecondaryEntitiesInSidePanel,
+} from './VulnerabilityManagement.helpers';
+import { selectors } from './VulnerabilityManagement.selectors';
 
 const entitiesKey = 'images';
 
@@ -108,17 +109,23 @@ describe('Vulnerability Management Images', () => {
         });
     });
 
+    it('should show a message when image scan data is incomplete', () => {
+        const fixturePath = 'images/vmImageOverview.json';
+
+        cy.fixture(fixturePath).then((body) => {
+            const { id } = body.data.result;
+            const staticResponse = { body };
+            visitVulnerabilityManagementEntityInSidePanel(entitiesKey, id, staticResponse);
+        });
+
+        cy.get(selectors.scanDataMessage);
+    });
+
     // Argument 3 in verify functions is index of column which has the links.
     // The one-based index includes checkbox, hidden, invisible.
 
     it('should display either links for image CVEs or text for No CVEs', () => {
-        verifyConditionalCVEs(
-            entitiesKey,
-            'image-cves',
-            3,
-            'vulnCounter',
-            getCountAndNounFromImageCVEsLinkResults
-        );
+        verifyConditionalCVEs(entitiesKey, 'image-cves', 3, 'vulnCounter');
     });
 
     it('should display links for deployments', () => {
@@ -130,10 +137,8 @@ describe('Vulnerability Management Images', () => {
     });
 
     it('should show entity icon, not back button, if there is only one item on the side panel stack', () => {
-        visitVulnerabilityManagementEntities(entitiesKey);
+        visitVulnerabilityManagementSecondaryEntitiesInSidePanel(entitiesKey, 'deployments', 9);
 
-        cy.get(`${selectors.deploymentCountLink}:eq(0)`).click({ force: true });
-        cy.wait(1000);
         cy.get(selectors.backButton).should('exist');
         cy.get(selectors.entityIcon).should('not.exist');
 
