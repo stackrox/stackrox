19eba631750e047445da5ec5e0e788fe99b317ea
Saif Chaudhry <schaudhr@redhat.com>
2023-07-10 10:11:47 -0700

    ROX-17849: Allowing creating a vuln report

---
+++ ui/apps/platform/src/Containers/Vulnerabilities/VulnerablityReporting/api/useCreateReport.ts
@@ -0,0 +1,117 @@
+import { useCallback, useState } from 'react';
+
+import { createReportConfiguration } from 'services/ReportsService';
+import {
+    Fixability,
+    ReportConfiguration,
+    VulnerabilityReportFilters,
+    VulnerabilityReportFiltersBase,
+} from 'types/reportConfigurationService.proto';
+import { ReportFormValues } from '../forms/useReportFormValues';
+
+type Result = {
+    data: ReportConfiguration | null;
+    isLoading: boolean;
+    error: string | null;
+};
+
+type CreateReportResult = {
+    createReport: (formValues: ReportFormValues) => void;
+} & Result;
+
+const defaultResult = {
+    data: null,
+    isLoading: false,
+    error: null,
+};
+
+function useCreateReport(): CreateReportResult {
+    const [result, setResult] = useState<Result>(defaultResult);
+
+    const createReport = useCallback((formValues: ReportFormValues) => {
+        setResult({
+            data: null,
+            isLoading: true,
+            error: null,
+        });
+
+        const { reportParameters } = formValues;
+
+        // transform form values to values to be sent through API
+        const fixability: Fixability =
+            reportParameters.cveStatus.length > 1 ? 'BOTH' : reportParameters.cveStatus[0];
+        const vulnReportFiltersBase: VulnerabilityReportFiltersBase = {
+            fixability,
+            severities: reportParameters.cveSeverities,
+            imageTypes: reportParameters.imageType,
+        };
+        let vulnReportFilters: VulnerabilityReportFilters;
+
+        if (reportParameters.cvesDiscoveredSince === 'SINCE_LAST_REPORT') {
+            vulnReportFilters = {
+                ...vulnReportFiltersBase,
+                lastSuccessfulReport: true,
+            };
+        } else if (
+            reportParameters.cvesDiscoveredSince === 'START_DATE' &&
+            reportParameters.cvesDiscoveredStartDate
+        ) {
+            vulnReportFilters = {
+                ...vulnReportFiltersBase,
+                startDate: new Date(reportParameters.cvesDiscoveredStartDate).toISOString(),
+            };
+        } else {
+            vulnReportFilters = {
+                ...vulnReportFiltersBase,
+                allVuln: true,
+            };
+        }
+
+        const reportData: ReportConfiguration = {
+            id: '',
+            name: reportParameters.reportName,
+            description: reportParameters.description,
+            type: 'VULNERABILITY',
+            vulnReportFilters,
+            resourceScope: {
+                collectionScope: {
+                    collectionId: reportParameters.reportScope?.id || '',
+                    collectionName: reportParameters.reportScope?.name || '',
+                },
+            },
+            notifiers: [],
+            schedule: {
+                intervalType: 'WEEKLY',
+                hour: 0,
+                minute: 0,
+                daysOfWeek: {
+                    days: [3],
+                },
+            },
+        };
+
+        // send API call
+        createReportConfiguration(reportData)
+            .then((response) => {
+                setResult({
+                    data: response,
+                    isLoading: false,
+                    error: null,
+                });
+            })
+            .catch((err) => {
+                setResult({
+                    data: null,
+                    isLoading: false,
+                    error: err.response.data.message,
+                });
+            });
+    }, []);
+
+    return {
+        ...result,
+        createReport,
+    };
+}
+
+export default useCreateReport;
