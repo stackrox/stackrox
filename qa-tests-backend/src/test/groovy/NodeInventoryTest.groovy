07f86332a91c037940beb0a28ff57c56778f9474
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2023-07-11 12:47:06 +0200

    Tune intervals and add one more repetition to setting env var

--- qa-tests-backend/src/test/groovy/NodeInventoryTest.groovy
+++ qa-tests-backend/src/test/groovy/NodeInventoryTest.groovy
@@ -38,17 +38,29 @@ class NodeInventoryTest extends BaseSpecification {
         boolean nodeInventoryContainerAvailable =
             orchestrator.containsDaemonSetContainer(Constants.STACKROX_NAMESPACE, "collector", "node-inventory")
         if (nodeInventoryContainerAvailable) {
-            log.info("Setting collector.node-inventory ROX_NODE_SCANNING_MAX_INITIAL_WAIT to 1s")
-            orchestrator.updateDaemonSetEnv(Constants.STACKROX_NAMESPACE, "collector", "node-inventory",
-                "ROX_NODE_SCANNING_MAX_INITIAL_WAIT", "1s")
+            // Sometimes one pod in daemon set will miss the env variable despite updating.
+            // Let's try this operation twice before giving up
+            waitForTrue(2, 20) {
+                log.info("Setting collector.node-inventory ROX_NODE_SCANNING_MAX_INITIAL_WAIT to 1s")
+                orchestrator.updateDaemonSetEnv(Constants.STACKROX_NAMESPACE, "collector", "node-inventory",
+                    "ROX_NODE_SCANNING_MAX_INITIAL_WAIT", "1s")
+                try {
+                    log.info("Wait for collector DS to be restarted with new values")
+                    waitForTrue(20, 10) {
+                        orchestrator.daemonSetEnvVarUpdated(Constants.STACKROX_NAMESPACE, "collector",
+                            "node-inventory", "ROX_NODE_SCANNING_MAX_INITIAL_WAIT", "1s")
+                    }
 
-            log.info("Wait for collector DS to be restarted with new values")
-            waitForTrue(20, 6) {
-                orchestrator.daemonSetEnvVarUpdated(Constants.STACKROX_NAMESPACE, "collector",
-                    "node-inventory", "ROX_NODE_SCANNING_MAX_INITIAL_WAIT", "1s")
-            }
-            waitForTrue(20, 6) {
-                orchestrator.daemonSetReady(Constants.STACKROX_NAMESPACE, "collector")
+                    log.info("Wait for collector DS to be ready")
+                    waitForTrue(20, 10) {
+                        orchestrator.daemonSetReady(Constants.STACKROX_NAMESPACE, "collector")
+                    }
+                }
+                catch (RuntimeException ignored) {
+                    log.info("Unable to bring collector ds to the desired state")
+                    return false
+                }
+                return true
             }
         }
         log.info("Waiting for scanner deployment to be ready")
