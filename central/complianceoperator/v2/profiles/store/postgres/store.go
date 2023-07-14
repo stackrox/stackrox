51c8453d9acd26f6dad7f17aa0e9a781841bd943
Dave Shrewsberry <dashrews@redhat.com>
2023-07-14 07:04:01 -0400

    rebase to regenerate

--- central/complianceoperator/v2/profiles/store/postgres/store.go
+++ central/complianceoperator/v2/profiles/store/postgres/store.go
@@ -6,7 +6,6 @@ import (
 	"context"
 	"time"
 
-	"github.com/hashicorp/go-multierror"
 	"github.com/jackc/pgx/v4"
 	"github.com/stackrox/rox/central/metrics"
 	"github.com/stackrox/rox/central/role/resources"
@@ -15,9 +14,7 @@ import (
 	"github.com/stackrox/rox/pkg/logging"
 	ops "github.com/stackrox/rox/pkg/metrics"
 	"github.com/stackrox/rox/pkg/postgres"
-	"github.com/stackrox/rox/pkg/postgres/pgutils"
 	pkgSchema "github.com/stackrox/rox/pkg/postgres/schema"
-	"github.com/stackrox/rox/pkg/sac"
 	pgSearch "github.com/stackrox/rox/pkg/search/postgres"
 	"github.com/stackrox/rox/pkg/sync"
 	"gorm.io/gorm"
@@ -26,8 +23,6 @@ import (
 const (
 	baseTable = "compliance_operator_profile_v2"
 	storeName = "ComplianceOperatorProfileV2"
-
-	batchAfter = 100
 
 	// using copyFrom, we may not even want to batch.  It would probably be simpler
 	// to deal with failures if we just sent it all.  Something to think about as we
@@ -41,10 +36,12 @@ var (
 	targetResource = resources.ComplianceOperator
 )
 
+type storeType = storage.ComplianceOperatorProfileV2
+
 // Store is the interface to interact with the storage for storage.ComplianceOperatorProfileV2
 type Store interface {
-	Upsert(ctx context.Context, obj *storage.ComplianceOperatorProfileV2) error
-	UpsertMany(ctx context.Context, objs []*storage.ComplianceOperatorProfileV2) error
+	Upsert(ctx context.Context, obj *storeType) error
+	UpsertMany(ctx context.Context, objs []*storeType) error
 	Delete(ctx context.Context, id string) error
 	DeleteByQuery(ctx context.Context, q *v1.Query) error
 	DeleteMany(ctx context.Context, identifiers []string) error
@@ -52,30 +49,31 @@ type Store interface {
 	Count(ctx context.Context) (int, error)
 	Exists(ctx context.Context, id string) (bool, error)
 
-	Get(ctx context.Context, id string) (*storage.ComplianceOperatorProfileV2, bool, error)
-	GetMany(ctx context.Context, identifiers []string) ([]*storage.ComplianceOperatorProfileV2, []int, error)
+	Get(ctx context.Context, id string) (*storeType, bool, error)
+	GetMany(ctx context.Context, identifiers []string) ([]*storeType, []int, error)
 	GetIDs(ctx context.Context) ([]string, error)
-	GetAll(ctx context.Context) ([]*storage.ComplianceOperatorProfileV2, error)
+	GetAll(ctx context.Context) ([]*storeType, error)
 
-	Walk(ctx context.Context, fn func(obj *storage.ComplianceOperatorProfileV2) error) error
+	Walk(ctx context.Context, fn func(obj *storeType) error) error
 }
 
 type storeImpl struct {
-	*pgSearch.GenericStore[storage.ComplianceOperatorProfileV2, *storage.ComplianceOperatorProfileV2]
-	db    postgres.DB
+	*pgSearch.GenericStore[storeType, *storeType]
 	mutex sync.RWMutex
 }
 
 // New returns a new Store instance using the provided sql instance.
 func New(db postgres.DB) Store {
 	return &storeImpl{
-		db: db,
-		GenericStore: pgSearch.NewGenericStore[storage.ComplianceOperatorProfileV2, *storage.ComplianceOperatorProfileV2](
+		GenericStore: pgSearch.NewGenericStore[storeType, *storeType](
 			db,
 			schema,
 			pkGetter,
+			insertIntoComplianceOperatorProfileV2,
+			copyFromComplianceOperatorProfileV2,
 			metricsSetAcquireDBConnDuration,
 			metricsSetPostgresOperationDurationTime,
+			pgSearch.GloballyScopedUpsertChecker[storeType, *storeType](targetResource),
 			targetResource,
 		),
 	}
@@ -83,7 +81,7 @@ func New(db postgres.DB) Store {
 
 // region Helper functions
 
-func pkGetter(obj *storage.ComplianceOperatorProfileV2) string {
+func pkGetter(obj *storeType) string {
 	return obj.GetId()
 }
 
@@ -145,7 +143,7 @@ func insertIntoComplianceOperatorProfileV2Rules(_ context.Context, batch *pgx.Ba
 	return nil
 }
 
-func (s *storeImpl) copyFromComplianceOperatorProfileV2(ctx context.Context, tx *postgres.Tx, objs ...*storage.ComplianceOperatorProfileV2) error {
+func copyFromComplianceOperatorProfileV2(ctx context.Context, s pgSearch.Deleter, tx *postgres.Tx, objs ...*storage.ComplianceOperatorProfileV2) error {
 
 	inputRows := [][]interface{}{}
 
@@ -156,21 +154,13 @@ func (s *storeImpl) copyFromComplianceOperatorProfileV2(ctx context.Context, tx 
 	var deletes []string
 
 	copyCols := []string{
-
 		"id",
-
 		"name",
-
 		"version",
-
 		"profileversion",
-
 		"producttype",
-
 		"standard",
-
 		"product",
-
 		"serialized",
 	}
 
@@ -186,21 +176,13 @@ func (s *storeImpl) copyFromComplianceOperatorProfileV2(ctx context.Context, tx 
 		}
 
 		inputRows = append(inputRows, []interface{}{
-
 			obj.GetId(),
-
 			obj.GetName(),
-
 			obj.GetVersion(),
-
 			obj.GetProfileVersion(),
-
 			obj.GetProductType(),
-
 			obj.GetStandard(),
-
 			obj.GetProduct(),
-
 			serialized,
 		})
 
@@ -232,7 +214,7 @@ func (s *storeImpl) copyFromComplianceOperatorProfileV2(ctx context.Context, tx 
 	for idx, obj := range objs {
 		_ = idx // idx may or may not be used depending on how nested we are, so avoid compile-time errors.
 
-		if err = s.copyFromComplianceOperatorProfileV2Rules(ctx, tx, obj.GetId(), obj.GetRules()...); err != nil {
+		if err = copyFromComplianceOperatorProfileV2Rules(ctx, s, tx, obj.GetId(), obj.GetRules()...); err != nil {
 			return err
 		}
 	}
@@ -240,18 +222,15 @@ func (s *storeImpl) copyFromComplianceOperatorProfileV2(ctx context.Context, tx 
 	return err
 }
 
-func (s *storeImpl) copyFromComplianceOperatorProfileV2Rules(ctx context.Context, tx *postgres.Tx, complianceOperatorProfileV2ID string, objs ...*storage.ComplianceOperatorProfileV2_Rule) error {
+func copyFromComplianceOperatorProfileV2Rules(ctx context.Context, s pgSearch.Deleter, tx *postgres.Tx, complianceOperatorProfileV2ID string, objs ...*storage.ComplianceOperatorProfileV2_Rule) error {
 
 	inputRows := [][]interface{}{}
 
 	var err error
 
 	copyCols := []string{
-
 		"compliance_operator_profile_v2_id",
-
 		"idx",
-
 		"rulename",
 	}
 
@@ -262,11 +241,8 @@ func (s *storeImpl) copyFromComplianceOperatorProfileV2Rules(ctx context.Context
 			"to simply use the object.  %s", obj)
 
 		inputRows = append(inputRows, []interface{}{
-
 			complianceOperatorProfileV2ID,
-
 			idx,
-
 			obj.GetRuleName(),
 		})
 
@@ -289,105 +265,9 @@ func (s *storeImpl) copyFromComplianceOperatorProfileV2Rules(ctx context.Context
 	return err
 }
 
-func (s *storeImpl) copyFrom(ctx context.Context, objs ...*storage.ComplianceOperatorProfileV2) error {
-	conn, err := s.AcquireConn(ctx, ops.Get)
-	if err != nil {
-		return err
-	}
-	defer conn.Release()
-
-	tx, err := conn.Begin(ctx)
-	if err != nil {
-		return err
-	}
-
-	if err := s.copyFromComplianceOperatorProfileV2(ctx, tx, objs...); err != nil {
-		if err := tx.Rollback(ctx); err != nil {
-			return err
-		}
-		return err
-	}
-	if err := tx.Commit(ctx); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (s *storeImpl) upsert(ctx context.Context, objs ...*storage.ComplianceOperatorProfileV2) error {
-	conn, err := s.AcquireConn(ctx, ops.Get)
-	if err != nil {
-		return err
-	}
-	defer conn.Release()
-
-	for _, obj := range objs {
-		batch := &pgx.Batch{}
-		if err := insertIntoComplianceOperatorProfileV2(ctx, batch, obj); err != nil {
-			return err
-		}
-		batchResults := conn.SendBatch(ctx, batch)
-		var result *multierror.Error
-		for i := 0; i < batch.Len(); i++ {
-			_, err := batchResults.Exec()
-			result = multierror.Append(result, err)
-		}
-		if err := batchResults.Close(); err != nil {
-			return err
-		}
-		if err := result.ErrorOrNil(); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
 // endregion Helper functions
 
-//// Interface functions
-
-// Upsert saves the current state of an object in storage.
-func (s *storeImpl) Upsert(ctx context.Context, obj *storage.ComplianceOperatorProfileV2) error {
-	defer metrics.SetPostgresOperationDurationTime(time.Now(), ops.Upsert, "ComplianceOperatorProfileV2")
-
-	scopeChecker := sac.GlobalAccessScopeChecker(ctx).AccessMode(storage.Access_READ_WRITE_ACCESS).Resource(targetResource)
-	if !scopeChecker.IsAllowed() {
-		return sac.ErrResourceAccessDenied
-	}
-
-	return pgutils.Retry(func() error {
-		return s.upsert(ctx, obj)
-	})
-}
-
-// UpsertMany saves the state of multiple objects in the storage.
-func (s *storeImpl) UpsertMany(ctx context.Context, objs []*storage.ComplianceOperatorProfileV2) error {
-	defer metrics.SetPostgresOperationDurationTime(time.Now(), ops.UpdateMany, "ComplianceOperatorProfileV2")
-
-	scopeChecker := sac.GlobalAccessScopeChecker(ctx).AccessMode(storage.Access_READ_WRITE_ACCESS).Resource(targetResource)
-	if !scopeChecker.IsAllowed() {
-		return sac.ErrResourceAccessDenied
-	}
-
-	return pgutils.Retry(func() error {
-		// Lock since copyFrom requires a delete first before being executed.  If multiple processes are updating
-		// same subset of rows, both deletes could occur before the copyFrom resulting in unique constraint
-		// violations
-		if len(objs) < batchAfter {
-			s.mutex.RLock()
-			defer s.mutex.RUnlock()
-
-			return s.upsert(ctx, objs...)
-		}
-		s.mutex.Lock()
-		defer s.mutex.Unlock()
-
-		return s.copyFrom(ctx, objs...)
-	})
-}
-
-//// Interface functions - END
-
-//// Used for testing
+// region Used for testing
 
 // CreateTableAndNewStore returns a new Store instance for testing.
 func CreateTableAndNewStore(ctx context.Context, db postgres.DB, gormDB *gorm.DB) Store {
@@ -411,4 +291,4 @@ func dropTableComplianceOperatorProfileV2Rules(ctx context.Context, db postgres.
 
 }
 
-//// Used for testing - END
+// endregion Used for testing
--- central/complianceoperator/v2/rules/store/postgres/store.go
+++ central/complianceoperator/v2/rules/store/postgres/store.go
@@ -6,7 +6,6 @@ import (
 	"context"
 	"time"
 
-	"github.com/hashicorp/go-multierror"
 	"github.com/jackc/pgx/v4"
 	"github.com/stackrox/rox/central/metrics"
 	"github.com/stackrox/rox/central/role/resources"
@@ -15,9 +14,7 @@ import (
 	"github.com/stackrox/rox/pkg/logging"
 	ops "github.com/stackrox/rox/pkg/metrics"
 	"github.com/stackrox/rox/pkg/postgres"
-	"github.com/stackrox/rox/pkg/postgres/pgutils"
 	pkgSchema "github.com/stackrox/rox/pkg/postgres/schema"
-	"github.com/stackrox/rox/pkg/sac"
 	pgSearch "github.com/stackrox/rox/pkg/search/postgres"
 	"github.com/stackrox/rox/pkg/sync"
 	"gorm.io/gorm"
@@ -26,8 +23,6 @@ import (
 const (
 	baseTable = "compliance_operator_rule_v2"
 	storeName = "ComplianceOperatorRuleV2"
-
-	batchAfter = 100
 
 	// using copyFrom, we may not even want to batch.  It would probably be simpler
 	// to deal with failures if we just sent it all.  Something to think about as we
@@ -41,10 +36,12 @@ var (
 	targetResource = resources.ComplianceOperator
 )
 
+type storeType = storage.ComplianceOperatorRuleV2
+
 // Store is the interface to interact with the storage for storage.ComplianceOperatorRuleV2
 type Store interface {
-	Upsert(ctx context.Context, obj *storage.ComplianceOperatorRuleV2) error
-	UpsertMany(ctx context.Context, objs []*storage.ComplianceOperatorRuleV2) error
+	Upsert(ctx context.Context, obj *storeType) error
+	UpsertMany(ctx context.Context, objs []*storeType) error
 	Delete(ctx context.Context, name string) error
 	DeleteByQuery(ctx context.Context, q *v1.Query) error
 	DeleteMany(ctx context.Context, identifiers []string) error
@@ -52,29 +49,30 @@ type Store interface {
 	Count(ctx context.Context) (int, error)
 	Exists(ctx context.Context, name string) (bool, error)
 
-	Get(ctx context.Context, name string) (*storage.ComplianceOperatorRuleV2, bool, error)
-	GetMany(ctx context.Context, identifiers []string) ([]*storage.ComplianceOperatorRuleV2, []int, error)
+	Get(ctx context.Context, name string) (*storeType, bool, error)
+	GetMany(ctx context.Context, identifiers []string) ([]*storeType, []int, error)
 	GetIDs(ctx context.Context) ([]string, error)
 
-	Walk(ctx context.Context, fn func(obj *storage.ComplianceOperatorRuleV2) error) error
+	Walk(ctx context.Context, fn func(obj *storeType) error) error
 }
 
 type storeImpl struct {
-	*pgSearch.GenericStore[storage.ComplianceOperatorRuleV2, *storage.ComplianceOperatorRuleV2]
-	db    postgres.DB
+	*pgSearch.GenericStore[storeType, *storeType]
 	mutex sync.RWMutex
 }
 
 // New returns a new Store instance using the provided sql instance.
 func New(db postgres.DB) Store {
 	return &storeImpl{
-		db: db,
-		GenericStore: pgSearch.NewGenericStore[storage.ComplianceOperatorRuleV2, *storage.ComplianceOperatorRuleV2](
+		GenericStore: pgSearch.NewGenericStore[storeType, *storeType](
 			db,
 			schema,
 			pkGetter,
+			insertIntoComplianceOperatorRuleV2,
+			copyFromComplianceOperatorRuleV2,
 			metricsSetAcquireDBConnDuration,
 			metricsSetPostgresOperationDurationTime,
+			pgSearch.GloballyScopedUpsertChecker[storeType, *storeType](targetResource),
 			targetResource,
 		),
 	}
@@ -82,7 +80,7 @@ func New(db postgres.DB) Store {
 
 // region Helper functions
 
-func pkGetter(obj *storage.ComplianceOperatorRuleV2) string {
+func pkGetter(obj *storeType) string {
 	return obj.GetName()
 }
 
@@ -117,7 +115,7 @@ func insertIntoComplianceOperatorRuleV2(_ context.Context, batch *pgx.Batch, obj
 	return nil
 }
 
-func (s *storeImpl) copyFromComplianceOperatorRuleV2(ctx context.Context, tx *postgres.Tx, objs ...*storage.ComplianceOperatorRuleV2) error {
+func copyFromComplianceOperatorRuleV2(ctx context.Context, s pgSearch.Deleter, tx *postgres.Tx, objs ...*storage.ComplianceOperatorRuleV2) error {
 
 	inputRows := [][]interface{}{}
 
@@ -128,17 +126,11 @@ func (s *storeImpl) copyFromComplianceOperatorRuleV2(ctx context.Context, tx *po
 	var deletes []string
 
 	copyCols := []string{
-
 		"name",
-
 		"version",
-
 		"ruleversion",
-
 		"ruletype",
-
 		"severity",
-
 		"serialized",
 	}
 
@@ -154,17 +146,11 @@ func (s *storeImpl) copyFromComplianceOperatorRuleV2(ctx context.Context, tx *po
 		}
 
 		inputRows = append(inputRows, []interface{}{
-
 			obj.GetName(),
-
 			obj.GetVersion(),
-
 			obj.GetRuleVersion(),
-
 			obj.GetRuleType(),
-
 			obj.GetSeverity(),
-
 			serialized,
 		})
 
@@ -196,105 +182,9 @@ func (s *storeImpl) copyFromComplianceOperatorRuleV2(ctx context.Context, tx *po
 	return err
 }
 
-func (s *storeImpl) copyFrom(ctx context.Context, objs ...*storage.ComplianceOperatorRuleV2) error {
-	conn, err := s.AcquireConn(ctx, ops.Get)
-	if err != nil {
-		return err
-	}
-	defer conn.Release()
-
-	tx, err := conn.Begin(ctx)
-	if err != nil {
-		return err
-	}
-
-	if err := s.copyFromComplianceOperatorRuleV2(ctx, tx, objs...); err != nil {
-		if err := tx.Rollback(ctx); err != nil {
-			return err
-		}
-		return err
-	}
-	if err := tx.Commit(ctx); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (s *storeImpl) upsert(ctx context.Context, objs ...*storage.ComplianceOperatorRuleV2) error {
-	conn, err := s.AcquireConn(ctx, ops.Get)
-	if err != nil {
-		return err
-	}
-	defer conn.Release()
-
-	for _, obj := range objs {
-		batch := &pgx.Batch{}
-		if err := insertIntoComplianceOperatorRuleV2(ctx, batch, obj); err != nil {
-			return err
-		}
-		batchResults := conn.SendBatch(ctx, batch)
-		var result *multierror.Error
-		for i := 0; i < batch.Len(); i++ {
-			_, err := batchResults.Exec()
-			result = multierror.Append(result, err)
-		}
-		if err := batchResults.Close(); err != nil {
-			return err
-		}
-		if err := result.ErrorOrNil(); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
 // endregion Helper functions
 
-//// Interface functions
-
-// Upsert saves the current state of an object in storage.
-func (s *storeImpl) Upsert(ctx context.Context, obj *storage.ComplianceOperatorRuleV2) error {
-	defer metrics.SetPostgresOperationDurationTime(time.Now(), ops.Upsert, "ComplianceOperatorRuleV2")
-
-	scopeChecker := sac.GlobalAccessScopeChecker(ctx).AccessMode(storage.Access_READ_WRITE_ACCESS).Resource(targetResource)
-	if !scopeChecker.IsAllowed() {
-		return sac.ErrResourceAccessDenied
-	}
-
-	return pgutils.Retry(func() error {
-		return s.upsert(ctx, obj)
-	})
-}
-
-// UpsertMany saves the state of multiple objects in the storage.
-func (s *storeImpl) UpsertMany(ctx context.Context, objs []*storage.ComplianceOperatorRuleV2) error {
-	defer metrics.SetPostgresOperationDurationTime(time.Now(), ops.UpdateMany, "ComplianceOperatorRuleV2")
-
-	scopeChecker := sac.GlobalAccessScopeChecker(ctx).AccessMode(storage.Access_READ_WRITE_ACCESS).Resource(targetResource)
-	if !scopeChecker.IsAllowed() {
-		return sac.ErrResourceAccessDenied
-	}
-
-	return pgutils.Retry(func() error {
-		// Lock since copyFrom requires a delete first before being executed.  If multiple processes are updating
-		// same subset of rows, both deletes could occur before the copyFrom resulting in unique constraint
-		// violations
-		if len(objs) < batchAfter {
-			s.mutex.RLock()
-			defer s.mutex.RUnlock()
-
-			return s.upsert(ctx, objs...)
-		}
-		s.mutex.Lock()
-		defer s.mutex.Unlock()
-
-		return s.copyFrom(ctx, objs...)
-	})
-}
-
-//// Interface functions - END
-
-//// Used for testing
+// region Used for testing
 
 // CreateTableAndNewStore returns a new Store instance for testing.
 func CreateTableAndNewStore(ctx context.Context, db postgres.DB, gormDB *gorm.DB) Store {
@@ -312,4 +202,4 @@ func dropTableComplianceOperatorRuleV2(ctx context.Context, db postgres.DB) {
 
 }
 
-//// Used for testing - END
+// endregion Used for testing
