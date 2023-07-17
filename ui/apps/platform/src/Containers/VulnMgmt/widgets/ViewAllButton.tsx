49472bbfe5bbe15ff507636556489db7f1b0258f
Mark Pedrotti <mark@stackrox.com>
2023-07-17 16:55:08 -0400

    fix(ui): Fix inconsistent case on Vulnerability Management dashboard

--- ui/apps/platform/src/Containers/VulnMgmt/widgets/ViewAllButton.tsx
+++ ui/apps/platform/src/Containers/VulnMgmt/widgets/ViewAllButton.tsx
@@ -4,7 +4,7 @@ import { HashLink } from 'react-router-hash-link';
 const ViewAllButton = ({ url }: { url: string }): ReactElement => {
     return (
         <HashLink to={url} className="btn-sm btn-base whitespace-nowrap no-underline">
-            View All
+            View all
         </HashLink>
     );
 };
