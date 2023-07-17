1f395dbf06e6b078581ca22a70e5c4ac0c6cd9b8
Frederico Bittencourt <fbittenc@redhat.com>
2023-07-17 11:40:32 +0200

    add docs

--- sensor/common/message/message.go
+++ sensor/common/message/message.go
@@ -6,23 +6,26 @@ import (
 	"github.com/stackrox/rox/generated/internalapi/central"
 )
 
+// ExpiringMessage is a wrapper on central.MsgFromSensor with the addition of a Context.
+// The context will be cancelled when the message is expired and should no longer be sent
+// to Central.
 type ExpiringMessage struct {
 	*central.MsgFromSensor
 	Context context.Context
 }
 
+// New creates an ExpiringMessage with msg and context.Background.
 func New(msg *central.MsgFromSensor) *ExpiringMessage {
-	return NewWithContext(msg, context.Background())
-}
-
-func NewWithContext(msg *central.MsgFromSensor, ctx context.Context) *ExpiringMessage {
-	expiringMessage := &ExpiringMessage{
+	return &ExpiringMessage{
 		MsgFromSensor: msg,
-		Context:       ctx,
+		Context:       context.Background(),
 	}
-	return expiringMessage
 }
 
+// Wait is a helper function that will wait until the context on the message is Done.
+// This should be used when sending on blocking channels to abort the process the message
+// expired in the meantime. If the context is not set, this function blocks forever (i.e. wait on
+// context.Background() channel).
 func (m *ExpiringMessage) Wait() <-chan struct{} {
 	if m.Context == nil {
 		return context.Background().Done()
@@ -30,6 +33,8 @@ func (m *ExpiringMessage) Wait() <-chan struct{} {
 	return m.Context.Done()
 }
 
+// IsExpired is a helper function that checks if the context already expired without blocking.
+// If the context isn't set this function will always return false.
 func (m *ExpiringMessage) IsExpired() bool {
 	if m.Context == nil {
 		return false
