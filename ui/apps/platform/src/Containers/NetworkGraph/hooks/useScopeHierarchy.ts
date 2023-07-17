6c245de693c9e19003e0907a1809bc9e8ec4455e
David Vail <dcvail17@gmail.com>
2023-07-17 15:35:05 -0400

    Clone default return object to avoid accidental writes

--- ui/apps/platform/src/Containers/NetworkGraph/hooks/useScopeHierarchy.ts
+++ ui/apps/platform/src/Containers/NetworkGraph/hooks/useScopeHierarchy.ts
@@ -1,3 +1,5 @@
+import cloneDeep from 'lodash/cloneDeep';
+
 import useURLSearch from 'hooks/useURLSearch';
 import { ClusterScopeObject } from 'services/RolesService';
 import { getScopeHierarchyFromSearch } from '../utils/hierarchyUtils';
@@ -19,5 +21,8 @@ const emptyScopeHierarchy = {
 export function useScopeHierarchy(availableClusters: ClusterScopeObject[]): NetworkScopeHierarchy {
     const { searchFilter } = useURLSearch();
 
-    return getScopeHierarchyFromSearch(searchFilter, availableClusters) ?? emptyScopeHierarchy;
+    return (
+        getScopeHierarchyFromSearch(searchFilter, availableClusters) ??
+        cloneDeep(emptyScopeHierarchy)
+    );
 }
