b0017c8095428b3335b1dd4d42ffbf80fe47c65f
Mandar Darwatkar <mandar@stackrox.com>
2023-07-18 14:01:57 -0700

    Remove legacy compliance integration feature flag (#6994)

--- pkg/features/list.go
+++ pkg/features/list.go
@@ -12,9 +12,6 @@ var (
 	// IntegrationsAsConfig enables loading integrations from config
 	IntegrationsAsConfig = registerFeature("Enable loading integrations from config", "ROX_INTEGRATIONS_AS_CONFIG", false)
 
-	// ComplianceOperatorCheckResults enables getting compliance results from the compliance operator
-	ComplianceOperatorCheckResults = registerFeature("Enable fetching of compliance operator results", "ROX_COMPLIANCE_OPERATOR_INTEGRATION", true)
-
 	// QuayRobotAccounts enables Robot accounts as credentials in Quay Image Integration.
 	QuayRobotAccounts = registerFeature("Enable Robot accounts in Quay Image Integration", "ROX_QUAY_ROBOT_ACCOUNTS", true)
 
