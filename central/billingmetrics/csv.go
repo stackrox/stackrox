792fd0476f3f2d94803faac29164479cd59530f6
Michaël Petrov <smartptr@gmail.com>
2023-07-21 16:28:43 +0200

    CHANGELOG.md, minor fix for csv

--- central/billingmetrics/csv.go
+++ central/billingmetrics/csv.go
@@ -11,6 +11,10 @@ import (
 	"github.com/stackrox/rox/pkg/protoconv"
 )
 
+var (
+	zeroTime = time.Unix(0, 0).UTC()
+)
+
 func writeCSV(metrics []storage.BillingMetrics, wio io.Writer) error {
 	w := csv.NewWriter(wio)
 	record := []string{"UTC Timestamp", "Nodes", "Millicores"}
@@ -18,7 +22,7 @@ func writeCSV(metrics []storage.BillingMetrics, wio io.Writer) error {
 		return errors.Wrap(err, "failed to write CSV header")
 	}
 	for _, m := range metrics {
-		record[0] = protoconv.ConvertTimestampToTimeOrNow(m.Ts).UTC().Format(time.RFC3339)
+		record[0] = protoconv.ConvertTimestampToTimeOrDefault(m.Ts, zeroTime).UTC().Format(time.RFC3339)
 		record[1] = fmt.Sprint(m.Sr.GetNodes())
 		record[2] = fmt.Sprint(m.Sr.GetMillicores())
 		if err := w.Write(record); err != nil {
