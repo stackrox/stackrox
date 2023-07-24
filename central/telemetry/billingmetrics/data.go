5689967befef2d710492e49f20931fbf04740564
Michaël Petrov <smartptr@gmail.com>
2023-07-24 10:16:45 +0200

    schedule periodic data insertion

---
+++ central/telemetry/billingmetrics/data.go
@@ -0,0 +1,69 @@
+package billingmetrics
+
+import (
+	"context"
+	"time"
+
+	"github.com/pkg/errors"
+	bmetrics "github.com/stackrox/rox/central/billingmetrics/store"
+	cluStore "github.com/stackrox/rox/central/cluster/datastore"
+	"github.com/stackrox/rox/central/sensor/service/pipeline/clustermetrics"
+	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/protoconv"
+	"github.com/stackrox/rox/pkg/sac"
+	"github.com/stackrox/rox/pkg/sac/resources"
+	"github.com/stackrox/rox/pkg/set"
+)
+
+var (
+	clusterReader = sac.AllowFixedScopes(
+		sac.AccessModeScopeKeys(storage.Access_READ_ACCESS),
+		sac.ResourceScopeKeys(resources.Cluster))
+
+	metricsWriter = sac.AllowFixedScopes(
+		sac.AccessModeScopeKeys(storage.Access_READ_ACCESS, storage.Access_READ_WRITE_ACCESS),
+		sac.ResourceScopeKeys(resources.Administration))
+
+	previousMetrics = &clustermetrics.BillingMetrics{}
+)
+
+func average(metrics ...*clustermetrics.BillingMetrics) clustermetrics.BillingMetrics {
+	n := int64(len(metrics))
+	a := clustermetrics.BillingMetrics{}
+	if n == 0 {
+		return a
+	}
+	for _, m := range metrics {
+		a.TotalNodes += m.TotalNodes
+		a.TotalMilliCores += m.TotalMilliCores
+	}
+	a.TotalNodes /= n
+	a.TotalMilliCores /= n
+	return a
+}
+
+func getClusterIDs() (set.StringSet, error) {
+	ctx := sac.WithGlobalAccessScopeChecker(context.Background(), clusterReader)
+
+	clusters, err := cluStore.Singleton().GetClusters(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "cluster datastore failure")
+	}
+	ids := set.NewStringSet()
+	for _, cluster := range clusters {
+		ids.Add(cluster.GetId())
+	}
+	return ids, nil
+}
+
+func checkIn(metrics clustermetrics.BillingMetrics) error {
+	ctx := sac.WithGlobalAccessScopeChecker(context.Background(), metricsWriter)
+
+	err := bmetrics.Singleton().Insert(ctx, &storage.BillingMetrics{
+		Ts: protoconv.ConvertTimeToTimestamp(time.Now()),
+		Sr: &storage.BillingMetrics_SecuredResources{
+			Nodes:      int32(metrics.TotalNodes),
+			Millicores: int32(metrics.TotalMilliCores)},
+	})
+	return errors.Wrap(err, "billing metrics datastore failure")
+}
---
+++ central/telemetry/billingmetrics/scheduler.go
@@ -0,0 +1,58 @@
+package billingmetrics
+
+import (
+	"time"
+
+	"github.com/stackrox/rox/central/sensor/service/pipeline/clustermetrics"
+	"github.com/stackrox/rox/pkg/concurrency"
+	"github.com/stackrox/rox/pkg/logging"
+)
+
+const period = 1 * time.Hour
+
+var (
+	log  = logging.LoggerForModule()
+	stop = concurrency.NewSignal()
+)
+
+func gather() {
+	ids, err := getClusterIDs()
+	if err != nil {
+		log.Debug("Failed to get cluster IDs for billing metrics snapshot: ", err)
+		return
+	}
+	log.Debugf("Cutting billing metrics for %d clusters: %v", len(ids), ids)
+	newMetrics := clustermetrics.CutMetrics(ids)
+	// Store the average values to smooth short (< 2 periods) peaks and drops.
+	if err := checkIn(average(previousMetrics, newMetrics)); err != nil {
+		log.Debug("Failed to store a billing metrics snapshot: ", err)
+	}
+	previousMetrics = newMetrics
+}
+
+func run() {
+	ticker := time.NewTicker(period)
+	defer ticker.Stop()
+	gather()
+	for {
+		select {
+		case <-ticker.C:
+			gather()
+		case <-stop.Done():
+			log.Debug("Billing metrics reporting stopped")
+			stop.Reset()
+			return
+		}
+	}
+}
+
+// Schedule initiates periodic data injections to the database with the
+// collected billing metrics.
+func Schedule() {
+	go run()
+}
+
+// Stop stops the scheduled timer
+func Stop() {
+	stop.Signal()
+}
