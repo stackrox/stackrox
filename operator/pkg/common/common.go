37b85c83a223b14958efbd2f0411312440410285
Matthias Meidinger <mmeiding@redhat.com>
2023-07-25 16:52:13 +0200

    Style

--- operator/pkg/common/common.go
+++ operator/pkg/common/common.go
@@ -3,7 +3,7 @@ package common
 import "github.com/stackrox/rox/pkg/env"
 
 var (
-	//OperatorOuterMode represents whether the Operator manages Central & the inner operator (outer mode)
+	// OperatorOuterMode represents whether the Operator manages Central & the inner operator (outer mode)
 	// 					or Secured Clusters (inner mode)
 	OperatorOuterMode = env.RegisterBooleanSetting("ROX_OPERATOR_OUTER_MODE", true)
 )
