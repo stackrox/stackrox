c3ecc11b72e9032d722270da7ebaf9ad2d12a5b1
lvalerom <lvalerom@redhat.com>
2023-07-26 08:25:45 +0200

    re-implementation of the contextProvider. GetContext is no longer blocking

--- sensor/common/context/context_provider.go
+++ sensor/common/context/context_provider.go
@@ -2,10 +2,11 @@ package contextprovider
 
 import (
 	"context"
+	"sync/atomic"
 
 	"github.com/stackrox/rox/generated/internalapi/central"
 	"github.com/stackrox/rox/pkg/centralsensor"
-	"github.com/stackrox/rox/pkg/concurrency"
+	"github.com/stackrox/rox/pkg/sync"
 	"github.com/stackrox/rox/sensor/common"
 	"github.com/stackrox/rox/sensor/common/message"
 )
@@ -21,45 +22,61 @@ var _ ContextProvider = (*contextProviderImpl)(nil)
 
 // NewContextProvider Initializes a new ContextProvider
 func NewContextProvider() ContextProvider {
-	return &contextProviderImpl{
-		centralReachable: concurrency.NewSignal(),
-		stopper:          concurrency.NewStopper(),
+	ret := &contextProviderImpl{
+		firstCentralConnection: &atomic.Bool{},
 	}
+	// We initialize here and not in Start to avoid races with other sensor components
+	// that might call GetContext on their respective Start functions.
+	ret.init(func() (context.Context, func()) {
+		return context.WithCancel(context.Background())
+	})
+	return ret
 }
 
 type contextProviderImpl struct {
-	cancelContextFn  func()
-	sensorContext    context.Context
-	centralReachable concurrency.Signal
-	stopper          concurrency.Stopper
+	mu              sync.RWMutex
+	cancelContextFn func()
+	sensorContext   context.Context
+	// newContextFn defines the function that creates a new context/cancelFunction.
+	// This is needed for testing purposes.
+	newContextFn func() (context.Context, func())
+	// firstCentralConnection indicates whether is the first time Central is reachable or not.
+	// This is needed to make sure any SensorComponent that calls GetContext on Start gets a
+	// valid context that won't be overriden on receiving the CentralReachable notification.
+	firstCentralConnection *atomic.Bool
 }
 
-// GetContext returns the sensor context. This call will block until central is reachable.
-// This blocking in behavior is needed to ensure unset or old contexts are not passed to other component.
+// GetContext returns the sensor context.
 func (c *contextProviderImpl) GetContext() context.Context {
-	select {
-	case <-c.centralReachable.Done():
-		return c.sensorContext
-	case <-c.stopper.Flow().StopRequested():
-		return nil
-	}
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	return c.sensorContext
+}
+
+// init sets the newContextFn and initializes the sensorContext and the cancelContextFn
+func (c *contextProviderImpl) init(ctxFn func() (context.Context, func())) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.newContextFn = ctxFn
+	c.sensorContext, c.cancelContextFn = c.newContextFn()
 }
 
 func (c *contextProviderImpl) Start() error {
 	return nil
 }
 
-func (c *contextProviderImpl) Stop(_ error) {
-	c.stopper.Client().Stop()
-}
+func (c *contextProviderImpl) Stop(_ error) {}
 
 func (c *contextProviderImpl) Notify(event common.SensorComponentEvent) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	switch event {
 	case common.SensorComponentEventCentralReachable:
-		c.sensorContext, c.cancelContextFn = context.WithCancel(context.Background())
-		c.centralReachable.Signal()
+		// We only re-create the context if it's not the first time central is reachable.
+		if !c.firstCentralConnection.CompareAndSwap(false, true) {
+			c.sensorContext, c.cancelContextFn = c.newContextFn()
+		}
 	case common.SensorComponentEventOfflineMode:
-		c.centralReachable.Reset()
 		c.cancelContextFn()
 	}
 }
