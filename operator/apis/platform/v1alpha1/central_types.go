774a370555416f0400b614936b9718fe35fd4ba6
Tomasz Janiszewski <tomek@redhat.com>
2023-07-26 15:54:21 +0200

    ROX-17225: expose openshift monitoring in operator (#6763)

--- operator/apis/platform/v1alpha1/central_types.go
+++ operator/apis/platform/v1alpha1/central_types.go
@@ -60,6 +60,29 @@ type CentralSpec struct {
 	// Overlays
 	//+operator-sdk:csv:customresourcedefinitions:type=spec,displayName=Overlays,order=8,xDescriptors={"urn:alm:descriptor:com.tectonic.ui:hidden"}
 	Overlays []*K8sObjectOverlay `json:"overlays,omitempty"`
+
+	// Monitoring configuration.
+	//+operator-sdk:csv:customresourcedefinitions:type=spec,order=9,xDescriptors={"urn:alm:descriptor:com.tectonic.ui:advanced"}
+	Monitoring *GlobalMonitoring `json:"monitoring,omitempty"`
+}
+
+// GlobalMonitoring defines settings related to monitoring.
+type GlobalMonitoring struct {
+	OpenShiftMonitoring *OpenShiftMonitoring `json:"openshift,omitempty"`
+}
+
+// OpenShiftMonitoring defines settings related to OpenShift Monitoring
+type OpenShiftMonitoring struct {
+	//+kubebuilder:validation:Default=true
+	//+kubebuilder:default=false
+	//+operator-sdk:csv:customresourcedefinitions:type=spec,order=1,xDescriptors={"urn:alm:descriptor:com.tectonic.ui:booleanSwitch"}
+	Enabled bool `json:"enabled"`
+}
+
+// IsOpenShiftMonitoringEnabled returns true if OpenShiftMonitoring is enabled.
+// This function is nil safe.
+func (m *GlobalMonitoring) IsOpenShiftMonitoringEnabled() bool {
+	return m != nil && m.OpenShiftMonitoring != nil && m.OpenShiftMonitoring.Enabled
 }
 
 // Egress defines settings related to outgoing network traffic.
