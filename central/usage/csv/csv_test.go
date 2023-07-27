3e537e8be5c1074cb2f8ed22d7a0742e859ed494
Michaël Petrov <smartptr@gmail.com>
2023-07-27 12:20:27 +0200

    refactoring

--- central/usage/csv/csv_test.go
+++ central/usage/csv/csv_test.go
@@ -2,6 +2,7 @@ package usagecsv
 
 import (
 	"bytes"
+	"net/url"
 	"testing"
 	"time"
 
@@ -35,4 +36,24 @@ func TestCSV(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.Equal(t, "Timestamp,Nodes,Cores\n2023-07-24T10:13:21Z,1,2\n2023-07-24T15:13:21Z,3,4\n", buf.String())
+}
+
+func TestGetTimeParam(t *testing.T) {
+	{
+		from, _ := time.Parse(time.RFC3339Nano, "2023-07-24T10:13:21.702316Z")
+		formValues := url.Values{"from": {from.Format(time.RFC3339Nano)}, "to": {"not a time"}}
+		v, err := getTimeParameter(formValues, "from", zeroTime)
+		assert.NoError(t, err)
+		assert.Equal(t, from.Unix(), v.GetSeconds())
+		assert.Equal(t, int32(from.Nanosecond()), v.GetNanos())
+	}
+	{
+		formValues := url.Values{"from": {"not a time"}}
+		_, err := getTimeParameter(formValues, "from", zeroTime)
+		assert.Error(t, err)
+		now := time.Now()
+		to, err := getTimeParameter(formValues, "to", now)
+		assert.NoError(t, err)
+		assert.Equal(t, now.Unix(), to.GetSeconds())
+	}
 }
