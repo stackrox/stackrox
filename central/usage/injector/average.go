35973e1322896bc8ff349dfb6f5defeb46f1031f
Michaël Petrov <smartptr@gmail.com>
2023-07-27 15:45:37 +0200

    refactoring

--- central/usage/injector/average.go
+++ central/usage/injector/average.go
@@ -4,15 +4,15 @@ import "github.com/stackrox/rox/generated/storage"
 
 func average(metrics ...*storage.Usage) *storage.Usage {
 	n := int32(len(metrics))
-	a := &storage.Usage{}
+	averageUsage := &storage.Usage{}
 	if n == 0 {
-		return a
+		return averageUsage
 	}
 	for _, m := range metrics {
-		a.NumNodes += m.NumNodes
-		a.NumCpuUnits += m.NumCpuUnits
+		averageUsage.NumNodes += m.NumNodes
+		averageUsage.NumCpuUnits += m.NumCpuUnits
 	}
-	a.NumNodes /= n
-	a.NumCpuUnits /= n
-	return a
+	averageUsage.NumNodes /= n
+	averageUsage.NumCpuUnits /= n
+	return averageUsage
 }
