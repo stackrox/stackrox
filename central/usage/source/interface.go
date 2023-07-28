f33bf085ce9dde5e62239b838b0f19214b9e0249
Michaël Petrov <smartptr@gmail.com>
2023-07-28 19:17:45 +0200

    source mocks

--- central/usage/source/interface.go
+++ central/usage/source/interface.go
@@ -1,6 +1,8 @@
 package source
 
 // UsageSource interface provides methods to get usage metrics from a source.
+//
+//go:generate mockgen-wrapper
 type UsageSource interface {
 	GetNodeCount() int64
 	GetCpuCapacity() int64
