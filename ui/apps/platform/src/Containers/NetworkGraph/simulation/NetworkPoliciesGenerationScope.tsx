1fe00d3022d9a5c62aaa436075837000c5b983bf
David Vail <dcvail17@gmail.com>
2023-07-31 12:17:08 -0400

    Fix lint

--- ui/apps/platform/src/Containers/NetworkGraph/simulation/NetworkPoliciesGenerationScope.tsx
+++ ui/apps/platform/src/Containers/NetworkGraph/simulation/NetworkPoliciesGenerationScope.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Flex } from '@patternfly/react-core';
-import pluralize from 'pluralize';
 import uniq from 'lodash/uniq';
 
 import { ClusterIcon, DeploymentIcon, NamespaceIcon } from '../common/NetworkGraphIcons';
