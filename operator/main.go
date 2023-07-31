89da63b5f5c15c816eab552f7e1c9e4f5c10e92b
Matthias Meidinger <mmeiding@redhat.com>
2023-07-31 18:00:47 +0200

    reenable webhook

--- operator/main.go
+++ operator/main.go
@@ -57,10 +57,9 @@ const (
 )
 
 var (
-	setupLog = ctrl.Log.WithName("setup")
-	scheme   = runtime.NewScheme()
-	// enableWebhooks             = env.RegisterBooleanSetting("ENABLE_WEBHOOKS", true) // FIXME: Flip back default
-	enableWebhooks             = env.RegisterBooleanSetting("ENABLE_WEBHOOKS", false) // FIXME: Flip back default
+	setupLog                   = ctrl.Log.WithName("setup")
+	scheme                     = runtime.NewScheme()
+	enableWebhooks             = env.RegisterBooleanSetting("ENABLE_WEBHOOKS", true)
 	enableProfiling            = env.RegisterBooleanSetting("ENABLE_PROFILING", false)
 	profilingThresholdFraction = env.RegisterSetting("PROFILING_THRESHOLD_FRACTION", env.WithDefault("0.8"))
 	memLimit                   = env.RegisterIntegerSetting("MEMORY_LIMIT_BYTES", 0)
