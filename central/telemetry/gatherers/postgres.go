acc822b0a75b7d5806a4bd0677c89ac727368cf1
Dave Shrewsberry <dashrews@redhat.com>
2023-08-02 15:10:29 -0400

    fix bug in telemetry gatherer

--- central/telemetry/gatherers/postgres.go
+++ central/telemetry/gatherers/postgres.go
@@ -8,6 +8,7 @@ import (
 	"github.com/stackrox/rox/pkg/errorhelpers"
 	"github.com/stackrox/rox/pkg/postgres"
 	"github.com/stackrox/rox/pkg/postgres/pgadmin"
+	"github.com/stackrox/rox/pkg/postgres/pgconfig"
 	"github.com/stackrox/rox/pkg/telemetry/data"
 )
 
@@ -27,16 +28,17 @@ func newPostgresGatherer(db postgres.DB, adminConfig *postgres.Config) *postgres
 func (d *postgresGatherer) Gather(ctx context.Context) *data.DatabaseStats {
 	errorList := errorhelpers.NewErrorList("postgres telemetry gather")
 
-	totalSize, err := pgadmin.GetTotalPostgresSize(d.adminConfig)
-	errorList.AddError(err)
-
 	dbStats := globaldb.CollectPostgresStats(ctx, d.db)
 	dbStats.Type = "postgres"
-	dbStats.UsedBytes = totalSize
+	
 	dbStats.DatabaseDetails = globaldb.CollectPostgresDatabaseSizes(d.adminConfig)
 
 	// Check Postgres remaining capacity
-	if !env.ManagedCentral.BooleanSetting() {
+	if !env.ManagedCentral.BooleanSetting() && !pgconfig.IsExternalDatabase() {
+		totalSize, err := pgadmin.GetTotalPostgresSize(d.adminConfig)
+		errorList.AddError(err)
+		dbStats.UsedBytes = totalSize
+
 		availableDBBytes, err := pgadmin.GetRemainingCapacity(d.adminConfig)
 		errorList.AddError(err)
 		dbStats.AvailableBytes = availableDBBytes
