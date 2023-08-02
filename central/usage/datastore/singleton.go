62b4345fb269248d2be6391d22816d817fc9d5b9
Michaël Petrov <smartptr@gmail.com>
2023-08-02 13:10:36 +0200

    remove unused logging

--- central/usage/datastore/singleton.go
+++ central/usage/datastore/singleton.go
@@ -1,15 +1,12 @@
 package datastore
 
 import (
-	"github.com/stackrox/rox/pkg/logging"
 	"github.com/stackrox/rox/pkg/sync"
 )
 
 var (
 	ds   DataStore
 	once sync.Once
-
-	log = logging.LoggerForModule()
 )
 
 // Singleton returns the singleton providing access to the usage store.
