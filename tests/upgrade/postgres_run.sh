154c959d4f0de037b083af715d4bc7fbf9b03097
cdu <cdu@redhat.com>
2023-08-03 12:09:10 -0700

    change rollback version

--- tests/upgrade/postgres_run.sh
+++ tests/upgrade/postgres_run.sh
@@ -84,7 +84,7 @@ test_upgrade_paths() {
     EARLIER_SHA="fe924fce30bbec4dbd37d731ccd505837a2c2575"
     EARLIER_TAG="3.74.0-1-gfe924fce30"
     # To test we remain backwards compatible rollback to 4.1.1
-    FORCE_ROLLBACK_VERSION="4.1.1"
+    FORCE_ROLLBACK_VERSION="4.1.2-2-g8489ac9584"
 
     cd "$REPO_FOR_TIME_TRAVEL"
     git checkout "$EARLIER_SHA"
