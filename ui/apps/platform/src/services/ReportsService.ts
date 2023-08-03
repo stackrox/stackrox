7cb501e33619ed97bfa9b44a497e8bc570e89a64
md2119 <mandar@stackrox.com>
2023-08-03 16:10:34 -0700

    consolidate envs

--- ui/apps/platform/src/services/ReportsService.ts
+++ ui/apps/platform/src/services/ReportsService.ts
@@ -161,7 +161,7 @@ export function fetchReportConfiguration(reportId: string): Promise<ReportConfig
 
 export function fetchReportStatus(id: string): Promise<ReportStatus | null> {
     return axios
-        .get<{ status: ReportStatus | null }>(`/v2/reports/${id}/status`)
+        .get<{ status: ReportStatus | null }>(`/v2/reports/jobs/${id}/status`)
         .then((response) => {
             return response.data?.status;
         });
