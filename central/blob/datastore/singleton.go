5a65b654079bbedaa19c1e885c7fd7693e186074
Cong Du <c-du@users.noreply.github.com>
2023-08-03 16:51:28 -0700

    ROX-17285: Add search to blob store (#6200)

--- central/blob/datastore/singleton.go
+++ central/blob/datastore/singleton.go
@@ -1,7 +1,9 @@
 package datastore
 
 import (
+	"github.com/stackrox/rox/central/blob/datastore/search"
 	"github.com/stackrox/rox/central/blob/datastore/store"
+	"github.com/stackrox/rox/central/blob/datastore/store/postgres"
 	"github.com/stackrox/rox/central/globaldb"
 	"github.com/stackrox/rox/pkg/sync"
 )
@@ -15,7 +17,11 @@ var (
 // Singleton returns the blob datastore
 func Singleton() Datastore {
 	once.Do(func() {
-		ds = NewDatastore(store.New(globaldb.GetPostgres()))
+		indexer := postgres.NewIndexer(globaldb.GetPostgres())
+		store := store.New(globaldb.GetPostgres())
+		searcher := search.New(store, indexer)
+
+		ds = NewDatastore(store, searcher)
 	})
 	return ds
 }
---
+++ migrator/migrations/m_186_to_m_187_add_blob_search/migration_test.go
@@ -0,0 +1,88 @@
+//go:build sql_integration
+
+package m186tom187
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"testing"
+
+	timestamp "github.com/gogo/protobuf/types"
+	"github.com/stackrox/rox/generated/storage"
+	afterSchema "github.com/stackrox/rox/migrator/migrations/m_186_to_m_187_add_blob_search/schema/after"
+	beforeSchema "github.com/stackrox/rox/migrator/migrations/m_186_to_m_187_add_blob_search/schema/before"
+	pghelper "github.com/stackrox/rox/migrator/migrations/postgreshelper"
+	"github.com/stackrox/rox/pkg/postgres/pgutils"
+	"github.com/stretchr/testify/suite"
+	"gorm.io/gorm"
+)
+
+const (
+	validFilePath = "2.4.0/collector-4.9.24-coreos.ko.gz"
+)
+
+type blobMigrationTestSuite struct {
+	suite.Suite
+	ctx context.Context
+
+	db     *pghelper.TestPostgres
+	gormDB *gorm.DB
+}
+
+func TestMigration(t *testing.T) {
+	suite.Run(t, new(blobMigrationTestSuite))
+}
+
+func (s *blobMigrationTestSuite) SetupTest() {
+	s.db = pghelper.ForT(s.T(), true)
+	s.ctx = context.Background()
+	s.gormDB = s.db.GetGormDB().WithContext(s.ctx)
+}
+
+func (s *blobMigrationTestSuite) TearDownTest() {
+	s.db.Teardown(s.T())
+}
+
+func (s *blobMigrationTestSuite) TestMigration() {
+	pgutils.CreateTableFromModel(s.ctx, s.gormDB, beforeSchema.CreateTableBlobsStmt)
+	batchSize = 9
+	blobs := s.createMockBlobs(20)
+	var convertedBlobs []beforeSchema.Blobs
+	for _, blob := range blobs {
+		converted, err := beforeSchema.ConvertBlobFromProto(blob)
+		s.Require().NoError(err)
+		convertedBlobs = append(convertedBlobs, *converted)
+	}
+	s.Require().NoError(s.gormDB.Create(convertedBlobs).Error)
+	s.Require().NoError(convert(s.gormDB))
+
+	var newBlobs []afterSchema.Blobs
+	s.Require().NoError(s.gormDB.Find(&newBlobs).Error)
+	s.Equal(len(blobs), len(newBlobs))
+	for i, nb := range newBlobs {
+		s.Contains(blobs, nb.Name)
+		blob := blobs[nb.Name]
+		s.Equal(blob.Length, nb.Length)
+		s.Equal(pgutils.NilOrTime(blob.ModifiedTime), nb.ModifiedTime)
+		converted, err := afterSchema.ConvertBlobToProto(&newBlobs[i])
+		s.Require().NoError(err)
+		s.Equal(blob, converted)
+	}
+}
+
+func (s *blobMigrationTestSuite) createMockBlobs(size int) map[string]*storage.Blob {
+	blobs := make(map[string]*storage.Blob, size)
+	for i := 0; i < size; i++ {
+		oid := uint32(80000 + size)
+		name := fmt.Sprintf("/path/mock/oid_%d", oid)
+		blob := &storage.Blob{
+			Name:         name,
+			Oid:          oid,
+			Length:       int64(rand.Int()),
+			ModifiedTime: timestamp.TimestampNow(),
+		}
+		blobs[name] = blob
+	}
+	return blobs
+}
