251f85432eaa2a3be6e15f9a1e4e4693a9e99565
Mandar Darwatkar <mandar@stackrox.com>
2023-08-03 19:32:45 -0700

    ROX-18756: Update GetReportStatus and CancelReport URL path (#7274)

--- pkg/env/vuln_report_enhancements.go
+++
@@ -1,6 +0,0 @@
-package env
-
-var (
-	// VulnReportingEnhancements enables APIs and UI pages for VM Reporting enhancements including downloadable reports
-	VulnReportingEnhancements = RegisterBooleanSetting("ROX_VULN_MGMT_REPORTING_ENHANCEMENTS", false)
-)
