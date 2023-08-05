53ff6a2ce977ab9ec28deb81ee13b26d71f191e5
JoukoVirtanen <jouko@stackrox.com>
2023-08-05 09:57:59 -0700

    Kube-burner works

--- .github/workflows/patch-both-clusters.sh
+++ .github/workflows/patch-both-clusters.sh
@@ -42,13 +42,13 @@ echo "cluster_name=${NAME//./-}" >> "$GITHUB_OUTPUT"
 
 printf "Long-running GKE cluster %s has been patched.\nAccess it by running \`./scripts/release-tools/setup-central-access.sh %s\` from your local machine." "${NAME//./-}" "${NAME//./-}" >> "$GITHUB_STEP_SUMMARY"
 
-
+bundle=bundle-7.yaml
 
 
 curl https://mirror.openshift.com/pub/rhacs/assets/latest/bin/Linux/roxctl --output roxctl_bin
 chmod 755 roxctl_bin
 
-./roxctl_bin -e https://"$CENTRAL_IP":443 -p "$ROX_ADMIN_PASSWORD" central init-bundles generate long-running-test --output init-bundle.yaml
+./roxctl_bin -e https://"$CENTRAL_IP":443 -p "$ROX_ADMIN_PASSWORD" central init-bundles generate long-running-test --output "$bundle"
 
 ls
 
@@ -61,7 +61,7 @@ helm repo add rhacs https://mirror.openshift.com/pub/rhacs/charts
 image_registry=quay.io/stackrox-io
 settings=(
     --namespace stackrox stackrox-secured-cluster-services --create-namespace rhacs/secured-cluster-services
-    --values init-bundle.yaml
+    --values "$bundle"
     --set clusterName=secured-cluster-test
     --set image.collector.registry="$image_registry"
     --set image.collector.name="collector"
@@ -85,4 +85,9 @@ curl --silent --location "https://github.com/cloud-bulldozer/kube-burner/release
 
 tar -zxvf "./kube-burner/kube-burner-${KUBE_BURNER_VERSION}.tar.gz" --directory ./kube-burner
 
-"$STACKROX_DIR"/.github/workflows/scripts/repeate-kube-burner.sh ./kube-burner/kube-burner "$STACKROX_DIR"/.github/workflows/other-configs/cluster-density-kube-burner.yml &
+kube_burner_config_file="$STACKROX_DIR"/.github/workflows/other-configs/cluster-density-kube-burner.yml 
+kube_burner_gen_config_file="$STACKROX_DIR"/.github/workflows/other-configs/cluster-density-kube-burner_gen.yml 
+
+sed "s|STACKROX_DIR|$STACKROX_DIR|" "$kube_burner_config_file" > "$kube_burner_gen_config_file" 
+
+nohup "$STACKROX_DIR"/.github/workflows/scripts/repeate-kube-burner.sh ./kube-burner/kube-burner "$kube_burner_gen_config_file" &
