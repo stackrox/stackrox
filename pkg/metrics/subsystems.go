f6967d76a15f7051ab11b50bd52ee87f92a23ec6
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2023-08-07 14:20:21 +0200

    Ensure no collision between Prometheus servers

--- pkg/metrics/subsystems.go
+++ pkg/metrics/subsystems.go
@@ -5,10 +5,11 @@ type Subsystem string
 
 // These consts enumerate all the subsystems that expose Prometheus metrics.
 const (
-	CentralSubsystem    Subsystem = "central"
-	SensorSubsystem     Subsystem = "sensor"
-	ComplianceSubsystem Subsystem = "compliance"
-	ScannerSubsystem    Subsystem = "scanner"
+	CentralSubsystem       Subsystem = "central"
+	SensorSubsystem        Subsystem = "sensor"
+	ComplianceSubsystem    Subsystem = "compliance"
+	NodeInventorySubsystem Subsystem = "nodeinventory"
+	ScannerSubsystem       Subsystem = "scanner"
 )
 
 func (s Subsystem) String() string {
