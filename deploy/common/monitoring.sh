8cf43d12601d844c82bcce1afa88b4174af6a99a
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2023-08-08 09:15:07 +0200

    Redo changes related to Prometheus server config

--- deploy/common/monitoring.sh
+++ deploy/common/monitoring.sh
@@ -3,6 +3,7 @@
 kubectl -n stackrox patch svc/sensor -p '{"spec":{"ports":[{"name":"monitoring","port":9090,"protocol":"TCP","targetPort":9090}]}}'
 kubectl -n stackrox patch svc/central -p '{"spec":{"ports":[{"name":"monitoring","port":9090,"protocol":"TCP","targetPort":9090}]}}'
 kubectl -n stackrox patch daemonset/collector --type='json' -p='[{"op": "add", "path": "/spec/template/spec/containers/1/ports", "value":[{"containerPort":9091,"name":"cmonitor","protocol":"TCP"}]}]'
+kubectl -n stackrox patch daemonset/collector --type='json' -p='[{"op": "add", "path": "/spec/template/spec/containers/2/ports", "value":[{"containerPort":9092,"name":"cmonitor","protocol":"TCP"}]}]'
 
 # Modify network policies to allow ingress
 kubectl apply -f - <<EOF
@@ -35,6 +36,24 @@ spec:
   ingress:
   - ports:
     - port: 9091
+      protocol: TCP
+  podSelector:
+    matchLabels:
+      app: collector
+  policyTypes:
+  - Ingress
+---
+apiVersion: networking.k8s.io/v1
+kind: NetworkPolicy
+metadata:
+  labels:
+    app.kubernetes.io/name: stackrox
+  name: allow-node-inventory-monitoring
+  namespace: stackrox
+spec:
+  ingress:
+  - ports:
+    - port: 9092
       protocol: TCP
   podSelector:
     matchLabels:
