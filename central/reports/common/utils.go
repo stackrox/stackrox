1db90b9327735d5a196a7c3f8e12b5e05af5a2e6
Charmik Sheth <101146970+charmik-redhat@users.noreply.github.com>
2023-08-09 13:32:49 -0700

    Address comments and rename scope rules var

--- central/reports/common/utils.go
+++ central/reports/common/utils.go
@@ -10,7 +10,7 @@ import (
 // For the purpose of vulnerability reporting, nil/empty list of rules would mean allow access to all clusters/namespaces.
 func ExtractAccessScopeRules(identity authn.Identity) []*storage.SimpleAccessScope_Rules {
 	roles := identity.Roles()
-	accessScopeRules := make([]*storage.SimpleAccessScope_Rules, 0, len(roles))
+	accessScopeRulesList := make([]*storage.SimpleAccessScope_Rules, 0, len(roles))
 	for _, role := range roles {
 		// Note: This mirrors the scope resolution logic in `func (c *authorizerDataCache) computeEffectiveAccessScope(...)`
 		//  defined in central/sac/authorizer/builtin_scoped_authorizer.go .
@@ -18,19 +18,19 @@ func ExtractAccessScopeRules(identity authn.Identity) []*storage.SimpleAccessSco
 		//  has rules = nil. However, for any other access scope, nil/empty scope rules translate to an "exclude all" access scope.
 		accessScope := role.GetAccessScope()
 		if accessScope == nil {
-			accessScopeRules = append(accessScopeRules, rolePkg.AccessScopeExcludeAll.GetRules())
+			accessScopeRulesList = append(accessScopeRulesList, rolePkg.AccessScopeExcludeAll.GetRules())
 		} else if accessScope.Id == rolePkg.AccessScopeIncludeAll.Id {
 			return nil
 		} else if accessScope.Id == rolePkg.AccessScopeExcludeAll.Id || accessScope.GetRules() == nil {
 			// nil/empty rules in a non-nil access scope means exclude all clusters/namespaces
 			// if the access scope is not same as rolePkg.AccessScopeIncludeAll
-			accessScopeRules = append(accessScopeRules, rolePkg.AccessScopeExcludeAll.GetRules())
+			accessScopeRulesList = append(accessScopeRulesList, rolePkg.AccessScopeExcludeAll.GetRules())
 		} else {
-			accessScopeRules = append(accessScopeRules, accessScope.GetRules())
+			accessScopeRulesList = append(accessScopeRulesList, accessScope.GetRules())
 		}
 	}
-	if len(accessScopeRules) == 0 {
-		accessScopeRules = append(accessScopeRules, rolePkg.AccessScopeExcludeAll.GetRules())
+	if len(accessScopeRulesList) == 0 {
+		return []*storage.SimpleAccessScope_Rules{rolePkg.AccessScopeExcludeAll.GetRules()}
 	}
-	return accessScopeRules
+	return accessScopeRulesList
 }
