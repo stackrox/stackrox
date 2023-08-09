fd91725b3410365f251e435909bac9327d063af2
Yury Kovalev <8366110+kovayur@users.noreply.github.com>
2023-08-09 18:25:59 +0200

    ROX-18585: Remove KernelModule selection from the drop-down menu (#7281)
    (cherry picked from commit 0de4089aa270a01be9bd694cfe2427ec9c06ddab)

--- operator/apis/platform/v1alpha1/securedcluster_types.go
+++ operator/apis/platform/v1alpha1/securedcluster_types.go
@@ -268,7 +268,7 @@ type CollectorContainerSpec struct {
 	// Note that CORE_BPF is on Tech Preview stage.
 	//+kubebuilder:validation:Default=EBPF
 	//+kubebuilder:default=EBPF
-	//+operator-sdk:csv:customresourcedefinitions:type=spec,order=1
+	//+operator-sdk:csv:customresourcedefinitions:type=spec,order=1,xDescriptors={"urn:alm:descriptor:com.tectonic.ui:select:EBPF", "urn:alm:descriptor:com.tectonic.ui:select:CORE_BPF", "urn:alm:descriptor:com.tectonic.ui:select:NoCollection"}
 	Collection *CollectionMethod `json:"collection,omitempty"`
 
 	// The image flavor to use for collector. "Regular" images are bigger in size, but contain probes
