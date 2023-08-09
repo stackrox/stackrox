73879acfa8ffb9af36c1fdc8415f8859787cc5c9
David Vail <dcvail17@gmail.com>
2023-08-09 08:51:29 -0400

    ROX-18467: Show scoped deployments in network policy simulator modal (#7162)

--- ui/apps/platform/src/Containers/NetworkGraph/simulation/NetworkPoliciesGenerationScope.tsx
+++ ui/apps/platform/src/Containers/NetworkGraph/simulation/NetworkPoliciesGenerationScope.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Flex } from '@patternfly/react-core';
+import { Button, Flex } from '@patternfly/react-core';
 import uniq from 'lodash/uniq';
 
 import { ClusterIcon, DeploymentIcon, NamespaceIcon } from '../common/NetworkGraphIcons';
 
 import './NetworkPoliciesGenerationScope.css';
+import DeploymentScopeModal from './DeploymentScopeModal';
 
 export type EntityScope = {
     // `granularity` refers to the most specific entity type that has been selected by the user.
@@ -25,51 +26,69 @@ function NetworkPoliciesGenerationScope({
     networkPolicyGenerationScope,
 }: NetworkPoliciesGenerationScopeProps) {
     const { granularity, cluster, deployments } = networkPolicyGenerationScope;
+    const [modalDeployments, setModalDeployments] = React.useState<
+        { namespace: string; name: string }[] | null
+    >(null);
 
     let deploymentElement = <span>All deployments</span>;
     let namespaceElement = <span>All namespaces</span>;
 
-    if (granularity === 'NAMESPACE' || granularity === 'DEPLOYMENT') {
+    if (granularity !== 'CLUSTER') {
         const namespaces = uniq(deployments.map((deployment) => deployment.namespace));
         const namespaceCount = namespaces.length;
         const namespaceText = namespaceCount === 1 ? namespaces[0] : `${namespaceCount} namespaces`;
         namespaceElement = <span>{namespaceText}</span>;
-    }
 
-    if (granularity === 'DEPLOYMENT') {
         const deploymentCount = deployments.length;
         const deploymentText =
             deploymentCount === 1 ? deployments[0].name : `${deploymentCount} deployments`;
 
-        deploymentElement = <span>{deploymentText}</span>;
+        deploymentElement = (
+            <Button
+                variant="link"
+                isInline
+                onClick={() => setModalDeployments(networkPolicyGenerationScope.deployments)}
+            >
+                {deploymentText}
+            </Button>
+        );
     }
 
     return (
-        <div className="network-policies-generation-scope">
-            <Flex
-                alignItems={{ default: 'alignItemsCenter' }}
-                spaceItems={{ default: 'spaceItemsSm' }}
-            >
-                <DeploymentIcon aria-label="Deployment" />
-                {deploymentElement}
-            </Flex>
+        <>
+            {modalDeployments && (
+                <DeploymentScopeModal
+                    deployments={modalDeployments}
+                    isOpen={modalDeployments !== null}
+                    onClose={() => setModalDeployments(null)}
+                />
+            )}
+            <div className="network-policies-generation-scope">
+                <Flex
+                    alignItems={{ default: 'alignItemsCenter' }}
+                    spaceItems={{ default: 'spaceItemsSm' }}
+                >
+                    <DeploymentIcon aria-label="Deployment" />
+                    {deploymentElement}
+                </Flex>
 
-            <Flex
-                alignItems={{ default: 'alignItemsCenter' }}
-                spaceItems={{ default: 'spaceItemsSm' }}
-            >
-                <NamespaceIcon aria-label="Namespace" />
-                {namespaceElement}
-            </Flex>
+                <Flex
+                    alignItems={{ default: 'alignItemsCenter' }}
+                    spaceItems={{ default: 'spaceItemsSm' }}
+                >
+                    <NamespaceIcon aria-label="Namespace" />
+                    {namespaceElement}
+                </Flex>
 
-            <Flex
-                alignItems={{ default: 'alignItemsCenter' }}
-                spaceItems={{ default: 'spaceItemsSm' }}
-            >
-                <ClusterIcon aria-label="Cluster" />
-                <span>{cluster}</span>
-            </Flex>
-        </div>
+                <Flex
+                    alignItems={{ default: 'alignItemsCenter' }}
+                    spaceItems={{ default: 'spaceItemsSm' }}
+                >
+                    <ClusterIcon aria-label="Cluster" />
+                    <span>{cluster}</span>
+                </Flex>
+            </div>
+        </>
     );
 }
 
