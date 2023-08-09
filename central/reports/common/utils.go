debee29f1e5b239786986ead3d9a4ec6073084e8
Charmik Sheth <101146970+charmik-redhat@users.noreply.github.com>
2023-08-09 00:43:30 -0700

    Fix comments

--- central/reports/common/utils.go
+++ central/reports/common/utils.go
@@ -7,20 +7,19 @@ import (
 )
 
 // ExtractAccessScopeRules extracts simple access scope rules from the given authenticated user identity
-// For the purpose of vulnerability reporting, nil/empty list of access scope rules would mean allow access to all clusters/namespaces.
+// For the purpose of vulnerability reporting, nil/empty list of rules would mean allow access to all clusters/namespaces.
 func ExtractAccessScopeRules(identity authn.Identity) []*storage.SimpleAccessScope_Rules {
 	roles := identity.Roles()
 	accessScopeRules := make([]*storage.SimpleAccessScope_Rules, 0, len(roles))
 	for _, role := range roles {
 		// Note: This mirrors the scope resolution logic in `func (c *authorizerDataCache) computeEffectiveAccessScope(...)`
 		//  defined in central/sac/authorizer/builtin_scoped_authorizer.go .
-		//  The reason for doing this that the access scope "AccessScopeIncludeAll" which allows all clusters/namespaces
-		//  has scope rules = nil. However, for any other access scope, nil/empty scope rules translates to deny all access scope.
+		//  The reason for doing this that the system access scope "AccessScopeIncludeAll" which includes all clusters/namespaces
+		//  has rules = nil. However, for any other access scope, nil/empty scope rules translate to an "exclude all" access scope.
 		accessScope := role.GetAccessScope()
 		if accessScope == nil || accessScope.Id == rolePkg.AccessScopeExcludeAll.Id {
 			accessScopeRules = append(accessScopeRules, rolePkg.AccessScopeExcludeAll.GetRules())
 		} else if accessScope.Id == rolePkg.AccessScopeIncludeAll.Id {
-			// nil list of access scope rules means include all
 			return nil
 		} else if accessScope.GetRules() == nil {
 			// nil/empty rules in a non-nil access scope rolePkg.AccessScopeIncludeAll.Id
