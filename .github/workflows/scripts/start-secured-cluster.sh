a7a22686f7d340e6280fda65949b6ae9dc9ca79b
JoukoVirtanen <jouko@stackrox.com>
2023-08-10 14:55:54 -0700

    Not creating secret for secured cluster

--- .github/workflows/scripts/start-secured-cluster.sh
+++ .github/workflows/scripts/start-secured-cluster.sh
@@ -11,4 +11,4 @@ export CLUSTER=secured-cluster
 
 ./deploy/k8s/sensor.sh
 
-kubectl -n stackrox create secret generic access-rhacs --from-literal="username=${ROX_ADMIN_USERNAME}" --from-literal="password=${ROX_ADMIN_PASSWORD}" --from-literal="central_url=https://${CENTRAL_IP}":443
+#kubectl -n stackrox create secret generic access-rhacs --from-literal="username=${ROX_ADMIN_USERNAME}" --from-literal="password=${ROX_ADMIN_PASSWORD}" --from-literal="central_url=https://${CENTRAL_IP}":443
