1fe666f1df3660679249e59abd333c370ffcfa06
JoukoVirtanen <jouko@stackrox.com>
2023-08-10 17:55:43 -0700

    Using wait_for_central from existing script

--- .github/workflows/scripts/start-acs.sh
+++ .github/workflows/scripts/start-acs.sh
@@ -4,6 +4,8 @@ set -euox pipefail
 
 cd $STACKROX_DIR
 
+source deploy/common/deploy.sh
+
 echo "GITHUB_OUTPUT= ${GITHUB_OUTPUT}"
 ./deploy/k8s/central.sh
 kubectl -n stackrox port-forward deploy/central 8000:8443 > /dev/null 2>&1 &
@@ -17,25 +19,13 @@ kubectl -n stackrox patch deploy/sensor -p '{"spec":{"template":{"spec":{"contai
 kubectl -n stackrox set env deploy/central MUTEX_WATCHDOG_TIMEOUT_SECS=0
 kubectl -n stackrox patch deploy/central -p '{"spec":{"template":{"spec":{"containers":[{"name":"central","resources":{"requests":{"memory":"3Gi","cpu":"2"},"limits":{"memory":"12Gi","cpu":"4"}}}]}}}}'
 
-max_tries=200
-tries=0
-while [ "$tries" -lt "$max_tries" ]; do
-    ready="$(kubectl -n stackrox get pod -l app=central -o jsonpath='{.items[*].status.containerStatuses[?(@.ready == true)]}')"
+CENTRAL_IP=$(kubectl -n stackrox get svc/central-loadbalancer -o json | jq -r '.status.loadBalancer.ingress[0] | .ip // .hostname')
 
-    if [[ -n "$ready" ]]; then
-	    echo "Central is running"
-	    break
-    fi
-
-    tries=$((tries + 1))
-    sleep 1
-done
-
-sleep 60
+API_ENDPOINT="${CENTRAL_IP}":443
+wait_for_central "${API_ENDPOINT}"
 
 ROX_ADMIN_PASSWORD=$(cat deploy/k8s/central-deploy/password)
 #echo "::add-mask::$ROX_ADMIN_PASSWORD"
-CENTRAL_IP=$(kubectl -n stackrox get svc/central-loadbalancer -o json | jq -r '.status.loadBalancer.ingress[0] | .ip // .hostname')
 kubectl -n stackrox create secret generic access-rhacs --from-literal="username=${ROX_ADMIN_USERNAME}" --from-literal="password=${ROX_ADMIN_PASSWORD}" --from-literal="central_url=https://${CENTRAL_IP}"
 echo "rox_password=${ROX_ADMIN_PASSWORD}" >> "$GITHUB_OUTPUT"
 echo "cluster_name=${NAME//./-}" >> "$GITHUB_OUTPUT"
