0108340d2b80d258f9dc3129957322c2b4c4adb6
David Vail <dcvail17@gmail.com>
2023-08-11 09:11:15 -0400

    Generalize and fix 'useFetchDeploymentCount' hook

--- ui/apps/platform/src/Containers/NetworkGraph/NetworkGraphPage.tsx
+++ ui/apps/platform/src/Containers/NetworkGraph/NetworkGraphPage.tsx
@@ -50,6 +50,7 @@ import {
     graphModel,
 } from './utils/modelUtils';
 import getSimulation from './utils/getSimulation';
+import { getSearchFilterFromScopeHierarchy } from './utils/simulatorUtils';
 import CIDRFormModal from './components/CIDRFormModal';
 import NetworkPolicySimulatorSidePanel, {
     clearSimulationQuery,
@@ -130,7 +131,10 @@ function NetworkGraphPage() {
     }
 
     const selectedClusterId = clusterFromUrl.id;
-    const { deploymentCount } = useFetchDeploymentCount(selectedClusterId);
+
+    const { deploymentCount } = useFetchDeploymentCount(
+        getSearchFilterFromScopeHierarchy(scopeHierarchy)
+    );
 
     const [prevEpochCount, setPrevEpochCount] = useState(0);
     const [currentEpochCount, setCurrentEpochCount] = useState(0);
@@ -155,6 +159,7 @@ function NetworkGraphPage() {
         const isQueryFilterComplete = isCompleteSearchFilter(remainingQuery);
 
         // only refresh the graph data from the API if both a cluster and at least one namespace are selected
+        // and the selected scope has at least one deployment
         const isClusterNamespaceSelected =
             clusterFromUrl.name && namespacesFromUrl.length > 0 && deploymentCount;
 
@@ -392,12 +397,7 @@ function NetworkGraphPage() {
                                         simulator={simulator}
                                         setNetworkPolicyModification={setNetworkPolicyModification}
                                         scopeHierarchy={scopeHierarchy}
-                                        networkPolicyGenerationScope={{
-                                            granularity: 'CLUSTER',
-                                            cluster: scopeHierarchy.cluster.name,
-                                            namespaces: [],
-                                            deployments: [],
-                                        }}
+                                        scopeDeploymentCount={deploymentCount ?? 0}
                                     />
                                 </DrawerPanelContent>
                             }
