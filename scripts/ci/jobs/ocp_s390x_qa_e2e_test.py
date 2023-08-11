53e44c0ca5ab84a8e7af5427b6246b9b0a5a61f8
jdao <jdao@redat.com>
2023-08-11 12:05:08 -0400

    Add s390x qa-e2e tests job

---
+++ scripts/ci/jobs/ocp_s390x_qa_e2e_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env -S python3 -u
+
+"""
+Run qa-tests-backend in a IBM CLOUD POWERVS Openshift cluster provided via automation-flavors/powervs.
+"""
+import os
+from base_qa_e2e_test import make_qa_e2e_test_runner_midstream
+from clusters import AutomationFlavorsCluster
+
+# set required test parameters
+os.environ["DEPLOY_STACKROX_VIA_OPERATOR"] = "true"
+os.environ["ORCHESTRATOR_FLAVOR"] = "openshift"
+os.environ["ROX_POSTGRES_DATASTORE"] = "true"
+os.environ["USE_MIDSTREAM_IMAGES"] = "true"
+os.environ["REMOTE_CLUSTER_ARCH"] = "s390x"
+
+make_qa_e2e_test_runner_midstream(cluster=AutomationFlavorsCluster()).run()
