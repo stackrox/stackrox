6f1c82c9db09a8459c829a05afbb4d92b79dd9ad
Dave Shrewsberry <dashrews@redhat.com>
2023-08-11 15:04:57 -0400

    go back to should

--- central/detection/lifecycle/singleton.go
+++ central/detection/lifecycle/singleton.go
@@ -41,7 +41,7 @@ func initialize() {
 	log.Infof("Injecting %d policies into detectors.", len(policies))
 	for _, policy := range policies {
 		err = manager.UpsertPolicy(policy)
-		log.Errorf("Unable to inject policy: %v", err)
+		utils.Should(errors.Wrap(err, "could not inject policy"))
 	}
 	log.Info("Done injecting policies.")
 
