37534b483c2dac344cefb5a0e020c3cdb9ce004e
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2023-08-14 14:13:05 +0200

    Do not use built-in deprecation message cause it mangles output

--- roxctl/generate/netpol/command.go
+++ roxctl/generate/netpol/command.go
@@ -11,11 +11,13 @@ import (
 func Command(cliEnvironment environment.Environment) *cobra.Command {
 	cmd := generate.NewNetpolGenerateCmd(cliEnvironment)
 	c := &cobra.Command{
-		Use:        "netpol <folder-path>",
-		Short:      cmd.ShortText(),
-		Long:       cmd.LongText(),
-		Deprecated: "for 'roxctl generate netpol' use 'roxctl netpol generate' instead",
-		Args:       cobra.ExactArgs(1),
+		Use:   "netpol <folder-path>",
+		Short: cmd.ShortText(),
+		Long:  cmd.LongText(),
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			cliEnvironment.Logger().WarnfLn("Command 'generate netpol' is deprecated. Use 'netpol generate' instead.")
+			return cobra.ExactArgs(1)(cmd, args)
+		},
 		RunE: func(c *cobra.Command, args []string) error {
 			return errors.Wrap(cmd.RunE(c, args), "running 'generate netpol' command")
 		},
