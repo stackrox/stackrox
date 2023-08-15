98b2cee081de0cc737856112f31ec7e75a4e5ce3
ivan-degtiarenko <degtiarenko@stackrox.com>
2023-08-15 18:12:57 +0200

    Improve the comment

--- pkg/k8sutil/client_config.go
+++ pkg/k8sutil/client_config.go
@@ -10,6 +10,7 @@ import (
 
 // GetK8sInClusterConfig returns k8s client config that can be used from within cluster.
 // It is adjusted to use DNS record instead of raw IP as API host in certain cases.
+// This can be used to more conveniently bypass the proxy.
 func GetK8sInClusterConfig() (*rest.Config, error) {
 	restCfg, err := rest.InClusterConfig()
 	if err != nil {
