d448cd20c92dde4f40a8206475be9acfb628757d
Ivan Degtiarenko <78353299+ivan-degtiarenko@users.noreply.github.com>
2023-08-15 19:21:23 +0200

    fix: Use DNS in k8s client for ACSCS (#7419)

---
+++ pkg/k8sutil/client_config.go
@@ -0,0 +1,26 @@
+package k8sutil
+
+import (
+	"net"
+	"os"
+
+	"github.com/stackrox/rox/pkg/env"
+	"k8s.io/client-go/rest"
+)
+
+// GetK8sInClusterConfig returns k8s client config that can be used from within cluster.
+// It is adjusted to use DNS record instead of raw IP as API host in certain cases.
+// This can be used to more conveniently bypass the proxy.
+func GetK8sInClusterConfig() (*rest.Config, error) {
+	restCfg, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, err
+	}
+	// Replacing raw IP address with kubernetes.default.svc
+	// allows for easier proxy configuration.
+	if env.ManagedCentral.BooleanSetting() {
+		port := os.Getenv("KUBERNETES_SERVICE_PORT")
+		restCfg.Host = "https://" + net.JoinHostPort("kubernetes.default.svc", port)
+	}
+	return restCfg, nil
+}
