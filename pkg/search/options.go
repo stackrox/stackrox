0b8f909de6f6f7c9b3af234a3976ede78a20467d
dhaus67 <dhaus@redhat.com>
2023-08-16 07:38:25 +0200

    ROX-18478: Create event service API (#7257)

--- pkg/search/options.go
+++ pkg/search/options.go
@@ -332,6 +332,9 @@ var (
 	ReportRequestType        = newFieldLabel("Report Request Type")
 	ReportNotificationMethod = newFieldLabel("Report Notification Method")
 
+	// Event fields.
+	EventLabel = newFieldLabel("Event Label")
+
 	// Test Search Fields
 	TestKey               = newFieldLabel("Test Key")
 	TestKey2              = newFieldLabel("Test Key 2")
