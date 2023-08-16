375327709f9832e7641e7c47d7d3f89a2a7f9b34
Charmik Sheth <101146970+charmik-redhat@users.noreply.github.com>
2023-08-16 07:52:40 -0700

    Make Report State:SUCCESS filter replacement in queries case insensitive (#7460)

--- central/reports/common/searcher.go
+++ central/reports/common/searcher.go
@@ -3,6 +3,7 @@ package common
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/gogo/protobuf/proto"
 	v1 "github.com/stackrox/rox/generated/api/v1"
@@ -73,7 +74,7 @@ func replaceSearchBySuccess(q *v1.Query) {
 		if !ok {
 			return
 		}
-		if mfQ.MatchFieldQuery.GetField() == search.ReportState.String() &&
+		if strings.EqualFold(mfQ.MatchFieldQuery.GetField(), search.ReportState.String()) &&
 			doesValueMatchSuccess(mfQ.MatchFieldQuery.GetValue()) {
 			*q = *search.NewQueryBuilder().
 				AddExactMatches(search.ReportState, storage.ReportStatus_GENERATED.String(), storage.ReportStatus_DELIVERED.String()).
@@ -85,6 +86,6 @@ func replaceSearchBySuccess(q *v1.Query) {
 }
 
 func doesValueMatchSuccess(val string) bool {
-	return val == apiV2.ReportStatus_SUCCESS.String() ||
-		val == search.ExactMatchString(apiV2.ReportStatus_SUCCESS.String())
+	return strings.EqualFold(val, apiV2.ReportStatus_SUCCESS.String()) ||
+		strings.EqualFold(val, search.ExactMatchString(apiV2.ReportStatus_SUCCESS.String()))
 }
