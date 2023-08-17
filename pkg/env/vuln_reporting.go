c961fb0c4ae77d36113c09c156d6710a6afcbe96
Charmik Sheth <101146970+charmik-redhat@users.noreply.github.com>
2023-08-17 11:04:42 -0700

    Enable ROX_VULN_MGMT_REPORTING_ENHANCEMENTS feature flag by default

--- pkg/env/vuln_reporting.go
+++ pkg/env/vuln_reporting.go
@@ -5,5 +5,5 @@ var (
 	ReportExecutionMaxConcurrency = RegisterIntegerSetting("ROX_REPORT_EXECUTION_MAX_CONCURRENCY", 5)
 
 	// VulnReportingEnhancements enables APIs and UI pages for VM Reporting enhancements including downloadable reports
-	VulnReportingEnhancements = RegisterBooleanSetting("ROX_VULN_MGMT_REPORTING_ENHANCEMENTS", false)
+	VulnReportingEnhancements = RegisterBooleanSetting("ROX_VULN_MGMT_REPORTING_ENHANCEMENTS", true)
 )
