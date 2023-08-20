c986d4fe73d7370c84d6e012385d6bbacef87ad6
Daniel Haus <dhaus@redhat.com>
2023-08-20 21:46:45 +0200

    rox-16856-watch-certs

---
+++ pkg/auth/authproviders/openshift/watcher.go
@@ -0,0 +1,104 @@
+package openshift
+
+import (
+	"context"
+	"os"
+	"path"
+	"time"
+
+	"github.com/stackrox/rox/pkg/k8scfgwatch"
+	"github.com/stackrox/rox/pkg/logging"
+	"go.uber.org/zap"
+)
+
+var (
+	log = logging.LoggerForModule()
+
+	_ k8scfgwatch.Handler = (*handler)(nil)
+)
+
+type notifyOnCertPoolUpdate = func()
+
+func watchCertPool(n notifyOnCertPoolUpdate) {
+	opts := k8scfgwatch.Options{
+		Interval: 5 * time.Second,
+		Force:    true,
+	}
+
+	_ = k8scfgwatch.WatchConfigMountDir(context.Background(), path.Dir(serviceOperatorCAPath),
+		k8scfgwatch.DeduplicateWatchErrors(&handler{readCAs: internalCAs, notifyBackend: n}), opts)
+	_ = k8scfgwatch.WatchConfigMountDir(context.Background(), path.Dir(injectedCAPath),
+		k8scfgwatch.DeduplicateWatchErrors(&handler{readCAs: injectedCAs, notifyBackend: n}), opts)
+}
+
+type handler struct {
+	notifyBackend func()
+	readCAs       func() ([][]byte, error)
+}
+
+func (h *handler) OnChange(_ string) (interface{}, error) {
+	return h.readCAs()
+}
+
+func (h *handler) OnStableUpdate(val interface{}, err error) {
+	// Ignore errors and nil values.
+	if err != nil || val == nil {
+		return
+	}
+
+	// Expect always a [][]byte.
+	caBytes := val.([][]byte)
+	if caBytes == nil {
+		log.Info("No updated CA bytes found, using the default system CA cert pool.")
+		return
+	}
+	h.notifyBackend()
+}
+
+func (h *handler) OnWatchError(err error) {
+	log.Errorw("Failed watching CAs. Not updating any CAs for Openshift auth providers",
+		logging.Err(err))
+}
+
+func internalCAs() ([][]byte, error) {
+	var caBytes [][]byte
+	ca, exists, err := readCA(serviceOperatorCAPath)
+	if err != nil {
+		return nil, err
+	}
+	if exists {
+		caBytes = append(caBytes, ca)
+	}
+	ca, exists, err = readCA(internalServicesCAPath)
+	if err != nil {
+		return nil, err
+	}
+	if exists {
+		caBytes = append(caBytes, ca)
+	}
+	return caBytes, nil
+}
+
+func injectedCAs() ([][]byte, error) {
+	caBytes, exists, err := readCA(injectedCAPath)
+	if err != nil {
+		return nil, err
+	}
+	if exists {
+		return [][]byte{caBytes}, nil
+	}
+	return nil, nil
+}
+
+func readCA(file string) ([]byte, bool, error) {
+	caBytes, err := os.ReadFile(file)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return nil, false, nil
+		}
+		log.Errorw("Reading CA file", logging.Err(err), zap.String("file", file))
+		return nil, false, err
+	}
+
+	return caBytes, true, nil
+}
