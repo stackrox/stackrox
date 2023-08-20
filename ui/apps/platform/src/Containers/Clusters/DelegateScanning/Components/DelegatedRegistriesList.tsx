3a2e1f7e2dc00928bea252942f10e88c40e68a9e
Van Wilson <vwilson@redhat.com>
2023-08-20 06:40:06 -0400

    ROX-17740: Create table of registry exceptions (#7341)

--- ui/apps/platform/src/Containers/Clusters/DelegateScanning/Components/DelegatedRegistriesList.tsx
+++ ui/apps/platform/src/Containers/Clusters/DelegateScanning/Components/DelegatedRegistriesList.tsx
@@ -1,27 +1,71 @@
+// TODO: remove lint override after @typescript-eslint deps can be resolved to ^5.2.x
+/* eslint-disable react/prop-types */
 import React from 'react';
 import {
     Bullseye,
     Button,
     Card,
-    CardBody,
+    CardFooter,
     EmptyState,
     EmptyStateBody,
     Title,
 } from '@patternfly/react-core';
+import PlusCircleIcon from '@patternfly/react-icons/dist/esm/icons/plus-circle-icon';
 
-import { DelegatedRegistry } from 'services/DelegatedRegistryConfigService';
+import {
+    DelegatedRegistry,
+    DelegatedRegistryCluster,
+} from 'services/DelegatedRegistryConfigService';
+import DelegatedRegistriesTable from './DelegatedRegistriesTable';
 
 type DelegatedRegistriesListProps = {
     registries: DelegatedRegistry[];
+    clusters: DelegatedRegistryCluster[];
+    selectedClusterId: string;
+    addRegistryRow: () => void;
+    deleteRow: (number) => void;
+    handlePathChange: (number, string) => void;
+    handleClusterChange: (number, string) => void;
+    // TODO: re-enable next type after @typescript-eslint deps can be resolved to ^5.2.x
+    // updateRegistriesOrder: (DelegatedRegistry[]) => void;
 };
 
-function DelegatedRegistriesList({ registries }: DelegatedRegistriesListProps) {
+function DelegatedRegistriesList({
+    registries,
+    clusters,
+    selectedClusterId,
+    handlePathChange,
+    handleClusterChange,
+    addRegistryRow,
+    deleteRow,
+    // TODO: remove lint override after @typescript-eslint deps can be resolved to ^5.2.x
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    updateRegistriesOrder,
+}: DelegatedRegistriesListProps) {
     return (
         <Card className="pf-u-mb-lg">
             {registries.length > 0 ? (
-                <CardBody>
-                    <p>(table goes here)</p>
-                </CardBody>
+                <>
+                    <DelegatedRegistriesTable
+                        registries={registries}
+                        clusters={clusters}
+                        selectedClusterId={selectedClusterId}
+                        handlePathChange={handlePathChange}
+                        handleClusterChange={handleClusterChange}
+                        deleteRow={deleteRow}
+                        // TODO: remove lint override after @typescript-eslint deps can be resolved to ^5.2.x
+                        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                        // @ts-ignore
+                        updateRegistriesOrder={updateRegistriesOrder}
+                        key="delegated-registries-table"
+                    />
+                    <CardFooter>
+                        <Button variant="link" icon={<PlusCircleIcon />} onClick={addRegistryRow}>
+                            Add registry
+                        </Button>
+                    </CardFooter>
+                </>
             ) : (
                 <Bullseye className="pf-u-flex-grow-1">
                     <EmptyState>
@@ -32,7 +76,9 @@ function DelegatedRegistriesList({ registries }: DelegatedRegistriesListProps) {
                             <p>All scans will be delegated to the default cluster.</p>
                             <p>You can override this for specific registries.</p>
                         </EmptyStateBody>
-                        <Button variant="primary">Add registry</Button>
+                        <Button variant="primary" onClick={addRegistryRow}>
+                            Add registry
+                        </Button>
                     </EmptyState>
                 </Bullseye>
             )}
