03764492f857a8faab2548e547287087b82a0093
Stephan Hesselmann <shesselm@redhat.com>
2023-08-22 00:56:54 +0200

    feat(notifications): notification interface (#7509)

---
+++ pkg/env/notifications.go
@@ -0,0 +1,4 @@
+package env
+
+// NotificationsStreamBufferSize is the buffer size of the notifications stream.
+var NotificationsStreamBufferSize = RegisterIntegerSetting("ROX_NOTIFICATIONS_STREAM_BUFFER_SIZE", 100)
