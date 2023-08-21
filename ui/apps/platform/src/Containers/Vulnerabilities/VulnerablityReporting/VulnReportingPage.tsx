dd3748e9c66f00cf4593ff0911c42ae809db5f48
Saif Chaudhry <sachaudh@users.noreply.github.com>
2023-08-21 11:57:27 -0700

    ROX-19163: VM Reporting 2.0 fixes (#7495)

--- ui/apps/platform/src/Containers/Vulnerabilities/VulnerablityReporting/VulnReportingPage.tsx
+++ ui/apps/platform/src/Containers/Vulnerabilities/VulnerablityReporting/VulnReportingPage.tsx
@@ -3,8 +3,9 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 
 import usePageAction from 'Containers/Vulnerabilities/VulnerablityReporting/hooks/usePageAction';
 import usePermissions from 'hooks/usePermissions';
-import { vulnerabilityReportsPath } from 'routePaths';
+import { vulnManagementReportsPath, vulnerabilityReportsPath } from 'routePaths';
 
+import TechPreviewBanner from 'Components/TechPreviewBanner';
 import VulnReportsPage from './VulnReports/VulnReportsPage';
 import CreateVulnReportPage from './ModifyVulnReport/CreateVulnReportPage';
 import EditVulnReportPage from './ModifyVulnReport/EditVulnReportPage';
@@ -32,34 +33,40 @@ function VulnReportingPage() {
         hasNotifierIntegrationReadAccess;
 
     return (
-        <Switch>
-            <Route
-                exact
-                path={vulnerabilityReportsPath}
-                render={(props) => {
-                    if (pageAction === 'create' && canReadWriteReports) {
-                        return <CreateVulnReportPage {...props} />;
-                    }
-                    if (pageAction === undefined) {
-                        return <VulnReportsPage {...props} />;
-                    }
-                    return <Redirect to={vulnerabilityReportsPath} />;
-                }}
+        <>
+            <TechPreviewBanner
+                featureURL={vulnManagementReportsPath}
+                featureName="Vulnerability Management (1.0) Reporting"
             />
-            <Route
-                exact
-                path={vulnerabilityReportPath}
-                render={(props) => {
-                    if (pageAction === 'edit' && canReadWriteReports) {
-                        return <EditVulnReportPage {...props} />;
-                    }
-                    if (pageAction === 'clone' && canReadWriteReports) {
-                        return <CloneVulnReportPage {...props} />;
-                    }
-                    return <ViewVulnReportPage {...props} />;
-                }}
-            />
-        </Switch>
+            <Switch>
+                <Route
+                    exact
+                    path={vulnerabilityReportsPath}
+                    render={(props) => {
+                        if (pageAction === 'create' && canReadWriteReports) {
+                            return <CreateVulnReportPage {...props} />;
+                        }
+                        if (pageAction === undefined) {
+                            return <VulnReportsPage {...props} />;
+                        }
+                        return <Redirect to={vulnerabilityReportsPath} />;
+                    }}
+                />
+                <Route
+                    exact
+                    path={vulnerabilityReportPath}
+                    render={(props) => {
+                        if (pageAction === 'edit' && canReadWriteReports) {
+                            return <EditVulnReportPage {...props} />;
+                        }
+                        if (pageAction === 'clone' && canReadWriteReports) {
+                            return <CloneVulnReportPage {...props} />;
+                        }
+                        return <ViewVulnReportPage {...props} />;
+                    }}
+                />
+            </Switch>
+        </>
     );
 }
 
