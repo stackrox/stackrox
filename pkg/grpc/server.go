27800de4c5339fb9d3b481cb8e1d0339290e7acc
Frederico Bittencourt <fbittenc@redhat.com>
2023-08-22 11:09:51 +0200

    ROX-18291: Add environment variable to configure GRPC message size (#7492)

--- pkg/grpc/server.go
+++ pkg/grpc/server.go
@@ -43,7 +43,7 @@ import (
 )
 
 const (
-	maxMsgSize                = 12 * 1024 * 1024
+	defaultMaxMsgSize         = 12 * 1024 * 1024
 	defaultMaxResponseMsgSize = 256 * 1024 * 1024 // 256MB
 )
 
@@ -55,6 +55,7 @@ func init() {
 var (
 	log = logging.LoggerForModule()
 
+	maxMsgSizeSetting         = env.RegisterIntegerSetting("ROX_GRPC_MAX_MESSAGE_SIZE", defaultMaxMsgSize)
 	maxResponseMsgSizeSetting = env.RegisterSetting("ROX_GRPC_MAX_RESPONSE_SIZE")
 	enableRequestTracing      = env.RegisterBooleanSetting("ROX_GRPC_ENABLE_REQUEST_TRACING", false)
 )
@@ -344,7 +345,7 @@ func (a *apiImpl) run(startedSig *concurrency.Signal) {
 		grpc.UnaryInterceptor(
 			grpc_middleware.ChainUnaryServer(a.unaryInterceptors()...),
 		),
-		grpc.MaxRecvMsgSize(maxMsgSize),
+		grpc.MaxRecvMsgSize(maxMsgSizeSetting.IntegerSetting()),
 		grpc.KeepaliveParams(keepalive.ServerParameters{
 			Time: 40 * time.Second,
 		}),
