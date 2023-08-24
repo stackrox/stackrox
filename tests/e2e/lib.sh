fb5342f55609958b1bb01f169a186741b5b103b3
mdafsanhossain <Mdafsan.Hossain@ibm.com>
2023-08-24 13:31:15 +0530

    Update support_pkg logic to account for both p/z

--- tests/e2e/lib.sh
+++ tests/e2e/lib.sh
@@ -289,10 +289,12 @@ deploy_sensor() {
 deploy_sensor_via_operator() {
     info "Deploying sensor via operator"
     #Temporarily upload the support package
-    if [[ "${REMOTE_CLUSTER_ARCH}" == "ppc64le" ]]; then
+    if [[ "${USE_MIDSTREAM_IMAGES}" == "true" ]]; then
         ROX_CENTRAL_ADDR=$(kubectl get routes/central -n stackrox -o json | jq -r '.spec.host'):443
         ROX_CENTRAL_PASS=$(kubectl -n stackrox get secret central-admin-pass -o go-template='{{index .data "password" | base64decode}}')
-        SUPPORT_URL=$(< operator/midstream/iib.json jq -r '.support_url')
+        # Constructing appropriate expression based on REMOTE_CLUSTER_ARCH
+        JQ_EXPR="< operator/midstream/iib.json jq -r '.support_url_${REMOTE_CLUSTER_ARCH}'"
+        SUPPORT_URL=$("${JQ_EXPR}")
         wget -O support-pkg.zip "$SUPPORT_URL"
         roxctl --endpoint "$ROX_CENTRAL_ADDR" --password "$ROX_CENTRAL_PASS" --insecure-skip-tls-verify collector support-packages upload support-pkg.zip
     fi
