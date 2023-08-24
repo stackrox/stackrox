b932fa918144ccd2ccb74dec2ded8522f0aa4e1a
Mark Pedrotti <pedrottimark@gmail.com>
2023-08-24 09:46:40 -0400

    ROX-19234: Add conditional RBAC for Vulnerabilities Reports 2.0 (#7566)

--- ui/apps/platform/src/Components/TechPreviewBanner.tsx
+++ ui/apps/platform/src/Components/TechPreviewBanner.tsx
@@ -2,20 +2,33 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Alert } from '@patternfly/react-core';
 
+import useIsRouteEnabled from 'hooks/useIsRouteEnabled';
+import { RouteKey } from 'routePaths';
+
 export type TechPreviewBannerProps = {
     featureURL: string;
     featureName: string;
+    routeKey: RouteKey;
 };
 
-function TechPreviewBanner({ featureURL, featureName }: TechPreviewBannerProps) {
+function TechPreviewBanner({ featureURL, featureName, routeKey }: TechPreviewBannerProps) {
+    const isRouteEnabled = useIsRouteEnabled();
+    const hasLink = isRouteEnabled(routeKey);
+
     return (
         <Alert
             variant="warning"
             isInline
+            component="p"
             title={
                 <span>
-                    This feature is in its Technology Preview phase. For optimal production
-                    performance, please rely on <Link to={featureURL}>{featureName}</Link>.
+                    This feature is in its Technology Preview phase.{' '}
+                    {hasLink && (
+                        <>
+                            For optimal production performance, please rely on{' '}
+                            <Link to={featureURL}>{featureName}</Link>.
+                        </>
+                    )}
                 </span>
             }
         />
