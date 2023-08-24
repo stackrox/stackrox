c38f016f423ee913cbc3b207efcac19e90b34b5d
Misha Sugakov <misha@stackrox.com>
2023-08-24 11:52:51 +0200

    Provide explicit `eval` command to `yq`
    Otherwise it produces an error in the output
    
    ```
    2023-08-23T18:45:06Z Applying operator manifests...
    Error: unknown command "[.. | select(key == \"grpcPodConfig\")].[].properties | has(\"securityContextConfig\")" for "yq"
    Run 'yq --help' for usage.
    ```

--- operator/hack/common.sh
+++ operator/hack/common.sh
@@ -50,7 +50,7 @@ function apply_operator_manifests() {
   local catalog_source_crd
   catalog_source_crd="$(kubectl get customresourcedefinitions.apiextensions.k8s.io catalogsources.operators.coreos.com -o yaml)"
   local disable_security_context_config="# "
-  if [[ "$(yq '[.. | select(key == "grpcPodConfig")].[].properties | has("securityContextConfig")' <<< "$catalog_source_crd")" == "true" ]]; then
+  if [[ "$(yq eval '[.. | select(key == "grpcPodConfig")].[].properties | has("securityContextConfig")' <<< "$catalog_source_crd")" == "true" ]]; then
       disable_security_context_config=""
   fi
 
