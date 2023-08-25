e4eba03ddc7ac4d84da14fc9ca086f8c259d763b
JoukoVirtanen <jouko@stackrox.com>
2023-08-24 21:22:06 -0700

    Fixed white space issues

--- scripts/release-tools/downstream/y-stream-setup.sh
+++ scripts/release-tools/downstream/y-stream-setup.sh
@@ -36,14 +36,14 @@ source "$DIR/perform_sed_or_gsed.sh"
 
 get_supported_versions() {
     local versions=()
-    
+
     supported_versions=($(curl -fsSL "https://access.redhat.com/product-life-cycles/api/v1/products?name=Red%20Hat%20Advanced%20Cluster%20Security%20for%20Kubernetes" | jq -r '.data[0].versions[] | select(.type == "Full Support") | .name'))
-    
+
     nversions=${#supported_versions[@]}
     for ((i = nversions - 1; i >= 0; i = i - 1)); do
         versions+=(${supported_versions[$i]})
     done
-    
+
     versions+=("$release")
 
     printf "%s\n" "${versions[@]}"
@@ -90,7 +90,7 @@ rm -f advisory_map.yml
 # Update release.yml with the correct version. Sinple sed should be safe, but check changes
 sed -i "s|$previous_release|$release|" release.yml
 
-# Update product.yml with the correct version. 
+# Update product.yml with the correct version.
 # This is a little more complicated since not all occurances of the old release should be changed
 # Check changes
 perform_sed_or_gsed "rhacs-$previous_release" "rhacs-$release" product.yml
