f4059a6e3089da4923ea6a21c4243b38f70330cc
Tomasz Janiszewski <tomek@redhat.com>
2023-08-28 16:33:13 +0200

    fix

--- pkg/notifiers/prune_test.go
+++ pkg/notifiers/prune_test.go
@@ -56,6 +56,7 @@ func TestFilterMap(t *testing.T) {
 		},
 	}
 	for _, c := range cases {
+		c := c
 		t.Run(c.name, func(t *testing.T) {
 			filterMap(c.inputMap, c.maxSize, &c.currSize)
 			assert.Equal(t, c.expectedMap, c.inputMap)
