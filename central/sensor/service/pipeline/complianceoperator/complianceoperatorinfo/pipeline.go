69a2b39940dcf09271f1b546a24c742a6dab749e
David Shrewsberry <99685630+dashrews78@users.noreply.github.com>
2023-09-01 13:52:09 -0400

    ROX-18308: compliance operator info wire central and sensor (#7418)
    connect sensor and central to pull metadata about compliance operator by cluster

--- central/sensor/service/pipeline/complianceoperator/complianceoperatorinfo/pipeline.go
+++ central/sensor/service/pipeline/complianceoperator/complianceoperatorinfo/pipeline.go
@@ -2,26 +2,34 @@ package complianceoperatorinfo
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/stackrox/rox/central/complianceoperator/v2/compliancemanager"
 	countMetrics "github.com/stackrox/rox/central/metrics"
 	"github.com/stackrox/rox/central/sensor/service/common"
 	"github.com/stackrox/rox/central/sensor/service/pipeline"
 	"github.com/stackrox/rox/central/sensor/service/pipeline/reconciliation"
 	"github.com/stackrox/rox/generated/internalapi/central"
+	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/features"
 	"github.com/stackrox/rox/pkg/metrics"
 )
 
 // GetPipeline returns an instantiation of this compliance operator info pipeline.
 func GetPipeline() pipeline.Fragment {
-	return NewPipeline()
+	return NewPipeline(compliancemanager.Singleton())
 }
 
 // NewPipeline returns a new instance of Pipeline.
-func NewPipeline() pipeline.Fragment {
-	return &pipelineImpl{}
+func NewPipeline(manager compliancemanager.Manager) pipeline.Fragment {
+	return &pipelineImpl{
+		manager: manager,
+	}
 }
 
-type pipelineImpl struct{}
+type pipelineImpl struct {
+	manager compliancemanager.Manager
+}
 
 func (s *pipelineImpl) Reconcile(_ context.Context, _ string, _ *reconciliation.StoreMap) error {
 	return nil
@@ -32,10 +40,34 @@ func (s *pipelineImpl) Match(msg *central.MsgFromSensor) bool {
 }
 
 // Run runs the pipeline template on the input and returns the output.
-func (s *pipelineImpl) Run(_ context.Context, _ string, msg *central.MsgFromSensor, _ common.MessageInjector) error {
+func (s *pipelineImpl) Run(ctx context.Context, clusterID string, msg *central.MsgFromSensor, _ common.MessageInjector) error {
 	defer countMetrics.IncrementResourceProcessedCounter(pipeline.ActionToOperation(msg.GetEvent().GetAction()), metrics.ComplianceOperatorInfo)
-	// Do something
-	return nil
+	if !features.ComplianceEnhancements.Enabled() {
+		return nil
+	}
+
+	var operatorErrors []string
+	operatorInfo := &storage.ComplianceIntegration{
+		Version:   msg.GetComplianceOperatorInfo().GetVersion(),
+		ClusterId: clusterID,
+		Namespace: msg.GetComplianceOperatorInfo().GetNamespace(),
+	}
+
+	if msg.GetComplianceOperatorInfo().GetStatusError() != "" {
+		operatorErrors = append(operatorErrors, msg.GetComplianceOperatorInfo().GetStatusError())
+	}
+
+	desiredPods := msg.GetComplianceOperatorInfo().GetTotalDesiredPods()
+	readyPods := msg.GetComplianceOperatorInfo().GetTotalReadyPods()
+
+	// if not ready, add it to the status errors
+	if readyPods < desiredPods {
+		operatorErrors = append(operatorErrors, fmt.Sprintf("compliance operator not ready.  Only %d pods are ready when %d are desired.", readyPods, desiredPods))
+	}
+
+	operatorInfo.StatusErrors = operatorErrors
+
+	return s.manager.ProcessComplianceOperatorInfo(ctx, operatorInfo)
 }
 
 func (s *pipelineImpl) OnFinish(_ string) {}
