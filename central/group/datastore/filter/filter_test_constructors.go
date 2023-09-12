f355afe0c6bd857ca1a13d74f941477b7920ceca
Yann Brillouet <ybrillou@redhat.com>
2023-09-12 18:26:17 +0200

    Fix style

--- central/group/datastore/filter/filter_test_constructors.go
+++ central/group/datastore/filter/filter_test_constructors.go
@@ -9,25 +9,25 @@ import (
 	"github.com/stackrox/rox/pkg/postgres"
 )
 
-// FilterFactory is an interface to generate filtered group fetchers
-type FilterFactory interface {
+// Factory is an interface to generate filtered group fetchers.
+type Factory interface {
 	FilteredRetriever() func(ctx context.Context, filter func(*storage.Group) bool) ([]*storage.Group, error)
 }
 
 // GetTestPostgresGroupFilterGenerator returns a generator for filtered group
 // retrieval connected to a postgres database.
-func GetTestPostgresGroupFilterGenerator(_ *testing.T, db postgres.DB) FilterFactory {
+func GetTestPostgresGroupFilterGenerator(_ *testing.T, db postgres.DB) Factory {
 	groupStore := postgresStore.New(db)
-	return &filterFactoryImpl{
+	return &factoryImpl{
 		store: groupStore,
 	}
 }
 
-type filterFactoryImpl struct {
+type factoryImpl struct {
 	store postgresStore.Store
 }
 
-func (f *filterFactoryImpl) FilteredRetriever() func(ctx context.Context, filter func(*storage.Group) bool) ([]*storage.Group, error) {
+func (f *factoryImpl) FilteredRetriever() func(ctx context.Context, filter func(*storage.Group) bool) ([]*storage.Group, error) {
 	return func(ctx context.Context, filter func(*storage.Group) bool) ([]*storage.Group, error) {
 		return GetFilteredWithStore(ctx, filter, f.store)
 	}
