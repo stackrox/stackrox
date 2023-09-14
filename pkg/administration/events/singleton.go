a10b97e5f3f30a91faa089811db61f9b94427bb2
Daniel Haus <dhaus@redhat.com>
2023-09-14 22:56:57 +0200

    Address review comments.

--- pkg/administration/events/singleton.go
+++ pkg/administration/events/singleton.go
@@ -12,6 +12,11 @@ var (
 )
 
 // Singleton returns an instance of the administration events stream.
+// Note that the Stream interface only holds events within memory and does not
+// require any database related interfaces, nor does it initialize them.
+//
+// Currently, the Stream is separated from `central` to allow packages under `pkg` to rely on it, instead
+// of requiring a dependency towards packages within `central`.
 func Singleton() Stream {
 	if !features.CentralEvents.Enabled() {
 		return nil
