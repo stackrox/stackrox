a3e23dcd61e3297c10b5cf74343fb66f5c8b47e2
Yann Brillouet <ybrillou@redhat.com>
2023-09-27 10:33:51 +0200

    feat(hackathon): allow multiple role selection in UI at API token generation

--- ui/apps/platform/src/Containers/Integrations/IntegrationForm/Forms/ApiTokenIntegrationForm/ApiTokenIntegrationForm.tsx
+++ ui/apps/platform/src/Containers/Integrations/IntegrationForm/Forms/ApiTokenIntegrationForm/ApiTokenIntegrationForm.tsx
@@ -30,6 +30,7 @@ import IntegrationFormActions from '../../IntegrationFormActions';
 import ApiTokenFormMessageAlert from './ApiTokenFormMessageAlert';
 import type { ApiTokenFormResponseMessage } from './ApiTokenFormMessageAlert';
 import useAllowedRoles from './useAllowedRoles';
+import RoleSelector from './RoleSelector';
 
 export type ApiTokenIntegrationFormValues = {
     name: string;
@@ -94,9 +95,17 @@ function ApiTokenIntegrationForm({
         return setFieldValue(event.target.id, value);
     }
 
-    function onRoleChange(id, selection) {
-        return setFieldValue(id, [selection]);
-    }
+    const onRoleSelect = (_, selected) => {
+        const newSelection = values.roles.find((roleFilter) => roleFilter === selected)
+            ? values.roles.filter((roleFilter) => roleFilter !== selected)
+            : values.roles.concat(selected);
+
+        return setFieldValue("roles", newSelection)
+    };
+
+    const onClearRoleSelections = () => {
+        return setFieldValue("roles", []);
+    };
 
     // The edit flow doesn't make sense for API Tokens so we'll show an empty state message here
     if (isEditing) {
@@ -168,26 +177,17 @@ function ApiTokenIntegrationForm({
                         </FormLabelGroup>
                         <FormLabelGroup
                             isRequired
-                            label="Role"
+                            label="Roles"
                             fieldId="roles"
                             touched={touched}
                             errors={errors}
                         >
-                            <SelectSingle
-                                id="roles"
-                                value={values.roles[0]}
-                                handleSelect={onRoleChange}
-                                isDisabled={!isEditable || isRolesLoading || isGenerated}
-                                placeholderText="Choose role..."
-                            >
-                                {roleNames.map((roleName) => {
-                                    return (
-                                        <SelectOption key={roleName} value={roleName}>
-                                            {roleName}
-                                        </SelectOption>
-                                    );
-                                })}
-                            </SelectSingle>
+                            <RoleSelector
+                                roles={roleNames}
+                                selectedRoles={values.roles}
+                                onRoleSelect={onRoleSelect}
+                                onRoleSelectionClear={onClearRoleSelections}
+                            />
                         </FormLabelGroup>
                         {isEditable && !isGenerated && (
                             <FormLabelGroup
