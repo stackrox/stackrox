7a1982ae08115f7e43c3fa359011ae6515d42a55
Tom Martensen <tmartens@redhat.com>
2023-09-27 14:52:11 +0200

    minor improvement

--- tests/performance/tests/testK6Integration.js
+++ tests/performance/tests/testK6Integration.js
@@ -1,3 +1,4 @@
+// TODO: these imports may be done in package.json
 import { jUnit, textSummary } from 'https://jslib.k6.io/k6-summary/0.0.2/index.js';
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
