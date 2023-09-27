c8ea4d9fe45b3f41e8a4b91c2ebaa957fda3e600
Tom Martensen <tmartens@redhat.com>
2023-09-27 14:53:40 +0200

    feat(hackathon): CI: Enhance K6 output formatting (#7890)

--- tests/performance/tests/testK6Integration.js
+++ tests/performance/tests/testK6Integration.js
@@ -1,3 +1,7 @@
+// TODO: these imports may be done in package.json
+import { jUnit, textSummary } from 'https://jslib.k6.io/k6-summary/0.0.2/index.js';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
 import { getHeaderWithAdminPass } from '../src/utils.js';
 import { mainDashboard } from '../groups/mainDashboard.js';
 
@@ -15,3 +19,13 @@ export default function main() {
     console.log('Running tests for admin scope');
     runAllGroups(getHeaderWithAdminPass(__ENV.ROX_ADMIN_PASSWORD), { sac_user: 'admin' });
 }
+
+export function handleSummary(data) {
+    return {
+      'stdout': textSummary(data, { indent: '  ', enableColors: true }), // the default text output to stdout
+      'performance-results/report.txt': textSummary(data, { indent: '  ', enableColors: false }), // the default text output to a file
+      'performance-results/report.xml': jUnit(data), // JUnit output to a file
+      'performance-results/report.json': JSON.stringify(data), // JSON output to a file
+      'performance-results/report.html': htmlReport(data), // HTML report
+    };
+  }
