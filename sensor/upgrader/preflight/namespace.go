354ec3e44e81d8c0beeec0a0458c8bc85e0bb8b0
Stephan Heßelmann <shesselm@redhat.com>
2023-10-02 14:19:12 +0200

    fix sensor upgrader Prometheus resources

--- sensor/upgrader/preflight/namespace.go
+++ sensor/upgrader/preflight/namespace.go
@@ -1,10 +1,17 @@
 package preflight
 
 import (
+	"github.com/stackrox/rox/pkg/k8sutil/k8sobjects"
 	"github.com/stackrox/rox/sensor/upgrader/common"
 	"github.com/stackrox/rox/sensor/upgrader/plan"
 	"github.com/stackrox/rox/sensor/upgrader/upgradectx"
 )
+
+// Resources created in system namespaces for OpenShift monitoring.
+var resourceExceptions = map[string][]string{
+	"kube-system":          {"RoleBinding"},
+	"openshift-monitoring": {"ServiceMonitor", "PrometheusRule"},
+}
 
 type namespaceCheck struct{}
 
@@ -12,9 +19,22 @@ func (namespaceCheck) Name() string {
 	return "Allowed namespaces"
 }
 
+func isException(resource *k8sobjects.ObjectRef) bool {
+	if kinds, ok := resourceExceptions[resource.Namespace]; ok {
+		for _, k := range kinds {
+			if k == resource.GVK.Kind {
+				return true
+			}
+		}
+	}
+	return false
+}
+
 func (namespaceCheck) Check(_ *upgradectx.UpgradeContext, execPlan *plan.ExecutionPlan, reporter checkReporter) error {
 	for _, act := range execPlan.Actions() {
-		if act.ObjectRef.Namespace != "" && act.ObjectRef.Namespace != common.Namespace {
+		act := act
+		doCheck := !isException(&act.ObjectRef)
+		if doCheck && act.ObjectRef.Namespace != "" && act.ObjectRef.Namespace != common.Namespace {
 			reporter.Errorf("To-be-%sd object %v is in disallowed namespace %s", act.ActionName, act.ObjectRef, common.Namespace)
 		}
 	}
