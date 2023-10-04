812cfbcf9b4e6cb53c27e929ffd8a4c7ceeea4d2
Daniel Haus <dhaus@redhat.com>
2023-10-04 16:10:55 +0200

    feat(admin events): add benchmarks for datastore

--- central/administration/events/datastore/internal/store/store.go
+++ central/administration/events/datastore/internal/store/store.go
@@ -14,4 +14,5 @@ type Store interface {
 	Get(ctx context.Context, id string) (*storage.AdministrationEvent, bool, error)
 	GetByQuery(ctx context.Context, query *v1.Query) ([]*storage.AdministrationEvent, error)
 	UpsertMany(ctx context.Context, objs []*storage.AdministrationEvent) error
+	DeleteMany(ctx context.Context, identifiers []string) error
 }
