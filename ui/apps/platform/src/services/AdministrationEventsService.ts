4edfcbdfed6d3f423bf067c71be7ec34dcbabc0d
Mark Pedrotti <pedrottimark@gmail.com>
2023-10-04 16:47:58 -0400

    ROX-19910: Add administration events query string to page address (#7958)

--- ui/apps/platform/src/services/AdministrationEventsService.ts
+++ ui/apps/platform/src/services/AdministrationEventsService.ts
@@ -1,3 +1,8 @@
+import qs from 'qs';
+
+import { SearchFilter } from 'types/search';
+import { SortOption } from 'types/table';
+
 import axios from './instance';
 
 import { Pagination } from './types';
@@ -30,25 +35,31 @@ export type AdministrationEvent = {
     createdAt: string; // ISO 8601
 };
 
-export type AdministrationEventType =
-    | 'ADMINISTRATION_EVENT_TYPE_UNKNOWN'
-    | 'ADMINISTRATION_EVENT_TYPE_GENERIC'
-    | 'ADMINISTRATION_EVENT_TYPE_LOG_MESSAGE';
+const types = [
+    'ADMINISTRATION_EVENT_TYPE_UNKNOWN',
+    'ADMINISTRATION_EVENT_TYPE_GENERIC',
+    'ADMINISTRATION_EVENT_TYPE_LOG_MESSAGE',
+] as const; // for isType function
 
-export type AdministrationEventLevel =
-    | 'ADMINISTRATION_EVENT_LEVEL_UNKNOWN'
-    | 'ADMINISTRATION_EVENT_LEVEL_INFO'
-    | 'ADMINISTRATION_EVENT_LEVEL_SUCCESS'
-    | 'ADMINISTRATION_EVENT_LEVEL_WARNING'
-    | 'ADMINISTRATION_EVENT_LEVEL_ERROR';
+export type AdministrationEventType = (typeof types)[number];
+
+const levels = [
+    'ADMINISTRATION_EVENT_LEVEL_UNKNOWN',
+    'ADMINISTRATION_EVENT_LEVEL_INFO',
+    'ADMINISTRATION_EVENT_LEVEL_SUCCESS',
+    'ADMINISTRATION_EVENT_LEVEL_WARNING',
+    'ADMINISTRATION_EVENT_LEVEL_ERROR',
+] as const; // for isLevel function
+
+export type AdministrationEventLevel = (typeof levels)[number];
 
 export type AdministrationEventsFilter = {
-    from: string; // ISO 8601 lower (older) boundary
-    until: string; // ISO 8601 upper (newer) boundary
-    domain: string;
-    resourceType: string;
-    type: AdministrationEventType;
-    level: AdministrationEventLevel;
+    from?: string; // ISO 8601 lower (older) boundary
+    until?: string; // ISO 8601 upper (newer) boundary
+    domain?: string[];
+    resourceType?: string[];
+    type?: AdministrationEventType[];
+    level?: AdministrationEventLevel[];
 };
 
 export type CountAdministrationEventsRequest = {
@@ -57,7 +68,7 @@ export type CountAdministrationEventsRequest = {
 
 // The total number of notifications after filtering and deduplication.
 export type CountAdministrationEventsResponse = {
-    count: string; // int64
+    count: number; // int32
 };
 
 export type GetAdministrationEventResponse = {
@@ -65,7 +76,7 @@ export type GetAdministrationEventResponse = {
 };
 
 export type ListAdministrationEventsRequest = {
-    pagination: Pagination;
+    pagination?: Pagination;
     filter: AdministrationEventsFilter;
 };
 
@@ -73,10 +84,10 @@ export type ListAdministrationEventsResponse = {
     events: AdministrationEvent[];
 };
 
-// TODO CountAdministrationEventsRequest
-export function countAdministrationEvents(): Promise<string> {
+export function countAdministrationEvents(filter: AdministrationEventsFilter): Promise<number> {
+    const params = qs.stringify({ filter }, { arrayFormat: 'repeat', allowDots: true });
     return axios
-        .get<CountAdministrationEventsResponse>(eventsCountUrl)
+        .get<CountAdministrationEventsResponse>(`${eventsCountUrl}?${params}`)
         .then((response) => response.data.count);
 }
 
@@ -86,9 +97,84 @@ export function getAdministrationEvent(id: string): Promise<AdministrationEvent>
         .then((response) => response.data.event);
 }
 
-// TODO ListAdministrationEventsRequest
-export function listAdministrationEvents(): Promise<AdministrationEvent[]> {
+export function listAdministrationEvents(
+    arg: ListAdministrationEventsRequest
+): Promise<AdministrationEvent[]> {
+    const params = qs.stringify(arg, { arrayFormat: 'repeat', allowDots: true });
     return axios
-        .get<ListAdministrationEventsResponse>(eventsUrl)
+        .get<ListAdministrationEventsResponse>(`${eventsUrl}?${params}`)
         .then((response) => response.data.events);
 }
+
+// Given searchFilter from useURLSearch hook, return validated filter argument.
+
+export function getAdministrationEventsFilter(
+    searchFilter: SearchFilter
+): AdministrationEventsFilter {
+    // For consistency with useURLSort hook, especially in case the table columns become sortable,
+    // useURLSearch hook also uses search strings.
+    // See proto/storage/administration_event.proto
+    return {
+        domain: getValue(searchFilter['Event Domain']),
+        level: getLevel(searchFilter['Event Level']),
+        resourceType: getValue(searchFilter['Resource Type']),
+        type: getType(searchFilter['Event Type']),
+    };
+}
+
+type SearchFilterValue = string | string[] | undefined;
+
+function getLevel(arg: SearchFilterValue): AdministrationEventLevel[] | undefined {
+    if (typeof arg === 'string' && isLevel(arg)) {
+        return [arg];
+    }
+
+    if (Array.isArray(arg)) {
+        return arg.filter(isLevel);
+    }
+
+    return undefined;
+}
+
+function isLevel(arg: string): arg is AdministrationEventLevel {
+    return levels.some((level) => level === arg);
+}
+
+function getType(arg: SearchFilterValue): AdministrationEventType[] | undefined {
+    if (typeof arg === 'string' && isType(arg)) {
+        return [arg];
+    }
+
+    if (Array.isArray(arg)) {
+        return arg.filter(isType);
+    }
+
+    return undefined;
+}
+
+function isType(arg: string): arg is AdministrationEventType {
+    return types.includes(arg as AdministrationEventType);
+}
+
+function getValue(arg: SearchFilterValue): string[] | undefined {
+    if (typeof arg === 'string') {
+        return [arg];
+    }
+
+    if (Array.isArray(arg)) {
+        return arg;
+    }
+
+    return undefined;
+}
+
+// For useURLSort hook.
+
+export const lastOccurredAtField = 'Last Updated';
+export const numOccurrencesField = 'Event Occurrence';
+
+export const sortFields = [lastOccurredAtField, numOccurrencesField]; // correspond to numOccurrences and lastOccurredAt
+export const defaultSortOption: SortOption = {
+    field: lastOccurredAtField,
+    direction: 'desc', // descending from most recent
+};
