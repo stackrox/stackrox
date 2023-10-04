7102407f096fa009d6cdc6df4574b5e81fa4440a
mdafsanhossain <Mdafsan.Hossain@ibm.com>
2023-10-04 15:23:41 +0530

    Change test flow to use custom tags
    Signed-off-by: mdafsanhossain <Mdafsan.Hossain@ibm.com>

--- .openshift-ci/base_qa_e2e_test.py
+++ .openshift-ci/base_qa_e2e_test.py
@@ -46,50 +46,13 @@ def make_qa_e2e_test_runner(cluster):
         ),
     )
 
-def make_qa_e2e_test_runner_midstream(cluster):
-    return ClusterTestSetsRunner(
-        cluster=cluster,
-        sets=[
-            {
-                "name": "QA tests part I",
-                "pre_test": PreSystemTests(),
-                "test": QaE2eTestPart1(),
-                "post_test": PostClusterTest(
-                    check_stackrox_logs=True,
-                    artifact_destination_prefix="part-1",
-                ),
-            },
-            {
-                "name": "QA tests part II",
-                "test": QaE2eTestPart2(),
-                "post_test": PostClusterTest(
-                    check_stackrox_logs=True,
-                    artifact_destination_prefix="part-2",
-                ),
-                "always_run": False,
-            },
-            {
-                "name": "DB backup and restore",
-                "test": QaE2eDBBackupRestoreTest(),
-                "post_test": CheckStackroxLogs(
-                    check_for_errors_in_stackrox_logs=True,
-                    artifact_destination_prefix="db-test",
-                ),
-                "always_run": False,
-            },
-        ],
-        final_post=FinalPost(
-            store_qa_tests_data=True,
-        ),
-    )
-
 def make_qa_e2e_test_runner_custom(cluster):
     return ClusterTestSetsRunner(
         cluster=cluster,
         sets=[
             {
                 "name": "Custom set of tests for p/z",
-                "pre_test": PreSystemTests(),
+                "pre_test": PreSystemTests(run_poll_for_system_test_images=False),
                 "test": CustomSetTest(),
                 "post_test": PostClusterTest(
                     check_stackrox_logs=True,
@@ -99,5 +62,6 @@ def make_qa_e2e_test_runner_custom(cluster):
         ],
         final_post=FinalPost(
             store_qa_tests_data=True,
+            handle_e2e_progress_failures=False,
         ),
     )
