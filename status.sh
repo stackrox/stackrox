df04826e4f1ac24b42cd25206ca1627cce091647
Tom Martensen <tmartens@redhat.com>
2023-10-04 15:11:27 +0200

    build roxctl rhtap

--- status.sh
+++ status.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+# TODO: this requires .git directory in the builder container
 echo "STABLE_MAIN_VERSION $(make --quiet --no-print-directory tag)"
 echo "STABLE_COLLECTOR_VERSION $(cat COLLECTOR_VERSION)"
 echo "STABLE_SCANNER_VERSION $(cat SCANNER_VERSION)"
+# TODO: this requires .git directory in the builder container
 echo "STABLE_GIT_SHORT_SHA $(make --quiet --no-print-directory shortcommit)"
