0ccfaaa200ec508cd6bfc906ff33be16fa7870a9
Matthias Meidinger <mmeiding@redhat.com>
2023-10-09 16:43:03 +0200

    Introduce tests

--- sensor/kubernetes/listener/resources/hash_reconciliation_test.go
+++ sensor/kubernetes/listener/resources/hash_reconciliation_test.go
@@ -56,3 +56,72 @@ func (s *HashReconciliationSuite) TestResourceToMessage() {
 		})
 	}
 }
+
+func (s *HashReconciliationSuite) TestEmptyDeploymentProvider() {
+	store := InitializeStore()
+	rc := NewResourceStoreReconciler(store)
+
+	// given the stores are empty, if you provide a single deduper entry, generate 1 delete event
+	dstate := map[string]uint64{
+		"Deployment:1234": 01234,
+	}
+
+	msgs := rc.ProcessHashes(dstate)
+
+	s.Equal(1, len(msgs))
+	s.Equal(msgs[0].GetEvent().Action, central.ResourceAction_REMOVE_RESOURCE)
+	s.Equal(msgs[0].GetEvent().GetDeployment().GetId(), "1234")
+}
+
+func (s *HashReconciliationSuite) TestDeploymentProviderSingleDelete() {
+	store := InitializeStore()
+	store.deploymentStore.addOrUpdateDeployment(createWrapWithID("123"))
+	store.deploymentStore.addOrUpdateDeployment(createWrapWithID("456"))
+	store.deploymentStore.addOrUpdateDeployment(createWrapWithID("678"))
+	rc := NewResourceStoreReconciler(store)
+
+	// given the stores are empty, if you provide a single deduper entry, generate 1 delete event
+	dstate := map[string]uint64{
+		"Deployment:1234": 87654,
+		"Deployment:456":  76543,
+	}
+
+	msgs := rc.ProcessHashes(dstate)
+
+	s.Equal(1, len(msgs))
+	s.Equal(msgs[0].GetEvent().Action, central.ResourceAction_REMOVE_RESOURCE)
+	s.Equal(msgs[0].GetEvent().GetDeployment().GetId(), "1234")
+}
+
+func (s *HashReconciliationSuite) TestDeploymentProviderMultiDelete() {
+	store := InitializeStore()
+	store.deploymentStore.addOrUpdateDeployment(createWrapWithID("123"))
+	store.deploymentStore.addOrUpdateDeployment(createWrapWithID("456"))
+	store.deploymentStore.addOrUpdateDeployment(createWrapWithID("678"))
+	rc := NewResourceStoreReconciler(store)
+
+	// given the stores are empty, if you provide a single deduper entry, generate 1 delete event
+	dstate := map[string]uint64{
+		"Deployment:1234": 87654,
+		"Deployment:456":  76543,
+		"Deployment:0987": 98755,
+	}
+
+	msgs := rc.ProcessHashes(dstate)
+
+	s.Equal(2, len(msgs))
+
+	ids := make([]string, 0)
+	for _, m := range msgs {
+		s.Require().Equal(central.ResourceAction_REMOVE_RESOURCE, m.GetEvent().GetAction())
+		ids = append(ids, m.GetEvent().GetDeployment().GetId())
+	}
+
+	s.ElementsMatch([]string{"0987", "1234"}, ids)
+}
+
+func createWrapWithID(id string) *deploymentWrap {
+	d := createDeploymentWrap()
+	d.Id = id
+	return d
+}
