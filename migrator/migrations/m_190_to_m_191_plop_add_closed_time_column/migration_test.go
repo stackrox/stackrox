289257c587de0e86bf9e9439492d0d4252d8933c
David Shrewsberry <99685630+dashrews78@users.noreply.github.com>
2023-10-09 15:47:24 -0400

    ROX-20017: fix plop migration revert simple transport (#8015)
    fix migrations to use serialized field explicitly in gorm queries to prevent stale statement caches.  Also re-applies PLOP migration and removes changes to use simple transport.

--- migrator/migrations/m_190_to_m_191_plop_add_closed_time_column/migration_test.go
+++ migrator/migrations/m_190_to_m_191_plop_add_closed_time_column/migration_test.go
@@ -6,8 +6,15 @@ import (
 	"context"
 	"testing"
 
+	"github.com/stackrox/rox/generated/storage"
+	updatedSchema "github.com/stackrox/rox/migrator/migrations/m_190_to_m_191_plop_add_closed_time_column/schema"
+	"github.com/stackrox/rox/migrator/migrations/m_190_to_m_191_plop_add_closed_time_column/test/schema"
 	pghelper "github.com/stackrox/rox/migrator/migrations/postgreshelper"
+	"github.com/stackrox/rox/migrator/types"
+	"github.com/stackrox/rox/pkg/postgres/pgutils"
 	"github.com/stackrox/rox/pkg/sac"
+	"github.com/stackrox/rox/pkg/testutils"
+	"github.com/stackrox/rox/pkg/timestamp"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -32,5 +39,43 @@ func (s *migrationTestSuite) TearDownSuite() {
 }
 
 func (s *migrationTestSuite) TestMigration() {
-	// TODO(ROX-20017): re-implement when migration fixed
+	dbs := &types.Databases{
+		GormDB:     s.db.GetGormDB(),
+		PostgresDB: s.db.DB,
+		DBCtx:      s.ctx,
+	}
+
+	// Create the old schema for testing
+	pgutils.CreateTableFromModel(dbs.DBCtx, dbs.GormDB, schema.CreateTableListeningEndpointsStmt)
+
+	// Add some plops
+	numPlop := 2000
+	var convertedPlops []schema.ListeningEndpoints
+	for i := 0; i < numPlop; i++ {
+		plop := &storage.ProcessListeningOnPortStorage{}
+		s.NoError(testutils.FullInit(plop, testutils.UniqueInitializer(), testutils.JSONFieldsFilter))
+		plop.CloseTimestamp = timestamp.Now().GogoProtobuf()
+		converted, err := schema.ConvertProcessListeningOnPortStorageFromProto(plop)
+		s.Require().NoError(err)
+		convertedPlops = append(convertedPlops, *converted)
+	}
+
+	s.Require().NoError(dbs.GormDB.Create(convertedPlops).Error)
+
+	// Apply the new schema to then ensure time field is empty
+	pgutils.CreateTableFromModel(dbs.DBCtx, dbs.GormDB, updatedSchema.CreateTableListeningEndpointsStmt)
+
+	var n int
+	err := s.db.DB.QueryRow(s.ctx, "SELECT COUNT(*) FROM listening_endpoints WHERE closetimestamp IS NULL;").Scan(&n)
+	s.NoError(err)
+	s.Require().Equal(numPlop, n)
+
+	// Now run the migration
+	s.Require().NoError(migration.Run(dbs))
+
+	// After the migration, timestamp should not be NULL
+	err = s.db.DB.QueryRow(s.ctx, "SELECT COUNT(*) FROM listening_endpoints WHERE closetimestamp IS NULL;").Scan(&n)
+	s.NoError(err)
+	s.Require().Equal(0, n)
+
 }
