bc102d6c31508a4e3d4bdd160a518744a75632b2
Charmik Sheth <101146970+charmik-redhat@users.noreply.github.com>
2023-10-09 17:19:11 -0700

    Fix style

--- pkg/notifiers/types.go
+++ pkg/notifiers/types.go
@@ -1,15 +1,36 @@
 package notifiers
 
 const (
-	JiraType           = "jira"
-	EmailType          = "email"
-	CSCCType           = "cscc"
-	SplunkType         = "splunk"
-	SlackType          = "slack"
-	TeamsType          = "teams"
-	PagerDutyType      = "pagerduty"
-	GenericType        = "generic"
-	SumoLogicType      = "sumologic"
+	// JiraType represents notifier type jira
+	JiraType = "jira"
+
+	// EmailType represents notifier type email
+	EmailType = "email"
+
+	// CSCCType represents notifier type cscc
+	CSCCType = "cscc"
+
+	// SplunkType represents notifier type splunk
+	SplunkType = "splunk"
+
+	// SlackType represents notifier type slack
+	SlackType = "slack"
+
+	// TeamsType represents notifier type teams
+	TeamsType = "teams"
+
+	// PagerDutyType represents notifier type pagerduty
+	PagerDutyType = "pagerduty"
+
+	// GenericType represents notifier type generic
+	GenericType = "generic"
+
+	// SumoLogicType represents notifier type sumologic
+	SumoLogicType = "sumologic"
+
+	// AWSSecurityHubType represents notifier type awsSecurityHub
 	AWSSecurityHubType = "awsSecurityHub"
-	SyslogType         = "syslog"
+
+	// SyslogType represents notifier type syslog
+	SyslogType = "syslog"
 )
