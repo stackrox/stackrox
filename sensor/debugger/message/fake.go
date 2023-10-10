f6cd4e215aae0c5ce73965b058ae5f98bc7e9ab6
lvalerom <lvalerom@redhat.com>
2023-10-10 18:21:01 +0200

    add central capabilities to the fake central hello message

--- sensor/debugger/message/fake.go
+++ sensor/debugger/message/fake.go
@@ -3,15 +3,17 @@ package message
 import (
 	"github.com/stackrox/rox/generated/internalapi/central"
 	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/centralsensor"
 )
 
 // SensorHello returns a fake SensorHello message
-func SensorHello(clsuterID string) *central.MsgToSensor {
+func SensorHello(clusterID string) *central.MsgToSensor {
 	return &central.MsgToSensor{
 		Msg: &central.MsgToSensor_Hello{
 			Hello: &central.CentralHello{
-				ClusterId:  clsuterID,
-				CertBundle: map[string]string{},
+				ClusterId:    clusterID,
+				CertBundle:   map[string]string{},
+				Capabilities: []string{centralsensor.SensorReconciliationOnReconnect},
 			},
 		},
 	}
