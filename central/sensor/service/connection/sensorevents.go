fabe4e75f3e5f783792659a9572a3d8c92f87c93
Frederico Bittencourt <fbittenc@redhat.com>
2023-10-10 18:13:46 +0200

    skip reconciliation and send hashes to sensor

--- central/sensor/service/connection/sensorevents.go
+++ central/sensor/service/connection/sensorevents.go
@@ -68,6 +68,10 @@ func stripTypePrefix(s string) string {
 	return s
 }
 
+func (s *sensorEventHandler) disableReconciliation() {
+	s.reconciliationMap.Close()
+}
+
 func (s *sensorEventHandler) addMultiplexed(ctx context.Context, msg *central.MsgFromSensor) {
 	event := msg.GetEvent()
 	if event == nil {
@@ -81,6 +85,11 @@ func (s *sensorEventHandler) addMultiplexed(ctx context.Context, msg *central.Ms
 	// of the initial synchronization process.
 	case *central.SensorEvent_Synced:
 		// Call the reconcile functions
+		log.Info("Receiving reconciliation event")
+		if s.reconciliationMap.IsClosed() {
+			log.Info("Ignoring SYNC since reconciliationMap is already closed")
+			return
+		}
 		if err := s.pipeline.Reconcile(ctx, s.reconciliationMap); err != nil {
 			log.Errorf("error reconciling state: %v", err)
 		}
