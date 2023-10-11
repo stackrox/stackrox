87a2d291eab79a0ae46eec63099a6d59d31dcbf9
Charmik Sheth <101146970+charmik-redhat@users.noreply.github.com>
2023-10-11 10:14:23 -0700

    ROX-19984: Report emails do not have custom subject and other bugs (#7977)

--- central/reports/scheduler/v2/reportgenerator/consts.go
+++ central/reports/scheduler/v2/reportgenerator/consts.go
@@ -51,13 +51,13 @@ const (
 		cveFieldsFragment
 	watchedImagesReportQueryOpName = "getWatchedImagesReportData"
 
-	defaultEmailSubjectTemplate = "({{.BrandedProductNameShort}}) Workload CVE Report for {{.ReportConfigName}}; Scope: {{.CollectionName}}"
+	defaultEmailSubjectTemplate = "{{.BrandedProductNameShort}} Workload CVE Report for {{.ReportConfigName}}; Scope: {{.CollectionName}}"
 
-	defaultEmailBodyTemplate = "{{.BrandedProductName}} ({{.BrandedProductNameShort}}) for Kubernetes has identified workload CVEs in the images matched by the following report configuration parameters. " +
+	defaultEmailBodyTemplate = "{{.BrandedPrefix}} for Kubernetes has identified workload CVEs in the images matched by the following report configuration parameters. " +
 		"The attached Vulnerability report lists those workload CVEs and associated details to help with remediation. " +
-		"Please review the vulnerable software packages/ components from the impacted images and update them to a version containing the fix, if one is available.\n"
+		"Please review the vulnerable software packages/components from the impacted images and update them to a version containing the fix, if one is available.\n"
 
-	defaultNoVulnsEmailBodyTemplate = "{{.BrandedProductName}} ({{.BrandedProductNameShort}}) for Kubernetes has found no workload CVEs in the images matched by the following report configuration parameters.\n"
+	defaultNoVulnsEmailBodyTemplate = "{{.BrandedPrefix}} for Kubernetes has found no workload CVEs in the images matched by the following report configuration parameters.\n"
 
 	paginatedQueryStartOffset = 0
 )
