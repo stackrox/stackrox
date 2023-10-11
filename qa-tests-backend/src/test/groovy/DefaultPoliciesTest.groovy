30d5cdd9b55598a1188a76ee7066be5dc686ff8b
pmurkute <pmurkute@redhat.com>
2023-10-11 17:26:49 +0000

    Enable DefaultPoliciesTest execution on P/Z

--- qa-tests-backend/src/test/groovy/DefaultPoliciesTest.groovy
+++ qa-tests-backend/src/test/groovy/DefaultPoliciesTest.groovy
@@ -47,6 +47,7 @@ import spock.lang.Stepwise
 import spock.lang.Tag
 import spock.lang.Unroll
 
+@Tag("PZ")
 // TODO(ROX-13738): Re-enable these tests in compatibility-test step
 @Stepwise // We need to verify all of the expected alerts are present before other tests.
 class DefaultPoliciesTest extends BaseSpecification {
