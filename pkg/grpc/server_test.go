50344fd6644182e7ee0d600fc79afd4f02344ac0
Mladen Todorovic <mtodor@redhat.com>
2023-10-12 11:27:56 +0200

    Add API rate limit for gRPC, HTTP and Stream requests

--- pkg/grpc/server_test.go
+++ pkg/grpc/server_test.go
@@ -1,17 +1,22 @@
 package grpc
 
 import (
+	"context"
 	"crypto/tls"
 	"fmt"
 	"net/http"
 	"testing"
 	"time"
 
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	v1 "github.com/stackrox/rox/generated/api/v1"
 	"github.com/stackrox/rox/pkg/concurrency"
 	"github.com/stackrox/rox/pkg/grpc/authz/allow"
+	"github.com/stackrox/rox/pkg/grpc/ratelimit"
 	"github.com/stackrox/rox/pkg/grpc/routes"
 	"github.com/stackrox/rox/pkg/mtls/verifier"
 	"github.com/stretchr/testify/suite"
+	"google.golang.org/grpc"
 )
 
 type APIServerSuite struct {
@@ -88,6 +93,201 @@ func (a *APIServerSuite) Test_CustomAPI() {
 		_, err := http.Get("https://localhost:8080/test")
 		a.Require().Error(err)
 		a.Require().False(endpointReached.IsDone())
+	})
+}
+
+func (a *APIServerSuite) Test_Server_RateLimit_HTTP_Integration() {
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+
+	a.Run("no limiter", func() {
+		cfg, endpointReached := configWithCustomRoute()
+
+		api := NewAPI(cfg)
+		a.Assert().NoError(api.Start().Wait())
+		defer func() {
+			api.Stop()
+		}()
+
+		for i := 0; i < 50; i++ {
+			resp, err := http.Get("https://localhost:8080/test")
+
+			a.Require().NoError(err)
+			a.Require().Equal(http.StatusOK, resp.StatusCode)
+		}
+		a.waitForSignal(endpointReached)
+	})
+
+	a.Run("default unlimited", func() {
+		cfg, endpointReached := configWithCustomRoute()
+		cfg.RateLimiter = ratelimit.NewRateLimiter(0)
+
+		api := NewAPI(cfg)
+		a.Assert().NoError(api.Start().Wait())
+		defer func() {
+			api.Stop()
+		}()
+
+		for i := 0; i < 50; i++ {
+			resp, err := http.Get("https://localhost:8080/test")
+
+			a.Require().NoError(err)
+			a.Require().Equal(http.StatusOK, resp.StatusCode)
+		}
+		a.waitForSignal(endpointReached)
+	})
+
+	a.Run("hit rate limit", func() {
+		cfg, endpointReached := configWithCustomRoute()
+		cfg.RateLimiter = ratelimit.NewRateLimiter(10)
+
+		api := NewAPI(cfg)
+		a.Assert().NoError(api.Start().Wait())
+		defer func() {
+			api.Stop()
+		}()
+
+		hitLimit := false
+		for i := 0; i < 30; i++ {
+			resp, err := http.Get("https://localhost:8080/test")
+			a.Require().NoError(err)
+
+			if i < 10 {
+				a.Require().Equal(http.StatusOK, resp.StatusCode)
+				continue
+			}
+
+			if resp.StatusCode != 200 {
+				a.Require().Equal(http.StatusTooManyRequests, resp.StatusCode)
+				hitLimit = true
+				break
+			}
+		}
+		a.Assert().True(hitLimit)
+
+		// Wait for rate limit to refill.
+		time.Sleep(2 * time.Second)
+
+		resp, err := http.Get("https://localhost:8080/test")
+		a.Assert().NoError(err)
+		a.Assert().Equal(http.StatusOK, resp.StatusCode)
+
+		a.waitForSignal(endpointReached)
+	})
+}
+
+// The pingServiceTestImpl is employed for the purpose of testing GRPC API invocations.
+// It is an implementation of the Ping service.
+type pingServiceTestImpl struct {
+	v1.UnimplementedPingServiceServer
+
+	RequestCount int
+}
+
+func (s *pingServiceTestImpl) RegisterServiceServer(grpcServer *grpc.Server) {
+	v1.RegisterPingServiceServer(grpcServer, s)
+}
+
+func (s *pingServiceTestImpl) RegisterServiceHandler(ctx context.Context, mux *runtime.ServeMux, conn *grpc.ClientConn) error {
+	return v1.RegisterPingServiceHandler(ctx, mux, conn)
+}
+
+func (s *pingServiceTestImpl) AuthFuncOverride(ctx context.Context, fullMethodName string) (context.Context, error) {
+	return ctx, allow.Anonymous().Authorized(ctx, fullMethodName)
+}
+
+func (s *pingServiceTestImpl) Ping(context.Context, *v1.Empty) (*v1.PongMessage, error) {
+	s.RequestCount++
+
+	result := &v1.PongMessage{
+		Status: "test",
+	}
+
+	return result, nil
+}
+
+func (a *APIServerSuite) Test_Server_RateLimit_GRPC_Integration() {
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+
+	a.Run("no limiter", func() {
+		cfg := defaultConf()
+
+		api := NewAPI(cfg)
+		pingService := &pingServiceTestImpl{}
+		api.Register(pingService)
+		a.Assert().NoError(api.Start().Wait())
+		defer func() { api.Stop() }()
+
+		numOfRequests := 50
+		for i := 0; i < numOfRequests; i++ {
+			resp, err := http.Get("https://localhost:8080/v1/ping")
+			a.Require().NoError(err)
+			a.Require().Equal(http.StatusOK, resp.StatusCode)
+		}
+		a.Assert().Equal(numOfRequests, pingService.RequestCount)
+	})
+
+	a.Run("default unlimited", func() {
+		cfg := defaultConf()
+		cfg.RateLimiter = ratelimit.NewRateLimiter(0)
+
+		api := NewAPI(cfg)
+		pingService := &pingServiceTestImpl{}
+		api.Register(pingService)
+		a.Assert().NoError(api.Start().Wait())
+		defer func() { api.Stop() }()
+
+		numOfRequests := 50
+		for i := 0; i < numOfRequests; i++ {
+			resp, err := http.Get("https://localhost:8080/v1/ping")
+			a.Require().NoError(err)
+			a.Require().Equal(http.StatusOK, resp.StatusCode)
+		}
+		a.Assert().Equal(numOfRequests, pingService.RequestCount)
+	})
+
+	a.Run("hit rate limit", func() {
+		cfg := defaultConf()
+		cfg.RateLimiter = ratelimit.NewRateLimiter(10)
+
+		api := NewAPI(cfg)
+		pingService := &pingServiceTestImpl{}
+		api.Register(pingService)
+		a.Assert().NoError(api.Start().Wait())
+		defer func() { api.Stop() }()
+
+		hitLimit := false
+		requestCount := 0
+		for requestCount < 30 {
+			requestCount++
+			resp, err := http.Get("https://localhost:8080/v1/ping")
+			a.Require().NoError(err)
+
+			if requestCount <= 10 {
+				a.Require().Equal(http.StatusOK, resp.StatusCode)
+				continue
+			}
+
+			if resp.StatusCode != 200 {
+				a.Require().Equal(http.StatusTooManyRequests, resp.StatusCode)
+				hitLimit = true
+
+				// Request was rejected because of rate limit.
+				requestCount--
+				break
+			}
+		}
+		a.Assert().True(hitLimit)
+		a.Assert().Equal(requestCount, pingService.RequestCount)
+
+		// Wait for rate limit to refill.
+		time.Sleep(2 * time.Second)
+
+		requestCount++
+		resp, err := http.Get("https://localhost:8080/v1/ping")
+		a.Assert().NoError(err)
+		a.Assert().Equal(http.StatusOK, resp.StatusCode)
+
+		a.Assert().Equal(requestCount, pingService.RequestCount)
 	})
 }
 
