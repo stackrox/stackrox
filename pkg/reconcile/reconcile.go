879b863cd63857e50ec75f4cc5a7a24a5baaca45
Matthias <Maddosaurus@users.noreply.github.com>
2023-10-12 16:30:02 +0200

    ROX-20019, ROX-19669: Reconcile deleted resources on Sensor (pt 1) (#7981)
    Co-authored-by: Piotr Rygielski <114479+vikin91@users.noreply.github.com>

---
+++ pkg/reconcile/reconcile.go
@@ -0,0 +1,8 @@
+package reconcile
+
+// Reconcilable is a Sensor object that supports reconciliation.
+// The Reconcile method is called after Sensor reconnects with Central and receives its state hashes.
+// Reconciliacion ensures that Sensor and Central have the same state.
+type Reconcilable interface {
+	ReconcileDelete(resType, resID string, resHash uint64) (string, error)
+}
