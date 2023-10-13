0970f5278c14d081aab977526352cf45fe2f6534
Dave Shrewsberry <dashrews@redhat.com>
2023-10-13 12:47:47 -0400

    added reference schema linter

---
+++ tools/roxvet/analyzers/migrationreferencedschema/analyzer.go
@@ -0,0 +1,41 @@
+package migrationreferencedschema
+
+import (
+	"go/ast"
+	"go/types"
+
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
+	"golang.org/x/tools/go/types/typeutil"
+)
+
+const (
+	doc = `check for usages of ResolveReferences`
+
+	resolveReferences = `walker/schema.ResolveReferences`
+)
+
+// Analyzer is the analyzer.
+var Analyzer = &analysis.Analyzer{
+	Name:     "migrationreferencedschemas",
+	Doc:      doc,
+	Requires: []*analysis.Analyzer{inspect.Analyzer},
+	Run:      run,
+}
+
+func run(pass *analysis.Pass) (interface{}, error) {
+	inspectResult := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+	nodeFilter := []ast.Node{(*ast.CallExpr)(nil)}
+	inspectResult.Preorder(nodeFilter, func(n ast.Node) {
+		call := n.(*ast.CallExpr)
+		fn, ok := typeutil.Callee(pass.TypesInfo, call).(*types.Func)
+		if ok && fn.FullName() == resolveReferences {
+			pass.Report(analysis.Diagnostic{
+				Pos:     n.Pos(),
+				Message: "Cannot resolve references in a migration as it walks the most recent proto.",
+			})
+		}
+	})
+	return nil, nil
+}
