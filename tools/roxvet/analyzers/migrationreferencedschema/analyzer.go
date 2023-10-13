80f35cf79990b1c54e6653c49be561e09fd282a5
Dave Shrewsberry <dashrews@redhat.com>
2023-10-13 13:50:00 -0400

    exclude most things

--- tools/roxvet/analyzers/migrationreferencedschema/analyzer.go
+++ tools/roxvet/analyzers/migrationreferencedschema/analyzer.go
@@ -1,8 +1,10 @@
 package migrationreferencedschema
 
 import (
+	"fmt"
 	"go/ast"
 	"go/types"
+	"strings"
 
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/inspect"
@@ -25,12 +27,33 @@ var Analyzer = &analysis.Analyzer{
 }
 
 func run(pass *analysis.Pass) (interface{}, error) {
+	root, valid, err := getRoot(pass.Pkg.Path())
+	if err != nil {
+		pass.Reportf(token.NoPos, "couldn't find valid root: %v", err)
+		return nil, nil
+	}
+	if !valid {
+		return nil, nil
+	}
+
+	// If we are not the migrator, simply move along
+	if !strings.HasPrefix(root, "migrator") {
+		return nil, nil
+	} else {
+		// This is allowed in frozen schema or the initial frozen schema as that code is isolaged
+		if strings.Contains(root, "postgreshelper") || strings.Contains(root, "frozenschema") {
+			return nil, nil
+		}
+	}
+
 	inspectResult := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
 	nodeFilter := []ast.Node{(*ast.CallExpr)(nil)}
 	inspectResult.Preorder(nodeFilter, func(n ast.Node) {
 		call := n.(*ast.CallExpr)
 		fn, ok := typeutil.Callee(pass.TypesInfo, call).(*types.Func)
 		if ok && fn.Name() == resolveReferences {
+			fmt.Printf("function full name is: %q\n", fn.FullName())
+			fmt.Printf("function package is: %q\n", fn.Pkg())
 			pass.Report(analysis.Diagnostic{
 				Pos:     n.Pos(),
 				Message: "Cannot resolve references in a migration as it walks the most recent proto.",
