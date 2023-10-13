123247a81115fff7d2af8caa4f2b1ef914a881ec
JoukoVirtanen <jouko@stackrox.com>
2023-10-13 10:01:01 -0700

    Adding pod in unit test

--- central/pod/datastore/datastore_impl_2_test.go
+++ central/pod/datastore/datastore_impl_2_test.go
@@ -17,9 +17,12 @@ import (
 	"github.com/stackrox/rox/pkg/postgres/pgtest"
 	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/pkg/fixtures/fixtureconsts"
+	"github.com/stackrox/rox/pkg/fixtures"
+	"github.com/stackrox/rox/pkg/process/filter"
 	"github.com/stackrox/rox/pkg/protoconv"
 	"github.com/stackrox/rox/pkg/sac"
 	"github.com/stackrox/rox/pkg/sac/resources"
+	"github.com/stackrox/rox/pkg/sac/testutils"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -36,8 +39,10 @@ type podDatastoreSuite struct {
 	plopStorage plopStore.Store
 
 	postgres *pgtest.TestPostgres
+	filter       filter.Filter
 
 	hasWriteCtx context.Context
+	ctx context.Context
 }
 
 func (s *podDatastoreSuite) SetupSuite() {
@@ -56,14 +61,23 @@ func (s *podDatastoreSuite) SetupSuite() {
         s.indicatorDataStore, _ = processIndicatorDataStore.New(
                 indicatorStorage, s.plopStorage, indicatorSearcher, nil)
 
-        s.plopDS = plopDataStore.New(s.plopStorage, s.indicatorDataStore)
+        //s.plopDS = plopDataStore.New(s.plopStorage, s.indicatorDataStore)
+	s.plopDS = nil
 
-	s.datastore = newDatastoreImpl(podStorage, podSearcher, s.indicatorDataStore, s.plopDS, nil)
+	s.filter = filter.NewFilter(5, 5, []int{5, 4, 3, 2, 1})
 
-	s.hasWriteCtx = sac.WithGlobalAccessScopeChecker(context.Background(),
-                sac.AllowFixedScopes(
-                        sac.AccessModeScopeKeys(storage.Access_READ_ACCESS, storage.Access_READ_WRITE_ACCESS),
-                        sac.ResourceScopeKeys(resources.DeploymentExtension)))
+	s.datastore = newDatastoreImpl(podStorage, podSearcher, s.indicatorDataStore, s.plopDS, s.filter)
+	podsSAC = sac.ForResource(resources.Deployment)
+
+	//s.hasWriteCtx = sac.WithGlobalAccessScopeChecker(context.Background(),
+        //        sac.AllowFixedScopes(
+        //                sac.AccessModeScopeKeys(storage.Access_READ_ACCESS, storage.Access_READ_WRITE_ACCESS),
+	//	resources.Deployment)
+
+	testContexts := testutils.GetNamespaceScopedTestContexts(context.Background(), s.T(),
+		resources.Deployment)
+
+	s.ctx = testContexts[testutils.UnrestrictedReadWriteCtx]
 }
 
 func (s *podDatastoreSuite) TearDownSuite() {
@@ -104,14 +118,19 @@ var (
                 },
                 DeploymentId: fixtureconsts.Deployment1,
         }
+
 )
 
 // Add plops, process indicators, pods. Delete one of the pods.
 // Check that the correct pod, process indicators, and plops are deleted.
 func (s *podDatastoreSuite) TestRemovePod() {
-	plopObjects := []*storage.ProcessListeningOnPortFromSensor{&openPlopObject}
-	s.NoError(s.datastore.plops.AddProcessListeningOnPort(
-		s.hasWriteCtx, plopObjects...))
+	//plopObjects := []*storage.ProcessListeningOnPortFromSensor{&openPlopObject}
+	//s.NoError(s.datastore.plops.AddProcessListeningOnPort(
+	//	s.hasWriteCtx, plopObjects...))
+
+
+	s.NoError(s.datastore.UpsertPod(s.ctx, fixtures.GetPod1()))
+//func (ds *datastoreImpl) UpsertPod(ctx context.Context, pod *storage.Pod) error {
 
 
 }
--- pkg/fixtures/pod.go
+++ pkg/fixtures/pod.go
@@ -3,12 +3,121 @@ package fixtures
 import (
 	"github.com/gogo/protobuf/types"
 	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/fixtures/fixtureconsts"
 )
 
 // GetPod returns a mock Pod
 func GetPod() *storage.Pod {
 	return &storage.Pod{
 		Id:           "nginx-7db9fccd9b-92hfs",
+		DeploymentId: GetDeployment().GetId(),
+		ClusterId:    "prod cluster",
+		Namespace:    "stackrox",
+		Started: &types.Timestamp{
+			Seconds: 0,
+		},
+		LiveInstances: []*storage.ContainerInstance{
+			{
+				InstanceId: &storage.ContainerInstanceID{
+					Id: "containerid",
+				},
+				ContainerName: "containername",
+				ImageDigest:   "sha256:035e674c761c8a9bffe25a4f7c552e617869d1c1bfb2f84074c3ee63f3018da4",
+				Started: &types.Timestamp{
+					Seconds: 2,
+				},
+			},
+			{
+				InstanceId: &storage.ContainerInstanceID{
+					Id: "othercontainerid",
+				},
+				ContainerName: "othercontainername",
+				ImageDigest:   "sha256:035e674c761c8a9bffe25a4f7c552e617869d1c1bfb2f84074c3ee63f3018da4",
+				Started: &types.Timestamp{
+					Seconds: 3,
+				},
+			},
+		},
+		TerminatedInstances: []*storage.Pod_ContainerInstanceList{
+			{
+				Instances: []*storage.ContainerInstance{
+					{
+						InstanceId: &storage.ContainerInstanceID{
+							Id: "containeridfirst",
+						},
+						ContainerName: "containername",
+						ImageDigest:   "sha256:035e674c761c8a9bffe25a4f7c552e617869d1c1bfb2f84074c3ee63f3018da4",
+						Started: &types.Timestamp{
+							Seconds: 0,
+						},
+						Finished: &types.Timestamp{
+							Seconds: 1,
+						},
+					},
+				},
+			},
+			{
+				Instances: []*storage.ContainerInstance{
+					{
+						InstanceId: &storage.ContainerInstanceID{
+							Id: "othercontainerid",
+						},
+						ContainerName: "othercontainername",
+						ImageDigest:   "sha256:035e674c761c8a9bffe25a4f7c552e617869d1c1bfb2f84074c3ee63f3018da4",
+						Started: &types.Timestamp{
+							Seconds: 1,
+						},
+						Finished: &types.Timestamp{
+							Seconds: 2,
+						},
+					},
+				},
+			},
+			{
+				Instances: []*storage.ContainerInstance{
+					{
+						InstanceId: &storage.ContainerInstanceID{
+							Id: "containeridinitfirst",
+						},
+						ContainerName: "containerinitname",
+						ImageDigest:   "sha256:035e674c761c8a9bffe25a4f7c552e617869d1c1bfb2f84074c3ee63f3018da4",
+						Started: &types.Timestamp{
+							Seconds: 0,
+						},
+						Finished: &types.Timestamp{
+							Seconds: 1,
+						},
+						ExitCode:          137,
+						TerminationReason: "Error",
+					},
+					{
+						InstanceId: &storage.ContainerInstanceID{
+							Id: "containeridinitsecond",
+						},
+						ContainerName: "containerinitname",
+						ImageDigest:   "sha256:035e674c761c8a9bffe25a4f7c552e617869d1c1bfb2f84074c3ee63f3018da4",
+						Started: &types.Timestamp{
+							Seconds: 1,
+							Nanos:   200,
+						},
+						Finished: &types.Timestamp{
+							Seconds: 1,
+							Nanos:   800,
+						},
+						ExitCode:          0,
+						TerminationReason: "Completed",
+					},
+				},
+			},
+		},
+	}
+}
+
+// GetPod returns a mock Pod
+func GetPod1() *storage.Pod {
+	return &storage.Pod{
+		Id:           fixtureconsts.PodUID1,
+		Name:         fixtureconsts.PodName1,
 		DeploymentId: GetDeployment().GetId(),
 		ClusterId:    "prod cluster",
 		Namespace:    "stackrox",
