79dbdc21f50e2a982fc23fb2175c36e0c79663e1
JoukoVirtanen <jouko@stackrox.com>
2023-10-15 11:30:04 -0700

    Trying to add a unit test for RemovePod

--- central/pod/datastore/datastore_impl_2_test.go
+++ central/pod/datastore/datastore_impl_2_test.go
@@ -5,7 +5,6 @@ package datastore
 import (
 	"context"
 	"testing"
-	"time"
 
 	podSearch "github.com/stackrox/rox/central/pod/datastore/internal/search"
 	podStore "github.com/stackrox/rox/central/pod/store/postgres"
@@ -13,13 +12,14 @@ import (
 	processIndicatorSearch "github.com/stackrox/rox/central/processindicator/search"
 	processIndicatorStorage "github.com/stackrox/rox/central/processindicator/store/postgres"
 	plopDataStore "github.com/stackrox/rox/central/processlisteningonport/datastore"
-	plopStore "github.com/stackrox/rox/central/processlisteningonport/store/postgres"
+	plopStore "github.com/stackrox/rox/central/processlisteningonport/store"
+	plopPostgresStore "github.com/stackrox/rox/central/processlisteningonport/store/postgres"
 	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/pkg/fixtures"
 	"github.com/stackrox/rox/pkg/fixtures/fixtureconsts"
 	"github.com/stackrox/rox/pkg/postgres/pgtest"
 	"github.com/stackrox/rox/pkg/process/filter"
-	"github.com/stackrox/rox/pkg/protoconv"
+	"github.com/stackrox/rox/pkg/sac"
 	"github.com/stackrox/rox/pkg/sac/resources"
 	"github.com/stackrox/rox/pkg/sac/testutils"
 	"github.com/stretchr/testify/suite"
@@ -40,8 +40,9 @@ type PodDatastoreSuite struct {
 	postgres *pgtest.TestPostgres
 	filter   filter.Filter
 
-	hasWriteCtx context.Context
-	ctx         context.Context
+	hasWriteCtx         context.Context
+	ctx                 context.Context
+	processIndicatorCtx context.Context
 }
 
 func (s *PodDatastoreSuite) SetupSuite() {
@@ -51,7 +52,7 @@ func (s *PodDatastoreSuite) SetupSuite() {
 	podIndexer := processIndicatorStorage.NewIndexer(s.postgres.DB)
 	podSearcher := podSearch.New(podStorage, podIndexer)
 
-	s.plopStorage = plopStore.NewFullStore(s.postgres.DB)
+	s.plopStorage = plopPostgresStore.NewFullStore(s.postgres.DB)
 
 	indicatorStorage := processIndicatorStorage.New(s.postgres.DB)
 	indicatorIndexer := processIndicatorStorage.NewIndexer(s.postgres.DB)
@@ -60,21 +61,27 @@ func (s *PodDatastoreSuite) SetupSuite() {
 	s.indicatorDataStore, _ = processIndicatorDataStore.New(
 		indicatorStorage, s.plopStorage, indicatorSearcher, nil)
 
-	s.plopDS = nil
+	//	s.plopDS = nil
+	s.plopDS = plopDataStore.New(s.plopStorage, s.indicatorDataStore)
 
 	s.filter = filter.NewFilter(5, 5, []int{5, 4, 3, 2, 1})
 
 	s.datastore = newDatastoreImpl(podStorage, podSearcher, s.indicatorDataStore, s.plopDS, s.filter)
 
 	// s.hasWriteCtx = sac.WithGlobalAccessScopeChecker(context.Background(),
-	//        sac.AllowFixedScopes(
-	//                sac.AccessModeScopeKeys(storage.Access_READ_ACCESS, storage.Access_READ_WRITE_ACCESS),
+	//       sac.AllowFixedScopes(
+	//               sac.AccessModeScopeKeys(storage.Access_READ_ACCESS, storage.Access_READ_WRITE_ACCESS),
 	//	resources.Deployment)
 
 	testContexts := testutils.GetNamespaceScopedTestContexts(context.Background(), s.T(),
 		resources.Deployment)
 
 	s.ctx = testContexts[testutils.UnrestrictedReadWriteCtx]
+
+	s.processIndicatorCtx = sac.WithGlobalAccessScopeChecker(context.Background(),
+		sac.AllowFixedScopes(
+			sac.AccessModeScopeKeys(storage.Access_READ_ACCESS, storage.Access_READ_WRITE_ACCESS),
+			sac.ResourceScopeKeys(resources.DeploymentExtension)))
 }
 
 func (s *PodDatastoreSuite) TearDownSuite() {
@@ -87,43 +94,80 @@ func (s *PodDatastoreSuite) SetupTest() {
 func (s *PodDatastoreSuite) TearDownTest() {
 }
 
-var (
-	openPlopObject = storage.ProcessListeningOnPortFromSensor{
-		Port:           1234,
-		Protocol:       storage.L4Protocol_L4_PROTOCOL_TCP,
-		CloseTimestamp: nil,
-		Process: &storage.ProcessIndicatorUniqueKey{
-			PodId:               fixtureconsts.PodUID1,
-			ContainerName:       "test_container1",
-			ProcessName:         "test_process1",
-			ProcessArgs:         "test_arguments1",
-			ProcessExecFilePath: "test_path1",
-		},
-		DeploymentId: fixtureconsts.Deployment1,
-	}
+func (s *PodDatastoreSuite) getProcessIndicatorsFromDB() []*storage.ProcessIndicator {
+	indicatorsFromDB := []*storage.ProcessIndicator{}
+	err := s.indicatorDataStore.WalkAll(s.processIndicatorCtx,
+		func(processIndicator *storage.ProcessIndicator) error {
+			indicatorsFromDB = append(indicatorsFromDB, processIndicator)
+			return nil
+		})
 
-	closedPlopObject = storage.ProcessListeningOnPortFromSensor{
-		Port:           1234,
-		Protocol:       storage.L4Protocol_L4_PROTOCOL_TCP,
-		CloseTimestamp: protoconv.ConvertTimeToTimestamp(time.Now()),
-		Process: &storage.ProcessIndicatorUniqueKey{
-			PodId:               fixtureconsts.PodUID1,
-			ContainerName:       "test_container1",
-			ProcessName:         "test_process1",
-			ProcessArgs:         "test_arguments1",
-			ProcessExecFilePath: "test_path1",
-		},
-		DeploymentId: fixtureconsts.Deployment1,
-	}
-)
+	s.NoError(err)
+
+	return indicatorsFromDB
+}
 
 // Add plops, process indicators, pods. Delete one of the pods.
 // Check that the correct pod, process indicators, and plops are deleted.
-func (s *PodDatastoreSuite) TestRemovePod() {
-	// plopObjects := []*storage.ProcessListeningOnPortFromSensor{&openPlopObject}
-	//s.NoError(s.datastore.plops.AddProcessListeningOnPort(
-	//	s.hasWriteCtx, plopObjects...))
+func (s PodDatastoreSuite) TestRemovePod() {
+	testNamespace := "test_namespace"
+
+	openPlopObject1 := fixtures.GetOpenPlopObject1()
+	openPlopObject2 := fixtures.GetOpenPlopObject2()
+	openPlopObject3 := fixtures.GetOpenPlopObject3()
+
+	plopObjects := []*storage.ProcessListeningOnPortFromSensor{&openPlopObject1, &openPlopObject2, &openPlopObject3}
+	s.NoError(s.datastore.plops.AddProcessListeningOnPort(
+		s.processIndicatorCtx, plopObjects...))
+
+	// Fetch inserted PLOP
+	newPlops, err := s.datastore.plops.GetProcessListeningOnPort(
+		s.processIndicatorCtx, fixtureconsts.Deployment1)
+	s.NoError(err)
+
+	s.Len(newPlops, 3)
 
 	s.NoError(s.datastore.UpsertPod(s.ctx, fixtures.GetPod1()))
+
+	indicator1 := fixtures.GetProcessIndicator1()
+	indicator2 := fixtures.GetProcessIndicator2()
+	indicator3 := fixtures.GetProcessIndicator3()
+	indicators := []*storage.ProcessIndicator{indicator1, indicator2, indicator3}
+
+	s.NoError(s.indicatorDataStore.AddProcessIndicators(s.processIndicatorCtx, indicators...))
+
+	s.NoError(s.datastore.RemovePod(s.processIndicatorCtx, fixtureconsts.PodUID1))
+
+	// Fetch inserted PLOP back after deleting pod
+	newPlops, err = s.datastore.plops.GetProcessListeningOnPort(
+		s.processIndicatorCtx, fixtureconsts.Deployment1)
+	s.NoError(err)
+
+	// Verify that the correct listening endpoints have been deleted
+	s.Len(newPlops, 1)
+
+	s.Equal(*newPlops[0], storage.ProcessListeningOnPort{
+		ContainerName: "test_container1",
+		PodId:         fixtureconsts.PodName2,
+		PodUid:        fixtureconsts.PodUID2,
+		DeploymentId:  fixtureconsts.Deployment1,
+		ClusterId:     fixtureconsts.Cluster1,
+		Namespace:     testNamespace,
+		Endpoint: &storage.ProcessListeningOnPort_Endpoint{
+			Port:     80,
+			Protocol: storage.L4Protocol_L4_PROTOCOL_TCP,
+		},
+		Signal: &storage.ProcessSignal{
+			Name:         "test_process2",
+			Args:         "test_arguments2",
+			ExecFilePath: "test_path2",
+		},
+	})
+
+	// Verify that the correct process indicators have been deleted
+	indicatorsFromDB := s.getProcessIndicatorsFromDB()
+	s.Len(indicatorsFromDB, 1)
+
+	s.Equal(*indicatorsFromDB[0], indicator1)
 
 }
---
+++ pkg/fixtures/process_listening_on_port.go
@@ -0,0 +1,81 @@
+package fixtures
+
+import (
+	"time"
+
+	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/fixtures/fixtureconsts"
+	"github.com/stackrox/rox/pkg/protoconv"
+)
+
+// GetOpenPlopObject1 Return an open plop object
+func GetOpenPlopObject1() storage.ProcessListeningOnPortFromSensor {
+	return storage.ProcessListeningOnPortFromSensor{
+		Port:           1234,
+		Protocol:       storage.L4Protocol_L4_PROTOCOL_TCP,
+		CloseTimestamp: nil,
+		Process: &storage.ProcessIndicatorUniqueKey{
+			PodId:               fixtureconsts.PodName1,
+			ContainerName:       "test_container1",
+			ProcessName:         "test_process1",
+			ProcessArgs:         "test_arguments1",
+			ProcessExecFilePath: "test_path1",
+		},
+		DeploymentId: fixtureconsts.Deployment1,
+		PodUid:       fixtureconsts.PodUID1,
+	}
+}
+
+// GetClosePlopObject1 Return an open plop object
+func GetClosePlopObject1() storage.ProcessListeningOnPortFromSensor {
+	return storage.ProcessListeningOnPortFromSensor{
+		Port:           1234,
+		Protocol:       storage.L4Protocol_L4_PROTOCOL_TCP,
+		CloseTimestamp: protoconv.ConvertTimeToTimestamp(time.Now()),
+		Process: &storage.ProcessIndicatorUniqueKey{
+			PodId:               fixtureconsts.PodName1,
+			ContainerName:       "test_container1",
+			ProcessName:         "test_process1",
+			ProcessArgs:         "test_arguments1",
+			ProcessExecFilePath: "test_path1",
+		},
+		DeploymentId: fixtureconsts.Deployment1,
+		PodUid:       fixtureconsts.PodUID1,
+	}
+}
+
+// GetOpenPlopObject2 Return an open plop object
+func GetOpenPlopObject2() storage.ProcessListeningOnPortFromSensor {
+	return storage.ProcessListeningOnPortFromSensor{
+		Port:           80,
+		Protocol:       storage.L4Protocol_L4_PROTOCOL_TCP,
+		CloseTimestamp: nil,
+		Process: &storage.ProcessIndicatorUniqueKey{
+			PodId:               fixtureconsts.PodName1,
+			ContainerName:       "test_container1",
+			ProcessName:         "test_process2",
+			ProcessArgs:         "test_arguments2",
+			ProcessExecFilePath: "test_path2",
+		},
+		DeploymentId: fixtureconsts.Deployment1,
+		PodUid:       fixtureconsts.PodUID1,
+	}
+}
+
+// GetOpenPlopObject3 Return an open plop object
+func GetOpenPlopObject3() storage.ProcessListeningOnPortFromSensor {
+	return storage.ProcessListeningOnPortFromSensor{
+		Port:           80,
+		Protocol:       storage.L4Protocol_L4_PROTOCOL_TCP,
+		CloseTimestamp: nil,
+		Process: &storage.ProcessIndicatorUniqueKey{
+			PodId:               fixtureconsts.PodName2,
+			ContainerName:       "test_container1",
+			ProcessName:         "test_process2",
+			ProcessArgs:         "test_arguments2",
+			ProcessExecFilePath: "test_path2",
+		},
+		DeploymentId: fixtureconsts.Deployment1,
+		PodUid:       fixtureconsts.PodUID2,
+	}
+}
