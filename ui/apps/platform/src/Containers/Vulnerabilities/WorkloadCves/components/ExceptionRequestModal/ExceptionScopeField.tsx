a3d23200f6e419cf7b1514a86a725110eb4dc38b
David Vail <dcvail17@gmail.com>
2023-10-17 10:57:56 -0400

    ROX-19358: Add formik validation and state tracking to deferral form (#8161)

--- ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/components/ExceptionRequestModal/ExceptionScopeField.tsx
+++ ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/components/ExceptionRequestModal/ExceptionScopeField.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import { FormGroup, FormGroupProps, Radio } from '@patternfly/react-core';
 
-import { ScopeContext } from './utils';
+import { useFormik } from 'formik';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { DeferralValues, ScopeContext } from './utils';
+
+const ALL = '.*';
 
 export type ExceptionScopeFieldProps = {
     fieldId: FormGroupProps['fieldId'];
     label: FormGroupProps['label'];
     scopeContext: ScopeContext;
+    formik: ReturnType<typeof useFormik<DeferralValues>>;
 };
 
-function ExceptionScopeField({ fieldId, label, scopeContext }: ExceptionScopeFieldProps) {
+function ExceptionScopeField({ fieldId, label, scopeContext, formik }: ExceptionScopeFieldProps) {
+    const { values } = formik;
+
     return (
         <FormGroup fieldId={fieldId} label={label} isRequired>
             {scopeContext === 'GLOBAL' && (
@@ -17,7 +24,12 @@ function ExceptionScopeField({ fieldId, label, scopeContext }: ExceptionScopeFie
                     id="scope-global"
                     name="scope-global"
                     isDisabled
-                    isChecked
+                    isChecked={
+                        values.scope.imageScope.registry === ALL &&
+                        values.scope.imageScope.remote === ALL &&
+                        values.scope.imageScope.tag === ALL
+                    }
+                    onChange={() => {}}
                     label="Selected CVEs across all images and deployments"
                 />
             )}
@@ -26,15 +38,22 @@ function ExceptionScopeField({ fieldId, label, scopeContext }: ExceptionScopeFie
                     <Radio
                         id="scope-single-image"
                         name="scope-single-image"
-                        isChecked={false}
+                        isChecked={
+                            values.scope.imageScope.registry === ALL &&
+                            values.scope.imageScope.remote === scopeContext.image.name &&
+                            values.scope.imageScope.tag === ALL
+                        }
                         onChange={() => {}}
                         label={`All tags within ${scopeContext.image.name}}`}
                     />
                     <Radio
                         id="scope-single-image-single-tag"
                         name="scope-single-image-single-tag"
-                        isChecked={false}
-                        onChange={() => {}}
+                        isChecked={
+                            values.scope.imageScope.registry === ALL &&
+                            values.scope.imageScope.remote === scopeContext.image.name &&
+                            values.scope.imageScope.tag === scopeContext.image.tag
+                        }
                         label={`Only ${scopeContext.image.name}:${scopeContext.image.tag}`}
                     />
                 </>
