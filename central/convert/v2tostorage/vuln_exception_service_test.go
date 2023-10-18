1e0ed84a3e99644d21d7256c50403c890c4d75db
Mandar Darwatkar <mandar@stackrox.com>
2023-10-18 13:44:17 -0700

    ROX-20246: Create vuln exception v2 service (#8219)

--- central/convert/v2tostorage/vuln_exception_service_test.go
+++ central/convert/v2tostorage/vuln_exception_service_test.go
@@ -1,10 +1,16 @@
-package storagetov2
+package v2tostorage
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stackrox/rox/central/convert/testutils"
+	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/grpc/authn"
+	mockIdentity "github.com/stackrox/rox/pkg/grpc/authn/mocks"
+	"github.com/stackrox/rox/pkg/sac"
 	"github.com/stretchr/testify/assert"
+	"go.uber.org/mock/gomock"
 )
 
 func TestVulnerabilityRequest(t *testing.T) {
@@ -36,5 +42,63 @@ func TestVulnerabilityRequest(t *testing.T) {
 		t,
 		testutils.GetTestVulnRequestWithUpdate(t),
 		VulnerabilityRequest(testutils.GetTestVulnExceptionWithUpdate(t)),
+	)
+
+	id := mockIdentity.NewMockIdentity(gomock.NewController(t))
+	id.EXPECT().UID().Return("userID").AnyTimes()
+	id.EXPECT().FullName().Return("userName").AnyTimes()
+	id.EXPECT().FriendlyName().Return("userName").AnyTimes()
+	ctx := authn.ContextWithIdentity(sac.WithAllAccess(context.Background()), id, t)
+
+	assert.EqualValues(
+		t,
+		func() *storage.VulnerabilityRequest {
+			req := testutils.GetTestVulnDeferralRequestFull(t)
+			// Reset the fields that are nondeterministic.
+			req.Id = ""
+			req.Name = ""
+			req.Approvers = nil
+			req.Comments[0].Id = ""
+			req.Comments[0].CreatedAt = nil
+			return req
+		}(),
+		func() *storage.VulnerabilityRequest {
+			converted := DeferVulnerabilityRequest(ctx,
+				testutils.GetTestCreateDeferVulnExceptionRequest(t))
+			assert.NotNil(t, converted.GetCreatedAt())
+			assert.NotNil(t, converted.GetLastUpdated())
+			// Reset the fields that are nondeterministic.
+			converted.CreatedAt = nil
+			converted.LastUpdated = nil
+			converted.Comments[0].Id = ""
+			converted.Comments[0].CreatedAt = nil
+			return converted
+		}(),
+	)
+
+	assert.EqualValues(
+		t,
+		func() *storage.VulnerabilityRequest {
+			req := testutils.GetTestVulnFPRequestFull(t)
+			// Reset the fields that are nondeterministic.
+			req.Id = ""
+			req.Name = ""
+			req.Approvers = nil
+			req.Comments[0].Id = ""
+			req.Comments[0].CreatedAt = nil
+			return req
+		}(),
+		func() *storage.VulnerabilityRequest {
+			converted := FalsePositiveVulnerabilityRequest(ctx,
+				testutils.GetTestCreateFPVulnExceptionRequest(t))
+			assert.NotNil(t, converted.GetCreatedAt())
+			assert.NotNil(t, converted.GetLastUpdated())
+			// Reset the fields that are nondeterministic.
+			converted.CreatedAt = nil
+			converted.LastUpdated = nil
+			converted.Comments[0].Id = ""
+			converted.Comments[0].CreatedAt = nil
+			return converted
+		}(),
 	)
 }
