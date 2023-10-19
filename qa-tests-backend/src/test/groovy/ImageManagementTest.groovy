b5fb34edd90e65bf4d6c00cfc303fb1bc1461f04
Alston Dias <adias@redhat.com>
2023-10-19 06:56:52 +0000

    reverted changes that are in #8212

--- qa-tests-backend/src/test/groovy/ImageManagementTest.groovy
+++ qa-tests-backend/src/test/groovy/ImageManagementTest.groovy
@@ -13,6 +13,7 @@ import services.PolicyService
 
 import spock.lang.Tag
 import spock.lang.Unroll
+import spock.lang.IgnoreIf
 import util.Env
 
 @Tag("PZDebug")
@@ -322,6 +323,7 @@ class ImageManagementTest extends BaseSpecification {
 
     @Tag("BAT")
     @Tag("Integration")
+    @IgnoreIf({ Env.REMOTE_CLUSTER_ARCH == "ppc64le" || Env.REMOTE_CLUSTER_ARCH == "s390x" })
     def "Verify CI/CD Integration Endpoint with notifications"() {
         when:
         "Clone and scope the policy for test"
@@ -346,7 +348,7 @@ class ImageManagementTest extends BaseSpecification {
 
         and:
         "Request Image Scan with sendNotifications"
-        def scanResults = Services.requestBuildImageScan("quay.io", "rhacs-eng/qa-multi-arch-busybox", "latest", true)
+        def scanResults = Services.requestBuildImageScan("quay.io", "quay/busybox", "latest", true)
 
         then:
         "verify violation matches expected violation status and notification sent"
@@ -359,9 +361,9 @@ class ImageManagementTest extends BaseSpecification {
             assert alert["policy"]["name"] == clone.name
             assert alert["image"] != null
             assert alert["deployment"] == null
-            assert alert["image"]["name"]["fullName"] == "quay.io/rhacs-eng/qa-multi-arch-busybox:latest"
+            assert alert["image"]["name"]["fullName"] == "quay.io/quay/busybox:latest"
             assert alert["image"]["name"]["registry"] == "quay.io"
-            assert alert["image"]["name"]["remote"] == "rhacs-eng/qa-multi-arch-busybox"
+            assert alert["image"]["name"]["remote"] == "quay/busybox"
             assert alert["image"]["name"]["tag"] == "latest"
         }
 
