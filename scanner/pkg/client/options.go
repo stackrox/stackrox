1f932c83d41839862518bc1d029da39aea92a361
J. Victor Martins <jvdm@sdf.org>
2023-10-20 17:53:41 -0700

    feat(scanner): Add a client-side interface for Scanner (#8019)

---
+++ scanner/pkg/client/options.go
@@ -0,0 +1,63 @@
+package client
+
+import (
+	"github.com/stackrox/rox/pkg/mtls"
+)
+
+var defaultOptions = options{
+	mtlsSubject:   mtls.ScannerSubject,
+	address:       ":8443",
+	serverName:    "scanner-v4.stackrox",
+	withTLSVerify: true,
+}
+
+// Option configures the options to create a scanner client.
+type Option func(*options)
+
+type options struct {
+	mtlsSubject   mtls.Subject
+	address       string
+	serverName    string
+	withTLSVerify bool
+}
+
+// GetCA loads and instantiates a Stackrox CA from the options specified.
+func (o *options) GetCA() (mtls.CA, error) {
+	// Options only support the default CA for now.
+	return mtls.LoadDefaultCA()
+}
+
+// WithSubject specifies the mTLS subject to use.
+func WithSubject(subject mtls.Subject) Option {
+	return func(o *options) {
+		o.mtlsSubject = subject
+	}
+}
+
+// WithServerName specifies the mTLS server name used to verify the server's certificate.
+func WithServerName(serverName string) Option {
+	return func(o *options) {
+		o.serverName = serverName
+	}
+}
+
+// WithoutTLSVerify disables TLS verification, preventing the reading and usage
+// of client certificates (mTLS).
+func WithoutTLSVerify(o *options) {
+	o.withTLSVerify = false
+}
+
+// WithAddress specifies the gRPC address to connect.
+func WithAddress(address string) Option {
+	return func(o *options) {
+		o.address = address
+	}
+}
+
+func makeOptions(opts ...Option) options {
+	o := defaultOptions
+	for _, opt := range opts {
+		opt(&o)
+	}
+	return o
+}
