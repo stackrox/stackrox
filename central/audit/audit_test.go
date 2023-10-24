812ba5eccc8784632511e2af517ed8b2733dc882
dhaus67 <dhaus@redhat.com>
2023-10-25 00:48:59 +0200

    ROX-20288: Audit log without permissions (#8242)

--- central/audit/audit_test.go
+++ central/audit/audit_test.go
@@ -6,9 +6,13 @@ import (
 
 	"github.com/pkg/errors"
 	v1 "github.com/stackrox/rox/generated/api/v1"
+	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/grpc/authn"
+	identityMocks "github.com/stackrox/rox/pkg/grpc/authn/mocks"
 	"github.com/stackrox/rox/pkg/grpc/authz/interceptor"
 	notifierMocks "github.com/stackrox/rox/pkg/notifier/mocks"
 	"github.com/stackrox/rox/pkg/sac"
+	"github.com/stackrox/rox/pkg/sac/resources"
 	"github.com/stackrox/rox/pkg/sync"
 	"github.com/stretchr/testify/suite"
 	"go.uber.org/mock/gomock"
@@ -18,17 +22,13 @@ import (
 type AuditLogTestSuite struct {
 	suite.Suite
 
-	mockCtrl     *gomock.Controller
 	notifierMock *notifierMocks.MockProcessor
+	identityMock *identityMocks.MockIdentity
 }
 
 func (suite *AuditLogTestSuite) SetupTest() {
-	suite.mockCtrl = gomock.NewController(suite.T())
-	suite.notifierMock = notifierMocks.NewMockProcessor(suite.mockCtrl)
-}
-
-func (suite *AuditLogTestSuite) TearDownTest() {
-	suite.mockCtrl.Finish()
+	suite.notifierMock = notifierMocks.NewMockProcessor(gomock.NewController(suite.T()))
+	suite.identityMock = identityMocks.NewMockIdentity(gomock.NewController(suite.T()))
 }
 
 func TestAuditLog(t *testing.T) {
@@ -79,6 +79,54 @@ func (suite *AuditLogTestSuite) TestCalculateAuditStatus() {
 	_, _ = interceptorFunc(ctxAuthorised, nil, serverInfo, handler(nil))
 	wg.Wait()
 	suite.Equal(v1.Audit_REQUEST_SUCCEEDED, auditMessage.Status)
+}
+
+func (suite *AuditLogTestSuite) TestPermissionsRemoval() {
+	userInfo := &storage.UserInfo{
+		Username:     "sample-user",
+		FriendlyName: "friendly-sample-user",
+		Permissions: &storage.UserInfo_ResourceToAccess{
+			ResourceToAccess: map[string]storage.Access{
+				resources.Administration.String(): storage.Access_READ_ACCESS,
+				resources.Integration.String():    storage.Access_READ_WRITE_ACCESS,
+				resources.Access.String():         storage.Access_NO_ACCESS,
+			},
+		},
+		Roles: []*storage.UserInfo_Role{
+			{
+				Name: "sample-role",
+				ResourceToAccess: map[string]storage.Access{
+					resources.Administration.String(): storage.Access_READ_ACCESS,
+				},
+			},
+			{
+				Name: "yet-another-sample-role",
+				ResourceToAccess: map[string]storage.Access{
+					resources.Integration.String(): storage.Access_READ_WRITE_ACCESS,
+				},
+			},
+		},
+	}
+	suite.identityMock.EXPECT().Service().Return(nil).AnyTimes()
+	suite.identityMock.EXPECT().User().Return(userInfo).AnyTimes()
+
+	ctxWithMockIdentity := authn.ContextWithIdentity(context.Background(), suite.identityMock,
+		suite.T())
+
+	a := &audit{}
+	withPermissions := a.newAuditMessage(ctxWithMockIdentity, "this is a test", "/v1./Test",
+		interceptor.AuthStatus{Error: nil}, nil)
+
+	suite.Equal(userInfo, withPermissions.GetUser())
+
+	a = &audit{withoutPermissions: true}
+	withoutPermissions := a.newAuditMessage(ctxWithMockIdentity, "this is a test", "/v1./Test",
+		interceptor.AuthStatus{Error: nil}, nil)
+	suite.NotEqual(userInfo, withoutPermissions.GetUser())
+	suite.Empty(withoutPermissions.GetUser().GetPermissions())
+	for _, userRole := range withoutPermissions.GetUser().GetRoles() {
+		suite.Empty(userRole.GetResourceToAccess())
+	}
 }
 
 func handler(err error) func(ctx context.Context, req interface{}) (interface{}, error) {
