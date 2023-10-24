109db0484ab16e97dce1174fee72ad778d9fcd89
Yann Brillouet <ybrillou@redhat.com>
2023-10-24 13:53:35 +0200

    Reduce LRU cache interface to used functions

--- pkg/cache/lru/lru.go
+++ pkg/cache/lru/lru.go
@@ -16,22 +16,27 @@ type LRU[K comparable, V any] interface {
 	Remove(key K) bool
 	// Get looks up a key's value from the cache.
 	Get(key K) (value V, ok bool)
-	// Contains checks if a key is in the cache, without updating the recent-ness
-	// or deleting it for being stale.
-	Contains(key K) (ok bool)
 	// Peek returns the key value (or undefined if not found) without updating
 	// the "recently used"-ness of the key.
 	Peek(key K) (value V, ok bool)
-	// RemoveOldest removes the oldest item from the cache.
-	RemoveOldest() (key K, value V, ok bool)
-	// GetOldest returns the oldest entry.
-	GetOldest() (key K, value V, ok bool)
 	// Keys returns a slice of the keys in the cache, from oldest to newest.
 	Keys() []K
-	// Values returns a slice of the values in the cache, from oldest to newest.
-	Values() []V
-	// Len returns the number of items in the cache.
-	Len() int
-	// Resize changes the cache size. Size of 0 means unlimited.
-	Resize(size int) (evicted int)
+	/*
+		// Here are the interface functions exposed by hashicorp golang-lru types,
+		// which are not used in our software (yet).
+
+		// Contains checks if a key is in the cache, without updating the recent-ness
+		// or deleting it for being stale.
+		Contains(key K) (ok bool)
+		// RemoveOldest removes the oldest item from the cache.
+		RemoveOldest() (key K, value V, ok bool)
+		// GetOldest returns the oldest entry.
+		GetOldest() (key K, value V, ok bool)
+		// Values returns a slice of the values in the cache, from oldest to newest.
+		Values() []V
+		// Len returns the number of items in the cache.
+		Len() int
+		// Resize changes the cache size. Size of 0 means unlimited.
+		Resize(size int) (evicted int)
+	*/
 }
