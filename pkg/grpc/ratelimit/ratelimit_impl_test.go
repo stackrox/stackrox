2fd1d8fd004685031c4ecdf034a997830d5d58e0
Mladen Todorovic <mtodor@gmail.com>
2023-10-25 17:58:57 +0200

    ROX-19561: Add throttling to API rate limit (#8330)

--- pkg/grpc/ratelimit/ratelimit_impl_test.go
+++ pkg/grpc/ratelimit/ratelimit_impl_test.go
@@ -5,35 +5,36 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stackrox/rox/pkg/sync"
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/time/rate"
 )
 
 func TestNewRateLimiterUnlimited(t *testing.T) {
-	rl := newRateLimiter(-1)
+	rl := newRateLimiter(-1, 0)
 	assert.Equal(t, rate.Inf, rl.tokenBucketLimiter.Limit())
 
-	rl = newRateLimiter(0)
+	rl = newRateLimiter(0, 0)
 	assert.Equal(t, rate.Inf, rl.tokenBucketLimiter.Limit())
 }
 
 func TestNewRateLimiterLimitedValue(t *testing.T) {
 	expectedRatePerSec := 100
-	rl := newRateLimiter(expectedRatePerSec)
+	rl := newRateLimiter(expectedRatePerSec, 0)
 
 	assert.Equal(t, rate.Limit(expectedRatePerSec), rl.tokenBucketLimiter.Limit())
 	assert.Equal(t, expectedRatePerSec, rl.tokenBucketLimiter.Burst())
 }
 
 func TestIncreaseLimit(t *testing.T) {
-	noLimitRL := newRateLimiter(0)
+	noLimitRL := newRateLimiter(0, 0)
 	assert.Equal(t, rate.Inf, noLimitRL.tokenBucketLimiter.Limit())
 
 	noLimitRL.IncreaseLimit(1)
 	assert.Equal(t, rate.Inf, noLimitRL.tokenBucketLimiter.Limit())
 
 	expectedRatePerSec := 100
-	rl := newRateLimiter(expectedRatePerSec)
+	rl := newRateLimiter(expectedRatePerSec, 0)
 	assert.Equal(t, rate.Limit(expectedRatePerSec), rl.tokenBucketLimiter.Limit())
 	assert.Equal(t, expectedRatePerSec, rl.tokenBucketLimiter.Burst())
 
@@ -50,14 +51,14 @@ func TestIncreaseLimit(t *testing.T) {
 }
 
 func TestDecreaseLimit(t *testing.T) {
-	noLimitRL := newRateLimiter(0)
+	noLimitRL := newRateLimiter(0, 0)
 	assert.Equal(t, rate.Inf, noLimitRL.tokenBucketLimiter.Limit())
 
 	noLimitRL.DecreaseLimit(1)
 	assert.Equal(t, rate.Inf, noLimitRL.tokenBucketLimiter.Limit())
 
 	expectedRatePerSec := 100
-	rl := newRateLimiter(expectedRatePerSec)
+	rl := newRateLimiter(expectedRatePerSec, 0)
 	assert.Equal(t, rate.Limit(expectedRatePerSec), rl.tokenBucketLimiter.Limit())
 	assert.Equal(t, expectedRatePerSec, rl.tokenBucketLimiter.Burst())
 
@@ -73,19 +74,100 @@ func TestDecreaseLimit(t *testing.T) {
 	assert.Equal(t, expectedRatePerSec, rl.tokenBucketLimiter.Burst())
 }
 
+func TestLimitNoThrottle(t *testing.T) {
+	tests := []struct {
+		name                string
+		maxPerSec           int
+		maxThrottleDuration time.Duration
+	}{
+		{"NoLimit 0s", 0, 0},
+		{"WithLimitHit 0s", 1, 0},
+		{"NoLimit 1ns", 0, time.Nanosecond},
+		{"WithLimitHit 1ns", 1, time.Nanosecond},
+		{"NoLimit under 1s", 0, time.Second - 1},
+		{"WithLimitHit under 1s", 1, time.Second - 1},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rl := newRateLimiter(tt.maxPerSec, tt.maxThrottleDuration)
+
+			r1 := rl.Limit()
+			r2 := rl.Limit()
+
+			assert.False(t, r1)
+			assert.True(t, tt.maxPerSec == 0 || r2)
+			assert.False(t, tt.maxPerSec == 0 && r2)
+		})
+	}
+}
+
+func TestLimitWithThrottle(t *testing.T) {
+	tests := []struct {
+		name                string
+		maxPerSec           int
+		maxThrottleDuration time.Duration
+	}{
+		{"NoLimit 1s", 0, time.Second},
+		{"WithLimitHit 1s", 1, time.Second},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rl := newRateLimiter(tt.maxPerSec, tt.maxThrottleDuration)
+
+			var wg sync.WaitGroup
+
+			numCalls := tt.maxPerSec + 10
+			resultChan := make(chan bool, numCalls)
+
+			for i := 0; i < numCalls; i++ {
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					resultChan <- rl.Limit()
+				}()
+			}
+
+			go func() {
+				wg.Wait()
+				close(resultChan)
+			}()
+
+			countLimitHit := 0
+			for result := range resultChan {
+				if result {
+					countLimitHit++
+				}
+			}
+
+			if tt.maxPerSec == 0 {
+				assert.Equal(t, countLimitHit, 0)
+			} else {
+				assert.Less(t, countLimitHit, numCalls)
+				assert.GreaterOrEqual(t, countLimitHit, tt.maxPerSec)
+			}
+		})
+	}
+}
+
 func BenchmarkRateLimiter(b *testing.B) {
 	tests := []struct {
-		name      string
-		maxPerSec int
+		name                string
+		maxPerSec           int
+		maxThrottleDuration time.Duration
 	}{
-		{"NoLimit", 0},
-		{"WithLimitHit", 1},
-		{"WithLimitNoHit", math.MaxInt - 1},
+		{"NoLimit NoThrottle", 0, 0},
+		{"WithLimitHit NoThrottle", 1, 0},
+		{"WithLimitNoHit NoThrottle", math.MaxInt - 1, 0},
+		{"NoLimit Throttle 1s", 0, time.Second},
+		{"WithLimitHit Throttle 1s", 1, time.Second},
+		{"WithLimitNoHit Throttle 1s", math.MaxInt - 1, time.Second},
 	}
 
 	for _, tt := range tests {
 		b.Run(tt.name, func(b *testing.B) {
-			l := NewRateLimiter(tt.maxPerSec)
+			l := NewRateLimiter(tt.maxPerSec, tt.maxThrottleDuration)
 			for i := 0; i < b.N; i++ {
 				l.Limit()
 			}
