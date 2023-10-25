0479cc90f8af9ae8ef4129f92ee34dd1e66a71d3
dhaus67 <dhaus@redhat.com>
2023-10-25 05:16:29 +0200

    ROX-20285: Add source IP to audit log message (#7987)

--- central/audit/audit.go
+++ central/audit/audit.go
@@ -130,6 +130,12 @@ func (a *audit) newAuditMessage(ctx context.Context, req interface{}, grpcFullMe
 		Endpoint: endpoint,
 		Method:   method,
 		Payload:  requestToAny(req),
+		SourceHeaders: &v1.Audit_Message_Request_SourceHeaders{
+			XForwardedFor: ri.Source.XForwardedFor,
+			RemoteAddr:    ri.Source.RemoteAddr,
+			RequestAddr:   ri.Source.RequestAddr,
+		},
+		SourceIp: ri.Source.GetSourceIP(),
 	}
 
 	msg.Status, msg.StatusReason = calculateAuditStatus(authError, requestError)
