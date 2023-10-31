29da4a565039b06b734573bdda541942d36f309d
Jan Schnathmeier <jschnath@redhat.com>
2023-10-31 13:24:52 +0100

    Add sync resources test

--- central/sensor/service/pipeline/nodes/pipeline_test.go
+++ central/sensor/service/pipeline/nodes/pipeline_test.go
@@ -11,10 +11,70 @@ import (
 	"github.com/stackrox/rox/central/sensor/service/common"
 	"github.com/stackrox/rox/generated/internalapi/central"
 	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/fixtures"
 	nodesEnricherMocks "github.com/stackrox/rox/pkg/nodes/enricher/mocks"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	"go.uber.org/mock/gomock"
 )
+
+func TestPipeline(t *testing.T) {
+	suite.Run(t, new(PipelineTestSuite))
+}
+
+type PipelineTestSuite struct {
+	suite.Suite
+
+	clusterStore  *clusterDatastoreMocks.MockDataStore
+	nodeDatastore *nodeDatastoreMocks.MockDataStore
+	enricher      *nodesEnricherMocks.MockNodeEnricher
+	riskManager   *riskManagerMocks.MockManager
+	pipeline      *pipelineImpl
+
+	mockCtrl *gomock.Controller
+}
+
+func (suite *PipelineTestSuite) SetupTest() {
+	suite.mockCtrl = gomock.NewController(suite.T())
+
+	suite.clusterStore = clusterDatastoreMocks.NewMockDataStore(suite.mockCtrl)
+	suite.nodeDatastore = nodeDatastoreMocks.NewMockDataStore(suite.mockCtrl)
+	suite.enricher = nodesEnricherMocks.NewMockNodeEnricher(suite.mockCtrl)
+	suite.riskManager = riskManagerMocks.NewMockManager(suite.mockCtrl)
+
+	suite.pipeline = &pipelineImpl{
+		clusterStore:  suite.clusterStore,
+		nodeDatastore: suite.nodeDatastore,
+		enricher:      suite.enricher,
+		riskManager:   suite.riskManager,
+	}
+}
+
+func (suite *PipelineTestSuite) TearDownTest() {
+	suite.mockCtrl.Finish()
+}
+
+func (suite *PipelineTestSuite) TestNodeSyncResources() {
+	ctx := context.Background()
+	node := fixtures.GetNode()
+
+	suite.clusterStore.EXPECT().GetClusterName(ctx, node.GetClusterId())
+	suite.enricher.EXPECT().EnrichNode(node)
+	suite.riskManager.EXPECT().CalculateRiskAndUpsertNode(node)
+
+	err := suite.pipeline.Run(ctx, node.GetClusterId(), &central.MsgFromSensor{
+		Msg: &central.MsgFromSensor_Event{
+			Event: &central.SensorEvent{
+				Id:     node.GetId(),
+				Action: central.ResourceAction_SYNC_RESOURCE,
+				Resource: &central.SensorEvent_Node{
+					Node: node,
+				},
+			},
+		},
+	}, nil)
+	suite.NoError(err)
+}
 
 func Test_pipelineImpl_Run(t *testing.T) {
 	createMsg := func(osImage string) *central.MsgFromSensor {
