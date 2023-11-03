39c26fb9cf231f95c159963f91f643cb4460bcd3
Yi Li <yli3@redhat.com>
2023-11-02 19:47:58 -0500

    Update pkg/env/scanner_v4_definitions_setting.go
    Co-authored-by: David Caravello <119438707+dcaravel@users.noreply.github.com>

--- pkg/env/scanner_v4_definitions_setting.go
+++ pkg/env/scanner_v4_definitions_setting.go
@@ -3,6 +3,6 @@ package env
 import "time"
 
 var (
-	// RepoMappingUpdateMaxInitialWait is the maximum wait time before the first repository mapping data
+	// RepoMappingUpdateMaxInitialWait is the maximum wait time before the first attempt to download data mapping cpes to repos.
 	RepoMappingUpdateMaxInitialWait = registerDurationSetting("ROX_MAPPING_UPDATE_MAX_INITIAL_WAIT", 3*time.Minute)
 )
