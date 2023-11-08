cb4fffa040507b7558823fad4ec71bb11ffa76ef
Tomasz Janiszewski <tomek@redhat.com>
2023-11-08 09:55:50 +0100

    chore(deps): upgrade pgx to v5 (#6916)
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

--- central/cve/cluster/datastore/store/postgres/full_store.go
+++ central/cve/cluster/datastore/store/postgres/full_store.go
@@ -6,7 +6,7 @@ import (
 	"time"
 
 	protoTypes "github.com/gogo/protobuf/types"
-	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v5"
 	"github.com/stackrox/rox/central/cve/cluster/datastore/store"
 	"github.com/stackrox/rox/central/cve/converter/v2"
 	"github.com/stackrox/rox/central/metrics"
--- pkg/postgres/mocks/pgxpool_conn.go
+++ pkg/postgres/mocks/pgxpool_conn.go
@@ -12,8 +12,8 @@ import (
 	context "context"
 	reflect "reflect"
 
-	pgconn "github.com/jackc/pgconn"
-	pgx "github.com/jackc/pgx/v4"
+	pgx "github.com/jackc/pgx/v5"
+	pgconn "github.com/jackc/pgx/v5/pgconn"
 	gomock "go.uber.org/mock/gomock"
 )
 
