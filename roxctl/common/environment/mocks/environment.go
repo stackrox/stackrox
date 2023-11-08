05798164e1c71ed19844150283ba6a844c736b5e
Stephan Hesselmann <shesselm@redhat.com>
2023-11-08 14:45:02 +0100

    ROX-19921: add retry timeout to roxctl (#7965)

--- roxctl/common/environment/mocks/environment.go
+++ roxctl/common/environment/mocks/environment.go
@@ -91,18 +91,22 @@ func (mr *MockEnvironmentMockRecorder) ConnectNames() *gomock.Call {
 }
 
 // GRPCConnection mocks base method.
-func (m *MockEnvironment) GRPCConnection() (*grpc.ClientConn, error) {
+func (m *MockEnvironment) GRPCConnection(connectionOpts ...common.GRPCOption) (*grpc.ClientConn, error) {
 	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "GRPCConnection")
+	varargs := []any{}
+	for _, a := range connectionOpts {
+		varargs = append(varargs, a)
+	}
+	ret := m.ctrl.Call(m, "GRPCConnection", varargs...)
 	ret0, _ := ret[0].(*grpc.ClientConn)
 	ret1, _ := ret[1].(error)
 	return ret0, ret1
 }
 
 // GRPCConnection indicates an expected call of GRPCConnection.
-func (mr *MockEnvironmentMockRecorder) GRPCConnection() *gomock.Call {
+func (mr *MockEnvironmentMockRecorder) GRPCConnection(connectionOpts ...any) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GRPCConnection", reflect.TypeOf((*MockEnvironment)(nil).GRPCConnection))
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GRPCConnection", reflect.TypeOf((*MockEnvironment)(nil).GRPCConnection), connectionOpts...)
 }
 
 // HTTPClient mocks base method.
