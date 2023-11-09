a4fec3b591e3e449a6cd8f3d806f4f698cfa050c
Van J. Wilson <vwilson@redhat.com>
2023-11-09 16:19:00 -0500

    Add smoke tests for Compliance 2.0 scan configs

--- ui/apps/platform/cypress/integration/compliance-enhanced/ComplianceEnhanced.helpers.js
+++ ui/apps/platform/cypress/integration/compliance-enhanced/ComplianceEnhanced.helpers.js
@@ -1,14 +1,56 @@
+import { visitFromLeftNavExpandable } from '../../helpers/nav';
 import { visit } from '../../helpers/visit';
 
 // path
 
 export const basePath = '/main/compliance-enhanced';
+export const statusDashboardPath = `${basePath}/status`;
+export const scanConfigsPath = `${basePath}/scan-configs`;
 
-// visit
+// visit helpers
+export const scanConfigsAlias = 'configurations';
+
+const routeMatcherMapToComplianceEnhancedDashboard = null;
+
+const routeMatcherMapToComplianceEnhancedScanConfigs = {
+    [scanConfigsAlias]: {
+        method: 'GET',
+        url: '/v2/compliance/scan/configurations*',
+    },
+};
+
+export function visitComplianceEnhancedFromLeftNav() {
+    visitFromLeftNavExpandable(
+        'Compliance (2.0)',
+        'Compliance Status',
+        routeMatcherMapToComplianceEnhancedDashboard
+    );
+
+    cy.location('pathname').should('eq', statusDashboardPath);
+}
 
 export function visitComplianceEnhancedDashboard() {
     // TODO: (vjw, 1 Nov 2023) add routes matchers to this function, after API is available for Status
-    visit(`${basePath}/status`);
+    visit(basePath, routeMatcherMapToComplianceEnhancedDashboard);
 
+    cy.location('pathname').should('eq', statusDashboardPath);
     cy.get(`h1:contains("Compliance")`);
+}
+
+export function visitComplianceEnhancedScanConfigsFromLeftNav() {
+    visitFromLeftNavExpandable(
+        'Compliance (2.0)',
+        'Scheduling',
+        routeMatcherMapToComplianceEnhancedScanConfigs
+    );
+
+    cy.location('pathname').should('eq', scanConfigsPath);
+    cy.get(`h1:contains("Scan schedules")`);
+}
+
+export function visitComplianceEnhancedScanConfigs() {
+    // TODO: (vjw, 1 Nov 2023) add routes matchers to this function, after API is available for Status
+    visit(scanConfigsPath, routeMatcherMapToComplianceEnhancedDashboard);
+
+    cy.get(`h1:contains("Scan schedules")`);
 }
--- ui/apps/platform/cypress/integration/compliance-enhanced/complianceEnhancedDashboard.test.js
+++ ui/apps/platform/cypress/integration/compliance-enhanced/complianceEnhancedDashboard.test.js
@@ -2,7 +2,11 @@ import withAuth from '../../helpers/basicAuth';
 import { hasFeatureFlag } from '../../helpers/features';
 import { getRegExpForTitleWithBranding } from '../../helpers/title';
 
-import { visitComplianceEnhancedDashboard } from './ComplianceEnhanced.helpers';
+import {
+    visitComplianceEnhancedDashboard,
+    visitComplianceEnhancedFromLeftNav,
+    visitComplianceEnhancedScanConfigsFromLeftNav,
+} from './ComplianceEnhanced.helpers';
 
 describe('Compliance Dashboard', () => {
     withAuth();
@@ -13,7 +17,13 @@ describe('Compliance Dashboard', () => {
         }
     });
 
-    it('should have title', () => {
+    it('should visit using the left nav', () => {
+        visitComplianceEnhancedFromLeftNav();
+
+        cy.title().should('match', getRegExpForTitleWithBranding('Compliance Status Dashboard'));
+    });
+
+    it('should have expected elements on the status page', () => {
         visitComplianceEnhancedDashboard();
 
         cy.title().should('match', getRegExpForTitleWithBranding('Compliance Status Dashboard'));
@@ -26,5 +36,33 @@ describe('Compliance Dashboard', () => {
         cy.get('th[scope="col"]:contains("Profiles")');
         cy.get('th[scope="col"]:contains("Failing Controls")');
         cy.get('th[scope="col"]:contains("Last Scanned")');
+    });
+
+    it('should visit scan configurations scheduling from the left nav', () => {
+        visitComplianceEnhancedScanConfigsFromLeftNav();
+
+        cy.title().should('match', getRegExpForTitleWithBranding('Scan Schedules'));
+    });
+
+    it('should have expected elements on the scans page', () => {
+        visitComplianceEnhancedScanConfigsFromLeftNav();
+
+        cy.title().should('match', getRegExpForTitleWithBranding('Scan Schedules'));
+
+        cy.get('th[scope="col"]:contains("Name")');
+        cy.get('th[scope="col"]:contains("Schedule")');
+        cy.get('th[scope="col"]:contains("Last run")');
+        cy.get('th[scope="col"]:contains("Clusters")');
+        cy.get('th[scope="col"]:contains("Profiles")');
+
+        cy.get('h2:contains("No scan schedules")');
+
+        cy.get('.pf-c-toolbar__content a:contains("Create scan schedule")').click();
+        cy.location('search').should('eq', '?action=create');
+
+        cy.go('back');
+
+        cy.get('.pf-c-empty-state__content a:contains("Create scan schedule")').click();
+        cy.location('search').should('eq', '?action=create');
     });
 });
