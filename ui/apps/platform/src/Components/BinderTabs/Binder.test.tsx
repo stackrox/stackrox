2e9adb30a6c6f9c58a1e180e0f646be2a54da986
Linda Song <lindahsong@gmail.com>
2023-11-10 11:46:35 -0800

    ROX-10222: React 18 upgrade (#8314)

--- ui/apps/platform/src/Components/BinderTabs/Binder.test.tsx
+++ ui/apps/platform/src/Components/BinderTabs/Binder.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import BinderTabs from './BinderTabs';
 import Tab from '../Tab';
@@ -19,7 +20,8 @@ describe('BinderTabs', () => {
         expect(screen.getByText('Tab 1 Content')).toBeDefined();
     });
 
-    test("selecting a new tab render's the new tab's contents", () => {
+    test("selecting a new tab render's the new tab's contents", async () => {
+        const user = userEvent.setup();
         render(
             <BinderTabs>
                 <Tab title="tab 1">
@@ -31,7 +33,7 @@ describe('BinderTabs', () => {
             </BinderTabs>
         );
 
-        screen.getByText('tab 2').click();
+        await act(() => user.click(screen.getByText('tab 2')));
 
         expect(screen.getByText('Tab 2 Content')).toBeDefined();
     });
--- ui/apps/platform/src/hooks/useSet.test.ts
+++ ui/apps/platform/src/hooks/useSet.test.ts
@@ -1,4 +1,4 @@
-import { renderHook, act } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react';
 import useSet from './useSet';
 
 test('useSet should accept a starting set', () => {
