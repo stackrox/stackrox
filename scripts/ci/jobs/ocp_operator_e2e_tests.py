76c02300cadb2f6171901d4bab6aa78c56d03375
Marcin Owsiany <porridge@redhat.com>
2023-11-14 12:46:53 +0100

    Bump nodes to 4 for operator e2e tests.

--- scripts/ci/jobs/ocp_operator_e2e_tests.py
+++ scripts/ci/jobs/ocp_operator_e2e_tests.py
@@ -5,11 +5,13 @@ Run operator e2e tests in a openshift 4 cluster provided via a hive cluster_clai
 """
 from runners import ClusterTestRunner
 from ci_tests import OperatorE2eTest
+from clusters import OpenShiftScaleWorkersCluster
 from pre_tests import PreSystemTests
 from post_tests import PostClusterTest, FinalPost
 
 
 ClusterTestRunner(
+    cluster=OpenShiftScaleWorkersCluster(),
     pre_test=PreSystemTests(),
     test=OperatorE2eTest(),
     post_test=PostClusterTest(collect_central_artifacts=False),
