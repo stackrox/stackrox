fcd6b3e33912b274637bbbc38ccd38114a80d572
Mark Pedrotti <mark@stackrox.com>
2023-11-15 10:51:59 -0500

    Rewrite object spread to remove one eslint disable comment

--- ui/apps/platform/src/test-utils/mocks/@patternfly/react-core.tsx
+++ ui/apps/platform/src/test-utils/mocks/@patternfly/react-core.tsx
@@ -1,12 +1,9 @@
 import * as PFReactCore from '@patternfly/react-core';
 
-// disable because unused debounce might be specified for rest spread idiom.
-/* eslint-disable @typescript-eslint/no-unused-vars */
-const { debounce, ...rest } = jest.requireActual('@patternfly/react-core');
-/* eslint-enable @typescript-eslint/no-unused-vars */
+const properties = jest.requireActual('@patternfly/react-core');
 
 // Overrides the PF `debounce` function to do nothing other than return the original function. This
 // can be used to avoid issues in tests that result from state updates in a debounced function.
-export const mockDebounce = { ...rest, debounce: (fn: () => void) => fn } as jest.Mock<
+export const mockDebounce = { ...properties, debounce: (fn: () => void) => fn } as jest.Mock<
     typeof PFReactCore
 >;
