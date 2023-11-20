840282ce026c4b19b0053257aecbd1d9ef9b99d8
Stephan Heßelmann <shesselm@redhat.com>
2023-11-20 01:46:48 +0100

    ROX-20783: add GCP credentials manager

---
+++ pkg/cloudproviders/gcp/cred_manager_impl.go
@@ -0,0 +1,76 @@
+package gcp
+
+import (
+	"context"
+
+	"github.com/stackrox/rox/pkg/secretinformer"
+	"github.com/stackrox/rox/pkg/sync"
+	"golang.org/x/oauth2/google"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+)
+
+const (
+	cloudCredentialsKey = "credentials"
+)
+
+type gcpCredentialsManagerImpl struct {
+	informer  *secretinformer.SecretInformer
+	stsConfig []byte
+	mutex     sync.RWMutex
+}
+
+var _ CredentialsManager = &gcpCredentialsManagerImpl{}
+
+// NewCredentialsManager creates a new GCP credential manager.
+func NewCredentialsManager(k8sClient kubernetes.Interface, namespace string, secretName string) *gcpCredentialsManagerImpl {
+	mgr := &gcpCredentialsManagerImpl{}
+	mgr.informer = secretinformer.NewSecretInformer(
+		namespace,
+		secretName,
+		k8sClient,
+		mgr.updateSecret,
+		mgr.updateSecret,
+		mgr.deleteSecret,
+	)
+	return mgr
+}
+
+func (c *gcpCredentialsManagerImpl) updateSecret(secret *v1.Secret) {
+	if stsConfig, ok := secret.Data[cloudCredentialsKey]; ok {
+		c.mutex.Lock()
+		defer c.mutex.Unlock()
+		c.stsConfig = []byte(stsConfig)
+		log.Infof("Updated GCP cloud credentials based on %s/%s", c.informer.Namespace, c.informer.SecretName)
+	}
+}
+
+func (c *gcpCredentialsManagerImpl) deleteSecret() {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	c.stsConfig = []byte{}
+	log.Infof("Deleted GCP cloud credentials based on %s/%s", c.informer.Namespace, c.informer.SecretName)
+}
+
+func (c *gcpCredentialsManagerImpl) Start() error {
+	return c.informer.Start()
+}
+
+func (c *gcpCredentialsManagerImpl) Stop() {
+	c.informer.Stop()
+}
+
+// GetCredentials returns GCP credentials based on the environment.
+//
+// The following sources are considered:
+//  1. Cloud credentials secret (stackrox/gcpCloudCredentials) containing the STS configuration
+//     for federated workload identities. Ignored if the secret does not exist.
+//  2. The default GCP credentials chain based on the pod's environment and metadata.
+func (c *gcpCredentialsManagerImpl) GetCredentials(ctx context.Context) (*google.Credentials, error) {
+	c.mutex.RLock()
+	defer c.mutex.RUnlock()
+	if len(c.stsConfig) > 0 {
+		return google.CredentialsFromJSONWithParams(ctx, c.stsConfig, google.CredentialsParams{})
+	}
+	return google.FindDefaultCredentials(ctx)
+}
---
+++ pkg/cloudproviders/gcp/cred_manager_impl_test.go
@@ -0,0 +1,136 @@
+package gcp
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
+)
+
+const (
+	fakeSTSConfig = `{
+   "type": "external_account",
+   "audience": "//iam.googleapis.com/projects/123456789/locations/global/workloadIdentityPools/test-pool/providers/test-provider",
+   "subject_token_type": "urn:ietf:params:oauth:token-type:jwt",
+   "token_url": "https://sts.googleapis.com/v1/token",
+   "service_account_impersonation_url": "https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/fake@stackrox.com:generateAccessToken",
+   "credential_source": {
+      "file": "/var/run/secrets/openshift/serviceaccount/token",
+      "format": {
+         "type": "text"
+      }
+   }
+}`
+	namespace  = "stackrox"
+	secretName = "gcpCloudCredentials"
+)
+
+func TestCredentialManager(t *testing.T) {
+	t.Parallel()
+	cases := map[string]struct {
+		setupFn  func(k8sClient *fake.Clientset) error
+		expected string
+	}{
+		"secret added": {
+			setupFn: func(k8sClient *fake.Clientset) error {
+				_, err := k8sClient.CoreV1().Secrets(namespace).Create(
+					context.Background(),
+					&v1.Secret{
+						ObjectMeta: metav1.ObjectMeta{Name: secretName},
+						Data: map[string][]byte{
+							cloudCredentialsKey: []byte(fakeSTSConfig),
+						},
+					},
+					metav1.CreateOptions{},
+				)
+				return err
+			},
+			expected: fakeSTSConfig,
+		},
+		"secret updated": {
+			setupFn: func(k8sClient *fake.Clientset) error {
+				_, err := k8sClient.CoreV1().Secrets(namespace).Create(
+					context.Background(),
+					&v1.Secret{
+						ObjectMeta: metav1.ObjectMeta{Name: secretName},
+						Data: map[string][]byte{
+							cloudCredentialsKey: []byte("xxx"),
+						},
+					},
+					metav1.CreateOptions{},
+				)
+				if err != nil {
+					return err
+				}
+				// Allow the state to propagate.
+				time.Sleep(10 * time.Millisecond)
+
+				_, err = k8sClient.CoreV1().Secrets(namespace).Update(
+					context.Background(),
+					&v1.Secret{
+						ObjectMeta: metav1.ObjectMeta{Name: secretName},
+						Data: map[string][]byte{
+							cloudCredentialsKey: []byte(fakeSTSConfig),
+						},
+					},
+					metav1.UpdateOptions{},
+				)
+				return err
+			},
+			expected: fakeSTSConfig,
+		},
+		"secret deleted": {
+			setupFn: func(k8sClient *fake.Clientset) error {
+				_, err := k8sClient.CoreV1().Secrets(namespace).Create(
+					context.Background(),
+					&v1.Secret{
+						ObjectMeta: metav1.ObjectMeta{Name: secretName},
+						Data: map[string][]byte{
+							cloudCredentialsKey: []byte(fakeSTSConfig),
+						},
+					},
+					metav1.CreateOptions{},
+				)
+				if err != nil {
+					return err
+				}
+				// Allow the state to propagate.
+				time.Sleep(10 * time.Millisecond)
+
+				return k8sClient.CoreV1().Secrets(namespace).Delete(
+					context.Background(),
+					secretName,
+					metav1.DeleteOptions{},
+				)
+			},
+			expected: "",
+		},
+	}
+
+	for name, c := range cases {
+		c := c
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			k8sClient := fake.NewSimpleClientset()
+			manager := NewCredentialsManager(k8sClient, namespace, secretName)
+			err := manager.Start()
+			require.NoError(t, err)
+			defer manager.Stop()
+
+			err = c.setupFn(k8sClient)
+			require.NoError(t, err)
+
+			// Assert that the secret data has been updated.
+			assert.EventuallyWithT(t, func(collect *assert.CollectT) {
+				manager.mutex.RLock()
+				defer manager.mutex.RUnlock()
+				assert.Equal(t, []byte(c.expected), manager.stsConfig)
+			}, 5*time.Second, 100*time.Millisecond)
+		})
+	}
+}
