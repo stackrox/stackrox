210e1b3fcb08c569853a6c950704a29a238f55e5
Mark Pedrotti <pedrottimark@gmail.com>
2023-11-20 17:05:59 -0500

    chore(ui): Upgrade jest and ts-jest 28 devDependencies (#8724)

--- ui/apps/platform/config-overrides.js
+++ ui/apps/platform/config-overrides.js
@@ -1,10 +1,18 @@
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
-module.exports = function override(config) {
-    config.plugins.push(
-        new MonacoWebpackPlugin({
-            languages: ['json', 'yaml', 'shell'],
-        })
-    );
-    return config;
+module.exports = {
+    jest: function override(config) {
+        config.transform['^.+\\.css$'] = '<rootDir>/react-app-rewired/jest/cssTransform.js';
+        config.transform['^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)'] =
+            '<rootDir>/react-app-rewired/jest/fileTransform.js';
+        return config;
+    },
+    webpack: function override(config) {
+        config.plugins.push(
+            new MonacoWebpackPlugin({
+                languages: ['json', 'yaml', 'shell'],
+            })
+        );
+        return config;
+    },
 };
--- ui/apps/platform/src/Containers/VulnMgmt/List/Cves/ListCVEs.utils.test.js
+++ ui/apps/platform/src/Containers/VulnMgmt/List/Cves/ListCVEs.utils.test.js
@@ -1,3 +1,10 @@
+/**
+ * @jest-environment node
+ *
+ * Reference Error: TextEncoder is not defined
+ * Maybe because of ReactDOMRenderer.renderToString in pdfUtils.js file.
+ */
+
 import entityTypes from 'constants/entityTypes';
 import useCases from 'constants/useCaseTypes';
 import WorkflowEntity from 'utils/WorkflowEntity';
