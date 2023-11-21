ea59f63172bf850a8d74d2c917ab07766bf72648
Tomasz Janiszewski <tomek@redhat.com>
2023-11-21 15:14:56 +0100

    chore: use go-tool to install proto (#8627)
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

---
+++ tools/proto/tools-import.go
@@ -0,0 +1,13 @@
+//go:build tools
+
+package tools
+
+// This file declares dependencies on tool for `go mod` purposes.
+// See https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
+// for an explanation of the approach.
+
+import (
+	// Tool dependencies, not used anywhere in the code.
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
+	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
+)
