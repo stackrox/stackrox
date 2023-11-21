ae3f5094a33147d1ce568d2420d575d49a21bea8
Frederico Bittencourt <fbittenc@redhat.com>
2023-11-21 13:08:30 +0100

    improve unit tests

--- central/sensor/service/connection/manager_test.go
+++ central/sensor/service/connection/manager_test.go
@@ -9,7 +9,7 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-func Test_AvoidLargePayloads(t *testing.T) {
+func Test_GetConnectionPreference(t *testing.T) {
 	wrappedErr := errors.Wrap(status.Error(codes.ResourceExhausted, "gRPC exhausted"), "recv error")
 	errToPreferenece := map[error]bool{
 		wrappedErr: false,
@@ -20,8 +20,21 @@ func Test_AvoidLargePayloads(t *testing.T) {
 		errors.New("custom err"): true,
 	}
 	for err, pref := range errToPreferenece {
-		m := manager{}
-		m.handleConnectionError("1234", err)
-		assert.Equal(t, pref, m.GetConnectionPreference("1234").SendDeduperState)
+		var testName string
+		if err == nil {
+			testName = "nil"
+		} else {
+			testName = err.Error()
+		}
+		t.Run(testName, func(t *testing.T) {
+			m := manager{}
+			m.handleConnectionError("1234", err)
+			assert.Equal(t, pref, m.GetConnectionPreference("1234").SendDeduperState)
+		})
 	}
+}
+
+func Test_GetConnectionPreference_DefaultsToTrue(t *testing.T) {
+	m := manager{}
+	assert.Equal(t, true, m.GetConnectionPreference("1234").SendDeduperState)
 }
