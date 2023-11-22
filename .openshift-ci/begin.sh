9cff5d213dd2f6817bbe469f8a77fbe589f820f8
Gavin Jefferies <gjefferi@redhat.com>
2023-11-22 09:06:39 -0800

    ROX-21015: Switch OCP jobs to 2 x e2-standard-8 workers (#8740)

--- .openshift-ci/begin.sh
+++ .openshift-ci/begin.sh
@@ -11,3 +11,15 @@ set -euo pipefail
 openshift_ci_debug
 
 info "It shall begin"
+
+if [[ -z "${SHARED_DIR:-}" ]]; then
+    echo "ERROR: There is no SHARED_DIR for step env sharing"
+    exit 0 # not fatal but worth highlighting
+fi
+
+if [[ "${JOB_NAME:-}" =~ -ocp-4- ]]; then
+    info "Setting worker node type and count for OCP 4 jobs"
+    # https://github.com/stackrox/automation-flavors/blob/e6daf10b7df49fc003584790e25def036b2a3b0b/openshift-4/entrypoint.sh#L76
+    echo "export WORKER_NODE_COUNT=2" | tee -a "${SHARED_DIR}/shared_env"
+    echo "export WORKER_NODE_TYPE=e2-standard-8" | tee -a "${SHARED_DIR}/shared_env"
+fi
