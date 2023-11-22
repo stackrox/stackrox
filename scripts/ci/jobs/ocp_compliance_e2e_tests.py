bd5f4929638d79bffceea7e6238a2464bca1676f
Gavin Jefferies <gjefferi@redhat.com>
2023-11-22 09:06:39 -0800

    ROX-21015: Switch OCP jobs to 2 x e2-standard-8 workers (#8740)

--- scripts/ci/jobs/ocp_compliance_e2e_tests.py
+++ scripts/ci/jobs/ocp_compliance_e2e_tests.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env -S python3 -u
 
 """
-Run integration tests with the compliance operator in a OCP cluster provided
-via an openshift/release hive cluster_claim.
+Run integration tests with the compliance operator in an OCP cluster.
 """
 import os
 from runners import ClusterTestRunner
-from clusters import OpenShiftScaleWorkersCluster
+from clusters import AutomationFlavorsCluster
 from pre_tests import PreSystemTests
 from ci_tests import ComplianceE2eTest
 from post_tests import PostClusterTest, FinalPost
@@ -17,11 +16,8 @@ os.environ["INSTALL_COMPLIANCE_OPERATOR"] = "true"
 os.environ["ORCHESTRATOR_FLAVOR"] = "openshift"
 os.environ["ROX_POSTGRES_DATASTORE"] = "true"
 
-# Scale up the cluster to support postgres
-cluster = OpenShiftScaleWorkersCluster(increment=1)
-
 ClusterTestRunner(
-    cluster=cluster,
+    cluster=AutomationFlavorsCluster(),
     pre_test=PreSystemTests(),
     test=ComplianceE2eTest(),
     post_test=PostClusterTest(
