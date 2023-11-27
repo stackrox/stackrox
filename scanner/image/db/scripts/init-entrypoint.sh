0da29e0f9b8766e43081f369c49bf8a86db99e74
Moritz Clasmeier <mclasmeier@redhat.com>
2023-11-27 20:35:06 +0100

    Delete relict

--- scanner/image/db/scripts/init-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -Eeo pipefail
-
-# Initialize DB if it does not exist
-if [ ! -s "$PGDATA/PG_VERSION" ]; then
-  initdb --auth-host=scram-sha-256 --auth-local=scram-sha-256 --pwfile /run/secrets/stackrox.io/secrets/password --data-checksums
-fi
