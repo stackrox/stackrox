46dd5f312c641d558b8b127ca71977b730d9f740
Matthias Meidinger <mmeiding@redhat.com>
2023-11-30 15:20:02 +0100

    Check in generated

--- generated/internalapi/central/deployment_enhancement.pb.go
+++ generated/internalapi/central/deployment_enhancement.pb.go
@@ -23,7 +23,7 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
-type DeploymentEnhancementRequest struct {
+type DeploymentEnhancementMessage struct {
 	Id                   string              `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
 	Namespace            string              `protobuf:"bytes,2,opt,name=namespace,proto3" json:"namespace,omitempty"`
 	Deployment           *storage.Deployment `protobuf:"bytes,3,opt,name=deployment,proto3" json:"deployment,omitempty"`
@@ -32,11 +32,86 @@ type DeploymentEnhancementRequest struct {
 	XXX_sizecache        int32               `json:"-"`
 }
 
+func (m *DeploymentEnhancementMessage) Reset()         { *m = DeploymentEnhancementMessage{} }
+func (m *DeploymentEnhancementMessage) String() string { return proto.CompactTextString(m) }
+func (*DeploymentEnhancementMessage) ProtoMessage()    {}
+func (*DeploymentEnhancementMessage) Descriptor() ([]byte, []int) {
+	return fileDescriptor_481d8d3bdf1f6bc6, []int{0}
+}
+func (m *DeploymentEnhancementMessage) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *DeploymentEnhancementMessage) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_DeploymentEnhancementMessage.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *DeploymentEnhancementMessage) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_DeploymentEnhancementMessage.Merge(m, src)
+}
+func (m *DeploymentEnhancementMessage) XXX_Size() int {
+	return m.Size()
+}
+func (m *DeploymentEnhancementMessage) XXX_DiscardUnknown() {
+	xxx_messageInfo_DeploymentEnhancementMessage.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_DeploymentEnhancementMessage proto.InternalMessageInfo
+
+func (m *DeploymentEnhancementMessage) GetId() string {
+	if m != nil {
+		return m.Id
+	}
+	return ""
+}
+
+func (m *DeploymentEnhancementMessage) GetNamespace() string {
+	if m != nil {
+		return m.Namespace
+	}
+	return ""
+}
+
+func (m *DeploymentEnhancementMessage) GetDeployment() *storage.Deployment {
+	if m != nil {
+		return m.Deployment
+	}
+	return nil
+}
+
+func (m *DeploymentEnhancementMessage) MessageClone() proto.Message {
+	return m.Clone()
+}
+func (m *DeploymentEnhancementMessage) Clone() *DeploymentEnhancementMessage {
+	if m == nil {
+		return nil
+	}
+	cloned := new(DeploymentEnhancementMessage)
+	*cloned = *m
+
+	cloned.Deployment = m.Deployment.Clone()
+	return cloned
+}
+
+type DeploymentEnhancementRequest struct {
+	Deployment           *DeploymentEnhancementMessage `protobuf:"bytes,1,opt,name=deployment,proto3" json:"deployment,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}                      `json:"-"`
+	XXX_unrecognized     []byte                        `json:"-"`
+	XXX_sizecache        int32                         `json:"-"`
+}
+
 func (m *DeploymentEnhancementRequest) Reset()         { *m = DeploymentEnhancementRequest{} }
 func (m *DeploymentEnhancementRequest) String() string { return proto.CompactTextString(m) }
 func (*DeploymentEnhancementRequest) ProtoMessage()    {}
 func (*DeploymentEnhancementRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_481d8d3bdf1f6bc6, []int{0}
+	return fileDescriptor_481d8d3bdf1f6bc6, []int{1}
 }
 func (m *DeploymentEnhancementRequest) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -65,21 +140,7 @@ func (m *DeploymentEnhancementRequest) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_DeploymentEnhancementRequest proto.InternalMessageInfo
 
-func (m *DeploymentEnhancementRequest) GetId() string {
-	if m != nil {
-		return m.Id
-	}
-	return ""
-}
-
-func (m *DeploymentEnhancementRequest) GetNamespace() string {
-	if m != nil {
-		return m.Namespace
-	}
-	return ""
-}
-
-func (m *DeploymentEnhancementRequest) GetDeployment() *storage.Deployment {
+func (m *DeploymentEnhancementRequest) GetDeployment() *DeploymentEnhancementMessage {
 	if m != nil {
 		return m.Deployment
 	}
@@ -101,19 +162,17 @@ func (m *DeploymentEnhancementRequest) Clone() *DeploymentEnhancementRequest {
 }
 
 type DeploymentEnhancementResponse struct {
-	Id                   string              `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
-	Namespace            string              `protobuf:"bytes,2,opt,name=namespace,proto3" json:"namespace,omitempty"`
-	Deployment           *storage.Deployment `protobuf:"bytes,3,opt,name=deployment,proto3" json:"deployment,omitempty"`
-	XXX_NoUnkeyedLiteral struct{}            `json:"-"`
-	XXX_unrecognized     []byte              `json:"-"`
-	XXX_sizecache        int32               `json:"-"`
+	Deployment           *DeploymentEnhancementMessage `protobuf:"bytes,1,opt,name=deployment,proto3" json:"deployment,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}                      `json:"-"`
+	XXX_unrecognized     []byte                        `json:"-"`
+	XXX_sizecache        int32                         `json:"-"`
 }
 
 func (m *DeploymentEnhancementResponse) Reset()         { *m = DeploymentEnhancementResponse{} }
 func (m *DeploymentEnhancementResponse) String() string { return proto.CompactTextString(m) }
 func (*DeploymentEnhancementResponse) ProtoMessage()    {}
 func (*DeploymentEnhancementResponse) Descriptor() ([]byte, []int) {
-	return fileDescriptor_481d8d3bdf1f6bc6, []int{1}
+	return fileDescriptor_481d8d3bdf1f6bc6, []int{2}
 }
 func (m *DeploymentEnhancementResponse) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -142,21 +201,7 @@ func (m *DeploymentEnhancementResponse) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_DeploymentEnhancementResponse proto.InternalMessageInfo
 
-func (m *DeploymentEnhancementResponse) GetId() string {
-	if m != nil {
-		return m.Id
-	}
-	return ""
-}
-
-func (m *DeploymentEnhancementResponse) GetNamespace() string {
-	if m != nil {
-		return m.Namespace
-	}
-	return ""
-}
-
-func (m *DeploymentEnhancementResponse) GetDeployment() *storage.Deployment {
+func (m *DeploymentEnhancementResponse) GetDeployment() *DeploymentEnhancementMessage {
 	if m != nil {
 		return m.Deployment
 	}
@@ -178,6 +223,7 @@ func (m *DeploymentEnhancementResponse) Clone() *DeploymentEnhancementResponse {
 }
 
 func init() {
+	proto.RegisterType((*DeploymentEnhancementMessage)(nil), "central.DeploymentEnhancementMessage")
 	proto.RegisterType((*DeploymentEnhancementRequest)(nil), "central.DeploymentEnhancementRequest")
 	proto.RegisterType((*DeploymentEnhancementResponse)(nil), "central.DeploymentEnhancementResponse")
 }
@@ -187,21 +233,75 @@ func init() {
 }
 
 var fileDescriptor_481d8d3bdf1f6bc6 = []byte{
-	// 209 bytes of a gzipped FileDescriptorProto
+	// 236 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x32, 0xc8, 0xcc, 0x2b, 0x49,
 	0x2d, 0xca, 0x4b, 0xcc, 0x49, 0x2c, 0xc8, 0xd4, 0x4f, 0x4e, 0xcd, 0x2b, 0x29, 0x4a, 0xcc, 0xd1,
 	0x4f, 0x49, 0x2d, 0xc8, 0xc9, 0xaf, 0xcc, 0x4d, 0xcd, 0x2b, 0x89, 0x4f, 0xcd, 0xcb, 0x48, 0xcc,
 	0x4b, 0x4e, 0x05, 0xb1, 0xf5, 0x0a, 0x8a, 0xf2, 0x4b, 0xf2, 0x85, 0xd8, 0xa1, 0xaa, 0xa4, 0x24,
 	0x8a, 0x4b, 0xf2, 0x8b, 0x12, 0xd3, 0x53, 0x91, 0x94, 0x43, 0x94, 0x28, 0x35, 0x32, 0x72, 0xc9,
-	0xb8, 0xc0, 0x05, 0x5d, 0x11, 0x46, 0x04, 0xa5, 0x16, 0x96, 0xa6, 0x16, 0x97, 0x08, 0xf1, 0x71,
+	0xb8, 0xc0, 0x05, 0x5d, 0x11, 0x46, 0xf8, 0xa6, 0x16, 0x17, 0x27, 0xa6, 0xa7, 0x0a, 0xf1, 0x71,
 	0x31, 0x65, 0xa6, 0x48, 0x30, 0x2a, 0x30, 0x6a, 0x70, 0x06, 0x31, 0x65, 0xa6, 0x08, 0xc9, 0x70,
 	0x71, 0xe6, 0x25, 0xe6, 0xa6, 0x16, 0x17, 0x24, 0x26, 0xa7, 0x4a, 0x30, 0x81, 0x85, 0x11, 0x02,
 	0x42, 0xc6, 0x5c, 0x5c, 0x08, 0x2b, 0x24, 0x98, 0x15, 0x18, 0x35, 0xb8, 0x8d, 0x84, 0xf5, 0xa0,
-	0xb6, 0xeb, 0x21, 0x2c, 0x0a, 0x42, 0x52, 0xa6, 0xd4, 0xc4, 0xc8, 0x25, 0x8b, 0xc3, 0x0d, 0xc5,
-	0x05, 0xf9, 0x79, 0xc5, 0xa9, 0x74, 0x70, 0x84, 0x93, 0xe4, 0x89, 0x47, 0x72, 0x8c, 0x17, 0x1e,
-	0xc9, 0x31, 0x3e, 0x78, 0x24, 0xc7, 0x38, 0xe3, 0xb1, 0x1c, 0x43, 0x14, 0x2c, 0xf4, 0x92, 0xd8,
-	0xc0, 0x41, 0x65, 0x0c, 0x08, 0x00, 0x00, 0xff, 0xff, 0x6a, 0x0c, 0x0c, 0xa9, 0x81, 0x01, 0x00,
-	0x00,
+	0xb6, 0xeb, 0x21, 0x2c, 0x0a, 0x42, 0x52, 0xa6, 0x94, 0x8a, 0xc3, 0x09, 0x41, 0xa9, 0x85, 0xa5,
+	0xa9, 0xc5, 0x25, 0x42, 0xae, 0x28, 0x86, 0x32, 0x82, 0x0d, 0x55, 0xd5, 0x83, 0xfa, 0x4d, 0x0f,
+	0x9f, 0xeb, 0x51, 0xac, 0x49, 0xe3, 0x92, 0xc5, 0x61, 0x4d, 0x71, 0x41, 0x7e, 0x5e, 0x71, 0x2a,
+	0x95, 0xec, 0x71, 0x92, 0x3c, 0xf1, 0x48, 0x8e, 0xf1, 0xc2, 0x23, 0x39, 0xc6, 0x07, 0x8f, 0xe4,
+	0x18, 0x67, 0x3c, 0x96, 0x63, 0x88, 0x82, 0xc5, 0x43, 0x12, 0x1b, 0x38, 0xd0, 0x8d, 0x01, 0x01,
+	0x00, 0x00, 0xff, 0xff, 0xa1, 0x0b, 0x73, 0x0d, 0xcb, 0x01, 0x00, 0x00,
+}
+
+func (m *DeploymentEnhancementMessage) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *DeploymentEnhancementMessage) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *DeploymentEnhancementMessage) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if m.XXX_unrecognized != nil {
+		i -= len(m.XXX_unrecognized)
+		copy(dAtA[i:], m.XXX_unrecognized)
+	}
+	if m.Deployment != nil {
+		{
+			size, err := m.Deployment.MarshalToSizedBuffer(dAtA[:i])
+			if err != nil {
+				return 0, err
+			}
+			i -= size
+			i = encodeVarintDeploymentEnhancement(dAtA, i, uint64(size))
+		}
+		i--
+		dAtA[i] = 0x1a
+	}
+	if len(m.Namespace) > 0 {
+		i -= len(m.Namespace)
+		copy(dAtA[i:], m.Namespace)
+		i = encodeVarintDeploymentEnhancement(dAtA, i, uint64(len(m.Namespace)))
+		i--
+		dAtA[i] = 0x12
+	}
+	if len(m.Id) > 0 {
+		i -= len(m.Id)
+		copy(dAtA[i:], m.Id)
+		i = encodeVarintDeploymentEnhancement(dAtA, i, uint64(len(m.Id)))
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
 }
 
 func (m *DeploymentEnhancementRequest) Marshal() (dAtA []byte, err error) {
@@ -237,20 +337,6 @@ func (m *DeploymentEnhancementRequest) MarshalToSizedBuffer(dAtA []byte) (int, e
 			i -= size
 			i = encodeVarintDeploymentEnhancement(dAtA, i, uint64(size))
 		}
-		i--
-		dAtA[i] = 0x1a
-	}
-	if len(m.Namespace) > 0 {
-		i -= len(m.Namespace)
-		copy(dAtA[i:], m.Namespace)
-		i = encodeVarintDeploymentEnhancement(dAtA, i, uint64(len(m.Namespace)))
-		i--
-		dAtA[i] = 0x12
-	}
-	if len(m.Id) > 0 {
-		i -= len(m.Id)
-		copy(dAtA[i:], m.Id)
-		i = encodeVarintDeploymentEnhancement(dAtA, i, uint64(len(m.Id)))
 		i--
 		dAtA[i] = 0xa
 	}
@@ -291,20 +377,6 @@ func (m *DeploymentEnhancementResponse) MarshalToSizedBuffer(dAtA []byte) (int, 
 			i = encodeVarintDeploymentEnhancement(dAtA, i, uint64(size))
 		}
 		i--
-		dAtA[i] = 0x1a
-	}
-	if len(m.Namespace) > 0 {
-		i -= len(m.Namespace)
-		copy(dAtA[i:], m.Namespace)
-		i = encodeVarintDeploymentEnhancement(dAtA, i, uint64(len(m.Namespace)))
-		i--
-		dAtA[i] = 0x12
-	}
-	if len(m.Id) > 0 {
-		i -= len(m.Id)
-		copy(dAtA[i:], m.Id)
-		i = encodeVarintDeploymentEnhancement(dAtA, i, uint64(len(m.Id)))
-		i--
 		dAtA[i] = 0xa
 	}
 	return len(dAtA) - i, nil
@@ -321,7 +393,7 @@ func encodeVarintDeploymentEnhancement(dAtA []byte, offset int, v uint64) int {
 	dAtA[offset] = uint8(v)
 	return base
 }
-func (m *DeploymentEnhancementRequest) Size() (n int) {
+func (m *DeploymentEnhancementMessage) Size() (n int) {
 	if m == nil {
 		return 0
 	}
@@ -335,6 +407,22 @@ func (m *DeploymentEnhancementRequest) Size() (n int) {
 	if l > 0 {
 		n += 1 + l + sovDeploymentEnhancement(uint64(l))
 	}
+	if m.Deployment != nil {
+		l = m.Deployment.Size()
+		n += 1 + l + sovDeploymentEnhancement(uint64(l))
+	}
+	if m.XXX_unrecognized != nil {
+		n += len(m.XXX_unrecognized)
+	}
+	return n
+}
+
+func (m *DeploymentEnhancementRequest) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
 	if m.Deployment != nil {
 		l = m.Deployment.Size()
 		n += 1 + l + sovDeploymentEnhancement(uint64(l))
@@ -351,14 +439,6 @@ func (m *DeploymentEnhancementResponse) Size() (n int) {
 	}
 	var l int
 	_ = l
-	l = len(m.Id)
-	if l > 0 {
-		n += 1 + l + sovDeploymentEnhancement(uint64(l))
-	}
-	l = len(m.Namespace)
-	if l > 0 {
-		n += 1 + l + sovDeploymentEnhancement(uint64(l))
-	}
 	if m.Deployment != nil {
 		l = m.Deployment.Size()
 		n += 1 + l + sovDeploymentEnhancement(uint64(l))
@@ -375,7 +455,7 @@ func sovDeploymentEnhancement(x uint64) (n int) {
 func sozDeploymentEnhancement(x uint64) (n int) {
 	return sovDeploymentEnhancement(uint64((x << 1) ^ uint64((int64(x) >> 63))))
 }
-func (m *DeploymentEnhancementRequest) Unmarshal(dAtA []byte) error {
+func (m *DeploymentEnhancementMessage) Unmarshal(dAtA []byte) error {
 	l := len(dAtA)
 	iNdEx := 0
 	for iNdEx < l {
@@ -398,10 +478,10 @@ func (m *DeploymentEnhancementRequest) Unmarshal(dAtA []byte) error {
 		fieldNum := int32(wire >> 3)
 		wireType := int(wire & 0x7)
 		if wireType == 4 {
-			return fmt.Errorf("proto: DeploymentEnhancementRequest: wiretype end group for non-group")
+			return fmt.Errorf("proto: DeploymentEnhancementMessage: wiretype end group for non-group")
 		}
 		if fieldNum <= 0 {
-			return fmt.Errorf("proto: DeploymentEnhancementRequest: illegal tag %d (wire type %d)", fieldNum, wire)
+			return fmt.Errorf("proto: DeploymentEnhancementMessage: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
 		case 1:
@@ -526,6 +606,93 @@ func (m *DeploymentEnhancementRequest) Unmarshal(dAtA []byte) error {
 	}
 	return nil
 }
+func (m *DeploymentEnhancementRequest) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowDeploymentEnhancement
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: DeploymentEnhancementRequest: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: DeploymentEnhancementRequest: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Deployment", wireType)
+			}
+			var msglen int
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowDeploymentEnhancement
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				msglen |= int(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			if msglen < 0 {
+				return ErrInvalidLengthDeploymentEnhancement
+			}
+			postIndex := iNdEx + msglen
+			if postIndex < 0 {
+				return ErrInvalidLengthDeploymentEnhancement
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			if m.Deployment == nil {
+				m.Deployment = &DeploymentEnhancementMessage{}
+			}
+			if err := m.Deployment.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipDeploymentEnhancement(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthDeploymentEnhancement
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.XXX_unrecognized = append(m.XXX_unrecognized, dAtA[iNdEx:iNdEx+skippy]...)
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
 func (m *DeploymentEnhancementResponse) Unmarshal(dAtA []byte) error {
 	l := len(dAtA)
 	iNdEx := 0
@@ -557,70 +724,6 @@ func (m *DeploymentEnhancementResponse) Unmarshal(dAtA []byte) error {
 		switch fieldNum {
 		case 1:
 			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Id", wireType)
-			}
-			var stringLen uint64
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowDeploymentEnhancement
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				stringLen |= uint64(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthDeploymentEnhancement
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex < 0 {
-				return ErrInvalidLengthDeploymentEnhancement
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.Id = string(dAtA[iNdEx:postIndex])
-			iNdEx = postIndex
-		case 2:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Namespace", wireType)
-			}
-			var stringLen uint64
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowDeploymentEnhancement
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				stringLen |= uint64(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthDeploymentEnhancement
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex < 0 {
-				return ErrInvalidLengthDeploymentEnhancement
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.Namespace = string(dAtA[iNdEx:postIndex])
-			iNdEx = postIndex
-		case 3:
-			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field Deployment", wireType)
 			}
 			var msglen int
@@ -649,7 +752,7 @@ func (m *DeploymentEnhancementResponse) Unmarshal(dAtA []byte) error {
 				return io.ErrUnexpectedEOF
 			}
 			if m.Deployment == nil {
-				m.Deployment = &storage.Deployment{}
+				m.Deployment = &DeploymentEnhancementMessage{}
 			}
 			if err := m.Deployment.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
