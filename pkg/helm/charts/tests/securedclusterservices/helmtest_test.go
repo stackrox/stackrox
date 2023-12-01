461d421ae44e163ecc160284931583637339b9ae
Moritz Clasmeier <mclasmeier@redhat.com>
2023-12-01 09:49:07 +0100

    Fix and improvement

--- pkg/helm/charts/tests/securedclusterservices/helmtest_test.go
+++ pkg/helm/charts/tests/securedclusterservices/helmtest_test.go
@@ -4,19 +4,9 @@ import (
 	"testing"
 
 	"github.com/stackrox/rox/image"
-	"github.com/stackrox/rox/pkg/helm/charts"
 	helmChartTestUtils "github.com/stackrox/rox/pkg/helm/charts/testutils"
 )
 
-func configureMetaValues(metaValues *charts.MetaValues) {
-	// Activate certain feature flags.
-	// This allows us to execute tests for features which are currenly disabled by default.
-	metaValues.FeatureFlags["ROX_SCANNER_V4"] = "true"
-}
-
 func TestWithHelmtest(t *testing.T) {
-	testSuiteOpts := helmChartTestUtils.RunHelmTestSuiteOpts{
-		MetaValuesOverridesFunc: configureMetaValues,
-	}
-	helmChartTestUtils.RunHelmTestSuite(t, "testdata/helmtest", image.SecuredClusterServicesChartPrefix, testSuiteOpts)
+	helmChartTestUtils.RunHelmTestSuite(t, "testdata/helmtest", image.SecuredClusterServicesChartPrefix, helmChartTestUtils.RunHelmTestSuiteOpts{})
 }
