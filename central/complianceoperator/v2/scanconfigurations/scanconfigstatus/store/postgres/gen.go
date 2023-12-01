29491559d6e766f27554cc26c8e884aeeead67b0
David Shrewsberry <99685630+dashrews78@users.noreply.github.com>
2023-12-01 13:43:48 -0500

    ROX-20602:  compliance scan config status pk (#8843)
    fix bug where compliance scan config status had the wrong primary key

--- central/complianceoperator/v2/scanconfigurations/scanconfigstatus/store/postgres/gen.go
+++ central/complianceoperator/v2/scanconfigurations/scanconfigstatus/store/postgres/gen.go
@@ -1,3 +1,3 @@
 package postgres
 
-//go:generate pg-table-bindings-wrapper --type=storage.ComplianceOperatorClusterScanConfigStatus --search-category COMPLIANCE_SCAN_CONFIG_STATUS --references=storage.Cluster,storage.ComplianceOperatorScanConfigurationV2 --get-all-func --feature-flag ComplianceEnhancements
+//go:generate pg-table-bindings-wrapper --type=storage.ComplianceOperatorClusterScanConfigStatus --search-category COMPLIANCE_SCAN_CONFIG_STATUS --references=storage.Cluster,storage.ComplianceOperatorScanConfigurationV2 --feature-flag ComplianceEnhancements
