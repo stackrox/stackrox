4944d880a80b59584c24150308056ba9a1708fda
lvalerom <lvalerom@redhat.com>
2023-12-05 12:06:09 +0100

    implement pausable queue

--- pkg/queue/pausable_queue.go
+++ pkg/queue/pausable_queue.go
@@ -1,5 +1,10 @@
 package queue
 
+import (
+	"github.com/stackrox/rox/pkg/concurrency"
+	"github.com/stackrox/rox/pkg/sync"
+)
+
 type PausableQueue[T comparable] interface {
 	Push(T)
 	Pull() T
@@ -14,7 +19,11 @@ type PausableQueue[T comparable] interface {
 type AggregateFunc[T comparable] func(x, y T) (T, bool)
 
 type pausableQueueImpl[T comparable] struct {
+	mu            sync.Mutex
 	internalQueue *Queue[T]
+	isRunning     concurrency.Signal
+	stop          concurrency.Signal
+	aggregators   []AggregateFunc[T]
 }
 
 type PausableQueueOption[T comparable] func(PausableQueue[T])
@@ -31,28 +40,90 @@ func WithPausableMaxSize[T comparable](size int) PausableQueueOption[T] {
 	}
 }
 
-func NewPausableQueue[T comparable](_ ...PausableQueueOption[T]) PausableQueue[T] {
-	return &pausableQueueImpl[T]{}
+func NewPausableQueue[T comparable](opts ...PausableQueueOption[T]) PausableQueue[T] {
+	q := &pausableQueueImpl[T]{
+		internalQueue: NewQueue[T](),
+		isRunning:     concurrency.NewSignal(),
+		stop:          concurrency.NewSignal(),
+	}
+	for _, opt := range opts {
+		opt(q)
+	}
+	return q
 }
 
-func (q *pausableQueueImpl[T]) SetSize(_ int) {}
+func (q *pausableQueueImpl[T]) SetSize(size int) {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+	q.internalQueue.maxSize = size
+}
 
-func (q *pausableQueueImpl[T]) AddAggregator(_ AggregateFunc[T]) {}
+func (q *pausableQueueImpl[T]) AddAggregator(aggregator AggregateFunc[T]) {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+	q.aggregators = append(q.aggregators, aggregator)
+}
 
-func (q *pausableQueueImpl[T]) Push(_ T) {}
+func (q *pausableQueueImpl[T]) Push(item T) {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	if len(q.aggregators) == 0 {
+		q.internalQueue.Push(item)
+		return
+	}
+
+	wasAggregated := false
+	for e := q.internalQueue.queue.Front(); e != nil; e = e.Next() {
+		element, ok := e.Value.(T)
+		if !ok {
+			continue
+		}
+		for _, fn := range q.aggregators {
+			val, aggregated := fn(element, item)
+			if aggregated {
+				e.Value = val
+				wasAggregated = true
+				break
+			}
+		}
+		if wasAggregated {
+			break
+		}
+	}
+
+	if !wasAggregated {
+		q.internalQueue.Push(item)
+	}
+}
 
 func (q *pausableQueueImpl[T]) Pull() T {
 	var ret T
-	return ret
+	if !q.isRunning.IsDone() {
+		return ret
+	}
+
+	return q.internalQueue.Pull()
 }
 
 func (q *pausableQueueImpl[T]) PullBlocking() T {
 	var ret T
-	return ret
+	select {
+	case <-q.stop.Done():
+		return ret
+	case <-q.isRunning.Done():
+		return q.internalQueue.PullBlocking(&q.stop)
+	}
 }
 
-func (q *pausableQueueImpl[T]) Pause() {}
+func (q *pausableQueueImpl[T]) Pause() {
+	q.isRunning.Reset()
+}
 
-func (q *pausableQueueImpl[T]) Resume() {}
+func (q *pausableQueueImpl[T]) Resume() {
+	q.isRunning.Signal()
+}
 
-func (q *pausableQueueImpl[T]) Stop() {}
+func (q *pausableQueueImpl[T]) Stop() {
+	q.stop.Signal()
+}
