1d81b7111da32038e5de1eac1b1e6d3c8d96d06a
Saif Chaudhry <sachaudh@users.noreply.github.com>
2023-12-05 16:12:03 -0800

    ROX-19385: Add flow for cancelling request (#8818)

---
+++ ui/apps/platform/src/Containers/Vulnerabilities/ExceptionManagement/components/RequestCancelButtonModal.tsx
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import { Alert, AlertVariant, Button, Flex, Modal, Spinner, Text } from '@patternfly/react-core';
+
+import {
+    VulnerabilityException,
+    cancelVulnerabilityException,
+} from 'services/VulnerabilityExceptionService';
+import { getAxiosErrorMessage } from 'utils/responseErrorUtils';
+import useModal from 'hooks/useModal';
+import useRestMutation from 'hooks/useRestMutation';
+import useAffectedImagesCount from '../hooks/useAffectedImagesCount';
+
+type RequestCancelButtonModalProps = {
+    exception: VulnerabilityException;
+    onSuccess: (vulnerabilityException: VulnerabilityException) => void;
+};
+
+function RequestCancelButtonModal({ exception, onSuccess }: RequestCancelButtonModalProps) {
+    const cancelRequestMutation = useRestMutation(cancelVulnerabilityException);
+    const { isAffectedImagesCountLoading, affectedImagesCount } = useAffectedImagesCount(exception);
+
+    const { isModalOpen, openModal, closeModal } = useModal();
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+    function cancelRequest() {
+        const payload = {
+            id: exception.id,
+        };
+        cancelRequestMutation.mutate(payload, {
+            onSuccess: (exception) => {
+                onSuccess(exception);
+                closeModal();
+            },
+            onError: (error) => {
+                setErrorMessage(getAxiosErrorMessage(error));
+            },
+        });
+    }
+
+    const isCancelRequestDisabled = isAffectedImagesCountLoading || cancelRequestMutation.isLoading;
+
+    return (
+        <>
+            <Button variant="secondary" onClick={openModal}>
+                Cancel request
+            </Button>
+            <Modal
+                variant="small"
+                title="Cancel request"
+                isOpen={isModalOpen}
+                onClose={closeModal}
+                actions={[
+                    <Button
+                        key="approve"
+                        variant="primary"
+                        isLoading={cancelRequestMutation.isLoading}
+                        isDisabled={isCancelRequestDisabled}
+                        onClick={cancelRequest}
+                    >
+                        Cancel request
+                    </Button>,
+                    <Button key="cancel" variant="link" onClick={closeModal}>
+                        Cancel
+                    </Button>,
+                ]}
+                showClose={false}
+            >
+                <Flex className="pf-u-py-md">
+                    {errorMessage && (
+                        <Alert isInline variant={AlertVariant.danger} title={errorMessage} />
+                    )}
+                    <Alert
+                        variant="warning"
+                        isInline
+                        title="Cancelling the request will return the CVEs to the 'Observed' status."
+                    >
+                        <Text>CVE count: {exception.cves.length}</Text>
+                        <Text>
+                            Affected images:{' '}
+                            {isAffectedImagesCountLoading ? (
+                                <Spinner
+                                    isSVG
+                                    size="md"
+                                    aria-label="Loading affected images count"
+                                />
+                            ) : (
+                                affectedImagesCount
+                            )}
+                        </Text>
+                    </Alert>
+                </Flex>
+            </Modal>
+        </>
+    );
+}
+
+export default RequestCancelButtonModal;
