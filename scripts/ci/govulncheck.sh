0bd74342fb9b4c1bdb912f8b297bb641d1073b0d
Connor Gorman <cgorman@redhat.com>
2023-12-06 10:18:17 -0800

    updates

--- scripts/ci/govulncheck.sh
+++ scripts/ci/govulncheck.sh
@@ -7,8 +7,9 @@ find bin/linux_amd64 -type f | while read -r file;
 do
     echo "Analyzing binary $file"
     $GOVULNCHECK_BIN -mode=binary --json "$file" > vulns.json
+    cat vulns.json >> all_vulns.json
     go run govulncheck/main.go vulns.json > filtered_vulns.json
-    cat filtered_vulns.json
+    cat filtered_vulns.json >> all_vulns.json
     jq '.data[] | select(.id) | [.id, .summary, .details] | @tsv' -r filtered_vulns.json | sort -u | while IFS=$'\t' read -r -a vulns
     do
       id="${vulns[0]}"
@@ -17,11 +18,15 @@ do
       echo "$id" "$summary"
       save_junit_failure "$id" "$summary" "$details"
     done
+    if [[ $(jq '.data | length' < filtered_vulns.json) == 0 ]]; then
+        save_junit_success $file "go scan"
+    fi
 done
 
-if [[ $(jq '.data | length' < filtered_vulns.json) != 0 ]]; then
-    jq '.data | length' < filtered_vulns.json
+go run govulncheck/main.go all_vulns.json > results.json
+if [[ $(jq '.data | length' < results.json) != 0 ]]; then
+    jq '.data | length' < results.json
     echo "Found vulnerabilities. If they are false positives, add them to govulncheck-allowlist.json" >&2
-    cat filtered_vulns.json
+    cat results.json
     exit 1
 fi
