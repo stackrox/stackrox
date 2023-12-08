6cbf418668808995c2c8425f61ede56a771a451f
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2023-12-08 13:39:22 +0100

    Try installing glibc to enable debug symbols

--- image/rhel/download.sh
+++ image/rhel/download.sh
@@ -19,7 +19,7 @@ output_dir="/output"
 mkdir -p "${output_dir}/go/bin"
 if [[ "$DEBUG_BUILD" == "yes" ]]; then
   if [[ "$goarch" == "amd64" || "$goarch" == "arm64" ]]; then
-    dnf install -y golang
+    dnf install -y golang glibc
     if [[ "$OSTYPE" != "linux-gnu"* ]]; then
       GOBIN='' GOOS=linux GOARCH="${goarch}" GOPATH="${output_dir}/go" go install github.com/go-delve/delve/cmd/dlv@latest
       mv "${output_dir}/go/bin/linux_${goarch}/dlv" "${output_dir}/go/bin/dlv"
