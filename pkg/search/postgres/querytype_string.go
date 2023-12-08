f58dcd52cb0be70ec0a3bb1bf1dc40e34b180469
Yann Brillouet <ybrillou@redhat.com>
2023-12-08 22:23:22 +0100

    re-generate files

--- pkg/search/postgres/querytype_string.go
+++ pkg/search/postgres/querytype_string.go
@@ -13,11 +13,12 @@ func _() {
 	_ = x[COUNT-2]
 	_ = x[DELETE-3]
 	_ = x[SELECT-4]
+	_ = x[DELETERETURNINGIDS-5]
 }
 
-const _QueryType_name = "SEARCHGETCOUNTDELETESELECT"
+const _QueryType_name = "SEARCHGETCOUNTDELETESELECTDELETERETURNINGIDS"
 
-var _QueryType_index = [...]uint8{0, 6, 9, 14, 20, 26}
+var _QueryType_index = [...]uint8{0, 6, 9, 14, 20, 26, 44}
 
 func (i QueryType) String() string {
 	if i < 0 || i >= QueryType(len(_QueryType_index)-1) {
