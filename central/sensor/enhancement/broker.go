6b5f4f593b21448e81c5d88ea5d20807e030a1f7
Matthias Meidinger <mmeiding@redhat.com>
2023-12-12 14:03:52 +0100

    Rework wait logic in broker to callbacks

--- central/sensor/enhancement/broker.go
+++ central/sensor/enhancement/broker.go
@@ -19,18 +19,16 @@ var (
 
 // The Broker coordinates and matches deployment enhancement requests to responses
 type Broker struct {
-	requests map[string]chan<- *central.DeploymentEnhancementResponse
-	lock     sync.Mutex
+	waiters map[string]func(msg *central.DeploymentEnhancementResponse)
+	lock    sync.Mutex
 }
 
 // NewBroker returns a new broker
 func NewBroker() *Broker {
-	return &Broker{
-		requests: make(map[string]chan<- *central.DeploymentEnhancementResponse),
-	}
+	return &Broker{}
 }
 
-// NotifyDeploymentReceived matches the ID of Sensors response to the request and notifies the waiting goroutine
+// NotifyDeploymentReceived matches the ID of Sensors response to the request and calls the right callback for it
 func (b *Broker) NotifyDeploymentReceived(msg *central.DeploymentEnhancementResponse) {
 	if msg == nil || msg.GetMsg() == nil {
 		log.Warnf("Received empty message, skipping enhancement notify")
@@ -39,24 +37,62 @@ func (b *Broker) NotifyDeploymentReceived(msg *central.DeploymentEnhancementResp
 
 	b.lock.Lock()
 	defer b.lock.Unlock()
-	if r, ok := b.requests[msg.GetMsg().GetId()]; ok {
-		// Once, to prevent writing to a closed channel if a msg dupe arrives
-		log.Debugf("Received answer for Deployment enrichment requestID %v", msg.GetMsg().GetId())
-		r <- msg
+	if callbackFn, ok := b.waiters[msg.GetMsg().GetId()]; ok {
+		log.Debugf("Received answer for Deployment enrichment requestID %s", msg.GetMsg().GetId())
+		callbackFn(msg)
 	}
 }
 
 // SendAndWaitForEnhancedDeployments sends a list of deployments to Sensor for additional data. Blocks while waiting.
 func (b *Broker) SendAndWaitForEnhancedDeployments(ctx context.Context, conn connection.SensorConnection, deployments []*storage.Deployment, timeout time.Duration) ([]*storage.Deployment, error) {
 	b.lock.Lock()
-	ch := make(chan *central.DeploymentEnhancementResponse)
 	id := uuid.NewV4().String()
-	b.requests[id] = ch
+	c := make(chan *central.DeploymentEnhancementResponse)
+	b.waiters[id] = func(msg *central.DeploymentEnhancementResponse) {
+		c <- msg
+	}
 	b.lock.Unlock()
 
 	log.Debugf("Sending Deployment Augmentation request to Sensor with requestID %s", id)
 
-	err := conn.InjectMessage(ctx, &central.MsgToSensor{
+	err := sendDeployments(ctx, conn, deployments, id)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to send message to cluster %s", conn.ClusterID())
+	}
+
+	enhanced, err := b.waitAndProcessResponse(c, timeout)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to process response for message %s", id)
+	}
+
+	b.lock.Lock()
+	if _, ok := b.waiters[id]; ok {
+		log.Debugf("Deregistering callback for id %s", id)
+		delete(b.waiters, id)
+	}
+	b.lock.Unlock()
+
+	return enhanced, nil
+}
+
+func (b *Broker) waitAndProcessResponse(c chan *central.DeploymentEnhancementResponse, timeout time.Duration) ([]*storage.Deployment, error) {
+	var deployments []*storage.Deployment
+	select {
+	case m, ok := <-c:
+		if !ok {
+			return nil, errors.New("enhancement channel closed unexpectedly")
+		}
+		if deployments = m.GetMsg().GetDeployments(); deployments == nil {
+			return nil, errors.New("enhanced deployments empty")
+		}
+		return deployments, nil
+	case <-time.After(timeout):
+		return nil, errors.New("timed out waiting for augmented deployment")
+	}
+}
+
+func sendDeployments(ctx context.Context, conn connection.SensorConnection, deployments []*storage.Deployment, id string) error {
+	return conn.InjectMessage(ctx, &central.MsgToSensor{
 		Msg: &central.MsgToSensor_DeploymentEnhancementRequest{
 			DeploymentEnhancementRequest: &central.DeploymentEnhancementRequest{
 				Msg: &central.DeploymentEnhancementMessage{
@@ -66,27 +102,4 @@ func (b *Broker) SendAndWaitForEnhancedDeployments(ctx context.Context, conn con
 			},
 		},
 	})
-	if err != nil {
-		return nil, errors.Wrapf(err, "failed to send message to cluster %s", conn.ClusterID())
-	}
-
-	select {
-	case m, ok := <-ch:
-		if !ok {
-			return nil, errors.New("augmented channel closed unexpectedly")
-		}
-		if deployments := m.GetMsg().GetDeployments(); deployments == nil {
-			return nil, errors.New("augmented deployments empty")
-		}
-		b.lock.Lock()
-		if r, ok := b.requests[id]; ok {
-			close(r)
-			delete(b.requests, id)
-		}
-		defer b.lock.Unlock()
-		return deployments, nil
-	case <-time.After(timeout):
-		return nil, errors.New("timed out waiting for augmented deployment")
-	}
-
 }
