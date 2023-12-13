4a2ce9a496ae806155cc014fca8cc5ea9c70db30
Saif Chaudhry <schaudhr@redhat.com>
2023-12-12 16:33:58 -0800

    ROX-20554: Removing a CVE when updating a request

--- ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/components/ExceptionRequestModal/CveSelections.tsx
+++ ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/components/ExceptionRequestModal/CveSelections.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, generatePath } from 'react-router-dom';
-import { Flex, List, ListItem, Text, pluralize } from '@patternfly/react-core';
+import { Flex, List, ListItem, Text, pluralize, Button, FlexItem } from '@patternfly/react-core';
+import { MinusCircleIcon, PlusCircleIcon } from '@patternfly/react-icons';
 
 import ExternalLink from 'Components/PatternFly/IconText/ExternalLink';
 import { vulnerabilitiesWorkloadCvesPath } from 'routePaths';
@@ -9,29 +10,67 @@ const vulnerabilitiesWorkloadCveSinglePath = `${vulnerabilitiesWorkloadCvesPath}
 
 export type CveSelectionsProps = {
     cves: { cve: string; summary: string; numAffectedImages: number }[];
+    selectedCVEIds: string[];
+    onAdd: (cve: string) => void;
+    onRemove: (cve: string) => void;
 };
 
-function CveSelections({ cves }: CveSelectionsProps) {
+function CveSelections({ cves, selectedCVEIds, onAdd, onRemove }: CveSelectionsProps) {
+    const onAddHandler = (cve: string) => () => {
+        onAdd(cve);
+    };
+
+    const onRemoveHandler = (cve: string) => () => {
+        onRemove(cve);
+    };
+
     return (
         <List isPlain isBordered>
-            {cves.map(({ cve, summary, numAffectedImages }) => (
-                <ListItem key={cve}>
-                    <Flex direction={{ default: 'column' }}>
-                        <Flex direction={{ default: 'row' }}>
-                            <ExternalLink>
-                                <Link
-                                    target="_blank"
-                                    to={generatePath(vulnerabilitiesWorkloadCveSinglePath, { cve })}
-                                >
-                                    {cve}
-                                </Link>
-                            </ExternalLink>
-                            <Text>Across {pluralize(numAffectedImages, 'image')}</Text>
+            {cves.map(({ cve, summary, numAffectedImages }) => {
+                const isSelected = selectedCVEIds.includes(cve);
+                return (
+                    <ListItem key={cve}>
+                        <Flex direction={{ default: 'column' }}>
+                            <Flex direction={{ default: 'row' }}>
+                                <ExternalLink>
+                                    <Link
+                                        target="_blank"
+                                        to={generatePath(vulnerabilitiesWorkloadCveSinglePath, {
+                                            cve,
+                                        })}
+                                    >
+                                        {cve}
+                                    </Link>
+                                </ExternalLink>
+                                <Text>Across {pluralize(numAffectedImages, 'image')}</Text>
+                                <FlexItem align={{ default: 'alignRight' }}>
+                                    {isSelected ? (
+                                        <Button
+                                            variant="link"
+                                            aria-label={`Remove ${cve}`}
+                                            onClick={onRemoveHandler(cve)}
+                                            icon={<MinusCircleIcon />}
+                                            isDanger
+                                        >
+                                            Remove
+                                        </Button>
+                                    ) : (
+                                        <Button
+                                            variant="link"
+                                            aria-label={`Add ${cve}`}
+                                            onClick={onAddHandler(cve)}
+                                            icon={<PlusCircleIcon />}
+                                        >
+                                            Add
+                                        </Button>
+                                    )}
+                                </FlexItem>
+                            </Flex>
+                            <Text>{summary}</Text>
                         </Flex>
-                        <Text>{summary}</Text>
-                    </Flex>
-                </ListItem>
-            ))}
+                    </ListItem>
+                );
+            })}
         </List>
     );
 }
