877833e6de93ca26e9c957ca26cd33497ec51c43
Gavin Jefferies <gjefferi@redhat.com>
2023-12-13 10:28:41 -0800

    feat(CI): stubs for perf scale jobs

---
+++ scripts/ci/jobs/ocp_perf_scale_tests.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env -S python3 -u
+
+"""
+Run the perf scale test in an OCP cluster
+"""
+import os
+from runners import ClusterTestRunner
+from clusters import AutomationFlavorsCluster
+from pre_tests import NullPreTest
+from ci_tests import NullTest
+from post_tests import NullPostTest
+
+ClusterTestRunner(
+    cluster=AutomationFlavorsCluster(),
+    pre_test=NullPreTest(),
+    test=NullTest(),
+    post_test=NullPostTest(),
+).run()
