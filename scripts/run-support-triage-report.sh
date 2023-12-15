80809b178b7f270de7611b162393d267f140311e
Tomasz Janiszewski <tomek@redhat.com>
2023-12-15 11:17:16 +0100

    chore(ci): add notification about untriaged issues (#9053)
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

---
+++ scripts/run-support-triage-report.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+# Create CI triage slack reminder.
+
+set -euo pipefail
+
+total_issues_in_filter() {
+    if [[ "$#" -ne 1 ]]; then
+        die "missing arg. usage: total_issues_in_filter <filter>"
+    fi
+
+    local filter="$1"
+    curl -sSfl \
+       -H "Authorization: Bearer $JIRA_TOKEN" \
+       -H "Content-Type: application/json" \
+       "https://issues.redhat.com/rest/api/latest/search?jql=filter=$filter&maxResults=0" | jq '.total'
+}
+
+slack_triage_report() {
+    local curr_filter=12388299
+    local prev_filter=12388044
+
+    local curr
+    curr=$(total_issues_in_filter $curr_filter)
+    local prev
+    prev=$(total_issues_in_filter $prev_filter)
+
+    local line="<!subteam^S04SU9AHJ4C> There are ${curr} untriaged issues (not including ${prev} leftovers from previous duty)"
+    local body
+    # shellcheck disable=SC2016
+    body='{
+    "blocks": [{
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": "'"${line}"'"
+        },
+        "accessory": {
+            "type": "button",
+            "text": {
+                "type": "plain_text",
+                "text": "Triage them 🔥",
+                "emoji": true
+            },
+            "url": "https://issues.redhat.com/secure/Dashboard.jspa?selectPageId=12342126"
+        }
+    }]}'
+    echo "Posting '$line' to slack"
+    jq -n "$body" | curl -sSfl -d @- -H 'Content-Type: application/json' "$SLACK_WEBHOOK"
+}
+
+slack_triage_report
