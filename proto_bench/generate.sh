6a7c5189737d275a6f9cac4834b936290a8b1316
Tomasz Janiszewski <tomek@redhat.com>
2023-12-18 15:44:22 +0100

    generate
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

---
+++ proto_bench/generate.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+PB_REL="https://github.com/protocolbuffers/protobuf/releases"
+curl -LO $PB_REL/download/v3.15.8/protoc-3.15.8-linux-x86_64.zip
+unzip protoc-3.15.8-linux-x86_64.zip -d $PWD
+
+mkdir -p bin
+export GOBIN=$PWD/bin
+
+go install github.com/gogo/protobuf/protoc-gen-gofast@latest
+
+mkdir -p vtproto
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install github.com/planetscale/vtprotobuf/cmd/protoc-gen-go-vtproto@latest
+./bin/protoc \
+    --go_out=vtproto --plugin protoc-gen-go="${GOBIN}/protoc-gen-go" \
+    --go-vtproto_out=vtproto --plugin protoc-gen-go-vtproto="${GOBIN}/protoc-gen-go-vtproto" \
+    --go-vtproto_opt=features=marshal+unmarshal+size+equal+clone+pool \
+    proto/cluster.proto;
+
+mkdir -p csproto
+go install github.com/CrowdStrike/csproto/cmd/protoc-gen-fastmarshal@latest
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+./bin/protoc \
+    --go_out=csproto --plugin protoc-gen-go="${GOBIN}/protoc-gen-fastmarshal" \
+    --fastmarshal_out=paths=source_relative:. \
+    proto/cluster.proto;
+
+mkdir -p gogo
+go install github.com/gogo/protobuf/protoc-gen-gofast@latest
+${GOBIN}/protoc --gofast_out=plugins=grpc:gogo proto/cluster.proto
---
+++ proto_bench/go.mod
@@ -0,0 +1,24 @@
+module github.com/stackrox/stackrox/proto_bench
+
+go 1.20
+
+require (
+	github.com/CrowdStrike/csproto v0.26.0
+	github.com/gogo/protobuf v1.3.2
+	github.com/planetscale/vtprotobuf v0.5.0
+	google.golang.org/protobuf v1.31.0
+)
+
+require (
+	github.com/Masterminds/goutils v1.1.1 // indirect
+	github.com/Masterminds/semver/v3 v3.2.0 // indirect
+	github.com/Masterminds/sprig/v3 v3.2.3 // indirect
+	github.com/google/uuid v1.1.1 // indirect
+	github.com/huandu/xstrings v1.4.0 // indirect
+	github.com/imdario/mergo v0.3.11 // indirect
+	github.com/mitchellh/copystructure v1.0.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.0 // indirect
+	github.com/shopspring/decimal v1.2.0 // indirect
+	github.com/spf13/cast v1.3.1 // indirect
+	golang.org/x/crypto v0.3.0 // indirect
+)
