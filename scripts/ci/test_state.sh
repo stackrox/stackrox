4bca5f4d3280970f561221e18ce0650d732c95f2
Gavin Jefferies <gjefferi@redhat.com>
2023-12-18 11:12:24 -0800

    fix(CI): reduce image failure e2e test spam (#9071)

--- scripts/ci/test_state.sh
+++ scripts/ci/test_state.sh
@@ -2,5 +2,5 @@
 
 # State files used to track test progress across script invocations.
 
-export STATE_IMAGES_AVAILABLE="/tmp/stackrox_ci_state_images_available"
-export STATE_DEPLOYED="/tmp/stackrox_ci_state_deployed"
+export STATE_IMAGES_AVAILABLE="${SHARED_DIR:-/tmp}/stackrox_ci_state_images_available"
+export STATE_DEPLOYED="${SHARED_DIR:-/tmp}/stackrox_ci_state_deployed"
