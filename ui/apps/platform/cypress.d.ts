559fa337a712fc5ee32f4f1d2111ce4227acc8b2
David Vail <dcvail17@gmail.com>
2023-12-19 13:43:08 -0500

    chore(ui): Set up Cypress component testing (#8807)

---
+++ ui/apps/platform/cypress.d.ts
@@ -0,0 +1,14 @@
+import { mount } from 'cypress/react';
+
+declare global {
+    namespace Cypress {
+        interface Chainable {
+            mount: typeof mount;
+            // This is a workaround for a Cypress issue where it doesn't recognize the response
+            // type when using an '@' alias. This will force a return type of 'any' instead.
+            //
+            // See issue: https://github.com/cypress-io/cypress/issues/24823
+            get<T extends any = any>(alias: `@${string}`): Chainable<T>;
+        }
+    }
+}
