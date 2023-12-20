80d09de94e0bdc30507a33e9f2a832201a840297
Moritz Clasmeier <111092021+mclasmeier@users.noreply.github.com>
2023-12-20 17:02:21 +0100

    ROX-19054: Add upgrade logic to Scanner V4 CI test (#8999)
    Co-authored-by: Moritz Clasmeier <mclasmeier@redhat.com>

--- tests/e2e/run-scanner-v4.sh
+++ tests/e2e/run-scanner-v4.sh
@@ -16,20 +16,62 @@ source "$ROOT/tests/scripts/setup-certs.sh"
 
 set -euo pipefail
 
+# Use
+#   CHART_BASE="/rhacs"
+#   export DEFAULT_IMAGE_REGISTRY="quay.io/rhacs-eng"
+# for the RHACS flavor.
+CHART_BASE=""
+export DEFAULT_IMAGE_REGISTRY="quay.io/stackrox-io"
+CURRENT_MAIN_IMAGE_TAG=${CURRENT_MAIN_IMAGE_TAG:-} # Setting a tag can be useful for local testing.
+EARLIER_CHART_VERSION="4.3.0"
+EARLIER_MAIN_IMAGE_TAG=$EARLIER_CHART_VERSION
+
 scannerV4_test() {
     info "Starting ScannerV4 test"
 
     require_environment "ORCHESTRATOR_FLAVOR"
-    require_environment "ROX_SCANNER_V4_ENABLED"
+    export OUTPUT_FORMAT=helm
+    export ROX_SCANNER_V4_ENABLED=true
+    export USE_LOCAL_ROXCTL=true
 
     export_test_environment
 
-    setup_gcp
+    if [[ "$CI" = "true" ]]; then
+        setup_gcp
+    fi
     setup_deployment_env false false
     remove_existing_stackrox_resources
-    setup_default_TLS_certs
 
+    if [[ "$CI" = "true" ]]; then
+        setup_default_TLS_certs
+    fi
+
+    # Prepare earlier version
+    if [[ -z "${CHART_REPOSITORY:-}" ]]; then
+        CHART_REPOSITORY=$(mktemp -d "helm-charts.XXXXXX" -p /tmp)
+    fi
+    if [[ ! -e "${CHART_REPOSITORY}/.git" ]]; then
+        git clone --depth 1 -b main https://github.com/stackrox/helm-charts "${CHART_REPOSITORY}"
+    fi
+
+    # Deploy earlier version without Scanner V4.
+    export CENTRAL_CHART_DIR_OVERRIDE="${CHART_REPOSITORY}${CHART_BASE}/${EARLIER_CHART_VERSION}/central-services"
+    info "Deplying central-services using chart $CENTRAL_CHART_DIR_OVERRIDE"
+    if [[ -n "${EARLIER_MAIN_IMAGE_TAG:-}" ]]; then
+        export MAIN_IMAGE_TAG=$EARLIER_MAIN_IMAGE_TAG
+        info "Overriding MAIN_IMAGE_TAG=$EARLIER_MAIN_IMAGE_TAG"
+    fi
     deploy_stackrox
+    unset MAIN_IMAGE_TAG
+    unset CENTRAL_CHART_DIR_OVERRIDE
+
+    # Upgrade to HEAD chart.
+    info "Upgrading central-services using HEAD Helm chart"
+    if [[ -n "${CURRENT_MAIN_IMAGE_TAG:-}" ]]; then
+        export MAIN_IMAGE_TAG=$CURRENT_MAIN_IMAGE_TAG
+        info "Overriding MAIN_IMAGE_TAG=$CURRENT_MAIN_IMAGE_TAG"
+    fi
+    deploy_stackrox # This is doing an `helm upgrade --install ...` under the hood.
 
     run_scannerV4_test
 }
