ca5446dcd7ba37048512d6996e6e87f414b1de76
J. Victor Martins <jvdm@sdf.org>
2023-12-20 17:18:26 -0300

    Encode scores in the severity field

--- scanner/updater/rhel/parser.go
+++ scanner/updater/rhel/parser.go
@@ -5,6 +5,9 @@ import (
 	"encoding/xml"
 	"fmt"
 	"io"
+	"net/url"
+	"strconv"
+	"strings"
 
 	"github.com/quay/goval-parser/oval"
 	"github.com/quay/zlog"
@@ -20,7 +23,7 @@ import (
 // Parse treats the data inside the provided io.ReadCloser as Red Hat
 // flavored OVAL XML. The distribution associated with vulnerabilities
 // is configured via the Updater. The repository associated with
-// vulnerabilies is based on the affected CPE list.
+// vulnerabilities is based on the affected CPE list.
 func (u *Updater) Parse(ctx context.Context, r io.ReadCloser) ([]*claircore.Vulnerability, error) {
 	ctx = zlog.ContextWithValues(ctx, "component", "rhel/Updater.Parse")
 	zlog.Info(ctx).Msg("starting parse")
@@ -64,7 +67,7 @@ func (u *Updater) Parse(ctx context.Context, r io.ReadCloser) ([]*claircore.Vuln
 				Description:        def.Description,
 				Issued:             def.Advisory.Issued.Date,
 				Links:              ovalutil.Links(def),
-				Severity:           def.Advisory.Severity,
+				Severity:           severity(ctx, def),
 				NormalizedSeverity: common.NormalizeSeverity(def.Advisory.Severity),
 				Repo: &claircore.Repository{
 					Name: affected,
@@ -82,6 +85,77 @@ func (u *Updater) Parse(ctx context.Context, r io.ReadCloser) ([]*claircore.Vuln
 		return nil, err
 	}
 	return vulns, nil
+}
+
+// severity returns the urlencoded Severity, CVSS scores and vectors based on the
+// OVAL definition. For advisories, the CVSS score is the maximum of all the
+// related CVEs' scores.
+func severity(ctx context.Context, def oval.Definition) string {
+	var cvss3, cvss2 struct {
+		score  float64
+		vector string
+	}
+
+	// For CVEs, there will only be 1 item in this slice.
+	// For RHSAs, RHBAs, etc., there will typically be 1 or more.
+	for _, cve := range def.Advisory.Cves {
+		if cve.Cvss3 != "" {
+			score, vector, found := strings.Cut(cve.Cvss3, "/")
+			if !found {
+				zlog.Warn(ctx).
+					Str("CVSS3", cve.Cvss3).
+					Msg("unexpected format")
+				continue
+			}
+			parsedScore, err := strconv.ParseFloat(score, 64)
+			if err != nil {
+				zlog.Warn(ctx).
+					Str("Vulnerability", def.Title).
+					Err(err).
+					Msg("parsing CVSS3")
+				continue
+			}
+			if parsedScore > cvss3.score {
+				cvss3.score = parsedScore
+				cvss3.vector = vector
+			}
+		}
+
+		if cve.Cvss2 != "" {
+			score, vector, found := strings.Cut(cve.Cvss2, "/")
+			if !found {
+				zlog.Warn(ctx).
+					Str("CVSS2", cve.Cvss2).
+					Msg("unexpected format")
+				continue
+			}
+			parsedScore, err := strconv.ParseFloat(score, 64)
+			if err != nil {
+				zlog.Warn(ctx).
+					Str("Vulnerability", def.Title).
+					Err(err).
+					Msg("parsing CVSS3")
+				continue
+			}
+			if parsedScore > cvss2.score {
+				cvss2.score = parsedScore
+				cvss2.vector = vector
+			}
+		}
+	}
+	severity := make(url.Values)
+	if def.Advisory.Severity != "" {
+		severity.Add("severity", def.Advisory.Severity)
+	}
+	if cvss3.score > 0 && cvss3.vector != "" {
+		severity.Add("cvss3_score", fmt.Sprintf("%.1f", cvss3.score))
+		severity.Add("cvss3_vector", cvss3.vector)
+	}
+	if cvss2.score > 0 && cvss2.vector != "" {
+		severity.Add("cvss2_score", fmt.Sprintf("%.1f", cvss2.score))
+		severity.Add("cvss2_vector", cvss2.vector)
+	}
+	return severity.Encode()
 }
 
 func isSkippableDefinitionType(defType ovalutil.DefinitionType, ignoreUnpatched bool) bool {
