e884c45be8dff282047ac22a0ee4747c04a43504
Yann Brillouet <ybrillou@redhat.com>
2023-12-21 14:57:13 +0100

    Extend compatibility layer

--- pkg/protoconv/time_test.go
+++ pkg/protoconv/time_test.go
@@ -38,36 +38,6 @@ func TestConvertTimeString(t *testing.T) {
 	}
 }
 
-func TestConvertTimestampToTimeOrError(t *testing.T) {
-	seconds := int64(2345678901)
-	nanos := int32(123456789)
-
-	var protoTS1 *types.Timestamp
-	protoTS2 := &types.Timestamp{
-		Seconds: seconds,
-		Nanos:   nanos,
-	}
-
-	_, errTS1 := ConvertTimestampToTimeOrError(protoTS1)
-	assert.Error(t, errTS1)
-
-	expectedTimeTS2 := time.Unix(seconds, int64(nanos))
-
-	timeTS2, errTS2 := ConvertTimestampToTimeOrError(protoTS2)
-	assert.NoError(t, errTS2)
-	assert.Equal(t, expectedTimeTS2.Local(), timeTS2.Local())
-}
-
-func TestConvertTimeToTimestampOrError(t *testing.T) {
-	seconds1 := int64(2345678901)
-	nanos1 := int32(123456789)
-	time1 := time.Unix(seconds1, int64(nanos1))
-
-	protoTS1, errTS1 := ConvertTimeToTimestampOrError(time1)
-	assert.NoError(t, errTS1)
-	assert.Equal(t, &types.Timestamp{Seconds: seconds1, Nanos: nanos1}, protoTS1)
-}
-
 func TestCompareTimestamps(t *testing.T) {
 	protoTS1 := &types.Timestamp{
 		Seconds: 2345678901,
