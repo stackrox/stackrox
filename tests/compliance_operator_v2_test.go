2401759aa0c8032d7ab756edc12548bdc59db755
Lance Bragstad <lbragsta@redhat.com>
2023-12-21 08:37:09 -0600

    Add e2e test for v2 compliance integrations
    Now that we have plumbing in place to stand up the Compliance Operator
    alongside ACS, and configure ACS to use the new v2 compliance work,
    let's start adding some e2e tests that exercise the API.
    
    This commit adds a basic example that ensures there is at least one
    integration available on a secured cluster, and that it is installed
    into an expected namespace.

--- tests/compliance_operator_v2_test.go
+++ tests/compliance_operator_v2_test.go
@@ -3,8 +3,25 @@
 package tests
 
 import (
+	"context"
 	"testing"
+
+	v2 "github.com/stackrox/rox/generated/api/v2"
+	"github.com/stackrox/rox/pkg/testutils/centralgrpc"
+	"github.com/stretchr/testify/assert"
 )
 
 // ACS API test suite for integration testing for the Compliance Operator.
-func TestComplianceV2(t *testing.T) {}
+func TestComplianceV2Integration(t *testing.T) {
+	conn := centralgrpc.GRPCConnectionToCentral(t)
+	client := v2.NewComplianceIntegrationServiceClient(conn)
+
+	q := &v2.RawQuery{Query: ""}
+	resp, err := client.ListComplianceIntegrations(context.TODO(), q)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Len(t, resp.Integrations, 1, "failed to assert there is only a single compliance integration")
+	assert.Equal(t, resp.Integrations[0].ClusterName, "remote", "failed to find integration for cluster called \"remote\"")
+	assert.Equal(t, resp.Integrations[0].Namespace, "openshift-compliance", "failed to find integration for \"openshift-compliance\" namespace")
+}
