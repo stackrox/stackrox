0dad5c17a54536442d0d961d2199dd3cd83eec43
Moritz Clasmeier <mclasmeier@redhat.com>
2024-01-02 11:37:17 +0100

    Rebase and revert unintended changes to shared templates

--- pkg/helm/charts/tests/centralservices/helmtest_test.go
+++ pkg/helm/charts/tests/centralservices/helmtest_test.go
@@ -4,9 +4,19 @@ import (
 	"testing"
 
 	"github.com/stackrox/rox/image"
+	"github.com/stackrox/rox/pkg/helm/charts"
 	helmChartTestUtils "github.com/stackrox/rox/pkg/helm/charts/testutils"
 )
 
+func configureMetaValues(metaValues *charts.MetaValues) {
+	// Activate certain feature flags.
+	// This allows us to execute tests for features which are currenly disabled by default.
+	metaValues.FeatureFlags["ROX_SCANNER_V4"] = "true"
+}
+
 func TestWithHelmtest(t *testing.T) {
-	helmChartTestUtils.RunHelmTestSuite(t, "testdata/helmtest", image.CentralServicesChartPrefix, helmChartTestUtils.RunHelmTestSuiteOpts{})
+	testSuiteOpts := helmChartTestUtils.RunHelmTestSuiteOpts{
+		MetaValuesOverridesFunc: configureMetaValues,
+	}
+	helmChartTestUtils.RunHelmTestSuite(t, "testdata/helmtest", image.CentralServicesChartPrefix, testSuiteOpts)
 }
