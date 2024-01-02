78e56a1292b27a3e45d16d15c5248803f6e5ea34
Yann Brillouet <ybrillou@redhat.com>
2024-01-02 16:30:43 +0100

    Move abstraction layer to own package

---
+++ pkg/protocompat/time.go
@@ -0,0 +1,29 @@
+package protocompat
+
+import (
+	"time"
+
+	gogoTimestamp "github.com/gogo/protobuf/types"
+)
+
+// TimestampNow returns a protobuf timestamp set to the current time.
+func TimestampNow() *gogoTimestamp.Timestamp {
+	return gogoTimestamp.TimestampNow()
+}
+
+// ConvertTimestampToTimeOrError converts a proto timestamp to a golang Time, or returns an error if there is one.
+func ConvertTimestampToTimeOrError(gogo *gogoTimestamp.Timestamp) (time.Time, error) {
+	return gogoTimestamp.TimestampFromProto(gogo)
+}
+
+// ConvertTimeToTimestampOrError converts golang time to proto timestamp.
+func ConvertTimeToTimestampOrError(goTime time.Time) (*gogoTimestamp.Timestamp, error) {
+	return gogoTimestamp.TimestampProto(goTime)
+}
+
+// CompareTimestamps compares two timestamps and returns zero if equal, a negative value if
+// the first timestamp is before the second or a positive value if the first timestamp is
+// after the second.
+func CompareTimestamps(t1 *gogoTimestamp.Timestamp, t2 *gogoTimestamp.Timestamp) int {
+	return t1.Compare(t2)
+}
