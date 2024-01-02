0af15bab9cf24057042dc279e0f1fd512d0e8a55
Yann Brillouet <ybrillou@redhat.com>
2024-01-02 17:53:10 +0100

    Move compatibility layer functions to protocompat

--- pkg/protoconv/time.go
+++ pkg/protoconv/time.go
@@ -23,16 +23,6 @@ func ConvertGoGoProtoTimeToGolangProtoTime(gogo *gogoTimestamp.Timestamp) *golan
 	}
 }
 
-// TimestampNow returns a protobuf timestamp set to the current time.
-func TimestampNow() *gogoTimestamp.Timestamp {
-	return gogoTimestamp.TimestampNow()
-}
-
-// ConvertTimestampToTimeOrError converts a proto timestamp to a golang Time, or returns an error if there is one.
-func ConvertTimestampToTimeOrError(gogo *gogoTimestamp.Timestamp) (time.Time, error) {
-	return gogoTimestamp.TimestampFromProto(gogo)
-}
-
 // ConvertTimestampToTimeOrNow converts a proto timestamp to a golang Time, and returns time.Now() if there is an error.
 func ConvertTimestampToTimeOrNow(gogo *gogoTimestamp.Timestamp) time.Time {
 	return ConvertTimestampToTimeOrDefault(gogo, time.Now())
@@ -45,11 +35,6 @@ func ConvertTimestampToTimeOrDefault(gogo *gogoTimestamp.Timestamp, defaultVal t
 		return defaultVal
 	}
 	return t
-}
-
-// ConvertTimeToTimestampOrError converts golang time to proto timestamp.
-func ConvertTimeToTimestampOrError(goTime time.Time) (*gogoTimestamp.Timestamp, error) {
-	return gogoTimestamp.TimestampProto(goTime)
 }
 
 // ConvertTimeToTimestampOrNow converts golang time to proto timestamp.
@@ -86,11 +71,4 @@ func MustConvertTimeToTimestamp(goTime time.Time) *gogoTimestamp.Timestamp {
 		panic(err)
 	}
 	return t
-}
-
-// CompareTimestamps compares two timestamps and returns zero if equal, a negative value if
-// the first timestamp is before the second or a positive value if the first timestamp is
-// after the second.
-func CompareTimestamps(t1 *gogoTimestamp.Timestamp, t2 *gogoTimestamp.Timestamp) int {
-	return t1.Compare(t2)
 }
