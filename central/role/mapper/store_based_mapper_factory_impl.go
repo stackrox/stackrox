9475d76a099aa47b7ec2e5ffa351418a713e33e3
Daniel Haus <dhaus@redhat.com>
2024-01-02 06:59:04 +0100

    fix

--- central/role/mapper/store_based_mapper_factory_impl.go
+++ central/role/mapper/store_based_mapper_factory_impl.go
@@ -3,16 +3,18 @@ package mapper
 import (
 	groupDataStore "github.com/stackrox/rox/central/group/datastore"
 	roleDataStore "github.com/stackrox/rox/central/role/datastore"
+	teamsDataStore "github.com/stackrox/rox/central/teams/datastore"
 	userDataStore "github.com/stackrox/rox/central/user/datastore"
 	"github.com/stackrox/rox/pkg/auth/permissions"
 )
 
 // NewStoreBasedMapperFactory returns a new instance of a Factory which will use the given stores to create RoleMappers.
-func NewStoreBasedMapperFactory(groups groupDataStore.DataStore, roles roleDataStore.DataStore, users userDataStore.DataStore) permissions.RoleMapperFactory {
+func NewStoreBasedMapperFactory(groups groupDataStore.DataStore, roles roleDataStore.DataStore, users userDataStore.DataStore, teams teamsDataStore.DataStore) permissions.RoleMapperFactory {
 	return &storeBasedMapperFactoryImpl{
 		groups: groups,
 		roles:  roles,
 		users:  users,
+		teams:  teams,
 	}
 }
 
@@ -20,6 +22,7 @@ type storeBasedMapperFactoryImpl struct {
 	groups groupDataStore.DataStore
 	roles  roleDataStore.DataStore
 	users  userDataStore.DataStore
+	teams  teamsDataStore.DataStore
 }
 
 // GetRoleMapper returns a role mapper for the given auth provider.
@@ -29,5 +32,6 @@ func (rm *storeBasedMapperFactoryImpl) GetRoleMapper(authProviderID string) perm
 		groups:         rm.groups,
 		roles:          rm.roles,
 		users:          rm.users,
+		teams:          rm.teams,
 	}
 }
