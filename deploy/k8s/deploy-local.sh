52e1bfb95e56a593388746d74dadd1af7be6b804
Dmitrii Dolgov <9erthalion6@gmail.com>
2024-01-02 16:15:59 +0100

    [Tests] Set core_bpf a default collection method
    Make core_bpf a default one for any kind of tests.

--- deploy/k8s/deploy-local.sh
+++ deploy/k8s/deploy-local.sh
@@ -5,7 +5,7 @@ set -e
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # assuming deployment on Kubernetes in Docker for Mac or Minikube
-export COLLECTION_METHOD="${COLLECTION_METHOD:-ebpf}"
+export COLLECTION_METHOD="${COLLECTION_METHOD:-core_bpf}"
 export MONITORING_SUPPORT="${MONITORING_SUPPORT:-false}"
 export POD_SECURITY_POLICIES="${POD_SECURITY_POLICIES:-false}"
 
