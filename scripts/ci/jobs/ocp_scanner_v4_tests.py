8f2b16a9be8a13a3b8122027ca220af10028ab4c
Moritz Clasmeier <111092021+mclasmeier@users.noreply.github.com>
2024-01-03 14:09:16 +0100

    ROX-21562: Add test stub for scanner v4 tests on OCP (#9197)
    Co-authored-by: Moritz Clasmeier <mclasmeier@redhat.com>

---
+++ scripts/ci/jobs/ocp_scanner_v4_tests.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env -S python3 -u
+
+"""
+Run the Scanner V4 tests in an OCP cluster
+"""
+
+# At the moment this is just here for letting CI pass when a new CI test step has been added
+# to the openshift/release repo.
+
+print("UNIMPLEMENTED")
