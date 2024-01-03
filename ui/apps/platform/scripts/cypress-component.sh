3f2fcc1bd23f65452267cd5c89b9cf6099c10887
David Vail <dcvail17@gmail.com>
2024-01-03 15:51:16 -0500

    Create dir before redirecting err

--- ui/apps/platform/scripts/cypress-component.sh
+++ ui/apps/platform/scripts/cypress-component.sh
@@ -3,4 +3,5 @@ artifacts_dir="${TEST_RESULTS_OUTPUT_DIR:-cypress/test-results}/artifacts"
 export CYPRESS_VIDEOS_FOLDER="${artifacts_dir}/videos"
 export CYPRESS_SCREENSHOTS_FOLDER="${artifacts_dir}/screenshots"
 
+mkdir -p "$artifacts_dir" && touch "${artifacts_dir:-/tmp}/cypress-err.txt"
 DEBUG="*" NO_COLOR=1 cypress "$@" 2> "${artifacts_dir:-/tmp}/cypress-err.txt"
