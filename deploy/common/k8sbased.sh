6dc746f9f1cce999bb0fe9278cd12cc8fae0680d
Simon Baumer <baeumer@pm.me>
2024-01-04 14:38:57 +0100

    WIP

--- deploy/common/k8sbased.sh
+++ deploy/common/k8sbased.sh
@@ -56,7 +56,8 @@ function hotload_binary {
 
   binary_path=$(realpath "$(git rev-parse --show-toplevel)/bin/linux_amd64/${local_name}")
   # set custom source path, e.g. when StackRox source is mounted into kind.
-  if -z $ROX_LOCAL_SOURCE_PATH; then
+  if [[ -n "$ROX_LOCAL_SOURCE_PATH" ]]; then
+      echo "ROX_LOCAL_SOURCE_PATH is set to $ROX_LOCAL_SOURCE_PATH."
       binary_path="$ROX_LOCAL_SOURCE_PATH/bin/linux_amd64/${local_name}"
   fi
 
