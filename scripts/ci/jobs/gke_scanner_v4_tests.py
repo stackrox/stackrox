771ddf01fdf06e8403cecc6e2809fe190a5f1fb5
Moritz Clasmeier <mclasmeier@redhat.com>
2024-01-04 11:02:00 +0100

    Disable PostClusterTest since the test cases already include a teardown

--- scripts/ci/jobs/gke_scanner_v4_tests.py
+++ scripts/ci/jobs/gke_scanner_v4_tests.py
@@ -15,12 +15,17 @@ os.environ["STORE_METRICS"] = "true"
 os.environ["ROX_POSTGRES_DATASTORE"] = "true"
 os.environ["ROX_BASELINE_GENERATION_DURATION"] = "5m"
 
+# The current Scanner v4 tests only test Scanner v4 installation in different scenarios.
+# Due to the way the tests are structured the different test cases include a teardown at the end.
+# This would cause the standard PostClusterTest would to fail, therefore we use the
+# NullPostTest() here.
+#
+# A seperate test focusing on Scanner v4 functionality (as opposed to just installation)
+# should use the standard PostClusterTest() machinery.
 ClusterTestRunner(
     cluster=GKECluster("scanner-v4-test", machine_type="e2-standard-8"),
     pre_test=PreSystemTests(),
     test=ScannerV4Test(),
-    post_test=PostClusterTest(
-        check_stackrox_logs=True,
-    ),
+    post_test=NullPostTest(),
     final_post=FinalPost(),
 ).run()
