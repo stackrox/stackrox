92fe8a70530b65727fa3b8a5f5a12bce8c5a3a16
Yann Brillouet <ybrillou@redhat.com>
2024-01-08 11:20:59 +0100

    refactor: use gogo DurationFromProto through compatibility layer

--- central/cve/node/datastore/datastore_impl.go
+++ central/cve/node/datastore/datastore_impl.go
@@ -179,7 +179,7 @@ func (ds *datastoreImpl) EnrichNodeWithSuppressedCVEs(node *storage.Node) {
 }
 
 func getSuppressExpiry(start *types.Timestamp, duration *types.Duration) (*types.Timestamp, error) {
-	d, err := types.DurationFromProto(duration)
+	d, err := protocompat.DurationFromProto(duration)
 	if err != nil || d == 0 {
 		return nil, err
 	}
