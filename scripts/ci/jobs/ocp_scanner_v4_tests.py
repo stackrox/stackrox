a4bc4724068eecc77fd0ee72811442826d511c97
Moritz Clasmeier <mclasmeier@redhat.com>
2024-01-10 11:33:55 +0100

    Try to run actual Scanner v4 test on OpenShift.

--- scripts/ci/jobs/ocp_scanner_v4_tests.py
+++ scripts/ci/jobs/ocp_scanner_v4_tests.py
@@ -3,6 +3,26 @@
 """
 Run the Scanner V4 tests in an OCP cluster
 """
+import os
+from runners import ClusterTestRunner
+from clusters import AutomationFlavorsCluster
+from ci_tests import ScannerV4Test
+from pre_tests import PreSystemTests
+from post_tests import NullPostTest, FinalPost
+
+os.environ["ORCHESTRATOR_FLAVOR"] = "openshift"
+os.environ["STORE_METRICS"] = "true"
+os.environ["ROX_POSTGRES_DATASTORE"] = "true"
+os.environ["ROX_BASELINE_GENERATION_DURATION"] = "5m"
+
+ClusterTestRunner(
+    cluster=AutomationFlavorsCluster(),
+    pre_test=PreSystemTests(),
+    test=ScannerV4Test(),
+    post_test=NullPostTest(),
+    final_post=FinalPost(),
+).run()
+
 
 # At the moment this is just here for letting CI pass when a new CI test step has been added
 # to the openshift/release repo.
