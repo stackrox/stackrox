159f38c6b72598602f7a5cddfe5732d3ae2ab369
Matthias Meidinger <mmeiding@redhat.com>
2024-01-11 09:50:41 +0100

    Switch to pointer receiver in pipeline

--- central/sensor/service/pipeline/enhancements/pipeline.go
+++ central/sensor/service/pipeline/enhancements/pipeline.go
@@ -34,22 +34,22 @@ func GetPipeline() pipeline.Fragment {
 	return NewEnhancementPipeline(enhancement.BrokerSingleton())
 }
 
-func (p pipelineImpl) OnFinish(_ string) {}
+func (p *pipelineImpl) OnFinish(_ string) {}
 
-func (p pipelineImpl) Capabilities() []centralsensor.CentralCapability {
+func (p *pipelineImpl) Capabilities() []centralsensor.CentralCapability {
 	return nil
 }
 
-func (p pipelineImpl) Match(msg *central.MsgFromSensor) bool {
+func (p *pipelineImpl) Match(msg *central.MsgFromSensor) bool {
 	return msg.GetDeploymentEnhancementResponse() != nil
 }
 
 // Run runs the pipeline template on the input and returns the output
-func (p pipelineImpl) Run(_ context.Context, _ string, msg *central.MsgFromSensor, _ common.MessageInjector) error {
+func (p *pipelineImpl) Run(_ context.Context, _ string, msg *central.MsgFromSensor, _ common.MessageInjector) error {
 	p.broker.NotifyDeploymentReceived(msg.GetDeploymentEnhancementResponse())
 	return nil
 }
 
-func (p pipelineImpl) Reconcile(_ context.Context, _ string, _ *reconciliation.StoreMap) error {
+func (p *pipelineImpl) Reconcile(_ context.Context, _ string, _ *reconciliation.StoreMap) error {
 	return nil
 }
