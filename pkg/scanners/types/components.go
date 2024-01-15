04e45a91831ded0c0c84cd267fd58d07308a413a
David Caravello <119438707+dcaravel@users.noreply.github.com>
2024-01-15 15:56:25 -0600

    add comments to fix style checks

--- pkg/scanners/types/components.go
+++ pkg/scanners/types/components.go
@@ -5,12 +5,15 @@ import (
 	v1 "github.com/stackrox/scanner/generated/scanner/api/v1"
 )
 
+// ScanComponents holds the different types of components (a.k.a index reports)
+// used by scanners when performing vulnerability matching.
 type ScanComponents struct {
 	v1comps        *v1.Components
 	v4comps        *v4.Contents
 	indexerVersion string
 }
 
+// NewScanComponents creates a new ScanComponents.
 func NewScanComponents(indexerVersion string, v1comps *v1.Components, v4comps *v4.Contents) *ScanComponents {
 	return &ScanComponents{
 		v1comps:        v1comps,
@@ -19,14 +22,18 @@ func NewScanComponents(indexerVersion string, v1comps *v1.Components, v4comps *v
 	}
 }
 
+// Clairify returns components used by the Clairify scanner.
 func (s *ScanComponents) Clairify() *v1.Components {
 	return s.v1comps
 }
 
+// ScannerV4 returns components used by the Scanner V4 scanner.
 func (s *ScanComponents) ScannerV4() *v4.Contents {
 	return s.v4comps
 }
 
+// ScannerType returns the scanner type for which components
+// have been populated for.
 func (s *ScanComponents) ScannerType() string {
 	if ScannerV4IndexerVersion(s.indexerVersion) {
 		return ScannerV4
