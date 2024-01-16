2a255f0aec2efa11eef734674b12c4e6c621e04f
RTann <rtannenb@redhat.com>
2024-01-16 08:50:30 -0800

    add TODO for alternate nodejs config

--- pkg/env/scannerv4.go
+++ pkg/env/scannerv4.go
@@ -8,5 +8,6 @@ var (
 	ScannerV4MaxRespMsgSize = RegisterIntegerSetting("ROX_SCANNER_V4_GRPC_MAX_RESPONSE_SIZE", 12*size.MB)
 
 	// ScannerV4NodeJSSupport specifies if Scanner v4 should support indexing/vuln matching NodeJS (npm) packages.
+	// TODO(ROX-21768): Support another alternative: show only NodeJS packages affected by fixable vulns (like Scanner v2).
 	ScannerV4NodeJSSupport = RegisterBooleanSetting("ROX_SCANNER_V4_NODE_JS_SUPPORT", true)
 )
