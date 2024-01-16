7e8878aeda3317f2adc190ad2e5eb650ace7dd62
Johannes Malsam <jmalsam@redhat.com>
2024-01-16 16:58:13 +0100

    add imageoverrides for secured cluster images

--- operator/pkg/securedcluster/values/translation/image_overrides.go
+++ operator/pkg/securedcluster/values/translation/image_overrides.go
@@ -4,10 +4,13 @@ import "github.com/stackrox/rox/operator/pkg/images"
 
 var (
 	imageOverrides = images.Overrides{
-		images.Main:          "image.main.fullRef",
-		images.CollectorSlim: "image.collector.slim.fullRef",
-		images.CollectorFull: "image.collector.full.fullRef",
-		images.ScannerSlim:   "image.scanner.fullRef",
-		images.ScannerSlimDB: "image.scannerDb.fullRef",
+		images.Main:             "image.main.fullRef",
+		images.CollectorSlim:    "image.collector.slim.fullRef",
+		images.CollectorFull:    "image.collector.full.fullRef",
+		images.ScannerSlim:      "image.scanner.fullRef",
+		images.ScannerSlimDB:    "image.scannerDb.fullRef",
+		images.ScannerV4DB:      "image.scannerV4.db.fullRef",
+		images.ScannerV4Indexer: "scannerV4.indexer.image.fullRef",
+		images.ScannerV4Matcher: "scannerV4.matcher.image.fullRef",
 	}
 )
