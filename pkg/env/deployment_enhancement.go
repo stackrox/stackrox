519796b12b5c3dba2800103507a9db36540ee91d
Matthias Meidinger <mmeiding@redhat.com>
2024-01-17 11:32:26 +0100

    Introduce env var for enhancement queue size

--- pkg/env/deployment_enhancement.go
+++ pkg/env/deployment_enhancement.go
@@ -6,4 +6,8 @@ var (
 	// CentralDeploymentEnhancementTimeout allows to configure the time Central waits for Sensor to answer to a
 	// DeploymentEnhancementRequest.
 	CentralDeploymentEnhancementTimeout = registerDurationSetting("ROX_CENTRAL_DEPLOYMENT_ENHANCE_TIMEOUT", 30*time.Second)
+
+	// SensorEnhancementQueueSize configures the size of the buffered channel that incoming
+	// DeploymentEnhancementRequests are queued in
+	SensorEnhancementQueueSize = RegisterIntegerSetting("ROX_SENSOR_ENHANCEMENT_QUEUE_SIZE", 50)
 )
