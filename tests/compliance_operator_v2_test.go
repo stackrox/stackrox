dcdce9400383ee6e448363419607bd980b45f1c8
Surabhi Kumar <surakuma@redhat.com>
2024-01-17 15:17:26 -0800

    Increased sleep time for scan config test

--- tests/compliance_operator_v2_test.go
+++ tests/compliance_operator_v2_test.go
@@ -60,7 +60,7 @@ func TestComplianceV2CreateGetScanConfigurations(t *testing.T) {
 	clusters, err := serviceCluster.GetClusters(ctx, &v1.GetClustersRequest{})
 	assert.NoError(t, err)
 	clusterID := clusters.GetClusters()[0].GetId()
-	testName := fmt.Sprintf("test_%s", uuid.NewV4().String())
+	testName := fmt.Sprintf("test-%s", uuid.NewV4().String())
 	req := &v2.ComplianceScanConfiguration{
 		ScanName: testName,
 		Id:       "",
@@ -114,8 +114,8 @@ func TestComplianceV2CreateGetScanConfigurations(t *testing.T) {
 		}
 		return errors.New("scan result not found")
 	}, retry.BetweenAttempts(func(previousAttemptNumber int) {
-		time.Sleep(2 * time.Second)
-	}), retry.Tries(20))
+		time.Sleep(60 * time.Second)
+	}), retry.Tries(10))
 	assert.NoError(t, err)
 }
 
@@ -129,7 +129,7 @@ func TestComplianceV2DeleteComplianceScanConfigurations(t *testing.T) {
 	assert.NoError(t, err)
 
 	clusterID := clusters.GetClusters()[0].GetId()
-	testName := fmt.Sprintf("test_%s", uuid.NewV4().String())
+	testName := fmt.Sprintf("test-%s", uuid.NewV4().String())
 	req := &v2.ComplianceScanConfiguration{
 		ScanName: testName,
 		Id:       "",
