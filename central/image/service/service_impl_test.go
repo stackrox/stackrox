792b9a3a65ec9c876f42dbec421c3483659dbc6a
David Caravello <119438707+dcaravel@users.noreply.github.com>
2024-01-17 18:07:16 -0600

    ROX-21041: ScannerV4 Delegated Scans: Handle Edge Cases (3/3) (#9352)

--- central/image/service/service_impl_test.go
+++ central/image/service/service_impl_test.go
@@ -3,8 +3,13 @@ package service
 import (
 	"testing"
 
+	iiStore "github.com/stackrox/rox/central/imageintegration/store"
+	v1 "github.com/stackrox/rox/generated/api/v1"
 	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/features"
 	"github.com/stackrox/rox/pkg/grpc/testutils"
+	pkgTestUtils "github.com/stackrox/rox/pkg/testutils"
+	"github.com/stackrox/rox/pkg/timestamp"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -50,4 +55,94 @@ func TestBuildNames(t *testing.T) {
 		assert.Len(t, names, 1)
 		assert.Equal(t, srcImage.GetFullName(), names[0].GetFullName())
 	})
+}
+
+func TestShouldUpdateExistingScan(t *testing.T) {
+	// These variables exist for readability.
+	var noExistingImg *storage.Image
+	var emptyReq *v1.EnrichLocalImageInternalRequest
+	feature := true
+	update := true
+	imgExists := true
+	v4DataSource := &storage.DataSource{Id: iiStore.DefaultScannerV4Integration.GetId()}
+	v4MatchReq := &v1.EnrichLocalImageInternalRequest{IndexerVersion: "v4"}
+	v2MatchReq := &v1.EnrichLocalImageInternalRequest{}
+	v2ExpiredScan := &storage.Image{Scan: &storage.ImageScan{ScanTime: timestamp.NowMinus(reprocessInterval * 2)}}
+	v2CurrentScan := &storage.Image{Scan: &storage.ImageScan{ScanTime: timestamp.NowMinus(0)}}
+	v4ExpiredScan := &storage.Image{Scan: &storage.ImageScan{ScanTime: v2ExpiredScan.Scan.ScanTime, DataSource: v4DataSource}}
+	v4CurrentScan := &storage.Image{Scan: &storage.ImageScan{ScanTime: timestamp.NowMinus(0), DataSource: v4DataSource}}
+
+	testCases := []struct {
+		desc           string
+		featureEnabled bool
+		imgExists      bool
+		existingImg    *storage.Image
+		req            *v1.EnrichLocalImageInternalRequest
+		expected       bool
+	}{
+		// Scanner V4 feature disabled.
+		{
+			"update if no existing image",
+			!feature, !imgExists, noExistingImg, emptyReq, update,
+		},
+		{
+			"update if no existing scan",
+			!feature, imgExists, &storage.Image{}, emptyReq, update,
+		},
+		{
+			"update if scan expired",
+			!feature, imgExists, v2ExpiredScan, emptyReq, update,
+		},
+		{
+			"no update if scan is current",
+			!feature, imgExists, v2CurrentScan, emptyReq, !update,
+		},
+		// Scanner V4 feature enabled.
+		{
+			"update if no existing image (feature enabled)",
+			feature, !imgExists, noExistingImg, emptyReq, update,
+		},
+		{
+			"update if no existing scan (feature enabled)",
+			feature, imgExists, &storage.Image{}, emptyReq, update,
+		},
+		{
+			"update if v2 scan expired and match request for v4",
+			feature, imgExists, v2ExpiredScan, v4MatchReq, update,
+		},
+		{
+			"update if v2 scan expired and match request for v2",
+			feature, imgExists, v2ExpiredScan, v2MatchReq, update,
+		},
+		{
+			"no update if v2 scan NOT expired and match request for v2",
+			feature, imgExists, v2CurrentScan, v2MatchReq, !update,
+		},
+		{
+			"update if v4 scan expired and match request for v4",
+			feature, imgExists, v4ExpiredScan, v4MatchReq, update,
+		},
+		{
+			"no update if v4 scan NOT expired and match request for v4",
+			feature, imgExists, v4CurrentScan, v4MatchReq, !update,
+		},
+		{
+			"no update if v4 scan expired and match request for v2",
+			feature, imgExists, v4ExpiredScan, v2MatchReq, !update,
+		},
+		{
+			"update if v2 scan NOT expired and match request for v4",
+			feature, imgExists, v2CurrentScan, v4MatchReq, update,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			pkgTestUtils.MustUpdateFeature(t, features.ScannerV4, tc.featureEnabled)
+
+			actual := shouldUpdateExistingScan(tc.imgExists, tc.existingImg, tc.req)
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+
 }
