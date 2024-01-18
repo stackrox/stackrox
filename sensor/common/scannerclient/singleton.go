a61075ee8508e9a5d59c6a8a914d702d6dc13a6f
David Caravello <119438707+dcaravel@users.noreply.github.com>
2024-01-17 21:17:26 -0600

    ROX-21041: ScannerV4 Delegated Scans: Use Central Caps (2/3) (#9350)

--- sensor/common/scannerclient/singleton.go
+++ sensor/common/scannerclient/singleton.go
@@ -1,15 +1,17 @@
 package scannerclient
 
 import (
+	"github.com/stackrox/rox/pkg/centralsensor"
 	"github.com/stackrox/rox/pkg/env"
 	"github.com/stackrox/rox/pkg/features"
 	"github.com/stackrox/rox/pkg/sync"
 	"github.com/stackrox/rox/pkg/utils"
+	"github.com/stackrox/rox/sensor/common/centralcaps"
 )
 
 var (
-	once          sync.Once
-	scannerClient ScannerClient
+	scannerClient      ScannerClient
+	scannerClientMutex sync.Mutex
 
 	isScannerV4Enabled = features.ScannerV4.Enabled()
 )
@@ -17,19 +19,41 @@ var (
 // GRPCClientSingleton returns a gRPC ScannerClient to a local Scanner.
 // Only one ScannerClient per Sensor is required.
 func GRPCClientSingleton() ScannerClient {
-	once.Do(func() {
-		if !env.LocalImageScanningEnabled.BooleanSetting() {
-			log.Infof("scanner disabled: %s is false, will not attempt to connect to a local scanner",
-				env.LocalImageScanningEnabled.EnvVar())
-			return
-		}
-		var err error
-		if isScannerV4Enabled {
-			scannerClient, err = dialV4()
-		} else {
-			scannerClient, err = dialV2()
-		}
-		utils.Should(err)
-	})
+	scannerClientMutex.Lock()
+	defer scannerClientMutex.Unlock()
+
+	if scannerClient != nil {
+		return scannerClient
+	}
+
+	if !env.LocalImageScanningEnabled.BooleanSetting() {
+		log.Infof("scanner disabled: %s is false, will not attempt to connect to a local scanner",
+			env.LocalImageScanningEnabled.EnvVar())
+		return nil
+	}
+	var err error
+	if isScannerV4Enabled && centralcaps.Has(centralsensor.ScannerV4Supported) {
+		log.Info("Creating Scanner V4 client")
+		scannerClient, err = dialV4()
+	} else {
+		log.Info("Creating Scanner V2 client")
+		scannerClient, err = dialV2()
+	}
+	utils.Should(err)
+
 	return scannerClient
+}
+
+// resetGRPCClient resets the current scanner client so that it will be recreated
+// on next retrieval.
+func resetGRPCClient() {
+	scannerClientMutex.Lock()
+	defer scannerClientMutex.Unlock()
+
+	if scannerClient == nil {
+		return
+	}
+
+	utils.Should(scannerClient.Close())
+	scannerClient = nil
 }
