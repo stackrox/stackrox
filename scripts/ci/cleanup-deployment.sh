b2c952b21c0f13a356518522040b548b180862db
Moritz Clasmeier <mclasmeier@redhat.com>
2024-01-18 21:22:40 +0100

    Use env bash in the shebang line

--- scripts/ci/cleanup-deployment.sh
+++ scripts/ci/cleanup-deployment.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
 namespace=${1:-stackrox}
 
--- scripts/k8s/get-lb-ip.sh
+++ scripts/k8s/get-lb-ip.sh
@@ -1,8 +1,8 @@
-#! /bin/sh
+#!/usr/bin/env bash
 
 NAMESPACE=${1:-stackrox}
-
 LB_IP=""
+
 until [ -n "${LB_IP}" ] && [ "${LB_IP}" != "null" ]; do
     LB_IP=$(kubectl -n "${NAMESPACE}" get svc/central-loadbalancer -o json | jq -r '.status.loadBalancer.ingress[0] | .ip // .hostname')
     sleep 1
