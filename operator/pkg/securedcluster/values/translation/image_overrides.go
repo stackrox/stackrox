c72d97fc547b86e47b4316a786155f3f330fdf44
Johannes Malsam <jmalsam@redhat.com>
2024-01-19 15:57:55 +0100

    fix image overrides

--- operator/pkg/securedcluster/values/translation/image_overrides.go
+++ operator/pkg/securedcluster/values/translation/image_overrides.go
@@ -9,8 +9,7 @@ var (
 		images.CollectorFull:    "image.collector.full.fullRef",
 		images.ScannerSlim:      "image.scanner.fullRef",
 		images.ScannerSlimDB:    "image.scannerDb.fullRef",
-		images.ScannerV4DB:      "image.scannerV4.db.fullRef",
-		images.ScannerV4Indexer: "image.scannerV4.indexer.fullRef",
-		images.ScannerV4Matcher: "image.scannerV4.matcher.fullRef",
+		images.ScannerV4DB:      "image.scannerV4DB.fullRef",
+		images.ScannerV4Indexer: "image.scannerV4.fullRef",
 	}
 )
