9bb2106bda7b729bf7c8d19b2ba559ce328e9f61
Moritz Clasmeier <mclasmeier@redhat.com>
2024-01-19 11:02:59 +0100

    renamed namespace -> sensor_namespace

--- scripts/ci/sensor-wait.sh
+++ scripts/ci/sensor-wait.sh
@@ -8,21 +8,21 @@ set -eu
 #
 
 sensor_wait() {
-    local namespace=${1:-stackrox}
+    local sensor_namespace=${1:-stackrox}
 
-    echo "Waiting for sensor to start in namespace ${namespace}"
+    echo "Waiting for sensor to start in namespace ${sensor_namespace}"
 
     start_time="$(date '+%s')"
     while true; do
-      sensor_json="$(kubectl -n "${namespace}" get deploy/sensor -o json)"
+      sensor_json="$(kubectl -n "${sensor_namespace}" get deploy/sensor -o json)"
       if [[ "$(jq '.status.replicas' <<<"${sensor_json}")" == 1 && "$(jq '.status.readyReplicas' <<<"${sensor_json}")" == 1 ]]; then
         break
       fi
       echo "Sensor replicas: $(jq '.status.replicas' <<<"${sensor_json}")"
       echo "Sensor readyReplicas: $(jq '.status.readyReplicas' <<<"${sensor_json}")"
       if (( $(date '+%s') - start_time > 1200 )); then
-        kubectl -n "${namespace}" get pod -o wide
-        kubectl -n "${namespace}" get deploy -o wide
+        kubectl -n "${sensor_namespace}" get pod -o wide
+        kubectl -n "${sensor_namespace}" get deploy -o wide
         echo >&2 "Timed out after 20m"
         exit 1
       fi
@@ -30,18 +30,18 @@ sensor_wait() {
     done
     echo "Sensor is running"
 
-    if ! kubectl -n "${namespace}" get ds/collector ; then
+    if ! kubectl -n "${sensor_namespace}" get ds/collector ; then
         return
     fi
 
     echo "Waiting for collectors to start"
     start_time="$(date '+%s')"
-    until [ "$(kubectl -n "${namespace}" get ds/collector | tail -n +2 | awk '{print $2}')" -eq "$(kubectl -n "${namespace}" get ds/collector | tail -n +2 | awk '{print $4}')" ]; do
-      echo "Desired collectors: $(kubectl -n "${namespace}" get ds/collector | tail -n +2 | awk '{print $2}')"
-      echo "Ready collectors: $(kubectl -n "${namespace}" get ds/collector | tail -n +2 | awk '{print $4}')"
+    until [ "$(kubectl -n "${sensor_namespace}" get ds/collector | tail -n +2 | awk '{print $2}')" -eq "$(kubectl -n "${sensor_namespace}" get ds/collector | tail -n +2 | awk '{print $4}')" ]; do
+      echo "Desired collectors: $(kubectl -n "${sensor_namespace}" get ds/collector | tail -n +2 | awk '{print $2}')"
+      echo "Ready collectors: $(kubectl -n "${sensor_namespace}" get ds/collector | tail -n +2 | awk '{print $4}')"
       if (( $(date '+%s') - start_time > 1200 )); then
-        kubectl -n "${namespace}" get pod -o wide
-        kubectl -n "${namespace}" get ds -o wide
+        kubectl -n "${sensor_namespace}" get pod -o wide
+        kubectl -n "${sensor_namespace}" get ds -o wide
         echo >&2 "Timed out after 20m"
         exit 1
       fi
