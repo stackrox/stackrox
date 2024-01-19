6bacc834d2bb6a41cff1ed354c0bc7be476c75da
Michaël <smartptr@gmail.com>
2024-01-19 18:46:34 +0100

    feat: `roxctl` to connect to central via port-forward (#7907)

---
+++ tests/roxctl/roxctl-k8s-context.sh
@@ -0,0 +1,43 @@
+#! /usr/bin/env bash
+
+# This test script requires ROX_PASSWORD to be set in the environment.
+
+[ -n "$ROX_PASSWORD" ]
+
+FAILURES=0
+
+eecho() {
+  echo "$@" >&2
+}
+
+test_roxctl_cmd() {
+  echo "Testing command: roxctl central whoami"
+  echo "Namespaces:"
+  kubectl get ns
+  echo "Switching the context to the stackrox namespace..."
+  kubectl config set-context --current --namespace stackrox
+  echo "Kube contexts:"
+  kubectl config get-contexts
+  echo "Central service endpoints:"
+  kubectl get ep central
+
+  # Verify central whoami using current k8s context.
+  if OUTPUT=$(roxctl --insecure-skip-tls-verify -p "$ROX_PASSWORD" central whoami --use-current-k8s-context \
+    2>&1); then
+      echo "[OK] roxctl central whoami using current k8s context works"
+  else
+      eecho "[FAIL] roxctl central whoami using current k8s context fails"
+      eecho "Captured output was:"
+      eecho "$OUTPUT"
+      FAILURES=$((FAILURES + 1))
+  fi
+}
+
+test_roxctl_cmd
+
+if [ $FAILURES -eq 0 ]; then
+  echo "Passed"
+else
+  echo "$FAILURES test failed"
+  exit 1
+fi
