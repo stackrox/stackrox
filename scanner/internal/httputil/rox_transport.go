ecaded1d58ca9c7b7f0648b755324916a73c797e
RTann <rtannenb@redhat.com>
2024-01-19 16:32:19 -0800

    update rox_transport

--- scanner/internal/httputil/rox_transport.go
+++ scanner/internal/httputil/rox_transport.go
@@ -1,12 +1,20 @@
 package httputil
 
 import (
+	"net"
 	"net/http"
+	"time"
 
 	"github.com/stackrox/rox/pkg/clientconn"
 	"github.com/stackrox/rox/pkg/httputil/proxy"
 	"github.com/stackrox/rox/pkg/mtls"
 )
+
+// defaultDialer is copied from http.DefaultTransport as of go1.20.10.
+var defaultDialer = &net.Dialer{
+	Timeout:   30 * time.Second,
+	KeepAlive: 30 * time.Second,
+}
 
 // RoxTransportOptions represents transport options for reaching out to Rox-related services.
 type RoxTransportOptions struct {
@@ -18,6 +26,7 @@ func RoxTransport(subject mtls.Subject, o RoxTransportOptions) (http.RoundTrippe
 	tlsConfig, err := clientconn.TLSConfig(subject, clientconn.TLSConfigOptions{
 		UseClientCert: clientconn.MustUseClientCert,
 	})
+
 	if err != nil {
 		return nil, err
 	}
@@ -25,5 +34,13 @@ func RoxTransport(subject mtls.Subject, o RoxTransportOptions) (http.RoundTrippe
 		Proxy:              proxy.FromConfig(),
 		TLSClientConfig:    tlsConfig,
 		DisableCompression: o.disableCompression,
+
+		// The rest are (more-or-less) copied from http.DefaultTransport as of go1.20.10.
+		DialContext:           defaultDialer.DialContext,
+		ForceAttemptHTTP2:     true,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
 	}, nil
 }
