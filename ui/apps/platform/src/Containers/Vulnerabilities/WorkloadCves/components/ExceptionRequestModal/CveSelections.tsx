aeb5e6c44c35d784256dcd1f803cf4bfb979bd03
Mark Pedrotti <mark@stackrox.com>
2024-01-22 14:40:38 -0500

    ROX-21838: Factor out reusable ExternalLink component

--- ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/components/ExceptionRequestModal/CveSelections.tsx
+++ ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/components/ExceptionRequestModal/CveSelections.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link, generatePath } from 'react-router-dom';
 import { Flex, List, ListItem, Text, pluralize } from '@patternfly/react-core';
-import { ExternalLinkAltIcon } from '@patternfly/react-icons';
 
+import ExternalLink from 'Components/PatternFly/IconText/ExternalLink';
 import { vulnerabilitiesWorkloadCvesPath } from 'routePaths';
 
 const vulnerabilitiesWorkloadCveSinglePath = `${vulnerabilitiesWorkloadCvesPath}/cves/:cve`;
@@ -18,14 +18,14 @@ function CveSelections({ cves }: CveSelectionsProps) {
                 <ListItem key={cve}>
                     <Flex direction={{ default: 'column' }}>
                         <Flex direction={{ default: 'row' }}>
-                            <Text>
+                            <ExternalLink>
                                 <Link
                                     target="_blank"
                                     to={generatePath(vulnerabilitiesWorkloadCveSinglePath, { cve })}
                                 >
-                                    {cve} <ExternalLinkAltIcon className="pf-u-display-inline" />
+                                    {cve}
                                 </Link>
-                            </Text>
+                            </ExternalLink>
                             <Text>Across {pluralize(numAffectedImages, 'image')}</Text>
                         </Flex>
                         <Text>{summary}</Text>
