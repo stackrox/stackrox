64c35bfeea1bf2d81d98ff6f63ca2db8a08f4f5f
David Shrewsberry <99685630+dashrews78@users.noreply.github.com>
2024-01-23 13:39:51 -0500

    ROX-20573:  compliance rules ingest (#9033)
    2 pipelines for rules.

--- pkg/postgres/schema/compliance_operator_rule_v2.go
+++ pkg/postgres/schema/compliance_operator_rule_v2.go
@@ -3,20 +3,29 @@
 package schema
 
 import (
+	"fmt"
 	"reflect"
 
+	v1 "github.com/stackrox/rox/generated/api/v1"
 	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/pkg/features"
 	"github.com/stackrox/rox/pkg/postgres"
 	"github.com/stackrox/rox/pkg/postgres/walker"
 	"github.com/stackrox/rox/pkg/sac/resources"
+	"github.com/stackrox/rox/pkg/search"
+	"github.com/stackrox/rox/pkg/search/postgres/mapping"
 )
 
 var (
 	// CreateTableComplianceOperatorRuleV2Stmt holds the create statement for table `compliance_operator_rule_v2`.
 	CreateTableComplianceOperatorRuleV2Stmt = &postgres.CreateStmts{
 		GormModel: (*ComplianceOperatorRuleV2)(nil),
-		Children:  []*postgres.CreateStmts{},
+		Children: []*postgres.CreateStmts{
+			&postgres.CreateStmts{
+				GormModel: (*ComplianceOperatorRuleV2Controls)(nil),
+				Children:  []*postgres.CreateStmts{},
+			},
+		},
 	}
 
 	// ComplianceOperatorRuleV2Schema is the go schema for table `compliance_operator_rule_v2`.
@@ -26,8 +35,17 @@ var (
 			return schema
 		}
 		schema = walker.Walk(reflect.TypeOf((*storage.ComplianceOperatorRuleV2)(nil)), "compliance_operator_rule_v2")
+		referencedSchemas := map[string]*walker.Schema{
+			"storage.Cluster": ClustersSchema,
+		}
+
+		schema.ResolveReferences(func(messageTypeName string) *walker.Schema {
+			return referencedSchemas[fmt.Sprintf("storage.%s", messageTypeName)]
+		})
+		schema.SetOptionsMap(search.Walk(v1.SearchCategory_COMPLIANCE_RULES, "complianceoperatorrulev2", (*storage.ComplianceOperatorRuleV2)(nil)))
 		schema.ScopingResource = resources.Compliance
 		RegisterTable(schema, CreateTableComplianceOperatorRuleV2Stmt, features.ComplianceEnhancements.Enabled)
+		mapping.RegisterCategoryToTable(v1.SearchCategory_COMPLIANCE_RULES, schema)
 		return schema
 	}()
 )
@@ -35,14 +53,24 @@ var (
 const (
 	// ComplianceOperatorRuleV2TableName specifies the name of the table in postgres.
 	ComplianceOperatorRuleV2TableName = "compliance_operator_rule_v2"
+	// ComplianceOperatorRuleV2ControlsTableName specifies the name of the table in postgres.
+	ComplianceOperatorRuleV2ControlsTableName = "compliance_operator_rule_v2_controls"
 )
 
 // ComplianceOperatorRuleV2 holds the Gorm model for Postgres table `compliance_operator_rule_v2`.
 type ComplianceOperatorRuleV2 struct {
-	Name            string               `gorm:"column:name;type:varchar;primaryKey"`
-	OperatorVersion string               `gorm:"column:operatorversion;type:varchar"`
-	RuleVersion     string               `gorm:"column:ruleversion;type:varchar"`
-	RuleType        string               `gorm:"column:ruletype;type:varchar"`
-	Severity        storage.RuleSeverity `gorm:"column:severity;type:integer"`
-	Serialized      []byte               `gorm:"column:serialized;type:bytea"`
+	ID         string               `gorm:"column:id;type:varchar;primaryKey"`
+	Name       string               `gorm:"column:name;type:varchar"`
+	RuleType   string               `gorm:"column:ruletype;type:varchar"`
+	Severity   storage.RuleSeverity `gorm:"column:severity;type:integer"`
+	ClusterID  string               `gorm:"column:clusterid;type:uuid;index:complianceoperatorrulev2_sac_filter,type:hash"`
+	Serialized []byte               `gorm:"column:serialized;type:bytea"`
+}
+
+// ComplianceOperatorRuleV2Controls holds the Gorm model for Postgres table `compliance_operator_rule_v2_controls`.
+type ComplianceOperatorRuleV2Controls struct {
+	ComplianceOperatorRuleV2ID  string                   `gorm:"column:compliance_operator_rule_v2_id;type:varchar;primaryKey"`
+	Idx                         int                      `gorm:"column:idx;type:integer;primaryKey;index:complianceoperatorrulev2controls_idx,type:btree"`
+	Standard                    string                   `gorm:"column:standard;type:varchar"`
+	ComplianceOperatorRuleV2Ref ComplianceOperatorRuleV2 `gorm:"foreignKey:compliance_operator_rule_v2_id;references:id;belongsTo;constraint:OnDelete:CASCADE"`
 }
--- tools/generate-helpers/pg-table-bindings/resource_type_test.go
+++ tools/generate-helpers/pg-table-bindings/resource_type_test.go
@@ -48,7 +48,7 @@ func TestGetResourceType(t *testing.T) {
 		{typ: &storage.ComplianceOperatorProfile{}, resourceType: globallyScoped},
 		{typ: &storage.ComplianceOperatorProfileV2{}, resourceType: indirectlyScoped},
 		{typ: &storage.ComplianceOperatorRule{}, resourceType: globallyScoped},
-		{typ: &storage.ComplianceOperatorRuleV2{}, resourceType: indirectlyScoped},
+		{typ: &storage.ComplianceOperatorRuleV2{}, resourceType: directlyScoped},
 		{typ: &storage.ComplianceOperatorScan{}, resourceType: globallyScoped},
 		{typ: &storage.ComplianceOperatorScanV2{}, resourceType: directlyScoped},
 		{typ: &storage.ComplianceOperatorScanConfigurationV2{}, resourceType: indirectlyScoped},
