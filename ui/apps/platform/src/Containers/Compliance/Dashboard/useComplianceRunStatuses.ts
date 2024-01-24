54d966b11600c3eb4675b237a68c5ca32d9b70c2
David Vail <dcvail17@gmail.com>
2024-01-24 13:07:31 -0500

    fix(ui): Add progress indicator for async Compliance 1.0 scans (#9363)

---
+++ ui/apps/platform/src/Containers/Compliance/Dashboard/useComplianceRunStatuses.ts
@@ -0,0 +1,111 @@
+import { useState } from 'react';
+import { gql, useApolloClient, useQuery } from '@apollo/client';
+
+import { resourceTypes } from 'constants/entityTypes';
+import {
+    AGGREGATED_RESULTS_ACROSS_ENTITY,
+    AGGREGATED_RESULTS_STANDARDS_BY_ENTITY,
+} from 'queries/controls';
+import {
+    CLUSTERS_COUNT,
+    DEPLOYMENTS_COUNT,
+    NAMESPACES_COUNT,
+    NODES_COUNT,
+} from './ComplianceDashboardTile';
+
+export type ComplianceRunStatusResponse = {
+    complianceRunStatuses: {
+        runs: { state: string }[];
+    };
+};
+
+function isCurrentScanIncomplete(
+    runs: ComplianceRunStatusResponse['complianceRunStatuses']['runs']
+) {
+    return runs.some((run) => run.state !== 'FINISHED');
+}
+
+const queriesToRefetchOnPollingComplete = [
+    CLUSTERS_COUNT,
+    NODES_COUNT,
+    NAMESPACES_COUNT,
+    DEPLOYMENTS_COUNT,
+    AGGREGATED_RESULTS_STANDARDS_BY_ENTITY(resourceTypes.CLUSTER),
+    AGGREGATED_RESULTS_ACROSS_ENTITY(resourceTypes.CLUSTER),
+    AGGREGATED_RESULTS_ACROSS_ENTITY(resourceTypes.NAMESPACE),
+    AGGREGATED_RESULTS_ACROSS_ENTITY(resourceTypes.NODE),
+];
+
+const complianceRunStatusesQuery = gql`
+    query runStatuses($latest: Boolean) {
+        complianceRunStatuses(latest: $latest) {
+            runs {
+                state
+            }
+        }
+    }
+`;
+
+const pollInterval = 10000;
+const variables = {
+    latest: true,
+};
+
+export type UseComplianceRunStatusesResponse = {
+    /* The latest compliance scan runs */
+    runs: ComplianceRunStatusResponse['complianceRunStatuses']['runs'];
+    error: unknown;
+    /* Fetches the latest compliance scan runs, and restarts polling, if necessary */
+    restartPolling: () => void;
+    /* Whether or not an in progress scan was detected during the lifetime of this hook */
+    inProgressScanDetected: boolean;
+    /* Whether or not the latest scan is incomplete */
+    isCurrentScanIncomplete: boolean;
+};
+
+export function useComplianceRunStatuses(): UseComplianceRunStatusesResponse {
+    const [inProgressScanDetected, setInProgressScanDetected] = useState(false);
+
+    const client = useApolloClient();
+
+    const { startPolling, stopPolling, error, data, refetch } = useQuery<
+        ComplianceRunStatusResponse,
+        typeof variables
+    >(complianceRunStatusesQuery, {
+        variables,
+        pollInterval,
+        onCompleted,
+        onError,
+        errorPolicy: 'all',
+        notifyOnNetworkStatusChange: true,
+        fetchPolicy: 'no-cache',
+    });
+
+    function onCompleted(data: ComplianceRunStatusResponse) {
+        if (isCurrentScanIncomplete(data.complianceRunStatuses.runs)) {
+            setInProgressScanDetected(true);
+        } else {
+            stopPolling();
+        }
+
+        return client.refetchQueries({ include: queriesToRefetchOnPollingComplete });
+    }
+
+    function onError() {
+        stopPolling();
+    }
+
+    return {
+        error,
+        runs: data?.complianceRunStatuses.runs ?? [],
+        restartPolling: () =>
+            refetch().then(({ data }) => {
+                if (isCurrentScanIncomplete(data.complianceRunStatuses.runs)) {
+                    setInProgressScanDetected(true);
+                    startPolling(pollInterval);
+                }
+            }),
+        inProgressScanDetected,
+        isCurrentScanIncomplete: isCurrentScanIncomplete(data?.complianceRunStatuses.runs ?? []),
+    };
+}
