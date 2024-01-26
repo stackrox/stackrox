9240d4500ca5131d9d6bceaeecf89f3f82842816
Khushboo Sancheti <42253461+clickboo@users.noreply.github.com>
2024-01-26 15:06:19 -0800

    ROX-21597: Scan setting bindings v2 pipeline, datastore and store. (#9505)

---
+++ central/complianceoperator/v2/scansettingbindings/datastore/datastore.go
@@ -0,0 +1,32 @@
+package datastore
+
+import (
+	"context"
+
+	pgStore "github.com/stackrox/rox/central/complianceoperator/v2/scansettingbindings/store/postgres"
+	"github.com/stackrox/rox/generated/storage"
+)
+
+// DataStore is the entry point for storing/retrieving compliance scan setting binding objects
+//
+//go:generate mockgen-wrapper
+type DataStore interface {
+	// GetScanSettingBinding retrieves the scan setting binding object from the database
+	GetScanSettingBinding(ctx context.Context, id string) (*storage.ComplianceOperatorScanSettingBindingV2, bool, error)
+
+	// UpsertScanSettingBinding adds the scan setting binding object to the database
+	UpsertScanSettingBinding(ctx context.Context, result *storage.ComplianceOperatorScanSettingBindingV2) error
+
+	// DeleteScanSettingBinding removes a scan setting binding object from the database
+	DeleteScanSettingBinding(ctx context.Context, id string) error
+
+	// GetScanSettingBindingsByCluster retrieves scan setting bindings by cluster
+	GetScanSettingBindingsByCluster(ctx context.Context, clusterID string) ([]*storage.ComplianceOperatorScanSettingBindingV2, error)
+}
+
+// New returns an instance of DataStore.
+func New(scanSettingBindingStorage pgStore.Store) DataStore {
+	return &datastoreImpl{
+		store: scanSettingBindingStorage,
+	}
+}
