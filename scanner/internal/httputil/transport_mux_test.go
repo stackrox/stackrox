922d12f7d73eb02aa91984902009c52f7dc4a44b
RTann <rtannenb@redhat.com>
2024-01-26 11:24:46 -0800

    add HTTP deny test

--- scanner/internal/httputil/transport_mux_test.go
+++ scanner/internal/httputil/transport_mux_test.go
@@ -12,9 +12,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestTransportMux(t *testing.T) {
-	t.Setenv(env.Namespace.EnvVar(), "rhacs")
-
+func testTransports(t *testing.T) (http.RoundTripper, http.RoundTripper, http.RoundTripper) {
 	defaultTransport := httputil.RoundTripperFunc(func(req *http.Request) (*http.Response, error) {
 		assert.Equal(t, "quay.io", req.URL.Host)
 		return &http.Response{
@@ -33,6 +31,13 @@ func TestTransportMux(t *testing.T) {
 			Body: io.NopCloser(strings.NewReader("Sensor")),
 		}, nil
 	})
+	return defaultTransport, centralTransport, sensorTransport
+}
+
+func TestTransportMux(t *testing.T) {
+	t.Setenv(env.Namespace.EnvVar(), "rhacs")
+
+	defaultTransport, centralTransport, sensorTransport := testTransports(t)
 
 	tr, err := transportMux(defaultTransport, options{
 		centralTransport: centralTransport,
@@ -62,6 +67,58 @@ func TestTransportMux(t *testing.T) {
 		},
 	} {
 		t.Run(testcase.name, func(t *testing.T) {
+			resp, err := c.Get(testcase.url)
+			require.NoError(t, err)
+			t.Cleanup(func() {
+				_ = resp.Body.Close
+			})
+			msg, err := io.ReadAll(resp.Body)
+			require.NoError(t, err)
+			assert.Equal(t, testcase.name, string(msg))
+		})
+	}
+}
+
+func TestTransportMux_deny(t *testing.T) {
+	t.Setenv(env.Namespace.EnvVar(), "rhacs")
+
+	defaultTransport, _, _ := testTransports(t)
+
+	tr, err := TransportMux(defaultTransport, WithDenyStackRoxServices(true))
+	require.NoError(t, err)
+
+	c := &http.Client{
+		Transport: tr,
+	}
+
+	for _, testcase := range []struct {
+		name      string
+		url       string
+		wantPanic bool
+	}{
+		{
+			name:      "Central",
+			url:       "https://central.rhacs.svc/api/extensions/scannerdefinitions?file=repo2cpe",
+			wantPanic: true,
+		},
+		{
+			name:      "Sensor",
+			url:       "https://sensor.rhacs.svc/api/extensions/scannerdefinitions?file=repo2cpe",
+			wantPanic: true,
+		},
+		{
+			name:      "Default",
+			url:       "https://quay.io/image_layer_query_here",
+			wantPanic: false,
+		},
+	} {
+		t.Run(testcase.name, func(t *testing.T) {
+			if testcase.wantPanic {
+				assert.Panics(t, func() {
+					_, _ = c.Get(testcase.url)
+				})
+				return
+			}
 			resp, err := c.Get(testcase.url)
 			require.NoError(t, err)
 			t.Cleanup(func() {
