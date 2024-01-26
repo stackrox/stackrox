4a055749a96f26728fdbb1dcb850d8f04ac333fa
Dave Shrewsberry <dashrews@redhat.com>
2024-01-26 10:11:53 -0500

    Revert "fix(indexer): Add Matcher to API auth (#9548)"
    This reverts commit 801e4495957747a42a22dbdf5b1f76995d1838f0.

--- scanner/services/indexer.go
+++ scanner/services/indexer.go
@@ -22,7 +22,7 @@ import (
 )
 
 var indexerAuth = perrpc.FromMap(map[authz.Authorizer][]string{
-	or.Or(idcheck.CentralOnly(), idcheck.SensorsOnly(), idcheck.ScannerV4MatcherOnly()): {
+	or.SensorOr(idcheck.CentralOnly()): {
 		"/scanner.v4.Indexer/CreateIndexReport",
 		"/scanner.v4.Indexer/GetIndexReport",
 		"/scanner.v4.Indexer/HasIndexReport",
