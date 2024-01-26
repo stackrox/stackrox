a53afbb33e226110ed77118fa5f53bd84842a0cf
Dave Shrewsberry <dashrews@redhat.com>
2024-01-26 10:11:43 -0500

    Revert "chore(scanner): retry DB connection upon startup (#9556)"
    This reverts commit 61823eeba29123976c6585fed3b7ac60aa591817.

--- scanner/indexer/indexer.go
+++ scanner/indexer/indexer.go
@@ -20,7 +20,7 @@ import (
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/quay/claircore"
 	"github.com/quay/claircore/alpine"
-	ccpostgres "github.com/quay/claircore/datastore/postgres"
+	"github.com/quay/claircore/datastore/postgres"
 	"github.com/quay/claircore/dpkg"
 	"github.com/quay/claircore/gobin"
 	ccindexer "github.com/quay/claircore/indexer"
@@ -37,7 +37,6 @@ import (
 	"github.com/stackrox/rox/pkg/env"
 	"github.com/stackrox/rox/pkg/utils"
 	"github.com/stackrox/rox/scanner/config"
-	"github.com/stackrox/rox/scanner/datastore/postgres"
 	"github.com/stackrox/rox/scanner/internal/version"
 )
 
@@ -98,7 +97,7 @@ func NewIndexer(ctx context.Context, cfg config.IndexerConfig) (Indexer, error) 
 		}
 	}()
 
-	store, err := ccpostgres.InitPostgresIndexerStore(ctx, pool, true)
+	store, err := postgres.InitPostgresIndexerStore(ctx, pool, true)
 	if err != nil {
 		return nil, fmt.Errorf("initializing postgres indexer store: %w", err)
 	}
