ff000fb0ec9a95846c8c2b44692252711f4816ed
Mark Pedrotti <mark@stackrox.com>
2024-01-31 11:40:17 -0500

    Update DiscoveredClustersTable before service is available

--- ui/apps/platform/src/Containers/Clusters/DiscoveredClusters/DiscoveredClustersTable.tsx
+++ ui/apps/platform/src/Containers/Clusters/DiscoveredClusters/DiscoveredClustersTable.tsx
@@ -1,22 +1,25 @@
 import React, { ReactElement } from 'react';
-import { TableComposable, Td, Th, Thead, Tr } from '@patternfly/react-table';
+import { TableComposable, Tbody, Td, Th, Thead, Tr } from '@patternfly/react-table';
 
 import { UseURLSortResult } from 'hooks/useURLSort';
 import { DiscoveredCluster, hasDiscoveredClustersFilter } from 'services/DiscoveredClustersService';
 import { SearchFilter } from 'types/search';
+import { getDistanceStrictAsPhrase } from 'utils/dateUtils';
 
 import DiscoveredClustersEmptyState from './DiscoveredClustersEmptyState';
 
-const colSpan = 4; // TODO separate Provider from
+const colSpan = 6;
 
 export type DiscoveredClustersTableProps = {
     clusters: DiscoveredCluster[];
+    currentDatetime: Date;
     getSortParams: UseURLSortResult['getSortParams'];
     searchFilter: SearchFilter;
 };
 
 function DiscoveredClustersTable({
     clusters,
+    currentDatetime,
     getSortParams,
     searchFilter,
 }: DiscoveredClustersTableProps): ReactElement {
@@ -24,10 +27,20 @@ function DiscoveredClustersTable({
         <TableComposable variant="compact" borders={false}>
             <Thead>
                 <Tr>
-                    <Th sort={getSortParams('TODO')}>Cluster</Th>
-                    <Th>Type</Th>
-                    <Th>Provider</Th>
-                    <Th>Region</Th>
+                    <Th width={25} sort={getSortParams('TODO')}>
+                        Cluster
+                    </Th>
+                    <Th width={15}>State</Th>
+                    <Th width={10}>Type</Th>
+                    <Th width={15} modifier="nowrap">
+                        Provider (region)
+                    </Th>
+                    <Th width={20} modifier="nowrap">
+                        Cloud source
+                    </Th>
+                    <Th width={15} modifier="nowrap" sort={getSortParams('TODO')}>
+                        First discovered
+                    </Th>
                 </Tr>
             </Thead>
             {clusters.length === 0 ? (
@@ -36,18 +49,31 @@ function DiscoveredClustersTable({
                     hasFilter={hasDiscoveredClustersFilter(searchFilter)}
                 />
             ) : (
-                clusters.map((cluster) => {
-                    const { id } = cluster;
+                <Tbody>
+                    {clusters.map((cluster) => {
+                        const { id } = cluster;
+                        const firstDiscovered = '2024-01-30T00:00:00Z'; // temporary placeholder
+                        const firstDiscoveredAsPhrase = getDistanceStrictAsPhrase(
+                            firstDiscovered,
+                            currentDatetime
+                        );
 
-                    return (
-                        <Tr key={id}>
-                            <Td dataLabel="Cluster">{'TODO'}</Td>
-                            <Td dataLabel="Type">{'TODO'}</Td>
-                            <Td dataLabel="Provider">{'TODO'}</Td>
-                            <Td dataLabel="Region">{'TODO'}</Td>
-                        </Tr>
-                    );
-                })
+                        return (
+                            <Tr key={id}>
+                                <Td dataLabel="Cluster">{'TODO'}</Td>
+                                <Td dataLabel="State">{'TODO'}</Td>
+                                <Td dataLabel="Type">{'TODO'}</Td>
+                                <Td dataLabel="Provider (region)" modifier="nowrap">
+                                    {'TODO'}
+                                </Td>
+                                <Td dataLabel="Cloud source">{'TODO'}</Td>
+                                <Td dataLabel="First discovered" modifier="nowrap">
+                                    {firstDiscoveredAsPhrase}
+                                </Td>
+                            </Tr>
+                        );
+                    })}
+                </Tbody>
             )}
         </TableComposable>
     );
