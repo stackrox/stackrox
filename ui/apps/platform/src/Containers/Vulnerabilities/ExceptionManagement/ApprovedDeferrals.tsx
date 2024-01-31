6af0f6f1c01c49833c05de64130c2cd8b3149699
Saif Chaudhry <sachaudh@users.noreply.github.com>
2024-01-31 11:26:08 -0800

    ROX-21476: Fix title on browser tab for exception management (#9554)

--- ui/apps/platform/src/Containers/Vulnerabilities/ExceptionManagement/ApprovedDeferrals.tsx
+++ ui/apps/platform/src/Containers/Vulnerabilities/ExceptionManagement/ApprovedDeferrals.tsx
@@ -20,6 +20,7 @@ import { fetchVulnerabilityExceptions } from 'services/VulnerabilityExceptionSer
 
 import SearchFilterChips from 'Components/PatternFly/SearchFilterChips';
 import NotFoundMessage from 'Components/NotFoundMessage';
+import PageTitle from 'Components/PageTitle';
 import {
     RequestExpires,
     RequestIDLink,
@@ -118,6 +119,7 @@ function ApprovedDeferrals() {
 
     return (
         <PageSection>
+            <PageTitle title="Exception Management - Approved Deferrals" />
             <Toolbar>
                 <ToolbarContent>
                     <FilterAutocompleteSelect
