882a7b49b6f912cc0e8bd06f5e66f8ba9578b8a1
bradr5 <braroger@redhat.com>
2024-02-01 10:13:10 -0600

    ROX-21880: leave out dashboard test

--- ui/apps/platform/src/Containers/MainPage/Sidebar/NavigationSidebar.tsx
+++ ui/apps/platform/src/Containers/MainPage/Sidebar/NavigationSidebar.tsx
@@ -13,7 +13,6 @@ import {
     clustersBasePath,
     collectionsBasePath,
     complianceBasePath,
-    complianceEnhancedStatusPath,
     complianceEnhancedClusterComplianceBasePath,
     configManagementPath,
     dashboardPath,
