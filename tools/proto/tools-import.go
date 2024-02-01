5d3afbfb0ea93c5219e44d2c8598aa8cc1a2ea97
Mladen Todorovic <mtodor@redhat.com>
2024-02-01 17:51:11 +0100

    Remove gogoproto.jsontag

--- tools/proto/tools-import.go
+++ tools/proto/tools-import.go
@@ -9,6 +9,7 @@ package tools
 import (
 	// Tool dependencies, not used anywhere in the code.
 	_ "github.com/bufbuild/buf/cmd/buf"
+	_ "github.com/favadi/protoc-go-inject-tag"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
 )
