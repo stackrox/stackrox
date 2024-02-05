c93e22a3b814dd46bf7a719a74d3d7a245405ec1
Mark Pedrotti <pedrottimark@gmail.com>
2024-02-05 12:29:49 -0500

    ROX-22193: Use Admin role for access to cluster init bundles (#9728)

--- ui/apps/platform/src/Containers/Integrations/IntegrationTiles/AuthenticationTokensSection.tsx
+++ ui/apps/platform/src/Containers/Integrations/IntegrationTiles/AuthenticationTokensSection.tsx
@@ -1,19 +1,23 @@
 import React, { ReactElement } from 'react';
 
-import usePermissions from 'hooks/usePermissions';
+import useAuthStatus from 'hooks/useAuthStatus';
 
 import APITokensTile from './APITokensTile';
 import ClusterInitBundles from './ClusterInitBundlesTile';
 import IntegrationsSection from './IntegrationsSection';
 
 function AuthenticationTokensSection(): ReactElement {
-    const { hasReadAccess } = usePermissions();
-    const hasReadAccessForAdministration = hasReadAccess('Administration');
+    // TODO after 4.4 release:
+    // Delete ClusterInitBundles tile from integrations.
+    // Delete unreachable code.
+    // Delete request from integration sagas and data from integrations reducer.
+    const { currentUser } = useAuthStatus();
+    const hasAdminRole = Boolean(currentUser?.userInfo?.roles.some(({ name }) => name === 'Admin')); // optional chaining just in case of the unexpected
 
     return (
         <IntegrationsSection headerName="Authentication Tokens" id="token-integrations">
             <APITokensTile />
-            {hasReadAccessForAdministration && <ClusterInitBundles />}
+            {hasAdminRole && <ClusterInitBundles />}
         </IntegrationsSection>
     );
 }
