38a44f79b8b8076c06f63f11cee32c7e59cf6d35
lvalerom <lvalerom@redhat.com>
2024-02-05 14:43:03 +0100

    setup ktunnel in the sensor-integration step

--- scripts/ci/jobs/gke_sensor_integration_tests.py
+++ scripts/ci/jobs/gke_sensor_integration_tests.py
@@ -8,6 +8,7 @@ from runners import ClusterTestRunner
 from clusters import GKECluster
 from pre_tests import PreSystemTests
 from ci_tests import SensorIntegration
+from post_tests import PostClusterTest, FinalPost
 
 # set required test parameters
 os.environ["ORCHESTRATOR_FLAVOR"] = "k8s"
@@ -16,4 +17,10 @@ ClusterTestRunner(
     pre_test=PreSystemTests(run_poll_for_system_test_images=False),
     cluster=GKECluster("sensor-integration-test"),
     test=SensorIntegration(),
+    post_test=PostClusterTest(
+        check_stackrox_logs=False,
+    ),
+    final_post=FinalPost(
+        store_qa_tests_data=False,
+    ),
 ).run()
