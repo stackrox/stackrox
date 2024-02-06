fad72bed858f6a48ac13c9439bdf6fb908499ac9
Mark Pedrotti <mark@stackrox.com>
2024-02-06 15:00:39 -0500

    ROX-22339: Remove init-bundle request from clusterInitBundleSagas

--- ui/apps/platform/src/sagas/index.js
+++ ui/apps/platform/src/sagas/index.js
@@ -2,7 +2,7 @@ import { all, fork } from 'redux-saga/effects';
 
 import apiTokens from './apiTokenSagas';
 import authProviders from './authSagas';
-import clusterInitBundles from './clusterInitBundleSagas';
+// import clusterInitBundles from './clusterInitBundleSagas';
 import integrations from './integrationSagas';
 import roles from './roleSagas';
 import searchAutoComplete from './searchAutocompleteSagas';
@@ -13,7 +13,8 @@ export default function* root() {
     yield all([
         fork(apiTokens),
         fork(authProviders),
-        fork(clusterInitBundles),
+        // Delete from reducers and sagas when we delete ROX_MOVE_INIT_BUNDLES_UI after 4.4 release.
+        // fork(clusterInitBundles),
         fork(integrations),
         fork(roles),
         fork(searchAutoComplete),
