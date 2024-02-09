ac4dbeaedfb27089dcb2210671722d6a4ebac649
dhaus67 <dhaus@redhat.com>
2024-02-09 11:21:21 +0100

    fix(cache-store): fix Delete for global resources. (#9839)

--- central/cloudsources/datastore/internal/store/postgres/store.go
+++ central/cloudsources/datastore/internal/store/postgres/store.go
@@ -55,7 +55,10 @@ type Store interface {
 
 // New returns a new Store instance using the provided sql instance.
 func New(db postgres.DB) Store {
-	return pgSearch.NewGenericStore[storeType, *storeType](
+	// Use of pgSearch.NewGenericStoreWithCache can be dangerous with high cardinality stores,
+	// and be the source of memory pressure. Think twice about the need for in-memory caching
+	// of the whole store.
+	return pgSearch.NewGenericStoreWithCache[storeType, *storeType](
 		db,
 		schema,
 		pkGetter,
@@ -63,6 +66,8 @@ func New(db postgres.DB) Store {
 		copyFromCloudSources,
 		metricsSetAcquireDBConnDuration,
 		metricsSetPostgresOperationDurationTime,
+		metricsSetCacheOperationDurationTime,
+
 		pgSearch.GloballyScopedUpsertChecker[storeType, *storeType](targetResource),
 		targetResource,
 	)
@@ -80,6 +85,10 @@ func metricsSetPostgresOperationDurationTime(start time.Time, op ops.Op) {
 
 func metricsSetAcquireDBConnDuration(start time.Time, op ops.Op) {
 	metrics.SetAcquireDBConnDuration(start, op, storeName)
+}
+
+func metricsSetCacheOperationDurationTime(start time.Time, op ops.Op) {
+	metrics.SetCacheOperationDurationTime(start, op, storeName)
 }
 
 func insertIntoCloudSources(batch *pgx.Batch, obj *storage.CloudSource) error {
