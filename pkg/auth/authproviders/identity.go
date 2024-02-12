46f03a1123f99398573f94c1916c0539a3791ad6
Ivan Degtiarenko <78353299+ivan-degtiarenko@users.noreply.github.com>
2024-02-12 20:53:42 +0100

    ROX-14548: Show underlying token in test page (#9301)

--- pkg/auth/authproviders/identity.go
+++ pkg/auth/authproviders/identity.go
@@ -30,6 +30,7 @@ func CreateRoleBasedIdentity(ctx context.Context, provider Provider, authResp *A
 	ud := &permissions.UserDescriptor{
 		UserID:     authResp.Claims.UserID,
 		Attributes: authResp.Claims.Attributes,
+		IdpToken:   authResp.IdpToken,
 	}
 
 	// config might contain semi-sensitive values, so strip it
