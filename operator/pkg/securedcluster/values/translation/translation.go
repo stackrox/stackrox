cea0d854f872786c3e2a904f99ec4d5792a4647e
Vlad Bologa <vbologa@redhat.com>
2024-02-13 18:13:08 +0100

    ROX-22479: set Scanner V4 env vars in Sensor correctly

--- operator/pkg/securedcluster/values/translation/translation.go
+++ operator/pkg/securedcluster/values/translation/translation.go
@@ -367,10 +367,12 @@ func (t Translator) getLocalScannerComponentValues(securedCluster platform.Secur
 func (t Translator) getLocalScannerV4ComponentValues(ctx context.Context, securedCluster platform.SecuredCluster, config scanner.AutoSenseResult) *translation.ValuesBuilder {
 	sv := translation.NewValuesBuilder()
 	s := securedCluster.Spec.ScannerV4
-	sv.SetBoolValue("disable", !config.DeployScannerResources)
+	sv.SetBoolValue("disable", !config.EnableLocalImageScanning)
 
-	translation.SetScannerV4ComponentValues(&sv, "indexer", s.Indexer)
-	translation.SetScannerV4DBValues(ctx, &sv, s.DB, platform.SecuredClusterGVK.Kind, securedCluster.GetNamespace(), t.client)
+	if config.DeployScannerResources {
+		translation.SetScannerV4ComponentValues(&sv, "indexer", s.Indexer)
+		translation.SetScannerV4DBValues(ctx, &sv, s.DB, platform.SecuredClusterGVK.Kind, securedCluster.GetNamespace(), t.client)
+	}
 
 	return &sv
 }
