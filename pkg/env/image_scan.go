ac313ea674c39f54fac6b4e3d3341541602c59f6
RTann <rtannenb@redhat.com>
2024-02-13 17:10:50 -0800

    chore(scanner): bump scan timeout from 6m to 10m

--- pkg/env/image_scan.go
+++ pkg/env/image_scan.go
@@ -4,5 +4,5 @@ import "time"
 
 var (
 	// ScanTimeout defines the image scan timeout duration.
-	ScanTimeout = registerDurationSetting("ROX_SCAN_TIMEOUT", 6*time.Minute)
+	ScanTimeout = registerDurationSetting("ROX_SCAN_TIMEOUT", 10*time.Minute)
 )
