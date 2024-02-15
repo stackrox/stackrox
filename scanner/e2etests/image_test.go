3f1da281c6fa26062f00c4e64e8dc9b661cd567a
J. Victor Martins <jvdm@sdf.org>
2024-02-14 16:27:15 -0800

    feat(scanner): Enable stand-alone E2E tests

--- scanner/e2etests/image_test.go
+++ scanner/e2etests/image_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"math"
 	"os"
 	"strings"
 	"testing"
@@ -31,7 +32,6 @@ type Vulnerability struct {
 // Vulnerability describes a feature in a TestCase.
 type Feature struct {
 	Name            string          `json:"Name"`
-	NamespaceName   string          `json:"NamespaceName"`
 	VersionFormat   string          `json:"VersionFormat"`
 	Version         string          `json:"Version"`
 	Arch            string          `json:"Arch"`
@@ -62,20 +62,31 @@ type TestCase struct {
 	// Test flags.
 	DisabledReason           string `json:"disabled_reason"`
 	OnlyCheckSpecifiedVulns  bool   `json:"only_check_specified_vulns"`
-	UncertifiedRhel          bool   `json:"uncertified_rhel"`
-	CheckProvidedExecutables bool   `json:"check_provided_executables"`
 }
 
 // TestImage is the E2E test for container image scans.
 func TestImage(t *testing.T) {
+	indexerAddr := os.Getenv("SCANNER_E2E_INDEXER_ADDRESS")
+	if indexerAddr == "" {
+		indexerAddr = ":8443"
+	}
+	matcherAddr := os.Getenv("SCANNER_E2E_MATCHER_ADDRESS")
+	if matcherAddr == "" {
+		matcherAddr = ":8443"
+	}
+	debug := os.Getenv("SCANNER_E2E_DEBUG") == "1"
+	t.Logf("Indexer Address: %s", indexerAddr)
+	t.Logf("Matcher Address: %s", matcherAddr)
+	t.Logf("Debug: %v", debug)
 	ctx := context.Background()
 	c, err := client.NewGRPCScanner(ctx,
-		client.WithAddress(":8443"),
+		client.WithIndexerAddress(indexerAddr),
+		client.WithMatcherAddress(matcherAddr),
 		client.SkipTLSVerification)
 	require.NoError(t, err)
 
 	testCases, err := loadTestCases("testdata/image_tests.json")
-	require.NoError(t, err)
+	require.NoError(t, err, "parsing testdata/image_tests.json")
 
 	for _, tc := range testCases {
 		t.Run(fmt.Sprintf("%s/%s", tc.Namespace, tc.Image), func(t *testing.T) {
@@ -93,12 +104,14 @@ func TestImage(t *testing.T) {
 			require.NoError(t, err)
 
 			expected := tc.TestWant
-			actual := tc.mapFillReport(vr)
-			defer func() {
-				if t.Failed() && vr != nil {
-					tc.logReport(t, vr)
-				}
-			}()
+			actual := tc.mapFillReport(t, vr)
+			if debug {
+				defer func() {
+					if t.Failed() && vr != nil {
+						tc.logReport(t, vr)
+					}
+				}()
+			}
 			assert.Equal(t, &expected, actual,
 				"The vulnerability report did not contain the expected values.")
 		})
@@ -136,17 +149,17 @@ func (tc *TestCase) authConfig() authn.Authenticator {
 	}
 }
 
-func (tc *TestCase) mapFillReport(vr *v4.VulnerabilityReport) *TestWant {
+func (tc *TestCase) mapFillReport(t *testing.T, vr *v4.VulnerabilityReport) *TestWant {
 	return &TestWant{
 		Namespace: mapNamespace(vr),
 		Source:    tc.Source,
-		Features:  tc.mapFillFeatures(vr),
+		Features:  tc.mapFillFeatures(t, vr),
 	}
 }
 
 // mapFillFeatures creates a features slice by converting values found in the
-// vulnerability report or using empty entries when not found.
-func (tc *TestCase) mapFillFeatures(vr *v4.VulnerabilityReport) []Feature {
+// vulnerability report.
+func (tc *TestCase) mapFillFeatures(t *testing.T, vr *v4.VulnerabilityReport) []Feature {
 	type VA struct {
 		V string
 		A string
@@ -159,65 +172,109 @@ func (tc *TestCase) mapFillFeatures(vr *v4.VulnerabilityReport) []Feature {
 			versions = make(map[VA]*v4.Package)
 			pkgs[p.Name] = versions
 		}
-		versions[VA{V: p.Version, A: p.Arch}] = p
+		va := VA{V: p.Version, A: p.Arch}
+		if _, ok := versions[va]; ok {
+			t.Logf("Ignoring a duplicated package-version found in the vulnerability report: %v", va)
+			continue
+		}
+		versions[va] = p
 	}
-	// Convert every expected package found in the report, or convert an empty
-	// package if not found.
+	// Convert every expected package found in the report.
 	var ret []Feature
 	for idx := range tc.Features {
 		f := &tc.Features[idx]
-		versions, nameFound := pkgs[f.Name]
-		var p *v4.Package
-		if nameFound {
-			version, versionFound := versions[VA{V: f.Version, A: f.Arch}]
-			if versionFound {
-				p = version
-			} else {
-				p = &v4.Package{Name: f.Name}
+		versions, ok := pkgs[f.Name]
+		if !ok {
+			t.Logf("Expected package name not found in the vulnerability report: %s", f.Name)
+			var c []string
+			for n := range pkgs {
+				if strings.Contains(n, f.Name) {
+					c = append(c, n)
+				}
 			}
-		} else {
-			p = &v4.Package{}
+			if len(c) > 0 {
+				t.Logf("Potential candidates: %v", c)
+			} else {
+				t.Log("No potential candidates found.")
+			}
+			continue
 		}
-		ret = append(ret, Feature{
-			Name:            p.GetName(),
-			NamespaceName:   mapNamespace(vr),
-			Version:         p.GetVersion(),
-			Arch:            p.GetArch(),
-			Vulnerabilities: tc.mapFillVulns(vr, p, f),
-			// TODO Pending fields not currently available in the vulnerability report.
-			VersionFormat: f.VersionFormat,
-			AddedBy:       f.AddedBy,
-			Location:      f.Location,
-			FixedBy:       f.FixedBy,
-		})
+		va := VA{V: f.Version, A: f.Arch}
+		p, ok := versions[va]
+		if !ok {
+			t.Logf("Package %q with {version arch} %v not found in the vulnerability report", f.Name, va)
+			t.Logf("Available versions: %+#v", versions)
+			continue
+		}
+		if p != nil {
+			env := environment(t, vr, p)
+			ret = append(ret, Feature{
+				Name:            p.GetName(),
+				Version:         p.GetVersion(),
+				Arch:            p.GetArch(),
+				Vulnerabilities: tc.mapFillVulns(t, vr.GetVulnerabilities(), vr.GetPackageVulnerabilities()[p.GetId()].GetValues(), f),
+				FixedBy:         p.GetFixedInVersion(),
+				AddedBy:         env.GetIntroducedIn(),
+				Location:        env.GetPackageDb(),
+			})
+		}
 	}
 	return ret
 }
 
-func (tc *TestCase) mapFillVulns(vr *v4.VulnerabilityReport, pkg *v4.Package, feat *Feature) []Vulnerability {
-	// Create map with all vulnerabilities found for the package found in the report.
+func environment(t *testing.T, vr *v4.VulnerabilityReport, pkg *v4.Package) *v4.Environment {
+	envList, ok := vr.GetContents().GetEnvironments()[pkg.GetId()]
+	if !ok {
+		return nil
+	}
+	envs := envList.GetEnvironments()
+	if len(envs) == 0 {
+		return nil
+	}
+	var pkgDB, introIn string
+	for i, e := range envs {
+		if i == 0 {
+			pkgDB, introIn = e.GetPackageDb(), e.GetIntroducedIn()
+			continue
+		}
+		if pkgDB != e.GetPackageDb() || introIn != e.GetIntroducedIn() {
+			t.Logf("Ignoring environment #%d for package %q: %v", i, pkg.GetName(), e)
+		}
+	}
+	return envs[0]
+}
+
+func (tc *TestCase) mapFillVulns(t *testing.T, vr map[string]*v4.VulnerabilityReport_Vulnerability, vrIDs []string, feat *Feature) []Vulnerability {
 	vrVulns := make(map[string]*v4.VulnerabilityReport_Vulnerability)
-	for _, id := range vr.GetPackageVulnerabilities()[pkg.GetId()].GetValues() {
-		v := vr.GetVulnerabilities()[id]
+	for _, id := range vrIDs {
+		v := vr[id]
+		if _, ok := vrVulns[v.GetName()]; ok {
+			continue
+		}
 		vrVulns[v.GetName()] = v
 	}
-	// Convert all package vulnerabilities.
 	var vulns []Vulnerability
 	for idx := range feat.Vulnerabilities {
 		featVuln := &feat.Vulnerabilities[idx]
 		// If not found, convert an empty vulnerability case.
-		v, ok := vrVulns[featVuln.Name]
+		vrVuln, ok := vrVulns[featVuln.Name]
 		if !ok {
-			v = &v4.VulnerabilityReport_Vulnerability{}
-		} else if !tc.OnlyCheckSpecifiedVulns {
-			// Delete from map, so we can check the remaining items.
-			delete(vrVulns, featVuln.Name)
+			t.Logf("Missing vulnerability in the report: %q", featVuln.Name)
+			continue
 		}
-		vulns = append(vulns, tc.mapFillVuln(v, featVuln))
+		// We delete the vuln from the VR vulns map because we want to check after this
+		// loop if anything was not matched (based on tc.OnlyCheckSpecifiedVulns).
+		delete(vrVulns, featVuln.Name)
+		vulns = append(vulns, tc.mapFillVuln(vrVuln, featVuln))
 	}
-	if !tc.OnlyCheckSpecifiedVulns {
-		// Add the remaining vulnerabilities.
+	if tc.OnlyCheckSpecifiedVulns {
+		return vulns
+	}
+	// See if there is any remaining vulnerabilities, and complain about them.
+	if len(vrVulns) > 0 {
+		t.Logf("Some vulnerabilities were not matched, and we want to match all of them")
 		for _, v := range vrVulns {
+			t.Logf("- Name: %q\n  ID: %q", v.GetName(), v.GetId())
 			vulns = append(vulns, tc.mapFillVuln(v, &Vulnerability{}))
 		}
 	}
@@ -244,36 +301,67 @@ func (tc *TestCase) mapFillVuln(v *v4.VulnerabilityReport_Vulnerability, featVul
 	if !ok {
 		severity = "Unknown"
 	}
+	var metadata map[string]any
+	// TODO Use the CVSS score source type provided by the report when
+	//      provided.
+	var metadataKey string
+	for k := range featVuln.Metadata {
+		metadataKey = k
+		break
+	}
+	f64 := func(f float32) float64 {
+		return math.Round(float64(f)*100) / 100
+	}
+	if v.GetCvss() != nil {
+		scores := map[string]any{}
+		if v.GetCvss().GetV2() != nil {
+			scores["CVSSv2"] = map[string]any{
+				"Score": f64(v.GetCvss().GetV2().GetBaseScore()),
+				"Vectors": v.GetCvss().GetV2().GetVector(),
+			}
+		}
+		if v.GetCvss().GetV3() != nil {
+			scores["CVSSv3"] = map[string]any{
+				"Score": f64(v.GetCvss().GetV3().GetBaseScore()),
+				"Vectors": v.GetCvss().GetV3().GetVector(),
+			}
+		}
+		metadata = map[string]any{metadataKey: scores}
+	}
 	return Vulnerability{
 		Name:        v.GetName(),
 		Description: v.GetDescription(),
 		Link:        link,
 		Severity:    severity,
 		FixedBy:     v.GetFixedInVersion(),
-		// TODO Pending fields not currently available in the vulnerability report.
-		Metadata: featVuln.Metadata,
+
+		Metadata: metadata,
 	}
 }
 
 func (tc *TestCase) logReport(t *testing.T, vr *v4.VulnerabilityReport) {
+	t.Log("Printing Packages and Vulnerabilities in Vulnerability Report:")
+	for _, pkg := range vr.GetContents().GetPackages() {
+		tc.logPackageAndVulns(t, vr, pkg)
+	}
+}
+
+func (tc *TestCase) logPackageAndVulns(t *testing.T, vr *v4.VulnerabilityReport, pkg *v4.Package) {
 	vulns := func(p *v4.Package) (v []*v4.VulnerabilityReport_Vulnerability) {
 		for _, id := range vr.GetPackageVulnerabilities()[p.GetId()].GetValues() {
 			v = append(v, vr.GetVulnerabilities()[id])
 		}
 		return v
 	}
-	t.Log("Printing Packages and Vulnerabilities in Vulnerability Report:")
-	for _, pkg := range vr.GetContents().GetPackages() {
-		s, err := json.MarshalIndent(struct {
-			Package         *v4.Package
-			Vulnerabilities []*v4.VulnerabilityReport_Vulnerability
-		}{
-			Package:         pkg,
-			Vulnerabilities: vulns(pkg),
-		}, "\t", " ")
-		if err != nil {
-			t.Logf("json marshalling failed: %v", err)
-		}
-		t.Logf("NameVersion: %s-%s:\n\n\t%s\n\n", pkg.GetName(), pkg.GetVersion(), s)
+	s, err := json.MarshalIndent(struct {
+		Package         *v4.Package
+		Vulnerabilities []*v4.VulnerabilityReport_Vulnerability
+	}{
+		Package:         pkg,
+		Vulnerabilities: vulns(pkg),
+	}, "\t", " ")
+	if err != nil {
+		t.Logf("json marshalling failed: %v", err)
 	}
+	t.Logf("NameVersion: %s-%s:\n\n\t%s\n\n", pkg.GetName(), pkg.GetVersion(), s)
 }
