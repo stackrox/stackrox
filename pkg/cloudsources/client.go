55163e9ff3408b89e42454965c0beea911b459e5
dhaus67 <dhaus@redhat.com>
2024-02-21 01:00:07 +0100

    fix(cloud-sources): use lower timeout for test API (#9989)

--- pkg/cloudsources/client.go
+++ pkg/cloudsources/client.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/pkg/cloudsources/discoveredclusters"
 	"github.com/stackrox/rox/pkg/cloudsources/ocm"
+	"github.com/stackrox/rox/pkg/cloudsources/opts"
 	"github.com/stackrox/rox/pkg/cloudsources/paladin"
 )
 
@@ -14,15 +15,17 @@ import (
 //go:generate mockgen-wrapper
 type Client interface {
 	GetDiscoveredClusters(ctx context.Context) ([]*discoveredclusters.DiscoveredCluster, error)
+
+	Ping(ctx context.Context) error
 }
 
 // NewClientForCloudSource creates a new Client based on the cloud source to fetch discovered clusters.
-func NewClientForCloudSource(source *storage.CloudSource) (Client, error) {
+func NewClientForCloudSource(source *storage.CloudSource, options ...opts.ClientOpts) (Client, error) {
 	switch source.GetType() {
 	case storage.CloudSource_TYPE_PALADIN_CLOUD:
-		return paladin.NewClient(source), nil
+		return paladin.NewClient(source, options...), nil
 	case storage.CloudSource_TYPE_OCM:
-		return ocm.NewClient(source)
+		return ocm.NewClient(source, options...)
 	default:
 		return nil, nil
 	}
--- pkg/cloudsources/paladin/client.go
+++ pkg/cloudsources/paladin/client.go
@@ -17,6 +17,7 @@ import (
 	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/pkg/clientconn"
 	"github.com/stackrox/rox/pkg/cloudsources/discoveredclusters"
+	"github.com/stackrox/rox/pkg/cloudsources/opts"
 	"github.com/stackrox/rox/pkg/errox"
 	"github.com/stackrox/rox/pkg/httputil"
 	"github.com/stackrox/rox/pkg/httputil/proxy"
@@ -191,15 +192,20 @@ func (p *paladinTransportWrapper) RoundTrip(req *http.Request) (*http.Response, 
 }
 
 // NewClient creates a client to interact with Paladin Cloud APIs.
-func NewClient(cfg *storage.CloudSource) *paladinClient {
+func NewClient(cfg *storage.CloudSource, options ...opts.ClientOpts) *paladinClient {
+	opt := opts.DefaultOpts()
+	for _, option := range options {
+		option(opt)
+	}
+
 	retryClient := retryablehttp.NewClient()
-	retryClient.RetryMax = 3
+	retryClient.RetryMax = opt.Retries
 	retryClient.Logger = nil
 	retryClient.HTTPClient.Transport = &paladinTransportWrapper{
 		baseTransport: proxy.RoundTripper(),
 		token:         cfg.GetCredentials().GetSecret(),
 	}
-	retryClient.HTTPClient.Timeout = 30 * time.Second
+	retryClient.HTTPClient.Timeout = opt.Timeout
 	retryClient.RetryWaitMin = 10 * time.Second
 
 	return &paladinClient{
@@ -207,6 +213,13 @@ func NewClient(cfg *storage.CloudSource) *paladinClient {
 		endpoint:      urlfmt.FormatURL(cfg.GetPaladinCloud().GetEndpoint(), urlfmt.HTTPS, urlfmt.NoTrailingSlash),
 		cloudSourceID: cfg.GetId(),
 	}
+}
+
+func (c *paladinClient) Ping(ctx context.Context) error {
+	// At the current time, no better API is known besides the asset API to confirm correct authN/Z setup and
+	// connectivity. In case we find a better API, we may switch to that.
+	_, err := c.getAssets(ctx)
+	return err
 }
 
 // GetDiscoveredClusters returns the discovered clusters from the Paladin Cloud API.
