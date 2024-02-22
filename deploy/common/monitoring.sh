779a5cd9333013159a2c5ec63fae05835670ee58
Moritz Clasmeier <111092021+mclasmeier@users.noreply.github.com>
2024-02-22 12:01:01 +0100

    ROX-22631: Support Scanner V4 in monitoring script (#10026)
    Co-authored-by: Moritz Clasmeier <mclasmeier@redhat.com>

--- deploy/common/monitoring.sh
+++ deploy/common/monitoring.sh
@@ -6,6 +6,8 @@ sensor_namespace=${2:-${SENSOR_NAMESPACE:-stackrox}}
 kubectl -n "${sensor_namespace}" patch svc/sensor -p '{"spec":{"ports":[{"name":"monitoring","port":9090,"protocol":"TCP","targetPort":9090}]}}'
 kubectl -n "${central_namespace}" patch svc/central -p '{"spec":{"ports":[{"name":"monitoring","port":9090,"protocol":"TCP","targetPort":9090}]}}'
 kubectl -n "${sensor_namespace}" patch daemonset/collector --type='json' -p='[{"op": "add", "path": "/spec/template/spec/containers/1/ports", "value":[{"containerPort":9091,"name":"cmonitor","protocol":"TCP"}]}]'
+kubectl -n "${central_namespace}" patch svc/scanner-v4-indexer -p '{"spec":{"ports":[{"name":"monitoring","port":9090,"protocol":"TCP","targetPort":"monitoring"}]}}'
+kubectl -n "${central_namespace}" patch svc/scanner-v4-matcher -p '{"spec":{"ports":[{"name":"monitoring","port":9090,"protocol":"TCP","targetPort":"monitoring"}]}}'
 
 # Modify network policies to allow ingress
 kubectl apply -f - <<EOF
@@ -23,7 +25,7 @@ spec:
       protocol: TCP
   podSelector:
     matchExpressions:
-    - {key: app, operator: In, values: [central, sensor]}
+    - {key: app, operator: In, values: [central, sensor, scanner-v4-indexer, scanner-v4-matcher]}
   policyTypes:
   - Ingress
 ---
