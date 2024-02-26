56671fe60abb79a1397f0e946f13c94f0446e621
Alex Rukletsov <alexr@stackrox.com>
2024-02-26 21:12:33 +0100

    Bump registry HTTP timeout to 90s.

--- pkg/registries/docker/docker.go
+++ pkg/registries/docker/docker.go
@@ -24,10 +24,17 @@ import (
 )
 
 const (
-	// GenericDockerRegistryType exposes the default registry type
-	GenericDockerRegistryType = "docker"
+	// This timeout is used as http.Client.Timeout for the registry's HTTP
+	// client and hence includes everything from connection to reading the
+	// response body. The timeout has been chosen rather arbitrarily, it is
+	// probably less harm in waiting a bit longer than in aborting early a
+	// request that is about to succeed.
+	//
+	// TODO(alexr): Consider setting ResponseHeaderTimeout for registry's
+	//   transport to make timeout errors more specific and hence facilitate
+	//   retries and troubleshooting.
+	registryClientTimeout = 90 * time.Second
 
-	registryTimeout  = 5 * time.Second
 	repoListInterval = 10 * time.Minute
 )
 
@@ -112,7 +119,7 @@ func NewDockerRegistryWithConfig(cfg Config, integration *storage.ImageIntegrati
 		return nil, err
 	}
 
-	client.Client.Timeout = registryTimeout
+	client.Client.Timeout = registryClientTimeout
 
 	var repoSet set.Set[string]
 	var ticker *time.Ticker
