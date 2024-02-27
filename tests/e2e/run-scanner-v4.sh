ef0dacb7df2ce2b0a53e90200b59e2fa129f78e0
Moritz Clasmeier <mclasmeier@redhat.com>
2024-02-27 14:04:32 +0100

    Publish junit report

--- tests/e2e/run-scanner-v4.sh
+++ tests/e2e/run-scanner-v4.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 
-ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../.. && pwd)"
+# shellcheck source=../../scripts/ci/lib.sh
+source "${ROOT}/scripts/ci/lib.sh"
+
+set -euo pipefail
+
+REPORTS_DIR=$(mktemp -d)
+FAILED=0
+
 bats \
     --print-output-on-failure \
     --verbose-run \
     --report-formatter junit \
-    "${ROOT}/run-scanner-v4.bats"
+    --output "${REPORTS_DIR}" \
+    "${ROOT}/tests/e2e/run-scanner-v4.bats" || FAILED=1
+
+info "Saving junit XML report..."
+store_test_results "${REPORTS_DIR}" reports
+
+exit "${FAILED}"
