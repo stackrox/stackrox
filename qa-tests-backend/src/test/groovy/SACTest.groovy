596e3f87ac03544baf00e659a299fbed48d0c54a
Tomasz Janiszewski <tomek@redhat.com>
2024-03-01 18:16:12 +0100

    ROX-19782: Skip sensor -> INTERNET connection (#10108)
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

--- qa-tests-backend/src/test/groovy/SACTest.groovy
+++ qa-tests-backend/src/test/groovy/SACTest.groovy
@@ -67,8 +67,10 @@ class SACTest extends BaseSpecification {
     static final private List<Deployment> DEPLOYMENTS = [DEPLOYMENT_QA1, DEPLOYMENT_QA2,]
 
     static final private UNSTABLE_FLOWS = [
-            // monitoring doesn't keep a persistent outgoing connection, so we might or might not see this flow.
+            // monitoring and sensor doesn't keep a persistent outgoing connection,
+            // so we might or might not see this flow.
             "stackrox/monitoring -> INTERNET",
+            "stackrox/sensor -> INTERNET",
     ] as Set
 
     // Increase the timeout conditionally based on whether we are running race-detection builds or within OpenShift
@@ -599,12 +601,12 @@ class SACTest extends BaseSpecification {
         def networkGraphWithAllAccess = NetworkGraphService.getNetworkGraph(null, "Namespace:stackrox")
         def allAccessFlows = NetworkGraphUtil.flowStrings(networkGraphWithAllAccess)
         allAccessFlows.removeAll(UNSTABLE_FLOWS)
-        log.info "${allAccessFlows}"
+        log.info "allAccessFlows: ${allAccessFlows}"
 
         def allAccessFlowsWithoutNeighbors = allAccessFlows.findAll {
             it.matches("(stackrox/.*|INTERNET) -> (stackrox/.*|INTERNET)")
         }
-        log.info "${allAccessFlowsWithoutNeighbors}"
+        log.info "allAccessFlowsWithoutNeighbors: ${allAccessFlowsWithoutNeighbors}"
 
         and:
         "Obtaining the network graph for the StackRox namespace with a SAC restricted token"
@@ -612,14 +614,14 @@ class SACTest extends BaseSpecification {
         def networkGraphWithSAC = NetworkGraphService.getNetworkGraph(null, "Namespace:stackrox")
         def sacFlows = NetworkGraphUtil.flowStrings(networkGraphWithSAC)
         sacFlows.removeAll(UNSTABLE_FLOWS)
-        log.info "${sacFlows}"
+        log.info "sacFlows: ${sacFlows}"
 
         and:
         "Obtaining the network graph for the StackRox namespace with a SAC restricted token and no query"
         def networkGraphWithSACNoQuery = NetworkGraphService.getNetworkGraph()
         def sacFlowsNoQuery = NetworkGraphUtil.flowStrings(networkGraphWithSACNoQuery)
         sacFlowsNoQuery.removeAll(UNSTABLE_FLOWS)
-        log.info "${sacFlowsNoQuery}"
+        log.info "sacFlowsNoQuery: ${sacFlowsNoQuery}"
 
         then:
         "Query-restricted and non-restricted flows should be equal under SAC"
