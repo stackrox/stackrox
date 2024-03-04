1e35e197a85475a38e5d4ec22b789a066db82c32
Dave Shrewsberry <dashrews@redhat.com>
2024-03-04 14:39:23 -0500

    more resolution of the nit

--- central/complianceoperator/v2/pruner/singleton.go
+++ central/complianceoperator/v2/pruner/singleton.go
@@ -3,12 +3,12 @@ package pruner
 import (
 	scanResult "github.com/stackrox/rox/central/complianceoperator/v2/checkresults/datastore"
 	compIntegration "github.com/stackrox/rox/central/complianceoperator/v2/integration/datastore"
-	profileDS "github.com/stackrox/rox/central/complianceoperator/v2/profiles/datastore"
-	compRuleDS "github.com/stackrox/rox/central/complianceoperator/v2/rules/datastore"
+	profile "github.com/stackrox/rox/central/complianceoperator/v2/profiles/datastore"
+	compRule "github.com/stackrox/rox/central/complianceoperator/v2/rules/datastore"
 	compScanSetting "github.com/stackrox/rox/central/complianceoperator/v2/scanconfigurations/datastore"
-	scanDS "github.com/stackrox/rox/central/complianceoperator/v2/scans/datastore"
-	scanSettingBindingDS "github.com/stackrox/rox/central/complianceoperator/v2/scansettingbindings/datastore"
-	suitesDS "github.com/stackrox/rox/central/complianceoperator/v2/suites/datastore"
+	scan "github.com/stackrox/rox/central/complianceoperator/v2/scans/datastore"
+	scanSettingBinding "github.com/stackrox/rox/central/complianceoperator/v2/scansettingbindings/datastore"
+	suites "github.com/stackrox/rox/central/complianceoperator/v2/suites/datastore"
 	"github.com/stackrox/rox/pkg/features"
 	"github.com/stackrox/rox/pkg/sync"
 )
@@ -24,7 +24,7 @@ func Singleton() Pruner {
 		return nil
 	}
 	once.Do(func() {
-		pruner = New(compIntegration.Singleton(), compScanSetting.Singleton(), scanResult.Singleton(), compRuleDS.Singleton(), profileDS.Singleton(), scanSettingBindingDS.Singleton(), scanDS.Singleton(), suitesDS.Singleton())
+		pruner = New(compIntegration.Singleton(), compScanSetting.Singleton(), scanResult.Singleton(), compRule.Singleton(), profile.Singleton(), scanSettingBinding.Singleton(), scan.Singleton(), suites.Singleton())
 	})
 	return pruner
 }
