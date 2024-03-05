7c6d7949981f39dc33bd108c573b3d91dd812714
lvalerom <lvalerom@redhat.com>
2024-03-05 17:48:37 +0100

    logs

--- central/networkpolicies/generator/graph.go
+++ central/networkpolicies/generator/graph.go
@@ -306,14 +306,20 @@ func (g *generator) generateNodeFromBaselineForDeployment(
 	// Temporarily elevate permissions to obtain all deployments in cluster.
 	elevatedCtx := sac.WithAllAccess(ctx)
 	// Since we only generate ingress flows, we only look at ingress peers for policy generation
+	log.Debugf("Baseline for deployment %s - %s in namespace %s. (locked: %t) (observation period: %v)",
+		deployment.GetId(), deployment.GetName(), deployment.GetNamespace(),
+		baseline.GetLocked(), baseline.GetObservationPeriodEnd())
 	for _, peer := range baseline.GetPeers() {
+		log.Debugf("Baseline Peer for deployment %s - %s in namespace %s: %+v", deployment.GetId(), deployment.GetName(), deployment.GetNamespace(), peer)
 		peerNode := g.populateNode(elevatedCtx, peer.GetEntity().GetInfo().GetId(), peer.GetEntity().GetInfo().GetType())
 		if peerNode == nil {
 			// Peer deployment probably has been deleted.
 			continue
 		}
 		for _, props := range peer.GetProperties() {
+			log.Debugf("Properties from peer node %+v --> %+v", peer.GetEntity().GetInfo(), props)
 			if !props.GetIngress() {
+				log.Debugf("Not ingress")
 				continue
 			}
 			var port portDesc
