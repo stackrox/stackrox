e4a8edbcd04a443ace7c773d386dc50a1f698c9f
Nakul Chander <1407686+theencee@users.noreply.github.com>
2024-03-05 09:52:41 -0800

    Revert "ROX-22797: Paginate ListPolicies (#10203)"
    This reverts commit 41b5d046077b749df6159a5758445e3dff39d84d.

--- central/policy/service/service_impl.go
+++ central/policy/service/service_impl.go
@@ -44,7 +44,6 @@ import (
 	"github.com/stackrox/rox/pkg/sac"
 	"github.com/stackrox/rox/pkg/sac/resources"
 	"github.com/stackrox/rox/pkg/search"
-	"github.com/stackrox/rox/pkg/search/paginated"
 	"github.com/stackrox/rox/pkg/search/predicate/basematchers"
 	"github.com/stackrox/rox/pkg/set"
 	"github.com/stackrox/rox/pkg/stringutils"
@@ -87,7 +86,6 @@ const (
 	uncategorizedCategory = `Uncategorized`
 	dryRunParallelism     = 8
 	identityUIDKey        = "identityUID"
-	maxPoliciesReturned   = 1000
 )
 
 var (
@@ -180,25 +178,23 @@ func convertPoliciesToListPolicies(policies []*storage.Policy) []*storage.ListPo
 // ListPolicies retrieves all policies in ListPolicy form according to the request.
 func (s *serviceImpl) ListPolicies(ctx context.Context, request *v1.RawQuery) (*v1.ListPoliciesResponse, error) {
 	resp := new(v1.ListPoliciesResponse)
-
-	parsedQuery, err := search.ParseQuery(request.GetQuery(), search.MatchAllIfEmpty())
-	if err != nil {
-		return nil, errors.Wrap(errox.InvalidArgs, err.Error())
-	}
-	// Fill in pagination.
-	pagination := request.GetPagination()
-	if request.GetPagination() == nil {
-		pagination = &v1.Pagination{
-			Limit: maxPoliciesReturned,
+	if request.GetQuery() == "" {
+		policies, err := s.policies.GetAllPolicies(ctx)
+		if err != nil {
+			return nil, err
 		}
+		resp.Policies = convertPoliciesToListPolicies(policies)
+	} else {
+		parsedQuery, err := search.ParseQuery(request.GetQuery())
+		if err != nil {
+			return nil, errors.Wrap(errox.InvalidArgs, err.Error())
+		}
+		policies, err := s.policies.SearchRawPolicies(ctx, parsedQuery)
+		if err != nil {
+			return nil, err
+		}
+		resp.Policies = convertPoliciesToListPolicies(policies)
 	}
-	paginated.FillPagination(parsedQuery, pagination, maxPoliciesReturned)
-
-	policies, err := s.policies.SearchRawPolicies(ctx, parsedQuery)
-	if err != nil {
-		return nil, err
-	}
-	resp.Policies = convertPoliciesToListPolicies(policies)
 
 	return resp, nil
 }
