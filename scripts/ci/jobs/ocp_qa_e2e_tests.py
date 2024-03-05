e7a1768b2ca87eec27dfba908ff4d8ea756097eb
davdhacs <105243888+davdhacs@users.noreply.github.com>
2024-03-05 14:47:43 -0700

    turn off workload ident setup for aws clusters

--- scripts/ci/jobs/ocp_qa_e2e_tests.py
+++ scripts/ci/jobs/ocp_qa_e2e_tests.py
@@ -7,14 +7,16 @@ import os
 from base_qa_e2e_test import make_qa_e2e_test_runner
 from clusters import AutomationFlavorsCluster
 
-print({key:str(val)[0:8] for key, val in os.environ.items()})
-
 # set required test parameters
 os.environ["DEPLOY_STACKROX_VIA_OPERATOR"] = "true"
 os.environ["ORCHESTRATOR_FLAVOR"] = "openshift"
-os.environ["SETUP_WORKLOAD_IDENTITIES"] = "false"
+os.environ["SETUP_WORKLOAD_IDENTITIES"] = "true"
 os.environ["ROX_POSTGRES_DATASTORE"] = "true"
 os.environ["ROX_RISK_REPROCESSING_INTERVAL"] = "15s"
 os.environ["ROX_SENSOR_CONNECTION_RETRY_MAX_INTERVAL"] = "30s"
+
+if ('aws' in os.environ.get('CLUSTER_PROFILE_NAME', '') or
+    'aws' in os.environ.get('CLUSTER_TYPE', '')):
+    os.environ["SETUP_WORKLOAD_IDENTITIES"] = "false"
 
 make_qa_e2e_test_runner(cluster=AutomationFlavorsCluster()).run()
