b4f772b4916f0e4c761a933645d80cdf3ffb979f
davdhacs <105243888+davdhacs@users.noreply.github.com>
2024-03-04 19:09:46 -0700

    add interop e2e target script

---
+++ scripts/ci/jobs/interop_ocp_qa_e2e_tests.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env -S python3 -u
+
+"""
+Run qa-tests-backend in an OCP cluster on the Interop testing environment.
+"""
+import os
+from base_qa_e2e_test import make_qa_e2e_test_runner
+from clusters import AutomationFlavorsCluster
+
+# set required test parameters
+os.environ["DEPLOY_STACKROX_VIA_OPERATOR"] = "true"
+os.environ["ORCHESTRATOR_FLAVOR"] = "openshift"
+os.environ["SETUP_WORKLOAD_IDENTITIES"] = "false"
+os.environ["ROX_POSTGRES_DATASTORE"] = "true"
+os.environ["ROX_RISK_REPROCESSING_INTERVAL"] = "15s"
+os.environ["ROX_SENSOR_CONNECTION_RETRY_MAX_INTERVAL"] = "30s"
+
+make_qa_e2e_test_runner(cluster=AutomationFlavorsCluster()).run()
