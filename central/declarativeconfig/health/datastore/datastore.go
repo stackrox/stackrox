e749837001ffc71d51e246ad8422def89271f0ba
Daniel Haus <dhaus@redhat.com>
2024-03-06 06:11:04 +0100

    chore(dec-config): add tests to access scope updater

--- central/declarativeconfig/health/datastore/datastore.go
+++ central/declarativeconfig/health/datastore/datastore.go
@@ -2,9 +2,12 @@ package datastore
 
 import (
 	"context"
+	"testing"
 
 	"github.com/stackrox/rox/central/declarativeconfig/health/datastore/store"
+	pgStore "github.com/stackrox/rox/central/declarativeconfig/health/datastore/store/postgres"
 	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/postgres"
 )
 
 // DataStore is the entry point for modifying declarative config health data.
@@ -23,4 +26,9 @@ func New(storage store.Store) DataStore {
 	return &datastoreImpl{
 		store: storage,
 	}
+}
+
+// GetTestPostgresDataStore provides a datastore connected to postgres for testing purposes.
+func GetTestPostgresDataStore(_ *testing.T, pool postgres.DB) DataStore {
+	return New(pgStore.New(pool))
 }
