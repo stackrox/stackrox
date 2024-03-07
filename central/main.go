c1dc153d6e612ac505d831f5430ad41f9bfc7639
Gavin Jefferies <gjefferi@redhat.com>
2024-03-07 12:47:15 -0800

    Critical to trip the check

--- central/main.go
+++ central/main.go
@@ -282,7 +282,8 @@ func main() {
 
 	log.Infof("Running StackRox Version: %s", pkgVersion.GetMainVersion())
 	log.Errorf("Failed to find categories associated with policy %s: %q. Error: 123",
-		"7b71fba0-2afb-4e4e-abf3-0f461cd76acc", pkgVersion.GetMainVersion())
+		"7b71fba0-2afb-4e4e-abf3-0f461cd76acc",
+		"Privileged Containers with Important and Critical Fixable CVEs")
 	ensureDB(ctx)
 
 	if !pgconfig.IsExternalDatabase() {
