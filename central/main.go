69a2a56c980d9c3b51c2b356fb455a825b0b770a
Gavin Jefferies <gjefferi@redhat.com>
2024-03-07 09:52:29 -0800

    force originating failure

--- central/main.go
+++ central/main.go
@@ -281,6 +281,8 @@ func main() {
 	devmode.StartOnDevBuilds("central")
 
 	log.Infof("Running StackRox Version: %s", pkgVersion.GetMainVersion())
+	log.Errorf("Failed to find categories associated with policy %s: %q. Error: 123",
+		"7b71fba0-2afb-4e4e-abf3-0f461cd76acc", pkgVersion.GetMainVersion())
 	ensureDB(ctx)
 
 	if !pgconfig.IsExternalDatabase() {
