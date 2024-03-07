b96a1979cefa3cb1ad920f78df90a57f7c557047
lvalerom <lvalerom@redhat.com>
2024-03-07 16:35:54 +0100

    use the uniqueue in the resolver

--- sensor/kubernetes/eventpipeline/resolver/resolver.go
+++ sensor/kubernetes/eventpipeline/resolver/resolver.go
@@ -2,16 +2,23 @@ package resolver
 
 import (
 	"github.com/stackrox/rox/pkg/concurrency"
+	"github.com/stackrox/rox/pkg/features"
 	"github.com/stackrox/rox/sensor/common/store"
 	"github.com/stackrox/rox/sensor/kubernetes/eventpipeline/component"
+	"github.com/stackrox/rox/sensor/kubernetes/eventpipeline/uniqueue"
 )
 
 // New instantiates a Resolver component.
 func New(outputQueue component.OutputQueue, provider store.Provider, queueSize int) component.Resolver {
-	return &resolverImpl{
-		outputQueue:   outputQueue,
-		innerQueue:    make(chan *component.ResourceEvent, queueSize),
-		storeProvider: provider,
-		stopper:       concurrency.NewStopper(),
+	res := &resolverImpl{
+		outputQueue:        outputQueue,
+		innerQueue:         make(chan *component.ResourceEvent, queueSize),
+		storeProvider:      provider,
+		stopper:            concurrency.NewStopper(),
+		deploymentRefQueue: nil,
 	}
+	if features.SensorAggregateDeploymentReferenceOptimization.Enabled() {
+		res.deploymentRefQueue = uniqueue.NewUniQueue()
+	}
+	return res
 }
