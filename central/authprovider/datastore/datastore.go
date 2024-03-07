f0ebcb2ae5347e6759859c6c2e4ff4bec34858ad
Daniel Haus <dhaus@redhat.com>
2024-03-07 02:13:06 +0100

    chore(dec-config): add tests to role updater

--- central/authprovider/datastore/datastore.go
+++ central/authprovider/datastore/datastore.go
@@ -1,13 +1,24 @@
 package datastore
 
 import (
+	"testing"
+
 	"github.com/stackrox/rox/central/authprovider/datastore/internal/store"
+	pgStore "github.com/stackrox/rox/central/authprovider/datastore/internal/store/postgres"
 	"github.com/stackrox/rox/pkg/auth/authproviders"
+	"github.com/stackrox/rox/pkg/postgres"
 )
 
 // New returns a new Store instance.
 func New(storage store.Store) authproviders.Store {
 	return &datastoreImpl{
 		storage: storage,
+	}
+}
+
+// GetTestPostgresDataStore provides a datastore connected to postgres for testing purposes.
+func GetTestPostgresDataStore(_ *testing.T, pool postgres.DB) authproviders.Store {
+	return &datastoreImpl{
+		storage: pgStore.New(pool),
 	}
 }
