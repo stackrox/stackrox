6c5591ece628300b4b1c54049f29f321bf40f832
Tomasz Janiszewski <tomek@redhat.com>
2024-03-11 19:53:25 +0100

    Update qa-tests-backend/scripts/migrate_protos.sh
    Co-authored-by: Mladen Todorovic <mtodor@gmail.com>

--- qa-tests-backend/scripts/migrate_protos.sh
+++ qa-tests-backend/scripts/migrate_protos.sh
@@ -8,6 +8,5 @@ cp -r ../proto/* ${JAVA_PATH}
 
 # Migrate v1 API protos from the Scanner repo
 SCANNER_DIR=$(go list -f '{{.Dir}}' -m github.com/stackrox/scanner)
-SCANNER_PROTO_BASE_PATH=$SCANNER_DIR/proto/
 # files from gomod cache have no write permission causing problems for gradle
-cp -r --no-preserve=mode ${SCANNER_PROTO_BASE_PATH}/* ${JAVA_PATH}
+cp -r --no-preserve=mode ${SCANNER_DIR}/proto/* ${JAVA_PATH}
