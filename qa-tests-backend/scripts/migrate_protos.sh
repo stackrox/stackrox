1da987fdb354a81ee58c362c0fb505037d051cba
Tomasz Janiszewski <tomek@redhat.com>
2024-03-11 20:54:08 +0100

    chore: cleanup qa protos migration script (#10257)
    Co-authored-by: Mladen Todorovic <mtodor@gmail.com>

--- qa-tests-backend/scripts/migrate_protos.sh
+++ qa-tests-backend/scripts/migrate_protos.sh
@@ -3,36 +3,10 @@
 JAVA_PATH=src/main/proto/
 
 # Migrate protos from the stackrox repo.
-
-for file in $(find ../proto/*); do
-    if [[ -d $file ]]; then
-        dir=${file#"../proto/"}
-        mkdir -p "${JAVA_PATH}${dir}"
-        echo "${JAVA_PATH}${dir}"
-    fi
-done
-
-for file in $(find ../proto/* -name '*.proto'); do
-    if [[ -f $file ]]; then
-        java_file=${file#"../proto/"}
-        sed -e ':a' -e 'N' -e '$!ba' -e 's/\[\([^]]*\)\]//g' "$file" | sed '/gogo/d' > "${JAVA_PATH}${java_file}"
-    fi
-done
+mkdir -p ${JAVA_PATH}
+cp -r ../proto/* ${JAVA_PATH}
 
 # Migrate v1 API protos from the Scanner repo
-
 SCANNER_DIR=$(go list -f '{{.Dir}}' -m github.com/stackrox/scanner)
-SCANNER_PROTO_BASE_PATH=$SCANNER_DIR/proto
-
-mkdir -p "${JAVA_PATH}scanner/api/v1"
-echo "${JAVA_PATH}scanner/api/v1"
-
-for file in $(find "$SCANNER_PROTO_BASE_PATH" -name '*.proto'); do
-    if [[ -f $file ]]; then
-        # Get relative path. Should be along the lines of scanner/api/v1/*.proto
-        rel_file=${file/"$SCANNER_PROTO_BASE_PATH"/""}
-        rel_file="${rel_file:1}"
-        sed -e ':a' -e 'N' -e '$!ba' -e 's/\[\([^]]*\)\]//g' "$file" | sed '/gogo/d' > "${JAVA_PATH}${rel_file}"
-    fi
-done
-
+# files from gomod cache have no write permission causing problems for gradle
+cp -r --no-preserve=mode ${SCANNER_DIR}/proto/* ${JAVA_PATH}
