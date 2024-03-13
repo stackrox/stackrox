95889a70762931e89e8aec1ed31abce667bb143a
Yann Brillouet <ybrillou@redhat.com>
2024-03-13 10:26:15 +0100

    post-rebase fixes

--- central/group/service/service_impl.go
+++ central/group/service/service_impl.go
@@ -3,7 +3,6 @@ package service
 import (
 	"context"
 
-	"github.com/gogo/protobuf/proto"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/pkg/errors"
 	"github.com/stackrox/rox/central/group/datastore"
