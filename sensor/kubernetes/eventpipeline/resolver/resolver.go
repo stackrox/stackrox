8a89ec4d21794e64094aff3aa881053ed331c689
lvalerom <lvalerom@redhat.com>
2024-03-13 08:45:39 +0100

    add metrics

--- sensor/kubernetes/eventpipeline/resolver/resolver.go
+++ sensor/kubernetes/eventpipeline/resolver/resolver.go
@@ -3,6 +3,7 @@ package resolver
 import (
 	"github.com/stackrox/rox/pkg/concurrency"
 	"github.com/stackrox/rox/pkg/features"
+	"github.com/stackrox/rox/sensor/common/metrics"
 	"github.com/stackrox/rox/sensor/common/store"
 	"github.com/stackrox/rox/sensor/kubernetes/eventpipeline/component"
 	"github.com/stackrox/rox/sensor/kubernetes/eventpipeline/uniqueue"
@@ -18,7 +19,7 @@ func New(outputQueue component.OutputQueue, provider store.Provider, queueSize i
 		deploymentRefQueue: nil,
 	}
 	if features.SensorAggregateDeploymentReferenceOptimization.Enabled() {
-		res.deploymentRefQueue = uniqueue.NewUniQueue()
+		res.deploymentRefQueue = uniqueue.NewUniQueue(uniqueue.WithMetrics(metrics.ResolverDedupingQueueSize))
 	}
 	return res
 }
