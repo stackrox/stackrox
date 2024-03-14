9d6728da093c6ccb22e63b5796f8e4b118b75441
Yann Brillouet <ybrillou@redhat.com>
2024-03-14 15:45:21 +0100

    chore(proto): workaround unsupported cp option on mac

--- qa-tests-backend/scripts/migrate_protos.sh
+++ qa-tests-backend/scripts/migrate_protos.sh
@@ -9,4 +9,6 @@ cp -r ../proto/* ${JAVA_PATH}
 # Migrate v1 API protos from the Scanner repo
 SCANNER_DIR=$(go list -f '{{.Dir}}' -m github.com/stackrox/scanner)
 # files from gomod cache have no write permission causing problems for gradle
-cp -r --no-preserve=mode ${SCANNER_DIR}/proto/* ${JAVA_PATH}
+cp -r ${SCANNER_DIR}/proto/* ${JAVA_PATH}
+chown -R $(whoami) ${JAVA_PATH}
+chmod -R u+rw ${JAVA_PATH}
