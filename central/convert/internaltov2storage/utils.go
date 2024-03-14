d70a0fa9beaf417121a520d6a57120b8bb218d18
Dave Shrewsberry <dashrews@redhat.com>
2024-03-14 11:38:55 -0400

    style

--- central/convert/internaltov2storage/utils.go
+++ central/convert/internaltov2storage/utils.go
@@ -9,12 +9,14 @@ var (
 	log = logging.LoggerForModule()
 )
 
+// BuildProfileRefID returns a UUID based off cluster, profileID, and product type
 func BuildProfileRefID(clusterID string, profileID string, productType string) string {
 	interimUUID := buildDeterministicID(clusterID, profileID)
 
 	return buildDeterministicID(interimUUID, productType)
 }
 
+// BuildScanRefID returns a UUID based off cluster and scan name
 func BuildScanRefID(clusterID string, scanName string) string {
 	return buildDeterministicID(clusterID, scanName)
 }
