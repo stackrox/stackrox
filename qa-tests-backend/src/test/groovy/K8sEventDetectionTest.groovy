c6b7c649fd42e427c223cf8b5130e8fd1cc52ee2
Tomasz Janiszewski <tomek@redhat.com>
2024-03-15 14:18:03 +0100

    chore: use common test image (#10194)
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

--- qa-tests-backend/src/test/groovy/K8sEventDetectionTest.groovy
+++ qa-tests-backend/src/test/groovy/K8sEventDetectionTest.groovy
@@ -20,7 +20,7 @@ class K8sEventDetectionTest extends BaseSpecification {
     static private registerDeployment(String name, boolean privileged) {
         DEPLOYMENTS.add(
             new Deployment().setName(name)
-                .setImage("quay.io/rhacs-eng/qa-multi-arch:nginx-1-14-alpine").addLabel("app", name).
+                .setImage(TEST_IMAGE).addLabel("app", name).
                 setPrivilegedFlag(privileged)
         )
         return name
