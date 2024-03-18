d6cfcf82421194569cda37a918037010b691b35d
davdhacs <105243888+davdhacs@users.noreply.github.com>
2024-03-18 13:35:45 -0600

    typo

--- scripts/ci/jobs/ocp_qa_e2e_tests.py
+++ scripts/ci/jobs/ocp_qa_e2e_tests.py
@@ -11,7 +11,7 @@ from clusters import AutomationFlavorsCluster
 os.environ["DEPLOY_STACKROX_VIA_OPERATOR"] = "true"
 os.environ["ORCHESTRATOR_FLAVOR"] = "openshift"
 if 'openshift-4' in os.environ.get('CLUSTER_FLAVOR_VARIANT', ''):
-    os.environ["SETUP_WORKLOAD_IDENTITIES"] = "true
+    os.environ["SETUP_WORKLOAD_IDENTITIES"] = "true"
 os.environ["ROX_POSTGRES_DATASTORE"] = "true"
 os.environ["ROX_RISK_REPROCESSING_INTERVAL"] = "15s"
 os.environ["ROX_SENSOR_CONNECTION_RETRY_MAX_INTERVAL"] = "30s"
