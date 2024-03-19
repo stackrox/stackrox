4f003b05500096d80350c24c478793623b35b169
Moritz Clasmeier <111092021+mclasmeier@users.noreply.github.com>
2024-03-19 16:44:19 +0100

    ROX-23096: Abort rendering of Helm charts if Helm version is < v3.9.0 (#10350)
    Co-authored-by: Moritz Clasmeier <mclasmeier@redhat.com>

--- pkg/helm/charts/tests/centralservices/base_suite_test.go
+++ pkg/helm/charts/tests/centralservices/base_suite_test.go
@@ -136,3 +136,33 @@ func (s *baseSuite) TestAllGeneratableExplicit() {
 		s.NotEmptyf(v, "unexpected empty rendered YAML %s", k)
 	}
 }
+
+func (s *baseSuite) TestHelmVersionRequirements() {
+	helmImage := image.GetDefaultImage()
+	helmVals, err := chartutil.ReadValues([]byte(autogenerateAll))
+	s.Require().NoError(err, "failed to parse values string %s", autogenerateAll)
+
+	confAllowUnsupporteVersion := "allowUnsupportedHelmVersion: true"
+	helmValsAllowingUnsupportedVersion, err := chartutil.ReadValues([]byte(confAllowUnsupporteVersion))
+	s.Require().NoError(err, "failed to parse values string %s", confAllowUnsupporteVersion)
+	helmValsAllowingUnsupportedVersion = chartutil.CoalesceTables(helmValsAllowingUnsupportedVersion, helmVals)
+
+	// Retrieve template files from box.
+	tpl, err := helmImage.GetCentralServicesChartTemplate()
+	s.Require().NoError(err, "error retrieving chart template")
+	ch, err := tpl.InstantiateAndLoad(metaUtil.MakeMetaValuesForTest(s.T()))
+	s.Require().NoError(err, "error instantiating chart")
+
+	effectiveInstallOpts := installOpts
+	effectiveInstallOpts.HelmVersion = "v3.8.99"
+
+	_, err = helmUtil.Render(ch, helmVals, effectiveInstallOpts)
+	s.Require().Error(err, "successfully rendered chart")
+
+	_, err = helmUtil.Render(ch, helmValsAllowingUnsupportedVersion, effectiveInstallOpts)
+	s.Require().NoError(err, "failed to render chart while allowing unsupported Helm versions")
+
+	effectiveInstallOpts.HelmVersion = "v3.9.0"
+	_, err = helmUtil.Render(ch, helmVals, effectiveInstallOpts)
+	s.Require().NoError(err, "failed to render chart")
+}
