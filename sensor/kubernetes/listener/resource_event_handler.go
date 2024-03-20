13f9c1cf9799f22a2ad2219b3f1b5c44b5936d65
lvalerom <lvalerom@redhat.com>
2024-03-20 16:42:24 +0100

    start co dispatchers in runtime

--- sensor/kubernetes/listener/resource_event_handler.go
+++ sensor/kubernetes/listener/resource_event_handler.go
@@ -82,7 +82,62 @@ func (k *listenerImpl) handleAllEvents() {
 	var profileLister cache.GenericLister
 	if resourceList, err := watcherUtils.ServerResourcesForGroup(k.client, complianceoperator.GetGroupVersion().String()); err != nil {
 		log.Errorf("Checking API resources for group %q: %v", complianceoperator.GetGroupVersion().String(), err)
-		go watcherRegistry.ForComplianceOperatorRules().Watch(&k.stopSig, k.stopCallback)
+		go watcherRegistry.ForComplianceOperatorRules().Watch(&k.stopSig, func(_ string) {
+			log.Info("Starting CO dispatchers in runtime")
+			podInformer := sif.Core().V1().Pods()
+			dispatchers := resources.NewDispatcherRegistry(
+				clusterID,
+				podInformer.Lister(),
+				profileLister,
+				processfilter.Singleton(),
+				k.configHandler,
+				k.credentialsManager,
+				k.traceWriter,
+				k.storeProvider,
+				k.client.Kubernetes(),
+			)
+			// The group that has no other object dependencies
+			noDependencyWaitGroup := &concurrency.WaitGroup{}
+			// we will single-thread event processing using this lock
+			var eventLock sync.Mutex
+			stopSignal := &k.stopSig
+			log.Info("initializing compliance operator informers")
+			crdSharedInformerFactory = dynamicinformer.NewDynamicSharedInformerFactory(k.client.Dynamic(), noResyncPeriod)
+			complianceResultInformer = crdSharedInformerFactory.ForResource(complianceoperator.ComplianceCheckResult.GroupVersionResource()).Informer()
+			complianceProfileInformer = crdSharedInformerFactory.ForResource(complianceoperator.Profile.GroupVersionResource()).Informer()
+			profileLister = crdSharedInformerFactory.ForResource(complianceoperator.Profile.GroupVersionResource()).Lister()
+
+			complianceScanSettingBindingsInformer = crdSharedInformerFactory.ForResource(complianceoperator.ScanSettingBinding.GroupVersionResource()).Informer()
+			complianceRuleInformer = crdSharedInformerFactory.ForResource(complianceoperator.Rule.GroupVersionResource()).Informer()
+			complianceScanInformer = crdSharedInformerFactory.ForResource(complianceoperator.ComplianceScan.GroupVersionResource()).Informer()
+			complianceTailoredProfileInformer = crdSharedInformerFactory.ForResource(complianceoperator.TailoredProfile.GroupVersionResource()).Informer()
+			complianceSuiteInformer = crdSharedInformerFactory.ForResource(complianceoperator.ComplianceSuite.GroupVersionResource()).Informer()
+			if crdSharedInformerFactory != nil {
+				log.Info("syncing compliance operator resources")
+				// Handle results, rules, and scan setting bindings first
+				handle(k.context, complianceResultInformer, dispatchers.ForComplianceOperatorResults(), k.outputQueue, &syncingResources, noDependencyWaitGroup, stopSignal, &eventLock)
+				handle(k.context, complianceRuleInformer, dispatchers.ForComplianceOperatorRules(), k.outputQueue, &syncingResources, noDependencyWaitGroup, stopSignal, &eventLock)
+				handle(k.context, complianceScanSettingBindingsInformer, dispatchers.ForComplianceOperatorScanSettingBindings(), k.outputQueue, &syncingResources, noDependencyWaitGroup, stopSignal, &eventLock)
+				handle(k.context, complianceScanInformer, dispatchers.ForComplianceOperatorScans(), k.outputQueue, &syncingResources, noDependencyWaitGroup, stopSignal, &eventLock)
+				handle(k.context, complianceSuiteInformer, dispatchers.ForComplianceOperatorSuites(), k.outputQueue, &syncingResources, noDependencyWaitGroup, stopSignal, &eventLock)
+			}
+			if !startAndWait(stopSignal, noDependencyWaitGroup, crdSharedInformerFactory) {
+				return
+			}
+
+			preTopLevelDeploymentWaitGroup := &concurrency.WaitGroup{}
+			// Compliance operator profiles are handled AFTER results, rules, and scan setting bindings have been synced
+			if complianceProfileInformer != nil {
+				handle(k.context, complianceProfileInformer, dispatchers.ForComplianceOperatorProfiles(), k.outputQueue, &syncingResources, preTopLevelDeploymentWaitGroup, stopSignal, &eventLock)
+			}
+			if complianceTailoredProfileInformer != nil {
+				handle(k.context, complianceTailoredProfileInformer, dispatchers.ForComplianceOperatorTailoredProfiles(), k.outputQueue, &syncingResources, preTopLevelDeploymentWaitGroup, stopSignal, &eventLock)
+			}
+
+			if !startAndWait(stopSignal, preTopLevelDeploymentWaitGroup, crdSharedInformerFactory) {
+				return
+			}
+		})
 	} else if watcherUtils.ResourceExists(resourceList, complianceoperator.ComplianceCheckResult.Name) {
 		log.Info("initializing compliance operator informers")
 		crdSharedInformerFactory = dynamicinformer.NewDynamicSharedInformerFactory(k.client.Dynamic(), noResyncPeriod)
