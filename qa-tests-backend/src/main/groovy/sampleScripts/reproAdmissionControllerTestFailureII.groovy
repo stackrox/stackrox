b7c50990126b11c525f21b8129f7c93076b19dbd
Gavin Jefferies <gjefferi@redhat.com>
2024-03-19 18:51:58 -0700

    wip

--- qa-tests-backend/src/main/groovy/sampleScripts/reproAdmissionControllerTestFailureII.groovy
+++ qa-tests-backend/src/main/groovy/sampleScripts/reproAdmissionControllerTestFailureII.groovy
@@ -1,11 +1,15 @@
 package sampleScripts
 
+import static util.Helpers.withRetry
+
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 import common.Constants
+import io.stackrox.proto.api.v1.SearchServiceOuterClass
 import io.stackrox.proto.storage.ClusterOuterClass
 import io.stackrox.proto.storage.ClusterOuterClass.AdmissionControllerConfig
+import io.stackrox.proto.storage.ImageOuterClass
 import io.stackrox.proto.storage.PolicyOuterClass
 import io.stackrox.proto.storage.PolicyOuterClass.Policy
 import io.stackrox.proto.storage.ScopeOuterClass
@@ -44,6 +48,8 @@ String TEST_NAMESPACE = "qa-admission-controller-test"
 
 String NGINX = "qanginx"
 String NGINX_IMAGE = "quay.io/rhacs-eng/qa-multi-arch:nginx-1.21.1"
+String NGINX_IMAGE_DIGEST = "sha256:6bf47794f923462389f5a2cda49cf5777f736db8563edc3ff78fb9d87e6e22ec"
+String NGINX_IMAGE_WITH_DIGEST = "quay.io/rhacs-eng/qa-multi-arch:nginx-1.21.1@${NGINX_IMAGE_DIGEST}"
 
 String CLONED_POLICY_SUFFIX = "(${TEST_NAMESPACE})"
 String LATEST_TAG = "Latest tag"
@@ -52,7 +58,8 @@ String SEVERITY = "Fixable Severity at least Important"
 Deployment NGINX_DEPLOYMENT = new Deployment()
             .setName(NGINX)
             .setNamespace(TEST_NAMESPACE)
-            .setImage(NGINX_IMAGE)
+        //     .setImage(NGINX_IMAGE)
+            .setImage(NGINX_IMAGE_WITH_DIGEST)
             .addLabel("app", "test")
 
 if (false) {
@@ -75,9 +82,6 @@ if (false) {
         // Wait for propagation to sensor
         sleep(10000)
 
-        // Pre run scan to avoid timeouts with inline scans in the tests below
-        ImageService.scanImage(NGINX_IMAGE)
-
         client.ensureNamespaceExists(TEST_NAMESPACE)
 
         AdmissionControllerConfig ac = AdmissionControllerConfig.newBuilder()
@@ -92,11 +96,40 @@ if (false) {
         sleep(5000)
 }
 
+// Pre run scan to avoid timeouts with inline scans in the tests below
+// ImageService.scanImage(NGINX_IMAGE)
+// ImageService.scanImage(NGINX_IMAGE_WITH_DIGEST)
+
+// Does nothing of value for this
+// ImageService.clearImageCaches()
+
+// ImageService.deleteImagesWithRetry(
+//         SearchServiceOuterClass.RawQuery.newBuilder()
+//                 .setQuery("Image:${NGINX_IMAGE_WITH_DIGEST}").build(), 
+//         true)
+
+// ImageService.deleteImagesWithRetry(
+//         SearchServiceOuterClass.RawQuery.newBuilder()
+//                 .setQuery("Image:${NGINX_IMAGE}").build(), 
+//         true)
+
+// Wait until we received metadata from all images we want to test. This will ensure that enrichment
+// has finalized.
+// withRetry(18, 10) {
+//         ImageOuterClass.Image image = ImageService.getImage(NGINX_IMAGE_DIGEST, false)
+//         assert image
+//         assert !image.getNotesList().contains(ImageOuterClass.Image.Note.MISSING_METADATA)
+//         LOG.debug("${image.getName()} ${image.getNotesList()}")
+// }
+
+// sleep(60000)
+
 def i=0
-while (i<1000) {
+while (i<10000) {
         LOG.debug("About to create")
         def created = client.createDeploymentNoWait(NGINX_DEPLOYMENT)
         LOG.debug("Created: ${created}")
         assert !created
         LOG.debug("Iteration: ${i++}")
 }
+
