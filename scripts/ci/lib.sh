576ad30d01efe2921601c893efe6c8ab0f4be9bc
Tomasz Janiszewski <tomek@redhat.com>
2024-03-21 17:54:02 +0100

    wip
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

--- scripts/ci/lib.sh
+++ scripts/ci/lib.sh
@@ -933,22 +933,18 @@ get_pr_details() {
 openshift_ci_mods() {
     info "BEGIN OpenShift CI mods"
 
-    set -x
     #TODO(janisz): remove after https://github.com/openshift/release/pull/49962 is merged
     info "Replace Go"
     go version
-	rm -rf "$GOPATH"
-	GOLANG_VERSION=1.21.8
-	GOLANG_SHA256=538b3b143dc7f32b093c8ffe0e050c260b57fc9d57a12c4140a639a8dd2b4e4f
-	url="https://dl.google.com/go/go$GOLANG_VERSION.linux-amd64.tar.gz"
-	wget --no-verbose -O go.tgz "$url"
-	echo "$GOLANG_SHA256 *go.tgz" | sha256sum -c -
-	tar -C "$GOPATH/.." -xvzf go.tgz
-	rm -rf go.tgz
-	mkdir -p "$GOPATH/src" "$GOPATH/bin"
-	chmod -R 777 "$GOPATH"
-	go version
-    set +x
+    whoami
+    GOLANG_VERSION=1.21.8
+    GOLANG_SHA256=538b3b143dc7f32b093c8ffe0e050c260b57fc9d57a12c4140a639a8dd2b4e4f
+    url="https://dl.google.com/go/go$GOLANG_VERSION.linux-amd64.tar.gz"
+    wget --no-verbose -O /tmp/go.tgz "$url"
+    echo "$GOLANG_SHA256 /tmp/go.tgz" | sha256sum -c -
+    tar -C /tmp/ -xzf /tmp/go.tgz
+    export PATH=/tmp/go/bin:$PATH
+    go version
 
     openshift_ci_debug
 
