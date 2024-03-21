d7b27511b87c6713a32baa5f2bc9aab58f37ad3d
Stephan Heßelmann <shesselm@redhat.com>
2024-03-21 02:45:16 +0100

    fix rebase

--- pkg/env/registry.go
+++ pkg/env/registry.go
@@ -10,18 +10,18 @@ var (
 	// RegistryDialerTimeout is the net.Dialer timeout of the client transport.
 	// It limits the time the dialer attempts the connection. The timeout is
 	// chosen as a small value to prevent unavailable registries from blocking
-	// image scanning.
+	// image scanning. When connecting to a proxy, this timeout only limits the
+	// connection attempt to the proxy and not the proxy target.
 	RegistryDialerTimeout = registerDurationSetting("ROX_REGISTRY_DIALER_TIMEOUT", 5*time.Second)
 
 	// RegistryResponseTimeout is the response header timeout of the client transport.
 	// It limits the time to wait for a server's response headers after fully
 	// writing the request.
-	RegistryResponseTimeout = registerDurationSetting("ROX_REGISTRY_RESPONSE_TIMEOUT", 60*time.Second)
+	RegistryResponseTimeout = registerDurationSetting("ROX_REGISTRY_RESPONSE_TIMEOUT", 10*time.Second)
 
 	// RegistryClientTimeout is used as http.Client.Timeout for the registry's HTTP
 	// client and hence includes everything from connection to reading the
-	// response body. The timeout has been chosen rather arbitrarily, it is
-	// probably less harm in waiting a bit longer than in aborting early a
-	// request that is about to succeed.
-	RegistryClientTimeout = registerDurationSetting("ROX_REGISTRY_CLIENT_TIMEOUT", 90*time.Second)
+	// response body. This timeout must not be chosen too large because it serves
+	// as a connection timeout for proxied registries.
+	RegistryClientTimeout = registerDurationSetting("ROX_REGISTRY_CLIENT_TIMEOUT", 10*time.Second)
 )
