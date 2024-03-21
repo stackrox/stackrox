0ae3a6a505aa87ad55b3055b5d06ec919ec72550
Stephan Heßelmann <shesselm@redhat.com>
2024-03-21 02:13:45 +0100

    reduce timeout to 10 seconds

--- pkg/registries/docker/docker.go
+++ pkg/registries/docker/docker.go
@@ -25,18 +25,18 @@ const (
 	// registryDialerTimeout is the net.Dialer timeout of the client transport.
 	// It limits the time the dialer attempts the connection. The timeout is
 	// chosen as a small value to prevent unavailable registries from blocking
-	// image scanning.
+	// image scanning. When connecting to a proxy, this timeout only limits the
+	// connection attempt to the proxy and not the proxy target.
 	registryDialerTimeout = 5 * time.Second
 	// registryResponseTimeout is the response header timeout of the client transport.
 	// It limits the time to wait for a server's response headers after fully
 	// writing the request.
-	registryResponseTimeout = 60 * time.Second
+	registryResponseTimeout = 10 * time.Second
 	// registryClientTimeout is used as http.Client.Timeout for the registry's HTTP
 	// client and hence includes everything from connection to reading the
-	// response body. The timeout has been chosen rather arbitrarily, it is
-	// probably less harm in waiting a bit longer than in aborting early a
-	// request that is about to succeed.
-	registryClientTimeout = 90 * time.Second
+	// response body. This timeout must not be chosen too large because it serves
+	// as a connection timeout for proxied registries.
+	registryClientTimeout = 10 * time.Second
 	repoListInterval      = 10 * time.Minute
 )
 
