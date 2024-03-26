c29db26abc73124a7d64a85d1aa55ba4c9ff2f85
Daniel Haus <dhaus@redhat.com>
2024-03-26 17:03:46 +0100

    feat(roxctl): add m2m exchange as a command

---
+++ roxctl/central/m2m/exchange/exchange.go
@@ -0,0 +1,121 @@
+package exchange
+
+import (
+	"bytes"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+	v1 "github.com/stackrox/rox/generated/api/v1"
+	"github.com/stackrox/rox/pkg/utils"
+	"github.com/stackrox/rox/roxctl/common/auth"
+	"github.com/stackrox/rox/roxctl/common/config"
+	"github.com/stackrox/rox/roxctl/common/environment"
+	"github.com/stackrox/rox/roxctl/common/flags"
+	"github.com/stackrox/rox/roxctl/common/util"
+)
+
+// Command to exchange an OIDC token for a short-lived access token.
+func Command(cliEnvironment environment.Environment) *cobra.Command {
+	exchangeCmd := exchangeCommand{env: cliEnvironment}
+
+	cmd := &cobra.Command{
+		Use:   "exchange",
+		Short: "Exchanges a OIDC token for a short-lived access token",
+		Long: `Obtain a short-lived access token from Central by exchanging an OIDC token.
+This works by configuring a machine-to-machine access configuration within Central beforehand.
+Based on the OIDC token's issuer, a short-lived access token will be exchanged.`,
+		RunE: util.RunENoArgs(func(command *cobra.Command) error {
+			if err := exchangeCmd.construct(command); err != nil {
+				return err
+			}
+			return exchangeCmd.exchange()
+		}),
+	}
+	cmd.Flags().StringVar(&exchangeCmd.token, "token", "",
+		"OIDC identity token to exchange for a short-lived access token")
+	utils.Must(cmd.MarkFlagRequired("token"))
+	flags.AddTimeoutWithDefault(cmd, 1*time.Minute)
+	return cmd
+}
+
+type exchangeCommand struct {
+	env        environment.Environment
+	timeout    time.Duration
+	centralURL *url.URL
+	token      string
+}
+
+func (e *exchangeCommand) construct(cmd *cobra.Command) error {
+	e.timeout = flags.Timeout(cmd)
+	centralURL, err := flags.CentralURL()
+	if err != nil {
+		return errors.Wrap(err, "retrieving Central URL")
+	}
+	e.centralURL = centralURL
+	return nil
+}
+
+func (e *exchangeCommand) exchange() error {
+	// The exchange API is anonymous, no auth is required.
+	httpClient, err := e.env.HTTPClient(e.timeout, auth.Anonymous())
+	if err != nil {
+		return errors.Wrap(err, "creating HTTP client")
+	}
+
+	req := &v1.ExchangeAuthMachineToMachineTokenRequest{
+		IdToken: e.token,
+	}
+	buf := &bytes.Buffer{}
+	m := jsonpb.Marshaler{}
+	if err := m.Marshal(buf, req); err != nil {
+		return errors.Wrap(err, "creating request body")
+	}
+
+	// Exchange the OIDC token for a short-lived access token.
+
+	resp, err := httpClient.DoReqAndVerifyStatusCode("/v1/auth/m2m/exchange", http.MethodPost,
+		http.StatusOK, buf)
+	if err != nil {
+		return errors.Wrap(err, "exchange request failed")
+	}
+	var exchangeResp v1.ExchangeAuthMachineToMachineTokenResponse
+	if err := jsonpb.Unmarshal(resp.Body, &exchangeResp); err != nil {
+		return errors.Wrap(err, "unmarshalling exchange request response")
+	}
+
+	// Store the OIDC token locally to allow other commands to make use of it.
+
+	cfgStore, err := e.env.ConfigStore()
+	if err != nil {
+		return errors.Wrap(err, "retrieving config store")
+	}
+	cfg, err := cfgStore.Read()
+	if err != nil {
+		return errors.Wrap(err, "reading configuration")
+	}
+	configKey := config.NewConfigKey(e.centralURL)
+
+	existingCfg := cfg.GetCentralConfigs().GetCentralConfig(configKey)
+	now := time.Now()
+	existingCfg.AccessConfig = &config.CentralAccessConfig{
+		AccessToken:  exchangeResp.GetAccessToken(),
+		IssuedAt:     &now,
+		ExpiresAt:    nil,
+		RefreshToken: "",
+	}
+
+	if err := cfgStore.Write(cfg); err != nil {
+		return errors.Wrap(err, "writing configuration")
+	}
+
+	e.env.Logger().InfofLn(`Successfully persisted the authentication information for central %s.
+
+You can now use the exchanged short-lived access token for all other commands!
+
+Note that in case the token is expired, you have to run "roxctl central machine-to-machine exchange" again.`, configKey)
+	return nil
+}
---
+++ roxctl/central/m2m/exchange/exchange_test.go
@@ -0,0 +1,107 @@
+package exchange
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+	"time"
+
+	"github.com/golang/protobuf/jsonpb"
+	v1 "github.com/stackrox/rox/generated/api/v1"
+	"github.com/stackrox/rox/roxctl/common"
+	"github.com/stackrox/rox/roxctl/common/auth"
+	"github.com/stackrox/rox/roxctl/common/config"
+	cfgMock "github.com/stackrox/rox/roxctl/common/config/mocks"
+	"github.com/stackrox/rox/roxctl/common/environment"
+	"github.com/stackrox/rox/roxctl/common/environment/mocks"
+	"github.com/stackrox/rox/roxctl/common/io"
+	"github.com/stackrox/rox/roxctl/common/printer"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+)
+
+func mockEnvWithHTTPClient(t *testing.T, store *cfgMock.MockStore) environment.Environment {
+	mockEnv := mocks.NewMockEnvironment(gomock.NewController(t))
+	testIO, _, _, _ := io.TestIO()
+	env := environment.NewTestCLIEnvironment(t, testIO, printer.DefaultColorPrinter())
+
+	mockEnv.EXPECT().InputOutput().AnyTimes().Return(env.InputOutput())
+	mockEnv.EXPECT().Logger().AnyTimes().Return(env.Logger())
+	mockEnv.EXPECT().GRPCConnection(gomock.Any()).AnyTimes().Return(nil, nil)
+	mockEnv.EXPECT().ColorWriter().AnyTimes().Return(env.ColorWriter())
+	mockEnv.EXPECT().ConfigStore().AnyTimes().Return(store, nil)
+	mockEnv.EXPECT().HTTPClient(gomock.Any(), gomock.Any()).AnyTimes().Return(
+		common.GetRoxctlHTTPClient(auth.Anonymous(), 30*time.Second, false, true, env.Logger()))
+
+	return mockEnv
+}
+
+func TestExchange(t *testing.T) {
+	server := httptest.NewServer(exchangeHandle(t, "test-token"))
+	defer server.Close()
+	// Required for picking up the endpoint used by GetRoxctlHTTPClient. Currently, it is not possible to inject this
+	// otherwise.
+	t.Setenv("ROX_ENDPOINT", server.URL)
+
+	serverURL, err := url.Parse(server.URL)
+	require.NoError(t, err)
+
+	mockStore := cfgMock.NewMockStore(gomock.NewController(t))
+	emptyCfg := &config.RoxctlConfig{CentralConfigs: map[string]*config.CentralConfig{}}
+	mockStore.EXPECT().Read().AnyTimes().Return(emptyCfg, nil)
+	cfgKey := config.NewConfigKey(serverURL)
+	mockStore.EXPECT().Write(matchesConfig(&config.RoxctlConfig{CentralConfigs: map[config.CentralURL]*config.CentralConfig{
+		cfgKey: {AccessConfig: &config.CentralAccessConfig{
+			AccessToken: "test-token",
+		}},
+	}}, cfgKey)).AnyTimes().Return(nil)
+
+	env := mockEnvWithHTTPClient(t, mockStore)
+
+	exchangeCmd := exchangeCommand{
+		env:        env,
+		centralURL: serverURL,
+	}
+	assert.NoError(t, exchangeCmd.exchange())
+}
+
+type centralCfgMatcher struct {
+	roxctlConfig *config.RoxctlConfig
+	configKey    string
+}
+
+func (c centralCfgMatcher) String() string {
+	return "config matcher"
+}
+
+func (c centralCfgMatcher) Matches(arg interface{}) bool {
+	cfgArg := arg.(*config.RoxctlConfig)
+	if cfgArg == nil {
+		return false
+	}
+
+	centralCfg, exists := cfgArg.CentralConfigs[c.configKey]
+	if !exists {
+		return false
+	}
+
+	return c.roxctlConfig.CentralConfigs[c.configKey].AccessConfig.AccessToken == centralCfg.AccessConfig.AccessToken &&
+		centralCfg.AccessConfig.IssuedAt != nil &&
+		centralCfg.AccessConfig.ExpiresAt == nil &&
+		c.roxctlConfig.CentralConfigs[c.configKey].AccessConfig.RefreshToken == centralCfg.AccessConfig.RefreshToken
+}
+
+func matchesConfig(roxctlConfig *config.RoxctlConfig, key string) gomock.Matcher {
+	return centralCfgMatcher{configKey: key, roxctlConfig: roxctlConfig}
+}
+
+func exchangeHandle(t *testing.T, token string) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		m := jsonpb.Marshaler{Indent: "  "}
+		assert.NoError(t, m.Marshal(writer, &v1.ExchangeAuthMachineToMachineTokenResponse{
+			AccessToken: token,
+		}))
+	}
+}
