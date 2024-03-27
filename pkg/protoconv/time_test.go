760ab432c6205a169a38a7055c7ddea0d422e019
Yann Brillouet <ybrillou@redhat.com>
2024-03-27 11:10:24 +0100

    Extend test coverage

--- pkg/protoconv/time_test.go
+++ pkg/protoconv/time_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gogo/protobuf/types"
 	"github.com/stackrox/rox/pkg/protocompat"
+	"github.com/stackrox/rox/pkg/timestamp"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -45,4 +46,22 @@ func TestReadableTime(t *testing.T) {
 	invalidTs := protocompat.GetProtoTimestampFromSeconds(-62234567890)
 	unreadable := ReadableTime(invalidTs)
 	assert.Equal(t, "<malformed time>", unreadable)
+}
+
+func TestConvertMicroTSToProtobufTS(t *testing.T) {
+	time0 := timestamp.MicroTS(0)
+	timestamp0 := ConvertMicroTSToProtobufTS(time0)
+	assert.NotNil(t, timestamp0)
+	assert.Equal(t, int64(0), timestamp0.Seconds)
+	assert.Equal(t, int32(0), timestamp0.Nanos)
+
+	timestamp1 := &types.Timestamp{
+		Seconds: 1518046140,
+		Nanos:   123456789,
+	}
+	time1 := timestamp.FromProtobuf(timestamp1)
+	convertedTimestamp1 := ConvertMicroTSToProtobufTS(time1)
+	assert.NotNil(t, convertedTimestamp1)
+	assert.Equal(t, timestamp1.Seconds, convertedTimestamp1.Seconds)
+	assert.Equal(t, int32(123456000), convertedTimestamp1.Nanos)
 }
