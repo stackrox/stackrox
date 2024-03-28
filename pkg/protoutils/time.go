ee1907a6f9c65295f0425215ee7994945a4dc190
Yann Brillouet <ybrillou@redhat.com>
2024-03-28 15:58:44 +0100

    Restore mistakenly removed function

--- pkg/protoutils/time.go
+++ pkg/protoutils/time.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gogo/protobuf/types"
 	"github.com/stackrox/rox/pkg/protocompat"
 	"github.com/stackrox/rox/pkg/protoconv"
+	"github.com/stackrox/rox/pkg/utils"
 )
 
 const (
@@ -27,6 +28,14 @@ func Sub(ts1, ts2 *types.Timestamp) time.Duration {
 func After(ts1, ts2 *types.Timestamp) bool {
 	diff := Sub(ts1, ts2)
 	return diff > 0
+}
+
+// MustGetProtoTimestampFromRFC3339NanoString generates a proto timestamp from a time string in RFC3339Nano format.
+// The function panics if an error is raised in the conversion process.
+func MustGetProtoTimestampFromRFC3339NanoString(timeStr string) *types.Timestamp {
+	timestamp, err := protocompat.GetProtoTimestampFromRFC3339NanoString(timeStr)
+	utils.CrashOnError(err)
+	return timestamp
 }
 
 // RoundTimestamp rounds up ts to the nearest multiple of d. In case of error, the function returns without rounding up.
