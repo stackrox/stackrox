07ec46c0632d950990bf298e4a4609347659fe3d
David Vail <dcvail17@gmail.com>
2024-03-29 13:25:42 -0400

    chore(ui): React CSSProperties override (#10562)

--- ui/apps/platform/src/react-app-env.d.ts
+++ ui/apps/platform/src/react-app-env.d.ts
@@ -4,7 +4,20 @@
 // var appears to be required to augment the type because of how its defined in the package
 declare var analytics: SegmentAnalytics.AnalyticsJS | undefined; // eslint-disable-line no-var
 
-declare module '*.ico' {
-    const src: string;
-    export default src;
+declare global {
+    // Allows importing of .ico files as a string representing the URL path to the file
+    module '*.ico' {
+        const src: string;
+        export default src;
+    }
+
+    namespace React {
+        // Extend CSSProperties to allow custom CSS properties
+        interface CSSProperties {
+            // Adds PatternFly CSS properties
+            [key: `--pf-v5-${string}`]: string | number | undefined;
+        }
+    }
 }
+
+export {};
