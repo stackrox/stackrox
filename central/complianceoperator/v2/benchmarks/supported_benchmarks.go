4ea682267ef7617212d0a7a1051b262c7cba3baf
Simon Baumer <baeumer@pm.me>
2024-04-02 12:24:11 +0200

    WIP

--- central/complianceoperator/v2/benchmarks/supported_benchmarks.go
+++ central/complianceoperator/v2/benchmarks/supported_benchmarks.go
@@ -1,35 +1,27 @@
 package benchmarks
 
-import (
-	"context"
-
-	"github.com/stackrox/rox/central/complianceoperator/v2/benchmarks/benchmarkstore/postgres"
-	"github.com/stackrox/rox/central/globaldb"
-	"github.com/stackrox/rox/generated/storage"
-)
-
-var benchmarkToComplianceOperatorMapping = map[string]*storage.ComplianceOperatorBenchmark{
-	"cis-ocp-1.4.0": {
-		Name: "CIS Red Hat OpenShift Container Platform Benchmark v1.4.0",
-		//TODO: Add version and more metadata to a benchmark
-	},
-}
-
-// GetByComplianceOperatorLabel returns a benchmark by its Compliance Operator Label.
-func GetByComplianceOperatorLabel(name string) *storage.ComplianceOperatorBenchmark {
-	return benchmarkToComplianceOperatorMapping[name]
-}
+//var benchmarkToComplianceOperatorMapping = map[string]*storage.ComplianceOperatorBenchmark{
+//	"cis-ocp-1.4.0": {
+//		Name: "CIS Red Hat OpenShift Container Platform Benchmark v1.4.0",
+//		//TODO: Add version and more metadata to a benchmark
+//	},
+//}
+//
+//// GetByComplianceOperatorLabel returns a benchmark by its Compliance Operator Label.
+//func GetByComplianceOperatorLabel(name string) *storage.ComplianceOperatorBenchmark {
+//	return benchmarkToComplianceOperatorMapping[name]
+//}
 
 // initializeBenchmarks upserts all supported benchmark profiles. A compliance operator profile can implement a benchmark.
-// On sync with compliance operator a link between a benchmark and a profile can be created automatically.
-// TODO(question): How to import static data to Central?
-func initializeBenchmarks(ctx context.Context) error {
-	db := globaldb.GetPostgres()
-	store := postgres.New(db)
-	for _, benchmark := range benchmarkToComplianceOperatorMapping {
-		if err := store.Upsert(ctx, benchmark); err != nil {
-			return err
-		}
-	}
-	return nil
-}
+//// On sync with compliance operator a link between a benchmark and a profile can be created automatically.
+//// TODO(question): How to import static data to Central?
+//func initializeBenchmarks(ctx context.Context) error {
+//	db := globaldb.GetPostgres()
+//	store := postgres.New(db)
+//	for _, benchmark := range benchmarkToComplianceOperatorMapping {
+//		if err := store.Upsert(ctx, benchmark); err != nil {
+//			return err
+//		}
+//	}
+//	return nil
+//}
