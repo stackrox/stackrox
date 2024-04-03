d847153f136b892fe5b37d66422b465a425bdd01
Daniel Haus <dhaus@redhat.com>
2024-04-03 17:21:32 +0200

    chore(slices): use stdlib slices.Reverse

--- pkg/sliceutils/reverse.go
+++ pkg/sliceutils/reverse.go
@@ -1,18 +1,10 @@
 package sliceutils
 
-// ReverseInPlace reverses the elements of the given slice in-place.
-func ReverseInPlace[T any](slice []T) {
-	l := len(slice)
-	for i := 0; i < l/2; i++ {
-		slice[i], slice[l-1-i] = slice[l-1-i], slice[i]
-	}
-}
+import "slices"
 
 // Reversed returns a slice that contains the elements of the input slice in reverse order.
 func Reversed[T any](slice []T) []T {
-	out := make([]T, 0, len(slice))
-	for i := len(slice) - 1; i >= 0; i-- {
-		out = append(out, slice[i])
-	}
-	return out
+	cloned := slices.Clone(slice)
+	slices.Reverse(cloned)
+	return cloned
 }
