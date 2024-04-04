8e5e351d367adac66f6c866b83a428a99a6ffa11
dhaus67 <dhaus@redhat.com>
2024-04-04 15:32:48 +0200

    chore(slices): use stdlib slices.Clone (#10610)

--- pkg/sliceutils/clone.go
+++ pkg/sliceutils/clone.go
@@ -1,18 +1,6 @@
 package sliceutils
 
-// ShallowClone clones a slice, creating a new slice and copying the contents of the underlying array.
-// If `in` is a nil slice, a nil slice is returned. If `in` is an empty slice, an empty slice is returned.
-func ShallowClone[T any](in []T) []T {
-	if in == nil {
-		return nil
-	}
-	if len(in) == 0 {
-		return []T{}
-	}
-	out := make([]T, len(in))
-	copy(out, in)
-	return out
-}
+import "slices"
 
 // ShallowClone2DSlice clones a 2D slice, creating a new slice and copying the contents of the underlying array.
 // If `in` is a nil slice, a nil slice is returned. If `in` is an empty slice, an empty slice is returned.
@@ -25,7 +13,7 @@ func ShallowClone2DSlice[T any](in [][]T) [][]T {
 	}
 	out := make([][]T, len(in))
 	for idx := range in {
-		out[idx] = ShallowClone[T](in[idx])
+		out[idx] = slices.Clone(in[idx])
 	}
 	return out
 }
