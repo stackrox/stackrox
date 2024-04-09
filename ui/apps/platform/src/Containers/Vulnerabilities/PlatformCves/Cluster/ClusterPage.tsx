12eeb37cb1baf3c23fb426ad5529d977e332827f
David Vail <dcvail17@gmail.com>
2024-04-09 12:55:08 -0400

    Adds Platform CVEs cluster page layout

--- ui/apps/platform/src/Containers/Vulnerabilities/PlatformCves/Cluster/ClusterPage.tsx
+++ ui/apps/platform/src/Containers/Vulnerabilities/PlatformCves/Cluster/ClusterPage.tsx
@@ -1,21 +1,49 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { PageSection, Breadcrumb, Divider, BreadcrumbItem, Skeleton } from '@patternfly/react-core';
+import {
+    PageSection,
+    Breadcrumb,
+    Divider,
+    BreadcrumbItem,
+    Skeleton,
+    Bullseye,
+} from '@patternfly/react-core';
+import { gql, useQuery } from '@apollo/client';
+import { ExclamationCircleIcon } from '@patternfly/react-icons';
 
 import PageTitle from 'Components/PageTitle';
 import BreadcrumbItemLink from 'Components/BreadcrumbItemLink';
+import EmptyStateTemplate from 'Components/PatternFly/EmptyStateTemplate';
+import { getAxiosErrorMessage } from 'utils/responseErrorUtils';
 
+import ClusterPageHeader, { ClusterMetadata, clusterMetadataFragment } from './ClusterPageHeader';
 import { getOverviewPagePath } from '../../utils/searchUtils';
 
 const platformCvesClusterOverviewPath = getOverviewPagePath('Platform', {
     entityTab: 'Cluster',
 });
 
+const clusterMetadataQuery = gql`
+    ${clusterMetadataFragment}
+    query getClusterMetadata($id: ID!) {
+        cluster(id: $id) {
+            ...ClusterMetadata
+        }
+    }
+`;
+
 // TODO - Update for PF5
 function ClusterPage() {
     const { clusterId } = useParams() as { clusterId: string };
 
-    const clusterName = 'TODO - cluster name';
+    const { data, error } = useQuery<{ cluster: ClusterMetadata }, { id: string }>(
+        clusterMetadataQuery,
+        {
+            variables: { id: clusterId },
+        }
+    );
+
+    const clusterName = data?.cluster?.name ?? '';
 
     return (
         <>
@@ -33,7 +61,22 @@ function ClusterPage() {
                 </Breadcrumb>
             </PageSection>
             <Divider component="div" />
-            <PageSection variant="light">Cluster ID: {clusterId}</PageSection>
+            {error ? (
+                <PageSection variant="light">
+                    <Bullseye>
+                        <EmptyStateTemplate
+                            title={getAxiosErrorMessage(error)}
+                            headingLevel="h2"
+                            icon={ExclamationCircleIcon}
+                            iconClassName="pf-v5-u-danger-color-100"
+                        />
+                    </Bullseye>
+                </PageSection>
+            ) : (
+                <PageSection variant="light">
+                    <ClusterPageHeader data={data?.cluster} />
+                </PageSection>
+            )}
         </>
     );
 }
