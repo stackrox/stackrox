c7fa34bd57d3c6a7bed8875a02fb60e215fc9e30
JoukoVirtanen <jouko@stackrox.com>
2024-04-12 18:45:06 -0700

    Added runtime_configuration_collections table

--- central/runtimeconfigurationcollection/store/postgres/gen.go
+++ central/runtimeconfigurationcollection/store/postgres/gen.go
@@ -1,3 +1,3 @@
 package postgres
 
-//go:generate pg-table-bindings-wrapper --type=storage.ResourceCollection --references=collections:storage.ResourceCollection --table=runtime_configuration_collections --search-category COLLECTIONS --cycle=EmbeddedCollections
+//go:generate pg-table-bindings-wrapper --type=storage.ResourceCollection --references=collections:storage.ResourceCollection --table=runtime_configuration_collections --search-category RUNTIME_CONFIGURATION_COLLECTIONS --cycle=EmbeddedCollections
