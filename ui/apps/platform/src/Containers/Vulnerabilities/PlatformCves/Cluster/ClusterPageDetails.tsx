46f85e264a6d21b3f9a050e26fac46790027a304
David Vail <dcvail17@gmail.com>
2024-04-17 08:10:10 -0400

    ROX-23072: Add detail tabs to cluster single page (#10702)

--- ui/apps/platform/src/Containers/Vulnerabilities/PlatformCves/Cluster/ClusterPageDetails.tsx
+++ ui/apps/platform/src/Containers/Vulnerabilities/PlatformCves/Cluster/ClusterPageDetails.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { PageSection, Text } from '@patternfly/react-core';
 
 // eslint-disable-next-line @typescript-eslint/ban-types
-export type NodePageDetailsProps = {};
+export type ClusterPageDetailsProps = {};
 
 // eslint-disable-next-line no-empty-pattern
-function NodePageDetails({}: NodePageDetailsProps) {
+function ClusterPageDetails({}: ClusterPageDetailsProps) {
     return (
         <>
-            <PageSection component="div" variant="light" className="pf-v5-u-py-md pf-v5-u-px-xl">
-                <Text>View details about this node</Text>
+            <PageSection component="div" variant="light" className="pf-v5-u-py-md pf-u-px-xl">
+                <Text>View details about this cluster</Text>
             </PageSection>
             <PageSection isFilled className="pf-v5-u-display-flex pf-v5-u-flex-direction-column">
                 <div className="pf-v5-u-flex-grow-1 pf-v5-u-background-color-100">Details</div>
@@ -18,4 +18,4 @@ function NodePageDetails({}: NodePageDetailsProps) {
     );
 }
 
-export default NodePageDetails;
+export default ClusterPageDetails;
