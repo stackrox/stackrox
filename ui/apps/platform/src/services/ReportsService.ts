c4e51cdec3417e5fb7a5586bd31c844093f13465
Mark Pedrotti <pedrottimark@gmail.com>
2024-04-18 17:04:10 -0400

    ROX-23678: Delete frontend code for VulnMgmt Reports 1.0 (#10795)

--- ui/apps/platform/src/services/ReportsService.ts
+++ ui/apps/platform/src/services/ReportsService.ts
@@ -1,6 +1,5 @@
 import queryString from 'qs';
 
-import { ReportConfiguration as ReportConfigurationV1 } from 'types/report.proto';
 import {
     ReportConfiguration,
     ReportHistoryResponse,
@@ -9,92 +8,9 @@ import {
     ReportStatus,
     RunReportResponse,
 } from 'services/ReportsService.types';
-import searchOptionsToQuery, { RestSearchOption } from 'services/searchOptionsToQuery';
 import { ApiSortOption } from 'types/search';
 import axios from './instance';
 import { Empty } from './types';
-
-const reportUrl = '/v1/report';
-const reportServiceUrl = `${reportUrl}/run`;
-const reportConfigurationsUrl = `${reportUrl}/configurations`;
-const reportConfigurationsCountUrl = '/v1/report-configurations-count';
-
-export function fetchReports(
-    options: RestSearchOption[] = [],
-    sortOption: ApiSortOption,
-    page: number,
-    pageSize: number
-): Promise<ReportConfigurationV1[]> {
-    const offset = page * pageSize;
-    const searchOptions: RestSearchOption[] = [...options];
-    const query = searchOptionsToQuery(searchOptions);
-    const queryObject: Record<string, string | Record<string, number | string | ApiSortOption>> = {
-        pagination: {
-            offset,
-            limit: pageSize,
-            sortOption,
-        },
-    };
-    if (query) {
-        queryObject.query = query;
-    }
-    const params = queryString.stringify(queryObject, { arrayFormat: 'repeat', allowDots: true });
-    return axios
-        .get<{ reportConfigs: ReportConfigurationV1[] }>(`${reportConfigurationsUrl}?${params}`)
-        .then((response) => {
-            return response?.data?.reportConfigs ?? [];
-        });
-}
-
-// TODO: need a way to get total reports count properly
-export function fetchReportsCount(options: RestSearchOption[] = []): Promise<number> {
-    const searchOptions: RestSearchOption[] = [...options];
-    const query = searchOptionsToQuery(searchOptions);
-    const queryObject =
-        searchOptions.length > 0
-            ? {
-                  query,
-              }
-            : {};
-
-    const params = queryString.stringify(queryObject, { arrayFormat: 'repeat', allowDots: true });
-
-    return axios
-        .get<{ count: number }>(`${reportConfigurationsCountUrl}?${params}`)
-        .then((response) => {
-            return response?.data?.count ?? 0;
-        });
-}
-
-export function fetchReportById(reportId: string): Promise<ReportConfigurationV1> {
-    return axios
-        .get<{ reportConfig: ReportConfigurationV1 }>(`${reportConfigurationsUrl}/${reportId}`)
-        .then((response) => {
-            return response?.data?.reportConfig ?? {};
-        });
-}
-
-export function saveReport(report: ReportConfigurationV1): Promise<ReportConfigurationV1> {
-    const apiPayload = {
-        reportConfig: report,
-    };
-
-    const promise = report.id
-        ? axios.put<ReportConfigurationV1>(`${reportConfigurationsUrl}/${report.id}`, apiPayload)
-        : axios.post<ReportConfigurationV1>(reportConfigurationsUrl, apiPayload);
-
-    return promise.then((response) => {
-        return response.data;
-    });
-}
-
-export function deleteReport(reportId: string): Promise<Empty> {
-    return axios.delete(`${reportConfigurationsUrl}/${reportId}`);
-}
-
-export function runReport(reportId: string): Promise<Empty> {
-    return axios.post(`${reportServiceUrl}/${reportId}`);
-}
 
 // The following functions are built around the new VM Reporting Enhancements
 
