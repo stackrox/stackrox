822b01aeb0fc918fcaaa332b40ecf7069ef66873
Brad Rogers <61400697+bradr5@users.noreply.github.com>
2024-04-25 10:32:04 -0500

    ROX-23797: Remove compliance v2 dashboard code (#10842)

--- ui/apps/platform/cypress/integration/compliance-enhanced/ComplianceEnhanced.helpers.js
+++ ui/apps/platform/cypress/integration/compliance-enhanced/ComplianceEnhanced.helpers.js
@@ -11,31 +11,12 @@ export const complianceEnhancedScanConfigsPath = `${basePath}/schedules`;
 // visit helpers
 export const scanConfigsAlias = 'configurations';
 
-// TODO: (vjw, 13 Nov 2023) after the API endpoints for the dashboard are published,
-// we will add them to the routeMatcherMap here
-const routeMatcherMapForComplianceDashboard = null;
-
 const routeMatcherMapForComplianceScanConfigs = {
     [scanConfigsAlias]: {
         method: 'GET',
         url: '/v2/compliance/scan/configurations*',
     },
 };
-
-export function visitComplianceEnhancedFromLeftNav(staticResponseMap) {
-    visitFromLeftNavExpandable(
-        'Compliance (2.0)',
-        'Compliance Status',
-        routeMatcherMapForComplianceDashboard,
-        staticResponseMap
-    );
-}
-
-export function visitComplianceEnhancedDashboard(staticResponseMap) {
-    // TODO: (vjw, 1 Nov 2023) add routes matchers to this function, after API is available for Status
-    visit(basePath, routeMatcherMapForComplianceDashboard, staticResponseMap);
-    cy.get(`h1:contains("Compliance")`);
-}
 
 export function visitComplianceEnhancedCoverageFromLeftNav(staticResponseMap) {
     visitFromLeftNavExpandable('Compliance (2.0)', 'Coverage', null, staticResponseMap);
