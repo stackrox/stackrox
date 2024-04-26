3fd4571c0d5a9ccf890fcfb6bb4cb78924dca5c8
Mladen Todorovic <mtodor@redhat.com>
2024-04-26 11:55:29 +0200

    Change proto gen tools to protobuf V2

--- tools/proto/tools-import.go
+++ tools/proto/tools-import.go
@@ -10,7 +10,8 @@ import (
 	// Tool dependencies, not used anywhere in the code.
 	_ "github.com/bufbuild/buf/cmd/buf"
 	_ "github.com/favadi/protoc-go-inject-tag"
-	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
-	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger"
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
+	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
+	_ "github.com/planetscale/vtprotobuf/cmd/protoc-gen-go-vtproto"
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
 )
