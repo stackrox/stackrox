5dc0af224ff90e2d469c490e3b565dde0aeed8ec
Tom Martensen <tmartens@redhat.com>
2024-04-29 11:49:32 +0200

    add -fast suffix for Konflux builds

--- make/tag.mk
+++ make/tag.mk
@@ -1,11 +1,12 @@
-ifeq ($(TAG),)
-TAG=$(shell git describe --tags --abbrev=10 --dirty --long --exclude '*-nightly-*')
-endif
+SUFFIX=
 
-define tag-impl
-echo $(TAG)
-endef
+ifeq ($(TAG),)
+ifeq ($(KONFLUX_CI),true)
+SUFFIX=-fast
+endif
+TAG=$(shell git describe --tags --abbrev=10 --dirty --long --exclude '*-nightly-*')$(SUFFIX)
+endif
 
 .PHONY: tag
 tag:
-	$(call tag-impl)
+	@echo $(TAG)
