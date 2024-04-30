74cf49fc75bbc812d51e9a7fa6b5362e9ef229dd
Luis Valero Martín <lvalerom@redhat.com>
2024-04-30 12:52:17 +0200

    ROX-17931: Refactor deduping queue (#10341)

--- sensor/kubernetes/eventpipeline/resolver/resolver.go
+++ sensor/kubernetes/eventpipeline/resolver/resolver.go
@@ -2,11 +2,11 @@ package resolver
 
 import (
 	"github.com/stackrox/rox/pkg/concurrency"
+	"github.com/stackrox/rox/pkg/dedupingqueue"
 	"github.com/stackrox/rox/pkg/features"
 	"github.com/stackrox/rox/sensor/common/metrics"
 	"github.com/stackrox/rox/sensor/common/store"
 	"github.com/stackrox/rox/sensor/kubernetes/eventpipeline/component"
-	"github.com/stackrox/rox/sensor/kubernetes/eventpipeline/uniqueue"
 )
 
 // New instantiates a Resolver component.
@@ -19,7 +19,8 @@ func New(outputQueue component.OutputQueue, provider store.Provider, queueSize i
 		deploymentRefQueue: nil,
 	}
 	if features.SensorAggregateDeploymentReferenceOptimization.Enabled() {
-		res.deploymentRefQueue = uniqueue.NewUniQueue(uniqueue.WithMetrics(metrics.ResolverDedupingQueueSize))
+		res.deploymentRefQueue = dedupingqueue.NewDedupingQueue[string](
+			dedupingqueue.WithSizeMetrics[string](metrics.ResolverDedupingQueueSize))
 	}
 	return res
 }
