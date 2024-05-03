63ec43e8c4cd5a3d992754aa531aa9be0d2f5ea8
Michaël Petrov <smartptr@gmail.com>
2024-05-03 14:51:31 +0200

    comment

--- pkg/telemetry/phonehome/headers_multimap.go
+++ pkg/telemetry/phonehome/headers_multimap.go
@@ -7,7 +7,7 @@ import (
 // headers wraps http.Header with a metadata.MD-like interface.
 type headers http.Header
 
-// Get implements HeadersMultiMap interface.
+// Get implements the getter interface.
 func (h headers) Get(key string) []string {
 	return http.Header(h).Values(key)
 }
