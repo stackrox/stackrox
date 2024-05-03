4ac3be7b9743174ea8ea714bea6898f76d80c922
David Caravello <119438707+dcaravel@users.noreply.github.com>
2024-05-03 13:51:52 -0500

    update changelog and add feature desc

--- pkg/features/list.go
+++ pkg/features/list.go
@@ -105,5 +105,5 @@ var (
 
 	// AttemptManifestDigest enables attempting to pull manifest digests from registres that historically did not
 	// support it but now appear to (ie: Nexus and RHEL).
-	AttemptManifestDigest = registerFeature("", "ROX_ATTEMPT_MANIFEST_DIGEST", true)
+	AttemptManifestDigest = registerFeature("Enables attempts to pull manifest digests for all registry integrations", "ROX_ATTEMPT_MANIFEST_DIGEST", true)
 )
