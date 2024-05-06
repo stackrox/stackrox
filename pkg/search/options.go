211a21d071a4e2fe0bf361f2223cfeace0af25a9
Charmik Sheth <101146970+charmik-redhat@users.noreply.github.com>
2024-05-06 10:47:37 -0700

    ROX-23579: Make cluster type and k8s version searchable in cluster (#10844)

--- pkg/search/options.go
+++ pkg/search/options.go
@@ -26,6 +26,8 @@ var (
 	ClusterScope          = newFieldLabel("Cluster Scope")
 	ClusterType           = newFieldLabel("Cluster Type")
 	ClusterDiscoveredTime = newFieldLabel("Cluster Discovered Time")
+	PlatformType          = newFieldLabel("Cluster Platform Type")
+	KubernetesVersion     = newFieldLabel("Cluster Kubernetes Version")
 
 	// cluster health search fields
 	ClusterStatus          = newFieldLabel("Cluster Status")
