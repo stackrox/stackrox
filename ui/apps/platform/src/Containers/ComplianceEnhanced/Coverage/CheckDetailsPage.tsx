9b60a6e768eadf4b13b5feb1fe7f7b677c30ab53
Saif Chaudhry <schaudhr@redhat.com>
2024-05-08 12:57:01 -0700

    ROX-23998: Adding check details page breadcrumbs

--- ui/apps/platform/src/Containers/ComplianceEnhanced/Coverage/CheckDetailsPage.tsx
+++ ui/apps/platform/src/Containers/ComplianceEnhanced/Coverage/CheckDetailsPage.tsx
@@ -1,29 +1,31 @@
 import React from 'react';
-import { useHistory, useParams } from 'react-router-dom';
-import { Button, PageSection } from '@patternfly/react-core';
+import { Breadcrumb, BreadcrumbItem, Divider, PageSection } from '@patternfly/react-core';
+import { useParams } from 'react-router-dom';
 
+import PageTitle from 'Components/PageTitle';
+import BreadcrumbItemLink from 'Components/BreadcrumbItemLink';
 import { complianceEnhancedCoveragePath } from 'routePaths';
 
-function CheckDetailsPage() {
-    const history = useHistory();
-    const { profileName } = useParams();
+function CheckDetails() {
+    const { checkName, profileName } = useParams();
+
+    const complianceCoverageChecksURL = `${complianceEnhancedCoveragePath}/profiles/${profileName}/checks`;
+
     return (
         <>
-            <PageSection variant="light">
-                <div>CheckDetailsPage</div>
-                <Button
-                    onClick={() => {
-                        history.push(
-                            `${complianceEnhancedCoveragePath}/profiles/${profileName}/checks`
-                        );
-                    }}
-                    variant="primary"
-                >
-                    Go to all checks (ProfileChecksPage)
-                </Button>
+            <PageTitle title="Compliance coverage - Check" />
+            <PageSection variant="light" className="pf-v5-u-py-md">
+                <Breadcrumb>
+                    <BreadcrumbItem>Compliance coverage</BreadcrumbItem>
+                    <BreadcrumbItemLink to={complianceCoverageChecksURL}>
+                        {profileName}
+                    </BreadcrumbItemLink>
+                    <BreadcrumbItem isActive>{checkName}</BreadcrumbItem>
+                </Breadcrumb>
             </PageSection>
+            <Divider component="div" />
         </>
     );
 }
 
-export default CheckDetailsPage;
+export default CheckDetails;
