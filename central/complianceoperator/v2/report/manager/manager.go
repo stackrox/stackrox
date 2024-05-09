456e9c64a9eb00c9d6e48188863b73efdd1c3ca6
Khushboo Sancheti <ks@stackrox.com>
2024-05-08 17:05:19 -0700

    misc changes

--- central/complianceoperator/v2/report/manager/manager.go
+++ central/complianceoperator/v2/report/manager/manager.go
@@ -2,6 +2,8 @@ package manager
 
 import (
 	"context"
+
+	"github.com/stackrox/rox/generated/storage"
 )
 
 // Manager implements the interface to run report jobs
@@ -9,5 +11,8 @@ import (
 //go:generate mockgen-wrapper
 type Manager interface {
 	// SubmitReportRequest queues an on demand report generation request for execution
-	SubmitReportRequest(ctx context.Context, scanConfigID string) error
+	SubmitReportRequest(ctx context.Context, scanConfig *storage.ComplianceOperatorScanConfigurationV2) error
+
+	Start()
+	Stop()
 }
--- central/complianceoperator/v2/report/manager/manager_impl.go
+++ central/complianceoperator/v2/report/manager/manager_impl.go
@@ -7,52 +7,60 @@ import (
 
 	"github.com/pkg/errors"
 	scanConfigurationDS "github.com/stackrox/rox/central/complianceoperator/v2/scanconfigurations/datastore"
+	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/pkg/concurrency"
 	"github.com/stackrox/rox/pkg/logging"
 	"github.com/stackrox/rox/pkg/sync"
 )
 
 var (
+	log          logging.Logger
 	maxsizeQueue = 10
 )
 
-type reportJob struct {
-	scanConfigID string
+type reportRequest struct {
+	scanConfig *storage.ComplianceOperatorScanConfigurationV2
 }
 
 type managerImpl struct {
 	datastore            scanConfigurationDS.DataStore
-	runningReportConfigs map[string]*reportJob
-	queuedReports        chan *reportJob // channel for queueing report job requests
-	stopper              concurrency.Stopper
-	mu                   sync.Mutex // Mutex to synchronize access to queuedReports map
-	wg                   sync.WaitGroup
+	runningReportConfigs map[string]*reportRequest
+	//channel for report job requests
+	reportRequests chan *reportRequest
+	stopper        concurrency.Stopper
+
+	//Mutex to synchronize access to runningReportConfigs map
+	mu sync.Mutex
 }
 
 func New(scanConfigDS scanConfigurationDS.DataStore) Manager {
-	manager := &managerImpl{
+	return &managerImpl{
 		datastore:            scanConfigDS,
 		stopper:              concurrency.NewStopper(),
-		runningReportConfigs: make(map[string]*reportJob),
+		runningReportConfigs: make(map[string]*reportRequest, m),
+		reportRequests:       make(chan *reportRequest, maxsizeQueue),
 	}
-	manager.queuedReports = make(chan *reportJob, maxsizeQueue)
-	return manager
 }
 
-func (m *managerImpl) SubmitReportRequest(ctx context.Context, scanConfigID string) error {
-	// verify can be queued based on max size of queue and job from same config is not queued before
-	// queue report job requests
+func (m *managerImpl) SubmitReportRequest(ctx context.Context, scanConfig *storage.ComplianceOperatorScanConfigurationV2) error {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 
-	if _, ok := m.runningReportConfigs[scanConfigID]; ok {
-		return errors.New("Job running for report config")
+	if _, ok := m.runningReportConfigs[scanConfig.GetId()]; ok {
+		return errors.New(fmt.Sprintf("Report request for scan configuration %q already in process", scanConfig.GetScanConfigName()))
 	}
-	// add job to queue
-	return m.appendToReportsQueue(scanConfigID)
+
+	log.Infof("Submitting report for scan at %v for execution", scanConfig.GetScanConfigName(), time.Now().Format(time.RFC822))
+	req := &reportRequest{
+		scanConfig,
+	}
+	m.reportRequests <- req
+	m.runningReportConfigs[scanConfig.GetId()] = req
+
+	return nil
 }
 
-func (m *managerImpl) start() {
+func (m *managerImpl) Start() {
 	go m.runReports()
 }
 
@@ -64,13 +72,13 @@ func (m *managerImpl) Stop() {
 	}
 }
 
-func (m *managerImpl) generateReport(job *reportJob) {
-	defer m.wg.Done()
-	// adding print statement for now. will implement logic to generate reports and call it generateReport in a follow up PR
-	fmt.Printf("running job for scan config id %s", job)
+func (m *managerImpl) generateReport(req *reportRequest) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
-	delete(m.runningReportConfigs, job.scanConfigID)
+
+	// TODO: Implement business logic for querying, formatting and sending report over email
+	logging.Infof("Executing report request for scan config %q", req.scanConfig.GetId())
+	delete(m.runningReportConfigs, req.scanConfig.GetId())
 }
 
 func (m *managerImpl) runReports() {
@@ -80,23 +88,8 @@ func (m *managerImpl) runReports() {
 		case <-m.stopper.Flow().StopRequested():
 			return
 		case req := <-m.queuedReports:
-			m.wg.Add(1)
-			logging.Infof("Executing report '%s' at %v", req.scanConfigID, time.Now().Format(time.RFC822))
-			m.generateReport(req)
-			m.wg.Wait()
+			logging.Infof("Executing report %q at %v", req.scanConfig.GetId(), time.Now().Format(time.RFC822))
+			go m.generateReport(req)
 		}
 	}
-}
-
-func (m *managerImpl) appendToReportsQueue(scanConfigID string) error {
-	if len(m.queuedReports) == maxsizeQueue {
-		return errors.New("Max number of jobs queued")
-	}
-	job := &reportJob{
-		scanConfigID,
-	}
-
-	m.queuedReports <- job
-	m.runningReportConfigs[scanConfigID] = job
-	return nil
 }
