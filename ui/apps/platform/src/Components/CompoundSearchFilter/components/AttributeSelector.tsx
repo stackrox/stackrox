b31272ca0f8e6a77039da64361146166f43bd2d6
Saif Chaudhry <sachaudh@users.noreply.github.com>
2024-05-13 17:37:42 -0700

    ROX-23506: Adding attribute selector (#11032)

--- ui/apps/platform/src/Components/CompoundSearchFilter/components/AttributeSelector.tsx
+++ ui/apps/platform/src/Components/CompoundSearchFilter/components/AttributeSelector.tsx
@@ -1,8 +1,54 @@
 import React from 'react';
+import { SelectOption } from '@patternfly/react-core';
 
-// @TODO: Fill this out
-function AttributeSelector() {
-    return <div />;
+import { CompoundSearchFilterConfig } from 'Components/CompoundSearchFilter/types';
+import { getEntityAttributes } from 'Components/CompoundSearchFilter/utils/utils';
+
+import SimpleSelect from './SimpleSelect';
+import { SelectedEntity } from './EntitySelector';
+
+export type SelectedAttribute = string | undefined;
+export type AttributeSelectorOnChange = (value: string | number | undefined) => void;
+
+export type AttributeSelectorProps = {
+    selectedEntity: SelectedEntity;
+    selectedAttribute: SelectedAttribute;
+    onChange: AttributeSelectorOnChange;
+    config: Partial<CompoundSearchFilterConfig>;
+};
+
+function AttributeSelector({
+    selectedEntity,
+    selectedAttribute,
+    onChange,
+    config,
+}: AttributeSelectorProps) {
+    if (!selectedEntity) {
+        return null;
+    }
+
+    const entityAttributes = getEntityAttributes(selectedEntity, config);
+
+    if (entityAttributes.length === 0) {
+        return null;
+    }
+
+    return (
+        <SimpleSelect
+            value={selectedAttribute}
+            onChange={onChange}
+            ariaLabelMenu="compound search filter attribute selector menu"
+            ariaLabelToggle="compound search filter attribute selector toggle"
+        >
+            {entityAttributes.map((attribute) => {
+                return (
+                    <SelectOption key={attribute.displayName} value={attribute.displayName}>
+                        {attribute.displayName}
+                    </SelectOption>
+                );
+            })}
+        </SimpleSelect>
+    );
 }
 
 export default AttributeSelector;
---
+++ ui/apps/platform/src/Components/CompoundSearchFilter/utils/utils.test.ts
@@ -0,0 +1,104 @@
+import {
+    CompoundSearchFilterConfig,
+    deploymentSearchFilterConfig,
+    imageCVESearchFilterConfig,
+    imageSearchFilterConfig,
+} from '../types';
+import { getEntities, getEntityAttributes, getDefaultEntity, getDefaultAttribute } from './utils';
+
+describe('utils', () => {
+    describe('getEntities', () => {
+        it('should get the entities in a config object', () => {
+            const config: Partial<CompoundSearchFilterConfig> = {
+                Image: imageSearchFilterConfig,
+                Deployment: deploymentSearchFilterConfig,
+                ImageCVE: imageCVESearchFilterConfig,
+            };
+
+            const result = getEntities(config);
+
+            expect(result).toStrictEqual(['Image', 'Deployment', 'ImageCVE']);
+        });
+    });
+
+    describe('getEntityAttributes', () => {
+        it('should get the attributes of an entity in a config object', () => {
+            const config: Partial<CompoundSearchFilterConfig> = {
+                Image: imageSearchFilterConfig,
+                Deployment: deploymentSearchFilterConfig,
+                ImageCVE: imageCVESearchFilterConfig,
+            };
+
+            const result = getEntityAttributes('ImageCVE', config);
+
+            expect(result).toStrictEqual([
+                {
+                    displayName: 'ID',
+                    filterChipLabel: 'Image CVE ID',
+                    searchTerm: 'CVE ID',
+                    inputType: 'autocomplete',
+                },
+                {
+                    displayName: 'Discovered Time',
+                    filterChipLabel: 'Image CVE Discovered Time',
+                    searchTerm: 'CVE Created Time',
+                    inputType: 'date-picker',
+                },
+                {
+                    displayName: 'CVSS',
+                    filterChipLabel: 'Image CVE CVSS',
+                    searchTerm: 'CVSS',
+                    inputType: 'dropdown-slider',
+                },
+                {
+                    displayName: 'Type',
+                    filterChipLabel: 'Image CVE Type',
+                    searchTerm: 'CVE Type',
+                    inputType: 'select',
+                },
+            ]);
+        });
+    });
+
+    describe('getDefaultEntity', () => {
+        it('should get the default (first) entity in a config object', () => {
+            const config: Partial<CompoundSearchFilterConfig> = {
+                Image: imageSearchFilterConfig,
+                Deployment: deploymentSearchFilterConfig,
+                ImageCVE: imageCVESearchFilterConfig,
+            };
+
+            const result = getDefaultEntity(config);
+
+            expect(result).toStrictEqual('Image');
+        });
+
+        // @TODO: Worth considering if we want to ignore vs. highlight the issue
+        it("should ignore entity names that aren't valid", () => {
+            const config = {
+                BOGUS: {
+                    hello: 'friend',
+                },
+                Image: imageSearchFilterConfig,
+            };
+
+            const result = getDefaultEntity(config);
+
+            expect(result).toStrictEqual('Image');
+        });
+    });
+
+    describe('getDefaultAttribute', () => {
+        it('should get the default (first) attribute of a specific entity in a config object', () => {
+            const config: Partial<CompoundSearchFilterConfig> = {
+                Image: imageSearchFilterConfig,
+                Deployment: deploymentSearchFilterConfig,
+                ImageCVE: imageCVESearchFilterConfig,
+            };
+
+            const result = getDefaultAttribute('ImageCVE', config);
+
+            expect(result).toStrictEqual('ID');
+        });
+    });
+});
