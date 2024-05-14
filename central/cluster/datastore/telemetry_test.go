d5b5d22c2db62b83500600d449bc1f31343d8642
Michaël Petrov <smartptr@gmail.com>
2024-05-14 17:41:48 +0200

    test eviction on time

--- central/cluster/datastore/telemetry_test.go
+++ central/cluster/datastore/telemetry_test.go
@@ -2,15 +2,34 @@ package datastore
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stackrox/rox/generated/storage"
+	cache "github.com/stackrox/rox/pkg/expiringcache"
 	"github.com/stackrox/rox/pkg/telemetry/phonehome"
 	"github.com/stackrox/rox/pkg/telemetry/phonehome/telemeter"
 	"github.com/stackrox/rox/pkg/telemetry/phonehome/telemeter/mocks"
 	"go.uber.org/mock/gomock"
 )
 
+type testClock struct {
+	t time.Time
+}
+
+func (tc *testClock) Now() time.Time {
+	return tc.t
+}
+
+func (tc *testClock) add(d time.Duration) {
+	tc.t = tc.t.Add(d)
+}
+
 func Test_sendProps(t *testing.T) {
+	var tc testClock = testClock{time.Now()}
+
+	// Override the global cache with custom clock for testing purposes:
+	clusterIdentityCache = cache.NewExpiringCacheWithClock(&tc, 1*time.Hour)
+
 	teleMock := mocks.NewMockTelemeter(gomock.NewController(t))
 
 	// Custom gomock.Matcher for telemeter.CallOptions:
@@ -73,6 +92,18 @@ func Test_sendProps(t *testing.T) {
 	})
 
 	t.Run("Send updated properties once per cluster", func(t *testing.T) {
+		first := captureTrack(cluster1.Id, "new").Times(1)
+		captureTrack(cluster2.Id, "new").Times(1).After(first)
+
+		sendProps(cfg, cluster1, map[string]any{"property": "new"})
+		sendProps(cfg, cluster2, map[string]any{"property": "new"})
+		sendProps(cfg, cluster1, map[string]any{"property": "new"})
+		sendProps(cfg, cluster2, map[string]any{"property": "new"})
+	})
+
+	t.Run("Send same properties in an hour and a second", func(t *testing.T) {
+		tc.add(1 * time.Hour)
+		tc.add(1 * time.Second)
 		first := captureTrack(cluster1.Id, "new").Times(1)
 		captureTrack(cluster2.Id, "new").Times(1).After(first)
 
