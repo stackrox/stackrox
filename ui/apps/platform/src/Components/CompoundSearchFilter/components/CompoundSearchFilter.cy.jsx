a646df4328fcbc3ebc5fffeb06c5e12ce2bcdd1d
Saif Chaudhry <sachaudh@users.noreply.github.com>
2024-05-13 17:37:42 -0700

    ROX-23506: Adding attribute selector (#11032)

--- ui/apps/platform/src/Components/CompoundSearchFilter/components/CompoundSearchFilter.cy.jsx
+++ ui/apps/platform/src/Components/CompoundSearchFilter/components/CompoundSearchFilter.cy.jsx
@@ -10,8 +10,11 @@ import {
 } from '../types';
 
 const selectors = {
-    selectToggle: 'button[aria-label="compound search filter entity selector toggle"]',
-    selectItems: 'ul[aria-label="compound search filter entity selector items"] li',
+    entitySelectToggle: 'button[aria-label="compound search filter entity selector toggle"]',
+    entitySelectItems: 'div[aria-label="compound search filter entity selector menu"] ul li',
+    entitySelectItem: (text) => `${selectors.entitySelectItems} button:contains(${text})`,
+    attributeSelectToggle: 'button[aria-label="compound search filter attribute selector toggle"]',
+    attributeSelectItems: 'div[aria-label="compound search filter attribute selector menu"] ul li',
 };
 
 function Wrapper({ config }) {
@@ -32,7 +35,7 @@ describe(Cypress.spec.relative, () => {
 
         setup(config);
 
-        cy.get(selectors.selectToggle).should('not.exist');
+        cy.get(selectors.entitySelectToggle).should('not.exist');
     });
 
     it('should display the Image entity in the entity selector', () => {
@@ -42,12 +45,12 @@ describe(Cypress.spec.relative, () => {
 
         setup(config);
 
-        cy.get(selectors.selectToggle).should('contain.text', 'Image');
+        cy.get(selectors.entitySelectToggle).should('contain.text', 'Image');
 
-        cy.get(selectors.selectToggle).click();
+        cy.get(selectors.entitySelectToggle).click();
 
-        cy.get(selectors.selectItems).should('have.length', 1);
-        cy.get(selectors.selectItems).eq(0).should('have.text', 'Image');
+        cy.get(selectors.entitySelectItems).should('have.length', 1);
+        cy.get(selectors.entitySelectItems).eq(0).should('have.text', 'Image');
     });
 
     it('should display Image and Deployment entities in the entity selector', () => {
@@ -58,13 +61,13 @@ describe(Cypress.spec.relative, () => {
 
         setup(config);
 
-        cy.get(selectors.selectToggle).should('contain.text', 'Image');
+        cy.get(selectors.entitySelectToggle).should('contain.text', 'Image');
 
-        cy.get(selectors.selectToggle).click();
+        cy.get(selectors.entitySelectToggle).click();
 
-        cy.get(selectors.selectItems).should('have.length', 2);
-        cy.get(selectors.selectItems).eq(0).should('have.text', 'Image');
-        cy.get(selectors.selectItems).eq(1).should('have.text', 'Deployment');
+        cy.get(selectors.entitySelectItems).should('have.length', 2);
+        cy.get(selectors.entitySelectItems).eq(0).should('have.text', 'Image');
+        cy.get(selectors.entitySelectItems).eq(1).should('have.text', 'Deployment');
     });
 
     it('should display Image, Deployment, and Cluster entities in the entity selector', () => {
@@ -76,13 +79,57 @@ describe(Cypress.spec.relative, () => {
 
         setup(config);
 
-        cy.get(selectors.selectToggle).should('contain.text', 'Image');
+        cy.get(selectors.entitySelectToggle).should('contain.text', 'Image');
 
-        cy.get(selectors.selectToggle).click();
+        cy.get(selectors.entitySelectToggle).click();
 
-        cy.get(selectors.selectItems).should('have.length', 3);
-        cy.get(selectors.selectItems).eq(0).should('have.text', 'Image');
-        cy.get(selectors.selectItems).eq(1).should('have.text', 'Deployment');
-        cy.get(selectors.selectItems).eq(2).should('have.text', 'Cluster');
+        cy.get(selectors.entitySelectItems).should('have.length', 3);
+        cy.get(selectors.entitySelectItems).eq(0).should('have.text', 'Image');
+        cy.get(selectors.entitySelectItems).eq(1).should('have.text', 'Deployment');
+        cy.get(selectors.entitySelectItems).eq(2).should('have.text', 'Cluster');
+    });
+
+    it('should display Image attributes in the attribute selector', () => {
+        const config = {
+            Image: imageSearchFilterConfig,
+            Deployment: deploymentSearchFilterConfig,
+        };
+
+        setup(config);
+
+        cy.get(selectors.attributeSelectToggle).should('contain.text', 'Name');
+
+        cy.get(selectors.attributeSelectToggle).click();
+
+        cy.get(selectors.attributeSelectItems).should('have.length', 8);
+        cy.get(selectors.attributeSelectItems).eq(0).should('have.text', 'Name');
+        cy.get(selectors.attributeSelectItems).eq(1).should('have.text', 'Operating System');
+        cy.get(selectors.attributeSelectItems).eq(2).should('have.text', 'Tag');
+        cy.get(selectors.attributeSelectItems).eq(3).should('have.text', 'CVSS');
+        cy.get(selectors.attributeSelectItems).eq(4).should('have.text', 'Label');
+        cy.get(selectors.attributeSelectItems).eq(5).should('have.text', 'Created Time');
+        cy.get(selectors.attributeSelectItems).eq(6).should('have.text', 'Scan Time');
+        cy.get(selectors.attributeSelectItems).eq(7).should('have.text', 'Registry');
+    });
+
+    it('should display Deployment attributes in the attribute selector', () => {
+        const config = {
+            Image: imageSearchFilterConfig,
+            Deployment: deploymentSearchFilterConfig,
+        };
+
+        setup(config);
+
+        cy.get(selectors.entitySelectToggle).click();
+        cy.get(selectors.entitySelectItem('Deployment')).click();
+
+        cy.get(selectors.attributeSelectToggle).should('contain.text', 'Name');
+
+        cy.get(selectors.attributeSelectToggle).click();
+
+        cy.get(selectors.attributeSelectItems).should('have.length', 3);
+        cy.get(selectors.attributeSelectItems).eq(0).should('have.text', 'Name');
+        cy.get(selectors.attributeSelectItems).eq(1).should('have.text', 'Label');
+        cy.get(selectors.attributeSelectItems).eq(2).should('have.text', 'Annotation');
     });
 });
