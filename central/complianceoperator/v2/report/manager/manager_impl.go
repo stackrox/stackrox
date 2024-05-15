91b107e1300533706047943ef05ce53c35903eeb
Surabhi Kumar <surakuma@redhat.com>
2024-05-15 08:45:48 -0700

    ROX-23952: Added compliance report manager (#11103)

---
+++ central/complianceoperator/v2/report/manager/manager_impl.go
@@ -0,0 +1,124 @@
+package manager
+
+import (
+	"context"
+	"fmt"
+	"sync/atomic"
+	"time"
+
+	"github.com/pkg/errors"
+	scanConfigurationDS "github.com/stackrox/rox/central/complianceoperator/v2/scanconfigurations/datastore"
+	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/concurrency"
+	"github.com/stackrox/rox/pkg/features"
+	"github.com/stackrox/rox/pkg/logging"
+	"github.com/stackrox/rox/pkg/sync"
+)
+
+var (
+	log         = logging.LoggerForModule()
+	maxRequests = 10
+)
+
+type reportRequest struct {
+	scanConfig *storage.ComplianceOperatorScanConfigurationV2
+}
+
+type managerImpl struct {
+	datastore            scanConfigurationDS.DataStore
+	runningReportConfigs map[string]*reportRequest
+	// channel for report job requests
+	reportRequests chan *reportRequest
+	stopper        concurrency.Stopper
+	// isStarted will make sure only one start routine runs for an instance of manager
+	isStarted atomic.Bool
+
+	// isStopped will prevent manager from being re-started once it is stopped
+	isStopped atomic.Bool
+
+	// Mutex to synchronize access to runningReportConfigs map
+	mu sync.Mutex
+}
+
+func New(scanConfigDS scanConfigurationDS.DataStore) Manager {
+	return &managerImpl{
+		datastore:            scanConfigDS,
+		stopper:              concurrency.NewStopper(),
+		runningReportConfigs: make(map[string]*reportRequest, maxRequests),
+		reportRequests:       make(chan *reportRequest, maxRequests),
+	}
+}
+
+func (m *managerImpl) SubmitReportRequest(ctx context.Context, scanConfig *storage.ComplianceOperatorScanConfigurationV2) error {
+	if features.ComplianceReporting.Enabled() {
+		return nil
+	}
+
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	if _, ok := m.runningReportConfigs[scanConfig.GetId()]; ok {
+		return errors.New(fmt.Sprintf("Report request for scan configuration %q already in process", scanConfig.GetScanConfigName()))
+	}
+
+	log.Infof("Submitting report for scan config %s at %v for execution.", scanConfig.GetScanConfigName(), time.Now().Format(time.RFC822))
+	req := &reportRequest{
+		scanConfig,
+	}
+	m.reportRequests <- req
+	m.runningReportConfigs[scanConfig.GetId()] = req
+
+	return nil
+}
+
+func (m *managerImpl) Start() {
+	if m.isStopped.Load() {
+		log.Error("Manager already stopped. It cannot be re-started once stopped.")
+		return
+	}
+	swapped := m.isStarted.CompareAndSwap(false, true)
+	if !swapped {
+		log.Error("Manager already running")
+		return
+	}
+	go m.runReports()
+}
+
+func (m *managerImpl) Stop() {
+	if m.isStarted.Load() {
+		log.Error("Manager not started")
+		return
+	}
+	swapped := m.isStopped.CompareAndSwap(false, true)
+	if !swapped {
+		log.Error("Compliance report manager already stopped")
+		return
+	}
+	m.stopper.Client().Stop()
+	err := m.stopper.Client().Stopped().Wait()
+	if err != nil {
+		logging.Errorf("Error stopping compliance report manager : %v", err)
+	}
+}
+
+func (m *managerImpl) generateReport(req *reportRequest) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	// TODO ROX-24172: Implement business logic for querying, formatting and sending report over email
+	logging.Infof("Executing report request for scan config %q", req.scanConfig.GetId())
+	delete(m.runningReportConfigs, req.scanConfig.GetId())
+}
+
+func (m *managerImpl) runReports() {
+	defer m.stopper.Flow().ReportStopped()
+	for {
+		select {
+		case <-m.stopper.Flow().StopRequested():
+			return
+		case req := <-m.reportRequests:
+			logging.Infof("Executing report %q at %v", req.scanConfig.GetId(), time.Now().Format(time.RFC822))
+			go m.generateReport(req)
+		}
+	}
+}
