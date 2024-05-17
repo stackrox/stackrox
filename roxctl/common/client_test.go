c2c7612a91559beb60ec622b4ddb6c4822f58449
Michaël <smartptr@gmail.com>
2024-05-17 11:54:19 +0200

    fix: drop execution environment header with no value (#11132)

--- roxctl/common/client_test.go
+++ roxctl/common/client_test.go
@@ -1,8 +1,12 @@
 package common
 
 import (
+	"net/http"
 	"testing"
 
+	"github.com/stackrox/rox/pkg/clientconn"
+	"github.com/stackrox/rox/pkg/env"
+	"github.com/stackrox/rox/pkg/telemetry/phonehome"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -16,4 +20,20 @@ func TestSanitizeHeaderValue(t *testing.T) {
 	} {
 		assert.Equal(t, expected, sanitizeHeaderValue(value), value)
 	}
+}
+
+func Test_setCustomHeaders(t *testing.T) {
+	headers := http.Header{}
+	setCustomHeaders(phonehome.Headers(headers).Set)
+	assert.Len(t, headers, 2)
+	assert.Equal(t, "", headers.Get(clientconn.RoxctlCommandHeader))
+	assert.Equal(t, "1", headers.Get(clientconn.RoxctlCommandIndexHeader))
+
+	t.Setenv(env.ExecutionEnvironment.EnvVar(), "test")
+	RoxctlCommand = "custom command"
+	setCustomHeaders(phonehome.Headers(headers).Set)
+	assert.Len(t, headers, 3)
+	assert.Equal(t, RoxctlCommand, headers.Get(clientconn.RoxctlCommandHeader))
+	assert.Equal(t, "2", headers.Get(clientconn.RoxctlCommandIndexHeader))
+	assert.Equal(t, "test", headers.Get(clientconn.ExecutionEnvironment))
 }
