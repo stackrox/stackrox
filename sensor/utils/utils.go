08d4cf1131dcfb2eea7e6857c62aa92842015b73
Simon Baumer <baeumer@pm.me>
2024-05-27 15:25:50 +0200

    WIP

--- sensor/utils/utils.go
+++ sensor/utils/utils.go
@@ -4,6 +4,29 @@ import (
 	"k8s.io/client-go/kubernetes"
 )
 
+const (
+	KubernetesLabelManagedBy = "app.kubernetes.io/managed-by"
+	KubernetesLabelPartOf    = "app.kubernetes.io/created-by-deployment-id"
+	KubernetesLabelCreatedBy = "app.kubernetes.io/created-by"
+	KubernetesLabelName      = "app.kubernetes.io/name"
+)
+
+// GetSensorKubernetesLabels returns the default labels for resources created by the sensor.
+func GetSensorKubernetesLabels() map[string]string {
+	return map[string]string{
+		KubernetesLabelManagedBy: "sensor",
+		KubernetesLabelCreatedBy: "sensor",
+		KubernetesLabelName:      "stackrox",
+	}
+}
+
+// GetSensorKubernetesAnnotations returns the default annotations for resources created by the sensor.
+func GetSensorKubernetesAnnotations() map[string]string {
+	return map[string]string{
+		"owner": "stackrox",
+	}
+}
+
 // HasAPI checks whether the kubernetes server supports the groupVersion API for the specified kind
 func HasAPI(client kubernetes.Interface, groupVersion, kind string) (bool, error) {
 	apiResourceList, err := client.Discovery().ServerResourcesForGroupVersion(groupVersion)
