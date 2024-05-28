f50783e20ab8e8d7753edd77b0e865cd3f64a7f8
Gavin Jefferies <gjefferi@redhat.com>
2024-05-28 08:28:29 -0700

    refactor(image-prefetcher): use ORCHESTRATOR var (#11245)

--- scripts/ci/jobs/ocp_perf_scale_tests.py
+++ scripts/ci/jobs/ocp_perf_scale_tests.py
@@ -3,11 +3,14 @@
 """
 Run the perf scale test in an OCP cluster
 """
+import os
 from runners import ClusterTestRunner
 from clusters import AutomationFlavorsCluster
 from pre_tests import NullPreTest
 from ci_tests import NullTest
 from post_tests import NullPostTest
+
+os.environ["ORCHESTRATOR_FLAVOR"] = "openshift"
 
 ClusterTestRunner(
     cluster=AutomationFlavorsCluster(),
