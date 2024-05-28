244d0378093a660e2e94f6bf1ba986d8783ef0dd
JoukoVirtanen <jouko@stackrox.com>
2024-05-28 16:18:11 -0700

    Changed SAC for listening endpoints in pod datastore

--- central/pod/datastore/datastore_impl_real_test.go
+++ central/pod/datastore/datastore_impl_real_test.go
@@ -52,7 +52,7 @@ func (s *PodDatastoreSuite) SetupSuite() {
 	s.plopAndPiCtx = sac.WithGlobalAccessScopeChecker(context.Background(),
 		sac.AllowFixedScopes(
 			sac.AccessModeScopeKeys(storage.Access_READ_ACCESS, storage.Access_READ_WRITE_ACCESS),
-			sac.ResourceScopeKeys(resources.DeploymentExtension)))
+			sac.ResourceScopeKeys(resources.Namespace)))
 }
 
 func (s *PodDatastoreSuite) SetupTest() {
