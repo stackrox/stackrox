7b8015647b609c8f7821159779fd71f1ccee8893
David Vail <dcvail17@gmail.com>
2024-05-29 08:36:17 -0400

    ROX-23033: +ROX-23035 Add sorting to Node CVE overview tables (#11271)

--- ui/apps/platform/src/Containers/Vulnerabilities/utils/sortFields.ts
+++ ui/apps/platform/src/Containers/Vulnerabilities/utils/sortFields.ts
@@ -3,9 +3,16 @@ export const CVE_SORT_FIELD = 'CVE';
 export const CVSS_SORT_FIELD = 'CVSS';
 export const CVE_TYPE_SORT_FIELD = 'CVE Type';
 export const CVE_COUNT_SORT_FIELD = 'CVE Count';
+export const OPERATING_SYSTEM_SORT_FIELD = 'Operating System';
 
 // Cluster sort fields
 export const CLUSTER_SORT_FIELD = 'Cluster';
 export const CLUSTER_CVE_STATUS_SORT_FIELD = 'Cluster CVE Fixable';
 export const CLUSTER_TYPE_SORT_FIELD = 'Cluster Platform Type';
 export const CLUSTER_KUBERNETES_VERSION_SORT_FIELD = 'Cluster Kubernetes Version';
+
+// Node sort fields
+export const NODE_SORT_FIELD = 'Node';
+export const NODE_TOP_CVSS_SORT_FIELD = 'Node Top CVSS';
+export const NODE_COUNT_SORT_FIELD = 'Node Count';
+export const NODE_SCAN_TIME_SORT_FIELD = 'Node Scan Time';
