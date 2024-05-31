c9265d54802465cc2bba5440ccd4761e248269b9
davdhacs <105243888+davdhacs@users.noreply.github.com>
2024-05-31 15:54:42 -0600

    Revert "test without retry"
    This reverts commit 2f8450ae58d58129e791352fa88f94bb4d2f6416.

--- scripts/ci/gke.sh
+++ scripts/ci/gke.sh
@@ -209,7 +209,7 @@ create_cluster() {
             fi
             info "Timed out"
             info "Attempting to delete the cluster before trying another zone"
-            gcloud container clusters delete "${CLUSTER_NAME}" || {
+            retry 10 true gcloud container clusters delete "${CLUSTER_NAME}" --async || {
                 info "An error occurred deleting the cluster: $?"
                 true
             }
@@ -326,7 +326,8 @@ teardown_gke_cluster() {
         "$SCRIPTS_ROOT/scripts/ci/cleanup-deployment.sh" 2>&1 | sed -e 's/^/out: /' || true
     fi
 
-    gcloud container clusters delete "$CLUSTER_NAME" --async
+    echo "Retry if delete fails before it becomes async (still in creation process)."
+    retry 10 true gcloud container clusters delete "$CLUSTER_NAME" --async
 
     info "Cluster deleting asynchronously"
 
