a5b92131220205af0469bda7b222b325314c328c
David Caravello <119438707+dcaravel@users.noreply.github.com>
2024-05-30 21:04:07 -0500

    ROX-23714: Lazy load registry repo list  (#11186)

--- pkg/registries/docker/docker_test.go
+++ pkg/registries/docker/docker_test.go
@@ -2,11 +2,15 @@ package docker
 
 import (
 	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 	"time"
 
+	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/pkg/images/utils"
 	"github.com/stackrox/rox/pkg/set"
+	"github.com/stackrox/rox/pkg/urlfmt"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -50,8 +54,45 @@ func TestRegistryMatch(t *testing.T) {
 				cfg:                  &Config{DisableRepoList: tc.disableRepoList},
 			}
 
+			// Prevent lazy load from attempting to change repo list under test.
+			r.repoListOnce.Do(func() {})
+
 			match := r.Match(img)
 			assert.Equal(t, tc.expected, match)
 		})
 	}
+}
+
+func TestLazyLoadRepoList(t *testing.T) {
+	var repoListCalls int
+	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/v2/_catalog" {
+			repoListCalls++
+			_, _ = w.Write([]byte(`{"repositories":["repo/path"]}`))
+			return
+		}
+	}))
+
+	c := &Config{
+		Endpoint:        s.URL,
+		DisableRepoList: false,
+	}
+
+	hostOnly := urlfmt.TrimHTTPPrefixes(s.URL)
+
+	r, err := NewDockerRegistryWithConfig(c, &storage.ImageIntegration{})
+	require.NoError(t, err)
+	assert.Zero(t, repoListCalls) // No repo list calls should have been made during construction.
+
+	imgStr := fmt.Sprintf("%s/repo/path:latest", hostOnly)
+	imgName, _, err := utils.GenerateImageNameFromString(imgStr)
+	require.NoError(t, err)
+	assert.True(t, r.Match(imgName))
+	assert.Equal(t, 1, repoListCalls) // Lazy load should have executed once.
+
+	imgStr = fmt.Sprintf("%s/no/match:latest", hostOnly)
+	imgName, _, err = utils.GenerateImageNameFromString(imgStr)
+	require.NoError(t, err)
+	assert.False(t, r.Match(imgName))
+	assert.Equal(t, 1, repoListCalls) // Lazy load should NOT have executed again.
 }
