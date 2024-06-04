c0148d773ca52ebeb1476c3ea2f9c609966a5d93
David Vail <dcvail17@gmail.com>
2024-06-04 17:18:14 -0400

    ROX-24349: Update global search links for VM 2.0 (#11350)

--- ui/apps/platform/src/Containers/Search/searchCategories.ts
+++ ui/apps/platform/src/Containers/Search/searchCategories.ts
@@ -10,7 +10,12 @@ import {
     riskBasePath,
     violationsBasePath,
     vulnManagementPath,
+    vulnerabilitiesNodeCvesPath,
+    vulnerabilitiesWorkloadCvesPath,
+    vulnerabilityNamespaceViewPath,
 } from 'routePaths';
+import { IsFeatureFlagEnabled } from 'hooks/useFeatureFlags';
+import { getQueryString } from 'utils/queryStringUtils';
 
 const configManagementRolesPath = `${configManagementPath}/roles`;
 const configManagementSecretsPath = `${configManagementPath}/secrets`;
@@ -57,139 +62,157 @@ export type SearchResultCategoryMap = Record<SearchResultCategory, SearchResultC
 // Global search route has conditional rendering according to resourceAccessRequirements in routePaths.ts file.
 // Therefore update that property if response ever adds search categories.
 
-// prettier-ignore
-const searchResultCategoryMap: SearchResultCategoryMap = {
-    ALERTS: {
-        filterOn: null,
-        viewLinks: [
-            {
-                basePath: violationsBasePath,
-                linkText: 'Violations',
-                routeKey: 'violations',
-            },
-        ],
-    },
-    CLUSTERS: {
-        filterOn: null,
-        viewLinks: [
-            {
-                basePath: clustersBasePath,
-                linkText: 'Clusters',
-                routeKey: 'clusters',
-            },
-        ],
-    },
-    DEPLOYMENTS: {
-        filterOn: {
-            filterCategory: 'Deployment',
-            filterLinks: [filterOnViolations],
+function getSearchResultCategoryMap(
+    isFeatureFlagEnabled: IsFeatureFlagEnabled
+): SearchResultCategoryMap {
+    const isVm2Ga = isFeatureFlagEnabled('ROX_VULN_MGMT_2_GA');
+    return {
+        ALERTS: {
+            filterOn: null,
+            viewLinks: [
+                {
+                    basePath: `${violationsBasePath}/:id`,
+                    linkText: 'Violations',
+                    routeKey: 'violations',
+                },
+            ],
         },
-        viewLinks: [
-            {
-                basePath: riskBasePath,
-                linkText: 'Risk',
-                routeKey: 'risk',
-            },
-        ],
-    },
-    IMAGES: {
-        filterOn: {
-            filterCategory: 'Image',
-            filterLinks: [filterOnViolations, filterOnRisk],
+        CLUSTERS: {
+            filterOn: null,
+            viewLinks: [
+                {
+                    basePath: `${clustersBasePath}/:id`,
+                    linkText: 'Clusters',
+                    routeKey: 'clusters',
+                },
+            ],
         },
-        viewLinks: [
-            {
-                basePath: vulnManagementImagesPath,
-                linkText: 'Images',
-                routeKey: 'vulnerability-management',
+        DEPLOYMENTS: {
+            filterOn: {
+                filterCategory: 'Deployment',
+                filterLinks: [filterOnViolations],
             },
-        ],
-    },
-    NAMESPACES: {
-        filterOn: null,
-        viewLinks: [
-            {
-                basePath: vulnManagementNamespacesPath,
-                linkText: 'Vulnerability Management',
-                routeKey: 'vulnerability-management',
-            },
-        ],
-    },
-    NODES: {
-        filterOn: null,
-        viewLinks: [
-            {
-                basePath: vulnManagementNodesPath,
-                linkText: 'Vulnerability Management',
-                routeKey: 'vulnerability-management',
-            },
-        ],
-    },
-    POLICIES: {
-        filterOn: {
-            filterCategory: 'Policy',
-            filterLinks: [filterOnViolations],
+            viewLinks: [
+                {
+                    basePath: `${riskBasePath}/:id`,
+                    linkText: 'Risk',
+                    routeKey: 'risk',
+                },
+            ],
         },
-        viewLinks: [
-            {
-                basePath: policiesBasePath,
-                linkText: 'Policies',
-                routeKey: 'policy-management',
+        IMAGES: {
+            filterOn: {
+                filterCategory: 'Image',
+                filterLinks: [filterOnViolations, filterOnRisk],
             },
-        ],
-    },
-    POLICY_CATEGORIES: {
-        filterOn: null,
-        viewLinks: [],
-    },
-    ROLES: {
-        filterOn: null,
-        viewLinks: [
-            {
-                basePath: configManagementRolesPath,
-                linkText: 'Configuration Management',
-                routeKey: 'configmanagement',
-            },
-        ],
-    },
-    ROLEBINDINGS: {
-        filterOn: null,
-        viewLinks: [],
-    },
-    SECRETS: {
-        filterOn: {
-            filterCategory: 'Secret',
-            filterLinks: [filterOnRisk],
+            viewLinks: [
+                {
+                    basePath: isVm2Ga
+                        ? `${vulnerabilitiesWorkloadCvesPath}/images/:id`
+                        : `${vulnManagementImagesPath}/:id`,
+                    linkText: 'Images',
+                    routeKey: 'vulnerability-management',
+                },
+            ],
         },
-        viewLinks: [
-            {
-                basePath: configManagementSecretsPath,
-                linkText: 'Configuration Management',
-                routeKey: 'configmanagement',
+        NAMESPACES: {
+            filterOn: null,
+            viewLinks: [
+                {
+                    basePath: isVm2Ga
+                        ? `${vulnerabilityNamespaceViewPath}${getQueryString({
+                              // TODO - Add regex searching support for namespace view ROX-24484 when ROX_VULN_MGMT_2_GA is enabled
+                              s: {
+                                  NAMESPACE: [':name'],
+                                  CLUSTER: [':locationTextForCategory'],
+                              },
+                          })}`
+                        : `${vulnManagementNamespacesPath}/:id`,
+                    linkText: 'Vulnerability Management',
+                    routeKey: 'vulnerability-management',
+                },
+            ],
+        },
+        NODES: {
+            filterOn: null,
+            viewLinks: [
+                {
+                    basePath: isVm2Ga
+                        ? `${vulnerabilitiesNodeCvesPath}/nodes/:id`
+                        : `${vulnManagementNodesPath}/:id`,
+                    linkText: 'Vulnerability Management',
+                    routeKey: 'vulnerability-management',
+                },
+            ],
+        },
+        POLICIES: {
+            filterOn: {
+                filterCategory: 'Policy',
+                filterLinks: [filterOnViolations],
             },
-        ],
-    },
-    SERVICE_ACCOUNTS: {
-        filterOn: null,
-        viewLinks: [
-            {
-                basePath: configManagementServiceAccountsPath,
-                linkText: 'Configuration Management',
-                routeKey: 'configmanagement',
+            viewLinks: [
+                {
+                    basePath: `${policiesBasePath}/:id`,
+                    linkText: 'Policies',
+                    routeKey: 'policy-management',
+                },
+            ],
+        },
+        POLICY_CATEGORIES: {
+            filterOn: null,
+            viewLinks: [],
+        },
+        ROLES: {
+            filterOn: null,
+            viewLinks: [
+                {
+                    basePath: `${configManagementRolesPath}/:id`,
+                    linkText: 'Configuration Management',
+                    routeKey: 'configmanagement',
+                },
+            ],
+        },
+        ROLEBINDINGS: {
+            filterOn: null,
+            viewLinks: [],
+        },
+        SECRETS: {
+            filterOn: {
+                filterCategory: 'Secret',
+                filterLinks: [filterOnRisk],
             },
-        ],
-    },
-    SUBJECTS: {
-        filterOn: null,
-        viewLinks: [], // because search result id property value is not the id, but the name
-    },
-};
+            viewLinks: [
+                {
+                    basePath: `${configManagementSecretsPath}/:id`,
+                    linkText: 'Configuration Management',
+                    routeKey: 'configmanagement',
+                },
+            ],
+        },
+        SERVICE_ACCOUNTS: {
+            filterOn: null,
+            viewLinks: [
+                {
+                    basePath: `${configManagementServiceAccountsPath}/:id`,
+                    linkText: 'Configuration Management',
+                    routeKey: 'configmanagement',
+                },
+            ],
+        },
+        SUBJECTS: {
+            filterOn: null,
+            viewLinks: [], // because search result id property value is not the id, but the name
+        },
+    };
+}
 
 // Given isRouteEnabled predicate function from useIsRouteEnabled hook,
 // return copy of map with filter and view links only for routes that are enabled.
 export function searchResultCategoryMapFilteredIsRouteEnabled(
-    isRouteEnabled: IsRouteEnabled
+    isRouteEnabled: IsRouteEnabled,
+    isFeatureFlagEnabled: IsFeatureFlagEnabled
 ): SearchResultCategoryMap {
+    const searchResultCategoryMap = getSearchResultCategoryMap(isFeatureFlagEnabled);
     const searchResultCategoryMapFiltered = cloneDeep(searchResultCategoryMap);
 
     Object.keys(searchResultCategoryMapFiltered).forEach((searchResultKey) => {
