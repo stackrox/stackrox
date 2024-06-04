8eeec985d776423034924371eea47047a29acf67
Mark Pedrotti <pedrottimark@gmail.com>
2024-06-03 21:48:10 -0400

    chore(ui): Update types devDependencies in ui (#11351)

--- ui/apps/platform/src/Containers/MainPage/Sidebar/NavigationContent.tsx
+++ ui/apps/platform/src/Containers/MainPage/Sidebar/NavigationContent.tsx
@@ -1,18 +1,20 @@
-import React, { ReactElement, ReactNode } from 'react';
+import React, { CSSProperties, ReactElement, ReactNode } from 'react';
 import { Label } from '@patternfly/react-core';
 
 import TechPreviewLabel from 'Components/PatternFly/TechPreviewLabel';
 
 type NavigationContentVariant = 'Deprecated' | 'TechPreview';
 
+const style = {
+    '--pf-v5-c-label__content--Color': 'var(--pf-v5-global--Color--dark-100)',
+} as CSSProperties;
+// Type assertion prevents TypeScript error:
+// error TS2353: Object literal may only specify known properties, and ''--pf-v5-c-label__content--Color'' does not exist in type 'Properties<string | number, string & {}>'.
+// The expected type comes from property 'style' which is declared here on type 'IntrinsicAttributes & LabelProps'
+
 const badgeMap = {
     Deprecated: (
-        <Label
-            isCompact
-            style={{
-                '--pf-v5-c-label__content--Color': 'var(--pf-v5-global--Color--dark-100)',
-            }}
-        >
+        <Label isCompact style={style}>
             Deprecated
         </Label>
     ),
