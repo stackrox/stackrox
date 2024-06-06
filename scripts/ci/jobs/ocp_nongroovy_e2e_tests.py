caeb0814a7280153e7868eb1bbe274e0462d336c
Marcin Owsiany <porridge@redhat.com>
2024-06-06 08:14:02 +0200

    tests: add a script to run nongroovy tests on ocp

---
+++ scripts/ci/jobs/ocp_nongroovy_e2e_tests.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env -S python3 -u
+
+"""
+Run tests/e2e in an OCP cluster
+"""
+import os
+from runners import ClusterTestRunner
+from clusters import AutomationFlavorsCluster
+from pre_tests import PreSystemTests
+from ci_tests import NonGroovyE2e
+from post_tests import PostClusterTest, FinalPost
+
+# set required test parameters
+os.environ["DEPLOY_STACKROX_VIA_OPERATOR"] = "true"
+os.environ["ORCHESTRATOR_FLAVOR"] = "k8s"
+
+os.environ["ROX_POSTGRES_DATASTORE"] = "true"
+os.environ["ROX_ACTIVE_VULN_MGMT"] = "true"
+
+ClusterTestRunner(
+    cluster=AutomationFlavorsCluster(),
+    pre_test=PreSystemTests(),
+    test=NonGroovyE2e(),
+    post_test=PostClusterTest(
+        check_stackrox_logs=False,
+    ),
+    final_post=FinalPost(),
+).run()
