7c3f9af6aca040a86417badc7b13c6c36ad8c4df
Saif Chaudhry <sachaudh@users.noreply.github.com>
2024-06-06 14:19:38 -0700

    ROX-24098: Adding profile check filters to compliance (#11349)

--- ui/apps/platform/src/Containers/ComplianceEnhanced/Coverage/ProfileChecksPage.tsx
+++ ui/apps/platform/src/Containers/ComplianceEnhanced/Coverage/ProfileChecksPage.tsx
@@ -1,20 +1,41 @@
 import React, { useCallback, useContext } from 'react';
 import { generatePath, useHistory, useParams } from 'react-router-dom';
-import { Divider, PageSection, Title } from '@patternfly/react-core';
+import {
+    Divider,
+    PageSection,
+    Title,
+    Toolbar,
+    ToolbarContent,
+    ToolbarGroup,
+    ToolbarItem,
+} from '@patternfly/react-core';
 
 import PageTitle from 'Components/PageTitle';
 import useRestQuery from 'hooks/useRestQuery';
 import useURLPagination from 'hooks/useURLPagination';
 import useURLSort from 'hooks/useURLSort';
 import { getComplianceProfileResults } from 'services/ComplianceResultsService';
-
-import { CHECK_NAME_QUERY } from './compliance.coverage.constants';
+import {
+    OnSearchPayload,
+    clusterSearchFilterConfig,
+    profileCheckSearchFilterConfig,
+} from 'Components/CompoundSearchFilter/types';
+import CompoundSearchFilter from 'Components/CompoundSearchFilter/components/CompoundSearchFilter';
+import { getFilteredConfig } from 'Components/CompoundSearchFilter/utils/searchFilterConfig';
+import useURLSearch from 'hooks/useURLSearch';
+import SearchFilterChips from 'Components/PatternFly/SearchFilterChips';
+import {
+    CHECK_NAME_QUERY,
+    CHECK_STATUS_QUERY,
+    CLUSTER_QUERY,
+} from './compliance.coverage.constants';
 import { DEFAULT_COMPLIANCE_PAGE_SIZE } from '../compliance.constants';
 import { coverageProfileChecksPath } from './compliance.coverage.routes';
 import { ComplianceProfilesContext } from './ComplianceProfilesProvider';
 import ProfilesToggleGroup from './ProfilesToggleGroup';
 import CoveragesPageHeader from './CoveragesPageHeader';
 import ProfileChecksTable from './ProfileChecksTable';
+import CheckStatusDropdown from './components/CheckStatusDropdown';
 
 function ProfileChecksPage() {
     const { profileName } = useParams();
@@ -27,12 +48,54 @@ function ProfileChecksPage() {
         defaultSortOption: { field: CHECK_NAME_QUERY, direction: 'asc' },
         onSort: () => setPage(1),
     });
+    const { searchFilter, setSearchFilter } = useURLSearch();
 
     const fetchProfileChecks = useCallback(
         () => getComplianceProfileResults(profileName, { sortOption, page, perPage }),
         [page, perPage, profileName, sortOption]
     );
     const { data: profileChecks, loading: isLoading, error } = useRestQuery(fetchProfileChecks);
+
+    const searchFilterConfig = {
+        'Profile Check': profileCheckSearchFilterConfig,
+        Cluster: getFilteredConfig(clusterSearchFilterConfig, ['Name']),
+    };
+
+    // @TODO: Consider making a function to make this more reusable
+    const onSearch = (payload: OnSearchPayload) => {
+        const { action, category, value } = payload;
+        const currentSelection = searchFilter[category] || [];
+        let newSelection = !Array.isArray(currentSelection) ? [currentSelection] : currentSelection;
+        if (action === 'ADD') {
+            newSelection.push(value);
+        } else if (action === 'REMOVE') {
+            newSelection = newSelection.filter((datum) => datum !== value);
+        } else {
+            // Do nothing
+        }
+        setSearchFilter({
+            ...searchFilter,
+            [category]: newSelection,
+        });
+    };
+
+    const onSelectCheckStatus = (
+        filterType: 'Compliance Check Status',
+        checked: boolean,
+        selection: string
+    ) => {
+        const currentSelection = searchFilter[filterType] || [];
+        let newSelection = !Array.isArray(currentSelection) ? [currentSelection] : currentSelection;
+        if (checked) {
+            newSelection.push(selection);
+        } else {
+            newSelection = newSelection.filter((value) => value !== selection);
+        }
+        setSearchFilter({
+            ...searchFilter,
+            [filterType]: newSelection,
+        });
+    };
 
     function handleProfilesToggleChange(selectedProfile: string) {
         const path = generatePath(coverageProfileChecksPath, {
@@ -51,9 +114,50 @@ function ProfileChecksPage() {
                     handleToggleChange={handleProfilesToggleChange}
                 />
             </PageSection>
-            <PageSection variant="default">
-                <PageSection variant="light" component="div">
-                    <Title headingLevel="h2">Profile results</Title>
+            <PageSection variant="default" className="pf-v5-u-py-0">
+                <PageSection variant="light" className="pf-v5-u-p-0">
+                    <Toolbar>
+                        <ToolbarContent>
+                            <ToolbarGroup className="pf-v5-u-w-100">
+                                <Title headingLevel="h2" className="pf-v5-u-py-md">
+                                    Profile results
+                                </Title>
+                            </ToolbarGroup>
+                            <ToolbarGroup className="pf-v5-u-w-100">
+                                <ToolbarItem className="pf-v5-u-flex-1">
+                                    <CompoundSearchFilter
+                                        config={searchFilterConfig}
+                                        searchFilter={searchFilter}
+                                        onSearch={onSearch}
+                                    />
+                                </ToolbarItem>
+                                <ToolbarItem align={{ default: 'alignRight' }}>
+                                    <CheckStatusDropdown
+                                        searchFilter={searchFilter}
+                                        onSelect={onSelectCheckStatus}
+                                    />
+                                </ToolbarItem>
+                            </ToolbarGroup>
+                            <ToolbarGroup className="pf-v5-u-w-100">
+                                <SearchFilterChips
+                                    filterChipGroupDescriptors={[
+                                        {
+                                            displayName: 'Profile Check',
+                                            searchFilterName: CHECK_NAME_QUERY,
+                                        },
+                                        {
+                                            displayName: 'Cluster',
+                                            searchFilterName: CLUSTER_QUERY,
+                                        },
+                                        {
+                                            displayName: 'Compliance Status',
+                                            searchFilterName: CHECK_STATUS_QUERY,
+                                        },
+                                    ]}
+                                />
+                            </ToolbarGroup>
+                        </ToolbarContent>
+                    </Toolbar>
                     <Divider />
                     <ProfileChecksTable
                         isLoading={isLoading}
