8edac49458e9c622ae50b39eacf8c81d3b1b2558
Johannes Malsam <60240743+johannes94@users.noreply.github.com>
2024-06-06 19:35:05 +0200

    ROX-22376: config changes for acscsemail integration (#11389)

--- pkg/env/managed.go
+++ pkg/env/managed.go
@@ -4,7 +4,7 @@ var (
 	// ManagedCentral is set to true to signal that the central is running as a managed instance.
 	ManagedCentral = RegisterBooleanSetting("ROX_MANAGED_CENTRAL", false)
 
-	ACSCSEmailURL = RegisterSetting("ROX_ACSCS_EMAIL_URL", WithDefault("https://emailsender.rhacs.svc.cluster.local"))
+	ACSCSEmailURL = RegisterSetting("ROX_ACSCS_EMAIL_URL", WithDefault("https://emailsender.rhacs.svc"))
 
 	// TenantID is set from the according central pod label with the Downward API.
 	TenantID = RegisterSetting("ROX_TENANT_ID", AllowEmpty())
