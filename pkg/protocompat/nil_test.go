01ace7c13da490b1c8b7dc7879eb8a8372f73e49
Tomasz Janiszewski <tomek@redhat.com>
2024-06-10 13:02:01 +0200

    move tests
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

--- pkg/protocompat/nil_test.go
+++ pkg/protocompat/nil_test.go
@@ -1,39 +1,38 @@
-package protoutils
+package protocompat
 
 import (
 	"testing"
 
 	"github.com/gogo/protobuf/proto"
 	"github.com/stackrox/rox/generated/storage"
-	"github.com/stackrox/rox/pkg/protocompat"
 	"github.com/stretchr/testify/assert"
 )
 
 var null = (*storage.Image)(nil)
 
 func TestErrorOnNilMarshal(t *testing.T) {
-	_, err := protocompat.Marshal(null)
+	_, err := Marshal(null)
 	assert.Equal(t, proto.ErrNil, err)
 
 	var msg *storage.Image
-	_, err = protocompat.Marshal(msg)
+	_, err = Marshal(msg)
 	assert.Equal(t, proto.ErrNil, err)
 
-	_, err = protocompat.Marshal(&storage.Image{})
+	_, err = Marshal(&storage.Image{})
 	assert.NoError(t, err)
 }
 
 func TestErrorOnNilUnmarshal(t *testing.T) {
-	err := protocompat.Unmarshal([]byte{}, null)
+	err := Unmarshal([]byte{}, null)
 	assert.NoError(t, err)
 
-	err = protocompat.Unmarshal(nil, null)
+	err = Unmarshal(nil, null)
 	assert.Equal(t, proto.ErrNil, err)
 
-	err = protocompat.Unmarshal(nil, null)
+	err = Unmarshal(nil, null)
 	assert.Equal(t, proto.ErrNil, err)
 
 	img := &storage.Image{}
-	err = protocompat.Unmarshal([]byte{}, img)
+	err = Unmarshal([]byte{}, img)
 	assert.NoError(t, err)
 }
