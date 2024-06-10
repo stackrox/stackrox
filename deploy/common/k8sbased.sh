197126159f9d1c7c830e3a9d6d292a52db763d66
Jouko Virtanen <jouko@stackrox.com>
2024-06-10 00:25:25 -0700

    ROX-23611: Force ebpf collection method for stackrox ebpf tests (#11324)

--- deploy/common/k8sbased.sh
+++ deploy/common/k8sbased.sh
@@ -759,6 +759,11 @@ function launch_sensor {
         helm_chart="${SENSOR_CHART_DIR_OVERRIDE}"
       fi
 
+      if [[ "${FORCE_COLLECTION_METHOD:-false}" == "true" ]]; then
+        echo "Forcing collection method"
+        extra_helm_config+=(--set "collector.forceCollectionMethod=true")
+      fi
+
       if [[ -n "$CI" ]]; then
         helm lint "${helm_chart}"
         helm lint "${helm_chart}" -n "${sensor_namespace}"
