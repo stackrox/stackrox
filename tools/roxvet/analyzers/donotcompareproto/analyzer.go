cb9c2c744c145ee83bd1249d6cf5918a5043fbb1
Tomasz Janiszewski <tomek@redhat.com>
2024-06-12 17:57:09 +0200

    uncomment banned assertions
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

--- tools/roxvet/analyzers/donotcompareproto/analyzer.go
+++ tools/roxvet/analyzers/donotcompareproto/analyzer.go
@@ -36,7 +36,9 @@ var (
 		"map[string]":   "MapEqual",
 	}
 
-	allowedCallerPackages []string
+	allowedCallerPackages = []string{
+		"github.com/stackrox/rox/central/delegatedregistryconfig/convert",
+	}
 
 	bannedEqualFunctions = set.NewFrozenStringSet(
 		"github.com/google/go-cmp/cmp.Equal",
@@ -44,18 +46,18 @@ var (
 	)
 
 	bannedAssertFunctions = set.NewFrozenStringSet(
-	// "(*github.com/stretchr/testify/assert.Assertions).Contains",
-	// "(*github.com/stretchr/testify/assert.Assertions).Equal",
-	// "(*github.com/stretchr/testify/assert.Assertions).NotEqual",
-	// "(*github.com/stretchr/testify/require.Assertions).Contains",
-	// "(*github.com/stretchr/testify/require.Assertions).Equal",
-	// "(*github.com/stretchr/testify/require.Assertions).NotEqual",
-	// "github.com/stretchr/testify/assert.Contains",
-	// "github.com/stretchr/testify/assert.Equal",
-	// "github.com/stretchr/testify/assert.NotEqual",
-	// "github.com/stretchr/testify/require.Contains",
-	// "github.com/stretchr/testify/require.Equal",
-	// "github.com/stretchr/testify/require.NotEqual",
+		// "(*github.com/stretchr/testify/assert.Assertions).Contains",
+		// "(*github.com/stretchr/testify/assert.Assertions).Equal",
+		// "(*github.com/stretchr/testify/assert.Assertions).NotEqual",
+		// "(*github.com/stretchr/testify/require.Assertions).Contains",
+		// "(*github.com/stretchr/testify/require.Assertions).Equal",
+		// "(*github.com/stretchr/testify/require.Assertions).NotEqual",
+		// "github.com/stretchr/testify/assert.Contains",
+		"github.com/stretchr/testify/assert.Equal",
+		// "github.com/stretchr/testify/assert.NotEqual",
+		// "github.com/stretchr/testify/require.Contains",
+		"github.com/stretchr/testify/require.Equal",
+		// "github.com/stretchr/testify/require.NotEqual",
 	)
 )
 
