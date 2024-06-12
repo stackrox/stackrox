ca38cfed7e9dabb48c0e5daa5f1db1958f9ef775
Gavin Jefferies <gjefferi@redhat.com>
2024-06-11 19:51:57 -0700

    chore(build): try centos stream9

--- tests/e2e/run-e2e-tests.sh
+++ tests/e2e/run-e2e-tests.sh
@@ -148,7 +148,7 @@ if [[ ! -f "/i-am-rox-ci-image" ]]; then
       --platform linux/amd64 \
       --rm -it \
       --entrypoint="$0" \
-      quay.io/stackrox-io/apollo-ci:stackrox-test-0.3.69 "$@"
+      quay.io/stackrox-io/apollo-ci:stackrox-build-0.3.69-2-g781d236515 "$@"
     exit 0
 fi
 
