9d0d93557cf34808622d98688c5e81fad18320ce
aaa5kameric <aaakameric5@gmail.com>
2024-06-13 09:30:29 +0200

    Fixed the Structure and placement of some definitions

--- central/apitoken/service/service_impl_test.go
+++ central/apitoken/service/service_impl_test.go
@@ -13,17 +13,12 @@ import (
 	"github.com/stackrox/rox/pkg/grpc/authn/mocks"
 	"github.com/stackrox/rox/pkg/testutils/roletest"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"go.uber.org/mock/gomock"
 )
 
 func TestServiceImpl_ListAllowedRoles_SortsRoleAlphabetically(t *testing.T) {
-	mockCtrl := gomock.NewController(t)
-	defer mockCtrl.Finish()
-
-	mockIdentity := mocks.NewMockIdentity(mockCtrl)
-
-	mockDatastore := roleMock.NewMockDataStore(mockCtrl)
-	roleOne := roletest.NewResolvedRole("Analyst", map[string]storage.Access{},
+	roleOne := roletest.NewResolvedRole("Writer", map[string]storage.Access{},
 		role.AccessScopeIncludeAll,
 	)
 
@@ -31,12 +26,17 @@ func TestServiceImpl_ListAllowedRoles_SortsRoleAlphabetically(t *testing.T) {
 		role.AccessScopeIncludeAll,
 	)
 
-	roleThree := roletest.NewResolvedRole("Writer", map[string]storage.Access{},
+	roleThree := roletest.NewResolvedRole("Analyst", map[string]storage.Access{},
 		role.AccessScopeIncludeAll,
 	)
 
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+
+	mockIdentity := mocks.NewMockIdentity(mockCtrl)
 	mockIdentity.EXPECT().Roles().Return([]permissions.ResolvedRole{roleOne, roleTwo, roleThree}).AnyTimes()
 
+	mockDatastore := roleMock.NewMockDataStore(mockCtrl)
 	mockDatastore.EXPECT().GetAllResolvedRoles(gomock.Any()).Return([]permissions.ResolvedRole{roleOne, roleTwo, roleThree}, nil)
 
 	s := &serviceImpl{roles: mockDatastore}
@@ -46,7 +46,7 @@ func TestServiceImpl_ListAllowedRoles_SortsRoleAlphabetically(t *testing.T) {
 
 	actual, err := s.ListAllowedTokenRoles(ctx, &v1.Empty{})
 
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
 	expected := []string{"Admin", "Analyst", "Writer"}
 	assert.Equal(t, expected, actual.RoleNames)
