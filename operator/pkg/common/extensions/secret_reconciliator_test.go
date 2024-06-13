606f6c168250ac6618fbe6991b3f9f06d76ac8cf
Tomasz Janiszewski <tomek@redhat.com>
2024-06-13 13:07:06 +0200

    revert changes on k8s objects
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

--- operator/pkg/common/extensions/secret_reconciliator_test.go
+++ operator/pkg/common/extensions/secret_reconciliator_test.go
@@ -9,7 +9,6 @@ import (
 	"github.com/stackrox/rox/operator/pkg/common/labels"
 	"github.com/stackrox/rox/operator/pkg/types"
 	"github.com/stackrox/rox/operator/pkg/utils/testutils"
-	"github.com/stackrox/rox/pkg/protoassert"
 	"github.com/stackrox/rox/pkg/uuid"
 	"github.com/stretchr/testify/suite"
 	v1 "k8s.io/api/core/v1"
@@ -192,7 +191,7 @@ func (s *secretReconcilerTestSuite) Test_ShouldExist_OnExistingManaged_PassingVa
 	err = s.client.Get(context.Background(), key, secret)
 	s.Require().NoError(err)
 
-	protoassert.Equal(s.T(), initSecret, secret)
+	s.Equal(initSecret, secret)
 }
 
 func (s *secretReconcilerTestSuite) Test_ShouldExist_OnExistingManaged_FailingValidation_ShouldFix() {
@@ -246,7 +245,7 @@ func (s *secretReconcilerTestSuite) Test_ShouldExist_OnExistingUnmanaged_Passing
 	secret := &v1.Secret{}
 	err = s.client.Get(context.Background(), key, secret)
 	s.Require().NoError(err)
-	protoassert.Equal(s.T(), initSecret, secret)
+	s.Equal(initSecret, secret)
 }
 
 func (s *secretReconcilerTestSuite) Test_ShouldExist_OnExistingUnmanaged_FailingValidation_ShouldDoNothingAndFail() {
@@ -274,5 +273,5 @@ func (s *secretReconcilerTestSuite) Test_ShouldExist_OnExistingUnmanaged_Failing
 	err = s.client.Get(context.Background(), key, secret)
 	s.Require().NoError(err)
 
-	protoassert.Equal(s.T(), initSecret, secret)
+	s.Equal(initSecret, secret)
 }
