55dd2847800511cf7442287efed55d0ce592474d
Gavin Jefferies <gjefferi@redhat.com>
2024-06-17 15:03:05 -0700

    explain missing gke*qa-e2e

--- scripts/ci/lib.sh
+++ scripts/ci/lib.sh
@@ -562,6 +562,8 @@ _image_prefetcher_prebuilt_start() {
 
 _image_prefetcher_system_start() {
     case "$CI_JOB_NAME" in
+    # ROX-24818: GKE is excluded from system image prefetch as it causes
+    # flakes in test.
     *-operator-e2e-tests|*ocp*qa-e2e-tests)
         image_prefetcher_start_set stackrox-images
         ;;
@@ -662,6 +664,8 @@ _image_prefetcher_prebuilt_await() {
 
 _image_prefetcher_system_await() {
     case "$CI_JOB_NAME" in
+    # ROX-24818: GKE is excluded from system image prefetch as it causes
+    # flakes in test.
     *-operator-e2e-tests|*ocp*qa-e2e-tests)
         image_prefetcher_await_set stackrox-images
         ;;
