cba45e2aa327ccb14fd6272c440e881096286f0a
Charmik Sheth <101146970+charmik-redhat@users.noreply.github.com>
2024-06-16 20:09:53 -0700

    Add migration for suppressed CVEs

--- migrator/runner/all.go
+++ migrator/runner/all.go
@@ -37,4 +37,5 @@ import (
 	_ "github.com/stackrox/rox/migrator/migrations/m_199_to_m_200_policy_updates_for_4_5"
 	_ "github.com/stackrox/rox/migrator/migrations/m_200_to_m_201_compliance_v2_for_4_5"
 	_ "github.com/stackrox/rox/migrator/migrations/m_200_to_m_201_vuln_request_v1_to_v2"
+	_ "github.com/stackrox/rox/migrator/migrations/m_201_to_m_202_vuln_requests_for_suppressed_cves"
 )
