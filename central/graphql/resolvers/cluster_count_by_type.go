97a365fa37d6104f04835aebcf72c024ef8a8d9a
Charmik Sheth <101146970+charmik-redhat@users.noreply.github.com>
2024-06-18 14:41:00 -0700

    Rename clusterCountByPlatformType

--- central/graphql/resolvers/cluster_count_by_type.go
+++ central/graphql/resolvers/cluster_count_by_type.go
@@ -10,7 +10,7 @@ import (
 func init() {
 	schema := getBuilder()
 	utils.Must(
-		schema.AddType("ClusterCountByPlatformType", []string{
+		schema.AddType("ClusterCountByType", []string{
 			"generic: Int!",
 			"kubernetes: Int!",
 			"openshift: Int!",
@@ -19,38 +19,38 @@ func init() {
 	)
 }
 
-type clusterCountByPlatformTypeResolver struct {
+type clusterCountByTypeResolver struct {
 	ctx  context.Context
 	root *Resolver
 	data platformcve.ClusterCountByPlatformType
 }
 
-func (resolver *Resolver) wrapClusterCountByPlatformTypeWithContext(ctx context.Context, value platformcve.ClusterCountByPlatformType, err error) (*clusterCountByPlatformTypeResolver, error) {
+func (resolver *Resolver) wrapClusterCountByTypeWithContext(ctx context.Context, value platformcve.ClusterCountByPlatformType, err error) (*clusterCountByTypeResolver, error) {
 	if err != nil {
 		return nil, err
 	}
 	if value == nil {
-		return &clusterCountByPlatformTypeResolver{ctx: ctx, root: resolver, data: platformcve.NewEmptyClusterCountByPlatformType()}, nil
+		return &clusterCountByTypeResolver{ctx: ctx, root: resolver, data: platformcve.NewEmptyClusterCountByPlatformType()}, nil
 	}
-	return &clusterCountByPlatformTypeResolver{ctx: ctx, root: resolver, data: value}, nil
+	return &clusterCountByTypeResolver{ctx: ctx, root: resolver, data: value}, nil
 }
 
 // Generic returns number of clusters of type GENERIC
-func (resolver *clusterCountByPlatformTypeResolver) Generic(_ context.Context) int32 {
+func (resolver *clusterCountByTypeResolver) Generic(_ context.Context) int32 {
 	return int32(resolver.data.GetGenericClusterCount())
 }
 
 // Kubernetes returns the number of clusters of type KUBERNETES
-func (resolver *clusterCountByPlatformTypeResolver) Kubernetes(_ context.Context) int32 {
+func (resolver *clusterCountByTypeResolver) Kubernetes(_ context.Context) int32 {
 	return int32(resolver.data.GetKubernetesClusterCount())
 }
 
 // Openshift returns the number of clusters of type OPENSHIFT
-func (resolver *clusterCountByPlatformTypeResolver) Openshift(_ context.Context) int32 {
+func (resolver *clusterCountByTypeResolver) Openshift(_ context.Context) int32 {
 	return int32(resolver.data.GetOpenshiftClusterCount())
 }
 
 // Openshift4 retruns the number of clusters of type OPENSHIFT4
-func (resolver *clusterCountByPlatformTypeResolver) Openshift4(_ context.Context) int32 {
+func (resolver *clusterCountByTypeResolver) Openshift4(_ context.Context) int32 {
 	return int32(resolver.data.GetOpenshift4ClusterCount())
 }
