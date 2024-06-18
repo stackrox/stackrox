ddc84471c94373e0333d3674e99c005d8d083867
Giles Hutton <ghutton@redhat.com>
2024-06-18 16:10:41 +0100

    Restore eBPF in roxctl (it'll be translated internally)

--- roxctl/sensor/generate/collection_method.go
+++ roxctl/sensor/generate/collection_method.go
@@ -11,6 +11,7 @@ var (
 	humanReadableToEnum = map[string]storage.CollectionMethod{
 		"default":  storage.CollectionMethod_UNSET_COLLECTION,
 		"none":     storage.CollectionMethod_NO_COLLECTION,
+		"ebpf":     storage.CollectionMethod_EBPF,
 		"core_bpf": storage.CollectionMethod_CORE_BPF,
 	}
 
