14127894ca86529b337f91de672fd28a40e646cc
Tom Martensen <tmartens@redhat.com>
2024-06-19 10:18:56 +0200

    implement must-gather

--- scripts/ci/run-must-gather.sh
+++ scripts/ci/run-must-gather.sh
@@ -23,7 +23,5 @@ else
     log_dir="/tmp/ocp-must-gather"
 fi
 
-# This will attempt to collect kube API server audit logs on OpenShift.
-# It would be great to do the same on other cluster types but that would be much harder do in a portable way.
-echo "$(date) Attempting to collect kube API server audit logs"
-(cd "${log_dir}" && oc version && oc adm must-gather --timeout=7m -- /usr/bin/gather_audit_logs && du -sh must-gather*) || true
+echo "$(date) Attempting to gather debugging information from an OpenShift cluster"
+oc adm must-gather --dest-dir "${log_dir}"
