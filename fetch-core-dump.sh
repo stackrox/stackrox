a9d6a6ca95905d4f4e15dcefed2380bedde2f7b4
Simon Baumer <baeumer@pm.me>
2024-06-20 13:28:12 +0200

    WIP

--- fetch-core-dump.sh
+++ fetch-core-dump.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # get sensor pod name
-sensor_pod_name=$(kubectl -n stackrox get pods -l app=sensor -o custom-columns=NAME:.metadata.name | tail -n 1)
+sensor_pod_name="$(kubectl -n stackrox get pods -l app=sensor -o=jsonpath='{.items[0].metadata.name}')"
 
 # Create core dump
 kubectl exec ${sensor_pod_name} -- /bin/bash -c "cd /var/cache/stackrox && /bin/bash -c 'gcore 1'"
 
 # Copy core-dump
-kubectl cp ${sensor_pod_name}:/var/cache/stackrox/core.1 ./core.1
+kubectl cp "${sensor_pod_name}:/var/cache/stackrox/core.1" "core-dump-${sensor_pod_name}"
