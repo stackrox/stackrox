9909dd334c5515a8b85398d0d0282a1767eee10b
David Caravello <119438707+dcaravel@users.noreply.github.com>
2024-06-21 12:02:17 -0500

    chore: manually backport #11359 into 4.4 release branch (#11644)

--- pkg/features/list.go
+++ pkg/features/list.go
@@ -105,4 +105,9 @@ var (
 
 	// UnqualifiedSearchRegistries enables support for unqualified search registries and short name aliases.
 	UnqualifiedSearchRegistries = registerFeature("Enable support for unqualified search registries and short name aliases", "ROX_UNQUALIFIED_SEARCH_REGISTRIES", false)
+
+	// SensorSingleScanPerImage when set to true forces Sensor to allow only a single scan per image to be active at any given
+	// time. Will only have an affect if UnqualifiedSearchRegistries is also enabled.
+	// TODO(ROX-24641): Remove dependency on the UnqualifiedSearchRegistries feature so that this is enabled by default.
+	SensorSingleScanPerImage = registerFeature("Sensor will only allow a single active scan per image", "ROX_SENSOR_SINGLE_SCAN", true)
 )
