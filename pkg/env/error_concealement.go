5e40169a540f5c01e87a7d31850ea636752a17ab
Michaël Petrov <smartptr@gmail.com>
2024-06-24 11:39:44 +0200

    comment update

--- pkg/env/error_concealement.go
+++ pkg/env/error_concealement.go
@@ -2,6 +2,6 @@ package env
 
 var (
 	// EnableErrorConcealment configures central to drop messages of errors, not
-	// wrapped with errox.SensitiveError.
+	// wrapped with errox.WithUserMessage.
 	EnableErrorConcealment = RegisterBooleanSetting("ROX_ENABLE_ERROR_CONCEALMENT", false)
 )
