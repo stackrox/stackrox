451b2672a907f12d1594566638bfc5a32a6303f7
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2024-06-25 16:39:24 +0200

    Remove attempts to validate an URL - this is too difficult

--- pkg/tlscheck/tlscheck.go
+++ pkg/tlscheck/tlscheck.go
@@ -5,7 +5,6 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
-	"net/url"
 	"strings"
 	"time"
 
@@ -19,22 +18,6 @@ const (
 	timeout = 2 * time.Second
 )
 
-// validateWithScheme ensures that the URL: (1) has scheme, (2) passes the validation of url.Parse.
-// If validation passes, the returned result contains scheme `tcp://` prepended to the input string.
-func validateWithScheme(s string) (string, error) {
-	u, err := url.Parse(s)
-	if err != nil {
-		return s, err
-	}
-	// We rerun this prependig the `tcp://` scheme, due to the following comment on url.Parse:
-	// "Trying to parse a hostname and path without a scheme
-	// is invalid but may not necessarily return an error, due to parsing ambiguities."
-	if !u.IsAbs() || u.Host == "" {
-		return validateWithScheme("tcp://" + s)
-	}
-	return u.String(), nil
-}
-
 // CheckTLS checks if the address is using TLS
 func CheckTLS(ctx context.Context, origAddr string) (bool, error) {
 	addr := urlfmt.TrimHTTPPrefixes(origAddr)
@@ -42,9 +25,8 @@ func CheckTLS(ctx context.Context, origAddr string) (bool, error) {
 		addr = addrSplits[0]
 	}
 
-	addr, err := validateWithScheme(addr)
-	if err != nil {
-		return false, err
+	if strings.Contains(addr, " ") {
+		addr = strings.TrimSpace(addr)
 	}
 
 	host, _, port, err := netutil.ParseEndpoint(addr)
