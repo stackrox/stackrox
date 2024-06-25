bc980b44f6cd97cf29e1e2aefd8fa0f2c83ab8b4
Tomasz Janiszewski <tomek@redhat.com>
2024-06-25 14:23:36 +0200

    fix: do not copy protos (#11582)
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>
    Co-authored-by: Mladen Todorovic <mtodor@gmail.com>

--- sensor/kubernetes/listener/resources/deployment_store_test.go
+++ sensor/kubernetes/listener/resources/deployment_store_test.go
@@ -219,7 +219,7 @@ func (s *deploymentStoreSuite) Test_BuildDeploymentWithDependencies() {
 	wrap := s.createDeploymentWrap(makeDeploymentObject("test-deployment", "test-ns", types.UID(uid)))
 	s.deploymentStore.addOrUpdateDeployment(wrap)
 
-	expectedExposureInfo := storage.PortConfig_ExposureInfo{
+	expectedExposureInfo := &storage.PortConfig_ExposureInfo{
 		Level:       storage.PortConfig_EXTERNAL,
 		ServiceName: "test.service",
 		ServicePort: 5432,
@@ -232,7 +232,7 @@ func (s *deploymentStoreSuite) Test_BuildDeploymentWithDependencies() {
 		PermissionLevel: storage.PermissionLevel_CLUSTER_ADMIN,
 		Exposures: []map[service.PortRef][]*storage.PortConfig_ExposureInfo{
 			{
-				service.PortRefOf(stubService()): []*storage.PortConfig_ExposureInfo{&expectedExposureInfo},
+				service.PortRefOf(stubService()): []*storage.PortConfig_ExposureInfo{expectedExposureInfo},
 			},
 		},
 	})
@@ -242,7 +242,7 @@ func (s *deploymentStoreSuite) Test_BuildDeploymentWithDependencies() {
 	s.Require().Len(deployment.GetPorts(), 1)
 	s.Require().Len(deployment.GetPorts()[0].GetExposureInfos(), 1)
 
-	s.Equal(expectedExposureInfo, *deployment.GetPorts()[0].GetExposureInfos()[0])
+	protoassert.Equal(s.T(), expectedExposureInfo, deployment.GetPorts()[0].GetExposureInfos()[0])
 	s.Equal(storage.PermissionLevel_CLUSTER_ADMIN, deployment.GetServiceAccountPermissionLevel(), "Service account permission level")
 
 	_, isBuilt = s.deploymentStore.GetBuiltDeployment(uid)
