8a5bd425ace83612434fbc088443595fddab0872
Gavin Jefferies <gjefferi@redhat.com>
2024-07-02 10:39:19 -0700

    fix(CI): cache latest images

--- qa-tests-backend/src/main/groovy/orchestratormanager/Kubernetes.groovy
+++ qa-tests-backend/src/main/groovy/orchestratormanager/Kubernetes.groovy
@@ -2323,6 +2323,7 @@ class Kubernetes implements OrchestratorMain {
         Container container = new Container(
                 name: deployment.containerName ? deployment.containerName : deployment.name,
                 image: deployment.image,
+                imagePullPolicy: "ifNotPresent",
                 command: deployment.command,
                 args: deployment.args,
                 ports: depPorts,
