471bf5c15402c92ec150bc4495bb71c03b6b29ef
Jan Schnathmeier <jschnath@redhat.com>
2024-07-03 17:02:52 +0200

    Add comments

--- pkg/sensorupgrader/namespace.go
+++ pkg/sensorupgrader/namespace.go
@@ -15,11 +15,13 @@ var (
 // GetSensorNamespace tries to read the sensor namespace out of the service account token dir, POD_NAMESPACE if that
 // fails, and defaults to Stackrox otherwise.
 func GetSensorNamespace() string {
+	// This attempts to load the namespace from the file serviceaccount/namespace
 	sensorNamespace, err := satoken.LoadNamespaceFromFile()
 	if err != nil {
 		log.Errorf("Failed to determine namespace from service account token file: %s", err)
 	}
 	if sensorNamespace == "" {
+		// This environment variable is configured to contain pod namespace by sensor YAML/helm file.
 		sensorNamespace = os.Getenv("POD_NAMESPACE")
 	}
 	if sensorNamespace == "" {
