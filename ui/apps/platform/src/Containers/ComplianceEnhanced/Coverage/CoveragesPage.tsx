1d354fe57b95623b3ea989ce91ad262e3471d744
bradr5 <braroger@redhat.com>
2024-07-03 10:35:13 -0500

    lint fix

--- ui/apps/platform/src/Containers/ComplianceEnhanced/Coverage/CoveragesPage.tsx
+++ ui/apps/platform/src/Containers/ComplianceEnhanced/Coverage/CoveragesPage.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext, useEffect, useState } from 'react';
+import React, { useCallback, useContext, useState } from 'react';
 import { Route, Switch, useParams } from 'react-router-dom';
 import {
     Bullseye,
