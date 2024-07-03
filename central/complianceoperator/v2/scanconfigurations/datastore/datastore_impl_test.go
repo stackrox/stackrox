3384fb933a658108c8d96d6068ed466da602c8b0
Simon Baumer <baeumer@pm.me>
2024-07-03 14:03:17 +0200

    WIP

--- central/complianceoperator/v2/scanconfigurations/datastore/datastore_impl_test.go
+++ central/complianceoperator/v2/scanconfigurations/datastore/datastore_impl_test.go
@@ -85,7 +85,6 @@ func (s *complianceScanConfigDataStoreTestSuite) SetupSuite() {
 
 func (s *complianceScanConfigDataStoreTestSuite) SetupTest() {
 	s.mockCtrl = gomock.NewController(s.T())
-	s.T().Setenv("POSTGRES_PORT", "5433")
 	s.db = pgtest.ForT(s.T())
 
 	s.storage = configStore.New(s.db)
