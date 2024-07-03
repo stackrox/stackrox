353def1cdd837641a9d9a3773354e8fb03f695cb
Charmik Sheth <101146970+charmik-redhat@users.noreply.github.com>
2024-07-03 01:52:11 -0700

    Add cluster migration for new columns (#11802)

--- migrator/runner/all.go
+++ migrator/runner/all.go
@@ -39,4 +39,5 @@ import (
 	_ "github.com/stackrox/rox/migrator/migrations/m_201_to_m_202_vuln_request_v1_to_v2"
 	_ "github.com/stackrox/rox/migrator/migrations/m_202_to_m_203_vuln_requests_for_suppressed_cves"
 	_ "github.com/stackrox/rox/migrator/migrations/m_203_to_m_204_openshift_policy_exclusions_for_4_5"
+	_ "github.com/stackrox/rox/migrator/migrations/m_204_to_m_205_clusters_platform_type_and_k8_version"
 )
