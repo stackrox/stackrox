cb02d5ae454eb865a11d7635ed33aaa1e1f95c36
Jan Schnathmeier <jschnath@redhat.com>
2024-07-09 10:45:28 +0200

    Add verbose logging to snapshot.go to see what's happening within GetSensorNamespace (revert this later)

--- pkg/sensorupgrader/namespace.go
+++ pkg/sensorupgrader/namespace.go
@@ -14,9 +14,17 @@ var (
 
 // GetSensorNamespace tries to read the sensor namespace out of the service account token dir, POD_NAMESPACE if that
 // fails, and defaults to Stackrox otherwise.
-func GetSensorNamespace() string {
+func GetSensorNamespace(params ...bool) string {
 	// Operator installs and some CI runs set a NAMESPACE env which needs to be adhered to so tests don't fail
 	sensorNamespace := os.Getenv("TEST_NAMESPACE")
+
+	verbose := false
+	if len(params) > 0 && params[0] == true {
+		verbose = true
+	}
+	if verbose {
+		log.Infof("sensorNamespace is %s after running 'os.Getenv(\"TEST_NAMESPACE\")'", sensorNamespace)
+	}
 	// This attempts to load the namespace from the file serviceaccount/namespace
 	if sensorNamespace == "" {
 		var err error
@@ -24,10 +32,16 @@ func GetSensorNamespace() string {
 		if err != nil {
 			log.Errorf("Failed to determine namespace from service account token file: %s", err)
 		}
+		if verbose {
+			log.Infof("sensorNamespace is %s after running 'satoken.LoadNamespaceFromFile()'", sensorNamespace)
+		}
 	}
 	if sensorNamespace == "" {
 		// This environment variable is configured to contain pod namespace by sensor YAML/helm file.
 		sensorNamespace = os.Getenv("POD_NAMESPACE")
+		if verbose {
+			log.Infof("sensorNamespace is %s after running 'os.Getenv(\"POD_NAMESPACE\")'", sensorNamespace)
+		}
 	}
 	if sensorNamespace == "" {
 		sensorNamespace = namespaces.StackRox
