f34a3958740aae2e18473d095cef0b653f467754
Saif Chaudhry <schaudhr@redhat.com>
2024-07-09 11:28:39 -0700

    additional changes

--- ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/Overview/string.utils.ts
+++ ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/Overview/string.utils.ts
@@ -27,7 +27,7 @@ export function getViewStateDescription(
     switch (vulnerabilityState) {
         case 'OBSERVED':
             return cveViewingMode === 'WITH_CVES'
-                ? 'Images and deployments observed with CVEs'
+                ? 'Images and deployments with observed CVEs'
                 : 'Images and deployments without observed CVEs (results might include false negatives due to scanner limitations, such as unsupported operating systems)';
         case 'DEFERRED':
             return 'Observed vulnerabilities that are approved by administrators to be deferred for a period of time or until fixable';
