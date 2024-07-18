51fd74897669c5340fa7c9ef348cc007191bbc85
Stephan Hesselmann <shesselm@redhat.com>
2024-07-18 15:57:31 +0200

    chore: remove admin events feature flag (#11977)

--- central/main.go
+++ central/main.go
@@ -360,10 +360,7 @@ func startServices() {
 	apiTokenExpiration.Singleton().Start()
 	administrationUsageInjector.Singleton().Start()
 	gcp.Singleton().Start()
-
-	if features.AdministrationEvents.Enabled() {
-		administrationEventHandler.Singleton().Start()
-	}
+	administrationEventHandler.Singleton().Start()
 
 	go registerDelayedIntegrations(iiStore.DelayedIntegrations)
 }
@@ -371,6 +368,7 @@ func startServices() {
 func servicesToRegister() []pkgGRPC.APIService {
 	// PLEASE KEEP THE FOLLOWING LIST SORTED.
 	servicesToRegister := []pkgGRPC.APIService{
+		administrationEventService.Singleton(),
 		administrationUsageService.Singleton(),
 		alertService.Singleton(),
 		apiTokenService.Singleton(),
@@ -458,10 +456,6 @@ func servicesToRegister() []pkgGRPC.APIService {
 		servicesToRegister = append(servicesToRegister, v2ComplianceRules.Singleton())
 		// TODO: this is only done to initialize the table. Once we have a service we can move this there
 		v2ComplianceBenchmark.Singleton()
-	}
-
-	if features.AdministrationEvents.Enabled() {
-		servicesToRegister = append(servicesToRegister, administrationEventService.Singleton())
 	}
 
 	if features.UnifiedCVEDeferral.Enabled() {
@@ -924,6 +918,7 @@ func waitForTerminationSignal() {
 		{apiTokenExpiration.Singleton(), "api token expiration notifier"},
 		{gcp.Singleton(), "GCP cloud credentials manager"},
 		{cloudSourcesManager.Singleton(), "cloud sources manager"},
+		{administrationEventHandler.Singleton(), "administration events handler"},
 	}
 
 	if features.VulnReportingEnhancements.Enabled() {
@@ -937,11 +932,6 @@ func waitForTerminationSignal() {
 	if features.ComplianceReporting.Enabled() {
 		stoppables = append(stoppables,
 			stoppableWithName{complianceReportManager.Singleton(), "compliance reports manager"})
-	}
-
-	if features.AdministrationEvents.Enabled() {
-		stoppables = append(stoppables,
-			stoppableWithName{administrationEventHandler.Singleton(), "administration events handler"})
 	}
 
 	var wg sync.WaitGroup
