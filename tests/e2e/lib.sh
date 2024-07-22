703bb3fd032749f3f3c1a7386f63d0727ab7a48f
Marcin Owsiany <porridge@redhat.com>
2024-07-22 18:01:43 +0200

    indent

--- tests/e2e/lib.sh
+++ tests/e2e/lib.sh
@@ -237,7 +237,7 @@ deploy_central_via_operator() {
     ROX_PASSWORD="$(tr -dc _A-Z-a-z-0-9 < /dev/urandom | head -c12 || true)"
     centralAdminPasswordBase64="$(echo "$ROX_PASSWORD" | base64)"
 
-    centralAdditionalCA="$(sed 's,^,        ,' "${TRUSTED_CA_FILE}")"
+    centralAdditionalCAIndented="$(sed 's,^,        ,' "${TRUSTED_CA_FILE}")"
 
     centralDefaultTlsSecretKeyBase64="$(base64 -w0 < "${ROX_DEFAULT_TLS_KEY_FILE}")"
     centralDefaultTlsSecretCertBase64="$(base64 -w0 < "${ROX_DEFAULT_TLS_CERT_FILE}")"
@@ -298,7 +298,7 @@ deploy_central_via_operator() {
     fi
     env - \
       centralAdminPasswordBase64="$centralAdminPasswordBase64" \
-      centralAdditionalCA="$centralAdditionalCA" \
+      centralAdditionalCAIndented="$centralAdditionalCAIndented" \
       centralDefaultTlsSecretKeyBase64="$centralDefaultTlsSecretKeyBase64" \
       centralDefaultTlsSecretCertBase64="$centralDefaultTlsSecretCertBase64" \
       central_exposure_loadBalancer_enabled="$central_exposure_loadBalancer_enabled" \
