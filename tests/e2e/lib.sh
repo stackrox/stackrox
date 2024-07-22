502956136ad713fd082a3623fc6bf7ce3cceb3ed
Marcin Owsiany <porridge@redhat.com>
2024-07-22 16:16:29 +0200

    no base64 encoding of pem

--- tests/e2e/lib.sh
+++ tests/e2e/lib.sh
@@ -237,7 +237,7 @@ deploy_central_via_operator() {
     ROX_PASSWORD="$(tr -dc _A-Z-a-z-0-9 < /dev/urandom | head -c12 || true)"
     centralAdminPasswordBase64="$(echo "$ROX_PASSWORD" | base64)"
 
-    centralAdditionalCABase64="$(base64 -w0 < "${TRUSTED_CA_FILE}")"
+    centralAdditionalCA="$(sed 's,^,        ,' "${TRUSTED_CA_FILE}")"
 
     centralDefaultTlsSecretKeyBase64="$(base64 -w0 < "${ROX_DEFAULT_TLS_KEY_FILE}")"
     centralDefaultTlsSecretCertBase64="$(base64 -w0 < "${ROX_DEFAULT_TLS_CERT_FILE}")"
@@ -298,7 +298,7 @@ deploy_central_via_operator() {
     fi
     env - \
       centralAdminPasswordBase64="$centralAdminPasswordBase64" \
-      centralAdditionalCABase64="$centralAdditionalCABase64" \
+      centralAdditionalCA="$centralAdditionalCA" \
       centralDefaultTlsSecretKeyBase64="$centralDefaultTlsSecretKeyBase64" \
       centralDefaultTlsSecretCertBase64="$centralDefaultTlsSecretCertBase64" \
       central_exposure_loadBalancer_enabled="$central_exposure_loadBalancer_enabled" \
