4ed53c7b04e63a0918d87a04f8824d67a3d20aed
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2024-07-23 12:34:40 +0200

    Add env var to enable upgrader

--- pkg/env/managed.go
+++ pkg/env/managed.go
@@ -3,6 +3,8 @@ package env
 var (
 	// ManagedCentral is set to true to signal that the central is running as a managed instance.
 	ManagedCentral = RegisterBooleanSetting("ROX_MANAGED_CENTRAL", false)
+	// UpgraderEnabled controlls whether the secured cluster auto-upgrader is enabled
+	UpgraderEnabled = RegisterBooleanSetting("ROX_UPGRADER_ENABLED", true)
 
 	ACSCSEmailURL = RegisterSetting("ROX_ACSCS_EMAIL_URL", WithDefault("https://emailsender.rhacs.svc"))
 
