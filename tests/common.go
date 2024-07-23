516fbcf1e917dc99999f4cc391bdde91354a2be2
Jan Schnathmeier <102952141+jschnath@users.noreply.github.com>
2024-07-23 14:32:50 +0200

    ROX-25183: Mandatory go:build tags for go e2e tests (#11933)

--- tests/common.go
+++ tests/common.go
@@ -1,3 +1,5 @@
+//go:build test_e2e || sql_integration || compliance
+
 package tests
 
 import (
@@ -26,6 +28,9 @@ import (
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
+
+//lint:file-ignore U1000 since common.go is included in several different go:build tags but not every function is used
+// within every tag, hence the linter incorrectly shows most functions in this file as unused.
 
 const (
 	nginxDeploymentName     = `nginx`
