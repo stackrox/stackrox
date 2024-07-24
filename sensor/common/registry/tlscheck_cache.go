56159142fdc7d06515bc2874b160057b82a170b5
David Caravello <119438707+dcaravel@users.noreply.github.com>
2024-07-24 08:38:25 -0500

    rebase due to expiring cache made generic

--- sensor/common/registry/tlscheck_cache.go
+++ sensor/common/registry/tlscheck_cache.go
@@ -28,7 +28,7 @@ type tlsCheckCacheImpl struct {
 	// performing checks on the same registry within the cache expiry
 	// window. An expiring cache is used because the TLS state of a registry
 	// may change.
-	results      expiringcache.Cache
+	results      expiringcache.Cache[string, *cacheEntry]
 	checkTLSFunc CheckTLS
 }
 
@@ -72,7 +72,7 @@ func (e *cacheEntry) checkTLS(ctx context.Context, registry string, checkTLSFunc
 
 func newTLSCheckCache(checkTLSFunc CheckTLS) *tlsCheckCacheImpl {
 	return &tlsCheckCacheImpl{
-		results:      expiringcache.NewExpiringCache(tlsCheckTTL),
+		results:      expiringcache.NewExpiringCache[string, *cacheEntry](tlsCheckTTL),
 		checkTLSFunc: checkTLSFunc,
 	}
 }
@@ -87,14 +87,14 @@ func (c *tlsCheckCacheImpl) CheckTLS(ctx context.Context, registry string) (secu
 	// First check the cache for an entry, and, if found, perform
 	// the TLS check. This is an optimization to avoid unnecessary
 	// allocations on cache hits.
-	entryI, ok := c.results.Get(registry)
+	entry, ok := c.results.Get(registry)
 	if ok {
-		return entryI.(*cacheEntry).checkTLS(ctx, registry, c.checkTLSFunc)
+		return entry.checkTLS(ctx, registry, c.checkTLSFunc)
 	}
 
 	// Otherwise, create a new cache entry in a semi-coordinated way,
 	// this may result in multiple cacheEntry objects being created
 	// however only one will enter the cache.
-	entry := c.results.GetOrSet(registry, &cacheEntry{}).(*cacheEntry)
+	entry = c.results.GetOrSet(registry, &cacheEntry{})
 	return entry.checkTLS(ctx, registry, c.checkTLSFunc)
 }
