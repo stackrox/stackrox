baa5bd97495fa43626f98016ab58dda8e4400c08
Michaël Petrov <smartptr@gmail.com>
2024-07-24 16:36:36 +0200

    comments

--- pkg/features/features.go
+++ pkg/features/features.go
@@ -6,7 +6,8 @@ import (
 	"strings"
 )
 
-// A FeatureFlag is a product behavior that can be enabled or disabled using an environment variable.
+// A FeatureFlag is a product behavior that can be enabled or disabled using an
+// environment variable.
 type FeatureFlag interface {
 	Name() string
 	EnvVar() string
@@ -20,7 +21,8 @@ var (
 	Flags = make(map[string]FeatureFlag)
 )
 
-// registerFeature global registers and returns a new feature flag that can be overridden from the default state regardless of build.
+// registerFeature registers and returns a new feature flag, configured with the
+// provided options.
 func registerFeature(name, envVar string, options ...option) FeatureFlag {
 	if !strings.HasPrefix(envVar, "ROX_") {
 		panic(fmt.Sprintf("invalid env var: %s, must start with ROX_", envVar))
