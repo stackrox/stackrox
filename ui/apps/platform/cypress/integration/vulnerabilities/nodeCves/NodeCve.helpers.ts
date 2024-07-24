b6d55d2efaaf80ec37dc2bbf57561bd3f69eede2
David Vail <dcvail17@gmail.com>
2024-07-24 15:02:34 -0400

    test(ui): node overview page tests (#12018)

--- ui/apps/platform/cypress/integration/vulnerabilities/nodeCves/NodeCve.helpers.ts
+++ ui/apps/platform/cypress/integration/vulnerabilities/nodeCves/NodeCve.helpers.ts
@@ -1,17 +1,39 @@
+import { graphql } from '../../../constants/apiEndpoints';
 import {
     getRouteMatcherMapForGraphQL,
     interactAndWaitForResponses,
 } from '../../../helpers/request';
 
+export function mockOverviewNodeCveListRequest() {
+    const opname = 'getNodeCVEs';
+    cy.intercept(
+        { method: 'POST', url: graphql(opname) },
+        { fixture: `vulnerabilities/nodeCves/${opname}.json` }
+    ).as(opname);
+}
+
+export function mockOverviewNodeListRequest() {
+    const opname = 'getNodes';
+    cy.intercept(
+        { method: 'POST', url: graphql(opname) },
+        { fixture: `vulnerabilities/nodeCves/${opname}.json` }
+    ).as(opname);
+}
+
 export function visitNodeCveOverviewPage() {
     cy.visit('/main/vulnerabilities/node-cves');
 }
 
-export function visitFirstNodeLinkFromTable() {
-    return interactAndWaitForResponses(
-        () => {
-            cy.get('tbody tr td[data-label="Node"] a').first().click();
-        },
-        getRouteMatcherMapForGraphQL(['getNodeMetadata'])
-    );
+export function visitFirstNodeLinkFromTable(): Cypress.Chainable<string> {
+    // Get the name of the first node in the table and pass it to the caller
+    return cy
+        .get('tbody tr td[data-label="Node"] a')
+        .first()
+        .then(($link) => {
+            interactAndWaitForResponses(
+                () => cy.wrap($link).click(),
+                getRouteMatcherMapForGraphQL(['getNodeMetadata'])
+            );
+            return cy.wrap($link.text());
+        });
 }
