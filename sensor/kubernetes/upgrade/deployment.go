e27d063e14ab34fe8c896d4f302e01f055a29e6a
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2024-07-25 12:05:17 +0200

    Add UpgraderImageOverwrite

--- sensor/kubernetes/upgrade/deployment.go
+++ sensor/kubernetes/upgrade/deployment.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/stackrox/rox/generated/internalapi/central"
+	"github.com/stackrox/rox/pkg/env"
 	"github.com/stackrox/rox/pkg/namespaces"
 	appsV1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
@@ -40,12 +41,15 @@ func toK8sEnvVars(triggerEnvVars []*central.SensorUpgradeTrigger_EnvVarDef) []v1
 }
 
 func (p *process) determineImage() (string, error) {
+	log.Infof("Sensor was instructed to run the upgrader with image: %s", p.trigger.GetImage())
+	// TODO: check the pullability of that image
+	if env.UpgraderImageOverwrite.Setting() != "" {
+		log.Infof("Sensor will use the follownig upgrader image instead: %s", env.UpgraderImageOverwrite.Setting())
+		return env.UpgraderImageOverwrite.Setting(), nil
+	}
 	if image := p.trigger.GetImage(); image != "" {
 		return image, nil
 	}
-
-	log.Infof("Sensor was instructed to run the upgrader with image: %s", p.trigger.GetImage())
-	// TODO: check the pullability of that image
 
 	// If the image is not specified, sensor uses the same image it's using to launch the upgrader.
 	// This code path will be hit during cert rotation.
