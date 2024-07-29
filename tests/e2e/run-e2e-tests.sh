4adb0f2e3c334d1bb951cd1e274458533ecd71aa
Brad Lugo <blugo@redhat.com>
2024-07-29 15:54:57 -0700

    bump rox-ci-image versions

--- tests/e2e/run-e2e-tests.sh
+++ tests/e2e/run-e2e-tests.sh
@@ -148,7 +148,7 @@ if [[ ! -f "/i-am-rox-ci-image" ]]; then
       --platform linux/amd64 \
       --rm -it \
       --entrypoint="$0" \
-      quay.io/stackrox-io/apollo-ci:stackrox-test-0.3.69-12-g470b9f23c1 "$@"
+      quay.io/stackrox-io/apollo-ci:stackrox-test-0.3.69-9-g9944fdc125 "$@"
     exit 0
 fi
 
