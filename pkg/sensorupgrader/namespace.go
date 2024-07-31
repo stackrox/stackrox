c44c53156f53c3dcc3f69ec4f51825a5060599e0
Jan Schnathmeier <jschnath@redhat.com>
2024-07-31 11:37:54 +0200

    Flip order of POD_NAMESPACE and SAToken namespace in GetSensorNamespace

--- pkg/sensorupgrader/namespace.go
+++ pkg/sensorupgrader/namespace.go
@@ -15,16 +15,22 @@ var (
 // GetSensorNamespace is a heuristic to determine in what namespace the Sensor runs.
 func GetSensorNamespace() string {
 	// This attempts to load the namespace from the file serviceaccount/namespace
-	sensorNamespace, err := satoken.LoadNamespaceFromFile()
-	if err != nil {
-		log.Errorf("Failed to determine namespace from service account token file: %s", err)
+	sensorNamespace := os.Getenv("POD_NAMESPACE")
+	if sensorNamespace == "" {
+		log.Infof("Failed to determine namespace from POD_NAMESPACE")
+	} else {
+		log.Infof("sensorNamespace is %s after running 'os.Getenv(\"POD_NAMESPACE\")'", sensorNamespace)
 	}
-	log.Infof("sensorNamespace is %s after running 'satoken.LoadNamespaceFromFile()'", sensorNamespace)
 
 	if sensorNamespace == "" {
 		// This environment variable is configured to contain pod namespace by sensor YAML/helm file.
-		sensorNamespace = os.Getenv("POD_NAMESPACE")
-		log.Infof("sensorNamespace is %s after running 'os.Getenv(\"POD_NAMESPACE\")'", sensorNamespace)
+		var err error
+		sensorNamespace, err = satoken.LoadNamespaceFromFile()
+		if err != nil {
+			log.Infof("Failed to determine namespace from service account token file: %s", err)
+		} else {
+			log.Infof("sensorNamespace is %s after running 'satoken.LoadNamespaceFromFile()'", sensorNamespace)
+		}
 	}
 	if sensorNamespace == "" {
 		sensorNamespace = namespaces.StackRox
