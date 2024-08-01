23c5b35be6b807fecae6f3b0edee17404cea1b0a
Jan Schnathmeier <102952141+jschnath@users.noreply.github.com>
2024-08-01 16:28:51 +0200

    ROX-24978: Compatibility tests should not test eol combinations (#11791)

--- scripts/ci/jobs/gke_version_compatibility_tests.py
+++ scripts/ci/jobs/gke_version_compatibility_tests.py
@@ -7,7 +7,6 @@ import logging
 import os
 import subprocess
 import sys
-
 from collections import namedtuple
 from pathlib import Path
 
@@ -20,7 +19,7 @@ from post_tests import PostClusterTest, FinalPost
 from runners import ClusterTestSetsRunner
 from clusters import GKECluster
 from get_latest_helm_chart_versions import (
-    get_latest_helm_chart_versions,
+    get_supported_helm_chart_versions,
     get_latest_helm_chart_version_for_specific_release,
 )
 
@@ -32,11 +31,8 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 # set required test parameters
 os.environ["ORCHESTRATOR_FLAVOR"] = "k8s"
 
-central_chart_versions = get_latest_helm_chart_versions(
-    "stackrox-central-services", 2)
-sensor_chart_versions = get_latest_helm_chart_versions(
-    "stackrox-secured-cluster-services", 3
-)
+central_chart_versions, sensor_chart_versions = get_supported_helm_chart_versions()
+
 makefile_path = Path(__file__).parent.parent.parent.parent
 latest_tag = subprocess.check_output(
     ["make", "tag", "-C", makefile_path, "--quiet", "--no-print-director"],
@@ -45,9 +41,11 @@ latest_tag = subprocess.check_output(
 ).strip()
 
 if len(central_chart_versions) == 0:
-    raise RuntimeError("Could not find central chart versions.")
+    logging.info("Found no older central chart versions to test against according to the product lifecycles API.")
 if len(sensor_chart_versions) == 0:
-    raise RuntimeError("Could not find sensor chart versions.")
+    logging.info("Found no older sensor chart versions to test against according to the product lifecycles API.")
+if len(central_chart_versions) == 0 or len(sensor_chart_versions) == 0:
+    logging.info("However versions with support exceptions will still be tested against.")
 
 ChartVersions = namedtuple(
     "Chart_versions", ["central_version", "sensor_version"])
@@ -67,8 +65,9 @@ test_tuples.extend(
     ]
 )
 
-# Support exception for latest central and sensor 3.74 as per
+# Currently there are no support exceptions, the last one expired on 2024-06-30, see:
 # https://issues.redhat.com/browse/ROX-18223
+# however a new support exception is being negotiated, add it here when it's ready
 support_exceptions = [
     ChartVersions(
         central_version=latest_tag,
@@ -84,33 +83,37 @@ test_tuples.extend(
     if support_exception not in test_tuples
 )
 
-sets = []
-for test_tuple in test_tuples:
-    os.environ["ROX_TELEMETRY_STORAGE_KEY_V1"] = 'DISABLED'
-    test_versions = f'{test_tuple.central_version}--{test_tuple.sensor_version}'
+if len(test_tuples) > 0:
+    sets = []
+    for test_tuple in test_tuples:
+        os.environ["ROX_TELEMETRY_STORAGE_KEY_V1"] = 'DISABLED'
+        test_versions = f'{test_tuple.central_version}--{test_tuple.sensor_version}'
 
-    # expected version string is like 74.x.x for ACS 3.74 versions
-    is_3_74_sensor = test_tuple.sensor_version.startswith('74')
+        # expected version string is like 74.x.x for ACS 3.74 versions
+        is_3_74_sensor = test_tuple.sensor_version.startswith('74')
 
-    sets.append(
-        {
-            "name": f'version compatibility tests: {test_versions}',
-            "test": QaE2eTestCompatibility(test_tuple.central_version, test_tuple.sensor_version),
-            "post_test": PostClusterTest(
-                    collect_collector_metrics=not is_3_74_sensor,
-                    check_stackrox_logs=True,
-                    artifact_destination_prefix=test_versions,
-            ),
-            # Collection not supported on 3.74
-            "pre_test": CollectionMethodOverridePreTest("NO_COLLECTION" if is_3_74_sensor else "core_bpf")
-        },
-    )
-ClusterTestSetsRunner(
-    cluster=GKECluster("compat-test",
-                       machine_type="e2-standard-8", num_nodes=2),
-    initial_pre_test=PreSystemTests(),
-    sets=sets,
-    final_post=FinalPost(
-        store_qa_tests_data=True,
-    ),
-).run()
+        sets.append(
+            {
+                "name": f'version compatibility tests: {test_versions}',
+                "test": QaE2eTestCompatibility(test_tuple.central_version, test_tuple.sensor_version),
+                "post_test": PostClusterTest(
+                        collect_collector_metrics=not is_3_74_sensor,
+                        check_stackrox_logs=True,
+                        artifact_destination_prefix=test_versions,
+                ),
+                # Collection not supported on 3.74
+                "pre_test": CollectionMethodOverridePreTest("NO_COLLECTION" if is_3_74_sensor else "core_bpf")
+            },
+        )
+    ClusterTestSetsRunner(
+        cluster=GKECluster("compat-test",
+                           machine_type="e2-standard-8", num_nodes=2),
+        initial_pre_test=PreSystemTests(),
+        sets=sets,
+        final_post=FinalPost(
+            store_qa_tests_data=True,
+        ),
+    ).run()
+else:
+    logging.info("There are currently no supported older versions or support exceptions that require compatibility "
+                 "testing.")
