c5b68b0b7e97755f4c6e8ccf5629acc0b7f76457
Jan Schnathmeier <jschnath@redhat.com>
2024-08-05 17:43:51 +0200

    Address feedback

--- pkg/pods/namespace.go
+++ pkg/pods/namespace.go
@@ -14,32 +14,34 @@ var (
 
 const (
 	// The corresponding environment variable is configured to contain pod namespace by sensor YAML/helm file using
-	// the Kubernetes Downward API
+	// the Kubernetes Downward API.
 	nsEnvVar = "POD_NAMESPACE"
 )
 
 type HeuristicType int
 
 const (
-	// NoSATokenNamespace NO_SA_TOKEN if this is set the SAToken namespace will not be queried
-	// This should be the default option unless we can be sure that the service account namespace contained within the
-	// SAToken namespace file indeed matches the namespace the pod is in. This is the case _most_ of the time but not
-	// always, eg. in our CI runners the service account namespace is set to <ci-op-HASH> instead, see
-	// https://github.com/openshift/ci-operator/blob/master/TEMPLATES.md
+	// NoSATokenNamespace is the default option. It does *not* read the SAToken
+	// file. For more information on why this mechanism is the default, see
+	//     https://issues.redhat.com/browse/ROX-12349
 	NoSATokenNamespace HeuristicType = iota
-	// UseSATokenNamespace USE_SA_TOKEN if this is set the SAToken will be queried first
-	// This option is here for legacy reasons since it has been how the namespace has been determined in CreateSensor
-	// since 2019, and it's unclear if removing this method would have side effects. In principle discarding the
-	// SAToken namespace method seems best as long as we can ensure that the POD_NAMESPACE env var is set to the
-	// namespace of its pod via the Kubernetes Downward API, see
-	// https://github.com/kubernetes/kubernetes/blob/release-1.0/docs/user-guide/downward-api.md
-	UseSATokenNamespace
+	// ConsiderSATokenNamespace switches the legacy mechanism which first reads the
+	// SAToken file namespace. While this mechanism works _most_ of the time, on
+	// our CI runners we saw the service account namespace set to `ci-op-<HASH`,
+	// see this for more information
+	//     https://github.com/openshift/ci-operator/blob/master/TEMPLATES.md
+	//
+	// This options is kept for now to avoid unexpected side effects but we aim
+	// to remove it in the future once we ensure that the POD_NAMESPACE env var
+	// is set correctly via the Kubernetes Downward API for all our pods, see
+	//     https://github.com/kubernetes/kubernetes/blob/release-1.0/docs/user-guide/downward-api.md
+	ConsiderSATokenNamespace
 )
 
 // GetPodNamespace is a heuristic to determine in what namespace a given Pod runs.
 func GetPodNamespace(heuristic HeuristicType) string {
-	sensorNamespace := ""
-	if heuristic == UseSATokenNamespace {
+	var sensorNamespace string
+	if heuristic == ConsiderSATokenNamespace {
 		var err error
 		sensorNamespace, err = satoken.LoadNamespaceFromFile()
 		if err != nil {
