c3267686e10858e1cbf12b72235b086803cc60ba
Jan Schnathmeier <jschnath@redhat.com>
2024-08-06 14:08:39 +0200

    Remove remaining hardcoded instance of stackrox namespace

--- sensor/kubernetes/upgrade/deployment.go
+++ sensor/kubernetes/upgrade/deployment.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/stackrox/rox/generated/internalapi/central"
-	"github.com/stackrox/rox/pkg/namespaces"
 	"github.com/stackrox/rox/pkg/pods"
 	appsV1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
@@ -47,7 +46,7 @@ func (p *process) determineImage() (string, error) {
 
 	// If the image is not specified, sensor uses the same image it's using to launch the upgrader.
 	// This code path will be hit during cert rotation.
-	sensorDeployment, err := p.k8sClient.AppsV1().Deployments(namespaces.StackRox).Get(p.ctx(), sensorDeploymentName, metav1.GetOptions{})
+	sensorDeployment, err := p.k8sClient.AppsV1().Deployments(pods.GetPodNamespace(pods.NoSATokenNamespace)).Get(p.ctx(), sensorDeploymentName, metav1.GetOptions{})
 	if err != nil {
 		return "", errors.Wrap(err, "failed to fetch sensor deployment from Kube")
 	}
