7093128ad1bf39aa8216f9517e132cf16ba8e0ee
Dave Shrewsberry <dashrews@redhat.com>
2024-08-09 14:21:09 -0400

    test target

--- generated/storage/compliance_integration_vtproto.pb.go
+++ generated/storage/compliance_integration_vtproto.pb.go
@@ -30,6 +30,7 @@ func (m *ComplianceIntegration) CloneVT() *ComplianceIntegration {
 	r.ComplianceNamespace = m.ComplianceNamespace
 	r.OperatorInstalled = m.OperatorInstalled
 	r.OperatorStatus = m.OperatorStatus
+	r.NewFieldToTestMakeTarget = m.NewFieldToTestMakeTarget
 	if rhs := m.StatusErrors; rhs != nil {
 		tmpContainer := make([]string, len(rhs))
 		copy(tmpContainer, rhs)
@@ -79,6 +80,9 @@ func (this *ComplianceIntegration) EqualVT(that *ComplianceIntegration) bool {
 	if this.OperatorStatus != that.OperatorStatus {
 		return false
 	}
+	if this.NewFieldToTestMakeTarget != that.NewFieldToTestMakeTarget {
+		return false
+	}
 	return string(this.unknownFields) == string(that.unknownFields)
 }
 
@@ -118,6 +122,13 @@ func (m *ComplianceIntegration) MarshalToSizedBufferVT(dAtA []byte) (int, error)
 	if m.unknownFields != nil {
 		i -= len(m.unknownFields)
 		copy(dAtA[i:], m.unknownFields)
+	}
+	if len(m.NewFieldToTestMakeTarget) > 0 {
+		i -= len(m.NewFieldToTestMakeTarget)
+		copy(dAtA[i:], m.NewFieldToTestMakeTarget)
+		i = protohelpers.EncodeVarint(dAtA, i, uint64(len(m.NewFieldToTestMakeTarget)))
+		i--
+		dAtA[i] = 0x42
 	}
 	if m.OperatorStatus != 0 {
 		i = protohelpers.EncodeVarint(dAtA, i, uint64(m.OperatorStatus))
@@ -207,6 +218,10 @@ func (m *ComplianceIntegration) SizeVT() (n int) {
 	}
 	if m.OperatorStatus != 0 {
 		n += 1 + protohelpers.SizeOfVarint(uint64(m.OperatorStatus))
+	}
+	l = len(m.NewFieldToTestMakeTarget)
+	if l > 0 {
+		n += 1 + l + protohelpers.SizeOfVarint(uint64(l))
 	}
 	n += len(m.unknownFields)
 	return n
@@ -440,6 +455,38 @@ func (m *ComplianceIntegration) UnmarshalVT(dAtA []byte) error {
 					break
 				}
 			}
+		case 8:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field NewFieldToTestMakeTarget", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return protohelpers.ErrIntOverflow
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return protohelpers.ErrInvalidLength
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return protohelpers.ErrInvalidLength
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.NewFieldToTestMakeTarget = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
 			skippy, err := protohelpers.Skip(dAtA[iNdEx:])
