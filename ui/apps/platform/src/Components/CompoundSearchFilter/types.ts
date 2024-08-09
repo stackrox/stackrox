fa4adb4311332ebe8efa1701a960cbc40739cfde
Saif Chaudhry <sachaudh@users.noreply.github.com>
2024-08-09 06:14:05 -0700

    ROX-25332: Adding Lifecycle stage to the policy violations filter (#12285)

--- ui/apps/platform/src/Components/CompoundSearchFilter/types.ts
+++ ui/apps/platform/src/Components/CompoundSearchFilter/types.ts
@@ -3,21 +3,25 @@ import { SearchCategory } from 'services/SearchService';
 
 // Compound search filter types
 
-export type InputType = 'autocomplete' | 'text' | 'date-picker' | 'condition-number' | 'select';
+export type BaseInputType = 'autocomplete' | 'text' | 'date-picker' | 'condition-number';
+export type InputType = BaseInputType | 'select';
 
 type BaseSearchFilterAttribute = {
     displayName: string;
     filterChipLabel: string;
     searchTerm: string;
-    inputType: InputType;
+    inputType: BaseInputType;
 };
 
-export interface SelectSearchFilterAttribute extends BaseSearchFilterAttribute {
+export type SelectSearchFilterAttribute = {
+    displayName: string;
+    filterChipLabel: string;
+    searchTerm: string;
     inputType: 'select';
     inputProps: {
         options: { label: string; value: string }[];
     };
-}
+};
 
 export type CompoundSearchFilterAttribute = BaseSearchFilterAttribute | SelectSearchFilterAttribute;
 
