0591fc76223bbd85ecf008c543d52fc710d979d1
Matthias Meidinger <mmeiding@redhat.com>
2024-08-09 15:06:18 +0200

    Squash previously tracked parent branches

--- pkg/scannerv4/enricher/nvd/return.go
+++
@@ -1,4 +0,0 @@
-package nvd
-
-// Type is the type of data returned from the Enricher's Enrich method.
-const Type = `message/vnd.stackrox.scannerv4.vulnerability; enricher=nvd schema=https://csrc.nist.gov/schema/nvd/api/2.0/source_api_json_2.0.schema`
