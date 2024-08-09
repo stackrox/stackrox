7e45314efbcb5765d3e9d8cd5fa641be14efba20
Cong Du <c-du@users.noreply.github.com>
2024-08-08 18:40:05 -0700

    ROX-25656: Add feature flag for Policy-As-Code (#12321)

--- pkg/features/list.go
+++ pkg/features/list.go
@@ -122,4 +122,7 @@ var (
 
 	// PolicyViolationsAdvancedFilters enables improved filtering capabilities for Policy Violations
 	PolicyViolationsAdvancedFilters = registerFeature("Enables improved filtering capabilities for Policy Violations", "ROX_POLICY_VIOLATIONS_ADVANCED_FILTERS", enabled)
+
+	// PolicyAsCode enables policy definition and lifecycle changes to be managed in external repositories.
+	PolicyAsCode = registerFeature("Enables policy definition and lifecycle changes to be managed in external repositories.", "ROX_POLICY_AS_CODE", unchangeableInProd)
 )
