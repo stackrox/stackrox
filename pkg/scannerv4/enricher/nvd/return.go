e143145089e44caac0cebb30d61794c5a4dcdca2
Matthias Meidinger <mmeiding@redhat.com>
2024-08-12 10:10:29 +0200

    Unify all scannerv4 constants into constants pacakge

--- pkg/scannerv4/enricher/nvd/return.go
+++
@@ -1,8 +0,0 @@
-package nvd
-
-const (
-	// Name of the enricher
-	Name = `nvd`
-	// Type is the type of data returned from the Enricher's Enrich method.
-	Type = `message/vnd.stackrox.scannerv4.vulnerability; enricher=` + Name + ` schema=https://csrc.nist.gov/schema/nvd/api/2.0/source_api_json_2.0.schema`
-)
