208649612cc69576d84b51b7c19c7f08be5336f6
davdhacs <105243888+davdhacs@users.noreply.github.com>
2024-08-16 11:16:33 -0600

    wait to destroy if ronconciling also

--- scripts/ci/gke.sh
+++ scripts/ci/gke.sh
@@ -329,7 +329,7 @@ teardown_gke_cluster() {
 
     for i in {1..10}; do
         gcloud container clusters describe "${CLUSTER_NAME}" --format "flattened(status)"
-        if [[ "$(gcloud container clusters describe "${CLUSTER_NAME}" --format 'get(status)')" != "PROVISIONING" ]]; then
+        if [[ ! "$(gcloud container clusters describe "${CLUSTER_NAME}" --format 'get(status)')" =~ PROVISIONING|RECONCILING ]]; then
             break
         fi
         info "Before deleting, waiting for cluster ${CLUSTER_NAME} to leave provisioning state (wait $i of 10)"
