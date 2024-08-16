030e3087195075a3be55157c5ca1af29fdd120ca
Michaël Petrov <smartptr@gmail.com>
2024-08-16 16:21:44 +0200

    tab->spaces

--- tests/scripts/launch-upgrader-test.sh
+++ tests/scripts/launch-upgrader-test.sh
@@ -9,13 +9,13 @@ ROX_ADMIN_PASSWORD="${ROX_ADMIN_PASSWORD:-$(< deploy/k8s/central-deploy/password
 ROX_API_ENDPOINT="${ROX_API_ENDPOINT:-localhost:8000}"
 
 escape() {
-	echo -n "$1 = \"${2//[\"\\]/\\&}\""
+    echo -n "$1 = \"${2//[\"\\]/\\&}\""
 }
 
 ROX_CLUSTER_ID="$(curl -sk --config <(escape user "admin:${ROX_ADMIN_PASSWORD}") "https://${ROX_API_ENDPOINT}/v1/clusters?query=cluster:${cluster_name}" | jq -r '.clusters[0].id')"
 if [[ -z "$ROX_CLUSTER_ID" ]]; then
-	echo >&2 "No such cluster: ${cluster_name}"
-	exit 1
+    echo >&2 "No such cluster: ${cluster_name}"
+    exit 1
 fi
 
 curl -sk --config <(escape user "admin:${ROX_ADMIN_PASSWORD}") "https://${ROX_API_ENDPOINT}/v1/sensorupgrades/cluster/${ROX_CLUSTER_ID}" -X POST
