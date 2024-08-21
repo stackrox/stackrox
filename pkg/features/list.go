61f396df75c990fe1cd98e71645585d742a5a457
Simon Bäumer <sbaumer@redhat.com>
2024-08-21 11:10:17 +0200

    ROX-25472: Add Microsoft Sentinel feature flag (#12465)

--- pkg/features/list.go
+++ pkg/features/list.go
@@ -122,4 +122,7 @@ var (
 
 	// PolicyAsCode enables policy definition and lifecycle changes to be managed in external repositories.
 	PolicyAsCode = registerFeature("Enables policy definition and lifecycle changes to be managed in external repositories.", "ROX_POLICY_AS_CODE", unchangeableInProd)
+
+	// MicrosoftSentinelNotifier enables the Microsoft Sentinel notifier.
+	MicrosoftSentinelNotifier = registerFeature("Enable the Microsoft Sentinel notifier", "ROX_MICROSOFT_SENTINEL")
 )
