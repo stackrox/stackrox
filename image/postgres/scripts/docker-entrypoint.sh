ee6b6a262f7c116072d7c9d52fb2382791764910
Dave Shrewsberry <dashrews@redhat.com>
2024-08-21 13:57:42 -0400

    messing around

--- image/postgres/scripts/docker-entrypoint.sh
+++ image/postgres/scripts/docker-entrypoint.sh
@@ -45,11 +45,11 @@ docker_create_db_directories() {
 
 	mkdir -p "$PGDATA"
 	# ignore failure since there are cases where we can't chmod (and PostgreSQL might fail later anyhow - it's picky about permissions of this directory)
-	chmod 700 "$PGDATA" || :
+#	chmod 700 "$PGDATA" || :
 
 	# ignore failure since it will be fine when using the image provided directory; see also https://github.com/docker-library/postgres/pull/289
 	mkdir -p /var/run/postgresql || :
-	chmod 775 /var/run/postgresql || :
+#	chmod 775 /var/run/postgresql || :
 
 	# Create the transaction log directory before initdb is run so the directory is owned by the correct user
 	if [ -n "$POSTGRES_INITDB_WALDIR" ]; then
@@ -57,7 +57,7 @@ docker_create_db_directories() {
 		if [ "$user" = '0' ]; then
 			find "$POSTGRES_INITDB_WALDIR" \! -user postgres -exec chown postgres '{}' +
 		fi
-		chmod 700 "$POSTGRES_INITDB_WALDIR"
+#		chmod 700 "$POSTGRES_INITDB_WALDIR"
 	fi
 
 	# allow the container to be started with `--user`
