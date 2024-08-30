8ed7595203f1d8b5c846901e6d5476a640671c21
Tomasz Janiszewski <tomek@redhat.com>
2024-08-30 11:47:34 +0200

    fix(pg): cleanup ScanRows and Marshaler (#12496)
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

--- pkg/protocompat/message.go
+++ pkg/protocompat/message.go
@@ -43,19 +43,6 @@ func MarshalTextString(m proto.Message) string {
 	return prototext.MarshalOptions{Multiline: true}.Format(m)
 }
 
-// Unmarshaler is a generic interface type wrapping around types that implement protobuf Unmarshaler.
-type Unmarshaler[T any] interface {
-	UnmarshalVTUnsafe(dAtA []byte) error
-	*T
-}
-
-// ClonedUnmarshaler is a generic interface type wrapping around types that implement protobuf Unmarshaler
-// and that have a Clone deep-copy method.
-type ClonedUnmarshaler[T any] interface {
-	Unmarshaler[T]
-	CloneVT() *T
-}
-
 // Merge merges src into dst.
 // Required and optional fields that are set in src will be set to that value in dst.
 // Elements of repeated fields will be appended.
