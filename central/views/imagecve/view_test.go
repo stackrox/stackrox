fdf0eed9b0327edd1e7fad1902eed19800420730
Brad Lugo <blugo@redhat.com>
2024-09-05 14:19:55 -0700

    Fix the rest of ImageCVEViewTestSuite

--- central/views/imagecve/view_test.go
+++ central/views/imagecve/view_test.go
@@ -186,8 +186,32 @@ func (s *ImageCVEViewTestSuite) TestGetImageCVECore() {
 }
 
 func (s *ImageCVEViewTestSuite) TestGetImageCVECoreSAC() {
-	for _, tc := range s.testCases() {
-		for key, sacTC := range s.sacTestCases() {
+	// Note: The new loop variable semantics introduced in go1.22 breaks these
+	// tests when using loop variables in closures such that the test code
+	// expects those loop variables to change on each iteration of the loop.
+	// The problem stems from what a closure captures, or "sees", loop
+	// variables on each iteration. Before go1.22, loop variables had per-loop
+	// scope, which is to say that any closure inside the loop using loop
+	// variables would have the reference of the loop variable updated on each
+	// iteration. In go1.22+, loop variables have per-iteration scope, which,
+	// for closures inside the loop, means that when a given closure first
+	// captures the variable, that value will always be used in that particular
+	// closure.
+	// To get around this, we define variables outside the loop and then set
+	// those variables to the loop variables on each iteration, then use the
+	// variables in the outer scope (relative to the loop) in the closure,
+	// effectively achieving per-loop scoped variables.
+	// References and further reading:
+	// - https://web.archive.org/web/20240905192132/https://go.dev/blog/go1.22
+	// - https://web.archive.org/web/20240905181916/https://go.dev/blog/loopvar-preview
+	// - https://web.archive.org/web/20240905191855/https://go.dev/wiki/LoopvarExperiment
+	// - https://web.archive.org/web/20240905192225/https://go.googlesource.com/proposal/+/master/design/60078-loopvar.md
+	// - https://web.archive.org/web/20240905191656/https://go101.org/blog/2024-03-01-for-loop-semantic-changes-in-go-1.22.html
+	var tc testCase
+	var key string
+	var sacTC map[string]bool
+	for _, tc = range s.testCases() {
+		for key, sacTC = range s.sacTestCases() {
 			s.T().Run(fmt.Sprintf("Image %s %s", key, tc.desc), func(t *testing.T) {
 				testCtxs := testutils.GetNamespaceScopedTestContexts(tc.ctx, s.T(), resources.Image)
 				ctx := testCtxs[key]
@@ -217,7 +241,7 @@ func (s *ImageCVEViewTestSuite) TestGetImageCVECoreSAC() {
 	}
 
 	// Testing one query against deployment access is sufficient.
-	tc := s.testCases()[0]
+	tc = s.testCases()[0]
 	s.T().Run(fmt.Sprintf("Deployment read access %s", tc.desc), func(t *testing.T) {
 		ctx := sac.WithGlobalAccessScopeChecker(tc.ctx,
 			sac.AllowFixedScopes(
@@ -253,8 +277,32 @@ func (s *ImageCVEViewTestSuite) TestGetImageIDs() {
 }
 
 func (s *ImageCVEViewTestSuite) TestGetImageIDsSAC() {
-	for _, tc := range s.testCases() {
-		for key, sacTC := range s.sacTestCases() {
+	// Note: The new loop variable semantics introduced in go1.22 breaks these
+	// tests when using loop variables in closures such that the test code
+	// expects those loop variables to change on each iteration of the loop.
+	// The problem stems from what a closure captures, or "sees", loop
+	// variables on each iteration. Before go1.22, loop variables had per-loop
+	// scope, which is to say that any closure inside the loop using loop
+	// variables would have the reference of the loop variable updated on each
+	// iteration. In go1.22+, loop variables have per-iteration scope, which,
+	// for closures inside the loop, means that when a given closure first
+	// captures the variable, that value will always be used in that particular
+	// closure.
+	// To get around this, we define variables outside the loop and then set
+	// those variables to the loop variables on each iteration, then use the
+	// variables in the outer scope (relative to the loop) in the closure,
+	// effectively achieving per-loop scoped variables.
+	// References and further reading:
+	// - https://web.archive.org/web/20240905192132/https://go.dev/blog/go1.22
+	// - https://web.archive.org/web/20240905181916/https://go.dev/blog/loopvar-preview
+	// - https://web.archive.org/web/20240905191855/https://go.dev/wiki/LoopvarExperiment
+	// - https://web.archive.org/web/20240905192225/https://go.googlesource.com/proposal/+/master/design/60078-loopvar.md
+	// - https://web.archive.org/web/20240905191656/https://go101.org/blog/2024-03-01-for-loop-semantic-changes-in-go-1.22.html
+	var tc testCase
+	var key string
+	var sacTC map[string]bool
+	for _, tc = range s.testCases() {
+		for key, sacTC = range s.sacTestCases() {
 			s.T().Run(fmt.Sprintf("Image %s %s", key, tc.desc), func(t *testing.T) {
 				// Such testcases are meant only for Get().
 				if tc.expectedErr != "" {
