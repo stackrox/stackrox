685a97cbdaf038377f2b0a98e35e4cb6d79dd10a
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2024-09-06 16:36:25 +0200

    ROX-25451: Re-enable Sensor upgrader (#12150)

--- pkg/env/managed.go
+++ pkg/env/managed.go
@@ -4,6 +4,9 @@ var (
 	// ManagedCentral is set to true to signal that the central is running as a managed instance.
 	ManagedCentral = RegisterBooleanSetting("ROX_MANAGED_CENTRAL", false)
 
+	// SensorUpgraderEnabled controls whether the secured cluster auto-upgrader is enabled
+	SensorUpgraderEnabled = RegisterBooleanSetting("ROX_SENSOR_UPGRADER_ENABLED", true)
+
 	ACSCSEmailURL = RegisterSetting("ROX_ACSCS_EMAIL_URL", WithDefault("https://emailsender.rhacs.svc"))
 
 	// TenantID is set from the according central pod label with the Downward API.
