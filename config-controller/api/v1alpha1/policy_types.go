cbc7e38fb99c256abd79ee5c87ccba58ea4548d7
Kyle Lape <klape@redhat.com>
2024-09-06 17:01:35 -0500

    Manual creation of policy crd spec
    Add test, conversion function, and modifications

--- config-controller/api/v1alpha1/policy_types.go
+++ config-controller/api/v1alpha1/policy_types.go
@@ -18,11 +18,200 @@ package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/stackrox/rox/generated/storage"
 )
 
 // PolicySpec defines the desired state of Policy
 type PolicySpec struct {
-	Policy string `json:"policy,omitempty"`
+	Name        string   `json:"name,omitempty"`
+	Description string   `json:"description,omitempty"`
+	Rationale   string   `json:"rationale,omitempty"`
+	Remediation string   `json:"remediation,omitempty"`
+	Disabled    bool     `json:"disabled,omitempty"`
+	Categories  []string `json:"categories,omitempty"`
+	// +kubebuilder:validation:Enum=DEPLOY;BUILD;RUNTIME
+	LifecycleStages []string    `json:"lifecycleStages,omitempty"`
+	Exclusions      []Exclusion `json:"exclusions,omitempty"`
+	Scope           []Scope     `json:"scope,omitempty"`
+	// +kubebuilder:validation:Enum=UNSET_SEVERITY;LOW_SEVERITY;MEDIUM_SEVERITY;HIGH_SEVERITY;CRITICAL_SEVERITY
+	Severity string `json:"severity,omitempty"`
+	// +kubebuilder:validation:Enum=UNSET_ENFORCEMENT;SCALE_TO_ZERO_ENFORCEMENT;UNSATISFIABLE_NODE_CONSTRAINT_ENFORCEMENT;KILL_POD_ENFORCEMENT;FAIL_BUILD_ENFORCEMENT;FAIL_KUBE_REQUEST_ENFORCEMENT;FAIL_DEPLOYMENT_CREATE_ENFORCEMENT;FAIL_DEPLOYMENT_UPDATE_ENFORCEMENT
+	EnforcementActions []string             `json:"enforcementActions,omitempty"`
+	Notifiers          []string             `json:"notifiers,omitempty"`
+	PolicyVersion      string               `json:"policyVersion,omitempty"`
+	PolicySections     []PolicySection      `json:"policySections,omitempty"`
+	MitreAttackVectors []MitreAttackVectors `json:"mitreAttackVectors,omitempty"`
+	CriteriaLocked     bool                 `json:"criteriaLocked,omitempty"`
+	MitreVectorsLocked bool                 `json:"mitreVectorsLocked,omitempty"`
+	IsDefault          bool                 `json:"isDefault,omitempty"`
+}
+
+func (p PolicySpec) ToProtobuf() *storage.Policy {
+	proto := storage.Policy{
+		Name:               p.Name,
+		Description:        p.Description,
+		Rationale:          p.Rationale,
+		Remediation:        p.Remediation,
+		Disabled:           p.Disabled,
+		Categories:         p.Categories,
+		Notifiers:          p.Notifiers,
+		PolicyVersion:      p.PolicyVersion,
+		CriteriaLocked:     p.CriteriaLocked,
+		MitreVectorsLocked: p.MitreVectorsLocked,
+		IsDefault:          p.IsDefault,
+	}
+
+	for _, ls := range p.LifecycleStages {
+		val, found := storage.LifecycleStage_value[ls]
+		if found {
+			proto.LifecycleStages = append(proto.LifecycleStages, storage.LifecycleStage(val))
+		}
+	}
+
+	for _, exclusion := range p.Exclusions {
+		protoExclusion := storage.Exclusion{
+			Name: exclusion.Name,
+		}
+
+		if exclusion.Deployment != (Deployment{}) {
+			protoExclusion.Deployment = &storage.Exclusion_Deployment{
+				Name: exclusion.Deployment.Name,
+			}
+
+			scope := exclusion.Deployment.Scope
+			if scope != (Scope{}) {
+				protoExclusion.Deployment.Scope = &storage.Scope{
+					Cluster:   scope.Cluster,
+					Namespace: scope.Namespace,
+				}
+			}
+
+			if scope.Label != (Label{}) {
+				protoExclusion.Deployment.Scope.Label = &storage.Scope_Label{
+					Key:   scope.Label.Key,
+					Value: scope.Label.Value,
+				}
+			}
+		}
+
+		proto.Exclusions = append(proto.Exclusions, &protoExclusion)
+	}
+
+	for _, scope := range p.Scope {
+		protoScope := &storage.Scope{
+			Cluster:   scope.Cluster,
+			Namespace: scope.Namespace,
+		}
+
+		if scope.Label != (Label{}) {
+			protoScope.Label = &storage.Scope_Label{
+				Key:   scope.Label.Key,
+				Value: scope.Label.Value,
+			}
+		}
+
+		proto.Scope = append(proto.Scope, protoScope)
+	}
+
+	val, found := storage.Severity_value[p.Severity]
+	if found {
+		proto.Severity = storage.Severity(val)
+	}
+
+	for _, ea := range p.EnforcementActions {
+		val, found := storage.EnforcementAction_value[ea]
+		if found {
+			proto.EnforcementActions = append(proto.EnforcementActions, storage.EnforcementAction(val))
+		}
+	}
+
+	for _, section := range p.PolicySections {
+		protoSection := &storage.PolicySection{
+			SectionName: section.SectionName,
+		}
+
+		for _, group := range section.PolicyGroups {
+			protoGroup := &storage.PolicyGroup{
+				FieldName: group.FieldName,
+				Negate:    group.Negate,
+			}
+
+			val, found = storage.BooleanOperator_value[group.BooleanOperator]
+			if found {
+				protoGroup.BooleanOperator = storage.BooleanOperator(val)
+			}
+
+			for _, value := range group.Values {
+				protoValue := &storage.PolicyValue{
+					Value: value.Value,
+				}
+				protoGroup.Values = append(protoGroup.Values, protoValue)
+			}
+			protoSection.PolicyGroups = append(protoSection.PolicyGroups, protoGroup)
+		}
+		proto.PolicySections = append(proto.PolicySections, protoSection)
+	}
+
+	for _, mitreAttackVectors := range p.MitreAttackVectors {
+		protoMitreAttackVetor := &storage.Policy_MitreAttackVectors{
+			Tactic:     mitreAttackVectors.Tactic,
+			Techniques: mitreAttackVectors.Techniques,
+		}
+
+		proto.MitreAttackVectors = append(proto.MitreAttackVectors, protoMitreAttackVetor)
+	}
+
+	return &proto
+}
+
+type Exclusion struct {
+	Name       string     `json:"name,omitempty"`
+	Deployment Deployment `json:"deployment,omitempty"`
+	Image      Image      `json:"image,omitempty"`
+	//TODO: Expiration
+}
+
+type Deployment struct {
+	Name  string `json:"name,omitempty"`
+	Scope Scope  `json:"scope,omitempty"`
+}
+
+type Image struct {
+	Name string `json:"name,omitempty"`
+}
+
+type Scope struct {
+	Cluster   string `json:"cluster,omitempty"`
+	Namespace string `json:"namespace,omitempty"`
+	Label     Label  `json:"label,omitempty"`
+}
+
+type Label struct {
+	Key   string `json:"key,omitempty"`
+	Value string `json:"value,omitempty"`
+}
+
+type PolicySection struct {
+	SectionName  string        `json:"sectionName,omitempty"`
+	PolicyGroups []PolicyGroup `json:"policyGroups,omitempty"`
+}
+
+type PolicyGroup struct {
+	FieldName string `json:"fieldName,omitempty"`
+	// +kubebuilder:validation:Enum=OR;AND `json:"//,omitempty"`
+	BooleanOperator string        `json:"booleanOperator,omitempty"`
+	Negate          bool          `json:"negate,omitempty"`
+	Values          []PolicyValue `json:"values,omitempty"`
+}
+
+type PolicyValue struct {
+	Value string `json:"value,omitempty"`
+}
+
+type MitreAttackVectors struct {
+	Tactic     string   `json:"tactic,omitempty"`
+	Techniques []string `json:"techniques,omitempty"`
 }
 
 // PolicyStatus defines the observed state of Policy
---
+++ config-controller/api/v1alpha1/policy_types_test.go
@@ -0,0 +1,72 @@
+package v1alpha1
+
+import (
+	"embed"
+	"encoding/json"
+	"strings"
+	"testing"
+
+	// "github.com/stackrox/rox/generated/storage"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/protobuf/encoding/protojson"
+)
+
+//go:embed test.json
+
+var files embed.FS
+
+func TestMarshalJSON(t *testing.T) {
+
+	bytes, err := files.ReadFile("test.json")
+
+	assert.NoError(t, err, "Failed to read policy file")
+
+	policyCRSpec := PolicySpec{}
+
+	assert.NoError(t, json.Unmarshal(bytes, &policyCRSpec), "Failed to unmarshal policy spec CR JSON")
+
+	expected := PolicySpec{
+		Name:            "Test policy",
+		Description:     "This is a test description",
+		Rationale:       "This is a test rationale",
+		Remediation:     "This is a test remediation",
+		Categories:      []string{"Security Best Practices"},
+		LifecycleStages: []string{"BUILD", "DEPLOY"},
+		Exclusions: []Exclusion{{
+			Name: "Don't alert on deployment collector in namespace stackrox",
+			Deployment: Deployment{
+				Name: "collector",
+				Scope: Scope{
+					Namespace: "stackrox",
+					Cluster:   "test",
+				},
+			}},
+		},
+		Severity:      "LOW_SEVERITY",
+		PolicyVersion: "1.1",
+		PolicySections: []PolicySection{{
+			SectionName: "Section name",
+			PolicyGroups: []PolicyGroup{{
+				FieldName: "Image Component",
+				Values: []PolicyValue{{
+					Value: "rpm|microdnf|dnf|yum=",
+				}},
+			}},
+		}},
+		CriteriaLocked:     true,
+		MitreVectorsLocked: true,
+		IsDefault:          false,
+	}
+
+	assert.Equal(t, expected, policyCRSpec)
+
+	protoPolicy := expected.ToProtobuf()
+
+	protoBytes, err := protojson.MarshalOptions{
+		Multiline: true,
+	}.Marshal(protoPolicy)
+
+	assert.NoError(t, err, "Failed to marshal protobuf")
+
+	assert.Equal(t, string(bytes), strings.ReplaceAll(string(protoBytes), ":  ", ": ")+"\n")
+}
