ed61fec044987e1e478deb39f23a1a4c7c9ca00e
Saif Chaudhry <schaudhr@redhat.com>
2024-09-10 09:39:32 -0700

    addressing comments

--- ui/apps/platform/src/Components/ReportJobsHelpAction.tsx
+++ ui/apps/platform/src/Components/ReportJobsHelpAction.tsx
@@ -15,26 +15,25 @@ function ReportJobsHelpAction({ reportType }: ReportJobsHelpActionProps) {
     const { hasReadWriteAccess } = usePermissions();
     const hasAdministrationReadWriteAccess = hasReadWriteAccess('Administration');
 
-    const bodyContent = hasAdministrationReadWriteAccess ? (
+    const bodyContent = (
         <>
-            This function displays requested jobs from various users, including their statuses. You
-            can monitor and audit both active and past job requests. For an optimal experience, we
-            recommend configuring the{' '}
-            <ExternalLink>
-                <a href={systemConfigPath} target="_blank" rel="noreferrer">
-                    {reportType} report retention limit
-                </a>
-            </ExternalLink>{' '}
-            to suit your needs. Report jobs will remain in the system until they exceed the limit
-            you set.
-        </>
-    ) : (
-        <>
-            This function shows requested jobs from various users, including their statuses. While
-            you can track and audit your active and past job requests, any changes to the{' '}
-            <b>{reportType} report retention limit</b> must be configured by your administrator to
-            ensure an optimal experience. Reports will remain in the system until they exceed the
-            retention limit set by the administrator.
+            <div>
+                This function displays the requested jobs from different users and includes their
+                statuses accordingly. While the function provides the ability to monitor and audit
+                your active and past requested jobs, we suggest configuring the{' '}
+                <strong>{reportType} report retention limit</strong> based on your needs in order to
+                ensure optimal user experience. All the report jobs will be kept in your system
+                until they exceed the limit set by you.
+            </div>
+            {hasAdministrationReadWriteAccess && (
+                <div className="pf-v5-u-mt-sm">
+                    <ExternalLink>
+                        <a href={systemConfigPath} target="_blank" rel="noopener noreferrer">
+                            System Configuration
+                        </a>
+                    </ExternalLink>
+                </div>
+            )}
         </>
     );
 
