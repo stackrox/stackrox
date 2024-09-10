3e0a917b54d4fe03479da2b7b80f72ea259d2ddb
Marcin Owsiany <porridge@redhat.com>
2024-09-10 13:11:46 +0200

    the rest

--- operator/tests/reconcile/mapkubeapis_test.go
+++ operator/tests/reconcile/mapkubeapis_test.go
@@ -11,7 +11,7 @@ import (
 	helmClient "github.com/operator-framework/helm-operator-plugins/pkg/client"
 	"github.com/operator-framework/helm-operator-plugins/pkg/reconciler"
 	"github.com/pkg/errors"
-	commonExtensions "github.com/stackrox/rox/operator/pkg/common/extensions"
+	commonExtensions "github.com/stackrox/rox/operator/internal/common/extensions"
 	"helm.sh/helm/v3/pkg/action"
 	"helm.sh/helm/v3/pkg/chart"
 	"helm.sh/helm/v3/pkg/storage"
