fac15e9ca59c9054478e8fcb020ae5e474b13601
David Shrewsberry <99685630+dashrews78@users.noreply.github.com>
2024-09-10 10:23:39 -0400

    ROX-26104: add index on policy id to alert table (#12640)
    Add an index on policy id in the alerts table.

--- generated/storage/policy.pb.go
+++ generated/storage/policy.pb.go
@@ -394,7 +394,7 @@ type Policy struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	Id              string           `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty" search:"Policy ID,store,hidden" sql:"pk"`                   // @gotags: search:"Policy ID,store,hidden" sql:"pk"
+	Id              string           `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty" search:"Policy ID,store,hidden" sql:"pk,index=btree"`                   // @gotags: search:"Policy ID,store,hidden" sql:"pk,index=btree"
 	Name            string           `protobuf:"bytes,2,opt,name=name,proto3" json:"name,omitempty" search:"Policy,store" sql:"unique"`               // @gotags: search:"Policy,store" sql:"unique"
 	Description     string           `protobuf:"bytes,3,opt,name=description,proto3" json:"description,omitempty" search:"Description"` // @gotags: search:"Description"
 	Rationale       string           `protobuf:"bytes,4,opt,name=rationale,proto3" json:"rationale,omitempty"`
