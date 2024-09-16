ed5191e254b5ad26d426e27f729c1f7a976a9a07
Moritz Clasmeier <mclasmeier@redhat.com>
2024-09-16 09:44:01 +0200

    Skip allocation in case user is nil

--- roxctl/central/crs/common.go
+++ roxctl/central/crs/common.go
@@ -3,14 +3,14 @@ package crs
 import "github.com/stackrox/rox/generated/storage"
 
 func getPrettyUser(user *storage.User) string {
-	attributePrecedence := []string{"email", "name", "userid"}
-	attributes := make(map[string]string)
-
 	if user == nil {
 		return "(unknown)"
 	}
 
+	attributePrecedence := []string{"email", "name", "userid"}
+
 	// Extract all attributes into map.
+	attributes := make(map[string]string)
 	for _, attr := range user.GetAttributes() {
 		attributes[attr.GetKey()] = attr.GetValue()
 	}
