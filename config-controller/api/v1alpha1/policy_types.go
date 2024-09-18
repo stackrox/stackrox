4a08d5554cbddc78352a562d2c54994d1c3171c0
Kyle Lape <klape@redhat.com>
2024-09-18 12:24:52 -0500

    Remove dummy field

--- config-controller/api/v1alpha1/policy_types.go
+++ config-controller/api/v1alpha1/policy_types.go
@@ -53,11 +53,6 @@ type SecurityPolicySpec struct {
 	CriteriaLocked     bool                 `json:"criteriaLocked,omitempty"`
 	MitreVectorsLocked bool                 `json:"mitreVectorsLocked,omitempty"`
 	IsDefault          bool                 `json:"isDefault,omitempty"`
-	DummyField         DummyStruct          `json:"dummyField,omitempty"`
-}
-
-type DummyStruct struct {
-	Something string `json:"something,omitempty"`
 }
 
 func (p SecurityPolicySpec) ToProtobuf() *storage.Policy {
