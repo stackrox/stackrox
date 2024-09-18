e741728e834a28291ca4dcacb732797f65f70ec9
Jan Schnathmeier <jschnath@redhat.com>
2024-09-18 17:23:14 +0200

    Add dummy target matching gke-nongroovy-compatibility-tests

---
+++ scripts/ci/jobs/gke_nongroovy_compatibility_tests.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env -S python3 -u
+
+"""
+Run version compatibility tests
+"""
+import logging
+
+logging.info("Dummy test target gke-nongroovy-comatibility-tests has been called successfully.")
