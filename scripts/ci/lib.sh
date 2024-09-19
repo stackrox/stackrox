1d1cfc3d7cd89e8dea24449eee7ca1e30755ef42
Mladen Todorovic <mtodor@gmail.com>
2024-09-19 16:19:50 +0200

    Fix mention only when new JIRA is created

--- scripts/ci/lib.sh
+++ scripts/ci/lib.sh
@@ -1903,7 +1903,8 @@ slack_workflow_failure() {
     repo=$(jq -r <<<"${github_context}" '.repository')
     run_id=$(jq -r <<<"${github_context}" '.run_id')
 
-    local mention_author="true"
+    # If global "mention_author" is set use that value.
+    local mention_author="${mention_author:-true}"
     local slack_mention=""
     _make_slack_mention
 
