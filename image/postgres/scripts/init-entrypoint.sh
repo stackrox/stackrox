b3345358db0c29aca0926596c6714c207337a0ee
Dave Shrewsberry <dashrews@redhat.com>
2024-09-19 13:33:17 -0400

    X-Smart-Squash: Squashed 25 commits:
    c5caf175e0 switch to scl images
    116926ea05 use scl image start
    a1d10c3e33 set env vars
    9ed8c3fbe0 skip init
    d45f25433b some debugging
    f7491f9e62 more debug
    d4f04818cc more debug
    aa064975ac more debug
    34065a22d0 more debug
    db7428b153 more debug
    55b522ffa8 more debug
    2796b1a587 more debug
    d78627c164 more debug
    a7170d3240 more debug
    c6e2cc4de1 more debug
    5af1a1a10f more debug
    2371315ffc more debug
    7e65861b34 more debug
    ed65934e42 more debug
    9f538a86de more debug
    6de77d789e more debug
    e28a79a70c more debug
    fbd24d6bf6 more debug
    887967ce93 more debug
    541368d718 revert to the way downstream is

--- image/postgres/scripts/init-entrypoint.sh
+++ image/postgres/scripts/init-entrypoint.sh
@@ -3,6 +3,8 @@
 set -Eeo pipefail
 
 # Initialize DB if it does not exist
+echo 'SHREWS -- Init DB if not exists'
 if [ ! -s "$PGDATA/PG_VERSION" ]; then
   initdb --auth-host=scram-sha-256 --auth-local=scram-sha-256 --pwfile /run/secrets/stackrox.io/secrets/password --data-checksums
+  echo 'SHREWS -- Finish init-db'
 fi
