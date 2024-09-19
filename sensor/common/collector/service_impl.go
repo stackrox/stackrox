5c971f1dc237b0315064758cb3acf3d0ef4026ce
JoukoVirtanen <jouko@stackrox.com>
2024-09-18 20:44:38 -0700

    Removed commented out code and logs for debugging. make style

--- sensor/common/collector/service_impl.go
+++ sensor/common/collector/service_impl.go
@@ -3,16 +3,12 @@ package collector
 import (
 	"context"
 
-	// "github.com/grpc-ecosystem/go-grpc-middleware/util/metautils"
-	// "github.com/grpc-ecosystem/grpc-gateway/runtime"
-        "github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
-	// "github.com/pkg/errors"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/stackrox/rox/generated/internalapi/central"
 	"github.com/stackrox/rox/generated/internalapi/sensor"
 	"github.com/stackrox/rox/pkg/centralsensor"
 	"github.com/stackrox/rox/pkg/grpc/authz/idcheck"
 	"github.com/stackrox/rox/pkg/logging"
-	// "github.com/stackrox/rox/pkg/protocompat"
 	"github.com/stackrox/rox/pkg/sync"
 	"github.com/stackrox/rox/sensor/common"
 	"github.com/stackrox/rox/sensor/common/message"
@@ -47,12 +43,10 @@ func (s *serviceImpl) Capabilities() []centralsensor.SensorCapability {
 }
 
 func (s *serviceImpl) ProcessMessage(msg *central.MsgToSensor) error {
-	log.Info("In ProcessMessage")
 	if msg.GetClusterConfig() != nil && msg.GetClusterConfig().GetConfig() != nil && msg.GetClusterConfig().GetConfig().GetCollectorConfig() != nil {
-		log.Infof("msg= %+v", msg)
 		s.collectorC <- common.MessageToCollectorWithAddress{
 			Msg: &sensor.MsgToCollector{
-				Msg: &sensor.MsgToCollector_CollectorConfig {
+				Msg: &sensor.MsgToCollector_CollectorConfig{
 					CollectorConfig: msg.GetClusterConfig().GetConfig().GetCollectorConfig(),
 				},
 			},
@@ -81,29 +75,20 @@ func newConnectionManager() *connectionManager {
 func (c *connectionManager) add(connection sensor.CollectorService_CommunicateServer) {
 	c.connectionLock.Lock()
 	defer c.connectionLock.Unlock()
-	log.Infof("In add")
 
-	// c.connectionMap[node] = connection
 	c.connectionMap[connection] = true
-	log.Infof("len(c.connectionMap)= %+v", len(c.connectionMap))
 }
 
 func (c *connectionManager) remove(connection sensor.CollectorService_CommunicateServer) {
 	c.connectionLock.Lock()
 	defer c.connectionLock.Unlock()
 
-	log.Info("In remove")
 	delete(c.connectionMap, connection)
-	log.Infof("len(c.connectionMap)= %+v", len(c.connectionMap))
 }
 
 func (s *serviceImpl) startSendingLoop() {
-	log.Info("In startSendingLoop")
 	for msg := range s.collectorC {
-		log.Infof("msg %+v", msg)
-		log.Infof("len(s.connectionManager.connectionMap)= %+v", len(s.connectionManager.connectionMap))
 		for conn := range s.connectionManager.connectionMap {
-			log.Infof("Sending msg")
 			err := conn.Send(msg.Msg)
 			if err != nil {
 				log.Info("Sending msg failed")
@@ -113,9 +98,7 @@ func (s *serviceImpl) startSendingLoop() {
 	}
 }
 
-//func (s *serviceImpl) Communicate(empty *protocompat.Empty, server sensor.CollectorService_CommunicateServer) error {
 func (s *serviceImpl) Communicate(server sensor.CollectorService_CommunicateServer) error {
-	log.Info("In Communicate")
 
 	s.connectionManager.add(server)
 	defer s.connectionManager.remove(server)
@@ -127,7 +110,6 @@ func (s *serviceImpl) Communicate(server sensor.CollectorService_CommunicateServ
 
 // RegisterServiceServer registers this service with the given gRPC Server.
 func (s *serviceImpl) RegisterServiceServer(grpcServer *grpc.Server) {
-	log.Info("In RegisterServiceServer")
 	sensor.RegisterCollectorServiceServer(grpcServer, s)
 }
 
@@ -138,6 +120,5 @@ func (s *serviceImpl) RegisterServiceHandler(context.Context, *runtime.ServeMux,
 
 // AuthFuncOverride specifies the auth criteria for this API.
 func (s *serviceImpl) AuthFuncOverride(ctx context.Context, fullMethodName string) (context.Context, error) {
-	log.Info("In AuthFuncOverride")
 	return ctx, idcheck.CollectorOnly().Authorized(ctx, fullMethodName)
 }
