f0470d9b20daa197ae8046a3047924910b3530f8
Saif Chaudhry <schaudhr@redhat.com>
2024-09-23 10:09:09 -0700

    refactor(ui): Clean up reporting related components

---
+++ ui/apps/platform/src/Components/ReportJob/MyActiveJobStatusTh.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Flex, FlexItem } from '@patternfly/react-core';
+
+import HelpIconTh from 'Containers/Vulnerabilities/VulnerablityReporting/VulnReports/HelpIconTh';
+
+function MyActiveJobStatusTh() {
+    return (
+        <HelpIconTh
+            popoverContent={
+                <Flex direction={{ default: 'column' }} spaceItems={{ default: 'spaceItemsMd' }}>
+                    <FlexItem>
+                        <p>
+                            The status of your last requested job from the{' '}
+                            <strong>active job queue</strong>. An <strong>active job queue</strong>{' '}
+                            includes any requested job with the status of <strong>preparing</strong>{' '}
+                            or <strong>waiting</strong> until completed.
+                        </p>
+                    </FlexItem>
+                    <FlexItem>
+                        <p>
+                            <strong>Preparing:</strong>
+                        </p>
+                        <p>Your last requested job is still being processed.</p>
+                    </FlexItem>
+                    <FlexItem>
+                        <p>
+                            <strong>Waiting:</strong>
+                        </p>
+                        <p>
+                            Your last requested job is in the queue and waiting to be processed
+                            since other requested jobs are being processed.
+                        </p>
+                    </FlexItem>
+                </Flex>
+            }
+        >
+            My active job status
+        </HelpIconTh>
+    );
+}
+
+export default MyActiveJobStatusTh;
