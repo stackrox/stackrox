d22e61fc349129134ef015f0a65c6b1cce689d2c
Mark Pedrotti <pedrottimark@gmail.com>
2024-09-24 12:03:57 -0400

    fix(ui): Add no-anchor-href-docs-string generic lint rule (#12775)

--- ui/apps/platform/eslint-plugins/pluginGeneric.js
+++ ui/apps/platform/eslint-plugins/pluginGeneric.js
@@ -4,7 +4,7 @@ const rules = {
     // ESLint naming convention for positive rules:
     // If your rule is enforcing the inclusion of something, use a short name without a special prefix.
 
-    'ExternalLink-a': {
+    'ExternalLink-anchor': {
         // Require ExternalLink with anchor element as child for consistent presentation of external links.
         meta: {
             type: 'problem',
@@ -51,16 +51,16 @@ const rules = {
                     if (node.name?.name === 'Link') {
                         if (
                             node.attributes.some(
-                                (nodeAttribute) =>
-                                    nodeAttribute.name?.name === 'target' &&
-                                    nodeAttribute.value?.value === '_blank'
+                                (attribute) =>
+                                    attribute.name?.name === 'target' &&
+                                    attribute.value?.value === '_blank'
                             )
                         ) {
                             if (
                                 !node.attributes.some(
-                                    (nodeAttribute) =>
-                                        nodeAttribute.name?.name === 'rel' &&
-                                        nodeAttribute.value?.value === 'noopener noreferrer'
+                                    (attribute) =>
+                                        attribute.name?.name === 'rel' &&
+                                        attribute.value?.value === 'noopener noreferrer'
                                 )
                             ) {
                                 context.report({
@@ -75,7 +75,7 @@ const rules = {
             };
         },
     },
-    'a-target-rel': {
+    'anchor-target-rel': {
         // Require props for consistent behavior and security of external links.
         meta: {
             type: 'problem',
@@ -91,14 +91,14 @@ const rules = {
                     if (node.name?.name === 'a') {
                         if (
                             !node.attributes.some(
-                                (nodeAttribute) =>
-                                    nodeAttribute.name?.name === 'target' &&
-                                    nodeAttribute.value?.value === '_blank'
+                                (attribute) =>
+                                    attribute.name?.name === 'target' &&
+                                    attribute.value?.value === '_blank'
                             ) ||
                             !node.attributes.some(
-                                (nodeAttribute) =>
-                                    nodeAttribute.name?.name === 'rel' &&
-                                    nodeAttribute.value?.value === 'noopener noreferrer'
+                                (attribute) =>
+                                    attribute.name?.name === 'rel' &&
+                                    attribute.value?.value === 'noopener noreferrer'
                             )
                         ) {
                             context.report({
@@ -116,6 +116,47 @@ const rules = {
     // ESLint naming convention for negative rules.
     // If your rule only disallows something, prefix it with no.
     // However, we can write forbid instead of disallow as the verb in description and message.
+
+    'no-anchor-href-docs-string': {
+        // Full path string lacks what getVersionedDocs function provides:
+        // Include version number so doc page corresponds to product version.
+        // Encapsulate openshift versus redhat in origin of path.
+        meta: {
+            type: 'problem',
+            docs: {
+                description:
+                    'Replace string with getVersionedDocs function call in href prop of anchor element for product docs',
+            },
+            schema: [],
+        },
+        create(context) {
+            const isDocs = (href) =>
+                href.startsWith('https://docs.openshift.com/acs/') ||
+                href.startsWith(
+                    'https://docs.redhat.com/en/documentation/red_hat_advanced_cluster_security_for_kubernetes/'
+                );
+            return {
+                JSXOpeningElement(node) {
+                    if (node.name?.name === 'a') {
+                        if (
+                            node.attributes.some(
+                                (attribute) =>
+                                    attribute.name?.name === 'href' &&
+                                    typeof attribute.value?.value === 'string' &&
+                                    isDocs(attribute.value.value)
+                            )
+                        ) {
+                            context.report({
+                                node,
+                                message:
+                                    'Replace full path string with getVersionedDocs function call in href prop of anchor element for product docs',
+                            });
+                        }
+                    }
+                },
+            };
+        },
+    },
 };
 
 const pluginKey = 'generic'; // key of pluginGeneric in eslint.config.js file
