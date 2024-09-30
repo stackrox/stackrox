3da0fbd6c3cc954f6b5b9efc90c54d981f0d0935
JoukoVirtanen <jouko@stackrox.com>
2024-09-29 19:37:16 -0700

    Simplified unused mocks

--- sensor/common/networkflow/service/service_impl_test.go
+++ sensor/common/networkflow/service/service_impl_test.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/stackrox/rox/generated/internalapi/sensor"
 	"github.com/stackrox/rox/pkg/concurrency"
-	mocksNetworkflowManager "github.com/stackrox/rox/sensor/common/networkflow/manager/mocks"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
@@ -20,7 +19,6 @@ func TestNetworkflowService(t *testing.T) {
 
 type networkflowServiceSuite struct {
 	suite.Suite
-	mockNetworkflowManager *mocksNetworkflowManager.MockManager
 	collectorConfigProtoStream *concurrency.ValueStream[*sensor.CollectorConfig]
 }
 
@@ -38,8 +36,7 @@ func (m *MockStream) Send(msg *sensor.MsgToCollector) error {
 }
 
 func (m *MockStream) SendMsg(msg interface{}) error {
-    args := m.Called(msg)
-    return args.Error(0)
+	return nil
 }
 
 func (m *MockStream) SendHeader(md metadata.MD) error {
@@ -47,32 +44,22 @@ func (m *MockStream) SendHeader(md metadata.MD) error {
 }
 
 func (m *MockStream) SetHeader(md metadata.MD) error {
-    args := m.Called(md)
-    return args.Error(0)
+	return nil
 }
 
 func (m *MockStream) SetTrailer(md metadata.MD) {
-    m.Called(md)
+	return
 }
 
 func (m *MockStream) Recv() (*sensor.NetworkConnectionInfoMessage, error) {
-	args := m.Called()
-	if msg, ok := args.Get(0).(*sensor.NetworkConnectionInfoMessage); ok {
-		return msg, nil
-	}
-	return nil, args.Error(1)
+	return nil, nil
 }
 
 func (m *MockStream) RecvMsg(_ interface{}) error {
-	args := m.Called()
-	return args.Error(0)
+	return nil
 }
 
 func (m *MockStream) Context() context.Context {
-	args := m.Called()
-	if ctx, ok := args.Get(0).(context.Context); ok {
-		return ctx
-	}
 	return context.Background()
 }
 
@@ -81,28 +68,28 @@ func (s *networkflowServiceSuite) CollectorConfigValueStream() concurrency.ReadO
 }
 
 func (s *networkflowServiceSuite) TestSendCollectorConfig() {
-    collectorConfig := &sensor.CollectorConfig{
-        NetworkConnectionConfig: &sensor.NetworkConnectionConfig{
-            EnableExternalIps: true,
-        },
-    }
-
-    collectorConfigIterator := s.CollectorConfigValueStream().Iterator(false)
-
-    s.collectorConfigProtoStream.Push(collectorConfig)
-    collectorConfigIterator = collectorConfigIterator.TryNext()
-
-    mockStream := new(MockStream)
-
-    mockStream.On("Send", mock.AnythingOfType("*sensor.MsgToCollector")).Return(nil).Once()
-
-    service := NewService(s.mockNetworkflowManager)
-
-    err := service.SendCollectorConfig(mockStream, collectorConfigIterator)
-
-    require.NoError(s.T(), err)
-
-    mockStream.AssertExpectations(s.T())
+	collectorConfig := &sensor.CollectorConfig{
+		NetworkConnectionConfig: &sensor.NetworkConnectionConfig{
+			EnableExternalIps: true,
+		},
+	}
+	
+	collectorConfigIterator := s.CollectorConfigValueStream().Iterator(false)
+	
+	s.collectorConfigProtoStream.Push(collectorConfig)
+	collectorConfigIterator = collectorConfigIterator.TryNext()
+	
+	mockStream := new(MockStream)
+	
+	mockStream.On("Send", mock.AnythingOfType("*sensor.MsgToCollector")).Return(nil).Once()
+	
+	service := &serviceImpl{}
+	
+	err := service.SendCollectorConfig(mockStream, collectorConfigIterator)
+	
+	require.NoError(s.T(), err)
+	
+	mockStream.AssertExpectations(s.T())
 }
 
 func (s *networkflowServiceSuite) TestSendCollectorConfigNoConfig() {
@@ -111,7 +98,7 @@ func (s *networkflowServiceSuite) TestSendCollectorConfigNoConfig() {
 
     mockStream := new(MockStream)
 
-    service := NewService(s.mockNetworkflowManager)
+    service := &serviceImpl{}
 
     err := service.SendCollectorConfig(mockStream, collectorConfigIterator)
 
@@ -121,27 +108,27 @@ func (s *networkflowServiceSuite) TestSendCollectorConfigNoConfig() {
 }
 
 func (s *networkflowServiceSuite) TestSendCollectorConfigErr() {
-    collectorConfig := &sensor.CollectorConfig{
-        NetworkConnectionConfig: &sensor.NetworkConnectionConfig{
-            EnableExternalIps: true,
-        },
-    }
+	collectorConfig := &sensor.CollectorConfig{
+		NetworkConnectionConfig: &sensor.NetworkConnectionConfig{
+			EnableExternalIps: true,
+		},
+	}
 
-    collectorConfigIterator := s.CollectorConfigValueStream().Iterator(false)
-
-    s.collectorConfigProtoStream.Push(collectorConfig)
-    collectorConfigIterator = collectorConfigIterator.TryNext()
-
-    mockStream := new(MockStream)
-
-    sendError := errors.New("failed to send collector config")
-    mockStream.On("Send", mock.AnythingOfType("*sensor.MsgToCollector")).Return(sendError).Once()
-
-    service := NewService(s.mockNetworkflowManager)
-
-    err := service.SendCollectorConfig(mockStream, collectorConfigIterator)
-
-    require.Error(s.T(), err)
-
-    mockStream.AssertExpectations(s.T())
+	collectorConfigIterator := s.CollectorConfigValueStream().Iterator(false)
+	
+	s.collectorConfigProtoStream.Push(collectorConfig)
+	collectorConfigIterator = collectorConfigIterator.TryNext()
+	
+	mockStream := new(MockStream)
+	
+	sendError := errors.New("failed to send collector config")
+	mockStream.On("Send", mock.AnythingOfType("*sensor.MsgToCollector")).Return(sendError).Once()
+	
+	service := &serviceImpl{}
+	
+	err := service.SendCollectorConfig(mockStream, collectorConfigIterator)
+	
+	require.Error(s.T(), err)
+	
+	mockStream.AssertExpectations(s.T())
 }
