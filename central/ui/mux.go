8e4375e21ba106fadacd361a7b7643f2b9ebd97b
Yann Brillouet <91869377+rhybrillou@users.noreply.github.com>
2024-10-01 10:32:19 +0200

    ROX-21381: Add security headers to UI mux (#12745)

--- central/ui/mux.go
+++ central/ui/mux.go
@@ -16,8 +16,31 @@ func Mux() http.Handler {
 		http.ServeFile(w, r, "/ui/service-worker.js")
 	})
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("X-Frame-Options", "sameorigin")
-		w.Header().Set("Content-Security-Policy", "frame-ancestors 'self'")
+		headers := map[string]string{
+			// Avoid page contents from being cached in either browsers or proxies.
+			// This should not impact the caching of static content delivered from
+			// /static routes.
+			"Cache-control": "no-store, no-cache",
+			// Used in pair with X-Frame-Options for the frame-ancestors part.
+			// Prevent the UI from being displayed in frames from foreign domains
+			// and thus avoid clickJacking.
+			"Content-Security-Policy": "frame-ancestors 'self'",
+			// Force use of HTTPS and prevent future uses of unencrypted HTTP
+			// as protection against Man in the middle attacks.
+			"Strict-Transport-Security": "max-age=31536000; includeSubDomains",
+			// Tell browsers to follow MIME types advertised in Content-Type headers
+			// and not guess them (protect against cross-site scripting and clickJacking).
+			"X-Content-Type-Options": "nosniff",
+			// Used in pair with Content-Security-Policy (frame-ancestors).
+			// Prevent the UI from being displayed in frames from foreign domains
+			// and thus avoid clickJacking.
+			"X-Frame-Options": "sameorigin",
+			// Protect old browsers against cross-site-scripting attacks.
+			"X-XSS-Protection": "1; mode=block",
+		}
+		for key, value := range headers {
+			w.Header().Set(key, value)
+		}
 		http.ServeFile(w, r, "/ui/index.html")
 	})
 	return mux
