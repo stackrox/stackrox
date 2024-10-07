b8974c7233b9e5ae51beba5dce74f54f276a76b2
Jan Schnathmeier <jschnath@redhat.com>
2024-10-07 23:52:01 +0200

    Run only a single test

--- .openshift-ci/get_compatibility_test_tuples.py
+++ .openshift-ci/get_compatibility_test_tuples.py
@@ -86,6 +86,8 @@ def get_compatibility_test_tuples():
         for support_exception in support_exceptions
         if support_exception not in test_tuples
     )
+    # DELETE THE LINE BELOW, removes all but the first test tuple
+    test_tuples = test_tuples[:1]
     return test_tuples
 
 
