a63b2e672f7beedc60e8b428239ec2ceaf5d8212
Brad Rogers <61400697+bradr5@users.noreply.github.com>
2024-10-08 15:07:15 -0500

    ROX-25524: Add enable/disable policy option on create (#12804)

--- ui/apps/platform/src/Containers/Policies/policies.constants.ts
+++ ui/apps/platform/src/Containers/Policies/policies.constants.ts
@@ -5,6 +5,5 @@ export const POLICY_DEFINITION_LIFECYCLE_ID = 'policy-definition-lifecycle';
 export const POLICY_DEFINITION_RULES_ID = 'policy-definition-rules';
 export const POLICY_BEHAVIOR_ID = 'policy-behavior';
 export const POLICY_BEHAVIOR_SCOPE_ID = 'policy-behavior-scope';
-export const POLICY_BEHAVIOR_ENFORCEMENT_ID = 'policy-behavior-enforcement';
-export const POLICY_BEHAVIOR_NOTIFIERS_ID = 'policy-behavior-notifiers';
+export const POLICY_BEHAVIOR_ACTIONS_ID = 'policy-behavior-actions';
 export const POLICY_REVIEW_ID = 'policy-review';
