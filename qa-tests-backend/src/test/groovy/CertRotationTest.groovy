d00891ca5c6c1e4c0ae9c27af99f348b395dd23e
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2024-10-11 18:09:51 +0200

    Update text in assertion

--- qa-tests-backend/src/test/groovy/CertRotationTest.groovy
+++ qa-tests-backend/src/test/groovy/CertRotationTest.groovy
@@ -246,7 +246,7 @@ class CertRotationTest extends BaseSpecification {
             SensorUpgradeService.triggerCertRotation(cluster.getId())
         } catch (StatusRuntimeException exc) {
             caughtException = true
-            assert exc.status.description.contains("cluster is Operator-managed and does not support auto upgrades; use the Operator for upgrades")
+            assert exc.status.description.contains("Helm controls the secured cluster version.")
         }
         assert caughtException
     }
