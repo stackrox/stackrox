6e6357bfb92ac0178eb2d77af1c726804939a50f
Charmik Sheth <101146970+charmik-redhat@users.noreply.github.com>
2024-10-14 10:15:13 -0700

    Rebase and update query timeout for images view

--- central/views/images/view_impl.go
+++ central/views/images/view_impl.go
@@ -5,11 +5,17 @@ import (
 
 	"github.com/stackrox/rox/central/views/common"
 	v1 "github.com/stackrox/rox/generated/api/v1"
+	"github.com/stackrox/rox/pkg/contextutil"
+	"github.com/stackrox/rox/pkg/env"
 	"github.com/stackrox/rox/pkg/postgres"
 	"github.com/stackrox/rox/pkg/postgres/walker"
 	"github.com/stackrox/rox/pkg/sac/resources"
 	"github.com/stackrox/rox/pkg/search"
 	pgSearch "github.com/stackrox/rox/pkg/search/postgres"
+)
+
+var (
+	queryTimeout = env.PostgresVMStatementTimeout.DurationSetting()
 )
 
 type imageCoreViewImpl struct {
@@ -29,8 +35,11 @@ func (v *imageCoreViewImpl) Get(ctx context.Context, query *v1.Query) ([]ImageCo
 	}
 	query = withSelectQuery(query)
 
+	queryCtx, cancel := contextutil.ContextWithTimeoutIfNotExists(ctx, queryTimeout)
+	defer cancel()
+
 	var results []*imageResponse
-	results, err = pgSearch.RunSelectRequestForSchema[imageResponse](ctx, v.db, v.schema, query)
+	results, err = pgSearch.RunSelectRequestForSchema[imageResponse](queryCtx, v.db, v.schema, query)
 	if err != nil {
 		return nil, err
 	}
