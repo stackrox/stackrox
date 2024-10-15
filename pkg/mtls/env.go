6c85ff5611cf841135d6ddd3573ec323716ab884
Moritz Clasmeier <mclasmeier@redhat.com>
2024-10-15 12:50:47 +0200

    Export MTLS path settings

--- pkg/mtls/env.go
+++ pkg/mtls/env.go
@@ -16,29 +16,34 @@ const (
 )
 
 var (
-	caFilePathSetting    = env.RegisterSetting(CAFileEnvName, env.WithDefault(defaultCACertFilePath))
-	caKeyFilePathSetting = env.RegisterSetting(CAKeyFileEnvName, env.WithDefault(defaultCAKeyFilePath))
-	certFilePathSetting  = env.RegisterSetting(CertFilePathEnvName, env.WithDefault(defaultCertFilePath))
-	keyFilePathSetting   = env.RegisterSetting(KeyFileEnvName, env.WithDefault(defaultKeyFilePath))
-	crsFilePathSetting   = env.RegisterSetting(CrsFilePathEnvName, env.WithDefault(defaultCrsFilePath))
+	// CAFilePathSetting allows configuring the CA certificate from the environment.
+	CAFilePathSetting = env.RegisterSetting(CAFileEnvName, env.WithDefault(defaultCACertFilePath))
+	// CAKeyFilePathSetting allows configuring the CA key from the environment.
+	CAKeyFilePathSetting = env.RegisterSetting(CAKeyFileEnvName, env.WithDefault(defaultCAKeyFilePath))
+	// CertFilePathSetting allows configuring the MTLS certificate from the environment.
+	CertFilePathSetting = env.RegisterSetting(CertFilePathEnvName, env.WithDefault(defaultCertFilePath))
+	// KeyFilePathSetting allows configuring the MTLS key from the environment.
+	KeyFilePathSetting = env.RegisterSetting(KeyFileEnvName, env.WithDefault(defaultKeyFilePath))
+	// CrsFilePathSetting allows configuring the CRS from the environment.
+	CrsFilePathSetting = env.RegisterSetting(CrsFilePathEnvName, env.WithDefault(defaultCrsFilePath))
 )
 
 // CAFilePath returns the path where the CA certificate is stored.
 func CAFilePath() string {
-	return caFilePathSetting.Setting()
+	return CAFilePathSetting.Setting()
 }
 
 // CertFilePath returns the path where the certificate is stored.
 func CertFilePath() string {
-	return certFilePathSetting.Setting()
+	return CertFilePathSetting.Setting()
 }
 
 // KeyFilePath returns the path where the key is stored.
 func KeyFilePath() string {
-	return keyFilePathSetting.Setting()
+	return KeyFilePathSetting.Setting()
 }
 
 // CrsFilePath returns the path where the CRS certificate is stored.
 func CrsFilePath() string {
-	return crsFilePathSetting.Setting()
+	return CrsFilePathSetting.Setting()
 }
