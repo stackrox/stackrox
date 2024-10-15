4900ae43c405b35297f97c2c0ff357b86dbc43ea
Surabhi Kumar <surakuma@redhat.com>
2024-10-15 11:52:20 -0700

    Fixed comments

--- pkg/booleanpolicy/violationmessages/printer/vuln.go
+++ pkg/booleanpolicy/violationmessages/printer/vuln.go
@@ -26,12 +26,39 @@ const (
     {{- if .ContainerName }} in container '{{.ContainerName}}'{{end}}
     {{- if .FixedBy}}, resolved by version {{.FixedBy}}{{end}}`
 
+	//CVE-2014-0160 (NVD CVSS 8) (severity Unknown) found in component 'heartbleed' (version 1.2) in container 'nginx'
 	nvdcveTemplate = `
     {{- if .FixedBy}}Fixable {{end}}{{.CVE}}{{if .NVDCVSS}} (NVD CVSS {{.NVDCVSS}}){{end}}{{if .Severity}} (severity {{.Severity}}){{end}} found
     {{- if .Component}} in component '{{.Component}}' (version {{.ComponentVersion}}){{end}}
     {{- if .ContainerName }} in container '{{.ContainerName}}'{{end}}
     {{- if .FixedBy}}, resolved by version {{.FixedBy}}{{end}}`
 )
+
+func nvdCvePrinter(fieldMap map[string][]string) ([]string, error) {
+
+	type cveResultFields struct {
+		ContainerName    string
+		ImageName        string
+		CVE              string
+		NVDCVSS          string
+		Severity         string
+		FixedBy          string
+		Component        string
+		ComponentVersion string
+	}
+	r := cveResultFields{}
+
+	var err error
+	if r.CVE, err = getSingleValueFromFieldMap(search.CVE.String(), fieldMap); err != nil {
+		return nil, err
+	}
+	r.ContainerName = maybeGetSingleValueFromFieldMap(augmentedobjs.ContainerNameCustomTag, fieldMap)
+	r.NVDCVSS = maybeGetSingleValueFromFieldMap(search.NVDCVSS.String(), fieldMap)
+	r.Severity = strings.Title(strings.TrimSuffix(strings.ToLower(maybeGetSingleValueFromFieldMap(search.Severity.String(), fieldMap)), "_vulnerability_severity"))
+	r.FixedBy = maybeGetSingleValueFromFieldMap(search.FixedBy.String(), fieldMap)
+	r.Component, r.ComponentVersion = getComponentAndVersion(fieldMap)
+	return executeTemplate(nvdcveTemplate, r)
+}
 
 func cvePrinter(fieldMap map[string][]string) ([]string, error) {
 
@@ -57,32 +84,6 @@ func cvePrinter(fieldMap map[string][]string) ([]string, error) {
 	r.FixedBy = maybeGetSingleValueFromFieldMap(search.FixedBy.String(), fieldMap)
 	r.Component, r.ComponentVersion = getComponentAndVersion(fieldMap)
 	return executeTemplate(cveTemplate, r)
-}
-
-func nvdcvePrinter(fieldMap map[string][]string) ([]string, error) {
-
-	type cveResultFields struct {
-		ContainerName    string
-		ImageName        string
-		CVE              string
-		NVDCVSS          string
-		Severity         string
-		FixedBy          string
-		Component        string
-		ComponentVersion string
-	}
-	r := cveResultFields{}
-
-	var err error
-	if r.CVE, err = getSingleValueFromFieldMap(search.CVE.String(), fieldMap); err != nil {
-		return nil, err
-	}
-	r.ContainerName = maybeGetSingleValueFromFieldMap(augmentedobjs.ContainerNameCustomTag, fieldMap)
-	r.NVDCVSS = maybeGetSingleValueFromFieldMap(search.NVDCVSS.String(), fieldMap)
-	r.Severity = strings.Title(strings.TrimSuffix(strings.ToLower(maybeGetSingleValueFromFieldMap(search.Severity.String(), fieldMap)), "_vulnerability_severity"))
-	r.FixedBy = maybeGetSingleValueFromFieldMap(search.FixedBy.String(), fieldMap)
-	r.Component, r.ComponentVersion = getComponentAndVersion(fieldMap)
-	return executeTemplate(nvdcveTemplate, r)
 }
 
 const (
