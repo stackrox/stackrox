36dff2cf68ef1ca08e471f0734fad17d0232b10d
Surabhi Kumar <surakuma@redhat.com>
2024-10-16 08:48:14 -0700

    Added logging to ebug

--- central/alert/service/service_impl.go
+++ central/alert/service/service_impl.go
@@ -23,6 +23,7 @@ import (
 	"github.com/stackrox/rox/pkg/grpc/authz"
 	"github.com/stackrox/rox/pkg/grpc/authz/perrpc"
 	"github.com/stackrox/rox/pkg/grpc/authz/user"
+	"github.com/stackrox/rox/pkg/logging"
 	pkgNotifier "github.com/stackrox/rox/pkg/notifier"
 	"github.com/stackrox/rox/pkg/processbaseline"
 	"github.com/stackrox/rox/pkg/protoconv"
@@ -36,6 +37,7 @@ import (
 
 var (
 	alertSAC = sac.ForResource(resources.Alert)
+	logger   = logging.LoggerForModule()
 )
 
 const (
@@ -149,6 +151,7 @@ func (s *serviceImpl) ListAlerts(ctx context.Context, request *v1.ListAlertsRequ
 		return nil, err
 	}
 	alerts, err := s.dataStore.SearchListAlerts(ctx, q)
+	logger.Infof("debug alert query %s alerts %s", q, alerts)
 	if err != nil {
 		return nil, err
 	}
