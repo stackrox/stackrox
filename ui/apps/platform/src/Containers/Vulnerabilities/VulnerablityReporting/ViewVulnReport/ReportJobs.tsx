ecaec7d6e483ce6de1bfeeb81f1b382eafec0575
Saif Chaudhry <schaudhr@redhat.com>
2024-10-16 11:10:50 -0700

    fix(ui): Delete downloadable report

--- ui/apps/platform/src/Containers/Vulnerabilities/VulnerablityReporting/ViewVulnReport/ReportJobs.tsx
+++ ui/apps/platform/src/Containers/Vulnerabilities/VulnerablityReporting/ViewVulnReport/ReportJobs.tsx
@@ -50,6 +50,7 @@ import { TemplatePreviewArgs } from 'Components/EmailTemplate/EmailTemplateModal
 import NotifierConfigurationView from 'Components/NotifierConfiguration/NotifierConfigurationView';
 
 import { RunState, runStates } from 'types/reportJob';
+import { deleteDownloadableReport } from 'services/ReportsService';
 import EmailTemplatePreview from '../components/EmailTemplatePreview';
 import ReportParametersDetails from '../components/ReportParametersDetails';
 import ScheduleDetails from '../components/ScheduleDetails';
@@ -99,6 +100,7 @@ function ReportJobs({ reportId }: RunHistoryProps) {
         onDeleteDownload,
         deleteDownloadError,
     } = useDeleteDownloadModal({
+        deleteDownloadFunc: deleteDownloadableReport,
         onCompleted: fetchReportSnapshots,
     });
 
