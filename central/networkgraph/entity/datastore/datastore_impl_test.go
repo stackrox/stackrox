cde66f4cc404a00463842b2a2eaa73e06ee35212
Giles Hutton <ghutton@redhat.com>
2024-10-16 09:48:15 +0100

    ROX-24948: external entities API extension (#12689)
    - Switch to the postgres backend for implementing network-entities API.
    - New route to get external flows per deployment
    
    Co-authored-by: Olivier Valentin <ovalenti@redhat.com>
    Co-authored-by: David Shrewsberry <99685630+dashrews78@users.noreply.github.com>

--- central/networkgraph/entity/datastore/datastore_impl_test.go
+++ central/networkgraph/entity/datastore/datastore_impl_test.go
@@ -226,6 +226,19 @@ func (suite *NetworkEntityDataStoreTestSuite) TestNetworkEntities() {
 	suite.NoError(err)
 	suite.Len(entities, 3)
 
+	// Test get by query
+	query = search.NewQueryBuilder().AddStrings(search.ExternalSourceAddress, "192.0.2.0/29").ProtoQuery()
+	entities, err = suite.ds.GetEntityByQuery(suite.globalReadAccessCtx, query)
+	suite.NoError(err)
+	// Expect 192.0.2.0/29 and 192.0.2.0/30 - the latter is a subset of the former
+	suite.Len(entities, 2)
+
+	// Expect no matching CIDRs for this query
+	query = search.NewQueryBuilder().AddStrings(search.ExternalSourceAddress, "255.255.255.0/24").ProtoQuery()
+	entities, err = suite.ds.GetEntityByQuery(suite.globalReadAccessCtx, query)
+	suite.NoError(err)
+	suite.Len(entities, 0)
+
 	// Test Delete
 	for _, c := range cases {
 		c := c
