ccronca/stackrox_stackrox

roxctl passwords

scale/signatures/create-signature-integrations.sh
@@ -10,7 +10,7 @@ source "$TEST_ROOT/tests/e2e/lib.sh"
 # Wait for central API to be reachable.
 wait_for_api
 
-require_environment "ROX_PASSWORD"
+require_environment "ROX_ADMIN_PASSWORD"
 require_environment "API_ENDPOINT"
 
 declare -a integrations=(
@@ -22,7 +22,7 @@ declare -a integrations=(
 for integrationJSON in "${integrations[@]}"
 do
   tmpOutput=$(mktemp)
-  status=$(curl -k --config <(escape user "admin:${ROX_PASSWORD}") -X POST \
+  status=$(curl -k --config <(escape user "admin:${ROX_ADMIN_PASSWORD}") -X POST \
     -d "${integrationJSON}" \
     -o "$tmpOutput" \
     -w "%{http_code}\n" \

scripts/lib.sh
@@ -32,7 +32,7 @@ escape() {
 roxcurl() {
     local url="$1"
     shift
-    curl -sk --config <(escape user "admin:${ROX_PASSWORD}") -k "https://${API_ENDPOINT}${url}" "$@"
+    curl -sk --config <(escape user "admin:${ROX_ADMIN_PASSWORD}") -k "https://${API_ENDPOINT}${url}" "$@"
 }
 
 is_release_version() {

tests/roxctl/slim-collector.sh
@@ -2,10 +2,10 @@
 
 set -uo pipefail
 
-# This test script requires API_ENDPOINT and ROX_PASSWORD to be set in the environment.
+# This test script requires API_ENDPOINT and ROX_ADMIN_PASSWORD to be set in the environment.
 
 [ -n "$API_ENDPOINT" ]
-[ -n "$ROX_PASSWORD" ]
+[ -n "$ROX_ADMIN_PASSWORD" ]
 
 echo "Using API_ENDPOINT $API_ENDPOINT"
 
@@ -28,7 +28,7 @@ curl_central() {
   url="$1"
   shift
   [[ -n "${url}" ]] || die "No URL specified"
-  curl --retry 5 --retry-connrefused -Sskf --config <(escape user "admin:${ROX_PASSWORD}") \
+  curl --retry 5 --retry-connrefused -Sskf --config <(escape user "admin:${ROX_ADMIN_PASSWORD}") \
     "https://${API_ENDPOINT}/${url}" "$@"
 }
 

tests/roxctl/token-file.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
 
-# This test script requires API_ENDPOINT and ROX_PASSWORD to be set in the environment.
+# This test script requires API_ENDPOINT and ROX_ADMIN_PASSWORD to be set in the environment.
 
 [ -n "$API_ENDPOINT" ]
-[ -n "$ROX_PASSWORD" ]
+[ -n "$ROX_ADMIN_PASSWORD" ]
 
 echo "Using API_ENDPOINT $API_ENDPOINT"
 
@@ -20,7 +20,7 @@ escape() {
 # Retrieve API token
 TOKEN_FILE=$(mktemp)
 curl -k -f \
-  --config <(escape user "admin:$ROX_PASSWORD") \
+  --config <(escape user "admin:$ROX_ADMIN_PASSWORD") \
   -d '{"name": "test", "role": "Admin"}' \
   --retry 5 \
   --retry-connrefused \
@@ -80,7 +80,6 @@ test_roxctl_cmd() {
 
   # Verify that a password on the command line has precedence over token in the environment
   if OUTPUT=$(ROX_API_TOKEN="invalid-token" roxctl --insecure-skip-tls-verify --insecure -e "$API_ENDPOINT" \
-    --password "$ROX_PASSWORD" \
     "$@" \
     2>&1); then
       echo "[OK] --password has precedence over ROX_API_TOKEN environment variable"

tests/upgrade/lib.sh
@@ -15,7 +15,7 @@ wait_for_central_reconciliation() {
     local success=0
     for i in $(seq 1 90); do
         local numDeployments
-        numDeployments="$(curl -sSk --config <(escape user "admin:$ROX_PASSWORD") "https://$API_ENDPOINT/v1/summary/counts" | jq '.numDeployments' -r)"
+        numDeployments="$(curl -sSk --config <(escape user "admin:$ROX_ADMIN_PASSWORD") "https://$API_ENDPOINT/v1/summary/counts" | jq '.numDeployments' -r)"
         echo "Try number ${i}. Number of deployments in Central: $numDeployments"
         [[ -n "$numDeployments" ]]
         if [[ "$numDeployments" -lt 100 ]]; then
@@ -81,7 +81,7 @@ validate_upgrade() {
 function roxcurl() {
   local url="$1"
   shift
-  curl --config <(escape user "admin:${ROX_PASSWORD}") -k "https://${API_ENDPOINT}${url}" "$@"
+  curl --config <(escape user "admin:${ROX_ADMIN_PASSWORD}") -k "https://${API_ENDPOINT}${url}" "$@"
 }
 
 deploy_earlier_postgres_central() {
@@ -93,11 +93,11 @@ deploy_earlier_postgres_central() {
     PATH="bin/$TEST_HOST_PLATFORM:$PATH" roxctl version
 
     # Let's try helm
-    ROX_PASSWORD="$(tr -dc _A-Z-a-z-0-9 < /dev/urandom | head -c12 || true)"
+    ROX_ADMIN_PASSWORD="$(tr -dc _A-Z-a-z-0-9 < /dev/urandom | head -c12 || true)"
     PATH="bin/$TEST_HOST_PLATFORM:$PATH" roxctl helm output central-services --image-defaults opensource --output-dir /tmp/early-stackrox-central-services-chart
 
     helm install -n stackrox --create-namespace stackrox-central-services /tmp/early-stackrox-central-services-chart \
-         --set central.adminPassword.value="${ROX_PASSWORD}" \
+         --set central.adminPassword.value="${ROX_ADMIN_PASSWORD}" \
          --set central.db.enabled=true \
          --set central.persistence.none=true \
          --set central.exposure.loadBalancer.enabled=true \
@@ -113,7 +113,7 @@ deploy_earlier_postgres_central() {
 
     ROX_USERNAME="admin"
     ci_export "ROX_USERNAME" "$ROX_USERNAME"
-    ci_export "ROX_PASSWORD" "$ROX_PASSWORD"
+    ci_export "ROX_ADMIN_PASSWORD" "$ROX_ADMIN_PASSWORD"
 }
 
 restore_4_1_backup() {
@@ -126,7 +126,7 @@ force_rollback() {
     info "Forcing a rollback to $FORCE_ROLLBACK_VERSION"
 
     local upgradeStatus
-    upgradeStatus=$(curl -sSk -X GET --config <(escape user "admin:$ROX_PASSWORD") https://"${API_ENDPOINT}"/v1/centralhealth/upgradestatus)
+    upgradeStatus=$(curl -sSk -X GET --config <(escape user "admin:$ROX_ADMIN_PASSWORD") https://"${API_ENDPOINT}"/v1/centralhealth/upgradestatus)
     echo "upgrade status: ${upgradeStatus}"
     test_equals_non_silent "$(echo "$upgradeStatus" | jq '.upgradeStatus.version' -r)" "$(make --quiet --no-print-directory tag)"
     test_equals_non_silent "$(echo "$upgradeStatus" | jq '.upgradeStatus.forceRollbackTo' -r)" "$FORCE_ROLLBACK_VERSION"
@@ -175,7 +175,7 @@ test_sensor_bundle() {
     info "Testing the sensor bundle"
 
     rm -rf sensor-remote
-    "$TEST_ROOT/bin/${TEST_HOST_PLATFORM}/roxctl" -e "$API_ENDPOINT" -p "$ROX_PASSWORD" sensor get-bundle remote
+    "$TEST_ROOT/bin/${TEST_HOST_PLATFORM}/roxctl" -e "$API_ENDPOINT" sensor get-bundle remote
     [[ -d sensor-remote ]]
 
     ./sensor-remote/sensor.sh
@@ -196,7 +196,7 @@ test_upgrader() {
     info "Creating a 'sensor-remote-new' cluster"
 
     rm -rf sensor-remote-new
-    "$TEST_ROOT/bin/${TEST_HOST_PLATFORM}/roxctl" -e "$API_ENDPOINT" -p "$ROX_PASSWORD" sensor generate k8s \
+    "$TEST_ROOT/bin/${TEST_HOST_PLATFORM}/roxctl" -e "$API_ENDPOINT" sensor generate k8s \
         --name remote-new \
         --create-admission-controller
 

ui/apps/platform/scripts/cypress.sh
@@ -3,7 +3,7 @@
 # Opens cypress with environment variables for feature flags and auth
 api_endpoint="${UI_BASE_URL:-https://localhost:8000}"
 
-if [[ -z "$ROX_USERNAME" || -z "$ROX_PASSWORD" ]]; then
+if [[ -z "$ROX_USERNAME" || -z "$ROX_ADMIN_PASSWORD" ]]; then
   # basic auth creds weren't set (e.g. by CI), assume local k8s deployment
   # shellcheck source=../../../../scripts/k8s/export-basic-auth-creds.sh
   source ../../../scripts/k8s/export-basic-auth-creds.sh ../../../deploy/k8s
@@ -13,8 +13,8 @@ escape() {
   echo -n "$1 = \"${2//[\"\\]/\\&}\""
 }
 
-if [[ -n "$ROX_PASSWORD" ]]; then
-  readarray -t arr < <(curl -sk --config <(escape user "admin:$ROX_PASSWORD") ${api_endpoint}/v1/featureflags | jq -cr '.featureFlags[] | {name: .envVar, enabled: .enabled}')
+if [[ -n "$ROX_ADMIN_PASSWORD" ]]; then
+  readarray -t arr < <(curl -sk --config <(escape user "admin:$ROX_ADMIN_PASSWORD") ${api_endpoint}/v1/featureflags | jq -cr '.featureFlags[] | {name: .envVar, enabled: .enabled}')
   for i in "${arr[@]}"; do
     name=$(echo $i | jq -rc .name)
     val=$(echo $i | jq -rc .enabled)

stackrox/stackrox

scale/signatures/create-signature-integrations.sh
@@ -10,7 +10,7 @@ source "$TEST_ROOT/tests/e2e/lib.sh"
 # Wait for central API to be reachable.
 wait_for_api
 
-require_environment "ROX_PASSWORD"
+require_environment "ROX_ADMIN_PASSWORD"
 require_environment "API_ENDPOINT"
 
 declare -a integrations=(
@@ -22,7 +22,7 @@ declare -a integrations=(
 for integrationJSON in "${integrations[@]}"
 do
   tmpOutput=$(mktemp)
-  status=$(curl -k --config <(curl_cfg user "admin:${ROX_PASSWORD}") -X POST \
+  status=$(curl -k --config <(curl_cfg user "admin:${ROX_ADMIN_PASSWORD}") -X POST \
     -d "${integrationJSON}" \
     -o "$tmpOutput" \
     -w "%{http_code}\n" \

scripts/lib.sh
@@ -32,7 +32,7 @@ curl_cfg() { # Use built-in echo to not expose $2 in the process list.
 roxcurl() {
     local url="$1"
     shift
-    curl -sk --config <(curl_cfg user "admin:${ROX_PASSWORD}") -k "https://${API_ENDPOINT}${url}" "$@"
+    curl -sk --config <(curl_cfg user "admin:${ROX_ADMIN_PASSWORD}") -k "https://${API_ENDPOINT}${url}" "$@"
 }
 
 is_release_version() {

tests/roxctl/slim-collector.sh
@@ -2,10 +2,10 @@
 
 set -uo pipefail
 
-# This test script requires API_ENDPOINT and ROX_PASSWORD to be set in the environment.
+# This test script requires API_ENDPOINT and ROX_ADMIN_PASSWORD to be set in the environment.
 
 [ -n "$API_ENDPOINT" ]
-[ -n "$ROX_PASSWORD" ]
+[ -n "$ROX_ADMIN_PASSWORD" ]
 
 echo "Using API_ENDPOINT $API_ENDPOINT"
 
@@ -28,7 +28,7 @@ curl_central() {
   url="$1"
   shift
   [[ -n "${url}" ]] || die "No URL specified"
-  curl --retry 5 --retry-connrefused -Sskf --config <(curl_cfg user "admin:${ROX_PASSWORD}") \
+  curl --retry 5 --retry-connrefused -Sskf --config <(curl_cfg user "admin:${ROX_ADMIN_PASSWORD}") \
     "https://${API_ENDPOINT}/${url}" "$@"
 }
 

tests/roxctl/token-file.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
 
-# This test script requires API_ENDPOINT and ROX_PASSWORD to be set in the environment.
+# This test script requires API_ENDPOINT and ROX_ADMIN_PASSWORD to be set in the environment.
 
 [ -n "$API_ENDPOINT" ]
-[ -n "$ROX_PASSWORD" ]
+[ -n "$ROX_ADMIN_PASSWORD" ]
 
 echo "Using API_ENDPOINT $API_ENDPOINT"
 
@@ -20,7 +20,7 @@ curl_cfg() { # Use built-in echo to not expose $2 in the process list.
 # Retrieve API token
 TOKEN_FILE=$(mktemp)
 curl -k -f \
-  --config <(curl_cfg user "admin:$ROX_PASSWORD") \
+  --config <(curl_cfg user "admin:$ROX_ADMIN_PASSWORD") \
   -d '{"name": "test", "role": "Admin"}' \
   --retry 5 \
   --retry-connrefused \
@@ -98,7 +98,6 @@ test_roxctl_cmd() {
 
   # Verify that a password on the command line has precedence over token in the environment
   if OUTPUT=$(ROX_API_TOKEN="invalid-token" roxctl --insecure-skip-tls-verify --insecure -e "$API_ENDPOINT" \
-    --password "$ROX_PASSWORD" \
     "$@" \
     2>&1); then
       echo "[OK] --password has precedence over ROX_API_TOKEN environment variable"

tests/upgrade/lib.sh
@@ -15,7 +15,7 @@ wait_for_central_reconciliation() {
     local success=0
     for i in $(seq 1 90); do
         local numDeployments
-        numDeployments="$(curl -sSk --config <(curl_cfg user "admin:$ROX_PASSWORD") "https://$API_ENDPOINT/v1/summary/counts" | jq '.numDeployments' -r)"
+        numDeployments="$(curl -sSk --config <(curl_cfg user "admin:$ROX_ADMIN_PASSWORD") "https://$API_ENDPOINT/v1/summary/counts" | jq '.numDeployments' -r)"
         echo "Try number ${i}. Number of deployments in Central: $numDeployments"
         [[ -n "$numDeployments" ]]
         if [[ "$numDeployments" -lt 100 ]]; then
@@ -81,7 +81,7 @@ validate_upgrade() {
 function roxcurl() {
   local url="$1"
   shift
-  curl --config <(curl_cfg user "admin:${ROX_PASSWORD}") -k "https://${API_ENDPOINT}${url}" "$@"
+  curl --config <(curl_cfg user "admin:${ROX_ADMIN_PASSWORD}") -k "https://${API_ENDPOINT}${url}" "$@"
 }
 
 deploy_earlier_postgres_central() {
@@ -93,11 +93,11 @@ deploy_earlier_postgres_central() {
     PATH="bin/$TEST_HOST_PLATFORM:$PATH" roxctl version
 
     # Let's try helm
-    ROX_PASSWORD="$(tr -dc _A-Z-a-z-0-9 < /dev/urandom | head -c12 || true)"
+    ROX_ADMIN_PASSWORD="$(tr -dc _A-Z-a-z-0-9 < /dev/urandom | head -c12 || true)"
     PATH="bin/$TEST_HOST_PLATFORM:$PATH" roxctl helm output central-services --image-defaults opensource --output-dir /tmp/early-stackrox-central-services-chart
 
     helm install -n stackrox --create-namespace stackrox-central-services /tmp/early-stackrox-central-services-chart \
-         --set central.adminPassword.value="${ROX_PASSWORD}" \
+         --set central.adminPassword.value="${ROX_ADMIN_PASSWORD}" \
          --set central.db.enabled=true \
          --set central.persistence.none=true \
          --set central.exposure.loadBalancer.enabled=true \
@@ -113,7 +113,7 @@ deploy_earlier_postgres_central() {
 
     ROX_USERNAME="admin"
     ci_export "ROX_USERNAME" "$ROX_USERNAME"
-    ci_export "ROX_PASSWORD" "$ROX_PASSWORD"
+    ci_export "ROX_ADMIN_PASSWORD" "$ROX_ADMIN_PASSWORD"
 }
 
 restore_4_1_backup() {
@@ -126,7 +126,7 @@ force_rollback() {
     info "Forcing a rollback to $FORCE_ROLLBACK_VERSION"
 
     local upgradeStatus
-    upgradeStatus=$(curl -sSk -X GET --config <(curl_cfg user "admin:${ROX_PASSWORD}") https://"${API_ENDPOINT}"/v1/centralhealth/upgradestatus)
+    upgradeStatus=$(curl -sSk -X GET --config <(curl_cfg user "admin:${ROX_ADMIN_PASSWORD}") https://"${API_ENDPOINT}"/v1/centralhealth/upgradestatus)
     echo "upgrade status: ${upgradeStatus}"
     test_equals_non_silent "$(echo "$upgradeStatus" | jq '.upgradeStatus.version' -r)" "$(make --quiet --no-print-directory tag)"
     test_equals_non_silent "$(echo "$upgradeStatus" | jq '.upgradeStatus.forceRollbackTo' -r)" "$FORCE_ROLLBACK_VERSION"
@@ -175,7 +175,7 @@ test_sensor_bundle() {
     info "Testing the sensor bundle"
 
     rm -rf sensor-remote
-    "$TEST_ROOT/bin/${TEST_HOST_PLATFORM}/roxctl" -e "$API_ENDPOINT" -p "$ROX_PASSWORD" sensor get-bundle remote
+    "$TEST_ROOT/bin/${TEST_HOST_PLATFORM}/roxctl" -e "$API_ENDPOINT" sensor get-bundle remote
     [[ -d sensor-remote ]]
 
     ./sensor-remote/sensor.sh
@@ -196,7 +196,7 @@ test_upgrader() {
     info "Creating a 'sensor-remote-new' cluster"
 
     rm -rf sensor-remote-new
-    "$TEST_ROOT/bin/${TEST_HOST_PLATFORM}/roxctl" -e "$API_ENDPOINT" -p "$ROX_PASSWORD" sensor generate k8s \
+    "$TEST_ROOT/bin/${TEST_HOST_PLATFORM}/roxctl" -e "$API_ENDPOINT" sensor generate k8s \
         --name remote-new \
         --create-admission-controller
 

ui/apps/platform/scripts/cypress.sh
@@ -3,7 +3,7 @@
 # Opens cypress with environment variables for feature flags and auth
 api_endpoint="${UI_BASE_URL:-https://localhost:8000}"
 
-if [[ -z "$ROX_USERNAME" || -z "$ROX_PASSWORD" ]]; then
+if [[ -z "$ROX_USERNAME" || -z "$ROX_ADMIN_PASSWORD" ]]; then
   # basic auth creds weren't set (e.g. by CI), assume local k8s deployment
   # shellcheck source=../../../../scripts/k8s/export-basic-auth-creds.sh
   source ../../../scripts/k8s/export-basic-auth-creds.sh ../../../deploy/k8s
@@ -13,8 +13,8 @@ curl_cfg() { # Use built-in echo to not expose $2 in the process list.
   echo -n "$1 = \"${2//[\"\\]/\\&}\""
 }
 
-if [[ -n "$ROX_PASSWORD" ]]; then
-  readarray -t arr < <(curl -sk --config <(curl_cfg user "admin:$ROX_PASSWORD") ${api_endpoint}/v1/featureflags | jq -cr '.featureFlags[] | {name: .envVar, enabled: .enabled}')
+if [[ -n "$ROX_ADMIN_PASSWORD" ]]; then
+  readarray -t arr < <(curl -sk --config <(curl_cfg user "admin:$ROX_ADMIN_PASSWORD") ${api_endpoint}/v1/featureflags | jq -cr '.featureFlags[] | {name: .envVar, enabled: .enabled}')
   for i in "${arr[@]}"; do
     name=$(echo $i | jq -rc .name)
     val=$(echo $i | jq -rc .enabled)