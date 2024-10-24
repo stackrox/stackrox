c543ff30b243891f7d65320e4b2b479c856b73bc
Vlad Bologa <vbologa@redhat.com>
2024-10-24 11:06:49 +0200

    ROX-26719: Repository for Secured Cluster certificates (#13023)

---
+++ sensor/kubernetes/certrefresh/securedcluster/cert_repo_init_test.go
@@ -0,0 +1,81 @@
+package securedcluster
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/protoassert"
+	"github.com/stretchr/testify/assert"
+	appsApiv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
+)
+
+var (
+	namespace        = "stackrox-ns"
+	sensorDeployment = &appsApiv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "sensor-deployment",
+			Namespace: namespace,
+		},
+	}
+	securedClusterCertificateSet = &storage.TypedServiceCertificateSet{
+		CaPem: make([]byte, 2),
+		ServiceCerts: []*storage.TypedServiceCertificate{
+			createServiceCertificate(storage.ServiceType_SENSOR_SERVICE),
+			createServiceCertificate(storage.ServiceType_ADMISSION_CONTROL_SERVICE),
+			createServiceCertificate(storage.ServiceType_COLLECTOR_SERVICE),
+			createServiceCertificate(storage.ServiceType_SCANNER_SERVICE),
+			createServiceCertificate(storage.ServiceType_SCANNER_DB_SERVICE),
+			createServiceCertificate(storage.ServiceType_SCANNER_V4_INDEXER_SERVICE),
+			createServiceCertificate(storage.ServiceType_SCANNER_V4_DB_SERVICE),
+		},
+	}
+)
+
+func TestEnsureServiceCertificates(t *testing.T) {
+	clientSet := fake.NewSimpleClientset(sensorDeployment)
+	secretsClient := clientSet.CoreV1().Secrets(namespace)
+	ctx := context.Background()
+
+	repo := NewServiceCertificatesRepo(sensorOwnerReference(sensorDeployment), namespace, secretsClient)
+	persistedCertificates, err := repo.EnsureServiceCertificates(ctx, securedClusterCertificateSet)
+
+	assert.NoError(t, err)
+	protoassert.SlicesEqual(t, securedClusterCertificateSet.ServiceCerts, persistedCertificates)
+
+	expectedSecretNames := []string{sensorSecretName, collectorSecretName, admissionControlSecretName,
+		scannerSecretName, scannerDbSecretName,
+		scannerV4IndexerSecretName, scannerV4DbSecretName}
+	assert.Equal(t, len(expectedSecretNames), len(persistedCertificates))
+
+	for _, secretName := range expectedSecretNames {
+		_, err = secretsClient.Get(ctx, secretName, metav1.GetOptions{})
+		assert.NoError(t, err)
+	}
+}
+
+func sensorOwnerReference(sensorDeployment *appsApiv1.Deployment) metav1.OwnerReference {
+	sensorDeploymentGVK := sensorDeployment.GroupVersionKind()
+	blockOwnerDeletion := false
+	isController := false
+	return metav1.OwnerReference{
+		APIVersion:         sensorDeploymentGVK.GroupVersion().String(),
+		Kind:               sensorDeploymentGVK.Kind,
+		Name:               sensorDeployment.GetName(),
+		UID:                sensorDeployment.GetUID(),
+		BlockOwnerDeletion: &blockOwnerDeletion,
+		Controller:         &isController,
+	}
+}
+
+func createServiceCertificate(serviceType storage.ServiceType) *storage.TypedServiceCertificate {
+	return &storage.TypedServiceCertificate{
+		ServiceType: serviceType,
+		Cert: &storage.ServiceCertificate{
+			CertPem: make([]byte, 0),
+			KeyPem:  make([]byte, 1),
+		},
+	}
+}
