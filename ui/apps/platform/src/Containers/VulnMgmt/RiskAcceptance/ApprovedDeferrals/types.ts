a568082bcc599eb13accfc26bcba558135764ca9
Mark Pedrotti <mark@stackrox.com>
2024-10-31 14:23:37 -0400

    chore(ui): Delete unreachable RiskAcceptance folder

--- ui/apps/platform/src/Containers/VulnMgmt/RiskAcceptance/ApprovedDeferrals/types.ts
+++
@@ -1,8 +0,0 @@
-export type ApprovedDeferralRequestAction = 'UPDATE' | 'UNDO';
-export type ApprovedDeferralRequestType = 'DEFERRAL';
-
-export type ApprovedDeferralRequestsToBeAssessed = {
-    type: ApprovedDeferralRequestType;
-    action: ApprovedDeferralRequestAction;
-    requestIDs: string[];
-} | null;
