d71fc5b526ceeffeff00d66d5e920e1709f0029f
Luis Valero Martín <lvalerom@redhat.com>
2024-11-04 08:58:44 +0100

    fix(reporting): Aggregate timestamps in the scan watcher (#13157)

--- central/complianceoperator/v2/report/manager/manager.go
+++ central/complianceoperator/v2/report/manager/manager.go
@@ -19,5 +19,6 @@ type Manager interface {
 	Stop()
 
 	HandleScan(ctx context.Context, scan *storage.ComplianceOperatorScanV2) error
+	HandleScanRemove(scanID string) error
 	HandleResult(ctx context.Context, result *storage.ComplianceOperatorCheckResultV2) error
 }
