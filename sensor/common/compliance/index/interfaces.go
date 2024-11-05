1eedd8708abb838315dd7db454608f8720306b69
RTann <rtannenb@redhat.com>
2024-11-04 16:53:06 -0800

    chore(nodescan): misc cleanup

--- sensor/common/compliance/index/interfaces.go
+++ sensor/common/compliance/index/interfaces.go
@@ -2,8 +2,8 @@ package index
 
 import v4 "github.com/stackrox/rox/generated/internalapi/scanner/v4"
 
-// IndexReportWrap wraps a v4.IndexReport with additional fields required by Sensor and Central
-type IndexReportWrap struct {
+// IndexReport wraps a v4.IndexReport with additional fields required by Sensor and Central
+type IndexReport struct {
 	NodeName    string
 	NodeID      string
 	IndexReport *v4.IndexReport
