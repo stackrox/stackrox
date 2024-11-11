695db68e9d3a5e2f5d20f50a82abf34f0bc9565e
Dave Shrewsberry <dashrews@redhat.com>
2024-11-11 12:25:31 -0500

    stupid migration

--- migrator/migrations/frozenschema/v73/active_components.go
+++ migrator/migrations/frozenschema/v73/active_components.go
@@ -28,14 +28,14 @@ var (
 	// ActiveComponentsSchema is the go schema for table `active_components`.
 	ActiveComponentsSchema = func() *walker.Schema {
 		schema := walker.Walk(reflect.TypeOf((*storage.ActiveComponent)(nil)), "active_components")
-		referencedSchemas := map[string]*walker.Schema{
-			"storage.Deployment":     DeploymentsSchema,
-			"storage.ImageComponent": ImageComponentsSchema,
-		}
+		//referencedSchemas := map[string]*walker.Schema{
+		//	"storage.Deployment":     DeploymentsSchema,
+		//	"storage.ImageComponent": ImageComponentsSchema,
+		//}
 
-		schema.ResolveReferences(func(messageTypeName string) *walker.Schema {
-			return referencedSchemas[fmt.Sprintf("storage.%s", messageTypeName)]
-		})
+		//schema.ResolveReferences(func(messageTypeName string) *walker.Schema {
+		//	return referencedSchemas[fmt.Sprintf("storage.%s", messageTypeName)]
+		//})
 		schema.SetOptionsMap(search.Walk(v1.SearchCategory_ACTIVE_COMPONENT, "activecomponent", (*storage.ActiveComponent)(nil)))
 		return schema
 	}()
