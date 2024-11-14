aa711f75a5be8e58651504974ad7d5ab85741a5b
Moritz Clasmeier <111092021+mclasmeier@users.noreply.github.com>
2024-11-14 10:37:27 +0100

    Update pkg/protoconv/certs/typed_service_certificates.go
    Co-authored-by: Marcin Owsiany <porridge@redhat.com>

--- pkg/protoconv/certs/typed_service_certificates.go
+++ pkg/protoconv/certs/typed_service_certificates.go
@@ -24,7 +24,7 @@ func ConvertTypedServiceCertificateSetToFileMap(certSet *storage.TypedServiceCer
 	for _, cert := range certSet.GetServiceCerts() {
 		serviceName := services.ServiceTypeToSlugName(cert.ServiceType)
 		if serviceName == "" {
-			utils.Should(errors.Errorf("invalid service type %v when creating certificate bundle to file map", cert.ServiceType))
+			utils.Should(errors.Errorf("invalid service type %v when converting certificate bundle to file map", cert.ServiceType))
 			continue // ignore
 		}
 		fileMap[serviceName+"-cert.pem"] = string(cert.Cert.CertPem)
