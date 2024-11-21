6bceb9dd4bea01f3e9ba5d442dbbd28806997c8f
Moritz Clasmeier <mclasmeier@redhat.com>
2024-11-22 00:23:36 +0100

    crs-40: move CRS file path/env settings from mtls to crs package

--- pkg/crs/env.go
+++ pkg/crs/env.go
@@ -1,5 +1,7 @@
 package crs
 
+import "github.com/stackrox/rox/pkg/env"
+
 const (
 	// LegacySensorServiceCertEnvName contains the environment variable name used by the CRS-flow to check for the
 	// existence of a legacy sensor service certificate (e.g. coming from an init-bundle).
@@ -12,4 +14,20 @@ const (
 	// Needs to be kept in sync with
 	// image/templates/helm/stackrox-secured-cluster/templates/sensor.yaml.htpl:spec.template.spec.initContainers[0].env.
 	SensorServiceCertEnvName = "ROX_SENSOR_SERVICE_CERT"
+
+	// crsFilePathEnvName is the env variable name for the CRS file.
+	crsFilePathEnvName = "ROX_CRS_FILE"
+
+	// defaultCRSFilePath is where the Cluster Registration Secret is expected by default.
+	defaultCRSFilePath = "/run/secrets/stackrox.io/crs/crs"
 )
+
+var (
+	// crsFilePathSetting allows configuring the CRS from the environment.
+	crsFilePathSetting = env.RegisterSetting(crsFilePathEnvName, env.WithDefault(defaultCRSFilePath))
+)
+
+// crsFilePath returns the path where the CRS certificate is stored.
+func crsFilePath() string {
+	return crsFilePathSetting.Setting()
+}
