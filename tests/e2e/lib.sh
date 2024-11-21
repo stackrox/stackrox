45d41c71045ccf98b8cdf9c3ba76437f7fc0adda
David Vail <dcvail17@gmail.com>
2024-11-21 14:58:11 -0500

    chore: Add feature flag for SBOM generation (#13398)

--- tests/e2e/lib.sh
+++ tests/e2e/lib.sh
@@ -181,6 +181,7 @@ export_test_environment() {
     ci_export ROX_PLATFORM_COMPONENTS "${ROX_PLATFORM_COMPONENTS:-true}"
     ci_export ROX_NVD_CVSS "${ROX_NVD_CVSS_UI:-true}"
     ci_export ROX_CVE_ADVISORY_SEPARATION "${ROX_CVE_ADVISORY_SEPARATION:-true}"
+    ci_export ROX_SBOM_GENERATION "${ROX_SBOM_GENERATION:-true}"
     ci_export ROX_CLUSTERS_PAGE_MIGRATION_UI "${ROX_CLUSTERS_PAGE_MIGRATION_UI:-true}"
 
     if is_in_PR_context && pr_has_label ci-fail-fast; then
@@ -320,6 +321,8 @@ deploy_central_via_operator() {
     customize_envVars+=$'\n      - name: ROX_CVE_ADVISORY_SEPARATION'
     customize_envVars+=$'\n        value: "true"'
     customize_envVars+=$'\n      - name: ROX_CLUSTERS_PAGE_MIGRATION_UI'
+    customize_envVars+=$'\n        value: "true"'
+    customize_envVars+=$'\n      - name: ROX_SBOM_GENERATION'
     customize_envVars+=$'\n        value: "true"'
 
     CENTRAL_YAML_PATH="tests/e2e/yaml/central-cr.envsubst.yaml"
