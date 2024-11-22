2130be2cff1855bac0621c5f4d6a76d74128046a
Moritz Clasmeier <mclasmeier@redhat.com>
2024-11-22 02:01:00 +0100

    add roundtrip test

--- pkg/protoconv/certs/typed_service_certificates_test.go
+++ pkg/protoconv/certs/typed_service_certificates_test.go
@@ -1,9 +1,13 @@
 package protoconv
 
 import (
+	"cmp"
+	"fmt"
+	"slices"
 	"testing"
 
 	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/protoutils"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -19,16 +23,16 @@ func makeTypedServiceCert(serviceType storage.ServiceType, certPem string, keyPe
 
 func TestConvertTypedServiceCertificateSetToFileMap(t *testing.T) {
 	cases := []struct {
-		description string
-		input       *storage.TypedServiceCertificateSet
-		expected    map[string]string
+		description     string
+		input           *storage.TypedServiceCertificateSet
+		expectedFileMap map[string]string
 	}{
 		{
 			description: "empty",
 			input: &storage.TypedServiceCertificateSet{
 				ServiceCerts: []*storage.TypedServiceCertificate{},
 			},
-			expected: map[string]string{},
+			expectedFileMap: map[string]string{},
 		},
 		{
 			description: "two certs",
@@ -39,7 +43,7 @@ func TestConvertTypedServiceCertificateSetToFileMap(t *testing.T) {
 					makeTypedServiceCert(storage.ServiceType_SCANNER_V4_INDEXER_SERVICE, "cert 2", "key 2"),
 				},
 			},
-			expected: map[string]string{
+			expectedFileMap: map[string]string{
 				"ca-cert.pem":                 "ca cert",
 				"admission-control-cert.pem":  "cert 1",
 				"admission-control-key.pem":   "key 1",
@@ -50,12 +54,38 @@ func TestConvertTypedServiceCertificateSetToFileMap(t *testing.T) {
 	}
 	for _, c := range cases {
 		t.Run(c.description, func(t *testing.T) {
+			// Convert input TypedServiceCertificateSet to a FileMap.
 			inputTypedServiceCertificateSet := c.input
+			inputCa := inputTypedServiceCertificateSet.GetCaPem()
+			inputCerts := inputTypedServiceCertificateSet.GetServiceCerts()
 			fileMap, err := ConvertTypedServiceCertificateSetToFileMap(inputTypedServiceCertificateSet)
 			assert.NoError(t, err)
-			assert.Equal(t, c.expected, fileMap)
-			// roundTripTypedServiceCertificateSet := ConvertFileMapToTypedServiceCertificateSet(fileMap)
-			// assert.Equal(t, inputTypedServiceCertificateSet, roundTripTypedServiceCertificateSet)
+			assert.Equal(t, c.expectedFileMap, fileMap)
+
+			// Convert FileMap back to TypedServiceCertificateSet.
+			roundTripTypedServiceCertificateSet, err := ConvertFileMapToTypedServiceCertificateSet(fileMap)
+			assert.NoError(t, err)
+			roundTripCa := roundTripTypedServiceCertificateSet.GetCaPem()
+			roundTripCerts := roundTripTypedServiceCertificateSet.GetServiceCerts()
+
+			// Check if the result matches the original TypedServiceCertificateSet we started with.
+			assert.Equal(t, inputCa, roundTripCa, "CAs differ")
+			// Note that converting from a slice to a map and back to a slice can change the order, hence the sorting.
+			sortTypedServiceCertificateSlice(inputCerts)
+			sortTypedServiceCertificateSlice(roundTripCerts)
+			if !protoutils.SlicesEqual(inputCerts, roundTripCerts) {
+				assert.Fail(t,
+					fmt.Sprintf("Not equal for test case %q:\n"+
+						"input certs: %v\n"+
+						"round-trip certs: %v\n", c.description, inputCerts, roundTripCerts))
+			}
 		})
 	}
+}
+
+// Sort TypedServiceCertificate slices by their service type.
+func sortTypedServiceCertificateSlice(certs []*storage.TypedServiceCertificate) {
+	slices.SortFunc(certs, func(a, b *storage.TypedServiceCertificate) int {
+		return cmp.Compare(a.GetServiceType(), b.GetServiceType())
+	})
 }
