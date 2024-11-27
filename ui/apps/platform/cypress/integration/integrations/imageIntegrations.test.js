6046f6137597a0f8cc9f24b2e770b5000a32ba96
Stephan Heßelmann <shesselm@redhat.com>
2024-11-27 20:38:22 +0100

    make sure endpoint is cleared in ui e2e test

--- ui/apps/platform/cypress/integration/integrations/imageIntegrations.test.js
+++ ui/apps/platform/cypress/integration/integrations/imageIntegrations.test.js
@@ -572,6 +572,8 @@ describe('Image Integrations', () => {
         // Step 1, check empty fields
         getInputByLabel('Integration name').clear().type(' ');
         getInputByLabel('Endpoint').clear().type(' ');
+        getInputByLabel('User').clear().type(' ');
+        getInputByLabel('Password').clear().type(' ').blur();
 
         getHelperElementByLabel('Integration name').contains('An integration name is required');
         getHelperElementByLabel('Endpoint').contains('An endpoint is required');
