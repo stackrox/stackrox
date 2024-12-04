4ee8f5ac9ba825142b186ffd3738ca284862eaa0
Randy Truong <rtruong@u.northwestern.edu>
2024-12-04 12:44:34 -0600

    Revert "chore: redundant == true conditionals for CI"
    This reverts commit 4a1b2087fee2be8a65dca8694b3cbcddd261afd8.

--- roxctl/common/flags/config_file.go
+++ roxctl/common/flags/config_file.go
@@ -55,12 +55,12 @@ func ConfigurationFileName() string {
 
 // ConfigurationFileChanged returns whether the configuration file is provided as an argument.
 func ConfigurationFileChanged() bool {
-	return configFileSet != nil && *configFileSet
+	return configFileSet != nil && *configFileSet == true
 }
 
 // CaCertificatePath returns the configuration-defined CA Certificate path.
 func CaCertificatePath() string {
-	if ConfigurationFileChanged() {
+	if ConfigurationFileChanged() == true {
 		return config.Instance.CaCertificatePath
 	}
 
@@ -69,7 +69,7 @@ func CaCertificatePath() string {
 
 // ApiTokenFilePath returns the configuration-defined API Token file path.
 func ApiTokenFilePath() string {
-	if ConfigurationFileChanged() {
+	if ConfigurationFileChanged() == true {
 		return config.Instance.ApiTokenFilePath
 	}
 
@@ -78,7 +78,7 @@ func ApiTokenFilePath() string {
 
 // Endpoint returns the configuration-defined endpoint.
 func Endpoint() string {
-	if ConfigurationFileChanged() {
+	if ConfigurationFileChanged() == true {
 		return config.Instance.Endpoint
 	}
 
