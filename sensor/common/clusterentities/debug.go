9880a9b17c726817fdf56b5e8634efb74a30a6e2
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2024-12-04 14:13:13 +0100

    Add mutex for the debug() functions

--- sensor/common/clusterentities/debug.go
+++ sensor/common/clusterentities/debug.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/pkg/errors"
+
 	"github.com/stackrox/rox/pkg/concurrency"
 )
 
@@ -57,20 +58,22 @@ func (e *containerIDsStore) debug() interface{} {
 	dbg["historicalContainerIDs"] = make(map[string]interface{})
 	dbg["reverseContainerIDMap"] = make(map[string]interface{})
 
-	for cID, metadata := range e.containerIDMap {
-		dbg["containerIDMap"][cID] = metadata
-	}
-	for cID, submap := range e.historicalContainerIDs {
-		for metadata, status := range submap {
-			dbg["historicalContainerIDs"][cID] = map[string]interface{}{
-				"metadata":  metadata,
-				"ticksLeft": status.ticksLeft,
+	concurrency.WithRLock(&e.mutex, func() {
+		for cID, metadata := range e.containerIDMap {
+			dbg["containerIDMap"][cID] = metadata
+		}
+		for cID, submap := range e.historicalContainerIDs {
+			for metadata, status := range submap {
+				dbg["historicalContainerIDs"][cID] = map[string]interface{}{
+					"metadata":  metadata,
+					"ticksLeft": status.ticksLeft,
+				}
 			}
 		}
-	}
-	for deplID, cIDSet := range e.reverseContainerIDMap {
-		dbg["reverseContainerIDMap"][deplID] = cIDSet.AsSlice()
-	}
+		for deplID, cIDSet := range e.reverseContainerIDMap {
+			dbg["reverseContainerIDMap"][deplID] = cIDSet.AsSlice()
+		}
+	})
 	return dbg
 }
 
@@ -80,25 +83,27 @@ func (e *podIPsStore) debug() interface{} {
 	dbg["reverseIPMap"] = make(map[string]interface{})
 	dbg["historicalIPs"] = make(map[string]interface{})
 
-	for addr, deplSet := range e.ipMap {
-		dbg["ipMap"][addr.AsNetIP().String()] = deplSet.AsSlice()
-	}
-	for deplID, addrSet := range e.reverseIPMap {
-		// addrSet.AsSlice() does not print well
-		arr := make([]string, 0, addrSet.Cardinality())
-		for _, addr := range addrSet.AsSlice() {
-			arr = append(arr, addr.String())
+	concurrency.WithRLock(&e.mutex, func() {
+		for addr, deplSet := range e.ipMap {
+			dbg["ipMap"][addr.AsNetIP().String()] = deplSet.AsSlice()
 		}
-		dbg["reverseIPMap"][deplID] = arr
-	}
-	for addr, submap := range e.historicalIPs {
-		for deplID, status := range submap {
-			dbg["historicalIPs"][addr.AsNetIP().String()] = map[string]interface{}{
-				"deplID":    deplID,
-				"ticksLeft": status.ticksLeft,
+		for deplID, addrSet := range e.reverseIPMap {
+			// addrSet.AsSlice() does not print well
+			arr := make([]string, 0, addrSet.Cardinality())
+			for _, addr := range addrSet.AsSlice() {
+				arr = append(arr, addr.String())
+			}
+			dbg["reverseIPMap"][deplID] = arr
+		}
+		for addr, submap := range e.historicalIPs {
+			for deplID, status := range submap {
+				dbg["historicalIPs"][addr.AsNetIP().String()] = map[string]interface{}{
+					"deplID":    deplID,
+					"ticksLeft": status.ticksLeft,
+				}
 			}
 		}
-	}
+	})
 	return dbg
 }
 
@@ -109,42 +114,44 @@ func (e *endpointsStore) debug() interface{} {
 	dbg["historicalEndpoints"] = make(map[string]map[string]interface{})
 	dbg["reverseHistoricalEndpoints"] = make(map[string]map[string]interface{})
 
-	for ep, submap := range e.endpointMap {
-		dbg["endpointMap"][ep.String()] = make(map[string]interface{})
-		for deplID, targetInfoSet := range submap {
-			dbg["endpointMap"][ep.String()][deplID] = targetInfoSet.AsSlice()
+	concurrency.WithRLock(&e.mutex, func() {
+		for ep, submap := range e.endpointMap {
+			dbg["endpointMap"][ep.String()] = make(map[string]interface{})
+			for deplID, targetInfoSet := range submap {
+				dbg["endpointMap"][ep.String()][deplID] = targetInfoSet.AsSlice()
+			}
 		}
-	}
-	dbg["reverseEndpointMap"]["deployments"] = make(map[string]interface{})
-	for deplID, setOfEp := range e.reverseEndpointMap {
-		// setOfEp.AsSlice() does not print well
-		arr := make([]string, 0, setOfEp.Cardinality())
-		for _, ep := range setOfEp.AsSlice() {
-			arr = append(arr, ep.String())
+		dbg["reverseEndpointMap"]["deployments"] = make(map[string]interface{})
+		for deplID, setOfEp := range e.reverseEndpointMap {
+			// setOfEp.AsSlice() does not print well
+			arr := make([]string, 0, setOfEp.Cardinality())
+			for _, ep := range setOfEp.AsSlice() {
+				arr = append(arr, ep.String())
+			}
+			// we need dummy entry "deployments" to fit into the dbg declaration
+			dbg["reverseEndpointMap"]["deployments"][deplID] = arr
 		}
-		// we need dummy entry "deployments" to fit into the dbg declaration
-		dbg["reverseEndpointMap"]["deployments"][deplID] = arr
-	}
-	for ep, submap := range e.historicalEndpoints {
-		dbg["historicalEndpoints"][ep.String()] = make(map[string]interface{})
-		for deplID, targetInfoSetMap := range submap {
-			for targetInfo, status := range targetInfoSetMap {
-				dbg["historicalEndpoints"][ep.String()][deplID] = map[string]interface{}{
-					"targetInfo": targetInfo,
-					"ticksLeft":  status.ticksLeft,
+		for ep, submap := range e.historicalEndpoints {
+			dbg["historicalEndpoints"][ep.String()] = make(map[string]interface{})
+			for deplID, targetInfoSetMap := range submap {
+				for targetInfo, status := range targetInfoSetMap {
+					dbg["historicalEndpoints"][ep.String()][deplID] = map[string]interface{}{
+						"targetInfo": targetInfo,
+						"ticksLeft":  status.ticksLeft,
+					}
 				}
 			}
 		}
-	}
-	dbg["reverseHistoricalEndpoints"] = make(map[string]map[string]interface{})
-	for deplID, submap := range e.reverseHistoricalEndpoints {
-		dbg["reverseHistoricalEndpoints"][deplID] = make(map[string]interface{})
-		for ep, status := range submap {
-			dbg["reverseHistoricalEndpoints"][deplID] = map[string]interface{}{
-				"endpoint":  ep.String(),
-				"ticksLeft": status.ticksLeft,
+		dbg["reverseHistoricalEndpoints"] = make(map[string]map[string]interface{})
+		for deplID, submap := range e.reverseHistoricalEndpoints {
+			dbg["reverseHistoricalEndpoints"][deplID] = make(map[string]interface{})
+			for ep, status := range submap {
+				dbg["reverseHistoricalEndpoints"][deplID] = map[string]interface{}{
+					"endpoint":  ep.String(),
+					"ticksLeft": status.ticksLeft,
+				}
 			}
 		}
-	}
+	})
 	return dbg
 }
