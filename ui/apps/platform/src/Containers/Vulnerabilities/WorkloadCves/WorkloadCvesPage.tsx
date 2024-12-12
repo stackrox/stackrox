aefbcb757cd93fffdf7bacd51faafea7e853c149
David Vail <dcvail17@gmail.com>
2024-12-12 11:09:19 -0500

    Add React.Context to Workload CVEs to allow view customization

--- ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/WorkloadCvesPage.tsx
+++ ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/WorkloadCvesPage.tsx
@@ -13,6 +13,7 @@ import ImagePage from './Image/ImagePage';
 import WorkloadCvesOverviewPage from './Overview/WorkloadCvesOverviewPage';
 import ImageCvePage from './ImageCve/ImageCvePage';
 import NamespaceViewPage from './NamespaceView/NamespaceViewPage';
+import { WorkloadCveViewContext } from './WorkloadCveViewContext';
 
 import './WorkloadCvesPage.css';
 
@@ -20,13 +21,32 @@ const vulnerabilitiesWorkloadCveSinglePath = `${vulnerabilitiesWorkloadCvesPath}
 const vulnerabilitiesWorkloadCveImageSinglePath = `${vulnerabilitiesWorkloadCvesPath}/images/:imageId`;
 const vulnerabilitiesWorkloadCveDeploymentSinglePath = `${vulnerabilitiesWorkloadCvesPath}/deployments/:deploymentId`;
 
-function WorkloadCvesPage() {
+const userWorkloadContext = {
+    pageTitle: 'Workload CVEs', // TODO Implement throughout in follow up
+    baseSearchFilter: {}, // TODO Implement throughout in follow up
+    createUrl: (path) => `${vulnerabilitiesWorkloadCvesPath}${path}`, // TODO Implement throughout in follow up
+};
+
+// TODO Update these values for Platform View
+const platformWorkloadContext = {
+    pageTitle: 'Workload CVEs', // TODO Implement throughout in follow up
+    baseSearchFilter: {}, // TODO Implement throughout in follow up
+    createUrl: (path) => `${vulnerabilitiesWorkloadCvesPath}${path}`, // TODO Implement throughout in follow up
+};
+
+export type WorkloadCvePageProps = {
+    view: 'user-workload' | 'platform-workload';
+};
+
+function WorkloadCvesPage({ view }: WorkloadCvePageProps) {
     const { hasReadAccess } = usePermissions();
     const hasReadAccessForIntegration = hasReadAccess('Integration');
     const hasReadAccessForNamespaces = hasReadAccess('Namespace');
 
+    const context = view === 'user-workload' ? userWorkloadContext : platformWorkloadContext;
+
     return (
-        <>
+        <WorkloadCveViewContext.Provider value={context}>
             {hasReadAccessForIntegration && <ScannerV4IntegrationBanner />}
             <Switch>
                 {hasReadAccessForNamespaces && (
@@ -48,12 +68,12 @@ function WorkloadCvesPage() {
                 </Route>
                 <Route>
                     <PageSection variant="light">
-                        <PageTitle title="Workload CVEs - Not Found" />
+                        <PageTitle title={`${context.pageTitle} - Not Found`} />
                         <PageNotFound />
                     </PageSection>
                 </Route>
             </Switch>
-        </>
+        </WorkloadCveViewContext.Provider>
     );
 }
 
