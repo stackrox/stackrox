839e103d7c02bc51525e29cf68cf85be23db5d39
Tom Martensen <tmartens@redhat.com>
2024-12-13 13:47:45 +0100

    read git-ref/scanner from image

--- .konflux/scripts/render_snapshot.py
+++ .konflux/scripts/render_snapshot.py
@@ -6,13 +6,17 @@ import time
 
 
 def load_image_refs():
-    return json.loads(os.getenv("IMAGE_REFS", "[]"))
+    return json.loads(os.getenv("IMAGE_REFS", '[{"component": "scanner", "ref": "", "revision": "", "repository": ""}]'))
 
 
-def process_component(component):
+def process_component(component, name_suffix):
+    if name_suffix != "":
+        name = f"{component["component"]}-{name_suffix}"
+    else:
+        name = component["component"]
     return {
         "containerImage": component["ref"],
-        "name": component["component"],
+        "name": name,
         "source": {
             "git": {
                 "revision": component["revision"],
@@ -37,10 +41,15 @@ def construct_snapshot(snapshot_name_prefix, application, components):
     }
 
 
+def determine_component_name_suffix(application):
+    return application.lstrip("acs-")
+
+
 if __name__ == '__main__':
     application = os.getenv("APPLICATION", "")
     image_refs = load_image_refs()
-    components = [process_component(c) for c in image_refs]
+    name_suffix = determine_component_name_suffix(application)
+    components = [process_component(c, name_suffix) for c in image_refs]
     snapshot = construct_snapshot(
         f"tm-{application}",
         application,
