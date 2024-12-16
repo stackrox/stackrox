f18ae885865b782c64ecf30a9ce4bcd1419290a6
Tom Martensen <tmartens@redhat.com>
2024-12-16 16:28:30 +0100

    fix UI rendering of snapshot

--- .konflux/scripts/render_snapshot.py
+++ .konflux/scripts/render_snapshot.py
@@ -26,13 +26,21 @@ def process_component(component, name_suffix):
     }
 
 
-def construct_snapshot(snapshot_name_prefix, application, components):
+def construct_snapshot(
+    snapshot_name_prefix,
+    pipeline_run_name,
+    application,
+    components
+):
     snapshot_name = f"{snapshot_name_prefix}-{int(time.time())}"
     return {
         "apiVersion": "appstudio.redhat.com/v1alpha1",
         "kind": "Snapshot",
         "metadata": {
-            "name": snapshot_name
+            "name": snapshot_name,
+            "labels": {
+                "appstudio.openshift.io/build-pipelinerun": pipeline_run_name
+            }
         },
         "spec": {
             "application": application,
@@ -47,11 +55,13 @@ def determine_component_name_suffix(application):
 
 if __name__ == '__main__':
     application = os.getenv("APPLICATION", "")
+    pipeline_run_name = os.getenv("PIPELINE_RUN_NAME", "")
     image_refs = load_image_refs()
     name_suffix = determine_component_name_suffix(application)
     components = [process_component(c, name_suffix) for c in image_refs]
     snapshot = construct_snapshot(
         f"tm-{application}",
+        pipeline_run_name,
         application,
         components
     )
