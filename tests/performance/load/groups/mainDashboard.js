6e3b7f218067e17a632fa056fa7fc2757422c717
Yann Brillouet <ybrillou@redhat.com>
2024-12-17 17:30:00 +0100

    Line-wrap SQL queries for better readability

--- tests/performance/load/groups/mainDashboard.js
+++ tests/performance/load/groups/mainDashboard.js
@@ -11,14 +11,35 @@ export function mainDashboard(host, headers, tags) {
         /** DB Queries
          *  - clusterCount => Cached store scan
          *  - nodeCount
-         *  select count(*) from nodes where nodes.ClusterId = $1
+         *  select count(*)
+         *   from nodes
+         *   where nodes.ClusterId = $1
+         *
          *  - violationCount ($12 = 0, $13 = 3)
-         *  select count(*) from alerts where ((alerts.ClusterId = $1 and (alerts.Namespace = $2 or ... or alerts.Namespace = $11)) and ((alerts.State = $12) or (alerts.State = $13)))
+         *  select count(*)
+         *   from alerts
+         *   where (
+         *     (alerts.ClusterId = $1 and (alerts.Namespace = $2 or ... or alerts.Namespace = $11))
+         *     and ((alerts.State = $12) or (alerts.State = $13))
+         *   )
+         *
          *  - deploymentCount => Cached store scan
+         *
          *  - imageCount
-         *  select count(distinct(images.Id)) from images inner join deployments_containers on images.Id = deployments_containers.Image_Id inner join deployments on deployments_containers.deployments_Id = deployments.Id where (deployments.ClusterId = $1 and (deployments.Namespace = $2 or ... or deployments.Namespace = $11))
+         *  select count(distinct(images.Id))
+         *   from images
+         *   inner join deployments_containers on images.Id = deployments_containers.Image_Id
+         *   inner join deployments on deployments_containers.deployments_Id = deployments.Id
+         *   where (
+         *     deployments.ClusterId = $1 and (deployments.Namespace = $2 or ... or deployments.Namespace = $11)
+         *   )
+         *
          *  - secretCount
-         *  select count(*) from secrets where (secrets.ClusterId = $1 and (secrets.Namespace = $2 or ... or secrets.Namespace = $11))
+         *  select count(*)
+         *   from secrets
+         *   where (
+         *     secrets.ClusterId = $1 and (secrets.Namespace = $2 or ... or secrets.Namespace = $11)
+         *   )
          **/
 
         http.post(
@@ -28,14 +49,36 @@ export function mainDashboard(host, headers, tags) {
         );
         /** DB Queries
          *  - clusters
-         *  select clusters.Id::text as Cluster_ID from clusters where clusters.Id = $1 order by clusters.Name asc
+         *  select clusters.Id::text as Cluster_ID
+         *   from clusters
+         *   where clusters.Id = $1
+         *   order by clusters.Name asc
+         *
          *  $2 = q1.clusters.Id
-         *  select "cluster_health_statuses".serialized from cluster_health_statuses inner join clusters on cluster_health_statuses.Id = clusters.Id where (clusters.Id = $1 and cluster_health_statuses.Id = ANY($2::uuid[]))
+         *  select "cluster_health_statuses".serialized
+         *   from cluster_health_statuses
+         *   inner join clusters on cluster_health_statuses.Id = clusters.Id
+         *   where (
+         *     clusters.Id = $1 and cluster_health_statuses.Id = ANY($2::uuid[])
+         *   )
+         *
          *  - namespaces
          *  $12 = q1.clusters.Id
-         *  select namespaces.Id::text as Namespace_ID from namespaces where ((namespaces.ClusterId = $1 and (namespaces.Name = $2 or ... or namespaces.Name = $11)) and namespaces.ClusterId = $12) order by namespaces.Name asc
+         *  select namespaces.Id::text as Namespace_ID
+         *   from namespaces where (
+         *     (namespaces.ClusterId = $1 and (namespaces.Name = $2 or ... or namespaces.Name = $11))
+         *     and namespaces.ClusterId = $12
+         *   )
+         *   order by namespaces.Name asc
+         *
          *  $12 = q3.namespaces.Id, $13 = q1.clusters.Id
-         *  select namespaces.Id::text as Namespace_ID from namespaces where ((namespaces.ClusterId = $1 and (namespaces.Name = $2 or ... or namespaces.Name = $11)) and (namespaces.Id = ANY($12::uuid[]) and namespaces.ClusterId = $13)) order by namespaces.Name asc
+         *  select namespaces.Id::text as Namespace_ID
+         *   from namespaces
+         *   where (
+         *     (namespaces.ClusterId = $1 and (namespaces.Name = $2 or ... or namespaces.Name = $11))
+         *     and (namespaces.Id = ANY($12::uuid[]) and namespaces.ClusterId = $13)
+         *   )
+         *   order by namespaces.Name asc
          *
          *  Note: cluster and namespace payloads are retrieved from the cached stores and have no SQL footprint.
          **/
@@ -47,7 +90,12 @@ export function mainDashboard(host, headers, tags) {
         );
         /** DB Queries
          *  - violationCount ($12 = 1..4, $13 = 0 -ViolationState_ACTIVE-, $14 = 3 -ViolationState_ATTEMPTED-)
-         *  select count(*) from alerts where ((alerts.ClusterId = $1 and (alerts.Namespace = $2 or ... or alerts.Namespace = $11)) and ((alerts.Policy_Severity = $12) and ((alerts.State = $13) or (alerts.State = $14))))
+         *  select count(*)
+         *   from alerts
+         *   where (
+         *     (alerts.ClusterId = $1 and (alerts.Namespace = $2 or ... or alerts.Namespace = $11))
+         *     and ((alerts.Policy_Severity = $12) and ((alerts.State = $13) or (alerts.State = $14)))
+         *   )
          */
 
         http.post(
@@ -57,9 +105,21 @@ export function mainDashboard(host, headers, tags) {
         );
         /** DB Queries
          *  - violations ($12 = 4, $13 = 0, $15 = 3)
-         *  select "alerts".serialized from alerts where ((alerts.ClusterId = $1 and (alerts.Namespace = $2 or ... or alerts.Namespace = $11)) and ((alerts.Policy_Severity = $12) and ((alerts.State = $13) or (alerts.State = $14)))) order by alerts.Time desc LIMIT 3
+         *  select "alerts".serialized
+         *   from alerts
+         *   where (
+         *     (alerts.ClusterId = $1 and (alerts.Namespace = $2 or ... or alerts.Namespace = $11))
+         *     and ((alerts.Policy_Severity = $12) and ((alerts.State = $13) or (alerts.State = $14)))
+         *   )
+         *   order by alerts.Time desc LIMIT 3
+         *
          *  (3x $12 = q1.alerts.serialized)
-         *  select "alerts".serialized from alerts where ((alerts.ClusterId = $1 and (alerts.Namespace = $2 or ... or alerts.Namespace = $11)) and alerts.Id = ANY($12::uuid[]))
+         *  select "alerts".serialized
+         *   from alerts
+         *   where (
+         *     (alerts.ClusterId = $1 and (alerts.Namespace = $2 or ... or alerts.Namespace = $11))
+         *     and alerts.Id = ANY($12::uuid[])
+         *   )
          */
 
         http.post(
@@ -69,14 +129,53 @@ export function mainDashboard(host, headers, tags) {
         );
         /** DB Queries
          *  - images
-         *  select distinct(images.Id) as Image_Sha, images.RiskScore as image_risk_score from images inner join deployments_containers on images.Id = deployments_containers.Image_Id inner join deployments on deployments_containers.deployments_Id = deployments.Id where (deployments.ClusterId = $1 and (deployments.Namespace = $2 or … or deployments.Namespace = $11)) order by images.RiskScore desc LIMIT 6
+         *  select distinct(images.Id) as Image_Sha, images.RiskScore as image_risk_score
+         *   from images
+         *   inner join deployments_containers on images.Id = deployments_containers.Image_Id
+         *   inner join deployments on deployments_containers.deployments_Id = deployments.Id
+         *   where (
+         *     deployments.ClusterId = $1 and (deployments.Namespace = $2 or … or deployments.Namespace = $11)
+         *   )
+         *   order by images.RiskScore desc LIMIT 6
+         *
          *  ($23..$26 = q1.images.Id)
-         *  select "images".serialized from images inner join deployments_containers on images.Id = deployments_containers.Image_Id inner join deployments on deployments_containers.deployments_Id = deployments.Id where ((deployments.ClusterId = $1 and (deployments.Namespace = $2 or … or deployments.Namespace = $11)) and ((deployments.ClusterId = $12 and (deployments.Namespace = $13 or … or deployments.Namespace = $22)) and (images.Id = $23 or images.Id = $24 or images.Id = $25 or images.Id = $26 or images.Id = $27 or images.Id = $28)))
+         *  select "images".serialized
+         *   from images
+         *   inner join deployments_containers on images.Id = deployments_containers.Image_Id
+         *   inner join deployments on deployments_containers.deployments_Id = deployments.Id
+         *   where (
+         *     (deployments.ClusterId = $1 and (deployments.Namespace = $2 or … or deployments.Namespace = $11))
+         *     and (
+         *       (deployments.ClusterId = $12 and (deployments.Namespace = $13 or … or deployments.Namespace = $22))
+         *       and (images.Id = $23 or images.Id = $24 or images.Id = $25 or images.Id = $26 or images.Id = $27 or images.Id = $28)
+         *     )
+         *   )
+         *
          *  - imageVulnerabilityCounter
          *  ($12 = false, $13 = true..false, $14 = q1.images.Id)
-         *  select distinct(image_cves.Id) as CVE_ID from image_cves inner join image_component_cve_edges on image_cves.Id = image_component_cve_edges.ImageCveId inner join image_component_edges on image_component_cve_edges.ImageComponentId = image_component_edges.ImageComponentId inner join images on image_component_edges.ImageId = images.Id inner join deployments_containers on images.Id = deployments_containers.Image_Id inner join deployments on deployments_containers.deployments_Id = deployments.Id where ((deployments.ClusterId = $1 and (deployments.Namespace = $2 or … or deployments.Namespace = $11)) and ((image_cves.Snoozed = $12 and image_component_cve_edges.IsFixable = $13) and images.Id = $14))
+         *  select distinct(image_cves.Id) as CVE_ID
+         *   from image_cves
+         *   inner join image_component_cve_edges on image_cves.Id = image_component_cve_edges.ImageCveId
+         *   inner join image_component_edges on image_component_cve_edges.ImageComponentId = image_component_edges.ImageComponentId
+         *   inner join images on image_component_edges.ImageId = images.Id
+         *   inner join deployments_containers on images.Id = deployments_containers.Image_Id
+         *   inner join deployments on deployments_containers.deployments_Id = deployments.Id
+         *   where (
+         *     (deployments.ClusterId = $1 and (deployments.Namespace = $2 or … or deployments.Namespace = $11))
+         *     and ((image_cves.Snoozed = $12 and image_component_cve_edges.IsFixable = $13) and images.Id = $14)
+         *   )
+         *
          *  ($12 = q3.image_cves.Id)
-         *  select "image_cves".serialized from image_cves inner join image_component_cve_edges on image_cves.Id = image_component_cve_edges.ImageCveId inner join image_component_edges on image_component_cve_edges.ImageComponentId = image_component_edges.ImageComponentId inner join deployments_containers on image_component_edges.ImageId = deployments_containers.Image_Id inner join deployments on deployments_containers.deployments_Id = deployments.Id where ((deployments.ClusterId = $1 and (deployments.Namespace = $2 or … or deployments.Namespace = $11)) and image_cves.Id = ANY($12::text[]))
+         *  select "image_cves".serialized
+         *   from image_cves
+         *   inner join image_component_cve_edges on image_cves.Id = image_component_cve_edges.ImageCveId
+         *   inner join image_component_edges on image_component_cve_edges.ImageComponentId = image_component_edges.ImageComponentId
+         *   inner join deployments_containers on image_component_edges.ImageId = deployments_containers.Image_Id
+         *   inner join deployments on deployments_containers.deployments_Id = deployments.Id
+         *   where (
+         *     (deployments.ClusterId = $1 and (deployments.Namespace = $2 or … or deployments.Namespace = $11))
+         *     and image_cves.Id = ANY($12::text[])
+         *   )
          */
 
         // GET v1/deploymentswithprocessinfo?pagination.offset=0&pagination.limit=6&pagination.sortOption.field=Deployment%20Risk%20Priority&pagination.sortOption.reversed=false
@@ -86,9 +185,16 @@ export function mainDashboard(host, headers, tags) {
         );
         /** DB Queries
          *  - 6 deployments in scope with highest risk
-         *  select deployments.Id::text as Deployment_ID from deployments where (deployments.ClusterId = $$ and (deployments.Namespace = $$ or deployments.Namespace = $$ or deployments.Namespace = $$ or deployments.Namespace = $$ or deployments.Namespace = $$ or deployments.Namespace = $$ or deployments.Namespace = $$ or deployments.Namespace = $$ or deployments.Namespace = $$ or deployments.Namespace = $$)) order by deployments.RiskScore desc LIMIT 6
+         *  select deployments.Id::text as Deployment_ID
+         *   from deployments
+         *   where
+         *     (deployments.ClusterId = $$ and (deployments.Namespace = $$ or ... or deployments.Namespace = $$))
+         *   order by deployments.RiskScore desc LIMIT 6
+         *
          *  - associated process data
-         *  select "process_baseline_results".serialized from process_baseline_results where process_baseline_results.DeploymentId = ANY($$::uuid[])
+         *  select "process_baseline_results".serialized
+         *   from process_baseline_results
+         *   where process_baseline_results.DeploymentId = ANY($$::uuid[])
          */
 
         // GET v1/deployments?pagination.offset=0&pagination.limit=6&pagination.sortOption.field=Deployment%20Risk%20Priority&pagination.sortOption.reversed=false
@@ -98,7 +204,11 @@ export function mainDashboard(host, headers, tags) {
         );
         /** DB Queries
          *  - 6 deployments in scope with highest risk
-         *  select deployments.Id::text as Deployment_ID from deployments where (deployments.ClusterId = $$ and (deployments.Namespace = $$ or deployments.Namespace = $$ or deployments.Namespace = $$ or deployments.Namespace = $$ or deployments.Namespace = $$ or deployments.Namespace = $$ or deployments.Namespace = $$ or deployments.Namespace = $$ or deployments.Namespace = $$ or deployments.Namespace = $$)) order by deployments.RiskScore desc LIMIT 6
+         *  select deployments.Id::text as Deployment_ID
+         *   from deployments
+         *   where
+         *     (deployments.ClusterId = $$ and (deployments.Namespace = $$ or deployments.Namespace = $$ or deployments.Namespace = $$ or deployments.Namespace = $$ or deployments.Namespace = $$ or deployments.Namespace = $$ or deployments.Namespace = $$ or deployments.Namespace = $$ or deployments.Namespace = $$ or deployments.Namespace = $$))
+         *   order by deployments.RiskScore desc LIMIT 6
          */
 
         http.post(
@@ -109,9 +219,24 @@ export function mainDashboard(host, headers, tags) {
         /** DB Queries
          *  - imageCount
          *  (query0..2 time ranges)
-         *  select count(distinct(images.Id)) from images inner join deployments_containers on images.Id = deployments_containers.Image_Id inner join deployments on deployments_containers.deployments_Id = deployments.Id where ((deployments.ClusterId = $1 and (deployments.Namespace = $2 or ... or deployments.Namespace = $11)) and images.Metadata_V1_Created > $12 and images.Metadata_V1_Created < $13)
+         *  select count(distinct(images.Id))
+         *   from images
+         *   inner join deployments_containers on images.Id = deployments_containers.Image_Id
+         *   inner join deployments on deployments_containers.deployments_Id = deployments.Id
+         *   where (
+         *     (deployments.ClusterId = $1 and (deployments.Namespace = $2 or ... or deployments.Namespace = $11))
+         *     and images.Metadata_V1_Created > $12 and images.Metadata_V1_Created < $13
+         *   )
+         *
          *  (query3 older than)
-         *  select count(distinct(images.Id)) from images inner join deployments_containers on images.Id = deployments_containers.Image_Id inner join deployments on deployments_containers.deployments_Id = deployments.Id where ((deployments.ClusterId = $1 and (deployments.Namespace = $2 or ... or deployments.Namespace = $11)) and images.Metadata_V1_Created <= $12)
+         *  select count(distinct(images.Id))
+         *   from images
+         *   inner join deployments_containers on images.Id = deployments_containers.Image_Id
+         *   inner join deployments on deployments_containers.deployments_Id = deployments.Id
+         *   where (
+         *     (deployments.ClusterId = $1 and (deployments.Namespace = $2 or ... or deployments.Namespace = $11))
+         *     and images.Metadata_V1_Created <= $12
+         *   )
          */
 
         // GET v1/alerts/summary/counts?request.query=&group_by=CATEGORY
@@ -121,7 +246,18 @@ export function mainDashboard(host, headers, tags) {
         );
         /** DB Queries
          *
-         *  select alerts.Id::text as Alert_ID, alerts.ClusterName, alerts.Policy_Severity, alerts.Policy_Categories from alerts where ((alerts.ClusterId = $$ and (alerts.Namespace = $$ or ... or alerts.Namespace = $$)) and (((alerts.ClusterName is not null and alerts.Policy_Severity is not null) and alerts.Policy_Categories is not null) and ((alerts.State = $$) or (alerts.State = $$))))
+         *  select alerts.Id::text as Alert_ID, alerts.ClusterName, alerts.Policy_Severity, alerts.Policy_Categories
+         *  from alerts
+         *  where (
+         *    (alerts.ClusterId = $$ and (alerts.Namespace = $$ or ... or alerts.Namespace = $$))
+         *    and (
+         *      (
+         *        (alerts.ClusterName is not null and alerts.Policy_Severity is not null)
+         *        and alerts.Policy_Categories is not null
+         *      )
+         *      and ((alerts.State = $$) or (alerts.State = $$))
+         *    )
+         *  )
          */
 
         http.post(
@@ -131,7 +267,9 @@ export function mainDashboard(host, headers, tags) {
         );
         /** DB Queries
          *  - complianceStandards ?
-         *  select "compliance_configs".serialized from compliance_configs where compliance_configs.StandardId = ANY($1::text[])
+         *  select "compliance_configs".serialized
+         *   from compliance_configs where
+         *   compliance_configs.StandardId = ANY($1::text[])
          *
          *  There probably is some caching involved here as well.
          */
