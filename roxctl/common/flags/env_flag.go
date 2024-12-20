f3e359f9d4915d0bc0bae06782bffe557d22ca0e
Randy Truong <rtruong@u.northwestern.edu>
2024-12-19 20:28:53 -0600

    feat: change precedence function definition to reflect inline values

--- roxctl/common/flags/env_flag.go
+++ roxctl/common/flags/env_flag.go
@@ -20,16 +20,20 @@ func flagOrSettingValue(flagValue string, flagChanged bool, setting env.Setting)
 
 // flagOrConfigurationValue will either return the following:
 //   - the flag value, if the flag value is not the default value (i.e. flagChanged != false).
-//   - the configuration value, if the flag value is the default value (i.e. flagChanged != false).
 //   - the setting's value, if the flag value is the default value (i.e. flag changed == false) _and_ the setting's value is non-empty.
-//   - the default value, if the flag value is the default value (i.e. flag changed != false) and the setting's value
-//     is empty.
-func flagOrConfigurationValue(flagValue string, flagChanged bool, configValue string, configValueChanged bool, setting env.Setting) string {
+//   - the configuration value, if the flag value is the default value and the setting's value is empty (i.e. flagChanged != false).
+//   - the default value, if the flag value is the default value (i.e. flag changed != false), the setting's value
+//     is empty, and there is no configuration value.
+func flagOrConfigurationValue(flagValue string, flagChanged bool, configInlineValue string, configInlineChanged bool, setting env.Setting) string {
 	if !flagChanged {
-
-		if configValueChanged {
-			return configValue
+		if setting.Setting() != "" {
+			return setting.Setting()
 		}
+
+		if configInlineChanged {
+			return configInlineValue
+		}
+	}
 
 		if setting.Setting() != "" {
 			return setting.Setting()
