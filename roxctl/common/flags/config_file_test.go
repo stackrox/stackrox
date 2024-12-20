12ab80380cacb0ad955382c25b736ebfa379a6c0
Randy Truong <rtruong@u.northwestern.edu>
2024-12-19 22:41:05 -0600

    test: kubeconfig-like test functions

--- roxctl/common/flags/config_file_test.go
+++ roxctl/common/flags/config_file_test.go
@@ -1,56 +1,181 @@
 package flags
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/pkg/errors"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestReadConfig(t *testing.T) {
+func TestNewReadConfig(t *testing.T) {
 
 	testCases := []struct {
-		configFile           string
-		expectedInstanceName string
-		expectedCaCertPath   string
-		expectedApiTokenFile string
-		expectedEndpoint     string
-		err                  string
+		configFile    string
+		configVersion string
+		instances     Instance
+		users         AuthInfo
+		contexts      Context
+		currContext   string
+		err           string
 	}{
 		{
-			configFile:           "./testdata/test_instance1.yaml",
-			expectedInstanceName: "profile-1",
-			expectedCaCertPath:   "./deploy/cert",
-			expectedApiTokenFile: "REDACTED",
-			expectedEndpoint:     "localhost:8000",
+			configFile:    "./testdata/test_config1.yaml",
+			configVersion: "v1",
+			instances: Instance{
+				InstanceName:          "endpoint",
+				Endpoint:              "hello",
+				CaCertificatePath:     "",
+				CaCertificate:         "",
+				Plaintext:             "",
+				DirectGRPC:            false,
+				ForceHTTP1:            false,
+				Insecure:              false,
+				InsecureSkipTLSVerify: false,
+			},
+			users: AuthInfo{
+				Username:         "test1",
+				Password:         "",
+				ApiToken:         "",
+				ApiTokenFilePath: "",
+			},
+			contexts: Context{
+				AuthInfo: "developer",
+				Instance: "endpoint",
+			},
+			currContext: "",
 		},
 		{
-			configFile:           "./testdata/test_instance2.yaml",
-			expectedInstanceName: "dev-environment",
-			expectedCaCertPath:   "/etc/ssl/certs",
-			expectedApiTokenFile: "/var/secrets/api-token",
-			expectedEndpoint:     "localhost:3000",
+			configFile:    "./testdata/test_config2.yaml",
+			configVersion: "1.0",
+			instances: Instance{
+				InstanceName:          "test-instance",
+				Endpoint:              "https://stackrox.example.com",
+				CaCertificatePath:     "/path/to/ca.crt",
+				CaCertificate:         "certificate-content",
+				Plaintext:             "some-text",
+				DirectGRPC:            true,
+				ForceHTTP1:            false,
+				Insecure:              false,
+				InsecureSkipTLSVerify: true,
+			},
+			users: AuthInfo{
+				Username:         "test-user",
+				Password:         "secure-password",
+				ApiToken:         "12345abcde",
+				ApiTokenFilePath: "/path/to/api/token",
+			},
+			contexts: Context{
+				AuthInfo: "test-user",
+				Instance: "test-instance",
+			},
+			currContext: "test-instance-context",
 		},
 		{
-			configFile:           "./testdata/test_instance3.yaml",
-			expectedInstanceName: "staging-profile",
-			expectedCaCertPath:   "./staging/certs",
-			expectedApiTokenFile: "staging-secret-token",
-			expectedEndpoint:     "staging.example.com:9000",
+			configFile:    "./testdata/test_config3.yaml",
+			configVersion: "1.0",
+			instances: Instance{
+				InstanceName:          "minimal-instance",
+				Endpoint:              "https://minimal.stackrox.example.com",
+				CaCertificate:         "",
+				CaCertificatePath:     "",
+				Plaintext:             "",
+				DirectGRPC:            false,
+				ForceHTTP1:            false,
+				Insecure:              false,
+				InsecureSkipTLSVerify: false,
+			},
+			users: AuthInfo{
+				Username:         "minimal-user",
+				Password:         "",
+				ApiToken:         "token-only",
+				ApiTokenFilePath: "",
+			},
+			contexts: Context{
+				AuthInfo: "minimal-user",
+				Instance: "minimal-instance",
+			},
+			currContext: "minimal-instance-context",
 		},
 		{
-			configFile:           "./testdata/test_instance4.yaml",
-			expectedInstanceName: "production-profile",
-			expectedCaCertPath:   "/usr/local/share/ca-certificates",
-			expectedApiTokenFile: "/home/user/.secrets/api-token",
-			expectedEndpoint:     "https://prod.stackrox.example.com",
+			configFile:    "./testdata/test_config4.yaml",
+			configVersion: "2.0",
+			instances: Instance{
+				InstanceName:          "insecure-instance",
+				Endpoint:              "https://insecure.stackrox.example.com",
+				CaCertificate:         "",
+				CaCertificatePath:     "",
+				Plaintext:             "",
+				DirectGRPC:            false,
+				ForceHTTP1:            false,
+				Insecure:              true,
+				InsecureSkipTLSVerify: true,
+			},
+			users: AuthInfo{
+				Username:         "insecure-user",
+				Password:         "",
+				ApiToken:         "",
+				ApiTokenFilePath: "/path/to/insecure/token",
+			},
+			contexts: Context{
+				AuthInfo: "insecure-user",
+				Instance: "insecure-instance",
+			},
+			currContext: "insecure-instance-context",
 		},
 		{
-			configFile:           "./testdata/test_instance5.yaml",
-			expectedInstanceName: "test-profile",
-			expectedCaCertPath:   "./test/certificates",
-			expectedApiTokenFile: "test-token-redacted",
-			expectedEndpoint:     "127.0.0.1:7000",
+			configFile:    "./testdata/test_config5.yaml",
+			configVersion: "2.1",
+			instances: Instance{
+				InstanceName:          "plaintext-grpc-instance",
+				Endpoint:              "https://grpc.stackrox.example.com",
+				CaCertificate:         "",
+				CaCertificatePath:     "",
+				Plaintext:             "plaintext-content",
+				DirectGRPC:            true,
+				ForceHTTP1:            false,
+				Insecure:              false,
+				InsecureSkipTLSVerify: false,
+			},
+			users: AuthInfo{
+				Username:         "grpc-user",
+				Password:         "grpc-secure-password",
+				ApiToken:         "",
+				ApiTokenFilePath: "",
+			},
+			contexts: Context{
+				AuthInfo: "grpc-user",
+				Instance: "plaintext-grpc-instance",
+			},
+			currContext: "plaintext-grpc-context",
+		},
+		{
+			configFile:    "./testdata/test_config6.yaml",
+			configVersion: "3.0",
+			instances: Instance{
+				InstanceName:          "full-instance",
+				Endpoint:              "https://full.stackrox.example.com",
+				CaCertificatePath:     "/path/to/full/ca.crt",
+				CaCertificate:         "full-certificate-content",
+				Plaintext:             "full-plaintext",
+				DirectGRPC:            false,
+				ForceHTTP1:            false,
+				Insecure:              false,
+				InsecureSkipTLSVerify: true,
+			},
+			users: AuthInfo{
+				Username:         "full-user",
+				Password:         "full-password",
+				ApiToken:         "full-api-token",
+				ApiTokenFilePath: "/path/to/full/api/token",
+			},
+			contexts: Context{
+				AuthInfo: "full-user",
+				Instance: "full-instance",
+			},
+			currContext: "full-instance-context",
 		},
 	}
 
