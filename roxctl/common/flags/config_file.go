fc3e81bfa080ff096621b5152ee87cb3476a1a25
Randy Truong <rtruong@u.northwestern.edu>
2024-12-19 20:25:23 -0600

    feat: kubeconfig-like structs in config_file.go

--- roxctl/common/flags/config_file.go
+++ roxctl/common/flags/config_file.go
@@ -17,17 +17,51 @@ import (
 )
 
 type Instance struct {
-	InstanceName      string `json:"name"`
-	CaCertificatePath string `json:"caCertPath,omitempty"`
-	CaCertificate     string `json:"caCert,omitempty"`
-	ApiTokenFilePath  string `json:"apiTokenPath,omitempty"`
-	ApiToken          string `json:"apiToken,omitempty"`
-	Endpoint          string `json:"endpoint,omitempty"`
+	InstanceName          string `json:"name"` // TODO(2): Is this the same as flags.endpoint.ServerName?
+	Endpoint              string `json:"endpoint,omitempty"`
+	CaCertificatePath     string `json:"caCertPath,omitempty"`
+	CaCertificate         string `json:"caCert,omitempty"`
+	Plaintext             string `json:"plaintext,omitempty"`
+	DirectGRPC            bool   `json:"directGrpc,omitempty"`
+	ForceHTTP1            bool   `json:"forceHttp,omitempty"`
+	Insecure              bool   `json:"insecure,omitempty"`
+	InsecureSkipTLSVerify bool   `json:"insecureSkipTLSVerify,omitempty"`
 }
 
+// Context is a tuple of references to a Stackrox instance (ie, how I communicate with a Stackrox
+// instance) and a user (ie, how I identify myself)
+type Context struct {
+	Instance string `json:"instance"`
+	AuthInfo string `json:"user"`
+}
+
+// AuthInfo contains information that describes identity information. This is used to tell the
+// Stackrox instance who you are.
+type AuthInfo struct {
+	// Username is the username for basic authentication to the Stackrox instance.
+	// +optional
+	Username string `json:"username"`
+	// Password is the password for basic authentication to the Stackrox instance.
+	// +optional
+	Password string `json:"password,omitempty"`
+	// ApiToken is an inline bearer token for authentication to the Stackrox instance.
+	// +optional
+	ApiToken string `json:"apiToken,omitempty"`
+	// ApiTokenFilePath is a pointer to a file that contains a bearer token (as described above).
+	// If both ApiToken and ApiTokenFilePath are present, then ApiToken takes precedence.
+	// +optional
+	ApiTokenFilePath string `json:"apiTokenPath,omitempty"` //
+}
+
+// InstanceConfig holds the information needed to connect to remote Stackrox instances as a
+// given user.
+// TODO: Enable users to store multiple instances, credentials, and contexts (maps?).
 type InstanceConfig struct {
-	Version  string   `json:"version"`
-	Instance Instance `yaml:"instance"`
+	Version     string    `json:"version"`
+	Instances   *Instance `json:"instances"`
+	AuthInfo    *AuthInfo `json:"users"`
+	Contexts    *Context  `json:"contexts"`        // TODO: Add context functionality
+	CurrContext string    `json:"current-context"` // TODO: Add current context functionality
 }
 
 var (
@@ -35,9 +69,31 @@ var (
 	configFileSet = pointers.Bool(false)
 	config        *InstanceConfig
 
-	configEndpointSet     = pointers.Bool(false)
-	configCaCertFileSet   = pointers.Bool(false)
-	configApiTokenFileSet = pointers.Bool(false)
+	// Flags related to Instance struct
+	configInstancesSet             = pointers.Bool(false) // Existence flag
+	configEndpointSet              = pointers.Bool(false)
+	configCaCertificatePathSet     = pointers.Bool(false)
+	configInlineCaCertificateSet   = pointers.Bool(false)
+	configPlaintextSet             = pointers.Bool(false)
+	configDirectGRPCSet            = pointers.Bool(false)
+	configForceHTTP1Set            = pointers.Bool(false)
+	configUseInsecureSet           = pointers.Bool(false)
+	configInsecureSkipTLSVerifySet = pointers.Bool(false)
+
+	// Flags related to AuthInfo struct
+	configAuthInfoSet         = pointers.Bool(false) // Existence flag
+	configUsernameSet         = pointers.Bool(false)
+	configPasswordSet         = pointers.Bool(false)
+	configApiTokenFilePathSet = pointers.Bool(false)
+	configInlineApiTokenSet   = pointers.Bool(false)
+
+	// Flags related to Context struct
+	configContextsSet         = pointers.Bool(false) // Existence flag
+	configContextInstanceSet  = pointers.Bool(false)
+	configContextsAuthInfoSet = pointers.Bool(false)
+
+	// Flags related to CurrContext
+	configCurrContextSet = pointers.Bool(false)
 
 	log = logging.LoggerForModule()
 )
@@ -63,46 +119,125 @@ func ConfigurationFileChanged() bool {
 	return configFileSet != nil && *configFileSet
 }
 
-// CaCertificatePath returns the configuration-defined CA Certificate path.
-func CaCertificatePath() string {
+func ConfigurationInstancesChanged() bool {
+	return ConfigurationFileChanged() && (configInstancesSet != nil && *configUseInsecureSet)
+}
+
+func ConfigurationAuthInfoChanged() bool {
+	return ConfigurationFileChanged() && (configAuthInfoSet != nil && *configAuthInfoSet)
+}
+
+func ConfigurationContextsChanged() bool {
+	return ConfigurationFileChanged() && (configContextsSet != nil && *configContextsSet)
+}
+
+func ConfigurationCurrContextChanged() bool {
+	return ConfigurationFileChanged() && (configCurrContextSet != nil && *configCurrContextSet)
+}
+
+// Endpoint returns the configuration-defined endpoint.
+func ConfigEndpoint() string {
 	if ConfigurationFileChanged() {
-		return config.Instance.CaCertificatePath
+		return config.Instances.Endpoint
 	}
 
 	return ""
 }
 
-// InlineCaCertificate returns the configuration-defined CA Certificate.
-func InlineCaCertificate() string {
-	if ConfigurationFileChanged() {
-		return config.Instance.CaCertificate
+// CaCertificatePath returns the configuration-defined CA Certificate path.
+func ConfigCaCertificatePath() string {
+	if ConfigurationAuthInfoChanged() {
+		return config.Instances.CaCertificatePath
+	}
+
+	return ""
+}
+
+// CaCertificate returns the configuration-defined inline CA Certificate.
+func ConfigInlineCaCertificate() string {
+	if ConfigurationContextsChanged() {
+		return config.Instances.CaCertificate
+	}
+
+	return ""
+}
+
+// Plaintext returns the configuration-defined plaintext.
+func ConfigPlaintext() string {
+	if ConfigurationContextsChanged() {
+		return config.Instances.Plaintext
+	}
+
+	return ""
+}
+
+// DirectGRPC returns the configuration-defined Direct GRPC option.
+func ConfigUseDirectGRPC() bool {
+	if ConfigurationContextsChanged() {
+		return config.Instances.DirectGRPC
+	}
+
+	return false // default value
+}
+
+// ForceHTTP1 returns the configuration-defined Force HTTP option.
+func ConfigForceHTTP1() bool {
+	if ConfigurationContextsChanged() {
+		return config.Instances.ForceHTTP1
+	}
+
+	return false
+}
+
+// ConfigUseInsecure returns the configuration-defined Insecure option.
+func ConfigUseInsecure() bool {
+	if ConfigurationContextsChanged() {
+		return config.Instances.Insecure
+	}
+
+	return false
+}
+
+// InsecureSkipTLSVerify returns the configuration-defined Insecure Skip TLS Verify option.
+func ConfigSkipTLSValidation() bool {
+	if ConfigurationContextsChanged() {
+		return config.Instances.InsecureSkipTLSVerify
+	}
+
+	return false
+}
+
+// Username returns the configuration-defined username.
+func ConfigUsername() string {
+	if ConfigurationAuthInfoChanged() {
+		return config.AuthInfo.Username
+	}
+
+	return ""
+}
+
+// Password returns the configuration-defined password.
+func ConfigPassword() string {
+	if ConfigurationAuthInfoChanged() {
+		return config.AuthInfo.Password
 	}
 
 	return ""
 }
 
 // ApiTokenFilePath returns the configuration-defined API Token file path.
-func ApiTokenFilePath() string {
+func ConfigApiTokenFilePath() string {
 	if ConfigurationFileChanged() {
-		return config.Instance.ApiTokenFilePath
+		return config.AuthInfo.ApiTokenFilePath
 	}
 
 	return ""
 }
 
 // InlineApiToken returns the configuration-defined ApiToken.
-func InlineApiToken() string {
+func ConfigInlineApiToken() string {
 	if ConfigurationFileChanged() {
-		return config.Instance.ApiToken
-	}
-
-	return ""
-}
-
-// Endpoint returns the configuration-defined endpoint.
-func Endpoint() string {
-	if ConfigurationFileChanged() {
-		return config.Instance.Endpoint
+		return config.AuthInfo.ApiToken
 	}
 
 	return ""
@@ -145,8 +280,8 @@ func checkFilePermissionsAndOwnership(path string) error {
 }
 
 // readConfig is a utilty function for reading YAML-based configuration files
-func readConfig(path string) (*Instance, error) {
-	var conf Instance
+func readConfig(path string) (*InstanceConfig, error) {
+	var conf InstanceConfig
 
 	err := checkFilePermissionsAndOwnership(path)
 
@@ -165,6 +300,14 @@ func readConfig(path string) (*Instance, error) {
 	if err := yaml.Unmarshal(bytes, &conf); err != nil {
 		return nil, errors.Wrapf(err, "failed to retrieve config from file %q", path)
 	}
+
+	fmt.Println("Here")
+
+	fmt.Printf("this is the configuration file struct: %+v\n", conf)
+	fmt.Printf("this is the instances struct: %+v\n", *conf.Instances)
+	fmt.Printf("this is the authinfo struct: %+v\n", *conf.AuthInfo)
+	fmt.Printf("this is the contexts struct: %+v\n", *conf.Contexts)
+	fmt.Printf("this is the current context: %+v\n", conf.CurrContext)
 
 	return &conf, nil
 }
@@ -186,24 +329,88 @@ func LoadConfig(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	config = &InstanceConfig{Instance: *instance}
+	config = instance
 
-	// TODO: Should it be file > flag > env?
-
-	if instance.Endpoint != "" {
-		endpoint = instance.Endpoint
-		configEndpointSet = pointers.Bool(true)
+	// TODO(2): Edit for when user submits multiple Instances
+	if config.Instances != nil {
+		configInstancesSet = pointers.Bool(true)
+		config.Instances.digest()
 	}
 
-	if instance.CaCertificatePath != "" {
-		caCertFile = instance.CaCertificatePath
-		configCaCertFileSet = pointers.Bool(true)
+	// TODO(2): Edit for when user submits multiple AuthInfo
+	if config.AuthInfo != nil {
+		configAuthInfoSet = pointers.Bool(true)
+		config.AuthInfo.digest()
 	}
 
-	if instance.ApiTokenFilePath != "" {
-		apiTokenFile = instance.ApiTokenFilePath
-		configApiTokenFileSet = pointers.Bool(true)
+	// TODO(2): Edit for when user submits multiple Contexts
+	if config.Contexts != nil {
+		configContextsSet = pointers.Bool(true)
+		config.Contexts.digest()
+	}
+
+	if config.CurrContext != "" {
+		configCurrContextSet = pointers.Bool(true)
 	}
 
 	return nil
+}
+
+// Instance.digest is a utility function that, given a particular instance, adjusts various flags
+func (i Instance) digest() {
+	if i.Endpoint != "" {
+		configEndpointSet = pointers.Bool(true)
+	}
+
+	if i.CaCertificatePath != "" {
+		configCaCertificatePathSet = pointers.Bool(true)
+	}
+	if i.CaCertificate != "" {
+		configInlineCaCertificateSet = pointers.Bool(true)
+	}
+	if i.Plaintext != "" {
+		configPlaintextSet = pointers.Bool(true)
+	}
+	if i.DirectGRPC {
+		configDirectGRPCSet = pointers.Bool(true)
+	}
+	if i.ForceHTTP1 {
+		configForceHTTP1Set = pointers.Bool(true)
+	}
+	if i.Insecure {
+		configUseInsecureSet = pointers.Bool(true)
+	}
+	if i.InsecureSkipTLSVerify {
+		configInsecureSkipTLSVerifySet = pointers.Bool(true)
+	}
+}
+
+// AuthInfo.digest is a utility function that, given particular AuthInfo, adjusts various flags
+func (a AuthInfo) digest() {
+	if a.Username != "" {
+		configUsernameSet = pointers.Bool(true)
+	}
+
+	if a.Password != "" {
+		configPasswordSet = pointers.Bool(true)
+	}
+
+	if a.ApiToken != "" {
+		configInlineApiTokenSet = pointers.Bool(true)
+	}
+
+	if a.ApiTokenFilePath != "" {
+		configApiTokenFilePathSet = pointers.Bool(true)
+	}
+}
+
+// Contexts.digest is a utility function that, given a particular Context, adjusts flags
+func (c Context) digest() {
+	if c.Instance != "" {
+		configContextInstanceSet = pointers.Bool(true)
+	}
+
+	if c.AuthInfo != "" {
+		configContextsAuthInfoSet = pointers.Bool(true)
+	}
 }
