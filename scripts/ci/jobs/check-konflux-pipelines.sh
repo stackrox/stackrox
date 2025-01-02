07c045ecd8b3e00037b78c4f0ed048073cdc6529
Tom Martensen <tmartens@redhat.com>
2025-01-02 09:40:03 +0100

    apply review comments to check-konflux-pipelines.sh

--- scripts/ci/jobs/check-konflux-pipelines.sh
+++ scripts/ci/jobs/check-konflux-pipelines.sh
@@ -9,19 +9,24 @@ ensure_create_snapshot_runs_last() {
     expected_runafter="$(yq eval '.spec.tasks[] | select(.name != "create-acs-style-snapshot") | .name' .tekton/operator-bundle-pipeline.yaml | sort)"
     actual_runafter="$(yq eval '.spec.tasks[] | select(.name == "create-acs-style-snapshot") | .runAfter[]' .tekton/operator-bundle-pipeline.yaml)"
 
-    if ! DIFF=$(diff <(echo "${expected_runafter}") <(echo "${actual_runafter}")); then
+    echo "➤ .tekton/operator-bundle-pipeline.yaml // create-acs-style-snapshot: task's runAfter contents shall match the expected ones (left - expected, right - actual)."
+    if ! diff --side-by-side <(echo "${expected_runafter}") <(echo "${actual_runafter}"); then
         echo >&2 -e """
-            ERROR:
-            Ensure that all previous tasks in the operator-bundle pipeline are mentioned
-            in the runAfter parameter for the create-acs-style-snapshot task.
+✗ ERROR:
 
-            This is what is different:
+The actual runAfter contents do not match the expectations.
+To resolve:
 
-            $DIFF
-            """
+1. Open .tekton/operator-bundle-pipeline.yaml and locate the create-acs-style-snapshot task
+2. Update the runAfter attribute of this task to this list of all previous tasks in the pipeline (sorted alphabetically):
+
+${expected_runafter}
+        """
         exit 1
+    else
+        echo "✓ No diff detected."
     fi
 }
 
-echo "Ensure that modifications to our Konflux pipelines follow our expectations and conventions"
+echo "Ensure consistency of our Konflux pipelines."
 ensure_create_snapshot_runs_last
