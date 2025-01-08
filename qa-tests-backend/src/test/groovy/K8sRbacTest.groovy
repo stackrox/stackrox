75c937dc3f6d6d3524fbab0e3a0d9560897490db
davdhacs <105243888+davdhacs@users.noreply.github.com>
2025-01-07 22:00:19 -0700

    reduce diff

--- qa-tests-backend/src/test/groovy/K8sRbacTest.groovy
+++ qa-tests-backend/src/test/groovy/K8sRbacTest.groovy
@@ -69,7 +69,7 @@ class K8sRbacTest extends BaseSpecification {
         "SR should have the same service accounts"
         // Make sure the qa namespace SA exists before running the test. That SA should be the most recent added.
         // This will ensure scraping is complete if this test spec is run first
-        withRetry(36, 5) {  // allow 3 minutes
+        withRetry(60, 3) {  // allow 3 minutes
             stackroxSAs = ServiceAccountService.getServiceAccounts()
             // list of service accounts from the orchestrator
             orchestratorSAs = orchestrator.getServiceAccounts()
