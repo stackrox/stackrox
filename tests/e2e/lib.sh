a91f0f5618f0a3642a4b49dda23aba290bcf3d6c
davdhacs <105243888+davdhacs@users.noreply.github.com>
2025-01-10 14:14:07 -0700

    list csrs

--- tests/e2e/lib.sh
+++ tests/e2e/lib.sh
@@ -1423,6 +1423,8 @@ setup_automation_flavor_e2e_cluster() {
     if [[ "$ci_job" =~ ^osd ]]; then
         info "Logging in to an OSD cluster"
         source "${SHARED_DIR}/dotenv"
+        cat "${SHARED_DIR}/dotenv"
+        set -x
         oc login "$CLUSTER_API_ENDPOINT" \
                 --username "$CLUSTER_USERNAME" \
                 --password "$CLUSTER_PASSWORD" \
@@ -1434,6 +1436,8 @@ setup_automation_flavor_e2e_cluster() {
                 --password "$CLUSTER_PASSWORD" \
                 --insecure-skip-tls-verify=true \
                 --loglevel 10
+        oc get csr -A || true
+        set +x
     fi
 }
 
