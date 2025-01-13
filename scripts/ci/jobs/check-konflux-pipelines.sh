bfe0d083b2b9a0a40937e9677634bec8970458f7
Tom Martensen <tmartens@redhat.com>
2025-01-13 11:13:36 +0100

    ROX-23316: check: confirm all components are part of snapshot (#13748)
    Co-authored-by: Misha Sugakov <537715+msugakov@users.noreply.github.com>

--- scripts/ci/jobs/check-konflux-pipelines.sh
+++ scripts/ci/jobs/check-konflux-pipelines.sh
@@ -5,6 +5,9 @@
 
 set -euo pipefail
 
+FAIL_FLAG="$(mktemp)"
+trap 'rm -f $FAIL_FLAG' EXIT
+
 ensure_create_snapshot_runs_last() {
     local pipeline_path=".tekton/operator-bundle-pipeline.yaml"
     local task_name="create-acs-style-snapshot"
@@ -24,11 +27,47 @@ To resolve:
 
 ${expected_runafter}
         "
-        exit 1
+        return 1
+    else
+        echo "✓ No diff detected."
+    fi
+}
+
+check_all_components_part_of_custom_snapshot() {
+    local pipeline_path=".tekton/operator-bundle-pipeline.yaml"
+    local task_name="create-acs-style-snapshot"
+
+    # Actual components are based on the COMPONENTS parameter and stored as sorted multi-line string.
+    actual_components="$(yq eval '.spec.tasks[] | select(.name == '\"${task_name}\"') | .params[] | select(.name == "COMPONENTS") | .value' "${pipeline_path}" | yq eval '.[].name' - | tr " " "\n" | sort)"
+    # Expected components are based on the wait-for-*-image task plus the operator-bundle and stored as a sorted multi-line string.
+    expected_components_from_images="$(yq eval '.spec.tasks[] | select(.name == "wait-for-*-image") | .name | sub("(wait-for-|-image)", "")' ${pipeline_path})"
+    expected_components=$(echo "${expected_components_from_images} operator-bundle" | tr " " "\n" | sort)
+
+    echo "➤ ${pipeline_path} // checking ${task_name}: COMPONENTS contents shall include all ACS images (left - expected, right - actual)."
+    if ! diff --side-by-side <(echo "${expected_components}") <(echo "${actual_components}"); then
+        echo >&2 -e "
+✗ ERROR:
+
+The actual COMPONENTS contents do not match the expectations.
+To resolve:
+
+1. Open ${pipeline_path} and locate the ${task_name} task
+2. Update the COMPONENTS parameter of this task to include entries for the missing components or delete references to removed components. COMPONENTS should include entries for (sorted alphabetically):
+
+${expected_components}
+        "
+        return 1
     else
         echo "✓ No diff detected."
     fi
 }
 
 echo "Ensure consistency of our Konflux pipelines."
-ensure_create_snapshot_runs_last
+ensure_create_snapshot_runs_last || { echo "ensure_create_snapshot_runs_last" >> "${FAIL_FLAG}"; }
+check_all_components_part_of_custom_snapshot || { echo "check_all_components_part_of_custom_snapshot" >> "${FAIL_FLAG}"; }
+
+if [[ -s "$FAIL_FLAG" ]]; then
+    echo >&2 "ERROR: Some Konflux pipeline consistency checks failed:"
+    cat >&2 "$FAIL_FLAG"
+    exit 1
+fi
