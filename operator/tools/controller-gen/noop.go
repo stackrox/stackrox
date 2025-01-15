689c275136e9b39858418ceedb381ebefa350b69
Tomasz Janiszewski <tomek@redhat.com>
2025-01-15 17:22:56 +0100

    chore: unify controller-gen
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

--- operator/tools/controller-gen/noop.go
+++
@@ -1,3 +0,0 @@
-package main
-
-// See https://github.com/stackrox/stackrox/pull/10125#discussion_r1507387689
--- operator/tools/controller-gen/tool.go
+++
@@ -1,7 +0,0 @@
-//go:build tools
-
-package main
-
-import (
-	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
-)
