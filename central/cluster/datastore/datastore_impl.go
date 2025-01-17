db26d042e4afd278be042a6a29eb5e61a8cffe23
Moritz Clasmeier <mclasmeier@redhat.com>
2025-01-17 10:21:01 +0100

    Central's cluster data store: Integrate support for registration counting.
    Only update cluster's InitBundleId with non-empty registrantIDs to keep the information of the registrantID which initially created this cluster.
    This is needed for the auto-revocation feature

--- central/cluster/datastore/datastore_impl.go
+++ central/cluster/datastore/datastore_impl.go
@@ -11,6 +11,7 @@ import (
 	"github.com/stackrox/rox/central/cluster/datastore/internal/search"
 	clusterStore "github.com/stackrox/rox/central/cluster/store/cluster"
 	clusterHealthStore "github.com/stackrox/rox/central/cluster/store/clusterhealth"
+	clusterInitStore "github.com/stackrox/rox/central/clusterinit/store"
 	compliancePruning "github.com/stackrox/rox/central/complianceoperator/v2/pruner"
 	clusterCVEDS "github.com/stackrox/rox/central/cve/cluster/datastore"
 	deploymentDataStore "github.com/stackrox/rox/central/deployment/datastore"
@@ -66,6 +67,7 @@ var (
 
 type datastoreImpl struct {
 	clusterStorage            clusterStore.Store
+	clusterInitStore          clusterInitStore.Store
 	clusterHealthStorage      clusterHealthStore.Store
 	clusterCVEDataStore       clusterCVEDS.DataStore
 	alertDataStore            alertDataStore.DataStore
@@ -877,6 +879,10 @@ func (ds *datastoreImpl) LookupOrCreateClusterFromConfig(ctx context.Context, cl
 		cluster = clusterByID
 	} else if clusterName != "" {
 		// At this point, we can be sure that the cluster does not exist.
+		if err := ds.clusterInitStore.RegistrationPossible(ctx, registrantID); err != nil {
+			return nil, err
+		}
+
 		cluster = &storage.Cluster{
 			Name:               clusterName,
 			InitBundleId:       registrantID,
@@ -892,6 +898,11 @@ func (ds *datastoreImpl) LookupOrCreateClusterFromConfig(ctx context.Context, cl
 
 		if _, err := ds.addClusterNoLock(ctx, cluster); err != nil {
 			return nil, errors.Wrapf(err, "failed to dynamically add cluster with name %q", clusterName)
+		}
+
+		if err := ds.clusterInitStore.RecordRegistration(ctx, registrantID); err != nil {
+			log.Warnf("cluster %q has been added, but recording the cluster registration using registrant ID %q failed", clusterName, registrantID)
+			return nil, errors.Wrapf(err, "recording cluster registration for registrant ID %q", registrantID)
 		}
 	} else {
 		return nil, errors.New("neither a cluster ID nor a cluster name was specified")
@@ -940,7 +951,9 @@ func (ds *datastoreImpl) LookupOrCreateClusterFromConfig(ctx context.Context, cl
 
 	cluster = cluster.CloneVT()
 	cluster.ManagedBy = manager
-	cluster.InitBundleId = registrantID
+	if registrantID != "" {
+		cluster.InitBundleId = registrantID
+	}
 	cluster.SensorCapabilities = sliceutils.CopySliceSorted(hello.GetCapabilities())
 	if securedClusterIsNotManagedManually(helmConfig) {
 		configureFromHelmConfig(cluster, clusterConfig)
