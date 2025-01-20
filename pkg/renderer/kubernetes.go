8689ea2e0bca0ba7b493dbf2579b90484d97ddd8
Charmik Sheth <101146970+charmik-redhat@users.noreply.github.com>
2025-01-20 10:59:13 -0800

    Fix cert gen issues

--- pkg/renderer/kubernetes.go
+++ pkg/renderer/kubernetes.go
@@ -35,6 +35,8 @@ const (
 	scannerOnly
 	// centralTLSOnly renders only the central tls secret.
 	centralTLSOnly
+	// centralDBTLSOnly renders only the central-db tls secret
+	centralDBTLSOnly
 	// scannerTLSOnly renders only the scanner tls secret
 	scannerTLSOnly
 	// centralDBOnly renders only the central db
@@ -74,7 +76,7 @@ func postProcessConfig(c *Config, mode mode, imageFlavor defaults.ImageFlavor) e
 		c.HelmImage = image.GetDefaultImage()
 	}
 
-	if mode == centralTLSOnly || mode == scannerTLSOnly || mode == scannerV4TLSOnly {
+	if mode == centralTLSOnly || mode == scannerTLSOnly || mode == scannerV4TLSOnly || mode == centralDBTLSOnly {
 		return nil
 	}
 	if c.ClusterType == storage.ClusterType_KUBERNETES_CLUSTER {
