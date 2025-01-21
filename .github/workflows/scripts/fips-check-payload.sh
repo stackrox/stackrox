aafa39d60f5f08b74d3585f1e8b9cac459eceb38
davdhacs <105243888+davdhacs@users.noreply.github.com>
2025-01-20 20:30:35 -0700

    back to brew

--- .github/workflows/scripts/fips-check-payload.sh
+++ .github/workflows/scripts/fips-check-payload.sh
@@ -16,7 +16,6 @@ GITHUB_STEP_SUMMARY=${GITHUB_STEP_SUMMARY:-/dev/null}
 
 image_prefix="${1:-}"
 default_image_prefix='brew.registry.redhat.io/rh-osbs/rhacs'
-default_image_prefix='quay.io/rhacs-eng/'
 #default_image_prefix='registry.redhat.io/advanced-cluster-security/rhacs'
 image_prefix="${image_prefix:-${default_image_prefix}}"
 
