11cd7c2cfc2d983afbdca217b6a52a7a531bf7af
Stephan Heßelmann <shesselm@redhat.com>
2025-01-23 17:17:16 +0100

    call Error() to be on the safe side

--- pkg/providers/metadata.go
+++ pkg/providers/metadata.go
@@ -37,7 +37,7 @@ func GetMetadata(ctx context.Context) *storage.ProviderMetadata {
 	errors.AddWrap(err, "Azure")
 
 	if err := errors.ToError(); err != nil {
-		log.Warn(err)
+		log.Warn(err.Error())
 	}
 
 	return nil
