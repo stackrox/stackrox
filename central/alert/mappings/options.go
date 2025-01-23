cf6020de1216b8bbc779efba728de7ceffc95b93
AJ Heflin <aheflin@redhat.com>
2025-01-23 14:53:22 -0500

    Update options.go to remove extraneous code

--- central/alert/mappings/options.go
+++ central/alert/mappings/options.go
@@ -3,7 +3,6 @@ package mappings
 import (
 	v1 "github.com/stackrox/rox/generated/api/v1"
 	"github.com/stackrox/rox/generated/storage"
-	"github.com/stackrox/rox/pkg/postgres/schema"
 	"github.com/stackrox/rox/pkg/search"
 )
 
@@ -18,16 +17,4 @@ func init() {
 	for _, opt := range optionsToRemove {
 		OptionsMap.Remove(opt)
 	}
-	alertOptions := schema.AlertsSchema.OptionsMap.Clone()
-
-	// There are more search terms in the alert proto due to the embeddings of policies.
-	// This pruning of options ensures that the search options are stable between RocksDB and Postgres
-	// while also ensuring that highlights work
-	for opt := range alertOptions.Original() {
-		if _, ok := OptionsMap.Get(string(opt)); !ok {
-			alertOptions.Remove(opt)
-		}
-	}
-	OptionsMap = alertOptions
-
 }
