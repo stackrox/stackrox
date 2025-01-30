519883b450aeb3f8ab408baba0c7cb45020a96d0
JoukoVirtanen <jouko@stackrox.com>
2025-01-30 14:41:23 -0800

    Trying to fix import paths

--- tests/performance/tests/testK6Integration.js
+++ tests/performance/tests/testK6Integration.js
@@ -2,10 +2,10 @@
 import { jUnit, textSummary } from 'https://jslib.k6.io/k6-summary/0.0.2/index.js';
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
-import { getHeaderWithAdminPass } from '../src/utils.js';
+import { getHeaderWithAdminPass } from '../load/src/utils.js';
 import { mainDashboard } from '../groups/mainDashboard.js';
 
-import { defaultOptions } from '../src/options.js';
+import { defaultOptions } from '../load/src/options.js';
 
 // k6 options.
 export const options = defaultOptions;
