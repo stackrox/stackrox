c8f02221bde5ac5d083e185c9fbcda1c1af6c61a
JoukoVirtanen <jouko@stackrox.com>
2025-01-31 13:31:57 -0800

    Working on getting the runtime configuration from collector

--- qa-tests-backend/src/main/groovy/util/CollectorUtil.groovy
+++ qa-tests-backend/src/main/groovy/util/CollectorUtil.groovy
@@ -4,6 +4,9 @@ import groovy.util.logging.Slf4j
 
 import orchestratormanager.OrchestratorMain
 
+import java.net.HttpURLConnection
+import java.net.URL
+
 @Slf4j
 class CollectorUtil {
     static final String RUNTIME_CONFIG_MAP_NAME = "collector-config"
@@ -15,14 +18,20 @@ class CollectorUtil {
 
     static enableExternalIps(OrchestratorMain orchestrator) {
         setExternalIps(orchestrator, ENABLED_VALUE)
+        def result = introspectionQuery("127.0.0.1", "/state/runtime-config")
+        println new String(result)
     }
 
     static disableExternalIps(OrchestratorMain orchestrator) {
         setExternalIps(orchestrator, DISABLED_VALUE)
+        def result = introspectionQuery("127.0.0.1", "/state/runtime-config")
+        println new String(result)
     }
 
     static deleteRuntimeConfig(OrchestratorMain orchestrator) {
         orchestrator.deleteConfigMap(RUNTIME_CONFIG_MAP_NAME, "stackrox")
+        def result = introspectionQuery("127.0.0.1", "/state/runtime-config")
+        println new String(result)
     }
 
     static private setExternalIps(OrchestratorMain orchestrator, String state) {
@@ -37,4 +46,34 @@ networking:
 
         orchestrator.createConfigMap(RUNTIME_CONFIG_MAP_NAME, data, "stackrox")
     }
+
+
+    def introspectionQuery(String collectorIP, String endpoint) {
+        def uri = "http://${collectorIP}:8080${endpoint}"
+        def connection = new URL(uri).openConnection() as HttpURLConnection
+    
+        try {
+            connection.requestMethod = "GET"
+            connection.connect()
+    
+            if (connection.responseCode != HttpURLConnection.HTTP_OK) {
+                throw new RuntimeException("IntrospectionQuery failed with ${connection.responseMessage}")
+            }
+    
+            return connection.inputStream.bytes
+        } catch (Exception e) {
+            throw new RuntimeException("Error making request: ${e.message}", e)
+        } finally {
+            connection.disconnect()
+        }
+    }
+    
+   //// Example usage:
+   //try {
+   //    def result = introspectionQuery("127.0.0.1", "/some-endpoint")
+   //    println new String(result)
+   //} catch (Exception e) {
+   //    println "Error: ${e.message}"
+   //}
+
 }
