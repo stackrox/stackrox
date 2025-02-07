071c0c2d894c29321de29df2e0756679a0e7ce2a
Mark Pedrotti <mark@stackrox.com>
2025-02-07 15:46:59 -0500

    ROX-27916: Update getVersionedDocs and docs links last changed in 4.4

--- ui/apps/platform/src/utils/versioning.ts
+++ ui/apps/platform/src/utils/versioning.ts
@@ -9,9 +9,13 @@ function getVersionMajorMinor(version: string): string {
     return '';
 }
 
+const basePath =
+    'https://docs.redhat.com/en/documentation/red_hat_advanced_cluster_security_for_kubernetes';
+
 // we may have to consider the release build in the future, where the version may be ahead of documentation that has not yet been created
-function getVersionedDocs(completeVersion: string, subPath = 'welcome/index.html'): string {
-    return `https://docs.openshift.com/acs/${getVersionMajorMinor(completeVersion)}/${subPath}`;
+function getVersionedDocs(completeVersion: string, subPath?: string): string {
+    const basePathWithVersion = `${basePath}/${getVersionMajorMinor(completeVersion)}`;
+    return subPath ? `${basePathWithVersion}/html/${subPath}` : basePathWithVersion;
 }
 
 export { getVersionMajorMinor, getVersionedDocs };
