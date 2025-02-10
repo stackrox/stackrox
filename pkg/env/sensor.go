6a15c1e89a503cddf0681d4f906e2f9b854b85b6
Luis Valero Martín <lvalerom@redhat.com>
2025-02-10 15:38:11 +0100

    feat(sensor): Make the responses channel buffered (#13925)

--- pkg/env/sensor.go
+++ pkg/env/sensor.go
@@ -81,4 +81,8 @@ var (
 
 	// SensorComplianceChannelBufferSize defines how many node scanning ACK messages may be buffered before sending them to Compliance
 	SensorComplianceChannelBufferSize = RegisterIntegerSetting("ROX_SENSOR_COMPLIANCE_CHANNEL_BUFFER_SIZE", 2)
+
+	// ResponsesChannelBufferSize defines how many messages to central are we buffering before dropping messages
+	// Setting this variable to zero will disable this feature.
+	ResponsesChannelBufferSize = RegisterIntegerSetting("ROX_RESPONSES_CHANNEL_BUFFER_SIZE", 0)
 )
