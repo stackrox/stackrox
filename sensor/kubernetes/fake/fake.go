ff45679aa24bb92d7d84acf4c4ea0bdb417a9390
Jan Schnathmeier <jschnath@redhat.com>
2025-02-11 16:45:03 +0100

    Disable manageflows go routine

--- sensor/kubernetes/fake/fake.go
+++ sensor/kubernetes/fake/fake.go
@@ -266,5 +266,5 @@ func (w *WorkloadManager) initializePreexistingResources() {
 		go w.manageNetworkPolicy(context.Background(), resource)
 	}
 
-	go w.manageFlows(context.Background(), w.workload.NetworkWorkload)
+	//go w.manageFlows(context.Background(), w.workload.NetworkWorkload)
 }
