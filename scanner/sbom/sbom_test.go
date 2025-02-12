d50e734b696b13489a33f7380a3b30a4b6f3d22a
David Caravello <119438707+dcaravel@users.noreply.github.com>
2025-02-11 19:36:25 -0600

    fix test description

--- scanner/sbom/sbom_test.go
+++ scanner/sbom/sbom_test.go
@@ -9,14 +9,14 @@ import (
 )
 
 func TestGetSBOM(t *testing.T) {
-	t.Run("error on nil opts", func(t *testing.T) {
+	t.Run("error on nil index report", func(t *testing.T) {
 		s := NewSBOMer()
 		_, err := s.GetSBOM(context.Background(), nil, nil)
 
 		assert.ErrorContains(t, err, "index report is required")
 	})
 
-	t.Run("error on nil index report", func(t *testing.T) {
+	t.Run("error on nil opts", func(t *testing.T) {
 		ir := &claircore.IndexReport{}
 		s := NewSBOMer()
 		_, err := s.GetSBOM(context.Background(), ir, nil)
