97e83bcfd4f64dcbbe5e6e314d0cd69517637bbc
lvalerom <lvalerom@redhat.com>
2025-02-12 10:09:05 +0100

    make the queue unbound by default

--- pkg/env/sensor.go
+++ pkg/env/sensor.go
@@ -75,7 +75,7 @@ var (
 	// 20000 * 1000 = 20 MB
 	// Notice: the actual size of each item is ~40 bytes since it holds pointers to the actual objects.
 	// Multiple items can hold a pointer to the same object (e.g. same Deployment) so these numbers are pessimistic because we assume all items hold different objects.
-	DetectorDeploymentBufferSize = RegisterIntegerSetting("ROX_SENSOR_DETECTOR_DEPLOYMENT_BUFFER_SIZE", 20000)
+	DetectorDeploymentBufferSize = RegisterIntegerSetting("ROX_SENSOR_DETECTOR_DEPLOYMENT_BUFFER_SIZE", 0)
 
 	// BufferScaleCeiling sets the upper limit queue.ScaleSize will scale buffers and queues to.
 	// In its default, the ceiling is defined as triple the relative size.
