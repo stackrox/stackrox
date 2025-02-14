d1099631992b1ad85a57fcbd97dea6409a2e62ed
Charmik Sheth <101146970+charmik-redhat@users.noreply.github.com>
2025-02-14 09:19:59 -0800

    ROX-24072: Expose central-db cert expiry in v1/credentialexpiry (#13870)
    Co-authored-by: Vlad Bologa <vbologa@redhat.com>
    (cherry picked from commit eaee48e4756ebe27d3362829978b95fb19ea4634)

--- central/credentialexpiry/service/service_impl.go
+++ central/credentialexpiry/service/service_impl.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"crypto/tls"
 	"crypto/x509"
+	"encoding/binary"
 	"fmt"
+	"io"
+	"net"
 	"sort"
 	"strings"
 	"time"
@@ -14,6 +17,7 @@ import (
 	iiDStore "github.com/stackrox/rox/central/imageintegration/datastore"
 	iiStore "github.com/stackrox/rox/central/imageintegration/store"
 	v1 "github.com/stackrox/rox/generated/api/v1"
+	"github.com/stackrox/rox/pkg/clientconn"
 	"github.com/stackrox/rox/pkg/errorhelpers"
 	"github.com/stackrox/rox/pkg/errox"
 	"github.com/stackrox/rox/pkg/features"
@@ -21,6 +25,7 @@ import (
 	"github.com/stackrox/rox/pkg/grpc/authz/perrpc"
 	"github.com/stackrox/rox/pkg/grpc/authz/user"
 	"github.com/stackrox/rox/pkg/mtls"
+	"github.com/stackrox/rox/pkg/postgres/pgconfig"
 	"github.com/stackrox/rox/pkg/protocompat"
 	"github.com/stackrox/rox/pkg/scanners/scannerv4"
 	"github.com/stackrox/rox/pkg/tlsutils"
@@ -54,6 +59,8 @@ func (s *serviceImpl) GetCertExpiry(ctx context.Context, request *v1.GetCertExpi
 		return s.getScannerCertExpiry(ctx)
 	case v1.GetCertExpiry_SCANNER_V4:
 		return s.getScannerV4CertExpiry(ctx)
+	case v1.GetCertExpiry_CENTRAL_DB:
+		return s.getCentralDBCertExpiry()
 	}
 	return nil, errors.Wrapf(errox.InvalidArgs, "invalid component: %v", request.GetComponent())
 }
@@ -63,6 +70,7 @@ func (s *serviceImpl) getCentralCertExpiry() (*v1.GetCertExpiry_Response, error)
 	if err != nil {
 		return nil, errors.Errorf("failed to retrieve leaf certificate: %v", err)
 	}
+
 	if len(cert.Certificate) == 0 {
 		return nil, errors.New("no central cert found")
 	}
@@ -75,6 +83,87 @@ func (s *serviceImpl) getCentralCertExpiry() (*v1.GetCertExpiry_Response, error)
 		return nil, errors.Errorf("failed to convert timestamp: %v", err)
 	}
 	return &v1.GetCertExpiry_Response{Expiry: expiry}, nil
+}
+
+func (s *serviceImpl) getCentralDBCertExpiry() (*v1.GetCertExpiry_Response, error) {
+	if pgconfig.IsExternalDatabase() {
+		return nil, nil
+	}
+	pgConfigMap, _, err := pgconfig.GetPostgresConfig()
+	if err != nil {
+		return nil, errors.Wrap(err, "Error reading central db config")
+	}
+	if pgConfigMap == nil {
+		return nil, errors.Wrap(errox.NotFound, "Central db config not found")
+	}
+
+	host, ok := pgConfigMap["host"]
+	if !ok {
+		return nil, errors.Wrap(errox.InvalidArgs, "'host' parameter not defined in central db config")
+	}
+	port, ok := pgConfigMap["port"]
+	if !ok {
+		return nil, errors.Wrap(errox.InvalidArgs, "'port' parameter not defined in central db config")
+	}
+	endpoint := fmt.Sprintf("%s:%s", host, port)
+
+	conn, err := net.Dial("tcp", endpoint)
+	if err != nil {
+		return nil, errors.Wrapf(err, "Failed to dial central db on endpoint '%s'", endpoint)
+	}
+	defer utils.IgnoreError(conn.Close)
+
+	tlsConfig, err := clientconn.TLSConfig(mtls.CentralDBSubject, clientconn.TLSConfigOptions{
+		InsecureSkipVerify: true,
+	})
+	if err != nil {
+		return nil, errors.Wrapf(err, "Failed to initialize TLS config for %q", mtls.CentralDBSubject.Identifier)
+	}
+	tlsConn, err := tlsConnectToCentralDB(conn, tlsConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	certs := tlsConn.ConnectionState().PeerCertificates
+	if len(certs) == 0 {
+		return nil, errors.Errorf("%q at %s returned no peer certs", mtls.CentralDBSubject.Identifier, endpoint)
+	}
+	leafCert := certs[0]
+	if leafCert == nil {
+		return nil, nil
+	}
+	if cn := leafCert.Subject.CommonName; cn != mtls.CentralDBSubject.CN() {
+		return nil, errors.Errorf("common name of %q at %s (%s) is not as expected", mtls.CentralDBSubject.Identifier, endpoint, cn)
+	}
+	if leafCert.NotAfter.IsZero() {
+		return nil, nil
+	}
+	certExpiry, err := protocompat.ConvertTimeToTimestampOrError(leafCert.NotAfter)
+	if err != nil {
+		return nil, err
+	}
+	return &v1.GetCertExpiry_Response{Expiry: certExpiry}, nil
+}
+
+// tlsConnectToCentralDB implements the protocol to establish a TLS connection to a postgres database server
+func tlsConnectToCentralDB(conn net.Conn, tlsConfig *tls.Config) (*tls.Conn, error) {
+	err := binary.Write(conn, binary.BigEndian, []int32{8, 80877103})
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to send initiation message to central db")
+	}
+	response := make([]byte, 1)
+	if _, err = io.ReadFull(conn, response); err != nil {
+		return nil, errors.Wrap(err, "Failed to receive a reply from central db")
+	}
+	if response[0] != 'S' {
+		return nil, errors.New("Central db refused TLS connection")
+	}
+	client := tls.Client(conn, tlsConfig)
+	err = client.Handshake()
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed TLS handshake with central db")
+	}
+	return client, nil
 }
 
 // ensureTLSAndReturnAddr returns an address from endpoint that can be passed to tls.Dial,
