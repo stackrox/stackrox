bd5374342341c2e0c8b55494293566212b0a26cc
Dave Shrewsberry <dashrews@redhat.com>
2025-02-14 08:44:56 -0500

    updating to 4.6

--- pkg/migrations/internal/fallback_seq_num.go
+++ pkg/migrations/internal/fallback_seq_num.go
@@ -5,5 +5,5 @@ var (
 	// that is supported by this database.  This is used in case of rollbacks in
 	// the event that a major change introduced an incompatible schema update we
 	// can inform that a rollback below this is not supported by the database
-	MinimumSupportedDBVersionSeqNum = 206
+	MinimumSupportedDBVersionSeqNum = 209
 )
