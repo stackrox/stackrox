52f45f0102aefcb2e21cc51b0a1e8a684f738bf5
Tomasz Janiszewski <tomek@redhat.com>
2025-02-18 16:23:18 +0100

    @CompileStatic
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

--- qa-tests-backend/src/main/groovy/util/Helpers.groovy
+++ qa-tests-backend/src/main/groovy/util/Helpers.groovy
@@ -1,5 +1,8 @@
 package util
 
+import groovy.transform.CompileStatic
+import io.fabric8.kubernetes.client.KubernetesClientException
+
 import common.Constants
 
 import groovy.util.logging.Slf4j
@@ -16,6 +19,7 @@ import org.spockframework.runtime.SpockAssertionError
 
 // Helpers defines useful helper methods. Is mixed in to every object in order to be visible everywhere.
 @Slf4j
+@CompileStatic
 class Helpers {
     static <V> V evaluateWithRetry(int retries, int pauseSecs, Closure<V> closure) {
         for (int i = 0; i < retries; i++) {
@@ -37,7 +41,7 @@ class Helpers {
         for (int i = 0; i < retries; i++) {
             try {
                 return closure()
-            } catch (io.fabric8.kubernetes.client.KubernetesClientException t) {
+            } catch (KubernetesClientException t) {
                 log.debug("Caught k8 client exception. Retrying in ${pauseSecs}s", t)
             }
             sleep pauseSecs * 1000
@@ -75,11 +79,6 @@ class Helpers {
 
     static String getStackRoxEndpoint(Object ignored) {
         return "https://" + Env.mustGetHostname() + ":" + Env.mustGetPort()
-    }
-
-    // withDo is like with, but returns a void so can safely be used in tests.
-    static void withDo(Object self, Closure closure) {
-        self.with(closure)
     }
 
     static void collectDebugForFailure(Throwable exception) {
@@ -137,10 +136,11 @@ class Helpers {
         log.debug "Will scan ${image} to ${saveName}"
 
         try {
-            Path imageScans = Paths.get(Env.QA_TEST_DEBUG_LOGS).resolve("image-scans")
+            Path p = Paths.get(Env.QA_TEST_DEBUG_LOGS)
+            Path imageScans = p.resolve('image-scans')
             new File(imageScans.toAbsolutePath().toString()).mkdirs()
 
-            Process proc = "./scripts/ci/roxctl.sh image scan -i ${image} -a".execute(null, new File(".."))
+            Process proc = "./scripts/ci/roxctl.sh image scan -i ${image} -a".execute([], new File(".."))
             String output = imageScans.resolve(saveName).toAbsolutePath()
             FileWriter sout = new FileWriter(output)
             StringBuilder serr = new StringBuilder()
@@ -161,16 +161,18 @@ class Helpers {
     }
 
     static void shellCmd(String cmd) {
-        def sout = new StringBuilder(), serr = new StringBuilder()
-        def proc = cmd.execute(null, new File(".."))
+        StringBuilder sout = new StringBuilder()
+        StringBuilder serr = new StringBuilder()
+        Process proc = cmd.execute([], new File(".."))
         proc.consumeProcessOutput(sout, serr)
         proc.waitFor()
         log.debug "Ran: ${cmd}\nExit: ${proc.exitValue()}\nStdout: $sout\nStderr: $serr"
     }
 
     static int shellCmdExitValue(String cmd) {
-        def sout = new StringBuilder(), serr = new StringBuilder()
-        def proc = cmd.execute(null, new File(".."))
+        StringBuilder sout = new StringBuilder()
+        StringBuilder serr = new StringBuilder()
+        Process proc = cmd.execute([], new File(".."))
         proc.consumeProcessOutput(sout, serr)
         proc.waitFor()
         log.debug "Ran: ${cmd}\nExit: ${proc.exitValue()}\nStdout: $sout\nStderr: $serr"
@@ -196,9 +198,9 @@ class Helpers {
             return
         }
 
-        Map<String, String> orchestratorTruncated = orchestratorAnnotations.clone()
+        Map<String, String> orchestratorTruncated = new HashMap<>(orchestratorAnnotations)
         Map<String, String> stackroxTruncated = new HashMap<>(stackroxAnnotations)
-        orchestratorAnnotations.keySet().each { name ->
+        orchestratorAnnotations.keySet().each {  String name ->
             if (orchestratorTruncated[name].length() > Constants.STACKROX_ANNOTATION_TRUNCATION_LENGTH) {
                 // Assert that the stackrox node has an entry for that annotation
                 assert stackroxTruncated.get(name) && stackroxTruncated[name].length() > 0
