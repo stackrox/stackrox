3463891e6d838a9d4a3ca9cb531ca71b8440d52a
Tomasz Janiszewski <tomek@redhat.com>
2025-02-19 13:42:52 +0100

    style
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

--- qa-tests-backend/src/main/groovy/common/Constants.groovy
+++ qa-tests-backend/src/main/groovy/common/Constants.groovy
@@ -30,9 +30,11 @@ class Constants {
     ]
     static final Map<String, List<String>> VIOLATIONS_ALLOWLIST = [
             // TODO(ROX-2659) Remove the fixable CVSS one from here, that's not okay.
-            "monitoring"          : ["CVSS >= 7", "Ubuntu Package Manager in Image", "Curl in Image", "Fixable CVSS >= 7",
-                                     ANY_FIXED_VULN_POLICY, "90-Day Image Age", "Fixable Severity at least Important"],
-            "scanner"             : ["Red Hat Package Manager Execution", "Red Hat Package Manager in Image", "Curl in Image"],
+            "monitoring"          : ["CVSS >= 7", "Ubuntu Package Manager in Image", "Curl in Image",
+                                     "Fixable CVSS >= 7", ANY_FIXED_VULN_POLICY, "90-Day Image Age",
+                                     "Fixable Severity at least Important"],
+            "scanner"             : ["Red Hat Package Manager Execution", "Red Hat Package Manager in Image",
+                                     "Curl in Image"],
             "collector"           : ["Ubuntu Package Manager in Image"],
             "authorization-plugin": ["Latest tag", "90-Day Image Age"],
             "webhookserver"       : ["90-Day Image Age"],
