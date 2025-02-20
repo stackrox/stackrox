f33c0d80a69435031d0d16525cb874b3fb0c591e
Stephan Heßelmann <shesselm@redhat.com>
2025-02-20 14:46:17 +0100

    var -> const

--- central/apitoken/datastore/types.go
+++ central/apitoken/datastore/types.go
@@ -1,4 +1,4 @@
 package datastore
 
 // The timestamp format / layout is borrowed from `pkg/search/postgres/query/time_query.go`.
-var TimestampLayout = "01/02/2006 3:04:05 PM MST"
+const TimestampLayout = "01/02/2006 3:04:05 PM MST"
