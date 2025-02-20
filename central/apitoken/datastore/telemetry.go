8e346cc4782ccf345a5ee35765dd876deaef2b5d
Stephan Heßelmann <shesselm@redhat.com>
2025-02-20 14:43:23 +0100

    use AddTotal

--- central/apitoken/datastore/telemetry.go
+++ central/apitoken/datastore/telemetry.go
@@ -2,10 +2,8 @@ package datastore
 
 import (
 	"context"
-	"fmt"
 	"time"
 
-	"github.com/pkg/errors"
 	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/pkg/sac"
 	"github.com/stackrox/rox/pkg/sac/resources"
@@ -22,46 +20,33 @@ func Gather(ds DataStore) phonehome.GatherFunc {
 				sac.ResourceScopeKeys(resources.Integration),
 			),
 		)
-		numTotalTokens, err := ds.Count(ctx, search.EmptyQuery())
-		if err != nil {
-			return nil, errors.Wrap(err, "counting all api tokens")
-		}
-
+		props := map[string]any{}
+		_ = phonehome.AddTotal(ctx, props, "API Tokens", func(ctx context.Context) (int, error) {
+			return ds.Count(ctx, search.EmptyQuery())
+		})
 		formattedNow := time.Now().Format(TimestampLayout)
-		numExpiredTokens, err := ds.Count(ctx,
-			search.NewQueryBuilder().
-				AddStrings(search.Expiration, fmt.Sprintf("<%s", formattedNow)).
-				ProtoQuery(),
-		)
-		if err != nil {
-			return nil, errors.Wrap(err, "counting expired api tokens")
-		}
-
-		numRevokedTokens, err := ds.Count(ctx,
-			search.NewQueryBuilder().
-				AddBools(search.Revoked, true).
-				ProtoQuery(),
-		)
-		if err != nil {
-			return nil, errors.Wrap(err, "counting revoked api tokens")
-		}
-
-		numValidTokens, err := ds.Count(ctx,
-			search.NewQueryBuilder().
-				AddBools(search.Revoked, false).
-				AddStrings(search.Expiration, fmt.Sprintf(">%s", formattedNow)).
-				ProtoQuery(),
-		)
-		if err != nil {
-			return nil, errors.Wrap(err, "counting valid api tokens")
-		}
-
-		props := map[string]any{
-			"Total API Tokens":         numTotalTokens,
-			"Total API Tokens Expired": numExpiredTokens,
-			"Total API Tokens Revoked": numRevokedTokens,
-			"Total API Tokens Valid":   numValidTokens,
-		}
+		_ = phonehome.AddTotal(ctx, props, "API Tokens Expired", func(ctx context.Context) (int, error) {
+			return ds.Count(ctx,
+				search.NewQueryBuilder().
+					AddStrings(search.Expiration, "<"+formattedNow).
+					ProtoQuery(),
+			)
+		})
+		_ = phonehome.AddTotal(ctx, props, "API Tokens Revoked", func(ctx context.Context) (int, error) {
+			return ds.Count(ctx,
+				search.NewQueryBuilder().
+					AddBools(search.Revoked, true).
+					ProtoQuery(),
+			)
+		})
+		_ = phonehome.AddTotal(ctx, props, "API Tokens Valid", func(ctx context.Context) (int, error) {
+			return ds.Count(ctx,
+				search.NewQueryBuilder().
+					AddBools(search.Revoked, false).
+					AddStrings(search.Expiration, ">"+formattedNow).
+					ProtoQuery(),
+			)
+		})
 		return props, nil
 	}
 }
