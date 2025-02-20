f189186c876cbd60e3838e08437c6ba472cf364d
Surabhi Kumar <surakuma@redhat.com>
2025-02-20 12:36:32 -0800

    Proto changes for RHSA

--- pkg/search/options.go
+++ pkg/search/options.go
@@ -64,6 +64,7 @@ var (
 	CVEOrphaned        = newFieldLabel("CVE Orphaned")
 	CVEOrphanedTime    = newFieldLabel("CVE Orphaned Time")
 	EPSSProbablity     = newFieldLabel("EPSS Probability")
+	AdvisoryID         = newFieldLabel("Advisory ID")
 
 	Component                      = newFieldLabel("Component")
 	ComponentID                    = newFieldLabel("Component ID")
