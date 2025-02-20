1da20e9c9602f5e6e19efbaa95e0c72ce7edd229
daynewlee <yli3@redhat.com>
2025-02-20 10:07:59 -0600

    set imageScanLowerBound

--- pkg/env/max_parallel_scans.go
+++ pkg/env/max_parallel_scans.go
@@ -5,9 +5,7 @@ var (
 	// and GetImageVulnerabilitiesInternal endpoints and separately sets the max active local scans in a secured cluster
 	MaxParallelImageScanInternal = RegisterIntegerSetting("ROX_MAX_PARALLEL_IMAGE_SCAN_INTERNAL", 30)
 
-	// MaxParallelDelegatedScanInternal defines the maximum number of parallel delegated scans initiated from Central.
-	// Since it consumes a portion of the total maximum parallel scans, it must be set lower than the collective limit
-	// for ScanImageInternal, EnrichLocalImageInternal, and GetImageVulnerabilitiesInternal endpoints.
-	// It separately configures the maximum number of active local scans within a secured cluster.
-	MaxParallelDelegatedScanInternal = RegisterIntegerSetting("ROX_MAX_PARALLEL_DELEGATED_SCAN_INTERNAL", 5)
+	// MaxParallelAdHocScanInternal defines the maximum number of parallel ad-hoc roxctl delegated scans initiated from Central.
+	// It must be set lower than the collective limit for ScanImageInternal, EnrichLocalImageInternal, and GetImageVulnerabilitiesInternal endpoints.
+	MaxParallelAdHocScanInternal = RegisterIntegerSetting("ROX_MAX_PARALLEL_ADHOC_SCAN_INTERNAL", 5)
 )
