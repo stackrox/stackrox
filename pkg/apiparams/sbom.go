a0870e9f41b640e852661a89e1fa0ac62e1ad996
David Caravello <119438707+dcaravel@users.noreply.github.com>
2025-02-24 09:45:04 -0600

    ROX-27607: create swagger doc for SBOM custom route (#14334)

--- pkg/apiparams/sbom.go
+++ pkg/apiparams/sbom.go
@@ -1,6 +1,7 @@
 package apiparams
 
 // SBOMRequestBody represents the HTTP API request for generating an SBOM from an image scan.
+// Any changes to this struct should be reflected in the central/docs/api_custom_routes/image_service_swagger.yaml
 type SBOMRequestBody struct {
 	// TODO(ROX-27920): re-introduce cluster flag when SBOM generation from delegated scans is implemented.
 	// Cluster   string `json:"cluster"`
