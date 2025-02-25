3b365850d340675fb8d2431248c86d81893a8abc
Marcin Owsiany <porridge@redhat.com>
2025-02-25 14:25:33 +0100

    tune a fixer

--- scripts/fixxxers/operator/30-manifests.sh
+++ scripts/fixxxers/operator/30-manifests.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
+go mod tidy
+# The above is to prevent this failure:
+# + protoc-gen-go
+# go: downloading google.golang.org/protobuf v1.36.5
+# make/protogen.mk:192: *** Cached directory of scanner dependency not found, run 'go mod tidy'.  Stop.
+
 make -C operator manifests
