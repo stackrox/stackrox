2e25aed4c98ab01d4158eec9e89cb8b8f7755856
Marcin Owsiany <porridge@redhat.com>
2025-02-25 09:48:37 +0100

    chore: PR Fixxxer chatops bot

---
+++ scripts/fixxxer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# This script is an entrypoint for the action .github/workflows/fixxxer.yaml
+set -euo pipefail
+
+msg_file=$(mktemp)
+trap 'rm -f "${msg_file}"' EXIT
+
+find scripts/fixxxers -name '*.sh' -type f -printf "%p\n" | while read -r script; do
+    echo "Running $script" > "$msg_file"
+    echo >> "$msg_file"
+    ./"$script" >> "$msg_file" 2>&1
+    if git diff --exit-code --quiet; then
+        continue
+    fi
+    git commit -a -F "$msg_file"
+done
---
+++ scripts/fixxxers/operator/30-manifests.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -euo pipefail
+
+make -C operator manifests
