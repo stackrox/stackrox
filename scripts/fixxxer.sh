21dd4478b0cbb1af886e91a53e0f54f711a714cf
Marcin Owsiany <porridge@redhat.com>
2025-02-25 14:21:00 +0100

    handle failures more gracefully

--- scripts/fixxxer.sh
+++ scripts/fixxxer.sh
@@ -8,7 +8,11 @@ trap 'rm -f "${msg_file}"' EXIT
 find scripts/fixxxers -name '*.sh' -type f -printf "%p\n" | while read -r script; do
     echo "Running $script" > "$msg_file"
     echo >> "$msg_file"
-    ./"$script" >> "$msg_file" 2>&1
+    if ! ./"$script" >> "$msg_file" 2>&1; then
+        echo "Script $script failed:" >&2
+        cat "$msg_file" >&2
+        exit 1
+    fi
     if git diff --exit-code --quiet; then
         continue
     fi
