2dd3aaceb716cda8bad9a37ad87148e34f0dc7ca
Yann Brillouet <ybrillou@redhat.com>
2025-02-26 16:43:04 +0100

    change row scanning

--- pkg/search/postgres/common.go
+++ pkg/search/postgres/common.go
@@ -984,7 +984,7 @@ func RunCountByRequestForSchema(ctx context.Context, schema *walker.Schema, q *v
 		for rows.Next() {
 			log.Info("processing row")
 			log.Infof("row raw value count %d", len(rows.RawValues()))
-			if err := rows.Scan(&bufferToScanRowInto); err != nil {
+			if err := rows.Scan(bufferToScanRowInto...); err != nil {
 				log.Errorf("Query issue: %s: %v", queryStr, err)
 				return nil, errors.Wrap(err, "error executing query")
 			}
