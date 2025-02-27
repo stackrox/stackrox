abe06be371b20267295db0f31796e8d6804c2575
Kyle Lape <klape@redhat.com>
2025-02-27 08:20:31 -0600

    Remove dead code

--- installer/manifest/manifest.go
+++ installer/manifest/manifest.go
@@ -163,106 +163,6 @@ func genTlsSecret(name string, ca mtls.CA, issueCert tlsCallback) (Resource, err
 	}, nil
 }
 
-func (m *manifestGenerator) applyTlsSecret(ctx context.Context, name string, issueCert tlsCallback) error {
-	secret, err := m.Client.CoreV1().Secrets(m.Config.Namespace).Get(ctx, name, metav1.GetOptions{})
-
-	if err != nil && !k8serrors.IsNotFound(err) {
-		return fmt.Errorf("Error fetching %s secret: %w", name, err)
-	}
-
-	if err == nil {
-		log.Infof("Secret %s already found.", name)
-		return nil
-	}
-
-	fileMap := make(map[string][]byte)
-	certgen.AddCACertToFileMap(fileMap, m.CA)
-
-	secret = &v1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: name,
-		},
-		Data: fileMap,
-	}
-
-	if err = issueCert(fileMap); err != nil {
-		return fmt.Errorf("issuing certificate for %s: %w", name, err)
-	}
-
-	_, err = m.Client.CoreV1().Secrets(m.Config.Namespace).Create(ctx, secret, metav1.CreateOptions{})
-
-	if err != nil {
-		if k8serrors.IsAlreadyExists(err) {
-			return fmt.Errorf("Race condition: Secret %s got created just before now: %w", name, err)
-		}
-		return fmt.Errorf("Error creating secret %s: %w", name, err)
-	}
-
-	return nil
-}
-
-type VolumeDefAndMount struct {
-	Name      string
-	MountPath string
-	ReadOnly  bool
-	Volume    v1.Volume
-}
-
-func (v VolumeDefAndMount) Apply(c *v1.Container, spec *v1.PodSpec) {
-	c.VolumeMounts = append(c.VolumeMounts, v1.VolumeMount{
-		Name:      v.Name,
-		MountPath: v.MountPath,
-		ReadOnly:  v.ReadOnly,
-	})
-	if spec != nil {
-		v.Volume.Name = v.Name
-		spec.Volumes = append(spec.Volumes, v.Volume)
-	}
-}
-
-func RestrictedSecurityContext(user int64) *v1.SecurityContext {
-	FALSE := false
-	TRUE := true
-	return &v1.SecurityContext{
-		RunAsUser:                &user,
-		RunAsGroup:               &user,
-		AllowPrivilegeEscalation: &FALSE,
-		SeccompProfile: &v1.SeccompProfile{
-			Type: v1.SeccompProfileTypeRuntimeDefault,
-		},
-		RunAsNonRoot: &TRUE,
-		Capabilities: &v1.Capabilities{
-			Drop: []v1.Capability{"ALL"},
-		},
-	}
-}
-
-func (m *manifestGenerator) createNonrootV2SCCRole(ctx context.Context) error {
-	name := "use-nonroot-v2-scc"
-	role := rbacv1.Role{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: name,
-		},
-		Rules: []rbacv1.PolicyRule{{
-			APIGroups:     []string{"security.openshift.io"},
-			Resources:     []string{"securitycontextconstraints"},
-			ResourceNames: []string{"nonroot-v2"},
-			Verbs:         []string{"use"},
-		}},
-	}
-	_, err := m.Client.RbacV1().Roles(m.Config.Namespace).Create(ctx, &role, metav1.CreateOptions{})
-
-	if err != nil {
-		if k8serrors.IsAlreadyExists(err) {
-			log.Infof("Role %s already exists", name)
-		} else {
-			return fmt.Errorf("Error creating role %s: %w", name, err)
-		}
-	}
-
-	return nil
-}
-
 func genClusterRoleBinding(serviceAccountName, roleName, ns string) Resource {
 	name := fmt.Sprintf("%s-%s-%s", ns, serviceAccountName, roleName)
 	binding := &rbacv1.ClusterRoleBinding{
@@ -315,107 +215,6 @@ func genRoleBinding(serviceAccountName, roleName, ns string) Resource {
 	}
 }
 
-func (m *manifestGenerator) createRoleBinding(ctx context.Context, serviceAccountName, roleName string) error {
-	name := fmt.Sprintf("%s-%s", serviceAccountName, roleName)
-	roleBinding := rbacv1.RoleBinding{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: name,
-		},
-		RoleRef: rbacv1.RoleRef{
-			APIGroup: "rbac.authorization.k8s.io",
-			Kind:     "Role",
-			Name:     roleName,
-		},
-		Subjects: []rbacv1.Subject{{
-			Kind:      "ServiceAccount",
-			Name:      serviceAccountName,
-			Namespace: m.Config.Namespace,
-		}},
-	}
-	_, err := m.Client.RbacV1().RoleBindings(m.Config.Namespace).Create(ctx, &roleBinding, metav1.CreateOptions{})
-
-	if err != nil {
-		if k8serrors.IsAlreadyExists(err) {
-			log.Infof("Role binding %s already exists", name)
-			if _, err := m.Client.RbacV1().RoleBindings(m.Config.Namespace).Update(ctx, &roleBinding, metav1.UpdateOptions{}); err != nil {
-				return fmt.Errorf("Error updating role binding %s: %w", name, err)
-			}
-		} else {
-			return fmt.Errorf("Error creating role binding %s: %w", name, err)
-		}
-	}
-
-	return nil
-}
-
-func (m *manifestGenerator) createClusterRoleBinding(ctx context.Context, serviceAccountName, roleName string) error {
-	name := fmt.Sprintf("%s-%s-%s", m.Config.Namespace, serviceAccountName, roleName)
-	roleBinding := rbacv1.ClusterRoleBinding{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: name,
-		},
-		RoleRef: rbacv1.RoleRef{
-			APIGroup: "rbac.authorization.k8s.io",
-			Kind:     "ClusterRole",
-			Name:     roleName,
-		},
-		Subjects: []rbacv1.Subject{{
-			Kind:      "ServiceAccount",
-			Name:      serviceAccountName,
-			Namespace: m.Config.Namespace,
-		}},
-	}
-	_, err := m.Client.RbacV1().ClusterRoleBindings().Create(ctx, &roleBinding, metav1.CreateOptions{})
-
-	if err != nil {
-		if k8serrors.IsAlreadyExists(err) {
-			log.Infof("Role binding %s already exists, skipping", name)
-		} else {
-			return fmt.Errorf("Error creating role binding %s: %w", name, err)
-		}
-	}
-
-	return nil
-}
-
-func (m *manifestGenerator) createServiceAccount(ctx context.Context, name string) error {
-	acct := v1.ServiceAccount{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: name,
-		},
-	}
-
-	_, err := m.Client.CoreV1().ServiceAccounts(m.Config.Namespace).Create(ctx, &acct, metav1.CreateOptions{})
-
-	if err != nil {
-		if k8serrors.IsAlreadyExists(err) {
-			log.Infof("Service account %s already exists", name)
-		} else {
-			return fmt.Errorf("Error creating service account %s: %w", name, err)
-		}
-	}
-
-	return nil
-}
-
-func (m *manifestGenerator) applyConfigMap(ctx context.Context, name string, cm *v1.ConfigMap) error {
-	cm.SetName(name)
-	_, err := m.Client.CoreV1().ConfigMaps(m.Config.Namespace).Create(ctx, cm, metav1.CreateOptions{})
-
-	if err != nil {
-		if k8serrors.IsAlreadyExists(err) {
-			_, err = m.Client.CoreV1().ConfigMaps(m.Config.Namespace).Update(ctx, cm, metav1.UpdateOptions{})
-			if err != nil {
-				return fmt.Errorf("Error updating configmap %s: %w", name, err)
-			}
-		} else {
-			return fmt.Errorf("Error creating configmap %s: %w", name, err)
-		}
-	}
-
-	return nil
-}
-
 func genService(name string, ports []v1.ServicePort) Resource {
 	svc := v1.Service{
 		Spec: v1.ServiceSpec{
@@ -448,36 +247,46 @@ func genServiceAccount(name string) Resource {
 	}
 }
 
-func (m *manifestGenerator) applyService(ctx context.Context, name string, ports []v1.ServicePort) error {
-	svc := &v1.Service{
-		Spec: v1.ServiceSpec{
-			Selector: map[string]string{
-				"app": name,
-			},
-			Ports: ports,
-		},
-	}
-	svc.SetName(name)
-	_, err := m.Client.CoreV1().Services(m.Config.Namespace).Create(ctx, svc, metav1.CreateOptions{})
-
-	if err != nil {
-		if k8serrors.IsAlreadyExists(err) {
-			_, err = m.Client.CoreV1().Services(m.Config.Namespace).Update(ctx, svc, metav1.UpdateOptions{})
-			if err != nil {
-				return fmt.Errorf("Error updating service %s: %w", name, err)
-			}
-		} else {
-			return fmt.Errorf("Error creating service %s: %w", name, err)
-		}
-	}
-
-	return nil
-}
-
 func toGVR(gvk schema.GroupVersionKind) schema.GroupVersionResource {
 	return schema.GroupVersionResource{
 		Group:    gvk.Group,
 		Version:  gvk.Version,
 		Resource: strings.ToLower(gvk.Kind + "s"),
+	}
+}
+
+type VolumeDefAndMount struct {
+	Name      string
+	MountPath string
+	ReadOnly  bool
+	Volume    v1.Volume
+}
+
+func (v VolumeDefAndMount) Apply(c *v1.Container, spec *v1.PodSpec) {
+	c.VolumeMounts = append(c.VolumeMounts, v1.VolumeMount{
+		Name:      v.Name,
+		MountPath: v.MountPath,
+		ReadOnly:  v.ReadOnly,
+	})
+	if spec != nil {
+		v.Volume.Name = v.Name
+		spec.Volumes = append(spec.Volumes, v.Volume)
+	}
+}
+
+func RestrictedSecurityContext(user int64) *v1.SecurityContext {
+	FALSE := false
+	TRUE := true
+	return &v1.SecurityContext{
+		RunAsUser:                &user,
+		RunAsGroup:               &user,
+		AllowPrivilegeEscalation: &FALSE,
+		SeccompProfile: &v1.SeccompProfile{
+			Type: v1.SeccompProfileTypeRuntimeDefault,
+		},
+		RunAsNonRoot: &TRUE,
+		Capabilities: &v1.Capabilities{
+			Drop: []v1.Capability{"ALL"},
+		},
 	}
 }
