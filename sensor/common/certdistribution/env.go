44ae4c238739961057cbce893630773d274b703c
Vlad Bologa <vbologa@redhat.com>
2025-02-27 16:18:09 +0100

    WIP: Remove Sensor certdistribution API

--- sensor/common/certdistribution/env.go
+++
@@ -1,8 +0,0 @@
-package certdistribution
-
-import "github.com/stackrox/rox/pkg/env"
-
-var (
-	// cacheDir is the directory in which certificates to be distributed are stored.
-	cacheDir = env.RegisterSetting("ROX_CERTIFICATE_CACHE_DIR", env.WithDefault("/var/cache/stackrox/.certificates"))
-)
