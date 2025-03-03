28cc11ec8c8a09f9007138c7101203c96251180f
Marcin Owsiany <porridge@redhat.com>
2025-03-03 12:23:32 +0100

    chore: sort fixxxers and mod tidy everywhere (#14463)

--- scripts/fixxxers/10-deps-go-mod-tidy.sh
+++ scripts/fixxxers/10-deps-go-mod-tidy.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 source scripts/lib.sh
 
-find operator/ -type f -name go.mod -printf "%h\n" | while read -r dir
+find . -type f -name go.mod -printf "%h\n" | while read -r dir
 do
     (info "Running \"go mod tidy\" in \"$dir\""; cd "$dir" && go mod tidy)
 done
--- scripts/fixxxers/operator/30-manifests.sh
+++ scripts/fixxxers/operator/30-manifests.sh
@@ -2,7 +2,8 @@
 set -euo pipefail
 
 go mod tidy
-# The above is to prevent this failure:
+# The above is to prevent the following failure. Should be done by a script in parent directory
+# but we're keeping it here to keep this script standalone.
 # + protoc-gen-go
 # go: downloading google.golang.org/protobuf v1.36.5
 # make/protogen.mk:192: *** Cached directory of scanner dependency not found, run 'go mod tidy'.  Stop.
