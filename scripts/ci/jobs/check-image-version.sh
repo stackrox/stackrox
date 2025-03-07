9ba21c6aaff90ae274cc4a406e2f699d769ea151
Tomasz Janiszewski <tomek@redhat.com>
2025-03-07 14:29:35 +0100

    fix(CI): check test image version in sync
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

---
+++ scripts/ci/jobs/check-image-version.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+SCRIPTS_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")"/../../.. && pwd)"
+# shellcheck source=../../../scripts/lib.sh
+source "$SCRIPTS_ROOT/scripts/lib.sh"
+
+set -euo pipefail
+
+echo 'Ensure that all test/build image are in the same version'
+
+"$SCRIPTS_ROOT/scripts/check-image-version.sh"
