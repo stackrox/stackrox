78de412341fa3c3c3ec9d5546b64bea5e0a1aefc
daynewlee <yli3@redhat.com>
2025-03-09 13:51:31 -0500

    added logs

--- pkg/images/enricher/enricher_impl.go
+++ pkg/images/enricher/enricher_impl.go
@@ -147,7 +147,7 @@ func (e *enricherImpl) delegateEnrichImage(ctx context.Context, enrichCtx Enrich
 		err = e.scanDelegator.ValidateCluster(enrichCtx.ClusterID)
 		shouldDelegate = true
 	}
-	log.Infof(">>>> delegateEnrichImage with enrichCtx, cluster ID is: %s", clusterID)
+
 	if err != nil || !shouldDelegate {
 		// If was an error or should not delegate, short-circuit.
 		return shouldDelegate, err
@@ -178,6 +178,7 @@ func (e *enricherImpl) delegateEnrichImage(ctx context.Context, enrichCtx Enrich
 	if err != nil {
 		return true, err
 	}
+
 	// Copy the fields from scannedImage into image, EnrichImage expecting modification in place
 	image.Reset()
 	protocompat.Merge(image, scannedImage)
@@ -225,7 +226,6 @@ func (e *enricherImpl) updateImageWithExistingImage(image *storage.Image, existi
 func (e *enricherImpl) EnrichImage(ctx context.Context, enrichContext EnrichmentContext, image *storage.Image) (EnrichmentResult, error) {
 	if shouldDelegate, err := e.delegateEnrichImage(ctx, enrichContext, image); shouldDelegate {
 		if err != nil {
-			log.Warnf(">>>> the current error is: %v", err)
 			if errors.Is(err, errox.InvalidArgs) {
 				// Log the warning and try to keep enriching
 				log.Warnf("No default cluster found for delegation: %v, proceed enriching", err)
