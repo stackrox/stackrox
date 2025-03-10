a5d24757939fd4e70fcd7ab6b4dd951d5787bbd4
David Shrewsberry <99685630+dashrews78@users.noreply.github.com>
2025-03-10 14:40:38 -0400

    ROX-28406: CVE data model loaders and gen (#14527)

--- central/graphql/resolvers/gen/main.go
+++ central/graphql/resolvers/gen/main.go
@@ -58,6 +58,8 @@ var (
 			reflect.TypeOf((*storage.ComplianceDomain_Cluster)(nil)),
 			reflect.TypeOf((*storage.ComplianceDomain_Deployment)(nil)),
 			reflect.TypeOf((*storage.ComplianceDomain_Node)(nil)),
+			reflect.TypeOf((*storage.ImageComponentV2)(nil)),
+			reflect.TypeOf((*storage.ImageCVEV2)(nil)),
 
 			reflect.TypeOf((*v1.ComplianceStandard)(nil)),
 			reflect.TypeOf((*v1.GenerateTokenResponse)(nil)),
