43677da41ba1ce67cf602977e824e34611aa8e73
Michaël Petrov <smartptr@gmail.com>
2025-03-11 12:27:37 +0100

    style

--- roxctl/utils/help_writer_test.go
+++ roxctl/utils/help_writer_test.go
@@ -25,19 +25,17 @@ func Test_helpWriter(t *testing.T) {
 	t.Run("write", func(t *testing.T) {
 		sb := &strings.Builder{}
 		w := makeHelpWriter(makeFormattingWriter(sb, 25, defaultTabWidth))
-		w.WriteLn("\nshort line of text")
+		w.WriteLn("short line of text")
 		w.WriteLn("somewhat long line of text")
 		w.Indent(2, 3).Write("<-2 sps")
 		w.WriteLn("\t<-\\t\\n->", "<-three spaces")
 		w.WriteLn("no indent")
-		assert.Equal(t, `
-short line of text
-somewhat long line of 
-text
-  <-2 sps	<-\t\n->
-   <-three spaces
-no indent
-`,
+		assert.Equal(t, "short line of text\n"+
+			"somewhat long line of \n"+
+			"text\n"+
+			"  <-2 sps\t<-\\t\\n->\n"+
+			"   <-three spaces\n"+
+			"no indent\n",
 			sb.String())
 	})
 }
