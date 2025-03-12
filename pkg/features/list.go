fa35d27801ccb832ac37ad154ea3f85adcc0ff9a
Saif Chaudhry <schaudhr@redhat.com>
2025-03-12 12:55:33 -0700

    fixing lint error

--- pkg/features/list.go
+++ pkg/features/list.go
@@ -118,7 +118,7 @@ var (
 	FlattenCVEData = registerFeature("Uses a flattened CVE Data Model improved accuracy and performance", "ROX_FLATTEN_CVE_DATA")
 
 	// Adds the ability to generate vulnerability reports based on filter views
-	VulnerabilityAdHocReports = registerFeature("Adds the ability to generate vulnerability reports based on filter views", "ROX_VULNERABILITY_AD_HOC_REPORTS",enabled)
+	VulnerabilityAdHocReports = registerFeature("Adds the ability to generate vulnerability reports based on filter views", "ROX_VULNERABILITY_AD_HOC_REPORTS", enabled)
 )
 
 // The following feature flags are related to Scanner V4.
