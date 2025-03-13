dd1ff7d55c964eac464be75f4031fc903b1b9b8d
Giles Hutton <ghutton@redhat.com>
2025-03-13 13:21:17 +0000

    Add Discovered field to netgraph testutils

--- pkg/networkgraph/testutils/utils.go
+++ pkg/networkgraph/testutils/utils.go
@@ -98,7 +98,8 @@ func GetExtSrcNetworkEntityInfo(id, name, cidr string, isDefault bool) *storage.
 				Source: &storage.NetworkEntityInfo_ExternalSource_Cidr{
 					Cidr: cidr,
 				},
-				Default: isDefault,
+				Default:    isDefault,
+				Discovered: !isDefault,
 			},
 		},
 	}
