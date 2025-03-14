82facbb075a9b9227988419de6928db51ced905f
Tomasz Janiszewski <tomek@redhat.com>
2025-03-14 13:27:45 +0100

    chore(rand): add tests to GenerateString (#14607)
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

--- pkg/random/random.go
+++ pkg/random/random.go
@@ -4,19 +4,25 @@ import (
 	"math/rand/v2"
 )
 
+// Charset is a string with available characters to use by GenerateString
+type Charset string
+
 const (
 	// AlphanumericCharacters is the set of alphanumeric characters
-	AlphanumericCharacters = `abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`
+	AlphanumericCharacters Charset = `abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`
 
 	// CaseInsensitiveAlpha is for use cases when the case is ignored
-	CaseInsensitiveAlpha = `abcdefghijklmnopqrstuvwxyz`
+	CaseInsensitiveAlpha Charset = `abcdefghijklmnopqrstuvwxyz`
 
 	// HexValues is for use cases when you need a valid hex string like in image digests
-	HexValues = `0123456789abcdef`
+	HexValues Charset = `0123456789abcdef`
 )
 
 // GenerateString generates a random string based on the passed number of characters and the character set
-func GenerateString(num int, charSet string) string {
+func GenerateString(num int, charSet Charset) string {
+	if charSet == "" {
+		return ""
+	}
 	var str string
 	for i := 0; i < num; i++ {
 		randInt := rand.IntN(len(charSet))
