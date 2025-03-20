eb92fe35ec8ff3793f58e470c9444925581ca06c
Tomasz Janiszewski <tomek@redhat.com>
2025-03-20 13:32:46 +0100

    revert policy service
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

--- qa-tests-backend/src/main/groovy/services/PolicyService.groovy
+++ qa-tests-backend/src/main/groovy/services/PolicyService.groovy
@@ -1,8 +1,6 @@
 package services
 
-import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
-
 import io.stackrox.proto.api.v1.Common
 import io.stackrox.proto.api.v1.PolicyServiceGrpc
 import io.stackrox.proto.api.v1.PolicyServiceOuterClass
@@ -12,9 +10,8 @@ import io.stackrox.proto.storage.PolicyOuterClass.Policy
 import io.stackrox.proto.storage.ScopeOuterClass
 
 @Slf4j
-@CompileStatic
 class PolicyService extends BaseService {
-    static PolicyServiceGrpc.PolicyServiceBlockingStub getPolicyClient() {
+    static getPolicyClient() {
         return PolicyServiceGrpc.newBlockingStub(getChannel())
     }
 
