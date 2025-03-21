3985ede02f6017cc6dbe38dc9e8caca8c312d69d
Tomasz Janiszewski <tomek@redhat.com>
2025-03-21 12:09:53 +0100

    fix
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

--- qa-tests-backend/scripts/copy_scanner_protos.sh
+++ qa-tests-backend/scripts/copy_scanner_protos.sh
@@ -5,5 +5,5 @@ JAVA_PATH=src/main/proto/
 # Migrate v1 API protos from the Scanner repo
 SCANNER_DIR=$(go list -f '{{.Dir}}' -m github.com/stackrox/scanner)
 # files from gomod cache have no write permission causing problems for gradle
-cp -r ${SCANNER_DIR}/proto/* ${JAVA_PATH}
-chmod -R u+w ${JAVA_PATH}
+cp -r ${SCANNER_DIR}/proto/scanner ${JAVA_PATH}
+chmod -R u+w ${JAVA_PATH}/scanner
