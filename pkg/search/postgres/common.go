6ebece293d856ca448d4ae29a8ac742fc9830e03
Tomasz Janiszewski <tomek@redhat.com>
2025-03-24 20:23:47 +0100

    feat(pg): increse cursor batch size
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

--- pkg/search/postgres/common.go
+++ pkg/search/postgres/common.go
@@ -1077,6 +1077,7 @@ func RunCursorQueryForSchemaFn[T any, PT pgutils.Unmarshaler[T]](ctx context.Con
 		return errors.Wrap(err, "creating cursor")
 	}
 
+	const cursorBatchSize = 1000
 	for {
 		rows, err := tx.Query(ctx, fmt.Sprintf("FETCH %d FROM %s", cursorBatchSize, cursor))
 		if err != nil {
