94662364675fb1f061a356d6793a37d219b94e91
Tomasz Janiszewski <tomek@redhat.com>
2025-03-24 11:52:15 +0100

    fix(proto): symlink protos instead of copy (#14690)
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

--- qa-tests-backend/scripts/copy_scanner_protos.sh
+++ qa-tests-backend/scripts/copy_scanner_protos.sh
@@ -2,12 +2,8 @@
 
 JAVA_PATH=src/main/proto/
 
-# Migrate protos from the stackrox repo.
-mkdir -p ${JAVA_PATH}
-cp -r ../proto/* ${JAVA_PATH}
-
 # Migrate v1 API protos from the Scanner repo
 SCANNER_DIR=$(go list -f '{{.Dir}}' -m github.com/stackrox/scanner)
 # files from gomod cache have no write permission causing problems for gradle
-cp -r ${SCANNER_DIR}/proto/* ${JAVA_PATH}
-chmod -R u+w ${JAVA_PATH}
+cp -r ${SCANNER_DIR}/proto/scanner ${JAVA_PATH}
+chmod -R u+w ${JAVA_PATH}/scanner
