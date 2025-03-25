620b5f1a2055a769187a04e87660a14b8cd4ce07
Michaël <smartptr@gmail.com>
2025-03-25 16:11:00 +0100

    chore(roxctl): Fix tab formatting in `roxctl` (#14567)

--- roxctl/utils/formatting_writer.go
+++ roxctl/utils/formatting_writer.go
@@ -3,13 +3,14 @@ package utils
 import (
 	"bufio"
 	"io"
+	"iter"
 	"strings"
 )
 
 const defaultTabWidth = 8
 
-// formattingWriter implements StringWriter interface.
-// It writes strings to the underlying writer indenting and wrapping the text.
+// formattingWriter writes strings to the underlying writer indenting and
+// wrapping the text.
 type formattingWriter struct {
 	raw      io.StringWriter
 	width    int
@@ -17,29 +18,21 @@ type formattingWriter struct {
 	tabWidth int
 
 	currentLine int
-	written     int
 	indentReset bool
 }
-
-var _ io.StringWriter = (*formattingWriter)(nil)
 
 func makeFormattingWriter(w io.StringWriter, width int, tabWidth int, indent ...int) *formattingWriter {
 	return &formattingWriter{raw: w, width: width, indent: indent, tabWidth: tabWidth}
 }
 
-// write is an internal method that writes the string to the underlying writer.
-func (w *formattingWriter) write(s string) error {
-	n, err := w.raw.WriteString(s)
-	w.currentLine += n
-	w.written += n
-	return err //nolint:wrapcheck
-}
-
-// writePadding is an internal method, that takes the next indent value and the
-// writes the according number of spaces. If the indent value is negative, it is
-// calculated as tabulation offset, i.e. the spaces are added to reach the
-// required offset. Returns true if a new line has been written.
-func (w *formattingWriter) writePadding() (bool, error) {
+// computePadding is an internal method, that takes the next indent value and
+// computes the required number of spaces. If the indent value is negative, it
+// is calculated as tabulation offset, i.e. the spaces are added to reach the
+// required offset. Returns true if a new line needs to been written.
+func (w *formattingWriter) computePadding() (bool, int) {
+	if w.currentLine != 0 && !w.indentReset {
+		return false, 0
+	}
 	w.indentReset = false
 	padding := w.indent.popNotLast()
 	if padding < 0 {
@@ -48,19 +41,12 @@ func (w *formattingWriter) writePadding() (bool, error) {
 		if padding > w.currentLine {
 			padding -= w.currentLine
 		} else {
-			return true, nil
+			return true, 0
 		}
 	} else if w.currentLine+padding > w.width {
-		return true, nil
+		return true, 0
 	}
-	return false, w.write(strings.Repeat(" ", padding))
-}
-
-// ln is an internal method that writes a new line to the underlying writer.
-func (w *formattingWriter) ln() error {
-	_, err := w.raw.WriteString("\n")
-	w.currentLine = 0
-	return err //nolint:wrapcheck
+	return false, padding
 }
 
 // SetIndent updates the indentation for the following writings.
@@ -71,46 +57,56 @@ func (w *formattingWriter) SetIndent(indent ...int) {
 
 // WriteString writes the string to the underlying writer, with the configured
 // indentation and wrapping.
-// Implements the StringWriter interface.
-func (w *formattingWriter) WriteString(s string) (int, error) {
-	w.written = 0
-	tokenScanner := bufio.NewScanner(strings.NewReader(s))
-	tokenScanner.Split(wordsAndDelimeters)
-	var err error
-	for err == nil && tokenScanner.Scan() {
-		token := tokenScanner.Text()
-		length := len(token)
-		switch token {
-		case "\t":
-			length = defaultTabWidth
-		case "\n":
-			if w.currentLine == 0 {
-				w.indent.popNotLast()
-				w.indentReset = false
-			}
-			err = w.ln()
-			continue
+func (w *formattingWriter) WriteString(s string) error {
+	for token := range w.tokens(strings.NewReader(s)) {
+		if _, err := w.raw.WriteString(token); err != nil {
+			return err //nolint:wrapcheck
 		}
-		ln := false
-		if w.currentLine == 0 || w.indentReset {
-			if ln, err = w.writePadding(); err != nil {
-				break
-			}
-		}
-		// Write a new line and a padding in case of line overflow:
-		if ln || w.currentLine+length > w.width {
-			if err = w.ln(); err != nil {
-				break
-			}
-			if token == " " {
-				// Do not write space token that caused line break.
+	}
+	return nil
+}
+
+// tokens is an internal iterator that issues tokens with paddings and wrapping.
+func (w *formattingWriter) tokens(r io.Reader) iter.Seq[string] {
+	return func(yield func(string) bool) {
+		tokenScanner := bufio.NewScanner(r)
+		tokenScanner.Split(wordsAndDelimeters)
+		for tokenScanner.Scan() {
+			token := tokenScanner.Text()
+			tokenLength := len(token)
+			switch token {
+			case "\t":
+				tokenLength = w.tabWidth
+			case "\n":
+				if w.currentLine == 0 {
+					w.indent.popNotLast()
+					w.indentReset = false
+				}
+				w.currentLine = 0
+				if !yield("\n") {
+					return
+				}
 				continue
 			}
-			if _, err = w.writePadding(); err != nil {
-				break
+			ln, padding := w.computePadding()
+			// Wrapping condition:
+			if ln || w.currentLine+padding+tokenLength > w.width {
+				w.currentLine = 0
+				if !yield("\n") {
+					return
+				}
+				if token == " " {
+					// Ignore the space that caused wrapping.
+					continue
+				}
+				// Re-compute padding after wrapping.
+				_, padding = w.computePadding()
+			}
+			w.currentLine += padding + tokenLength
+			if (padding > 0 && !yield(strings.Repeat(" ", padding))) ||
+				!yield(token) {
+				return
 			}
 		}
-		err = w.write(token)
 	}
-	return w.written, err
 }
