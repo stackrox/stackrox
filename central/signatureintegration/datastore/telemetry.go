f07c7f0200b1041b7a034ff1ae15835604149335
Michaël <smartptr@gmail.com>
2025-03-26 15:56:31 +0100

    ROX-28422: Signature integrations metrics: certs and keys totals (#14735)
    Co-authored-by: Stephan Hesselmann <shesselm@redhat.com>

--- central/signatureintegration/datastore/telemetry.go
+++ central/signatureintegration/datastore/telemetry.go
@@ -16,10 +16,44 @@ var Gather phonehome.GatherFunc = func(ctx context.Context) (map[string]any, err
 		sac.AllowFixedScopes(
 			sac.AccessModeScopeKeys(storage.Access_READ_ACCESS),
 			sac.ResourceScopeKeys(resources.Integration)))
-	totals := make(map[string]any)
-	si := Singleton()
-	if err := phonehome.AddTotal(ctx, totals, "Signature Integrations", si.CountSignatureIntegrations); err != nil {
-		return nil, errors.Wrap(err, "failed to get signature integrations")
+
+	integrations, err := Singleton().GetAllSignatureIntegrations(ctx)
+	if err != nil {
+		return nil, errors.WithMessage(err, "failed to get all signature integrations")
 	}
-	return totals, nil
+	return computeTelemetryProperties(ctx, integrations), nil
+}
+
+func computeTelemetryProperties(ctx context.Context, integrations []*storage.SignatureIntegration) map[string]any {
+	if len(integrations) == 0 {
+		return nil
+	}
+
+	totalPublicKeys, totalCertificates := 0, 0
+	totalCertsWithCustomChain, totalCertsWithIntermediateCert := 0, 0
+	for _, i := range integrations {
+		totalPublicKeys += len(i.GetCosign().GetPublicKeys())
+		totalCertificates += len(i.GetCosignCertificates())
+		for _, cert := range i.GetCosignCertificates() {
+			if len(cert.GetCertificateChainPemEnc()) > 0 {
+				totalCertsWithCustomChain++
+			}
+			if len(cert.GetCertificatePemEnc()) > 0 {
+				totalCertsWithIntermediateCert++
+			}
+		}
+	}
+
+	totals := make(map[string]any)
+	_ = phonehome.AddTotal(ctx, totals,
+		"Signature Integrations", phonehome.Len(integrations))
+	_ = phonehome.AddTotal(ctx, totals,
+		"Signature Integration Cosign Public Keys", phonehome.Constant(totalPublicKeys))
+	_ = phonehome.AddTotal(ctx, totals,
+		"Signature Integration Certificates", phonehome.Constant(totalCertificates))
+	_ = phonehome.AddTotal(ctx, totals,
+		"Signature Integration With Custom Certificate", phonehome.Constant(totalCertsWithIntermediateCert))
+	_ = phonehome.AddTotal(ctx, totals,
+		"Signature Integration With Custom Chain", phonehome.Constant(totalCertsWithCustomChain))
+	return totals
 }
