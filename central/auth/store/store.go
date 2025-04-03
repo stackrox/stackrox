e8f31d5aa07fd2847460ab32dc3a231c6b3fb878
Tomasz Janiszewski <tomek@redhat.com>
2025-04-03 17:14:52 +0200

    chore(pg): cleanup storeWrapper
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

--- central/auth/store/store.go
+++ central/auth/store/store.go
@@ -21,32 +21,16 @@ type Store interface {
 // The reason for requiring this is that we also have an in-memory store for the auth machine to machine config,
 // and require rollbacks for upsert and delete.
 type storeWrapper struct {
-	db    pgPkg.DB
-	store authPGStore.Store
+	db pgPkg.DB
+	authPGStore.Store
 }
 
 // New creates a new store.
 func New(db pgPkg.DB) Store {
 	return &storeWrapper{
 		db:    db,
-		store: authPGStore.New(db),
+		Store: authPGStore.New(db),
 	}
-}
-
-func (s *storeWrapper) Get(ctx context.Context, id string) (*storage.AuthMachineToMachineConfig, bool, error) {
-	return s.store.Get(ctx, id)
-}
-
-func (s *storeWrapper) Upsert(ctx context.Context, obj *storage.AuthMachineToMachineConfig) error {
-	return s.store.Upsert(ctx, obj)
-}
-
-func (s *storeWrapper) Delete(ctx context.Context, id string) error {
-	return s.store.Delete(ctx, id)
-}
-
-func (s *storeWrapper) GetAll(ctx context.Context) ([]*storage.AuthMachineToMachineConfig, error) {
-	return s.store.GetAll(ctx)
 }
 
 // Begin begins a transaction, returning the transaction context from the given context and the transaction itself.
