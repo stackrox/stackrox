a1f545aad12460dc1ad984040209217ae744c20e
Saif Chaudhry <schaudhr@redhat.com>
2025-04-04 15:14:45 -0700

    ROX-28474: Adding on demand reports table

--- ui/apps/platform/src/Containers/Vulnerabilities/VulnerablityReporting/VulnReports/OnDemandReportsTab.tsx
+++ ui/apps/platform/src/Containers/Vulnerabilities/VulnerablityReporting/VulnReports/OnDemandReportsTab.tsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { Card, CardBody, PageSection, Text } from '@patternfly/react-core';
+
+import { getTableUIState } from 'utils/getTableUIState';
+import useRestQuery from 'hooks/useRestQuery';
+import { fetchOnDemandReportHistory } from 'services/ReportsService';
 import PageTitle from 'Components/PageTitle';
+import OnDemandReportsTable from './OnDemandReportsTable';
 
 function OnDemandReportsTab() {
+    // @TODO: Pass query, pagination, sorting to API call
+    const fetchOnDemandReportsHistoryCallback = useCallback(() => fetchOnDemandReportHistory(), []);
+    const { data, isLoading, error } = useRestQuery(fetchOnDemandReportsHistoryCallback);
+
+    // @TODO: Add polling
+
+    const tableState = getTableUIState({
+        isLoading,
+        data,
+        error,
+        searchFilter: {},
+        isPolling: false,
+    });
+
     return (
         <>
             <PageTitle title="Vulnerability reporting - On-demand reports" />
+            <PageSection variant="light">
+                <Text>
+                    Check job status and download on-demand reports in CSV format. Requests are
+                    purged according to retention settings.
+                </Text>
+            </PageSection>
+            <PageSection>
+                <Card>
+                    <CardBody className="pf-v5-u-p-0">
+                        <OnDemandReportsTable
+                            tableState={tableState}
+                            onClearFilters={() => {
+                                // @TODO: Clear search filter and reset pagination
+                            }}
+                        />
+                    </CardBody>
+                </Card>
+            </PageSection>
         </>
     );
 }
