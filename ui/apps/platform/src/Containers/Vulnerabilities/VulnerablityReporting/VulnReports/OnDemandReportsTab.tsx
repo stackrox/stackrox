4a958fc42fb473c1d320082e40763220ac535a8e
Saif Chaudhry <schaudhr@redhat.com>
2025-04-04 15:14:45 -0700

    ROX-28474: Adding on demand reports table

--- ui/apps/platform/src/Containers/Vulnerabilities/VulnerablityReporting/VulnReports/OnDemandReportsTab.tsx
+++ ui/apps/platform/src/Containers/Vulnerabilities/VulnerablityReporting/VulnReports/OnDemandReportsTab.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Divider, Flex, PageSection, Text } from '@patternfly/react-core';
+
 import PageTitle from 'Components/PageTitle';
 
 function OnDemandReportsTab() {
     return (
         <>
             <PageTitle title="Vulnerability reporting - On-demand reports" />
+            <PageSection variant="light">
+                <Text>
+                    Check job status and download on-demand reports in CSV format. Requests are
+                    purged according to retention settings.
+                </Text>
+            </PageSection>
+            <PageSection padding={{ default: 'noPadding' }}></PageSection>
         </>
     );
 }
