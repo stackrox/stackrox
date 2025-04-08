8dcfbe3cf20bf7b8730121db97663bd83a091156
Brad Rogers <61400697+bradr5@users.noreply.github.com>
2025-04-08 11:46:43 -0500

    fix(ui): Fix compliance filter chips (#14889)

--- ui/apps/platform/src/Containers/ComplianceEnhanced/searchFilterConfig.ts
+++ ui/apps/platform/src/Containers/ComplianceEnhanced/searchFilterConfig.ts
@@ -1,6 +1,9 @@
 import { clusterAttributes } from 'Components/CompoundSearchFilter/attributes/cluster';
 import { profileCheckAttributes } from 'Components/CompoundSearchFilter/attributes/profileCheck';
 import { CompoundSearchFilterEntity } from 'Components/CompoundSearchFilter/types';
+import { FilterChipGroupDescriptor } from 'Components/PatternFly/SearchFilterChips';
+
+import { CHECK_STATUS_QUERY } from './Coverage/compliance.coverage.constants';
 
 export const profileCheckSearchFilterConfig: CompoundSearchFilterEntity = {
     displayName: 'Profile check',
@@ -12,4 +15,9 @@ export const clusterSearchFilterConfig: CompoundSearchFilterEntity = {
     displayName: 'Cluster',
     searchCategory: 'CLUSTERS',
     attributes: clusterAttributes,
+};
+
+export const complianceStatusFilterChipDescriptors: FilterChipGroupDescriptor = {
+    displayName: 'Compliance status',
+    searchFilterName: CHECK_STATUS_QUERY,
 };
