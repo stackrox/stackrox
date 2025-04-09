559bcac1308a3c9239fba4115d6f2ef75310e586
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2025-04-09 16:10:40 +0200

    Fix sensor integration test

--- sensor/tests/connection/runtime/runtime_test.go
+++ sensor/tests/connection/runtime/runtime_test.go
@@ -46,7 +46,7 @@ func Test_SensorIntermediateRuntimeEvents(t *testing.T) {
 
 	flowC := make(chan *sensor.NetworkConnectionInfoMessage, 1000)
 	config.NetworkFlowTraceWriter = helper.NewNetworkFlowTraceWriter(ctx, flowC)
-	signalC := make(chan *sensor.SignalStreamMessage, 1000)
+	signalC := make(chan *storage.ProcessSignal, 1000)
 	config.ProcessIndicatorTraceWriter = helper.NewProcessIndicatorTraceWriter(ctx, signalC)
 
 	if config.RealCerts {
@@ -123,8 +123,8 @@ func Test_SensorIntermediateRuntimeEvents(t *testing.T) {
 			},
 		}
 		expectedSignals := []helper.ExpectedSignalMessageFn{
-			func(msg *sensor.SignalStreamMessage) bool {
-				return msg.GetSignal().GetProcessSignal().GetName() == "curl" && msg.GetSignal().GetProcessSignal().GetContainerId() == talkContainerIds[0]
+			func(msg *storage.ProcessSignal) bool {
+				return msg.GetName() == "curl" && msg.GetContainerId() == talkContainerIds[0]
 			},
 		}
 		go helper.WaitToReceiveMessagesFromCollector(ctx, &messagesReceivedSignal,
