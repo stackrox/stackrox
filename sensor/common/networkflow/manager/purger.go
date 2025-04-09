af46aaff970e00d6d4efca81aac7fc147fd53ead
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2025-04-09 16:55:59 +0200

    Add full unit test coverage for purger rules

--- sensor/common/networkflow/manager/purger.go
+++ sensor/common/networkflow/manager/purger.go
@@ -5,48 +5,53 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/stackrox/rox/pkg/concurrency"
+	"github.com/stackrox/rox/pkg/sync"
 	"github.com/stackrox/rox/pkg/timestamp"
 	flowMetrics "github.com/stackrox/rox/sensor/common/networkflow/metrics"
 )
 
-func (m *networkFlowManager) purgeStaleEndpoints(tickerC <-chan time.Time, maxAge time.Duration) {
+func (m *networkFlowManager) runPurger(tickerC <-chan time.Time, maxAge time.Duration) {
 	for {
 		select {
 		case <-m.stopper.Flow().StopRequested():
 			return
 		case <-tickerC:
-			m.runAllPurgerRules(maxAge)
+			npae := purgeActiveEndpoints(&m.activeEndpointsMutex, maxAge, m.activeEndpoints, m.clusterEntities)
+			npac := purgeActiveConnections(&m.activeConnectionsMutex, maxAge, m.activeConnections, m.clusterEntities)
+			nphe, nphc := purgeHostConns(&m.connectionsByHostMutex, maxAge, m.connectionsByHost, m.clusterEntities)
+			log.Debugf("Purger deleted: "+
+				"%d active endpoints, %d active connections, "+
+				"%d host endpoints, %d host connections",
+				npae, npac, nphe, nphc)
 		}
 	}
 }
 
-func (m *networkFlowManager) runAllPurgerRules(maxAge time.Duration) {
-	concurrency.WithLock(&m.activeEndpointsMutex, func() {
-		log.Debug("Purging active endpoints")
-		purgeActiveEndpointsNoLock(maxAge, m.activeEndpoints, m.clusterEntities)
-	})
-
-	concurrency.WithLock(&m.activeConnectionsMutex, func() {
-		log.Debug("Purging active connections")
-		purgeActiveConnectionsNoLock(maxAge, m.activeConnections, m.clusterEntities)
-	})
-
+func purgeHostConns(mutex *sync.Mutex, maxAge time.Duration, hostConns map[string]*hostConnections, store EntityStore) (numPurgedEps, numPurgedConns int) {
 	timer := prometheus.NewTimer(flowMetrics.ActiveEndpointsPurgerDuration.WithLabelValues("hostConns"))
 	defer timer.ObserveDuration()
-	concurrency.WithLock(&m.connectionsByHostMutex, func() {
-		for _, conns := range m.connectionsByHost {
-			concurrency.WithLock(&conns.mutex, func() {
-				purgeHostConnsNoLock(maxAge, conns, m.clusterEntities)
+	numPurgedEps = 0
+	numPurgedConns = 0
+	concurrency.WithLock(mutex, func() {
+		for _, c := range hostConns {
+			concurrency.WithLock(&c.mutex, func() {
+				npe, npc := purgeHostConnsNoLock(maxAge, c, store)
+				numPurgedEps += npe
+				numPurgedConns += npc
 			})
 		}
 	})
+	return numPurgedEps, numPurgedConns
 }
 
-func purgeHostConnsNoLock(maxAge time.Duration, conns *hostConnections, store EntityStore) {
+func purgeHostConnsNoLock(maxAge time.Duration, conns *hostConnections, store EntityStore) (numPurgedEps, numPurgedConns int) {
+	numPurgedEps = 0
+	numPurgedConns = 0
 	for endpoint, status := range conns.endpoints {
 		// remove if the endpoint is not in the store (also not in history)
 		if len(store.LookupByEndpoint(endpoint.endpoint)) == 0 {
 			delete(conns.endpoints, endpoint)
+			numPurgedEps++
 			flowMetrics.PurgerEvents.WithLabelValues("hostEndpoint", "endpoint-gone").Inc()
 			continue
 		}
@@ -56,6 +61,7 @@ func purgeHostConnsNoLock(maxAge time.Duration, conns *hostConnections, store En
 			if cutOff.After(status.tsAdded) {
 				flowMetrics.PurgerEvents.WithLabelValues("hostEndpoint", "max-age-reached").Inc()
 				delete(conns.endpoints, endpoint)
+				numPurgedEps++
 			}
 		}
 	}
@@ -65,6 +71,7 @@ func purgeHostConnsNoLock(maxAge time.Duration, conns *hostConnections, store En
 		if !found {
 			delete(conns.connections, conn)
 			flowMetrics.PurgerEvents.WithLabelValues("hostConnection", "containerID-gone").Inc()
+			numPurgedConns++
 			continue
 		}
 		if maxAge > 0 {
@@ -73,20 +80,31 @@ func purgeHostConnsNoLock(maxAge time.Duration, conns *hostConnections, store En
 			if cutOff.After(status.tsAdded) {
 				flowMetrics.PurgerEvents.WithLabelValues("hostConnection", "max-age-reached").Inc()
 				delete(conns.connections, conn)
+				numPurgedConns++
 			}
 		}
 	}
+	return numPurgedEps, numPurgedConns
+}
+
+func purgeActiveEndpoints(mutex *sync.Mutex, maxAge time.Duration, activeEndpoints map[containerEndpoint]*containerEndpointIndicatorWithAge, store EntityStore) int {
+	timer := prometheus.NewTimer(flowMetrics.ActiveEndpointsPurgerDuration.WithLabelValues("activeEndpoints"))
+	defer timer.ObserveDuration()
+	return concurrency.WithLock1(mutex, func() int {
+		log.Debug("Purging active endpoints")
+		return purgeActiveEndpointsNoLock(maxAge, activeEndpoints, store)
+	})
 }
 
 func purgeActiveEndpointsNoLock(maxAge time.Duration,
 	endpoints map[containerEndpoint]*containerEndpointIndicatorWithAge,
-	store EntityStore) {
-	timer := prometheus.NewTimer(flowMetrics.ActiveEndpointsPurgerDuration.WithLabelValues("activeEndpoints"))
-	defer timer.ObserveDuration()
+	store EntityStore) int {
+	numPurged := 0
 	for endpoint, age := range endpoints {
 		// Remove if the endpoint is not in the store (also not in history)
 		if len(store.LookupByEndpoint(endpoint.endpoint)) == 0 {
 			delete(endpoints, endpoint)
+			numPurged++
 			flowMetrics.PurgerEvents.WithLabelValues("activeEndpoint", "endpoint-gone").Inc()
 			continue
 		}
@@ -94,6 +112,7 @@ func purgeActiveEndpointsNoLock(maxAge time.Duration,
 		_, found, _ := store.LookupByContainerID(endpoint.containerID)
 		if !found {
 			delete(endpoints, endpoint)
+			numPurged++
 			flowMetrics.PurgerEvents.WithLabelValues("activeEndpoint", "containerID-gone").Inc()
 			continue
 		}
@@ -103,21 +122,34 @@ func purgeActiveEndpointsNoLock(maxAge time.Duration,
 			if cutOff.After(age.lastUpdate) {
 				flowMetrics.PurgerEvents.WithLabelValues("activeEndpoint", "max-age-reached").Inc()
 				delete(endpoints, endpoint)
+				numPurged++
 			}
 		}
 	}
+	return numPurged
+}
+
+func purgeActiveConnections(mutex *sync.Mutex, maxAge time.Duration, activeConnections map[connection]*networkConnIndicatorWithAge, store EntityStore) int {
+	timer := prometheus.NewTimer(flowMetrics.ActiveEndpointsPurgerDuration.WithLabelValues("activeConnections"))
+	defer timer.ObserveDuration()
+	return concurrency.WithLock1(mutex, func() int {
+		log.Debug("Purging active connections")
+		return purgeActiveConnectionsNoLock(maxAge, activeConnections, store)
+	})
 }
 
 func purgeActiveConnectionsNoLock(maxAge time.Duration,
 	conns map[connection]*networkConnIndicatorWithAge,
-	store EntityStore) {
+	store EntityStore) int {
 	timer := prometheus.NewTimer(flowMetrics.ActiveEndpointsPurgerDuration.WithLabelValues("activeConnections"))
 	defer timer.ObserveDuration()
+	numPurged := 0
 	for conn, age := range conns {
 		// Remove if the related container is not found (but keep historical)
 		_, found, _ := store.LookupByContainerID(conn.containerID)
 		if !found {
 			delete(conns, conn)
+			numPurged++
 			flowMetrics.PurgerEvents.WithLabelValues("activeConnection", "containerID-gone").Inc()
 			continue
 		}
@@ -127,7 +159,9 @@ func purgeActiveConnectionsNoLock(maxAge time.Duration,
 			if cutOff.After(age.lastUpdate) {
 				flowMetrics.PurgerEvents.WithLabelValues("activeConnection", "max-age-reached").Inc()
 				delete(conns, conn)
+				numPurged++
 			}
 		}
 	}
+	return numPurged
 }
