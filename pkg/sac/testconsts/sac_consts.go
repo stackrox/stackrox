924df81efa0b76dbca27cc7045c2d3f4ee0fa231
Yann Brillouet <ybrillou@redhat.com>
2025-04-14 09:59:56 +0200

    fix test consts

--- pkg/sac/testconsts/sac_consts.go
+++ pkg/sac/testconsts/sac_consts.go
@@ -2,10 +2,10 @@ package testconsts
 
 // Clusters and Namespaces for scoped access control tests
 const (
-	Cluster1     = "aaaaaaaa-bbbb-4041-0000-111111111111"
-	Cluster2     = "aaaaaaaa-bbbb-4041-0000-222222222222"
-	Cluster3     = "aaaaaaaa-bbbb-4041-0000-333333333333"
-	WrongCluster = "aaaaaaaa-bbbb-4041-0000-999999999999"
+	Cluster1     = "aaaaaaaa-bbbb-4011-0000-111111111111"
+	Cluster2     = "aaaaaaaa-bbbb-4011-0000-222222222222"
+	Cluster3     = "aaaaaaaa-bbbb-4011-0000-333333333333"
+	WrongCluster = "aaaaaaaa-bbbb-4011-0000-999999999999"
 
 	NamespaceA = "namespaceA"
 	NamespaceB = "namespaceB"
