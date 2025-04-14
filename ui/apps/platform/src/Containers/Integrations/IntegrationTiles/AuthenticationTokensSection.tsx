5314ee8468d49d04c2f742eaeac70ac8ab4a3876
David Vail <dcvail17@gmail.com>
2025-04-14 15:33:02 -0400

    ROX-28875: Remove init-bundles and crs from integrations page (#14967)

--- ui/apps/platform/src/Containers/Integrations/IntegrationTiles/AuthenticationTokensSection.tsx
+++ ui/apps/platform/src/Containers/Integrations/IntegrationTiles/AuthenticationTokensSection.tsx
@@ -1,29 +1,19 @@
 import React, { ReactElement } from 'react';
 
-import useAuthStatus from 'hooks/useAuthStatus';
 import usePermissions from 'hooks/usePermissions';
 
 import APITokensTile from './APITokensTile';
-import ClusterInitBundles from './ClusterInitBundlesTile';
-import ClusterRegistrationSecrets from './ClusterRegistrationSecretsTile';
 import IntegrationsSection from './IntegrationsSection';
 import MachineAccessTile from './MachineAccessTile';
 
 function AuthenticationTokensSection(): ReactElement {
-    // TODO after 4.4 release:
-    // Delete ClusterInitBundles tile from integrations.
-    // Delete unreachable code.
     // Delete request from integration sagas and data from integrations reducer.
-    const { currentUser } = useAuthStatus();
     const { hasReadAccess } = usePermissions();
-    const hasAdminRole = Boolean(currentUser?.userInfo?.roles.some(({ name }) => name === 'Admin')); // optional chaining just in case of the unexpected
     const hasReadAccessForAccess = hasReadAccess('Access');
 
     return (
         <IntegrationsSection headerName="Authentication Tokens" id="token-integrations">
             <APITokensTile />
-            {hasAdminRole && <ClusterInitBundles />}
-            {hasAdminRole && <ClusterRegistrationSecrets />}
             {hasReadAccessForAccess && <MachineAccessTile />}
         </IntegrationsSection>
     );
--- ui/apps/platform/src/types/integration.ts
+++ ui/apps/platform/src/types/integration.ts
@@ -16,11 +16,7 @@ export type IntegrationType =
     | SignatureIntegrationType
     | CloudSourceIntegrationType;
 
-export type AuthProviderType =
-    | 'apitoken'
-    | 'clusterInitBundle'
-    | 'clusterRegistrationSecret'
-    | 'machineAccess';
+export type AuthProviderType = 'apitoken' | 'machineAccess';
 
 // Investigate why the following occur in tableColumnDescriptor but not in integrationsList:
 /*
