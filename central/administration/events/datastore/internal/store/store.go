67ca4adf0f3cd410460980dfc431ddb80a136573
Tomasz Janiszewski <tomek@redhat.com>
2025-04-16 13:25:13 +0200

    deprecate
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

--- central/administration/events/datastore/internal/store/store.go
+++ central/administration/events/datastore/internal/store/store.go
@@ -11,7 +11,9 @@ import (
 type Store interface {
 	Count(ctx context.Context, q *v1.Query) (int, error)
 	Get(ctx context.Context, id string) (*storage.AdministrationEvent, bool, error)
+	// Deprecated: use GetByQueryFn instead
 	GetByQuery(ctx context.Context, query *v1.Query) ([]*storage.AdministrationEvent, error)
+	GetByQueryFn(ctx context.Context, query *v1.Query, fn func(obj *storage.AdministrationEvent) error) error
 	UpsertMany(ctx context.Context, objs []*storage.AdministrationEvent) error
 	DeleteMany(ctx context.Context, identifiers []string) error
 	GetMany(ctx context.Context, identifiers []string) ([]*storage.AdministrationEvent, []int, error)
