ebcc7d9d3de00160315d8e492a25964d6aead53f
RTann <rtannenb@redhat.com>
2025-04-24 15:31:12 -0700

    fix config and add comment

--- pkg/env/central_endpoint.go
+++ pkg/env/central_endpoint.go
@@ -1,5 +1,7 @@
 package env
 
+// These environment variables are used in deployment files.
+// Please check the files before deleting and check their usage in the code before editing.
 var (
 	// CentralEndpoint is used to provide Central's reachable endpoint to other StackRox services.
 	CentralEndpoint = RegisterSetting("ROX_CENTRAL_ENDPOINT", WithDefault("central.stackrox.svc:443"),
