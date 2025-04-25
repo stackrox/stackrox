620117e69b5699b6e5d446b5d6f97bf8ac3c0cf0
Vlad Bologa <vbologa@redhat.com>
2025-04-25 19:01:55 +0200

    Refactor testSecretReconciliation

--- operator/internal/central/extensions/common_test.go
+++ operator/internal/central/extensions/common_test.go
@@ -62,6 +62,54 @@ func basicSpecWithScanner(scannerEnabled bool, scannerV4Enabled bool) platform.C
 
 // TODO(ROX-9453): Refactor this to be used also by Secured Cluster reconciler extensions.
 func testSecretReconciliation(t *testing.T, runFn func(ctx context.Context, central *platform.Central, client ctrlClient.Client, direct ctrlClient.Reader, statusUpdater func(updateStatusFunc), log logr.Logger) error, c secretReconciliationTestCase) {
+	central := buildFakeCentral(c)
+	client := buildFakeClient(t, c, central)
+	statusUpdater := func(statusFunc updateStatusFunc) {
+		statusFunc(&central.Status)
+	}
+
+	// Verify that an initial invocation does not touch any of the existing unmanaged secrets, and creates
+	// the expected managed ones.
+	err := runFn(context.Background(), central.DeepCopy(), client, client, statusUpdater, logr.Discard())
+	if c.ExpectedError == "" {
+		require.NoError(t, err)
+	} else {
+		require.Error(t, err)
+		assert.Contains(t, err.Error(), c.ExpectedError)
+		return
+	}
+
+	if c.VerifyStatus != nil {
+		c.VerifyStatus(t, &central.Status)
+	}
+
+	secretsList, secretsByName := listSecrets(t, client)
+	verifyUnmanagedSecretsNotChanged(t, c.Existing, secretsByName)
+	verifyNotCreatedSecrets(t, c.ExpectedNotExistingSecrets, secretsByName)
+	verifyCreatedSecrets(t, c.ExpectedCreatedSecrets, secretsByName, central.Name)
+	assert.Empty(t, secretsByName, "one or more unexpected secrets exist")
+
+	// Verify that a second invocation does not further change the cluster state
+	err = runFn(context.Background(), central.DeepCopy(), client, client, statusUpdater, logr.Discard())
+	assert.NoError(t, err, "second invocation of reconciliation function failed")
+	if c.VerifyStatus != nil {
+		c.VerifyStatus(t, &central.Status)
+	}
+	verifySecretsMatch(t, client, secretsList)
+
+	// Fake deletion of the CR
+	central.DeletionTimestamp = new(metav1.Time)
+	*central.DeletionTimestamp = metav1.Now()
+
+	err = runFn(context.Background(), central.DeepCopy(), client, client, statusUpdater, logr.Discard())
+	assert.NoError(t, err, "deletion of CR resulted in error")
+
+	_, postDeletionSecretsByName := listSecrets(t, client)
+	verifyUnmanagedSecretsNotChanged(t, c.Existing, postDeletionSecretsByName)
+	assert.Empty(t, postDeletionSecretsByName, "newly created secrets remain after deletion")
+}
+
+func buildFakeCentral(c secretReconciliationTestCase) *platform.Central {
 	central := &platform.Central{
 		TypeMeta: metav1.TypeMeta{
 			APIVersion: "platform.stackrox.io/v1alpha1",
@@ -79,10 +127,10 @@ func testSecretReconciliation(t *testing.T, runFn func(ctx context.Context, cent
 		*central.DeletionTimestamp = metav1.Now()
 	}
 
-	statusUpdater := func(statusFunc updateStatusFunc) {
-		statusFunc(&central.Status)
-	}
+	return central
+}
 
+func buildFakeClient(t *testing.T, c secretReconciliationTestCase, central *platform.Central) ctrlClient.Client {
 	var existingSecrets []ctrlClient.Object
 	for _, existingSecret := range c.Existing {
 		existingSecrets = append(existingSecrets, existingSecret.DeepCopy())
@@ -106,33 +154,23 @@ func testSecretReconciliation(t *testing.T, runFn func(ctx context.Context, cent
 		WithRuntimeObjects(otherExisting...).
 		Build()
 
-	client = interceptor.NewClient(client, c.InterceptedK8sAPICalls)
+	return interceptor.NewClient(client, c.InterceptedK8sAPICalls)
+}
 
-	// Verify that an initial invocation does not touch any of the existing unmanaged secrets, and creates
-	// the expected managed ones.
-	err := runFn(context.Background(), central.DeepCopy(), client, client, statusUpdater, logr.Discard())
-	if c.ExpectedError == "" {
-		require.NoError(t, err)
-	} else {
-		require.Error(t, err)
-		assert.Contains(t, err.Error(), c.ExpectedError)
-		return
-	}
-
-	if c.VerifyStatus != nil {
-		c.VerifyStatus(t, &central.Status)
-	}
-
+func listSecrets(t *testing.T, client ctrlClient.Client) (*v1.SecretList, map[string]v1.Secret) {
 	secretsList := &v1.SecretList{}
-	err = client.List(context.Background(), secretsList, ctrlClient.InNamespace(testutils.TestNamespace))
+	err := client.List(context.Background(), secretsList, ctrlClient.InNamespace(testutils.TestNamespace))
 	require.NoError(t, err)
 
 	secretsByName := make(map[string]v1.Secret)
 	for _, secret := range secretsList.Items {
 		secretsByName[secret.Name] = secret
 	}
+	return secretsList, secretsByName
+}
 
-	for _, existingSecret := range c.Existing {
+func verifyUnmanagedSecretsNotChanged(t *testing.T, existing []*v1.Secret, secretsByName map[string]v1.Secret) {
+	for _, existingSecret := range existing {
 		found, ok := secretsByName[existingSecret.Name]
 		if !assert.Truef(t, ok, "pre-existing unmanaged secret %s is gone", existingSecret.Name) {
 			continue
@@ -140,20 +178,29 @@ func testSecretReconciliation(t *testing.T, runFn func(ctx context.Context, cent
 		assert.Equalf(t, existingSecret.Data, found.Data, "data of pre-existing unmanaged secret %s has changed", existingSecret.Name)
 		delete(secretsByName, existingSecret.Name)
 	}
+}
 
-	for _, notExistingSecret := range c.ExpectedNotExistingSecrets {
-		_, ok := secretsByName[notExistingSecret]
-		assert.Falsef(t, ok, "secret %s was created", notExistingSecret)
+func verifyNotCreatedSecrets(t *testing.T, expectedNotExisting []string, secretsByName map[string]v1.Secret) {
+	for _, name := range expectedNotExisting {
+		_, ok := secretsByName[name]
+		assert.Falsef(t, ok, "secret %s was created", name)
 	}
+}
 
-	for name, verifyFunc := range c.ExpectedCreatedSecrets {
+func verifyCreatedSecrets(
+	t *testing.T,
+	expectedCreatedSecrets map[string]secretVerifyFunc,
+	secretsByName map[string]v1.Secret,
+	ownerName string,
+) {
+	for name, verifyFunc := range expectedCreatedSecrets {
 		found, ok := secretsByName[name]
 		if !assert.True(t, ok, "expected secret %s was not created", name) {
 			continue
 		}
 		hasOwnerRef := false
 		for _, ownerRef := range found.ObjectMeta.GetOwnerReferences() {
-			if ownerRef.Name == "test-central" {
+			if ownerRef.Name == ownerName {
 				hasOwnerRef = true
 			}
 		}
@@ -161,48 +208,16 @@ func testSecretReconciliation(t *testing.T, runFn func(ctx context.Context, cent
 		verifyFunc(t, found.Data)
 		delete(secretsByName, name)
 	}
+}
 
-	assert.Empty(t, secretsByName, "one or more unexpected secrets exist")
-
-	// Verify that a second invocation does not further change the cluster state
-	err = runFn(context.Background(), central.DeepCopy(), client, client, statusUpdater, logr.Discard())
-	assert.NoError(t, err, "second invocation of reconciliation function failed")
-
-	if c.VerifyStatus != nil {
-		c.VerifyStatus(t, &central.Status)
-	}
-
-	secretsList2 := &v1.SecretList{}
-	err = client.List(context.Background(), secretsList2, ctrlClient.InNamespace(testutils.TestNamespace))
+func verifySecretsMatch(
+	t *testing.T,
+	client ctrlClient.Client,
+	initialSecrets *v1.SecretList,
+) {
+	secretsList := &v1.SecretList{}
+	err := client.List(context.Background(), secretsList, ctrlClient.InNamespace(testutils.TestNamespace))
 	require.NoError(t, err)
 
-	assert.ElementsMatch(t, secretsList.Items, secretsList2.Items, "second invocation changed the cluster state")
-
-	// Fake deletion of the CR
-	central.DeletionTimestamp = new(metav1.Time)
-	*central.DeletionTimestamp = metav1.Now()
-
-	err = runFn(context.Background(), central.DeepCopy(), client, client, statusUpdater, logr.Discard())
-	assert.NoError(t, err, "deletion of CR resulted in error")
-
-	secretsList3 := &v1.SecretList{}
-	err = client.List(context.Background(), secretsList3, ctrlClient.InNamespace(testutils.TestNamespace))
-	require.NoError(t, err)
-
-	postDeletionSecretsByName := make(map[string]v1.Secret)
-	for _, secret := range secretsList3.Items {
-		postDeletionSecretsByName[secret.Name] = secret
-	}
-
-	// Verify pre-existing secrets still exist
-	for _, existingSecret := range c.Existing {
-		found, ok := postDeletionSecretsByName[existingSecret.Name]
-		if !assert.Truef(t, ok, "pre-existing secret %s is gone", existingSecret.Name) {
-			continue
-		}
-		assert.Equalf(t, existingSecret.Data, found.Data, "data of pre-existing secret %s has changed", existingSecret.Name)
-		delete(postDeletionSecretsByName, existingSecret.Name)
-	}
-
-	assert.Empty(t, postDeletionSecretsByName, "newly created secrets remain after deletion")
+	assert.ElementsMatch(t, initialSecrets.Items, secretsList.Items, "second invocation changed the cluster state")
 }
