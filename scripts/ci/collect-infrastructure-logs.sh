92bb362673a9e49e18cf748a24e86e4927adaf2b
davdhacs <105243888+davdhacs@users.noreply.github.com>
2025-04-24 23:25:48 -0600

    timeouts

--- scripts/ci/collect-infrastructure-logs.sh
+++ scripts/ci/collect-infrastructure-logs.sh
@@ -39,48 +39,54 @@ proxy_pid=$!
 sleep 5 # Let kubectl proxy stabilize
 mkdir -p "${log_dir}"/infrastructure
 declare -a waiters
-( curl -s http://localhost:8001/logs/kube-apiserver.log > "${log_dir}"/infrastructure/kube-apiserver.log 2>&1 \
-  || { echo "curl exitcode:$?"; echo 'Can we just ignore exitcode 18? downloaded log file tail:'; tail "${log_dir}"/infrastructure/kube-apiserver.log; } ) &
+
+{ timeout 120s time kubectl -v logs -l component=kube-apiserver -n kube-system --insecure-skip-tls-verify-backend=true --ignore-errors=true > "${log_dir}"/infrastructure/kube-apiserver-kubectl.log \
+      2>&1; echo $?; ls -latr "${log_dir}"/infrastructure/kube-apiserver-kubectl.log; tail -3 "${log_dir}"/infrastructure/kube-apiserver-kubectl.log; } | sed 's/^/kubectl: /' &
 waiters+=($!)
 
-( curl --retry 5 -s http://localhost:8001/logs/kube-apiserver.log -o "${log_dir}"/infrastructure/kube-apiserver-o.log 2>&1 | sed 's/^/-o: /' \
-  || { echo "curl exitcode:$?"; echo 'Can we just ignore exitcode 18? downloaded log file tail:'; tail "${log_dir}"/infrastructure/kube-apiserver-o.log; } ) &
+{ timeout 120s curl -s http://localhost:8001/logs/kube-apiserver.log > "${log_dir}"/infrastructure/kube-apiserver.log 2>&1 \
+  || { echo "curl exitcode:$?"; echo 'Can we just ignore exitcode 18? downloaded log file tail:'; tail "${log_dir}"/infrastructure/kube-apiserver.log; } } &
 waiters+=($!)
 
-( curl -v \
+{ timeout 120s curl --retry 5 -s http://localhost:8001/logs/kube-apiserver.log -o "${log_dir}"/infrastructure/kube-apiserver-o.log 2>&1 \
+  || { echo "curl exitcode:$?"; echo 'Can we just ignore exitcode 18? downloaded log file tail:'; tail "${log_dir}"/infrastructure/kube-apiserver-o.log; }; } | sed 's/^/-o: /' &
+waiters+=($!)
+
+{ timeout 120s curl -v \
     --retry 5 \
     -s http://localhost:8001/logs/kube-apiserver.log \
-    -o "${log_dir}"/infrastructure/kube-apiserver-retry.log 2>&1 | sed 's/^/none: /'; echo $? ) &
+    -o "${log_dir}"/infrastructure/kube-apiserver-retry.log 2>&1; echo $?; } | sed 's/^/none: /' &
 waiters+=($!)
 
-( curl -v \
+{ timeout 120s curl -v \
     --retry 5 \
     --range 0-99999999,-99999999 \
     -s http://localhost:8001/logs/kube-apiserver.log \
-    -o "${log_dir}"/infrastructure/kube-apiserver-range.log 2>&1 | sed 's/^/range: /'; echo $? ) &
+    -o "${log_dir}"/infrastructure/kube-apiserver-range.log 2>&1; echo $?; } | sed 's/^/range: /' &
 waiters+=($!)
 
-( timeout 120s curl -v \
+{ timeout 120s curl -v \
     --retry 5 \
     --ignore-content-length \
     -s http://localhost:8001/logs/kube-apiserver.log \
-    -o "${log_dir}"/infrastructure/kube-apiserver-range-ignorelength.log 2>&1 | sed 's/^/ignore-len: /'; echo $? ) &
+    -o "${log_dir}"/infrastructure/kube-apiserver-range-ignorelength.log 2>&1; echo $?; } | sed 's/^/ignore-len: /' &
 waiters+=($!)
 
-( retry 5 true curl -v \
+{ retry 5 true curl -v \
     --retry 2 \
     --retry-all-errors \
     --continue-at - \
     -s http://localhost:8001/logs/kube-apiserver.log \
-    -o "${log_dir}"/infrastructure/kube-apiserver-breakfix.log 2>&1 | sed 's/^/breakfix: /'; echo $? ) &
+    -o "${log_dir}"/infrastructure/kube-apiserver-breakfix.log 2>&1; echo $?; } | sed 's/^/breakfix: /' &
 waiters+=($!)
 
-( retry 5 true curl -v \
+{ retry 5 true curl -v \
     --continue-at - \
     -s http://localhost:8001/logs/kube-apiserver.log \
-    -o "${log_dir}"/infrastructure/kube-apiserver-continue.log 2>&1 | sed 's/^/continue: /'; echo $? ) &
+    -o "${log_dir}"/infrastructure/kube-apiserver-continue.log 2>&1; echo $?; } | sed 's/^/continue: /' &
 waiters+=($!)
 
+echo "waiting for bg procs: ${waiters[*]}"
 wait ${waiters[*]}
 for testlogfile in "${log_dir}"/infrastructure/kube-apiserver-*log; do
   echo ">>> ${testlogfile}"
