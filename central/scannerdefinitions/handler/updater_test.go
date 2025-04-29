b3e5b47d644403de8cd7344981c768d2b742552b
Moritz Clasmeier <mclasmeier@redhat.com>
2025-04-29 08:14:15 +0200

    Temporarily deactivate scanner unit test (ROX-29099)

--- central/scannerdefinitions/handler/updater_test.go
+++ central/scannerdefinitions/handler/updater_test.go
@@ -3,7 +3,6 @@ package handler
 import (
 	"archive/zip"
 	"net/http"
-	"os"
 	"path/filepath"
 	"testing"
 	"time"
@@ -15,7 +14,7 @@ import (
 )
 
 const (
-	defURL = "https://definitions.stackrox.io/e799c68a-671f-44db-9682-f24248cd0ffe/diff.zip"
+	// defURL = "https://definitions.stackrox.io/e799c68a-671f-44db-9682-f24248cd0ffe/diff.zip"
 
 	mappingURL = "https://definitions.stackrox.io/v4/redhat-repository-mappings/mapping.zip"
 
@@ -32,37 +31,39 @@ func assertOnFileExistence(t *testing.T, path string, shouldExist bool) {
 	assert.Equal(t, shouldExist, exists)
 }
 
-func TestUpdate(t *testing.T) {
-	filePath := filepath.Join(t.TempDir(), "dump.zip")
-	u := newUpdater(file.New(filePath), &http.Client{Timeout: 30 * time.Second}, defURL, 1*time.Hour)
-	// Should fetch first time.
-	require.NoError(t, u.doUpdate())
-	assertOnFileExistence(t, filePath, true)
+// TODO(ROX-29099):
+//
+// func TestUpdate(t *testing.T) {
+// 	filePath := filepath.Join(t.TempDir(), "dump.zip")
+// 	u := newUpdater(file.New(filePath), &http.Client{Timeout: 30 * time.Second}, defURL, 1*time.Hour)
+// 	// Should fetch first time.
+// 	require.NoError(t, u.doUpdate())
+// 	assertOnFileExistence(t, filePath, true)
 
-	lastUpdatedTime := time.Now()
-	mustSetModTime(t, filePath, lastUpdatedTime)
-	// Should not fetch since it can't be updated in a time in the future.
-	require.NoError(t, u.doUpdate())
-	assert.Equal(t, lastUpdatedTime.UTC(), mustGetModTime(t, filePath))
-	assertOnFileExistence(t, filePath, true)
+// 	lastUpdatedTime := time.Now()
+// 	mustSetModTime(t, filePath, lastUpdatedTime)
+// 	// Should not fetch since it can't be updated in a time in the future.
+// 	require.NoError(t, u.doUpdate())
+// 	assert.Equal(t, lastUpdatedTime.UTC(), mustGetModTime(t, filePath))
+// 	assertOnFileExistence(t, filePath, true)
 
-	// Should definitely fetch.
-	mustSetModTime(t, filePath, nov23)
-	require.NoError(t, u.doUpdate())
-	assert.True(t, lastUpdatedTime.UTC().After(mustGetModTime(t, filePath)))
-	assert.True(t, mustGetModTime(t, filePath).After(nov23.UTC()))
-	assertOnFileExistence(t, filePath, true)
-}
+// 	// Should definitely fetch.
+// 	mustSetModTime(t, filePath, nov23)
+// 	require.NoError(t, u.doUpdate())
+// 	assert.True(t, lastUpdatedTime.UTC().After(mustGetModTime(t, filePath)))
+// 	assert.True(t, mustGetModTime(t, filePath).After(nov23.UTC()))
+// 	assertOnFileExistence(t, filePath, true)
+// }
 
-func mustGetModTime(t *testing.T, path string) time.Time {
-	fi, err := os.Stat(path)
-	require.NoError(t, err)
-	return fi.ModTime().UTC()
-}
+// func mustGetModTime(t *testing.T, path string) time.Time {
+// 	fi, err := os.Stat(path)
+// 	require.NoError(t, err)
+// 	return fi.ModTime().UTC()
+// }
 
-func mustSetModTime(t *testing.T, path string, modTime time.Time) {
-	require.NoError(t, os.Chtimes(path, time.Now(), modTime))
-}
+// func mustSetModTime(t *testing.T, path string, modTime time.Time) {
+// 	require.NoError(t, os.Chtimes(path, time.Now(), modTime))
+// }
 
 func TestMappingUpdate(t *testing.T) {
 	filePath := filepath.Join(t.TempDir(), "test.zip")
