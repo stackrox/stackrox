aab94bd39b25e5d2025207976496fd1b2a496915
Stephan Hesselmann <shesselm@redhat.com>
2025-04-29 15:56:06 +0200

    ROX-29089: rekor integration telemetry (#15097)

--- central/signatureintegration/datastore/telemetry_test.go
+++ central/signatureintegration/datastore/telemetry_test.go
@@ -9,20 +9,22 @@ import (
 )
 
 func TestGather(t *testing.T) {
-
 	assert.Empty(t, computeTelemetryProperties(context.Background(), []*storage.SignatureIntegration{}))
 
 	props := computeTelemetryProperties(context.Background(), []*storage.SignatureIntegration{
 		{},
 		{
-			Cosign: goodCosignConfig,
+			Cosign:          goodCosignConfig,
+			TransparencyLog: &storage.TransparencyLogVerification{Enabled: true, ValidateOffline: true},
 		},
 		{
 			CosignCertificates: goodCosignCertificateVerificationConfig,
+			TransparencyLog:    &storage.TransparencyLogVerification{Enabled: true, Url: "https://custom.rekor"},
 		},
 		{
 			Cosign:             goodCosignConfig,
 			CosignCertificates: goodCosignCertificateVerificationConfig,
+			TransparencyLog:    &storage.TransparencyLogVerification{Enabled: true, Url: "https://rekor.sigstore.dev"},
 		},
 		{
 			Cosign: goodCosignConfig,
@@ -35,11 +37,15 @@ func TestGather(t *testing.T) {
 	})
 
 	expectedProps := map[string]any{
-		"Total Signature Integration Certificates":            3,
-		"Total Signature Integration Cosign Public Keys":      3,
-		"Total Signature Integration With Custom Certificate": 1,
-		"Total Signature Integration With Custom Chain":       2,
-		"Total Signature Integrations":                        5,
+		"Total Signature Integration Certificates":                                 3,
+		"Total Signature Integration Cosign Public Keys":                           3,
+		"Total Signature Integration With Certificate Transparency Log Validation": 2,
+		"Total Signature Integration With Custom Certificate":                      1,
+		"Total Signature Integration With Custom Chain":                            2,
+		"Total Signature Integration With Transparency Log Custom Rekor URL":       1,
+		"Total Signature Integration With Transparency Log Offline Validation":     1,
+		"Total Signature Integration With Transparency Log Validation":             3,
+		"Total Signature Integrations":                                             5,
 	}
 	assert.Equal(t, expectedProps, props)
 }
