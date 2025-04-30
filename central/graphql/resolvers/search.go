279cacadcd94efa4b8b7980b373bebdd25badbdf
Dave Shrewsberry <dashrews@redhat.com>
2025-04-30 10:30:39 -0400

    update graphql autocomplete

---
+++ central/graphql/resolvers/search.go
@@ -0,0 +1,201 @@
+package resolvers
+
+import (
+	"context"
+	"math"
+
+	alertDataStore "github.com/stackrox/rox/central/alert/datastore"
+	"github.com/stackrox/rox/central/graphql/resolvers/inputtypes"
+	"github.com/stackrox/rox/central/graphql/resolvers/searchers"
+	"github.com/stackrox/rox/central/rbac/service"
+	searchService "github.com/stackrox/rox/central/search/service"
+	v1 "github.com/stackrox/rox/generated/api/v1"
+	"github.com/stackrox/rox/pkg/search"
+	"github.com/stackrox/rox/pkg/search/paginated"
+	"github.com/stackrox/rox/pkg/utils"
+)
+
+func init() {
+	schema := getBuilder()
+	utils.Must(
+		schema.AddQuery("searchOptions(categories: [SearchCategory!]): [String!]!"),
+		schema.AddQuery("globalSearch(categories: [SearchCategory!], query: String!): [SearchResult!]!"),
+		schema.AddQuery("searchAutocomplete(categories: [SearchCategory!], query: String!): [String!]!"),
+	)
+}
+
+// PaginatedQuery represents a query with pagination info
+type PaginatedQuery struct {
+	Query      *string
+	ScopeQuery *string
+	Pagination *inputtypes.Pagination
+}
+
+// PaginationWrapper represents pagination without any query
+type PaginationWrapper struct {
+	Pagination *inputtypes.Pagination
+}
+
+// AsV1QueryOrEmpty returns a proto query or empty proto query if pagination query is empty
+func (r *PaginatedQuery) AsV1QueryOrEmpty() (*v1.Query, error) {
+	var q *v1.Query
+	if r == nil || r.Query == nil {
+		q := search.EmptyQuery()
+		paginated.FillPagination(q, r.Pagination.AsV1Pagination(), math.MaxInt32)
+		return q, nil
+	}
+	q, err := search.ParseQuery(*r.Query, search.MatchAllIfEmpty())
+	if err != nil {
+		return nil, err
+	}
+	paginated.FillPagination(q, r.Pagination.AsV1Pagination(), math.MaxInt32)
+	return q, nil
+}
+
+// AsV1ScopeQueryOrEmpty returns a proto query or empty proto query if pagination query is empty
+func (r *PaginatedQuery) AsV1ScopeQueryOrEmpty() (*v1.Query, error) {
+	var q *v1.Query
+	if r == nil || r.ScopeQuery == nil {
+		q := search.EmptyQuery()
+		return q, nil
+	}
+	q, err := search.ParseQuery(*r.ScopeQuery, search.MatchAllIfEmpty())
+	if err != nil {
+		return nil, err
+	}
+	return q, nil
+}
+
+// String returns a String representation of PaginatedQuery
+func (r *PaginatedQuery) String() string {
+	if r == nil || r.Query == nil {
+		return ""
+	}
+	return *r.Query
+}
+
+// IsEmpty means no query was specified
+func (r *PaginatedQuery) IsEmpty() bool {
+	return r == nil || r.Query == nil
+}
+
+// RawQuery represents a raw query
+type RawQuery struct {
+	Query      *string
+	ScopeQuery *string
+}
+
+// AsV1QueryOrEmpty returns a proto query or empty proto query if raw query is empty
+func (r RawQuery) AsV1QueryOrEmpty(opts ...search.ParseQueryOption) (*v1.Query, error) {
+	if r.Query == nil {
+		return search.EmptyQuery(), nil
+	}
+	opts = append(opts, search.MatchAllIfEmpty())
+	return search.ParseQuery(*r.Query, opts...)
+}
+
+// AsV1ScopeQueryOrEmpty returns a proto query or empty proto query if pagination query is empty
+func (r *RawQuery) AsV1ScopeQueryOrEmpty() (*v1.Query, error) {
+	var q *v1.Query
+	if r == nil || r.ScopeQuery == nil {
+		q := search.EmptyQuery()
+		return q, nil
+	}
+	q, err := search.ParseQuery(*r.ScopeQuery, search.MatchAllIfEmpty())
+	if err != nil {
+		return nil, err
+	}
+	return q, nil
+}
+
+// String returns a String representation of RawQuery
+func (r RawQuery) String() string {
+	if r.Query == nil {
+		return ""
+	}
+	return *r.Query
+}
+
+// IsEmpty means no query specified
+func (r RawQuery) IsEmpty() bool {
+	return r.Query == nil
+}
+
+func (resolver *Resolver) getAutoCompleteSearchers() map[v1.SearchCategory]search.Searcher {
+	searchers := map[v1.SearchCategory]search.Searcher{
+		v1.SearchCategory_ALERTS:                   &alertDataStore.DefaultStateAlertDataStoreImpl{DataStore: &resolver.ViolationsDataStore},
+		v1.SearchCategory_CLUSTERS:                 resolver.ClusterDataStore,
+		v1.SearchCategory_DEPLOYMENTS:              resolver.DeploymentDataStore,
+		v1.SearchCategory_IMAGES:                   resolver.ImageDataStore,
+		v1.SearchCategory_POLICIES:                 resolver.PolicyDataStore,
+		v1.SearchCategory_SECRETS:                  resolver.SecretsDataStore,
+		v1.SearchCategory_NAMESPACES:               resolver.NamespaceDataStore,
+		v1.SearchCategory_NODES:                    resolver.NodeDataStore,
+		v1.SearchCategory_COMPLIANCE:               resolver.ComplianceAggregator,
+		v1.SearchCategory_SERVICE_ACCOUNTS:         resolver.ServiceAccountsDataStore,
+		v1.SearchCategory_ROLES:                    resolver.K8sRoleStore,
+		v1.SearchCategory_ROLEBINDINGS:             resolver.K8sRoleBindingStore,
+		v1.SearchCategory_IMAGE_COMPONENTS:         resolver.ImageComponentDataStore,
+		v1.SearchCategory_SUBJECTS:                 service.NewSubjectSearcher(resolver.K8sRoleBindingStore),
+		v1.SearchCategory_IMAGE_VULNERABILITIES:    resolver.ImageCVEDataStore,
+		v1.SearchCategory_NODE_VULNERABILITIES:     searchers.NewNonOrphanedNodeCVESearcher(resolver.NodeCVEDataStore),
+		v1.SearchCategory_CLUSTER_VULNERABILITIES:  resolver.ClusterCVEDataStore,
+		v1.SearchCategory_NODE_COMPONENTS:          resolver.NodeComponentDataStore,
+		v1.SearchCategory_POLICY_CATEGORIES:        resolver.PolicyCategoryDataStore,
+		v1.SearchCategory_IMAGE_COMPONENTS_V2:      resolver.ImageComponentV2DataStore,
+		v1.SearchCategory_IMAGE_VULNERABILITIES_V2: resolver.ImageCVEV2DataStore,
+	}
+
+	return searchers
+}
+
+func (resolver *Resolver) getSearchFuncs() map[v1.SearchCategory]searchService.SearchFunc {
+
+	searchfuncs := map[v1.SearchCategory]searchService.SearchFunc{
+		v1.SearchCategory_ALERTS:                   resolver.ViolationsDataStore.SearchAlerts,
+		v1.SearchCategory_CLUSTERS:                 resolver.ClusterDataStore.SearchResults,
+		v1.SearchCategory_DEPLOYMENTS:              resolver.DeploymentDataStore.SearchDeployments,
+		v1.SearchCategory_IMAGES:                   resolver.ImageDataStore.SearchImages,
+		v1.SearchCategory_POLICIES:                 resolver.PolicyDataStore.SearchPolicies,
+		v1.SearchCategory_SECRETS:                  resolver.SecretsDataStore.SearchSecrets,
+		v1.SearchCategory_NAMESPACES:               resolver.NamespaceDataStore.SearchResults,
+		v1.SearchCategory_NODES:                    resolver.NodeDataStore.SearchNodes,
+		v1.SearchCategory_SERVICE_ACCOUNTS:         resolver.ServiceAccountsDataStore.SearchServiceAccounts,
+		v1.SearchCategory_ROLES:                    resolver.K8sRoleStore.SearchRoles,
+		v1.SearchCategory_ROLEBINDINGS:             resolver.K8sRoleBindingStore.SearchRoleBindings,
+		v1.SearchCategory_IMAGE_COMPONENTS:         resolver.ImageComponentDataStore.SearchImageComponents,
+		v1.SearchCategory_SUBJECTS:                 service.NewSubjectSearcher(resolver.K8sRoleBindingStore).SearchSubjects,
+		v1.SearchCategory_IMAGE_VULNERABILITIES:    resolver.ImageCVEDataStore.SearchImageCVEs,
+		v1.SearchCategory_NODE_VULNERABILITIES:     searchers.NewNonOrphanedNodeCVESearcher(resolver.NodeCVEDataStore).SearchNodeCVEs,
+		v1.SearchCategory_CLUSTER_VULNERABILITIES:  resolver.ClusterCVEDataStore.SearchClusterCVEs,
+		v1.SearchCategory_NODE_COMPONENTS:          resolver.NodeComponentDataStore.SearchNodeComponents,
+		v1.SearchCategory_POLICY_CATEGORIES:        resolver.PolicyCategoryDataStore.SearchPolicyCategories,
+		v1.SearchCategory_IMAGE_COMPONENTS_V2:      resolver.ImageComponentV2DataStore.SearchImageComponents,
+		v1.SearchCategory_IMAGE_VULNERABILITIES_V2: resolver.ImageCVEV2DataStore.SearchImageCVEs,
+	}
+
+	return searchfuncs
+}
+
+type searchRequest struct {
+	Query      string
+	Categories *[]string
+}
+
+// SearchAutocomplete returns autocomplete responses for the given partial query.
+func (resolver *Resolver) SearchAutocomplete(ctx context.Context, args searchRequest) ([]string, error) {
+	return searchService.RunAutoComplete(ctx, args.Query, toSearchCategories(args.Categories), resolver.getAutoCompleteSearchers())
+}
+
+// SearchOptions gets all search options available for the listed categories
+func (resolver *Resolver) SearchOptions(_ context.Context, args struct{ Categories *[]string }) ([]string, error) {
+	return searchService.Options(toSearchCategories(args.Categories)), nil
+}
+
+// GlobalSearch returns search results for the given categories and query.
+// Note: there is not currently a way to request the underlying object from SearchResult; it might be nice to have
+// this in the future.
+func (resolver *Resolver) GlobalSearch(ctx context.Context, args searchRequest) ([]*searchResultResolver, error) {
+	results, _, err := searchService.GlobalSearch(ctx, args.Query, toSearchCategories(args.Categories), resolver.getSearchFuncs())
+	return resolver.wrapSearchResults(results, err)
+}
---
+++ central/graphql/resolvers/search_test.go
@@ -0,0 +1,481 @@
+//go:build sql_integration
+
+package resolvers
+
+import (
+	"context"
+	"fmt"
+	"math"
+	"testing"
+
+	alertMocks "github.com/stackrox/rox/central/alert/datastore/mocks"
+	clusterMocks "github.com/stackrox/rox/central/cluster/datastore/mocks"
+	clusterCVEMocks "github.com/stackrox/rox/central/cve/cluster/datastore/mocks"
+	imageCVEMocks "github.com/stackrox/rox/central/cve/image/datastore/mocks"
+	imageCVEV2Mocks "github.com/stackrox/rox/central/cve/image/v2/datastore/mocks"
+	nodeCVEMocks "github.com/stackrox/rox/central/cve/node/datastore/mocks"
+	deploymentMocks "github.com/stackrox/rox/central/deployment/datastore/mocks"
+	"github.com/stackrox/rox/central/graphql/resolvers/inputtypes"
+	"github.com/stackrox/rox/central/graphql/resolvers/loaders"
+	imageDS "github.com/stackrox/rox/central/image/datastore"
+	imageMocks "github.com/stackrox/rox/central/image/datastore/mocks"
+	imageComponentMocks "github.com/stackrox/rox/central/imagecomponent/datastore/mocks"
+	imageComponentV2Mocks "github.com/stackrox/rox/central/imagecomponent/v2/datastore/mocks"
+	namespaceMocks "github.com/stackrox/rox/central/namespace/datastore/mocks"
+	npsMocks "github.com/stackrox/rox/central/networkpolicies/datastore/mocks"
+	nodeMocks "github.com/stackrox/rox/central/node/datastore/mocks"
+	nodeComponentMocks "github.com/stackrox/rox/central/nodecomponent/datastore/mocks"
+	policyMocks "github.com/stackrox/rox/central/policy/datastore/mocks"
+	policyCategoryMocks "github.com/stackrox/rox/central/policycategory/datastore/mocks"
+	k8sroleMocks "github.com/stackrox/rox/central/rbac/k8srole/datastore/mocks"
+	k8sRoleBindingDataStore "github.com/stackrox/rox/central/rbac/k8srolebinding/datastore"
+	k8srolebindingMocks "github.com/stackrox/rox/central/rbac/k8srolebinding/datastore/mocks"
+	globalSearch "github.com/stackrox/rox/central/search"
+	secretMocks "github.com/stackrox/rox/central/secret/datastore/mocks"
+	serviceAccountMocks "github.com/stackrox/rox/central/serviceaccount/datastore/mocks"
+	imagesView "github.com/stackrox/rox/central/views/images"
+	v1 "github.com/stackrox/rox/generated/api/v1"
+	"github.com/stackrox/rox/pkg/fixtures"
+	"github.com/stackrox/rox/pkg/grpc/authz/allow"
+	"github.com/stackrox/rox/pkg/pointers"
+	"github.com/stackrox/rox/pkg/postgres/pgtest"
+	"github.com/stackrox/rox/pkg/protoassert"
+	"github.com/stackrox/rox/pkg/sac"
+	"github.com/stackrox/rox/pkg/search"
+	"github.com/stackrox/rox/pkg/search/postgres/aggregatefunc"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+)
+
+func TestSearchCategories(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	cluster := clusterMocks.NewMockDataStore(ctrl)
+	deployment := deploymentMocks.NewMockDataStore(ctrl)
+	namespace := namespaceMocks.NewMockDataStore(ctrl)
+	secret := secretMocks.NewMockDataStore(ctrl)
+	nps := npsMocks.NewMockDataStore(ctrl)
+	violations := alertMocks.NewMockDataStore(ctrl)
+	images := imageMocks.NewMockDataStore(ctrl)
+	policies := policyMocks.NewMockDataStore(ctrl)
+	nodes := nodeMocks.NewMockDataStore(ctrl)
+	serviceAccounts := serviceAccountMocks.NewMockDataStore(ctrl)
+	roles := k8sroleMocks.NewMockDataStore(ctrl)
+	rolebindings := k8srolebindingMocks.NewMockDataStore(ctrl)
+	components := imageComponentMocks.NewMockDataStore(ctrl)
+
+	resolver := &Resolver{
+		ClusterDataStore:          cluster,
+		DeploymentDataStore:       deployment,
+		PolicyDataStore:           policies,
+		NamespaceDataStore:        namespace,
+		SecretsDataStore:          secret,
+		NetworkPoliciesStore:      nps,
+		ViolationsDataStore:       violations,
+		ImageDataStore:            images,
+		ServiceAccountsDataStore:  serviceAccounts,
+		NodeDataStore:             nodes,
+		K8sRoleBindingStore:       rolebindings,
+		K8sRoleStore:              roles,
+		ImageComponentDataStore:   components,
+		PolicyCategoryDataStore:   policyCategoryMocks.NewMockDataStore(ctrl),
+		ImageCVEDataStore:         imageCVEMocks.NewMockDataStore(ctrl),
+		NodeCVEDataStore:          nodeCVEMocks.NewMockDataStore(ctrl),
+		ClusterCVEDataStore:       clusterCVEMocks.NewMockDataStore(ctrl),
+		NodeComponentDataStore:    nodeComponentMocks.NewMockDataStore(ctrl),
+		ImageComponentV2DataStore: imageComponentV2Mocks.NewMockDataStore(ctrl),
+		ImageCVEV2DataStore:       imageCVEV2Mocks.NewMockDataStore(ctrl),
+	}
+
+	searchCategories := resolver.getAutoCompleteSearchers()
+	searchFuncs := resolver.getSearchFuncs()
+
+	for globalCategory := range globalSearch.GetGlobalSearchCategories() {
+		if globalCategory == v1.SearchCategory_IMAGE_INTEGRATIONS {
+			continue
+		}
+		assert.True(t, searchCategories[globalCategory] != nil, "global search category %s does not exist in auto complete", globalCategory)
+	}
+	for category := range searchCategories {
+		switch category {
+		case v1.SearchCategory_COMPLIANCE:
+			continue
+		default:
+			assert.True(t, searchFuncs[category] != nil, "search category %s does not have a search func", category.String())
+		}
+	}
+}
+
+func TestAsV1QueryOrEmpty(t *testing.T) {
+	for _, tc := range []struct {
+		desc      string
+		arg       PaginatedQuery
+		expectedQ *v1.Query
+	}{
+		{
+			desc: "simple query",
+			arg: PaginatedQuery{
+				Query: pointers.String("CVE:abc"),
+			},
+			expectedQ: search.NewQueryBuilder().AddStrings(search.CVE, "abc").
+				WithPagination(search.NewPagination().Limit(math.MaxInt32)).ProtoQuery(),
+		},
+		{
+			desc: "simple query w/ plus in value",
+			arg: PaginatedQuery{
+				Query: pointers.String("CVE:ab+c"),
+			},
+			expectedQ: search.NewQueryBuilder().AddStrings(search.CVE, "ab+c").
+				WithPagination(search.NewPagination().Limit(math.MaxInt32)).ProtoQuery(),
+		},
+		{
+			desc: "exact query",
+			arg: PaginatedQuery{
+				Query: pointers.String("CVE:\"abc\""),
+			},
+			expectedQ: search.NewQueryBuilder().AddExactMatches(search.CVE, "abc").
+				WithPagination(search.NewPagination().Limit(math.MaxInt32)).ProtoQuery(),
+		},
+		{
+			desc: "exact query w/ plus in value",
+			arg: PaginatedQuery{
+				Query: pointers.String("CVE:\"ab+c\""),
+			},
+			expectedQ: search.NewQueryBuilder().AddExactMatches(search.CVE, "ab+c").
+				WithPagination(search.NewPagination().Limit(math.MaxInt32)).ProtoQuery(),
+		},
+		{
+			desc: "conjunction query",
+			arg: PaginatedQuery{
+				Query: pointers.String("CVE:abc+Image:xyz"),
+			},
+			expectedQ: search.NewQueryBuilder().
+				AddStrings(search.CVE, "abc").AddStrings(search.ImageName, "xyz").
+				WithPagination(search.NewPagination().Limit(math.MaxInt32)).ProtoQuery(),
+		},
+		{
+			desc: "disjunction query",
+			arg: PaginatedQuery{
+				Query: pointers.String("CVE:abc,xyz"),
+			},
+			expectedQ: search.NewQueryBuilder().AddStrings(search.CVE, "abc", "xyz").
+				WithPagination(search.NewPagination().Limit(math.MaxInt32)).ProtoQuery(),
+		},
+		{
+			desc: "conjunction & disjunctions",
+			arg: PaginatedQuery{
+				Query: pointers.String("CVE:abc,xyz+Image:img1,img2"),
+			},
+			expectedQ: search.NewQueryBuilder().
+				AddStrings(search.CVE, "abc", "xyz").AddStrings(search.ImageName, "img1", "img2").
+				WithPagination(search.NewPagination().Limit(math.MaxInt32)).ProtoQuery(),
+		},
+		{
+			desc: "query + sort",
+			arg: PaginatedQuery{
+				Query: pointers.String("CVE:abc"),
+				Pagination: &inputtypes.Pagination{
+					SortOption: &inputtypes.SortOption{
+						Field: pointers.String("Image"),
+					},
+				},
+			},
+			expectedQ: search.NewQueryBuilder().AddStrings(search.CVE, "abc").WithPagination(
+				search.NewPagination().AddSortOption(search.NewSortOption(search.ImageName)).Limit(math.MaxInt32),
+			).ProtoQuery(),
+		},
+		{
+			desc: "query + sort + limit",
+			arg: PaginatedQuery{
+				Query: pointers.String("CVE:abc"),
+				Pagination: &inputtypes.Pagination{
+					SortOption: &inputtypes.SortOption{
+						Field: pointers.String("Image"),
+					},
+					Limit: pointers.Int32(10),
+				},
+			},
+			expectedQ: search.NewQueryBuilder().AddStrings(search.CVE, "abc").WithPagination(
+				search.NewPagination().AddSortOption(search.NewSortOption(search.ImageName)).Limit(10),
+			).ProtoQuery(),
+		},
+		{
+			desc: "query + sort + aggregate + limit",
+			arg: PaginatedQuery{
+				Query: pointers.String("CVE:abc"),
+				Pagination: &inputtypes.Pagination{
+					SortOption: &inputtypes.SortOption{
+						Field: pointers.String("Image"),
+						AggregateBy: &inputtypes.AggregateBy{
+							AggregateFunc: pointers.String("count"),
+						},
+					},
+					Limit: pointers.Int32(10),
+				},
+			},
+			expectedQ: search.NewQueryBuilder().AddStrings(search.CVE, "abc").WithPagination(
+				search.NewPagination().AddSortOption(
+					search.NewSortOption(search.ImageName).AggregateBy(aggregatefunc.Count, false),
+				).Limit(10),
+			).ProtoQuery(),
+		},
+		{
+			desc: "query + primary sort + secondary sort + limit",
+			arg: PaginatedQuery{
+				Query: pointers.String("CVE:abc"),
+				Pagination: &inputtypes.Pagination{
+					SortOptions: &[]*inputtypes.SortOption{
+						{
+							Field: pointers.String("Image"),
+						},
+						{
+							Field: pointers.String("Component"),
+						},
+					},
+					Limit: pointers.Int32(10),
+				},
+			},
+			expectedQ: search.NewQueryBuilder().AddStrings(search.CVE, "abc").WithPagination(
+				search.NewPagination().
+					AddSortOption(search.NewSortOption(search.ImageName)).
+					AddSortOption(search.NewSortOption(search.Component)).
+					Limit(10),
+			).ProtoQuery(),
+		},
+		{
+			desc: "query + primary sort w/ aggregate + secondary sort  + limit",
+			arg: PaginatedQuery{
+				Query: pointers.String("CVE:abc"),
+				Pagination: &inputtypes.Pagination{
+					SortOptions: &[]*inputtypes.SortOption{
+						{
+							Field: pointers.String("Image"),
+							AggregateBy: &inputtypes.AggregateBy{
+								AggregateFunc: pointers.String("count"),
+							},
+						},
+						{
+							Field: pointers.String("Component"),
+						},
+					},
+					Limit: pointers.Int32(10),
+				},
+			},
+			expectedQ: search.NewQueryBuilder().AddStrings(search.CVE, "abc").WithPagination(
+				search.NewPagination().
+					AddSortOption(search.NewSortOption(search.ImageName).AggregateBy(aggregatefunc.Count, false)).
+					AddSortOption(search.NewSortOption(search.Component)).
+					Limit(10),
+			).ProtoQuery(),
+		},
+		{
+			desc: "query + primary sort + secondary sort  w/ aggregate + limit",
+			arg: PaginatedQuery{
+				Query: pointers.String("CVE:abc"),
+				Pagination: &inputtypes.Pagination{
+					SortOptions: &[]*inputtypes.SortOption{
+						{
+							Field: pointers.String("Image"),
+						},
+						{
+							Field: pointers.String("Component"),
+							AggregateBy: &inputtypes.AggregateBy{
+								AggregateFunc: pointers.String("count"),
+							},
+						},
+					},
+					Limit: pointers.Int32(10),
+				},
+			},
+			expectedQ: search.NewQueryBuilder().AddStrings(search.CVE, "abc").WithPagination(
+				search.NewPagination().
+					AddSortOption(search.NewSortOption(search.ImageName)).
+					AddSortOption(search.NewSortOption(search.Component).AggregateBy(aggregatefunc.Count, false)).
+					Limit(10),
+			).ProtoQuery(),
+		},
+		{
+			desc: "query + nil sort",
+			arg: PaginatedQuery{
+				Query: pointers.String("CVE:abc"),
+				Pagination: &inputtypes.Pagination{
+					SortOptions: &[]*inputtypes.SortOption{nil},
+				},
+			},
+			expectedQ: search.NewQueryBuilder().AddStrings(search.CVE, "abc").
+				WithPagination(search.NewPagination().Limit(math.MaxInt32)).ProtoQuery(),
+		},
+		{
+			desc: "query + empty sorts",
+			arg: PaginatedQuery{
+				Query: pointers.String("CVE:abc"),
+				Pagination: &inputtypes.Pagination{
+					SortOptions: &[]*inputtypes.SortOption{
+						{},
+						{},
+					},
+				},
+			},
+			expectedQ: search.NewQueryBuilder().AddStrings(search.CVE, "abc").
+				WithPagination(search.NewPagination().
+					AddSortOption(search.NewSortOption("")).
+					AddSortOption(search.NewSortOption("")).Limit(math.MaxInt32),
+				).ProtoQuery(),
+		},
+		{
+			desc: "query + primary sort + secondary sort w/ invalid aggregate + limit",
+			arg: PaginatedQuery{
+				Query: pointers.String("CVE:abc"),
+				Pagination: &inputtypes.Pagination{
+					SortOptions: &[]*inputtypes.SortOption{
+						{
+							Field: pointers.String("Image"),
+						},
+						{
+							Field: pointers.String("Component"),
+							AggregateBy: &inputtypes.AggregateBy{
+								AggregateFunc: pointers.String("trinity"),
+							},
+						},
+					},
+					Limit: pointers.Int32(10),
+				},
+			},
+			expectedQ: search.NewQueryBuilder().AddStrings(search.CVE, "abc").WithPagination(
+				search.NewPagination().
+					AddSortOption(search.NewSortOption(search.ImageName)).
+					AddSortOption(search.NewSortOption(search.Component).AggregateBy(aggregatefunc.Unset, false)).
+					Limit(10),
+			).ProtoQuery(),
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			actual, err := tc.arg.AsV1QueryOrEmpty()
+			assert.NoError(t, err)
+			protoassert.Equal(t, tc.expectedQ, actual)
+		})
+	}
+}
+
+func TestSubjectAutocompleteSearch(t *testing.T) {
+
+	testDB := pgtest.ForT(t)
+	testGormDB := testDB.GetGormDB(t)
+	defer pgtest.CloseGormDB(t, testGormDB)
+
+	roleBindingDatastore := k8sRoleBindingDataStore.GetTestPostgresDataStore(t, testDB.DB)
+
+	ctx := loaders.WithLoaderContext(sac.WithAllAccess(context.Background()))
+	roleBindings := fixtures.GetMultipleK8sRoleBindings(2, 3)
+	for _, roleBinding := range roleBindings {
+		require.NoError(t, roleBindingDatastore.UpsertRoleBinding(ctx, roleBinding))
+	}
+
+	resolver, _ := SetupTestResolver(t, roleBindingDatastore)
+	allowAllCtx := SetAuthorizerOverride(ctx, allow.Anonymous())
+
+	testCases := []struct {
+		desc     string
+		request  searchRequest
+		expected []string
+	}{
+		{
+			desc: "Subject name autocomplete",
+			request: searchRequest{
+				Query:      fmt.Sprintf("Subject:%s", roleBindings[0].Subjects[1].Name),
+				Categories: &[]string{"SUBJECTS"},
+			},
+			expected: []string{roleBindings[0].Subjects[1].Name},
+		},
+		{
+			desc: "Subject Kind autocomplete",
+			request: searchRequest{
+				Query:      "Subject Kind:",
+				Categories: &[]string{"SUBJECTS"},
+			},
+			expected: []string{"user", "group"},
+		},
+		{
+			desc: "Cluster name autocomplete",
+			request: searchRequest{
+				Query:      fmt.Sprintf("Cluster:%s", roleBindings[1].ClusterName),
+				Categories: &[]string{"SUBJECTS"},
+			},
+			expected: []string{roleBindings[1].ClusterName},
+		},
+		{
+			desc: "Cluster role autocomplete",
+			request: searchRequest{
+				Query:      "Cluster Role:tr",
+				Categories: &[]string{"SUBJECTS"},
+			},
+			expected: []string{"true"},
+		},
+		{
+			desc: "Cluster name + Subject name autocomplete",
+			request: searchRequest{
+				Query:      fmt.Sprintf("Cluster:%s+Subject:", roleBindings[0].ClusterName),
+				Categories: &[]string{"SUBJECTS"},
+			},
+			expected: []string{roleBindings[0].Subjects[1].Name, roleBindings[0].Subjects[2].Name},
+		},
+		{
+			desc: "Autocomplete on unsupported option",
+			request: searchRequest{
+				Query:      "Deployment:d1",
+				Categories: &[]string{"SUBJECTS"},
+			},
+			expected: []string{},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			results, err := resolver.SearchAutocomplete(allowAllCtx, tc.request)
+			require.NoError(t, err)
+			require.ElementsMatch(t, tc.expected, results)
+		})
+	}
+}
+
+func TestImageLabelAutoCompleteSearch(t *testing.T) {
+	testDB := pgtest.ForT(t)
+	testGormDB := testDB.GetGormDB(t)
+	defer pgtest.CloseGormDB(t, testGormDB)
+
+	imageDatastore := imageDS.GetTestPostgresDataStore(t, testDB.DB)
+	ctx := loaders.WithLoaderContext(sac.WithAllAccess(context.Background()))
+
+	resolver, _ := SetupTestResolver(t, imageDatastore, imagesView.NewImageView(testDB.DB))
+	allowAllCtx := SetAuthorizerOverride(ctx, allow.Anonymous())
+
+	// Case: nil labels
+	image := fixtures.GetImage()
+	image.GetMetadata().GetV1().Labels = nil
+
+	require.NoError(t, imageDatastore.UpsertImage(ctx, image))
+
+	request := searchRequest{
+		Query:      "Image Label:",
+		Categories: &[]string{"IMAGES"},
+	}
+	results, err := resolver.SearchAutocomplete(allowAllCtx, request)
+	require.NoError(t, err)
+	require.ElementsMatch(t, []string{}, results)
+
+	// Case: Non-empty labels
+	image.GetMetadata().GetV1().Labels = map[string]string{
+		"k1": "v1",
+		"k2": "v2",
+	}
+
+	require.NoError(t, imageDatastore.UpsertImage(ctx, image))
+
+	request = searchRequest{
+		Query:      "Image Label:",
+		Categories: &[]string{"IMAGES"},
+	}
+	results, err = resolver.SearchAutocomplete(allowAllCtx, request)
+	require.NoError(t, err)
+	require.ElementsMatch(t, []string{"k1=v1", "k2=v2"}, results)
+}
