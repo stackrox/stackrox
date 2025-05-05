c9fdb6f01577ed81fb435b09d022228370c2b948
David Shrewsberry <99685630+dashrews78@users.noreply.github.com>
2025-05-05 09:32:56 -0400

    ROX-29109:  cdm riskiest dashboard (#15118)

--- pkg/search/derivationtype_string.go
+++ pkg/search/derivationtype_string.go
@@ -12,11 +12,12 @@ func _() {
 	_ = x[SimpleReverseSortDerivationType-1]
 	_ = x[MaxDerivationType-2]
 	_ = x[CustomFieldType-3]
+	_ = x[MinDerivationType-4]
 }
 
-const _DerivationType_name = "CountDerivationTypeSimpleReverseSortDerivationTypeMaxDerivationTypeCustomFieldType"
+const _DerivationType_name = "CountDerivationTypeSimpleReverseSortDerivationTypeMaxDerivationTypeCustomFieldTypeMinDerivationType"
 
-var _DerivationType_index = [...]uint8{0, 19, 50, 67, 82}
+var _DerivationType_index = [...]uint8{0, 19, 50, 67, 82, 99}
 
 func (i DerivationType) String() string {
 	if i < 0 || i >= DerivationType(len(_DerivationType_index)-1) {
