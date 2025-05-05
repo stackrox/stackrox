c1b4f97d38cc4650d4e3adf006d972872902e0a4
Michaël Petrov <smartptr@gmail.com>
2025-05-05 11:06:25 +0200

    undo unnecessary changes in lib.sh

--- scripts/ci/lib.sh
+++ scripts/ci/lib.sh
@@ -154,7 +154,7 @@ get_central_debug_dump() {
 
     require_environment "API_ENDPOINT"
     require_environment "ROX_ADMIN_PASSWORD"
-    ROX_SERVER_NAME="" roxctl -e "${API_ENDPOINT}" \
+    roxctl -e "${API_ENDPOINT}" \
         central debug dump --output-dir "${output_dir}" \
         --insecure-skip-tls-verify
     ls -l "${output_dir}"
