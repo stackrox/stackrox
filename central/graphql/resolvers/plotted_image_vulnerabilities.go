37172701c4c6b7b2c9089a3154351a20b3979659
Dave Shrewsberry <dashrews@redhat.com>
2025-05-05 16:22:13 -0400

    plotted stuff

--- central/graphql/resolvers/plotted_image_vulnerabilities.go
+++ central/graphql/resolvers/plotted_image_vulnerabilities.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/stackrox/rox/central/graphql/resolvers/loaders"
 	v1 "github.com/stackrox/rox/generated/api/v1"
+	"github.com/stackrox/rox/pkg/features"
 	"github.com/stackrox/rox/pkg/search"
 	"github.com/stackrox/rox/pkg/utils"
 )
@@ -52,6 +53,28 @@ func (resolver *Resolver) PlottedImageVulnerabilities(ctx context.Context, args 
 			},
 		},
 	}
+
+	if features.FlattenCVEData.Enabled() {
+		// get loader
+		vulnLoader, err := loaders.GetImageCVEV2Loader(ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		allCveIds, err := vulnLoader.GetIDs(ctx, query)
+		if err != nil {
+			return nil, err
+		}
+
+		fixableCount, err := vulnLoader.CountFromQuery(ctx,
+			search.ConjunctionQuery(query, search.NewQueryBuilder().AddBools(search.Fixable, true).ProtoQuery()))
+		if err != nil {
+			return nil, err
+		}
+
+		return resolver.wrapPlottedImageVulnerabilitiesWithContext(ctx, allCveIds, int(fixableCount))
+	}
+
 	query = tryUnsuppressedQuery(query)
 
 	vulnLoader, err := loaders.GetImageCVELoader(ctx)
