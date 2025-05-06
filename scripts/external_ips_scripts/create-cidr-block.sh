bdb6977ed39f3ae46c0a3109da163f78736c32af
JoukoVirtanen <jouko@stackrox.com>
2025-05-06 11:59:06 -0700

    Adds scripts for creating collector config, setting environment variables for external IPs, and deployments with external connections

---
+++ scripts/external_ips_scripts/create-cidr-block.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -eou pipefail
+
+ROX_ENDPOINT=${1:-localhost:8000}
+cidr_block=${2:-8.8.8.0/24}
+cidr_name=${3:-"MyCIDR"}
+
+clusters_json="$(curl --location --silent --request GET "https://${ROX_ENDPOINT}/v1/clusters" -k --header "Authorization: Bearer $ROX_API_TOKEN")"
+
+cluster_id="$(echo "$clusters_json" | jq -r '.clusters[0].id')"
+
+cidr_json='{"entity": {"cidr": "'"$cidr_block"'", "name": "'"$cidr_name"'", "id": ""}}'
+
+
+create_cidr_block_response_json="$(curl --location --silent --request POST --data "$cidr_json" "https://${ROX_ENDPOINT}/v1/networkgraph/cluster/$cluster_id/externalentities" -k --header "Authorization: Bearer $ROX_API_TOKEN")"
+
+echo "$create_cidr_block_response_json" | jq
---
+++ scripts/external_ips_scripts/setup-env-var.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+export ROX_EXTERNAL_IPS=true
+export ROX_NETWORK_GRAPH_EXTERNAL_IPS=true
