59c8ae2f3d13e964c6f8faf4d4c695bdb64849c8
JoukoVirtanen <jouko@stackrox.com>
2025-05-06 18:20:00 -0700

    Added a scenario that creates some deployments with traffic to external ips

--- scripts/external_ips_scripts/create-cidr-block.sh
+++ scripts/external_ips_scripts/create-cidr-block.sh
@@ -3,7 +3,7 @@ set -eou pipefail
 
 ROX_ENDPOINT=${1:-localhost:8000}
 cidr_block=${2:-8.8.8.0/24}
-cidr_name=${3:-"MyCIDR"}
+cidr_name=${3:-"testCIDR"}
 
 clusters_json="$(curl --location --silent --request GET "https://${ROX_ENDPOINT}/v1/clusters" -k --header "Authorization: Bearer $ROX_API_TOKEN")"
 
---
+++ scripts/external_ips_scripts/scenario.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -eou pipefail
+
+# Running these commands one by one is recommended instead of running this script.
+# Check the UI after each command.
+# This assumes that ACS has been deployed with ROX_EXTERNAL_IPS and ROX_NETWORK_GRAPH_EXTERNAL_IPS
+
+# Cleanup from previous runs
+kubectl delete ns qa
+kubectl -n stackrox delete configmap collector-config
+
+kubectl create ns qa
+
+./create-secret-for-qa.sh
+
+# Enable collection of external IPs by collector 
+./enable-collector-external-ips.sh
+
+#sleep 60
+
+# Create a deployment that reaches out to google in the qa namespace
+kubectl create -f external-destination-source.yml 
+
+# Create a deployment that reaches out to 8.8.8.8
+./create-deployment-with-ext-ip.sh 8.8.8.8 53 1 
+
+# Create a CIDR block that matches the above deployment
+./create-cidr-block.sh 8.8.8.0/24 testCIDR
+
+# Create a deployment that reaches out to 1.1.1.1
+./create-deployment-with-ext-ip.sh 1.1.1.1 53 2 
