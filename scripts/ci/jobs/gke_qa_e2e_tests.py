16081d288d6f128d0918887f3ea8dfe3c332e502
Robby Cochran <rcochran@redhat.com>
2025-05-09 11:48:52 -0700

    set nodeselectors

--- scripts/ci/jobs/gke_qa_e2e_tests.py
+++ scripts/ci/jobs/gke_qa_e2e_tests.py
@@ -14,4 +14,7 @@ os.environ["ROX_ACTIVE_VULN_MGMT"] = "true"
 os.environ["ROX_RISK_REPROCESSING_INTERVAL"] = "15s"
 os.environ["ROX_SENSOR_CONNECTION_RETRY_MAX_INTERVAL"] = "30s"
 
+os.environ["ROX_CENTRAL_EXTRA_HELM_VALUES_FILE"] = "central-arm64-values.yaml"
+os.environ["ROX_SENSOR_EXTRA_HELM_VALUES_FILE"] = "sensor-arm64-values.yaml"
+
 make_qa_e2e_test_runner(cluster=GKECluster("qa-e2e-test", machine_type="t2a-standard-4")).run()
