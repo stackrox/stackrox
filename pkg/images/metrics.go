72f032aac1d2862425380d258bef1be9dd9e5e9b
Jan Schnathmeier <jschnath@redhat.com>
2025-05-09 16:42:24 +0200

    Change metric name and fix bug where scan semaphore metrics were not decreased when acquiring a scan semaphore failed

--- pkg/images/metrics.go
+++ pkg/images/metrics.go
@@ -9,19 +9,19 @@ var (
 	ScanSemaphoreQueueSize = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: metrics.PrometheusNamespace,
 		Subsystem: metrics.CentralSubsystem.String(),
-		Name:      "image_scan_semaphore_queue_size",
+		Name:      "scan_semaphore_queue_size",
 		Help:      "A gauge vector that tracks the size of the queue for the scan semaphores used in scans.",
 	}, []string{"location"})
 	ScanSemaphoreHoldingSize = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: metrics.PrometheusNamespace,
 		Subsystem: metrics.CentralSubsystem.String(),
-		Name:      "image_scan_semaphore_holding_size",
+		Name:      "scan_semaphore_holding_size",
 		Help:      "A gauge vector that tracks the number of requests successfully holding the scan semaphores.",
 	}, []string{"location"})
 	ScanSemaphoreLimit = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: metrics.PrometheusNamespace,
 		Subsystem: metrics.CentralSubsystem.String(),
-		Name:      "image_scan_semaphore_limit",
+		Name:      "scan_semaphore_limit",
 		Help:      "A gauge vector that tracks the limit of available scan semaphores.",
 	}, []string{"location"})
 )
