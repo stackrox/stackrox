6a8dbebd2fb0bae52db4502b64558b3ddb10aa4a
Mark Pedrotti <pedrottimark@gmail.com>
2025-05-14 12:30:44 -0400

    ROX-29228: Add Advisory type and update AdvisoryLinkOrText component (#15284)

--- ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/Tables/AdvisoryLinkOrText.tsx
+++ ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/Tables/AdvisoryLinkOrText.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react';
 
 import ExternalLink from 'Components/PatternFly/IconText/ExternalLink';
+import { Advisory } from 'types/cve.proto';
 
 // https://access.redhat.com/security/updates/advisory
 // Red Hat publishes three types of errata:
@@ -11,34 +12,20 @@ import ExternalLink from 'Components/PatternFly/IconText/ExternalLink';
 // https://access.redhat.com/articles/explaining_redhat_errata
 // All advisories are given a year and a sequential number, which starts at 0001 and ends at the number of advisories shipped for that year.
 
-export function isRedHatAdvisory(advisory: string) {
-    return /^RH[SBE]A-\d\d\d\d:\d+$/.test(advisory);
-}
-
 export type AdvisoryLinkOrTextProps = {
-    advisory: string | undefined;
+    advisory: Advisory | null | undefined;
 };
 
 function AdvisoryLinkOrText({ advisory }: AdvisoryLinkOrTextProps): ReactNode {
-    if (typeof advisory === 'string') {
-        if (isRedHatAdvisory(advisory)) {
-            return (
-                <ExternalLink>
-                    <a
-                        href={`https://access.redhat.com/errata/${advisory}`}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        {advisory}
-                    </a>
-                </ExternalLink>
-            );
-        }
-
-        // Unexpected, because other advisories like GHSA are not separated from CVE.
-        if (advisory.length !== 0) {
-            return advisory;
-        }
+    if (advisory) {
+        const { advisoryId, advisoryLink } = advisory;
+        return (
+            <ExternalLink>
+                <a href={advisoryLink} target="_blank" rel="noopener noreferrer">
+                    {advisoryId}
+                </a>
+            </ExternalLink>
+        );
     }
 
     return '-';
