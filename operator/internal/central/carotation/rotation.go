8417b9b10e2af0b1079acd4026b5c138c3d87437
Vlad Bologa <vbologa@redhat.com>
2025-05-19 11:10:55 +0200

    Perform CA rotation after 10 minutes for testing

--- operator/internal/central/carotation/rotation.go
+++ operator/internal/central/carotation/rotation.go
@@ -29,19 +29,15 @@ const (
 // Important: The function assumes that input certificates have valid time ranges.
 func DetermineAction(primary, secondary *x509.Certificate, current time.Time) Action {
 	startTime := primary.NotBefore
-	validityDuration := primary.NotAfter.Sub(primary.NotBefore)
-	fifthOfValidityDuration := validityDuration / 5
 
-	// Add secondary CA after 3/5 of validity
-	addSecondaryCATime := startTime.Add(3 * fifthOfValidityDuration)
+	// HACK: add secondary CA after 5 minutes (considering the 5 minute backdating)
+	addSecondaryCATime := startTime.Add(10 * time.Minute)
 	if current.After(addSecondaryCATime) && secondary == nil {
 		return AddSecondary
 	}
 
-	// Promote secondary to primary after 4/5 of the primary's validity period has elapsed.
-	// If no secondary CA exists at this point, the rotation will be done in two steps:
-	// first reconcile - AddSecondary, subsequent reconcile - PromoteSecondary
-	promoteSecondaryCATime := startTime.Add(4 * fifthOfValidityDuration)
+	// HACK: promote secondary CA to primary after 10 minutes
+	promoteSecondaryCATime := startTime.Add(15 * time.Minute)
 	if current.After(promoteSecondaryCATime) {
 		return PromoteSecondary
 	}
