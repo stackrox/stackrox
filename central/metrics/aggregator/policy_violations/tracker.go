44ed809744e0dce0ece539754fcf3ce764cd30b3
Michaël Petrov <smartptr@gmail.com>
2025-05-20 22:51:32 +0200

    fix and refactor policy getters

--- central/metrics/aggregator/policy_violations/tracker.go
+++ central/metrics/aggregator/policy_violations/tracker.go
@@ -28,10 +28,10 @@ var labelOrder = common.MakeLabelOrderMap([]common.Label{
 
 func MakeTrackerConfig() *common.TrackerConfig {
 	return common.MakeTrackerConfig("violations", "aggregated policy violations",
-		labelOrder, common.Bind2nd(trackViolationsMetrics, alertDS.Singleton()))
+		labelOrder, common.Bind3rd(trackViolationsMetrics, alertDS.Singleton()))
 }
 
-func trackViolationsMetrics(ctx context.Context, ds alertDS.DataStore) iter.Seq[common.Finding] {
+func trackViolationsMetrics(ctx context.Context, _ common.MetricLabelsExpressions, ds alertDS.DataStore) iter.Seq[common.Finding] {
 	return func(yield func(common.Finding) bool) {
 		// Optimization opportunity:
 		// The resource filter is known at this point, so a more precise query could be constructed here.
@@ -44,39 +44,28 @@ func trackViolationsMetrics(ctx context.Context, ds alertDS.DataStore) iter.Seq[
 	}
 }
 
-func makeFinding(alert *storage.ListAlert) common.Finding {
-	alert.GetPolicy().GetCategories()
-	return func(label common.Label) string {
-		switch label {
-		case "Cluster":
-			return alert.GetCommonEntityInfo().GetClusterName()
-		case "Namespace":
-			return alert.GetCommonEntityInfo().GetNamespace()
-		case "Resource":
-			return alert.GetResource().GetName()
-		case "Deployment":
-			return alert.GetDeployment().GetName()
-		case "IsActiveDeployment":
-			if alert.GetDeployment().GetInactive() {
-				return "false"
-			}
-			return "true"
-		case "Policy":
-			return alert.GetPolicy().GetName()
-		case "Categories":
-			return strings.Join(alert.GetPolicy().GetCategories(), ",")
-		case "Severity":
-			return alert.GetPolicy().GetSeverity().String()
-		case "Action":
-			return alert.GetEnforcementAction().String()
-		case "Stage":
-			return alert.GetLifecycleStage().String()
-		case "EnforcementCount":
-			return strconv.Itoa(int(alert.GetEnforcementCount()))
-		case "State":
-			return alert.GetState().String()
-		default:
-			return ""
+var getters = map[common.Label]func(alert *storage.ListAlert) string{
+	"Cluster":    func(alert *storage.ListAlert) string { return alert.GetCommonEntityInfo().GetClusterName() },
+	"Namespace":  func(alert *storage.ListAlert) string { return alert.GetCommonEntityInfo().GetNamespace() },
+	"Resource":   func(alert *storage.ListAlert) string { return alert.GetResource().GetName() },
+	"Deployment": func(alert *storage.ListAlert) string { return alert.GetDeployment().GetName() },
+	"IsActiveDeployment": func(alert *storage.ListAlert) string {
+		if alert.GetDeployment().GetInactive() {
+			return "false"
 		}
+		return "true"
+	},
+	"Policy":           func(alert *storage.ListAlert) string { return alert.GetPolicy().GetName() },
+	"Categories":       func(alert *storage.ListAlert) string { return strings.Join(alert.GetPolicy().GetCategories(), ",") },
+	"Severity":         func(alert *storage.ListAlert) string { return alert.GetPolicy().GetSeverity().String() },
+	"Action":           func(alert *storage.ListAlert) string { return alert.GetEnforcementAction().String() },
+	"Stage":            func(alert *storage.ListAlert) string { return alert.GetLifecycleStage().String() },
+	"EnforcementCount": func(alert *storage.ListAlert) string { return strconv.Itoa(int(alert.GetEnforcementCount())) },
+	"State":            func(alert *storage.ListAlert) string { return alert.GetState().String() },
+}
+
+func makeFinding(alert *storage.ListAlert) common.Finding {
+	return func(label common.Label) string {
+		return getters[label](alert)
 	}
 }
