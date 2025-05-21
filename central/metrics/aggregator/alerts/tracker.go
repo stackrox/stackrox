f3f83c6990ba8f79ea7f133900858f7a73c0401e
Michaël Petrov <smartptr@gmail.com>
2025-05-21 11:53:27 +0200

    label getter

--- central/metrics/aggregator/alerts/tracker.go
+++ central/metrics/aggregator/alerts/tracker.go
@@ -12,55 +12,42 @@ import (
 	"github.com/stackrox/rox/generated/storage"
 )
 
-var labelOrder = common.MakeLabelOrderMap([]common.Label{
-	"Cluster",
-	"Namespace",
-	"Deployment",
-	"IsActiveDeployment",
-	"Resource",
-	"Policy",
-	"Categories",
-	"Severity",
-	"Action",
-	"Stage",
-	"EnforcementCount",
-	"State",
-})
-
-var getters = map[common.Label]func(alert *storage.ListAlert) string{
-	"Cluster":    func(alert *storage.ListAlert) string { return alert.GetCommonEntityInfo().GetClusterName() },
-	"Namespace":  func(alert *storage.ListAlert) string { return alert.GetCommonEntityInfo().GetNamespace() },
-	"Resource":   func(alert *storage.ListAlert) string { return alert.GetResource().GetName() },
-	"Deployment": func(alert *storage.ListAlert) string { return alert.GetDeployment().GetName() },
-	"IsActiveDeployment": func(alert *storage.ListAlert) string {
-		if alert.GetDeployment().GetInactive() {
-			return "false"
-		}
-		return "true"
-	},
-	"Policy":           func(alert *storage.ListAlert) string { return alert.GetPolicy().GetName() },
-	"Categories":       func(alert *storage.ListAlert) string { return strings.Join(alert.GetPolicy().GetCategories(), ",") },
-	"Severity":         func(alert *storage.ListAlert) string { return alert.GetPolicy().GetSeverity().String() },
-	"Action":           func(alert *storage.ListAlert) string { return alert.GetEnforcementAction().String() },
-	"Stage":            func(alert *storage.ListAlert) string { return alert.GetLifecycleStage().String() },
-	"EnforcementCount": func(alert *storage.ListAlert) string { return strconv.Itoa(int(alert.GetEnforcementCount())) },
-	"State":            func(alert *storage.ListAlert) string { return alert.GetState().String() },
+var getters = []common.LabelGetter[*storage.ListAlert]{
+	{Label: "Cluster", Getter: func(alert *storage.ListAlert) string { return alert.GetCommonEntityInfo().GetClusterName() }},
+	{Label: "Namespace", Getter: func(alert *storage.ListAlert) string { return alert.GetCommonEntityInfo().GetNamespace() }},
+	{Label: "Resource", Getter: func(alert *storage.ListAlert) string { return alert.GetResource().GetName() }},
+	{Label: "Deployment", Getter: func(alert *storage.ListAlert) string { return alert.GetDeployment().GetName() }},
+	{Label: "IsDeploymentActive", Getter: isDeploymentActive},
+	{Label: "Policy", Getter: func(alert *storage.ListAlert) string { return alert.GetPolicy().GetName() }},
+	{Label: "Categories", Getter: func(alert *storage.ListAlert) string { return strings.Join(alert.GetPolicy().GetCategories(), ",") }},
+	{Label: "Severity", Getter: func(alert *storage.ListAlert) string { return alert.GetPolicy().GetSeverity().String() }},
+	{Label: "Action", Getter: func(alert *storage.ListAlert) string { return alert.GetEnforcementAction().String() }},
+	{Label: "Stage", Getter: func(alert *storage.ListAlert) string { return alert.GetLifecycleStage().String() }},
+	{Label: "EnforcementCount", Getter: func(alert *storage.ListAlert) string { return strconv.Itoa(int(alert.GetEnforcementCount())) }},
+	{Label: "State", Getter: func(alert *storage.ListAlert) string { return alert.GetState().String() }},
 }
 
 func MakeTrackerConfig(gauge func(string, prometheus.Labels, int)) *common.TrackerConfig[*storage.ListAlert] {
 	return common.MakeTrackerConfig(
 		"alerts",
 		"aggregated policy violation alerts",
-		labelOrder,
 		getters,
 		common.Bind3rd(trackAlertsMetrics, alertDS.Singleton()),
 		gauge)
 }
 
+func isDeploymentActive(alert *storage.ListAlert) string {
+	if alert.GetDeployment().GetInactive() {
+		return "false"
+	}
+	return "true"
+}
+
 func trackAlertsMetrics(ctx context.Context, _ common.MetricLabelsExpressions, ds alertDS.DataStore) iter.Seq[*storage.ListAlert] {
 	return func(yield func(*storage.ListAlert) bool) {
 		// Optimization opportunity:
-		// The resource filter is known at this point, so a more precise query could be constructed here.
+		// The resource filter is known at this point, so a more precise query
+		// could be constructed here.
 		_ = ds.WalkAll(ctx, func(alert *storage.ListAlert) error {
 			if !yield(alert) {
 				return common.ErrStopIterator
