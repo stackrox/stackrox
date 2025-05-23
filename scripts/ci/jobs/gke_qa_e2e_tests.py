75546eb08ec9d2d8559aa6a1119648d09a6da733
Robby Cochran <rcochran@redhat.com>
2025-05-22 22:36:11 -0700

    webhookserver

--- scripts/ci/jobs/gke_qa_e2e_tests.py
+++ scripts/ci/jobs/gke_qa_e2e_tests.py
@@ -16,15 +16,8 @@ os.environ["ROX_RISK_REPROCESSING_INTERVAL"] = "15s"
 os.environ["ROX_SENSOR_CONNECTION_RETRY_MAX_INTERVAL"] = "30s"
 
 # tbd: restore this file and create gke_qa_e2e_arm64_tests.py with below config
-# deploy via helm to set node selectors for running on GKE arm64 nodes
 os.environ["REMOTE_CLUSTER_ARCH"] = "arm64"
 os.environ["ARM64_NODESELECTORS"] = "true"
-# os.environ["OUTPUT_FORMAT"] = "helm"
-# os.environ["SENSOR_HELM_DEPLOY"] = "true"
-# os.environ["ROX_DEPLOY_SENSOR_WITH_CRS"] = "false"
-# os.environ["ROX_CENTRAL_EXTRA_HELM_VALUES_FILE"] = "central-arm64-values.yaml"
-# os.environ["ROX_SENSOR_EXTRA_HELM_VALUES_FILE"] = "sensor-arm64-values.yaml"
-
 os.environ["IMAGE_PREFETCH_DISABLED"] = "true"
 
 make_qa_e2e_test_runner(cluster=GKECluster("qa-e2e-test", machine_type="t2a-standard-8")).run()
