505110a9e6987abbe0f1b20128b7c4417af9c2d9
Robby Cochran <rcochran@redhat.com>
2025-05-22 21:42:19 -0700

    default

--- scripts/ci/jobs/gke_qa_e2e_tests.py
+++ scripts/ci/jobs/gke_qa_e2e_tests.py
@@ -18,11 +18,12 @@ os.environ["ROX_SENSOR_CONNECTION_RETRY_MAX_INTERVAL"] = "30s"
 # tbd: restore this file and create gke_qa_e2e_arm64_tests.py with below config
 # deploy via helm to set node selectors for running on GKE arm64 nodes
 os.environ["REMOTE_CLUSTER_ARCH"] = "arm64"
-os.environ["OUTPUT_FORMAT"] = "helm"
-os.environ["SENSOR_HELM_DEPLOY"] = "true"
-os.environ["ROX_DEPLOY_SENSOR_WITH_CRS"] = "false"
-os.environ["ROX_CENTRAL_EXTRA_HELM_VALUES_FILE"] = "central-arm64-values.yaml"
-os.environ["ROX_SENSOR_EXTRA_HELM_VALUES_FILE"] = "sensor-arm64-values.yaml"
+os.environ["ARM64_NODESELECTORS"] = "true"
+# os.environ["OUTPUT_FORMAT"] = "helm"
+# os.environ["SENSOR_HELM_DEPLOY"] = "true"
+# os.environ["ROX_DEPLOY_SENSOR_WITH_CRS"] = "false"
+# os.environ["ROX_CENTRAL_EXTRA_HELM_VALUES_FILE"] = "central-arm64-values.yaml"
+# os.environ["ROX_SENSOR_EXTRA_HELM_VALUES_FILE"] = "sensor-arm64-values.yaml"
 
 os.environ["IMAGE_PREFETCH_DISABLED"] = "true"
 
