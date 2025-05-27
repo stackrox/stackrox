a1c98b78be5376ca914d0b07d1d539f5a5e7152a
Moritz Clasmeier <111092021+mclasmeier@users.noreply.github.com>
2025-05-27 15:30:58 +0200

    ROX-29199: New Operator Defaulting for Central (#15315)
    Co-authored-by: Moritz Clasmeier <mclasmeier@redhat.com>
    Co-authored-by: Marcin Owsiany <porridge@redhat.com>

---
+++ operator/api/v1alpha1/central_defaults.go
@@ -0,0 +1,70 @@
+/*
+Copyright 2025.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"dario.cat/mergo"
+	"github.com/pkg/errors"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	runtime "k8s.io/apimachinery/pkg/runtime"
+)
+
+func centralDefaultsToUnstructured(central *Central) (map[string]interface{}, error) {
+	defaults, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&central.Defaults)
+	if err != nil {
+		return nil, err
+	}
+	return defaults, nil
+}
+
+// AddCentralDefaultsToUnstructured adds the defaults from Central.Defaults to the unstructured object.
+func AddCentralDefaultsToUnstructured(u *unstructured.Unstructured, central *Central) error {
+	defaults, err := centralDefaultsToUnstructured(central)
+	if err != nil {
+		return err
+	}
+	u.Object["defaults"] = defaults
+	return nil
+}
+
+// AddUnstructuredDefaultsToCentral adds the defaults from the unstructured object to Central.
+func AddUnstructuredDefaultsToCentral(central *Central, u *unstructured.Unstructured) error {
+	defaultsInterface, found := u.Object["defaults"]
+	if !found {
+		return nil
+	}
+	unstructuredDefaults, ok := defaultsInterface.(map[string]interface{})
+	if !ok {
+		return errors.New("unstructured Central defaults of unexpected type")
+	}
+	typedDefaults := CentralSpec{}
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(unstructuredDefaults, &typedDefaults); err != nil {
+		return errors.Wrap(err, "converting defaults from unstructured object into CentralSpec")
+	}
+	central.Defaults = typedDefaults
+
+	return nil
+}
+
+// MergeCentralDefaultsIntoSpec merges the defaults from Central.Defaults into Central.Spec.
+// Modifies content of central.
+func MergeCentralDefaultsIntoSpec(central *Central) error {
+	if err := mergo.Merge(&central.Spec, central.Defaults); err != nil {
+		return errors.Wrap(err, "merging Central Defaults into Spec")
+	}
+	return nil
+}
---
+++ operator/internal/central/extensions/reconcile_defaulting.go
@@ -0,0 +1,125 @@
+package extensions
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/go-logr/logr"
+	"github.com/operator-framework/helm-operator-plugins/pkg/extensions"
+	"github.com/pkg/errors"
+	platform "github.com/stackrox/rox/operator/api/v1alpha1"
+	"github.com/stackrox/rox/operator/internal/common/defaulting"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrlClient "sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var defaultingFlows = []defaulting.CentralDefaultingFlow{
+	defaulting.CentralScannerV4DefaultingFlow,
+}
+
+// This extension executes "defaulting flows". A Central defaulting flow is of type
+// defaulting.CentralDefaultingFlow, which is essentially a function that acts on
+// `status`, `metadata.annotations` as well as `spec` and `defaults` (both of type `CentralSpec`)
+// of a Central CR.
+//
+// A defaulting flow shall
+//   - derive default values based on 'status', 'annotations' and 'spec' and store them in 'defaults'.
+//   - add a new annotation in order to persist current defaulting choices.
+func FeatureDefaultingExtension(client ctrlClient.Client) extensions.ReconcileExtension {
+	return func(ctx context.Context, u *unstructured.Unstructured, _ func(extensions.UpdateStatusFunc), l logr.Logger) error {
+		return reconcileFeatureDefaults(ctx, client, u, l)
+	}
+}
+
+func reconcileFeatureDefaults(ctx context.Context, client ctrlClient.Client, u *unstructured.Unstructured, logger logr.Logger) error {
+	logger = logger.WithName("extension-feature-defaults")
+	if u.GroupVersionKind() != platform.CentralGVK {
+		logger.Error(errUnexpectedGVK, "unable to reconcile central", "expectedGVK", platform.CentralGVK, "actualGVK", u.GroupVersionKind())
+		return errUnexpectedGVK
+	}
+
+	if u.GetDeletionTimestamp() != nil {
+		logger.Info("skipping extension run due to deletionTimestamp being present on Central custom resource")
+		return nil
+	}
+
+	central := platform.Central{}
+	if err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, &central); err != nil {
+		return errors.Wrap(err, "converting unstructured object to Central")
+	}
+
+	err := setDefaultsAndPersist(ctx, logger, &central, client)
+	if err != nil {
+		return err
+	}
+
+	u.Object, err = runtime.DefaultUnstructuredConverter.ToUnstructured(&central)
+	if err != nil {
+		return errors.Wrap(err, "converting Central to unstructured object after extension execution")
+	}
+
+	if err := platform.AddCentralDefaultsToUnstructured(u, &central); err != nil {
+		return errors.Wrap(err, "enriching unstructured Central object with defaults")
+	}
+
+	return nil
+}
+
+func setDefaultsAndPersist(ctx context.Context, logger logr.Logger, central *platform.Central, client ctrlClient.Client) error {
+	origCentral := central.DeepCopy()
+
+	// This may update central.Defaults and central's embedded annotations.
+	for _, flow := range defaultingFlows {
+		if err := executeSingleDefaultingFlow(logger, central, client, flow); err != nil {
+			return err
+		}
+	}
+
+	// We persist the annotations immediately during (first-time) execution of this extension to make sure
+	// that this information is already persisted in the Kubernetes resource before we
+	// can realistically end up in a situation where reconcilliation might need to be retried.
+	newResourceVersion, err := patchCentralAnnotations(ctx, logger, client, origCentral, central.GetAnnotations())
+	if err != nil {
+		return errors.Wrap(err, "patching Central annotations")
+	}
+	central.SetResourceVersion(newResourceVersion)
+	return nil
+}
+
+func executeSingleDefaultingFlow(logger logr.Logger, central *platform.Central, client ctrlClient.Client, flow defaulting.CentralDefaultingFlow) error {
+	logger = logger.WithName(fmt.Sprintf("defaulting-flow-%s", flow.Name))
+	annotations := central.GetAnnotations()
+	if annotations == nil {
+		annotations = make(map[string]string)
+	}
+
+	// to be thrown away after defaulting flow execution.
+	spec := central.Spec.DeepCopy()
+	status := central.Status.DeepCopy()
+
+	err := flow.DefaultingFunc(logger, status, annotations, spec, &central.Defaults)
+	if err != nil {
+		return errors.Wrapf(err, "Central defaulting flow %s failed", flow.Name)
+	}
+	central.SetAnnotations(annotations)
+
+	return nil
+}
+
+func patchCentralAnnotations(ctx context.Context, logger logr.Logger, client ctrlClient.Client, central *platform.Central, annotations map[string]string) (string, error) {
+	// MergeFromWithOptimisticLock causes the resourceVersion to be checked prior to patching.
+	patch := ctrlClient.MergeFromWithOptions(central, ctrlClient.MergeFromWithOptimisticLock{})
+	newCentral := central.DeepCopy()
+	newCentral.SetAnnotations(annotations)
+	if err := client.Patch(ctx, newCentral, patch); err != nil {
+		return "", err
+	}
+
+	newResourceVersion := newCentral.GetResourceVersion()
+	logger.Info("patched Central object",
+		"oldResourceVersion", central.GetResourceVersion(),
+		"newResourceVersion", newResourceVersion)
+
+	return newResourceVersion, nil
+}
