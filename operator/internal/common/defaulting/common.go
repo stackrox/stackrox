108ab5ca0185b7eb5bc65c962cb58eb45881cc3e
Moritz Clasmeier <mclasmeier@redhat.com>
2025-05-27 09:27:37 +0200

    PR Review: Comment for CentralDefaultingFlow

--- operator/internal/common/defaulting/common.go
+++ operator/internal/common/defaulting/common.go
@@ -12,6 +12,8 @@ func initializedDeepCopy(spec *platform.ScannerV4Spec) *platform.ScannerV4Spec {
 	return spec.DeepCopy()
 }
 
+// CentralDefaultingFlow defines a defaulting flow for the Central CR.
+// Any mutation to either `spec` or `status` is not preserved.
 type CentralDefaultingFlow struct {
 	Name           string
 	DefaultingFunc func(logger logr.Logger, status *platform.CentralStatus, annotations map[string]string, spec *platform.CentralSpec, defaults *platform.CentralSpec) error
