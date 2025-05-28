d35334a2382d4d0a3f16463dd716e1f98ad3815d
Dave Shrewsberry <dashrews@redhat.com>
2025-05-28 13:12:28 -0400

    debug priority as it makes no sense at all

--- central/graphql/resolvers/image_components.go
+++ central/graphql/resolvers/image_components.go
@@ -182,6 +182,7 @@ func (resolver *Resolver) ImageComponents(ctx context.Context, q PaginatedQuery)
 		// Stash a single instance of a Component to aid in normalizing
 		foundComponent := make(map[normalizedImageComponent]*storage.ImageComponentV2)
 		for _, comp := range comps {
+			log.Infof("SHREWS -- priority %d", comp.GetPriority())
 			normalized := normalizedImageComponent{
 				name:    comp.GetName(),
 				version: comp.GetVersion(),
--- central/views/imagecomponentflat/view_impl.go
+++ central/views/imagecomponentflat/view_impl.go
@@ -93,7 +93,7 @@ func withSelectComponentCoreResponseQuery(q *v1.Query) *v1.Query {
 		search.NewQuerySelect(search.ComponentVersion).Proto(),
 		search.NewQuerySelect(search.OperatingSystem).Proto(),
 		search.NewQuerySelect(search.ComponentTopCVSS).AggrFunc(aggregatefunc.Max).Proto(),
-		search.NewQuerySelect(search.ComponentPriority).AggrFunc(aggregatefunc.Max).Proto(),
+		//search.NewQuerySelect(search.ComponentPriority).AggrFunc(aggregatefunc.Max).Proto(),
 		search.NewQuerySelect(search.ComponentRiskScore).AggrFunc(aggregatefunc.Max).Proto(),
 	}
 
