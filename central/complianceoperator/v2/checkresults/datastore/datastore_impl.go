6b70598b512c1932c976c6247a6d66408a38ed40
lvalerom <lvalerom@redhat.com>
2025-05-28 19:30:22 +0200

    use NilOrTime in DeleteOldResults instead of AsTime

--- central/complianceoperator/v2/checkresults/datastore/datastore_impl.go
+++ central/complianceoperator/v2/checkresults/datastore/datastore_impl.go
@@ -14,6 +14,7 @@ import (
 	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/pkg/postgres"
 	"github.com/stackrox/rox/pkg/postgres/schema"
+	"github.com/stackrox/rox/pkg/protocompat"
 	"github.com/stackrox/rox/pkg/sac"
 	"github.com/stackrox/rox/pkg/sac/resources"
 	"github.com/stackrox/rox/pkg/search"
@@ -459,7 +460,7 @@ func (d *datastoreImpl) DeleteOldResults(ctx context.Context, lastStartedTimesta
 		lastStartedTimestampColumnName,
 		lastStartedTimestampColumnName,
 	)
-	_, err := d.db.Exec(ctx, deleteStmt, scanRefIDs, lastStartedTimestamp.AsTime())
+	_, err := d.db.Exec(ctx, deleteStmt, scanRefIDs, protocompat.NilOrTime(lastStartedTimestamp))
 	if err != nil {
 		return err
 	}
