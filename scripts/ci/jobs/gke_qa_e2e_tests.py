311f27115e084e043e15686c6ae8730f236d0076
Robby Cochran <rcochran@redhat.com>
2025-05-28 12:52:14 -0700

    restore

--- scripts/ci/jobs/gke_qa_e2e_tests.py
+++ scripts/ci/jobs/gke_qa_e2e_tests.py
@@ -15,9 +15,4 @@ os.environ["ROX_ACTIVE_VULN_MGMT"] = "true"
 os.environ["ROX_RISK_REPROCESSING_INTERVAL"] = "15s"
 os.environ["ROX_SENSOR_CONNECTION_RETRY_MAX_INTERVAL"] = "30s"
 
-# Before merging, restore this file after openshift job added
-os.environ["REMOTE_CLUSTER_ARCH"] = "arm64"
-os.environ["ARM64_NODESELECTORS"] = "true"
-os.environ["IMAGE_PREFETCH_DISABLED"] = "true"
-
-make_qa_e2e_test_runner(cluster=GKECluster("qa-e2e-test", machine_type="t2a-standard-8")).run()
+make_qa_e2e_test_runner(cluster=GKECluster("qa-e2e-test")).run()
