d76b807962a0f714b21f517dc5152bdc92a7cd38
Moritz Clasmeier <mclasmeier@redhat.com>
2025-06-04 17:03:46 +0200

    Add exponential backoff to tlsutils.DialContext
    And use it for the "roxctl central certs" command.

--- pkg/tlsutils/dial_context.go
+++ pkg/tlsutils/dial_context.go
@@ -3,7 +3,11 @@ package tlsutils
 import (
 	"context"
 	"crypto/tls"
+	"net"
+	"time"
 
+	"github.com/cenkalti/backoff/v4"
+	"github.com/hashicorp/go-multierror"
 	"github.com/pkg/errors"
 	"github.com/stackrox/rox/pkg/errox"
 	"github.com/stackrox/rox/pkg/logging"
@@ -12,6 +16,37 @@ import (
 var (
 	log = logging.LoggerForModule()
 )
+
+func DialContextWithRetries(ctx context.Context, network, addr string, tlsConfig *tls.Config) (*tls.Conn, error) {
+	expBackoff := backoff.NewExponentialBackOff(
+		backoff.WithInitialInterval(2*time.Second),
+		backoff.WithMultiplier(2),
+		backoff.WithMaxElapsedTime(0), // We will simply use the deadline of the provided context instead.
+	)
+	expBackoffWithCtx := backoff.WithContext(expBackoff, ctx)
+	var dialConn net.Conn
+	var dialErr error
+
+	err := backoff.RetryNotify(func() error {
+		dialConn, dialErr = DialContext(ctx, network, addr, tlsConfig)
+		if dialErr != nil {
+			return dialErr
+		}
+		return nil
+	}, expBackoffWithCtx, func(err error, d time.Duration) {
+		log.Warnf("tls dial failed: %v, retrying after %s", err, d.Round(time.Second))
+	})
+	if err != nil {
+		var multiErr *multierror.Error
+		if dialErr != nil {
+			multiErr = multierror.Append(multiErr, dialErr)
+		}
+		multiErr = multierror.Append(multiErr, err)
+		return nil, multiErr
+	}
+
+	return dialConn.(*tls.Conn), nil
+}
 
 // DialContext attempts to establishes a TLS-enabled connection in a context-aware manner.
 func DialContext(ctx context.Context, network, addr string, tlsConfig *tls.Config) (*tls.Conn, error) {
