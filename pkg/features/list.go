635766bda0eba88303d5c38e9389de12eeb1eb4c
Mark Pedrotti <mark@stackrox.com>
2025-06-09 15:38:30 -0400

    ROX-29654: Delete ROX_CVE_ADVISORY_SEPARATION feature flag

--- pkg/features/list.go
+++ pkg/features/list.go
@@ -101,9 +101,6 @@ var (
 	// NetworkGraphExternalIPs enables displaying external IPs in the network graph
 	NetworkGraphExternalIPs = registerFeature("Display external ips in the UI", "ROX_NETWORK_GRAPH_EXTERNAL_IPS", enabled)
 
-	// Display RHSA/RHBA/RHEA advisory separately from associated CVE.
-	CVEAdvisorySeparation = registerFeature("Display RHSA/RHBA/RHEA advisory separately from associated CVE", "ROX_CVE_ADVISORY_SEPARATION", enabled)
-
 	// Display Exploit Prediction Scoring System (EPSS) score.
 	EPSSScore = registerFeature("Display Exploit Prediction Scoring System (EPSS) score", "ROX_EPSS_SCORE", enabled)
 
