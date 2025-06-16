413260a53b31b7a393e109eeb0bb9717d94c481d
David Vail <dcvail17@gmail.com>
2025-06-16 11:47:09 -0400

    Inline ScanConfigWizard local subcomponents

--- ui/apps/platform/src/Containers/ComplianceEnhanced/Schedules/Wizard/ScanConfigWizardFooter.tsx
+++ ui/apps/platform/src/Containers/ComplianceEnhanced/Schedules/Wizard/ScanConfigWizardFooter.tsx
@@ -22,74 +22,64 @@ function ScanConfigWizardFooter({
     const firstStepId = wizardSteps[0].id;
     const lastStepId = wizardSteps[wizardSteps.length - 1].id;
 
-    function renderButtons(activeStepId, onNext, onBack) {
-        return (
-            <>
-                {activeStepId !== lastStepId ? (
-                    <Button
-                        variant="primary"
-                        type="submit"
-                        isDisabled={disableClusterNext && activeStepId === wizardSteps[1].id}
-                        onClick={() => proceedToNextStepIfValid(onNext, activeStepId)}
-                    >
-                        Next
-                    </Button>
-                ) : (
-                    <Button
-                        variant="primary"
-                        type="submit"
-                        isDisabled={isSaving}
-                        onClick={onSave}
-                        isLoading={isSaving}
-                    >
-                        Save
-                    </Button>
-                )}
-                <Button
-                    variant="secondary"
-                    onClick={onBack}
-                    isDisabled={activeStepId === firstStepId}
-                >
-                    Back
-                </Button>
-                <Button variant="link" onClick={openModal}>
-                    Cancel
-                </Button>
-            </>
-        );
-    }
-
-    function renderModal(leaveWizard: () => void) {
-        return (
-            <Modal
-                variant="small"
-                title="Confirm cancel"
-                isOpen={isModalOpen}
-                onClose={closeModal}
-                actions={[
-                    <Button key="confirm" variant="primary" onClick={leaveWizard}>
-                        Confirm
-                    </Button>,
-                    <Button key="cancel" variant="secondary" onClick={closeModal}>
-                        Cancel
-                    </Button>,
-                ]}
-            >
-                <p>
-                    Are you sure you want to cancel? Any unsaved changes will be lost. You will be
-                    taken back to the list of scan configurations.
-                </p>
-            </Modal>
-        );
-    }
-
     return (
         <WizardFooter>
             <WizardContextConsumer>
                 {({ activeStep, onNext, onBack, onClose }) => (
                     <>
-                        {renderButtons(activeStep.id, onNext, onBack)}
-                        {renderModal(onClose)}
+                        {activeStep.id !== lastStepId ? (
+                            <Button
+                                variant="primary"
+                                type="submit"
+                                isDisabled={
+                                    disableClusterNext && activeStep.id === wizardSteps[1].id
+                                }
+                                onClick={() =>
+                                    proceedToNextStepIfValid(onNext, String(activeStep.id))
+                                }
+                            >
+                                Next
+                            </Button>
+                        ) : (
+                            <Button
+                                variant="primary"
+                                type="submit"
+                                isDisabled={isSaving}
+                                onClick={onSave}
+                                isLoading={isSaving}
+                            >
+                                Save
+                            </Button>
+                        )}
+                        <Button
+                            variant="secondary"
+                            onClick={onBack}
+                            isDisabled={activeStep.id === firstStepId}
+                        >
+                            Back
+                        </Button>
+                        <Button variant="link" onClick={openModal}>
+                            Cancel
+                        </Button>
+                        <Modal
+                            variant="small"
+                            title="Confirm cancel"
+                            isOpen={isModalOpen}
+                            onClose={closeModal}
+                            actions={[
+                                <Button key="confirm" variant="primary" onClick={onClose}>
+                                    Confirm
+                                </Button>,
+                                <Button key="cancel" variant="secondary" onClick={closeModal}>
+                                    Cancel
+                                </Button>,
+                            ]}
+                        >
+                            <p>
+                                Are you sure you want to cancel? Any unsaved changes will be lost.
+                                You will be taken back to the list of scan configurations.
+                            </p>
+                        </Modal>
                     </>
                 )}
             </WizardContextConsumer>
