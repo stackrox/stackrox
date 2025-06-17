cb278792bc68f40d2538d652908c277d8911b546
Guzman <guz@redhat.com>
2025-06-17 16:17:36 +0200

    ROX-29160: Add default signature integration with Red Hat Release Key 3
    This will be used by an upcoming default policy that checks that Red Hat
    images are signed.

---
+++ central/signatureintegration/datastore/singleton_test.go
@@ -0,0 +1,27 @@
+package datastore
+
+import (
+	"testing"
+
+	"github.com/stackrox/rox/central/signatureintegration/store"
+	mockSIStore "github.com/stackrox/rox/central/signatureintegration/store/mocks"
+	"go.uber.org/mock/gomock"
+)
+
+func TestCreateDefaultRedHatSignatureIntegration(t *testing.T) {
+	t.Run("should not add default integration when it already exists", func(t *testing.T) {
+		s := mockSIStore.NewMockSignatureIntegrationStore(gomock.NewController(t))
+		s.EXPECT().Get(gomock.Any(), gomock.Any()).Return(
+			store.DefaultRedHatSignatureIntegration, true, nil)
+
+		createDefaultRedHatSignatureIntegration(s)
+	})
+
+	t.Run("should add default integration when it doesn't exist", func(t *testing.T) {
+		s := mockSIStore.NewMockSignatureIntegrationStore(gomock.NewController(t))
+		s.EXPECT().Get(gomock.Any(), gomock.Any()).Return(nil, false, nil)
+		s.EXPECT().Upsert(gomock.Any(), store.DefaultRedHatSignatureIntegration).Return(nil)
+
+		createDefaultRedHatSignatureIntegration(s)
+	})
+}
