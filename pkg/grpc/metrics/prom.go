3287c67b1aa32a784318133e91f78b6eb2d7b76e
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2025-06-23 09:15:26 +0200

    Drop subsystem to avoid metric duplication
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- pkg/grpc/metrics/prom.go
+++ pkg/grpc/metrics/prom.go
@@ -6,30 +6,22 @@ import (
 )
 
 var (
-	alpnEndpointMetrics = make(map[string]*prometheus.CounterVec)
+	alpnEndpointMetric = prometheus.NewCounterVec(
+		// Leaving subsystem empty to skip duplicating this metric for each component
+		prometheus.CounterOpts{
+			Namespace: pkgMetrics.PrometheusNamespace,
+			Subsystem: "", // empty, so fqName = rox_endpoint_tls_handshakes_with_negotiated_alp_total
+			Name:      "endpoint_tls_handshakes_with_negotiated_alp_total",
+			Help:      "Number of finished TLS handshakes ...",
+		},
+		[]string{"subsystem", "endpoint", "remoteAddr", "alp"},
+	)
 )
 
 func init() {
-	// Only Central and Sensor expose an endpoint that demultiplexes connections based on ALPN.
-	for _, subsystem := range []pkgMetrics.Subsystem{pkgMetrics.CentralSubsystem, pkgMetrics.SensorSubsystem} {
-		metric := newALPMetricForSub(subsystem.String())
-		alpnEndpointMetrics[subsystem.String()] = metric
-		prometheus.MustRegister(metric)
-	}
-}
-
-func newALPMetricForSub(sub string) *prometheus.CounterVec {
-	return prometheus.NewCounterVec(prometheus.CounterOpts{
-		Namespace: pkgMetrics.PrometheusNamespace,
-		Subsystem: sub,
-		Name:      "endpoint_tls_handshakes_with_negotiated_alp_total",
-		Help: "Number of finished TLS handshakes to a given endpoint with " +
-			"given Application Level Protocol being negotiated as a result of ALPN.",
-	}, []string{"endpoint", "remoteAddr", "alp"})
+	prometheus.MustRegister(alpnEndpointMetric)
 }
 
 func ObserveALPN(sub, endpointAddr, remoteAddr, alp string) {
-	if metric, found := alpnEndpointMetrics[sub]; found {
-		metric.WithLabelValues(endpointAddr, remoteAddr, alp).Inc()
-	}
+	alpnEndpointMetric.WithLabelValues(sub, endpointAddr, remoteAddr, alp).Inc()
 }
