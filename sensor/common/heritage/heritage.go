f1e131b6f568cce22aaee9165a71e4bae85be6fa
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2025-06-24 12:53:59 +0200

    Revert "Use the standard sensor labels for the heritage configMap"
    This reverts commit 6c830af7e4c6e70c761f8d71ae86f26a21bb575e.

--- sensor/common/heritage/heritage.go
+++ sensor/common/heritage/heritage.go
@@ -4,7 +4,6 @@ import (
 	"cmp"
 	"context"
 	"fmt"
-	"maps"
 	"slices"
 	"strings"
 	"sync/atomic"
@@ -15,8 +14,6 @@ import (
 	"github.com/stackrox/rox/pkg/logging"
 	"github.com/stackrox/rox/pkg/sync"
 	"github.com/stackrox/rox/pkg/version"
-	"github.com/stackrox/rox/sensor/upgrader/common"
-	"github.com/stackrox/rox/sensor/utils"
 	v1 "k8s.io/api/core/v1"
 	apiErrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -87,9 +84,6 @@ type Manager struct {
 	// Cache the data for the current instance of Sensor
 	currentSensor SensorMetadata
 
-	// Set of orchestrator labels that should be set when creating the configMap
-	cmLabels map[string]string
-
 	maxSize int
 	minSize int
 	maxAge  time.Duration
@@ -107,19 +101,11 @@ func NewHeritageManager(ns string, client k8sClient, start time.Time) *Manager {
 		log.Warnf("Value of %s is lower than minimum=%d. Applying %d",
 			env.PastSensorsMaxEntries.EnvVar(), heritageMinSize, heritageMinSize)
 	}
-
-	cmLabels := utils.GetSensorKubernetesLabels()
-	maps.Insert(cmLabels, maps.All(map[string]string{
-		common.UpgradeResourceLabelKey: common.UpgradeResourceLabelValue, // delete-sensor script relies on this label.
-		"app.kubernetes.io/component":  common.UpgradeResourceLabelValue,
-	}))
-
 	return &Manager{
 		cacheIsPopulated: atomic.Bool{},
 		k8sClient:        client,
 		cache:            []*SensorMetadata{},
 		namespace:        ns,
-		cmLabels:         cmLabels,
 		currentSensor: SensorMetadata{
 			SensorVersion: version.GetMainVersion(),
 			SensorStart:   start,
@@ -208,7 +194,7 @@ func (h *Manager) UpsertConfigMap(ctx context.Context, now time.Time) error {
 }
 
 func (h *Manager) write(ctx context.Context, data ...*SensorMetadata) error {
-	cm, err := pastSensorDataToConfigMap(h.cmLabels, data...)
+	cm, err := pastSensorDataToConfigMap(data...)
 	if err != nil {
 		return errors.Wrap(err, "converting past sensor data to config map")
 	}
