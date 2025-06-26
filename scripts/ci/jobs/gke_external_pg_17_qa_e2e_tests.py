c3318181ba4eb02eadff1481ce7f2a732590746f
Dave Shrewsberry <dashrews@redhat.com>
2025-06-26 16:58:35 -0400

    trying to clean up some stuff

--- scripts/ci/jobs/gke_external_pg_17_qa_e2e_tests.py
+++ scripts/ci/jobs/gke_external_pg_17_qa_e2e_tests.py
@@ -3,9 +3,6 @@
 """
 Run qa-tests-backend in a GKE cluster with external PG 17
 """
-# import os
-# from base_qa_e2e_test import make_qa_e2e_test_runner
-# from clusters import GKECluster
 import os
 from runners import ClusterTestRunner
 from clusters import GKECluster
@@ -15,13 +12,11 @@ from post_tests import PostClusterTest, FinalPost
 
 # set test parameters
 os.environ["ORCHESTRATOR_FLAVOR"] = "k8s"
-os.environ["GCP_IMAGE_TYPE"] = "cos_containerd"
 
 os.environ["ROX_ACTIVE_VULN_MGMT"] = "true"
 os.environ["ROX_RISK_REPROCESSING_INTERVAL"] = "15s"
 os.environ["ROX_SENSOR_CONNECTION_RETRY_MAX_INTERVAL"] = "30s"
 
-# make_qa_e2e_test_runner(cluster=GKECluster("qa-external-pg-17-e2e-test")).run()
 ClusterTestRunner(
     cluster=GKECluster("byodb-test", machine_type="e2-standard-8"),
     pre_test=PreSystemTests(),
