ad6562a837b4f4bf8ec44331213beccf315f59f1
Saif Chaudhry <schaudhr@redhat.com>
2025-06-30 16:27:26 -0700

    modified NetworkSimulatorActions

--- ui/apps/platform/src/Containers/NetworkGraph/simulation/NetworkSimulatorActions.tsx
+++ ui/apps/platform/src/Containers/NetworkGraph/simulation/NetworkSimulatorActions.tsx
@@ -1,12 +1,7 @@
-import { DropEvent, Split, SplitItem } from '@patternfly/react-core';
-import {
-    Dropdown,
-    DropdownDirection,
-    DropdownItem,
-    DropdownToggle,
-} from '@patternfly/react-core/deprecated';
+import { DropdownItem, DropEvent, Split, SplitItem } from '@patternfly/react-core';
 import React from 'react';
 import UploadYAMLButton from './UploadYAMLButton';
+import MenuDropdown from 'Components/PatternFly/MenuDropdown';
 
 type NetworkSimulatorActionsProps = {
     generateNetworkPolicies: () => void;
@@ -15,74 +10,37 @@ type NetworkSimulatorActionsProps = {
     openNotifyYAMLModal?: () => void;
 };
 
-const actionsDropdownId = 'network-simulator-actions-dropdown';
-
-const labels = {
-    generate: 'Rebuild rules from active traffic',
-    undo: 'Revert rules to previously applied YAML',
-    notify: 'Share YAML with notifiers',
-};
-
 function NetworkSimulatorActions({
     generateNetworkPolicies,
     undoNetworkPolicies,
     onFileInputChange,
     openNotifyYAMLModal,
 }: NetworkSimulatorActionsProps) {
-    const [isActionsOpen, setIsActionsOpen] = React.useState(false);
-
-    const onToggle = (isOpen: boolean) => {
-        setIsActionsOpen(isOpen);
-    };
-
-    const onFocus = () => {
-        const element = document.getElementById(actionsDropdownId);
-        element?.focus();
-    };
-
-    const onSelect = () => {
-        setIsActionsOpen(false);
-        onFocus();
-    };
-
-    const actionsDropdownItems = [
-        <DropdownItem key="generate" tooltip="" onClick={generateNetworkPolicies}>
-            {labels.generate}
-        </DropdownItem>,
-        <DropdownItem key="undo" tooltip="" onClick={undoNetworkPolicies}>
-            {labels.undo}
-        </DropdownItem>,
-    ];
-
-    if (openNotifyYAMLModal) {
-        actionsDropdownItems.unshift(
-            <DropdownItem key="notify" tooltip="" onClick={openNotifyYAMLModal}>
-                {labels.notify}
-            </DropdownItem>
-        );
-    }
-
     return (
         <Split hasGutter className="pf-v5-u-p-md">
             <SplitItem>
                 <UploadYAMLButton onFileInputChange={onFileInputChange} />
             </SplitItem>
             <SplitItem>
-                <Dropdown
-                    direction={DropdownDirection.up}
-                    position="left"
-                    onSelect={onSelect}
-                    toggle={
-                        <DropdownToggle
-                            id={actionsDropdownId}
-                            onToggle={(_event, isOpen: boolean) => onToggle(isOpen)}
-                        >
-                            Actions
-                        </DropdownToggle>
-                    }
-                    isOpen={isActionsOpen}
-                    dropdownItems={actionsDropdownItems}
-                />
+                <MenuDropdown
+                    popperProps={{
+                        direction: 'up',
+                        position: 'start',
+                    }}
+                    toggleText="Actions"
+                >
+                    {openNotifyYAMLModal && (
+                        <DropdownItem key="notify" onClick={openNotifyYAMLModal}>
+                            Share YAML with notifiers
+                        </DropdownItem>
+                    )}
+                    <DropdownItem key="generate" onClick={generateNetworkPolicies}>
+                        Rebuild rules from active traffic
+                    </DropdownItem>
+                    <DropdownItem key="undo" onClick={undoNetworkPolicies}>
+                        Revert rules to previously applied YAML
+                    </DropdownItem>
+                </MenuDropdown>
             </SplitItem>
         </Split>
     );
