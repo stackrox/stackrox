a4326d10aac66ff3e97f692c2d91f3720a361640
Moritz Clasmeier <mclasmeier@redhat.com>
2025-07-01 13:47:27 +0200

    Central clusterinit service: Style fix

--- central/clusterinit/service/service_impl.go
+++ central/clusterinit/service/service_impl.go
@@ -139,7 +139,7 @@ func (s *serviceImpl) GenerateCRS(ctx context.Context, request *v1.CRSGenRequest
 	validUntil := fromProtoTimestamp(request.GetValidUntil())
 	validFor := request.GetValidFor().AsDuration()
 	if !validUntil.IsZero() && validFor != 0 {
-		return nil, status.Errorf(codes.InvalidArgument, "cannot use validUntil and validFor at the same time")
+		return nil, status.Error(codes.InvalidArgument, "cannot use validUntil and validFor at the same time")
 	}
 
 	if validFor != 0 {
