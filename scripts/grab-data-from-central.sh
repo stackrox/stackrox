2ff90de73031822540083f92a80eb90bae9ac1d9
Dave Shrewsberry <dashrews@redhat.com>
2025-07-03 11:21:05 -0400

    fix script

--- scripts/grab-data-from-central.sh
+++ scripts/grab-data-from-central.sh
@@ -49,7 +49,7 @@ main() {
 
     // backup not supported for external databases
     local byodb="${BYODB_TEST:-false}"
-    if [[ "${byodb}" == "false" ]] then
+    if [ "${byodb}" == "false" ]; then
         roxctl --ca="" --insecure-skip-tls-verify -e "${api_endpoint}" central backup --output "${dest}"
     fi
 
