1d7535d8db29ca2b7e5dfffe2c278bd1d50e4377
Kyle Lape <klape@redhat.com>
2025-07-03 22:52:44 -0500

    ROX-29931: First draft of Virtual Machine API
    * Virtual machine proto includes ID, name, and namespace
    * Proto also includes Scan, borrowed from Image
    * Basic CRD service (create, retrieve, delete - no update)
    * Intend to remove create later. Added now just to get data in
    * Ephemeral data store - does not use DB for now
    * Unit tests
    * Sensor pipeline coming in a follow up PR

---
+++ central/virtualmachine/service/service.go
@@ -0,0 +1,30 @@
+package service
+
+import (
+	"context"
+
+	"github.com/stackrox/rox/central/role/sachelper"
+	"github.com/stackrox/rox/central/virtualmachine/datastore"
+	v1 "github.com/stackrox/rox/generated/api/v1"
+	"github.com/stackrox/rox/pkg/grpc"
+)
+
+// Service provides the interface to the microservice that serves alert data.
+type Service interface {
+	grpc.APIService
+
+	AuthFuncOverride(ctx context.Context, fullMethodName string) (context.Context, error)
+
+	v1.VirtualMachineServiceServer
+}
+
+// New returns a new Service instance using the given DataStore.
+func New(
+	datastore datastore.DataStore,
+	clusterSACHelper sachelper.ClusterSacHelper,
+) Service {
+	return &serviceImpl{
+		datastore:        datastore,
+		clusterSACHelper: clusterSACHelper,
+	}
+}
