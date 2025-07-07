3e912060962e2b9974c5a2d6ae63420d2a173ee0
Guzman <guz@redhat.com>
2025-07-07 10:05:15 +0200

    Ensure default integration doesn't exist if feature flag is disabled
    Without this we could encounter a situation where:
    
    - User starts Stackrox with the feature flag enabled
    - Default integration is created
    - User restarts central with the feature flag disabled
    - Default integration is still in the DB
    - It is shown in the UI, which can be confusing to users
    - Central behaves in ways that could be confusing to users, see [1]
    
    To prevent this, we ensure that the integration doesn't exist if central
    starts with the feature flag disabled: if it previously existed, it will
    be deleted.
    
    [1] https://github.com/stackrox/stackrox/pull/15761/commits/dc673fb82f83762dad3005d8186afe237166675b

--- central/signatureintegration/datastore/singleton.go
+++ central/signatureintegration/datastore/singleton.go
@@ -20,30 +20,60 @@ var (
 	instance DataStore
 )
 
-func createDefaultRedHatSignatureIntegration(siStore store.SignatureIntegrationStore) {
-	ctx := sac.WithGlobalAccessScopeChecker(context.Background(), sac.AllowAllAccessScopeChecker())
+// createDefaultRedHatSignatureIntegration creates the default Red Hat signature integration if it doesn't exist.
+func createDefaultRedHatSignatureIntegration(ctx context.Context, siStore store.SignatureIntegrationStore) {
 	if _, exists, err := siStore.Get(ctx, signatures.DefaultRedHatSignatureIntegration.GetId()); err != nil {
 		utils.Should(errors.Wrap(err, "unable to detect if default Red Hat signature integration exists"))
 		return
 	} else if exists {
-		// Nothing to do, integration exists.
+		log.Debug("Default Red Hat signature integration already exists, not upserting")
 		return
 	}
 
-	log.Infof("Upserting default Red Hat signature integration %q (%s)",
-		signatures.DefaultRedHatSignatureIntegration.GetName(),
-		signatures.DefaultRedHatSignatureIntegration.GetId())
+	log.Info("Upserting default Red Hat signature integration")
 	err := siStore.Upsert(ctx, signatures.DefaultRedHatSignatureIntegration)
 	utils.Should(errors.Wrap(err, "unable to upsert default Red Hat signature integration"))
+}
+
+// removeDefaultRedHatSignatureIntegration removes the default Red Hat signature integration if it exists.
+func removeDefaultRedHatSignatureIntegration(ctx context.Context, siStore store.SignatureIntegrationStore) {
+	if _, exists, err := siStore.Get(ctx, signatures.DefaultRedHatSignatureIntegration.GetId()); err != nil {
+		utils.Should(errors.Wrap(err, "unable to detect if default Red Hat signature integration exists"))
+		return
+	} else if !exists {
+		log.Debug("Default Red Hat signature integration does not exist, not deleting")
+		return
+	}
+
+	log.Info("Deleting default Red Hat signature integration")
+	err := siStore.Delete(ctx, signatures.DefaultRedHatSignatureIntegration.GetId())
+	utils.Should(errors.Wrap(err, "unable to delete default Red Hat signature integration"))
+}
+
+// setupDefaultRedHatSignatureIntegration ensures the presence of the default Red Hat signature integration is
+// in line with the RedHatImagesSignedPolicy feature flag.
+func setupDefaultRedHatSignatureIntegration(siStore store.SignatureIntegrationStore) {
+	log.Debugf("Setting up default Red Hat signature integration %q (%s)",
+		signatures.DefaultRedHatSignatureIntegration.GetName(),
+		signatures.DefaultRedHatSignatureIntegration.GetId())
+
+	// Decide what to do based on the feature flag
+	log.Debugf("RedHatImagesSignedPolicy feature flag is %t", features.RedHatImagesSignedPolicy.Enabled())
+
+	ctx := sac.WithGlobalAccessScopeChecker(context.Background(), sac.AllowAllAccessScopeChecker())
+
+	if features.RedHatImagesSignedPolicy.Enabled() {
+		createDefaultRedHatSignatureIntegration(ctx, siStore)
+	} else {
+		removeDefaultRedHatSignatureIntegration(ctx, siStore)
+	}
 }
 
 // Singleton returns the sole instance of the DataStore service.
 func Singleton() DataStore {
 	once.Do(func() {
 		storage := pgStore.New(globaldb.GetPostgres())
-		if features.RedHatImagesSignedPolicy.Enabled() {
-			createDefaultRedHatSignatureIntegration(storage)
-		}
+		setupDefaultRedHatSignatureIntegration(storage)
 		instance = New(storage, policyDataStore.Singleton())
 	})
 	return instance
--- central/signatureintegration/datastore/singleton_test.go
+++ central/signatureintegration/datastore/singleton_test.go
@@ -1,6 +1,7 @@
 package datastore
 
 import (
+	"context"
 	"testing"
 
 	mockSIStore "github.com/stackrox/rox/central/signatureintegration/store/mocks"
@@ -9,19 +10,47 @@ import (
 )
 
 func TestCreateDefaultRedHatSignatureIntegration(t *testing.T) {
+	ctx := context.Background()
+
 	t.Run("should not add default integration when it already exists", func(t *testing.T) {
 		s := mockSIStore.NewMockSignatureIntegrationStore(gomock.NewController(t))
-		s.EXPECT().Get(gomock.Any(), gomock.Any()).Return(
+
+		s.EXPECT().Get(gomock.Any(), signatures.DefaultRedHatSignatureIntegration.GetId()).Return(
 			signatures.DefaultRedHatSignatureIntegration, true, nil)
 
-		createDefaultRedHatSignatureIntegration(s)
+		createDefaultRedHatSignatureIntegration(ctx, s)
 	})
 
 	t.Run("should add default integration when it doesn't exist", func(t *testing.T) {
 		s := mockSIStore.NewMockSignatureIntegrationStore(gomock.NewController(t))
-		s.EXPECT().Get(gomock.Any(), gomock.Any()).Return(nil, false, nil)
+
+		s.EXPECT().Get(gomock.Any(), signatures.DefaultRedHatSignatureIntegration.GetId()).Return(
+			nil, false, nil)
 		s.EXPECT().Upsert(gomock.Any(), signatures.DefaultRedHatSignatureIntegration).Return(nil)
 
-		createDefaultRedHatSignatureIntegration(s)
+		createDefaultRedHatSignatureIntegration(ctx, s)
+	})
+}
+
+func TestRemoveDefaultRedHatSignatureIntegration(t *testing.T) {
+	ctx := context.Background()
+
+	t.Run("should not delete default integration when it doesn't exist", func(t *testing.T) {
+		s := mockSIStore.NewMockSignatureIntegrationStore(gomock.NewController(t))
+
+		s.EXPECT().Get(gomock.Any(), signatures.DefaultRedHatSignatureIntegration.GetId()).Return(
+			nil, false, nil)
+
+		removeDefaultRedHatSignatureIntegration(ctx, s)
+	})
+
+	t.Run("should delete default integration when it exists", func(t *testing.T) {
+		s := mockSIStore.NewMockSignatureIntegrationStore(gomock.NewController(t))
+
+		s.EXPECT().Get(gomock.Any(), signatures.DefaultRedHatSignatureIntegration.GetId()).Return(
+			signatures.DefaultRedHatSignatureIntegration, true, nil)
+		s.EXPECT().Delete(gomock.Any(), signatures.DefaultRedHatSignatureIntegration.GetId()).Return(nil)
+
+		removeDefaultRedHatSignatureIntegration(ctx, s)
 	})
 }
