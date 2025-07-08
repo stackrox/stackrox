50928014877f4cb1efa944c606abbb2ba22e74ac
davdhacs <105243888+davdhacs@users.noreply.github.com>
2025-07-08 12:56:57 -0600

    skip gke cleanup

--- scripts/ci/cleanup-deployment.sh
+++ scripts/ci/cleanup-deployment.sh
@@ -2,4 +2,7 @@
 
 namespace=${1:-stackrox}
 
-kubectl -n "${namespace}" get cm,deploy,ds,networkpolicy,pv,pvc,secret,svc,serviceaccount -o name | xargs kubectl -n "${namespace}" delete --wait
+echo "unexpected execution of cleanup scripts - SKIPPING cleanup"
+echo "[skipped cmd:] kubectl -n ${namespace} get cm,deploy,ds,networkpolicy,pv,pvc,secret,svc,serviceaccount -o name | xargs kubectl -n ${namespace} delete --wait"
+echo 'exit 1 to fail if this is executed'
+exit 1
