955c00fcb425fc6ecf7c0a9ee689241de44b8088
Mark Pedrotti <mark@stackrox.com>
2025-07-14 12:04:31 -0400

    ROX-30110: Enable export-default-react lint rule with ignores

--- ui/apps/platform/src/Components/PatternFly/ErrorBoundary/ErrorBoundary.tsx
+++ ui/apps/platform/src/Components/PatternFly/ErrorBoundary/ErrorBoundary.tsx
@@ -63,4 +63,7 @@ function ErrorBoundaryWrapper({ children }: { children: ReactNode }) {
     return <ErrorBoundary location={location.pathname}>{children}</ErrorBoundary>;
 }
 
+// Encapsulate ErrorBoundaryWrapper as implementation detail,
+// especially since ErrorBoundary appears in Find results.
+// eslint-disable-next-line limited/export-default-react
 export default ErrorBoundaryWrapper;
