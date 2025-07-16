ff3791f6a7932681caa011749927c3d21c110456
davdhacs <105243888+davdhacs@users.noreply.github.com>
2025-07-16 11:25:24 -0600

    try files-changed as an action

---
+++ .github/actions/files-changed/find-changes.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -x
+declare -A filters="$1"
+base=${2:-origin/master}
+sha=${3:-$(git rev-parse HEAD)}
+
+set +e
+
+if [[ -z "${filters[@]}" ]]; then
+  echo 'No filters provided. Please re-try with filters set like "( ["ui"]="ui/**" ["gha"]=".github/**" )"'
+  exit 1
+fi
+
+# compare with merge-base
+echo "# Find changes..."
+changes=$(git diff --name-only ${base}...${sha} \
+  | tee >( cat >&2 ))
+
+echo "# Match filters..."
+# for each labeled ex
+IFS=$'\n'
+declare -A matches
+for k in "${!filters[@]}"; do
+  exp="${filters[$k]}"
+  for line in ${changes}; do
+    if [[ $line == $exp ]]; then
+      matches["$k"]="${matches[$k]:-}${matches[$k]:+ }${line}"
+    fi
+  done
+done
+
+for k in "${!matches[@]}"; do
+  echo "$k=\"${matches[$k]}\""
+done \
+  | tee ${GITHUB_OUTPUT:-/dev/null}
