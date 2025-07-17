98ad8684d9eb0093d51cad424cb96ebbe60478dc
davdhacs <105243888+davdhacs@users.noreply.github.com>
2025-07-16 23:15:03 -0600

    and skip ossls

--- .github/actions/files-changed/find-changes.sh
+++ .github/actions/files-changed/find-changes.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 
-set -x
 declare -A filters="$1"
 base=${2:-$(git rev-parse origin/master)}
 sha=${3:-$(git rev-parse HEAD)}
 
 if [[ -z "${filters[@]}" ]]; then
-  echo 'No filters provided. Please re-try with filters set like "( ["ui"]="ui/**" ["gha"]=".github/**" )"'
+  echo 'No filters provided. Please re-try with filters set like "( ["ui"]="ui/**" ["gha"]=".github/**" )"' >&2
   exit 1
 fi
 
 # compare with merge-base
-echo "# Find changes..."
+echo "# Find changes..." >&2
 changes=$(git diff --name-only ${base}...${sha} \
   | tee >( cat >&2 ))
 
-echo "# Match filters..."
+echo "# Match filters..." >&2
 # for each labeled ex
 IFS=$'\n'
 declare -A matches
@@ -28,7 +27,7 @@ for k in "${!filters[@]}"; do
   done
 done
 
-for k in "${!matches[@]}"; do
-  echo "$k=\"${matches[$k]}\""
-done \
-  | tee ${GITHUB_OUTPUT:-/dev/null}
+#for k in "${!matches[@]}"; do
+#  echo "$k=\"${matches[$k]}\""
+#done \
+echo "matches=\"${!matches[@]}\""
