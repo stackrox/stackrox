28fd2ecb6b268ef3c678546c967f1eaf646c41fb
Tomasz Janiszewski <tomek@redhat.com>
2025-07-17 17:26:05 +0200

    queues
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

--- sensor/common/sensor/central_receiver_impl.go
+++ sensor/common/sensor/central_receiver_impl.go
@@ -1,7 +1,9 @@
 package sensor
 
 import (
+	"context"
 	"io"
+	"time"
 
 	"github.com/stackrox/rox/generated/internalapi/central"
 	"github.com/stackrox/rox/pkg/concurrency"
@@ -29,11 +31,36 @@ func (s *centralReceiverImpl) Stopped() concurrency.ReadOnlyErrorSignal {
 
 // Take in data processed by central, run post-processing, then send it to the output channel.
 func (s *centralReceiverImpl) receive(stream central.SensorService_CommunicateClient, onStops ...func()) {
+	componentsQueues := make(map[string]chan *central.MsgToSensor, len(s.receivers))
+	for _, r := range s.receivers {
+		componentsQueues[r.Name()] = make(chan *central.MsgToSensor, 1)
+	}
+
 	defer func() {
 		s.stopper.Flow().ReportStopped()
 		runAll(onStops...)
 		s.finished.Done()
+		for _, ch := range componentsQueues {
+			close(ch)
+		}
 	}()
+
+	sendToAll := func(ctx context.Context, msg *central.MsgToSensor) {
+		ctx, cancel := context.WithTimeout(ctx, time.Second)
+		defer cancel()
+		for name, ch := range componentsQueues {
+			select {
+			case ch <- msg:
+				log.Debug("Sending msg to %s", name)
+			case <-ctx.Done():
+				log.Error("Failed to send msg %T to %s receiver channel", msg, name)
+			}
+		}
+	}
+
+	for _, receiver := range s.receivers {
+		go process(componentsQueues[receiver.Name()], receiver, receiver.Name())
+	}
 
 	for {
 		select {
@@ -58,11 +85,16 @@ func (s *centralReceiverImpl) receive(stream central.SensorService_CommunicateCl
 				s.stopper.Flow().StopWithError(err)
 				return
 			}
-			for _, r := range s.receivers {
-				if err := r.ProcessMessage(msg); err != nil {
-					log.Error(err)
-				}
-			}
+			go sendToAll(stream.Context(), msg)
 		}
 	}
+}
+
+func process(ch <-chan *central.MsgToSensor, r common.CentralReceiver, name string) {
+	for msg := range ch {
+		if err := r.ProcessMessage(msg); err != nil {
+			log.Errorf("%s: %+v", name, err)
+		}
+	}
+	log.Infof("Stopping %s", name)
 }
