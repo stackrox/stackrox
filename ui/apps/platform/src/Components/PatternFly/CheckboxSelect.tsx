f57a3f00c9756cbda434747559c3d8a2a19142d8
Saif Chaudhry <schaudhr@redhat.com>
2025-07-18 11:39:24 -0700

    refactor(ui): Migrate CheckboxSelect from deprecated implementation

--- ui/apps/platform/src/Components/PatternFly/CheckboxSelect.tsx
+++ ui/apps/platform/src/Components/PatternFly/CheckboxSelect.tsx
@@ -1,14 +1,21 @@
 import React, { ReactElement, useState } from 'react';
-import { Select, SelectOptionObject, SelectOptionProps } from '@patternfly/react-core/deprecated';
+import {
+    Select,
+    SelectOption,
+    MenuToggle,
+    MenuToggleElement,
+    Badge,
+    Flex,
+    FlexItem,
+} from '@patternfly/react-core';
 
 export type CheckboxSelectProps = {
     id?: string;
-    name?: string;
     selections: string[];
     onChange: (selection: string[]) => void;
-    onBlur?: React.FocusEventHandler<HTMLTextAreaElement>;
+    onBlur?: React.FocusEventHandler<HTMLDivElement>;
     ariaLabel: string;
-    children: ReactElement<SelectOptionProps>[];
+    children: ReactElement<typeof SelectOption>[];
     placeholderText?: string;
     toggleIcon?: ReactElement;
     toggleId?: string;
@@ -17,7 +24,6 @@ export type CheckboxSelectProps = {
 
 function CheckboxSelect({
     id,
-    name,
     selections,
     onChange,
     onBlur,
@@ -30,13 +36,13 @@ function CheckboxSelect({
 }: CheckboxSelectProps): ReactElement {
     const [isOpen, setIsOpen] = useState(false);
 
-    function onToggle(isExpanded: boolean) {
-        setIsOpen(isExpanded);
+    function onToggle() {
+        setIsOpen(!isOpen);
     }
 
     function onSelect(
-        event: React.MouseEvent | React.ChangeEvent,
-        selection: string | SelectOptionObject
+        _event: React.MouseEvent<Element, MouseEvent> | undefined,
+        selection: string | number | undefined
     ) {
         if (typeof selection !== 'string' || !selections || !onChange) {
             return;
@@ -48,21 +54,44 @@ function CheckboxSelect({
         }
     }
 
+    const toggle = (toggleRef: React.Ref<MenuToggleElement>) => (
+        <MenuToggle
+            id={toggleId}
+            ref={toggleRef}
+            onClick={onToggle}
+            isExpanded={isOpen}
+            icon={toggleIcon}
+            aria-label={ariaLabel}
+        >
+            <Flex
+                alignItems={{ default: 'alignItemsCenter' }}
+                spaceItems={{ default: 'spaceItemsSm' }}
+            >
+                <FlexItem>{placeholderText}</FlexItem>
+                {selections.length > 0 && <Badge isRead>{selections.length}</Badge>}
+            </Flex>
+        </MenuToggle>
+    );
+
     return (
         <Select
             id={id}
-            name={name}
-            variant="checkbox"
-            toggleIcon={toggleIcon}
-            onToggle={(_event, isExpanded: boolean) => onToggle(isExpanded)}
-            onSelect={onSelect}
-            onBlur={onBlur}
-            selections={selections}
-            isOpen={isOpen}
-            placeholderText={placeholderText}
+            role="menu"
             aria-label={ariaLabel}
-            toggleId={toggleId}
-            menuAppendTo={menuAppendTo}
+            isOpen={isOpen}
+            selected={selections}
+            onSelect={onSelect}
+            onOpenChange={(nextOpen: boolean) => setIsOpen(nextOpen)}
+            toggle={toggle}
+            shouldFocusToggleOnSelect
+            popperProps={
+                menuAppendTo
+                    ? {
+                          appendTo: menuAppendTo,
+                      }
+                    : undefined
+            }
+            onBlur={onBlur}
         >
             {children}
         </Select>
--- ui/apps/platform/src/Containers/Vulnerabilities/VulnerablityReporting/forms/ReportParametersForm.tsx
+++ ui/apps/platform/src/Containers/Vulnerabilities/VulnerablityReporting/forms/ReportParametersForm.tsx
@@ -8,11 +8,11 @@ import {
     Form,
     FormGroup,
     PageSection,
+    SelectOption,
     TextArea,
     TextInput,
     Title,
 } from '@patternfly/react-core';
-import { SelectOption } from '@patternfly/react-core/deprecated';
 import { FormikProps } from 'formik';
 import { cloneDeep } from 'lodash';
 
@@ -124,7 +124,6 @@ function ReportParametersForm({ title, formik }: ReportParametersFormParams): Re
                 >
                     <CheckboxSelect
                         toggleId="reportParameters.cveSeverities"
-                        name="reportParameters.cveSeverities"
                         ariaLabel="CVE severity checkbox select"
                         selections={formik.values.reportParameters.cveSeverities}
                         onChange={handleCheckboxSelectChange('reportParameters.cveSeverities')}
@@ -187,7 +186,6 @@ function ReportParametersForm({ title, formik }: ReportParametersFormParams): Re
                 >
                     <CheckboxSelect
                         toggleId="reportParameters.cveStatus"
-                        name="reportParameters.cveStatus"
                         ariaLabel="CVE status checkbox select"
                         selections={formik.values.reportParameters.cveStatus}
                         onChange={handleCheckboxSelectChange('reportParameters.cveStatus')}
@@ -209,7 +207,6 @@ function ReportParametersForm({ title, formik }: ReportParametersFormParams): Re
                 >
                     <CheckboxSelect
                         toggleId="reportParameters.imageType"
-                        name="reportParameters.imageType"
                         ariaLabel="Image type checkbox select"
                         selections={formik.values.reportParameters.imageType}
                         onChange={handleCheckboxSelectChange('reportParameters.imageType')}
