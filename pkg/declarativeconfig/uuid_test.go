611a662b97ba21a638f8332ef3abee9d173f83f2
Yann Brillouet <ybrillou@redhat.com>
2025-07-18 16:09:53 +0200

    Extend unit test coverage

--- pkg/declarativeconfig/uuid_test.go
+++ pkg/declarativeconfig/uuid_test.go
@@ -8,40 +8,44 @@ import (
 )
 
 func TestDeterministicUUIDs(t *testing.T) {
-	declarativeUUIDFuncs := []func(string) uuid.UUID{
-		NewDeclarativeAuthProviderUUID,
-		NewDeclarativeGroupUUID,
-		NewDeclarativePermissionSetUUID,
-		NewDeclarativeAccessScopeUUID,
-		NewDeclarativeNotifierUUID,
-		NewDeclarativeHandlerUUID,
+	declarativeUUIDFuncs := map[string]func(string) uuid.UUID{
+		authProviderUUIDNS:  NewDeclarativeAuthProviderUUID,
+		groupUUIDNS:         NewDeclarativeGroupUUID,
+		permissionSetUUIDNS: NewDeclarativePermissionSetUUID,
+		accessScopeUUIDNS:   NewDeclarativeAccessScopeUUID,
+		notifierUUIDNS:      NewDeclarativeNotifierUUID,
+		handlerUUIDNS:       NewDeclarativeHandlerUUID,
+		authM2MConfigUUIDNS: NewDeclarativeM2MAuthConfigUUID,
 	}
 	dummyName := "dummy-resource"
 
-	// 1. Test that using the same name will lead to the same UUID being created.
-	for _, f := range declarativeUUIDFuncs {
-		firstID := f(dummyName)
-		secondID := f(dummyName)
-		assert.Equal(t, firstID, secondID)
-	}
+	for typeName, f := range declarativeUUIDFuncs {
+		t.Run(typeName, func(it *testing.T) {
+			// 1. Test that using the same name will lead to the same UUID being created.
+			firstID := f(dummyName)
+			secondID := f(dummyName)
+			assert.Equal(it, firstID, secondID)
+			firstIDString := firstID.String()
+			secondIDString := secondID.String()
+			assert.Equal(it, firstIDString, secondIDString)
 
-	// 2. Test that using the same name won't lead to clashes within the different namespaces.
-	for i, f := range declarativeUUIDFuncs {
-		id := f(dummyName)
-		for j, f := range declarativeUUIDFuncs {
-			if j == i {
-				continue
+			// 2. Test that using the same name won't lead to clashes within the different namespaces.
+			for otherTypeName, otherUUIDFunc := range declarativeUUIDFuncs {
+				if typeName == otherTypeName {
+					continue
+				}
+				otherID := otherUUIDFunc(dummyName)
+				assert.NotEqual(it, firstID, otherID)
+				otherIDString := otherID.String()
+				assert.NotEqual(it, firstIDString, otherIDString)
 			}
-			otherID := f(dummyName)
-			assert.NotEqual(t, id, otherID)
-		}
-	}
 
-	// 3. Test that using different names in the same namespace will lead to different UUIDs being created.
-	secondDummyName := "another-dummy-resource"
-	for _, f := range declarativeUUIDFuncs {
-		firstID := f(dummyName)
-		secondID := f(secondDummyName)
-		assert.NotEqual(t, firstID, secondID)
+			// 3. Test that using different names in the same namespace will lead to different UUIDs being created.
+			anotherDummyName := "another-dummy-resource"
+			anotherID := f(anotherDummyName)
+			assert.NotEqual(it, firstID, anotherID)
+			anotherIDString := anotherID.String()
+			assert.NotEqual(it, firstIDString, anotherIDString)
+		})
 	}
 }
