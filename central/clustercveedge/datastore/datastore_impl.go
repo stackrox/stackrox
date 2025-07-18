858fc8d5621cbaaacc9025a5b86e88b600167388
Dave Shrewsberry <dashrews@redhat.com>
2025-07-18 10:53:55 -0400

    remove some searchers

--- central/clustercveedge/datastore/datastore_impl.go
+++ central/clustercveedge/datastore/datastore_impl.go
@@ -3,7 +3,6 @@ package datastore
 import (
 	"context"
 
-	"github.com/stackrox/rox/central/clustercveedge/search"
 	"github.com/stackrox/rox/central/clustercveedge/store"
 	v1 "github.com/stackrox/rox/generated/api/v1"
 	"github.com/stackrox/rox/generated/storage"
@@ -11,24 +10,39 @@ import (
 )
 
 type datastoreImpl struct {
-	storage  store.Store
-	searcher search.Searcher
+	storage store.Store
 }
 
 func (ds *datastoreImpl) Search(ctx context.Context, q *v1.Query) ([]searchPkg.Result, error) {
-	return ds.searcher.Search(ctx, q)
+	return ds.storage.Search(ctx, edgefields.TransformFixableFieldsQuery(q))
 }
 
 func (ds *datastoreImpl) SearchEdges(ctx context.Context, q *v1.Query) ([]*v1.SearchResult, error) {
-	return ds.searcher.SearchEdges(ctx, q)
-}
-
-func (ds *datastoreImpl) SearchRawEdges(ctx context.Context, q *v1.Query) ([]*storage.ClusterCVEEdge, error) {
-	imgs, err := ds.searcher.SearchRawEdges(ctx, q)
+	results, err := ds.Search(ctx, q)
 	if err != nil {
 		return nil, err
 	}
-	return imgs, nil
+
+	cves, missingIndices, err := ds.storage.GetMany(ctx, searchPkg.ResultsToIDs(results))
+	if err != nil {
+		return nil, err
+	}
+	results = searchPkg.RemoveMissingResults(results, missingIndices)
+	return convertMany(cves, results), nil
+}
+
+func (ds *datastoreImpl) SearchRawEdges(ctx context.Context, q *v1.Query) ([]*storage.ClusterCVEEdge, error) {
+	q = edgefields.TransformFixableFieldsQuery(q)
+	var edges []*storage.ClusterCVEEdge
+	err := ds.storage.GetByQueryFn(ctx, q, func(edge *storage.ClusterCVEEdge) error {
+		edges = append(edges, edge)
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return edges, nil
 }
 
 func (ds *datastoreImpl) Count(ctx context.Context, q *v1.Query) (int, error) {
@@ -57,4 +71,22 @@ func (ds *datastoreImpl) GetBatch(ctx context.Context, ids []string) ([]*storage
 		return nil, err
 	}
 	return edges, nil
+}
+
+func convertMany(cves []*storage.ClusterCVEEdge, results []searchPkg.Result) []*v1.SearchResult {
+	outputResults := make([]*v1.SearchResult, len(cves))
+	for index, sar := range cves {
+		outputResults[index] = convertOne(sar, &results[index])
+	}
+	return outputResults
+}
+
+func convertOne(obj *storage.ClusterCVEEdge, result *searchPkg.Result) *v1.SearchResult {
+	return &v1.SearchResult{
+		Category:       v1.SearchCategory_CLUSTER_VULN_EDGE,
+		Id:             obj.GetId(),
+		Name:           obj.GetId(),
+		FieldToMatches: searchPkg.GetProtoMatchesMap(result.Matches),
+		Score:          result.Score,
+	}
 }
