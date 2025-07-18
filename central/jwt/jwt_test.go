7e24b3d19c529bf25959cd52203bb131143cdb14
Michaël <github@parameta.lol>
2025-07-18 11:07:39 +0200

    ROX-30087: cache tokens in the implicit m2m exchange flow (#16035)

--- central/jwt/jwt_test.go
+++ central/jwt/jwt_test.go
@@ -8,8 +8,10 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/stackrox/rox/central/auth/m2m/mocks"
+	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/pkg/auth/tokens"
 	"github.com/stackrox/rox/pkg/errox"
+	"github.com/stackrox/rox/pkg/expiringcache"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/mock/gomock"
 )
@@ -61,6 +63,9 @@ func TestM2MValidator(t *testing.T) {
 			},
 			setupExchangerSet: func(mes *mocks.MockTokenExchangerSet) {
 				mockExchanger := mocks.NewMockTokenExchanger(mockCtrl)
+				mockExchanger.EXPECT().Config().Times(1).Return(&storage.AuthMachineToMachineConfig{
+					TokenExpirationDuration: "1h",
+				})
 				mockExchanger.EXPECT().ExchangeToken(gomock.Any(), tokenWithIssuer("other-issuer")).
 					Return("exchanged-token", nil)
 				mes.EXPECT().GetTokenExchanger("other-issuer").
@@ -80,6 +85,9 @@ func TestM2MValidator(t *testing.T) {
 			token: tokenWithIssuer("failing-issuer"),
 			setupExchangerSet: func(mes *mocks.MockTokenExchangerSet) {
 				mockExchanger := mocks.NewMockTokenExchanger(mockCtrl)
+				mockExchanger.EXPECT().Config().Times(1).Return(&storage.AuthMachineToMachineConfig{
+					TokenExpirationDuration: "1h",
+				})
 				mockExchanger.EXPECT().ExchangeToken(gomock.Any(), tokenWithIssuer("failing-issuer")).
 					Return("", errTest)
 				mes.EXPECT().GetTokenExchanger("failing-issuer").
@@ -102,8 +110,9 @@ func TestM2MValidator(t *testing.T) {
 			exchangerSet.EXPECT().HasExchangersConfigured().Times(1).Return(true)
 
 			validator := &m2mValidator{
-				TokenExchangerSet: exchangerSet,
-				roxValidator:      c.mockRoxValidator,
+				TokenExchangerSet:    exchangerSet,
+				roxValidator:         c.mockRoxValidator,
+				exchangedTokensCache: make(map[string]expiringcache.Cache[string, string]),
 			}
 
 			token, err := validator.Validate(ctx, c.token)
@@ -115,4 +124,70 @@ func TestM2MValidator(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestTokenCache(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	mes := mocks.NewMockTokenExchangerSet(mockCtrl)
+
+	roxValidator := func(ctx context.Context, token string) (*tokens.TokenInfo, error) {
+		assert.Equal(t, tokenWithIssuer(roxIssuer), token)
+		return &tokens.TokenInfo{Token: token}, nil
+	}
+
+	validator := &m2mValidator{
+		TokenExchangerSet:    mes,
+		roxValidator:         mockRoxValidator(roxValidator),
+		exchangedTokensCache: make(map[string]expiringcache.Cache[string, string]),
+	}
+
+	t.Run("1h expiration", func(t *testing.T) {
+		mockExchanger := mocks.NewMockTokenExchanger(mockCtrl)
+		mockExchanger.EXPECT().Config().Times(1).
+			Return(&storage.AuthMachineToMachineConfig{
+				TokenExpirationDuration: "1h",
+			})
+
+		mes.EXPECT().GetTokenExchanger("1h-issuer").
+			Times(2).
+			Return(mockExchanger, true)
+		mockExchanger.EXPECT().ExchangeToken(gomock.Any(), tokenWithIssuer("1h-issuer")).
+			Times(1). // Exchange once.
+			Return("exchanged-token", nil)
+
+		// Exchange and cache:
+		token, err := validator.exchange(context.Background(), "1h-issuer", tokenWithIssuer("1h-issuer"))
+		assert.NoError(t, err)
+		assert.Equal(t, "exchanged-token", token)
+
+		// No exchange, return cached:
+		token, err = validator.exchange(context.Background(), "1h-issuer", tokenWithIssuer("1h-issuer"))
+		assert.NoError(t, err)
+		assert.Equal(t, "exchanged-token", token)
+	})
+
+	t.Run("1m expiration", func(t *testing.T) {
+		mockExchanger := mocks.NewMockTokenExchanger(mockCtrl)
+		mockExchanger.EXPECT().Config().Times(1).
+			Return(&storage.AuthMachineToMachineConfig{
+				TokenExpirationDuration: "1m",
+			})
+
+		mes.EXPECT().GetTokenExchanger("1m-issuer").
+			Times(2).
+			Return(mockExchanger, true)
+		mockExchanger.EXPECT().ExchangeToken(gomock.Any(), tokenWithIssuer("1m-issuer")).
+			Times(2). // Exchange twice.
+			Return("exchanged-token", nil)
+
+		// Exchange and don't cache:
+		token, err := validator.exchange(context.Background(), "1m-issuer", tokenWithIssuer("1m-issuer"))
+		assert.NoError(t, err)
+		assert.Equal(t, "exchanged-token", token)
+
+		// Exchange again:
+		token, err = validator.exchange(context.Background(), "1m-issuer", tokenWithIssuer("1m-issuer"))
+		assert.NoError(t, err)
+		assert.Equal(t, "exchanged-token", token)
+	})
 }
