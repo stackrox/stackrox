49f8861f4207a35e8ee628bb33852e2b64fc7297
Dave Shrewsberry <dashrews@redhat.com>
2025-07-18 08:50:29 -0400

    remove policy integration searcher

--- central/imageintegration/store/mocks/store.go
+++ central/imageintegration/store/mocks/store.go
@@ -88,6 +88,20 @@ func (mr *MockStoreMockRecorder) Get(ctx, id any) *gomock.Call {
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Get", reflect.TypeOf((*MockStore)(nil).Get), ctx, id)
 }
 
+// GetByQueryFn mocks base method.
+func (m *MockStore) GetByQueryFn(ctx context.Context, query *v1.Query, fn func(*storage.ImageIntegration) error) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "GetByQueryFn", ctx, query, fn)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// GetByQueryFn indicates an expected call of GetByQueryFn.
+func (mr *MockStoreMockRecorder) GetByQueryFn(ctx, query, fn any) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GetByQueryFn", reflect.TypeOf((*MockStore)(nil).GetByQueryFn), ctx, query, fn)
+}
+
 // PruneMany mocks base method.
 func (m *MockStore) PruneMany(ctx context.Context, identifiers []string) error {
 	m.ctrl.T.Helper()
