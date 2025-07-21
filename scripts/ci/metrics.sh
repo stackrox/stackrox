dbcd057cf257d4aea97cd0544752d9646be152c5
Tomasz Janiszewski <tomek@redhat.com>
2025-07-21 12:52:23 +0200

    fix
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

--- scripts/ci/metrics.sh
+++ scripts/ci/metrics.sh
@@ -149,7 +149,7 @@ bq_save_job_record() {
     sql_params=()
 
     local columns="stopped_at"
-    local values="TIMESTAMP_SECONDS($(date -u +%s))"
+    local values="TIMESTAMP_SECONDS(${EPOCHSECONDS:-$(date -u +%s)})"
 
     # Process additional field-value pairs
     while [[ "$#" -ne 0 ]]; do
