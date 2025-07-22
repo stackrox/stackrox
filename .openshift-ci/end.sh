8f41a86b87b45469c0d82c3c222f73fade2fbb97
Tomasz Janiszewski <tomek@redhat.com>
2025-07-22 14:16:07 +0200

    Apply suggestion from @janisz

--- .openshift-ci/end.sh
+++ .openshift-ci/end.sh
@@ -13,7 +13,6 @@ end() {
 
     if [[ -f "${SHARED_DIR:-}/shared_env" ]]; then
         # shellcheck disable=SC1091
-        cat "${SHARED_DIR:-}"/shared_env
         source "${SHARED_DIR:-}/shared_env"
     fi
 
