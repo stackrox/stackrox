d96e4f7aa90eec6f3a103bcf418a1919280216c9
David Vail <dcvail17@gmail.com>
2025-07-23 17:05:17 -0400

    Fix eslint for webpack config

--- ui/apps/platform/eslint.config.js
+++ ui/apps/platform/eslint.config.js
@@ -732,6 +732,7 @@ module.exports = [
                         path.join(__dirname, 'eslint.config.js'),
                         path.join(__dirname, 'postcss.config.js'),
                         path.join(__dirname, 'tailwind.config.js'), // only for @tailwindcss/forms
+                        path.join(__dirname, 'webpack.ocp-plugin.config.js'),
                         path.join(__dirname, 'vite.config.js'),
                     ],
                 },
