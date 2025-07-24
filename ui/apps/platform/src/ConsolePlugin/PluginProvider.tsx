5ecd4b9c58bd98148d89cc1155ff287bb7223ae4
David Vail <dcvail17@gmail.com>
2025-07-24 14:03:47 -0400

    Adds adapter for all api requests to delegate to consoleFetch

--- ui/apps/platform/src/ConsolePlugin/PluginProvider.tsx
+++ ui/apps/platform/src/ConsolePlugin/PluginProvider.tsx
@@ -3,22 +3,15 @@ import { ApolloProvider } from '@apollo/client';
 
 import axios from 'services/instance';
 import configureApolloClient from '../init/configureApolloClient';
+import consoleFetchAxiosAdapter from './consoleFetchAxiosAdapter';
 
-const baseURL = '/api/proxy/plugin/advanced-cluster-security/api-service/';
+// The console requires a custom fetch implementation via `consoleFetch` to correctly pass headers such
+// as X-CSRFToken to API requests. All of our current code uses `axios` to make API requests, so we need
+// to override the default adapter to use `consoleFetch` instead of XMLHttpRequest.
+const proxyBaseURL = '/api/proxy/plugin/advanced-cluster-security/api-service';
+axios.defaults.adapter = (config) => consoleFetchAxiosAdapter(proxyBaseURL, config);
 
 const apolloClient = configureApolloClient();
-
-axios.interceptors.request.use((config) => {
-    const updatedConfig = { ...config, baseURL };
-
-    // Note - in production authorization is handled in-cluster by the console and will overwrite this header. When
-    // running locally, we need to inject the token manually to allow API requests to the ACS API.
-    if (process.env.NODE_ENV === 'development' && process.env.ACS_CONSOLE_DEV_TOKEN) {
-        updatedConfig.headers.Authorization = `Bearer ${process.env.ACS_CONSOLE_DEV_TOKEN}`;
-    }
-
-    return updatedConfig;
-});
 
 export default function PluginProvider({ children }) {
     return <ApolloProvider client={apolloClient}>{children}</ApolloProvider>;
