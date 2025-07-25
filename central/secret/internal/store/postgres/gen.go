c1d66751e7ed8a91c41c861199627be1e2d42267
Dave Shrewsberry <dashrews@redhat.com>
2025-07-25 11:11:33 -0400

    start working secrets

--- central/secret/internal/store/postgres/gen.go
+++ central/secret/internal/store/postgres/gen.go
@@ -1,3 +1,3 @@
 package postgres
 
-//go:generate pg-table-bindings-wrapper --type=storage.Secret --search-category SECRETS
+//go:generate pg-table-bindings-wrapper --type=storage.Secret --search-category SECRETS --default-sort search.CreatedTime.String()
