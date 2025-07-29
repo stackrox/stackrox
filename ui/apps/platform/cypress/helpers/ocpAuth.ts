99ffa401dadd52f03434d205a2752b9c7263b609
David Vail <dcvail17@gmail.com>
2025-07-29 15:05:23 -0400

    (WIP) debug in ci

---
+++ ui/apps/platform/cypress/helpers/ocpAuth.ts
@@ -0,0 +1,13 @@
+export function withOcpAuth() {
+    if (Cypress.env('OCP_BRIDGE_AUTH_DISABLED')) {
+        return;
+    }
+
+    cy.session('ocp-session-auth', () => {
+        cy.visit('/');
+        cy.get('input[name="username"]').type(Cypress.env('CLUSTER_USERNAME'));
+        cy.get('input[name="password"]').type(Cypress.env('CLUSTER_PASSWORD'));
+        cy.get('button[type="submit"]').click();
+        // TODO Handle OCP welcome modal
+    });
+}
