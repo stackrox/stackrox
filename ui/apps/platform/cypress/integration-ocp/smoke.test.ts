36fe0cf497541473e2a0d86ec423c16e44d76d8d
David Vail <dcvail17@gmail.com>
2025-07-29 15:05:23 -0400

    (WIP) debug in ci

--- ui/apps/platform/cypress/integration-ocp/smoke.test.ts
+++ ui/apps/platform/cypress/integration-ocp/smoke.test.ts
@@ -1,7 +1,9 @@
-import withAuth from '../helpers/basicAuth';
+import { withOcpAuth } from '../helpers/ocpAuth';
 
 describe('Basic connectivity to the OCP plugin', () => {
     it('should open the OCP web console', () => {
+        // withOcpAuth();
+
         cy.visit('/');
         // TODO Handle auth/skip auth in dev
         // TODO Handle OCP welcome modal
