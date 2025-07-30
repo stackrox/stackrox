e8e9efb3ab11061b4426bb0479fbfe2c5f572884
JoukoVirtanen <jouko@stackrox.com>
2025-07-30 14:33:35 -0700

    Fixed blank space

--- scale/dev/port-forward.sh
+++ scale/dev/port-forward.sh
@@ -35,8 +35,8 @@ done
 echo
 
 nohup kubectl port-forward -n "${namespace}" svc/central "${port}:443" 1>/dev/null 2>&1 &
-until nc -z 127.0.0.1 "$port"; do 
-    sleep 1 
+until nc -z 127.0.0.1 "$port"; do
+    sleep 1
     echo "Waiting for port forward"
 done
 echo "Access central on https://localhost:${port}"
