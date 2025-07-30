5da0c12f52294bad21474b0d09cc1b55689c8bed
Stephan Heßelmann <shesselm@redhat.com>
2025-07-30 11:32:39 +0200

    fix compilation

--- central/image/service/service_impl.go
+++ central/image/service/service_impl.go
@@ -493,7 +493,7 @@ func (s *serviceImpl) GetImageVulnerabilitiesInternal(ctx context.Context, reque
 	}
 
 	comps := scannerTypes.NewScanComponents("", request.GetComponents(), nil)
-	_, err = s.enricher.EnrichWithVulnerabilities(img, comps, request.GetNotes())
+	_, err := s.enricher.EnrichWithVulnerabilities(img, comps, request.GetNotes())
 	if err != nil {
 		return nil, err
 	}
@@ -575,7 +575,7 @@ func (s *serviceImpl) EnrichLocalImageInternal(ctx context.Context, request *v1.
 	forceScanUpdate := true
 	// Always pull the image from the store if the ID != "" and rescan is not forced. Central will manage the reprocessing over the images.
 	if imgID != "" && !request.GetForce() {
-		existingImg, imgExists, err = s.datastore.GetImage(ctx, imgID)
+		existingImg, imgExists, err := s.datastore.GetImage(ctx, imgID)
 		if err != nil {
 			s.informScanWaiter(request.GetRequestId(), nil, err)
 			return nil, err
@@ -671,6 +671,7 @@ func (s *serviceImpl) EnrichLocalImageInternal(ctx context.Context, request *v1.
 		_ = s.saveImage(img)
 	}
 
+	var err error
 	if hasErrors && request.GetRequestId() != "" {
 		// Send an actual error to the waiter so that error handling can be done (ie: retry)
 		// Without this a bare image is returned that will have notes such as MISSING_METADATA
