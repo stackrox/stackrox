1ab9f6ca50cfa56365d1bbac060d7dc98d890592
AJ Heflin <aheflin@redhat.com>
2025-07-30 09:49:36 -0400

    Change to use mapper datastore

--- central/cve/matcher/singleton.go
+++ central/cve/matcher/singleton.go
@@ -2,7 +2,7 @@ package matcher
 
 import (
 	clusterDataStore "github.com/stackrox/rox/central/cluster/datastore"
-	imageDataStore "github.com/stackrox/rox/central/image/datastore"
+	mapperStore "github.com/stackrox/rox/central/imagev2/mapper/datastore"
 	nsDataStore "github.com/stackrox/rox/central/namespace/datastore"
 	"github.com/stackrox/rox/pkg/sync"
 	"github.com/stackrox/rox/pkg/utils"
@@ -15,7 +15,7 @@ var (
 
 func initialize() {
 	var err error
-	cveMatcher, err = NewCVEMatcher(clusterDataStore.Singleton(), nsDataStore.Singleton(), imageDataStore.Singleton())
+	cveMatcher, err = NewCVEMatcher(clusterDataStore.Singleton(), nsDataStore.Singleton(), mapperStore.Singleton())
 	utils.CrashOnError(err)
 }
 
