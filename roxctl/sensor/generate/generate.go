4f4e70b1c5bd8851a48de6ad90f8a0f980be7423
Khushboo Sancheti <ks@stackrox.com>
2025-07-30 11:14:58 +0530

    ROX-24956: Set default adm controller timeout value to 0 in roxctl

--- roxctl/sensor/generate/generate.go
+++ roxctl/sensor/generate/generate.go
@@ -212,7 +212,7 @@ func Command(cliEnvironment environment.Environment) *cobra.Command {
 	// in multiple places (the Helm chart defaults, on central side, within roxctl). It might be a better approach to have roxctl not propagate a default
 	// timeout to central, allowing central to inject a default timeout. This could, in principle, be achieved by having a default timeout of 0 here. But, due to
 	// the bug described in ROX-24956, this is currently not testable. Hence, we should pick this up again when that ticket has been taken care of.
-	c.PersistentFlags().Int32Var(&ac.TimeoutSeconds, "admission-controller-timeout", 3, "Timeout in seconds for the admission controller.")
+	c.PersistentFlags().Int32Var(&ac.TimeoutSeconds, "admission-controller-timeout", 0, "Timeout in seconds for the admission controller.")
 	utils.Must(c.PersistentFlags().MarkDeprecated("admission-controller-timeout", warningAdmissionControllerTimeoutSet))
 
 	c.PersistentFlags().BoolVar(&ac.ScanInline, "admission-controller-scan-inline", true, "Get scans inline when using the admission controller.")
