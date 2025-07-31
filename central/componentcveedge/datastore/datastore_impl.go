21145a4cffd5d836fce07dedd4b977081d18ed3a
Dave Shrewsberry <dashrews@redhat.com>
2025-07-31 15:52:21 -0400

    remove remaining CVE Edge searchers

--- central/componentcveedge/datastore/datastore_impl.go
+++ central/componentcveedge/datastore/datastore_impl.go
@@ -3,7 +3,7 @@ package datastore
 import (
 	"context"
 
-	"github.com/stackrox/rox/central/componentcveedge/search"
+	"github.com/pkg/errors"
 	"github.com/stackrox/rox/central/componentcveedge/store"
 	v1 "github.com/stackrox/rox/generated/api/v1"
 	"github.com/stackrox/rox/generated/storage"
@@ -11,28 +11,43 @@ import (
 )
 
 type datastoreImpl struct {
-	storage  store.Store
-	searcher search.Searcher
+	storage store.Store
 }
 
 func (ds *datastoreImpl) Search(ctx context.Context, q *v1.Query) ([]searchPkg.Result, error) {
-	return ds.searcher.Search(ctx, q)
+	return ds.storage.Search(ctx, q)
 }
 
 func (ds *datastoreImpl) SearchEdges(ctx context.Context, q *v1.Query) ([]*v1.SearchResult, error) {
-	return ds.searcher.SearchEdges(ctx, q)
-}
-
-func (ds *datastoreImpl) SearchRawEdges(ctx context.Context, q *v1.Query) ([]*storage.ComponentCVEEdge, error) {
-	imgs, err := ds.searcher.SearchRawEdges(ctx, q)
+	// TODO(ROX-29943): remove 2 pass database queries\
+	results, err := ds.Search(ctx, q)
 	if err != nil {
 		return nil, err
 	}
-	return imgs, nil
+
+	cves, missingIndices, err := ds.storage.GetMany(ctx, searchPkg.ResultsToIDs(results))
+	if err != nil {
+		return nil, err
+	}
+	results = searchPkg.RemoveMissingResults(results, missingIndices)
+	return convertMany(cves, results)
+}
+
+func (ds *datastoreImpl) SearchRawEdges(ctx context.Context, q *v1.Query) ([]*storage.ComponentCVEEdge, error) {
+	var edges []*storage.ComponentCVEEdge
+	err := ds.storage.WalkByQuery(ctx, q, func(edge *storage.ComponentCVEEdge) error {
+		edges = append(edges, edge)
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return edges, nil
 }
 
 func (ds *datastoreImpl) Count(ctx context.Context, q *v1.Query) (int, error) {
-	return ds.searcher.Count(ctx, q)
+	return ds.storage.Count(ctx, q)
 }
 
 func (ds *datastoreImpl) Get(ctx context.Context, id string) (*storage.ComponentCVEEdge, bool, error) {
@@ -49,4 +64,26 @@ func (ds *datastoreImpl) Exists(ctx context.Context, id string) (bool, error) {
 		return false, err
 	}
 	return true, nil
+}
+
+func convertMany(cves []*storage.ComponentCVEEdge, results []searchPkg.Result) ([]*v1.SearchResult, error) {
+	if len(cves) != len(results) {
+		return nil, errors.Errorf("expected %d CVEs but got %d", len(results), len(cves))
+	}
+
+	outputResults := make([]*v1.SearchResult, len(cves))
+	for index, sar := range cves {
+		outputResults[index] = convertOne(sar, &results[index])
+	}
+	return outputResults, nil
+}
+
+func convertOne(obj *storage.ComponentCVEEdge, result *searchPkg.Result) *v1.SearchResult {
+	return &v1.SearchResult{
+		Category:       v1.SearchCategory_COMPONENT_VULN_EDGE,
+		Id:             obj.GetId(),
+		Name:           obj.GetId(),
+		FieldToMatches: searchPkg.GetProtoMatchesMap(result.Matches),
+		Score:          result.Score,
+	}
 }
