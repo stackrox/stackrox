685b795759f4479d0d47b1577aae6a91974fe374
Tomasz Janiszewski <tomek@redhat.com>
2025-07-31 13:14:45 +0200

    fix: improve sensor component identification (#16179)
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>
    Co-authored-by: Piotr Rygielski <114479+vikin91@users.noreply.github.com>

--- sensor/common/compliance/auditlog_manager_impl.go
+++ sensor/common/compliance/auditlog_manager_impl.go
@@ -1,7 +1,6 @@
 package compliance
 
 import (
-	"context"
 	"time"
 
 	"github.com/stackrox/rox/generated/internalapi/central"
@@ -13,6 +12,7 @@ import (
 	"github.com/stackrox/rox/pkg/sync"
 	"github.com/stackrox/rox/sensor/common"
 	"github.com/stackrox/rox/sensor/common/message"
+	"github.com/stackrox/rox/sensor/common/unimplemented"
 )
 
 const (
@@ -20,7 +20,12 @@ const (
 )
 
 // auditLogCollectionManagerImpl manages the lifecycle of audit log collection within the cluster
+// This component doesn't actually process or handle any messages sent to Sensor. It uses the sensor component
+// so that the lifecycle (start, stop) can be handled when Sensor starts up. The actual messages from central to
+// enable/disable audit log collection is handled as part of the dynamic config in config.Handler which then calls
+// the specific APIs in this manager.
 type auditLogCollectionManagerImpl struct {
+	unimplemented.Receiver
 	clusterIDGetter func() string
 
 	enabled                         concurrency.Flag
@@ -71,14 +76,6 @@ func (a *auditLogCollectionManagerImpl) Notify(e common.SensorComponentEvent) {
 
 func (a *auditLogCollectionManagerImpl) Capabilities() []centralsensor.SensorCapability {
 	return []centralsensor.SensorCapability{centralsensor.AuditLogEventsCap}
-}
-
-func (a *auditLogCollectionManagerImpl) ProcessMessage(_ context.Context, _ *central.MsgToSensor) error {
-	// This component doesn't actually process or handle any messages sent to Sensor. It uses the sensor component
-	// so that the lifecycle (start, stop) can be handled when Sensor starts up. The actual messages from central to
-	// enable/disable audit log collection is handled as part of the dynamic config in config.Handler which then calls
-	// the specific APIs in this manager.
-	return nil
 }
 
 func (a *auditLogCollectionManagerImpl) ResponsesC() <-chan *message.ExpiringMessage {
