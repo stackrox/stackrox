91b1ef80b24fad217baa36f070da5c168a10119d
Moritz Clasmeier <mclasmeier@redhat.com>
2025-07-31 10:02:31 +0200

    Remove unused imports

--- qa-tests-backend/src/test/groovy/ClusterInitBundleTest.groovy
+++ qa-tests-backend/src/test/groovy/ClusterInitBundleTest.groovy
@@ -4,10 +4,8 @@ import io.stackrox.proto.api.v1.ApiTokenService
 
 import services.BaseService
 import services.ClusterInitBundleService
-import services.ClusterService
 import util.Env
 
-import org.junit.Assume
 import spock.lang.IgnoreIf
 import spock.lang.Shared
 import spock.lang.Tag
