40bfd9d98a51d4e4684c3eedca68b746fd8df02d
Moritz Clasmeier <mclasmeier@redhat.com>
2025-08-01 12:08:02 +0200

    Reverted most changes to the main test suite

--- pkg/helm/charts/tests/securedclusterservices/helmtest_test.go
+++ pkg/helm/charts/tests/securedclusterservices/helmtest_test.go
@@ -4,25 +4,9 @@ import (
 	"testing"
 
 	"github.com/stackrox/rox/image"
-	"github.com/stackrox/rox/pkg/helm/charts"
 	helmChartTestUtils "github.com/stackrox/rox/pkg/helm/charts/testutils"
 )
 
-func enableDesiredFeatureFlags(metaVals *charts.MetaValues) {
-	featureFlagsToEnable := []string{
-		"ROX_ADMISSION_CONTROLLER_CONFIG",
-	}
-	if metaVals.FeatureFlags == nil {
-		metaVals.FeatureFlags = make(map[string]interface{})
-	}
-	for _, featureFlag := range featureFlagsToEnable {
-		metaVals.FeatureFlags[featureFlag] = true
-	}
-}
-
 func TestWithHelmtest(t *testing.T) {
-	testSuiteOpts := helmChartTestUtils.RunHelmTestSuiteOpts{
-		MetaValuesOverridesFunc: enableDesiredFeatureFlags,
-	}
-	helmChartTestUtils.RunHelmTestSuite(t, "testdata/helmtest", image.SecuredClusterServicesChartPrefix, testSuiteOpts)
+	helmChartTestUtils.RunHelmTestSuite(t, "testdata/helmtest", image.SecuredClusterServicesChartPrefix, helmChartTestUtils.RunHelmTestSuiteOpts{})
 }
