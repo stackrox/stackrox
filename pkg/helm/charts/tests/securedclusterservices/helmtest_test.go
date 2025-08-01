8ee5bf7b3caf079b98452fe048f56b909d9882ef
Moritz Clasmeier <mclasmeier@redhat.com>
2025-08-01 11:09:54 +0200

    Enable FeatureFlag in test suite

--- pkg/helm/charts/tests/securedclusterservices/helmtest_test.go
+++ pkg/helm/charts/tests/securedclusterservices/helmtest_test.go
@@ -4,9 +4,25 @@ import (
 	"testing"
 
 	"github.com/stackrox/rox/image"
+	"github.com/stackrox/rox/pkg/helm/charts"
 	helmChartTestUtils "github.com/stackrox/rox/pkg/helm/charts/testutils"
 )
 
+func enableDesiredFeatureFlags(metaVals *charts.MetaValues) {
+	featureFlagsToEnable := []string{
+		"ROX_ADMISSION_CONTROLLER_CONFIG",
+	}
+	if metaVals.FeatureFlags == nil {
+		metaVals.FeatureFlags = make(map[string]interface{})
+	}
+	for _, featureFlag := range featureFlagsToEnable {
+		metaVals.FeatureFlags[featureFlag] = true
+	}
+}
+
 func TestWithHelmtest(t *testing.T) {
-	helmChartTestUtils.RunHelmTestSuite(t, "testdata/helmtest", image.SecuredClusterServicesChartPrefix, helmChartTestUtils.RunHelmTestSuiteOpts{})
+	testSuiteOpts := helmChartTestUtils.RunHelmTestSuiteOpts{
+		MetaValuesOverridesFunc: enableDesiredFeatureFlags,
+	}
+	helmChartTestUtils.RunHelmTestSuite(t, "testdata/helmtest", image.SecuredClusterServicesChartPrefix, testSuiteOpts)
 }
