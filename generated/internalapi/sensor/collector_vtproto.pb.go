ef549357d4574da5c3f601a42c5ca773771d6f87
Giles Hutton <ghutton@redhat.com>
2025-08-04 14:52:00 +0100

    Adds file system storage proto

--- generated/internalapi/sensor/collector_vtproto.pb.go
+++ generated/internalapi/sensor/collector_vtproto.pb.go
@@ -127,6 +127,7 @@ func (m *ProcessSignal) CloneVT() *ProcessSignal {
 	r.Gid = m.Gid
 	r.Scraped = m.Scraped
 	r.LoginUid = m.LoginUid
+	r.Username = m.Username
 	if rhs := m.LineageInfo; rhs != nil {
 		tmpContainer := make([]*ProcessSignal_LineageInfo, len(rhs))
 		for k, v := range rhs {
@@ -306,6 +307,9 @@ func (this *ProcessSignal) EqualVT(that *ProcessSignal) bool {
 		}
 	}
 	if this.LoginUid != that.LoginUid {
+		return false
+	}
+	if this.Username != that.Username {
 		return false
 	}
 	return string(this.unknownFields) == string(that.unknownFields)
@@ -574,6 +578,13 @@ func (m *ProcessSignal) MarshalToSizedBufferVT(dAtA []byte) (int, error) {
 		i -= len(m.unknownFields)
 		copy(dAtA[i:], m.unknownFields)
 	}
+	if len(m.Username) > 0 {
+		i -= len(m.Username)
+		copy(dAtA[i:], m.Username)
+		i = protohelpers.EncodeVarint(dAtA, i, uint64(len(m.Username)))
+		i--
+		dAtA[i] = 0x6a
+	}
 	if m.LoginUid != 0 {
 		i = protohelpers.EncodeVarint(dAtA, i, uint64(m.LoginUid))
 		i--
@@ -796,6 +807,10 @@ func (m *ProcessSignal) SizeVT() (n int) {
 	}
 	if m.LoginUid != 0 {
 		n += 1 + protohelpers.SizeOfVarint(uint64(m.LoginUid))
+	}
+	l = len(m.Username)
+	if l > 0 {
+		n += 1 + l + protohelpers.SizeOfVarint(uint64(l))
 	}
 	n += len(m.unknownFields)
 	return n
@@ -1655,6 +1670,38 @@ func (m *ProcessSignal) UnmarshalVT(dAtA []byte) error {
 					break
 				}
 			}
+		case 13:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Username", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return protohelpers.ErrIntOverflow
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return protohelpers.ErrInvalidLength
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return protohelpers.ErrInvalidLength
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Username = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
 			skippy, err := protohelpers.Skip(dAtA[iNdEx:])
@@ -2563,6 +2610,42 @@ func (m *ProcessSignal) UnmarshalVTUnsafe(dAtA []byte) error {
 					break
 				}
 			}
+		case 13:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Username", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return protohelpers.ErrIntOverflow
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return protohelpers.ErrInvalidLength
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return protohelpers.ErrInvalidLength
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			var stringValue string
+			if intStringLen > 0 {
+				stringValue = unsafe.String(&dAtA[iNdEx], intStringLen)
+			}
+			m.Username = stringValue
+			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
 			skippy, err := protohelpers.Skip(dAtA[iNdEx:])
