aaf96c3e52a4c2fcabc419153d926dbbd5b04063
Stephan Heßelmann <shesselm@redhat.com>
2025-08-05 15:16:42 +0200

    add virtual machine metrics

---
+++ sensor/common/virtualmachine/metrics/metrics.go
@@ -0,0 +1,41 @@
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/stackrox/rox/pkg/metrics"
+)
+
+var (
+	StatusCentralNotReadyLabels = prometheus.Labels{"status": "central not ready"}
+	StatusErrorLabels           = prometheus.Labels{"status": "error"}
+	StatusSuccessLabels         = prometheus.Labels{"status": "success"}
+	StatusTimeoutLabels         = prometheus.Labels{"status": "timeout"}
+)
+
+// VirtualMachineReceived is a metric meant to replace and provide extra context on
+var VirtualMachineReceived = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Namespace: metrics.PrometheusNamespace,
+		Subsystem: metrics.SensorSubsystem.String(),
+		Name:      "virtual_machines_received_total",
+		Help:      "Total number of virtual machines received by this Sensor",
+	},
+)
+
+// VirtualMachineSent is a metric meant to replace and provide extra context on
+var VirtualMachineSent = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Namespace: metrics.PrometheusNamespace,
+		Subsystem: metrics.SensorSubsystem.String(),
+		Name:      "virtual_machines_sent_total",
+		Help:      "Total number of virtual machines sent by this Sensor",
+	},
+	[]string{"status"},
+)
+
+func init() {
+	prometheus.MustRegister(
+		VirtualMachineReceived,
+		VirtualMachineSent,
+	)
+}
