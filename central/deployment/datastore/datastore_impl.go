d3b3118ffce4add7d429a5a75f5a8587959db200
Khushboo Sancheti <42253461+clickboo@users.noreply.github.com>
2025-08-07 18:27:28 +0530

    ROX-30413: Optimize deployment ranking (#16269)

--- central/deployment/datastore/datastore_impl.go
+++ central/deployment/datastore/datastore_impl.go
@@ -85,25 +85,12 @@ func (ds *datastoreImpl) initializeRanker() {
 		sac.AllowFixedScopes(
 			sac.AccessModeScopeKeys(storage.Access_READ_ACCESS), sac.ResourceScopeKeys(resources.Deployment)))
 
-	results, err := ds.Search(readCtx, pkgSearch.EmptyQuery())
-	if err != nil {
-		log.Error(err)
-		return
-	}
-
 	clusterScores := make(map[string]float32)
 	nsScores := make(map[string]float32)
-	for _, id := range pkgSearch.ResultsToIDs(results) {
-		deployment, found, err := ds.deploymentStore.Get(readCtx, id)
-		if err != nil {
-			log.Error(err)
-			continue
-		} else if !found {
-			continue
-		}
-
+	// The store search function does not use select fields, only views do. Hence empty query is used in the walk below
+	err := ds.deploymentStore.WalkByQuery(readCtx, pkgSearch.EmptyQuery(), func(deployment *storage.Deployment) error {
 		riskScore := deployment.GetRiskScore()
-		ds.deploymentRanker.Add(id, deployment.GetRiskScore())
+		ds.deploymentRanker.Add(deployment.GetId(), riskScore)
 
 		// TODO: ROX-6235: account for nodes in cluster risk
 		// aggregate deployment risk scores to get cluster risk score
@@ -111,6 +98,12 @@ func (ds *datastoreImpl) initializeRanker() {
 
 		// aggregate deployment risk scores to obtain namespace risk score
 		nsScores[deployment.GetNamespaceId()] += riskScore
+
+		return nil
+	})
+	if err != nil {
+		log.Errorf("unable to initialize deployment ranking: %v", err)
+		return
 	}
 
 	if ds.nsRanker != nil {
