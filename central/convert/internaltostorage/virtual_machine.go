7740f7017aeeb12fd45f7288dcbc8c54ef492722
Stephan Heßelmann <shesselm@redhat.com>
2025-08-08 12:47:55 +0200

    add component list to protos

--- central/convert/internaltostorage/virtual_machine.go
+++ central/convert/internaltostorage/virtual_machine.go
@@ -5,14 +5,40 @@ import (
 	"github.com/stackrox/rox/generated/storage"
 )
 
-// ComplianceOperatorSuite converts message from sensor to storage message
-func VirtualMachine(sensorData *central.VirtualMachine) *storage.VirtualMachine {
+func convertSource(val central.SourceType) storage.SourceType {
+	return storage.SourceType(central.SourceType_value[val.String()])
+}
+
+func convertComponents(components []*central.VirtualMachineComponent) []*storage.EmbeddedImageScanComponent {
+	converted := []*storage.EmbeddedImageScanComponent{}
+	for _, c := range components {
+		converted = append(converted, &storage.EmbeddedImageScanComponent{
+			Architecture: c.GetArchitecture(),
+			License: &storage.License{
+				Name: c.GetLicense().GetName(),
+				Type: c.GetLicense().GetType(),
+				Url:  c.GetLicense().GetUrl(),
+			},
+			Location: c.GetLocation(),
+			Name:     c.GetName(),
+			Source:   convertSource(c.GetSource()),
+			Version:  c.GetVersion(),
+		})
+	}
+	return converted
+}
+
+// VirtualMachine converts an internalapi Central message to a storage message.
+func VirtualMachine(virtualMachine *central.VirtualMachine) *storage.VirtualMachine {
 	return &storage.VirtualMachine{
-		ClusterId:   sensorData.GetClusterId(),
-		ClusterName: sensorData.GetClusterName(),
-		Facts:       sensorData.GetFacts(),
-		Id:          sensorData.GetId(),
-		Name:        sensorData.GetName(),
-		Namespace:   sensorData.GetNamespace(),
+		ClusterId:   virtualMachine.GetClusterId(),
+		ClusterName: virtualMachine.GetClusterName(),
+		Facts:       virtualMachine.GetFacts(),
+		Id:          virtualMachine.GetId(),
+		Name:        virtualMachine.GetName(),
+		Namespace:   virtualMachine.GetNamespace(),
+		Scan: &storage.VirtualMachineScan{
+			Components: convertComponents(virtualMachine.GetComponents()),
+		},
 	}
 }
