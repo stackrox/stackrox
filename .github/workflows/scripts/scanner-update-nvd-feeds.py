59279890a630ae793e9a7bc388faf7bcaa3d84a4
Ross Tannenbaum <rtannenb@redhat.com>
2025-08-12 09:59:07 -0700

    ROX-28353: use NVD 2.0 JSON feeds (#16257)

--- .github/workflows/scripts/scanner-update-nvd-feeds.py
+++ .github/workflows/scripts/scanner-update-nvd-feeds.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 
 """
-Fetch NVD data from the JSON 1.1 feed archives, parse them into API 2.0
-schema and save into files.
+Fetch NVD data from the JSON 2.0 feed archives and save into files.
 """
 
 import gzip
@@ -17,70 +16,23 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s: %(levelname)s: %(name)s %(message)s")
 
+DEFAULT_BASE_URL = "https://nvd.nist.gov/feeds/json/cve/2.0/"
 
-def convert_cve_feed_to_api(cve_feed):
-    """Convert NVDCVEFeedJSON10DefCVEItem to CVEAPIJSON20DefCVEItem."""
+class FeedLoader:
 
-    # Required fields.
+    log = logging.getLogger("FeedLoader")
 
-    cve_api = {"id": cve_feed['cve']['CVE_data_meta']['ID'],
-               "published": cve_feed['publishedDate'],
-               "lastModified": cve_feed['lastModifiedDate'],
-               "descriptions": [],
-               "metrics": {}}
-
-    # Description.
-
-    for desc in cve_feed['cve'].get('description', {}).get('description_data', []):
-        cve_api["descriptions"].append({"lang": desc["lang"],
-                                        "value": desc["value"]})
-
-    # Metrics.
-
-    impact = cve_feed.get('impact', {})
-    cvssMetricV2 = impact.get('baseMetricV2', {}).get('cvssV2', {})
-    cvssMetricV3 = impact.get('baseMetricV3', {}).get('cvssV3', {})
-
-    if cvssMetricV2:
-        key = "cvssMetricV2"
-        metric = {"type": "Primary",
-                  "cvssData": {
-                      "version": "2.0",
-                      "vectorString": cvssMetricV2["vectorString"],
-                      "baseScore": cvssMetricV2["baseScore"]}}
-        cve_api["metrics"][key] = [metric]
-
-    if cvssMetricV3:
-        version = cvssMetricV3["version"]
-        key = "cvssMetricV30" if version == "3.0" else "cvssMetricV31"
-        metric = {"type": "Primary",
-                  "cvssData": {
-                      "version": version,
-                      "vectorString": cvssMetricV3["vectorString"],
-                      "baseScore": cvssMetricV3["baseScore"]}}
-        cve_api["metrics"][key] = [metric]
-
-    return {"cve": cve_api}
-
-
-class LegacyLoader:
-
-    BASE_URL = "https://nvd.nist.gov/feeds/json/cve/1.1/"
-
-    log = logging.getLogger("LegacyLoader")
-
-    def __init__(self, base_url=None):
-        self._base_url = base_url or self.BASE_URL
+    def __init__(self, base_url):
+        self._base_url = base_url
 
     def fetch(self, year):
-        url = parse.urljoin(self._base_url, f"nvdcve-1.1-{year}.json.gz")
+        url = parse.urljoin(self._base_url, f"nvdcve-2.0-{year}.json.gz")
         self.log.info("fetching and decompressing: %s", url)
         with request.urlopen(url, timeout=60) as resp:
             with gzip.GzipFile(fileobj=resp) as gz:
-                cve_feed = json.load(gz)
-        items = cve_feed.get("CVE_Items", [])
-        self.log.info("cve count: %d", len(items))
-        yield from (convert_cve_feed_to_api(c) for c in items)
+                data = json.load(gz)
+        yield from (v for v in data["vulnerabilities"]
+        if v["cve"]["vulnStatus"].lower() != "rejected")
 
 
 def parse_args():
@@ -110,7 +62,8 @@ def parse_args():
         default=date.today().year)
     parser.add_argument(
         "--base-url",
-        help="URL for the NVD CVE feed (version 1.1).")
+        help="URL for the NVD CVE feed (version 2.0).",
+        default=DEFAULT_BASE_URL)
     parser.add_argument(
         "--file-pattern",
         help="File name pattern of the NVD feed files.",
@@ -120,7 +73,7 @@ def parse_args():
 
 
 def main(args):
-    loader = LegacyLoader(args.base_url)
+    loader = FeedLoader(args.base_url)
     for year in range(args.start_year, args.end_year + 1):
         path = os.path.join(args.output, args.file_pattern % {'year': year})
         logging.info("fetching '%d' to '%s'", year, path)
