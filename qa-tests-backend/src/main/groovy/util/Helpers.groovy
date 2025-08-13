9d8510de6b9d3b9ee70e734fa678270f33deb801
davdhacs <105243888+davdhacs@users.noreply.github.com>
2025-08-12 22:49:18 -0600

    remove debug log of collect vars

--- qa-tests-backend/src/main/groovy/util/Helpers.groovy
+++ qa-tests-backend/src/main/groovy/util/Helpers.groovy
@@ -88,7 +88,6 @@ class Helpers {
 
     @CompileDynamic
     static void collectDebugForFailure(Throwable exception) {
-        log.info("collectDebugForFailure" + exception)
         if (!collectDebug()) {
             return
         }
@@ -183,10 +182,6 @@ class Helpers {
     }
 
     private static boolean collectDebug() {
-        log.warn("Debug collection skip check: "+
-                 "(CI: ${Env.IN_CI} ||"+
-                 " GATHER_QA_TEST_DEBUG_LOGS: ${Env.GATHER_QA_TEST_DEBUG_LOGS}) && "+
-                 " QA_TEST_DEBUG_LOGS: ${Env.QA_TEST_DEBUG_LOGS} != ''")
         if ((Env.IN_CI || Env.GATHER_QA_TEST_DEBUG_LOGS) && (Env.QA_TEST_DEBUG_LOGS != "")) {
             return true
         }
