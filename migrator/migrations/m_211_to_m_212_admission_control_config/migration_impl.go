ce92ca1fe815dc4ce5fb6fa2eec8e4969e2c687e
Khushboo Sancheti <ks@stackrox.com>
2025-08-14 23:50:45 +0530

    WIP migration

---
+++ migrator/migrations/m_211_to_m_212_admission_control_config/migration_impl.go
@@ -0,0 +1,118 @@
+package m211tom212
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/migrator/migrations/m_211_to_m_212_admission_control_config/schema"
+	"github.com/stackrox/rox/migrator/types"
+	"github.com/stackrox/rox/pkg/logging"
+	"github.com/stackrox/rox/pkg/postgres/pgutils"
+	"github.com/stackrox/rox/pkg/sac"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
+
+var (
+	batchSize = 2000
+	log       = logging.LoggerForModule()
+)
+
+// Perform cluster migration for admission controller config
+func migrate(database *types.Databases) error {
+	ctx := sac.WithAllAccess(context.Background())
+	db := database.GormDB
+	pgutils.CreateTableFromModel(ctx, db, schema.CreateTableClustersStmt)
+
+	return fixAdmissionControllerConfig(ctx, db)
+}
+
+func fixAdmissionControllerConfig(ctx context.Context, database *gorm.DB) error {
+	db := database.WithContext(ctx).Table(schema.ClustersTableName)
+	var updatedClusters []*schema.Clusters
+	var count int
+	err := db.Transaction(func(tx *gorm.DB) error {
+		rows, err := tx.Select("serialized").Rows()
+		if err != nil {
+			return errors.Wrapf(err, "failed to iterate table %s", schema.ClustersTableName)
+		}
+		for rows.Next() {
+			var obj schema.Clusters
+			if err = tx.ScanRows(rows, &obj); err != nil {
+				return errors.Wrap(err, "failed to scan rows")
+			}
+			proto, err := ConvertClusterToProto(&obj)
+			if err != nil {
+				return errors.Wrapf(err, "failed to convert %+v to proto", obj)
+			}
+
+			// For clusters deployed using manifest install only - Helm and operator managed cluster config is taken care of
+			// through the Helm values and operator manifest which is then communicated to Central after upgrade and after
+			// sensor connections to Central are formed
+			if proto.GetHelmConfig() == nil {
+				if proto.GetDynamicConfig() != nil && proto.GetDynamicConfig().GetAdmissionControllerConfig() != nil {
+					ac := proto.GetDynamicConfig().GetAdmissionControllerConfig()
+					ac.ScanInline = true
+					if ac.GetEnabled() || ac.GetEnforceOnUpdates() {
+						ac.Enabled = true
+						ac.EnforceOnUpdates = true
+					}
+				}
+
+				converted, err := ConvertClusterFromProto(proto)
+				if err != nil {
+					return errors.Wrapf(err, "failed to convert from proto %+v", proto)
+				}
+
+				updatedClusters = append(updatedClusters, converted)
+				count++
+			}
+		}
+		if rows.Err() != nil {
+			return errors.Wrapf(rows.Err(), "failed to get rows for %s", schema.ClustersTableName)
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	if len(updatedClusters) > 0 {
+		if err = db.
+			Clauses(clause.OnConflict{UpdateAll: true}).
+			Model(schema.CreateTableClustersStmt.GormModel).
+			CreateInBatches(&updatedClusters, batchSize).Error; err != nil {
+			return errors.Wrap(err, "failed to upsert all converted objects")
+		}
+	}
+	log.Infof("Fixed admission controller configuration for %d clusters", count)
+	return nil
+}
+
+// ConvertClusterToProto converts Gorm model `Clusters` to its protobuf type object
+func ConvertClusterToProto(m *schema.Clusters) (*storage.Cluster, error) {
+	var msg storage.Cluster
+	if err := msg.UnmarshalVTUnsafe(m.Serialized); err != nil {
+		return nil, err
+	}
+	return &msg, nil
+}
+
+// ConvertClusterFromProto converts a `*storage.Cluster` to Gorm model
+func ConvertClusterFromProto(obj *storage.Cluster) (*schema.Clusters, error) {
+	serialized, err := obj.MarshalVT()
+	if err != nil {
+		return nil, err
+	}
+	model := &schema.Clusters{
+		ID:                                obj.GetId(),
+		Name:                              obj.GetName(),
+		Type:                              obj.GetType(),
+		Labels:                            obj.GetLabels(),
+		StatusProviderMetadataClusterType: obj.GetStatus().GetProviderMetadata().GetCluster().GetType(),
+		StatusOrchestratorMetadataVersion: obj.GetStatus().GetOrchestratorMetadata().GetVersion(),
+		Serialized:                        serialized,
+	}
+	return model, nil
+}
---
+++ migrator/migrations/m_211_to_m_212_admission_control_config/migration_test.go
@@ -0,0 +1,115 @@
+//go:build sql_integration
+
+package m211tom212
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/migrator/migrations/m_211_to_m_212_admission_control_config/schema"
+	"github.com/stackrox/rox/migrator/migrations/m_211_to_m_212_admission_control_config/store"
+	pghelper "github.com/stackrox/rox/migrator/migrations/postgreshelper"
+	"github.com/stackrox/rox/migrator/types"
+	"github.com/stackrox/rox/pkg/postgres/pgutils"
+	"github.com/stackrox/rox/pkg/sac"
+	"github.com/stackrox/rox/pkg/search"
+	"github.com/stackrox/rox/pkg/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type migrationTestSuite struct {
+	suite.Suite
+
+	db  *pghelper.TestPostgres
+	ctx context.Context
+}
+
+func TestMigration(t *testing.T) {
+	suite.Run(t, new(migrationTestSuite))
+}
+
+func (s *migrationTestSuite) SetupSuite() {
+	s.ctx = sac.WithAllAccess(context.Background())
+	s.db = pghelper.ForT(s.T(), false)
+
+	pgutils.CreateTableFromModel(s.ctx, s.db.GetGormDB(), schema.CreateTableClustersStmt)
+}
+
+func (s *migrationTestSuite) TestMigration() {
+	clusters := []*storage.Cluster{
+		s.getTestCluster("helm-cluster", true),
+		s.getTestCluster("manifest-install-cluster", false),
+	}
+
+	store := store.New(s.db)
+	require.NoError(s.T(), store.UpsertMany(s.ctx, clusters))
+
+	dbs := &types.Databases{
+		GormDB:     s.db.GetGormDB(),
+		PostgresDB: s.db.DB,
+		DBCtx:      s.ctx,
+	}
+
+	s.Require().NoError(migration.Run(dbs))
+
+	result, err := store.GetByQuery(s.ctx, search.EmptyQuery())
+	assert.NoError(s.T(), err)
+	assert.ElementsMatch(s.T(), collectIDs(clusters...), collectIDs(result...))
+
+	result, err = store.GetByQuery(s.ctx,
+		search.NewQueryBuilder().AddExactMatches(search.Cluster, clusters[0].Name).ProtoQuery())
+	assert.NoError(s.T(), err)
+
+	assert.EqualValues(s.T(), result[0].HelmConfig.DynamicConfig.AdmissionControllerConfig.ScanInline, false)
+	assert.EqualValues(s.T(), result[0].HelmConfig.DynamicConfig.AdmissionControllerConfig.Enabled, true)
+	assert.EqualValues(s.T(), result[0].HelmConfig.DynamicConfig.AdmissionControllerConfig.EnforceOnUpdates, false)
+
+	result, err = store.GetByQuery(s.ctx,
+		search.NewQueryBuilder().AddExactMatches(search.Cluster, clusters[1].Name).ProtoQuery())
+	assert.NoError(s.T(), err)
+
+	assert.EqualValues(s.T(), result[0].DynamicConfig.AdmissionControllerConfig.ScanInline, true)
+	assert.EqualValues(s.T(), result[0].DynamicConfig.AdmissionControllerConfig.Enabled, true)
+	assert.EqualValues(s.T(), result[0].DynamicConfig.AdmissionControllerConfig.EnforceOnUpdates, true)
+
+}
+
+func collectIDs(objs ...*storage.Cluster) []string {
+	var ids []string
+	for _, obj := range objs {
+		ids = append(ids, obj.GetId())
+	}
+	return ids
+}
+
+func (s *migrationTestSuite) getTestCluster(name string, helmManaged bool) *storage.Cluster {
+	c := &storage.Cluster{
+		Id:        uuid.NewV4().String(),
+		Name:      name,
+		Type:      storage.ClusterType_OPENSHIFT_CLUSTER,
+		MainImage: "quay.io/stackrox-io/main",
+	}
+	acc := &storage.AdmissionControllerConfig{
+		Enabled:          true,
+		TimeoutSeconds:   0,
+		ScanInline:       false,
+		DisableBypass:    false,
+		EnforceOnUpdates: false,
+	}
+
+	if helmManaged {
+		c.HelmConfig = &storage.CompleteClusterConfig{
+			DynamicConfig: &storage.DynamicClusterConfig{
+				AdmissionControllerConfig: acc,
+			},
+		}
+	} else {
+		c.DynamicConfig = &storage.DynamicClusterConfig{
+			AdmissionControllerConfig: acc,
+		}
+	}
+	return c
+}
