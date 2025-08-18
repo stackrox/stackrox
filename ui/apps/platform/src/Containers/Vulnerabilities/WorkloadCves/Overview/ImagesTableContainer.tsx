875188939c26a34f2a5c4f5849ed6d7bafe97422
David Vail <dcvail17@gmail.com>
2025-08-18 08:42:53 -0400

    Update column mgmt for Image overview table

--- ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/Overview/ImagesTableContainer.tsx
+++ ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/Overview/ImagesTableContainer.tsx
@@ -4,11 +4,12 @@ import { Divider, ToolbarItem } from '@patternfly/react-core';
 
 import useURLSort from 'hooks/useURLSort';
 import useURLPagination from 'hooks/useURLPagination';
+import usePermissions from 'hooks/usePermissions';
 
 import { getTableUIState } from 'utils/getTableUIState';
 import { getPaginationParams } from 'utils/searchUtils';
 import { SearchFilter } from 'types/search';
-import { useManagedColumns } from 'hooks/useManagedColumns';
+import { overrideManagedColumns, useManagedColumns } from 'hooks/useManagedColumns';
 import ColumnManagementButton from 'Components/ColumnManagementButton';
 import ImageOverviewTable, {
     Image,
@@ -72,7 +73,18 @@ function ImagesTableContainer({
         searchFilter,
     });
 
-    const managedColumns = useManagedColumns(tableId, defaultColumns);
+    const managedColumnState = useManagedColumns(tableId, defaultColumns);
+
+    const { hasReadWriteAccess } = usePermissions();
+    const hasWriteAccessForImage = hasReadWriteAccess('Image'); // SBOM Generation mutates image scan state.
+    const hasActionColumn = hasWriteAccessForWatchedImage || hasWriteAccessForImage;
+
+    const columnConfig = overrideManagedColumns(managedColumnState.columns, {
+        cvesBySeverity: showCveDetailFields ? {} : { isUntoggleAble: true, isShown: false },
+        rowActions: {
+            isShown: hasActionColumn,
+        },
+    });
 
     return (
         <>
@@ -84,7 +96,10 @@ function ImagesTableContainer({
                 isFiltered={isFiltered}
             >
                 <ToolbarItem align={{ default: 'alignRight' }}>
-                    <ColumnManagementButton managedColumnState={managedColumns} />
+                    <ColumnManagementButton
+                        columnConfig={columnConfig}
+                        onApplyColumns={managedColumnState.setVisibility}
+                    />
                 </ToolbarItem>
             </TableEntityToolbar>
             <Divider component="div" />
@@ -101,12 +116,11 @@ function ImagesTableContainer({
                     hasWriteAccessForWatchedImage={hasWriteAccessForWatchedImage}
                     onWatchImage={onWatchImage}
                     onUnwatchImage={onUnwatchImage}
-                    showCveDetailFields={showCveDetailFields}
                     onClearFilters={() => {
                         onFilterChange({});
                         pagination.setPage(1);
                     }}
-                    columnVisibilityState={managedColumns.columns}
+                    columnVisibilityState={columnConfig}
                 />
             </div>
         </>
