9378b3d5f28634295b5026ad823f00f4a96c04f9
bradr5 <braroger@redhat.com>
2025-08-18 14:55:42 -0500

    ROX-30524: Virtual machines overview header and description

--- ui/apps/platform/src/Containers/Vulnerabilities/VirtualMachineCves/Overview/VirtualMachineCvesOverviewPage.tsx
+++ ui/apps/platform/src/Containers/Vulnerabilities/VirtualMachineCves/Overview/VirtualMachineCvesOverviewPage.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Flex, PageSection, Text, Title } from '@patternfly/react-core';
+
+import PageTitle from 'Components/PageTitle';
 
 function VirtualMachineCvesOverviewPage() {
-    return <>Virtual Machine CVEs</>;
+    return (
+        <>
+            <PageTitle title="Virtual Machine CVEs Overview" />
+            <PageSection component="div" variant="light">
+                <Flex direction={{ default: 'column' }}>
+                    <Title headingLevel="h1">Virtual Machine Vulnerabilities</Title>
+                    <Text>Prioritize and remediate observed CVEs across virtual machines</Text>
+                </Flex>
+            </PageSection>
+        </>
+    );
 }
 
 export default VirtualMachineCvesOverviewPage;
