18cfdfa855bf43594f5cf6164fed7d929df17554
davdhacs <105243888+davdhacs@users.noreply.github.com>
2025-08-18 08:13:11 -0600

    add integration-ocp path and allow internet access

--- ui/apps/platform/cypress.config.js
+++ ui/apps/platform/cypress.config.js
@@ -7,7 +7,8 @@
  */
 
 module.exports = {
-    blockHosts: ['*.*'], // Browser options
+    // TODO: can we allow the ocp console but block other internet access? do we need to?
+    // blockHosts: ['*.*'], // Browser options
     chromeWebSecurity: false, // Browser options
     defaultCommandTimeout: 8000, // Timeouts options
     numTestsKeptInMemory: 0, // Global options
@@ -25,7 +26,7 @@ module.exports = {
 
     e2e: {
         baseUrl: 'https://localhost:3000',
-        specPattern: 'cypress/integration/**/*.test.{js,ts}',
+        specPattern: 'cypress/integration*/**/*.test.{js,ts}',
         viewportHeight: 850, // Viewport options
         viewportWidth: 1440, // Viewport options
         setupNodeEvents: (on) => {
