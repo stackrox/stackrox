ec8b6c171433499dfb53183a40819409b32a1302
davdhacs <105243888+davdhacs@users.noreply.github.com>
2025-08-19 22:49:57 -0600

    remove ui test debug

--- ui/apps/platform/cypress.config.js
+++ ui/apps/platform/cypress.config.js
@@ -7,7 +7,7 @@
  */
 
 module.exports = {
-    // blockHosts: ['*.*'], // Browser options
+    blockHosts: ['*.*'], // Browser options
     chromeWebSecurity: false, // Browser options
     defaultCommandTimeout: 8000, // Timeouts options
     numTestsKeptInMemory: 0, // Global options
@@ -25,7 +25,7 @@ module.exports = {
 
     e2e: {
         baseUrl: 'https://localhost:3000',
-        specPattern: 'cypress/integration*/**/*.test.{js,ts}',
+        specPattern: 'cypress/integration/**/*.test.{js,ts}',
         viewportHeight: 850, // Viewport options
         viewportWidth: 1440, // Viewport options
         setupNodeEvents: (on) => {
