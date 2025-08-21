4634cd1889e75eb91d41c7eb6c4dabc50510a29a
Michaël Petrov <github@parameta.lol>
2025-08-21 10:41:03 +0200

    fix(phonehome): release tests

--- pkg/telemetry/phonehome/client_release_test.go
+++ pkg/telemetry/phonehome/client_release_test.go
@@ -26,28 +26,33 @@ func TestNewClient_release(t *testing.T) {
 	}))
 	defer server.Close()
 
-	newTestClient := func(t *testing.T, key string) *Client {
-		return NewClient(
-			WithConnectionConfiguration("", key, server.URL),
+	t.Run("no key", func(t *testing.T) {
+		// This is a self-managed installation case.
+		c := NewClient("test", "Test", "0.0.0",
+			WithStorageKey(""),
+			WithEndpoint(server.URL),
+			WithConfigURL(server.URL),
 			WithConfigureCallback(func(rc *RuntimeConfig) {
 				t.Logf("reconfigured with %v", rc)
 			}),
 		)
-	}
-
-	t.Run("no key", func(t *testing.T) {
-		// This is a self-managed installation case.
-		c := newTestClient(t, "")
-		assert.True(t, c.IsActive())
-		assert.False(t, c.IsEnabled())
+		assert.True(t, c.IsEnabled())
+		assert.False(t, c.IsActive())
 		assert.Equal(t, remoteKey, c.GetStorageKey(), "should fetch the key")
 	})
 
 	t.Run("DISABLED key", func(t *testing.T) {
 		// This is release CI and infra clusters case.
-		c := newTestClient(t, DisabledKey)
-		assert.False(t, c.IsActive())
+		c := NewClient("test", "Test", "0.0.0",
+			WithStorageKey(DisabledKey),
+			WithEndpoint(server.URL),
+			WithConfigURL(server.URL),
+			WithConfigureCallback(func(rc *RuntimeConfig) {
+				t.Logf("reconfigured with %v", rc)
+			}),
+		)
 		assert.False(t, c.IsEnabled())
+		assert.False(t, c.IsActive())
 		assert.Equal(t, DisabledKey, c.GetStorageKey())
 	})
 }
