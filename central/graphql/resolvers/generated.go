af9eacaa5ecfe9eadd2cf5ba93597f6058220eb1
Khushboo Sancheti <ks@stackrox.com>
2025-08-25 21:46:31 +0530

    ROX-30654: Add new field to email notifier to specify local hostname for smtp hello

--- central/graphql/resolvers/generated.go
+++ central/graphql/resolvers/generated.go
@@ -607,6 +607,7 @@ func registerGeneratedTypes(builder generator.SchemaBuilder) {
 		"allowUnauthenticatedSmtp: Boolean!",
 		"disableTLS: Boolean!",
 		"from: String!",
+		"hostnameHeloEhlo: String!",
 		"password: String!",
 		"sender: String!",
 		"server: String!",
@@ -7344,6 +7345,11 @@ func (resolver *emailResolver) DisableTLS(ctx context.Context) bool {
 
 func (resolver *emailResolver) From(ctx context.Context) string {
 	value := resolver.data.GetFrom()
+	return value
+}
+
+func (resolver *emailResolver) HostnameHeloEhlo(ctx context.Context) string {
+	value := resolver.data.GetHostnameHeloEhlo()
 	return value
 }
 
--- generated/storage/notifier.pb.go
+++ generated/storage/notifier.pb.go
@@ -681,8 +681,9 @@ type Email struct {
 	From                  string           `protobuf:"bytes,7,opt,name=from,proto3" json:"from,omitempty"`
 	StartTLSAuthMethod    Email_AuthMethod `protobuf:"varint,8,opt,name=startTLSAuthMethod,proto3,enum=storage.Email_AuthMethod" json:"startTLSAuthMethod,omitempty"`
 	// Set to true to allow unauthenticated SMTP
-	AllowUnauthenticatedSmtp bool `protobuf:"varint,9,opt,name=allow_unauthenticated_smtp,json=allowUnauthenticatedSmtp,proto3" json:"allow_unauthenticated_smtp,omitempty" scrub:"disableDependentIfTrue"` // @gotags: scrub:"disableDependentIfTrue"
-	SkipTLSVerify            bool `protobuf:"varint,10,opt,name=skipTLSVerify,proto3" json:"skipTLSVerify,omitempty"`
+	AllowUnauthenticatedSmtp bool   `protobuf:"varint,9,opt,name=allow_unauthenticated_smtp,json=allowUnauthenticatedSmtp,proto3" json:"allow_unauthenticated_smtp,omitempty" scrub:"disableDependentIfTrue"` // @gotags: scrub:"disableDependentIfTrue"
+	SkipTLSVerify            bool   `protobuf:"varint,10,opt,name=skipTLSVerify,proto3" json:"skipTLSVerify,omitempty"`
+	HostnameHeloEhlo         string `protobuf:"bytes,11,opt,name=hostname_helo_ehlo,json=hostnameHeloEhlo,proto3" json:"hostname_helo_ehlo,omitempty"` // the local FQDN sent by the client in the EHLO/HELO command. Optional, if left blank, localhost will be used
 	unknownFields            protoimpl.UnknownFields
 	sizeCache                protoimpl.SizeCache
 }
@@ -785,6 +786,13 @@ func (x *Email) GetSkipTLSVerify() bool {
 		return x.SkipTLSVerify
 	}
 	return false
+}
+
+func (x *Email) GetHostnameHeloEhlo() string {
+	if x != nil {
+		return x.HostnameHeloEhlo
+	}
+	return ""
 }
 
 type Splunk struct {
@@ -1662,7 +1670,7 @@ const file_storage_notifier_proto_rawDesc = "" +
 	"\x0fdisablePriority\x18\a \x01(\bR\x0fdisablePriority\x1ae\n" +
 	"\x0fPriorityMapping\x12-\n" +
 	"\bseverity\x18\x01 \x01(\x0e2\x11.storage.SeverityR\bseverity\x12#\n" +
-	"\rpriority_name\x18\x02 \x01(\tR\fpriorityName\"\xbb\x03\n" +
+	"\rpriority_name\x18\x02 \x01(\tR\fpriorityName\"\xe9\x03\n" +
 	"\x05Email\x12\x16\n" +
 	"\x06server\x18\x01 \x01(\tR\x06server\x12\x16\n" +
 	"\x06sender\x18\x02 \x01(\tR\x06sender\x12\x1a\n" +
@@ -1676,7 +1684,8 @@ const file_storage_notifier_proto_rawDesc = "" +
 	"\x12startTLSAuthMethod\x18\b \x01(\x0e2\x19.storage.Email.AuthMethodR\x12startTLSAuthMethod\x12<\n" +
 	"\x1aallow_unauthenticated_smtp\x18\t \x01(\bR\x18allowUnauthenticatedSmtp\x12$\n" +
 	"\rskipTLSVerify\x18\n" +
-	" \x01(\bR\rskipTLSVerify\"0\n" +
+	" \x01(\bR\rskipTLSVerify\x12,\n" +
+	"\x12hostname_helo_ehlo\x18\v \x01(\tR\x10hostnameHeloEhlo\"0\n" +
 	"\n" +
 	"AuthMethod\x12\f\n" +
 	"\bDISABLED\x10\x00\x12\t\n" +
