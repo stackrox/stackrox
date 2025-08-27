2926b0d5019b30aced86a281a2ebdbd32b2b2d9a
Mark Pedrotti <mark@stackrox.com>
2025-08-27 15:24:00 -0400

    ROX-30605: Replace classic cluster summary with PatternFly elements

---
+++ ui/apps/platform/src/Containers/Clusters/Components/ClusterMetadata.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+    DescriptionList,
+    DescriptionListGroup,
+    DescriptionListTerm,
+    DescriptionListDescription,
+} from '@patternfly/react-core';
+
+import type { ClusterStatus } from 'types/cluster.proto';
+
+import { formatBuildDate, formatCloudProvider, formatKubernetesVersion } from '../cluster.helpers';
+
+export type ClusterMetadataProps = {
+    status: ClusterStatus;
+};
+
+function ClusterMetadata({ status }: ClusterMetadataProps) {
+    return (
+        <DescriptionList>
+            <DescriptionListGroup>
+                <DescriptionListTerm>ClusterStatus</DescriptionListTerm>
+                <DescriptionListDescription>
+                    {formatKubernetesVersion(status?.orchestratorMetadata)}
+                </DescriptionListDescription>
+            </DescriptionListGroup>
+            <DescriptionListGroup>
+                <DescriptionListTerm>Build date</DescriptionListTerm>
+                <DescriptionListDescription>
+                    {formatBuildDate(status?.orchestratorMetadata)}
+                </DescriptionListDescription>
+            </DescriptionListGroup>
+            <DescriptionListGroup>
+                <DescriptionListTerm>Cloud provider</DescriptionListTerm>
+                <DescriptionListDescription>
+                    {formatCloudProvider(status?.providerMetadata)}
+                </DescriptionListDescription>
+            </DescriptionListGroup>
+        </DescriptionList>
+    );
+}
+
+export default ClusterMetadata;
