dd824c11b26a6271b5c91cf8eb3bf810ef0e7dfd
Vlad Bologa <vbologa@redhat.com>
2025-08-27 10:22:33 +0200

    Update feature flag comment

--- sensor/kubernetes/certrefresh/env.go
+++ sensor/kubernetes/certrefresh/env.go
@@ -2,9 +2,10 @@ package certrefresh
 
 import "github.com/stackrox/rox/pkg/env"
 
-// ROX_SENSOR_CA_ROTATION_ENABLED enables Sensor to advertise CA rotation support to Central.
-// This does not gate retrieval or persistence of CA bundles; it only affects the capability set.
+// ROX_SENSOR_CA_ROTATION_ENABLED enables Sensor to advertise CA rotation support to Central,
+// by adding the SensorCARotationSupported capability to Sensor's capabilities.
+// This does not gate retrieval or persistence of CA bundles.
 // Having the capability set will cause Central to issue Secured Cluster certificates signed by the
 // newer CA, if the Central CA has been rotated.
-// TODO: Remove when epic ROX-20262 is complete.
+// TODO: Enable when epic ROX-20262 is complete.
 var sensorCARotationEnabled = env.RegisterBooleanSetting("ROX_SENSOR_CA_ROTATION_ENABLED", false)
