e6125dba8b511f8bfa5ad3a873eba1581280cc49
lvalerom <lvalerom@redhat.com>
2025-08-28 12:57:19 +0200

    add availability checker for VM

---
+++ sensor/kubernetes/listener/watcher/virtualmachine/availabilitychecker.go
@@ -0,0 +1,23 @@
+package virtualmachine
+
+import (
+	"github.com/stackrox/rox/pkg/k8sapi"
+	"github.com/stackrox/rox/pkg/virtualmachine"
+	"github.com/stackrox/rox/sensor/kubernetes/client"
+	"github.com/stackrox/rox/sensor/kubernetes/listener/watcher/availability"
+)
+
+type availabilityChecker interface {
+	Available(client.Interface) bool
+	AppendToCRDWatcher(availability.CrdWatcher) error
+	GetResources() []k8sapi.APIResource
+}
+
+// NewAvailabilityChecker creates a new AvailabilityChecker
+func NewAvailabilityChecker() availabilityChecker {
+	resources := []k8sapi.APIResource{
+		virtualmachine.VirtualMachine,
+		virtualmachine.VirtualMachineInstance,
+	}
+	return availability.NewChecker(virtualmachine.GetGroupVersion(), resources)
+}
