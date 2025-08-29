c026faff65611d483aee6bdda876fa1254e0dd76
Tomasz Janiszewski <tomek@redhat.com>
2025-08-29 10:36:58 +0200

    fix(ci): junit paths for compliance tests
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

--- tests/e2e/run-compliance-e2e.sh
+++ tests/e2e/run-compliance-e2e.sh
@@ -44,7 +44,7 @@ run_compliance_e2e_tests() {
     rm -f FAIL
     make -C tests compliance-v2-tests || touch FAIL
 
-    store_test_results "compliance/test-results/reports" "reports"
+    store_test_results "tests/compliance-v2-tests-results" "compliance-v2-tests-results"
 
     if is_OPENSHIFT_CI; then
         cp -a compliance/test-results/artifacts/* "${ARTIFACT_DIR}/" || true
