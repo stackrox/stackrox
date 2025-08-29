8e1de129752e6e5d6a9650d5374de503e15c2a0b
David Vail <dcvail17@gmail.com>
2025-08-29 12:18:29 -0400

    Add implementation for Workload Security tab

---
+++ ui/apps/platform/src/ConsolePlugin/hooks/useWorkloadId.ts
@@ -0,0 +1,37 @@
+import { useCallback } from 'react';
+
+import useRestQuery from 'hooks/useRestQuery';
+import { listDeployments } from 'services/DeploymentsService';
+import type { SearchFilter, ApiSortOption } from 'types/search';
+
+export function useWorkloadId({ ns, name }: { ns: string | undefined; name: string | undefined }) {
+    const deploymentIdQuery = useCallback(() => {
+        // Quote search values to ensure exact match instead of prefix match
+        const searchFilter: SearchFilter = { Namespace: `"${ns}"`, Deployment: `"${name}"` };
+        const sortOption: ApiSortOption = { field: 'Deployment', reversed: false };
+        return listDeployments(searchFilter, sortOption, 1, 10);
+    }, [ns, name]);
+
+    const { data, isLoading, error } = useRestQuery(deploymentIdQuery);
+    const id = data?.[0]?.id;
+
+    if (!ns || !name) {
+        return {
+            id: undefined,
+            isLoading: false,
+            error: new Error(
+                `An invalid namespace or name was provided. Namespace: ${ns} Name: ${name}`
+            ),
+        };
+    }
+
+    if (!id && !isLoading && !error) {
+        return {
+            id: undefined,
+            isLoading: false,
+            error: new Error(`A workload id could not be found. Namespace: ${ns} Name: ${name}`),
+        };
+    }
+
+    return { id, isLoading, error };
+}
