3c94d94bb6a43001d110e7dcfd869bcbc9f0cfa6
Michaël Petrov <github@parameta.lol>
2025-09-01 16:17:13 +0200

    fix rebase

--- central/metrics/custom/policy_violations/labels.go
+++ central/metrics/custom/policy_violations/labels.go
@@ -8,30 +8,27 @@ import (
 	"github.com/stackrox/rox/generated/storage"
 )
 
-var (
-	lazyLabels = []tracker.LazyLabel[*finding]{
-		// Alert
-		{Label: "Cluster", Getter: func(f *finding) string { return f.GetClusterName() }},
-		{Label: "Namespace", Getter: func(f *finding) string { return f.GetNamespace() }},
-		{Label: "Resource", Getter: func(f *finding) string { return f.GetResource().GetName() }},
-		{Label: "Deployment", Getter: func(f *finding) string { return f.GetDeployment().GetName() }},
-		{Label: "IsDeploymentActive", Getter: func(f *finding) string { return strconv.FormatBool(!f.GetDeployment().GetInactive()) }},
-		{Label: "IsPlatformComponent", Getter: func(f *finding) string { return strconv.FormatBool(f.GetPlatformComponent()) }},
-		{Label: "Policy", Getter: func(f *finding) string { return f.GetPolicy().GetName() }},
-		{Label: "Categories", Getter: func(f *finding) string { return strings.Join(f.GetPolicy().GetCategories(), ",") }},
-		{Label: "Severity", Getter: func(f *finding) string { return f.GetPolicy().GetSeverity().String() }},
-		{Label: "Action", Getter: func(f *finding) string { return f.GetEnforcement().GetAction().String() }},
-		{Label: "Message", Getter: func(f *finding) string { return f.GetEnforcement().GetMessage() }},
-		{Label: "Stage", Getter: func(f *finding) string { return f.GetLifecycleStage().String() }},
-		{Label: "State", Getter: func(f *finding) string { return f.GetState().String() }},
-		{Label: "Entity", Getter: func(f *finding) string { return f.GetEntityType().String() }},
-		{Label: "EntityName", Getter: getEntityName},
+var lazyLabels = []tracker.LazyLabel[*finding]{
+	// Alert
+	{Label: "Cluster", Getter: func(f *finding) string { return f.GetClusterName() }},
+	{Label: "Namespace", Getter: func(f *finding) string { return f.GetNamespace() }},
+	{Label: "Resource", Getter: func(f *finding) string { return f.GetResource().GetName() }},
+	{Label: "Deployment", Getter: func(f *finding) string { return f.GetDeployment().GetName() }},
+	{Label: "IsDeploymentActive", Getter: func(f *finding) string { return strconv.FormatBool(!f.GetDeployment().GetInactive()) }},
+	{Label: "IsPlatformComponent", Getter: func(f *finding) string { return strconv.FormatBool(f.GetPlatformComponent()) }},
+	{Label: "Policy", Getter: func(f *finding) string { return f.GetPolicy().GetName() }},
+	{Label: "Categories", Getter: func(f *finding) string { return strings.Join(f.GetPolicy().GetCategories(), ",") }},
+	{Label: "Severity", Getter: func(f *finding) string { return f.GetPolicy().GetSeverity().String() }},
+	{Label: "Action", Getter: func(f *finding) string { return f.GetEnforcement().GetAction().String() }},
+	{Label: "Message", Getter: func(f *finding) string { return f.GetEnforcement().GetMessage() }},
+	{Label: "Stage", Getter: func(f *finding) string { return f.GetLifecycleStage().String() }},
+	{Label: "State", Getter: func(f *finding) string { return f.GetState().String() }},
+	{Label: "Entity", Getter: func(f *finding) string { return f.GetEntityType().String() }},
+	{Label: "EntityName", Getter: getEntityName},
 
-		// Violation
-		{Label: "Type", Getter: func(f *finding) string { return f.GetType().String() }},
-	}
-	order = tracker.MakeLabelOrderMap(lazyLabels)
-)
+	// Violation
+	{Label: "Type", Getter: func(f *finding) string { return f.GetType().String() }},
+}
 
 type finding struct {
 	err error
