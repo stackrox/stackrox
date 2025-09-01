1f13d712fd47653a44de4cb7f57298892a111dbe
Moritz Clasmeier <mclasmeier@redhat.com>
2025-09-01 10:26:36 +0200

    PR Review

--- operator/api/v1alpha1/defaults_merging.go
+++ operator/api/v1alpha1/defaults_merging.go
@@ -7,23 +7,23 @@ import (
 type nonDereferencingLeafTransformer struct{}
 
 func (t nonDereferencingLeafTransformer) Transformer(typ reflect.Type) func(dst, src reflect.Value) error {
-	// Only handle pointer types
-	if typ.Kind() == reflect.Ptr {
-		elem := typ.Elem()
-
-		// If pointer to struct: return no transformer function so that mergo continues traversing.
-		// Retain mergo's merging behaviour for maps and slices.
-		if elem.Kind() == reflect.Struct || elem.Kind() == reflect.Map || elem.Kind() == reflect.Slice {
-			return nil
-		}
-
-		// Use custom transformer function for other (leaf) types.
-		return func(dst, src reflect.Value) error {
-			if dst.IsNil() && !src.IsNil() {
-				dst.Set(src)
-			}
-			return nil
-		}
+	// Only handle pointer types here.
+	if typ.Kind() != reflect.Ptr {
+		return nil
 	}
-	return nil
+
+	elemKind := typ.Elem().Kind()
+
+	// If pointer to struct, map or slice: return no transformer function so that mergo continues traversing.
+	if elemKind == reflect.Struct || elemKind == reflect.Map || elemKind == reflect.Slice {
+		return nil
+	}
+
+	// Use custom transformer function for other (leaf) types: override nil fields with non-nil defaults.
+	return func(dst, src reflect.Value) error {
+		if dst.IsNil() && !src.IsNil() {
+			dst.Set(src)
+		}
+		return nil
+	}
 }
