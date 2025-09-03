4d132c320e7c0022450bed0b597c64a493d42d63
Michael Heß <GrimmiMeloni@users.noreply.github.com>
2025-09-03 17:39:48 +0200

    Update operator/bundle_helpers/copy-crd-and-patch.sh
    Co-authored-by: Marcin Owsiany <porridge@redhat.com>

--- operator/bundle_helpers/copy-crd-and-patch.sh
+++ operator/bundle_helpers/copy-crd-and-patch.sh
@@ -8,7 +8,6 @@ set -euo pipefail
 mkdir -p build/bundle/manifests
 cp -v ../config-controller/config/crd/bases/config.stackrox.io_securitypolicies.yaml build/bundle/manifests/
 
-# Call patch-csv.py with input and output files, passing through all arguments
 "$(dirname "$0")/patch-csv.py" "$@" \
     < bundle/manifests/rhacs-operator.clusterserviceversion.yaml \
     > build/bundle/manifests/rhacs-operator.clusterserviceversion.yaml
