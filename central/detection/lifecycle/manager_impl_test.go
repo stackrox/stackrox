c1245952171c64589410b17d250460110b51f8e0
JoukoVirtanen <jouko@stackrox.com>
2025-09-05 14:24:48 -0700

    Fixes after rebase

--- central/detection/lifecycle/manager_impl_test.go
+++ central/detection/lifecycle/manager_impl_test.go
@@ -28,7 +28,7 @@ var (
 		ManagedBy: storage.ManagerType_MANAGER_TYPE_HELM_CHART,
 		HelmConfig: &storage.CompleteClusterConfig{
 			DynamicConfig: &storage.DynamicClusterConfig{
-				AutolockProcessBaseline: &storage.AutolockProcessBaseline{
+				AutoLockProcessBaseline: &storage.AutoLockProcessBaseline{
 					Enabled: true,
 				},
 			},
@@ -39,7 +39,7 @@ var (
 		ManagedBy: storage.ManagerType_MANAGER_TYPE_HELM_CHART,
 		HelmConfig: &storage.CompleteClusterConfig{
 			DynamicConfig: &storage.DynamicClusterConfig{
-				AutolockProcessBaseline: &storage.AutolockProcessBaseline{
+				AutoLockProcessBaseline: &storage.AutoLockProcessBaseline{
 					Enabled: false,
 				},
 			},
@@ -49,7 +49,7 @@ var (
 	clusterAutolockManualEnabled = &storage.Cluster{
 		ManagedBy: storage.ManagerType_MANAGER_TYPE_MANUAL,
 		DynamicConfig: &storage.DynamicClusterConfig{
-			AutolockProcessBaseline: &storage.AutolockProcessBaseline{
+			AutoLockProcessBaseline: &storage.AutoLockProcessBaseline{
 				Enabled: true,
 			},
 		},
@@ -266,6 +266,12 @@ func (suite *ManagerTestSuite) TestAutoLockProcessBaselines() {
 	suite.mockCtrl.Finish()
 
 	suite.cluster.EXPECT().GetCluster(gomock.Any(), key.GetClusterId()).Return(clusterAutolockDisabled, true, nil)
+	suite.manager.autoLockProcessBaselines(baselines)
+	suite.mockCtrl.Finish()
+
+	suite.cluster.EXPECT().GetCluster(gomock.Any(), key.GetClusterId()).Return(clusterAutolockManualEnabled, true, nil)
+	suite.baselines.EXPECT().UserLockProcessBaseline(gomock.Any(), key, true).Return(baseline, nil)
+	suite.connectionManager.EXPECT().SendMessage(gomock.Any(), gomock.Any())
 	suite.manager.autoLockProcessBaselines(baselines)
 	suite.mockCtrl.Finish()
 
