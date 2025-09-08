13803f3e3a35aacbaf09da61d3aee60f0a381950
Yann Brillouet <ybrillou@redhat.com>
2025-09-08 18:56:16 +0200

    Duplicate protos dropping search fields for compact table model

--- central/convert/storagetov2/virtual_machine.go
+++ central/convert/storagetov2/virtual_machine.go
@@ -44,8 +44,8 @@ func VirtualMachineScan(scan *storage.VirtualMachineScan) *v2.VirtualMachineScan
 	return &v2.VirtualMachineScan{
 		ScannerVersion: scan.GetScannerVersion(),
 		ScanTime:       scan.GetScanTime(),
-		Components:     ScanComponents(scan.GetComponents()),
-		DataSource:     DataSource(scan.GetDataSource()),
+		Components:     VirtualMachineScanComponents(scan.GetComponents()),
+		DataSource:     VirtualMachineDataSource(scan.GetDataSource()),
 		Notes:          convertVirtualMachineScanNotes(scan.GetNotes()),
 	}
 }
@@ -81,5 +81,169 @@ func convertVirtualMachineScanNote(note storage.VirtualMachineScan_Note) v2.Virt
 		return v2.VirtualMachineScan_CERTIFIED_RHEL_SCAN_UNAVAILABLE
 	default:
 		return v2.VirtualMachineScan_UNSET
+	}
+}
+
+func VirtualMachineScanComponents(components []*storage.EmbeddedVirtualMachineScanComponent) []*v2.ScanComponent {
+	if len(components) == 0 {
+		return nil
+	}
+
+	var ret []*v2.ScanComponent
+	for _, component := range components {
+		if component == nil {
+			continue
+		}
+		ret = append(ret, VirtualMachineScanComponent(component))
+	}
+
+	return ret
+}
+
+func VirtualMachineScanComponent(component *storage.EmbeddedVirtualMachineScanComponent) *v2.ScanComponent {
+	if component == nil {
+		return nil
+	}
+
+	result := &v2.ScanComponent{
+		Name:         component.GetName(),
+		Version:      component.GetVersion(),
+		License:      VirtualMachineLicense(component.GetLicense()),
+		Vulns:        EmbeddedVirtualMachineVulnerabilities(component.GetVulns()),
+		Source:       convertVirtualMachineSourceType(component.GetSource()),
+		Location:     component.GetLocation(),
+		RiskScore:    component.GetRiskScore(),
+		FixedBy:      component.GetFixedBy(),
+		Executables:  VirtualMachineExecutables(component.GetExecutables()),
+		Architecture: component.GetArchitecture(),
+	}
+
+	if component.GetTopCvss() != 0 {
+		result.SetTopCvss = &v2.ScanComponent_TopCvss{
+			TopCvss: component.GetTopCvss(),
+		}
+	}
+
+	return result
+}
+
+func VirtualMachineLicense(license *storage.EmbeddedVirtualMachineScanComponent_License) *v2.License {
+	if license == nil {
+		return nil
+	}
+
+	return &v2.License{
+		Name: license.GetName(),
+		Type: license.GetType(),
+		Url:  license.GetUrl(),
+	}
+}
+
+func EmbeddedVirtualMachineVulnerabilities(vulns []*storage.EmbeddedVirtualMachineVulnerability) []*v2.EmbeddedVulnerability {
+	if len(vulns) == 0 {
+		return nil
+	}
+
+	var ret []*v2.EmbeddedVulnerability
+	for _, vuln := range vulns {
+		if vuln == nil {
+			continue
+		}
+		ret = append(ret, EmbeddedVirtualMachineVulnerability(vuln))
+	}
+
+	return ret
+}
+
+func EmbeddedVirtualMachineVulnerability(vuln *storage.EmbeddedVirtualMachineVulnerability) *v2.EmbeddedVulnerability {
+	if vuln == nil {
+		return nil
+	}
+
+	result := &v2.EmbeddedVulnerability{
+		Cve:               vuln.GetCve(),
+		Summary:           vuln.GetSummary(),
+		Link:              vuln.GetLink(),
+		VulnerabilityType: convertVirtualMachineVulnerabilityType(vuln.GetVulnerabilityType()),
+		Severity:          convertSeverity(vuln.GetSeverity()),
+		CvssV3:            CvssV3(vuln.GetCvssV3()),
+		PublishedOn:       vuln.GetPublishedOn(),
+		LastModified:      vuln.GetLastModified(),
+	}
+
+	if vuln.GetFixedBy() != "" {
+		result.SetFixedBy = &v2.EmbeddedVulnerability_FixedBy{
+			FixedBy: vuln.GetFixedBy(),
+		}
+	}
+
+	return result
+}
+
+func convertVirtualMachineVulnerabilityType(vt storage.EmbeddedVirtualMachineVulnerability_VulnerabilityType) v2.VulnerabilityType {
+	// These enum mappings work as-is at the time of this writing
+	// Any new enums will need to be reflected accordingly
+	return v2.VulnerabilityType(vt)
+}
+
+func convertVirtualMachineSourceType(source storage.EmbeddedVirtualMachineScanComponent_SourceType) v2.SourceType {
+	switch source {
+	case storage.EmbeddedVirtualMachineScanComponent_OS:
+		return v2.SourceType_OS
+	case storage.EmbeddedVirtualMachineScanComponent_PYTHON:
+		return v2.SourceType_PYTHON
+	case storage.EmbeddedVirtualMachineScanComponent_JAVA:
+		return v2.SourceType_JAVA
+	case storage.EmbeddedVirtualMachineScanComponent_RUBY:
+		return v2.SourceType_RUBY
+	case storage.EmbeddedVirtualMachineScanComponent_NODEJS:
+		return v2.SourceType_NODEJS
+	case storage.EmbeddedVirtualMachineScanComponent_GO:
+		return v2.SourceType_GO
+	case storage.EmbeddedVirtualMachineScanComponent_DOTNETCORERUNTIME:
+		return v2.SourceType_DOTNETCORERUNTIME
+	case storage.EmbeddedVirtualMachineScanComponent_INFRASTRUCTURE:
+		return v2.SourceType_INFRASTRUCTURE
+	default:
+		return v2.SourceType_OS
+	}
+}
+
+func VirtualMachineExecutables(executables []*storage.EmbeddedVirtualMachineScanComponent_Executable) []*v2.ScanComponent_Executable {
+	if len(executables) == 0 {
+		return nil
+	}
+
+	var ret []*v2.ScanComponent_Executable
+	for _, executable := range executables {
+		if executable == nil {
+			continue
+		}
+		ret = append(ret, VirtualMachineExecutable(executable))
+	}
+
+	return ret
+}
+
+func VirtualMachineExecutable(executable *storage.EmbeddedVirtualMachineScanComponent_Executable) *v2.ScanComponent_Executable {
+	if executable == nil {
+		return nil
+	}
+
+	return &v2.ScanComponent_Executable{
+		Path:         executable.GetPath(),
+		Dependencies: executable.GetDependencies(),
+	}
+}
+
+func VirtualMachineDataSource(ds *storage.VirtualMachineScan_DataSource) *v2.DataSource {
+	if ds == nil {
+		return nil
+	}
+
+	return &v2.DataSource{
+		Id:     ds.GetId(),
+		Name:   ds.GetName(),
+		Mirror: ds.GetMirror(),
 	}
 }
--- central/convert/v2tostorage/virtual_machine.go
+++ central/convert/v2tostorage/virtual_machine.go
@@ -44,9 +44,174 @@ func VirtualMachineScan(scan *v2.VirtualMachineScan) *storage.VirtualMachineScan
 	return &storage.VirtualMachineScan{
 		ScannerVersion: scan.GetScannerVersion(),
 		ScanTime:       scan.GetScanTime(),
-		Components:     ScanComponents(scan.GetComponents()),
-		DataSource:     DataSource(scan.GetDataSource()),
+		Components:     VirtualMachineScanComponents(scan.GetComponents()),
+		DataSource:     VirtualMachineDataSource(scan.GetDataSource()),
 		Notes:          convertVirtualMachineScanNotes(scan.GetNotes()),
+	}
+}
+
+func VirtualMachineScanComponents(components []*v2.ScanComponent) []*storage.EmbeddedVirtualMachineScanComponent {
+	if len(components) == 0 {
+		return nil
+	}
+
+	var ret []*storage.EmbeddedVirtualMachineScanComponent
+	for _, component := range components {
+		if component == nil {
+			continue
+		}
+		ret = append(ret, VirtualMachineScanComponent(component))
+	}
+
+	return ret
+}
+
+func VirtualMachineScanComponent(component *v2.ScanComponent) *storage.EmbeddedVirtualMachineScanComponent {
+	if component == nil {
+		return nil
+	}
+
+	result := &storage.EmbeddedVirtualMachineScanComponent{
+		Name:         component.GetName(),
+		Version:      component.GetVersion(),
+		License:      VirtualMachineLicense(component.GetLicense()),
+		Vulns:        EmbeddedVirtualMachineVulnerabilities(component.GetVulns()),
+		Source:       convertVirtualMachineSourceType(component.GetSource()),
+		Location:     component.GetLocation(),
+		RiskScore:    component.GetRiskScore(),
+		FixedBy:      component.GetFixedBy(),
+		Executables:  VirtualMachineExecutables(component.GetExecutables()),
+		Architecture: component.GetArchitecture(),
+	}
+
+	if component.GetTopCvss() != 0 {
+		result.SetTopCvss = &storage.EmbeddedVirtualMachineScanComponent_TopCvss{
+			TopCvss: component.GetTopCvss(),
+		}
+	}
+
+	return result
+}
+
+func VirtualMachineLicense(license *v2.License) *storage.EmbeddedVirtualMachineScanComponent_License {
+	if license == nil {
+		return nil
+	}
+
+	return &storage.EmbeddedVirtualMachineScanComponent_License{
+		Name: license.GetName(),
+		Type: license.GetType(),
+		Url:  license.GetUrl(),
+	}
+}
+
+func EmbeddedVirtualMachineVulnerabilities(vulns []*v2.EmbeddedVulnerability) []*storage.EmbeddedVirtualMachineVulnerability {
+	if len(vulns) == 0 {
+		return nil
+	}
+
+	var ret []*storage.EmbeddedVirtualMachineVulnerability
+	for _, vuln := range vulns {
+		if vuln == nil {
+			continue
+		}
+		ret = append(ret, EmbeddedVirtualMachineVulnerability(vuln))
+	}
+
+	return ret
+}
+
+func EmbeddedVirtualMachineVulnerability(vuln *v2.EmbeddedVulnerability) *storage.EmbeddedVirtualMachineVulnerability {
+	if vuln == nil {
+		return nil
+	}
+
+	result := &storage.EmbeddedVirtualMachineVulnerability{
+		Cve:               vuln.GetCve(),
+		Summary:           vuln.GetSummary(),
+		Link:              vuln.GetLink(),
+		VulnerabilityType: convertVirtualMachineVulnerabilityType(vuln.GetVulnerabilityType()),
+		Severity:          convertSeverity(vuln.GetSeverity()),
+		// CvssV2 is not available in v2 API
+		CvssV3:       CvssV3(vuln.GetCvssV3()),
+		PublishedOn:  vuln.GetPublishedOn(),
+		LastModified: vuln.GetLastModified(),
+	}
+
+	if vuln.GetFixedBy() != "" {
+		result.SetFixedBy = &storage.EmbeddedVirtualMachineVulnerability_FixedBy{
+			FixedBy: vuln.GetFixedBy(),
+		}
+	}
+
+	return result
+}
+
+func convertVirtualMachineVulnerabilityType(vt v2.VulnerabilityType) storage.EmbeddedVirtualMachineVulnerability_VulnerabilityType {
+	// These enum mappings work as-is at the time of this writing
+	// Any new enums will need to be reflected accordingly
+	return storage.EmbeddedVirtualMachineVulnerability_VulnerabilityType(vt)
+}
+
+func convertVirtualMachineSourceType(source v2.SourceType) storage.EmbeddedVirtualMachineScanComponent_SourceType {
+	switch source {
+	case v2.SourceType_OS:
+		return storage.EmbeddedVirtualMachineScanComponent_OS
+	case v2.SourceType_PYTHON:
+		return storage.EmbeddedVirtualMachineScanComponent_PYTHON
+	case v2.SourceType_JAVA:
+		return storage.EmbeddedVirtualMachineScanComponent_JAVA
+	case v2.SourceType_RUBY:
+		return storage.EmbeddedVirtualMachineScanComponent_RUBY
+	case v2.SourceType_NODEJS:
+		return storage.EmbeddedVirtualMachineScanComponent_NODEJS
+	case v2.SourceType_GO:
+		return storage.EmbeddedVirtualMachineScanComponent_GO
+	case v2.SourceType_DOTNETCORERUNTIME:
+		return storage.EmbeddedVirtualMachineScanComponent_DOTNETCORERUNTIME
+	case v2.SourceType_INFRASTRUCTURE:
+		return storage.EmbeddedVirtualMachineScanComponent_INFRASTRUCTURE
+	default:
+		return storage.EmbeddedVirtualMachineScanComponent_OS
+	}
+}
+
+func VirtualMachineExecutables(executables []*v2.ScanComponent_Executable) []*storage.EmbeddedVirtualMachineScanComponent_Executable {
+	if len(executables) == 0 {
+		return nil
+	}
+
+	var ret []*storage.EmbeddedVirtualMachineScanComponent_Executable
+	for _, executable := range executables {
+		if executable == nil {
+			continue
+		}
+		ret = append(ret, VirtualMachineExecutable(executable))
+	}
+
+	return ret
+}
+
+func VirtualMachineExecutable(executable *v2.ScanComponent_Executable) *storage.EmbeddedVirtualMachineScanComponent_Executable {
+	if executable == nil {
+		return nil
+	}
+
+	return &storage.EmbeddedVirtualMachineScanComponent_Executable{
+		Path:         executable.GetPath(),
+		Dependencies: executable.GetDependencies(),
+	}
+}
+
+func VirtualMachineDataSource(ds *v2.DataSource) *storage.VirtualMachineScan_DataSource {
+	if ds == nil {
+		return nil
+	}
+
+	return &storage.VirtualMachineScan_DataSource{
+		Id:     ds.GetId(),
+		Name:   ds.GetName(),
+		Mirror: ds.GetMirror(),
 	}
 }
 
