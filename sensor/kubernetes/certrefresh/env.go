b57d522b7d50844d6d84b740558c666b11c71601
Vlad Bologa <vbologa@redhat.com>
2025-09-09 16:33:07 +0200

    ROX-27964: separate feature flag for CA rotation by install method (#16688)

--- sensor/kubernetes/certrefresh/env.go
+++ sensor/kubernetes/certrefresh/env.go
@@ -1,11 +1,32 @@
 package certrefresh
 
-import "github.com/stackrox/rox/pkg/env"
+import (
+	"github.com/stackrox/rox/pkg/env"
+	"github.com/stackrox/rox/sensor/common/installmethod"
+)
 
-// ROX_SENSOR_CA_ROTATION_ENABLED enables Sensor to advertise CA rotation support to Central,
-// by adding the SensorCARotationSupported capability to Sensor's capabilities.
-// This does not gate retrieval or persistence of CA bundles.
-// Having the capability set will cause Central to issue Secured Cluster certificates signed by the
-// newer CA, if the Central CA has been rotated.
-// TODO: Enable when epic ROX-20262 is complete.
-var sensorCARotationEnabled = env.RegisterBooleanSetting("ROX_SENSOR_CA_ROTATION_ENABLED", false)
+// ROX_SENSOR_CA_ROTATION_OPERATOR_ENABLED enables CA rotation support for Operator-managed Sensors.
+// When enabled, Operator-managed Sensors will advertise the SensorCARotationSupported capability to Central,
+// causing Central to issue Secured Cluster certificates signed by the newer CA during CA rotation.
+// TODO(ROX-20262): Enable when epic ROX-20262 is complete.
+var sensorCARotationOperatorEnabled = env.RegisterBooleanSetting("ROX_SENSOR_CA_ROTATION_OPERATOR_ENABLED", false)
+
+// ROX_SENSOR_CA_ROTATION_HELM_ENABLED enables CA rotation support for Helm-managed Sensors.
+// When enabled, Helm-managed Sensors will advertise the SensorCARotationSupported capability to Central,
+// causing Central to issue Secured Cluster certificates signed by the newer CA during CA rotation.
+var sensorCARotationHelmEnabled = env.RegisterBooleanSetting("ROX_SENSOR_CA_ROTATION_HELM_ENABLED", false)
+
+// SensorCARotationEnabled returns whether CA rotation capabilities should be enabled for this Sensor,
+// based on the installation method and corresponding environment variable settings.
+// Returns true if CA rotation is enabled for the current install method, false otherwise.
+func SensorCARotationEnabled() bool {
+	switch installmethod.Get() {
+	case "operator":
+		return sensorCARotationOperatorEnabled.BooleanSetting()
+	case "helm":
+		return sensorCARotationHelmEnabled.BooleanSetting()
+	default:
+		// Manual/manifest installations do not support CA rotation
+		return false
+	}
+}
