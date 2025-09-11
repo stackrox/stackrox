2dd18450c47c5a81ceec69260292f9902d3685ea
Mark Pedrotti <mark@stackrox.com>
2025-09-11 16:57:29 -0400

    Add missing comment for close brace of if statement

--- ui/apps/platform/src/Containers/Vulnerabilities/components/CvePageHeader.tsx
+++ ui/apps/platform/src/Containers/Vulnerabilities/components/CvePageHeader.tsx
@@ -65,7 +65,7 @@ function CvePageHeader({ data }: CvePageHeaderProps) {
         //             isKnownToBeUsedInRansomwareCampaigns
         //         />
         //     );
-        }
+        // }
     }
     */
     if (hasEpssProbabilityLabel) {
