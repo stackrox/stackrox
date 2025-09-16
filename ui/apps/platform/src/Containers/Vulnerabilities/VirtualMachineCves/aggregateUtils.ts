2a223b44cd49ddbada648b777a38aa1ab41ac304
bradr5 <braroger@redhat.com>
2025-09-16 11:58:28 -0500

    ROX-30635: Virtual machine vulnerabilities table

--- ui/apps/platform/src/Containers/Vulnerabilities/VirtualMachineCves/aggregateUtils.ts
+++ ui/apps/platform/src/Containers/Vulnerabilities/VirtualMachineCves/aggregateUtils.ts
@@ -1,5 +1,6 @@
 import type { VirtualMachine } from 'services/VirtualMachineService';
-import type { CVSSV3Severity } from 'types/vulnerability.proto';
+import type { ScanComponent } from 'types/scanComponent.proto';
+import type { CVSSV3Severity, EmbeddedVulnerability } from 'types/vulnerability.proto';
 
 // Most if not all functions in this file will be removed once backend filtering is implemented.
 
@@ -23,4 +24,61 @@ export function getVirtualMachineSeveritiesCount(
     });
 
     return severityCounts;
+}
+
+type AffectedComponent = Omit<ScanComponent, 'vulns'>;
+type CVEId = EmbeddedVulnerability['cve'];
+
+export type CVEEntry = EmbeddedVulnerability & {
+    affectedComponents: AffectedComponent[];
+};
+
+// Dictionary keyed by CVE (example: "CVE-2024-00010": { affectedComponents: [...] })
+type CVEDictionary = Record<CVEId, CVEEntry>;
+
+export function getVirtualMachineCvesDictionaryData(virtualMachine: VirtualMachine): CVEDictionary {
+    const components = virtualMachine?.scan?.components ?? [];
+
+    const cvesWithAffectedComponents: CVEDictionary = {};
+
+    components.forEach((comp) => {
+        const { vulns, ...componentDetails } = comp;
+        const componentWithoutVulns: AffectedComponent = componentDetails;
+        vulns.forEach((vulnerability) => {
+            if (cvesWithAffectedComponents[vulnerability.cve]) {
+                cvesWithAffectedComponents[vulnerability.cve].affectedComponents.push(
+                    componentWithoutVulns
+                );
+            } else {
+                cvesWithAffectedComponents[vulnerability.cve] = {
+                    ...vulnerability,
+                    affectedComponents: [componentWithoutVulns],
+                };
+            }
+        });
+    });
+
+    return cvesWithAffectedComponents;
+}
+
+export type CVEWithAffectedComponents = EmbeddedVulnerability & {
+    cve: CVEId;
+    affectedComponents: AffectedComponent[];
+};
+
+// Returns a sorted list of CVE entries with their affected components
+export function getVirtualMachineCvesListData(
+    virtualMachine?: VirtualMachine
+): CVEWithAffectedComponents[] {
+    if (!virtualMachine) {
+        return [];
+    }
+
+    const dict = getVirtualMachineCvesDictionaryData(virtualMachine);
+    return Object.keys(dict)
+        .sort()
+        .map((cve) => ({
+            ...dict[cve],
+            affectedComponents: dict[cve].affectedComponents,
+        }));
 }
