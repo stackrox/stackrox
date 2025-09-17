a54525c4f4e29616288922ec994c83f0968fbacd
Saif Chaudhry <schaudhr@redhat.com>
2025-09-17 10:44:17 -0700

    feat(ui): add developer preview label for Virtual Machines tab
    Signed-off-by: Saif Chaudhry <schaudhr@redhat.com>

---
+++ ui/apps/platform/src/Components/PatternFly/DeveloperPreviewLabel.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Button, Flex, FlexItem, Label, LabelProps, Popover } from '@patternfly/react-core';
+import { InfoCircleIcon } from '@patternfly/react-icons';
+
+export type DeveloperPreviewLabelProps = Omit<LabelProps, 'children'>;
+
+function DeveloperPreviewLabel({ className, ...props }: DeveloperPreviewLabelProps) {
+    const popoverContent = (
+        <div>
+            Developer preview features are not intended to be used in production environments. The
+            clusters deployed with the developer preview features are considered to be development
+            cluster and are not supported through the Red Hat Customer Portal case management
+            system.
+        </div>
+    );
+
+    return (
+        <Label
+            isCompact
+            color="purple"
+            className={`pf-v5-u-font-weight-light pf-v5-u-font-family-sans-serif ${className ?? ''}`}
+            {...props}
+        >
+            <Flex
+                direction={{ default: 'row' }}
+                alignItems={{ default: 'alignItemsCenter' }}
+                spaceItems={{ default: 'spaceItemsXs' }}
+            >
+                <FlexItem>
+                    <Popover
+                        aria-label="Developer preview info"
+                        bodyContent={popoverContent}
+                        position="top"
+                        triggerAction="hover"
+                    >
+                        <Button
+                            variant="plain"
+                            color="purple"
+                            isInline
+                            aria-label="Show developer preview info"
+                            className="pf-v5-u-p-0 pf-v5-u-color-purple-500"
+                        >
+                            <InfoCircleIcon color="var(--pf-v5-global--palette--purple-500)" />
+                        </Button>
+                    </Popover>
+                </FlexItem>
+                <FlexItem>Developer preview</FlexItem>
+            </Flex>
+        </Label>
+    );
+}
+
+export default DeveloperPreviewLabel;
--- ui/apps/platform/src/Containers/MainPage/Navigation/HorizontalSubnav.tsx
+++ ui/apps/platform/src/Containers/MainPage/Navigation/HorizontalSubnav.tsx
@@ -10,6 +10,8 @@ import {
     NavItemSeparator,
     NavItem,
     NavList,
+    Flex,
+    FlexItem,
 } from '@patternfly/react-core';
 
 import {
@@ -29,6 +31,7 @@ import { IsFeatureFlagEnabled } from 'hooks/useFeatureFlags';
 import { HasReadAccess } from 'hooks/usePermissions';
 import { hasSearchKeyValue } from 'utils/searchUtils';
 import { ensureExhaustive } from 'utils/type.utils';
+import DeveloperPreviewLabel from 'Components/PatternFly/DeveloperPreviewLabel';
 import NavigationItem from './NavigationItem';
 import { filterNavDescriptions, isActiveLink, NavDescription } from './utils';
 
@@ -101,7 +104,18 @@ function getSubnavDescriptionGroups(
             },
             {
                 type: 'link',
-                content: 'Virtual Machines',
+                content: () => (
+                    <Flex
+                        direction={{ default: 'row' }}
+                        alignItems={{ default: 'alignItemsCenter' }}
+                        spaceItems={{ default: 'spaceItemsMd' }}
+                    >
+                        <FlexItem>Virtual Machines</FlexItem>
+                        <FlexItem>
+                            <DeveloperPreviewLabel />
+                        </FlexItem>
+                    </Flex>
+                ),
                 path: vulnerabilitiesVirtualMachineCvesPath,
                 routeKey: 'vulnerabilities/virtual-machine-cves',
             },
