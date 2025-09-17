8f50a958b1c1b23edc8615eaecb6334c39e6c106
Saif Chaudhry <schaudhr@redhat.com>
2025-09-17 12:45:10 -0700

    feat(ui): add external link to developer preview label popover
    Signed-off-by: Saif Chaudhry <schaudhr@redhat.com>

--- ui/apps/platform/src/Components/PatternFly/DeveloperPreviewLabel.tsx
+++ ui/apps/platform/src/Components/PatternFly/DeveloperPreviewLabel.tsx
@@ -2,16 +2,32 @@ import React from 'react';
 import { Button, Flex, FlexItem, Label, LabelProps, Popover } from '@patternfly/react-core';
 import { InfoCircleIcon } from '@patternfly/react-icons';
 
+import ExternalLink from 'Components/PatternFly/IconText/ExternalLink';
+
 export type DeveloperPreviewLabelProps = Omit<LabelProps, 'children'>;
 
 function DeveloperPreviewLabel({ className, ...props }: DeveloperPreviewLabelProps) {
     const popoverContent = (
-        <div>
-            Developer preview features are not intended to be used in production environments. The
-            clusters deployed with the developer preview features are considered to be development
-            cluster and are not supported through the Red Hat Customer Portal case management
-            system.
-        </div>
+        <Flex direction={{ default: 'column' }} spaceItems={{ default: 'spaceItemsXs' }}>
+            <FlexItem>
+                Developer preview features are not intended to be used in production environments.
+                The clusters deployed with the developer preview features are considered to be
+                development cluster and are not supported through the Red Hat Customer Portal case
+                management system.
+            </FlexItem>
+            <FlexItem>
+                <ExternalLink>
+                    <a
+                        href="https://access.redhat.com/articles/6966848"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        onClick={(e) => e.stopPropagation()}
+                    >
+                        Learn more
+                    </a>
+                </ExternalLink>
+            </FlexItem>
+        </Flex>
     );
 
     return (
