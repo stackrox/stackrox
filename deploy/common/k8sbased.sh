567378f0bb7316e8ce1fc9da642b9a455defb913
JoukoVirtanen <jouko@stackrox.com>
2025-09-19 22:03:56 -0700

    Trying to set dynamic config

--- deploy/common/k8sbased.sh
+++ deploy/common/k8sbased.sh
@@ -624,6 +624,7 @@ function launch_sensor {
     local extra_config=()
     local scanner_extra_config=()
     local extra_json_config=''
+    local extra_dynamic_json_config=''
     local extra_helm_config=()
 
     local collector_priority_class_name="stackrox-collector-dev"
@@ -653,6 +654,7 @@ function launch_sensor {
 
     if [[ "${SECURED_CLUSTER_AUTO_LOCK_PROCESS_BASELINES:-}" == "true" ]]; then
         extra_config+=("--auto-lock-process-baselines=true")
+        extra_dynamic_json_config+='"autoLockProcessBaselinesConfig": {"enabled": true}'
         extra_helm_config+=(--set "autoLockProcessBaselines.enabled=true")
     fi
 
@@ -896,6 +898,7 @@ function launch_sensor {
             echo >&2 "Please make sure to have a roxctl version ${MAIN_IMAGE_TAG} in PATH."
             exit 1
         fi
+	extra_json_config+='${extra_json_config}, "dynamicConfig": ${extra_dynamic_json_config}"
         get_cluster_zip "$API_ENDPOINT" "$CLUSTER" "${CLUSTER_TYPE}" "${MAIN_IMAGE}" "$CLUSTER_API_ENDPOINT" "$k8s_dir" "$COLLECTION_METHOD" "$extra_json_config"
         unzip "$k8s_dir/sensor-deploy.zip" -d "$k8s_dir/sensor-deploy"
         rm "$k8s_dir/sensor-deploy.zip"
