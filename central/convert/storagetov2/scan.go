8ea2572efa359397b5fc9793eac78020711006aa
Yann Brillouet <ybrillou@redhat.com>
2025-09-22 18:06:37 +0200

    remove unused conversion functions + update proto.lock

--- central/convert/storagetov2/scan.go
+++ central/convert/storagetov2/scan.go
@@ -5,44 +5,6 @@ import (
 	"github.com/stackrox/rox/generated/storage"
 )
 
-func ScanComponents(components []*storage.EmbeddedImageScanComponent) []*v2.ScanComponent {
-	if len(components) == 0 {
-		return nil
-	}
-
-	var ret []*v2.ScanComponent
-	for _, component := range components {
-		if component == nil {
-			continue
-		}
-		ret = append(ret, ScanComponent(component))
-	}
-
-	return ret
-}
-
-func ScanComponent(component *storage.EmbeddedImageScanComponent) *v2.ScanComponent {
-	if component == nil {
-		return nil
-	}
-
-	result := &v2.ScanComponent{
-		Name:         component.GetName(),
-		Version:      component.GetVersion(),
-		Vulns:        EmbeddedVulnerabilities(component.GetVulns()),
-		RiskScore:    component.GetRiskScore(),
-		Architecture: component.GetArchitecture(),
-	}
-
-	if component.GetTopCvss() != 0 {
-		result.SetTopCvss = &v2.ScanComponent_TopCvss{
-			TopCvss: component.GetTopCvss(),
-		}
-	}
-
-	return result
-}
-
 func EmbeddedVirtualMachineScanComponents(components []*storage.EmbeddedVirtualMachineScanComponent) []*v2.ScanComponent {
 	if components == nil {
 		return nil
