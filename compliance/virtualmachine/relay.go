4426495ff65742f5bc0045762ea78b63164a503a
Guzman <guz@redhat.com>
2025-09-23 15:16:17 +0200

    Retry failing calls to UpsertVirtualMachineIndexReport

--- compliance/virtualmachine/relay.go
+++ compliance/virtualmachine/relay.go
@@ -4,13 +4,17 @@ import (
 	"context"
 	"io"
 	"net"
+	"net/url"
 	"strconv"
 
+	"github.com/google/go-containerregistry/pkg/v1/remote/transport"
 	"github.com/mdlayher/vsock"
 	"github.com/pkg/errors"
 	"github.com/stackrox/rox/generated/internalapi/sensor"
 	v1 "github.com/stackrox/rox/generated/internalapi/virtualmachine/v1"
+	"github.com/stackrox/rox/pkg/errox"
 	"github.com/stackrox/rox/pkg/logging"
+	"github.com/stackrox/rox/pkg/retry"
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/proto"
 )
@@ -136,15 +140,42 @@ func sendReportToSensor(ctx context.Context, report *v1.IndexReport, sensorClien
 	req := &sensor.UpsertVirtualMachineIndexReportRequest{
 		IndexReport: report,
 	}
-	resp, err := sensorClient.UpsertVirtualMachineIndexReport(ctx, req)
-	if err != nil {
-		return errors.Wrap(err, "calling sensor VM index client")
-	}
-	if !resp.Success {
-		return errors.New("Sensor failed to handle virtual machine index report")
-	}
+	log.Debugf("Sending index report to sensor: %v", req)
 
-	log.Debugf("Successfully sent index report to sensor")
+	// Considering a timeout of 5 seconds and 10 tries with exponential backoff, the maximum time spent in this function
+	// is around 1 min 40 s. Given that each virtual machine sends an index report every 4 hours, these retries seem
+	// reasonable and are unlikely to cause issues.
+	err := retry.WithRetry(func() error {
+		resp, err := sensorClient.UpsertVirtualMachineIndexReport(ctx, req)
 
-	return nil
+		if resp != nil && !resp.Success {
+			// This can't happen as of this writing (Success is only false when an error is returned) but is
+			// theoretically possible, let's add retries too.
+			if err == nil {
+				err = retry.MakeRetryable(errors.New("Sensor failed to handle virtual machine index report"))
+			}
+		}
+
+		var transportErr *transport.Error
+		var urlError *url.Error
+		if errors.As(err, &transportErr) && transportErr.Temporary() {
+			return retry.MakeRetryable(err)
+		}
+		if errors.As(err, &urlError) && urlError.Temporary() {
+			return retry.MakeRetryable(err)
+		}
+		if errors.Is(err, errox.ResourceExhausted) {
+			return retry.MakeRetryable(err)
+		}
+		return err
+	},
+		retry.WithContext(ctx),
+		retry.OnFailedAttempts(func(e error) {
+			log.Warnf("Error sending index report to sensor, retrying. Error was: %v", e)
+		}),
+		retry.Tries(10), // With current wait values in exponential backoff logic, this takes around 50 s
+		retry.OnlyRetryableErrors(),
+		retry.WithExponentialBackoff())
+
+	return err
 }
--- compliance/virtualmachine/relay_test.go
+++ compliance/virtualmachine/relay_test.go
@@ -3,8 +3,10 @@ package virtualmachine
 import (
 	"context"
 	"testing"
+	"time"
 
 	v1 "github.com/stackrox/rox/generated/internalapi/virtualmachine/v1"
+	"github.com/stackrox/rox/pkg/errox"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 )
@@ -43,6 +45,54 @@ func (s *relayTestSuite) TestVsockConnectionHandlerRejectsMalformedData() {
 	require.Error(s.T(), err)
 
 	s.True(conn.closed, "connection should be closed after handling")
+}
+
+func (s *relayTestSuite) TestSendReportToSensorRetries() {
+	conn := s.defaultVsockConn()
+
+	cases := map[string]struct {
+		err         error
+		respSuccess bool
+		shouldRetry bool
+	}{
+		"retryable error is retried": {
+			err:         errox.ResourceExhausted,
+			respSuccess: false,
+			shouldRetry: true,
+		},
+		"nonretryable error is not retried": {
+			err:         errox.NotImplemented,
+			respSuccess: false,
+			shouldRetry: false,
+		},
+		"Unsuccessful request is retried": {
+			err:         nil,
+			respSuccess: false,
+			shouldRetry: true,
+		},
+	}
+	for name, c := range cases {
+		s.Run(name, func() {
+			client := newMockSensorClient().withError(c.err)
+			if !c.respSuccess {
+				client = client.withUnsuccessfulResponse()
+			}
+
+			// The retry logic uses withExponentialBackoff, which currently has an initial delay between retries of
+			// 100 ms, therefore after 500 ms the failing call has been retried already
+			ctx, cancel := context.WithTimeout(s.ctx, 500*time.Millisecond)
+			defer cancel()
+
+			err := handleVsockConnection(ctx, conn, client)
+
+			s.Error(err)
+			s.True(conn.closed, "connection should be closed after handling")
+
+			retried := len(client.capturedRequests) > 1
+
+			s.Equal(c.shouldRetry, retried)
+		})
+	}
 }
 
 func (s *relayTestSuite) defaultVsockConn() *mockVsockConn {
