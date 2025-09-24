ae48ae69334708012640774578b7332d86d12cac
JoukoVirtanen <jouko@stackrox.com>
2025-09-24 14:37:04 -0700

    Added unit tests

--- sensor/common/detector/baseline/baseline_test.go
+++ sensor/common/detector/baseline/baseline_test.go
@@ -35,6 +35,91 @@ func TestDeduplication(t *testing.T) {
 	assert.Equal(t, 2, entry.refCount)
 }
 
+func TestDeduplicationModifyBaseline(t *testing.T) {
+	// Test that optimized implementation actually deduplicates
+	optimized := newOptimizedBaselineEvaluator().(*optimizedBaselineEvaluator)
+
+	// Create two identical baselines
+	baseline1 := createTestBaseline("deployment-1", "container-1", 25)
+	baseline2 := createTestBaseline("deployment-2", "container-2", 25)
+	// Add a baseline for an existing container with one more process
+	baseline3 := createTestBaseline("deployment-1", "container-1", 26)
+
+	optimized.AddBaseline(baseline1)
+	optimized.AddBaseline(baseline2)
+	optimized.AddBaseline(baseline3)
+
+	// Should have 2 deployment entries and two baselines
+	assert.Equal(t, 2, len(optimized.deploymentBaselines))
+	assert.Equal(t, 2, len(optimized.processSets))
+
+	// Both deployments should reference different process sets
+	key1 := optimized.deploymentBaselines["deployment-1"]["container-1"]
+	key2 := optimized.deploymentBaselines["deployment-2"]["container-2"]
+	assert.NotEqual(t, key1, key2)
+
+	// Both process sets should have a ref count of 1
+	entry1 := optimized.processSets[key1]
+	assert.Equal(t, 1, entry1.refCount)
+	entry2 := optimized.processSets[key2]
+	assert.Equal(t, 1, entry2.refCount)
+
+	// The second container also gets the same process so they should
+	// share the same process set again
+	baseline4 := createTestBaseline("deployment-2", "container-2", 26)
+	optimized.AddBaseline(baseline4)
+
+	// Should have 2 deployment entries but only 1 process set
+	assert.Equal(t, 2, len(optimized.deploymentBaselines))
+	assert.Equal(t, 1, len(optimized.processSets))
+
+	// Both deployments should reference the same process set
+	key1 = optimized.deploymentBaselines["deployment-1"]["container-1"]
+	key2 = optimized.deploymentBaselines["deployment-2"]["container-2"]
+	assert.Equal(t, key1, key2)
+
+	// Process set should have reference count of 2
+	entry := optimized.processSets[key1] // key1 is now the content hash directly
+	assert.Equal(t, 2, entry.refCount)
+}
+
+func TestDeduplicationRemoveDeployment(t *testing.T) {
+	// Test that optimized implementation actually deduplicates
+	optimized := newOptimizedBaselineEvaluator().(*optimizedBaselineEvaluator)
+
+	// Create two identical baselines
+	baseline1 := createTestBaseline("deployment-1", "container-1", 25)
+	baseline2 := createTestBaseline("deployment-2", "container-2", 25)
+	baseline3 := createTestBaseline("deployment-1", "container-2", 25)
+
+	optimized.AddBaseline(baseline1)
+	optimized.AddBaseline(baseline2)
+	optimized.AddBaseline(baseline3)
+
+	// Should have 2 deployment entries but only 1 process set
+	assert.Equal(t, 2, len(optimized.deploymentBaselines))
+	assert.Equal(t, 1, len(optimized.processSets))
+
+	// Both deployments should reference the same process set
+	key1 := optimized.deploymentBaselines["deployment-1"]["container-1"]
+	key2 := optimized.deploymentBaselines["deployment-2"]["container-2"]
+	assert.Equal(t, key1, key2)
+
+	// Process set should have reference count of 3
+	entry := optimized.processSets[key1]
+	assert.Equal(t, 3, entry.refCount)
+
+	optimized.RemoveDeployment("deployment-1")
+
+	assert.Equal(t, 1, len(optimized.deploymentBaselines))
+	assert.Equal(t, 1, len(optimized.processSets))
+
+	// Process set should have reference count of 1
+	// because two have been removed
+	entry = optimized.processSets[key1]
+	assert.Equal(t, 1, entry.refCount)
+}
+
 func TestNilSafety(t *testing.T) {
 	// Test that both implementations handle nil ProcessIndicator safely
 	testCases := []struct {
