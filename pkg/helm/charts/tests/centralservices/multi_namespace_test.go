f52a4cd61803c5f0f0f9b9ba076d6ef37836421b
Michaël Petrov <github@parameta.lol>
2025-09-25 14:16:01 +0200

    tests

--- pkg/helm/charts/tests/centralservices/multi_namespace_test.go
+++ pkg/helm/charts/tests/centralservices/multi_namespace_test.go
@@ -32,6 +32,8 @@ var (
 	allowedGlobalResources = []string{
 		"/StorageClass/stackrox-gke-ssd",
 		"/CustomResourceDefinition/securitypolicies.config.stackrox.io",
+		"/ClusterRole/create-token-reviews",
+		"/ClusterRoleBinding/central-token-review",
 	}
 )
 
