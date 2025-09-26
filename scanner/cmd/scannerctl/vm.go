36db294a45e5b1841bba6a2677c1d9770a65c7d7
Stephan Heßelmann <shesselm@redhat.com>
2025-09-26 10:04:01 +0200

    add file input arg and rework a bit

--- scanner/cmd/scannerctl/vm.go
+++ scanner/cmd/scannerctl/vm.go
@@ -1,55 +1,66 @@
 package main
 
 import (
-	_ "embed"
 	"context"
-	"encoding/json"
+	_ "embed"
 	"fmt"
+	"os"
 
 	"github.com/google/go-containerregistry/pkg/name"
-	"github.com/stackrox/rox/pkg/jsonutil"
 	"github.com/spf13/cobra"
 	v4 "github.com/stackrox/rox/generated/internalapi/scanner/v4"
+	"github.com/stackrox/rox/pkg/jsonutil"
 )
 
 const mockDigest = "registry/repository@sha256:deadb33fdeadb33fdeadb33fdeadb33fdeadb33fdeadb33fdeadb33fdeadb33f"
 
-//go:embed fixtures/indexContents.json
-var contentsJson []byte
+//go:embed fixtures/indexReport.json
+var indexReportFixture []byte
 
 // scanCmd creates the scan command.
 func scanVM(ctx context.Context) *cobra.Command {
 	cmd := cobra.Command{
 		Use:   "vm",
-		Short: "Perform vulnerability scans.",
+		Short: "Perform vulnerability scans. Index report files can be provided as input.",
 		Args:  cobra.ExactArgs(0),
 	}
+	flags := cmd.PersistentFlags()
+	indexReportFlag := flags.String(
+		"index-report-file",
+		"",
+		"Path to the index report in json format. Defaults to 'fixtures/indexReport.json'.")
 	cmd.RunE = func(cmd *cobra.Command, args []string) error {
-		// Create scanner client.
 		scanner, err := factory.Create(ctx)
 		if err != nil {
-			return fmt.Errorf("create client: %w", err)
+			return fmt.Errorf("creating scanner client: %w", err)
 		}
 
 		digest, err := name.NewDigest(mockDigest)
 		if err != nil {
-			fmt.Printf("Failed to parse digest: %v\n", err)
+			return fmt.Errorf("parsing digest: %w", err)
 		}
 
-		var contents v4.Contents
-		if err := jsonutil.JSONBytesToProto(contentsJson, &contents); err != nil {
-			return fmt.Errorf("converting index contents to proto: %w", err)
+		indexReportJson := indexReportFixture
+		if *indexReportFlag != "" {
+			indexReportJson, err = os.ReadFile(*indexReportFlag)
+			if err != nil {
+				return fmt.Errorf("reading index report %q: %w", *indexReportFlag, err)
+			}
+		}
+		var indexReport v4.IndexReport
+		if err := jsonutil.JSONBytesToProto(indexReportJson, &indexReport); err != nil {
+			return fmt.Errorf("encoding index report: %w", err)
 		}
 
-		vr, err := scanner.GetVulnerabilities(ctx, digest, &contents)
+		vulnReport, err := scanner.GetVulnerabilities(ctx, digest, indexReport.GetContents())
 		if err != nil {
 			return fmt.Errorf("scanning: %w", err)
 		}
-		reportJSON, err := json.MarshalIndent(vr, "", "  ")
+		vulnReportJson, err := jsonutil.ProtoToJSON(vulnReport)
 		if err != nil {
-			return fmt.Errorf("decoding report: %w", err)
+			return fmt.Errorf("decoding vulnerability report: %w", err)
 		}
-		fmt.Println(string(reportJSON))
+		fmt.Println(vulnReportJson)
 		return nil
 	}
 	return &cmd
