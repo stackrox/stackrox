f7d90f5545cb6bc56c61c5eb4264ad28b08aff3e
Yann Brillouet <ybrillou@redhat.com>
2025-09-30 18:18:31 +0200

    restore more scan notes

--- central/convert/storagetov2/virtual_machine.go
+++ central/convert/storagetov2/virtual_machine.go
@@ -57,6 +57,10 @@ func convertVirtualMachineScanNote(note storage.VirtualMachineScan_Note) v2.Virt
 	switch note {
 	case storage.VirtualMachineScan_UNSET:
 		return v2.VirtualMachineScan_UNSET
+	case storage.VirtualMachineScan_OS_UNKNOWN:
+		return v2.VirtualMachineScan_OS_UNKNOWN
+	case storage.VirtualMachineScan_OS_UNSUPPORTED:
+		return v2.VirtualMachineScan_OS_UNSUPPORTED
 	default:
 		return v2.VirtualMachineScan_UNSET
 	}
