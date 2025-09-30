c2f361c7197cc76a85590b8b9e7c46f931e9a4d3
Kyle Lape <klape@redhat.com>
2025-09-30 08:47:35 -0500

    feat(vm4vm): Restore Virtual Machine Scan data #16969

--- central/convert/storagetov2/virtual_machine.go
+++ central/convert/storagetov2/virtual_machine.go
@@ -1,6 +1,7 @@
 package storagetov2
 
 import (
+	"github.com/stackrox/rox/central/convert/helpers"
 	v2 "github.com/stackrox/rox/generated/api/v2"
 	"github.com/stackrox/rox/generated/storage"
 )
@@ -19,6 +20,7 @@ func VirtualMachine(vm *storage.VirtualMachine) *v2.VirtualMachine {
 		VsockCid:    vm.GetVsockCid(),
 		State:       convertVirtualMachineState(vm.GetState()),
 		LastUpdated: vm.GetLastUpdated(),
+		Scan:        VirtualMachineScan(vm.GetScan()),
 	}
 }
 
@@ -32,5 +34,30 @@ func convertVirtualMachineState(state storage.VirtualMachine_State) v2.VirtualMa
 		return v2.VirtualMachine_RUNNING
 	default:
 		return v2.VirtualMachine_UNKNOWN
+	}
+}
+
+func VirtualMachineScan(scan *storage.VirtualMachineScan) *v2.VirtualMachineScan {
+	if scan == nil {
+		return nil
+	}
+	return &v2.VirtualMachineScan{
+		ScanTime:        scan.GetScanTime(),
+		OperatingSystem: scan.GetOperatingSystem(),
+		Notes:           VirtualMachineScanNotes(scan.GetNotes()),
+		Components:      EmbeddedVirtualMachineScanComponents(scan.GetComponents()),
+	}
+}
+
+func VirtualMachineScanNotes(notes []storage.VirtualMachineScan_Note) []v2.VirtualMachineScan_Note {
+	return helpers.ConvertEnumArray(notes, convertVirtualMachineScanNote)
+}
+
+func convertVirtualMachineScanNote(note storage.VirtualMachineScan_Note) v2.VirtualMachineScan_Note {
+	switch note {
+	case storage.VirtualMachineScan_UNSET:
+		return v2.VirtualMachineScan_UNSET
+	default:
+		return v2.VirtualMachineScan_UNSET
 	}
 }
