856da1b5d822f8ed55f5d2adf114d2ff598897fb
Charmik Sheth <101146970+charmik-redhat@users.noreply.github.com>
2025-10-02 01:28:52 -0700

    update reprocessor and detector

--- central/detection/service/singleton.go
+++ central/detection/service/singleton.go
@@ -29,6 +29,7 @@ func initialize() {
 		enrichment.ImageEnricherSingleton(),
 		enrichment.ImageEnricherV2Singleton(),
 		imageDatastore.Singleton(),
+		enrichment.ImageEnricherV2Singleton(),
 		manager.Singleton(),
 		enrichment.Singleton(),
 		buildTimeDetection.SingletonDetector(),
