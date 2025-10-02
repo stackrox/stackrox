ff88a000d2b5ee65fda20e0df1ffdea3591887b8
Stephan Heßelmann <shesselm@redhat.com>
2025-10-02 14:51:55 +0200

    print host path and add env var for mapping url

--- compliance/virtualmachines/agent/config/config.go
+++ compliance/virtualmachines/agent/config/config.go
@@ -3,10 +3,11 @@ package config
 import "time"
 
 type AgentConfig struct {
-	DaemonMode    bool
-	IndexHostPath string
-	IndexInterval time.Duration
-	Timeout       time.Duration
-	Verbose       bool
-	VsockPort     uint32
+	DaemonMode          bool
+	IndexHostPath       string
+	IndexInterval       time.Duration
+	RepoToCPEMappingURL string
+	Timeout             time.Duration
+	Verbose             bool
+	VsockPort           uint32
 }
