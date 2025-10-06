ae9c4db9622e059bd41b1faf633aa265210db0c9
Charmik Sheth <101146970+charmik-redhat@users.noreply.github.com>
2025-10-06 09:55:44 -0700

    ROX-30483: Temporaily fix multiple deployed images with same SHA issue in reports (#17059)

--- central/reports/scheduler/v2/reportgenerator/report_gen_bench_test.go
+++ central/reports/scheduler/v2/reportgenerator/report_gen_bench_test.go
@@ -80,7 +80,7 @@ func BenchmarkReportGenerator(b *testing.B) {
 		storage.VulnerabilityReportFilters_WATCHED,
 	}
 
-	expectedRowCount := 5000
+	expectedRowCount := 5002
 
 	reportSnap := testReportSnapshot(collection.GetId(), fixability, severities, imageTypes, nil)
 
