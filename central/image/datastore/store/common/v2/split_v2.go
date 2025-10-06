be9024062fce63fcd622517f589c395e70fce03e
Dave Shrewsberry <dashrews@redhat.com>
2025-10-06 15:54:02 -0400

    reverting ID back to index based

--- central/image/datastore/store/common/v2/split_v2.go
+++ central/image/datastore/store/common/v2/split_v2.go
@@ -35,8 +35,8 @@ func SplitV2(image *storage.Image, withComponents bool) (ImageParts, error) {
 func splitComponentsV2(parts ImageParts) ([]ComponentParts, error) {
 	ret := make([]ComponentParts, 0, len(parts.Image.GetScan().GetComponents()))
 	componentMap := make(map[string]*storage.EmbeddedImageScanComponent)
-	for _, component := range parts.Image.GetScan().GetComponents() {
-		generatedComponentV2, err := GenerateImageComponentV2(parts.Image.GetScan().GetOperatingSystem(), parts.Image, component)
+	for index, component := range parts.Image.GetScan().GetComponents() {
+		generatedComponentV2, err := GenerateImageComponentV2(parts.Image.GetScan().GetOperatingSystem(), parts.Image, index, component)
 		if err != nil {
 			return nil, err
 		}
@@ -68,8 +68,8 @@ func splitComponentsV2(parts ImageParts) ([]ComponentParts, error) {
 func splitCVEsV2(imageID string, componentID string, embedded *storage.EmbeddedImageScanComponent) ([]CVEParts, error) {
 	ret := make([]CVEParts, 0, len(embedded.GetVulns()))
 	cveMap := make(map[string]*storage.EmbeddedVulnerability)
-	for _, cve := range embedded.GetVulns() {
-		convertedCVE, err := utils.EmbeddedVulnerabilityToImageCVEV2(imageID, componentID, cve)
+	for index, cve := range embedded.GetVulns() {
+		convertedCVE, err := utils.EmbeddedVulnerabilityToImageCVEV2(imageID, componentID, index, cve)
 		if err != nil {
 			return nil, err
 		}
@@ -92,11 +92,8 @@ func splitCVEsV2(imageID string, componentID string, embedded *storage.EmbeddedI
 }
 
 // GenerateImageComponentV2 returns top-level image component from embedded component.
-func GenerateImageComponentV2(os string, image *storage.Image, from *storage.EmbeddedImageScanComponent) (*storage.ImageComponentV2, error) {
-	componentID, err := scancomponent.ComponentIDV2(from, image.GetId())
-	if err != nil {
-		return nil, err
-	}
+func GenerateImageComponentV2(os string, image *storage.Image, index int, from *storage.EmbeddedImageScanComponent) (*storage.ImageComponentV2, error) {
+	componentID := scancomponent.ComponentIDV2(from, image.GetId(), index)
 
 	ret := &storage.ImageComponentV2{
 		Id:              componentID,
