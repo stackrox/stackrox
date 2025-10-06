ab68c448ab962f7c62d9e5ba3f250753fc44a1ba
Dave Shrewsberry <dashrews@redhat.com>
2025-10-06 10:47:17 -0400

    more deletions

--- central/componentcveedge/datastore/store/postgres/gen.go
+++
@@ -1,3 +0,0 @@
-package postgres
-
-//go:generate pg-table-bindings-wrapper --type=storage.ComponentCVEEdge --table=image_component_cve_edges --references=storage.ImageComponent,image_cves:storage.ImageCVE --schema-only
