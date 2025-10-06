aa9d740a623d9b3211b76cab0c915d4d24f00c9b
RTann <rtannenb@redhat.com>
2025-10-06 12:07:29 -0700

    update pipeline test

--- central/sensor/service/pipeline/nodeindex/pipeline_test.go
+++ central/sensor/service/pipeline/nodeindex/pipeline_test.go
@@ -1,7 +1,6 @@
 package nodeindex
 
 import (
-	"context"
 	"testing"
 
 	clusterDatastoreMocks "github.com/stackrox/rox/central/cluster/datastore/mocks"
@@ -22,7 +21,7 @@ func TestPipelineWithEmptyIndex(t *testing.T) {
 	p := &pipelineImpl{}
 	expectedError := "unexpected resource type"
 
-	err := p.Run(context.Background(), "", nil, nil)
+	err := p.Run(t.Context(), "", nil, nil)
 
 	assert.Contains(t, err.Error(), expectedError)
 }
@@ -30,13 +29,18 @@ func TestPipelineWithEmptyIndex(t *testing.T) {
 func TestPipelineWithIncorrectAction(t *testing.T) {
 	t.Setenv(features.NodeIndexEnabled.EnvVar(), "true")
 	t.Setenv(features.ScannerV4.EnvVar(), "true")
-	p := &pipelineImpl{}
-	msg := createMsg()
-	msg.GetEvent().Action = central.ResourceAction_REMOVE_RESOURCE
+	for _, ir := range []*v4.IndexReport{
+		mockIndexReport,
+		mockDeprecatedIndexReport,
+	} {
+		p := &pipelineImpl{}
+		msg := createMsg(ir)
+		msg.GetEvent().Action = central.ResourceAction_REMOVE_RESOURCE
 
-	err := p.Run(context.Background(), "", msg, nil)
+		err := p.Run(t.Context(), "", msg, nil)
 
-	assert.Nil(t, err)
+		assert.NoError(t, err)
+	}
 }
 
 func TestPipelineEnrichesAndUpserts(t *testing.T) {
@@ -48,41 +52,46 @@ func TestPipelineEnrichesAndUpserts(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	clusterStore := clusterDatastoreMocks.NewMockDataStore(ctrl)
 	nodeDatastore := nodeDatastoreMocks.NewMockDataStore(ctrl)
-	nodeDatastore.EXPECT().GetNode(gomock.Any(), gomock.Eq("1")).Times(1).Return(&node, true, nil)
+	nodeDatastore.EXPECT().GetNode(gomock.Any(), gomock.Eq("1")).Times(2).Return(&node, true, nil)
 	riskManager := riskManagerMocks.NewMockManager(ctrl)
-	riskManager.EXPECT().CalculateRiskAndUpsertNode(gomock.Any()).Times(1).Return(nil)
+	riskManager.EXPECT().CalculateRiskAndUpsertNode(gomock.Any()).Times(2).Return(nil)
 	enricher := nodesEnricherMocks.NewMockNodeEnricher(ctrl)
-	enricher.EXPECT().EnrichNodeWithVulnerabilities(gomock.Any(), nil, gomock.Any()).Times(1).Return(nil)
+	enricher.EXPECT().EnrichNodeWithVulnerabilities(gomock.Any(), nil, gomock.Any()).Times(2).Return(nil)
 
-	p := &pipelineImpl{
-		clusterStore:  clusterStore,
-		nodeDatastore: nodeDatastore,
-		riskManager:   riskManager,
-		enricher:      enricher,
+	for _, ir := range []*v4.IndexReport{
+		mockIndexReport,
+		mockDeprecatedIndexReport,
+	} {
+		p := &pipelineImpl{
+			clusterStore:  clusterStore,
+			nodeDatastore: nodeDatastore,
+			riskManager:   riskManager,
+			enricher:      enricher,
+		}
+		msg := createMsg(ir)
+		msg.GetEvent().Action = central.ResourceAction_UNSET_ACTION_RESOURCE
+
+		err := p.Run(t.Context(), "", msg, nil)
+
+		assert.NoError(t, err)
 	}
-	msg := createMsg()
-	msg.GetEvent().Action = central.ResourceAction_UNSET_ACTION_RESOURCE
-
-	err := p.Run(context.Background(), "", msg, nil)
-
-	assert.Nil(t, err)
 }
 
-func createMsg() *central.MsgFromSensor {
+func createMsg(ir *v4.IndexReport) *central.MsgFromSensor {
 	return &central.MsgFromSensor{
 		Msg: &central.MsgFromSensor_Event{
 			Event: &central.SensorEvent{
 				Id: "1",
 				Resource: &central.SensorEvent_IndexReport{
-					IndexReport: createIndexReport(),
+					IndexReport: ir,
 				},
 			},
 		},
 	}
 }
 
-func createIndexReport() *v4.IndexReport {
-	return &v4.IndexReport{
+var (
+	mockIndexReport = &v4.IndexReport{
 		HashId:  "",
 		State:   "7", // IndexFinished
 		Success: true,
@@ -141,10 +150,76 @@ func createIndexReport() *v4.IndexReport {
 				{
 					PackageDb:     "sqlite:usr/share/rpm",
 					IntroducedIn:  "sha256:aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+					RepositoryIds: []string{"cpe:/o:redhat:enterprise_linux:9::fastdatapath", "cpe:/a:redhat:openshift:4.16::el9"},
+				},
+			},
+			}},
+		},
+	}
+
+	mockDeprecatedIndexReport = &v4.IndexReport{
+		HashId:  "",
+		State:   "7", // IndexFinished
+		Success: true,
+		Err:     "",
+		Contents: &v4.Contents{
+			PackagesDEPRECATED: []*v4.Package{
+				{
+					Id:      "0",
+					Name:    "openssh-clients",
+					Version: "8.7p1-38.el9",
+					Kind:    "binary",
+					Source: &v4.Package{
+						Name:    "openssh",
+						Version: "8.7p1-38.el9",
+						Kind:    "source",
+						Source:  nil,
+						Cpe:     "cpe:2.3:*:*:*:*:*:*:*:*:*:*:*",
+					},
+					PackageDb:      "sqlite:usr/share/rpm",
+					RepositoryHint: "hash:sha256:f52ca767328e6919ec11a1da654e92743587bd3c008f0731f8c4de3af19c1830|key:199e2f91fd431d51",
+					Arch:           "x86_64",
+					Cpe:            "cpe:2.3:*:*:*:*:*:*:*:*:*:*:*",
+				},
+				{
+					Id:      "1",
+					Name:    "skopeo",
+					Version: "2:1.14.4-2.rhaos4.16.el9",
+					Kind:    "binary",
+					Source: &v4.Package{
+						Name:    "skopeo",
+						Version: "2:1.14.4-2.rhaos4.16.el9",
+						Kind:    "source",
+						Cpe:     "cpe:2.3:*:*:*:*:*:*:*:*:*:*:*",
+					},
+					PackageDb:      "sqlite:usr/share/rpm",
+					RepositoryHint: "hash:sha256:072a75d1b9b36457751ef05031fd69615f21ebaa935c30d74d827328b78fa694|key:199e2f91fd431d51",
+					Arch:           "x86_64",
+					Cpe:            "cpe:2.3:*:*:*:*:*:*:*:*:*:*:*",
+				},
+			},
+			RepositoriesDEPRECATED: []*v4.Repository{
+				{
+					Id:   "0",
+					Name: "cpe:/o:redhat:enterprise_linux:9::fastdatapath",
+					Key:  "rhel-cpe-repository",
+					Cpe:  "cpe:2.3:o:redhat:enterprise_linux:9:*:fastdatapath:*:*:*:*:*",
+				},
+				{
+					Id:   "1",
+					Name: "cpe:/a:redhat:openshift:4.16::el9",
+					Key:  "rhel-cpe-repository",
+					Cpe:  "cpe:2.3:a:redhat:openshift:4.16:*:el9:*:*:*:*:*",
+				},
+			},
+			EnvironmentsDEPRECATED: map[string]*v4.Environment_List{"1": {Environments: []*v4.Environment{
+				{
+					PackageDb:     "sqlite:usr/share/rpm",
+					IntroducedIn:  "sha256:aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
 					RepositoryIds: []string{"0", "1"},
 				},
 			},
 			}},
 		},
 	}
-}
+)
