1efcb8f0fc249c7c86afcb7663289432367e0d26
Tomasz Janiszewski <tomek@redhat.com>
2025-10-06 13:36:46 +0200

    Apply suggestion from @parametalol
    Co-authored-by: Michaël <github@parameta.lol>

--- sensor/common/clusterentities/store_ips_test.go
+++ sensor/common/clusterentities/store_ips_test.go
@@ -371,7 +371,7 @@ func (s *ClusterEntitiesStoreTestSuite) TestMemoryAboutPastIPs() {
 				s.T().Logf("IP listener (tick %d): %s", tickNo, ipListener.String())
 				// convert to slice of strings to enable using Contains assertion
 				var historicalIPs []string
-				for address := range maps.Keys(store.podIPsStore.historicalIPs) {
+				for address := range store.podIPsStore.historicalIPs {
 					historicalIPs = append(historicalIPs, address.String())
 				}
 				var currentIPs []string
