ad3b70131080db94ddeaec154fe7168392c16066
Charmik Sheth <101146970+charmik-redhat@users.noreply.github.com>
2025-10-06 14:35:13 -0700

    Add debug logs

--- sensor/kubernetes/listener/resources/convert.go
+++ sensor/kubernetes/listener/resources/convert.go
@@ -360,6 +360,7 @@ func (w *deploymentWrap) populateImageMetadata(localImages set.StringSet, pods .
 				image.IsClusterLocal = localImages.Contains(image.GetName().GetFullName())
 				updateImageWithNewerImageName(image, runtimeImageName, true)
 				if features.FlattenImageData.Enabled() {
+					log.Infof("Setting imagev2 id for name %s and digest %s", image.GetName().GetFullName(), image.GetId())
 					image.IdV2 = imageUtils.NewImageV2ID(image.GetName(), image.GetId())
 				}
 				continue
@@ -383,6 +384,7 @@ func (w *deploymentWrap) populateImageMetadata(localImages set.StringSet, pods .
 				image.IsClusterLocal = localImages.Contains(image.GetName().GetFullName())
 				updateImageWithNewerImageName(image, runtimeImageName, false)
 				if features.FlattenImageData.Enabled() {
+					log.Infof("Setting imagev2 id for name %s and digest %s", image.GetName().GetFullName(), digest)
 					image.IdV2 = imageUtils.NewImageV2ID(image.GetName(), digest)
 				}
 			}
