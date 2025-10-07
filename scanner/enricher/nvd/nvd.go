d8da5c09366ef2cd2a021092a24d84b86dff4d34
Tomasz Janiszewski <tomek@redhat.com>
2025-10-07 20:47:52 +0200

    fix(nvd): close response on error
    https://github.com/stackrox/stackrox/pull/17129/files#r2410915841

--- scanner/enricher/nvd/nvd.go
+++ scanner/enricher/nvd/nvd.go
@@ -422,7 +422,7 @@ func (e *Enricher) tryQuery(ctx context.Context, req *http.Request) (*http.Respo
 		Str("nvd_message", req.Header.Get("message")).
 		Msg("NVD response")
 	if resp.StatusCode != 200 {
-		_ = resp.Body.Close
+		_ = resp.Body.Close()
 		return nil, fmt.Errorf("unexpected status code when querying %s: %d", req.URL.String(), resp.StatusCode)
 	}
 	return resp, nil
