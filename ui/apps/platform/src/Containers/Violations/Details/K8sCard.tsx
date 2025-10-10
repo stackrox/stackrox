f14f2b658388cfd295f145598501af824c9ee7a4
Mark Pedrotti <pedrottimark@gmail.com>
2025-10-10 15:48:33 -0400

    ROX-31210: Use import type in Containers Violations (#17203)

--- ui/apps/platform/src/Containers/Violations/Details/K8sCard.tsx
+++ ui/apps/platform/src/Containers/Violations/Details/K8sCard.tsx
@@ -1,4 +1,5 @@
-import React, { ReactElement, useState } from 'react';
+import React, { useState } from 'react';
+import type { ReactElement } from 'react';
 import capitalize from 'lodash/capitalize';
 import {
     Card,
