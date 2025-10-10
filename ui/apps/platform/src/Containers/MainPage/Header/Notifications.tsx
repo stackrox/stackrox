73998037bd0e3a6a480e981c22888d606e7eb1dd
Mark Pedrotti <pedrottimark@gmail.com>
2025-10-10 15:46:19 -0400

    ROX-31208: Use import type in Containers MainPage (#17199)

--- ui/apps/platform/src/Containers/MainPage/Header/Notifications.tsx
+++ ui/apps/platform/src/Containers/MainPage/Header/Notifications.tsx
@@ -1,4 +1,5 @@
-import React, { ReactElement } from 'react';
+import React from 'react';
+import type { ReactElement } from 'react';
 import { useSelector } from 'react-redux';
 import { ToastContainer, toast } from 'react-toastify';
 
