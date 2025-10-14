126fbb9eeb6f2559ac4ed96f50856e137cd87171
Dave Shrewsberry <dashrews@redhat.com>
2025-10-14 10:25:21 -0400

    test fix

---
+++ pkg/cve/cve_test.go
@@ -0,0 +1,14 @@
+package cve
+
+import (
+	_ "embed"
+	"testing"
+
+	"github.com/stackrox/rox/generated/storage"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCVETypesAreAccountedFor(t *testing.T) {
+	// + 1 for unknown type
+	assert.Equal(t, len(storage.CVE_CVEType_name), len(clusterCVETypes)+len(componentCVETypes)+1)
+}
