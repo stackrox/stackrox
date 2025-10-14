6ca7147ad0942208417d6d4821c06c461076494d
Marcin Owsiany <porridge@redhat.com>
2025-10-14 14:50:26 +0200

    tests: introduce a TestSet class
    This deserves to be a first class citizen. Also, segregates
    responsibility between this and the runner class better.

--- .openshift-ci/runners.py
+++ .openshift-ci/runners.py
@@ -11,6 +11,56 @@ from ci_tests import NullTest
 from post_tests import NullPostTest
 
 
+class TestSet:
+    """Represents a test set, i.e. a test with pre-test and post-test logic."""
+
+    def __init__(self, name, pre=None, test=None, post=None, always_run=True):
+        self._name = name
+        self._pre_test = pre if pre else NullPreTest()
+        self._actual_test = test if test else NullTest()
+        self._post_test = post if post else NullPostTest()
+        self._always_run = always_run
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def always_run(self):
+        return self._always_run
+
+    def run(self):
+        exception = None
+        try:
+            log_event("About to run pre test", self)
+            self._pre_test.run()
+            log_event("pre test completed", self)
+        except Exception as err:
+            log_event(f"ERROR: pre test failed [{err}]", self)
+            exception = err
+        if exception is None:
+            try:
+                log_event("About to run test", self)
+                self._actual_test.run()
+                log_event("test completed", self)
+            except Exception as err:
+                log_event(f"ERROR: test failed [{err}]", self)
+                exception = err
+            try:
+                log_event("About to run post test", self)
+                self._post_test.run(
+                    test_outputs=self._actual_test.test_outputs,
+                )
+                log_event("post test completed", self)
+            except Exception as err:
+                log_event(f"ERROR: post test failed [{err}]", self)
+                if exception is None:
+                    exception = err
+
+        if exception is not None:
+            raise exception
+
+
 class ClusterTestSetsRunner:
     """A cluster test runner that runs multiple sets of pre, test & post steps
     wrapped by a cluster provision and with similar semantics to
@@ -37,41 +87,31 @@ class ClusterTestSetsRunner:
 
         if exception is None:
             for idx, test_set in enumerate(self.sets):
-                test_set = {
-                    **{
-                        "name": f"set {idx + 1}",
-                        "pre_test": NullPreTest(),
-                        "test": NullTest(),
-                        "post_test": NullPostTest(),
-                        "always_run": True,
-                    },
-                    **test_set,
-                }
-                if exception is None or test_set["always_run"]:
+                if exception is None or test_set.always_run:
                     try:
-                        self.log_event("About to run", test_set)
-                        self.run_test_set(test_set)
-                        self.log_event("run completed", test_set)
+                        log_event("About to run", test_set)
+                        test_set.run()
+                        log_event("run completed", test_set)
                     except Exception as err:
-                        self.log_event(f"ERROR: run failed [{err}]", test_set)
+                        log_event(f"ERROR: run failed [{err}]", test_set)
                         if exception is None:
                             exception = err
 
         try:
-            self.log_event("About to teardown")
+            log_event("About to teardown")
             self.cluster.teardown()
-            self.log_event("teardown completed")
+            log_event("teardown completed")
         except Exception as err:
-            self.log_event(f"ERROR: teardown failed [{err}]")
+            log_event(f"ERROR: teardown failed [{err}]")
             if exception is None:
                 exception = err
 
         try:
-            self.log_event("About to run final post")
+            log_event("About to run final post")
             self.final_post.run()
-            self.log_event("final post completed")
+            log_event("final post completed")
         except Exception as err:
-            self.log_event(f"ERROR: final post failed [{err}]")
+            log_event(f"ERROR: final post failed [{err}]")
             if exception is None:
                 exception = err
 
@@ -81,65 +121,35 @@ class ClusterTestSetsRunner:
     def cluster_provision(self):
         exception = None
         try:
-            self.log_event("About to provision")
+            log_event("About to provision")
             self.cluster.provision()
-            self.log_event("provisioned")
+            log_event("provisioned")
         except Exception as err:
-            self.log_event(f"ERROR: provision failed [{err}]")
+            log_event(f"ERROR: provision failed [{err}]")
             exception = err
         return exception
 
     def run_initial_pre_test(self, exception):
         if exception is None:
             try:
-                self.log_event("About to run initial pre test")
+                log_event("About to run initial pre test")
                 self.initial_pre_test.run()
-                self.log_event("initial pre test completed")
+                log_event("initial pre test completed")
             except Exception as err:
-                self.log_event(f"ERROR: initial pre test failed [{err}]")
+                log_event(f"ERROR: initial pre test failed [{err}]")
                 exception = err
         return exception
 
-    def run_test_set(self, test_set):
-        exception = None
-        try:
-            self.log_event("About to run pre test", test_set)
-            test_set["pre_test"].run()
-            self.log_event("pre test completed", test_set)
-        except Exception as err:
-            self.log_event(f"ERROR: pre test failed [{err}]", test_set)
-            exception = err
-        if exception is None:
-            try:
-                self.log_event("About to run test", test_set)
-                test_set["test"].run()
-                self.log_event("test completed", test_set)
-            except Exception as err:
-                self.log_event(f"ERROR: test failed [{err}]", test_set)
-                exception = err
-            try:
-                self.log_event("About to run post test", test_set)
-                test_set["post_test"].run(
-                    test_outputs=test_set["test"].test_outputs,
-                )
-                self.log_event("post test completed", test_set)
-            except Exception as err:
-                self.log_event(f"ERROR: post test failed [{err}]", test_set)
-                if exception is None:
-                    exception = err
 
-        if exception is not None:
-            raise exception
-
-    def log_event(self, msg, test_set=None):
-        now = datetime.now()
-        time = now.strftime("%H:%M:%S")
-        marker = "****"
-        if test_set is not None and test_set["name"] is not None:
-            msg = f"{msg} [{test_set['name']}]"
-        print(marker)
-        print(f"{marker} {time}: {msg}")
-        print(marker)
+def log_event(msg, test_set=None):
+    now = datetime.now()
+    time = now.strftime("%H:%M:%S")
+    marker = "****"
+    if test_set and test_set.name:
+        msg = f"{msg} [{test_set.name}]"
+    print(marker)
+    print(f"{marker} {time}: {msg}")
+    print(marker)
 
 
 # pylint: disable=too-many-arguments
@@ -162,12 +172,5 @@ class ClusterTestRunner(ClusterTestSetsRunner):
         super().__init__(
             cluster=cluster,
             final_post=final_post,
-            sets=[
-                {
-                    "name": None,
-                    "pre_test": pre_test,
-                    "test": test,
-                    "post_test": post_test,
-                }
-            ],
+            sets=[TestSet(name=None, pre=pre_test, test=test, post=post_test)],
         )
