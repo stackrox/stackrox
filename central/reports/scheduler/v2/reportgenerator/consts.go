d4f6adaf941024a3b55b7ba9d65b0dcb67546fcf
Surabhi Kumar <surakuma@redhat.com>
2025-10-14 18:12:55 -0700

    paginate select query and build cve responses list

--- central/reports/scheduler/v2/reportgenerator/consts.go
+++ central/reports/scheduler/v2/reportgenerator/consts.go
@@ -8,4 +8,10 @@ const (
 		"Please review the vulnerable software packages/components from the impacted images and update them to a version containing the fix, if one is available.\n"
 
 	defaultNoVulnsEmailBodyTemplate = "{{.BrandedPrefix}} for Kubernetes has found no workload CVEs in the images matched by the following report configuration parameters.\n"
+
+	// Maximum number of CVE results to fetch per page
+	DefaultCVEPageSize = 1000
+
+	// Maximum number of pages to process before switching to streaming
+	MaxPagesBeforeStream = 100
 )
