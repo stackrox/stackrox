6c53fb79641829e25c692fb856280b61abdc717b
Giles Hutton <ghutton@redhat.com>
2025-10-15 11:07:28 +0100

    Fix protolock and test

--- pkg/alert/convert/convert_test.go
+++ pkg/alert/convert/convert_test.go
@@ -11,11 +11,13 @@ func TestAlertAndListAlertResourceTypesAreInSync(t *testing.T) {
 	assert.Equal(t, storage.ListAlert_ResourceType_name[0], "DEPLOYMENT")
 	assert.Equal(t, storage.Alert_Resource_ResourceType_name[0], "UNKNOWN")
 
-	assert.Equal(t, len(storage.Alert_Resource_ResourceType_value), len(storage.ListAlert_ResourceType_value))
+	// Ignore ListAlert_HOST in this assertion.
+	assert.Equal(t, len(storage.Alert_Resource_ResourceType_value), len(storage.ListAlert_ResourceType_value)-1)
 	for i, at := range storage.Alert_Resource_ResourceType_name {
 		if r := storage.Alert_Resource_ResourceType(i); r == storage.Alert_Resource_UNKNOWN {
 			continue
 		}
+
 		assert.Contains(t, storage.ListAlert_ResourceType_value, at)
 		assert.Equal(t, at, storage.ListAlert_ResourceType_name[i])
 	}
