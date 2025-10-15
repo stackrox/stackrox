2431bb227365cacafdd606859a1eb6229be92e6d
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2025-10-15 11:45:29 +0200

    Start queue in running state

--- sensor/common/detector/queue/queue.go
+++ sensor/common/detector/queue/queue.go
@@ -47,6 +47,7 @@ func NewQueue[T comparable](stopper concurrency.Stopper, name string, size int, 
 
 // Start the queue.
 func (q *Queue[T]) Start() {
+	q.isRunning.Signal() // Start in running state
 	go q.run()
 }
 
