fb11992d877ecbc247dd65e2bb963007d8c1de4c
Mark Pedrotti <pedrottimark@gmail.com>
2025-10-15 16:47:19 -0400

    ROX-31255: Add no-default-import-react and ignores (#17263)

--- ui/apps/platform/eslint.config.js
+++ ui/apps/platform/eslint.config.js
@@ -795,6 +795,75 @@ module.exports = [
         },
     },
     {
+        files: ['**/*.{js,jsx,ts,tsx}'],
+        ignores: [
+            'src/Components/**',
+            'src/ConsolePlugin/**',
+            'src/Containers/AccessControl/**',
+            'src/Containers/Administration/**',
+            'src/Containers/Audit/**',
+            'src/Containers/Clusters/**',
+            'src/Containers/Collections/**',
+            'src/Containers/Compliance/**', // deprecated
+            'src/Containers/ComplianceEnhanced/**',
+            'src/Containers/ConfigManagement/**',
+            'src/Containers/Dashboard/**',
+            'src/Containers/Docs/**',
+            'src/Containers/ExceptionConfiguration/**',
+            'src/Containers/Images/**',
+            'src/Containers/Integrations/**',
+            'src/Containers/Login/**',
+            'src/Containers/MainPage/**',
+            'src/Containers/MitreAttackVectors/**',
+            'src/Containers/NetworkGraph/**',
+            'src/Containers/Policies/**',
+            'src/Containers/PolicyCategories/**',
+            'src/Containers/PolicyManagement/**',
+            'src/Containers/Risk/**',
+            'src/Containers/Search/**',
+            'src/Containers/SystemConfig/**',
+            'src/Containers/SystemHealth/**',
+            'src/Containers/User/**',
+            'src/Containers/Violations/**',
+            'src/Containers/VulnMgmt/**', // deprecated
+            'src/Containers/Vulnerabilities/**',
+            'src/Containers/Workflow/**', // deprecated
+            'src/Containers/*.{js,jsx,ts,tsx}',
+            'src/constants/**',
+            'src/hooks/**',
+            'src/providers/**',
+            'src/test-utils/**',
+            'src/utils/**',
+            'src/*.{js,jsx,ts,tsx}',
+        ],
+
+        // After deprecated folders have been deleted:
+        // Move jsxPragma property to languageOptions at module scope.
+        // Move react rules into appropriate configuration objects.
+
+        // Set parserOptions and turn off rules explicitly,
+        // instead of implicitlu via jsx-runtime configuration of eslint-plugin-react package.
+
+        languageOptions: {
+            parserOptions: {
+                // https://typescript-eslint.io/packages/parser/#jsxpragma
+                // If you are using the new JSX transform you can set this to null.
+                jsxPragma: null,
+            },
+        },
+
+        plugins: {
+            limited: pluginLimited,
+            react: pluginReact,
+        },
+        rules: {
+            // After ignores array consists only of deprecated folders, delete limited rule (which has autofix).
+            'limited/no-default-import-react': 'error',
+            'react/jsx-uses-react': 'off',
+            'react/react-in-jsx-scope': 'off',
+        },
+    },
+    {
         files: ['*.js', 'tailwind-plugins/*.js'], // non-product files
 
         languageOptions: {
