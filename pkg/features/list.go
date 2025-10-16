8fdd3a26a348a73770ed5f4993052a894ad7958e
JoukoVirtanen <jouko@stackrox.com>
2025-10-15 22:22:27 -0700

    Not enabling the feature by default

--- pkg/features/list.go
+++ pkg/features/list.go
@@ -127,7 +127,7 @@ var (
 	AutoLockProcessBaselines = registerFeature("Locks process baselines when their deployments leave the observation period", "ROX_AUTO_LOCK_PROCESS_BASELINES", enabled)
 
 	// SFA enables monitoring of sensitive files.
-	SensitiveFileActivity = registerFeature("Enable sensitive file monitoring", "ROX_SENSITIVE_FILE_ACTIVITY", enabled) // Enabling temporarily for easier testing don't merge this way
+	SensitiveFileActivity = registerFeature("Enable sensitive file monitoring", "ROX_SENSITIVE_FILE_ACTIVITY")
 )
 
 // The following feature flags are related to Scanner V4.
