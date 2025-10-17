313e95cb323ad5a441ce436c327d5e58bd816f8c
AJ Heflin <aheflin@redhat.com>
2025-10-17 14:31:31 -0400

    Add migration for deployment containers inside of alerts

---
+++ migrator/migrations/m_214_to_m_215_populate_alert_deployment_containers_imageidv2/migration_impl.go
@@ -0,0 +1,54 @@
+package m214tom215
+
+import (
+	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/migrator/migrations/m_214_to_m_215_populate_alert_deployment_containers_imageidv2/schema"
+	alertStore "github.com/stackrox/rox/migrator/migrations/m_214_to_m_215_populate_alert_deployment_containers_imageidv2/store"
+	"github.com/stackrox/rox/migrator/types"
+	"github.com/stackrox/rox/pkg/postgres/pgutils"
+	"github.com/stackrox/rox/pkg/search"
+	"github.com/stackrox/rox/pkg/uuid"
+)
+
+// If so increment the `MinimumSupportedDBVersionSeqNum` to the `CurrentDBVersionSeqNum` of the release immediately
+// following the release that cannot tolerate the change in pkg/migrations/internal/fallback_seq_num.go.
+//
+// For example, in 4.2 a column `column_v2` is added to replace the `column_v1` column in 4.1.
+// All the code from 4.2 onward will not reference `column_v1`. At some point in the future a rollback to 4.1
+// will not longer be supported and we want to remove `column_v1`. To do so, we will upgrade the schema to remove
+// the column and update the `MinimumSupportedDBVersionSeqNum` to be the value of `CurrentDBVersionSeqNum` in 4.2
+// as 4.1 will no longer be supported. The migration process will inform the user of an error when trying to migrate
+// to a software version that can no longer be supported by the database.
+
+func migrate(database *types.Databases) error {
+	// Use databases.DBCtx to take advantage of the transaction wrapping present in the migration initiator
+	pgutils.CreateTableFromModel(database.DBCtx, database.GormDB, schema.CreateTableAlertsStmt)
+
+	db := database.PostgresDB
+	store := alertStore.New(db)
+
+	page := 0
+	pageSize := 10000
+	for {
+		batch := make([]*storage.Alert, 0, pageSize)
+		pagination := search.NewPagination().Limit(int32(pageSize)).Offset(int32(page * pageSize))
+		query := search.NewQueryBuilder().WithPagination(pagination).ProtoQuery()
+		_ = store.WalkByQuery(database.DBCtx, query, func(alert *storage.Alert) error {
+			for _, container := range alert.GetDeployment().GetContainers() {
+				container.GetImage().IdV2 = uuid.NewV5FromNonUUIDs(container.GetImage().GetName().GetFullName(), container.GetImage().GetId()).String()
+			}
+			batch = append(batch, alert)
+			return nil
+		})
+		err := store.UpsertMany(database.DBCtx, batch)
+		if err != nil {
+			return err
+		}
+		if len(batch) < pageSize {
+			break
+		}
+		page++
+	}
+
+	return nil
+}
---
+++ migrator/migrations/m_214_to_m_215_populate_alert_deployment_containers_imageidv2/migration_test.go
@@ -0,0 +1,160 @@
+//go:build sql_integration
+
+package m214tom215
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/migrator/migrations/m_214_to_m_215_populate_alert_deployment_containers_imageidv2/schema"
+	"github.com/stackrox/rox/migrator/migrations/m_214_to_m_215_populate_alert_deployment_containers_imageidv2/store"
+	pghelper "github.com/stackrox/rox/migrator/migrations/postgreshelper"
+	"github.com/stackrox/rox/migrator/types"
+	"github.com/stackrox/rox/pkg/fixtures/fixtureconsts"
+	"github.com/stackrox/rox/pkg/postgres/pgutils"
+	"github.com/stackrox/rox/pkg/sac"
+	"github.com/stackrox/rox/pkg/search"
+	"github.com/stretchr/testify/suite"
+)
+
+type migrationTestSuite struct {
+	suite.Suite
+
+	store store.Store
+	db    *pghelper.TestPostgres
+	ctx   context.Context
+}
+
+func TestMigration(t *testing.T) {
+	suite.Run(t, new(migrationTestSuite))
+}
+
+func (s *migrationTestSuite) SetupSuite() {
+	s.ctx = sac.WithAllAccess(context.Background())
+	s.db = pghelper.ForT(s.T(), false)
+	pgutils.CreateTableFromModel(s.ctx, s.db.GetGormDB(), schema.CreateTableAlertsStmt)
+	s.store = store.New(s.db)
+}
+
+func (s *migrationTestSuite) TestMigration() {
+	alerts := map[string]*storage.Alert{
+		fixtureconsts.Alert1: {
+			Id: fixtureconsts.Alert1,
+			Policy: &storage.Policy{
+				Id:          "policy-1",
+				Name:        "Test Policy 1",
+				Description: "Test policy description 1",
+				Disabled:    false,
+				Categories:  []string{"Test"},
+				Severity:    storage.Severity_HIGH_SEVERITY,
+				EnforcementActions: []storage.EnforcementAction{
+					storage.EnforcementAction_SCALE_TO_ZERO_ENFORCEMENT,
+				},
+			},
+			LifecycleStage: storage.LifecycleStage_DEPLOY,
+			ClusterId:      "cluster-1",
+			ClusterName:    "Test Cluster",
+			Namespace:      "default",
+			NamespaceId:    "ns-uuid-1",
+			Entity: &storage.Alert_Deployment_{
+				Deployment: &storage.Alert_Deployment{
+					Id:       "deployment-1",
+					Name:     "Test Deployment 1",
+					Inactive: false,
+					Containers: []*storage.Alert_Deployment_Container{
+						{
+							Name: "container-1",
+							Image: &storage.ContainerImage{
+								Id: "sha256:123456789abcdef123456789abcdef123456789abcdef123456789abcdef1234",
+								Name: &storage.ImageName{
+									Registry: "docker.io",
+									Remote:   "myorg/myimage",
+									Tag:      "latest",
+									FullName: "docker.io/myorg/myimage:latest",
+								},
+								IdV2: "",
+							},
+						},
+					},
+				},
+			},
+		},
+		fixtureconsts.Alert2: {
+			Id: fixtureconsts.Alert2,
+			Policy: &storage.Policy{
+				Id:          "policy-2",
+				Name:        "Test Policy 2",
+				Description: "Test policy description 2",
+				Disabled:    false,
+				Categories:  []string{"Another"},
+				Severity:    storage.Severity_MEDIUM_SEVERITY,
+			},
+			LifecycleStage: storage.LifecycleStage_DEPLOY,
+			ClusterId:      "cluster-2",
+			ClusterName:    "Another Cluster",
+			Namespace:      "dev",
+			NamespaceId:    "ns-uuid-2",
+			Entity: &storage.Alert_Deployment_{
+				Deployment: &storage.Alert_Deployment{
+					Id:       "deployment-2",
+					Name:     "Another Deployment",
+					Inactive: true,
+					Containers: []*storage.Alert_Deployment_Container{
+						{
+							Name: "container-2",
+							Image: &storage.ContainerImage{
+								Id: "sha256:deadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef",
+								Name: &storage.ImageName{
+									Registry: "gcr.io",
+									Remote:   "org/app",
+									Tag:      "v1.0.0",
+									FullName: "gcr.io/org/app:v1.0.0",
+								},
+								IdV2: "",
+							},
+						},
+						{
+							Name: "container-3",
+							Image: &storage.ContainerImage{
+								Id: "sha256:cafebabecafebabecafebabecafebabecafebabecafebabecafebabecafebabe",
+								Name: &storage.ImageName{
+									Registry: "quay.io",
+									Remote:   "repo/image",
+									Tag:      "stable",
+									FullName: "quay.io/repo/image:stable",
+								},
+								IdV2: "",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	dbs := &types.Databases{
+		GormDB:     s.db.GetGormDB(),
+		PostgresDB: s.db.DB,
+		DBCtx:      s.ctx,
+	}
+
+	for _, alert := range alerts {
+		err := s.store.Upsert(dbs.DBCtx, alert)
+		s.Require().NoError(err)
+	}
+
+	alerts[fixtureconsts.Alert1].GetDeployment().GetContainers()[0].GetImage().IdV2 = "8abd44b6-754e-5f67-a68e-457d304b7fd5"
+	alerts[fixtureconsts.Alert2].GetDeployment().GetContainers()[0].GetImage().IdV2 = "9c824579-76de-5836-9b89-51c5986384ac"
+	alerts[fixtureconsts.Alert2].GetDeployment().GetContainers()[1].GetImage().IdV2 = "f6f12b0b-be0c-543e-baae-c97c3cb79584"
+
+	s.Require().NoError(migration.Run(dbs))
+
+	_ = s.store.WalkByQuery(dbs.DBCtx, search.EmptyQuery(), func(alert *storage.Alert) error {
+		expectedAlert, found := alerts[alert.Id]
+		s.Require().True(found)
+		s.Require().Equal(expectedAlert, alert)
+		return nil
+	})
+
+}
