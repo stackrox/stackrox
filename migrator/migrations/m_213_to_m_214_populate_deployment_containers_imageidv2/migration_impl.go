c0c4e82d8714c75fa787f4a81da37cb73ff0f579
AJ Heflin <aheflin@redhat.com>
2025-10-20 14:57:53 -0400

    Fix batching

--- migrator/migrations/m_213_to_m_214_populate_deployment_containers_imageidv2/migration_impl.go
+++ migrator/migrations/m_213_to_m_214_populate_deployment_containers_imageidv2/migration_impl.go
@@ -2,6 +2,8 @@ package m213tom214
 
 import (
 	"github.com/cloudflare/cfssl/log"
+	"github.com/hashicorp/go-multierror"
+	"github.com/jackc/pgx/v5"
 	"github.com/stackrox/rox/migrator/migrations/m_213_to_m_214_populate_deployment_containers_imageidv2/schema"
 	"github.com/stackrox/rox/migrator/types"
 	"github.com/stackrox/rox/pkg/postgres"
@@ -17,39 +19,44 @@ func migrate(database *types.Databases) error {
 
 	db := database.PostgresDB
 
-	getStmt := `SELECT image_name_fullname, image_id FROM deployments_containers WHERE image_id is not null AND image_id != '' AND image_name_fullname is not null AND image_name_fullname != '' AND image_idv2 is not null AND image_idv2 != ''`
-	rows, err := db.Query(database.DBCtx, getStmt)
+	conn, err := db.Acquire(database.DBCtx)
+	defer conn.Release()
 	if err != nil {
 		return err
 	}
-	defer rows.Close()
-
-	containers, err := readRows(rows)
-	if err != nil {
-		return err
-	}
-	tx, err := db.Begin(database.DBCtx)
-	if err != nil {
-		return err
-	}
-	for idx, container := range containers {
-		if idx+1%BATCH_SIZE == 0 {
-			err = tx.Commit(database.DBCtx)
-			if err != nil {
-				return err
-			}
-			tx, err = db.Begin(database.DBCtx)
-			if err != nil {
-				return err
-			}
-		}
-		updateStmt := `UPDATE deployments_containers SET image_idv2 = $1 WHERE image_name_fullname = $2 AND image_id = $3`
-		_, err = tx.Exec(database.DBCtx, updateStmt, uuid.NewV5FromNonUUIDs(container.ImageNameFullName, container.ImageID).String(), container.ImageNameFullName, container.ImageID)
+	for {
+		batch := pgx.Batch{}
+		getStmt := `SELECT image_name_fullname, image_id FROM deployments_containers WHERE image_id is not null AND image_id != '' AND image_name_fullname is not null AND image_name_fullname != '' AND (image_idv2 is null OR image_idv2 = '') LIMIT $1`
+		rows, err := db.Query(database.DBCtx, getStmt, BATCH_SIZE)
 		if err != nil {
 			return err
 		}
+		defer rows.Close()
+
+		containers, err := readRows(rows)
+		if err != nil {
+			return err
+		}
+		for _, container := range containers {
+			updateStmt := `UPDATE deployments_containers SET image_idv2 = $1 WHERE image_name_fullname = $2 AND image_id = $3`
+			batch.Queue(updateStmt, uuid.NewV5FromNonUUIDs(container.ImageNameFullName, container.ImageID).String(), container.ImageNameFullName, container.ImageID)
+		}
+		batchResults := conn.SendBatch(database.DBCtx, &batch)
+		var result *multierror.Error
+		for i := 0; i < batch.Len(); i++ {
+			_, err = batchResults.Exec()
+			result = multierror.Append(result, err)
+		}
+		if err = batchResults.Close(); err != nil {
+			return err
+		}
+		if err = result.ErrorOrNil(); err != nil {
+			return err
+		}
+		if len(containers) != BATCH_SIZE {
+			return nil
+		}
 	}
-	return tx.Commit(database.DBCtx)
 }
 
 func readRows(rows *postgres.Rows) ([]*schema.DeploymentsContainers, error) {
