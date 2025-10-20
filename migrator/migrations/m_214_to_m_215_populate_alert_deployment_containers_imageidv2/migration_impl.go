fca601fa6103fb7d4c8b4af7abe9cf7c05cd4396
AJ Heflin <aheflin@redhat.com>
2025-10-20 15:01:07 -0400

    Fix paging

--- migrator/migrations/m_214_to_m_215_populate_alert_deployment_containers_imageidv2/migration_impl.go
+++ migrator/migrations/m_214_to_m_215_populate_alert_deployment_containers_imageidv2/migration_impl.go
@@ -10,16 +10,6 @@ import (
 	"github.com/stackrox/rox/pkg/uuid"
 )
 
-// If so increment the `MinimumSupportedDBVersionSeqNum` to the `CurrentDBVersionSeqNum` of the release immediately
-// following the release that cannot tolerate the change in pkg/migrations/internal/fallback_seq_num.go.
-//
-// For example, in 4.2 a column `column_v2` is added to replace the `column_v1` column in 4.1.
-// All the code from 4.2 onward will not reference `column_v1`. At some point in the future a rollback to 4.1
-// will not longer be supported and we want to remove `column_v1`. To do so, we will upgrade the schema to remove
-// the column and update the `MinimumSupportedDBVersionSeqNum` to be the value of `CurrentDBVersionSeqNum` in 4.2
-// as 4.1 will no longer be supported. The migration process will inform the user of an error when trying to migrate
-// to a software version that can no longer be supported by the database.
-
 func migrate(database *types.Databases) error {
 	// Use databases.DBCtx to take advantage of the transaction wrapping present in the migration initiator
 	pgutils.CreateTableFromModel(database.DBCtx, database.GormDB, schema.CreateTableAlertsStmt)
@@ -27,11 +17,10 @@ func migrate(database *types.Databases) error {
 	db := database.PostgresDB
 	store := alertStore.New(db)
 
-	page := 0
 	pageSize := 10000
 	for {
 		batch := make([]*storage.Alert, 0, pageSize)
-		pagination := search.NewPagination().Limit(int32(pageSize)).Offset(int32(page * pageSize))
+		pagination := search.NewPagination().Limit(int32(pageSize))
 		query := search.NewQueryBuilder().AddExactMatches(search.EntityType, storage.Alert_DEPLOYMENT.String()).WithPagination(pagination).ProtoQuery()
 		_ = store.WalkByQuery(database.DBCtx, query, func(alert *storage.Alert) error {
 			shouldAppend := false
@@ -54,7 +43,6 @@ func migrate(database *types.Databases) error {
 		if len(batch) < pageSize {
 			break
 		}
-		page++
 	}
 
 	return nil
