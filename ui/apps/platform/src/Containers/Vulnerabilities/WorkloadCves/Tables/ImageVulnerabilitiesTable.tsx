f79afa93857c7f63d912ac181f528e5f423b2504
Mark Pedrotti <mark@stackrox.com>
2025-10-21 09:39:27 -0400

    ROX-31356: Delete commented-out or unreachable totalAdvisories

--- ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/Tables/ImageVulnerabilitiesTable.tsx
+++ ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/Tables/ImageVulnerabilitiesTable.tsx
@@ -58,8 +58,7 @@ import ExceptionDetailsCell from '../components/ExceptionDetailsCell';
 import PartialCVEDataAlert from '../../components/PartialCVEDataAlert';
 import useWorkloadCveViewContext from '../hooks/useWorkloadCveViewContext';
 import { infoForEpssProbability } from './infoForTh';
-// totalAdvisories out of scope for MVP
-// import { formatEpssProbabilityAsPercent, formatTotalAdvisories } from './table.utils';
+// import { formatEpssProbabilityAsPercent } from './table.utils';
 import { formatEpssProbabilityAsPercent } from './table.utils';
 
 export const tableId = 'WorkloadCvesImageVulnerabilitiesTable';
@@ -99,13 +98,6 @@ export const defaultColumns = {
         title: 'EPSS probability',
         isShownByDefault: true,
     },
-    // totalAdvisories out of scope for MVP
-    /*
-    totalAdvisories: {
-        title: 'Advisories',
-        isShownByDefault: true,
-    },
-    */
     affectedComponents: {
         title: 'Affected components',
         isShownByDefault: true,
@@ -237,9 +229,6 @@ function ImageVulnerabilitiesTable({
                     >
                         EPSS probability
                     </Th>
-                    {/* isAdvisoryColumnEnabled && (
-                        <Th className={getVisibilityClass('totalAdvisories')}>Advisories</Th>
-                    ) */}
                     <Th className={getVisibilityClass('affectedComponents')}>
                         Affected components
                         {isFiltered && <DynamicColumnIcon />}
@@ -288,8 +277,6 @@ function ImageVulnerabilitiesTable({
                         );
                         const isFixableInImage = getIsSomeVulnerabilityFixable(vulnerabilities);
                         const epssProbability = cveBaseInfo?.epss?.epssProbability;
-                        // totalAdvisories out of scope for MVP
-                        // const totalAdvisories = undefined;
 
                         const labels: ReactNode[] = [];
                         /*
@@ -404,15 +391,6 @@ function ImageVulnerabilitiesTable({
                                     >
                                         {formatEpssProbabilityAsPercent(epssProbability)}
                                     </Td>
-                                    {/* isAdvisoryColumnEnabled && (
-                                        <Td
-                                            className={getVisibilityClass('totalAdvisories')}
-                                            modifier="nowrap"
-                                            dataLabel="Advisories"
-                                        >
-                                            {formatTotalAdvisories(totalAdvisories)}
-                                        </Td>
-                                    ) */}
                                     <Td
                                         className={getVisibilityClass('affectedComponents')}
                                         dataLabel="Affected components"
