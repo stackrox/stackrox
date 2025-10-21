79846460ecf8ed1074ea01825b320847d66b8183
Guzman <guz@redhat.com>
2025-10-22 01:22:24 +0200

    fix(VM): Increase relay vsock connection size limit to 16 MB (#17370)

--- pkg/env/virtualmachine.go
+++ pkg/env/virtualmachine.go
@@ -1,9 +1,9 @@
 package env
 
 var (
-	// VirtualMachinesVsockConnMaxSizeKB defines the maximum size of incoming vsock connections. The 4 MB default
-	// allows connections carrying index reports with up to approximately 10000 packages.
-	VirtualMachinesVsockConnMaxSizeKB = RegisterIntegerSetting("ROX_VIRTUAL_MACHINES_VSOCK_CONN_MAX_SIZE_KB", 4096)
+	// VirtualMachinesVsockConnMaxSizeKB defines the maximum size of incoming vsock connections. The 16 MB default
+	// allows connections carrying index reports with up to approximately 6400 packages.
+	VirtualMachinesVsockConnMaxSizeKB = RegisterIntegerSetting("ROX_VIRTUAL_MACHINES_VSOCK_CONN_MAX_SIZE_KB", 16384)
 
 	// VirtualMachinesVsockPort defines the port where the virtual machine relay will listen for incoming vsock
 	// connections carrying virtual machine index reports.
