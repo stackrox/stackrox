015adcdaddee73334c7a7cbc3ec68b4427fc87ba
Mark Pedrotti <pedrottimark@gmail.com>
2025-10-21 16:51:33 -0400

    ROX-30444: Add no-import-namespace lint rule except for yup (#17417)

--- ui/apps/platform/src/Containers/Risk/Process/BinaryCollapsible.jsx
+++ ui/apps/platform/src/Containers/Risk/Process/BinaryCollapsible.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import * as Icon from 'react-feather';
+import { ChevronDown, ChevronUp } from 'react-feather';
 
 import CollapsibleCard from 'Components/CollapsibleCard';
 
@@ -24,11 +24,11 @@ function BinaryCollapsible({ commandLineArgs, children }) {
     }
 
     function renderWhenOpened() {
-        return renderHeader('bg-primary-100', <Icon.ChevronUp className="h-4 w-4" />);
+        return renderHeader('bg-primary-100', <ChevronUp className="h-4 w-4" />);
     }
 
     function renderWhenClosed() {
-        return renderHeader('bg-base-100', <Icon.ChevronDown className="h-4 w-4" />);
+        return renderHeader('bg-base-100', <ChevronDown className="h-4 w-4" />);
     }
 
     return (
