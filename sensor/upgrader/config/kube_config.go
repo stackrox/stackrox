f368c992bd4e0c9303425d66b192f2a9802bfbe3
Stephan Heßelmann <shesselm@redhat.com>
2025-10-21 17:49:54 +0200

    ROX-30745 / ROX-28063: add kube-timeout flag to upgrader

--- sensor/upgrader/config/kube_config.go
+++ sensor/upgrader/config/kube_config.go
@@ -1,34 +1,39 @@
 package config
 
 import (
+	"time"
+
 	"github.com/pkg/errors"
 	"github.com/stackrox/rox/sensor/upgrader/flags"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-func loadKubeConfig() (*rest.Config, error) {
+func loadKubeConfig(timeout time.Duration) (*rest.Config, error) {
 	switch cs := *flags.KubeConfigSource; cs {
 	case "in-cluster":
 		cfg, err := rest.InClusterConfig()
 		if err != nil {
 			return nil, errors.Wrap(err, "loading in-cluster Kubernetes config")
 		}
+		cfg.Timeout = timeout
 		return cfg, nil
 	case "kubectl":
-		return loadKubeCtlConfig()
+		return loadKubeCtlConfig(timeout)
 	default:
 		return nil, errors.Errorf("invalid kube config source %q", cs)
 	}
 }
 
-func loadKubeCtlConfig() (*rest.Config, error) {
+func loadKubeCtlConfig(timeout time.Duration) (*rest.Config, error) {
 	config, err := clientcmd.NewDefaultClientConfigLoadingRules().Load()
 	if err != nil {
 		return nil, errors.Wrap(err, "loading default Kubernetes client config")
 	}
 
-	cfg, err := clientcmd.NewDefaultClientConfig(*config, &clientcmd.ConfigOverrides{}).ClientConfig()
+	cfg, err := clientcmd.NewDefaultClientConfig(*config, &clientcmd.ConfigOverrides{
+		Timeout: timeout.String(),
+	}).ClientConfig()
 	if err != nil {
 		return nil, errors.Wrap(err, "creating Kubernetes client config from kubeconfig")
 	}
--- sensor/upgrader/flags/flags.go
+++ sensor/upgrader/flags/flags.go
@@ -2,9 +2,12 @@ package flags
 
 import (
 	"flag"
+	"time"
 )
 
 var (
 	// KubeConfigSource is the source of the kubernetes config.
 	KubeConfigSource = flag.String("kube-config", "in-cluster", "source for the Kubernetes config")
+	// KubeTimeout is the timeout for Kubernetes API operations.
+	KubeTimeout = flag.Duration("kube-timeout", 60*time.Second, "timeout for Kubernetes API operations")
 )
