a6d993cf27ba176f128d498e35db84c6fe418e63
JoukoVirtanen <jouko@stackrox.com>
2025-10-22 16:55:45 -0700

    Reduced logging

--- central/networkgraph/flow/datastore/internal/store/postgres/store.go
+++ central/networkgraph/flow/datastore/internal/store/postgres/store.go
@@ -712,7 +712,7 @@ func (s *flowStoreImpl) pruneOrphanExternalEntities(ctx context.Context, srcFlow
 			}
 		}
 
-		log.Infof("Pruned %d orphaned discovered entities", totalPruned)
+		log.Debugf("Pruned %d orphaned discovered entities", totalPruned)
 	}
 
 	return nil
