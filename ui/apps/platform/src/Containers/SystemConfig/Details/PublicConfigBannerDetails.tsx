9a89a38143de0aeb66371dda1c1ab2e4f1c5467f
Mark Pedrotti <pedrottimark@gmail.com>
2025-10-23 12:50:27 -0400

    ROX-31385: Delete React in SystemConfig (#17469)

--- ui/apps/platform/src/Containers/SystemConfig/Details/PublicConfigBannerDetails.tsx
+++ ui/apps/platform/src/Containers/SystemConfig/Details/PublicConfigBannerDetails.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import type { ReactElement } from 'react';
 import capitalize from 'lodash/capitalize';
 import {
--- ui/apps/platform/src/Containers/SystemConfig/SystemConfigPage.tsx
+++ ui/apps/platform/src/Containers/SystemConfig/SystemConfigPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import type { ReactElement, ReactNode } from 'react';
 import {
     Alert,
