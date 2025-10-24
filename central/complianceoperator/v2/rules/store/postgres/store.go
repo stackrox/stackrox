65369ebd83574c6c4ead0e65d9eb5de7e412079e
Tomasz Janiszewski <tomek@redhat.com>
2025-10-24 08:21:15 +0200

    generate
    Signed-off-by: Tomasz Janiszewski <tomek@redhat.com>

--- central/complianceoperator/v2/rules/store/postgres/store.go
+++ central/complianceoperator/v2/rules/store/postgres/store.go
@@ -187,10 +187,6 @@ func copyFromComplianceOperatorRuleV2(ctx context.Context, s pgSearch.Deleter, t
 
 	for objBatch := range slices.Chunk(objs, batchSize) {
 		for _, obj := range objBatch {
-			// Todo: ROX-9499 Figure out how to more cleanly template around this issue.
-			log.Debugf("This is here for now because there is an issue with pods_TerminatedInstances where the obj "+
-				"in the loop is not used as it only consists of the parent ID and the index.  Putting this here as a stop gap "+
-				"to simply use the object.  %s", obj)
 
 			serialized, marshalErr := obj.MarshalVT()
 			if marshalErr != nil {
@@ -253,10 +249,6 @@ func copyFromComplianceOperatorRuleV2Controls(ctx context.Context, s pgSearch.De
 	idx := 0
 	for objBatch := range slices.Chunk(objs, batchSize) {
 		for _, obj := range objBatch {
-			// Todo: ROX-9499 Figure out how to more cleanly template around this issue.
-			log.Debugf("This is here for now because there is an issue with pods_TerminatedInstances where the obj "+
-				"in the loop is not used as it only consists of the parent ID and the index.  Putting this here as a stop gap "+
-				"to simply use the object.  %s", obj)
 
 			inputRows = append(inputRows, []interface{}{
 				complianceOperatorRuleV2ID,
