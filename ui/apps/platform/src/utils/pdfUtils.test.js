3381435f2f28b1352bc559237cdf5605b67e7e82
Saif Chaudhry <schaudhr@redhat.com>
2025-10-27 18:20:17 -0700

    fix(ui): lint error

--- ui/apps/platform/src/utils/pdfUtils.test.js
+++ ui/apps/platform/src/utils/pdfUtils.test.js
@@ -1,4 +1,4 @@
-import { describe, it, expect, beforeEach, afterEach, vi } from 'vitest';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 import ReactDOMServer from 'react-dom/server';
 import createPDFTable from './pdfUtils';
 
