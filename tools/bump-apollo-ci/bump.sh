cd596a3ea7347722e5c67230ab83039dab38c896
Tom Martensen <tmartens@redhat.com>
2025-10-30 15:13:39 +0100

    update: base master

--- tools/bump-apollo-ci/bump.sh
+++ tools/bump-apollo-ci/bump.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 GIT_REPO_TOP="$(git rev-parse --show-toplevel)"
 
 pull_latest_master() {
-    git fetch origin tm/bump-all-apollo-ci-references
-    git merge --ff-only origin/tm/bump-all-apollo-ci-references
+    git fetch origin master
+    git merge --ff-only origin/master
 }
 
 find_references() {
@@ -111,7 +111,7 @@ push_and_create_pr() {
         local pr_url
         pr_url=$(gh pr create \
             --draft \
-            --base tm/bump-all-apollo-ci-references \
+            --base master \
             --title "chore: bump apollo-ci to $version" \
             --body "This PR updates all apollo-ci image references to version $version.
 
