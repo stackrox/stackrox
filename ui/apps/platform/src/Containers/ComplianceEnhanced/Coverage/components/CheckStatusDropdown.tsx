dafa0202bd3101edf290cefb29c79022fc40c23c
David Vail <dcvail17@gmail.com>
2025-10-31 09:01:49 -0400

    ROX-31315: Migrate remaining checkbox select dropdowns (#17537)

--- ui/apps/platform/src/Containers/ComplianceEnhanced/Coverage/components/CheckStatusDropdown.tsx
+++ ui/apps/platform/src/Containers/ComplianceEnhanced/Coverage/components/CheckStatusDropdown.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
-import { Select, SelectOption } from '@patternfly/react-core/deprecated';
+import React from 'react';
+import { SelectOption } from '@patternfly/react-core';
 
+import CheckboxSelect from 'Components/PatternFly/CheckboxSelect';
+import { searchValueAsArray } from 'utils/searchUtils';
 import type { SearchFilter } from 'types/search';
 
 type CheckStatusDropdownProps = {
@@ -9,34 +11,28 @@ type CheckStatusDropdownProps = {
 };
 
 function CheckStatusDropdown({ searchFilter, onSelect }: CheckStatusDropdownProps) {
-    const [checkStatusIsOpen, setCheckStatusIsOpen] = useState(false);
+    const selections = searchValueAsArray(searchFilter['Compliance Check Status']);
 
-    function onCheckStatusToggle(isOpen: boolean) {
-        setCheckStatusIsOpen(isOpen);
+    function handleItemSelect(selection: string, checked: boolean) {
+        onSelect('Compliance Check Status', checked, selection);
     }
 
     return (
-        <Select
-            variant="checkbox"
-            aria-label="Check status filter menu items"
+        <CheckboxSelect
+            selections={selections}
+            onItemSelect={handleItemSelect}
+            ariaLabel="Check status filter menu items"
             toggleAriaLabel="Check status filter menu toggle"
-            onToggle={(_event, isOpen: boolean) => onCheckStatusToggle(isOpen)}
-            onSelect={(event, selection) => {
-                const { checked } = event.target as HTMLInputElement;
-                onSelect('Compliance Check Status', checked, selection.toString());
-            }}
-            selections={searchFilter['Compliance Check Status']}
-            isOpen={checkStatusIsOpen}
             placeholderText="Compliance status"
         >
-            <SelectOption key="PASS" value="Pass" />
-            <SelectOption key="FAIL" value="Fail" />
-            <SelectOption key="ERROR" value="Error" />
-            <SelectOption key="INFO" value="Info" />
-            <SelectOption key="MANUAL" value="Manual" />
-            <SelectOption key="NOT_APPLICABLE" value="Not Applicable" />
-            <SelectOption key="INCONSISTENT" value="Inconsistent" />
-        </Select>
+            <SelectOption value="Pass">Pass</SelectOption>
+            <SelectOption value="Fail">Fail</SelectOption>
+            <SelectOption value="Error">Error</SelectOption>
+            <SelectOption value="Info">Info</SelectOption>
+            <SelectOption value="Manual">Manual</SelectOption>
+            <SelectOption value="Not Applicable">Not Applicable</SelectOption>
+            <SelectOption value="Inconsistent">Inconsistent</SelectOption>
+        </CheckboxSelect>
     );
 }
 
