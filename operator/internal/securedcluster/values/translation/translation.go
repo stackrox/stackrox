aaa1ed7970a44f6cd83955fbb2eca9974817db8d
Dmitrii Dolgov <9erthalion6@gmail.com>
2025-11-03 15:07:42 +0100

    Add Fact behind a feature flag
    Allow to spin up Fact [1] as part of Collector DaemonSet
    
    [1]: https://github.com/stackrox/fact/

--- operator/internal/securedcluster/values/translation/translation.go
+++ operator/internal/securedcluster/values/translation/translation.go
@@ -403,6 +403,7 @@ func (t Translator) getCollectorValues(perNode *platform.PerNodeSpec) *translati
 	cv.AddAllFrom(t.getCollectorContainerValues(perNode.Collector))
 	cv.AddAllFrom(t.getComplianceContainerValues(perNode.Compliance))
 	cv.AddAllFrom(t.getNodeInventoryContainerValues(perNode.NodeInventory))
+	cv.AddAllFrom(t.getSFAContainerValues(perNode.SFA))
 
 	return &cv
 }
@@ -454,6 +455,17 @@ func (t Translator) getNodeInventoryContainerValues(nodeInventory *platform.Cont
 
 	cv := translation.NewValuesBuilder()
 	cv.AddChild("nodeScanningResources", translation.GetResources(nodeInventory.Resources))
+
+	return &cv
+}
+
+func (t Translator) getSFAContainerValues(sfaContainerSpec *platform.ContainerSpec) *translation.ValuesBuilder {
+	if sfaContainerSpec == nil {
+		return nil
+	}
+
+	cv := translation.NewValuesBuilder()
+	cv.AddChild("sfaResources", translation.GetResources(sfaContainerSpec.Resources))
 
 	return &cv
 }
