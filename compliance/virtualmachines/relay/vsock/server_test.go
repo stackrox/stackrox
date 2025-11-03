ee97345cac6a6d55b1691793480f75aa6edb24fa
Guzman <guz@redhat.com>
2025-11-03 11:00:14 +0100

    Extract vsock server from relay.go

---
+++ compliance/virtualmachines/relay/vsock/server_test.go
@@ -0,0 +1,33 @@
+package vsock
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/sync/semaphore"
+)
+
+func TestSemaphore(t *testing.T) {
+	ctx := context.Background()
+
+	vsockServer := &ServerImpl{
+		semaphore:        semaphore.NewWeighted(1),
+		semaphoreTimeout: 5 * time.Millisecond,
+	}
+
+	// First should succeed
+	err := vsockServer.AcquireSemaphore(ctx)
+	require.NoError(t, err)
+
+	// Second should time out
+	err = vsockServer.AcquireSemaphore(ctx)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "failed to acquire semaphore")
+
+	// After releasing once, a new acquire should succeed
+	vsockServer.ReleaseSemaphore()
+	err = vsockServer.AcquireSemaphore(ctx)
+	require.NoError(t, err)
+}
