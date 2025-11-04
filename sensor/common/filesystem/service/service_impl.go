0cc8f1df634032d49dd121b94433e066da966816
JoukoVirtanen <jouko@stackrox.com>
2025-11-04 10:48:13 -0800

    Using a set of stoppers. A stopper is now used in receiveMessages

--- sensor/common/filesystem/service/service_impl.go
+++ sensor/common/filesystem/service/service_impl.go
@@ -7,8 +7,10 @@ import (
 	"github.com/pkg/errors"
 	"github.com/stackrox/rox/generated/internalapi/sensor"
 	sensorAPI "github.com/stackrox/rox/generated/internalapi/sensor"
+	"github.com/stackrox/rox/pkg/concurrency"
 	"github.com/stackrox/rox/pkg/grpc/authz/idcheck"
 	"github.com/stackrox/rox/pkg/logging"
+	"github.com/stackrox/rox/pkg/set"
 	"github.com/stackrox/rox/sensor/common/filesystem/pipeline"
 	"google.golang.org/grpc"
 )
@@ -22,6 +24,7 @@ func NewService(pipeline *pipeline.Pipeline, activityChan chan *sensorAPI.FileAc
 	srv := &serviceImpl{
 		pipeline:     pipeline,
 		activityChan: activityChan,
+		stoppers:     set.NewSet[concurrency.Stopper](),
 	}
 
 	return srv
@@ -31,9 +34,18 @@ type serviceImpl struct {
 	sensor.UnimplementedFileActivityServiceServer
 	pipeline     *pipeline.Pipeline
 	activityChan chan *sensorAPI.FileActivity
+	stoppers     set.Set[concurrency.Stopper]
 }
 
 func (s *serviceImpl) Stop() {
+	// Stop all active connections
+	for _, stopper := range s.stoppers.AsSlice() {
+		stopper.Client().Stop() // Signal the receiveMessages that it needs to stop
+	}
+	// Wait for all connections to stop
+	for _, stopper := range s.stoppers.AsSlice() {
+		<-stopper.Client().Stopped().Done() // Wait for receiveMessages to stop
+	}
 	close(s.activityChan)
 }
 
@@ -53,11 +65,17 @@ func (s *serviceImpl) AuthFuncOverride(ctx context.Context, fullMethodName strin
 }
 
 func (s *serviceImpl) Communicate(stream sensor.FileActivityService_CommunicateServer) error {
-	return s.receiveMessages(stream)
+	// Create a stopper for this agent connection
+	stopper := concurrency.NewStopper()
+	s.stoppers.Add(stopper)
+	defer s.stoppers.Remove(stopper)
+
+	return s.receiveMessages(stream, stopper)
 }
 
-func (s *serviceImpl) receiveMessages(stream sensor.FileActivityService_CommunicateServer) error {
+func (s *serviceImpl) receiveMessages(stream sensor.FileActivityService_CommunicateServer, stopper concurrency.Stopper) error {
 	log.Info("Starting file system stream server")
+	defer stopper.Flow().ReportStopped() // Signal the function has stopped
 	for {
 		msg, err := stream.Recv()
 		if err != nil {
@@ -65,6 +83,10 @@ func (s *serviceImpl) receiveMessages(stream sensor.FileActivityService_Communic
 		}
 
 		log.Debug("Got file activity: ", msg)
-		s.activityChan <- msg
+		select {
+		case <-stopper.Flow().StopRequested(): // Stop the function
+			return nil
+		case s.activityChan <- msg:
+		}
 	}
 }
