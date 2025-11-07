f1b7f9aa63560089ce44cba5777dbd7d58a9b5b1
David Shrewsberry <99685630+dashrews78@users.noreply.github.com>
2025-11-07 05:37:57 -0500

    ROX-31558: remove old data model stuff from views (#17624)

--- central/views/imagecve/view_test.go
+++ central/views/imagecve/view_test.go
@@ -17,7 +17,6 @@ import (
 	v1 "github.com/stackrox/rox/generated/api/v1"
 	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/pkg/cve"
-	"github.com/stackrox/rox/pkg/features"
 	"github.com/stackrox/rox/pkg/fixtures"
 	imageSamples "github.com/stackrox/rox/pkg/fixtures/image"
 	"github.com/stackrox/rox/pkg/pointers"
@@ -618,7 +617,7 @@ func (s *ImageCVEViewTestSuite) testCases() []testCase {
 				Level: v1.SearchCategory_IMAGES,
 				Parent: &scoped.Scope{
 					IDs:   []string{cve.ID("CVE-2022-1552", "debian:8")},
-					Level: v1.SearchCategory_IMAGE_VULNERABILITIES,
+					Level: v1.SearchCategory_IMAGE_VULNERABILITIES_V2,
 				},
 			}),
 			q: search.NewQueryBuilder().
@@ -953,22 +952,6 @@ func compileExpected(images []*storage.Image, filter *filterImpl, options views.
 
 				val.TopCVSS = pointers.Float32(max(val.GetTopCVSS(), vuln.GetCvss()))
 
-				var id string
-				if !features.FlattenCVEData.Enabled() {
-					id = cve.ID(val.GetCVE(), image.GetScan().GetOperatingSystem())
-					var found bool
-					for _, seenID := range val.GetCVEIDs() {
-						if seenID == id {
-							found = true
-							break
-						}
-					}
-
-					if !found {
-						val.CVEIDs = append(val.CVEIDs, id)
-					}
-				}
-
 				if val.GetFirstDiscoveredInSystem().After(vulnTime) {
 					val.FirstDiscoveredInSystem = &vulnTime
 				}
@@ -1009,11 +992,6 @@ func compileExpected(images []*storage.Image, filter *filterImpl, options views.
 
 	expected := make([]*imageCVECoreResponse, 0, len(cveMap))
 	for _, entry := range cveMap {
-		if !features.FlattenCVEData.Enabled() {
-			sort.SliceStable(entry.CVEIDs, func(i, j int) bool {
-				return entry.CVEIDs[i] < entry.CVEIDs[j]
-			})
-		}
 		expected = append(expected, entry)
 	}
 	if options.SkipGetImagesBySeverity {
