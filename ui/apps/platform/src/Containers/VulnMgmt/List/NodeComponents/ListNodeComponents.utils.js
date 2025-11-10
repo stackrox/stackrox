a1a0bbff85564d22abb9f16864e6d19a6bd70c6f
Mark Pedrotti <pedrottimark@gmail.com>
2025-11-10 15:34:37 -0500

    ROX-31596: Delete ROX_ACTIVE_VULN_MGMT in ui (#17698)

--- ui/apps/platform/src/Containers/VulnMgmt/List/NodeComponents/ListNodeComponents.utils.js
+++ ui/apps/platform/src/Containers/VulnMgmt/List/NodeComponents/ListNodeComponents.utils.js
@@ -1,19 +1,10 @@
 import entityTypes from 'constants/entityTypes';
 
 export function getFilteredComponentColumns(columns, workflowState) {
-    const shouldKeepActiveColumn =
-        workflowState.isCurrentSingle(entityTypes.DEPLOYMENT) ||
-        workflowState.isPrecedingSingle(entityTypes.DEPLOYMENT) ||
-        (workflowState.getSingleAncestorOfType(entityTypes.DEPLOYMENT) &&
-            workflowState.getSingleAncestorOfType(entityTypes.IMAGE));
-
     const shouldRemoveColumns = !workflowState.isPreceding(entityTypes.IMAGE);
 
     return columns.filter((col) => {
         switch (col.accessor) {
-            case 'isActive': {
-                return !!shouldKeepActiveColumn;
-            }
             case 'source': {
                 return !shouldRemoveColumns;
             }
