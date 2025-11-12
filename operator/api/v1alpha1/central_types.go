03e8542836439f58dec7420535c8dedf87580231
Moritz Clasmeier <mclasmeier@redhat.com>
2025-11-12 13:14:27 +0100

    central_types: Printer Column: adminPassword

--- operator/api/v1alpha1/central_types.go
+++ operator/api/v1alpha1/central_types.go
@@ -678,6 +678,7 @@ type CentralComponentStatus struct {
 //+kubebuilder:subresource:status
 //+operator-sdk:csv:customresourcedefinitions:resources={{Deployment,v1,""},{Secret,v1,""},{Service,v1,""},{Route,v1,""}}
 //+kubebuilder:printcolumn:name="Version",type=string,JSONPath=`.status.reconciledVersion`
+//+kubebuilder:printcolumn:name="AdminPassword",type=string,JSONPath=`.spec.central.adminPasswordSecret.name`
 //+genclient
 
 // Central is the configuration template for the central services. This includes the API server, persistent storage,
