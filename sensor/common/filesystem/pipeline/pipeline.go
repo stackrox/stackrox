889610b603776a4b89d128b8af1af1fe11bb8c65
Giles Hutton <ghutton@redhat.com>
2025-11-13 16:20:09 +0000

    ROX-31454: adds node file event detection

--- sensor/common/filesystem/pipeline/pipeline.go
+++ sensor/common/filesystem/pipeline/pipeline.go
@@ -190,6 +190,7 @@ func (p *Pipeline) run() {
 
 				// Do node based detection but for now just log
 				log.Infof("Node FS event on %s = %+v", node.GetName(), event)
+				p.detector.ProcessFileAccess(p.msgCtx, event)
 			}
 
 			// TODO: Send event to detector
