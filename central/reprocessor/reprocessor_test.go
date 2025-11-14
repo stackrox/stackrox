73480c035325ad2e89cb5a2e9966e8d719c29c6b
Charmik Sheth <101146970+charmik-redhat@users.noreply.github.com>
2025-11-13 16:36:55 -0800

    Fix test

--- central/reprocessor/reprocessor_test.go
+++ central/reprocessor/reprocessor_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/stackrox/rox/central/ranking"
 	"github.com/stackrox/rox/generated/storage"
 	"github.com/stackrox/rox/pkg/concurrency"
+	"github.com/stackrox/rox/pkg/features"
 	"github.com/stackrox/rox/pkg/fixtures"
 	"github.com/stackrox/rox/pkg/images/utils"
 	"github.com/stackrox/rox/pkg/postgres/pgtest"
