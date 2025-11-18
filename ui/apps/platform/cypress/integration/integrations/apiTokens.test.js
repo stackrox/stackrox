aeafa5dd85834352cba604d8dc7fd73642b9098d
Yann Brillouet <ybrillou@redhat.com>
2025-11-18 15:56:11 +0100

    Update test role selector label

--- ui/apps/platform/cypress/integration/integrations/apiTokens.test.js
+++ ui/apps/platform/cypress/integration/integrations/apiTokens.test.js
@@ -41,7 +41,7 @@ describe('API Tokens', () => {
 
         // Step 2, check valid from and generate
         getInputByLabel('Token name').clear().type(apiTokenName);
-        getSelectButtonByLabel('Roles').click();
+        getSelectButtonByLabel('Select roles').click();
         getSelectOption('Admin').click();
 
         generateCreatedAuthProvidersIntegrationInForm(integrationType);
