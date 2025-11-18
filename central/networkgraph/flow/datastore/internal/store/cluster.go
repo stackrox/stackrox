2d7d75a172499bb82ceae95b9c7e79f67c0ebfb8
Yann Brillouet <ybrillou@redhat.com>
2025-11-18 18:15:11 +0100

    chore(cleanup): drop network flow table partition for removed clusters

--- central/networkgraph/flow/datastore/internal/store/cluster.go
+++ central/networkgraph/flow/datastore/internal/store/cluster.go
@@ -7,6 +7,7 @@ type ClusterStore interface {
 	GetFlowStore(clusterID string) FlowStore
 
 	CreateFlowStore(ctx context.Context, clusterID string) (FlowStore, error)
+	RemoveFlowStore(ctx context.Context, clusterID string) error
 }
 
 //go:generate mockgen-wrapper
