44bedac799fbd410e42635760561b14f86d52641
David Vail <dcvail17@gmail.com>
2025-11-19 16:11:11 -0500

    Fix PF codemod color replacement

--- ui/apps/platform/src/Containers/ComplianceEnhanced/Coverage/compliance.coverage.constants.ts
+++ ui/apps/platform/src/Containers/ComplianceEnhanced/Coverage/compliance.coverage.constants.ts
@@ -10,8 +10,7 @@ export const FAILING_LABEL_COLOR = 'red';
 export const MANUAL_VAR_COLOR =
     'var(--pf-t--temp--dev--tbd)'; /* CODEMODS: original v5 color was --pf-v5-global--warning-color--100 */
 export const MANUAL_LABEL_COLOR = 'grey';
-export const OTHER_VAR_COLOR =
-    'var(--pf-t--temp--dev--tbd)'; /* CODEMODS: original v5 color was --pf-v5-global--disabled-color--100 */
+export const OTHER_VAR_COLOR = 'var(--pf-t--global--icon--color--subtle)';
 export const OTHER_LABEL_COLOR = 'gold';
 export const PASSING_VAR_COLOR =
     'var(--pf-t--temp--dev--tbd)'; /* CODEMODS: original v5 color was --pf-v5-global--primary-color--100 */
