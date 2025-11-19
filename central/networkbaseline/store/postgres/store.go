81e95038b1225b65fa7685d4cf4d9d6044e7aa8c
Yann Brillouet <91869377+rhybrillou@users.noreply.github.com>
2025-11-19 13:09:02 +0100

    refactor(tests): remove datamodel helpers for policy and network (#17780)

--- central/networkbaseline/store/postgres/store.go
+++ central/networkbaseline/store/postgres/store.go
@@ -21,7 +21,6 @@ import (
 	"github.com/stackrox/rox/pkg/sac/resources"
 	"github.com/stackrox/rox/pkg/search"
 	pgSearch "github.com/stackrox/rox/pkg/search/postgres"
-	"gorm.io/gorm"
 )
 
 const (
@@ -187,23 +186,3 @@ func copyFromNetworkBaselines(ctx context.Context, s pgSearch.Deleter, tx *postg
 }
 
 // endregion Helper functions
-
-// region Used for testing
-
-// CreateTableAndNewStore returns a new Store instance for testing.
-func CreateTableAndNewStore(ctx context.Context, db postgres.DB, gormDB *gorm.DB) Store {
-	pkgSchema.ApplySchemaForTable(ctx, gormDB, baseTable)
-	return New(db)
-}
-
-// Destroy drops the tables associated with the target object type.
-func Destroy(ctx context.Context, db postgres.DB) {
-	dropTableNetworkBaselines(ctx, db)
-}
-
-func dropTableNetworkBaselines(ctx context.Context, db postgres.DB) {
-	_, _ = db.Exec(ctx, "DROP TABLE IF EXISTS network_baselines CASCADE")
-
-}
-
-// endregion Used for testing
