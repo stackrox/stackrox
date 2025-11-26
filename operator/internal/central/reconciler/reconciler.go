657626e872bf3df8f7fb2ef802b98deaba7c8ae4
Moritz Clasmeier <mclasmeier@redhat.com>
2025-11-26 23:30:55 +0100

    Use predicate for Helm reconciler.

--- operator/internal/central/reconciler/reconciler.go
+++ operator/internal/central/reconciler/reconciler.go
@@ -9,6 +9,7 @@ import (
 	centralTranslation "github.com/stackrox/rox/operator/internal/central/values/translation"
 	commonExtensions "github.com/stackrox/rox/operator/internal/common/extensions"
 	"github.com/stackrox/rox/operator/internal/common/rendercache"
+	statusController "github.com/stackrox/rox/operator/internal/common/status"
 	"github.com/stackrox/rox/operator/internal/legacy"
 	"github.com/stackrox/rox/operator/internal/proxy"
 	"github.com/stackrox/rox/operator/internal/reconciler"
@@ -17,6 +18,7 @@ import (
 	"github.com/stackrox/rox/operator/internal/values/translation"
 	"github.com/stackrox/rox/pkg/version"
 	ctrl "sigs.k8s.io/controller-runtime"
+	ctrlClient "sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
@@ -54,12 +56,19 @@ func RegisterNewReconciler(mgr ctrl.Manager, selector string) error {
 		pkgReconciler.WithPostExtension(commonExtensions.ReconcileProductVersionStatusExtension(version.GetMainVersion())),
 	}
 
-	opts := make([]pkgReconciler.Option, 0, len(otherPreExtensions)+len(postExtensions)+7)
+	// Plug in custom event predicate to skip reconciliation for updates caused by the status controller.
+	// to prevent unnecessary Helm dry-runs.
+	predicates := []pkgReconciler.Option{
+		pkgReconciler.WithPredicate(statusController.SkipStatusControllerUpdates[ctrlClient.Object]{}),
+	}
+
+	opts := make([]pkgReconciler.Option, 0, len(otherPreExtensions)+len(postExtensions)+len(predicates)+7)
 	opts = append(opts, extraEventWatcher)
 	opts = append(opts, pkgReconciler.WithPreExtension(extensions.VerifyCollisionFreeCentral(mgr.GetClient())))
 	opts = append(opts, pkgReconciler.WithPreExtension(extensions.FeatureDefaultingExtension(mgr.GetClient())))
 	opts = append(opts, otherPreExtensions...)
 	opts = append(opts, postExtensions...)
+	opts = append(opts, predicates...)
 	opts = append(opts, pkgReconciler.WithReconcilePeriod(extensions.InitBundleReconcilePeriod))
 	opts = append(opts, pkgReconciler.WithPauseReconcileAnnotation(commonExtensions.PauseReconcileAnnotation))
 
