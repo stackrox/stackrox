d318837f65086624d2693c8fced1262fa75fc96c
Michaël Petrov <github@parameta.lol>
2025-11-28 22:54:13 +0100

    reimplement IsPlatformWorkload

--- central/metrics/custom/image_vulnerabilities/labels.go
+++ central/metrics/custom/image_vulnerabilities/labels.go
@@ -5,14 +5,15 @@ import (
 
 	"github.com/stackrox/rox/central/cve/image/v2/datastore/store"
 	"github.com/stackrox/rox/central/metrics/custom/tracker"
+	platformmatcher "github.com/stackrox/rox/central/platform/matcher"
+	"github.com/stackrox/rox/generated/storage"
 )
 
 var lazyLabels = tracker.LazyLabelGetters[*finding]{
-	"Cluster":    func(f *finding) string { return f.ClusterName },
-	"Namespace":  func(f *finding) string { return f.Namespace },
-	"Deployment": func(f *finding) string { return f.DeploymentName },
-	// Note: IsPlatformWorkload removed - requires full deployment object which isn't available
-	// in the flattened SQL result. Could be added if needed by joining deployment table.
+	"Cluster":            func(f *finding) string { return f.ClusterName },
+	"Namespace":          func(f *finding) string { return f.Namespace },
+	"Deployment":         func(f *finding) string { return f.DeploymentName },
+	"IsPlatformWorkload": isPlatformWorkload,
 
 	"ImageID":          func(f *finding) string { return f.ImageID },
 	"ImageRegistry":    func(f *finding) string { return f.ImageRegistry },
@@ -28,6 +29,15 @@ var lazyLabels = tracker.LazyLabelGetters[*finding]{
 	"EPSSProbability": func(f *finding) string { return strconv.FormatFloat(float64(f.EPSSProbability), 'f', 1, 32) },
 	// Note: EPSSPercentile not available in denormalized image_cves_v2 table
 	"IsFixable": func(f *finding) string { return strconv.FormatBool(f.FixedBy != "") },
+}
+
+func isPlatformWorkload(f *finding) string {
+	// MatchDeployment only needs the Namespace field, which we have in the
+	// flattened finding.
+	isPlatform, _ := platformmatcher.Singleton().MatchDeployment(&storage.Deployment{
+		Namespace: f.Namespace,
+	})
+	return strconv.FormatBool(isPlatform)
 }
 
 type finding = store.DeploymentVulnFinding
