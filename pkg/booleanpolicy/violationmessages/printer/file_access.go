d201bf2763bab83313a4483d618f5ebfbc0bad11
Giles Hutton <ghutton@redhat.com>
2025-12-01 11:50:46 +0000

    fix(sfa): updates the file access violation printer to make the message clearer for multiple accesses to similar files

--- pkg/booleanpolicy/violationmessages/printer/file_access.go
+++ pkg/booleanpolicy/violationmessages/printer/file_access.go
@@ -16,21 +16,35 @@ func UpdateFileAccessAlertViolationMessage(v *storage.Alert_FileAccessViolation)
 
 	pathSet := set.NewStringSet()
 	for _, fa := range accesses {
-		pathSet.Add(fa.GetFile().GetMountedPath())
+		pathSet.Add(fa.GetFile().GetNodePath())
 	}
 
 	var sb strings.Builder
 
 	if pathSet.Cardinality() < 10 {
-		for i, fa := range accesses {
-			if i > 0 {
-				sb.WriteString("; ")
-			}
-			fmt.Fprintf(&sb, "'%v' accessed (%v) by %v",
-				fa.GetFile().GetMountedPath(),
-				fa.GetOperation(),
-				fa.GetProcess().GetSignal().GetName())
+		// Construct a string for each distinct path, and accumulate
+		// file operation, so we can show all the kinds of activity
+		// for each file, to provide the most important info.
+
+		pathToOperation := make(map[string][]string, pathSet.Cardinality())
+		for _, fa := range accesses {
+			path := fa.GetFile().GetNodePath()
+			pathToOperation[path] = append(pathToOperation[path], fa.GetOperation().String())
 		}
+
+		parts := make([]string, 0, pathSet.Cardinality())
+
+		// sorted to make this more deterministic which means both that
+		// the output is more consistent, and it is more testable.
+		paths := pathSet.AsSortedSlice(func(a, b string) bool {
+			return strings.Compare(a, b) < 0
+		})
+
+		for _, path := range paths {
+			parts = append(parts, fmt.Sprintf("'%v' accessed (%s)", path, strings.Join(pathToOperation[path], ", ")))
+		}
+
+		fmt.Fprintf(&sb, "%s", strings.Join(parts, "; "))
 	} else {
 		fmt.Fprintf(&sb, "%d sensitive files accessed", pathSet.Cardinality())
 	}
