d997794b488d28aac255da2e07edb30fa5a27330
Khushboo Sancheti <ks@stackrox.com>
2025-12-01 19:44:25 +0530

    few simplifications

--- pkg/booleanpolicy/violationmessages/printer/file_access.go
+++ pkg/booleanpolicy/violationmessages/printer/file_access.go
@@ -2,52 +2,46 @@ package printer
 
 import (
 	"fmt"
+	"maps"
+	"slices"
 	"strings"
 
 	"github.com/stackrox/rox/generated/storage"
-	"github.com/stackrox/rox/pkg/set"
+)
+
+const (
+	maxPaths = 10
 )
 
 func UpdateFileAccessAlertViolationMessage(v *storage.Alert_FileAccessViolation) {
-	accesses := v.GetAccesses()
-	if len(accesses) == 0 {
+	if len(v.GetAccesses()) == 0 {
 		return
 	}
 
-	pathSet := set.NewStringSet()
-	for _, fa := range accesses {
-		pathSet.Add(fa.GetFile().GetNodePath())
+	// Construct a string for each distinct path, and accumulate
+	// file operation, so we can show all the kinds of activity
+	// for each file, to provide the most important info.
+	pathToOperation := make(map[string][]string, 0)
+	for _, fa := range v.GetAccesses() {
+		path := fa.GetFile().GetNodePath()
+		pathToOperation[path] = append(pathToOperation[path], fa.GetOperation().String())
 	}
 
-	var sb strings.Builder
-
-	if pathSet.Cardinality() < 10 {
-		// Construct a string for each distinct path, and accumulate
-		// file operation, so we can show all the kinds of activity
-		// for each file, to provide the most important info.
-
-		pathToOperation := make(map[string][]string, pathSet.Cardinality())
-		for _, fa := range accesses {
-			path := fa.GetFile().GetNodePath()
-			pathToOperation[path] = append(pathToOperation[path], fa.GetOperation().String())
-		}
-
-		parts := make([]string, 0, pathSet.Cardinality())
-
-		// sorted to make this more deterministic which means both that
-		// the output is more consistent, and it is more testable.
-		paths := pathSet.AsSortedSlice(func(a, b string) bool {
-			return strings.Compare(a, b) < 0
-		})
-
-		for _, path := range paths {
-			parts = append(parts, fmt.Sprintf("'%v' accessed (%s)", path, strings.Join(pathToOperation[path], ", ")))
-		}
-
-		fmt.Fprintf(&sb, "%s", strings.Join(parts, "; "))
-	} else {
-		fmt.Fprintf(&sb, "%d sensitive files accessed", pathSet.Cardinality())
+	if len(pathToOperation) >= maxPaths {
+		v.Message = fmt.Sprintf("%d sensitive files accessed", len(pathToOperation))
+		return
 	}
 
-	v.Message = sb.String()
+	// sorted to make this more deterministic which means both that
+	// the output is more consistent, and it is more testable.
+	paths := slices.SortedFunc(maps.Keys(pathToOperation), func(a, b string) int {
+		return strings.Compare(a, b)
+	})
+
+	parts := make([]string, 0, len(pathToOperation))
+	for _, path := range paths {
+		parts = append(parts, fmt.Sprintf("'%v' accessed (%s)", path, strings.Join(pathToOperation[path], ", ")))
+	}
+
+	v.Message = fmt.Sprintf("%s", strings.Join(parts, "; "))
 }
