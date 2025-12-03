618c01aa1dc9a3cd986be7c56e23a0027b86e098
AJ Heflin <aheflin@redhat.com>
2025-12-03 10:07:11 -0500

    Revert "ROX-31406: Add non-migration method of updating policy categories (#17804)"
    This reverts commit ed9c7ab230d157d834463eeaa0f801383b7fee0b.

--- central/policycategory/utils/utils.go
+++ central/policycategory/utils/utils.go
@@ -1,33 +1,12 @@
 package utils
 
-import (
-	"strings"
-
-	"github.com/stackrox/rox/generated/storage"
-	"golang.org/x/text/cases"
-	"golang.org/x/text/language"
-)
-
-var (
-	titleCase = cases.Title(language.English)
-)
+import "github.com/stackrox/rox/generated/storage"
 
 // GetCategoryNameToIDs gets a map of category name to category id
 func GetCategoryNameToIDs(categories []*storage.PolicyCategory) map[string]string {
-	lowerNameIDMap := make(map[string]*storage.PolicyCategory)
 	nameIDMap := make(map[string]string, len(categories))
 	for _, c := range categories {
-		// ROX-31406
-		// If the entry exists, but the one we're looking at doesn't match the titleCase spec, we want to use this, as
-		// it's likely "more uppercase" than the current candidate for this name.
-		if _, found := lowerNameIDMap[strings.ToLower(c.GetName())]; found && titleCase.String(c.GetName()) != c.GetName() {
-			lowerNameIDMap[strings.ToLower(c.GetName())] = c
-		} else if !found {
-			lowerNameIDMap[strings.ToLower(c.GetName())] = c
-		}
-	}
-	for _, c := range categories {
-		nameIDMap[c.GetName()] = lowerNameIDMap[strings.ToLower(c.GetName())].GetId()
+		nameIDMap[c.GetName()] = c.GetId()
 	}
 	return nameIDMap
 }
