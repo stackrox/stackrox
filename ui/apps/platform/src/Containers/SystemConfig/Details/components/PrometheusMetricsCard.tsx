5c6f816257d30dc2a2f3ab5d0957be931a993c29
Michaël Petrov <github@parameta.lol>
2025-12-03 17:26:37 +0100

    nit

--- ui/apps/platform/src/Containers/SystemConfig/Details/components/PrometheusMetricsCard.tsx
+++ ui/apps/platform/src/Containers/SystemConfig/Details/components/PrometheusMetricsCard.tsx
@@ -156,11 +156,11 @@ function predefinedMetricTableRow(
             <Td key={`${category}-${metric}-descriptors`}>
                 {labelGroup(predefinedMetrics[category][metric])}
             </Td>
-            {showFilters ? (
+            {showFilters ?? (
                 <Td key={`${category}-${metric}-filters`}>
                     {filterGroup(predefinedMetrics[category][metric])}
                 </Td>
-            ) : null}
+            )}
         </Tr>
     );
 }
@@ -231,7 +231,7 @@ function PrometheusMetricsTable({
                     <Th width={30}>Metric name</Th>
                     <Th width={10}>Origin</Th>
                     <Th>Labels</Th>
-                    {showFilters ? <Th>Filters</Th> : null}
+                    {showFilters ?? <Th>Filters</Th>}
                 </Tr>
             </Thead>
             <Tbody>
@@ -283,7 +283,7 @@ function PrometheusMetricsTable({
                             </Td>
                             <Td>Custom</Td>
                             <Td>{labelGroup(labels)}</Td>
-                            {showFilters ? <Td>{filterGroup(labels)}</Td> : null}
+                            {showFilters ?? <Td>{filterGroup(labels)}</Td>}
                         </Tr>
                     );
                 })}
