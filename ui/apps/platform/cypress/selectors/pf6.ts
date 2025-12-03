cbcf5ae880d3ca7c5efbe8d7b7ec45e07a55ea7a
David Vail <dcvail17@gmail.com>
2025-12-03 08:50:19 -0500

    ROX-31767: Fix selectors for logout test (#17952)

--- ui/apps/platform/cypress/selectors/pf6.ts
+++ ui/apps/platform/cypress/selectors/pf6.ts
@@ -1,7 +1,6 @@
-const navSelectors = {
-    nav: 'nav[data-ouia-component-type="PF6/Nav"]',
-    navExpandable: `li[data-ouia-component-type="PF6/NavExpandable"]`,
-    navItem: `li[data-ouia-component-type="PF6/NavItem"]`,
+const dropdownSelectors = {
+    dropdown: 'div[data-ouia-component-type="PF6/Dropdown"]',
+    dropdownItem: '*[data-ouia-component-type="PF6/DropdownItem"]',
 } as const;
 
 const menu = 'div[data-ouia-component-type="PF6/Menu"]';
@@ -11,13 +10,20 @@ const menuSelectors = {
     menuItem: `${menu} *[role="menuitem"]`,
 } as const;
 
+const navSelectors = {
+    nav: 'nav[data-ouia-component-type="PF6/Nav"]',
+    navExpandable: `li[data-ouia-component-type="PF6/NavExpandable"]`,
+    navItem: `li[data-ouia-component-type="PF6/NavItem"]`,
+} as const;
+
 const tabsSelectors = {
     tab: '[data-ouia-component-type="PF6/Tab"]',
     tabButton: '[data-ouia-component-type="PF6/TabButton"]',
 } as const;
 
 export default {
-    ...navSelectors,
+    ...dropdownSelectors,
     ...menuSelectors,
+    ...navSelectors,
     ...tabsSelectors,
 };
