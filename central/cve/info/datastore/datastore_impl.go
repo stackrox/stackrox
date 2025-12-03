d694555d300eaa90eff8371b2ba4338258b6b371
AJ Heflin <aheflin@redhat.com>
2025-12-03 11:38:47 -0500

    Update datastore

--- central/cve/info/datastore/datastore_impl.go
+++ central/cve/info/datastore/datastore_impl.go
@@ -17,9 +17,9 @@ func (ds *datastoreImpl) Search(ctx context.Context, q *v1.Query) ([]searchPkg.R
 	return ds.storage.Search(ctx, q)
 }
 
-func (ds *datastoreImpl) SearchRawImageCVETimes(ctx context.Context, q *v1.Query) ([]*storage.ImageCVETime, error) {
-	times := make([]*storage.ImageCVETime, 0)
-	err := ds.storage.GetByQueryFn(ctx, q, func(cve *storage.ImageCVETime) error {
+func (ds *datastoreImpl) SearchRawImageCVEInfos(ctx context.Context, q *v1.Query) ([]*storage.ImageCVEInfo, error) {
+	times := make([]*storage.ImageCVEInfo, 0)
+	err := ds.storage.GetByQueryFn(ctx, q, func(cve *storage.ImageCVEInfo) error {
 		times = append(times, cve)
 		return nil
 	})
@@ -30,7 +30,7 @@ func (ds *datastoreImpl) Exists(ctx context.Context, id string) (bool, error) {
 	return ds.storage.Exists(ctx, id)
 }
 
-func (ds *datastoreImpl) Get(ctx context.Context, id string) (*storage.ImageCVETime, bool, error) {
+func (ds *datastoreImpl) Get(ctx context.Context, id string) (*storage.ImageCVEInfo, bool, error) {
 	return ds.storage.Get(ctx, id)
 }
 
@@ -38,15 +38,15 @@ func (ds *datastoreImpl) Count(ctx context.Context, q *v1.Query) (int, error) {
 	return ds.storage.Count(ctx, q)
 }
 
-func (ds *datastoreImpl) GetBatch(ctx context.Context, ids []string) (times []*storage.ImageCVETime, err error) {
+func (ds *datastoreImpl) GetBatch(ctx context.Context, ids []string) (times []*storage.ImageCVEInfo, err error) {
 	times, _, err = ds.storage.GetMany(ctx, ids)
 	return
 }
 
-func (ds *datastoreImpl) Upsert(ctx context.Context, time *storage.ImageCVETime) error {
+func (ds *datastoreImpl) Upsert(ctx context.Context, time *storage.ImageCVEInfo) error {
 	return ds.storage.Upsert(ctx, time)
 }
 
-func (ds *datastoreImpl) UpsertMany(ctx context.Context, times []*storage.ImageCVETime) error {
+func (ds *datastoreImpl) UpsertMany(ctx context.Context, times []*storage.ImageCVEInfo) error {
 	return ds.storage.UpsertMany(ctx, times)
 }
