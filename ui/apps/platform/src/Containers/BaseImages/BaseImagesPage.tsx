f78270a66d646289a7f805682c9c86f8b3de877a
Saif Chaudhry <sachaudh@users.noreply.github.com>
2025-12-08 11:13:07 -0800

    ROX-31795: Add base images page with state management (#18052)
    Signed-off-by: Saif Chaudhry <schaudhr@redhat.com>

--- ui/apps/platform/src/Containers/BaseImages/BaseImagesPage.tsx
+++ ui/apps/platform/src/Containers/BaseImages/BaseImagesPage.tsx
@@ -1,11 +1,119 @@
-import { PageSection, Title } from '@patternfly/react-core';
+import { useCallback, useState } from 'react';
+import { isAxiosError } from 'axios';
+import {
+    Alert,
+    Button,
+    Divider,
+    Flex,
+    FlexItem,
+    PageSection,
+    Text,
+    Title,
+} from '@patternfly/react-core';
+
+import {
+    addBaseImage as addBaseImageFn,
+    deleteBaseImage as deleteBaseImageFn,
+    getBaseImages,
+} from 'services/BaseImagesService';
+import useRestMutation from 'hooks/useRestMutation';
+import useRestQuery from 'hooks/useRestQuery';
+import { getAxiosErrorMessage } from 'utils/responseErrorUtils';
+
+import BaseImagesModal from './BaseImagesModal';
+import BaseImagesTable from './BaseImagesTable';
 
 function BaseImagesPage() {
+    const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+
+    const baseImagesRequest = useRestQuery(useCallback(getBaseImages, []));
+
+    const addBaseImageMutation = useRestMutation(
+        (data: { baseImageRepoPath: string; baseImageTagPattern: string }) =>
+            addBaseImageFn(data.baseImageRepoPath, data.baseImageTagPattern),
+        {
+            onSuccess: () => {
+                baseImagesRequest.refetch();
+            },
+        }
+    );
+
+    const deleteBaseImageMutation = useRestMutation((id: string) => deleteBaseImageFn(id), {
+        onSuccess: () => {
+            baseImagesRequest.refetch();
+        },
+    });
+
+    function handleCloseAddModal() {
+        setIsAddModalOpen(false);
+    }
+
+    function handleOpenAddModal() {
+        addBaseImageMutation.reset();
+        setIsAddModalOpen(true);
+    }
+
+    function handleRemoveBaseImage(id: string) {
+        deleteBaseImageMutation.mutate(id);
+    }
+
+    const baseImages = baseImagesRequest.data ?? [];
+
     return (
-        <PageSection variant="light">
-            <Title headingLevel="h1">Base Images</Title>
-            {/* Page content will be implemented later */}
-        </PageSection>
+        <>
+            <PageSection variant="light">
+                <Flex
+                    alignItems={{ default: 'alignItemsCenter' }}
+                    spaceItems={{ default: 'spaceItemsLg' }}
+                >
+                    <FlexItem flex={{ default: 'flex_1' }}>
+                        <Title headingLevel="h1">Base Images</Title>
+                        <Text>
+                            Manage approved base images for vulnerability tracking and
+                            layer-specific filtering
+                        </Text>
+                    </FlexItem>
+                    <Button variant="primary" onClick={handleOpenAddModal}>
+                        Add base image
+                    </Button>
+                </Flex>
+            </PageSection>
+            <Divider component="div" />
+            <PageSection>
+                {deleteBaseImageMutation.isError && (
+                    <Alert
+                        variant="danger"
+                        isInline
+                        title="Error removing base image"
+                        component="p"
+                        className="pf-v5-u-mb-lg"
+                    >
+                        {getAxiosErrorMessage(deleteBaseImageMutation.error)}
+                    </Alert>
+                )}
+                <BaseImagesTable
+                    baseImages={baseImages}
+                    onRemove={(baseImage) => handleRemoveBaseImage(baseImage.id)}
+                    isRemoveInProgress={deleteBaseImageMutation.isLoading}
+                    isLoading={baseImagesRequest.isLoading && !baseImagesRequest.data}
+                    error={baseImagesRequest.error as Error | null}
+                />
+            </PageSection>
+            <BaseImagesModal
+                isOpen={isAddModalOpen}
+                onClose={handleCloseAddModal}
+                onSave={() =>
+                    addBaseImageMutation.mutate({
+                        baseImageRepoPath: '',
+                        baseImageTagPattern: '',
+                    })
+                }
+                isSuccess={addBaseImageMutation.isSuccess}
+                isError={addBaseImageMutation.isError}
+                isSubmitting={addBaseImageMutation.isLoading}
+                error={isAxiosError(addBaseImageMutation.error) ? addBaseImageMutation.error : null}
+            />
+        </>
     );
 }
 
