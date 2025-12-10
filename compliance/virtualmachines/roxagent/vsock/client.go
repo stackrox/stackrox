75fa169305040969deadba2ae7373c335342e774
Guzman <guz@redhat.com>
2025-12-10 14:26:38 +0100

    Apply suggestion from @vikin91
    Co-authored-by: Piotr Rygielski <114479+vikin91@users.noreply.github.com>

--- compliance/virtualmachines/roxagent/vsock/client.go
+++ compliance/virtualmachines/roxagent/vsock/client.go
@@ -35,7 +35,7 @@ func (c *Client) SendIndexReport(report *v4.IndexReport) error {
 	if c.Verbose {
 		reportJson, err := jsonutil.ProtoToJSON(wrappedReport)
 		if err != nil {
-			log.Errorf("Failed to convert index report to JSON (vsockCid=%d): %v", vsockCid, err)
+			log.Errorf("Failed to convert index report to JSON (vsockCid=%s): %v", wrappedReport.VsockCid, err)
 		} else {
 			fmt.Println(reportJson)
 		}
