3931ae6c7bc5f196b9f7b264e601b895ae74058b
daynewlee <yli3@redhat.com>
2025-12-11 16:40:40 -0600

    added baseimage/datastore/

---
+++ central/baseimage/datastore/datastore.go
@@ -0,0 +1,19 @@
+package datastore
+
+import (
+	"context"
+
+	"github.com/stackrox/rox/generated/storage"
+)
+
+// DataStore provides access to base image repositories.
+//
+//go:generate mockgen-wrapper
+type DataStore interface {
+
+	// ListRepositories returns all base images and their layers whose first layer matches the specified digest.
+	// Only the first-layer digest is used for matching.
+	// Returns empty slice if no repositories configured.
+	// Returns error only for system failures (database connection, etc.).
+	ListCandidateBaseImages(ctx context.Context, firstLayer string) ([]*storage.BaseImage, error)
+}
