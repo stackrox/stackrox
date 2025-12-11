a477725eb280ca5d168bbd5bc495b95251b07ea7
Saif Chaudhry <schaudhr@redhat.com>
2025-12-10 21:24:34 -0800

    fix(ui): addressing comments
    Signed-off-by: Saif Chaudhry <schaudhr@redhat.com>

--- ui/apps/platform/src/routePaths.test.ts
+++ ui/apps/platform/src/routePaths.test.ts
@@ -5,6 +5,13 @@ import type { HasReadAccess } from 'hooks/usePermissions';
 
 import { isRouteEnabled } from './routePaths';
 
+/*
+ * TODO: Consider refactoring route access tests to use persona-based testing.
+ * Instead of checking individual resource names (e.g., 'Image'), we could test
+ * as personas: `asPersona('readOnlyAnalyst')` that encapsulates the expected
+ * permissions. This would make tests more readable and aligned with real user roles.
+ * See: https://github.com/stackrox/stackrox/pull/18130#discussion_r2607802777
+ */
 describe('routePaths', () => {
     describe('isRouteEnabled for base-images', () => {
         it('should enable route when feature flag is enabled and user has Image read access', () => {
