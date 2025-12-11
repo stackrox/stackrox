ed6690c19b36af9bbd003431212685d7d672fd14
Dave Shrewsberry <dashrews@redhat.com>
2025-12-11 12:06:49 -0500

    adding the test

--- migrator/migrations/m_214_to_m_215_process_indicator_to_btree/migration_test.go
+++ migrator/migrations/m_214_to_m_215_process_indicator_to_btree/migration_test.go
@@ -6,16 +6,21 @@ import (
 	"context"
 	"testing"
 
+	"github.com/stackrox/rox/generated/storage"
+	oldSchema "github.com/stackrox/rox/migrator/migrations/m_212_to_m_213_add_container_start_column_to_indicators/test/schema"
 	pghelper "github.com/stackrox/rox/migrator/migrations/postgreshelper"
 	"github.com/stackrox/rox/migrator/types"
+	"github.com/stackrox/rox/pkg/postgres/pgutils"
 	"github.com/stackrox/rox/pkg/sac"
+	"github.com/stackrox/rox/pkg/testutils"
+	"github.com/stackrox/rox/pkg/uuid"
 	"github.com/stretchr/testify/suite"
 )
 
 type migrationTestSuite struct {
 	suite.Suite
 
-	db *pghelper.TestPostgres
+	db  *pghelper.TestPostgres
 	ctx context.Context
 }
 
@@ -23,37 +28,82 @@ func TestMigration(t *testing.T) {
 	suite.Run(t, new(migrationTestSuite))
 }
 
-
 func (s *migrationTestSuite) SetupSuite() {
 	s.ctx = sac.WithAllAccess(context.Background())
 	s.db = pghelper.ForT(s.T(), false)
 	// TODO(dont-merge): Create the schemas and tables required for the pre-migration dataset push to DB
 }
 
-
-
-
 func (s *migrationTestSuite) TestMigration() {
-	// TODO(dont-merge): instantiate any store required for the pre-migration dataset push to DB
-
-	// TODO(dont-merge): push the pre-migration dataset to DB
-
 	dbs := &types.Databases{
 		GormDB:     s.db.GetGormDB(),
 		PostgresDB: s.db.DB,
 		DBCtx:      s.ctx,
 	}
 
+	pgutils.CreateTableFromModel(dbs.DBCtx, dbs.GormDB, oldSchema.CreateTableProcessIndicatorsStmt)
+
+	// Add some process indicators
+	var indicators []*storage.ProcessIndicator
+	numIndicators := 3000
+	for i := 0; i < numIndicators; i++ {
+		processIndicator := &storage.ProcessIndicator{}
+		s.NoError(testutils.FullInit(processIndicator, testutils.UniqueInitializer(), testutils.JSONFieldsFilter))
+		indicators = append(indicators, processIndicator)
+	}
+
+	var convertedProcessIndicators []oldSchema.ProcessIndicators
+	for _, processIndicator := range indicators {
+		// spreading these across some deployments to set up search test
+		processIndicator.Id = uuid.NewV4().String()
+
+		converted, err := oldSchema.ConvertProcessIndicatorFromProto(processIndicator)
+		s.Require().NoError(err)
+		convertedProcessIndicators = append(convertedProcessIndicators, *converted)
+	}
+	if len(convertedProcessIndicators) > 0 {
+		s.Require().NoError(dbs.GormDB.CreateInBatches(convertedProcessIndicators, numIndicators).Error)
+	}
+	log.Info("Created the indicators")
+
+	// Verify hash indexes
+	row := s.db.QueryRow(s.ctx, hashIndexQuery, tableName, deploymentIndex)
+	var exists bool
+	s.Require().NoError(row.Scan(&exists))
+	s.Require().True(exists)
+
+	row = s.db.QueryRow(s.ctx, hashIndexQuery, tableName, deploymentIndex)
+	s.Require().NoError(row.Scan(&exists))
+	s.Require().True(exists)
+
 	s.Require().NoError(migration.Run(dbs))
 
-	// TODO(dont-merge): instantiate any store required for the post-migration dataset pull from DB
+	// Verfiy hash indexes no longer exist.
+	row = s.db.QueryRow(s.ctx, hashIndexQuery, tableName, deploymentIndex)
+	s.Require().NoError(row.Scan(&exists))
+	s.Require().False(exists)
 
-	// TODO(dont-merge): pull the post-migration dataset from DB
+	row = s.db.QueryRow(s.ctx, hashIndexQuery, tableName, deploymentIndex)
+	s.Require().NoError(row.Scan(&exists))
+	s.Require().False(exists)
 
-	// TODO(dont-merge): validate that the post-migration dataset has the expected content
+	// Verify btree indexes
+	btreeIndexQuery := `SELECT EXISTS(
+	SELECT tab.relname, cls.relname, am.amname
+	FROM pg_index idx
+	JOIN pg_class cls ON cls.oid=idx.indexrelid
+	JOIN pg_class tab ON tab.oid=idx.indrelid
+	JOIN pg_am am ON am.oid=cls.relam
+	where tab.relname = $1 AND
+	am.amname = 'btree' AND cls.relname = $2
+	)`
+	row = s.db.QueryRow(s.ctx, btreeIndexQuery, tableName, deploymentIndex)
+	s.Require().NoError(row.Scan(&exists))
+	s.Require().True(exists)
 
-	// TODO(dont-merge): validate that pre-migration queries and statements execute against the
-	// post-migration database to ensure backwards compatibility
+	row = s.db.QueryRow(s.ctx, btreeIndexQuery, tableName, deploymentIndex)
+	s.Require().NoError(row.Scan(&exists))
+	s.Require().True(exists)
 
 }
 
