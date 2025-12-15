56c37e69ffcbf6910d9a30a6bd1a10fbd5a71be1
David Vail <dcvail17@gmail.com>
2025-12-15 13:33:27 -0500

    Update Risk table page layout to PF

--- ui/apps/platform/src/Containers/Risk/RiskTablePage.tsx
+++ ui/apps/platform/src/Containers/Risk/RiskTablePage.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from '@apollo/client';
+import { PageSection } from '@patternfly/react-core';
 
-import { PageBody } from 'Components/Panel';
 import { searchCategories } from 'constants/entityTypes';
 import { SEARCH_OPTIONS_QUERY } from 'queries/search';
 import useURLPagination from 'hooks/useURLPagination';
@@ -45,10 +45,10 @@ function RiskTablePage() {
                     urlSearch.setSearchFilter(newSearchFilter);
                 }}
             />
-            <PageBody>
-                <div className="flex-shrink-1 overflow-hidden w-full">
+            {/* Nested PageSection here for visual consistency **as-is**. Once we move to Patternfly 6, we can remove this and clean up */}
+            <PageSection>
+                <PageSection variant="light" component="div">
                     <RiskTablePanel
-                        isViewFiltered={isViewFiltered}
                         sortOption={urlSort.sortOption}
                         getSortParams={urlSort.getSortParams}
                         searchFilter={urlSearch.searchFilter}
@@ -58,8 +58,8 @@ function RiskTablePage() {
                         }}
                         pagination={urlPagination}
                     />
-                </div>
-            </PageBody>
+                </PageSection>
+            </PageSection>
         </>
     );
 }
