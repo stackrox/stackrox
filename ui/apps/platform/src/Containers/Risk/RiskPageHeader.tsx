d0a82c9f2d90dfc8dde58849c3d74aa918951be8
David Vail <dcvail17@gmail.com>
2025-12-15 13:39:00 -0500

    Move search to table panel section

--- ui/apps/platform/src/Containers/Risk/RiskPageHeader.tsx
+++ ui/apps/platform/src/Containers/Risk/RiskPageHeader.tsx
@@ -1,30 +1,14 @@
-import entityTypes, { searchCategories } from 'constants/entityTypes';
 import PageHeader from 'Components/PageHeader';
-import {
-    ORCHESTRATOR_COMPONENTS_KEY,
-    orchestratorComponentsOption,
-} from 'utils/orchestratorComponents';
-import SearchFilterInput from 'Components/SearchFilterInput';
 import useIsRouteEnabled from 'hooks/useIsRouteEnabled';
 import usePermissions from 'hooks/usePermissions';
-import searchOptionsToQuery from 'services/searchOptionsToQuery';
 
 import CreatePolicyFromSearch from './CreatePolicyFromSearch';
-import type { SearchFilter } from 'types/search';
 
 type RiskPageHeaderProps = {
     isViewFiltered: boolean;
-    searchOptions: string[];
-    searchFilter: SearchFilter;
-    onSearch: (newSearchFilter: SearchFilter) => void;
 };
 
-function RiskPageHeader({
-    isViewFiltered,
-    searchOptions,
-    searchFilter,
-    onSearch,
-}: RiskPageHeaderProps) {
+function RiskPageHeader({ isViewFiltered }: RiskPageHeaderProps) {
     const isRouteEnabled = useIsRouteEnabled();
     const { hasReadWriteAccess } = usePermissions();
     // Require READ_WRITE_ACCESS to create plus READ_ACCESS to other resources for Policies route.
@@ -32,22 +16,8 @@ function RiskPageHeader({
         hasReadWriteAccess('WorkflowAdministration') && isRouteEnabled('policy-management');
 
     const subHeader = isViewFiltered ? 'Filtered view' : 'Default view';
-    const autoCompleteCategory = searchCategories[entityTypes.DEPLOYMENT];
-
-    const orchestratorComponentShowState = localStorage.getItem(ORCHESTRATOR_COMPONENTS_KEY);
-    const prependAutocompleteQuery =
-        orchestratorComponentShowState !== 'true' ? orchestratorComponentsOption : [];
     return (
         <PageHeader header="Risk" subHeader={subHeader}>
-            <SearchFilterInput
-                className="w-full"
-                searchFilter={searchFilter}
-                searchOptions={searchOptions}
-                searchCategory={autoCompleteCategory}
-                placeholder="Filter deployments"
-                handleChangeSearchFilter={onSearch}
-                autocompleteQueryPrefix={searchOptionsToQuery(prependAutocompleteQuery)}
-            />
             {hasWriteAccessForCreatePolicy && <CreatePolicyFromSearch />}
         </PageHeader>
     );
--- ui/apps/platform/src/Containers/Risk/RiskTablePage.tsx
+++ ui/apps/platform/src/Containers/Risk/RiskTablePage.tsx
@@ -1,15 +1,22 @@
 import { useQuery } from '@apollo/client';
-import { PageSection } from '@patternfly/react-core';
+import { Divider, PageSection } from '@patternfly/react-core';
 
-import { searchCategories } from 'constants/entityTypes';
+import entityTypes, { searchCategories } from 'constants/entityTypes';
 import { SEARCH_OPTIONS_QUERY } from 'queries/search';
 import useURLPagination from 'hooks/useURLPagination';
 import useURLSort from 'hooks/useURLSort';
 import useURLSearch from 'hooks/useURLSearch';
 import { getHasSearchApplied } from 'utils/searchUtils';
 
-import RiskPageHeader from './RiskPageHeader';
+import SearchFilterInput from 'Components/SearchFilterInput';
+import searchOptionsToQuery from 'services/searchOptionsToQuery';
+import {
+    ORCHESTRATOR_COMPONENTS_KEY,
+    orchestratorComponentsOption,
+} from 'utils/orchestratorComponents';
+
 import RiskTablePanel, { sortFields, defaultSortOption } from './RiskTablePanel';
+import RiskPageHeader from './RiskPageHeader';
 
 const DEFAULT_RISK_PAGE_SIZE = 20;
 
@@ -34,20 +41,32 @@ function RiskTablePage() {
     const filteredSearchOptions = searchOptions.filter(
         (option) => option !== 'Orchestrator Component'
     );
+
+    const autoCompleteCategory = searchCategories[entityTypes.DEPLOYMENT];
+
+    const orchestratorComponentShowState = localStorage.getItem(ORCHESTRATOR_COMPONENTS_KEY);
+    const prependAutocompleteQuery =
+        orchestratorComponentShowState !== 'true' ? orchestratorComponentsOption : [];
+
     return (
         <>
-            <RiskPageHeader
-                isViewFiltered={isViewFiltered}
-                searchOptions={filteredSearchOptions}
-                searchFilter={urlSearch.searchFilter}
-                onSearch={(newSearchFilter) => {
-                    urlPagination.setPage(1);
-                    urlSearch.setSearchFilter(newSearchFilter);
-                }}
-            />
+            <RiskPageHeader isViewFiltered={isViewFiltered} />
             {/* Nested PageSection here for visual consistency **as-is**. Once we move to Patternfly 6, we can remove this and clean up */}
             <PageSection>
                 <PageSection variant="light" component="div">
+                    <SearchFilterInput
+                        className="w-full"
+                        searchFilter={urlSearch.searchFilter}
+                        searchOptions={filteredSearchOptions}
+                        searchCategory={autoCompleteCategory}
+                        placeholder="Filter deployments"
+                        handleChangeSearchFilter={(newSearchFilter) => {
+                            urlSearch.setSearchFilter(newSearchFilter);
+                            urlPagination.setPage(1);
+                        }}
+                        autocompleteQueryPrefix={searchOptionsToQuery(prependAutocompleteQuery)}
+                    />
+                    <Divider component="div" className="pf-v5-u-mt-lg" />
                     <RiskTablePanel
                         sortOption={urlSort.sortOption}
                         getSortParams={urlSort.getSortParams}
