ea38682c4aa25da34f061720eff44822e001a50a
Mark Pedrotti <pedrottimark@gmail.com>
2025-12-18 11:12:04 -0500

    ROX-32270: Rewrite with switch in CompoundSearchFilterInputField (#18284)

--- ui/apps/platform/src/Components/CompoundSearchFilter/components/CompoundSearchFilterInputField.tsx
+++ ui/apps/platform/src/Components/CompoundSearchFilter/components/CompoundSearchFilterInputField.tsx
@@ -1,4 +1,6 @@
 import type { SearchFilter } from 'types/search';
+import { ensureExhaustive } from 'utils/type.utils';
+
 import type {
     CompoundSearchFilterAttribute,
     CompoundSearchFilterEntity,
@@ -28,57 +30,53 @@ function CompoundSearchFilterInputField({
     searchFilter,
     additionalContextFilter,
 }: CompoundSearchFilterInputFieldProps) {
-    if (attribute.inputType === 'text') {
-        return <SearchFilterText attribute={attribute} onSearch={onSearch} />;
+    const { inputType } = attribute;
+    switch (inputType) {
+        case 'text':
+            return <SearchFilterText attribute={attribute} onSearch={onSearch} />;
+        case 'date-picker':
+            return <SearchFilterConditionDate attribute={attribute} onSearch={onSearch} />;
+        case 'condition-number':
+            return <SearchFilterConditionNumber attribute={attribute} onSearch={onSearch} />;
+        case 'condition-text':
+            return <SearchFilterConditionText attribute={attribute} onSearch={onSearch} />;
+        case 'autocomplete':
+            return (
+                <SearchFilterAutocompleteSelect
+                    additionalContextFilter={additionalContextFilter}
+                    attribute={attribute}
+                    onSearch={onSearch}
+                    searchCategory={entity.searchCategory}
+                    searchFilter={searchFilter}
+                />
+            );
+        case 'select-exclusive-double':
+            return (
+                <SearchFilterSelectExclusiveDouble
+                    attribute={attribute}
+                    onSearch={onSearch}
+                    searchFilter={searchFilter}
+                />
+            );
+        case 'select-exclusive-single':
+            return (
+                <SearchFilterSelectExclusiveSingle
+                    attribute={attribute}
+                    onSearch={onSearch}
+                    searchFilter={searchFilter}
+                />
+            );
+        case 'select':
+            return (
+                <SearchFilterSelectInclusive
+                    attribute={attribute}
+                    onSearch={onSearch}
+                    searchFilter={searchFilter}
+                />
+            );
+        default:
+            return ensureExhaustive(inputType);
     }
-    if (attribute.inputType === 'date-picker') {
-        return <SearchFilterConditionDate attribute={attribute} onSearch={onSearch} />;
-    }
-    if (attribute.inputType === 'condition-number') {
-        return <SearchFilterConditionNumber attribute={attribute} onSearch={onSearch} />;
-    }
-    if (attribute.inputType === 'condition-text') {
-        return <SearchFilterConditionText attribute={attribute} onSearch={onSearch} />;
-    }
-    if (attribute.inputType === 'autocomplete') {
-        return (
-            <SearchFilterAutocompleteSelect
-                additionalContextFilter={additionalContextFilter}
-                attribute={attribute}
-                onSearch={onSearch}
-                searchCategory={entity.searchCategory}
-                searchFilter={searchFilter}
-            />
-        );
-    }
-    if (attribute.inputType === 'select-exclusive-double') {
-        return (
-            <SearchFilterSelectExclusiveDouble
-                attribute={attribute}
-                onSearch={onSearch}
-                searchFilter={searchFilter}
-            />
-        );
-    }
-    if (attribute.inputType === 'select-exclusive-single') {
-        return (
-            <SearchFilterSelectExclusiveSingle
-                attribute={attribute}
-                onSearch={onSearch}
-                searchFilter={searchFilter}
-            />
-        );
-    }
-    if (attribute.inputType === 'select') {
-        return (
-            <SearchFilterSelectInclusive
-                attribute={attribute}
-                onSearch={onSearch}
-                searchFilter={searchFilter}
-            />
-        );
-    }
-    return <div />;
 }
 
 export default CompoundSearchFilterInputField;
