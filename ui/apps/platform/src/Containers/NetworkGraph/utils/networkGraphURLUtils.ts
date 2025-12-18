15cc5d2c6dede2b397e541fa1dc770bc0aeba1d3
David Vail <dcvail17@gmail.com>
2025-12-18 14:04:41 -0500

    Lift URL util to avoid x-container calls

--- ui/apps/platform/src/Containers/NetworkGraph/utils/networkGraphURLUtils.ts
+++ ui/apps/platform/src/Containers/NetworkGraph/utils/networkGraphURLUtils.ts
@@ -1,27 +1,4 @@
-import { networkBasePath } from 'routePaths';
-import { getQueryString } from 'utils/queryStringUtils';
 import type { SearchFilter } from 'types/search';
-
-type GetURLLinkToDeploymentParams = {
-    cluster: string;
-    namespace: string;
-    deploymentId: string;
-};
-
-export function getURLLinkToDeployment({
-    cluster,
-    namespace,
-    deploymentId,
-}: GetURLLinkToDeploymentParams) {
-    const queryString = getQueryString({
-        s: {
-            Cluster: cluster,
-            Namespace: namespace,
-        },
-    });
-    const networkGraphLink = `${networkBasePath}/deployment/${deploymentId}${queryString}`;
-    return networkGraphLink;
-}
 
 export function getPropertiesForAnalytics(searchFilter: SearchFilter) {
     const cluster = searchFilter?.Cluster?.toString() ? 1 : 0;
