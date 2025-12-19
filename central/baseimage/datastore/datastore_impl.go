d5fe8fd411bf767193fdee907869c18d3d8b758a
daynewlee <yli3@redhat.com>
2025-12-19 09:44:13 -0600

    added empty layers check

--- central/baseimage/datastore/datastore_impl.go
+++ central/baseimage/datastore/datastore_impl.go
@@ -2,15 +2,13 @@ package datastore
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/pkg/errors"
 	baseImageStore "github.com/stackrox/rox/central/baseimage/store/postgres"
 	"github.com/stackrox/rox/generated/storage"
-	"github.com/stackrox/rox/pkg/logging"
 	"github.com/stackrox/rox/pkg/postgres"
-	"github.com/stackrox/rox/pkg/sac"
-	"github.com/stackrox/rox/pkg/sac/resources"
 	"github.com/stackrox/rox/pkg/uuid"
 )
 
@@ -19,18 +17,13 @@ type datastoreImpl struct {
 	db      postgres.DB
 }
 
-var (
-	log       = logging.LoggerForModule()
-	imagesSAC = sac.ForResource(resources.ImageAdministration)
-)
-
 const (
 	baseImagesTable = "base_images"
 	// The 'firstlayerdigest' column is indexed.
 	listByFirstLayerQuery = "SELECT id FROM " + baseImagesTable + " WHERE firstlayerdigest = $1"
-
 	// Query to find the ID by manifest digest
 	getByManifestDigestQuery = "SELECT id FROM " + baseImagesTable + " WHERE manifestdigest = $1"
+	upsertChunkSize          = 100
 )
 
 // New creates a new DataStore instance backed by PostgreSQL.
@@ -42,39 +35,48 @@ func New(store baseImageStore.Store, db postgres.DB) DataStore {
 }
 
 func (ds *datastoreImpl) UpsertImage(ctx context.Context, image *storage.BaseImage, digests []string) error {
-	ok, err := imagesSAC.WriteAllowed(ctx)
-	if err != nil {
-		return err
+	if _, err := layers(image, digests); err != nil {
+		return fmt.Errorf("upsert image %s: %w", image.GetId(), err)
 	}
-	if !ok {
-		return sac.ErrResourceAccessDenied
-	}
-
-	layers(image, digests)
 	return ds.storage.Upsert(ctx, image)
 }
 
 func (ds *datastoreImpl) UpsertImages(
-	ctx context.Context, imagesWithLayer map[*storage.BaseImage][]string,
+	ctx context.Context,
+	imagesWithLayer map[*storage.BaseImage][]string,
 ) error {
-	ok, err := imagesSAC.WriteAllowed(ctx)
-	if err != nil {
-		return err
-	}
-	if !ok {
-		return sac.ErrResourceAccessDenied
-	}
-
 	batch := make([]*storage.BaseImage, 0, len(imagesWithLayer))
 	for img, digests := range imagesWithLayer {
-		layers(img, digests)
+		if _, err := layers(img, digests); err != nil {
+			return fmt.Errorf("prepare layers for image %s: %w", img.GetId(), err)
+		}
+
 		batch = append(batch, img)
 	}
 
-	return ds.storage.UpsertMany(ctx, batch)
+	if len(batch) <= upsertChunkSize {
+		return ds.storage.UpsertMany(ctx, batch)
+	}
+
+	// Process in chunks. This reduces pressure on storage and avoids parameter/time limits.
+	for start := 0; start < len(batch); start += upsertChunkSize {
+		end := start + upsertChunkSize
+		if end > len(batch) {
+			end = len(batch)
+		}
+
+		if err := ds.storage.UpsertMany(ctx, batch[start:end]); err != nil {
+			return fmt.Errorf("upsert images chunk [%d:%d]: %w", start, end, err)
+		}
+	}
+	return nil
 }
 
-func layers(image *storage.BaseImage, digests []string) []*storage.BaseImageLayer {
+func layers(image *storage.BaseImage, digests []string) ([]*storage.BaseImageLayer, error) {
+	if len(digests) == 0 {
+		return nil, fmt.Errorf("layers: empty digests for image %s", image.GetId())
+	}
+
 	layers := make([]*storage.BaseImageLayer, 0, len(digests))
 
 	for i, digest := range digests {
@@ -86,24 +88,12 @@ func layers(image *storage.BaseImage, digests []string) []*storage.BaseImageLaye
 		})
 	}
 
-	if len(digests) > 0 && image.GetFirstLayerDigest() != digests[0] {
-		log.Warnf(
-			"FirstLayerDigest mismatch for image %s: claims %s but first layer is %s.",
-			image.GetId(), image.GetFirstLayerDigest(), digests[0],
-		)
-		image.FirstLayerDigest = digests[0]
-	}
-
+	image.FirstLayerDigest = digests[0]
 	image.Layers = layers
-	return layers
+	return layers, nil
 }
 
 func (ds *datastoreImpl) ListCandidateBaseImages(ctx context.Context, firstLayer string) ([]*storage.BaseImage, error) {
-	if ok, err := imagesSAC.ReadAllowed(ctx); err != nil {
-		return nil, err
-	} else if !ok {
-		return nil, sac.ErrResourceAccessDenied
-	}
 	rows, err := ds.db.Query(ctx, listByFirstLayerQuery, firstLayer)
 	if err != nil {
 		return nil, err
@@ -127,6 +117,7 @@ func (ds *datastoreImpl) ListCandidateBaseImages(ctx context.Context, firstLayer
 		return nil, nil
 	}
 
+	// Use the search framework to load full base image objects
 	baseImages, _, err := ds.storage.GetMany(ctx, ids)
 	if err != nil {
 		return nil, err
@@ -136,11 +127,6 @@ func (ds *datastoreImpl) ListCandidateBaseImages(ctx context.Context, firstLayer
 }
 
 func (ds *datastoreImpl) GetBaseImage(ctx context.Context, manifestDigest string) (*storage.BaseImage, bool, error) {
-	if ok, err := imagesSAC.ReadAllowed(ctx); err != nil {
-		return nil, false, err
-	} else if !ok {
-		return nil, false, sac.ErrResourceAccessDenied
-	}
 	row := ds.db.QueryRow(ctx, getByManifestDigestQuery, manifestDigest)
 
 	var id string
