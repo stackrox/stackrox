a173f879b28ff0a493ab4afdcb0dafaa1d101a6e
Michaël <github@parameta.lol>
2025-12-23 17:43:08 +0100

    ROX-30930: phonehome policy usage option 2 (#17972)
    Co-authored-by: Stephan Hesselmann <shesselm@redhat.com>

--- central/main.go
+++ central/main.go
@@ -685,6 +685,7 @@ func addCentralIdentityGatherers(c *phonehomeClient.CentralClient) {
 	add(roleDataStore.Gather)
 	add(signatureIntegrationDS.Gather)
 	add(complianceScanDS.GatherProfiles(complianceScanDS.Singleton()))
+	add(policyDataStore.Gather)
 }
 
 func registerDelayedIntegrations(integrationsInput []iiStore.DelayedIntegration) {
