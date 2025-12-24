59fd03580c30b37fa14b0c899e817dd46717d748
Saif Chaudhry <schaudhr@redhat.com>
2025-12-23 23:48:28 -0800

    fix(ui): fixing tests
    Signed-off-by: Saif Chaudhry <schaudhr@redhat.com>

--- ui/apps/platform/cypress/integration/baseImages/baseImages.selectors.ts
+++ ui/apps/platform/cypress/integration/baseImages/baseImages.selectors.ts
@@ -10,7 +10,6 @@ export const selectors = {
         baseImagePath: 'th:contains("Base image path")',
         addedBy: 'th:contains("Added by")',
     },
-    tableRows: 'tbody tr',
     rowKebabButton: 'button[aria-label="Kebab toggle"]',
     removeAction: 'button:contains("Remove")',
 
@@ -19,10 +18,6 @@ export const selectors = {
         title: 'h2:contains("Add base image path")',
         input: 'input#baseImagePath',
         saveButton: 'button:contains("Save")',
-        cancelButton: 'button:contains("Cancel")',
-        successAlert: '.pf-v5-c-alert:contains("Base image successfully added")',
-        errorAlert: '.pf-v5-c-alert:contains("Error adding base image")',
-        validationError: '.pf-v5-c-helper-text__item.pf-m-error',
     },
 
     // Delete confirmation modal
@@ -30,6 +25,5 @@ export const selectors = {
         title: 'h2:contains("Delete base image?")',
         confirmButton: '*[role="dialog"] button:contains("Delete")',
         cancelButton: '*[role="dialog"] button:contains("Cancel")',
-        errorAlert: '.pf-v5-c-alert:contains("Error removing base image")',
     },
 };
