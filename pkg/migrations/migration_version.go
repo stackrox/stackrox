696d5ab3eda0d4260875a36f56964d1702d88e6c
Dave Shrewsberry <dashrews@redhat.com>
2026-01-05 12:32:34 -0500

    clean up some unused stuff

--- pkg/migrations/migration_version.go
+++ pkg/migrations/migration_version.go
@@ -2,26 +2,12 @@ package migrations
 
 import (
 	"time"
-
-	"github.com/stackrox/rox/pkg/logging"
-)
-
-const (
-	// MigrationVersionFile records the latest central version in databases.
-	MigrationVersionFile     = "migration_version.yaml"
-	migrationVersionFileMode = 0644
-	lastRocksDBVersion       = "3.74.0"
-)
-
-var (
-	log = logging.LoggerForModule()
 )
 
 // MigrationVersion is the last central version and migration sequence number
 // that run with a database successfully. If central was up and ready to serve,
 // it will update the migration version of current database if needed.
 type MigrationVersion struct {
-	dbPath        string
 	MainVersion   string `yaml:"image"`
 	SeqNum        int    `yaml:"database"`
 	MinimumSeqNum int    `yaml:"mindatabase"`
