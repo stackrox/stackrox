cdb31207efde53081b7807f436da6d0cdf5bf9d8
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2026-01-08 15:28:35 +0100

    Remove the redundant capability check

--- sensor/common/virtualmachine/index/handler_impl.go
+++ sensor/common/virtualmachine/index/handler_impl.go
@@ -207,12 +207,6 @@ func (h *handlerImpl) handleIndexReport(
 		log.Warn("Received nil virtual machine index report: not sending to Central")
 		return
 	}
-
-	if !centralcaps.Has(centralsensor.VirtualMachinesSupported) {
-		outcome = metrics.IndexReportHandlingMessageToCentralCapabilityMissing
-		log.Warnf("Central does not have virtual machine capability: not sending index report for vsock_cid=%q", indexReport.GetVsockCid())
-		return
-	}
 	log.Debugf("Handling virtual machine index report with vsock_cid=%q...", indexReport.GetVsockCid())
 
 	msg, outcome, err := h.newMessageToCentral(indexReport)
