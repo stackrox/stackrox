681114c8ca0f6dfb5dfd57908be47a4254b47df2
Stephan Heßelmann <shesselm@redhat.com>
2026-01-12 13:16:26 +0100

    refactor: use TLSConfig for Central connection without client cert
    Replace AuthenticatedHTTPTransport with clientconn.TLSConfig using
    DontUseClientCert to avoid injecting Sensor's client certificate in
    proxy requests. Authentication is handled via Bearer token only.
    
    Still ensures serviceCertFallbackVerifier is properly used for
    StackRox service certificates.
    
    Also update ErrorHandler to use httputil.WriteError for consistency.

--- sensor/common/centralproxy/proxy.go
+++ sensor/common/centralproxy/proxy.go
@@ -1,7 +1,6 @@
 package centralproxy
 
 import (
-	"crypto/tls"
 	"crypto/x509"
 	"fmt"
 	"net/http"
@@ -9,9 +8,10 @@ import (
 	"net/url"
 
 	"github.com/pkg/errors"
+	"github.com/stackrox/rox/pkg/clientconn"
 	pkghttputil "github.com/stackrox/rox/pkg/httputil"
+	"github.com/stackrox/rox/pkg/mtls"
 	"github.com/stackrox/rox/pkg/mtls/verifier"
-	"google.golang.org/grpc/codes"
 )
 
 // newCentralReverseProxy creates a new ReverseProxy that forwards requests to Central.
@@ -25,13 +25,21 @@ func newCentralReverseProxy(baseURL *url.URL, certs []*x509.Certificate, token s
 		certPool.AddCert(cert)
 	}
 
-	baseTransport := pkghttputil.DefaultTransport()
-	baseTransport.TLSClientConfig = &tls.Config{
-		RootCAs:    certPool,
-		ServerName: baseURL.Hostname(),
+	// Use TLSConfig without client certificate to keep authn/z based on the user's
+	// Bearer token, but include serviceCertFallbackVerifier for proper StackRox service
+	// cert handling.
+	tlsConf, err := clientconn.TLSConfig(mtls.CentralSubject, clientconn.TLSConfigOptions{
+		ServerName:    baseURL.Hostname(),
+		RootCAs:       certPool,
+		UseClientCert: clientconn.DontUseClientCert,
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "creating TLS config")
 	}
 
-	// Minimal wrapper that adds Bearer token authentication.
+	baseTransport := pkghttputil.DefaultTransport()
+	baseTransport.TLSClientConfig = tlsConf
+
 	tokenTransport := pkghttputil.RoundTripperFunc(func(req *http.Request) (*http.Response, error) {
 		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
 		return baseTransport.RoundTrip(req)
@@ -43,7 +51,9 @@ func newCentralReverseProxy(baseURL *url.URL, certs []*x509.Certificate, token s
 			r.SetURL(baseURL)
 		},
 		ErrorHandler: func(w http.ResponseWriter, r *http.Request, err error) {
-			pkghttputil.WriteGRPCStyleErrorf(w, codes.Internal, "failed to contact central: %v", err)
+			pkghttputil.WriteError(w,
+				pkghttputil.Errorf(http.StatusInternalServerError, "failed to contact central: %v", err),
+			)
 		},
 	}, nil
 }
