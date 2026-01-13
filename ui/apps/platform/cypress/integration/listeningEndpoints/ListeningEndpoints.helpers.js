824f6289252a5ea7c30e6c2669f7cff4f799e135
Mark Pedrotti <mark@stackrox.com>
2026-01-13 12:54:51 -0500

    ROX-32561: Render CompoundSearchFilterLabels in listening endpoints

--- ui/apps/platform/cypress/integration/listeningEndpoints/ListeningEndpoints.helpers.js
+++ ui/apps/platform/cypress/integration/listeningEndpoints/ListeningEndpoints.helpers.js
@@ -1,27 +1,7 @@
-import selectors from './ListeningEndpoints.selectors';
 import { visitFromLeftNavExpandable } from '../../helpers/nav';
 
 export function visitListeningEndpointsFromLeftNav() {
     visitFromLeftNavExpandable('Network', 'Listening Endpoints');
 
     cy.get('h1:contains("Listening endpoints")');
-}
-
-export function selectFilterEntity(entity) {
-    cy.get(selectors.entityDropdownToggle).click();
-    cy.get(`${selectors.entityDropdownToggle} button`)
-        .contains(new RegExp(`^${entity}$`, 'i'))
-        .click();
-}
-
-export function addEntityFilterValue(entity, value) {
-    cy.get(selectors.filterInputBox(entity)).type(value);
-    cy.get(selectors.filterAutocompleteResultItem)
-        .contains(new RegExp(`^${value}$`, 'i'))
-        .click();
-}
-
-export function addEntityFilter(entity, value) {
-    selectFilterEntity(entity);
-    addEntityFilterValue(entity, value);
 }
