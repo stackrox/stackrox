a60f7b6f8cb8f435cbbac02c51f9d4de192091df
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2026-01-15 13:28:16 +0100

    Add facts field to v2.VirtualMachine

--- central/convert/storagetov2/virtual_machine_test.go
+++ central/convert/storagetov2/virtual_machine_test.go
@@ -41,6 +41,9 @@ func TestVirtualMachine(t *testing.T) {
 				Name:        "test-vm",
 				ClusterId:   "cluster-456",
 				ClusterName: "test-cluster",
+				Facts: map[string]string{
+					"Guest OS": "Red Hat Enterprise Linux",
+				},
 				VsockCid:    int32(42),
 				State:       storage.VirtualMachine_RUNNING,
 				LastUpdated: timestamp,
@@ -52,6 +55,9 @@ func TestVirtualMachine(t *testing.T) {
 				Name:        "test-vm",
 				ClusterId:   "cluster-456",
 				ClusterName: "test-cluster",
+				Facts: map[string]string{
+					"Guest OS": "Red Hat Enterprise Linux",
+				},
 				VsockCid:    int32(42),
 				State:       v2.VirtualMachine_RUNNING,
 				LastUpdated: timestamp,
