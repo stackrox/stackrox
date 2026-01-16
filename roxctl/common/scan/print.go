c3b9856ef57f11065c4e5dfe25f9b4d18478859a
David Caravello <119438707+dcaravel@users.noreply.github.com>
2026-01-16 14:34:02 -0600

    remove image reference from generic print

--- roxctl/common/scan/print.go
+++ roxctl/common/scan/print.go
@@ -2,9 +2,9 @@ package scan
 
 import "github.com/stackrox/rox/roxctl/common/logger"
 
-// PrintCVESummary print summary of amount of CVEs found
-func PrintCVESummary(image string, cveSummary map[string]int, out logger.Logger) {
-	out.PrintfLn("Scan results for image: %s", image)
+// PrintCVESummary print summary of amount of CVEs found.
+func PrintCVESummary(name string, cveSummary map[string]int, out logger.Logger) {
+	out.PrintfLn("Scan results for: %s", name)
 	out.PrintfLn("(%s: %d, %s: %d, %s: %d, %s: %d, %s: %d, %s: %d)\n",
 		totalComponentsMapKey, cveSummary[totalComponentsMapKey],
 		totalVulnerabilitiesMapKey, cveSummary[totalVulnerabilitiesMapKey],
@@ -14,7 +14,7 @@ func PrintCVESummary(image string, cveSummary map[string]int, out logger.Logger)
 		CriticalCVESeverity, cveSummary[CriticalCVESeverity.String()])
 }
 
-// PrintCVEWarning print warning with amount of CVEs found in components
+// PrintCVEWarning print warning with amount of CVEs found in components.
 func PrintCVEWarning(numOfVulns int, numOfComponents int, out logger.Logger) {
 	if numOfVulns != 0 {
 		out.WarnfLn("A total of %d unique vulnerabilities were found in %d components",
