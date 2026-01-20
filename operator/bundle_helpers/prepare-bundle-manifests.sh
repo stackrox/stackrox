7994a7180a2e19e8aae31f7aa7b411db96b3876d
Marcin Owsiany <porridge@redhat.com>
2026-01-20 13:00:58 +0100

    chore: include securitypolicies CRD using kustomize, not cp

--- operator/bundle_helpers/prepare-bundle-manifests.sh
+++ operator/bundle_helpers/prepare-bundle-manifests.sh
@@ -9,8 +9,6 @@ mkdir -p build/
 rm -rf build/bundle
 cp -a bundle build/
 
-cp -v ../config-controller/config/crd/bases/config.stackrox.io_securitypolicies.yaml build/bundle/manifests/
-
 "$(dirname "$0")/patch-csv.py" "$@" \
     < bundle/manifests/rhacs-operator.clusterserviceversion.yaml \
     > build/bundle/manifests/rhacs-operator.clusterserviceversion.yaml
