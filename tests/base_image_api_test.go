c4169cf3d1bbaba1f98d6a4a535e1d3578d22cd5
daynewlee <yli3@redhat.com>
2026-01-20 10:27:37 -0600

    added TestCreateBaseImageReference_Success

--- tests/base_image_api_test.go
+++ tests/base_image_api_test.go
@@ -10,6 +10,8 @@ import (
 	v2 "github.com/stackrox/rox/generated/api/v2"
 	"github.com/stackrox/rox/pkg/testutils/centralgrpc"
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func TestBaseImageServicePing(t *testing.T) {
@@ -58,7 +60,6 @@ func TestBaseImageServicePing(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			err := tc.call()
 
-			// If the service wasn't registered, it would likely return Unimplemented.
 			if err != nil {
 				st, ok := status.FromError(err)
 				assert.True(t, ok, "Expected a gRPC status error")
@@ -66,4 +67,46 @@ func TestBaseImageServicePing(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestCreateBaseImageReference_Success(t *testing.T) {
+	if !reach("https://quay.io", 3*time.Second) {
+		t.Skip("Skipping test: quay.io is currently unreachable")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	conn := centralgrpc.GRPCConnectionToCentral(t)
+	client := v2.NewBaseImageServiceV2Client(conn)
+
+	req := &v2.CreateBaseImageReferenceRequest{
+		BaseImageRepoPath:   "quay.io/rh_ee_yli3/alpine",
+		BaseImageTagPattern: ".*",
+	}
+
+	resp, err := client.CreateBaseImageReference(ctx, req)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+	assert.NotNil(t, resp.GetBaseImageReference())
+
+	ref := resp.GetBaseImageReference()
+	assert.Equal(t, req.GetBaseImageRepoPath(), ref.GetBaseImageRepoPath())
+	assert.Equal(t, req.GetBaseImageTagPattern(), ref.GetBaseImageTagPattern())
+	assert.NotEmpty(t, ref.GetId())
+}
+
+func reach(url string, timeout time.Duration) bool {
+	client := http.Client{
+		Timeout: timeout,
+	}
+	// Use HEAD to minimize data transfer
+	resp, err := client.Head(url)
+	if err != nil {
+		return false
+	}
+	defer resp.Body.Close()
+
+	return resp.StatusCode >= 200 && resp.StatusCode < 400
 }
