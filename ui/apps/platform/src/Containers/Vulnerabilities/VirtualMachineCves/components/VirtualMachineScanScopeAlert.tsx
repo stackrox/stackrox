2b4cc1a1e108bf800f62d07d8c4a26b4e7a469b9
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2026-01-23 16:14:33 +0100

    Add "by default" to the banner text.
    Co-authored-by: Guzman <guz@redhat.com>

--- ui/apps/platform/src/Containers/Vulnerabilities/VirtualMachineCves/components/VirtualMachineScanScopeAlert.tsx
+++ ui/apps/platform/src/Containers/Vulnerabilities/VirtualMachineCves/components/VirtualMachineScanScopeAlert.tsx
@@ -6,7 +6,7 @@ function VirtualMachineScanScopeAlert() {
             isInline
             component="p"
             variant="info"
-            title="The results show only DNF packages from Red Hat repositories. System packages preinstalled with the VM image are scanned only after registration (for example, 'subscription-manager register' or 'rhc connect') and at least one DNF transaction (for example, 'dnf install' or 'dnf update'). Scan results refresh every 4 hours."
+            title="The results show only DNF packages from Red Hat repositories. System packages preinstalled with the VM image are scanned only after registration (for example, 'subscription-manager register' or 'rhc connect') and at least one DNF transaction (for example, 'dnf install' or 'dnf update'). Scan results refresh every 4 hours by default."
         />
     );
 }
