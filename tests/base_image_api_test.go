69f35950baa7958bc1495c709da3291689487b4b
daynewlee <yli3@redhat.com>
2026-01-25 21:06:39 -0600

    minor

--- tests/base_image_api_test.go
+++ tests/base_image_api_test.go
@@ -102,12 +102,13 @@ func reach(url string, timeout time.Duration) bool {
 	client := http.Client{
 		Timeout: timeout,
 	}
-	// Use HEAD to minimize data transfer
 	resp, err := client.Head(url)
 	if err != nil {
 		return false
 	}
-	defer resp.Body.Close()
+	defer func() {
+		_ = resp.Body.Close()
+	}()
 
 	return resp.StatusCode >= 200 && resp.StatusCode < 400
 }
