fcf7fdce2ec74feb659d7e1d2c504930c9d873b7
Marcin Owsiany <porridge@redhat.com>
2026-01-29 13:48:41 +0100

    chore(operator): Go implemetation of fix-spec-descriptor-order

---
+++ operator/bundle_helpers/cmd/patch_csv.go
@@ -0,0 +1,7 @@
+package cmd
+
+// PatchCSV patches a ClusterServiceVersion YAML file with version and image information.
+// This is a placeholder implementation - to be implemented in future phases.
+func PatchCSV(args []string) error {
+	panic("not yet implemented")
+}
