f1c166e8493e9c58137d96ad87c4db1bdfc98322
Michaël Petrov <github@parameta.lol>
2026-02-02 10:23:47 +0100

    fix: set not enabled if no metrics

--- central/metrics/custom/tracker/configuration.go
+++ central/metrics/custom/tracker/configuration.go
@@ -43,5 +43,5 @@ type Configuration struct {
 }
 
 func (cfg *Configuration) isEnabled() bool {
-	return cfg != nil && len(cfg.metrics) >= 0 && cfg.period > 0
+	return cfg != nil && len(cfg.metrics) > 0 && cfg.period > 0
 }
