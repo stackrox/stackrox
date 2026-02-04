08e297cc2a74abe8f195005a7e5d81422a1004c6
David Vail <dcvail17@gmail.com>
2026-02-04 08:51:32 -0500

    ROX-31779: [part 2] Fix layout on VM->Nodes pages (#18812)

--- ui/apps/platform/src/Containers/Vulnerabilities/NodeCves/Node/NodePageDetails.tsx
+++ ui/apps/platform/src/Containers/Vulnerabilities/NodeCves/Node/NodePageDetails.tsx
@@ -1,12 +1,11 @@
 import {
     Bullseye,
-    Card,
-    CardBody,
     Content,
     DescriptionList,
     DescriptionListDescription,
     DescriptionListGroup,
     DescriptionListTerm,
+    Divider,
     Flex,
     PageSection,
     Spinner,
@@ -28,108 +27,88 @@ function NodePageDetails({ nodeId }: NodePageDetailsProps) {
 
     return (
         <>
-            <PageSection
-                hasBodyWrapper={false}
-                component="div"
-                className="pf-v6-u-py-md pf-v6-u-px-xl"
-            >
+            <PageSection component="div">
                 <Content component="p">View details about this node</Content>
             </PageSection>
-            <PageSection
-                hasBodyWrapper={false}
-                isFilled
-                className="pf-v6-u-display-flex pf-v6-u-flex-direction-column"
-            >
-                <Card>
-                    <CardBody>
-                        {error ? (
-                            <Bullseye>
-                                <EmptyStateTemplate
-                                    title="There was an error loading the node details"
-                                    headingLevel="h2"
-                                    icon={ExclamationCircleIcon}
-                                    status="danger"
-                                >
-                                    {getAxiosErrorMessage(error)}
-                                </EmptyStateTemplate>
-                            </Bullseye>
-                        ) : loading ? (
-                            <Bullseye>
-                                <Spinner size="xl" />
-                            </Bullseye>
-                        ) : (
-                            data && (
-                                <Flex
-                                    direction={{ default: 'column' }}
-                                    spaceItems={{ default: 'spaceItemsXl' }}
-                                >
-                                    <DescriptionList
-                                        columnModifier={{ default: '1Col', lg: '2Col' }}
-                                    >
-                                        <DescriptionListGroup>
-                                            <DescriptionListTerm>Cluster</DescriptionListTerm>
-                                            <DescriptionListDescription>
-                                                {data.node.cluster.name}
-                                            </DescriptionListDescription>
-                                        </DescriptionListGroup>
-                                        {data.node.containerRuntimeVersion && (
-                                            <DescriptionListGroup>
-                                                <DescriptionListTerm>
-                                                    Container runtime
-                                                </DescriptionListTerm>
-                                                <DescriptionListDescription>
-                                                    {data.node.containerRuntimeVersion}
-                                                </DescriptionListDescription>
-                                            </DescriptionListGroup>
-                                        )}
-                                        {data.node.joinedAt && (
-                                            <DescriptionListGroup>
-                                                <DescriptionListTerm>Join time</DescriptionListTerm>
-                                                <DescriptionListDescription>
-                                                    {getDateTime(data.node.joinedAt)}
-                                                </DescriptionListDescription>
-                                            </DescriptionListGroup>
-                                        )}
-                                        {data.node.scanTime && (
-                                            <DescriptionListGroup>
-                                                <DescriptionListTerm>Scan time</DescriptionListTerm>
-                                                <DescriptionListDescription>
-                                                    {getDateTime(data.node.scanTime)}
-                                                </DescriptionListDescription>
-                                            </DescriptionListGroup>
-                                        )}
-                                        {data.node.kernelVersion && (
-                                            <DescriptionListGroup>
-                                                <DescriptionListTerm>
-                                                    Kernel version
-                                                </DescriptionListTerm>
-                                                <DescriptionListDescription>
-                                                    {data.node.kernelVersion}
-                                                </DescriptionListDescription>
-                                            </DescriptionListGroup>
-                                        )}
-                                        {data.node.kubeletVersion && (
-                                            <DescriptionListGroup>
-                                                <DescriptionListTerm>Kubelet</DescriptionListTerm>
-                                                <DescriptionListDescription>
-                                                    {data.node.kubeletVersion}
-                                                </DescriptionListDescription>
-                                            </DescriptionListGroup>
-                                        )}
-                                    </DescriptionList>
-                                    <ExpandableLabelSection
-                                        toggleText="Labels"
-                                        labels={data.node.labels}
-                                    />
-                                    <ExpandableLabelSection
-                                        toggleText="Annotations"
-                                        labels={data.node.annotations}
-                                    />
-                                </Flex>
-                            )
-                        )}
-                    </CardBody>
-                </Card>
+            <Divider component="div" />
+            <PageSection isFilled>
+                {error ? (
+                    <Bullseye>
+                        <EmptyStateTemplate
+                            title="There was an error loading the node details"
+                            headingLevel="h2"
+                            icon={ExclamationCircleIcon}
+                            status="danger"
+                        >
+                            {getAxiosErrorMessage(error)}
+                        </EmptyStateTemplate>
+                    </Bullseye>
+                ) : loading ? (
+                    <Bullseye>
+                        <Spinner size="xl" />
+                    </Bullseye>
+                ) : (
+                    data && (
+                        <Flex
+                            direction={{ default: 'column' }}
+                            spaceItems={{ default: 'spaceItemsXl' }}
+                        >
+                            <DescriptionList columnModifier={{ default: '1Col', lg: '2Col' }}>
+                                <DescriptionListGroup>
+                                    <DescriptionListTerm>Cluster</DescriptionListTerm>
+                                    <DescriptionListDescription>
+                                        {data.node.cluster.name}
+                                    </DescriptionListDescription>
+                                </DescriptionListGroup>
+                                {data.node.containerRuntimeVersion && (
+                                    <DescriptionListGroup>
+                                        <DescriptionListTerm>Container runtime</DescriptionListTerm>
+                                        <DescriptionListDescription>
+                                            {data.node.containerRuntimeVersion}
+                                        </DescriptionListDescription>
+                                    </DescriptionListGroup>
+                                )}
+                                {data.node.joinedAt && (
+                                    <DescriptionListGroup>
+                                        <DescriptionListTerm>Join time</DescriptionListTerm>
+                                        <DescriptionListDescription>
+                                            {getDateTime(data.node.joinedAt)}
+                                        </DescriptionListDescription>
+                                    </DescriptionListGroup>
+                                )}
+                                {data.node.scanTime && (
+                                    <DescriptionListGroup>
+                                        <DescriptionListTerm>Scan time</DescriptionListTerm>
+                                        <DescriptionListDescription>
+                                            {getDateTime(data.node.scanTime)}
+                                        </DescriptionListDescription>
+                                    </DescriptionListGroup>
+                                )}
+                                {data.node.kernelVersion && (
+                                    <DescriptionListGroup>
+                                        <DescriptionListTerm>Kernel version</DescriptionListTerm>
+                                        <DescriptionListDescription>
+                                            {data.node.kernelVersion}
+                                        </DescriptionListDescription>
+                                    </DescriptionListGroup>
+                                )}
+                                {data.node.kubeletVersion && (
+                                    <DescriptionListGroup>
+                                        <DescriptionListTerm>Kubelet</DescriptionListTerm>
+                                        <DescriptionListDescription>
+                                            {data.node.kubeletVersion}
+                                        </DescriptionListDescription>
+                                    </DescriptionListGroup>
+                                )}
+                            </DescriptionList>
+                            <ExpandableLabelSection toggleText="Labels" labels={data.node.labels} />
+                            <ExpandableLabelSection
+                                toggleText="Annotations"
+                                labels={data.node.annotations}
+                            />
+                        </Flex>
+                    )
+                )}
             </PageSection>
         </>
     );
