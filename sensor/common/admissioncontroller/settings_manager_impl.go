2b9c882a5e2fd01df7c8c38ae4e3643adcf03b54
Giles Hutton <ghutton@redhat.com>
2026-02-18 16:34:00 +0000

    ROX-33022: configure fact on policy sync
    These changes allow for automatic configuration of Fact based on
    policies defined by the user. It constructs a set of path prefixes and
    writes them to a Fact ConfigMap, which is hot-reloaded by the Fact
    agent.
    
    This also includes a fairly significant refactor of the admission
    controller settings / config map persistence, so that there is a generic
    persistence component that can be initialized for specific settings
    (admission controller, and now Fact)

--- sensor/common/admissioncontroller/settings_manager_impl.go
+++ sensor/common/admissioncontroller/settings_manager_impl.go
@@ -1,6 +1,10 @@
 package admissioncontroller
 
 import (
+	"compress/gzip"
+	"time"
+
+	"github.com/pkg/errors"
 	"github.com/stackrox/rox/generated/internalapi/central"
 	"github.com/stackrox/rox/generated/internalapi/sensor"
 	"github.com/stackrox/rox/generated/storage"
@@ -9,18 +13,23 @@ import (
 	"github.com/stackrox/rox/pkg/centralsensor"
 	"github.com/stackrox/rox/pkg/concurrency"
 	"github.com/stackrox/rox/pkg/env"
+	"github.com/stackrox/rox/pkg/gziputil"
 	pkgPolicies "github.com/stackrox/rox/pkg/policies"
 	"github.com/stackrox/rox/pkg/protocompat"
 	"github.com/stackrox/rox/pkg/sync"
 	"github.com/stackrox/rox/pkg/uuid"
 	"github.com/stackrox/rox/sensor/common/centralcaps"
+	"github.com/stackrox/rox/sensor/common/configmap"
 	"github.com/stackrox/rox/sensor/common/store"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type settingsManager struct {
 	mutex                         sync.Mutex
 	currSettings                  *sensor.AdmissionControlSettings
 	settingsStream                *concurrency.ValueStream[*sensor.AdmissionControlSettings]
+	configStream                  *concurrency.ValueStream[*v1.ConfigMap]
 	sensorEventsStream            *concurrency.ValueStream[*sensor.AdmCtrlUpdateResourceRequest]
 	hasClusterConfig, hasPolicies bool
 	centralEndpoint               string
@@ -38,6 +47,7 @@ type clusterIDWaiter interface {
 // NewSettingsManager creates a new settings manager for admission control settings.
 func NewSettingsManager(clusterID clusterIDWaiter, deployments store.DeploymentStore, pods store.PodStore) SettingsManager {
 	return &settingsManager{
+		configStream:       concurrency.NewValueStream[*v1.ConfigMap](nil),
 		settingsStream:     concurrency.NewValueStream[*sensor.AdmissionControlSettings](nil),
 		sensorEventsStream: concurrency.NewValueStream[*sensor.AdmCtrlUpdateResourceRequest](nil),
 		centralEndpoint:    env.CentralEndpoint.Setting(),
@@ -85,7 +95,7 @@ func (p *settingsManager) UpdatePolicies(policies []*storage.Policy) {
 	newSettings.RuntimePolicies = &storage.PolicyList{Policies: runtimePolicies}
 
 	if p.hasClusterConfig && p.hasPolicies {
-		p.settingsStream.Push(newSettings)
+		p.pushSettings(newSettings)
 	}
 
 	p.currSettings = newSettings
@@ -103,7 +113,7 @@ func (p *settingsManager) UpdateConfig(config *storage.DynamicClusterConfig) {
 	newSettings.ClusterConfig = clonedConfig
 
 	if p.hasClusterConfig && p.hasPolicies {
-		p.settingsStream.Push(newSettings)
+		p.pushSettings(newSettings)
 	}
 	p.currSettings = newSettings
 }
@@ -116,13 +126,26 @@ func (p *settingsManager) FlushCache() {
 	newSettings.CacheVersion = uuid.NewV4().String()
 
 	if p.hasClusterConfig && p.hasPolicies {
-		p.settingsStream.Push(newSettings)
+		p.pushSettings(newSettings)
 	}
 	p.currSettings = newSettings
 }
 
+func (p *settingsManager) pushSettings(newSettings *sensor.AdmissionControlSettings) {
+	p.settingsStream.Push(newSettings)
+	if config, err := p.settingsToConfigMap(newSettings); err != nil {
+		log.Errorf("failed to create config map: %v", err)
+	} else {
+		p.configStream.Push(config)
+	}
+}
+
 func (p *settingsManager) SettingsStream() concurrency.ReadOnlyValueStream[*sensor.AdmissionControlSettings] {
 	return p.settingsStream
+}
+
+func (p *settingsManager) ConfigMapStream() concurrency.ReadOnlyValueStream[*v1.ConfigMap] {
+	return p.configStream
 }
 
 func (p *settingsManager) SensorEventsStream() concurrency.ReadOnlyValueStream[*sensor.AdmCtrlUpdateResourceRequest] {
@@ -172,4 +195,62 @@ func (p *settingsManager) convertAndPush(event *central.SensorEvent) {
 	}
 
 	p.sensorEventsStream.Push(converted)
+}
+
+func (p *settingsManager) settingsToConfigMap(settings *sensor.AdmissionControlSettings) (*v1.ConfigMap, error) {
+	clusterConfig := settings.GetClusterConfig()
+	enforcedDeployTimePolicies := settings.GetEnforcedDeployTimePolicies()
+	runtimePolicies := settings.GetRuntimePolicies()
+	if settings == nil || clusterConfig == nil || enforcedDeployTimePolicies == nil || runtimePolicies == nil {
+		return nil, nil
+	}
+
+	configBytes, err := clusterConfig.MarshalVT()
+	if err != nil {
+		return nil, errors.Wrap(err, "marshaling cluster config")
+	}
+	configBytesGZ, err := gziputil.Compress(configBytes, gzip.BestCompression)
+	if err != nil {
+		return nil, errors.Wrap(err, "compressing cluster config")
+	}
+
+	deployTimePoliciesBytes, err := enforcedDeployTimePolicies.MarshalVT()
+	if err != nil {
+		return nil, errors.Wrap(err, "encoding deploy-time policies")
+	}
+	deployTimePoliciesBytesGZ, err := gziputil.Compress(deployTimePoliciesBytes, gzip.BestCompression)
+	if err != nil {
+		return nil, errors.Wrap(err, "compressing deploy-time policies")
+	}
+
+	runTimePoliciesBytes, err := runtimePolicies.MarshalVT()
+	if err != nil {
+		return nil, errors.Wrap(err, "encoding run-time policies")
+	}
+	runTimePoliciesBytesGZ, err := gziputil.Compress(runTimePoliciesBytes, gzip.BestCompression)
+	if err != nil {
+		return nil, errors.Wrap(err, "compressing run-time policies")
+	}
+
+	return &v1.ConfigMap{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "ConfigMap",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        admissioncontrol.ConfigMapName,
+			Annotations: configmap.InfoAnnotations("admission-control"),
+		},
+		Data: map[string]string{
+			admissioncontrol.LastUpdateTimeDataKey:  settings.GetTimestamp().AsTime().Format(time.RFC3339Nano),
+			admissioncontrol.CacheVersionDataKey:    settings.GetCacheVersion(),
+			admissioncontrol.CentralEndpointDataKey: settings.GetCentralEndpoint(),
+			admissioncontrol.ClusterIDDataKey:       settings.GetClusterId(),
+		},
+		BinaryData: map[string][]byte{
+			admissioncontrol.ConfigGZDataKey:             configBytesGZ,
+			admissioncontrol.DeployTimePoliciesGZDataKey: deployTimePoliciesBytesGZ,
+			admissioncontrol.RunTimePoliciesGZDataKey:    runTimePoliciesBytesGZ,
+		},
+	}, nil
 }
---
+++ sensor/common/configmap/persister.go
@@ -0,0 +1,127 @@
+package configmap
+
+import (
+	"fmt"
+
+	"context"
+
+	"github.com/pkg/errors"
+	"github.com/stackrox/rox/pkg/centralsensor"
+	"github.com/stackrox/rox/pkg/concurrency"
+	"github.com/stackrox/rox/pkg/logging"
+	"github.com/stackrox/rox/sensor/common"
+	"github.com/stackrox/rox/sensor/common/message"
+	"github.com/stackrox/rox/sensor/common/unimplemented"
+	v1 "k8s.io/api/core/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	v1client "k8s.io/client-go/kubernetes/typed/core/v1"
+)
+
+const (
+	annotationInfoKey     = `stackrox.io/info`
+	annotationInfoTextFmt = `ConfigMap for %s service. Automatically generated - do not modify. Your changes will be overwritten.`
+)
+
+var (
+	log = logging.LoggerForModule()
+)
+
+func InfoAnnotations(serviceName string) map[string]string {
+	return map[string]string{
+		annotationInfoKey: fmt.Sprintf(annotationInfoTextFmt, serviceName),
+	}
+}
+
+type configMapPersister struct {
+	unimplemented.Receiver
+
+	name string
+
+	stopSig concurrency.ErrorSignal
+
+	client v1client.ConfigMapInterface
+
+	settingsStreamIt concurrency.ValueStreamIter[*v1.ConfigMap]
+}
+
+func NewConfigMapPersister(name, namespace string, k8s kubernetes.Interface, settings concurrency.ValueStreamIter[*v1.ConfigMap]) common.SensorComponent {
+	return &configMapPersister{
+		name:             name,
+		client:           k8s.CoreV1().ConfigMaps(namespace),
+		settingsStreamIt: settings,
+	}
+}
+
+func (p *configMapPersister) Start() error {
+	if !p.stopSig.Reset() {
+		return errors.New("config persister was already started")
+	}
+
+	go p.run()
+	return nil
+}
+
+func (p *configMapPersister) Name() string {
+	return fmt.Sprintf("%s.configMapPersister", p.name)
+}
+
+func (p *configMapPersister) Stop() {
+	p.stopSig.Signal()
+}
+
+func (p *configMapPersister) Notify(common.SensorComponentEvent) {}
+
+func (p *configMapPersister) Capabilities() []centralsensor.SensorCapability {
+	return nil
+}
+
+func (p *configMapPersister) ResponsesC() <-chan *message.ExpiringMessage {
+	return nil
+}
+
+func (p *configMapPersister) ctx() context.Context {
+	return concurrency.AsContext(&p.stopSig)
+}
+
+func (p *configMapPersister) run() {
+	// Attempt to apply the initial config, if any.
+	if err := p.applyCurrentConfigMap(p.ctx()); err != nil {
+		log.Errorf("Could not apply admission controller config map: %v", err)
+	}
+
+	for !p.stopSig.IsDone() {
+		select {
+		case <-p.stopSig.Done():
+			return
+
+		case <-p.settingsStreamIt.Done():
+			p.settingsStreamIt = p.settingsStreamIt.TryNext()
+
+			if err := p.applyCurrentConfigMap(p.ctx()); err != nil {
+				log.Errorf("Could not apply admission controller config map: %v", err)
+			}
+		}
+	}
+}
+
+func (p *configMapPersister) applyCurrentConfigMap(ctx context.Context) error {
+	configMap := p.settingsStreamIt.Value()
+	if configMap == nil {
+		return nil
+	}
+
+	_, err := p.client.Create(ctx, configMap, metav1.CreateOptions{})
+	if err != nil {
+		if !k8serrors.IsAlreadyExists(err) {
+			return errors.Wrap(err, "telling Kubernetes to create config map")
+		}
+
+		if _, err := p.client.Update(ctx, configMap, metav1.UpdateOptions{}); err != nil {
+			return errors.Wrap(err, "telling Kubernetes to update existing config map")
+		}
+	}
+
+	return nil
+}
