862f8056348a7fca79ad1dd197a868df09437e55
Guzman <guz@redhat.com>
2026-02-25 13:24:10 +0100

    Add new lines btween paragraphs and use list for cap figures

--- compliance/virtualmachines/roxagent/cmd/cmd.go
+++ compliance/virtualmachines/roxagent/cmd/cmd.go
@@ -31,9 +31,12 @@ func RootCmd(ctx context.Context) *cobra.Command {
 	// Shortening this interval results in more frequent scans and therefore more load,
 	// which, assuming the throughput continues to be limited by scanning capacity,
 	// reduces the number of VMs that Stackrox can handle.
-	// As of February 2026, one report every 4 h results in the default Stackrox deployment
-	// being able to handle around 4500 VMs, while one report every hour results in a
-	// capacity of around 1100 VMs. See the documentation for more details.
+	//
+	// As of February 2026, the measured capacity of a default Stackrox deployment is:
+	//   - 4500 VMs if the report interval is 4 hours
+	//   - 1100 VMs if the report interval is 1 hour
+	//
+	// See the documentation for more details.
 	cmd.Flags().DurationVar(&cfg.IndexInterval, "index-interval", 240*time.Minute,
 		fmt.Sprintf(
 			"Interval at which index reports are sent in daemon mode (minimum: %v). "+
