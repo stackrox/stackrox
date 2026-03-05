a6bc06b3144f1b8fedd3648e6d00fc60887f4fe4
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2026-03-05 13:36:22 +0100

    Fix the bug where deployments may accumulate in ipMap

--- sensor/common/clusterentities/store_ips.go
+++ sensor/common/clusterentities/store_ips.go
@@ -183,10 +183,9 @@ func (e *podIPsStore) deleteDeploymentFromCurrent(deploymentID string) {
 	ips := e.reverseIPMap[deploymentID]
 	for _, address := range ips.AsSlice() {
 		deploymentsHavingIP := e.ipMap[address]
-		if deploymentsHavingIP.Cardinality() < 2 {
+		deploymentsHavingIP.Remove(deploymentID)
+		if deploymentsHavingIP.Cardinality() == 0 {
 			delete(e.ipMap, address)
-		} else {
-			log.Debugf("The same pod IP %s belongs to 2 or more deployments:%v !", address, deploymentsHavingIP.AsSlice())
 		}
 	}
 	delete(e.reverseIPMap, deploymentID)
