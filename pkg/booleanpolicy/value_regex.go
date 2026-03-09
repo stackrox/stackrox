c1e6ecb75d7976cae60390a1d80291f839f65b37
Giles Hutton <ghutton@redhat.com>
2026-03-09 15:00:29 +0000

    ROX-33001: reinstates RENAME file operation (#18945)

--- pkg/booleanpolicy/value_regex.go
+++ pkg/booleanpolicy/value_regex.go
@@ -35,7 +35,7 @@ var (
 	kubernetesNameRegex                      = createRegex(`(?i:[a-z0-9])(?i:[-:a-z0-9]*[a-z0-9])?`)
 	ipAddressValueRegex                      = createRegex(fmt.Sprintf(`(%s)|(%s)`, ipv4Regex, ipv6Regex))
 	signatureIntegrationIDValueRegex         = createRegex(regexp.QuoteMeta(signatures.SignatureIntegrationIDPrefix) + "[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}")
-	fileOperationRegex                       = createRegex(`(?i:OPEN|CREATE|UNLINK|OWNERSHIP_CHANGE|PERMISSION_CHANGE)`)
+	fileOperationRegex                       = createRegex(`(?i:OPEN|CREATE|RENAME|UNLINK|OWNERSHIP_CHANGE|PERMISSION_CHANGE)`)
 )
 
 func createRegex(s string) *regexp.Regexp {
