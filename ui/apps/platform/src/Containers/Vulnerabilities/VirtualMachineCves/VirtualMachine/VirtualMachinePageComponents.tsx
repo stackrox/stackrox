ebeacf14d6b955287f908001cab002f603ba19d8
Mark Pedrotti <pedrottimark@gmail.com>
2026-03-10 11:45:26 -0400

    ROX-33419: Replace frontend SCANNABLE with Scan Status search filter (#19277)

--- ui/apps/platform/src/Containers/Vulnerabilities/VirtualMachineCves/VirtualMachine/VirtualMachinePageComponents.tsx
+++ ui/apps/platform/src/Containers/Vulnerabilities/VirtualMachineCves/VirtualMachine/VirtualMachinePageComponents.tsx
@@ -28,17 +28,16 @@ import {
     getVirtualMachineComponentsTableData,
 } from '../aggregateUtils';
 import { virtualMachineComponentSearchFilterConfig } from '../../searchFilterConfig';
-import { scannableStatuses } from '../../types';
+import { scanStatuses } from '../../types';
 import VirtualMachineComponentsPageTable from './VirtualMachineComponentsPageTable';
 
-export const attributeForScannable: SelectSearchFilterAttribute = {
+export const attributeForScanStatus: SelectSearchFilterAttribute = {
     displayName: 'Scan status',
     filterChipLabel: 'Scan status',
-    searchTerm: 'SCANNABLE', // TODO can it become 'Scannable' instead of ALL CAPS before GA?
+    searchTerm: 'Scan Status',
     inputType: 'select',
     inputProps: {
-        // TODO can value become true and file instead of Scanned and Not scanned before GA?
-        options: scannableStatuses.map((label) => ({ label, value: label })),
+        options: scanStatuses.map((label) => ({ label, value: label })),
     },
 };
 
@@ -116,7 +115,7 @@ function VirtualMachinePageComponents({
         setPage(1);
     };
 
-    const onSearchScannable: OnSearchCallback = (payload) => {
+    const onSearchScanStatus: OnSearchCallback = (payload) => {
         setSearchFilter(updateSearchFilter(searchFilter, payload));
         setPage(1);
     };
@@ -135,16 +134,16 @@ function VirtualMachinePageComponents({
                         </ToolbarItem>
                         <ToolbarItem>
                             <SearchFilterSelectInclusive
-                                attribute={attributeForScannable}
+                                attribute={attributeForScanStatus}
                                 isSeparate
-                                onSearch={onSearchScannable}
+                                onSearch={onSearchScanStatus}
                                 searchFilter={searchFilter}
                             />
                         </ToolbarItem>
                     </ToolbarGroup>
                     <ToolbarGroup className="pf-v5-u-w-100">
                         <CompoundSearchFilterLabels
-                            attributesSeparateFromConfig={[attributeForScannable]}
+                            attributesSeparateFromConfig={[attributeForScanStatus]}
                             config={searchFilterConfig}
                             searchFilter={searchFilter}
                             onFilterChange={setSearchFilter}
