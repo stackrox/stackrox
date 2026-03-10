707ca7f82a302193b714b37ee5f0a777354300a4
Dave Shrewsberry <dashrews@redhat.com>
2026-03-10 11:06:03 -0400

    removing the gen vm store as we will custom one later

--- central/virtualmachine/v2/datastore/store/postgres/gen.go
+++ central/virtualmachine/v2/datastore/store/postgres/gen.go
@@ -1,3 +1,3 @@
 package postgres
 
-//go:generate pg-table-bindings-wrapper --type=storage.VirtualMachineV2 --search-category VIRTUAL_MACHINES_V2 --no-copy-from --search-scope VIRTUAL_MACHINE_VULNERABILITIES_V2,VIRTUAL_MACHINE_COMPONENTS_V2,VIRTUAL_MACHINE_SCANS_V2,VIRTUAL_MACHINES_V2,NAMESPACES,CLUSTERS
+//go:generate pg-table-bindings-wrapper --type=storage.VirtualMachineV2 --search-category VIRTUAL_MACHINES_V2 --schema-only --search-scope VIRTUAL_MACHINE_VULNERABILITIES_V2,VIRTUAL_MACHINE_COMPONENTS_V2,VIRTUAL_MACHINE_SCANS_V2,VIRTUAL_MACHINES_V2,NAMESPACES,CLUSTERS
