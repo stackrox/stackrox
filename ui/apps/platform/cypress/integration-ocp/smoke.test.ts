119a39834fdcddc3bc5df6a354496dcb8fa41a50
David Vail <dcvail17@gmail.com>
2026-03-19 06:25:18 -0700

    ROX-33427: Add correct version to OCP plugin metadata (#19486)

--- ui/apps/platform/cypress/integration-ocp/smoke.test.ts
+++ ui/apps/platform/cypress/integration-ocp/smoke.test.ts
@@ -1,11 +1,44 @@
+import { visitFromConsoleLeftNavExpandable } from '../helpers/nav';
 import { withOcpAuth } from '../helpers/ocpAuth';
 
-describe('Basic connectivity to the OCP plugin', () => {
+describe('Basic tests of the OCP plugin', () => {
     it('should open the OCP web console', () => {
         withOcpAuth();
 
         cy.visit('/');
 
         cy.get('h1:contains("Overview")');
+    });
+
+    describe('Plugin version', () => {
+        beforeEach(() => {
+            withOcpAuth();
+        });
+
+        it('should display correct plugin version in the plugin manifest', () => {
+            // The plugin manifest is served at the basePath configured in the ConsolePlugin resource
+            // which is /api/plugins/<plugin-name>/plugin-manifest.json
+            cy.request('/api/plugins/advanced-cluster-security/plugin-manifest.json').then(
+                (response) => {
+                    expect(response.status).to.equal(200);
+                    expect(response.body).to.have.property('name', 'advanced-cluster-security');
+                    expect(response.body).to.have.property('version');
+
+                    expect(response.body.version).to.match(/^[1-9]\d*\.\d+\.\d+/);
+                }
+            );
+        });
+
+        it('should display plugin information in cluster settings', () => {
+            visitFromConsoleLeftNavExpandable('Administration', 'Dynamic Plugins');
+
+            cy.get('td[data-label="name"]:contains("advanced-cluster-security")')
+                .parent()
+                .within(() => {
+                    cy.get('td[data-label="version"]')
+                        .invoke('text')
+                        .should('match', /^[1-9]\d*\.\d+\.\d+/);
+                });
+        });
     });
 });
