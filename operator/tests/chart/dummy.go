1d466df32367d9212cce6110f7f8d1bb5fcb2598
Marcin Owsiany <porridge@redhat.com>
2026-03-20 18:22:48 +0100

    Trim trailing whitespace

--- operator/tests/chart/dummy.go
+++ operator/tests/chart/dummy.go
@@ -1,5 +1,5 @@
 // Presence of this file is a workaround for the following error
-// when running unit tests: 
+// when running unit tests:
 // Run GOTAGS=release make go-unit-tests
 // mkdir -p bin/{darwin_amd64,darwin_arm64,linux_amd64,linux_arm64,linux_ppc64le,linux_s390x,windows_amd64}
 // + test-prep
