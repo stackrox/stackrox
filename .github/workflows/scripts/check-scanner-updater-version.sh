dde014e87ddda977873888a355c6ee50b5bb0d57
Surabhi Kumar <surakuma@redhat.com>
2026-03-22 19:44:01 -0700

    Fixes

--- .github/workflows/scripts/check-scanner-updater-version.sh
+++ .github/workflows/scripts/check-scanner-updater-version.sh
@@ -3,7 +3,7 @@
 # scanner/updater/version/RELEASE_VERSION on the master branch.
 #
 # Usage (locally):
-#   bash local-env.sh check-scanner-version <version>
+# bash local-env.sh check-scanner-updater-version <version>
 
 set -euo pipefail
 
@@ -18,7 +18,7 @@ SCANNER_VERSION=$(gh api -H "Accept: application/vnd.github.v3.raw+json" \
   "/repos/${GITHUB_REPOSITORY}/contents/$SCANNER_VERSION_PATH?ref=master")
 
 if ! grep -Fqx "$VERSION" <<<"$SCANNER_VERSION"; then
-    gh_log error "Release version $VERSION (inferred from the tag '$TAG') not added to $SCANNER_VERSION_PATH in master branch"
+    gh_log error "Release version $VERSION (inferred from the tag '$TAG') is not present in $SCANNER_VERSION_PATH in master branch"
     gh_summary "Release version not found in $SCANNER_VERSION_PATH in master branch"
     gh_summary "Most likely, this is because the PR to update scanner version file created by \`start-release\` workflow is not merged"
     gh_summary "➡️  Please check the PR created by \`start-release\` workflow that started this release."
