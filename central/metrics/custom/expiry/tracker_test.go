0ee53732c06f0a87ba7a6cf16e465455e2640a1e
Michaël Petrov <github@parameta.lol>
2026-03-24 18:24:32 +0100

    minor test refactoring

--- central/metrics/custom/expiry/tracker_test.go
+++ central/metrics/custom/expiry/tracker_test.go
@@ -10,7 +10,6 @@ import (
 	v1 "github.com/stackrox/rox/generated/api/v1"
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc"
-	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type mockService struct {
@@ -24,14 +23,10 @@ func (ms *mockService) AuthFuncOverride(context.Context, string) (context.Contex
 }
 
 func (ms *mockService) GetCertExpiry(_ context.Context, req *v1.GetCertExpiry_Request) (*v1.GetCertExpiry_Response, error) {
-	if req.GetComponent() == 2 {
+	if req.GetComponent() == v1.GetCertExpiry_SCANNER_V4 {
 		return nil, errTest
 	}
-	return &v1.GetCertExpiry_Response{
-		Expiry: &timestamppb.Timestamp{
-			Seconds: int64(req.GetComponent()) * 60 * 60,
-		},
-	}, nil
+	return nil, nil
 }
 
 func (ms *mockService) RegisterServiceHandler(context.Context, *runtime.ServeMux, *grpc.ClientConn) error {
@@ -51,8 +46,6 @@ func Test_track(t *testing.T) {
 	for f := range track(context.Background(), &s) {
 		components = append(components, f.component)
 	}
-	assert.Equal(t, cap(components), len(components)+2,
-		"should ignore UNKNOWN and SCANNER")
-	assert.ElementsMatch(t, []string{"SCANNER_V4", "CENTRAL_DB", "CENTRAL"}, components,
-		"should have no UNKNOWN and SCANNER")
+	assert.ElementsMatch(t, []string{"SCANNER", "CENTRAL_DB", "CENTRAL"}, components,
+		"should have no UNKNOWN and SCANNER_V4")
 }
