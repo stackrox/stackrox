f784f4a1fcb21f9fe77ce7e9875b82a5f19597b6
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2026-03-24 15:27:33 +0100

    ROX-33757: Fix moving to history in clusterentities store (#19565)
    (cherry picked from commit a6eb641506bb2049b898fc1cacbede62cafd8b85)

--- sensor/common/clusterentities/store_endpoints.go
+++ sensor/common/clusterentities/store_endpoints.go
@@ -121,11 +121,7 @@ func (e *endpointsStore) purgeNoLock(deploymentID string) {
 	// so let's make a temporary copy.
 	endpointsSet := e.reverseEndpointMap[deploymentID]
 	for ep := range endpointsSet {
-		if e.historyEnabled() {
-			e.moveToHistory(deploymentID, ep)
-		} else {
-			e.deleteFromCurrent(deploymentID, ep)
-		}
+		e.moveToHistory(deploymentID, ep)
 	}
 }
 
@@ -216,9 +212,12 @@ func (e *endpointsStore) removeFromHistoryIfExpired(deploymentID string, ep net.
 	return false
 }
 
-// moveToHistory is a convenience function that removes data from the current map and adds it to history
+// moveToHistory is a convenience function that removes data from the current map and adds it to history.
+// If history is disabled, it just deletes the data from the current map.
 func (e *endpointsStore) moveToHistory(deploymentID string, ep net.NumericEndpoint) {
-	e.addToHistory(deploymentID, ep)
+	if e.historyEnabled() {
+		e.addToHistory(deploymentID, ep)
+	}
 	e.deleteFromCurrent(deploymentID, ep)
 }
 
