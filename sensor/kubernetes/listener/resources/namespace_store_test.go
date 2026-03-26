312b5ab2c7e80fbe7fb56cac749fc4310859b9e8
Alex Vulaj <avulaj@redhat.com>
2026-03-26 14:22:43 -0400

    Move GetAll to PR3: Remove from sensor label providers

--- sensor/kubernetes/listener/resources/namespace_store_test.go
+++ sensor/kubernetes/listener/resources/namespace_store_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 
 	"github.com/stackrox/rox/generated/storage"
-	"github.com/stackrox/rox/pkg/protoassert"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -61,69 +60,4 @@ func TestNamespaceStore_GetNamespaceLabels(t *testing.T) {
 	labels, err = store.GetNamespaceLabels(ctx, "cluster-id", "namespace-2")
 	require.NoError(t, err)
 	assert.Equal(t, map[string]string{"app": "backend", "env": "staging"}, labels)
-}
-
-func TestNamespaceStore_GetAll(t *testing.T) {
-	store := newNamespaceStore()
-
-	// Initially empty
-	namespaces := store.GetAll()
-	assert.Empty(t, namespaces)
-
-	// Add some namespaces
-	ns1 := &storage.NamespaceMetadata{
-		Id:   "ns-123",
-		Name: "namespace-1",
-		Labels: map[string]string{
-			"app": "frontend",
-		},
-	}
-
-	ns2 := &storage.NamespaceMetadata{
-		Id:   "ns-456",
-		Name: "namespace-2",
-		Labels: map[string]string{
-			"app": "backend",
-		},
-	}
-
-	ns3 := &storage.NamespaceMetadata{
-		Id:   "ns-789",
-		Name: "namespace-3",
-		Labels: map[string]string{
-			"app": "database",
-		},
-	}
-
-	store.addNamespace(ns1)
-	store.addNamespace(ns2)
-	store.addNamespace(ns3)
-
-	// GetAll should return all three
-	namespaces = store.GetAll()
-	assert.Len(t, namespaces, 3)
-
-	// Verify all namespaces are present (order not guaranteed)
-	namespaceMap := make(map[string]*storage.NamespaceMetadata)
-	for _, ns := range namespaces {
-		namespaceMap[ns.GetName()] = ns
-	}
-
-	protoassert.Equal(t, ns1, namespaceMap["namespace-1"])
-	protoassert.Equal(t, ns2, namespaceMap["namespace-2"])
-	protoassert.Equal(t, ns3, namespaceMap["namespace-3"])
-
-	// Remove one and verify GetAll reflects the change
-	store.removeNamespace(ns2)
-	namespaces = store.GetAll()
-	assert.Len(t, namespaces, 2)
-
-	namespaceMap = make(map[string]*storage.NamespaceMetadata)
-	for _, ns := range namespaces {
-		namespaceMap[ns.GetName()] = ns
-	}
-
-	protoassert.Equal(t, ns1, namespaceMap["namespace-1"])
-	protoassert.Equal(t, ns3, namespaceMap["namespace-3"])
-	assert.Nil(t, namespaceMap["namespace-2"])
 }
