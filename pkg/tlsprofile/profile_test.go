a90efa35f3de1906ac5359ba69a4e500e5109dd3
Vlad Bologa <vbologa@redhat.com>
2026-03-30 20:43:18 +0200

    ROX-33255: Allow TLS 1.0/1.1 in tlsprofile (#19652)

--- pkg/tlsprofile/profile_test.go
+++ pkg/tlsprofile/profile_test.go
@@ -29,6 +29,8 @@ func (s *profileSuite) TestParseMinVersion() {
 		want    uint16
 		wantErr bool
 	}{
+		{"TLSv1.0", tls.VersionTLS10, false},
+		{"TLSv1.1", tls.VersionTLS11, false},
 		{"TLSv1.2", tls.VersionTLS12, false},
 		{"TLSv1.3", tls.VersionTLS13, false},
 		{" TLSv1.3 ", tls.VersionTLS13, false},
