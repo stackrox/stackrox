57bdeac7d15f02ee105ba2d72a68c25af9a2e377
Mark Pedrotti <mark@stackrox.com>
2026-04-07 16:45:05 -0400

    ROX-33978: Add item to create scheduled image vulnerabilities report

--- ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/components/CreateReportDropdown.tsx
+++ ui/apps/platform/src/Containers/Vulnerabilities/WorkloadCves/components/CreateReportDropdown.tsx
@@ -3,12 +3,35 @@ import type { MouseEvent as ReactMouseEvent, Ref } from 'react';
 import { Dropdown, DropdownItem, DropdownList, MenuToggle } from '@patternfly/react-core';
 import type { MenuToggleElement } from '@patternfly/react-core';
 
+import useFeatureFlags from 'hooks/useFeatureFlags';
+import { ensureExhaustive } from 'utils/type.utils';
+
+const dropdownItems = [
+    {
+        text: 'Export report as CSV',
+        description:
+            'Export a view-based CSV report from this view using the filters you’ve applied.',
+    },
+    {
+        text: 'Create scheduled report',
+        description: 'Create a scheduled report from this view using the filters you’ve applied.',
+        featureFlagDependency: 'ROX_VULNERABILITY_REPORTS_ENHANCED_FILTERING',
+    },
+] as const;
+
+type DropdownItemText = (typeof dropdownItems)[number]['text'];
+
 export type CreateReportDropdownProps = {
-    onSelect: (value: string | number | undefined) => void;
+    onSelectExportReportAsCSV: () => void;
+    onSelectCreateScheduledReport: () => void;
 };
 
-function CreateReportDropdown({ onSelect }: CreateReportDropdownProps) {
+function CreateReportDropdown({
+    onSelectExportReportAsCSV,
+    onSelectCreateScheduledReport,
+}: CreateReportDropdownProps) {
     const [isOpen, setIsOpen] = useState(false);
+    const { isFeatureFlagEnabled } = useFeatureFlags();
 
     const onToggleClick = () => {
         setIsOpen((prev) => !prev);
@@ -16,9 +39,18 @@ function CreateReportDropdown({ onSelect }: CreateReportDropdownProps) {
 
     const onSelectHandler = (
         _event: ReactMouseEvent<Element, MouseEvent> | undefined,
-        value: string | number | undefined
+        value: DropdownItemText
     ) => {
-        onSelect(value);
+        switch (value) {
+            case 'Export report as CSV':
+                onSelectExportReportAsCSV();
+                break;
+            case 'Create scheduled report':
+                onSelectCreateScheduledReport();
+                break;
+            default:
+                ensureExhaustive(value);
+        }
         setIsOpen(false);
     };
 
@@ -37,13 +69,18 @@ function CreateReportDropdown({ onSelect }: CreateReportDropdownProps) {
                 popperProps={{ position: 'right', appendTo: () => document.body }}
             >
                 <DropdownList>
-                    <DropdownItem
-                        value="Export report as CSV"
-                        key="Export report as CSV"
-                        description="Export a view-based CSV report from this view using the filters you've applied."
-                    >
-                        Export report as CSV
-                    </DropdownItem>
+                    {dropdownItems.map((dropdownItem) => {
+                        const { description, text } = dropdownItem;
+                        const isEnabled =
+                            !('featureFlagDependency' in dropdownItem) ||
+                            isFeatureFlagEnabled(dropdownItem.featureFlagDependency); // DELETE NOT
+
+                        return isEnabled ? (
+                            <DropdownItem key={text} value={text} description={description}>
+                                {text}
+                            </DropdownItem>
+                        ) : null;
+                    })}
                 </DropdownList>
             </Dropdown>
         </>
