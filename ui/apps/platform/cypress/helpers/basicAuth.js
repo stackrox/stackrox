427479c760a2bd6d29c8c1da70058faf5c51793e
Saif Chaudhry <schaudhr@redhat.com>
2026-04-13 10:31:21 -0700

    ROX-33599: Log warning when ROX_AUTH_TOKEN is missing in basicAuth
    Without this, a missing token causes the beforeEach hook to silently
    skip auth setup, leading to confusing "unauthorized" test failures
    that don't point to the actual root cause.
    
    Note: this change was partially generated by AI.
    Signed-off-by: Saif Chaudhry <schaudhr@redhat.com>

--- ui/apps/platform/cypress/helpers/basicAuth.js
+++ ui/apps/platform/cypress/helpers/basicAuth.js
@@ -4,6 +4,8 @@ export default () => {
         cy.env(['ROX_AUTH_TOKEN']).then(({ ROX_AUTH_TOKEN }) => {
             if (ROX_AUTH_TOKEN) {
                 localStorage.setItem('access_token', ROX_AUTH_TOKEN);
+            } else {
+                cy.log('WARNING: ROX_AUTH_TOKEN is not set, tests will run unauthenticated');
             }
         });
     });
