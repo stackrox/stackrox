ac95ed1a0a999f1088a2eaa63bab1774a745aab7
Moritz Clasmeier <mclasmeier@redhat.com>
2026-04-14 14:57:51 +0200

    Migrate two test suites to roxie deployments
    * Proide new roxie-based deployment function
    * Provide compatibility deployment function using roxie under the hood
    * Migrate two test suites to use roxie-based flow: gke_qa_e2e_tests, ocp_qa_e2e_tests.

---
+++ tests/e2e/lib-yaml.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC1091
+
+set -euo pipefail
+
+# For roxie-based deployments we use yq to manipulate the YAML overrides file, and these functions are high-level helpers for that.
+# The functions modify the file in-place.
+
+merge_yaml() {
+    local input="$1"
+    local tmpfile; tmpfile="$(mktemp)"
+
+    yq eval-all '(select(fi == 0) // {}) * select(fi == 1)' "$input" <(cat) > "$tmpfile"
+    cat "$tmpfile" > "$input"
+    rm -f "$tmpfile"
+}
+
+patch_yaml() {
+    local input="$1"
+    local patch="$2"
+
+    yq -i eval "$patch" "$input"
+}
+
+set_custom_env() {
+    local override_file="$1"
+    local component="$2"
+    local name="$3"
+    local value="$4"
+
+    local path=".${component}.spec.customize.envVars"
+
+    info "  ${name}=${value}"
+    # Needs to be initialized to list first, if doesn't exist.
+    init_yaml_path_as_list "$override_file" "$path"
+    patch_yaml "$override_file" "${path} += {\"name\": \"${name}\", \"value\": \"${value}\"}"
+}
+
+set_overlay_env() {
+    local override_file="$1"
+    local component="$2"
+    local api_version="$3"
+    local kind="$4"
+    local resource_name="$5"
+    local container="$6"
+    local name="$7"
+    local value="$8"
+
+    local overlays=".${component}.spec.overlays"
+
+    info "  ${name}=${value} for ${kind}/${resource_name}"
+    # Initialize the overlays list if it doesn't exist.
+    init_yaml_path_as_list "$override_file" "$overlays"
+    # Add a new overlay element for the given api_version/kind/name if it doesn't already exist.
+    local new_empty_overlay="{\"apiVersion\": \"${api_version}\", \"kind\": \"${kind}\", \"name\": \"${resource_name}\", \"patches\": []}"
+    patch_yaml "$override_file" "${overlays} += [${new_empty_overlay}] | ${overlays} |= unique_by([.name, .kind])"
+
+    # Add new patch.
+    local env_path="spec.template.spec.containers[name:${container}].env"
+    local env_entry="{\"name\": \"${name}\", \"value\": \"${value}\"}"
+    local patch="{\"path\":\"${env_path}[-1]\", \"value\": (${env_entry} | toyaml)}"
+    patch_yaml "$override_file" "(${overlays}[] | select(.name == \"${resource_name}\" and .kind == \"${kind}\").patches) += $patch"
+}
+
+init_yaml_path_as_list() {
+    local override_file="$1"
+    local path="$2"
+
+    patch_yaml "$override_file" "${path} = (${path} // [])"
+}
+
+if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
+    if [[ "$#" -lt 1 ]]; then
+        die "When invoked at the command line a method is required."
+    fi
+    fn="$1"
+    shift
+    "$fn" "$@"
+fi
