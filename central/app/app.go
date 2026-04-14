c1d2c6a1a7f8a3069a8e382719d6f2b036d06953
Tomasz Janiszewski <tomek@redhat.com>
2026-04-14 17:16:24 +0200

    fix: simplify central/app to only GraphQL loaders (remove other PRs' changes)
    - Remove metrics.Init() (belongs in metrics PR)
    - Remove compliance, notifiers, backup Init() calls (belong in other PRs)
    - Remove initComponentLogic (belongs in remaining PR)
    - Remove central/app/init.go (belongs in remaining PR)
    
    This PR only introduces GraphQL loaders infrastructure.

--- central/app/app.go
+++ central/app/app.go
@@ -1,13 +1,7 @@
 package app
 
 import (
-	centralChecks "github.com/stackrox/rox/central/compliance/checks"
-	"github.com/stackrox/rox/central/compliance/standards/metadata"
-	backupPlugins "github.com/stackrox/rox/central/externalbackups/plugins/all"
 	"github.com/stackrox/rox/central/graphql/resolvers/loaders"
-	"github.com/stackrox/rox/central/metrics"
-	"github.com/stackrox/rox/central/notifiers"
-	"github.com/stackrox/rox/pkg/compliance/checks"
 	"github.com/stackrox/rox/pkg/memlimit"
 	"github.com/stackrox/rox/pkg/premain"
 )
@@ -19,12 +13,5 @@ func Run() {
 	memlimit.SetMemoryLimit()
 	premain.StartMain()
 
-	metrics.Init()
 	loaders.Init()
-	checks.Init()
-	centralChecks.Init()
-	notifiers.Init()
-	metadata.Init()
-	backupPlugins.Init()
-	initComponentLogic()
 }
