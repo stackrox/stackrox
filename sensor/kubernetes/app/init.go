4836bc5fb0a213548baf2218c5af5c322954f29b
Tomasz Janiszewski <tomek@redhat.com>
2026-04-14 17:19:34 +0200

    fix: move listener resources metrics init to kubernetes app
    roxvet error: sensor/common cannot import from sensor/kubernetes/listener/resources/metrics
    
    Moved listenerresourcesmetrics.Init() from sensor/common/metrics to sensor/kubernetes/app/init.go
    where it belongs (kubernetes-specific, not common).

--- sensor/kubernetes/app/init.go
+++ sensor/kubernetes/app/init.go
@@ -2,8 +2,10 @@ package app
 
 import (
 	"github.com/stackrox/rox/sensor/common/metrics"
+	listenerresourcesmetrics "github.com/stackrox/rox/sensor/kubernetes/listener/resources/metrics"
 )
 
 func initMetrics() {
 	metrics.Init()
+	listenerresourcesmetrics.Init()
 }
