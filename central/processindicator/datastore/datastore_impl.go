11f1fd78f33dc4ae26849d5d13f9648c1620f3c1
Jouko Virtanen <jvirtane@redhat.com>
2026-04-16 08:44:19 -0700

    ROX-32873: Metrics for process arguments (#18794)
    Co-authored-by: Dmitrii Dolgov <9erthalion6@gmail.com>

--- central/processindicator/datastore/datastore_impl.go
+++ central/processindicator/datastore/datastore_impl.go
@@ -98,6 +98,7 @@ func (ds *datastoreImpl) AddProcessIndicators(ctx context.Context, indicators ..
 				return err
 			}
 		} else {
+			recordProcessIndicatorsBatchAdded(identifierBatch)
 			log.Debugf("successfully added a batch of %d process indicators", len(identifierBatch))
 		}
 	}
