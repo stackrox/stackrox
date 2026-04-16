32c7b005dd9e4c4f96cc125242a9a16acb046c13
JoukoVirtanen <jouko@stackrox.com>
2026-04-15 19:36:32 -0700

    Corrected comment

--- central/processindicator/datastore/metrics.go
+++ central/processindicator/datastore/metrics.go
@@ -74,9 +74,7 @@ func getProcessArgsSizeChars(indicator *storage.ProcessIndicator) int {
 	// RuneCountInString returns the number of UTF-8 characters.
 	// For ASCII this is going to be equivalent to len(), but
 	// it is not going to be equivalent for special characters.
-	// len() is O(1), but RuneCountInString is O(n). This should
-	// not be a problem because metrics are handled async and should
-	// not block other processes such as database writes.
+	// len() is O(1), but RuneCountInString is O(n).
 	return utf8.RuneCountInString(indicator.GetSignal().GetArgs())
 }
 
