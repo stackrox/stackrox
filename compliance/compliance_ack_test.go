a70688c07e3bfdbd32597e06bc8d8fd16db1c9cd
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2026-04-20 12:09:23 +0200

    Fix style

--- compliance/compliance_ack_test.go
+++ compliance/compliance_ack_test.go
@@ -193,7 +193,6 @@ func TestResolveVMRelayResourceID(t *testing.T) {
 	}
 
 	for _, tc := range testCases {
-		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 			actual := resolveVMRelayResourceID(tc.input)
