cf70f20f22436275aa8de90e5693acd3da966a28
davdhacs <105243888+davdhacs@users.noreply.github.com>
2026-04-21 13:37:50 -0600

    matching roxie-in-ci state

--- scripts/ci/jobs/gke_qa_e2e_tests.py
+++ scripts/ci/jobs/gke_qa_e2e_tests.py
@@ -10,8 +10,6 @@ from clusters import GKECluster
 # set test parameters
 os.environ["ORCHESTRATOR_FLAVOR"] = "k8s"
 os.environ["USE_ROXIE_DEPLOY"] = "true"
-os.environ["KUBERNETES_PROVIDER"] = "gke"
-os.environ["USE_ROXIE_DEPLOY"] = "true"
 os.environ["GCP_IMAGE_TYPE"] = "cos_containerd"
 
 os.environ["SCANNER_V4_DB_STORAGE_CLASS"] = "stackrox-gke-ssd"
