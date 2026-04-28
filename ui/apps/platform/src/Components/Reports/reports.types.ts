b8d243fb20028185007494875445e18fd60d1283
Mark Pedrotti <pedrottimark@gmail.com>
2026-04-28 10:14:52 -0400

    ROX-34411: Add DeliveryView and render query in report configuration (#20194)

--- ui/apps/platform/src/Components/Reports/reports.types.ts
+++ ui/apps/platform/src/Components/Reports/reports.types.ts
@@ -1,3 +1,11 @@
+import type { NotifierConfiguration } from 'services/ReportsService.types';
+import type { Schedule } from 'types/schedule.proto';
+
+export type DeliveryType = {
+    notifiers: NotifierConfiguration[];
+    schedule: Schedule | null;
+};
+
 export type DetailsType = {
     name: string;
     description: string;
