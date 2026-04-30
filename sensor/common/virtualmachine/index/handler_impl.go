d79cb9c4defb6b9b102e43f6f0ce619153afb48f
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2026-04-30 09:56:48 +0200

    Address review

--- sensor/common/virtualmachine/index/handler_impl.go
+++ sensor/common/virtualmachine/index/handler_impl.go
@@ -467,6 +467,9 @@ func agentFactsFromDiscoveredData(discoveredData *v1.DiscoveredData) map[string]
 	case v1.DnfMetadataStatus_UNAVAILABLE:
 		facts[pkgVM.DNFMetadataStatusKey] = pkgVM.DNFMetadataStatusUnavailable
 	}
+	if len(facts) == 0 {
+		return nil
+	}
 	return facts
 }
 
