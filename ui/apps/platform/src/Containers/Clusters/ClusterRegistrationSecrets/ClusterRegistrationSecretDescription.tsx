6ed297c0953c0d4b8c3d50b3309c65d1d9c5add0
David Vail <dcvail17@gmail.com>
2026-05-01 06:37:38 -0700

    ROX-34489: Group CRS detail page attrs by key with header (#20310)

--- ui/apps/platform/src/Containers/Clusters/ClusterRegistrationSecrets/ClusterRegistrationSecretDescription.tsx
+++ ui/apps/platform/src/Containers/Clusters/ClusterRegistrationSecrets/ClusterRegistrationSecretDescription.tsx
@@ -4,52 +4,88 @@ import {
     DescriptionListDescription,
     DescriptionListGroup,
     DescriptionListTerm,
+    Flex,
+    List,
+    ListItem,
+    Title,
 } from '@patternfly/react-core';
 
-import type { ClusterRegistrationSecret } from 'services/ClustersService';
+import type { ClusterRegistrationSecret, InitBundleAttribute } from 'services/ClustersService';
 
 export type ClusterRegistrationSecretDescriptionProps = {
     clusterRegistrationSecret: ClusterRegistrationSecret;
 };
 
+function groupAttributesByKey(attributes: InitBundleAttribute[]): Map<string, string[]> {
+    const grouped = new Map<string, string[]>();
+    attributes.forEach(({ key, value }) => {
+        const existing = grouped.get(key) ?? [];
+        grouped.set(key, [...existing, value]);
+    });
+    return grouped;
+}
+
 function ClusterRegistrationSecretDescription({
     clusterRegistrationSecret,
 }: ClusterRegistrationSecretDescriptionProps): ReactElement {
+    const groupedAttributes = groupAttributesByKey(clusterRegistrationSecret.createdBy.attributes);
+
     return (
-        <DescriptionList isCompact isHorizontal className="pf-v6-u-p-lg">
-            <DescriptionListGroup>
-                <DescriptionListTerm>Name</DescriptionListTerm>
-                <DescriptionListDescription>
-                    {clusterRegistrationSecret.name}
-                </DescriptionListDescription>
-            </DescriptionListGroup>
-            <DescriptionListGroup>
-                <DescriptionListTerm>Created by</DescriptionListTerm>
-                <DescriptionListDescription>
-                    {clusterRegistrationSecret.createdBy.id}
-                </DescriptionListDescription>
-            </DescriptionListGroup>
-            {clusterRegistrationSecret.createdBy.attributes.map((attribute) => {
-                return (
-                    <DescriptionListGroup key={attribute.key}>
-                        <DescriptionListTerm>{attribute.key}</DescriptionListTerm>
-                        <DescriptionListDescription>{attribute.value}</DescriptionListDescription>
-                    </DescriptionListGroup>
-                );
-            })}
-            <DescriptionListGroup>
-                <DescriptionListTerm>Created at</DescriptionListTerm>
-                <DescriptionListDescription>
-                    {clusterRegistrationSecret.createdAt}
-                </DescriptionListDescription>
-            </DescriptionListGroup>
-            <DescriptionListGroup>
-                <DescriptionListTerm>Expires at</DescriptionListTerm>
-                <DescriptionListDescription>
-                    {clusterRegistrationSecret.expiresAt}
-                </DescriptionListDescription>
-            </DescriptionListGroup>
-        </DescriptionList>
+        <Flex direction={{ default: 'column' }} gap={{ default: 'gapLg' }}>
+            <DescriptionList
+                isCompact
+                isHorizontal
+                horizontalTermWidthModifier={{ default: '20ch' }}
+            >
+                <DescriptionListGroup>
+                    <DescriptionListTerm>Name</DescriptionListTerm>
+                    <DescriptionListDescription>
+                        {clusterRegistrationSecret.name}
+                    </DescriptionListDescription>
+                </DescriptionListGroup>
+                <DescriptionListGroup>
+                    <DescriptionListTerm>Created by</DescriptionListTerm>
+                    <DescriptionListDescription>
+                        {clusterRegistrationSecret.createdBy.id}
+                    </DescriptionListDescription>
+                </DescriptionListGroup>
+                <DescriptionListGroup>
+                    <DescriptionListTerm>Created at</DescriptionListTerm>
+                    <DescriptionListDescription>
+                        {clusterRegistrationSecret.createdAt}
+                    </DescriptionListDescription>
+                </DescriptionListGroup>
+                <DescriptionListGroup>
+                    <DescriptionListTerm>Expires at</DescriptionListTerm>
+                    <DescriptionListDescription>
+                        {clusterRegistrationSecret.expiresAt}
+                    </DescriptionListDescription>
+                </DescriptionListGroup>
+            </DescriptionList>
+            {groupedAttributes.size > 0 && (
+                <Flex direction={{ default: 'column' }} gap={{ default: 'gapMd' }}>
+                    <Title headingLevel="h2">Attributes</Title>
+                    <DescriptionList
+                        isCompact
+                        isHorizontal
+                        horizontalTermWidthModifier={{ default: '20ch' }}
+                    >
+                        {Array.from(groupedAttributes.entries()).map(([key, values]) => (
+                            <DescriptionListGroup key={key}>
+                                <DescriptionListTerm>{key}</DescriptionListTerm>
+                                <DescriptionListDescription>
+                                    <List isPlain>
+                                        {values.map((value) => (
+                                            <ListItem key={value}>{value}</ListItem>
+                                        ))}
+                                    </List>
+                                </DescriptionListDescription>
+                            </DescriptionListGroup>
+                        ))}
+                    </DescriptionList>
+                </Flex>
+            )}
+        </Flex>
     );
 }
 
