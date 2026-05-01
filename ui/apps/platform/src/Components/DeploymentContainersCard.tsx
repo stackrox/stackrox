d7ad52dd47e6dde90592fd34935a7a4d9e6af1e7
David Vail <dcvail17@gmail.com>
2026-05-01 06:38:01 -0700

    ROX-34016: Init container display (#20073)

--- ui/apps/platform/src/Components/DeploymentContainersCard.tsx
+++ ui/apps/platform/src/Components/DeploymentContainersCard.tsx
@@ -1,5 +1,6 @@
 import { Card, CardBody, CardTitle, Stack, StackItem } from '@patternfly/react-core';
 
+import useContainerGroups from 'hooks/useContainerGroups';
 import type { Container } from 'types/deployment.proto';
 import DeploymentContainerConfig from './DeploymentContainerConfig';
 
@@ -9,31 +10,59 @@ type DeploymentContainersCardProps = {
     getImageUrl: (imageId: string) => string;
 };
 
+function ContainerGroup({
+    containers,
+    getImageUrl,
+}: {
+    containers: Container[];
+    getImageUrl: (imageId: string) => string;
+}) {
+    return (
+        <Stack hasGutter>
+            {containers.map((container) => (
+                <StackItem key={container.id}>
+                    <DeploymentContainerConfig container={container} getImageUrl={getImageUrl} />
+                </StackItem>
+            ))}
+        </Stack>
+    );
+}
+
 function DeploymentContainersCard({
     containers,
     title,
     getImageUrl,
 }: DeploymentContainersCardProps) {
+    const { regularContainers, initContainers } = useContainerGroups(containers);
+
     return (
-        <Card>
-            <CardTitle component="h3">{title}</CardTitle>
-            <CardBody>
-                {containers && containers.length > 0 ? (
-                    <Stack hasGutter>
-                        {containers.map((container) => (
-                            <StackItem key={container.id}>
-                                <DeploymentContainerConfig
-                                    container={container}
-                                    getImageUrl={getImageUrl}
-                                />
-                            </StackItem>
-                        ))}
-                    </Stack>
-                ) : (
-                    'None'
-                )}
-            </CardBody>
-        </Card>
+        <Stack hasGutter>
+            <StackItem>
+                <Card>
+                    <CardTitle component="h3">{title}</CardTitle>
+                    <CardBody>
+                        {regularContainers.length > 0 ? (
+                            <ContainerGroup
+                                containers={regularContainers}
+                                getImageUrl={getImageUrl}
+                            />
+                        ) : (
+                            'None'
+                        )}
+                    </CardBody>
+                </Card>
+            </StackItem>
+            {initContainers.length > 0 && (
+                <StackItem>
+                    <Card>
+                        <CardTitle component="h3">Init container configuration</CardTitle>
+                        <CardBody>
+                            <ContainerGroup containers={initContainers} getImageUrl={getImageUrl} />
+                        </CardBody>
+                    </Card>
+                </StackItem>
+            )}
+        </Stack>
     );
 }
 
