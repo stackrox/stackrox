f5ca2cf95c5d4793e0aa73116fca0e29eb80ead9
Aleksandr Kurlov <akurlov@redhat.com>
2026-05-04 12:01:09 +0200

    Add helm chart truncate tests

---
+++ pkg/helm/charts/tests/centralservices/chart_label_truncation_test.go
@@ -0,0 +1,19 @@
+package centralservices
+
+import (
+	"testing"
+
+	"github.com/stackrox/rox/image"
+	"github.com/stackrox/rox/pkg/helm/charts"
+	helmChartTestUtils "github.com/stackrox/rox/pkg/helm/charts/testutils"
+)
+
+func TestChartLabelTruncation(t *testing.T) {
+	testSuiteOpts := helmChartTestUtils.RunHelmTestSuiteOpts{
+		MetaValuesOverridesFunc: func(vals *charts.MetaValues) {
+			// Create a very long chart version that will exceed 63 chars (combined with the chart name)
+			vals.Versions.ChartVersion = "400.0.0-extremely-long-prerelease-version-string-for-testing-"
+		},
+	}
+	helmChartTestUtils.RunHelmTestSuite(t, "testdata/helmtest-truncation", image.CentralServicesChartPrefix, testSuiteOpts)
+}
