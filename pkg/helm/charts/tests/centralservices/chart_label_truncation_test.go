656e839e05675460a7bdd7729428c52eaab81294
Aleksandr Kurlov <akurlov@redhat.com>
2026-05-07 17:37:54 +0200

    Add a commet about why label string ends with non alphanumeric

--- pkg/helm/charts/tests/centralservices/chart_label_truncation_test.go
+++ pkg/helm/charts/tests/centralservices/chart_label_truncation_test.go
@@ -12,6 +12,7 @@ func TestChartLabelTruncation(t *testing.T) {
 	testSuiteOpts := helmChartTestUtils.RunHelmTestSuiteOpts{
 		MetaValuesOverridesFunc: func(vals *charts.MetaValues) {
 			// Create a very long chart version that will exceed 63 chars (combined with the chart name)
+			// which ends with a non alphanumeric character to test the truncation and sanitization logic.
 			vals.Versions.ChartVersion = "400.0.0-extremely-long-prerelease-version-string-for-testing-"
 		},
 	}
