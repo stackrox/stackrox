b1f6525436c61968565e145f4eebe8ff377b54e2
JoukoVirtanen <jouko@stackrox.com>
2026-05-07 17:03:23 -0700

    Updated go in install-dependencies.sh and made things configurable

--- tests/performance/scale/tests/install-dependencies.sh
+++ tests/performance/scale/tests/install-dependencies.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 set -eoux pipefail
 
+GO_VERSION="${GO_VERSION:-1.25.9}"
+KUBECTL_VERSION="${KUBECTL_VERSION:-v1.30.0}"
+KUBE_BURNER_VERSION="${KUBE_BURNER_VERSION:-1.4.3}"
+ROXCTL_VERSION="${ROXCTL_VERSION:-4.4.2}"
+STACKROX_BRANCH="${STACKROX_BRANCH:-jv-automate-perf-tests-4.11}"
+
 git clone https://github.com/stackrox/stackrox.git
 cd "${HOME}/stackrox"
-git checkout jv-automate-perf-tests-stable
+git checkout "${STACKROX_BRANCH}"
 cd "${HOME}"
 
 git clone https://github.com/stackrox/workflow.git
-export PATH=${PATH}:/home/jvirtane/workflow/bin
+export PATH="${PATH}:${HOME}/workflow/bin"
 
-curl -LO https://dl.k8s.io/release/v1.30.0/bin/linux/amd64/kubectl
+curl -LO "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-
-export KUBE_BURNER_VERSION=1.4.3
 
 mkdir -p ./kube-burner
 curl --silent --location "https://github.com/cloud-bulldozer/kube-burner/releases/download/v${KUBE_BURNER_VERSION}/kube-burner-${KUBE_BURNER_VERSION}-$(uname -s)-$(uname -m).tar.gz" --output "./kube-burner/kube-burner-${KUBE_BURNER_VERSION}.tar.gz"
@@ -29,11 +33,11 @@ helm repo add rhacs https://mirror.openshift.com/pub/rhacs/charts
 helm repo update
 
 arch="$(uname -m | sed "s/x86_64//")"; arch="${arch:+-$arch}"
-curl -f -o roxctl "https://mirror.openshift.com/pub/rhacs/assets/4.4.2/bin/Linux/roxctl${arch}"
+curl -f -o roxctl "https://mirror.openshift.com/pub/rhacs/assets/${ROXCTL_VERSION}/bin/Linux/roxctl${arch}"
 chmod +x roxctl
 sudo cp roxctl /usr/local/bin
 
 sudo apt-get install jq -y
 sudo snap install yq
 
-sudo snap install go --channel=1.21/stable --classic
+sudo snap install go --channel="${GO_VERSION}/stable" --classic
