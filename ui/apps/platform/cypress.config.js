5ff3fa8923cd67a011628bfcc3c1af1c25567710
David Vail <dcvail17@gmail.com>
2026-05-11 12:43:36 -0400

    Replace cypress-vite with minimal webpack config

--- ui/apps/platform/cypress.config.js
+++ ui/apps/platform/cypress.config.js
@@ -1,11 +1,95 @@
 const path = require('path');
 
-const cypressVite = require('cypress-vite');
+const webpack = require('webpack');
 
-// This is needed because cypress-axe will attempt to resolve the axe.min.js file internally using CommonJS require,
-// which is not supported in Vite. Instead, we resolve the path to the file using CommonJS require in the cypress config
-// and make the path available at test runtime when injecting the axe core.
+// Resolve the axe-core path in Node so it can be passed to the browser via Cypress.env('AXE_CORE_PATH')
 const axeCorePath = require.resolve('axe-core/axe.min.js');
+
+const outputDir = path.resolve(__dirname, 'cypress/webpack-output');
+const compilers = {};
+
+function makeWebpackConfig(filePath) {
+    return {
+        mode: 'development',
+        devtool: 'inline-source-map',
+        entry: { [filePath.replace(/[/\\]/g, '_')]: filePath },
+        output: { path: outputDir, filename: '[name].js' },
+        resolve: { extensions: ['.ts', '.js'] },
+        module: {
+            rules: [
+                {
+                    test: /\.[jt]s$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'ts-loader',
+                        options: {
+                            transpileOnly: true,
+                            configFile: path.resolve(__dirname, 'cypress/tsconfig.json'),
+                        },
+                    },
+                },
+            ],
+        },
+    };
+}
+
+function compile(compiler) {
+    return new Promise((resolve, reject) => {
+        compiler.run((err, stats) => {
+            if (err) {
+                return reject(err);
+            }
+            if (stats.hasErrors()) {
+                return reject(new Error(stats.toString({ errors: true })));
+            }
+            return resolve();
+        });
+    });
+}
+
+function webpackPreprocessor(file) {
+    const { filePath, shouldWatch } = file;
+    const bundlePath = path.join(outputDir, `${filePath.replace(/[/\\]/g, '_')}.js`);
+
+    if (shouldWatch) {
+        if (compilers[filePath]) {
+            return compilers[filePath].promise;
+        }
+
+        const compiler = webpack(makeWebpackConfig(filePath));
+        const bundle = { initial: true };
+        bundle.promise = new Promise((resolve, reject) => {
+            compiler.watch({}, (err, stats) => {
+                if (err || stats.hasErrors()) {
+                    const error = err || new Error(stats.toString({ errors: true }));
+                    reject(error);
+                    return;
+                }
+                resolve(bundlePath);
+                bundle.promise = Promise.resolve(bundlePath);
+                if (!bundle.initial) {
+                    file.emit('rerun');
+                }
+                bundle.initial = false;
+            });
+        });
+
+        compilers[filePath] = bundle;
+
+        file.on('close', () => {
+            delete compilers[filePath];
+            compiler.close(() => {});
+        });
+
+        return bundle.promise;
+    }
+
+    const compiler = webpack(makeWebpackConfig(filePath));
+    return compile(compiler).then(() => {
+        compiler.close(() => {});
+        return bundlePath;
+    });
+}
 
 /*
  * The helper function intended to provide automatic code completion for configuration in many popular code editors
@@ -48,7 +132,7 @@ module.exports = {
                     return path.join(...paths);
                 },
             });
-            on('file:preprocessor', cypressVite.vitePreprocessor());
+            on('file:preprocessor', webpackPreprocessor);
             return config;
         },
     },
