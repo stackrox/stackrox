45cf31b1baf81f46f06c9eeb935094089552bbed
Moritz Clasmeier <mclasmeier@redhat.com>
2026-05-18 11:09:52 +0200

    roxie-based teardown

--- qa-tests-backend/scripts/run-part-1.sh
+++ qa-tests-backend/scripts/run-part-1.sh
@@ -44,7 +44,13 @@ config_part_1() {
 
     setup_gcp
     setup_deployment_env false false
-    remove_existing_stackrox_resources
+    if [[ "$use_roxie_deploy" == "true" ]]; then
+        info "Using roxie-based pre-test teardown"
+        roxie teardown all --single-namespace
+    else
+        info "Using traditional pre-test teardown"
+        remove_existing_stackrox_resources
+    fi
     setup_default_TLS_certs "$ROOT/$DEPLOY_DIR/default_TLS_certs"
 
     image_prefetcher_system_await
