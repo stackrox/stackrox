bb896663baa25add2b15c4158421f92d7dacd44f
Saif Chaudhry <sachaudh@users.noreply.github.com>
2026-05-19 08:54:15 -0700

    ROX-34095: Remove shared PDF export infrastructure and dependencies (#20667)
    Signed-off-by: Saif Chaudhry <schaudhr@redhat.com>

--- ui/apps/platform/src/Components/ExportButton.jsx
+++ ui/apps/platform/src/Components/ExportButton.jsx
@@ -2,14 +2,12 @@ import { Component } from 'react';
 import PropTypes from 'prop-types';
 import { FileText } from 'react-feather';
 import onClickOutside from 'react-onclickoutside';
-import { toast } from 'react-toastify';
 import { Button } from '@patternfly/react-core';
 
 import downloadCSV from 'services/CSVDownloadService';
-import WorkflowPDFExportButton from 'Components/WorkflowPDFExportButton';
 import ButtonClassic from 'Components/Button';
-import useCaseTypes from 'constants/useCaseTypes';
 import entityTypes from 'constants/entityTypes';
+import useCaseTypes from 'constants/useCaseTypes';
 import { addBrandedTimestampToString } from 'utils/dateUtils';
 
 const queryParamMap = {
@@ -18,21 +16,6 @@ const queryParamMap = {
     ALL: '',
 };
 
-const complianceDownloadUrl = '/api/compliance/export/csv';
-
-function checkVulnMgmtSupport(page, type) {
-    return (
-        page === useCaseTypes.VULN_MANAGEMENT &&
-        (type === entityTypes.CVE ||
-            type === entityTypes.IMAGE_CVE ||
-            type === entityTypes.NODE_CVE ||
-            type === entityTypes.CLUSTER_CVE)
-    );
-}
-
-function checkComplianceSupport(page, type) {
-    return page === useCaseTypes.COMPLIANCE && Object.keys(queryParamMap).includes(type);
-}
 class ExportButton extends Component {
     static propTypes = {
         className: PropTypes.string,
@@ -40,13 +23,8 @@ class ExportButton extends Component {
         fileName: PropTypes.string,
         type: PropTypes.string,
         id: PropTypes.string,
-        pdfId: PropTypes.string,
-        tableOptions: PropTypes.shape({}),
-        customCsvExportHandler: PropTypes.func,
         page: PropTypes.string,
         disabled: PropTypes.bool,
-        isExporting: PropTypes.bool,
-        setIsExporting: PropTypes.func,
     };
 
     static defaultProps = {
@@ -55,13 +33,8 @@ class ExportButton extends Component {
         fileName: 'compliance',
         type: null,
         id: '',
-        pdfId: '',
-        tableOptions: {},
-        customCsvExportHandler: null,
         page: '',
         disabled: false,
-        isExporting: false,
-        setIsExporting: () => {},
     };
 
     constructor(props) {
@@ -69,73 +42,35 @@ class ExportButton extends Component {
 
         this.state = {
             toggleWidget: false,
-            csvIsDownloading: false,
         };
     }
 
     handleClickOutside = () => this.setState({ toggleWidget: false });
 
     downloadCSVFile = () => {
-        const { id, fileName, page, type, customCsvExportHandler } = this.props;
+        const { id, fileName, type } = this.props;
         const csvName = addBrandedTimestampToString(fileName);
 
-        if (checkVulnMgmtSupport(page, type)) {
-            if (!customCsvExportHandler) {
-                throw new Error('A CSV export handler was not supplied to handle CSV export');
-            }
-
-            this.setState({ csvIsDownloading: true });
-            customCsvExportHandler(csvName)
-                .catch((err) => {
-                    toast(`An error occurred while trying to export: ${err}`);
-                })
-                .finally(() => {
-                    this.setState({ toggleWidget: false, csvIsDownloading: false });
-                });
-        } else {
-            // otherwise, use legacy compliance CSV export
-            let queryStr = '';
-            let value = null;
-
-            // Support for StandardId & ClusterId only
-            if (queryParamMap[type]) {
-                if (id) {
-                    value = id;
-                }
-                queryStr = `${queryParamMap[type]}=${value}`;
-            }
-
-            downloadCSV(csvName, complianceDownloadUrl, queryStr);
+        let queryStr = '';
+        if (queryParamMap[type] && id) {
+            queryStr = `${queryParamMap[type]}=${id}`;
         }
+
+        downloadCSV(csvName, '/api/compliance/export/csv', queryStr);
     };
 
-    isCsvSupported = () => {
+    isSupported = () => {
         const { page, type } = this.props;
-
-        const isVulnMgmtSupportedPage = checkVulnMgmtSupport(page, type);
-        const isComplianceSupportedPage = checkComplianceSupport(page, type);
-
-        return isVulnMgmtSupportedPage || isComplianceSupportedPage;
+        return page === useCaseTypes.COMPLIANCE && Object.keys(queryParamMap).includes(type);
     };
 
     renderContent = () => {
-        const csvButtonText =
-            this.props.type === entityTypes.CVE
-                ? 'Download CVES as CSV'
-                : 'Download Evidence as CSV';
-        const { toggleWidget, csvIsDownloading } = this.state;
-        if (!toggleWidget) {
+        if (!this.state.toggleWidget) {
             return null;
         }
 
-        const headerText = this.props.fileName;
-
-        const fileName = addBrandedTimestampToString(headerText);
-
-        const wrapperClass = !!this.props.pdfId && !this.isCsvSupported() ? 'min-w-64' : '';
-
-        return !!this.props.pdfId || this.isCsvSupported() ? (
-            <div className={`absolute right-0 z-20 flex flex-col text-base-600 ${wrapperClass}`}>
+        return this.isSupported() ? (
+            <div className="absolute right-0 z-20 flex flex-col text-base-600">
                 <div className="arrow-up self-end mr-5" />
                 <ul
                     className="bg-base-100 rounded"
@@ -146,33 +81,10 @@ class ExportButton extends Component {
                 >
                     <li className="p-4 border-b">
                         <div className="flex">
-                            {!!this.props.pdfId && (
-                                <WorkflowPDFExportButton
-                                    id={this.props.pdfId}
-                                    className={`min-w-48 ${
-                                        this.isCsvSupported() ? 'mr-2' : 'w-full'
-                                    }`}
-                                    tableOptions={this.props.tableOptions}
-                                    fileName={fileName}
-                                    pdfTitle={headerText}
-                                    isExporting={this.props.isExporting}
-                                    setIsExporting={this.props.setIsExporting}
-                                />
-                            )}
-                            {this.isCsvSupported() && (
-                                <Button
-                                    variant="primary"
-                                    onClick={this.downloadCSVFile}
-                                    isLoading={csvIsDownloading}
-                                >
-                                    {csvButtonText}
-                                </Button>
-                            )}
+                            <Button variant="primary" onClick={this.downloadCSVFile}>
+                                Download Evidence as CSV
+                            </Button>
                         </div>
-                    </li>
-                    <li className="hidden">
-                        <span>or share to</span>
-                        <div>Slack</div>
                     </li>
                 </ul>
             </div>
