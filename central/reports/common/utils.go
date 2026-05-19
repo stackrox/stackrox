95890a4b139c6a5a04da0f6fbb27ccae82772224
Surabhi <surakuma@redhat.com>
2026-05-19 15:54:15 -0700

    Fix validation check to use getscoperef

--- central/reports/common/utils.go
+++ central/reports/common/utils.go
@@ -50,13 +50,13 @@ func IsV2ReportConfig(config *storage.ReportConfiguration) bool {
 }
 
 // HasValidResourceScope returns true if the report config has a non-empty resource scope
-// containing either a collection ID or an entity scope. Returns false when ResourceScope
+// containing either a collection ID , entity scope etc. Returns false when ResourceScope
 // is nil or is an empty message with no scope_reference set. Later can happen in downgrade scenarios.
 func HasValidResourceScope(scope *storage.ResourceScope) bool {
-	if scope == nil {
+	if scope == nil || scope.GetScopeReference() == nil {
 		return false
 	}
-	return scope.GetCollectionId() != "" || scope.GetEntityScope() != nil
+	return true
 }
 
 // WithoutV2ReportConfigs adds a conjunction query to exclude v2 report configs
