000369e7af88e72c7049da7ed323b88cff602744
Piotr Rygielski <114479+vikin91@users.noreply.github.com>
2026-05-20 09:42:26 +0200

    Add comment

--- scripts/ci/jobs/ocp_vm_scanning_e2e_tests.py
+++ scripts/ci/jobs/ocp_vm_scanning_e2e_tests.py
@@ -24,6 +24,7 @@ os.environ["SENSOR_HELM_MANAGED"] = "true"
 os.environ["INSTALL_CNV_OPERATOR"] = "true"
 os.environ["ROX_VIRTUAL_MACHINES"] = "true"
 os.environ["ROX_SCANNER_V4"] = "true"
+# Avoid default rate limiting of VM index reports set to 1 report per minute per VM.
 os.environ["ROX_VM_RELAY_MAX_REPORTS_PER_MINUTE"] = "10"
 os.environ["VM_IMAGES"] = ",".join([
     "quay.io/rhacs-eng/vm-images:rhel9-dnf-primed-latest",
