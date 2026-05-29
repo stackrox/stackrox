221fe84ef232b8ce8db6fa28a8915f99c9c5e67a
Alex Rukletsov <alexr@redhat.com>
2026-05-29 17:43:56 +0200

    ROX-27600, ROX-27170, ROX-25991: Add a retry for `oc login` on OSD (#20850)

--- tests/e2e/lib.sh
+++ tests/e2e/lib.sh
@@ -1879,10 +1879,14 @@ setup_automation_flavor_e2e_cluster() {
     if [[ "$ci_job" =~ ^osd ]]; then
         info "Logging in to an OSD cluster"
         source "${SHARED_DIR}/dotenv"
-        oc login "$CLUSTER_API_ENDPOINT" \
+        # OSD API server certificates may not be fully propagated right after
+        # cluster creation, causing transient x509 errors (ROX-27600).
+        retry 5 true \
+            oc login "$CLUSTER_API_ENDPOINT" \
                 --username "$CLUSTER_USERNAME" \
                 --password "$CLUSTER_PASSWORD" \
-                --insecure-skip-tls-verify=true
+                --insecure-skip-tls-verify=true \
+            || die "Failed to log in to OSD cluster"
     fi
 
     # Export console credentials for OCP UI e2e tests (Cypress browser login)
