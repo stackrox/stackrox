3a52723b5a3535291da422e670c752bf5503bbc9
David House <105243888+davdhacs@users.noreply.github.com>
2026-05-29 22:21:28 -0600

    Apply suggestion from @davdhacs

--- tests/e2e/lib.sh
+++ tests/e2e/lib.sh
@@ -285,7 +285,7 @@ deploy_stackrox_with_custom_central_and_sensor_versions() {
     ci_export OUTPUT_FORMAT "helm"
 
     # Repo name can't be too long or `helm search repo [REPO_NAME] -l` cuts off part of the name and the regex below fails.
-    helm_repo_name="tmp-srox-compat"
+    local helm_repo_name="tmp-srox-compat"
     local helm_chart_url="https://raw.githubusercontent.com/stackrox/helm-charts/main/opensource"
     if ! helm repo list -o json | jq -e --arg name "$helm_repo_name" --arg url "$helm_chart_url" \
         'any(.[]; .name == $name and .url == $url)'; then
