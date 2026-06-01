d571e83ca146879e56a6a14c697419c013bd18c3
Surabhi <surakuma@redhat.com>
2026-06-01 15:36:27 -0700

    trigger build with prerelease

--- central/detection/lifecycle/singleton.go
+++ central/detection/lifecycle/singleton.go
@@ -45,7 +45,7 @@ func initialize() {
 	log.Infof("Injecting %d policies into detectors.", len(policies))
 	for _, policy := range policies {
 		err = manager.UpsertPolicy(policy)
-		utils.Should(errors.Wrap(err, "could not inject policy"))
+		log.Error(errors.Wrap(err, "could not inject policy"))
 	}
 	log.Info("Done injecting policies.")
 
