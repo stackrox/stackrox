2cbeb1acc7cfafc74c3e05c4afd6b6a92052824c
Alex Vulaj <avulaj@redhat.com>
2026-06-22 14:51:18 -0400

    ROX-35227: fix Artifactory test always returning success
    Artifactory's /v2 ping endpoint does not require auth, so the default
    Test() always succeeded even with invalid credentials. Override Test()
    to call Repositories() which requires valid credentials.
    
    Partially generated by AI.

--- pkg/registries/artifactory/artifactory.go
+++ pkg/registries/artifactory/artifactory.go
@@ -1,17 +1,26 @@
 package artifactory
 
 import (
+	"github.com/heroku/docker-registry-client/registry"
+	"github.com/pkg/errors"
 	"github.com/stackrox/rox/generated/storage"
+	"github.com/stackrox/rox/pkg/logging"
 	"github.com/stackrox/rox/pkg/registries/docker"
 	"github.com/stackrox/rox/pkg/registries/types"
 )
+
+var log = logging.LoggerForModule()
 
 // Creator provides the type and registries.Creator to add to the registry of image registries.
 func Creator() (string, types.Creator) {
 	return types.ArtifactoryType,
 		func(integration *storage.ImageIntegration, options ...types.CreatorOption) (types.Registry, error) {
 			cfg := types.ApplyCreatorOptions(options...)
-			return docker.NewDockerRegistry(integration, false, cfg.GetMetricsHandler())
+			reg, err := docker.NewDockerRegistry(integration, false, cfg.GetMetricsHandler())
+			if err != nil {
+				return nil, err
+			}
+			return &artifactoryRegistry{Registry: reg}, nil
 		}
 }
 
@@ -21,6 +30,31 @@ func CreatorWithoutRepoList() (string, types.Creator) {
 	return types.ArtifactoryType,
 		func(integration *storage.ImageIntegration, options ...types.CreatorOption) (types.Registry, error) {
 			cfg := types.ApplyCreatorOptions(options...)
-			return docker.NewDockerRegistry(integration, true, cfg.GetMetricsHandler())
+			reg, err := docker.NewDockerRegistry(integration, true, cfg.GetMetricsHandler())
+			if err != nil {
+				return nil, err
+			}
+			return &artifactoryRegistry{Registry: reg}, nil
 		}
+}
+
+var _ types.Registry = (*artifactoryRegistry)(nil)
+
+type artifactoryRegistry struct {
+	*docker.Registry
+}
+
+// Test overrides the default docker Test because Artifactory's /v2 ping
+// endpoint does not require authentication, so Ping always succeeds even
+// with invalid credentials.
+func (a *artifactoryRegistry) Test() error {
+	_, err := a.Client.Repositories()
+	if err != nil {
+		log.Errorf("error testing Artifactory integration: %v", err)
+		if e, _ := err.(*registry.ClientError); e != nil {
+			return errors.Errorf("error testing Artifactory integration (code: %d). Please check Central logs for full error", e.Code())
+		}
+		return err
+	}
+	return nil
 }
