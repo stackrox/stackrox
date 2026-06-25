b9202a15d5b5c2d23cc7f805f94f808853a9fd3a
Alex Vulaj <avulaj@redhat.com>
2026-06-25 15:23:35 -0400

    ROX-34960: add e2e tests for evaluation filter and CRD support (#21332)

--- pkg/features/list.go
+++ pkg/features/list.go
@@ -141,7 +141,7 @@ var (
 	BackgroundMigration = registerFeature("Enable long-running background migrations in Central", "ROX_BACKGROUND_MIGRATION", enabled)
 
 	// EvaluationFilter enables evaluation filtering in policy evaluation.
-	EvaluationFilter = registerFeature("Enable evaluation filtering in policy evaluation", "ROX_EVALUATION_FILTER")
+	EvaluationFilter = registerFeature("Enable evaluation filtering in policy evaluation", "ROX_EVALUATION_FILTER", enabled)
 )
 
 // The following feature flags are related to Scanner V4.
