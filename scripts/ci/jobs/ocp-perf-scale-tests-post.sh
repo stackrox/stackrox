0f9d8e6e4e56b85107710028925797bfb6b04da4
Tomasz Janiszewski <tomek@redhat.com>
2026-07-15 16:08:14 +0200

    Apply suggestion from @janisz

--- scripts/ci/jobs/ocp-perf-scale-tests-post.sh
+++ scripts/ci/jobs/ocp-perf-scale-tests-post.sh
@@ -70,7 +70,6 @@ info "Central API is responsive, getting diagnostics"
 # IPI manages the cluster lifecycle, but end.sh expects these variables
 # to generate correct JUnit records (see PR #21101).
 set_ci_shared_export CREATE_CLUSTER_OUTCOME "${OUTCOME_PASSED}"
-set_ci_shared_export DESTROY_CLUSTER_OUTCOME "${OUTCOME_PASSED}"
 set_ci_shared_export CLUSTER_FLAVOR_VARIANT "openshift-4"
 
 # Get central diagnostics bundle
