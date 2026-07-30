91af021cd3b827e7341020929b11f7d907b36fbe
Michaël <github@parameta.lol>
2026-07-30 11:48:44 +0200

    refactoring

--- central/metrics/custom/expiry/tracker.go
+++ central/metrics/custom/expiry/tracker.go
@@ -5,7 +5,7 @@ import (
 	"time"
 
 	clusterDS "github.com/stackrox/rox/central/cluster/datastore"
-	"github.com/stackrox/rox/central/credentialexpiry/service"
+	creds "github.com/stackrox/rox/central/credentialexpiry/service"
 	"github.com/stackrox/rox/central/metrics"
 	"github.com/stackrox/rox/central/metrics/custom/tracker"
 	v1 "github.com/stackrox/rox/generated/api/v1"
@@ -19,7 +19,7 @@ import (
 // "Name" label rather than by a fixed enum value.
 const securedClusterComponent = "SECURED_CLUSTER"
 
-func New(s service.Service, clusters clusterDS.DataStore) *tracker.TrackerBase[*finding] {
+func New(s creds.Service, clusters clusterDS.DataStore) *tracker.TrackerBase[*finding] {
 	return tracker.MakeTrackerBase(
 		metrics.Expiry,
 		"hours before certificate expires",
@@ -30,47 +30,58 @@ func New(s service.Service, clusters clusterDS.DataStore) *tracker.TrackerBase[*
 	)
 }
 
-func track(ctx context.Context, s service.Service, clusters clusterDS.DataStore) tracker.FindingErrorSequence[*finding] {
+func track(ctx context.Context, s creds.Service, clusters clusterDS.DataStore) tracker.FindingErrorSequence[*finding] {
 	return func(yield func(*finding, error) bool) {
 		if s != nil {
-			for i, component := range v1.GetCertExpiry_Component_name {
-				if v1.GetCertExpiry_Component(i) == v1.GetCertExpiry_UNKNOWN {
-					continue
-				}
-				result, err := s.GetCertExpiry(ctx, &v1.GetCertExpiry_Request{
-					Component: v1.GetCertExpiry_Component(i),
-				})
-				if err != nil {
-					// Ignore particular component errors, as they do not affect
-					// other components metrics.
-					logging.LoggerForModule().Errorw("Failed to get certificate expiry",
-						logging.String("component", component), logging.Err(err))
-					continue
-				}
-				f := &finding{component: component}
-				if result != nil {
-					f.hoursUntilExpiration = int(time.Until(result.GetExpiry().AsTime()).Hours())
-				}
-				if !yield(f, nil) {
-					return
-				}
+			if !fromCredsService(ctx, s, yield) {
+				return
 			}
 		}
 		if clusters == nil {
 			return
 		}
-		collector := tracker.NewFindingCollector(yield)
-		collector.Finally(clusters.WalkClusters(ctx, func(cluster *storage.Cluster) error {
-			expiry := cluster.GetStatus().GetCertExpiryStatus().GetSensorCertExpiry()
-			if expiry == nil {
-				// The cluster hasn't connected yet, so there is no cert to report on.
-				return nil
-			}
-			return collector.Yield(&finding{
-				component:            securedClusterComponent,
-				name:                 cluster.GetName(),
-				hoursUntilExpiration: int(time.Until(expiry.AsTime()).Hours()),
-			})
-		}))
+		fromClustersDS(ctx, clusters, yield)
 	}
+}
+
+func fromCredsService(ctx context.Context, s creds.Service, yield func(*finding, error) bool) bool {
+	for i, component := range v1.GetCertExpiry_Component_name {
+		if v1.GetCertExpiry_Component(i) == v1.GetCertExpiry_UNKNOWN {
+			continue
+		}
+		result, err := s.GetCertExpiry(ctx, &v1.GetCertExpiry_Request{
+			Component: v1.GetCertExpiry_Component(i),
+		})
+		if err != nil {
+			// Ignore particular component errors, as they do not affect
+			// other components metrics.
+			logging.LoggerForModule().Errorw("Failed to get certificate expiry",
+				logging.String("component", component), logging.Err(err))
+			continue
+		}
+		f := &finding{component: component}
+		if result != nil {
+			f.hoursUntilExpiration = int(time.Until(result.GetExpiry().AsTime()).Hours())
+		}
+		if !yield(f, nil) {
+			return false
+		}
+	}
+	return true
+}
+
+func fromClustersDS(ctx context.Context, clusters clusterDS.DataStore, yield func(*finding, error) bool) {
+	collector := tracker.NewFindingCollector(yield)
+	collector.Finally(clusters.WalkClusters(ctx, func(cluster *storage.Cluster) error {
+		expiry := cluster.GetStatus().GetCertExpiryStatus().GetSensorCertExpiry()
+		if expiry == nil {
+			// The cluster hasn't connected yet, so there is no cert to report on.
+			return nil
+		}
+		return collector.Yield(&finding{
+			component:            securedClusterComponent,
+			name:                 cluster.GetName(),
+			hoursUntilExpiration: int(time.Until(expiry.AsTime()).Hours()),
+		})
+	}))
 }
