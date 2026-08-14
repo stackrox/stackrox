1918073d0a996aa0a4281fc2d2212cc8a0295407
Surabhi Kumar <surakuma@redhat.com>
2026-08-14 16:03:54 -0700

    ROX-33402: UI changes to  fix reporting rbac (#21099)

--- ui/apps/platform/src/Containers/Vulnerabilities/VulnerablityReporting/VulnReports/VulnReportingLayout.tsx
+++ ui/apps/platform/src/Containers/Vulnerabilities/VulnerablityReporting/VulnReports/VulnReportingLayout.tsx
@@ -1,30 +1,44 @@
-import { PageSection, Tab, Tabs, Title } from '@patternfly/react-core';
-import { Outlet, useLocation, useNavigate } from 'react-router-dom-v5-compat';
+import { PageSection, Tab, TabContent, Tabs, Title } from '@patternfly/react-core';
+import { useLocation, useNavigate } from 'react-router-dom-v5-compat';
 
 import PageTitle from 'Components/PageTitle';
+import usePermissions from 'hooks/usePermissions';
 import {
     vulnerabilityConfigurationReportsPath,
     vulnerabilityViewBasedReportsPath,
 } from 'routePaths';
 
-const tabs = [
-    {
-        id: 'report-configuration',
-        title: 'Report configurations',
-        path: vulnerabilityConfigurationReportsPath,
-    },
-    {
-        id: 'view-based-reports',
-        title: 'View-based reports',
-        path: vulnerabilityViewBasedReportsPath,
-    },
-];
+import ConfigReportsTab from './ConfigReportsTab';
+import ViewBasedReportsTab from './ViewBasedReportsTab';
 
 function VulnReportingLayout() {
     const location = useLocation();
     const navigate = useNavigate();
 
-    const activeTabIndex = tabs.findIndex((tab) => location.pathname.startsWith(tab.path));
+    const { hasReadAccess } = usePermissions();
+    const isReportConfigurationEnabled = hasReadAccess('WorkflowAdministration');
+
+    const tabs = [
+        ...(isReportConfigurationEnabled
+            ? [
+                  {
+                      id: 'report-configuration',
+                      title: 'Report configurations',
+                      path: vulnerabilityConfigurationReportsPath,
+                      content: <ConfigReportsTab />,
+                  },
+              ]
+            : []),
+        {
+            id: 'view-based-reports',
+            title: 'View-based reports',
+            path: vulnerabilityViewBasedReportsPath,
+            content: <ViewBasedReportsTab />,
+        },
+    ];
+
+    const tabIndexFound = tabs.findIndex((tab) => location.pathname.startsWith(tab.path));
+    const activeTabIndex = tabIndexFound >= 0 ? tabIndexFound : 0;
 
     const onTabSelect = (_event, tabIndex) => {
         navigate(tabs[tabIndex].path);
@@ -49,12 +63,12 @@ function VulnReportingLayout() {
                             key={tab.id}
                             eventKey={index}
                             title={tab.title}
-                            tabContentId={`${tab.id}-tab-content`}
+                            tabContentId={tab.id}
                         />
                     ))}
                 </Tabs>
             </PageSection>
-            <Outlet />
+            <TabContent id={tabs[activeTabIndex].id}>{tabs[activeTabIndex].content}</TabContent>
         </>
     );
 }
