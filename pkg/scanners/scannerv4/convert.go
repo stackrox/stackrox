a8e3b048af8954b13f80107426aa98fb617d5a41
David Caravello <119438707+dcaravel@users.noreply.github.com>
2026-08-17 17:26:09 -0500

    populate origin field

--- pkg/scanners/scannerv4/convert.go
+++ pkg/scanners/scannerv4/convert.go
@@ -295,6 +295,7 @@ func buildEmbeddedVulnerability(ccVuln *v4.VulnerabilityReport_Vulnerability, en
 		CisaKev:               cisaKevEnabled(ccVuln),
 		FixAvailableTimestamp: ccVuln.GetFixedDate(),
 		Datasource:            vulnDataSource(ccVuln, envOS),
+		Origin:                vulnOrigin(ccVuln.GetUpdater()),
 	}
 	if err := setScoresAndScoreVersions(vuln, ccVuln.GetCvssMetrics()); err != nil {
 		utils.Should(err)
@@ -867,4 +868,33 @@ func mergeScoringFields(dst, src *storage.EmbeddedVulnerability) {
 	dst.Epss = src.GetEpss()
 	dst.Exploit = src.GetExploit()
 	dst.CisaKev = src.GetExploit() != nil
+	dst.Origin = src.GetOrigin()
+}
+
+var updaterOrigins = []struct {
+	prefix string
+	origin storage.VulnOrigin
+}{
+	{"alpine-", storage.VulnOrigin_VULN_ORIGIN_ALPINE},
+	{"aws-", storage.VulnOrigin_VULN_ORIGIN_AMAZON},
+	{"debian/", storage.VulnOrigin_VULN_ORIGIN_DEBIAN},
+	{"oracle-", storage.VulnOrigin_VULN_ORIGIN_ORACLE},
+	{"osv/", storage.VulnOrigin_VULN_ORIGIN_OSV},
+	{"photon-", storage.VulnOrigin_VULN_ORIGIN_PHOTON},
+	{"rhel-", storage.VulnOrigin_VULN_ORIGIN_RED_HAT},
+	{"suse-", storage.VulnOrigin_VULN_ORIGIN_SUSE},
+	{"ubuntu/", storage.VulnOrigin_VULN_ORIGIN_UBUNTU},
+}
+
+// vulnOrigin translates a Claircore updater name into VulnOrigin.
+//
+// TODO(ROX-36340): Replace this prefix based translation with something
+// more stable.
+func vulnOrigin(updater string) storage.VulnOrigin {
+	for _, e := range updaterOrigins {
+		if strings.HasPrefix(updater, e.prefix) {
+			return e.origin
+		}
+	}
+	return storage.VulnOrigin_VULN_ORIGIN_OTHER
 }
