cadc326f5b6c3efa9b28a0238edd6788960cc0c1
Yann Brillouet <ybrillou@redhat.com>
2026-08-18 11:50:42 +0200

    ROX-33973: Create/plug OIDC AuthProvider with role resolution from ACM

--- pkg/auth/authproviders/oidc/backend_factory_impl.go
+++ pkg/auth/authproviders/oidc/backend_factory_impl.go
@@ -20,6 +20,10 @@ const (
 	// TypeName is the standard type name for OIDC auth providers.
 	TypeName = "oidc"
 
+	// TypeNameWithOPPAccessControl is the type name for OpenShift auth providers
+	// with role resolution from ACM.
+	TypeNameWithOPPAccessControl = authproviders.OIDCTypeNameWithACMRoles
+
 	callbackRelativePath = "callback"
 
 	nonceTTL     = 1 * time.Minute
