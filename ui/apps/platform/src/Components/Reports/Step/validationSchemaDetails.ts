3a82d01a46b68b2766be3723f75cb37393928f7d
Mark Pedrotti <mark@stackrox.com>
2026-08-21 14:18:19 -0400

    Do TODO that I overlooked

--- ui/apps/platform/src/Components/Reports/Step/validationSchemaDetails.ts
+++ ui/apps/platform/src/Components/Reports/Step/validationSchemaDetails.ts
@@ -1,6 +1,6 @@
 import * as yup from 'yup';
 
-import type { DetailsType } from 'Components/Reports/reports.types'; // TODO ./
+import type { DetailsType } from '../reports.types';
 
 export const validationSchemaDetails: yup.ObjectSchema<DetailsType> = yup.object().shape({
     name: yup.string().trim().required('Report name is required'),
