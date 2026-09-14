dc6aefb79be043aebd48b9f533e93f7dcc587cb6
Misha Sugakov <msugakov@redhat.com>
2026-09-14 19:06:19 +0200

    Remove remaining mentions of ScannerImage/ScannerDBImage
    as well as the functions themselves now that nothing else references
    them.

--- pkg/renderer/kubernetes.go
+++ pkg/renderer/kubernetes.go
@@ -55,12 +55,6 @@ func postProcessConfig(c *Config, mode mode, imageFlavor defaults.ImageFlavor) e
 	if c.K8sConfig.CentralDBImage == "" {
 		c.K8sConfig.CentralDBImage = imageFlavor.CentralDBImage()
 	}
-	if c.K8sConfig.ScannerImage == "" {
-		c.K8sConfig.ScannerImage = imageFlavor.ScannerImage()
-	}
-	if c.K8sConfig.ScannerDBImage == "" {
-		c.K8sConfig.ScannerDBImage = imageFlavor.ScannerDBImage()
-	}
 	if c.K8sConfig.ScannerV4Image == "" {
 		c.K8sConfig.ScannerV4Image = imageFlavor.ScannerV4Image()
 	}
