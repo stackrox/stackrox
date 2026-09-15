0af3679b69f356e7b3b936673e2b36c5ab18cd83
Guzman <guz@redhat.com>
2026-09-15 13:59:52 +0200

    Improve comment

--- sensor/kubernetes/complianceoperator/types.go
+++ sensor/kubernetes/complianceoperator/types.go
@@ -16,6 +16,8 @@ const (
 // supported; conversely, old Sensor builds that predate the node_roles field ignore
 // it via normal protobuf forward-compatibility and fall back to this same default.
 // See the node_roles comment in proto/internalapi/central/compliance_operator.proto.
+// Keep in sync with central/complianceoperator/v2/scanconfigurations/service/convert.go
+// and the UI default in ui/.../Schedules/compliance.scanConfigs.utils.tsx.
 func defaultNodeRoles() []string {
 	return []string{"master", "worker"}
 }
