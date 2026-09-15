765713304680084051285890f1617b5f971f1157
Guzman <guz@redhat.com>
2026-09-15 13:40:29 +0200

    Remove unneeded refactor

--- central/complianceoperator/v2/compliancemanager/utils.go
+++ central/complianceoperator/v2/compliancemanager/utils.go
@@ -3,13 +3,6 @@ package compliancemanager
 import "github.com/stackrox/rox/generated/internalapi/central"
 
 func buildScanConfigSensorMsg(msgID string, cron string, profiles []string, profileRefs []*central.ApplyComplianceScanConfigRequest_BaseScanSettings_ProfileReference, configName string, createConfig bool, nodeRoles []string) *central.MsgToSensor {
-	baseScanSettings := &central.ApplyComplianceScanConfigRequest_BaseScanSettings{
-		ScanName:       configName,
-		StrictNodeScan: true,
-		Profiles:       profiles,
-		ProfileRefs:    profileRefs,
-		NodeRoles:      nodeRoles,
-	}
 
 	if createConfig {
 		return &central.MsgToSensor{
@@ -20,8 +13,14 @@ func buildScanConfigSensorMsg(msgID string, cron string, profiles []string, prof
 							Id: msgID,
 							ScanRequest: &central.ApplyComplianceScanConfigRequest_ScheduledScan_{
 								ScheduledScan: &central.ApplyComplianceScanConfigRequest_ScheduledScan{
-									ScanSettings: baseScanSettings,
-									Cron:         cron,
+									ScanSettings: &central.ApplyComplianceScanConfigRequest_BaseScanSettings{
+										ScanName:       configName,
+										StrictNodeScan: true,
+										Profiles:       profiles,
+										ProfileRefs:    profileRefs,
+										NodeRoles:      nodeRoles,
+									},
+									Cron: cron,
 								},
 							},
 						},
@@ -39,8 +38,14 @@ func buildScanConfigSensorMsg(msgID string, cron string, profiles []string, prof
 						Id: msgID,
 						ScanRequest: &central.ApplyComplianceScanConfigRequest_UpdateScan{
 							UpdateScan: &central.ApplyComplianceScanConfigRequest_UpdateScheduledScan{
-								ScanSettings: baseScanSettings,
-								Cron:         cron,
+								ScanSettings: &central.ApplyComplianceScanConfigRequest_BaseScanSettings{
+									ScanName:       configName,
+									StrictNodeScan: true,
+									Profiles:       profiles,
+									ProfileRefs:    profileRefs,
+									NodeRoles:      nodeRoles,
+								},
+								Cron: cron,
 							},
 						},
 					},
