1d5b92fcec8663d14368db222d315aea9851b10a
Dave Shrewsberry <dashrews@redhat.com>
2026-09-21 16:58:41 -0400

    fix snapshot filename

--- tests/e2e/lib.sh
+++ tests/e2e/lib.sh
@@ -1814,10 +1814,10 @@ restore_postgres_backup() {
     if is_CI; then
         setup_gcp
     fi
-    gsutil cp gs://stackrox-ci-upgrade-test-fixtures/upgrade-test-dbs/postgres_db_4_10.sql.zip .
+    gsutil cp gs://stackrox-ci-upgrade-test-fixtures/upgrade-test-dbs/postgres_db_4.10.sql.zip .
 
     roxctl -e "$API_ENDPOINT" --ca "" --insecure-skip-tls-verify \
-            central db restore --timeout 5m postgres_db_4_10.sql.zip
+            central db restore --timeout 5m postgres_db_4.10.sql.zip
 }
 
 update_public_config() {
