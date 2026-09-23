dbacd9470382f7efe98a300854700ca259720982
Misha Sugakov <msugakov@redhat.com>
2026-09-23 10:17:37 +0200

    Wait for `central-db` before `central`

--- deploy/common/k8sbased.sh
+++ deploy/common/k8sbased.sh
@@ -635,7 +635,9 @@ function launch_central {
     # On some systems there's a race condition when port-forward connects to central but its pod then gets deleted due
     # to ongoing modifications to the central deployment. This port-forward dies and the script hangs "Waiting for
     # Central to respond" until it times out. Waiting for rollout status should help not get into such situation.
+    # Central-db is waited first because it needs to be online before Central can start.
     rollout_wait_timeout="10m"
+    kubectl -n "${central_namespace}" rollout status deploy/central-db --timeout="${rollout_wait_timeout}"
     kubectl -n "${central_namespace}" rollout status deploy/central --timeout="${rollout_wait_timeout}"
 
     # if we have specified that we want to use a load balancer, then use that endpoint instead of localhost
